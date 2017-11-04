9b2662292945e22e98d2370547a29b8c40105bba
Ian Dees <ian.dees@gmail.com>
2017-11-03 21:34:32 -0500

    guessed wrong on working directory

--- ci/run_changed_spiders.sh
+++ ci/run_changed_spiders.sh
@@ -3,6 +3,6 @@
 echo $(pwd)
 for spider in $(git diff --name-only HEAD..$TRAVIS_BRANCH | grep 'locations/spiders')
 do
-    output=$(./run_one_spider.sh $spider)
+    output=$(./ci/run_one_spider.sh $spider)
     echo $output
 done
