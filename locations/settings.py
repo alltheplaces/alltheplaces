a93948bcf58114b33d304f30443033dcbf8a05fa
Ian Dees <ian.dees@gmail.com>
2017-11-17 11:39:55 -0600

    Disable HTTP caching by default

--- locations/settings.py
+++ locations/settings.py
@@ -91,7 +91,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+# HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
