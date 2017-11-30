f9a1ec9ae3309b86eba4179a4a2fbd48bd75ee57
Colin Casazza <Colanzio5@gmail.com>
2017-11-29 16:50:42 -0800

    Making changes to pull

--- locations/spiders/blue_bottle_cafe.py
+++ locations/spiders/blue_bottle_cafe.py
@@ -4,27 +4,32 @@ import json
 from locations.items import GeojsonPointItem
 
 class BlueBottleCafeSpider(scrapy.Spider):
+
     name = "bluebottlecafe"
     allowed_domains = ["www.bluebottlecoffee.com"]
     start_urls = (
         'https://bluebottlecoffee.com/api/cafe_search/fetch.json?coordinates=false&query=true&search_value=all',
     )
+
     def parse(self, response):
         results = json.loads(response.body_as_unicode())
-        for e in results["cafes"]:
-            for store_data in results["cafes"][e]:
+        for region_name in results["cafes"]:
+            for store_data in results["cafes"][region_name]:
 
-                add = store_data['address'].replace('\n', ' ').replace('\r', '').replace('<br>', ',')
+                address_string = store_data['address'].replace('\n', ' ').replace('\r', '').replace('<br>', ', ')
+                address_data = address_string.split(", ")
+
                 properties = {
                     'name': store_data['name'],
-                    'addr:full': add,
-                    'addr:city': store_data['region'],
+                    'addr:full': address_string,
+                    'addr:city': address_string.split(", ")[1].replace(" ", ""),
+                    'region': store_data['region'].title(),
                     'website': store_data['url'],
                     'ref': store_data['id'],
                 }
                 lon_lat = [
-                    float(store_data['longitude']),
-                    float(store_data['latitude']),
+                    store_data['longitude'],
+                    store_data['latitude'],
                 ]
 
                 yield GeojsonPointItem(
