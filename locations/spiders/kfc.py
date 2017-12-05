495e9680ae7c1b9c1071c9f840df7881f5d4934b
SelfnessAid <daxianjin59@hotmail.com>
2017-12-05 08:07:43 -0500

    add a Spider to KFC#15

---
+++ locations/spiders/kfc.py
@@ -0,0 +1,50 @@
+import json
+import re
+import scrapy
+from locations.items import GeojsonPointItem
+
+class KFCSpider(scrapy.Spider):
+    name = "kfc"
+    allowed_domains = ["www.kfc.com"]
+
+    def start_requests(self):
+        url = 'https://services.kfc.com/services/query/locations'
+
+        headers = {
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Origin': 'https://www.kfc.com',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Referer': 'https://www.kfc.com/store-locator?query=90210',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+        form_data = {
+            'address': '90210',
+            'distance': '100'
+        }
+
+        yield scrapy.http.FormRequest(
+            url=url, method='POST', formdata=form_data,
+            headers=headers, callback=self.parse
+        )
+
+    def parse(self, response):
+        data = json.loads(response.body_as_unicode())
+        stores = data['results']
+        print(stores)
+        for store in stores:
+            properties = {
+                'ref': store['entityID'],
+                'name': store['storeNumber'],
+                'addr_full': store['addressLine'],
+                'city': store['city'],
+                'state': store['state'],
+                'postcode': store['postalCode'],
+                'lat': store['latitude'],
+                'lon': store['longitude'],
+                'phone': store['businessPhone']
+            }
+
+            yield GeojsonPointItem(**properties)
+
