23850204bc380590aed7a4310f0b0ec4cbd71604
Smart Dev <dev@Smarts-Mac.local>
2017-12-06 14:01:59 -0800

    potbelly sandwich spider. fixes #124

--- locations/spiders/potbelly_sandwichshop.py
+++ locations/spiders/potbelly_sandwichshop.py
@@ -12,6 +12,23 @@ class PotbellySandwichSpider(scrapy.Spider):
         'https://api-origin.potbelly.com/proxy/v15/apps/1055/locations?lat=41.8781136&lng=-87.62979819999998&fulfillment_types=pickup&has_breakfast=false&in_delivery_area=false',
     )
 
+    def start_requests(self):
+
+        url = self.start_urls[0]
+
+        headers = {
+                   'Accept': 'application/json, text/plain, */*',
+                   'Accept-Language': 'en-US,en;q=0.9,ru;q=0.6',
+                   'Origin': 'https://www.potbelly.com',
+                   'Accept-Encoding': 'gzip, deflate, br',
+                   'Referer': 'https://www.potbelly.com/store-locator',
+                   'Connection': 'keep-alive',
+                   }
+
+        url = self.start_urls[0]
+
+        yield scrapy.Request(url=url, headers=headers, callback=self.parse)
+
     def parse(self, response):
         results = json.loads(response.body_as_unicode())
         for data in results:
@@ -25,7 +42,7 @@ class PotbellySandwichSpider(scrapy.Spider):
                 'state': data['location']['region'],
                 'postcode': data['location']['postal_code'],
                 'phone': data['location']['phone'],
-                'website': data['location']['pickup_menu_url'],
+                'website': data['location']['facebook_url'],
                 'opening_hours': data['location']['hours']
             }
 
