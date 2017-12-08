045ed559239a125899b15f3d23109d9615304d7e
vlad555 <vladsimple@gmail.com>
2017-12-08 02:50:49 -0800

    adding time

--- locations/spiders/mcmenamins.py
+++ locations/spiders/mcmenamins.py
@@ -5,6 +5,16 @@ import re
 
 from locations.items import GeojsonPointItem
 
+DAYS={
+    'Monday':'Mo',
+    'Tuesday':'Tu',
+    'Wednesday':'We',
+    'Friday':'Fr',
+    'Thursday':'Th',
+    'Saturday':'Sa',
+    'Sunday':'Su',
+}
+
 class McmenaminsSpider(scrapy.Spider):
     name = "mcmenamins"
     allowed_domains = ["mcmenamins.com","google.com"]
@@ -13,55 +23,40 @@ class McmenaminsSpider(scrapy.Spider):
     )
 
     def store_hours(self, store_hours):
-        day_groups = []
-        this_day_group = None
+        result=''
         for line in store_hours:
-            day = line['day'][0].upper()+line['day'][1].lower()
+            sl=re.search(r'(\w+)\s*(-|&|–)?\s*(\w+)?,\s*(\d+):?(\d+)?\s*((a\.m\.)|(p\.m\.)|noon|midnight)\s*\'til\s*(\d+):?(\d+)?\s*((a\.m\.)|(p\.m\.)|noon|midnight)',line)
 
-            if not line['intervals']:
+            if not sl:
                 continue
+            result+=DAYS[sl[1]]+(('-'+DAYS[sl[3]]) if sl[3] else '')+' '
+            i=int(sl[4])
 
-            for period in line['intervals']:
-                hours = '{0}:{1}-{2}:{3}'.format(
-                    str(period['start'])[:2],
-                    str(period['start'])[2:],
-                    str(period['end'])[:2],
-                    str(period['end'])[2:],
-                )
+            if sl[6]=='a.m.' or sl[6]=='noon':
+                result+='{:02d}'.format(int(sl[4]))
+            else:
+                result+='{:02d}'.format(i+12)
 
-            if not this_day_group:
-                this_day_group = {
-                    'from_day': day,
-                    'to_day': day,
-                    'hours': hours
-                }
-            elif this_day_group['hours'] != hours:
-                day_groups.append(this_day_group)
-                this_day_group = {
-                    'from_day': day,
-                    'to_day': day,
-                    'hours': hours
-                }
-            elif this_day_group['hours'] == hours:
-                this_day_group['to_day'] = day
+            if sl[5]:
+                result+=':'+sl[5]+'-'
+            else:
+                result+=':00'+'-'
 
-        if this_day_group:
-            day_groups.append(this_day_group)
+            i=int(sl[9])
 
-        opening_hours = ""
-        if len(day_groups) == 1 and day_groups[0]['hours'] in ('00:00-23:59', '00:00-00:00'):
-            opening_hours = '24/7'
-        else:
-            for day_group in day_groups:
-                if day_group['from_day'] == day_group['to_day']:
-                    opening_hours += '{from_day} {hours}; '.format(**day_group)
-                elif day_group['from_day'] == 'Mo' and day_group['to_day'] == 'Su':
-                    opening_hours += '{hours}; '.format(**day_group)
-                else:
-                    opening_hours += '{from_day}-{to_day} {hours}; '.format(**day_group)
-            opening_hours = opening_hours[:-2]
+            if sl[11]=='midnight':
+                result+='00'
+            elif sl[9]=='a.m.' or sl[6]=='noon':
+                result+='{:02d}'.format(int(sl[9]))
+            else:
+                result+='{:02d}'.format(i+12)
 
-        return opening_hours
+            if sl[10]:
+                result+=':'+sl[10]+'; '
+            else:
+                result+=':00'+'; '
+
+        return result.rstrip('; ')
 
     def phone_normalize(self, phone):
         r=re.search(r'\+?(\s+)*(\d{1})?(\s|\()*(\d{3})(\s+|\))*(\d{3})(\s+|-)?(\d{2})(\s+|-)?(\d{2})',phone)
@@ -71,9 +66,10 @@ class McmenaminsSpider(scrapy.Spider):
         shops = response.xpath('//div[@id="MainContent_eatDrinkLocations"]/div[contains(@class,"all")]')
         i=1
         for path in shops:
-            yield scrapy.Request(response.urljoin(path.xpath('.//div/div[@class="tm-panel-titlebg"]/a/@href').extract_first()), callback=self.parse_store, meta={
-                   'ref':path.xpath('.//@id').extract_first(),
-                    })
+            if i<4:
+                yield scrapy.Request(response.urljoin(path.xpath('.//div/div[@class="tm-panel-titlebg"]/a/@href').extract_first()), callback=self.parse_store, meta={
+                       'ref':path.xpath('.//@id').extract_first(),
+                        })
 
     def parse_store(self, response):
         google_pos=response.xpath('//div[@class="mcm-logo-address"]')[0].xpath('.//a[contains(@href,"maps.google")]/@href')[0].extract()
@@ -87,6 +83,7 @@ class McmenaminsSpider(scrapy.Spider):
                 'state':address_parts[3].strip(),
                 'postcode':address_parts[4].strip(),
                 'phone':self.phone_normalize(response.xpath('//div[@class="mcm-logo-address"]')[0].xpath('.//ul/li/a/@href').extract_first()),
+                'open_hours':self.store_hours(response.xpath('//div[@id="MainContent_hoursText"]/p/text()').extract()),
                 })
 
     def google_maps(self, response):
@@ -98,7 +95,7 @@ class McmenaminsSpider(scrapy.Spider):
             phone=response.meta.get('phone'),
             website=response.meta.get('website'),
             ref=response.meta.get('ref'),
-            opening_hours='',
+            opening_hours=response.meta.get('open_hours'),
             addr_full=response.meta.get('addr_full'),
             city=response.meta.get('city'),
             state=response.meta.get('state'),
