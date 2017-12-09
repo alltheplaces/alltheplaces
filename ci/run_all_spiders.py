e7999bd8afa05854aac25cc5f16fd8555031aa5b
Ian Dees <ian.dees@gmail.com>
2017-12-09 13:19:49 -0600

    Print some stats in the crawler

--- ci/run_all_spiders.py
+++ ci/run_all_spiders.py
@@ -1,5 +1,6 @@
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
+from scrapy import signals
 
 
 if __name__ == '__main__':
@@ -12,7 +13,22 @@ if __name__ == '__main__':
     settings.set('FEED_FORMAT', 'ndgeojson')
     settings.get('ITEM_PIPELINES')['locations.pipelines.ApplySpiderNamePipeline'] = 100
 
+    def spider_opened(spider):
+        print("Spider %s opened" % spider.name)
+
+    def spider_closed(spider):
+        print("Spider %s closed (%s) after %0.1f sec, %d items" % (
+            spider.name,
+            spider.crawler.stats.get_value('finish_reason'),
+            (spider.crawler.stats.get_value('finish_time') -
+                spider.crawler.stats.get_value('start_time')).total_seconds(),
+            spider.crawler.stats.get_value('item_scraped_count'),
+        ))
+
     process = CrawlerProcess(settings)
     for spider_name in process.spider_loader.list():
-        process.crawl(spider_name)
+        crawler = process.create_crawler(spider_name)
+        crawler.signals.connect(spider_closed, signals.spider_closed)
+        crawler.signals.connect(spider_opened, signals.spider_opened)
+        process.crawl(crawler)
     process.start()
