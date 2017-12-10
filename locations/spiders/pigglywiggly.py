3c5acb2000da6d19448c1ca2bdd1955495ff7c32
Austen Holmberg <aholm2@pdx.edu>
2017-12-10 15:45:44 -0800

    Added scraper for WI locations

--- locations/spiders/pigglywiggly.py
+++ locations/spiders/pigglywiggly.py
@@ -6,20 +6,15 @@ import logging
 
 from locations.items import GeojsonPointItem
 
-# Currently, this spider only gathers location data from their non - wisconsin and illinois stores.
-# Gathering data from those locations involves sending a request to an api that is specifically
-# for their stores in those two states.
 class PigglyWigglySpider(scrapy.Spider):
+    ''' This spider scrapes from two different places, an api which has their stores in Wisconsin
+        and Illinois, and a page which has all of their other stores. Cookies are used for the
+        api request.
+    '''
     name = "pigglywiggly"
     allowed_domains = ["pigglywiggly.com"]
-    
-    start_urls = (
-        'https://www.pigglywiggly.com/store-locations',
-    )
 
-    # NOTE: this buildRequest function (and parse_wi) don't work, their api returns 401.
-    # If they worked, these functions would gather location data from their wisconsin and illinois stores.
-    def buildRequest(self):
+    def start_requests(self):
         url = 'https://www.shopthepig.com/api/m_store_location'
         headers = {
             'x-newrelic-id': 'XQYBWFVVGwAEVFNRBQcP',
@@ -29,19 +24,45 @@ class PigglyWigglySpider(scrapy.Spider):
             'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
             'accept': 'application/json, text/plain, */*',
             'referer': 'https://www.shopthepig.com/stores',
-            'authority': 'www.shopthepig.com',
-            'cookie': '__cfduid=d5633fa71f6dd159d419cbb684774a76d1512714801; SESSb159e7a0d4a6fad9ba3abc7fadef99ec=h3o7xcjnfcERSRrqJVh0soQdUI5IFIBDIQlytOZkhIU; XSRF-TOKEN=eF2m10r8n51nsRgBSv1xSvhAGtCo8E84BExlmn54Vvc;',
+        }
+        cookies = {
+            '__cfduid': 'db0a53231376d78a40dd7fd728fa896f51512948321',
+            'SESSb159e7a0d4a6fad9ba3abc7fadef99ec': 'h3o7xcjnfcERSRrqJVh0soQdUI5IFIBDIQlytOZkhIU',
+            'XSRF-TOKEN': 'eF2m10r8n51nsRgBSv1xSvhAGtCo8E84BExlmn54Vvc',
+            'has_js': 1,
         }
 
         yield scrapy.http.FormRequest(
-            url=url, method='GET',
-            headers=headers, callback=self.parse_wi
+            url=url, headers=headers, callback=self.parse_wi, cookies=cookies
+        )
+
+        yield scrapy.Request(
+            'https://www.pigglywiggly.com/store-locations',
+            callback=self.parse_nonwi,
         )
 
     def parse_wi(self, response):
-        logging.info(response.body_as_unicode())
+        data = json.loads(response.body_as_unicode())
+        stores = data['stores']
+        for store in stores:
+            unp = {
+                'ref': store['storeID'],
+                'name': store['storeName'],
+                'addr_full': store['normalized_address'],
+                'city': store['normalized_city'],
+                'state': store['normalized_state'],
+                'postcode': store['normalized_zip'],
+                'lat': store['latitude'],
+                'lon': store['longitude'],
+                'phone': store['phone']
+            }
+            properties = {}
+            for key in unp:
+                if unp[key]: properties[key] = unp[key]
 
-    def parse(self, response):
+            yield GeojsonPointItem(**properties)
+
+    def parse_nonwi(self, response):
         for state_url in response.xpath('//div[@class="views-field-province-1"]/span[@class="field-content"]/a/@href').extract():
             yield scrapy.Request(
                 response.urljoin(state_url),
@@ -75,4 +96,3 @@ class PigglyWigglySpider(scrapy.Spider):
             properties['ref'] = ref
 
             yield GeojsonPointItem(**properties)
-
