61fd0bd3d3a39f19c60fd723b8864044b606b7b6
whoissteven <34196991+whoissteven@users.noreply.github.com>
2017-12-10 03:05:08 -0500

    Add files via upload
    Added validation of location page by checking if error 500 is found

--- locations/spiders/amctheatres.py
+++ locations/spiders/amctheatres.py
@@ -23,16 +23,19 @@ class AmcTheatresSpider(scrapy.Spider):
             )
 
     def parse_store(self, response):
-        properties = {
-            'name': response.xpath('/html/body/div/div/main/div/section/div/div/div/div/div/div/h1/text()').extract_first(),
-            'website': response.request.url,
-            'ref': response.xpath('/html/body/div/div/main/div/section/div/div/div/div/div/div/h1/text()').extract_first(),
-            'addr_full': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"addressLine1":"')[1].split('"')[0],
-            'city': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"city":"')[1].split('"')[0],
-            'state': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"state":"')[1].split('"')[0],
-            'postcode': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"postalCode":"')[1].split('"')[0],
-             'lat': float(response.xpath('/html/head/meta[17][@content]').extract_first().split('content="')[1].split('"')[0]),
-             'lon': float(response.xpath('/html/head/meta[18][@content]').extract_first().split('content="')[1].split('"')[0]),
-        }
-        
-        yield GeojsonPointItem(**properties)
+        if response.xpath('/html/body/div/div/main/section/div/div/div/div/div/div/h4/text()').extract_first() == "Error 500":
+            pass
+        else:
+            properties = {
+                'name': response.xpath('/html/body/div/div/main/div/section/div/div/div/div/div/div/h1/text()').extract_first(),
+                'website': response.request.url,
+                'ref': response.xpath('/html/body/div/div/main/div/section/div/div/div/div/div/div/h1/text()').extract_first(),
+                'addr_full': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"addressLine1":"')[1].split('"')[0],
+                'city': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"city":"')[1].split('"')[0],
+                'state': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"state":"')[1].split('"')[0],
+                'postcode': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"postalCode":"')[1].split('"')[0],
+                 'lat': float(response.xpath('/html/head/meta[17][@content]').extract_first().split('content="')[1].split('"')[0]),
+                 'lon': float(response.xpath('/html/head/meta[18][@content]').extract_first().split('content="')[1].split('"')[0]),
+            }
+
+            yield GeojsonPointItem(**properties)
