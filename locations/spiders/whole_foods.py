6cc587c5364433de31e1c3ccae298990a21dcc42
AnthoneyKalasho <anthoneykalasho@gmail.com>
2017-12-10 02:25:59 -0800

    Fixed error with regex

--- locations/spiders/whole_foods.py
+++ locations/spiders/whole_foods.py
@@ -14,30 +14,47 @@ class WholeFoodsSpider(scrapy.Spider):
         'https://www.wholefoodsmarket.com/stores/list/365',
     )
 
+    def parse_phone(self, phone):
+        phone = phone.replace('.','')
+        phone = phone.replace(')','')
+        phone = phone.replace('(','')
+        phone = phone.replace('_','')
+        phone = phone.replace('-','')
+        phone = phone.replace('+','')
+        phone = phone.replace(' ','')
+        phone = phone.replace('','')
+        return phone
+
+
+
     def parse(self, response):
-      if("?page=" in response.url):
-        for match in response.xpath("//div[contains(@class,'views-field-title')]/parent::div/parent::div"):
-          yield GeojsonPointItem(
-            ref=string.capwords(match.xpath(".//h4[contains(@class,'field-content')]/a/text()").extract_first()),
-            addr_full=match.xpath(".//div[contains(@class,'thoroughfare')]/text()").extract_first(),
-            city=match.xpath(".//span[contains(@class,'locality')]/text()").extract_first(),
-            state=match.xpath(".//span[contains(@class,'state')]/text()").extract_first(),
-            postcode=match.xpath(".//span[contains(@class,'postal-code')]/text()").extract_first(),
-            phone=re.sub('[^0-9]','', match.xpath(".//div[contains(@class,'storefront-phone')]/span[contains(@class,'strong')]/following-sibling::text()").extract_first()),
-            website= "https://www.wholefoodsmarket.com" + match.xpath(".//h4[contains(@class,'field-content')]/a/@href").extract_first(),
-            country=match.xpath(".//span[contains(@class,'country')]/text()").extract_first(),
-          )
-        yield scrapy.Request("https://www.wholefoodsmarket.com" + response.xpath("//li[contains(@class,'pager-next')]/a/@href").extract_first(),callback=self.parse)
-      else:
-        for match in response.xpath("//div[contains(@class,'views-field-title')]/parent::div"):
-          yield GeojsonPointItem(
-            ref=string.capwords(match.xpath(".//h4[contains(@class,'field-content')]/a/text()").extract_first()),
-            addr_full=match.xpath(".//div[contains(@class,'thoroughfare')]/text()").extract_first(),
-            city=match.xpath(".//span[contains(@class,'locality')]/text()").extract_first(),
-            state=match.xpath(".//div[contains(@class,'state')]/text()").extract_first(),
-            postcode=match.xpath(".//div[contains(@class,'postal-code')]/text()").extract_first(),
-            phone=re.sub('[^0-9]','', match.xpath(".//div[contains(@class,'phone-number')]/div/text()").extract_first()),
-            website= "https://www.wholefoodsmarket.com" + match.xpath(".//h4[contains(@class,'field-content')]/a/@href").extract_first(),
-            country=match.xpath(".//span[contains(@class,'country')]/text()").extract_first(),
-          )
+        if("?page=" in response.url):
+            for match in response.xpath("//div[contains(@class,'views-field-title')]/parent::div/parent::div"):
+                yield GeojsonPointItem(
+                    ref=string.capwords(match.xpath(".//h4[contains(@class,'field-content')]/a/text()").extract_first()),
+                    addr_full=match.xpath(".//div[contains(@class,'thoroughfare')]/text()").extract_first(),
+                    city=match.xpath(".//span[contains(@class,'locality')]/text()").extract_first(),
+                    state=match.xpath(".//span[contains(@class,'state')]/text()").extract_first(),
+                    postcode=match.xpath(".//span[contains(@class,'postal-code')]/text()").extract_first(),
+                    phone=self.parse_phone(match.xpath(".//div[contains(@class,'storefront-phone')]/span[contains(@class,'strong')]/following-sibling::text()").extract_first().strip()),
+                    website= "https://www.wholefoodsmarket.com" + match.xpath(".//h4[contains(@class,'field-content')]/a/@href").extract_first(),
+                    country=match.xpath(".//span[contains(@class,'country')]/text()").extract_first(),
+                )
+                nextUrl = response.xpath("//li[contains(@class,'pager-next')]/a/@href").extract_first()
+                if (response.xpath("//li[contains(@class,'pager-next')]/a/@href").extract_first() is None):
+                    continue
+                else:
+                    yield scrapy.Request("https://www.wholefoodsmarket.com" + nextUrl,callback=self.parse)
+        else:
+            for match in response.xpath("//div[contains(@class,'views-field-title')]/parent::div"):
+                yield GeojsonPointItem(
+                    ref=string.capwords(match.xpath(".//h4[contains(@class,'field-content')]/a/text()").extract_first()),
+                    addr_full=match.xpath(".//div[contains(@class,'thoroughfare')]/text()").extract_first(),
+                    city=match.xpath(".//span[contains(@class,'locality')]/text()").extract_first(),
+                    state=match.xpath(".//div[contains(@class,'state')]/text()").extract_first(),
+                    postcode=match.xpath(".//div[contains(@class,'postal-code')]/text()").extract_first(),
+                    phone=self.parse_phone(match.xpath(".//div[contains(@class,'phone-number')]/div/text()").extract_first().strip()),
+                    website= "https://www.wholefoodsmarket.com" + match.xpath(".//h4[contains(@class,'field-content')]/a/@href").extract_first(),
+                    country=match.xpath(".//span[contains(@class,'country')]/text()").extract_first(),
+                )
 
