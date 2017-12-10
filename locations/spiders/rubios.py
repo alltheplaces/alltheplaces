f720025a731aafc1d8aea8927421f3b787681820
whoissteven <34196991+whoissteven@users.noreply.github.com>
2017-12-10 04:55:12 -0500

    Add files via upload
    Added if statement that if store hours not found use list w/o store_hours.

--- locations/spiders/rubios.py
+++ locations/spiders/rubios.py
@@ -35,7 +35,35 @@ class RubiosSpider(scrapy.Spider):
             'state': response.css('.store-info').extract_first().replace('\t','').split('itemprop="addressRegion">')[1].split('</span>')[0],
             'postcode': response.css('.store-info').extract_first().replace('\t','').split('itemprop="postalCode">')[1].split('</span>')[0],
             'phone': response.css('.store-info').extract_first().replace('\t','').split('"tel:')[1].split('"')[0],
-            'opening_hours': response.css('.store-info').extract_first().replace('\t','').split('<span class="oh-display-label" style="width: 6.6em;">')[1].split('<br></span>')[0].replace('</span><span class="oh-display-times oh-display-hours">','').strip(),
+            'website': response.request.url,
+            'store_hours': response.css('.store-info').extract_first().replace('\t', '').split('<span class="oh-display-label" style="width: 6.6em;">')[1].split('<br></span>')[0].replace('</span><span class="oh-display-times oh-display-hours">', '').strip(),
+            'lon': float(response.xpath('//head/script[9]').extract_first().split('"lon":')[1].split('}')[0]),
+            'lat': float(response.xpath('//head/script[9]').extract_first().split('"lat":')[1].split(',')[0]),
+        }
+        store_hours = response.css('.store-info').extract_first().replace('\t', '').split('<span class="oh-display-label" style="width: 6.6em;">')[1].split('<br></span>')[0].replace('</span><span class="oh-display-times oh-display-hours">', '').strip()
+        if store_hours is not None or '':
+            properties = {
+            'name': response.css('.store-info').extract_first().replace('\t','').split('<span itemprop="name">')[1].split('</span>')[0],
+            'ref': response.css('.store-info').extract_first().replace('\t','').split('"addressLocality">')[1].split('</span>')[0],
+            'addr_full': response.css('.store-info').extract_first().replace('\t','').split('itemprop="streetAddress">')[1].split('</span>')[0],
+            'city': response.css('.store-info').extract_first().replace('\t','').split('"addressLocality">')[1].split('</span>')[0],
+            'state': response.css('.store-info').extract_first().replace('\t','').split('itemprop="addressRegion">')[1].split('</span>')[0],
+            'postcode': response.css('.store-info').extract_first().replace('\t','').split('itemprop="postalCode">')[1].split('</span>')[0],
+            'phone': response.css('.store-info').extract_first().replace('\t','').split('"tel:')[1].split('"')[0],
+            'website': response.request.url,
+            'store_hours': response.css('.store-info').extract_first().replace('\t', '').split('<span class="oh-display-label" style="width: 6.6em;">')[1].split('<br></span>')[0].replace('</span><span class="oh-display-times oh-display-hours">', '').strip(),
+            'lon': float(response.xpath('//head/script[9]').extract_first().split('"lon":')[1].split('}')[0]),
+            'lat': float(response.xpath('//head/script[9]').extract_first().split('"lat":')[1].split(',')[0]),
+        }
+        else:
+            properties = {
+            'name': response.css('.store-info').extract_first().replace('\t','').split('<span itemprop="name">')[1].split('</span>')[0],
+            'ref': response.css('.store-info').extract_first().replace('\t','').split('"addressLocality">')[1].split('</span>')[0],
+            'addr_full': response.css('.store-info').extract_first().replace('\t','').split('itemprop="streetAddress">')[1].split('</span>')[0],
+            'city': response.css('.store-info').extract_first().replace('\t','').split('"addressLocality">')[1].split('</span>')[0],
+            'state': response.css('.store-info').extract_first().replace('\t','').split('itemprop="addressRegion">')[1].split('</span>')[0],
+            'postcode': response.css('.store-info').extract_first().replace('\t','').split('itemprop="postalCode">')[1].split('</span>')[0],
+            'phone': response.css('.store-info').extract_first().replace('\t','').split('"tel:')[1].split('"')[0],
             'website': response.request.url,
             'lon': float(response.xpath('//head/script[9]').extract_first().split('"lon":')[1].split('}')[0]),
             'lat': float(response.xpath('//head/script[9]').extract_first().split('"lat":')[1].split(',')[0]),
