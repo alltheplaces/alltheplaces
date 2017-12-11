28d92cb230625634c107bc7172c9c1a5c0b9f49d
Ethan Beal-Brown <ethanbealbrown@gmail.com>
2017-12-10 18:21:31 -0800

    made changes Ian asked for, added scraping for lat and lon

--- locations/spiders/keyfood.py
+++ locations/spiders/keyfood.py
@@ -4,54 +4,49 @@ import re
 from locations.items import GeojsonPointItem
 from scrapy.selector import Selector
 
-import random
-
 class KeyfoodSpider(scrapy.Spider):
-	name = "keyfood"
-	allowed_domains = ["keyfood.mywebgrocer.com"]
-	start_urls = (
-		'http://keyfood.mywebgrocer.com/StoreLocator.aspx',
-	)
+    name = "keyfood"
+    allowed_domains = ["keyfood.mywebgrocer.com"]
+    start_urls = (
+        'http://keyfood.mywebgrocer.com/StoreLocator.aspx',
+    )
 
-	def parse(self, response):
+    def parse(self, response):
+        states = ['CT','NJ','NY','PA']
+        
+        for state in states:
+            yield scrapy.FormRequest.from_response(response=response,
+                                            formdata={'postBack':'1', 'action':'GL', 'stateSelIndex':'', 'citySelIndex':'', 'selStates':state, 'selCities':'', 'txtZipCode':'', 'selZipCodeRadius':'5' },
+                                            clickdata={'class': 'submitButton'},
+                                            dont_filter=True,
+                                            callback=self.parse_state)
 
-		# works ok for now, but should probably just populate with actual contents of dropdown, in case they add new states.
-		states = ['CT','NJ','NY','PA']
+    def parse_state(self,response):
+        sel = Selector(response)
+        stores = sel.css('div.StoreBox')
 
-		for state in states:
+        for store in stores:
+            name = store.css('.StoreTitle').xpath("text()").extract()
+            address = store.css('.StoreAddress p').xpath("text()").extract()
+            address1 = address[0]
+            address2 = address[len(address)-1].split(',')
+            store_hours = store.css('.StoreHours p.tInfo').xpath("text()").extract()
+            phone = store.css('.StoreContact p.tInfo').xpath("text()").extract()
+            latlon = str(store.css('a.SingleItemLinkText').extract())
+            latlon = (re.search('ll=(.*)0"', latlon)).group(1)
+            lat,lon = latlon.split(',')
 
-			yield scrapy.FormRequest.from_response(response=response,
-											formdata={'postBack':'1', 'action':'GL', 'stateSelIndex':'', 'citySelIndex':'', 'selStates':state, 'selCities':'', 'txtZipCode':'', 'selZipCodeRadius':'5' },
-											clickdata={'class': 'submitButton'},
-											dont_filter=True,
-											callback=self.parse_state)
-	
-	def parse_state(self,response):
-		sel = Selector(response)
-		stores = sel.css('div.StoreBox')
+            properties = {
+                'name': ''.join(name),
+                'ref': ''.join(name).replace('(', '').replace(')', '').replace(' ', '_'),
+                'street': address1,
+                'city': address2[0],
+                'state': address2[1].split(' ')[1],
+                'postcode': address2[1].split(' ')[2],
+                'opening_hours': store_hours,
+                'phone': ''.join(phone)[7:],
+                'lat': lat,
+                'lon': lon,
+            }
 
-		for store in stores:
-
-
-
-			name = store.css('.StoreTitle').xpath("text()").extract()
-			address = store.css('.StoreAddress p').xpath("text()").extract()
-			address1 = address[0]
-			address2 = address[len(address)-1].split(',')
-			store_hours = store.css('.StoreHours p.tInfo').xpath("text()").extract()
-			phone = store.css('.StoreContact p.tInfo').xpath("text()").extract()
-
-			properties = {
-				'name': ''.join(name),
-				'ref': ''.join(name).replace('(', '').replace(')', '').replace(' ', '_'),
-				'street': address1,
-				'city': address2[0],
-				'state': address2[1].split(' ')[1],
-				'postcode': address2[1].split(' ')[2],
-				'opening_hours': store_hours,
-				'phone': ''.join(phone)[7:]
-			}
-
-			# print(random.randint(1,1000000))
-
-			yield GeojsonPointItem(**properties)
+            yield GeojsonPointItem(**properties)
