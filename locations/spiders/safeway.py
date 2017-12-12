3fe5a0a31c60633480f517887b652ed8c4458d08
Ian Dees <ian.dees@gmail.com>
2017-12-12 09:27:12 -0600

    Clean up parsing, use opening hours

--- locations/spiders/safeway.py
+++ locations/spiders/safeway.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 import re
 
 from locations.items import GeojsonPointItem
@@ -11,6 +12,57 @@ class SafewaySpider(scrapy.Spider):
     start_urls = (
         'https://local.safeway.com/sitemap.xml',
     )
+
+    def store_hours(self, store_hours):
+        day_groups = []
+        this_day_group = None
+        for day_info in store_hours:
+            day = day_info['day'][:2].title()
+
+            hour_intervals = []
+            for interval in day_info['intervals']:
+                f_time = str(interval['start']).zfill(4)
+                t_time = str(interval['end']).zfill(4)
+                hour_intervals.append('{}:{}-{}:{}'.format(
+                    f_time[0:2],
+                    f_time[2:4],
+                    t_time[0:2],
+                    t_time[2:4],
+                ))
+            hours = ','.join(hour_intervals)
+
+            if not this_day_group:
+                this_day_group = {
+                    'from_day': day,
+                    'to_day': day,
+                    'hours': hours
+                }
+            elif this_day_group['hours'] != hours:
+                day_groups.append(this_day_group)
+                this_day_group = {
+                    'from_day': day,
+                    'to_day': day,
+                    'hours': hours
+                }
+            elif this_day_group['hours'] == hours:
+                this_day_group['to_day'] = day
+
+        day_groups.append(this_day_group)
+
+        opening_hours = ""
+        if len(day_groups) == 1 and day_groups[0]['hours'] in ('00:00-23:59', '00:00-00:00'):
+            opening_hours = '24/7'
+        else:
+            for day_group in day_groups:
+                if day_group['from_day'] == day_group['to_day']:
+                    opening_hours += '{from_day} {hours}; '.format(**day_group)
+                elif day_group['from_day'] == 'Su' and day_group['to_day'] == 'Sa':
+                    opening_hours += '{hours}; '.format(**day_group)
+                else:
+                    opening_hours += '{from_day}-{to_day} {hours}; '.format(**day_group)
+            opening_hours = opening_hours[:-2]
+
+        return opening_hours
 
     def parse(self, response):
         response.selector.remove_namespaces()
@@ -27,18 +79,21 @@ class SafewaySpider(scrapy.Spider):
                 )
 
     def parse_store(self, response):
-
         properties = {
-            'name': response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[1]/h1/span/span[2]/text()').extract_first(),
-            'website': response.request.url,
-            'ref': response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[1]/h1/span/span[2]/text()').extract_first(),
-            'opening_hours': " ".join(response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[2]/div[1]/div[2]/div[@data-days]').extract_first().split("data-days=\'")[1].split("\'")[0].replace(']','').replace('{','').replace('"day"','').replace('"intervals"','').replace(':','').replace('"',' ').replace(',','').replace('[','').replace('}','').replace('end','close').replace('start','open').split()),
-            'addr_full': response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[1]/div[1]/address/span[1]/span/text()').extract_first(),
-            'city': response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[1]/div[1]/address/span[2]/span[1]/text()').extract_first(),
-            'state': response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[1]/div[1]/address/abbr[1]/text()').extract_first(),
-            'postcode': response.xpath('/html/body/main/div[2]/div/section[1]/div[2]/div[1]/div[1]/address/span[3]/text()').extract_first(),
-             'lat': float(response.xpath('//*[@id="js-map-config-dir-map-desktop"]').extract_first().split('"latitude":')[1].split(',')[0]),
-             'lon': float(response.xpath('//*[@id="js-map-config-dir-map-desktop"]').extract_first().split('"longitude":')[1].split(',')[0]),
+            'name': response.xpath('//span[@class="LocationName-geo"]/text()').extract_first(),
+            'website': response.url,
+            'ref': response.url,
+            'addr_full': response.xpath('//span[@itemprop="streetAddress"]/span/text()').extract_first(),
+            'city': response.xpath('//span[@itemprop="addressLocality"]/text()').extract_first(),
+            'state': response.xpath('//abbr[@itemprop="addressRegion"]/text()').extract_first(),
+            'postcode': response.xpath('//span[@itemprop="postalCode"]/text()').extract_first().strip(),
+            'lat': float(response.xpath('//meta[@itemprop="latitude"]/@content').extract_first()),
+            'lon': float(response.xpath('//meta[@itemprop="longitude"]/@content').extract_first()),
         }
+
+        hours = json.loads(response.xpath('//div[@class="c-location-hours-details-wrapper js-location-hours"]/@data-days').extract_first())
+        opening_hours = self.store_hours(hours) if hours else None
+        if opening_hours:
+            properties['opening_hours'] = opening_hours
 
         yield GeojsonPointItem(**properties)
