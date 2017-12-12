139f9b4379578f9b4767eef56dac94f08063c688
Ian Dees <ian.dees@gmail.com>
2017-12-12 11:13:35 -0600

    Use just the sitemap

--- locations/spiders/amctheatres.py
+++ locations/spiders/amctheatres.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import re
-import json
 
 from locations.items import GeojsonPointItem
 
@@ -15,27 +13,18 @@ class AmcTheatresSpider(scrapy.Spider):
 
     def parse(self, response):
         response.selector.remove_namespaces()
-        city_urls = response.xpath('//url/loc/text()').extract()
-        for path in city_urls:
-            yield scrapy.Request(
-                path.strip(),
-                callback=self.parse_store,
-            )
-
-    def parse_store(self, response):
-        if response.xpath('/html/body/div/div/main/section/div/div/div/div/div/div/h4/text()').extract_first() == "Error 500":
-            pass
-        else:
+        theater_elems = response.xpath('//url')
+        for theater_elem in theater_elems:
             properties = {
-                'name': response.xpath('/html/body/div/div/main/div/section/div/div/div/div/div/div/h1/text()').extract_first(),
-                'website': response.request.url,
-                'ref': response.xpath('/html/body/div/div/main/div/section/div/div/div/div/div/div/h1/text()').extract_first(),
-                'addr_full': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"addressLine1":"')[1].split('"')[0],
-                'city': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"city":"')[1].split('"')[0],
-                'state': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"state":"')[1].split('"')[0],
-                'postcode': response.xpath('//*[@id="relay-data"]/text()').extract_first().split('"postalCode":"')[1].split('"')[0],
-                 'lat': float(response.xpath('/html/head/meta[17][@content]').extract_first().split('content="')[1].split('"')[0]),
-                 'lon': float(response.xpath('/html/head/meta[18][@content]').extract_first().split('content="')[1].split('"')[0]),
+                'website': theater_elem.xpath('.//loc/text()').extract_first(),
+                'name': theater_elem.xpath('.//Attribute[@name="title"]/text()').extract_first(),
+                'ref': theater_elem.xpath('.//Attribute[@name="theatreId"]/text()').extract_first(),
+                'addr_full': theater_elem.xpath('.//Attribute[@name="addressLine1"]/text()').extract_first(),
+                'city': theater_elem.xpath('.//Attribute[@name="city"]/text()').extract_first(),
+                'state': theater_elem.xpath('.//Attribute[@name="state"]/text()').extract_first(),
+                'postcode': theater_elem.xpath('.//Attribute[@name="postalCode"]/text()').extract_first(),
+                'lat': float(theater_elem.xpath('.//Attribute[@name="latitude"]/text()').extract_first()),
+                'lon': float(theater_elem.xpath('.//Attribute[@name="longitude"]/text()').extract_first()),
             }
 
             yield GeojsonPointItem(**properties)
