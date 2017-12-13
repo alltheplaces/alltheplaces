25d374dbbc386879da7496edd50167e5645c6eba
Ian Dees <ian.dees@gmail.com>
2017-12-13 07:14:32 -0600

    Correct formatting

--- locations/spiders/rubios.py
+++ locations/spiders/rubios.py
@@ -17,30 +17,26 @@ class RubiosSpider(scrapy.Spider):
         city_urls = response.xpath('//url/loc/text()').extract()
         regex = re.compile(r'http\S+rubios.com/store-locations/\S+/\S+/\S+')
         for path in city_urls:
-            if re.search(regex,path):
+            if re.search(regex, path):
                 yield scrapy.Request(
                     path.strip(),
                     callback=self.parse_store,
                 )
-            else:
-                pass
 
     def parse_store(self, response):
 
         properties = {
-        'name': response.xpath('//span[@itemprop="name"]/text()').extract_first(),
-        'ref': response.xpath('//span[@itemprop="name"]/text()').extract_first(),
-        'addr_full': response.xpath('//span[@itemprop="streetAddress"]/text()').extract_first(),
-        'city': response.xpath('//span[@itemprop="addressLocality"]/text()').extract_first(),
-        'state': response.xpath('//span[@itemprop="addressRegion"]/text()').extract_first(),
-        'postcode': response.xpath('//span[@itemprop="postalCode"]/text()').extract_first(),
-        'phone': response.xpath('//span[@itemprop="telephone"]/a/text()').extract_first(),
-        'website': response.request.url,
-        'opening_hours': "".join(response.xpath('//div/div/div/span/span/span/text()').extract()).strip(),
-        'lon': float(response.xpath('//head/script[9]').extract_first().split('"coordinates":[')[1].split(']')[0].split(',')[0]),
-        'lat': float(response.xpath('//head/script[9]').extract_first().split('"coordinates":[')[1].split(']')[0].split(',')[1]),
+            'name': response.xpath('//span[@itemprop="name"]/text()').extract_first(),
+            'ref': response.xpath('//span[@itemprop="name"]/text()').extract_first(),
+            'addr_full': response.xpath('//span[@itemprop="streetAddress"]/text()').extract_first(),
+            'city': response.xpath('//span[@itemprop="addressLocality"]/text()').extract_first(),
+            'state': response.xpath('//span[@itemprop="addressRegion"]/text()').extract_first(),
+            'postcode': response.xpath('//span[@itemprop="postalCode"]/text()').extract_first(),
+            'phone': response.xpath('//span[@itemprop="telephone"]/a/text()').extract_first(),
+            'website': response.url,
+            'opening_hours': "".join(response.xpath('//div/div/div/span/span/span/text()').extract()).strip(),
+            'lon': float(response.xpath('//head/script[9]').extract_first().split('"coordinates":[')[1].split(']')[0].split(',')[0]),
+            'lat': float(response.xpath('//head/script[9]').extract_first().split('"coordinates":[')[1].split(']')[0].split(',')[1]),
         }
-
-
 
         yield GeojsonPointItem(**properties)
