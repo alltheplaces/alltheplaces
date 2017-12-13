af651beec6bc2405a3e5e50b23a041a6f49a1cf7
Ethan Beal-Brown <ethanbealbrown@gmail.com>
2017-12-12 21:12:37 -0800

    not so pretty, but appears to be working

--- locations/spiders/keyfood.py
+++ locations/spiders/keyfood.py
@@ -4,6 +4,82 @@ import re
 from locations.items import GeojsonPointItem
 from scrapy.selector import Selector
 
+def opening_hours(datestring):
+        hour_match = re.findall(r"(\d{1,2}:\d{1,2}.(am|pm|-)|\d{1,2}(am|pm))", datestring)
+        hours = [str(i[0]) for i in hour_match]
+
+        try:
+            hours = [str(i[0]) for i in hour_match]
+            mil_hrs = list(hours)
+        except:
+            pass
+
+        if (mil_hrs):
+            for i in range(len(mil_hrs)):
+                hour = mil_hrs[i]            
+                hour = re.sub(r'[AaMm]{2}', '', hour.replace('-',''))
+
+                if re.search(r'[PpMm]{2}', hour) is not None:
+                    hour = re.sub(r'[PpMm]{2}', '', hour)
+                    hour = hour.replace(":",".")
+                    hour = '%.2f' % (float(hour)+12)
+                    hour = hour.replace('.',":")
+                
+                if (len(hour) == 1):
+                    hour = "0" + hour + ":00"
+                elif (len(hour) == 2):
+                    hour = hour + ":00"
+                elif (len(hour) == 4):
+                    hour = "0" + hour
+
+                mil_hrs[i] = hour
+
+            first = (datestring.split(hours[0])[0])
+            second = ""
+            third = ""
+            fourth = ""
+            opening_hours = ""
+
+            try:
+                regex = str('(?<=' + hours[1] + ')(.*?)(?=' + hours[2] + ')')
+                second = ((re.search(regex,datestring)).group(0)).lstrip()
+                regex = str('(?<=' + hours[3] + ')(.*?)(?=' + hours[4] + ')')
+                # print ("regex is " + regex)
+                third = ((re.search(regex,datestring)).group(0)).lstrip()
+                regex = str(hours[5] + '(?!.*' + hours[5] + ')(.*?)(?=' + hours[6] + ')')
+                fourth = ((re.search(regex,datestring)).group(1)).lstrip()
+                
+            except:
+                pass
+
+            for i in range(8):
+                try:
+                    mil_hrs[i]
+                except:
+                    mil_hrs.append("")
+
+            def fmt(period, first=0):
+                if period is not "":
+                    if not first:
+                        period = ", " + period
+                period = period.replace(" - ", "-").replace(" -", "-").replace(".", "").replace(':', ' ').replace("  ", " ")
+                return period
+
+            def hours(a,b):
+                if (a == "" or b == ""):
+                    return ("")
+                else:
+                    return (a + "-" + b)
+
+            strings = [fmt(first, 1), hours(mil_hrs[0], mil_hrs[1]), fmt(second), hours(mil_hrs[2], mil_hrs[3]), fmt(third), hours(mil_hrs[4], mil_hrs[5]), fmt(fourth), hours(mil_hrs[6], mil_hrs[7])]
+            opening_hours = ''.join(filter(None, strings))
+
+            day_dict = {'Mon':'Mo', 'Tue':'Tu', 'Wed':'We', 'Thu':'Th', 'Fri':'Fr', 'Sat':'Sa', 'Sun':'Su', 'Monday':'Mo', 'Tuesday':'Tu', 'Wednesday':'We', 'Thursday':'Th', 'Thurs':'Th', 'Thur':'Th', 'Friday':'Fr', 'Saturday':'Sa', 'Sunday':'Su', '24 hours/7 days a week':'24/7', 'Please contact store for hours':'N/A'}
+            pattern = re.compile(r'\b(' + '|'.join(day_dict.keys()) + r')\b')
+            opening_hours = pattern.sub(lambda x: day_dict[x.group()], ''.join(opening_hours))
+
+            return (opening_hours.title())
+
 class KeyfoodSpider(scrapy.Spider):
     name = "keyfood"
     allowed_domains = ["keyfood.mywebgrocer.com"]
@@ -13,7 +89,7 @@ class KeyfoodSpider(scrapy.Spider):
 
     def parse(self, response):
         states = ['CT','NJ','NY','PA']
-        
+
         for state in states:
             yield scrapy.FormRequest.from_response(response=response,
                                             formdata={'postBack':'1', 'action':'GL', 'stateSelIndex':'', 'citySelIndex':'', 'selStates':state, 'selCities':'', 'txtZipCode':'', 'selZipCodeRadius':'5' },
@@ -22,8 +98,7 @@ class KeyfoodSpider(scrapy.Spider):
                                             callback=self.parse_state)
 
     def parse_state(self,response):
-        sel = Selector(response)
-        stores = sel.css('div.StoreBox')
+        stores = response.css('div.StoreBox')
 
         for store in stores:
             name = store.css('.StoreTitle').xpath("text()").extract()
@@ -43,10 +118,10 @@ class KeyfoodSpider(scrapy.Spider):
                 'city': address2[0],
                 'state': address2[1].split(' ')[1],
                 'postcode': address2[1].split(' ')[2],
-                'opening_hours': store_hours,
+                'opening_hours': opening_hours(''.join(store_hours)),
                 'phone': ''.join(phone)[7:],
-                'lat': lat,
-                'lon': lon,
+                'lat': float(lat),
+                'lon': float(lon),
             }
 
             yield GeojsonPointItem(**properties)
