38877665f653ce09d1400c7c937efa0772373320
Colin Casazza <Colanzio5@gmail.com>
2017-12-14 18:10:20 -0800

    added spider for vea json endpoint - lat/lon data missing from endpoint

---
+++ locations/spiders/vea.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from locations.items import GeojsonPointItem
+
+
+class VeaSpider(scrapy.Spider):
+
+    name = "vea"
+    allowed_domains = ["www.supermercadosvea.com"]
+    start_urls = (
+        'http://www.supermercadosvea.com.ar/sucursales-obtener.html',
+    )
+
+    def parse(self, response):
+
+        store_list = json.loads(response.body_as_unicode())
+        for store in store_list:
+            properties = {
+                'name': store['descripcion'],
+                'addr_full': store['direccion'],
+                'city': store['vea_localidades_desc'],
+                'opening_hours': store['horarios'],
+                'phone': store['telefonos'],
+                'ref': str(store['codigo']).replace('SM ', ''),
+            }
+            yield GeojsonPointItem(**properties)
