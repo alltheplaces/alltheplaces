7f80ffa8134b836fb78eb63a3936637c352d6374
josh0203e <josh0203be@hotmail.com>
2017-12-15 18:49:00 -0800

    fix the problem of coopfood

--- locations/spiders/coopfood.py
+++ locations/spiders/coopfood.py
@@ -6,24 +6,14 @@ class CoopFoodSpider(scrapy.Spider):
     name = "coopfood"
     allowed_domains = ["api.coop.co.uk"]
     download_delay = 0.5
-    state = True
-    def start_requests(self):
-        for page in range(1000):
-            if(self.state==False):
-                break
-            else:
-                url = 'https://api.coop.co.uk/locationservices/finder/food/?location=54.9966124%2C-7.308574799999974&distance=30000000000&always_one=true&format=json&page='+str(page)
-                yield scrapy.Request(
-                    url=url,
-                    callback=self.parse
-                )
+    page_number =1
+    start_urls = (
+        'https://api.coop.co.uk/locationservices/finder/food/?location=54.9966124%2C-7.308574799999974&distance=30000000000&always_one=true&format=json',
+    )
 
     def parse(self, response):
       try:
         data = json.loads(response.body_as_unicode())
-        if(data['next']==None):
-            self.state = False
-
         for store in data['results']:
                 open_hours = store['opening_hours']
                 clean_hours = ''
@@ -31,19 +21,21 @@ class CoopFoodSpider(scrapy.Spider):
                    if(time['opens']!=None and time['closes'] != None):
                     clean_hours = clean_hours +time['name'][:2] +' '+time['opens']+'-'+time['closes']+' ; '
                 properties = {
-                    "ref": store['url'],
-                    "name": store['name'] ,
-                    "opening_hours": clean_hours,
-                    "website": "https://finder.coop.co.uk"+store['url'],
-                    "addr_full":  store['street_address']+" "+store['street_address2']+" "+store['street_address3'],
-                    "city": store['town'],
-                    "postcode": store['postcode'],
-                    "country": 'United Kingdom',
-                    "lon": float( store['position']['x']),
-                    "lat": float(store['position']['y']),
-                    "phone":store["phone"],
+                    "ref" : store['url'],
+                    "name" : store['name'] ,
+                    "opening_hours" : clean_hours,
+                    "website" : "https://finder.coop.co.uk"+store['url'],
+                    "addr_full" :  store['street_address']+" "+store['street_address2']+" "+store['street_address3'],
+                    "city" : store['town'],
+                    "postcode" : store['postcode'],
+                    "country" : 'United Kingdom',
+                    "lon" : float( store['position']['x']),
+                    "lat" : float(store['position']['y']),
+                    "phone" : store["phone"],
                 }
                 yield GeojsonPointItem(**properties)
+        if (data['next'] != None):
+            self.page_number = self.page_number+1
+            yield scrapy.Request(url=self.start_urls[0]+'&page='+str(self.page_number))
       except ValueError:
-          self.state =  False
           return
