8b90753d7ffaaee7a2177f45e52291d81cf04d41
Eagle19243 <eagle19243@gmail.com>
2017-12-16 17:16:03 +0200

    for #146

---
+++ locations/spiders/giantfood.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from locations.items import GeojsonPointItem
+
+STATES = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+          'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+          'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+          'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+          'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+WEEKDAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
+
+class GiantFoodSpider(scrapy.Spider):
+    name = "giantfood"
+    allowed_domains = ["giantfood.com"]
+        
+    def start_requests(self):
+        url = 'https://giantfood.com/auth/api/public/synergy/locator/GNTL/locate/grocery/15/details/'
+        headers = {
+            'authorization': 'Bearer b3e76cc34746bca47c730542c8611f8856dfc891142f2f3ce4e6a7ea0363320f'
+        }
+        for state in STATES :
+            yield scrapy.http.Request(
+                '{}?q={}'.format(url, state),
+                self.parse,
+                method = 'GET',
+                headers = headers
+            )
+
+    def parse(self, response):
+        result = json.loads(response.body_as_unicode())
+        for store in result['stores'] :
+            ref             = store['storeId']
+            name            = 'Giant'
+            website         = 'https://giantfood.com/location-details/#/store/detail/' + store['storeNo']
+            street          = '{} {}'.format(store['address1'], store['address2'])
+            city            = store['city']
+            state           = store['state']
+            postcode        = store['zip']
+            lat             = store['latitude']
+            lon             = store['longitude']
+            phone           = store['details']['departments'][0]['phone']
+            opening_hours   = self.hours(store['details']['departments'][0]['storeDeptHours'])
+
+            properties = {
+                'ref'           : ref,
+                'name'          : name,
+                'website'       : website,
+                'lat'           : lat,
+                'lon'           : lon,
+                'website'       : website,
+                'opening_hours' : opening_hours,
+                'street'        : street,
+                'city'          : city,
+                'state'         : state,
+                'postcode'      : postcode
+            }
+
+            yield GeojsonPointItem(**properties)
+
+    def hours(self, store_hours):
+        hours = ''
+        for data in store_hours:
+            if data['24hr'] :
+                open_time = '00:00'
+                close_time = '24:00'
+            if 'openTime' in data.keys() : 
+                open_time = data['openTime']
+            elif 'pickupStartTime' in data.keys():
+                open_time = data['pickupStartTime']
+            else :
+                open_time = '00:00'
+            
+            if 'closeTime' in data.keys() :
+                close_time = data['closeTime']
+            elif 'pickupEndTime' in data.keys() : 
+                close_time = data['pickupEndTime']
+            else :
+                close_time = '24:00'
+
+            hours = hours + '{} {}-{}; '.format(
+                WEEKDAYS[data['day'] - 1],
+                open_time,
+                close_time
+            )
+        
+        return hours
