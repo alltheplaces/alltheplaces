f20911e6045019fd340afe15ef6f0877af58692b
Ian Dees <ian.dees@gmail.com>
2017-12-15 21:59:10 -0600

    Formatting, remove redundant code

--- locations/spiders/hamptoninn.py
+++ locations/spiders/hamptoninn.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-import json
 
 from locations.items import GeojsonPointItem
 
@@ -16,38 +15,29 @@ class HamptonInnSpider(scrapy.Spider):
     def parse(self, response):
         response.selector.remove_namespaces()
         city_urls = response.xpath('//url/loc/text()').extract()
-        regex = re.compile(r'http(://|ps://)(www.|)hamptoninn\d.hilton.com/en/hotels/\w+/\S+/maps-directions/index.html')
+        regex = re.compile(r'http(://|s://)(www.|)hamptoninn\d.hilton.com/en/hotels/\w+/\S+/maps-directions/index.html')
         for path in city_urls:
-            if re.search(regex,path):
+            if re.search(regex, path):
                 yield scrapy.Request(
                     path.strip(),
                     callback=self.parse_store,
                 )
-            else:
-                pass
 
     def parse_store(self, response):
-
-        regex = re.compile(r'http(://|ps://)(www.|)hamptoninn\d.hilton.com/en/hotels/\w+/\S+/maps-directions/index.html')
-        if re.search(regex,response.request.url):
+        regex = re.compile(r'http(://|s://)(www.|)hamptoninn\d.hilton.com/en/hotels/\w+/\S+/maps-directions/index.html')
+        if re.search(regex, response.url):
             properties = {
-            'name': response.xpath('//span[@class="property-name"]/text()').extract_first(),
-            'ref': response.xpath('//span[@class="property-name"]/text()').extract_first(),
-            'addr_full': response.xpath('//span[@class="property-streetAddress"]/text()').extract_first(),
-            'city': response.xpath('//span[@class="property-addressLocality"]/text()').extract_first(),
-            'state': response.xpath('//span[@class="property-addressRegion"]/text()').extract_first(),
-            'postcode': response.xpath('//span[@class="property-postalCode"]/text()').extract_first(),
-            'phone': response.xpath('//span[@class="property-telephone"]/text()').extract_first(),
-            # 'fax': response.xpath('//span[@class="property-fax"]/text()').extract_first(),
-            'website': response.xpath('//h1/a/@href').extract_first(),
-            'opening_hours': response.xpath('//div[@class="policy_component_left_pane"]/div[@class="title"]/text()').extract_first() + " " + " ".join(response.xpath('//div[@class="policy_component_left_pane"]/div[2]/text()').extract_first().split()) + " - " + response.xpath('//div[@class="policy_component_right_pane"]/div[@class="title"]/text()').extract_first() + " " + " ".join(response.xpath('//div[@class="policy_component_right_pane"]/div[2]/text()').extract_first().split()),
-            'lat': float(response.xpath('//meta[@name="geo.position"]/@content').extract_first().split(';')[0]),
-            'lon': float(response.xpath('//meta[@name="geo.position"]/@content').extract_first().split(';')[1]),
+                'name': response.xpath('//span[@class="property-name"]/text()').extract_first(),
+                'ref': response.xpath('//span[@class="property-name"]/text()').extract_first(),
+                'addr_full': response.xpath('//span[@class="property-streetAddress"]/text()').extract_first(),
+                'city': response.xpath('//span[@class="property-addressLocality"]/text()').extract_first(),
+                'state': response.xpath('//span[@class="property-addressRegion"]/text()').extract_first(),
+                'postcode': response.xpath('//span[@class="property-postalCode"]/text()').extract_first(),
+                'phone': response.xpath('//span[@class="property-telephone"]/text()').extract_first(),
+                'website': response.xpath('//h1/a/@href').extract_first(),
+                'opening_hours': response.xpath('//div[@class="policy_component_left_pane"]/div[@class="title"]/text()').extract_first() + " " + " ".join(response.xpath('//div[@class="policy_component_left_pane"]/div[2]/text()').extract_first().split()) + " - " + response.xpath('//div[@class="policy_component_right_pane"]/div[@class="title"]/text()').extract_first() + " " + " ".join(response.xpath('//div[@class="policy_component_right_pane"]/div[2]/text()').extract_first().split()),
+                'lat': float(response.xpath('//meta[@name="geo.position"]/@content').extract_first().split(';')[0]),
+                'lon': float(response.xpath('//meta[@name="geo.position"]/@content').extract_first().split(';')[1]),
             }
 
-            open('/tmp/json.txt', 'w').write(str(properties))
-
             yield GeojsonPointItem(**properties)
-
-        else:
-            pass
