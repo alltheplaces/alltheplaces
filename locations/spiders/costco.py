740b6a307517ec1bd433fdb4369e84c48e5420d3
Ian Dees <ian.dees@gmail.com>
2017-12-16 21:15:56 -0600

    Spider from the list of locations

--- locations/spiders/costco.py
+++ locations/spiders/costco.py
@@ -5,27 +5,28 @@ import re
 
 from locations.items import GeojsonPointItem
 
-HEADERS = {
-           'Host': 'www.costco.com',
-           'Accept-Encoding': 'gzip, deflate, br',
-           'Accept-Language': 'en-US,en;q=0.9',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-           'Connection': 'keep-alive',
-           'Cache-Control': 'max-age=0',
-           'Upgrade-Insecure-Requests': '1',
-           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-           }
-URL = 'https://www.costco.com/AjaxWarehouseBrowseLookupView?langId=-1&storeId=10301&numOfWarehouses=10&hasGas=false&hasTires=false&hasFood=false&hasHearing=false&hasPharmacy=false&hasOptical=false&hasBusiness=false&tiresCheckout=0&isTransferWarehouse=false&populateWarehouseDetails=true&warehousePickupCheckout=false&latitude=42.0031051635742&longitude=-89.9876327514648'
+DAYS_NAME = {
+    'm': 'Mo',
+    't': 'Tu',
+    'w': 'We',
+    's': 'Th',
+    'f': 'Fr',
+    'f ': 'Fr',
+    'sun. ': 'Su',
+    'sat': 'Sa',
+}
+
 
 class CostcoSpider(scrapy.Spider):
     name = "costco"
     allowed_domains = ['www.costco.com']
-
-    def start_requests(self):
-        form_data = {'action': 'udfd_update_locations', 'data': '3211'}
-
-        yield scrapy.http.FormRequest(url=URL, method='GET', headers=HEADERS, callback=self.parse)
+    start_urls = (
+        'https://www.costco.com/WarehouseLocatorBrowseView',
+    )
+    download_delay = 0.3
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0',
+    }
 
     def normalize_time(self, time_str):
         match = re.search(r'(.*)(am|pm| a.m| p.m)', time_str)
@@ -39,7 +40,9 @@ class CostcoSpider(scrapy.Spider):
 
     def store_hours(self, store_hours):
         opening_hours = []
-        days_name = {'m':'Mo', 't':'Tu', 'w':'We', 's':'Si', 'f':'Fr'}
+
+        if not store_hours:
+            return None
 
         for day_info in store_hours:
             if day_info.lower().find('close') > -1:
@@ -50,7 +53,7 @@ class CostcoSpider(scrapy.Spider):
             day = day.split('-')
 
             if len(day) == 2:
-                day = days_name[day[0].lower()] + '-' + days_name[day[1].lower()]
+                day = DAYS_NAME[day[0].lower()] + '-' + DAYS_NAME[day[1].lower()]
             else:
                 day = day[0][:2]
 
@@ -65,21 +68,29 @@ class CostcoSpider(scrapy.Spider):
         return '; '.join(opening_hours)
 
     def parse(self, response):
+        container_ids = response.xpath('//div[@class="form-item statePanel"]/select/option/@value').extract()
+        for container_id in container_ids:
+            yield scrapy.Request(
+                'https://www.costco.com/AjaxWarehouseBrowseLookupView?catalogId=10701&parentGeoNode=' + container_id,
+                callback=self.parse_stores
+            )
+
+    def parse_stores(self, response):
         stores = json.loads(response.body_as_unicode())
 
         for store in stores:
-
             props = {
                 'lat': store.get('latitude'),
                 'lon': store.get('longitude'),
-                'ref': str(store.get('identifier')),
+                'ref': store.get('identifier'),
                 'phone': self._clean_text(store.get('phone')),
                 'name': store.get('displayName'),
                 'addr_full': store.get('address1'),
                 'city': store.get('city'),
                 'state': store.get('state'),
                 'postcode': store.get('zipCode'),
-                'website': store.get('url'),
+                'country': store.get('country'),
+                'website': 'https://www.costco.com/warehouse-locations/store-{}.html'.format(store.get('identifier')),
                 'opening_hours': self.store_hours(store.get('warehouseHours')),
             }
 
