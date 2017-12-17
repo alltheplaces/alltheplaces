610285793034f60e527b8bf61cbf47f6ea41bd6d
DerekMcDaniel <30557517+DerekMcDaniel@users.noreply.github.com>
2017-12-17 15:46:52 -0500

    Coffee Time
    added coffee time spider

---
+++ locations/spiders/coffee_time.py
@@ -0,0 +1,59 @@
+import scrapy
+from locations.items import GeojsonPointItem
+
+
+class BobsSpider(scrapy.Spider):
+    name = 'coffeetime'
+    download_delay = 0
+    allowed_domains = ['www.coffeetime.com']
+    start_urls = ['http://www.coffeetime.com/locations.aspx?address=&Countryui=CA&pageNumber=1']
+
+    def parse_store(self, response):
+        container = response.xpath('//div[contains(@class, "col-md-9")]')
+        print(container)
+        for i in container:
+            street = i.xpath('.//div/p[1]/text()').extract_first().strip()
+            cty_st_zip = i.xpath('.//div/p[2]/text()').extract_first().strip()
+
+            # Keep missing phone/<li> from breaking script
+            if i.xpath('.//div/ul/li[2]/p/text()').extract_first():
+                phone = i.xpath('.//div/ul/li[1]/p/text()')\
+                    .extract_first().strip()
+            else:
+                phone = ''
+
+            if i.xpath('.//div/ul/li[2]/p/a/@href').extract_first():
+                lat = i.xpath('.//div/ul/li[2]/p/a/@href').extract_first(
+                    ).split('=')[2].split(',')[0]
+                lon = i.xpath('.//div/ul/li[2]/p/a/@href').extract_first(
+                    ).split('=')[2].split(',')[1]
+            else:
+                lat = i.xpath('.//div/ul/li/p/a/@href').extract_first(
+                ).split('=')[2].split(',')[0]
+                lon = i.xpath('.//div/ul/li/p/a/@href').extract_first(
+                ).split('=')[2].split(',')[1]
+
+            city = cty_st_zip.split(', ')[0]
+            state = cty_st_zip.split(', ')[1].split(', ')[0]
+            postcode = cty_st_zip.split(', ')[2]
+
+            addr_full = "{} {}, {} {}".format(street, city, state, postcode)
+
+            yield GeojsonPointItem(
+                ref=street,
+                city=city,
+                state=state,
+                postcode=postcode,
+                addr_full=addr_full,
+                phone=phone,
+                lat=lat,
+                lon=lon,
+                country="CA"
+            )
+
+    def parse(self, response):
+        base_url = "http://www.coffeetime.com/locations.aspx?" \
+                   "address=&Countryui=CA&pageNumber="
+        for i in range(1, 9):
+            page = base_url + str(i)
+            yield scrapy.Request(page, callback=self.parse_store)
