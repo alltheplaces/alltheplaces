eade8ef0dd0e62dbe28387bd0a00268baf1bbe9e
whoissteven <34196991+whoissteven@users.noreply.github.com>
2017-12-19 03:39:43 -0500

    Add files via upload

--- locations/spiders/thelibrary.py
+++ locations/spiders/thelibrary.py
@@ -27,7 +27,6 @@ class TheLibrarySpider(scrapy.Spider):
         response.selector.remove_namespaces()
         xml_urls = response.xpath('//url/loc/text()').extract()
         for path in xml_urls:
-            print('YES: '+path.strip())
             yield scrapy.Request(
                 path.strip(),
                 callback=self.parse_store,
@@ -37,25 +36,7 @@ class TheLibrarySpider(scrapy.Spider):
 
     def parse_store(self, response):
 
-        if str(response.xpath('.//div[@id="lib-data"]/p[2]/text()').extract()[-1]).strip():
-            properties = {
-                'name': response.xpath('//h1/text()').extract_first().strip(),
-                'ref': response.xpath('//h1/text()').extract_first().strip(),
-                'addr_full': response.xpath('//div[@id="lib-data"]/p[2]/text()').extract_first().strip(),
-                'city': response.xpath('//div[@id="lib-data"]/p[2]/text()').extract()[1].strip().split(',')[0],
-                'state': response.xpath('//div[@id="lib-data"]/p/text()').extract()[1].split(',')[1].strip().split('\xa0')[0],
-                'country': str(response.xpath('.//div[@id="lib-data"]/p[2]/text()').extract()[-1]).strip(),
-                'postcode': response.xpath('//div[@id="lib-data"]/p[2]/text()').extract()[1].split(',')[1].strip().split('\xa0')[0],
-                'phone': response.xpath('//div[@id="lib-data"]/p[3]').extract_first().split('\xa0')[1].split('<br>')[0],
-                'website': response.request.url,
-                # 'lat': float(response.xpath('//div/div[@class="location-actions"]/a[@href]').extract_first().split('q=')[1].split('%')[0].split(',')[0]),
-                # 'lon': float(response.xpath('//div/div[@class="location-actions"]/a[@href]').extract_first().split('q=')[1].split('%')[0].split(',')[1]),
-            }
-        else:
-            if response.xpath('//div[@id="lib-data"]/p[2]').extract_first():
-                phoneNumber = response.xpath('//div[@id="lib-data"]/p[2]').extract_first().split('\xa0')[1].split('<br>')[0]
-            else:
-                phoneNumber = response.xpath('//div[@id="lib-data"]/p[2]').extract_first()
+        if "United States" in response.css('#lib-data').extract_first():
             properties = {
             'name': response.xpath('//h1/text()').extract_first().strip(),
             'ref': response.xpath('//h1/text()').extract_first().strip(),
@@ -63,10 +44,30 @@ class TheLibrarySpider(scrapy.Spider):
             'city': response.xpath('//div[@id="lib-data"]/p/text()').extract()[1].strip().split(',')[0],
             'state': response.xpath('//div[@id="lib-data"]/p/text()').extract()[1].split(',')[1].strip().split('\xa0')[0],
             'postcode': response.xpath('//div[@id="lib-data"]/p/text()').extract()[1].split(',')[1].strip().split('\t')[-1],
-            'phone':  phoneNumber,
+            # 'phone':  phoneNumber,
             'website': response.request.url,
             # 'lat': float(response.xpath('//div/div[@class="location-actions"]/a[@href]').extract_first().split('q=')[1].split('%')[0].split(',')[0]),
             # 'lon': float(response.xpath('//div/div[@class="location-actions"]/a[@href]').extract_first().split('q=')[1].split('%')[0].split(',')[1]),
+            }
+        else:
+            # if response.xpath('//div[@id="lib-data"]/p[2]').extract_first():
+            #     phoneNumber = response.xpath('//div[@id="lib-data"]/p[2]').extract_first().split('\xa0')[1].split('<br>')[0]
+            # else:
+            #     phoneNumber = response.xpath('//div[@id="lib-data"]/p[2]').extract_first()
+
+
+            properties = {
+                'name': response.xpath('//h1/text()').extract_first().strip(),
+                'ref': response.xpath('//h1/text()').extract_first().strip(),
+                'addr_full': response.css('#lib-data').extract_first().split('<p>')[1].split('\n\t\t\t\t\t\t')[1].replace('<br>',''),
+                'city': response.css('#lib-data').extract_first().split('<p>')[1].split('\n\t\t\t\t\t\t')[2].split(',')[0],
+                'state': response.xpath('//div[@id="lib-data"]/p/text()').extract()[1].split(',')[1].strip().split('\xa0')[0],
+                'country': response.css('#lib-data').extract_first().split('<p>')[1].split('\n\t\t\t\t\t\t')[4].split('<')[-2].replace('br>',''),
+                'postcode': response.css('#lib-data').extract_first().split('<p>')[1].split('\n\t\t\t\t\t\t')[3].replace('\xa0',''),
+                'phone': response.xpath('//div[@id="lib-data"]/p[3]').extract_first().split('\xa0')[1].split('<br>')[0],
+                'website': response.request.url,
+                # 'lat': float(response.xpath('//div/div[@class="location-actions"]/a[@href]').extract_first().split('q=')[1].split('%')[0].split(',')[0]),
+                # 'lon': float(response.xpath('//div/div[@class="location-actions"]/a[@href]').extract_first().split('q=')[1].split('%')[0].split(',')[1]),
             }
 
         yield GeojsonPointItem(**properties)
