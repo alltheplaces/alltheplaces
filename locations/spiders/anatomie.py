526c4a7cd869f36c1587ed024eca9a2406751b0b
josh0203e <josh0203be@hotmail.com>
2017-12-20 12:15:15 -0800

    anatomie

---
+++ locations/spiders/anatomie.py
@@ -0,0 +1,31 @@
+import scrapy
+import re
+import json
+import lxml
+from locations.items import GeojsonPointItem
+class AnatomieSpider(scrapy.Spider):
+
+    name = "anatomie"
+    allowed_domains = ["stores.boldapps.net"]
+    download_delay = 0
+    start_urls = (
+        'https://stores.boldapps.net/front-end/get_surrounding_stores.php?shop=anatomie-store.myshopify.com&latitude=38.331258&longitude=-121.54841799999997&max_distance=100000&limit=10000&calc_distance=1',
+    )
+
+    def parse(self, response):
+        json_data = json.loads(response.body_as_unicode())
+        for item in json_data['stores']:
+            address = lxml.html.fromstring(item['summary'])
+            properties = {
+                'addr_full': address.xpath('normalize-space(.//span[@class="address"]/text())'),
+                'phone': '',
+                'name' : address.xpath('normalize-space(.//span[@class="name"]/text())'),
+                'city': address.xpath('normalize-space(.//span[@class="city"]/text())'),
+                'state': address.xpath('normalize-space(.//span[@class="prov_state"]/text())'),
+                'postcode':address.xpath('normalize-space(.//span[@class="postal_zip"]/text())'),
+                'ref': item['store_id'],
+                'website': response.url,
+                'lat': item['lng'],
+                'lon': item['lat'],
+            }
+            yield GeojsonPointItem(**properties)
