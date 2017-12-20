e1a4541ec13fb11e846f715a41a13e7414dcb137
Avi Mosseri <evan.mosseri@gmail.com>
2017-12-20 00:47:48 -0600

    finished supercuts

---
+++ locations/spiders/supercuts.py
@@ -0,0 +1,69 @@
+import scrapy
+from scrapy.spiders import BaseSpider
+from locations.items import GeojsonPointItem
+import json
+import re
+from io import StringIO
+from scrapy.http import HtmlResponse
+from functools import partial
+from datetime import datetime
+
+default_headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/63.0.3239.84 Safari/537.36"}
+
+
+def get_hours(hour_list):
+    ret = []
+    for hours in hour_list:
+        from pprint import pprint
+        open_time = datetime.strptime(hours["hours"]["open"], "%I:%M %p").strftime("%H:%M")
+        close_time = datetime.strptime(hours["hours"]["close"], "%I:%M %p").strftime("%H:%M")
+        split_days = hours["days"].split(" - ")
+        if "-" in hours["days"] and len(split_days) == 2:
+            start_day, end_day = split_days
+            ret.append("{} {}-{}".format("-".join([start_day[:2], end_day[:2]]), open_time, close_time))
+        else:
+            ret.append("{} {}-{}".format(hours["days"][:2], open_time, close_time))
+    return "; ".join(ret)
+
+class SuperCutsScraper(scrapy.Spider):
+    name = "supercuts"
+
+    start_urls = [
+        'https://www.supercuts.com/salon-directory.html'
+    ]
+
+    def parse(self, response):
+        states = [x.xpath("@href").extract_first() for x in response.css("a[href*='supercuts.com/locations/']")]
+        for state_url in states:
+            yield scrapy.Request(state_url, callback=self.parse_cities)
+
+    def parse_cities(self, response):
+        cities = [x.xpath("@href").extract_first() for x in response.css("a[href*='locations/']")]
+        for city_url in cities:
+            yield scrapy.Request(response.urljoin(city_url), callback=self.parse_locations)
+
+    def parse_locations(self, response):
+        locations = [x.xpath("@href").extract_first() for x in response.css("a[href*='locations/']")]
+        salon_base = "https://info3.regiscorp.com/salonservices/siteid/1/salon/{}"
+        for location in locations:
+            salon_id = location.strip(".html").split("-")[-1]
+            if salon_id.isnumeric():
+                yield scrapy.Request(salon_base.format(salon_id),
+                                     callback=partial(self.parse_result, trunk={"url": response.urljoin(location)}))
+
+    def parse_result(self, response, trunk={}):
+        result = json.loads(response.body_as_unicode())
+        yield GeojsonPointItem(lat=result["latitude"],
+                               lon=result["longitude"],
+                               name="Supercuts " + result["name"].title(),
+                               addr_full=result["address"],
+                               city=result["city"],
+                               state=result["state"],
+                               postcode=result["zip"],
+                               phone=result["phonenumber"],
+                               website=trunk["url"],
+                               opening_hours=get_hours(result["store_hours"]),
+                               ref=response.url.split("/")[-1]
+                               )
