7317be279e5e46b293d0cb534192310767753270
whoissteven <34196991+whoissteven@users.noreply.github.com>
2017-12-20 03:10:27 -0500

    Add files via upload
    Added error handling.

--- locations/spiders/pdq.py
+++ locations/spiders/pdq.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import time
 from scrapy.utils.gz import gunzip, is_gzipped
 
 from locations.items import GeojsonPointItem
@@ -27,28 +28,33 @@ class PDQSpider(scrapy.Spider):
             pass
 
     def parse_store(self, response):
-
-        if response.xpath('//div[@class="hours"]/div').extract():
-            storeHoursHTML = str(response.xpath('//div[@class="hours"]/div').extract()).replace("'",'').replace('[','').replace(']','')
-            p = re.compile(r'<.*?>')
-            storeHours = p.sub('', storeHoursHTML)
-            storeHours = storeHours.replace('\\n', ' - ')
-            storeHours = "".join(storeHours.strip())
+        if "Sitefinity trial version" in response.xpath('//title/text()').extract():
+            yield scrapy.Request(
+                response.request.url,
+                callback=self.parse_store,
+            )
         else:
-            storeHours = response.xpath('//div[@class="hours"]/div').extract()
+            if response.xpath('//div[@class="hours"]/div').extract():
+                storeHoursHTML = str(response.xpath('//div[@class="hours"]/div').extract()).replace("'",'').replace('[','').replace(']','')
+                p = re.compile(r'<.*?>')
+                storeHours = p.sub('', storeHoursHTML)
+                storeHours = storeHours.replace('\\n', ' - ')
+                storeHours = "".join(storeHours.strip())
+            else:
+                storeHours = response.xpath('//div[@class="hours"]/div').extract()
 
-        properties = {
-        'name': response.xpath('//div[@class="name"]/h1/text()').extract_first(),
-        'ref': response.xpath('//div[@class="name"]/h1/text()').extract_first(),
-        'addr_full': response.xpath('//div[@class="address"]/text()').extract_first(),
-        'city': response.xpath('//div[@class="address"]/text()[2]').extract_first().split(',')[0],
-        'state': response.xpath('//div[@class="address"]/text()[2]').extract_first().split()[1],
-        'postcode': response.xpath('//div[@class="address"]/text()[2]').extract_first().split()[-1],
-        'phone': response.xpath('//tel/a[@href]/text()').extract_first(),
-        'website': response.request.url,
-        'opening_hours': storeHours,
-        'lat': float(response.xpath('//div[@class="address"]/a/@href').extract_first().split('@')[1].split(',')[0]),
-        'lon': float(response.xpath('//div[@class="address"]/a/@href').extract_first().split('@')[1].split(',')[1]),
-        }
+            properties = {
+            'name': response.xpath('//div[@class="name"]/h1/text()').extract_first(),
+            'ref': response.xpath('//div[@class="name"]/h1/text()').extract_first(),
+            'addr_full': response.xpath('//div[@class="address"]/text()').extract_first().strip(),
+            'city': response.xpath('//div[@class="address"]/text()[2]').extract_first().split(',')[0],
+            'state': response.xpath('//div[@class="address"]/text()[2]').extract_first().split()[1],
+            'postcode': response.xpath('//div[@class="address"]/text()[2]').extract_first().split()[-1],
+            'phone': response.xpath('//tel/a[@href]/text()').extract_first(),
+            'website': response.request.url,
+            'opening_hours': storeHours,
+            'lat': float(response.xpath('//div[@class="address"]/a/@href').extract_first().split('@')[1].split(',')[0]),
+            'lon': float(response.xpath('//div[@class="address"]/a/@href').extract_first().split('@')[1].split(',')[1]),
+            }
 
-        yield GeojsonPointItem(**properties)
+            yield GeojsonPointItem(**properties)
