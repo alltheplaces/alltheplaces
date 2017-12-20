a891d79545b68691797247fc914eb8fef3fb913f
Ian Dees <ian.dees@gmail.com>
2017-12-20 17:02:54 -0600

    Clean up formatting on greatclips spider

--- locations/spiders/greatclips.py
+++ locations/spiders/greatclips.py
@@ -5,20 +5,21 @@ import re
 
 from locations.items import GeojsonPointItem
 
+
 class GreatclipsSpider(scrapy.Spider):
     name = "greatclips"
-    allowed_domains = ["greatclips.com","stylewaretouch.net",""]
+    allowed_domains = ["greatclips.com", "stylewaretouch.net"]
     start_urls = (
         'https://www.stylewaretouch.net/checkin/wa/jsonMarkers?client=locator&lat=-34&lng=85&tzoffset=200&callback=onSuccess&failureCallback=onFailure&stores=',
     )
 
     def store_hours(self, store_hours):
-        if not store_hours or len(store_hours)<5:
+        if not store_hours or len(store_hours) < 5:
             return
 
         stri = ''
         match = re.search(r'(\d{1,2})(:(\d{1,2}))?\s*(am|AM|mp|PM|pm)?\s*-\s*(\d{1,2})(:(\d{1,2}))?\s*(am|pm|mp|PM|AM)',store_hours)
-        
+
         if not match:
             return store_hours
 
@@ -28,19 +29,22 @@ class GreatclipsSpider(scrapy.Spider):
         return stri.rstrip(';')
 
     def parse(self, response):
-        for i in range(1,10000): #all shops grouped in ther database by codes experimentally was uncovered all groups lies in 1..10000 range (checked till 100000) 
+        # all shops grouped in ther database by codes experimentally was
+        # uncovered all groups lies in 1..10000 range (checked till 100000)
+        for i in range(1, 10000):
             yield scrapy.Request(
                 self.start_urls[0]+str(i),
                 callback=self.parse_shop,
-                meta={'pos':str(i)}
             )
 
     def parse_shop(self, response):
-        if response.text.find('onFailure')!=-1:
+        if response.text.find('onFailure') != -1:
             return
-        shops=json.loads(response.text.lstrip('onSuccess(').rstrip(')'))
-        if not len(shops):
+
+        shops = json.loads(response.text.lstrip('onSuccess(').rstrip(')'))
+        if not shops:
             return
+
         for shop in shops:
             props = {}
 
@@ -56,8 +60,5 @@ class GreatclipsSpider(scrapy.Spider):
             props['country'] = shop['CountryCode']
 
             props['opening_hours'] = 'Mo-Fr '+self.store_hours(shop['hours_mon'])+';Sa '+self.store_hours(shop['hours_sat'])+';Su '+self.store_hours(shop['hours_sun'])
-
-
-            
 
             yield GeojsonPointItem(**props)
