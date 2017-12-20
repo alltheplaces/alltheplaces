4a7547b30706f5db5e8543891375ac97d19b4978
Ian Dees <ian.dees@gmail.com>
2017-12-20 17:45:51 -0600

    Formatting, casting to float

--- locations/spiders/heb.py
+++ locations/spiders/heb.py
@@ -1,14 +1,20 @@
 import scrapy
 from locations.items import GeojsonPointItem
-import json
 import re
-from scrapy.http import HtmlResponse
 from xml.dom import minidom
-import ast
 
 
-def get_hours(sidebar):
-    try:
+class HEBSpider(scrapy.Spider):
+    name = "heb"
+
+    start_urls = ["https://www.heb.com/sitemap/storeSitemap.xml"]
+
+    def parse(self, response):
+        document = minidom.parseString(response.body_as_unicode())
+        for url in (node.firstChild.nodeValue for node in document.getElementsByTagName("loc")):
+            yield scrapy.Request(url=url, callback=self.parse_store, meta={"url": url})
+
+    def get_hours(self, sidebar):
         hours = []
         ret = []
         is_hours = False
@@ -32,23 +38,10 @@ def get_hours(sidebar):
                 new_range = range[:2]
             ret.append("{} {}-{}".format(new_range, start, new_end))
         return "; ".join(ret)
-    except:
-        pass
-
-
-class HEBSpider(scrapy.Spider):
-    name = "heb"
-
-    start_urls = ["https://www.heb.com/sitemap/storeSitemap.xml"]
-
-    def parse(self, response):
-        document = minidom.parseString(response.body_as_unicode())
-        for url in (node.firstChild.nodeValue for node in document.getElementsByTagName("loc")):
-            yield scrapy.Request(url=url, callback=self.parse_store, meta={"url": url})
 
     def parse_store(self, response):
         dat = re.findall(r"search_createMapPin\(.*\);", response.body_as_unicode())
-        if len(dat):
+        if dat:
             pin_call = dat[0]
             store_str = pin_call[len("search_createMapPin("):-2]
             if len(store_str.split(",")) == 17:
@@ -56,8 +49,8 @@ class HEBSpider(scrapy.Spider):
                 if lat and lon:
                     sidebar = response.css(".hoursDetail span::text, .hoursDetail p:not(:contains('\t'))::text").extract()
                     yield GeojsonPointItem(
-                        lat=lat,
-                        lon=lon,
+                        lat=float(lat),
+                        lon=float(lon),
                         name=name.strip('"'),
                         addr_full=address.strip('"'),
                         city=city.strip('"'),
@@ -65,6 +58,6 @@ class HEBSpider(scrapy.Spider):
                         postcode=zip.strip('"'),
                         phone=phone.replace(" - ", "-").strip('"'),
                         website=response.meta.get("url"),
-                        opening_hours=get_hours(sidebar),
+                        opening_hours=self.get_hours(sidebar),
                         ref=response.meta.get("url")
                     )
