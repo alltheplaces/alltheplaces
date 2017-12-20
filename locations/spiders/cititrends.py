82f8007ffc164980c8c94c4ab794a4655f884784
josh0203e <josh0203be@hotmail.com>
2017-12-20 14:11:33 -0800

    cititrends

--- locations/spiders/cititrends.py
+++ locations/spiders/cititrends.py
@@ -2,17 +2,7 @@ import scrapy
 import re
 from locations.items import GeojsonPointItem
 
-DAY_MAPPING = {
-    'Monday': 'Mo',
-    'Tuesday': 'Tu',
-    'W': 'We',
-    'F': 'Fr',
-    'Sat': 'Sa',
-    'Sun': 'Su'
-}
-
-
-class CVSSpider(scrapy.Spider):
+class CitiTrendsSpider(scrapy.Spider):
 
     name = "cititrends"
     allowed_domains = ["locations.cititrends.com"]
@@ -21,68 +11,15 @@ class CVSSpider(scrapy.Spider):
         'https://locations.cititrends.com/',
     )
 
-    def parse_day(self, day):
-
-        if re.search('Sat', day) or re.search('Sun', day):
-            return DAY_MAPPING[day.strip()]
-
-        if re.search('-', day):
-            days = day.split('-')
-            osm_days = []
-            if len(days) == 2:
-                for day in days:
-                    osm_day = DAY_MAPPING[day.strip()]
-                    osm_days.append(osm_day)
-            return "-".join(osm_days)
-
-    def parse_times(self, times):
-        if times.strip() == 'Open 24 hours':
-            return '24/7'
-        hours_to = [x.strip() for x in times.split('-')]
-        cleaned_times = []
-
-        for hour in hours_to:
-            if re.search('PM$', hour):
-                hour = re.sub('PM', '', hour).strip()
-                hour_min = hour.split(":")
-                if int(hour_min[0]) < 12:
-                    hour_min[0] = str(12 + int(hour_min[0]))
-                cleaned_times.append(":".join(hour_min))
-
-            if re.search('AM$', hour):
-                hour = re.sub('AM', '', hour).strip()
-                hour_min = hour.split(":")
-                if len(hour_min[0]) <2:
-                    hour_min[0] = hour_min[0].zfill(2)
-                else:
-                    hour_min[0] = str(12 + int(hour_min[0]))
-
-                cleaned_times.append(":".join(hour_min))
-        return "-".join(cleaned_times)
-
-    def parse_hours(self, lis):
-        hours = []
-        for li in lis:
-            day = li.xpath('normalize-space(.//span[@class="day single-day"]/text() | .//span[@class="day"]/text())').extract_first()
-            times = li.xpath('.//span[@class="timings"]/text()').extract_first()
-            if times and day:
-                parsed_time = self.parse_times(times)
-                parsed_day = self.parse_day(day)
-                hours.append(parsed_day + ' ' + parsed_time)
-
-        return "; ".join(hours)
-
     def parse_stores(self, response):
-        raw_phone = response.xpath('normalize-space(//a[@class="tel_phone_numberDetail"]/@href)').extract_first()
-
         properties = {
             'addr_full': response.xpath('normalize-space(//span[@class="c-address-street-1"]/text())').extract_first(),
             'phone': response.xpath('normalize-space(//span[@itemprop="telephone"]/text())').extract_first(),
             'city': response.xpath('normalize-space(//span[@itemprop="addressLocality"]/text())').extract_first(),
             'state': response.xpath('normalize-space(//span[@itemprop="addressRegion"]/text())').extract_first(),
             'postcode': response.xpath('normalize-space(//span[@itemprop="postalCode"]/text())').extract_first(),
-            'ref':re.findall(r"[a-z]{2}\/[^()]+\/[^.]+",response.url)[0].replace('/','_'),
-            'website':response.url,
+            'ref': re.findall(r"[a-z]{2}\/[^()]+\/[^.]+",response.url)[0].replace('/','_'),
+            'website': response.url,
             'lat': float(response.xpath('normalize-space(//meta[@itemprop="latitude"]/@content)').extract_first()),
             'lon': float(response.xpath('normalize-space(//meta[@itemprop="longitude"]/@content)').extract_first()),
         }
@@ -91,7 +28,6 @@ class CVSSpider(scrapy.Spider):
         hours = "; ".join(hours)
         if hours:
             properties['opening_hours'] = hours
-
         yield GeojsonPointItem(**properties)
 
     def parse_city_stores(self, response):
