e2bc7cd027ae7242d4583995b00a1a8e8f88572e
Smart Dev <dev@Smarts-Mac.local>
2017-12-21 09:47:50 -0800

    Jiffy Lube add lat, lon, opening hours. fixes #533

--- locations/spiders/jiffylube.py
+++ locations/spiders/jiffylube.py
@@ -22,20 +22,62 @@ class JiffyLubeSpider(scrapy.Spider):
             yield scrapy.Request(
                 '{}/{}'.format(base_url, state)
             )
-        
+
+    def store_hours(self, store_hours):
+        opening_hours = []
+        for day_hours in store_hours:
+            day_hours = day_hours.replace('Mon', 'Mo').replace('Tue', 'Tu').replace('Wed', 'We').replace('Thu', 'Th').replace('Fri', 'Fr').replace('Sat', 'Sa').replace('Sun', 'Su')
+            day_hours = day_hours.replace('Weekdays', 'Mo-Fr').replace('Weekends', 'Sa-Su').replace('Holidays', 'PH').replace('Everyday', 'Mo-Su')
+            day_hours = day_hours.replace('midnight', '12 PM').replace(':', '')
+            hours = ''
+            match = re.search(r'(\d{1,2}) (A|P)M - (\d{1,2}) (A|P)M', day_hours)
+            if match:
+                (f_hr, f_ampm, t_hr, t_ampm) = match.groups()
+                f_hr = int(f_hr)
+                if f_ampm == 'P':
+                    f_hr += 12
+                elif f_ampm == 'A' and f_hr == 12:
+                    f_hr = 0
+                t_hr = int(t_hr)
+                if t_ampm == 'P':
+                    t_hr += 12
+                elif t_ampm == 'A' and t_hr == 12:
+                    t_hr = 0
+                
+                hours = '{:02d}:{}-{:02d}:{}'.format(
+                    f_hr,
+                    '00',
+                    t_hr,
+                    '00',
+                )
+                day_hours = day_hours.replace(match.group(), hours)
+            opening_hours.append(day_hours)
+        return "; ".join(opening_hours)
+    
     def parse(self, response):
         stores = response.xpath('//div[@class="locations-by-state"]/div[@class="state-locations"]')
         for store in stores:
-            ref = store.xpath('div[@class="state-location-info"]/a[@class="title"]/@href').extract_first()
-            match = re.search(r'\/(\d+)', ref)
-            properties = {
-                "ref": match.group(1),
-                "name": store.xpath('div[@class="state-location-info"]/a[@class="title"]/text()').extract_first(),
-                "addr_full": store.xpath('div[@class="state-location-info"]/a[@class="address"]/div[1]/span/text()').extract_first(),
-                "city": store.xpath('div[@class="state-location-info"]/a[@class="address"]/div[2]/span[@itemprop="addressLocality"]/text()').extract_first()[:2],
-                "state": store.xpath('div[@class="state-location-info"]/a[@class="address"]/div[2]/span[@itemprop="addressRegion"]/text()').extract_first(),
-                "postcode": store.xpath('div[@class="state-location-info"]/a[@class="address"]/div[2]/span[@itemprop="postalCode"]/text()').extract_first(),
-                "phone": store.xpath('div[@class="state-location-info"]/a[@class="phone-number show"]/span[@itemprop="telephone"]/text()').extract_first(),
-            }
+            url = store.xpath('div[@class="state-location-info"]/a[@class="title"]/@href').extract_first()
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_stores)
 
-            yield GeojsonPointItem(**properties)
+    def parse_stores(self, response):
+        store = response.xpath('//div[@class="location-information"]')
+        name = response.xpath('//div[@class="location--singleLocation"]/h1[@class="location-title"]/text()[last()]').extract_first()
+        match = re.search(r'\#(\d+)', name)
+        ng_script = response.xpath('//div[@class="container group"]/@data-ng-init').extract_first()
+        coor_match = re.search(r'latitude":(-?\d+\.\d+),"longitude":(-?\d+\.\d+)', ng_script)
+
+        properties = {
+            'ref': match.group(1),
+            'name': name,
+            'addr_full': store.xpath('div[@class="location-information-address"]/a/div[1]/span/text()').extract_first(),
+            'city': store.xpath('div[@class="location-information-address"]/a/div[2]/span[1]/text()').extract_first(),
+            'state': store.xpath('div[@class="location-information-address"]/a/div[2]/span[2]/text()').extract_first(),
+            'postcode': store.xpath('div[@class="location-information-address"]/a/div[2]/span[3]/text()').extract_first(),
+            'phone': store.xpath('div[@class="location-information-phoneNumber"]/strong/span/a/text()').extract_first(),
+            'lat': coor_match.group(1),
+            'lon': coor_match.group(2),
+            'opening_hours': self.store_hours(store.xpath('div[@class="location-information-hours"]/div[@itemprop="openingHours"]/text()').extract()),
+        }
+
+        yield GeojsonPointItem(**properties)    
