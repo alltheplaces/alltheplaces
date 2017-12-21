ca21f6c249c391dad2172271c9ffeba1e556e667
Kristine P <kristinelpetrosyan@gmail.com>
2017-12-20 21:30:03 -0500

    Fixed Exception

--- locations/spiders/popeyes.py
+++ locations/spiders/popeyes.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import json
+import re
 import scrapy
+
 from locations.items import GeojsonPointItem
 
 HOURS_XPATH = 'normalize-space(//div[starts-with(@class, "hours-section ")])'
 
+
+def replacement(x):
+    return "##" + x.group().strip()
 
 class PopeyesSpider(scrapy.Spider):
 
@@ -15,27 +20,32 @@ class PopeyesSpider(scrapy.Spider):
         'https://locations.popeyes.com/',
     )
 
-    def normalize_hours(self, hours, url):
-
+    def normalize_hours(self, hours):
         all_days = []
         reversed_hours = {}
+        day_hours = hours.replace('Open 24 Hours', '').replace('DINE-IN HOURS', '').replace(
+                                  'AM', '').replace('PM','').replace('-', '').strip()
 
-        day_hours = hours.replace('Open 24 Hours', '').replace(
-            'DINE-IN HOURS', '').replace('AM', '').replace('PM','').replace('-', '').split(r' ')
-        day_hours = filter(None, day_hours)
-        day_hours = zip(*[iter(day_hours)]*3)
+        day_hours = re.sub(r'Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday', replacement, day_hours)
+        day_hours = [x for x in filter(None, day_hours.split('##'))]
+
 
         for day_hour in day_hours:
-            short_day = day_hour[0].strip().title()[:2]
-            to_hr = '{}:{}'.format(int(day_hour[2].split(':')[0]) + 12, day_hour[2].split(':')[1])
-            epoch = '{}-{}'.format(day_hour[1].strip(), to_hr.strip())
+            d_hr = [x for x in day_hour.split(' ') if x]
+            if len(d_hr) == 3:
+                short_day = d_hr[0].strip().title()[:2]
+                to_hr = '{}:{}'.format(int(d_hr[2].split(':')[0]) + 12, d_hr[2].split(':')[1])
+                from_hr = '{}:{}'.format(int(d_hr[1].split(':')[0]) + 12, d_hr[1].split(':')[1])
+                epoch = '{}-{}'.format(from_hr.strip(), to_hr.strip())
+            else:
+                short_day = d_hr[0].strip().title()[:2]
+                epoch = '00:00-24:00'
+
             reversed_hours.setdefault(epoch, [])
             reversed_hours[epoch].append(short_day)
-
         if len(reversed_hours) == 1 and list(reversed_hours)[0] == '00:00-24:00':
             return '24/7'
         opening_hours = []
-
         for key, value in reversed_hours.items():
             if len(value) == 1:
                 opening_hours.append('{} {}'.format(value[0], key))
@@ -44,10 +54,11 @@ class PopeyesSpider(scrapy.Spider):
                     '{}-{} {}'.format(value[0], value[-1], key))
         return "; ".join(opening_hours)
 
+
+
     def parse_location(self, location):
 
-        opening_hours = self.normalize_hours(
-            location.xpath(HOURS_XPATH).extract_first(), location.url)
+        opening_hours = self.normalize_hours(location.xpath(HOURS_XPATH).extract_first())
         props = {
             'addr_full': location.xpath(
                 '//meta[@property="restaurant:contact_info:street_address"]/@content').extract_first(),
