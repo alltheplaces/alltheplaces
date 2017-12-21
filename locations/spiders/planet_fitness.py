ff16c3a9e83301161dc9251979c4b30d745fee23
Ian Dees <ian.dees@gmail.com>
2017-12-21 09:39:10 -0600

    Use remove_namespaces to simplify spider code

--- locations/spiders/planet_fitness.py
+++ locations/spiders/planet_fitness.py
@@ -1,30 +1,29 @@
 import scrapy
 from locations.items import GeojsonPointItem
-import json
-from scrapy.http import XmlResponse, HtmlResponse
 import re
 
 
 class PlanetFitnessSpider(scrapy.Spider):
     name = "planet-fitness"
     allowed_domains = ["planetfitness.com"]
-
-    def start_requests(self):
-        sitemap_url = "https://www.planetfitness.com/sitemap.xml"
-        yield scrapy.Request(url=sitemap_url, headers={"accept": "text/xml"})
+    start_urls = (
+        "https://www.planetfitness.com/sitemap.xml",
+    )
 
     def parse(self, response):
-        # wrong content-type from server, hacky solution by specifying HtmlResponse
-        new_response = HtmlResponse(url=response.url, body=response.body)
-        urls = [url for url in new_response.css("loc::text").extract() if "https://www.planetfitness.com/gyms/" in url]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_gym)
+        response.selector.remove_namespaces()
+        city_urls = response.xpath('//url/loc/text()').extract()
+        for path in city_urls:
+            if "https://www.planetfitness.com/gyms/" in path:
+                yield scrapy.Request(
+                    path,
+                    callback=self.parse_gym
+                )
 
     def parse_gym(self, response):
         coordinates = re.findall(r"(?<=\.setLngLat\(\[).*(?=\]\))", response.body_as_unicode())
         lat, lon = coordinates[0].split(", ") if len(coordinates) else [None, None]
-        iframe = response.css("iframe[src*='https://mico.myiclubonline.com']::attr('src')")
-        club_number = iframe.extract_first("=-1").split("=")[-1]
+
         point = {
             "lat": lat,
             "lon": lon,
@@ -36,7 +35,6 @@ class PlanetFitnessSpider(scrapy.Spider):
             "country": response.css(".country::text").extract_first(),
             "phone": response.css(".field--name-field-phone div::text").extract_first(default="").strip(),
             "website": response.url,
-            "opening_hours": "24/7",
             "ref": response.url
         }
 
