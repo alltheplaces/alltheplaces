fe46f9c925637aae48b35004bb83f2f31738bfc2
josh0203e <josh0203be@hotmail.com>
2017-12-20 17:29:33 -0800

    nike

---
+++ locations/spiders/nike.py
@@ -0,0 +1,33 @@
+import scrapy
+import re
+from locations.items import GeojsonPointItem
+
+class NikeSpider(scrapy.Spider):
+
+    name = "nike"
+    allowed_domains = ["www.nike.com"]
+    download_delay = 0.5
+    start_urls = (
+        'https://www.nike.com/us/en_us/retail/en/directory',
+    )
+
+    def parse_stores(self, response):
+        properties = {
+            'name': response.xpath('normalize-space(//meta[@itemprop="name"]/@content)').extract_first(),
+            'addr_full': response.xpath('normalize-space(//meta[@itemprop="streetAddress"]/@content)').extract_first(),
+            'phone':response.xpath('normalize-space(//meta[@itemprop="telephone"]/@content)').extract_first(),
+            'city': response.xpath('normalize-space(//meta[@itemprop="addressLocality"]/@content)').extract_first(),
+            'state':response.xpath('normalize-space(//meta[@itemprop="addressRegion"]/@content)').extract_first(),
+            'postcode': response.xpath('normalize-space(//meta[@itemprop="postalCode"]/@content)').extract_first(),
+            'ref': re.findall(r"[^\/]+$" ,response.url)[0],
+            'website': response.url,
+            'lat': float(response.xpath('normalize-space(//meta[@itemprop="latitude"]/@content)').extract_first()),
+            'lon': float(response.xpath('normalize-space(//meta[@itemprop="longitude"]/@content)').extract_first()),
+        }
+        yield GeojsonPointItem(**properties)
+
+    def parse(self, response):
+        urls = response.xpath('//div[@class="bwt-directory-store-wrapper bwt-hide-on-mobile"]/a/@href').extract()
+        print(urls)
+        for path in urls:
+            yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
