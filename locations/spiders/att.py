1c232bb00311e6f07680e24a06663ce7f0c61f1c
Ian Dees <ian.dees@gmail.com>
2017-12-20 18:41:52 -0600

    Clean up formatting, don't need minidom

--- locations/spiders/att.py
+++ locations/spiders/att.py
@@ -1,32 +1,6 @@
 import scrapy
-from scrapy.spiders import BaseSpider
 from locations.items import GeojsonPointItem
 import json
-import re
-from io import StringIO
-from scrapy.http import HtmlResponse
-from xml.dom import minidom
-
-default_headers = {
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/63.0.3239.84 Safari/537.36"}
-
-
-def process_hours(result):
-    ret = []
-    # for days in hours_str.split(", "):
-    #     range, hours = days.split(" ")
-    #     start_range, end_range = range.split("-")
-    #     start_split, end_split = start_range.split(":"), end_range.split(":")
-    #     start_split[0] = start_split[0].zfill(2)
-    #     end_split[0] = "%02d" % (int(end_split[0]) + 12)
-    #     range = "-".join(x[:2].title() for x in range.split("-"))
-    for day in ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]:
-        open, close = result[day.lower() + "_open"], result[day.lower() + "_close"]
-        if "-1" not in (open, close):
-            ret.append("{} {}-{}".format(day[:2], open, close))
-
-    return "; ".join(ret)
 
 
 class ATTScraper(scrapy.Spider):
@@ -35,27 +9,42 @@ class ATTScraper(scrapy.Spider):
     start_urls = ['https://www.att.com/sitemapfiles/stores-sitemap.xml']
     api_base = "https://www.att.com/apis/maps/v2/locator/place/cpid/{}.json"
 
+    def process_hours(result):
+        ret = []
+        for day in ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]:
+            open, close = result[day.lower() + "_open"], result[day.lower() + "_close"]
+            if "-1" not in (open, close):
+                ret.append("{} {}-{}".format(day[:2], open, close))
+
+        return "; ".join(ret)
+
     def parse(self, response):
-        document = minidom.parseString(response.body_as_unicode())
-        for loc in document.getElementsByTagName("loc"):
-            url = loc.firstChild.nodeValue
-            store_id = url.split("/")[-1]
-            yield scrapy.Request(url=self.api_base.format(store_id), callback=self.parse_location, meta={"url": url})
+        response.selector.remove_namespaces()
+        locations = response.xpath('//url/loc/text()')
+        for loc in locations:
+            url = loc.extract()
+            store_id = url.rsplit('/', 1)[-1]
+            yield scrapy.Request(
+                self.api_base.format(store_id),
+                callback=self.parse_location,
+                meta={"url": loc.extract()}
+            )
 
     def parse_location(self, response):
         results = json.loads(response.body_as_unicode())["results"]
-        if len(results):
+        if results:
             store_data = results[0]
-            yield GeojsonPointItem(lat=store_data["lat"],
-                                   lon=store_data["lon"],
-                                   name=store_data["name"],
-                                   addr_full=(store_data["address1"] + " "
-                                              + (store_data.get("address2") or "")).strip(),
-                                   city=store_data["city"],
-                                   state=store_data["region"],
-                                   postcode=store_data["postal"],
-                                   phone=store_data["phone"].strip(),
-                                   website=response.meta["url"],
-                                   opening_hours = process_hours(store_data),
-                                   ref=store_data["id"]
-                                   )
+            yield GeojsonPointItem(
+                lat=store_data["lat"],
+                lon=store_data["lon"],
+                name=store_data["name"],
+                addr_full=(store_data["address1"] + " "
+                          + (store_data.get("address2") or "")).strip(),
+                city=store_data["city"],
+                state=store_data["region"],
+                postcode=store_data["postal"],
+                phone=store_data["phone"].strip(),
+                website=response.meta["url"],
+                opening_hours=self.process_hours(store_data),
+                ref=store_data["id"]
+            )
