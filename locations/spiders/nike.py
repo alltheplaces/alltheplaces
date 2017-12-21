9d48f5020a58bb6ca4df186c132f189910f0ba39
josh0203e <josh0203be@hotmail.com>
2017-12-20 17:30:32 -0800

    nike

--- locations/spiders/nike.py
+++ locations/spiders/nike.py
@@ -6,7 +6,7 @@ class NikeSpider(scrapy.Spider):
 
     name = "nike"
     allowed_domains = ["www.nike.com"]
-    download_delay = 0.5
+    download_delay = 1.5
     start_urls = (
         'https://www.nike.com/us/en_us/retail/en/directory',
     )
@@ -15,9 +15,9 @@ class NikeSpider(scrapy.Spider):
         properties = {
             'name': response.xpath('normalize-space(//meta[@itemprop="name"]/@content)').extract_first(),
             'addr_full': response.xpath('normalize-space(//meta[@itemprop="streetAddress"]/@content)').extract_first(),
-            'phone':response.xpath('normalize-space(//meta[@itemprop="telephone"]/@content)').extract_first(),
+            'phone': response.xpath('normalize-space(//meta[@itemprop="telephone"]/@content)').extract_first(),
             'city': response.xpath('normalize-space(//meta[@itemprop="addressLocality"]/@content)').extract_first(),
-            'state':response.xpath('normalize-space(//meta[@itemprop="addressRegion"]/@content)').extract_first(),
+            'state': response.xpath('normalize-space(//meta[@itemprop="addressRegion"]/@content)').extract_first(),
             'postcode': response.xpath('normalize-space(//meta[@itemprop="postalCode"]/@content)').extract_first(),
             'ref': re.findall(r"[^\/]+$" ,response.url)[0],
             'website': response.url,
@@ -28,6 +28,5 @@ class NikeSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.xpath('//div[@class="bwt-directory-store-wrapper bwt-hide-on-mobile"]/a/@href').extract()
-        print(urls)
         for path in urls:
             yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
