e9edcaad7f497f2848f7ae48183c59972204f067
evan mosseri <evan.mosseri@gmail.com>
2017-12-20 20:24:48 -0600

    fixed formatting... again.

--- locations/spiders/payless.py
+++ locations/spiders/payless.py
@@ -2,6 +2,20 @@ import scrapy
 from locations.items import GeojsonPointItem
 import json
 
+def process_hours(hours_str):
+    days = hours_str.split("; ")
+    out = []
+    for day in days:
+        if day:
+            prefix, hours = day.split(" ")
+            start, end = hours.split("-")
+            start_hours, start_minutes = int(start[:2]), int(start[2:])
+            end_hours, end_minutes = int(end[:2]), int(end[2:])
+            end_hours += 12
+            formatted = "%s %02d:%02d-%02d:%02d" % (prefix, start_hours, start_minutes, end_hours, end_minutes)
+            out.append(formatted)
+    return "; ".join(out)
+
 
 class PaylessSpider(scrapy.Spider):
     name = "payless"
@@ -25,19 +39,19 @@ class PaylessSpider(scrapy.Spider):
                 website = self.base_url.format(store["number"])
                 point = {}
 
-                point["lat"] = store["latitude"],
-                point["lon"] = store["longitude"],
-                point["name"] = store["name"],
-                point["addr_full"] = "{street}, {city}, {stateCode}, {postalCode}".format(street=street, **store),
-                point["housenumber"] = store["address1"].split(" ")[0] if has_house_number else None,
-                point["street"] = " ".join(store["address1"].split(" ")[1:]) if has_house_number else store["address1"],
-                point["city"] = store["city"],
-                point["state"] = store["stateCode"],
-                point["postcode"] = store["postalCode"],
-                point["country"] = store["countryCode"],
-                point["phone"] = store["phone"],
-                point["website"] = website,
-                point["opening_hours"] = store["storeHours"].replace("<br>", "; ").replace(" :", ": ").title(),
-                point["ref"] = store["number"],
+                point["lat"] = store["latitude"]
+                point["lon"] = store["longitude"]
+                point["name"] = store["name"]
+                point["addr_full"] = "{street}, {city}, {stateCode}, {postalCode}".format(street=street, **store)
+                point["housenumber"] = store["address1"].split(" ")[0] if has_house_number else None
+                point["street"] = " ".join(store["address1"].split(" ")[1:]) if has_house_number else store["address1"]
+                point["city"] = store["city"]
+                point["state"] = store["stateCode"]
+                point["postcode"] = store["postalCode"]
+                point["country"] = store["countryCode"]
+                point["phone"] = store["phone"]
+                point["website"] = website
+                point["opening_hours"] = process_hours(store["storeHours"].replace("<br>", "; ").replace(" :", ": ").title())
+                point["ref"] = store["number"]
 
                 yield GeojsonPointItem(**point)
