aa22e30272a24167253e1cec8c8fec0339a9e3f6
Ian Dees <ian.dees@gmail.com>
2017-12-20 20:57:41 -0600

    Splitting the lat/lon so the comma goes away

--- locations/spiders/currys_pc_word.py
+++ locations/spiders/currys_pc_word.py
@@ -4,18 +4,23 @@ from locations.items import GeojsonPointItem
 
 countries = ['England', 'Uk', 'Ireland', 'Scotland']
 
-formdata = {'sFormName': 'storeFinder',
-                'storeFind': 'true',
-                'subaction': 'stores_map',
-                'sStoreKeywordHidden':'england', # Previous search. The choice of england was arbitrary 
-                'sStoreGeo': '',          # Appears to not be used
-                'sStoreKeyword': ''}      # Search term 
+formdata = {
+    'sFormName': 'storeFinder',
+    'storeFind': 'true',
+    'subaction': 'stores_map',
+    'sStoreKeywordHidden': 'england', # Previous search. The choice of england was arbitrary
+    'sStoreGeo': '',          # Appears to not be used
+    'sStoreKeyword': ''}      # Search term
 
-headers = {'Referer': 'https://www.pcworld.co.uk/gbuk/s/find-a-store.html', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64…) Gecko/20100101 Firefox/57.0'}
+headers = {
+    'Referer': 'https://www.pcworld.co.uk/gbuk/s/find-a-store.html',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64…) Gecko/20100101 Firefox/57.0'
+}
+
 
 class CurrysPcWorldSpider(scrapy.Spider):
     name = "currys"
-    allowed_domains = ["https://www.pcworld.co.uk"]  
+    allowed_domains = ["https://www.pcworld.co.uk"]
 
     def start_requests(self):
         search_url = 'https://www.pcworld.co.uk/gbuk/s/find-a-store.html'
@@ -27,20 +32,21 @@ class CurrysPcWorldSpider(scrapy.Spider):
 
     def parse(self, response):
         stores = response.xpath('//ul[@class = "borderdList storesList"]//li')
-        
-        for store in stores: 
-            latitude, longitude = store.xpath('@data-location').extract_first().split()
+
+        for store in stores:
+            latitude, longitude = store.xpath('@data-location').extract_first().split(', ')
             address = store.css('div[class="address bsp"]').extract_first()
-            address = address.split(sep='\n') # Separating the data 
+            address = address.split(sep='\n') # Separating the data
             address = [i.strip() for i in address] # Lots of whitespace in the websites' text. Removing.
             street_address = address[2]
             city, postal_code = address[3].split(sep=',') # Only relevant section
             store_url = store.css('a::attr(href)').extract_first()
-            
+
             yield GeojsonPointItem(
-                ref = store_url,
-                lat = latitude,
-                lon = longitude,
-                addr_full = street_address,
-                city = city,
-                country = response.meta['country'])  
+                ref=store_url,
+                lat=latitude,
+                lon=longitude,
+                addr_full=street_address,
+                city=city,
+                country=response.meta['country']
+            )
