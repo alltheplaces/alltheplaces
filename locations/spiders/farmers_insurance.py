8c043e42c3d6ef8482dd026456fe654bc94dcc85
Ian Dees <ian.dees@gmail.com>
2017-12-20 20:24:20 -0600

    Formatting

--- locations/spiders/farmers_insurance.py
+++ locations/spiders/farmers_insurance.py
@@ -1,29 +1,25 @@
 import scrapy
-from scrapy.spiders import BaseSpider
 from locations.items import GeojsonPointItem
 import json
-import re
-from io import StringIO
-from scrapy.http import HtmlResponse
-
-def get_hours(hours_json):
-    days = json.loads(hours_json)
-    out_hours = []
-
-    for day in days:
-        start_day = day["day"][:2].title()
-        intervals = day["intervals"]
-        hours = ["%04d-%04d" % (interval["start"], interval["end"]) for interval in intervals]
-        if len(intervals):
-            out_hours.append("{} {}".format(start_day, ",".join(hours)))
-    if len(out_hours):
-        return "; ".join(out_hours)
 
 
 class FarmersInsuranceSpider(scrapy.Spider):
     name = "farmers-insurance"
 
     start_urls = ["https://agents.farmers.com/"]
+
+    def get_hours(self, hours_json):
+        days = json.loads(hours_json)
+        out_hours = []
+
+        for day in days:
+            start_day = day["day"][:2].title()
+            intervals = day["intervals"]
+            hours = ["%04d-%04d" % (interval["start"], interval["end"]) for interval in intervals]
+            if len(intervals):
+                out_hours.append("{} {}".format(start_day, ",".join(hours)))
+        if len(out_hours):
+            return "; ".join(out_hours)
 
     def parse(self, response):
         paths = [response.urljoin(x.xpath("@href").extract_first()) for x in
@@ -45,19 +41,19 @@ class FarmersInsuranceSpider(scrapy.Spider):
             agent_name = agent.css(".location-title-link::text").extract_first()
             hours_json = agent.css(".c-location-hours-details-wrapper").xpath("@data-days").extract_first()
 
-            point = {}
-            point["lat"] = lat.strip()
-            point["lon"] = lon.strip()
-            point["city"] = agent.css(".c-address-city::text").extract_first()
-            point["state"] = agent.css(".c-address-state::text").extract_first()
-            point["postcode"] = agent.css(".c-address-postal-code::text").extract_first()
-            point["country"] = agent.css(".c-address-country-name::text").extract_first()
-            point["phone"] = agent.css(".location-phone a::text").extract_first()
-            point["website"] = response.urljoin(agent.css(".location-title-link").xpath("@href").extract_first())
-            point["ref"] = agent_number
-            point["name"] = "Farmers Insurance Agent - {}, Agent #{}".format(agent_name, agent_number)
-            point["addr_full"] = " ".join([row.css("::text").extract_first() for row in agent.css(".c-AddressRow")][:2])
-            point["opening_hours"] = get_hours(hours_json)
-            yield GeojsonPointItem(
-                **point
-            )
+            point = {
+                "lat": lat.strip(),
+                "lon": lon.strip(),
+                "city": agent.css(".c-address-city::text").extract_first(),
+                "state": agent.css(".c-address-state::text").extract_first(),
+                "postcode": agent.css(".c-address-postal-code::text").extract_first(),
+                "country": agent.css(".c-address-country-name::text").extract_first(),
+                "phone": agent.css(".location-phone a::text").extract_first(),
+                "website": response.urljoin(agent.css(".location-title-link").xpath("@href").extract_first()),
+                "ref": agent_number,
+                "name": "Farmers Insurance Agent - {}, Agent #{}".format(agent_name, agent_number),
+                "addr_full": " ".join([row.css("::text").extract_first() for row in agent.css(".c-AddressRow")][:2]),
+                "opening_hours": self.get_hours(hours_json),
+            }
+
+            yield GeojsonPointItem(**point)
