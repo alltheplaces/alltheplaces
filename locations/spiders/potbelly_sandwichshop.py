1d5a9220bf872ddcb5c04137fc6d9e0a0d8d1ec5
Ian Dees <ian.dees@gmail.com>
2019-02-23 18:29:10 -0600

    Fix potbelly spider

--- locations/spiders/potbelly_sandwichshop.py
+++ locations/spiders/potbelly_sandwichshop.py
@@ -11,20 +11,12 @@ class PotbellySandwichSpider(scrapy.Spider):
     name = "potbelly_sandwich"
     allowed_domains = ["www.potbelly.com"]
     start_urls = (
-        'https://www.potbelly.com/dist/main.js',
+        'https://api-potbelly-production.fuzzstaging.com/proxy/all-locations',
     )
 
     def parse(self, response):
         results = response.body_as_unicode()
-        sub_str = find_between(results, 'staticLocations=', "shown:!0}}]}")
-        js_obj = sub_str + "shown:!0}}]"
-        js_obj = js_obj.replace('!0', 'true')
-        js_obj = js_obj.replace('!1', 'false')
-        js_obj = js_obj.replace(',hours:', ',"hours":')
-        js_obj = js_obj.replace('\\r\\n', '; ')
-        js_obj = re.sub(r'(-?[a-zA-Z]+):(\d{1,2}:)', r'\1: \2', js_obj)
-        cleaner_js = re.sub(r'([a-z_]+):([^/ ])', r'"\1":\2', js_obj)
-        locations = json.loads(cleaner_js)
+        locations = json.loads(results)
 
         for data in locations:
             properties = {
@@ -42,12 +34,3 @@ class PotbellySandwichSpider(scrapy.Spider):
             }
 
             yield GeojsonPointItem(**properties)
-
-
-def find_between(s, first, last):
-    try:
-        start = s.index(first) + len(first)
-        end = s.index(last, start)
-        return s[start:end]
-    except ValueError:
-        return ""
