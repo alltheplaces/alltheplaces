195d6b144ac1bac5c0bebb6175fbc069ecd3dd4e
Jen Weiss <2924736+thismakessand@users.noreply.github.com>
2019-05-06 08:51:22 -0500

    Add Cabela's to Bass Pro Shops spider

--- locations/spiders/bass_pro.py
+++ locations/spiders/bass_pro.py
@@ -1,73 +1,43 @@
 # -*- coding: utf-8 -*-
+import json
+import re
 import scrapy
-from locations.items import GeojsonPointItem
 
-base_url = 'https://stores.basspro.com/'
+from locations.items import GeojsonPointItem
 
 
 class BassProSpider(scrapy.Spider):
     name = "basspro"
     download_delay = 0
-    allowed_domains = ["stores.basspro.com"]
-    start_urls = ['https://stores.basspro.com']
-    times = ''
-
-    def parse_store(self, response):
-        name = response.xpath('//span[@class="location-name-geo"]/text()')\
-            .extract_first()
-        street = response.xpath('//span[@class="c-address-street-1"]/text()')\
-            .extract_first()
-        city = response.xpath('//span[@itemprop="addressLocality"]/text()')\
-            .extract_first()
-        state = response.xpath('//abbr[@itemprop="addressRegion"]/text()')\
-            .extract_first()
-        postcode = response.xpath('//*[@itemprop="postalCode"]/text()') \
-            .extract_first()
-        phone = response.xpath('//span[@id="telephone"]/text()')\
-            .extract_first()
-        lat = response.xpath('//*[@itemprop="latitude"]/@content')\
-            .extract_first()
-        lon = response.xpath('//*[@itemprop="longitude"]/@content')\
-            .extract_first()
-        addr_full = "{} {}, {} {}".format(street, city, state, postcode)
-
-        hours_container = response.xpath(
-            '//div[@class="nap-hours"]//tr[contains('
-            '@class, "-hours-details-row")]')
-
-        for i in hours_container:
-            day = i.xpath('.//td[1]/text()').extract_first()[:2]
-            hour_start = i.xpath(
-                './/*/@data-open-interval-start').extract_first()
-            hour_end = i.xpath(
-                './/*/@data-open-interval-end').extract_first()
-
-            if hour_start:
-                if len(hour_start) < 4:
-                    hour_start = str(hour_start).zfill(4)
-                hour_start = hour_start[:2] + ':' + hour_start[2:]
-                hour_end = hour_end[:2] + ':' + hour_end[2:]
-                self.times += " {} {}-{} ".format(day, hour_start, hour_end)
-            else:
-                self.times += (day + ' off')
-
-        yield GeojsonPointItem(
-            ref=city,
-            name=name,
-            street=street,
-            city=city,
-            state=state,
-            postcode=postcode,
-            addr_full=addr_full,
-            phone=phone,
-            lat=lat,
-            lon=lon,
-            opening_hours=self.times
-        )
-        self.times = ''
+    allowed_domains = ["basspro.com", "cabelas.com", "cabelas.ca"]
+    start_urls = ('https://www.cabelas.com/stores',)
 
     def parse(self, response):
-        stores = response.xpath(
-            '//a[@class="location-card-title-link"]/@href').extract()
-        for store in stores:
-            yield scrapy.Request(base_url + store, callback=self.parse_store)
+        brands = {
+            'cab': "Cabela's",
+            'bps': "Bass Pro Shops"
+        }
+
+        script = response.xpath('//script[contains(text(), "var locs")]').extract_first()
+        data = re.search(r'var\slocs\s=\s\[(.*?)\];', script).groups()[0]
+        data = json.loads('[' + data + ']')
+
+        for store in data:
+            lat, lon = store['point'].split(',')
+
+            yield GeojsonPointItem(
+                ref=store['key'],
+                name=store['name'],
+                city=store['address']['city'],
+                state=store['address']['region'],
+                postcode=store['address']['postalcsode'],
+                addr_full=" ".join([store['address']['address1'], store['address'].get('address2', '')]).strip(),
+                phone=store['phone'],
+                lat=float(lat.strip()),
+                lon=float(lon.strip()),
+                website=response.urljoin(store["pageurl"]),
+                extras={
+                    "brand": brands[store['brand']],
+                    "number": store['key']
+                }
+            )
