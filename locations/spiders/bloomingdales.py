167019ce84e3d99439042a15b3ba7fbbcd795100
Jen Weiss <2924736+thismakessand@users.noreply.github.com>
2019-07-24 15:10:40 -0500

    Fix Macy's (site change); Add Bloomingdale's

--- locations/spiders/bloomingdales.py
+++ locations/spiders/bloomingdales.py
@@ -17,12 +17,12 @@ DAY_MAPPING = {
 }
 
 
-class MacysSpider(scrapy.Spider):
-    name = 'macys'
-    allowed_domains = ['macys.com']
+class BloomingdalesSpider(scrapy.Spider):
+    name = 'bloomingdales'
+    allowed_domains = ['bloomingdales.com']
     download_delay = 0.2
     start_urls = (
-        'https://l.macys.com/',
+        'https://locations.bloomingdales.com/',
     )
 
     def parse_hours(self, hours):
@@ -30,7 +30,7 @@ class MacysSpider(scrapy.Spider):
         opening_hours = OpeningHours()
 
         for hour in hours:
-            if not hour["isClosed"]:
+            if hour["intervals"]:
                 opening_hours.add_range(day=DAY_MAPPING[hour["day"]],
                                         open_time=str(hour["intervals"][0]["start"]),
                                         close_time=str(hour["intervals"][0]["end"]),
@@ -38,17 +38,11 @@ class MacysSpider(scrapy.Spider):
 
         return opening_hours.as_opening_hours()
 
-
     def parse(self, response):
-        urls = response.xpath('//div[@class="Directory-content"]//li/a/@href').extract()
+        urls = response.xpath('//li[@class="c-LocationGridList-item"]//a[contains(text(), "Website")]/@href').extract()
 
-        if not urls:
-            urls = response.xpath('//div[@class="Directory-content"]//li//a[contains(text(), "Store Details")]/@href').extract()
         for url in urls:
-            if '.html' not in url:
-                yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
-            else:
-                yield scrapy.Request(response.urljoin(url))
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
 
     def parse_store(self, response):
 
@@ -57,18 +51,19 @@ class MacysSpider(scrapy.Spider):
 
         properties = {
             'name': brand + ' ' + name,
-            'addr_full': response.xpath('//meta[@itemprop="streetAddress"]/@content').extract_first(),
-            'city': response.xpath('//meta[@itemprop="addressLocality"]/@content').extract_first(),
+            'addr_full': response.xpath('//span[contains(@class, "c-address-street-1")]/text()').extract_first(),
+            'city': response.xpath('//span[@itemprop="addressLocality"]/text()').extract_first(),
             'state': response.xpath('//abbr[@itemprop="addressRegion"]/text()').extract_first(),
-            'postcode': response.xpath('//span[@itemprop="postalCode"]/text()').extract_first(),
+            'postcode': response.xpath('//span[@itemprop="postalCode"]/text()').extract_first().strip(),
             'country': response.xpath('//abbr[@itemprop="addressCountry"]/text()').extract_first(),
             'ref': re.search(r'.*/(.*)$', response.url).groups()[0],
             'lat': response.xpath('//span[@class="coordinates"]/meta[@itemprop="latitude"]/@content').extract_first(),
             'lon': response.xpath('//span[@class="coordinates"]/meta[@itemprop="longitude"]/@content').extract_first(),
-            'phone': response.xpath('//div[@itemprop="telephone"]//a/text()').extract_first(),
-            'website': response.url,
+            'phone': response.xpath('//span[@itemprop="telephone"]/text()').extract_first(),
+            'website': response.url
         }
-        hours = self.parse_hours(response.xpath('//span[contains(@class, "c-hours-today")]/@data-days').extract_first())
+
+        hours = self.parse_hours(response.xpath('//span[contains(@class, "c-location-hours-today")]/@data-days').extract_first())
 
         if hours:
             properties["opening_hours"] = hours
