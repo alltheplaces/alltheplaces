ea4aa625f0504d6ca39b164e889c3c8a0d7e38f2
Nicole <nbeecher@indeed.com>
2019-09-16 10:31:17 -0500

    Fix spider for Tim Hortons

--- locations/spiders/timhortons.py
+++ locations/spiders/timhortons.py
@@ -2,52 +2,67 @@ import scrapy
 import re
 from locations.items import GeojsonPointItem
 
-class TimhortonsSpider(scrapy.Spider):
 
+class TimhortonsSpider(scrapy.Spider):
     name = "timhortons"
     allowed_domains = ["locations.timhortons.com"]
+    custom_settings = {
+        'DUPEFILTER_CLASS': 'scrapy.dupefilters.BaseDupeFilter',
+    }
     download_delay = 0
     start_urls = (
-        'https://locations.timhortons.com/?q_loc=',
+        'https://locations.timhortons.com/',
     )
+
     def parse_stores(self, response):
-        ref = re.findall(r"[^(\/)]+.html$" ,response.url)
-        if(len(ref)>0):
-            ref = ref[0].split('.')[0]
+        name1 = response.xpath('//span[@class="LocationName-brand"]//text()').extract_first()
+        name2 = response.xpath('//span[@class="LocationName-geo"]//text()').extract_first()
+        name = name1 + ' ' + name2
+
         properties = {
+            'ref': response.url,
+            'name': name,
+            'lat': response.xpath('normalize-space(//meta[@itemprop="latitude"]/@content)').extract_first(),
+            'lon': response.xpath('normalize-space(//meta[@itemprop="longitude"]/@content)').extract_first(),
             'addr_full': response.xpath('normalize-space(//meta[@itemprop="streetAddress"]/@content)').extract_first(),
-            'phone': response.xpath('normalize-space(//div[@class="Nap-phone"]/div[@class="c-phone-number c-phone-main-number"]/a[@class="c-phone-number-link c-phone-main-number-link"]/text())').extract_first(),
             'city': response.xpath('normalize-space(//span[@itemprop="addressLocality"]/text())').extract_first(),
             'state': response.xpath('normalize-space(//abbr[@itemprop="addressRegion"]/text())').extract_first(),
+            'country': response.xpath('//address[@class="c-address"]/@data-country').extract_first(),
             'postcode': response.xpath('normalize-space(//span[@itemprop="postalCode"]/text())').extract_first(),
-            'ref': ref,
             'website': response.url,
-            'lat':  response.xpath('normalize-space(//meta[@itemprop="latitude"]/@content)').extract_first(),
-            'lon':  response.xpath('normalize-space(//meta[@itemprop="longitude"]/@content)').extract_first(),
+            'phone': response.xpath(
+                'normalize-space(//div[@class="Nap-phone"]/div[@class="c-phone-number c-phone-main-number"]/a[@class="c-phone-number-link c-phone-main-number-link"]/text())').extract_first()
         }
+
         yield GeojsonPointItem(**properties)
-    def parse_city_stores(self ,response):
-        stores = response.xpath('//div[@class="Teaser-headerWrapper"]/h2/a/@href').extract()
-        for store in stores:
+
+    def parse_city_stores(self, response):
+        stores = response.xpath(
+            '//*[@class="Teaser-titleLink" and @data-ya-track!="nearby_locations"]/@href').extract()
+        if len(stores) != 0:
+            for store in stores:
                 yield scrapy.Request(response.urljoin(store), callback=self.parse_stores)
+        else:
+            yield scrapy.Request(response.url, callback=self.parse_stores)
+
+    def parse_city(self, response):
+        try:
+            cities = response.xpath('//*[@class="c-directory-list-content-item"]/a/@href').extract()
+            for city in cities:
+                yield scrapy.Request(response.urljoin(city), callback=self.parse_city_stores)
+        except:
+            yield scrapy.Request(response.url, callback=self.parse_city_stores)
 
     def parse_state(self, response):
         urls = response.xpath('//div[@class="c-directory-list-content-wrapper"]/ul/li/a/@href').extract()
         for path in urls:
             pattern = re.compile("^[a-z]{2}\/[^()]+\/[^()]+.html$")
             if (pattern.match(path.strip())):
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
-            else:
                 yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
+            else:
+                yield scrapy.Request(response.urljoin(path), callback=self.parse_city)
 
     def parse(self, response):
         urls = response.xpath('//div[@class="c-directory-list-content-wrapper"]/ul/li/a/@href').extract()
         for path in urls:
-            pattern = re.compile("^[a-z]{2}.html$")
-            pattern1 = re.compile("^[a-z]{2}\/[^()]+\/[^()]+.html$")
-            if(pattern.match(path.strip())):
-               yield scrapy.Request(response.urljoin(path), callback=self.parse_state)
-            elif(pattern1.match(path.strip())):
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
-            else:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
+            yield scrapy.Request(response.urljoin(path), callback=self.parse_state)
