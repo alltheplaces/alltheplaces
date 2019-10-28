c48a61a24aa94121dd8b36940db485b76a56094f
hereticaneue <45669137+hereticaneue@users.noreply.github.com>
2019-10-28 13:20:04 -0500

    Fix spider for Staples

--- locations/spiders/staples.py
+++ locations/spiders/staples.py
@@ -2,17 +2,20 @@
 import scrapy
 import json
 import re
-
+import datetime
 from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
 
-US_STATES = [
-  'al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'dc', 'de', 'fl',
-  'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me', 
-  'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 
-  'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri', 
-  'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 
-  'wy'
-]
+
+DAY_MAPPING = {
+    'Mon': 'Mo',
+    'Tue': 'Tu',
+    'Wed': 'We',
+    'Thu': 'Th',
+    'Fri': 'Fr',
+    'Sat': 'Sa',
+    'Sun': 'Su'
+}
 
 class StaplesSpider(scrapy.Spider):
 
@@ -22,66 +25,67 @@ class StaplesSpider(scrapy.Spider):
         'https://stores.staples.com/',
     )
 
-    def get_hours(self, day_hours):
-        opening_hours = []
-        for day in day_hours:
-            hours = []
-            for interval in day['intervals']:
-                if interval['start'] < 1000:
-                    interval['start'] = '0' + str(interval['start'])
-                start = str(interval['start'])
-                end = str(interval['end'])
-                hours.append('{}:{}-{}:{}'.format(start[:2], start[2:], end[:2], end[2:]))
-            current_hours = ', '.join(hours)
-            opening_hours.append('{} {}'.format(day['day'][:2], current_hours))
-        return "; ".join(opening_hours)
+    def parse_hours(self, elements):
+        opening_hours = OpeningHours()
 
-    def get_store_info(self, store):
-        hours = store.xpath('//div[@class="c-location-hours-details-wrapper js-location-hours"]/@data-days').extract_first()
-        opening_hours = self.get_hours(json.loads(hours)) or ''
+        for elem in elements:
+            day = elem.xpath('.//td[@class="c-hours-details-row-day"]/text()').extract_first()
+            intervals = elem.xpath('.//td[@class="c-hours-details-row-intervals"]')
 
-        props = {
-            'addr_full': store.xpath('normalize-space(.//span[@class="c-address-street-1"]/text())').extract_first(),
-            'city': store.xpath('.//span[@class="c-address-city"]/span/text()').extract_first(),
-            'state': store.xpath('.//abbr[@class="c-address-state"]/text()').extract_first(),
-            'postcode': store.xpath('normalize-space(.//span[@class="c-address-postal-code"]/text())').extract_first(),
-            'country': store.xpath('.//abbr[@class="c-address-country-name c-address-country-us"]/text()').extract_first(),
-            'phone': store.xpath('.//span[@class="c-phone-number-span c-phone-main-number-span"]/text()').extract_first(),
-            'ref': store.xpath('.//div[@class="c-address-store-code"]/text()').extract_first(),
-            'website': store.url,
-            'opening_hours': opening_hours,
-            'lat': float(store.xpath('//div[@class="desktop"]//span[@class="coordinates"]/meta[@itemprop="latitude"]/@content').extract_first()) or '',
-            'lon': float(store.xpath('//div[@class="desktop"]//span[@class="coordinates"]/meta[@itemprop="longitude"]/@content').extract_first()) or '',
+            if intervals.xpath('./text()').extract_first() == "Closed":
+                continue
+            if intervals.xpath('./span/text()').extract_first() == "Open 24 hours":
+                opening_hours.add_range(day=DAY_MAPPING[day],
+                                        open_time='0:00',
+                                        close_time='23:59')
+            else:
+                start_time = elem.xpath(
+                    './/span[@class="c-hours-details-row-intervals-instance-open"]/text()').extract_first()
+                end_time = elem.xpath(
+                    './/span[@class="c-hours-details-row-intervals-instance-close"]/text()').extract_first()
+                opening_hours.add_range(day=DAY_MAPPING[day],
+                                        open_time=datetime.datetime.strptime(start_time, '%H:%M %p').strftime('%H:%M'),
+                                        close_time=datetime.datetime.strptime(end_time, '%H:%M %p').strftime('%H:%M'))
+
+        return opening_hours.as_opening_hours()
+
+    def parse_store(self, response):
+        ref = re.search(r'.+/(.+)$', response.url).group(1)
+
+        address1 = response.xpath('//span[@class="c-address-street-1"]/text()').extract_first()
+        address2 = response.xpath('//span[@class="c-address-street-2"]/text()').extract_first() or ''
+
+        properties = {
+            'addr_full': " ".join([address1, address2]).strip(),
+            'phone': response.xpath('//span[@itemprop="telephone"]/text()').extract_first(),
+            'city': response.xpath('//span[@class="c-address-city"]/text()').extract_first(),
+            'state': response.xpath('//span[@itemprop="addressRegion"]/text()').extract_first(),
+            'postcode': response.xpath('//span[@itemprop="postalCode"]/text()').extract_first(),
+            'country': response.xpath('//abbr[@itemprop="addressCountry"]/text()').extract_first(),
+            'ref': ref,
+            'website': response.url,
+            'lat': float(response.xpath('//meta[@itemprop="latitude"]/@content').extract_first()),
+            'lon': float(response.xpath('//meta[@itemprop="longitude"]/@content').extract_first()),
+            'name': response.xpath('//h1[@itemprop="name"]/text()').extract_first()
         }
-        return GeojsonPointItem(**props)
 
-    def parse_store(self, store):
-        city_stores = store.xpath('//div[@class="Directory-wrapper"]//article[@class="Teaser"]//a[@data-ya-track="visit_page"]/@href').extract()
-        if len(city_stores) > 0:
-            for city_store in city_stores:
-                yield scrapy.Request(
-                    store.urljoin(city_store),
-                    callback=self.get_store_info
-                )
-        else:
-            yield self.get_store_info(store)
-    
-    def parse_state(self, response):
-        cities = response.xpath('//li[@class="c-directory-list-content-item"]/a/@href').extract()
-        for city in cities:
-            yield scrapy.Request(
-                response.urljoin(city),
-                callback=self.parse_store
-            )
-    
+        hours = self.parse_hours(response.xpath('//table[@class="c-hours-details"]//tbody/tr'))
+
+        if hours:
+            properties['opening_hours'] = hours
+
+        yield GeojsonPointItem(**properties)
+
     def parse(self, response):
-        states = response.xpath('//a[@class="c-directory-list-content-item-link"]/@href').extract()
-        for state in states:
-            callback = self.parse_state
-            if state not in US_STATES:
-                # states with a single store link directly to the store site
-                callback = self.get_store_info
-            yield scrapy.Request(
-                response.urljoin(state), 
-                callback=callback
-            )
+        urls = response.xpath('//a[@class="Directory-listLink"]/@href').extract()
+        is_store_list = response.xpath('//section[contains(@class,"LocationList")]').extract()
+
+        if not urls and is_store_list:
+            urls = response.xpath('//a[contains(@class,"Teaser-titleLink")]/@href').extract()
+
+        for url in urls:
+
+            if re.search(r'.{2}/.+/.+',url):
+                yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
+            else:
+                yield scrapy.Request(response.urljoin(url))
