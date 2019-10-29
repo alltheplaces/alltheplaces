19974e6fc6f2d188f158905d49ec21b04ace3faa
wessport <wesporter92@gmail.com>
2019-10-29 17:09:23 -0500

    Repairs T-Mobile spider

--- locations/spiders/tmobile_us.py
+++ locations/spiders/tmobile_us.py
@@ -1,100 +1,95 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import json
-import re
+from urllib.parse import urlencode
+
+import scrapy
 
 from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
+DAY_MAPPING = {'Monday': 'Mo',
+               'Tuesday': 'Tu',
+               'Wednesday': 'We',
+               'Thursday': 'Th',
+               'Friday': 'Fr',
+               'Saturday': 'Sa',
+               'Sunday': 'Su'}
 
 
 class TMobileUSSpider(scrapy.Spider):
-
     name = "tmobile_us"
     allowed_domains = ["www.t-mobile.com"]
+    download_delay = 0.2
+
+    def parse_hours(self, store_hours):
+        opening_hours = OpeningHours()
+        if store_hours is None:
+            return
+
+        for store_day in store_hours:
+            day = DAY_MAPPING[store_day.get("day")]
+            open_time = store_day.get("opens")
+            close_time = store_day.get("closes")
+            if open_time is None and close_time is None:
+                continue
+            opening_hours.add_range(day=day,
+                                    open_time=open_time,
+                                    close_time=close_time,
+                                    time_format='%H:%M'
+                                    )
+
+        return opening_hours.as_opening_hours()
 
     def start_requests(self):
-        url = 'https://www.t-mobile.com/srvspub/storelocsearch?prepaidStores=true&authorizedRetailers=true&coporateStores=true&start=%s&count=50&dist=6000&latcentre=45.0&lngcentre=-90.0&sortBy=Store+Type'
+        base_url = 'https://onmyj41p3c.execute-api.us-west-2.amazonaws.com/prod/getStoresByCoordinates?'
 
-        for start in range(0, 6000, 50):
-            yield scrapy.Request(url % start)
+        # params = {
+        #     'latitude': '{}'.format('21.2621394012'),
+        #     'longitude': '{}'.format('-158.1838736264'),
+        #     # 'latitude': '{}'.format(lat),
+        #     # 'longitude': '{}'.format(lon),
+        #     'count': '1000',
+        #     'radius': '25',
+        #     'ignoreLoadingBar': 'false'
+        # }
+        #
+        # yield scrapy.http.Request(base_url + urlencode(params), callback=self.parse)
 
-    def store_hours(self, store_hours):
-        opening_hours = []
-        for day_hours in store_hours:
-            day_hours = day_hours.replace('Monday', 'Mo').replace('Tuesday', 'Tu').replace('Wednesday', 'We').replace('Thursday', 'Th').replace('Friday', 'Fr').replace('Saturday', 'Sa').replace('Sunday', 'Su')
-            day_hours = day_hours.replace('Weekdays', 'Mo-Fr').replace('Weekends', 'Sa-Su').replace('Holidays', 'PH').replace('Everyday', 'Mo-Su')
-            day_hours = day_hours.replace('midnight', '12 PM')
+        with open('./locations/searchable_points/us_centroids_25mile_radius.csv') as points:
 
-            hours = ''
-            match = re.search(r'(\d{1,2}):(\d{2}) (A|P)M to (\d{1,2}):(\d{2}) (A|P)M', day_hours)
-            if match:
-                (f_hr, f_min, f_ampm, t_hr, t_min, t_ampm) = match.groups()
+            next(points)  # Ignore the header
+            for point in points:
+                _, lat, lon = point.strip().split(',')
 
-                f_hr = int(f_hr)
-                if f_ampm == 'P':
-                    f_hr += 12
-                elif f_ampm == 'A' and f_hr == 12:
-                    f_hr = 0
-                t_hr = int(t_hr)
-                if t_ampm == 'P':
-                    t_hr += 12
-                elif t_ampm == 'A' and t_hr == 12:
-                    t_hr = 0
+                params = {
+                    'latitude': '{}'.format(lat),
+                    'longitude': '{}'.format(lon),
+                    'count': '1000',
+                    'radius': '25',
+                    'ignoreLoadingBar': 'false'
+                }
 
-                hours = '{:02d}:{}-{:02d}:{}'.format(
-                    f_hr,
-                    f_min,
-                    t_hr,
-                    t_min,
-                )
-                day_hours = day_hours.replace(match.group(), hours)
-                opening_hours.append(day_hours)
-            else:
-                match = re.search(r'(\d{1,2}):(\d{2}) (A|P)M-(\d{1,2}):(\d{2}) (A|P)M', day_hours)
-                if match:
-                    (f_hr, f_min, f_ampm, t_hr, t_min, t_ampm) = match.groups()
-                    f_hr = int(f_hr)
-                    if f_ampm == 'P':
-                        f_hr += 12
-                    elif f_ampm == 'A' and f_hr == 12:
-                        f_hr = 0
-                    t_hr = int(t_hr)
-                    if t_ampm == 'P':
-                        t_hr += 12
-                    elif t_ampm == 'A' and t_hr == 12:
-                        t_hr = 0
-
-                    hours = '{:02d}:{}-{:02d}:{}'.format(
-                        f_hr,
-                        f_min,
-                        t_hr,
-                        t_min,
-                    )
-                    day_hours = day_hours.replace(match.group(), hours)
-                opening_hours.append(day_hours)
-        return "; ".join(opening_hours)
+                yield scrapy.http.Request(base_url + urlencode(params), callback=self.parse)
 
     def parse(self, response):
-        results = json.loads(response.body_as_unicode())
+        data = json.loads(response.body_as_unicode())
 
-        base_url = 'https://www.t-mobile.com'
-        for store in results['stores']:
+        for store in data:
             properties = {
-                'name': store['store_name'],
-                'city': store['primary_city'],
-                'ref': store['locationID'],
-                'lon': store['long'],
-                'lat': store['lat'],
-                'addr_full': store['addressline'],
-                'phone': store['phone'],
-                'state': store['subdivision'],
-                'country': store['country_region'],
-                'postcode': store['postal_code'],
+                'name': store["name"],
+                'ref': store["id"],
+                'addr_full': store["location"]["address"]["streetAddress"],
+                'city': store["location"]["address"]["addressLocality"],
+                'state': store["location"]["address"]["addressRegion"],
+                'postcode': store["location"]["address"]["postalCode"],
+                'phone': store.get("telephone"),
+                'website': store.get("url") or response.url,
+                'lat': float(store["location"]["latitude"]),
+                'lon': float(store["location"]["longitude"]),
             }
 
-            if 'storeHours' in store:
-                properties['opening_hours'] = self.store_hours(store['storeHours'])
-
-            if 'rspPath' in store:
-                properties['website'] = base_url + store['rspPath']
+            hours = self.parse_hours(store.get("hours", []))
+            if hours:
+                properties["opening_hours"] = hours
 
             yield GeojsonPointItem(**properties)
