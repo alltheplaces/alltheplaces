6e89503af22609dae519126c6abd80a177d9ee3a
chris48s <chris.shaw480@gmail.com>
2019-11-03 21:04:01 +0000

    add spider for Vue

---
+++ locations/spiders/vue.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import json
+from urllib import parse
+import scrapy
+from locations.items import GeojsonPointItem
+
+
+class VueSpider(scrapy.Spider):
+    name = "vue"
+    start_urls = ("https://www.myvue.com/data/locations/",)
+
+    def parse(self, response):
+        data = json.loads(response.body_as_unicode())
+        for letter in data["venues"]:
+            for cinema in letter["cinemas"]:
+                yield response.follow(
+                    f"https://www.myvue.com/cinema/{cinema['link_name']}/getting-here",
+                    self.parse_cinema,
+                )
+
+    def parse_cinema(self, response):
+        cinema_name = response.xpath(
+            '//h2[@class="article-title gradient-fill"]/text()'
+        ).extract_first()
+
+        address_parts = response.xpath('//img[@alt="location-pin"]/../text()').extract()
+        address_parts = [a.strip() for a in address_parts if a.strip()]
+
+        maps_link = response.xpath('//a[text()="Get directions"]/@href').extract_first()
+        query_string = parse.urlparse(maps_link).query
+        query_dict = parse.parse_qs(query_string)
+        coords = query_dict["q"][0].split(",")
+
+        properties = {
+            "ref": response.url.split("/")[4],
+            "name": cinema_name,
+            "addr_full": ", ".join(address_parts),
+            "postcode": address_parts[-1],
+            "lat": float(coords[0]),
+            "lon": float(coords[1]),
+            "website": response.url.replace("/getting-here", ""),
+        }
+        yield GeojsonPointItem(**properties)
