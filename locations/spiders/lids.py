bbb6d62e9fec2f07a5899d294f9bf27ad424a266
LadyLatitude <54322494+LadyLatitude@users.noreply.github.com>
2019-11-07 15:40:11 -0600

    Add spider for Lids.

---
+++ locations/spiders/lids.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from locations.items import GeojsonPointItem
+import json
+from locations.hours import OpeningHours
+import datetime
+
+Days = ['Mo', 'Tu', 'We', 'Th', 'Fr']
+
+class LidsSpider(scrapy.Spider):
+    name = "lids"
+    allowed_domains = ["lids.com"]
+
+    def start_requests(self):
+        url="https://www.lids.com/api/stores?lat=30.2729209&long=-97.74438630000002&num=1200&shipToStore=false"
+        yield scrapy.Request(url)
+
+    def parse_hours(self, hours):
+        opening_hours = OpeningHours()
+
+        if "monFriOpen" in hours:
+            day = 'Mo-Fr'
+            open_time = datetime.datetime.strptime(hours['monFriOpen'], '%H:%M %p').strftime('%H:%M')
+            close_time = datetime.datetime.strptime(hours['monFriClose'], '%H:%M %p').strftime('%H:%M')
+            for day in Days:
+                opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
+
+        if "satOpen" in hours:
+            day = 'Sa'
+            open_time = datetime.datetime.strptime(hours['satOpen'], '%H:%M %p').strftime('%H:%M')
+            close_time = datetime.datetime.strptime(hours['satClose'], '%H:%M %p').strftime('%H:%M')
+            opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
+
+        if "sunOpen" in hours:
+            day = 'Su'
+            open_time = datetime.datetime.strptime(hours['sunOpen'], '%H:%M %p').strftime('%H:%M')
+            close_time = datetime.datetime.strptime(hours['sunClose'], '%H:%M %p').strftime('%H:%M')
+            opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
+
+        return opening_hours.as_opening_hours()
+
+    def parse(self, response):
+        ldata = json.loads(response.body_as_unicode())
+
+        for row in ldata:
+
+            properties = {
+                'ref': row["key"],
+                'name': row["name"],
+                'addr_full': row["address1"],
+                'city': row["city"],
+                'postcode': row["zip"],
+                'lat': row["latitude"],
+                'lon': row["longitude"],
+                'phone': row["phone"],
+                'state': row["state"],
+            }
+
+            #h = ["monFriOpen", "satOpen", "sunOpen", "monFriClose", "satClose", "sunClose"]
+            hours = self.parse_hours(row)
+
+            if hours:
+                properties['opening_hours'] = hours
+
+            yield GeojsonPointItem(**properties)
