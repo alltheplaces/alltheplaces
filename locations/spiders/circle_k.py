4e55ea8d7517726c5ad66ca8f7957899c116f388
Nicole <nbeecher@indeed.com>
2019-11-13 09:50:19 -0600

    Fix spider for Circle K

--- locations/spiders/circle_k.py
+++ locations/spiders/circle_k.py
@@ -10,24 +10,47 @@ class CircleKSpider(scrapy.Spider):
 
     name = "circle_k"
     allowed_domains = ["www.circlek.com"]
+
     start_urls = (
-        'https://www.circlek.com/copy_new_stores.php?lat=36.70099&lng=-95.93642399999999&distance=16000&services=',
+        'https://www.circlek.com/stores_new.php?lat=30.352998399999997&lng=-97.79609599999999&distance=80000&services=&region=global',
     )
 
     def parse(self, response):
         results = json.loads(response.body_as_unicode())
-        for data in results:
-            properties = {
-                'ref': data['id'],
-                'lat': float(data['latitude']) if data['latitude'] else None,
-                'lon': float(data['longitude']) if data['longitude'] else None,
-                'addr_full': data['address'],
-                'website': 'https://www.circlek.com/store-locator/us/{}/{}/{}'.format(
-                    data['city'].lower(),
-                    data['address'].lower().replace(' ', '-'),
-                    data['cost_center'],
-                ),
-                'city': data['city'],
-            }
 
-            yield GeojsonPointItem(**properties)
+        for storeid, store in results['stores'].items():
+            yield scrapy.Request(url=response.urljoin(store['url']), callback=self.parse_state,
+                                 meta={'ref': store['cost_center'], 'addr_full': store['address'],
+                                       'city': store['city'], 'country': store['country'], 'lat': store['latitude'],
+                                       'lon': store['longitude']})
+
+    def parse_state(self, response):
+        ## Not all countries/store pages follow the same format
+        if response.meta['country'] == 'US':
+            state = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
+            postal = response.xpath('//*[@class="heading-small"]/span[3]/text()').extract_first()
+        elif response.meta['country'] in ['CA', 'Canada']:
+            if len(response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()) < 4:
+                state = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
+                postal = response.xpath('//*[@class="heading-small"]/span[3]/text()').extract_first()
+            else:
+                state = ""
+                postal = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
+        else:
+            state = ""
+            postal = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
+
+        properties = {
+            'ref': response.meta['ref'],
+            'name': 'Circle K',
+            'addr_full': response.meta['addr_full'],
+            'city': response.meta['city'],
+            'state': state,
+            'postcode': postal,
+            'country': response.meta['country'],
+            'lat': response.meta['lat'],
+            'lon': response.meta['lon'],
+            'website': response.url
+        }
+
+        yield GeojsonPointItem(**properties)
