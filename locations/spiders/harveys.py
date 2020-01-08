238e47e3df7afa3b9fd704f5e3f051ae26b4d3db
nbeecher <35203761+nbeecher@users.noreply.github.com>
2020-01-08 15:14:01 -0600

    Add session id refresh for each run

--- locations/spiders/harveys.py
+++ locations/spiders/harveys.py
@@ -13,11 +13,20 @@ class HarveysSpider(scrapy.Spider):
     item_attributes = {'brand': "Harvey's"}
     allowed_domains = ['harveys.ca']
     start_urls = [
-        'https://aws-api.harveys.ca/CaraAPI/servlet/VESBCmdServlet?application=VECOMV1&service=OrganizationService&command=getStoreList&reqJSON=%7B%22request%22%3A%7B%22requestHeader%22%3A%7B%22caller%22%3A%22Mobile%22%2C%22sessionId%22%3A%224297736%22%7D%2C%22requestContent%22%3A%7B%22@class%22%3A%22storeListRqstModel%22%2C%22eCommOnly%22%3A%22N%22%2C%22fromLatitude%22%3A90.000%2C%22toLatitude%22%3A0.000%2C%22fromLongitude%22%3A-180.000%2C%22toLongitude%22%3A-1.56301%7D%7D%7D',
+        'https://aws-api.harveys.ca/CaraAPI/servlet/VESBCmdServlet?application=VECOMV1&service=AuthenticationService&command=createSession&reqJSON=%7B%22request%22%3A%7B%22requestHeader%22%3A%7B%22caller%22%3A%22Mobile%22%7D%2C%22requestContent%22%3A%7B%22@class%22%3A%22createSessionRqstModel%22%2C%22lang%22%3A%22en%22%2C%22version%22%3A%221.0.1%22%2C%22appType%22%3A%22web%22%7D%7D%7D',
     ]
-    download_delay = 2
 
     def parse(self, response):
+        base_url = 'https://aws-api.harveys.ca/CaraAPI/servlet/VESBCmdServlet?application=VECOMV1&service=OrganizationService&command=getStoreList&reqJSON=%7B%22request%22%3A%7B%22requestHeader%22%3A%7B%22caller%22%3A%22Mobile%22%2C%22sessionId%22%3A%22{session_id}%22%7D%2C%22requestContent%22%3A%7B%22@class%22%3A%22storeListRqstModel%22%2C%22eCommOnly%22%3A%22N%22%2C%22fromLatitude%22%3A90.0000%2C%22toLatitude%22%3A0.00000%2C%22fromLongitude%22%3A-180.0000%2C%22toLongitude%22%3A-1.56301%7D%7D%7D'
+        info = json.loads(response.body_as_unicode())
+
+        session_id = info["response"]["responseContent"]["veSessionID"]
+
+        url = base_url.format(session_id=session_id)
+        yield scrapy.Request(url=url, callback=self.parse_places)
+
+    def parse_places(self, response):
+        print(response.url)
         places = json.loads(response.body_as_unicode())
 
         for place in places["response"]["responseContent"]["storeModel"]:
