aa33080a572c751077bf33cd005a18bd210e43b3
nbeecher <35203761+nbeecher@users.noreply.github.com>
2020-01-08 11:27:21 -0600

    add download delay

--- locations/spiders/harveys.py
+++ locations/spiders/harveys.py
@@ -15,6 +15,7 @@ class HarveysSpider(scrapy.Spider):
     start_urls = [
         'https://aws-api.harveys.ca/CaraAPI/servlet/VESBCmdServlet?application=VECOMV1&service=OrganizationService&command=getStoreList&reqJSON=%7B%22request%22%3A%7B%22requestHeader%22%3A%7B%22caller%22%3A%22Mobile%22%2C%22sessionId%22%3A%224297736%22%7D%2C%22requestContent%22%3A%7B%22@class%22%3A%22storeListRqstModel%22%2C%22eCommOnly%22%3A%22N%22%2C%22fromLatitude%22%3A90.000%2C%22toLatitude%22%3A0.000%2C%22fromLongitude%22%3A-180.000%2C%22toLongitude%22%3A-1.56301%7D%7D%7D',
     ]
+    download_delay = 2
 
     def parse(self, response):
         places = json.loads(response.body_as_unicode())
