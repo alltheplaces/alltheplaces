707885dd1524b2187c716aa9b9802c6782d7e807
thekuiperbelt <25801804+thekuiperbelt@users.noreply.github.com>
2020-01-08 12:26:18 -0600

    Fixed spider, it was yielding incorrect location information

--- locations/spiders/dierbergs.py
+++ locations/spiders/dierbergs.py
@@ -4,37 +4,27 @@ import scrapy
 from locations.items import GeojsonPointItem
 
 class DierbergsSpider(scrapy.Spider):
+    download_delay = 0.2
     name = "dierbergs"
-    item_attributes = { 'brand': "Dierberg's" }
+    item_attributes = {'brand': "Dierberg's"}
     allowed_domains = ["www.dierbergs.com"]
 
-    start_urls = (
-        'http://www.dierbergs.com/MyDierbergs/Locations',
-    )
-
-    def parse_Ref(self, data):
-        data = data.xpath(".//img")[0]
-        ref = data.xpath("@alt").extract_first()
-        match = re.search(r'Store #(\d{1,})', ref)
-        ref = match.groups()[0]
-        return ref
 
     def parse(self, response):
         data = response.xpath('.//div[@class="location-listing-item row"]')
+        base_url = 'http://www.dierbergs.com'
 
         for store in data:
-            ref = self.parse_Ref(store)
             properties = {
-                'ref': ref,
-                'addr_full': store.xpath("//span[@class='address']//text()").extract_first().strip(),
-                'city': store.xpath("//span[@class='city']//text()").extract_first().strip(),
-                'state': store.xpath("//span[@class='state']//text()").extract_first().strip(),
-                'postcode': store.xpath("//span[@class='zip']//text()").extract_first().strip(),
-                'phone': store.xpath("//span[@class='phone']//text()").extract_first().strip(),
-                'name': store.xpath(".//strong//text()").extract_first().strip(),
+                'ref': store.xpath(".//strong//text()").extract_first().strip(),
+                'addr_full': store.xpath(".//span[@class='address']//text()").extract_first().strip(),
+                'city': store.xpath(".//span[@class='city']//text()").extract_first().strip(),
+                'state': store.xpath(".//span[@class='state']//text()").extract_first().strip(),
+                'postcode': store.xpath(".//span[@class='zip']//text()").extract_first().strip(),
+                'phone': store.xpath(".//span[@class='phone']//a//text()").extract_first().strip(),
                 'lon': store.xpath("@data-lon").extract_first(),
-                'lat': store.xpath("@data-lat").extract_first()
+                'lat': store.xpath("@data-lat").extract_first(),
+                'website': base_url + store.xpath(".//a[@target='_top']//@href").extract_first()
             }
 
             yield GeojsonPointItem(**properties)
-            
