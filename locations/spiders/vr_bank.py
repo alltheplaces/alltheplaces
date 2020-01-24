805d69950222bb9c88426bbefd25ac3d3e307227
Kalpesh <kalpesh@Kalpeshs-MacBook-Air.local>
2020-01-24 15:10:35 -0500

    Add spider for vr.de bank

---
+++ locations/spiders/vr_bank.py
@@ -0,0 +1,146 @@
+import scrapy
+import json
+import re
+
+from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
+
+DAY_MAPPING = {
+    'Montag': 'Mo',
+    'Dienstag': 'Tu',
+    'Mittwoch': 'We',
+    'Donnerstag': 'Th',
+    'Freitag': 'Fr',
+    'Samstag': 'Sa',
+    'Sonntag': 'Su'
+}
+
+
+class VRBankSpider(scrapy.Spider):
+    name = "vr_bank"
+    allowed_domains = ["www.vr.de"]
+    start_urls = ['https://www.vr.de/service/filialen-a-z/a.html']
+
+    download_delay = 1
+
+    def process_hours(self, store_hours):
+        opening_hours = OpeningHours()
+
+        day = time = open_time = close_time = ""
+
+        if store_hours is None:
+            return
+
+        for hour in store_hours:
+            hour = hour.replace(": ", "#")
+            try:
+                day, time = hour.split("#")
+            except Exception:
+                pass
+
+            if time:
+                time = time.replace("24:00", "00:00")
+                tms = []
+                if 'und' in time:
+                    tms = [tm.strip() for tm in time.split('und')]
+                else:
+                    tms.append(time)
+
+                for tm in tms:
+                    try:
+                        open_time, close_time = [t.strip() for t in tm.replace('Uhr', '').strip().split("-")]
+                    except Exception:
+                        pass
+
+                    if open_time and close_time and day:
+                        opening_hours.add_range(
+                            day=DAY_MAPPING[day],
+                            open_time=open_time,
+                            close_time=close_time,
+                            time_format='%H:%M'
+                        )
+                    else:
+                        continue
+
+        return opening_hours.as_opening_hours()
+
+    def parse_details(self, response):
+        name = street = zip = city = phone = website = latitude = longitude = ""
+
+        try:
+            name = response.xpath('//h1[@itemprop="name"]/text()').get()
+        except Exception:
+            pass
+
+        try:
+            street = response.xpath('//span[@itemprop="streetAddress"]/text()').get()
+        except Exception:
+            pass
+        try:
+            zip = response.xpath('//span[@itemprop="postalCode"]/text()').get()
+        except Exception:
+            pass
+
+        try:
+            city = response.xpath('//span[@itemprop="addressLocality"]/text()').get()
+        except Exception:
+            pass
+
+        try:
+            phone = response.xpath('//li[@itemprop="telephone"]/a/span/text()').get()
+        except Exception:
+            pass
+
+        try:
+            website = response.xpath('//li[@itemprop="url"]/a/span/text()').get()
+        except Exception:
+            pass
+
+        try:
+            latitude = re.search(r'"lat":([-+]?[0-9]*\.?[0-9]*)', response.text)
+        except Exception:
+            pass
+
+        try:
+            longitude = re.search(r'"lng":([-+]?[0-9]*\.?[0-9]*)', response.text)
+        except Exception:
+            pass
+
+        try:
+            hours = response.xpath('//p[@itemprop="openingHoursSpecification"]/text()').getall()
+        except Exception:
+            pass
+
+        properties = {
+            'ref': response.meta.get('url'),
+            'name': name,
+            'city': city,
+            'postcode': zip,
+            'phone': phone,
+            'website': website,
+            'lat': latitude,
+            'lon': longitude
+        }
+
+        if hours:
+            properties['opening_hours'] = self.process_hours(hours)
+
+        yield GeojsonPointItem(**properties)
+
+    def get_index(self, response):
+        list = response.xpath('//div[has-class("module module-teaserlist ym-clearfix")]/div/a/@href').getall()
+        for item in list:
+            yield scrapy.Request(
+                url=item,
+                callback=self.parse_details,
+                meta={'url': item}
+            )
+
+    def parse(self, response):
+        index = response.xpath('//div[has-class("module module-linklist ym-clearfix")]/ul/li/a/@href').getall()
+        for page in index:
+            yield scrapy.Request(
+                url=page,
+                callback=self.get_index
+            )
