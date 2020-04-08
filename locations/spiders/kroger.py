0202194cd7c81bd3b48e480d24942df550170781
Thomas Garner <thomas536@gmail.com>
2020-04-07 18:09:09 -0700

    Convert Kroger to use sitemap and ld+json
    When I ran it initally, it hang on the intial request. So using more
    browswer like headers allowed the response to complete. I was able to
    get some requests in, but eventually it started returning 403s. I added
    a delay, but I have no idea what will keep it below their crawling
    threshold.
    
    Added wikidata.

--- locations/spiders/kroger.py
+++ locations/spiders/kroger.py
@@ -1,142 +1,74 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import json
+import scrapy
 
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
+from scrapy.selector import Selector
+
+
+default_headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/63.0.3239.84 Safari/537.36"}
 
 
 class KrogerSpider(scrapy.Spider):
     name = "kroger"
-    item_attributes = { 'brand': "Kroger" }
+    item_attributes = { 'brand': "Kroger", 'brand_wikidata': "Q153417" }
     allowed_domains = ["www.kroger.com"]
-    start_urls = (
-        'https://www.kroger.com/stores?address=37.7578595,-79.76804&includeThirdPartyFuel=true&maxResults=50&radius=3000&showAllStores=false&useLatLong=true',
-    )
+    download_delay = 0.2
 
-    store_types = {
-        '' : "unknown-blank",
-        'C': "grocery",
-        'F': "unknown-f",
-        'G': "gas station",
-        'I': "unknown-i",
-        'J': "unknown-j",
-        'M': "grocery",
-        'Q': "unknown-q",
-        'S': "grocery",
-        'X': "unknown-x",
-    }
+    def start_requests(self):
+        urls = [
+	    'https://www.kroger.com/storelocator-sitemap.xml',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, headers=default_headers)
 
-    ll_requests = set()
-
-    def store_hours(self, store_hours):
-        if all([h == '' for h in store_hours.values()]):
-            return None
-        else:
-            day_groups = []
-            this_day_group = None
-            for day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'):
-                day_open = store_hours[day + 'Open']
-                day_close = store_hours[day + 'Close']
-                hours = day_open + "-" + day_close
-                day_short = day.title()[:2]
-
-                if not this_day_group:
-                    this_day_group = dict(from_day=day_short, to_day=day_short, hours=hours)
-                elif this_day_group['hours'] == hours:
-                    this_day_group['to_day'] = day_short
-                elif this_day_group['hours'] != hours:
-                    day_groups.append(this_day_group)
-                    this_day_group = dict(from_day=day_short, to_day=day_short, hours=hours)
-            day_groups.append(this_day_group)
-
-            if len(day_groups) == 1:
-                opening_hours = day_groups[0]['hours']
-                if opening_hours == '07:00-07:00':
-                    opening_hours = '24/7'
-            else:
-                opening_hours = ''
-                for day_group in day_groups:
-                    if day_group['from_day'] == day_group['to_day']:
-                        opening_hours += '{from_day} {hours}; '.format(**day_group)
-                    else:
-                        opening_hours += '{from_day}-{to_day} {hours}; '.format(**day_group)
-                opening_hours = opening_hours[:-2]
-
-            return opening_hours
-
-    def phone_number(self, phone):
-        return '{}-{}-{}'.format(phone[0:3], phone[3:6], phone[6:10])
-
-    def address(self, address):
-        if not address:
-            return None
-
-        (num, rest) = address['addressLineOne'].split(' ', 1)
-        addr_tags = {
-            "housenumber": num.strip(),
-            "street": rest.strip(),
-            "city": address['city'],
-            "state": address['state'],
-            "postcode": address['zipCode'],
-        }
-
-        return addr_tags
 
     def parse(self, response):
-        data = json.loads(response.body_as_unicode())
+        xml = Selector(response)
+        xml.remove_namespaces()
 
-        bounding_box = {
-            'min_lat': 100,
-            'max_lat': -100,
-            'min_lon': 300,
-            'max_lon': -300,
+        urls = xml.xpath('//loc/text()').extract()
+        for url in urls:
+            yield scrapy.Request(url, callback=self.parse_store, headers=default_headers)
+
+    def parse_hours(self, hours):
+        opening_hours = OpeningHours()
+
+        for hour in hours:
+            try:
+                opening_hours.add_range(day=hour["dayOfWeek"].replace("http://schema.org/", "")[:2],
+                                        open_time=hour["opens"],
+                                        close_time=hour["closes"],
+                                        time_format="%H:%M:%S")
+            except:
+                continue  # closed or no time range given
+
+    def parse_store(self, response):
+        data = response.xpath('//script[@type="application/ld+json"]/text()').extract_first()
+        if data:
+            data = json.loads(data)
+        else:
+            return
+
+        properties = {
+            'ref': response.url,
+            'name': "%s Kroger" % data["name"],
+            'addr_full': data["address"]["streetAddress"].strip(),
+            'city': data["address"]["addressLocality"].strip(),
+            'state': data["address"]["addressRegion"],
+            'postcode': data["address"]["postalCode"],
+            'country': data["address"].get("addressCountry"),
+            'phone': data.get("telephone"),
+            'lat': float(data["geo"]["latitude"]),
+            'lon': float(data["geo"]["longitude"]),
+            'website': data.get("url") or response.url,
         }
 
-        for store in data:
-            store_information = store['storeInformation']
-            store_hours = store['storeHours']
+        hours = self.parse_hours(data['openingHoursSpecification'])
+        if hours:
+            properties["opening_hours"] = hours
 
-            properties = {
-                "phone": self.phone_number(store_information['phoneNumber']),
-                "ref": store_information['recordId'],
-                "name": store_information['localName'],
-                "type": self.store_types[store_information['storeType']],
-                "opening_hours": self.store_hours(store_hours),
-            }
-
-            address = self.address(store_information['address'])
-            if address:
-                properties.update(address)
-
-            lon_lat = [
-                float(store_information['latLong']['longitude']),
-                float(store_information['latLong']['latitude']),
-            ]
-
-            bounding_box['min_lat'] = min(bounding_box['min_lat'], lon_lat[1])
-            bounding_box['max_lat'] = max(bounding_box['max_lat'], lon_lat[1])
-            bounding_box['min_lon'] = min(bounding_box['min_lon'], lon_lat[0])
-            bounding_box['max_lon'] = max(bounding_box['max_lon'], lon_lat[0])
-
-            yield GeojsonPointItem(**properties)
-
-        if data:
-            box_corners = [
-                '{},{}'.format(bounding_box['min_lat'], bounding_box['min_lon']),
-                '{},{}'.format(bounding_box['max_lat'], bounding_box['min_lon']),
-                '{},{}'.format(bounding_box['min_lat'], bounding_box['max_lon']),
-                '{},{}'.format(bounding_box['max_lat'], bounding_box['max_lon']),
-            ]
-
-            for corner in box_corners:
-                if corner in self.ll_requests:
-                    self.logger.info("Skipping request for %s because we already did it", corner)
-                else:
-                    self.ll_requests.add(corner)
-                    yield scrapy.Request(
-                        'https://www.kroger.com/stores?address={}&includeThirdPartyFuel=true&maxResults=50&radius=3000&showAllStores=false&useLatLong=true'.format(
-                            corner
-                        ),
-                    )
-        else:
-            self.logger.info("No results")
+        yield GeojsonPointItem(**properties)
