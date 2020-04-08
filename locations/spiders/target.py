a39cef7b8cb69db607103cdd449b18751f9780fe
Thomas Garner <thomas536@gmail.com>
2020-04-07 17:24:31 -0700

    Convert Target to use sitemap and use ld+json
    This was returning zero results before due to the search url returning
    503.

--- locations/spiders/target.py
+++ locations/spiders/target.py
@@ -1,99 +1,62 @@
 import json
 import scrapy
+
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
-
-
-def process_hours(hours):
-    if 'Hours' not in hours:
-        return None
-
-    opening_hours = ''
-    days = hours['Hours']
-    for day in days:
-        shortname = day['ShortName']
-        if '-' in shortname:
-            startday, endday = shortname.split('-')
-            if startday == "M":
-                startday = "Mo"
-            if endday == "M":
-                endday = "Mo"
-            shortname = '-'.join([startday, endday])
-        timeperiod = day['TimePeriod']
-        starttime = timeperiod['BeginTime'][:-3]
-        endtime = timeperiod['ThruTime'][:-3]
-        time_together = '-'.join([starttime, endtime])
-        opening_hours += "{shortname} {time_together};".format(
-            shortname=shortname,
-            time_together=time_together
-        )
-    return opening_hours[:-1]
+from scrapy.selector import Selector
 
 
 class TargetSpider(scrapy.Spider):
-    ''' The Target api allows us a maximum search radius of 625 miles, so we have
-        to use multiple api requests with zip codes from different states to
-        search the whole country.
-
-        NOTE: There is a "key" variable used in the url for the requests.
-        It seems that this does not vary between natural requests (those sent
-        from a browser), but it might be something that expires and renders requests
-        invalid. So, if this spider stops working, that's probably why.
-    '''
     name = "target"
-    item_attributes = { 'brand': "Target" }
+    item_attributes = { 'brand': "Target", 'brand_wikidata': "Q1046951" }
     allowed_domains = ["target.com"]
-
-    def start_requests(self):
-        zips = [
-            '97062', '90021', '73301', '82633', '29401', '60007', '04019',
-            '87101', '59601', '39056', '14201', '55111', '94203', '64030',
-            '33601', '10001', '98101', '37011', '58501', '78501', '97501'
-        ]
-
-        template = 'https://api.target.com/v2/store?nearby={zipcode}&range=625&limit=999999&locale=en-US&key=eb2551e4accc14f38cc42d32fbc2b2ea'
-
-        headers = {
-            'Accept': 'application/json',
-        }
-
-        for zipcode in zips:
-            yield scrapy.http.FormRequest(
-                url=template.format(zipcode=zipcode),
-                method='GET',
-                headers=headers,
-                callback=self.parse
-            )
-
+    start_urls = (
+        'https://www.target.com/sl/sitemap_001.xml.gz',
+    )
 
     def parse(self, response):
-        data = json.loads(response.body_as_unicode())
-        stores = data['Locations']['Location']
-        for store in stores:
-            loc_info = store['Address']
-            properties = {
-                'ref': store['ID'],
-                'name': store['Name'],
-                'addr_full': loc_info['AddressLine1'],
-                'city': loc_info['City'],
-                'state': loc_info['Subdivision'],
-                'country': loc_info['CountryName'],
-                'postcode': loc_info['PostalCode'],
-                'lat': loc_info['Latitude'],
-                'lon': loc_info['Longitude'],
-            }
+        xml = Selector(response)
+        xml.remove_namespaces()
 
-            phones = store['TelephoneNumber']
-            if type(phones) == "list":
-                for i in phones:
-                    if i['FunctionalTypeDescription'] == 'Main':
-                        properties['phone'] = i['PhoneNumber']
-            elif type(phones) == 'dict':
-                if phones['FunctionalTypeDescription'] == 'Main':
-                    properties['phone'] = phones['PhoneNumber']
+        urls = xml.xpath('//loc/text()').extract()
+        for url in urls:
+            yield scrapy.Request(url, callback=self.parse_store)
 
-            if 'OperatingHours' in store:
-                processed_hours = process_hours(store['OperatingHours'])
-                if processed_hours:
-                    properties['opening_hours'] = processed_hours
+    def parse_hours(self, hours):
+        opening_hours = OpeningHours()
 
-            yield GeojsonPointItem(**properties)
+        for hour in hours:
+            try:
+                opening_hours.add_range(day=hour["dayOfWeek"].replace("http://schema.org/", "")[:2],
+                                        open_time=hour["opens"],
+                                        close_time=hour["closes"],
+                                        time_format="%H:%M:%S")
+            except:
+                continue  # closed or no time range given
+
+    def parse_store(self, response):
+        data = response.xpath('//script[@type="application/ld+json"]/text()').extract_first()
+        if data:
+            data = json.loads(data)
+        else:
+            return
+
+        properties = {
+            'ref': response.url.split('/')[-1],
+            'name': data["name"],
+            'addr_full': data["address"]["streetAddress"].strip(),
+            'city': data["address"]["addressLocality"].strip(),
+            'state': data["address"]["addressRegion"],
+            'postcode': data["address"]["postalCode"],
+            'country': data["address"].get("addressCountry"),
+            'phone': data.get("telephone"),
+            'lat': float(data["geo"]["latitude"]),
+            'lon': float(data["geo"]["longitude"]),
+            'website': data.get("url") or response.url,
+        }
+
+        hours = self.parse_hours(data['openingHoursSpecification'])
+        if hours:
+            properties["opening_hours"] = hours
+
+        yield GeojsonPointItem(**properties)
