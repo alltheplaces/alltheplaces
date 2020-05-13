3ab53ebbec9d79e81bb990bb4e2eba49aa56365c
Reid Beels <reid@thedyrt.com>
2020-05-13 11:25:06 -0700

    Add amenity and diesel tagging to Casey's spider

--- locations/spiders/caseys_general_store.py
+++ locations/spiders/caseys_general_store.py
@@ -13,7 +13,7 @@ Seven_Days = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
 
 class CaseysGeneralStoreSpider(scrapy.Spider):
     name = "caseys_general_store"
-    item_attributes = { 'brand': "Casey's General Store" }
+    item_attributes = {'brand': "Casey's General Store"}
     allowed_domains = ["caseys.com"]
 
     def start_requests(self):
@@ -47,26 +47,32 @@ class CaseysGeneralStoreSpider(scrapy.Spider):
                 if ":" in open_time:
                     open_time = datetime.strptime(open_time, '%I:%M%p')
                 else:
-                    open_time = datetime.strptime(open_time, '%I%p').strftime('%H:%M')
+                    open_time = datetime.strptime(
+                        open_time, '%I%p').strftime('%H:%M')
                 if ":" in close_time:
                     close_time = datetime.strptime(close_time, '%I:%M%p')
                 else:
-                    close_time = datetime.strptime(close_time, '%I%p').strftime('%H:%M')
+                    close_time = datetime.strptime(
+                        close_time, '%I%p').strftime('%H:%M')
             else:
                 for day in Seven_Days:
-                    opening_hours.add_range(day=day, open_time="00:00", close_time="00:00", time_format='%H:%M')
+                    opening_hours.add_range(
+                        day=day, open_time="00:00", close_time="00:00", time_format='%H:%M')
 
             if operation_times[0] == 'Mon-Sun':
                 for day in Seven_Days:
-                    opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
+                    opening_hours.add_range(
+                        day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
 
             elif operation_times[0] == 'Mon-Fri':
                 for day in Seven_Days[:5]:
-                    opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
+                    opening_hours.add_range(
+                        day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
 
             elif operation_times[0] == 'Sat-Sun':
                 for day in Seven_Days[5:]:
-                    opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
+                    opening_hours.add_range(
+                        day=day, open_time=open_time, close_time=close_time, time_format='%H:%M')
 
         return opening_hours.as_opening_hours()
 
@@ -76,6 +82,7 @@ class CaseysGeneralStoreSpider(scrapy.Spider):
         for store in data["stores"]:
             postal = store["address"]["postalCode"]
             postal = postal.split("-")
+            amenities = store["amenities"]
 
             properties = {
                 'ref': store["code"],
@@ -88,7 +95,13 @@ class CaseysGeneralStoreSpider(scrapy.Spider):
                 'lat': store["geoPoint"]["latitude"],
                 'lon': store["geoPoint"]["longitude"],
                 'phone': store["address"]["phone"],
-                'website': "https://www.caseys.com" + store["locationUrl"]
+                'website': "https://www.caseys.com" + store["locationUrl"],
+                'extras': {
+                    'amenity:fuel': True,
+                    'car_wash': any('carwash' == a['key'] for a in amenities) or None,
+                    'fuel:diesel': any('diesel' == a['key'] for a in amenities) or None,
+                    'shop': 'convenience'
+                }
             }
 
             try:
