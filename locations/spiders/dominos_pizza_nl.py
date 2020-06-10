91a936ebc373aba07f8f0ef66399e3d40304a5ab
thekuiperbelt <25801804+thekuiperbelt@users.noreply.github.com>
2020-06-10 10:24:52 -0500

    Adding international spiders for Dominos Pizza

--- locations/spiders/dominos_pizza_nl.py
+++ locations/spiders/dominos_pizza_nl.py
@@ -6,24 +6,19 @@ from locations.items import GeojsonPointItem
 
 
 class DomionsPizzaWorldwideSpider(scrapy.Spider):
-    name = "dominos_pizza_de"
-    item_attributes = {'brand': "Dominos Pizza Germany"}
-    allowed_domains = ["dominos.de"]
+    name = "dominos_pizza_nl"
+    item_attributes = {'brand': "Dominos Pizza Netherlands"}
+    allowed_domains = ["dominos.nl"]
 
     start_urls = (
-        'https://www.dominos.de/store',
+        'https://www.dominos.nl/winkels',
     )
 
     def parse(self,response):
-        store_urls = response.xpath('//link[@rel="canonical"]/@href').extract()
+        store_urls = response.xpath('//ul[@id="region-links"]/li/a/@href').extract()
         for store_url in store_urls:
-            yield scrapy.Request(response.urljoin(store_url), callback=self.parse_region)
+            yield scrapy.Request(response.urljoin(store_url), callback=self.parse_locality)
 
-
-    def parse_region(self, response):
-        regions = response.xpath('//ul[@id="region-links"]/li/a/@href').extract()
-        for region in regions:
-            yield scrapy.Request(response.urljoin(region), callback=self.parse_locality)
 
     def parse_locality(self, response):
         stores = response.xpath('//div[@class="store-information"]/h4/a/@href').extract()
@@ -31,7 +26,7 @@ class DomionsPizzaWorldwideSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(store), callback=self.parse_store)
 
     def parse_store(self, response):
-        ref = re.search(r'.+-(.+?)/?(?:\.html|$)', response.url).group(1)
+        ref = re.search(r'.+/(.+?)/?(?:\.html|$)', response.url).group(1)
         country = re.search(r'\.([a-z]{2})\/', response.url).group(1)
         address_data = response.xpath('//a[@id="open-map-address"]/text()').extract()
         locality_data = address_data[1].strip()
@@ -39,8 +34,8 @@ class DomionsPizzaWorldwideSpider(scrapy.Spider):
             'ref': ref.strip('/'),
             'name': response.xpath('//h1[@class="storetitle"]/text()').extract_first(),
             'addr_full': address_data[0].strip().strip(','),
-            'city': re.search(r'(.*) (.*[A-Za-z])', locality_data).group(2),
-            'postcode': re.search(r'(.*) (.*[A-Za-z])', locality_data).group(1),
+            'city': re.search(r'(.*) ([A-Z]{2}) (.*)', locality_data).group(1),
+            'postcode': re.search(r'(.*) ([A-Z]{2}) (.*)', locality_data).group(3),
             'country': country,
             'lat': float(response.xpath('//div[@class="store-details-info"]/div[1]/input[1]/@value').extract_first().replace(",", ".")),
             'lon': float(response.xpath('//div[@class="store-details-info"]/div[1]/input[2]/@value').extract_first().replace(",", ".")),
