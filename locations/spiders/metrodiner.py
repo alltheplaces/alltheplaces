d3b86510258113c719f837030fb921fc537464a4
Kelley Jabr <kjabr@indeed.com>
2020-10-02 10:58:29 -0500

    Add spider for Mettro Diner. Closes #1553.

---
+++ locations/spiders/metrodiner.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import re
+import scrapy
+from scrapy.selector import Selector
+import json
+from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
+DAY_MAPPING = {
+    '1': 'Mo',
+    '2': 'Tu',
+    '3': 'We',
+    '4': 'Th',
+    '5': 'Fr',
+    '6': 'Sa',
+    '7': 'Su'
+}
+
+class MetroDinerSpider(scrapy.Spider):
+    name = "metrodiner"
+    allowed_domains = ['metrodiner.com']
+    start_urls = [
+        'https://metrodiner.com/locations-sitemap.xml',
+    ]
+
+    def parse_hours(self, hours):
+        opening_hours = OpeningHours()
+        hours = hours.split(";")
+        hours = hours[:7]
+
+        for days in hours:
+            day, open_time, close_time = days.split(",")
+
+            for eachday in days:
+                opening_hours.add_range(day=DAY_MAPPING[day],
+                                        open_time=open_time,
+                                        close_time=close_time,
+                                        time_format='%H%M')
+
+        return opening_hours.as_opening_hours()
+
+    def parse_location(self, response):
+        data = response.xpath('//div[@class="site-header__top-wrapper container"]/div/script/text()').re_first(r'locations:\s*(\[.*\])')
+        data = json.loads(data)
+        data = data[0]
+
+        properties = {
+            'addr_full': data["address"],
+            'phone': data["phone"],
+            'country': 'US',
+            'ref': response.url,
+            'website': response.url,
+            'lat': float(data["latitude"]),
+            'lon': float(data["longitude"]),
+            'name': data["name"]
+        }
+
+        hours = self.parse_hours(data["hours"])
+        if hours:
+            properties["opening_hours"] = hours
+
+        yield GeojsonPointItem(**properties)
+
+    def parse(self, response):
+        xml = Selector(response)
+        xml.remove_namespaces()
+
+        urls = xml.xpath('//loc/text()').extract()
+        urls = [url.strip() for url in urls]
+
+        for url in urls:
+            if re.search(r'orlando-university',url):
+                pass
+            elif re.search(r'locations/',url):
+                yield scrapy.Request(url, callback=self.parse_location)
