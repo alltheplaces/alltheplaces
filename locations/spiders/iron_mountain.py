c5a449b03267fd6dbd7e3b92ebb00bcdb071f2e1
Ritik Mody <ritikashishmody@gmail.com>
2020-10-03 00:56:59 +0530

    added iron mountain spider

---
+++ locations/spiders/iron_mountain.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+from locations.items import GeojsonPointItem
+
+
+class IronMountainSpider(scrapy.Spider):
+    name = "iron_mountain"
+    start_urls = (
+        'https://locations.ironmountain.com/',
+    )
+    
+    def parse(self, response):
+        urls = response.xpath('//*[@id="browse-expand"]/div/div[1]/div/li/div/a/@href').extract()
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_list,meta={'country': "USA"})
+        urls = response.xpath('//*[@id="browse-expand"]/div/div[2]/div/li/div/a/@href').extract()
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_list,meta={'country': "Canada"})
+
+    def parse_list(self,response):
+        urls = response.xpath('//*[@id="main-container"]/div[2]/div[3]/div[1]/div/div[2]/div[2]/ul/li/div/a/@href').extract()
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_location,meta={'country': response.meta.get("country")})
+
+    def parse_city(self,response):
+        urls = response.xpath('//*[@id="main-container"]/div[2]/div[3]/div[1]/div/div[2]/div[2]/ul/li/a/@href').extract()
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_location,meta={'country': response.meta.get("country")})
+    
+    def parse_location(self,response):
+        data = re.sub("'","",response.xpath('//*[@id="gmap"]/script/text()').extract()[0])
+        data = data.replace("false","False")
+        data = data.replace("true","True")
+        data = data.replace("null","None")
+        data = data.replace("RLS.defaultData = ","")
+        data = data.replace(";","")
+        address = eval("{"+str.join(",",eval(data)["markerData"][0]["info"].replace("<\\/div>","").split(",")[1:]))
+        yield GeojsonPointItem(**{
+            "addr_full":address["address_1"],
+            "city":address["city"],
+            "state":response.xpath('//*[@id="main-container"]/div[2]/div[1]/ol/li[2]/a/text()').extract()[0].strip(" "),
+            "postcode":address["post_code"],
+            "lat":float(address["lat"]),
+            "lon":float(address["lng"]),
+            "phone":address["local_phone"],
+            "website":address["url"].replace("\\",""),
+            "country":response.meta.get("country"),
+            "ref":address["url"].replace("\\","")
+        })
