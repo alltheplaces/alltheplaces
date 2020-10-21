f98a9455a1bd09833fef384a147f7b5dc57f97f3
Ian Dees <ian.dees@gmail.com>
2020-10-21 08:53:01 -0500

    Update pet_suites.py

--- locations/spiders/pet_suites.py
+++ locations/spiders/pet_suites.py
@@ -10,6 +10,7 @@ class PetSuitesSpider(scrapy.Spider):
     start_urls = (
         'https://liveapi.yext.com/v2/accounts/me/entities?filter=%7B%20%22meta.folderId%22%3A%7B%22%24in%22%3A[249017]%7D%7D&limit=50&api_key=2bc9758495549d8bd15fe1c10fdcd617&v=20161012',
     )
+
     def process_hours(self, hours):
         output = OpeningHours()
         for day, data in hours.items():
@@ -24,19 +25,18 @@ class PetSuitesSpider(scrapy.Spider):
 
     def parse(self, response):
         data = json.loads(response.text).get("response")
-        for idx, entity in enumerate(data['entities']):
-            properties = dict(lat=entity['yextDisplayCoordinate']['latitude'],
-                        lon=entity['geocodedCoordinate']['longitude'],
-                        name=entity['name'],
-                        addr_full=entity['address']['line1'],
-                        city=entity['address']['city'],
-                        state=entity['address']['region'],
-                        postcode=entity['address']['postalCode'],
-                        country=entity['address']['countryCode'],
-                        phone=entity['mainPhone'],
-                        website=entity['websiteUrl']['url'],
-                        opening_hours=self.process_hours(entity['hours']),
-                        ref=idx)
-            yield GeojsonPointItem(**properties)
-
-
+        for entity in data['entities']:
+            yield GeojsonPointItem(
+                lat=entity['geocodedCoordinate']['latitude'],
+                lon=entity['geocodedCoordinate']['longitude'],
+                name=entity['name'],
+                addr_full=entity['address']['line1'],
+                city=entity['address']['city'],
+                state=entity['address']['region'],
+                postcode=entity['address']['postalCode'],
+                country=entity['address']['countryCode'],
+                phone=entity['mainPhone'],
+                website=entity['websiteUrl']['url'],
+                opening_hours=self.process_hours(entity['hours']),
+                ref=entity['googlePlaceId'],
+            )
