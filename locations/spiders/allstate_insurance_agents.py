7076ffc9d40f5e9b8bae9163557ce319ddcb07b4
Jen Weiss <2924736+thismakessand@users.noreply.github.com>
2020-11-25 15:45:26 -0600

    Fix Allstate agents spider

--- locations/spiders/allstate_insurance_agents.py
+++ locations/spiders/allstate_insurance_agents.py
@@ -1,49 +1,64 @@
 import scrapy
 import re
 from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
 
 class AllstateInsurnceAgentsSpider(scrapy.Spider):
 
     name = "allstate_insurance_agents"
-    item_attributes = { 'brand': "Allstate Insurance Agents" }
+    item_attributes = {'brand': "Allstate", 'brand_wikidata': 'Q2645636'}
     allowed_domains = ["agents.allstate.com"]
     download_delay = 0.5
     start_urls = (
         'https://agents.allstate.com/',
     )
 
+    def parse_hours(self, hours):
+        opening_hours = OpeningHours()
+        for hour in hours:
+            day, times = hour.split(' ')
+            if times == "Closed":
+                continue
+            open_time, close_time = times.split('-')
+            opening_hours.add_range(day, open_time=open_time, close_time=close_time)
+
+        return opening_hours.as_opening_hours()
+
     def parse_stores(self, response):
         properties = {
-            'addr_full': response.xpath('normalize-space(//span[@class="c-address-street c-address-street-1"]/text())').extract_first()+' '+response.xpath('normalize-space(//span[@class="c-address-street c-address-street-2"]/text())').extract_first(),
-            'phone' : response.xpath('normalize-space(//span[@class="c-phone-number-text-number"]/text())').extract_first(),
-            'city' : response.xpath('normalize-space(//span[@class="c-address-city"]/span/text())').extract_first(),
-            'state' : response.xpath('normalize-space(//span[@class="c-address-state"]/text())').extract_first(),
-            'postcode' : response.xpath('normalize-space(//span[@class="c-address-postal-code"]/text())').extract_first(),
-            'ref' : re.findall('[^\/]+$' , response.url)[0].split('.')[0],
-            'website' : response.xpath('//link[@rel="canonical"]/@href').extract_first(),
-            'lat' : float(response.xpath('normalize-space(//meta[@name="geo.position"]/@content)').extract_first().split(';')[0]),
-            'lon' :float(response.xpath('normalize-space(//meta[@name="geo.position"]/@content)').extract_first().split(';')[1])
+            'name': response.xpath('//span[@class="Hero-name"]/text()').extract_first(),
+            'addr_full': response.xpath('normalize-space(//meta[@itemprop="streetAddress"]/@content)').extract_first(),
+            'phone': response.xpath('normalize-space(//*[@itemprop="telephone"]/@content)').extract_first(),
+            'city': response.xpath('normalize-space(//meta[@itemprop="addressLocality"]/@content)').extract_first(),
+            'state': response.xpath('normalize-space(//abbr[@itemprop="addressRegion"]/text())').extract_first(),
+            'postcode': response.xpath('normalize-space(//span[@itemprop="postalCode"]/text())').extract_first(),
+            'ref': re.findall('[^\/]+$', response.url)[0].split('.')[0],
+            'website': response.xpath('//link[@rel="canonical"]/@href').extract_first(),
+            'lat': float(response.xpath('normalize-space(//meta[@name="geo.position"]/@content)').extract_first().split(';')[0]),
+            'lon': float(response.xpath('normalize-space(//meta[@name="geo.position"]/@content)').extract_first().split(';')[1])
         }
-        hours = response.xpath('//meta[@itemprop="openingHours"]/@content').extract()
-        content_hours = " ;".join(hours)
+        hours = response.xpath('//tr[@itemprop="openingHours"]/@content').extract()
+        content_hours = self.parse_hours(hours)
         if content_hours:
             properties['opening_hours'] = content_hours
         yield GeojsonPointItem(**properties)
 
     def parse_city_stores(self, response):
-        stores = response.xpath('//h3[@class="agent-name"]/a[@class="agent-name-link"]/@href').extract()
+        stores = response.xpath('//a[contains(@class, "Teaser-title")]/@href').extract()
         for store in stores:
                 yield scrapy.Request(response.urljoin(store), callback=self.parse_stores)
 
     def parse_state(self, response):
-        city_urls = response.xpath('//div[@class="row c-directory-list-content-wrapper"]/div[@class="col-sm-2"]/div[@class="c-directory-list-content"]/div/a/@href').extract()
+        city_urls = response.xpath('//li[@class="Directory-listItem"]/a/@href').extract()
         for path in city_urls:
             if re.search(r"usa\/[a-z]{2}\/[^\/]+$", path):
                 yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
             else:
                 yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
+
     def parse(self, response):
-        urls = response.xpath('//div[@class="row c-directory-list-content-wrapper"]/div[@class="col-sm-2"]/div[@class="c-directory-list-content"]/div/a/@href').extract()
+        urls = response.xpath('//li[@class="Directory-listItem"]/a/@href').extract()
         for path in urls:
             if re.search(r"usa\/[a-z]{2}\/[^\/]+$", path):
                 yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
