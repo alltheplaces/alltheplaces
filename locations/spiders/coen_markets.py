550807afbb7f263d032aabd0677f4eaf0d420538
Josh Lee <jleedev@gmail.com>
2021-04-13 22:03:13 +0000

    Add coen markets
    55 locations in PA, WV, and OH; some former Cogo's.

---
+++ locations/spiders/coen_markets.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from scrapy.selector.unified import Selector
+
+from locations.items import GeojsonPointItem
+
+
+class CoenMarketsSpider(scrapy.Spider):
+    name = "coen_markets"
+    item_attributes = {"name": "Coen", "brand": "Coen Markets"}
+    start_urls = ["https://coen1923.com/locations/search"]
+
+    def parse(self, response):
+        for location in response.json()["locations"]:
+            url_title = location["url_title"]
+            # Note: embedded in an iframe; not useful as item's website
+            store_url = f"https://coen1923.com/locations/location/{url_title}"
+            yield scrapy.Request(
+                store_url, self.parse_store, cb_kwargs={"js": location}
+            )
+
+    def parse_store(self, response, js):
+        props = {}
+        props["addr_full"] = Selector(text=js["address"]).xpath("//p/text()").get()
+        props["ref"] = js["url_title"]
+        props["lat"] = js["coordinates"][0]
+        props["lon"] = js["coordinates"][1]
+        props["city"] = js["city"]
+        props["state"] = js["state"]
+        props["postcode"] = js["zip"]
+        props["phone"] = js["phone_number"]
+        hours = response.css(".hours p:not(:empty)").xpath("text()").get()
+        props["opening_hours"] = hours
+        return GeojsonPointItem(**props)
