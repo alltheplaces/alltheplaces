3eda10e26f34fc38202300da8c6b836e372576dd
nbeecher <nbeecher@indeed.com>
2021-04-13 16:23:04 -0500

    Edit spider to include house number

--- locations/spiders/albert_heijn.py
+++ locations/spiders/albert_heijn.py
@@ -5,7 +5,7 @@ import json
 
 class AlbertHeijnSpider(scrapy.Spider):
     name = 'albert_heijn'
-    item_attributes = { 'brand': "Albert Heijn" }
+    item_attributes = {'brand': "Albert Heijn"}
     allowed_domains = ['www.ah.nl']
 
     def start_requests(self):
@@ -15,15 +15,19 @@ class AlbertHeijnSpider(scrapy.Spider):
     def parse(self, response):
         stores = json.loads(response.body_as_unicode())
         for store in stores['stores']:
+            try:
+                phone_number = store['phoneNumber']
+            except:
+                phone_number = ""
             yield GeojsonPointItem(
-                lat = store['lat'],
-                lon = store['lng'],
-                addr_full = store['street'],
-                city = store['city'],
-                phone = store['phoneNumber'],
-                state = "",
-                postcode = store['zip'],
-                ref = store['no'],
-                country = "Netherlands",
-                website = "https://www.ah.nl/winkel/albert-heijn/%s/%s/%s"%(store['city'],store['street'],store['no'])
+                lat=store['lat'],
+                lon=store['lng'],
+                addr_full="%s %s" % (store['street'], store["housenr"]),
+                city=store['city'],
+                phone=phone_number,
+                state="",
+                postcode=store['zip'],
+                ref=store['no'],
+                country="Netherlands",
+                website="https://www.ah.nl/winkel/albert-heijn/%s/%s/%s" % (store['city'], store['street'], store['no'])
                 )
