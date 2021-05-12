46a13f0ee1f4ebaa1e6f8abb31104f412db4122f
Ian Dees <ian.dees@gmail.com>
2021-05-12 11:07:47 -0500

    Fix indent

--- locations/spiders/national.py
+++ locations/spiders/national.py
@@ -5,18 +5,18 @@ import scrapy
 from locations.items import GeojsonPointItem
 
 HEADERS = {
-            'authority': 'prd.location.enterprise.com',
-            'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-            'accept': 'application/json, text/plain, */*',
-            'sec-ch-ua-mobile': '?0',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-            'sec-fetch-site': 'cross-site',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'origin': 'https://www.nationalcar.com',
-            'referer': 'https://www.nationalcar.com/',
-            'accept-language': 'en-US,en;q=0.9'
-        }
+    'authority': 'prd.location.enterprise.com',
+    'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+    'accept': 'application/json, text/plain, */*',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'origin': 'https://www.nationalcar.com',
+    'referer': 'https://www.nationalcar.com/',
+    'accept-language': 'en-US,en;q=0.9'
+}
 
 
 class NationalSpider(scrapy.Spider):
