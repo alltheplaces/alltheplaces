cdfd5f88f3aefc278795b97e13eab85673434881
LadyLatitude <54322494+LadyLatitude@users.noreply.github.com>
2021-05-12 10:24:35 -0500

    new spider for national

---
+++ locations/spiders/national.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import json
+import scrapy
+
+from locations.items import GeojsonPointItem
+
+HEADERS = {
+            'authority': 'prd.location.enterprise.com',
+            'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+            'accept': 'application/json, text/plain, */*',
+            'sec-ch-ua-mobile': '?0',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'origin': 'https://www.nationalcar.com',
+            'referer': 'https://www.nationalcar.com/',
+            'accept-language': 'en-US,en;q=0.9'
+        }
+
+
+class NationalSpider(scrapy.Spider):
+    name = "national"
+    allowed_domains = ["nationalcar.com"]
+    start_urls = ['https://www.nationalcar.com/en/car-rental/locations.html']
+
+    def start_requests(self):
+        yield scrapy.Request(
+            'https://prd.location.enterprise.com/enterprise-sls/search/location/national/web/all?cor=US&dto=true',
+            headers=HEADERS
+        )
+
+    def parse(self, response):
+        loc_data = json.loads(response.body_as_unicode())
+
+        for loc in loc_data:
+
+            properties = {
+                'name': loc["name"],
+                'brand': loc["brand"],
+                'phone': loc["phones"][0],
+                'addr_full': loc["address"]["street_addresses"],
+                'city': loc["address"]["city"],
+                'state': loc["address"]["country_subdivision_code"],
+                'postcode': loc["address"]["postal"],
+                'country': loc["address"]["country_code"],
+                'lat': float(loc["gps"]["latitude"]),
+                'lon': float(loc["gps"]["longitude"]),
+                'ref': loc["id"],
+            }
+
+            yield GeojsonPointItem(**properties)
