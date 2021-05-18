ae5d03714e2ef9676ad045bc11a03f3c55067f50
Kalpesh Tawde <kalpeshtawde@outlook.com>
2021-05-18 11:35:32 -0400

    corrected url

--- locations/spiders/tegut_de.py
+++ locations/spiders/tegut_de.py
@@ -2,7 +2,6 @@ import scrapy
 import json
 
 from locations.items import GeojsonPointItem
-from locations.hours import OpeningHours
 
 
 DAY_MAPPING = {
@@ -20,7 +19,7 @@ DAY_MAPPING = {
 class DennsDeSpider(scrapy.Spider):
     name = "tegut_de"
     allowed_domains = ["www.tegut.com"]
-    start_urls = ['https://www.tegut.com/maerkte/marktsuche/seite/1.html?mktegut%5Baddress%5D=Stuttgart&mktegut%5Bradius%5D=200&mktegut%5Bsubmit%5D=Markt%20suchen&cHash=8ab892e19ff6a5ad0c5898779e7dca6f#stores']
+    start_urls = ['https://www.tegut.com/maerkte/marktsuche.html?mktegut%5Baddress%5D=Stuttgart&mktegut%5Bradius%5D=2000&mktegut%5Bsubmit%5D=Markt+suchen']
 
     def parse_data(self, response):
         data = response.xpath(
