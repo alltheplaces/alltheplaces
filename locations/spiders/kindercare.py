05be862d5c6a08d04040ce3b94f3f1867fe89ac9
Jen Weiss <2924736+thismakessand@users.noreply.github.com>
2021-06-08 13:22:36 -0500

    Add KinderCare Learning Centers

---
+++ locations/spiders/kindercare.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import json
+import re
+
+import scrapy
+
+from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
+
+class KindercareSpider(scrapy.Spider):
+    name = "kindercare"
+    item_attributes = {'brand': 'KinderCare Learning Centers', 'brand_wikidata': 'Q6410551'}
+    allowed_domains = ['kindercare.com']
+    start_urls = [
+        'https://www.kindercare.com/our-centers',
+    ]
+    download_delay = 0.5
+
+    def parse_location(self, response):
+        data = json.loads(response.xpath('//script[@type="application/ld+json" and contains(text(), "streetAddress")]/text()').extract_first())
+
+        properties = {
+            'name': data["name"],
+            'ref': data["@id"],
+            'addr_full': data["address"]["streetAddress"],
+            'city': data["address"]["addressLocality"],
+            'state': data["address"]["addressRegion"],
+            'postcode': data["address"]["postalCode"],
+            'country': data["address"]["addressCountry"],
+            'phone': data.get("telephone"),
+            'website': response.url,
+            'lat': float(data["geo"]["latitude"]),
+            'lon': float(data["geo"]["longitude"]),
+        }
+
+        yield GeojsonPointItem(**properties)
+
+    def parse(self, response):
+        urls = response.xpath('//div[contains(@class, "link-index-results")]//li/a/@href').extract()
+
+        for url in urls:
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_location)
