58422e03adee60be9a8b545e83ad1a8376e96c1e
thekuiperbelt <25801804+thekuiperbelt@users.noreply.github.com>
2021-06-24 09:30:56 -0500

    Updated Aldi UK spider

--- locations/spiders/aldi_uk.py
+++ locations/spiders/aldi_uk.py
@@ -16,7 +16,7 @@ class AldiUkSpider(scrapy.Spider):
     def start_requests(self):
         url = "https://www.aldi.co.uk/api/store-finder/search?latitude={lat}&longitude={lng}"
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
-        with open('./locations/searchable_points/eu_centroids_20km_radius_country.csv') as points:
+        with open('./locations/searchable_points/eu_centroids_120km_radius_country.csv') as points:
             reader = csv.DictReader(points)
             for point in reader:
                 if point["country"] == "UK":
@@ -27,17 +27,20 @@ class AldiUkSpider(scrapy.Spider):
 
     def parse(self, response):
         data = json.loads(response.body_as_unicode())
-        stores = data['results']
-        for store in stores:
-            properties = {
-                'ref': store['code'],
-                'name': store['name'],
-                'addr_full': store['name'].strip('ALDI - '),
-                'city': store['address'][0],
-                'postcode': store['address'][1],
-                'lat': store['latlng']['lat'],
-                'lon': store['latlng']['lng'],
-                'website': response.url
-            }
+        try:
+            stores = data['results']
+            for store in stores:
+                properties = {
+                    'ref': store['code'],
+                    'name': store['name'],
+                    'addr_full': store['name'].strip('ALDI - '),
+                    'city': store['address'][0],
+                    'postcode': store['address'][1],
+                    'lat': store['latlng']['lat'],
+                    'lon': store['latlng']['lng'],
+                    'website': response.url
+                }
 
-            yield GeojsonPointItem(**properties)
+                yield GeojsonPointItem(**properties)
+        except:
+            pass
