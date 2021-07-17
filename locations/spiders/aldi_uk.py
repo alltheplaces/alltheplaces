abfa934506ce7fa6f8a6945c39ebdc32977ea3f7
Ian Dees <ian.dees@gmail.com>
2021-07-17 09:38:22 -0500

    Formatting cleanup.

--- locations/spiders/aldi_uk.py
+++ locations/spiders/aldi_uk.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import json
 import csv
-import requests
 import scrapy
 import re
 
 from locations.items import GeojsonPointItem
 
 COOKIES = {
-            "bm_sz": "04B124C1C96D68082A9F61BAAAF0B6D5~YAAQdjsvF22E8Xl6AQAACr1VfAxPEt+enarZyrOZrBaNvyuX71lK5QPuDR/FgDEWBZVMRhjiIf000W7Z1PiAjxobrz2Y5LcYMH3CvUNvpdS3MjVLUMGwMEBCf9L5nD5Gs9ho2YL8T7Tz7lYvpolvaOlJnKrHyhCFxxk/uyBZ2G/0QrGKLwSaCQShDsz7ink=",
-            "_abck": "440E40C406E69413DCCC08ABAA3E9022~-1~YAAQdjsvF26E8Xl6AQAACr1VfAYznoJdJhX7TNIZW1Rfh6qRhzquXg+L1TWoaL7nZUjXlNls2iPIKFQrCdrWqY/CNXW+mHyXibInMflIXJi5VVB/Swq53kABYJDuXYSlCunYvJAzMSr1q12NOYswz134Y8HRNzVWhkb2jMS5whmHxS/v0vniIvS1TQtKjEQlMGzQYmN41CmLX0JobipQhDtUB4VyNwztb2DCAZiqDX8BLwWg7h/DtPd4158qU69hNhayFTgWmD76/MiR8/T536tMmcoRyWLl4fEtP/XUmKOcksuZO7dbfNxXBffTxIXPYwf1eO77LNuZTCQq5kfsGZLJX8ODju2KSjnIF1vdnyHAe98FDIm+hw==~-1~-1~-1"
-        }
+    "bm_sz": "04B124C1C96D68082A9F61BAAAF0B6D5~YAAQdjsvF22E8Xl6AQAACr1VfAxPEt+enarZyrOZrBaNvyuX71lK5QPuDR/FgDEWBZVMRhjiIf000W7Z1PiAjxobrz2Y5LcYMH3CvUNvpdS3MjVLUMGwMEBCf9L5nD5Gs9ho2YL8T7Tz7lYvpolvaOlJnKrHyhCFxxk/uyBZ2G/0QrGKLwSaCQShDsz7ink=",
+    "_abck": "440E40C406E69413DCCC08ABAA3E9022~-1~YAAQdjsvF26E8Xl6AQAACr1VfAYznoJdJhX7TNIZW1Rfh6qRhzquXg+L1TWoaL7nZUjXlNls2iPIKFQrCdrWqY/CNXW+mHyXibInMflIXJi5VVB/Swq53kABYJDuXYSlCunYvJAzMSr1q12NOYswz134Y8HRNzVWhkb2jMS5whmHxS/v0vniIvS1TQtKjEQlMGzQYmN41CmLX0JobipQhDtUB4VyNwztb2DCAZiqDX8BLwWg7h/DtPd4158qU69hNhayFTgWmD76/MiR8/T536tMmcoRyWLl4fEtP/XUmKOcksuZO7dbfNxXBffTxIXPYwf1eO77LNuZTCQq5kfsGZLJX8ODju2KSjnIF1vdnyHAe98FDIm+hw==~-1~-1~-1"
+}
 
 HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -25,7 +24,7 @@ HEADERS = {
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-    }
+}
 
 class AldiUkSpider(scrapy.Spider):
     name = "aldiuk"
@@ -34,7 +33,7 @@ class AldiUkSpider(scrapy.Spider):
     download_delay = 0.5
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
-        }
+    }
 
     def start_requests(self):
         url = 'https://www.aldi.co.uk/sitemap/store-en_gb-gbp'
@@ -46,7 +45,7 @@ class AldiUkSpider(scrapy.Spider):
             cookies=COOKIES,
             headers=HEADERS,
             callback=self.parse
-            )
+        )
         
     def parse(self, response):
         response.selector.remove_namespaces()
@@ -61,7 +60,7 @@ class AldiUkSpider(scrapy.Spider):
                 cookies=COOKIES,
                 headers=HEADERS,
                 callback=self.parse_store
-                )
+            )
         
     def parse_store(self, response):
         
@@ -73,16 +72,16 @@ class AldiUkSpider(scrapy.Spider):
         geodata = json.loads(geojson_data)
 
         properties = {
-        'name': data['seoData']['name'],
-        'ref': data['seoData']['name'],
-        'addr_full': data['seoData']['address']['streetAddress'],
-        'city': data['seoData']['address']['addressLocality'],
-        'postcode': data['seoData']['address']['postalCode'],
-        'country': data['seoData']['address']['addressCountry'],
-        'website': response.request.url,
-        'opening_hours': str(data['seoData']['openingHours']).replace('[','').replace(']','').replace("'",''),
-        'lat': float(geodata['store']['latlng']['lat']),
-        'lon': float(geodata['store']['latlng']['lng']),
+            'name': data['seoData']['name'],
+            'ref': data['seoData']['name'],
+            'addr_full': data['seoData']['address']['streetAddress'],
+            'city': data['seoData']['address']['addressLocality'],
+            'postcode': data['seoData']['address']['postalCode'],
+            'country': data['seoData']['address']['addressCountry'],
+            'website': response.request.url,
+            'opening_hours': str(data['seoData']['openingHours']).replace('[','').replace(']','').replace("'",''),
+            'lat': geodata['store']['latlng']['lat'],
+            'lon': geodata['store']['latlng']['lng'],
         }
 
         yield GeojsonPointItem(**properties)
