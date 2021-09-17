64092d360f14b8fe5b2540367bf5974dc44756ed
Josh Lee <jleedev@gmail.com>
2021-09-17 12:41:09 +0000

    Fix thales_fr
    Read kml instead of parsing the google my maps javascript
    Fix misuse of local variables that produced incorrect results

--- locations/spiders/thales_fr.py
+++ locations/spiders/thales_fr.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import io
 import json
 import re
+import zipfile
 
 import scrapy
 
@@ -10,41 +12,50 @@ from locations.hours import OpeningHours
 
 class ThalesFrSpider(scrapy.Spider):
     name = "thales_fr"
-    item_attributes = {'brand': "Thales"}
-    allowed_domains = ['google.com']
-    start_urls = [
-        'https://www.google.com/maps/d/u/0/embed?mid=1yA7rNTU7COi4W6wrLiZ8Ro0Kem8&ll=48.40753354596532%2C-4.58267066250005&z=6',
-    ]
+    item_attributes = {"brand": "Thales"}
+    allowed_domains = ["google.com"]
+    start_urls = ["https://www.google.com/maps/d/kml?mid=zOpSQcuS01RE.kLfnXlw7pPcM"]
 
     def parse(self, response):
-        data = response.xpath(
-            '//script[@type="text/javascript" and contains(text(), "_pageData")]/text()').extract_first()
+        kmz = zipfile.ZipFile(io.BytesIO(response.body))
+        kml = kmz.open(kmz.filelist[0]).read()
+        data = scrapy.Selector(text=kml, type="xml")
+        data.remove_namespaces()
+        for ref, placemark in enumerate(data.xpath("//Placemark")):
+            yield self.parse_location(ref, placemark)
 
-        places = re.findall(r"Entity(.*?)KO", data)
-
-        for place in places:
-            addr = re.search(r'Address\\",\[\\"(.*?)\\"\]', place).group(1)
-            if "France" not in addr:
-                pass
+    def parse_location(self, ref, placemark):
+        addr = re.search(
+            "Address: (.*?)<br>",
+            placemark.xpath(".//description/text()").extract_first(),
+        )[1]
+        if "France" not in addr:
+            addr_full = addr
+            street = locality = postal = ""
+        else:
+            addr_split = addr.split(",")
+            if len(addr_split) == 4:
+                street = addr_split[0] + addr_split[1]
+                postal = re.search(r"\d{5}", addr_split[2]).group(0)
+                locality = ""
             else:
-                addr_split = addr.split(",")
-                if len(addr_split) == 4:
-                    street = addr_split[0] + addr_split[1]
-                    postal = re.search(r'\d{5}', addr_split[2]).group(0)
-                else:
-                    street = addr_split[0]
-                    postal = re.search(r'\d{5}', addr_split[1]).group(0)
-                    locality = addr_split[1].replace(postal, "").strip()
+                street = addr_split[0]
+                postal = re.search(r"\d{5}", addr_split[1]).group(0)
+                locality = addr_split[1].replace(postal, "").strip()
 
-            properties = {
-                'ref': re.search(r'null,(\d*?)\]', place).group(1),
-                'name': re.search(r'\[\\"(.*?)\\"\]', place).group(1).replace("\\\\u0026", "&"),
-                'addr_full': street,
-                'city': locality,
-                'postcode': postal,
-                'country': 'FR',
-                'lat': re.search(r'Latitude\\",\[\\"(.*?)\\', place).group(1),
-                'lon': re.search(r'Longitude\\",\[\\"(.*?)\\', place).group(1)
-            }
+        x, y, z = (
+            placemark.xpath(".//coordinates/text()").extract_first().strip().split(",")
+        )
 
-            yield GeojsonPointItem(**properties)
+        properties = {
+            "ref": ref,
+            "name": placemark.xpath(".//name/text()").extract_first(),
+            "addr_full": street,
+            "city": locality,
+            "postcode": postal,
+            "country": "FR",
+            "lat": y,
+            "lon": x,
+        }
+
+        return GeojsonPointItem(**properties)
