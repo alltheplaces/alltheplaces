a15c0e1eeb9b9a49901458c5acfb942e6afcae23
Boomerwv1 <8587049+boomerwv1@users.noreply.github>
2021-09-25 18:14:07 -0400

    updating SC

--- locations/spiders/south_carolina.py
+++ locations/spiders/south_carolina.py
@@ -2,6 +2,7 @@
 import re
 
 import scrapy
+import json
 
 from locations.items import GeojsonPointItem
 
@@ -15,39 +16,33 @@ class SCSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        replace_list = (']', '[', '}', '"', '\\t')
         cat = ('State Park', 'Library', 'Department of Health and Human Resources',
                'Court House', 'Department of Mental Health', 'Department of Motor Vehicles', 'SC Works')
 
-        zipre = response.xpath('//body//text()').extract()
-        ziptext = zipre[0]
-        ziptext = ziptext.replace(',SC', ', SC')
-        for item in replace_list:
-            ziptext = ziptext.replace(item, '')
+        data = json.loads(json.dumps(response.json()))
+        # ziptext = zipre[0]
+        # ziptext = ziptext.replace(',SC', ', SC')
+        # for item in replace_list:
+        #     ziptext = ziptext.replace(item, '')
+        #
+        # ziptext2 = ziptext.split('{')
+        print(data[0])
 
-        ziptext2 = ziptext.split('{')
-        for i in ziptext2:
-            try:
-                if i.startswith('Id'):
-                    i = i.replace(', ', " ")
-                    j = i.split(',')
-                    print(cat[int((j[4].split(':')[1]))-1])
+        for i in data:
+            print (i['Address'])
 
-                    properties = {
-                        'ref': j[0].split(':')[1],
-                        'name': j[1].split(':')[1],
-                        'extras': cat[int((j[4].split(':')[1])) - 1],
-                        'addr_full': j[6].split(':')[1] + ' ' + j[5].split(':')[1],
-                        'city': '',
-                        'state': '',
-                        'postcode': '',
-                        'country': 'US',
-                        'phone': j[7].split(':')[1],
-                        'lat': float(j[2].split(':')[1]),
-                        'lon': -abs((float(j[3].split(':')[1]))),
-                    }
+            properties = {
+                'ref': i['Id'],
+                'name': i['Description'],
+                'extras': (cat[int(i['CategoryId']) - 1]),
+                'addr_full': i['Address'],
+                'city': '',
+                'state': '',
+                'postcode': i['Zipcode'],
+                'country': 'US',
+                'phone': i['Telephone'],
+                'lat': float(i['Latitude']),
+                'lon': -abs(float(i['Longitude'])),
+            }
 
-                    yield GeojsonPointItem(**properties)
-
-            except:
-                pass
+            yield GeojsonPointItem(**properties)
