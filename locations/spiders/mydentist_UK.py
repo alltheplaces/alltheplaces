531134d9d51ac251791c6e7b4474f56d313f15c4
Boomerwv1 <8587049+boomerwv1@users.noreply.github>
2021-10-04 16:34:37 -0400

    Adding Spider for MyDentist UK

--- locations/spiders/mydentist_UK.py
+++ locations/spiders/mydentist_UK.py
@@ -23,41 +23,33 @@ class MyDentistSpider(scrapy.Spider):
                 lati = row[1]
                 long = row[2]
                 country = row[3]
-                print(row)
                 if country == "UK":
-                    print(country)
                     searchurl = 'https://www.mydentist.co.uk/Workflow/DentistSearch/_SearchResult?searchData=&clientLat={la}&clientLng={lo}&searchoption=private%2Cnhs%2Cortho%2C&numberOfResults=1000&widgetName=DentistSearch&quickContronFormOptions=&isquickContactForm=False&_=1632838745154'.format(la=lati, lo=long)
                     yield scrapy.Request(response.urljoin(searchurl), callback=self.parse_search)
 
     def parse_search(self, response):
-        print('Searching....')
         stores = response.xpath('//div[@class="practice-details"]').extract()
-        count = 0
         for i in stores:
-            count = count + 1
             store = re.sub(' +', ' ', i)
-            try:
-                address = re.search('r>\r\n ([^;]*) <br>', store).group(1).split(', ')[0]
-                city = re.search('r>\r\n ([^;]*) <br>', store).group(1).split(', ')[1]
-                zip = re.search('r>\r\n ([^;]*) <br>', store).group(1).split(', ')[2]
-                phone = re.search('tel">([^;]*)</a>\r\n', store).group(1)
-                lat = float(re.search('latitude="([^;]*)" data-lon', store).group(1))
-                lon = float(re.search('longitude="([^;]*)">\r\n <strong', store).group(1))
-                ref = re.search('data-title="([^;]*)" data-lati', store).group(1)
+            store = re.sub('\\r\\n', '', store)
+            address = re.search('/strong><br> (.+?) <br>', store).group(1).split(', ')[0]
+            city = re.search('/strong><br> (.+?) <br>', store).group(1).split(', ')[1]
+            zip = re.search('/strong><br> (.+?) <br>', store).group(1).split(', ')[2]
+            phone = re.search('tel">(.+?)</a>', store).group(1)
+            lat = float(re.search('latitude="(.+?)" data-lon', store).group(1))
+            lon = float(re.search('longitude="(.+?)"> <strong', store).group(1))
+            ref = re.search('data-title="(.+?)" data-lati', store).group(1)
 
-                properties = {
-                    'ref': ref,
-                    'name': 'MyDentist',
-                    'addr_full': address,
-                    'city': city,
-                    'postcode': zip,
-                    'country': 'UK',
-                    'phone': phone,
-                    'lat': lat,
-                    'lon': lon
-                }
+            properties = {
+                'ref': ref,
+                'name': 'MyDentist',
+                'addr_full': address,
+                'city': city,
+                'postcode': zip,
+                'country': 'UK',
+                'phone': phone,
+                'lat': lat,
+                'lon': lon
+            }
 
-                yield GeojsonPointItem(**properties)
-            except:
-                print(store)
-                pass
+            yield GeojsonPointItem(**properties)
