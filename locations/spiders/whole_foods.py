447cb4cb9fd8ff7851c8381b72a5b0c9349ab2af
mecklenborg <mecklenborg@indeed.com>
2021-10-23 14:12:38 -0500

    Update Whole Foods spider to handle redirects and parse UK store data correctly

--- locations/spiders/whole_foods.py
+++ locations/spiders/whole_foods.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import scrapy
 import re
 
@@ -10,25 +12,36 @@ import json
 class WholeFoodsSpider(scrapy.Spider):
     name = "whole_foods"
     item_attributes = { 'brand': "Whole Foods" }
-    allowed_domains = ["www.wholefoodsmarket.com"]
+    allowed_domains = ['wholefoodsmarket.com', 'wholefoodsmarket.co.uk']
     start_urls = (
         'https://www.wholefoodsmarket.com/sitemap/sitemap-stores.xml',
+        'https://www.wholefoodsmarket.co.uk/sitemap.xml',
     )
 
     def parse(self, response):
         response.selector.remove_namespaces()
         city_urls = response.xpath('//url/loc/text()').extract()
-        regex = re.compile(r'https://www.wholefoodsmarket.com/stores/\S+')
+        us_regex = re.compile(r'https://www.wholefoodsmarket.com/stores/\S+')
+        uk_regex = re.compile(r'https://www.wholefoodsmarket.co.uk/stores/\S+')
         for path in city_urls:
-            if re.search(regex, path):
+            if re.search(us_regex, path):
                 yield scrapy.Request(
                     path.strip(),
-                    callback=self.parse_store,
+                    callback=self.parse_us_store
+                )
+            elif re.search(uk_regex, path):
+                yield scrapy.Request(
+                    path.strip(),
+                    callback=self.parse_uk_store
                 )
             else:
                 pass
 
-    def parse_store(self, response):
+    def parse_us_store(self, response):
+        # Handle redirects to main page (closed store) and UK pages
+        if response.request.meta.get('redirect_urls'):
+            return
+
         store_json = json.loads(
             response.xpath('//script[@type="application/ld+json"]/text()').extract_first()
         )
@@ -57,3 +70,30 @@ class WholeFoodsSpider(scrapy.Spider):
             )
 
         return opening_hours.as_opening_hours()
+
+    def parse_uk_store(self, response):
+        # Handle redirects to main page (closed store)
+        if response.request.meta.get('redirect_urls'):
+            return
+
+        store_text = response.xpath(
+            '//script[@type="text/javascript" and contains(text(), "storeAPIData")]/text()'
+        ).extract_first()
+        store_json = json.loads(
+            store_text[store_text.find('{'):store_text.rfind('}')+1]
+        )['initialProps']['siteData']['storeAPIData']
+
+        yield GeojsonPointItem(
+            ref=store_json['folder'],
+            name=store_json['name'],
+            lat=float(store_json['geo_location']['coordinates'][0]),
+            lon=float(store_json['geo_location']['coordinates'][1]),
+            addr_full=store_json['address'],
+            city=store_json['city'],
+            state=store_json['state'],
+            postcode=store_json['zip_code'],
+            country=store_json['country'],
+            phone=store_json['phone'],
+            website=response.url,
+            opening_hours=store_json['hours'],
+        )
