73d9a89c7815d5c337d15baf8e67df07ca14a0f2
kyungahlim <60443165+kyungahlim@users.noreply.github.com>
2021-10-27 21:59:36 -0500

    Adding a spider for Primrose Schools

---
+++ locations/spiders/primrose_schools.py
@@ -0,0 +1,49 @@
+import json
+
+import scrapy
+
+from locations.items import GeojsonPointItem
+
+class PrimroseSchoolsSpider(scrapy.Spider):
+    name = "primrose_schools"
+    item_attributes = {'brand': 'Primrose Schools'}
+    allowed_domains = ["primroseschools.com"]
+
+    start_urls = ([
+        'https://www.primroseschools.com/find-a-school/'
+    ])
+
+    def parse(self, response):
+        with open('./locations/searchable_points/us_centroids_50mile_radius.csv') as points:
+            next(points)
+            for point in points:
+                row = point.replace('\n', '').split(',')
+                lati = row[1]
+                long = row[2]
+                searchurl = 'https://www.primroseschools.com/find-a-school/?search_string=USA&latitude={la}&longitude={lo}'.format(la=lati, lo=long)
+                yield scrapy.Request(response.urljoin(searchurl), callback=self.parse_search)
+
+    def parse_search(self, response):
+        content = response.xpath('//script[@type="application/json"]/text()').get()
+        schools = json.loads(content)
+        for i in schools:
+            if i["address_1"]:
+                properties = {
+                    'name': i["name"],
+                    'addr_full': i["address_1"] + " " + i["address_2"],
+                    'city': i["city"],
+                    'state': i["state"],
+                    'postcode': i["zip_code"],
+                    'phone': i["phone"],
+                    'ref': i['id'],
+                    'website': "https://www.primroseschools.com" + i["url"],
+                    'lat': float(i["latitude"]),
+                    'lon': float(i["longitude"]),
+                }
+                yield GeojsonPointItem(**properties)
+
+
+
+
+
+
