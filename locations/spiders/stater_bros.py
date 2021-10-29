6800b171bc334c6bb60e91837da281ee1ebb275d
kurasanthoshkumar <kurasanthoshkumar@gmail.com>
2021-10-29 21:51:42 +0530

    fix for #3020

--- locations/spiders/stater_bros.py
+++ locations/spiders/stater_bros.py
@@ -4,7 +4,7 @@ import json
 from locations.items import GeojsonPointItem
 
 class StaterBrosSpider(scrapy.Spider):
-    name = 'stater-bros'
+    name = 'stater_bros'
     item_attributes = { 'brand': "Stater Bros" }
     allowed_domains = ['www.staterbros.com']
 
@@ -20,11 +20,14 @@ class StaterBrosSpider(scrapy.Spider):
         for index, store in enumerate(stores):
           properties = {
             'addr_full': store.xpath('@data-address').extract_first(),
-            'phone': store.xpath('div[@class="left"]/div[@class="phone"]/p/text()').extract()[1],
             'ref': index,
             'lon': store.xpath('@data-longitude').extract_first(),
             'lat': store.xpath('@data-latitude').extract_first(),
             'opening_hours': ' '.join(stores[0].xpath('div[@class="right"]/div[@class="hours"]/p/text()').extract()[:2]),
             'name': store.xpath('div[@class="left"]/div[@class="name"]/text()').extract_first()
           }
+
+          if len(store.xpath('div[@class="left"]/div[@class="phone"]/p/text()').extract()) > 0:
+            properties['phone'] = store.xpath('div[@class="left"]/div[@class="phone"]/p/text()').extract()[1]
+          
           yield GeojsonPointItem(**properties)
