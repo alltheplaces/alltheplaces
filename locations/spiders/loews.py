fb3d0ddae7c21b5b224f333c84c231a6e68102af
Boomerwv1 <8587049+boomerwv1@users.noreply.github>
2021-10-31 23:29:08 -0400

    Adding spider for Loews Hotels

---
+++ locations/spiders/loews.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import re
+import json
+
+import scrapy
+
+from locations.items import GeojsonPointItem
+
+class LoewsHotelsSpider(scrapy.Spider):
+    #download_delay = 0.2
+    name = "loews"
+    item_attributes = {'brand': "Loews Hotels", 'brand_wikidata': "Q6666622"}
+    allowed_domains = ["loewshotels.com"]
+    start_urls = (
+        'https://www.loewshotels.com/destinations',
+    )
+
+    def parse(self, response):
+        urls = response.xpath('//div[@class="row"]//p//a/@href').extract()
+        x = '/omni-partnership'
+        for url in urls:
+            if url.startswith('/booking'):
+                pass
+            elif "omni" in url:
+                pass
+            elif "hotel-1000" in url:
+                pass
+            elif "policy" in url:
+                pass
+            elif "mokara" in url:
+                pass
+            else:
+                yield scrapy.Request(response.urljoin(url), callback=self.parse_loc)
+
+    def parse_loc(self, response):
+        data = json.loads(response.xpath('//script[@type="application/ld+json" and contains(text(), "address")]/text()').extract_first())
+        try:
+            properties = {
+                'ref': data['name'],
+                'name': data['name'],
+                'addr_full': response.xpath('//span[@class="street-address"]/text()')[0].extract().strip('[', ']'),
+                'city': data['address']['addressLocality'],
+                'state': data['address']['addressRegion'],
+                'postcode': data['address']['postalCode'],
+                'country': data['address']['addressCountry'],
+                'phone': data.get("telephone"),
+                'lat': float(data['geo']['latitude']),
+                'lon': float(data['geo']['longitude']),
+            }
+
+            yield GeojsonPointItem(**properties)
+        except:
+            properties = {
+                'ref': data['name'],
+                'name': data['name'],
+                'addr_full': response.xpath('//span[@class="street-address"]/text()').extract()[0].strip('[', ']'),
+                'city': data['address']['addressLocality'],
+                'state': data['address']['addressRegion'],
+                'postcode': data['address']['postalCode'],
+                'country': data['address']['addressCountry'],
+                'phone': data.get("telephone"),
+            }
+
+            yield GeojsonPointItem(**properties)
