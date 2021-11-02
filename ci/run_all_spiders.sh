7ffdd8ef1d4f943300e1ee8c97f615fd61cee23f
Ian Dees <ian.dees@gmail.com>
2021-11-01 19:53:19 -0500

    Don't let a non-zero exit code through to xargs
    Based on https://serverfault.com/a/568933. Also clean up some shell lint hints.

--- ci/run_all_spiders.sh
+++ ci/run_all_spiders.sh
@@ -33,19 +33,20 @@ mkdir -p "${SPIDER_RUN_DIR}"
 (>&2 echo "Write out a file with scrapy commands to parallelize")
 for spider in $(scrapy list)
 do
-    echo "--output ${SPIDER_RUN_DIR}/output/${spider}.geojson:geojson --logfile ${SPIDER_RUN_DIR}/logs/${spider}.txt --loglevel ERROR --set TELNETCONSOLE_ENABLED=0 --set CLOSESPIDER_TIMEOUT=${SPIDER_TIMEOUT} --set LOGSTATS_FILE=${SPIDER_RUN_DIR}/stats/${spider}.json ${spider}" >> ${SPIDER_RUN_DIR}/commands.txt
+    echo "timeout -k 15s 4h scrapy crawl --output ${SPIDER_RUN_DIR}/output/${spider}.geojson:geojson --logfile ${SPIDER_RUN_DIR}/logs/${spider}.txt --loglevel ERROR --set TELNETCONSOLE_ENABLED=0 --set CLOSESPIDER_TIMEOUT=${SPIDER_TIMEOUT} --set LOGSTATS_FILE=${SPIDER_RUN_DIR}/stats/${spider}.json ${spider}" >> ${SPIDER_RUN_DIR}/commands.txt
 done
 
-mkdir -p ${SPIDER_RUN_DIR}/logs
-mkdir -p ${SPIDER_RUN_DIR}/stats
-mkdir -p ${SPIDER_RUN_DIR}/output
-SPIDER_COUNT=$(wc -l < ${SPIDER_RUN_DIR}/commands.txt | tr -d ' ')
+mkdir -p "${SPIDER_RUN_DIR}/logs"
+mkdir -p "${SPIDER_RUN_DIR}/stats"
+mkdir -p "${SPIDER_RUN_DIR}/output"
+SPIDER_COUNT=$(wc -l < "${SPIDER_RUN_DIR}/commands.txt" | tr -d ' ')
 
 (>&2 echo "Running ${SPIDER_COUNT} spiders ${PARALLELISM} at a time")
-xargs -t -L 1 -P ${PARALLELISM} -a "${SPIDER_RUN_DIR}/commands.txt" timeout -k 15s 4h scrapy crawl
+xargs -t -L 1 -P ${PARALLELISM} -a "${SPIDER_RUN_DIR}/commands.txt" -i sh -c "{} || true"
 
-if [ ! $? -eq 0 ]; then
-    (>&2 echo "Xargs failed with exit code ${?}")
+retval=$?
+if [ ! $retval -eq 0 ]; then
+    (>&2 echo "xargs failed with exit code ${retval}")
     exit 1
 fi
 (>&2 echo "Done running spiders")
@@ -53,20 +54,20 @@ fi
 OUTPUT_LINECOUNT=$(cat ${SPIDER_RUN_DIR}/output/*.geojson | wc -l | tr -d ' ')
 (>&2 echo "Generated ${OUTPUT_LINECOUNT} lines")
 
-echo "{\"count\": ${SPIDER_COUNT}, \"results\": []}" >> ${SPIDER_RUN_DIR}/output/results.json
+echo "{\"count\": ${SPIDER_COUNT}, \"results\": []}" >> "${SPIDER_RUN_DIR}/output/results.json"
 for spider in $(scrapy list)
 do
     spider_out_geojson="${SPIDER_RUN_DIR}/output/${spider}.geojson"
     spider_out_log="${SPIDER_RUN_DIR}/logs/${spider}.txt"
     statistics_json="${SPIDER_RUN_DIR}/stats/${spider}.json"
 
-    feature_count=$(jq --raw-output '.item_scraped_count' ${statistics_json})
+    feature_count=$(jq --raw-output '.item_scraped_count' "${statistics_json}")
 
     if [ "${feature_count}" == "null" ]; then
         feature_count="0"
     fi
 
-    error_count=$(jq --raw-output '."log_count/ERROR"' ${statistics_json})
+    error_count=$(jq --raw-output '."log_count/ERROR"' "${statistics_json}")
 
     if [ "${error_count}" == "null" ]; then
         error_count="0"
@@ -75,21 +76,20 @@ do
     echo "Spider ${spider} has ${feature_count} features, ${error_count} errors"
 
     # use JQ to create an overall results JSON
-    cat ${SPIDER_RUN_DIR}/output/results.json | \
-        jq --compact-output \
-            --arg spider_name ${spider} \
-            --arg spider_feature_count ${feature_count} \
-            --arg spider_error_count ${error_count} \
-            '.results += [{"spider": $spider_name, "errors": $spider_error_count | tonumber, "features": $spider_feature_count | tonumber}]' \
-        > ${SPIDER_RUN_DIR}/output/results.json
+    jq --compact-output \
+        --arg spider_name ${spider} \
+        --arg spider_feature_count ${feature_count} \
+        --arg spider_error_count ${error_count} \
+        '.results += [{"spider": $spider_name, "errors": $spider_error_count | tonumber, "features": $spider_feature_count | tonumber}]' \
+        "${SPIDER_RUN_DIR}/output/results.json"
 done
 (>&2 echo "Wrote out summary JSON")
 
 (>&2 echo "Concatenating and compressing output files")
-tar -czf ${SPIDER_RUN_DIR}/output.tar.gz -C ${SPIDER_RUN_DIR} ./output
+tar -czf "${SPIDER_RUN_DIR}/output.tar.gz" -C "${SPIDER_RUN_DIR}" ./output
 
 (>&2 echo "Concatenating and compressing log files")
-tar -czf ${SPIDER_RUN_DIR}/logs.tar.gz -C ${SPIDER_RUN_DIR} ./logs
+tar -czf "${SPIDER_RUN_DIR}/logs.tar.gz" -C "${SPIDER_RUN_DIR}" ./logs
 
 (>&2 echo "Saving log and output files to ${RUN_S3_PREFIX}")
 aws s3 sync \
@@ -97,7 +97,8 @@ aws s3 sync \
     "${SPIDER_RUN_DIR}/" \
     "${RUN_S3_PREFIX}/"
 
-if [ ! $? -eq 0 ]; then
+retval=$?
+if [ ! $retval -eq 0 ]; then
     (>&2 echo "Couldn't sync to s3")
     exit 1
 fi
@@ -118,7 +119,8 @@ aws s3 cp \
     "${SPIDER_RUN_DIR}/info_embed.html" \
     "s3://${S3_BUCKET}/runs/latest/info_embed.html"
 
-if [ ! $? -eq 0 ]; then
+retval=$?
+if [ ! $retval -eq 0 ]; then
     (>&2 echo "Couldn't save info embed to s3")
     exit 1
 fi
