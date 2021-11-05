f8078466d16807ba749fb422cd8538c8d4468fdb
Ian Dees <ian.dees@gmail.com>
2021-11-04 21:34:40 -0500

    Try setting a user agent?

--- locations/spiders/bmo_harris.py
+++ locations/spiders/bmo_harris.py
@@ -14,6 +14,7 @@ class BMOHarrisSpider(scrapy.Spider):
     start_urls = (
         'https://branches.bmoharris.com/',
     )
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
 
     def parse_store(self, response):
         properties = {
