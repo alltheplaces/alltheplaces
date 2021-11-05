17efa157ec7e07225e32b479052392c79150ff32
Ian Dees <ian.dees@gmail.com>
2021-11-04 21:45:23 -0500

    Fix goodwill spider

--- locations/spiders/goodwill.py
+++ locations/spiders/goodwill.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import csv
 import json
+import scrapy
 from locations.items import GeojsonPointItem
 
 CATEGORY_MAPPING = {
@@ -14,33 +15,34 @@ CATEGORY_MAPPING = {
 
 class GoodwillSpider(scrapy.Spider):
     name = "goodwill"
-    item_attributes = { 'brand': "Goodwill" }
+    item_attributes = {'brand': "Goodwill", 'brand_wikidata': "Q5583655"}
     allowed_domains = ['www.goodwill.org']
     download_delay = 0.2
 
     def start_requests(self):
-        url = 'https://www.goodwill.org/getLocations.php'
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        url = 'https://www.goodwill.org/GetLocAPI.php'
 
         with open('./locations/searchable_points/us_centroids_25mile_radius.csv') as points:
-
-            next(points)  # Ignore the header
-            for point in points:
-                _, lat, lon = point.strip().split(',')
-
+            reader = csv.DictReader(points)
+            for point in reader:
                 # Unable to find a way to specify a search radius
                 # Appears to use a set search radius somewhere > 25mi, using 25mi to be safe
                 form_data = {
-                    'lat': '{}'.format(lat),
-                    'lng': '{}'.format(lon),
+                    'lat': point['latitude'],
+                    'lng': point['longitude'],
                     'cats': '3,1,2,4,5'  # Includes donation sites
                 }
 
-                yield scrapy.http.FormRequest(url=url, method='POST', formdata=form_data, headers=headers,
-                                              callback=self.parse)
+                yield scrapy.http.FormRequest(
+                    url=url,
+                    method='POST',
+                    formdata=form_data,
+                    headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                    callback=self.parse,
+                )
 
     def parse(self, response):
-        data = json.loads(response.body_as_unicode())
+        data = json.loads(response.text)
 
         for store in data:
             service_codes = store.get("services")
