208ab1ef92af96dd97a289be1eb8df43f69dff32
Ian Dees <ian.dees@gmail.com>
2021-11-10 22:20:36 -0600

    Do a better job of tracking historical runs

--- ci/run_all_spiders.sh
+++ ci/run_all_spiders.sh
@@ -42,7 +42,7 @@ mkdir -p "${SPIDER_RUN_DIR}/output"
 SPIDER_COUNT=$(wc -l < "${SPIDER_RUN_DIR}/commands.txt" | tr -d ' ')
 
 (>&2 echo "Running ${SPIDER_COUNT} spiders ${PARALLELISM} at a time")
-xargs -t -L 1 -P ${PARALLELISM} -a "${SPIDER_RUN_DIR}/commands.txt" -i sh -c "{} || true"
+xargs -t -L 1 -P "${PARALLELISM}" -a "${SPIDER_RUN_DIR}/commands.txt" -i sh -c "{} || true"
 
 retval=$?
 if [ ! $retval -eq 0 ]; then
@@ -51,10 +51,10 @@ if [ ! $retval -eq 0 ]; then
 fi
 (>&2 echo "Done running spiders")
 
-OUTPUT_LINECOUNT=$(cat ${SPIDER_RUN_DIR}/output/*.geojson | wc -l | tr -d ' ')
+OUTPUT_LINECOUNT=$(wc -l < "${SPIDER_RUN_DIR}/output/*.geojson" | tr -d ' ')
 (>&2 echo "Generated ${OUTPUT_LINECOUNT} lines")
 
-echo "{\"count\": ${SPIDER_COUNT}, \"results\": []}" >> "${SPIDER_RUN_DIR}/output/results.json"
+echo "{\"count\": ${SPIDER_COUNT}, \"results\": []}" >> "${SPIDER_RUN_DIR}/stats/_results.json"
 for spider in $(scrapy list)
 do
     spider_out_geojson="${SPIDER_RUN_DIR}/output/${spider}.geojson"
@@ -77,11 +77,12 @@ do
 
     # use JQ to create an overall results JSON
     jq --compact-output \
-        --arg spider_name ${spider} \
+        --arg spider_name "${spider}" \
         --arg spider_feature_count ${feature_count} \
         --arg spider_error_count ${error_count} \
         '.results += [{"spider": $spider_name, "errors": $spider_error_count | tonumber, "features": $spider_feature_count | tonumber}]' \
-        "${SPIDER_RUN_DIR}/output/results.json"
+        "${SPIDER_RUN_DIR}/stats/_results.json" > "${SPIDER_RUN_DIR}/stats/_results.json.tmp"
+    mv "${SPIDER_RUN_DIR}/stats/_results.json.tmp" "${SPIDER_RUN_DIR}/stats/_results.json"
 done
 (>&2 echo "Wrote out summary JSON")
 
@@ -104,11 +105,12 @@ if [ ! $retval -eq 0 ]; then
 fi
 
 (>&2 echo "Saving embed to https://data.alltheplaces.xyz/runs/latest/info_embed.html")
-OUTPUT_FILESIZE=$(du ${SPIDER_RUN_DIR}/output.tar.gz | awk '{printf "%0.1f", $1/1024}')
+OUTPUT_FILESIZE=$(du "${SPIDER_RUN_DIR}/output.tar.gz")
+OUTPUT_FILESIZE_PRETTY=$(echo "$OUTPUT_FILESIZE" | awk '{printf "%0.1f", $1/1024}')
 cat > "${SPIDER_RUN_DIR}/info_embed.html" << EOF
 <html><body>
 <a href="${RUN_URL_PREFIX}/output.tar.gz">Download</a>
-(${OUTPUT_FILESIZE} MB)<br/><small>$(printf "%'d" ${OUTPUT_LINECOUNT}) rows from
+(${OUTPUT_FILESIZE_PRETTY} MB)<br/><small>$(printf "%'d" "${OUTPUT_LINECOUNT}") rows from
 ${SPIDER_COUNT} spiders, updated $(date)</small>
 </body></html>
 EOF
@@ -124,3 +126,41 @@ if [ ! $retval -eq 0 ]; then
     (>&2 echo "Couldn't save info embed to s3")
     exit 1
 fi
+
+jq --compact-output \
+    --arg run_id "${RUN_TIMESTAMP}" \
+    --arg run_output_url "${RUN_URL_PREFIX}/output.tar.gz" \
+    --arg run_stats_url "${RUN_URL_PREFIX}/stats/_results.json" \
+    --arg run_start_time "${RUN_START}" \
+    --arg run_output_size "${OUTPUT_FILESIZE}" \
+    --arg run_spider_count "${SPIDER_COUNT}" \
+    '{"run_id": $run_id, "output_url": $run_output_url, "stats_url": $run_stats_url, "start_time": $run_start_time, "size_bytes": $run_output_size | tonumber, "spiders": run_spider_count | tonumber}' \
+    > latest.json
+
+aws s3 cp \
+    --only-show-errors \
+    latest.json \
+    "s3://${S3_BUCKET}/runs/latest.json"
+
+(>&2 echo "Saving latest.json to https://data.alltheplaces.xyz/runs/latest.json")
+
+aws s3 cp \
+    --only-show-errors \
+    "s3://${S3_BUCKET}/runs/history.json" \
+    history.json
+
+if [ ! -f history.json ]; then
+    echo '[]' > history.json
+fi
+
+jq --compact-output \
+    --argjson latest_run_info "$(<latest.json)" \
+    '. += [$latest_run_info]' history.json > history.json.tmp
+mv history.json.tmp history.json
+
+(>&2 echo "Saving history.json to https://data.alltheplaces.xyz/runs/history.json")
+
+aws s3 cp \
+    --only-show-errors \
+    history.json \
+    "s3://${S3_BUCKET}/runs/history.json"
