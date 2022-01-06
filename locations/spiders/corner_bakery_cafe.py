9af1afefe6975832e59c0cc24e8dac3c7741bd1c
nbeecher <nbeecher@indeed.com>
2022-01-06 15:45:55 -0600

    Add spider for corner bakery cafe

---
+++ locations/spiders/corner_bakery_cafe.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import json
+import re
+
+import scrapy
+
+from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
+
+class CornerBakeryCafeSpider(scrapy.Spider):
+    name = "corner_bakery_cafe"
+    allowed_domains = ['cornerbakerycafe.com']
+    start_urls = [
+        'https://cornerbakerycafe.com/locations/all',
+    ]
+
+    def parse(self, response):
+        place_urls = response.xpath('//*[@class="loc-icon-home"]/a[1]/@href').extract()
+        for path in place_urls:
+            yield scrapy.Request(
+                url=response.urljoin(path),
+                callback=self.parse_store,
+            )
+
+    def parse_hours(self, hours):
+        opening_hours = OpeningHours()
+
+        for hour in hours:
+            days, times = hour.split(" ")
+            open_hour, close_hour = times.split("-")
+            if len(days) > 2:
+                day = days.split(",")
+                for d in day:
+                    opening_hours.add_range(day=d,
+                                            open_time=open_hour,
+                                            close_time=close_hour,
+                                            time_format='%H:%M')
+            else:
+                opening_hours.add_range(day=days,
+                                        open_time=open_hour,
+                                        close_time=close_hour,
+                                        time_format='%H:%M')
+
+        return opening_hours.as_opening_hours()
+
+    def parse_store(self, response):
+        data = json.loads(
+            response.xpath('//script[@type="application/ld+json"]/text()').extract_first().replace('\r\n', ''))
+
+        properties = {
+            'ref': re.search(r'.+/(.+?)/?(?:\.html|$)', response.url).group(1),
+            'name': data["name"],
+            'addr_full': data["address"]["streetAddress"],
+            'city': data["address"]["addressLocality"],
+            'state': data["address"]["addressRegion"],
+            'postcode': data["address"]["postalCode"],
+            'country': data["address"]["addressCountry"],
+            'lat': data["geo"]["latitude"],
+            'lon': data["geo"]["longitude"],
+            'phone': data["telephone"],
+            'website': data["url"]
+        }
+
+        try:
+            hours = self.parse_hours(data["openingHours"])
+            if hours:
+                properties['opening_hours'] = hours
+        except:
+            pass
+
+        yield GeojsonPointItem(**properties)
