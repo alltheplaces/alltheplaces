8022349366c5fc758c757ba8b023ef42615087de
Josh Lee <jleedev@gmail.com>
2022-01-21 03:15:05 +0000

    Fix crackerbarrel

--- locations/spiders/crackerbarrel.py
+++ locations/spiders/crackerbarrel.py
@@ -2,105 +2,46 @@
 import scrapy
 import json
 
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
+
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 
 class CrackerBarrelSpider(scrapy.Spider):
     name = "crackerbarrel"
-    item_attributes = { 'brand': "Cracker Barrel", 'brand_wikidata': "Q4492609" }
-    allowed_domains = ["locations.crackerbarrel.com"]
-    start_urls = (
-        'https://locations.crackerbarrel.com/',
-    )
-
-    def store_hours(self, store_hours):
-        day_groups = []
-        this_day_group = None
-        for line in store_hours:
-            # Applebees always seems to have a single dow
-            # in each opening hours object
-            day = line['dayOfWeek'][0][:2]
-
-            hours = '{}-{}'.format(
-                line['opens'],
-                line['closes'],
-            )
-
-            if not this_day_group:
-                this_day_group = {
-                    'from_day': day,
-                    'to_day': day,
-                    'hours': hours
-                }
-            elif this_day_group['hours'] != hours:
-                day_groups.append(this_day_group)
-                this_day_group = {
-                    'from_day': day,
-                    'to_day': day,
-                    'hours': hours
-                }
-            elif this_day_group['hours'] == hours:
-                this_day_group['to_day'] = day
-
-        day_groups.append(this_day_group)
-
-        opening_hours = ""
-        if len(day_groups) == 1 and day_groups[0]['hours'] in ('00:00-23:59', '00:00-00:00'):
-            opening_hours = '24/7'
-        else:
-            for day_group in day_groups:
-                if day_group['from_day'] == day_group['to_day']:
-                    opening_hours += '{from_day} {hours}; '.format(**day_group)
-                elif day_group['from_day'] == 'Su' and day_group['to_day'] == 'Sa':
-                    opening_hours += '{hours}; '.format(**day_group)
-                else:
-                    opening_hours += '{from_day}-{to_day} {hours}; '.format(**day_group)
-            opening_hours = opening_hours[:-2]
-
-        return opening_hours
-
-    def address(self, address):
-        if not address:
-            return None
-
-        addr_tags = {
-            "addr_full": address['streetAddress'],
-            "city": address['addressLocality'],
-            "state": address['addressRegion'],
-            "postcode": address['postalCode'],
-            "country": address['addressCountry'],
-        }
-
-        return addr_tags
+    item_attributes = {"brand": "Cracker Barrel", "brand_wikidata": "Q4492609"}
+    allowed_domains = ["crackerbarrel.com"]
+    start_urls = ("https://www.crackerbarrel.com/sitemap.xml",)
 
     def parse(self, response):
-        urls = response.xpath('//div[@class="itemlist"]/a/@href')
-        for path in urls:
-            yield scrapy.Request(
-                response.urljoin(path.extract().strip()),
-            )
-
-        urls = response.xpath('//div[@class="itemlist_fullwidth"]/a/@href')
-        for path in urls:
-            yield scrapy.Request(
-                response.urljoin(path.extract()),
-                callback=self.parse_store,
-            )
+        response.selector.remove_namespaces()
+        for url in response.xpath("//loc/text()").extract():
+            if "/Locations/States/" in url and url.count("/") == 7:
+                yield scrapy.Request(url, callback=self.parse_store)
 
     def parse_store(self, response):
-        data = json.loads(response.xpath('//script[@type="application/ld+json"]/text()')[1].extract())
+        ldjson = response.xpath('//script[@type="application/json"]/text()').get()
+        data = json.loads(ldjson)["sitecore"]["route"]["fields"]
+
+        hours = OpeningHours()
+        for day in DAYS:
+            start_time = data[f"{day}_Open"]["value"]
+            end_time = data[f"{day}_Close"]["value"]
+            hours.add_range(day[:2], start_time, end_time, "%H:%M %p")
 
         properties = {
-            'phone': data['telephone'],
-            'website': response.xpath('//head/link[@rel="canonical"]/@href').extract_first(),
-            'ref': data['@id'],
-            'opening_hours': self.store_hours(data['openingHoursSpecification']),
-            'lon': float(data['geo']['longitude']),
-            'lat': float(data['geo']['latitude']),
+            "ref": data["Store Number"]["value"],
+            "lat": data["Latitude"]["value"],
+            "lon": data["Longitude"]["value"],
+            "website": response.url,
+            "name": data["Alternate Name"]["value"],
+            "addr_full": data["Address 1"]["value"],
+            "city": data["City"]["value"],
+            "state": data["State"]["value"],
+            "postcode": data["Zip"]["value"],
+            "country": data["Country"]["value"],
+            "phone": data["Phone"]["value"],
+            "opening_hours": hours.as_opening_hours(),
         }
-
-        address = self.address(data['address'])
-        if address:
-            properties.update(address)
-
         yield GeojsonPointItem(**properties)
