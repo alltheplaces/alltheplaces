5c452abd0ba3094faeb431d314c98d684f080b9b
tlunderwood <tlunderwood@github.com>
2022-03-23 23:27:42 -0500

    Adding Smile Brands Inc to AllThePlaces

---
+++ locations/spiders/smilebrands.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import json
+import re
+
+import scrapy
+
+from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
+
+
+class SmilebrandsSpider(scrapy.Spider):
+    name = "smilebrands"
+    item_attributes = {'brand': 'Smile Brands Inc.'}
+    allowed_domains = ['smilebrands.com']
+    start_urls = [
+        'https://smilebrands.com/wp-admin/admin-ajax.php?action=store_search&lat=39.11553&lng=-94.62679&max_results=100000&search_radius=100&autoload=1',
+    ]
+
+    def parse(self, response):
+        data = json.loads(json.dumps(response.json()))
+        for place in data:
+            properties = {
+                'name': place['store'],
+                'ref': place['address'],
+                'addr_full': place['address'],
+                'city': place['city'],
+                'state': place['state'],
+                'postcode': place['zip'],
+                'country': "US",
+                'lat': place['lat'],
+                'lon': place['lng'],
+                'website': place['url'],
+            }
+            yield GeojsonPointItem(**properties)
