8da81e5f0fbb1ef73d603444d3104699dfda3dd5
Josh Lee <jleedev@gmail.com>
2022-04-01 00:49:32 +0000

    Fix awrestaurants

--- locations/spiders/awrestaurants.py
+++ locations/spiders/awrestaurants.py
@@ -4,12 +4,13 @@ import urllib.parse
 
 import scrapy
 
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
 
 
 class AwrestaurantsSpider(scrapy.Spider):
     name = "awrestaurants"
-    item_attributes = {"brand": "A&W Restaurants"}
+    item_attributes = {"brand": "A&W Restaurants", "brand_wikidata": "Q277641"}
     allowed_domains = ["awrestaurants.com"]
     start_urls = ("https://awrestaurants.com/sitemap.xml",)
 
@@ -17,32 +18,32 @@ class AwrestaurantsSpider(scrapy.Spider):
         response.selector.remove_namespaces()
         for url in response.xpath("//loc/text()").extract():
             if "/locations/" in url:
+                # Rewrite from staging domain…
                 path = urllib.parse.urlparse(url).path
                 yield scrapy.Request(response.urljoin(path), callback=self.parse_store)
 
     def parse_store(self, response):
-        script = response.xpath(
-            '//script[contains(text(), "store")]/text()'
-        ).extract_first()
-        data = json.loads(script[script.index("{") : 1 + script.rindex("}")])
-        store = data["store"]
+        data = json.loads(
+            response.xpath('//script[@type="application/ld+json"]/text()').get()
+        )
 
-        hours_text = [
-            s.strip()
-            for s in response.css(".store-header__details-hours ::text").extract()
-        ]
-        hours = "; ".join(s for s in hours_text if s)
+        hours = OpeningHours()
+        for row in data["openingHoursSpecification"]:
+            if {"opens", "closes"} <= row.keys():
+                day = row["dayOfWeek"].lstrip("https://schema.org/")[:2]
+                hours.add_range(day, row["opens"], row["closes"], "%H:%M:%S")
 
         properties = {
             "ref": response.url,
             "website": response.url,
-            "lat": store["lat"],
-            "lon": store["long"],
-            "addr_full": store["address"],
-            "city": store["city"],
-            "state": store["state"],
-            "postcode": store["zip"],
-            "phone": store["phone"],
-            "opening_hours": hours,
+            "lat": data["geo"]["latitude"],
+            "lon": data["geo"]["longitude"],
+            "name": data["name"],
+            "addr_full": data["address"]["streetAddress"],
+            "city": data["address"]["addressLocality"],
+            "state": data["address"]["addressRegion"],
+            "postcode": data["address"]["postalCode"],
+            "phone": data["telephone"],
+            "opening_hours": hours.as_opening_hours(),
         }
         return GeojsonPointItem(**properties)
