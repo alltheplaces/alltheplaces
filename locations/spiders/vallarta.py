571d6fc06c812eed604bbfdec942908d5894e446
mecklenborg <mecklenborg@indeed.com>
2022-04-07 10:40:29 -0500

    Add Vallarta spider

---
+++ locations/spiders/vallarta.py
@@ -0,0 +1,64 @@
+import scrapy
+
+from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours, DAYS
+
+
+class LowesFoodSpider(scrapy.Spider):
+    name = "vallarta"
+    item_attributes = {"brand": "Vallarta Supermarkets", "brand_wikidata": "Q7911833"}
+    allowed_domains = ["vallartasupermarkets.com"]
+    download_delay = 0.2
+    start_urls = ("https://vallartasupermarkets.com/store-sitemap.xml",)
+
+    def parse(self, response):
+        response.selector.remove_namespaces()
+        urls = response.xpath("//url/loc/text()").extract()
+        for url in urls:
+            if url == "https://vallartasupermarkets.com/store-locations/":
+                continue
+            yield scrapy.Request(
+                url=url, callback=self.parse_store, meta={"url": url}
+            )
+
+    def parse_store(self, response):
+
+        address = (
+            response.xpath("//div[@class='blade store-location']/div/div/div[2]/p[1]/text()").extract()
+        )
+
+        # No lat/lon in source code; Google map link contains address
+        yield GeojsonPointItem(
+            ref=response.url.split("/")[-2],
+            name=response.xpath("//div[@class='page-breadcrumb']/span/text()").extract_first(),
+            addr_full=address[1].strip(),
+            city=address[2].split(',')[0].strip(),
+            state=address[2].split(' ')[-2].strip(),
+            postcode=address[2].split(' ')[-1].strip(),
+            country="United States",
+            phone=response.xpath("//a[@class='tel']/text()").extract_first(),
+            website=response.url,
+            opening_hours=self.parse_hours(
+                response.xpath("//div[contains(@class, 'days')]/text()").extract_first().strip(),
+                response.xpath("//div[contains(@class, 'hours')]//p/text()").extract_first().strip()
+            ),
+        )
+
+    def parse_hours(self, days, hours):
+        opening_hours = OpeningHours()
+
+        if "7 Days a Week" in days:
+            open_time = hours.split(" - ")[0]
+            close_time = hours.split(" - ")[1]
+
+            for DAY in DAYS:
+                opening_hours.add_range(
+                    day=DAY,
+                    open_time=open_time,
+                    close_time=close_time,
+                    time_format="%I:%M%p",
+                )
+        else:
+            return None
+
+        return opening_hours.as_opening_hours()
