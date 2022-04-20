7ac7d69f157f1814427fa0bce8eed815cff2ce53
Josh Lee <jleedev@gmail.com>
2022-04-20 13:14:08 +0000

    Code format

--- locations/spiders/royallepage.py
+++ locations/spiders/royallepage.py
@@ -11,13 +11,13 @@ class RoyalLePageSpider(scrapy.Spider):
     name = "royallepage"
     allowed_domains = ["royallepage.ca"]
     start_urls = [
-        "https://www.royallepage.ca/en/search/offices/?lat=&lng=&address=&designations=&address_type=&city_name=&prov_code=&sortby=&transactionType=OFFICE&name=&location=&language=&specialization=All",
+        "https://www.royallepage.ca/en/search/offices/?lat=&lng=&address=&designations=&address_type=&city_name=&prov_code=&sortby=&transactionType=OFFICE&name=&location=&language=&specialization=All"
     ]
 
     def parse_location(self, response):
         map_script = response.xpath('//script/text()[contains(., "staticMap")]').get()
-        lat = re.search('latitude: (.*?),?$', map_script, flags=re.M).group(1)
-        lon = re.search('longitude: (.*?),?$', map_script, flags=re.M).group(1)
+        lat = re.search("latitude: (.*?),?$", map_script, flags=re.M).group(1)
+        lon = re.search("longitude: (.*?),?$", map_script, flags=re.M).group(1)
 
         properties = {
             "brand": "Royal LePage",
