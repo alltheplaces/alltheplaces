f16b4fbeea8a2a000f87af528bfcd05c1db1bb66
Cj Malone <CjMalone@mail.com>
2022-04-25 22:57:45 +0100

    Remove Lidl FR API callback

--- locations/spiders/lidl_fr.py
+++ locations/spiders/lidl_fr.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-import json
 import re
-
 import scrapy
 
 from locations.items import GeojsonPointItem
-from locations.hours import OpeningHours
 
 
 class LidlFRSpider(scrapy.Spider):
@@ -13,22 +10,19 @@ class LidlFRSpider(scrapy.Spider):
     item_attributes = {"brand": "Lidl", "brand_wikidata": "Q151954"}
     allowed_domains = ["virtualearth.net"]
     start_urls = [
-        "https://spatial.virtualearth.net/REST/v1/data/717c7792c09a4aa4a53bb789c6bb94ee/Filialdaten-FR/Filialdaten-FR?$filter=Adresstyp%20Eq%201&$top=250&$format=json&$skip=0&key=AgC167Ojch2BCIEvqkvyrhl-yLiZLv6nCK_p0K1wyilYx4lcOnTjm6ud60JnqQAa&Jsonp=displayResultStores"
+        "https://spatial.virtualearth.net/REST/v1/data/717c7792c09a4aa4a53bb789c6bb94ee/Filialdaten-FR/Filialdaten-FR?$filter=Adresstyp%20Eq%201&$top=250&$format=json&$skip=0&key=AgC167Ojch2BCIEvqkvyrhl-yLiZLv6nCK_p0K1wyilYx4lcOnTjm6ud60JnqQAa"
     ]
     download_delay = 1
 
     def parse(self, response):
-        data = json.loads(
-            re.search(r"displayResultStores\((.*)\)", response.text).groups()[0]
-        )
-
+        data = response.json()
         stores = data["d"]["results"]
 
         for store in stores:
             properties = {
                 "name": store["ShownStoreName"],
                 "ref": store["EntityID"],
-                "addr_full": store["AddressLine"],
+                "street": store["AddressLine"],
                 "city": store["Locality"],
                 "postcode": store["PostalCode"],
                 "country": store["CountryRegion"],
