e352998ef64eb9757aff5f29a16b9b0b9b5f4f36
Cj Malone <CjMalone@mail.com>
2022-04-28 09:33:20 +0100

    Add Superdrug hours

--- locations/spiders/superdrug.py
+++ locations/spiders/superdrug.py
@@ -4,6 +4,7 @@ import json
 import scrapy
 
 from locations.items import GeojsonPointItem
+from locations.hours import OpeningHours
 
 
 class SuperdrugSpider(scrapy.Spider):
@@ -32,9 +33,11 @@ class SuperdrugSpider(scrapy.Spider):
         )
 
         properties = {
-            "name": data["name"],
-            "ref": data["name"],
-            "addr_full": data["address"]["streetAddress"],
+            "name": data["name"].replace("Superdrug", "").strip(),
+            "ref": data["@id"],
+            "street_address": data["address"]["streetAddress"]
+            .replace("Superdrug", "")
+            .strip(),
             "city": data["address"]["addressLocality"],
             "state": data["address"]["addressRegion"],
             "postcode": data["address"]["postalCode"],
@@ -52,4 +55,15 @@ class SuperdrugSpider(scrapy.Spider):
                 ).extract_first()
             ),
         }
+
+        oh = OpeningHours()
+
+        for rule in data["OpeningHoursSpecification"]:
+            oh.add_range(
+                day=rule["dayOfWeek"][0:2],
+                open_time=rule["opens"],
+                close_time=rule["closes"],
+                time_format="%I:%M %p",
+            )
+
         yield GeojsonPointItem(**properties)
