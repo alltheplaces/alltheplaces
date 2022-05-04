d87b6be9b2c49bc68a8454d44bf49d3af679b208
Cj Malone <CjMalone@mail.com>
2022-05-04 13:23:47 +0100

    Add Hungry Horse spider

---
+++ locations/spiders/hungry_horse.py
@@ -0,0 +1,36 @@
+import json
+
+from scrapy.spiders import SitemapSpider
+from locations.items import GeojsonPointItem
+
+
+class HungryHorseSpider(SitemapSpider):
+    name = "hungry_horse"
+    item_attributes = {"brand": "Hungry Horse", "brand_wikidata": "Q5943510"}
+    allowed_domains = ["www.hungryhorse.co.uk"]
+    sitemap_urls = ["https://www.hungryhorse.co.uk/xml-sitemap"]
+    sitemap_rules = [
+        (
+            "https:\/\/www\.hungryhorse\.co\.uk\/pubs\/([\w-]+)\/([\w-]+)\/$",
+            "parse",
+        ),
+    ]
+    download_delay = 0.5
+
+    def parse(self, response):
+        ld = json.loads(
+            response.xpath('//script[@type="application/ld+json"]/text()').get()
+        )
+        properties = {
+            "ref": ld["branchCode"],
+            "website": response.request.url,
+            "name": ld["name"],
+            "phone": ld["telephone"],
+            "opening_hours": "; ".join(ld["openingHours"]),
+            "lat": ld["geo"]["latitude"],
+            "lon": ld["geo"]["longitude"],
+            "addr_full": ld["address"] + ", United Kingdom",
+            "country": "GB",
+        }
+
+        yield GeojsonPointItem(**properties)
