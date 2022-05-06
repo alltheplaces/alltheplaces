cb6f6ea7e6ef49c700b7779e4833b8512f8f4c89
Cj Malone <CjMalone@mail.com>
2022-05-06 14:57:55 +0100

    Move Specsavers to a sitemap spider

--- locations/spiders/specsavers.py
+++ locations/spiders/specsavers.py
@@ -6,13 +6,17 @@ from locations.items import GeojsonPointItem
 from locations.hours import OpeningHours
 
 
-class SpecsaversSpider(scrapy.Spider):
+class SpecsaversSpider(scrapy.spiders.SitemapSpider):
     download_delay = 5
     name = "specsavers"
     item_attributes = {"brand": "Specsavers", "brand_wikidata": "Q2000610"}
     allowed_domains = ["specsavers.co.uk"]
-    start_urls = [
-        "https://www.specsavers.co.uk/stores/full-store-list",
+    sitemap_urls = ["https://www.specsavers.co.uk/sitemap.xml"]
+    sitemap_rules = [
+        (
+            "https:\/\/www\.specsavers\.co\.uk\/stores\/([\w-]+)",
+            "parse_store",
+        ),
     ]
 
     def parse_store(self, response):
@@ -40,11 +44,3 @@ class SpecsaversSpider(scrapy.Spider):
         properties["opening_hours"] = oh.as_opening_hours()
 
         yield GeojsonPointItem(**properties)
-
-    def parse(self, response):
-        urls = response.xpath(
-            '//div[contains(@class,"view-full-store-list")]/div/div/div/div/ul/li/a/@href'
-        ).extract()
-
-        for url in urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
