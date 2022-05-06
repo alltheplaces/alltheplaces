9c1b5ad3e24881720e8c8bd675e13ab8bca67ccc
Cj Malone <CjMalone@mail.com>
2022-05-06 10:00:59 +0100

    Fix Specsavers by switching it to Linked Data

--- locations/spiders/specsavers.py
+++ locations/spiders/specsavers.py
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import json
 import scrapy
 
 from locations.items import GeojsonPointItem
 from locations.hours import OpeningHours
-
-DAY_MAPPING = {
-    "Monday": "Mo",
-    "Tuesday": "Tu",
-    "Wednesday": "We",
-    "Thursday": "Th",
-    "Friday": "Fr",
-    "Saturday": "Sa",
-    "Sunday": "Su",
-}
 
 
 class SpecsaversSpider(scrapy.Spider):
@@ -25,77 +15,29 @@ class SpecsaversSpider(scrapy.Spider):
         "https://www.specsavers.co.uk/stores/full-store-list",
     ]
 
-    def parse_hours(self, elements):
-        opening_hours = OpeningHours()
-
-        for elem in elements:
-            day = elem.split(" ")[0]
-            hours = elem.split(" ")[1:]
-
-            if hours[0] == "Closed":
-                continue
-            elif hours[0] == "Open 24 hours":
-                opening_hours.add_range(
-                    day=DAY_MAPPING[day], open_time="0:00", close_time="23:59"
-                )
-            else:
-                start_time = hours[0]
-                end_time = hours[2]
-                opening_hours.add_range(
-                    day=DAY_MAPPING[day], open_time=start_time, close_time=end_time
-                )
-
-        return opening_hours.as_opening_hours()
-
     def parse_store(self, response):
-        lat, lon = "", ""
-
-        try:
-            lat = float(
-                response.xpath(
-                    '//div[contains(@class,"aui-store-page")]/@data-store-geo-loc-lat'
-                ).extract_first()
-            )
-            lon = float(
-                response.xpath(
-                    '//div[contains(@class,"aui-store-page")]/@data-store-geo-loc-lng'
-                ).extract_first()
-            )
-        except:
-            pass
+        jsonld = response.xpath('//script[@type="application/ld+json"]/text()').get()
+        ld = json.loads(jsonld)
 
         properties = {
-            "addr_full": response.xpath(
-                '//span[@itemprop="streetAddress"]/text()'
-            ).extract_first(),
-            "phone": response.xpath(
-                '//span[@class="contact--store-telephone--text"]/text()'
-            ).extract_first(),
-            "city": response.xpath(
-                '//span[@itemprop="addressLocality"]/text()'
-            ).extract_first(),
-            "state": response.xpath(
-                '//span[@itemprop="addressRegion"]/text()'
-            ).extract_first(),
-            "postcode": response.xpath(
-                '//span[@itemprop="postalCode"]/text()'
-            ).extract_first(),
-            "country": "GB",
-            "ref": response.url,
-            "website": response.url,
-            "lat": lat,
-            "lon": lon,
-            "name": response.xpath(
-                '//h1[@class="store-header--title"]/text()'
-            ).extract_first(),
+            "phone": ld["telephone"],
+            "street_address": ld["address"]["streetAddress"],
+            "city": ld["address"]["addressLocality"],
+            "postcode": ld["address"]["postalCode"],
+            "country": ld["address"]["addressCountry"],
+            "ref": ld["@id"],
+            "website": ld["url"],
+            "lat": ld["geo"]["latitude"],
+            "lon": ld["geo"]["longitude"],
+            "name": ld["name"],
         }
 
-        hours = self.parse_hours(
-            response.xpath('//tr[@itemprop="openingHours"]/@content').extract()
-        )
+        oh = OpeningHours()
+        for rule in ld["openingHoursSpecification"]:
+            for day in rule["dayOfWeek"]:
+                oh.add_range(day[:2], rule["opens"], rule["closes"])
 
-        if hours:
-            properties["opening_hours"] = hours
+        properties["opening_hours"] = oh.as_opening_hours()
 
         yield GeojsonPointItem(**properties)
 
