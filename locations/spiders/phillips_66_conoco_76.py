e1468f5836da3a3a7f05d5a906d946fc4ea9eb90
Cj Malone <CjMalone@mail.com>
2022-05-12 07:51:43 +0100

    Switch to ConocoPhillips wikidata

--- locations/spiders/phillips_66_conoco_76.py
+++ locations/spiders/phillips_66_conoco_76.py
@@ -8,7 +8,7 @@ HEADERS = {"Content-Type": "application/json"}
 
 BRANDS = {"U76": "76", "P66": "Phillips 66", "CON": "Conoco"}
 
-WIKIBRANDS = {"U76": "Q1658320", "P66": "Q1656230", "CON": "Q1126518"}
+WIKIBRANDS = {"U76": "Q1658320", "P66": "Q1656230", "CON": "Q214763"}
 
 
 class Phillips66Spider(scrapy.Spider):
