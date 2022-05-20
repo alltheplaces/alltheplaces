269378dc8ca2e315fd232426c6b6e51e030ca103
Cj Malone <CjMalone@mail.com>
2022-05-20 01:03:34 +0100

    Move ProvidenceHealthServicesSpider to a SitemapSpider

--- locations/spiders/providence_health_services.py
+++ locations/spiders/providence_health_services.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
-import re
 
-import scrapy
-
+from scrapy.spiders import SitemapSpider
 from locations.items import GeojsonPointItem
-from locations.hours import OpeningHours
 
 
-class ProvidenceHealthServicesSpider(scrapy.Spider):
+class ProvidenceHealthServicesSpider(SitemapSpider):
     name = "providence_health_services"
     item_attributes = {
         "brand": "Providence Health Services",
@@ -16,14 +13,13 @@ class ProvidenceHealthServicesSpider(scrapy.Spider):
     }
     allowed_domains = ["providence.org"]
     download_delay = 0.2
-    start_urls = ["https://www.providence.org/sitemap.xml"]
-
-    def parse(self, response):
-        response.selector.remove_namespaces()
-        for url in response.xpath("//loc/text()").extract():
-            if not ("/locations/" in url and url.count("/") == 4):
-                continue
-            yield scrapy.Request(url, callback=self.parse_location)
+    sitemap_urls = ["https://www.providence.org/sitemap.xml"]
+    sitemap_rules = [
+        (
+            "https:\/\/www\.providence\.org\/locations\/[-\w]+$",
+            "parse_location",
+        )
+    ]
 
     def parse_location(self, response):
         ldjson = json.loads(
