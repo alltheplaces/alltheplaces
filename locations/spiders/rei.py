9b44343bbdc5d306432381991e77622618389508
Cj Malone <CjMalone@mail.com>
2022-05-26 09:31:38 +0100

    Improve rei output fields

--- locations/spiders/rei.py
+++ locations/spiders/rei.py
@@ -5,20 +5,10 @@ import re
 from locations.items import GeojsonPointItem
 from scrapy.spiders import SitemapSpider
 
-DAY_MAPPING = {
-    "Mon": "Mo",
-    "Tue": "Tu",
-    "Wed": "We",
-    "Thu": "Th",
-    "Fri": "Fr",
-    "Sat": "Sa",
-    "Sun": "Su",
-}
-
 
 class ReiSpider(SitemapSpider):
     name = "rei"
-    item_attributes = {"brand": "REI", "brand_wikidata": "Q3414933"}
+    item_attributes = {"brand": "REI", "brand_wikidata": "Q3414933", "country": "US"}
     allowed_domains = ["www.rei.com"]
     sitemap_urls = ["https://www.rei.com/sitemap-stores.xml"]
     sitemap_rules = [("https:\/\/www\.rei\.com\/stores\/[-\w]+$", "parse_store")]
@@ -28,7 +18,12 @@ class ReiSpider(SitemapSpider):
 
     def sitemap_filter(self, entries):
         for entry in entries:
-            if entry["loc"] != "https://www.rei.com/stores/bike-shop":
+            if not entry["loc"] in [
+                "https://www.rei.com/stores/bike-shop",
+                "https://www.rei.com/stores/ski-snowboard-shop",
+                "https://www.rei.com/stores/rentals",
+                "https://www.rei.com/stores/map",
+            ]:
                 yield entry
 
     # Fix formatting for ["Mon - Fri 10:00-1800","Sat 12:00-18:00"]
@@ -37,14 +32,14 @@ class ReiSpider(SitemapSpider):
         start_day, seperator, end_day, time_range = re.search(
             pattern, range.strip()
         ).groups()
-        result = DAY_MAPPING[start_day]
+        result = start_day[:2]
         if end_day:
-            result += "-" + DAY_MAPPING[end_day]
+            result += "-" + end_day[:2]
         result += " " + time_range
         return result
 
     def fix_opening_hours(self, opening_hours):
-        return ";".join(map(self.format_days, opening_hours))
+        return "; ".join(map(self.format_days, opening_hours))
 
     def parse_store(self, response):
         json_string = response.xpath(
@@ -61,14 +56,14 @@ class ReiSpider(SitemapSpider):
         properties = {
             "lat": store_dict["geo"].get("latitude"),
             "lon": store_dict["geo"].get("longitude"),
-            "addr_full": store_dict["address"].get("streetAddress"),
+            "name": store_dict["name"],
+            "street_address": store_dict["address"].get("streetAddress"),
             "city": store_dict["address"].get("addressLocality"),
             "state": store_dict["address"].get("addressRegion"),
             "postcode": store_dict["address"].get("postalCode"),
-            "country": store_data_model.get("countryCode"),
             "opening_hours": self.fix_opening_hours(store_dict["openingHours"]),
             "phone": store_dict.get("telephone"),
-            "website": store_dict.get("url"),
+            "website": response.url,
             "ref": store_data_model.get("storeNumber"),
         }
 
