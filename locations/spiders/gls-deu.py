5ca0fb392606a1d76665a9cbdaba949a3600314d
Ebubeker <ebubeker1rexha@gmail.com>
2022-06-04 17:14:24 +0200

    issues have been fixed. usedLongLat is needed to prevent the same store to be duplicated

--- locations/spiders/gls-deu.py
+++ locations/spiders/gls-deu.py
@@ -1,66 +1,70 @@
 import scrapy
 import json
 import csv
+from locations.items import GeojsonPointItem
+
+HEADERS = {"X-Requested-With": "XMLHttpRequest"}
+STORELOCATOR = "https://api.gls-pakete.de/parcelshops?version=4&coordinates={},{}&distance=40"
 
 class GenspiderSpider(scrapy.Spider):
-    name = 'glsdeu'
-    allowed_domains = ['api.gls-pakete.de']
-    index = 1
-    start_urls = ['https://api.gls-pakete.de/parcelshops?version=4&coordinates=50.88246,8.91596&distance=50']
-    usedLongLat = []
+    name = 'glsdeuSpider'
+    allowed_domains = ['https://www.gls-pakete.de/']
     deList = []
+    usedLongLat = []
 
-    with open('../searchable_points/eu_centroids_40km_radius_country.csv', 'r') as openFile:
-      results = csv.reader(openFile)
-      for result in results:
-          if(result[3] == 'DE'):
-              data = {
-                  "longitude": result[2],
-                  "latitude": result[1]
-              }
-              deList.append(data)
+    def start_requests(self):
+        searchable_point_files = [
+            "./locations/searchable_points/eu_centroids_40km_radius_country.csv",
+        ]
 
+        for point_file in searchable_point_files:
+            with open(point_file) as openFile:
+                results = csv.DictReader(openFile)
+                for result in results:
+                    if(result["country"] == "DE"):
+                        longitude = format(float(result["longitude"]), '.5f')
+                        latitude = format(float(result["latitude"]), '.5f')
+                        request = scrapy.Request(
+                            url=STORELOCATOR.format(str(latitude), str(longitude)),
+                            headers=HEADERS,
+                            callback=self.parse,
+                        )
+                        yield request
 
     def parse(self, response):
-
         firstResults = json.loads(response.body)
         results = firstResults['shops']
+        item = GeojsonPointItem()
+
         for result in results:
             address = result['address']
+            phone = result['phone']
             coordinates = address['coordinates']
             longitude = coordinates['longitude']
             latitude = coordinates['latitude']
             name = address['name']
+            openingHours = result['openingHours']
             state = 1
+
+            item['name'] = name
+            item["lat"] = latitude
+            item["lon"] = longitude
+            item["street"] = address["street"]
+            item["housenumber"] = address['houseNumber']
+            item["city"] = address['city']
+            item["postcode"] = address['postalCode']
+            item["phone"] = phone['number']
+            item["opening_hours"] = openingHours
 
             if(GenspiderSpider.usedLongLat):
                 for used in GenspiderSpider.usedLongLat:
-                    if(used['longitute'] == float(longitude) and used['latitude'] == float(latitude)):
+                    if (used["longitude"] == longitude and used["latitude"] == latitude):
                         state = 0
 
             if(state == 1):
-                dictToAdd = {
-                    "longitute": float(longitude),
-                    "latitude": float(latitude)
+                data = {
+                    "longitude": longitude,
+                    "latitude": latitude
                 }
-
-                GenspiderSpider.usedLongLat.append(dictToAdd)
-
-                yield {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [float(longitude), float(latitude)]
-                    },
-                    "properties": {
-                        "name": name
-                    }
-                }
-
-
-        next_link = 'https://api.gls-pakete.de/parcelshops?version=4&coordinates=' + str(format(float(GenspiderSpider.deList[GenspiderSpider.index]['latitude']), '.5f')) + ',' + str(format(float(GenspiderSpider.deList[GenspiderSpider.index]['longitude']), '.5f')) + '&distance=50'
-
-        if GenspiderSpider.index < 16477:
-            GenspiderSpider.index += 1
-            print(GenspiderSpider.index)
-            yield response.follow(next_link, callback=self.parse)
+                GenspiderSpider.usedLongLat.append(data)
+                yield item
