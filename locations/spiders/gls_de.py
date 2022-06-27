aad7bd0d5f7ba7b2fe10b85ade64e76d1945dc6b
iandees <iandees@users.noreply.github.com>
2022-06-27 18:16:21 +0000

    :art: Format Python code with psf/black

--- locations/spiders/gls_de.py
+++ locations/spiders/gls_de.py
@@ -5,9 +5,10 @@ from locations.items import GeojsonPointItem
 HEADERS = {"X-Requested-With": "XMLHttpRequest"}
 STORELOCATOR = "https://api.gls-pakete.de/parcelshops?version=4&coordinates={:0.5},{:0.5}&distance=40"
 
+
 class GeneralLogisticsSystemsSpider(scrapy.Spider):
-    name = 'gls_de'
-    allowed_domains = ['https://www.gls-pakete.de/']
+    name = "gls_de"
+    allowed_domains = ["https://www.gls-pakete.de/"]
 
     def start_requests(self):
         searchable_point_files = [
@@ -18,7 +19,7 @@ class GeneralLogisticsSystemsSpider(scrapy.Spider):
             with open(point_file) as openFile:
                 results = csv.DictReader(openFile)
                 for result in results:
-                    if(result["country"] == "DE"):
+                    if result["country"] == "DE":
                         longitude = float(result["longitude"])
                         latitude = float(result["latitude"])
                         request = scrapy.Request(
@@ -30,26 +31,26 @@ class GeneralLogisticsSystemsSpider(scrapy.Spider):
 
     def parse(self, response):
         first_results = response.json()
-        results = first_results['shops']
+        results = first_results["shops"]
 
         for result in results:
             item = GeojsonPointItem()
-            address = result['address']
-            phone = result['phone']
-            coordinates = address['coordinates']
-            longitude = coordinates['longitude']
-            latitude = coordinates['latitude']
-            name = address['name']
-            opening_hours = result['openingHours']
+            address = result["address"]
+            phone = result["phone"]
+            coordinates = address["coordinates"]
+            longitude = coordinates["longitude"]
+            latitude = coordinates["latitude"]
+            name = address["name"]
+            opening_hours = result["openingHours"]
 
-            item['ref'] = result['id']
-            item['name'] = name
+            item["ref"] = result["id"]
+            item["name"] = name
             item["lat"] = latitude
             item["lon"] = longitude
             item["street"] = address["street"]
-            item["housenumber"] = address['houseNumber']
-            item["city"] = address['city']
-            item["postcode"] = address['postalCode']
-            item["phone"] = phone['number']
+            item["housenumber"] = address["houseNumber"]
+            item["city"] = address["city"]
+            item["postcode"] = address["postalCode"]
+            item["phone"] = phone["number"]
             item["opening_hours"] = opening_hours
             yield item
