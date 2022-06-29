72a4c6c5093fc755090a0a4f23aaa902b9e572d1
Cj Malone <CjMalone@mail.com>
2022-06-29 10:39:20 +0100

    Fix baylorscottwhite

--- locations/spiders/baylorscottwhitehealth.py
+++ locations/spiders/baylorscottwhitehealth.py
@@ -1,4 +1,6 @@
 import scrapy
+
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
 
 
@@ -8,46 +10,61 @@ class BaylorScottWhiteSpider(scrapy.Spider):
         "brand": "Baylor Scott & White Health",
         "brand_wikidata": "Q41568258",
     }
-    allowed_domains = ["www.bswhealth.com", "phyndapi.bswapi.com"]
-    start_urls = ("https://www.bswhealth.com/",)
+    allowed_domains = ["phyndapi.bswapi.com"]
+    download_delay = 1
+    base_url = (
+        "https://phyndapi.bswapi.com/V4/Places/GetLocations"
+        "?location=30.259317199999998,-97.7393817"
+        "&distance=100000"
+    )
 
     def start_requests(self):
-        url = "https://phyndapi.bswapi.com/V2/Places/GetLocations?&location=30.259317199999998,-97.7393817&distance=100000&LineOfBusiness=BSWH&pageNumber=1&sortSeed=1049&perPage=1000&SortBy=Distance&DocSortBy=NextAvailableAppointment&overrideRules=false"
-
-        headers = {
-            "Referer": "https://www.bswhealth.com/",
-            "Cookie": "ARRAffinity=abb1bf08643cf24445e2af4eb0b40e562ee3a33b388db503d33b42e5adafc29f;Path=/;HttpOnly;Secure;Domain=phyndapi.bswapi.com",
-            "Authorization": "phyndapi.bswapi.com",
-            "x-bsw-clientid": "BSWHealth.com",
-        }
-
-        yield scrapy.http.FormRequest(
-            url,
-            self.parse,
-            method="GET",
-            headers=headers,
+        yield scrapy.Request(
+            self.base_url + "&perPage=1",
+            callback=self.get_pages,
         )
 
-    def parse(self, response):
-        ldata = response.json()
+    def get_pages(self, response):
+        total_count = response.json()["locationCount"]
+        page_number = 0
+        page_size = 100
 
-        ldata = ldata["locationResults"]
+        while page_number * page_size < total_count:
+            yield scrapy.Request(
+                self.base_url + f"&perPage={page_size}&pageNumber={page_number+1}"
+            )
+            page_number += 1
+
+    def parse(self, response):
+        ldata = response.json()["locationResults"]
 
         for row in ldata:
 
             properties = {
                 "ref": row["locationID"],
                 "name": row["locationName"],
-                "addr_full": " ".join(
-                    [row["locationStreet1"], row.get("locationStreet2", "") or ""]
+                "street_address": " ".join(
+                    [row["locationStreet1"], row.get("locationStreet2", "")]
                 ).strip(),
                 "city": row["locationCity"],
                 "postcode": row["locationZip"],
                 "state": row["locationState"],
                 "lat": row["coordinates"]["lat"],
                 "lon": row["coordinates"]["lon"],
-                "phone": row["locationPhone"],
+                "phone": row["locationPhone"].replace(".", " "),
                 "website": row["locationUrl"],
+                "image": row["photoUrl"],
             }
+
+            oh = OpeningHours()
+            for day in row["dailyHours"]:
+                oh.add_range(
+                    day["weekDayName"][:2],
+                    day["openingTime"],
+                    day["closingTime"],
+                    "%H:%M:%S",
+                )
+
+            properties["opening_hours"] = oh.as_opening_hours()
 
             yield GeojsonPointItem(**properties)
