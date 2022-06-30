267766283fb41a3a2697a7772e96c94043f21bb5
Cj Malone <CjMalone@mail.com>
2022-06-30 10:51:40 +0100

    Move to SitemapSpider

--- locations/spiders/societe_generale.py
+++ locations/spiders/societe_generale.py
@@ -1,27 +1,19 @@
 # -*- coding: utf-8 -*-
 import json
-import xmltodict
 
-import scrapy
-
+from scrapy.spiders import SitemapSpider
 from locations.items import GeojsonPointItem
 
 
-class SocieteGeneraleSpider(scrapy.Spider):
+class SocieteGeneraleSpider(SitemapSpider):
     name = "societe_generale"
     item_attributes = {"brand": "Societe Generale", "brand_wikidata": "Q270363"}
     allowed_domains = ["societegenerale.com", "agences.societegenerale.fr"]
-    start_urls = [
+    sitemap_urls = [
         "https://agences.societegenerale.fr/banque-assurance/sitemap_agence_pois.xml",
     ]
 
     def parse(self, response):
-        data = xmltodict.parse(response.text)
-        for url in data["urlset"]["url"]:
-            if "loc" in url:
-                yield scrapy.Request(url=url["loc"], callback=self.parse_store)
-
-    def parse_store(self, response):
         # Get all script tags with type application/ld+json
         store = json.loads(
             response.xpath('//script[@type="application/ld+json"]/text()').get()
