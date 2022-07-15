044013e162748ae32ab3f9f415b83f340041e490
Cj Malone <CjMalone@mail.com>
2022-07-15 13:28:03 +0100

    Add lat lon linked data test

--- tests/test_ld_parser.py
+++ tests/test_ld_parser.py
@@ -43,6 +43,33 @@ def test_ld():
     assert i["postcode"] == "94086"
     assert i["street_address"] == "1901 Lemur Ave"
     assert i["name"] == "GreatFood"
-    assert i["opening_hours"] == "Mo-Th 11:00-14:30,17:00-21:30; Fr-Sa 11:00-14:30,17:00-22:00"
+    assert (
+            i["opening_hours"]
+            == "Mo-Th 11:00-14:30,17:00-21:30; Fr-Sa 11:00-14:30,17:00-22:00"
+    )
     assert i["phone"] == "(408) 714-1489"
     assert i["website"] == "http://www.greatfood.com"
+    assert i["ref"] is None
+
+
+def test_ld_lat_lon():
+    i = GeojsonPointItem()
+    i.from_linked_data(
+        json.loads(
+            """
+            {
+                "@context": "https://schema.org",
+                "@type": "Place",
+                "geo": {
+                    "@type": "GeoCoordinates",
+                    "latitude": "40.75",
+                    "longitude": "-73.98"
+                },
+                "name": "Empire State Building"
+            }
+            """
+        )
+    )
+
+    assert i["lat"] == "40.75"
+    assert i["lon"] == "-73.98"
