3fa49c2069ea9301a2d800c9a4e31dada2181751
Romeo <romeo@echo-analytics.io>
2022-07-21 11:50:41 +0200

    fix: clean

--- locations/spiders/pandaexpress.py
+++ locations/spiders/pandaexpress.py
@@ -11,7 +11,7 @@ class PandaExpressSpider(scrapy.Spider):
     name = "pandaexpress"
     item_attributes = {"brand": "Panda Express", "brand_wikidata": "Q1358690"}
     allowed_domains = ["pandaexpress.com"]
-    # download_delay = 0.5
+
     def start_requests(self):
         with open(
             "./locations/searchable_points/us_centroids_10mile_radius.csv"
@@ -35,7 +35,7 @@ class PandaExpressSpider(scrapy.Spider):
                     "lng": point["longitude"],
                 }
 
-                url = f"https://nomnom-prod-api.pandaexpress.com/restaurants/near?lat={params.get('lat')}&long={params.get('lng')}&radius=100&limit=200&nomnom=calendars&nomnom_calendars_from=20220720&nomnom_calendars_to=20220731&nomnom_exclude_extref=99997,99996,99987,99988,99989,99994,11111,8888,99998,99999,0000"
+                url = f"https://nomnom-prod-api.pandaexpress.com/restaurants/near?lat={params.get('lat')}&long={params.get('lng')}&radius=100&limit=200&nomnom=calendars&nomnom_calendars_from=20220720&nomnom_calendars_to=20220731&nomnom_exclude_extref=99997,99996,99987,99988,99989,99994,11111,8888,99998,99999,0000 "
                 yield scrapy.Request(url, callback=self.parse_stores)
 
     def parse_stores(self, response):
