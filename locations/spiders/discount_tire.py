666a6aaa1ebe089466ec29accf0d11a12221010d
Ian Dees <ian.dees@gmail.com>
2022-07-22 12:32:03 -0700

    Fix Discount Tire

--- locations/spiders/discount_tire.py
+++ locations/spiders/discount_tire.py
@@ -1,54 +1,50 @@
 # -*- coding: utf-8 -*-
 import json
-import logging
 import re
-
 import scrapy
 
 from locations.items import GeojsonPointItem
+from scrapy.spiders import SitemapSpider
 
 URL = "https://data.discounttire.com/webapi/discounttire.graph"
 
 
-class DiscountTireSpider(scrapy.Spider):
+class DiscountTireSpider(SitemapSpider):
     name = "discount_tire"
     item_attributes = {"brand": "Discount Tire", "brand_wikidata": "Q5281735"}
     allowed_domains = ["discounttire.com"]
-    start_urls = [
+    sitemap_urls = [
         "https://www.discounttire.com/sitemap.xml",
     ]
-    download_delay = 3.0
+    sitemap_follow = [
+        r".*Discount-Tire-Sitemap-Categories-Content.*\.xml",
+    ]
+    sitemap_rules = [
+        (
+            r"^https://www.discounttire.com/store/.*",
+            "parse_site",
+        )
+    ]
+    download_delay = 5.0
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
     }
 
-    def parse(self, response):
-        response.selector.remove_namespaces()
-        url = response.xpath(
-            '//loc[contains(text(), "Sitemap-Categories")]/text()'
-        ).extract_first()
-
-        yield scrapy.Request(url=url, callback=self.parse_site)
-
     def parse_site(self, response):
-        response.selector.remove_namespaces()
-        urls = response.xpath('//loc[contains(text(), "/store/")]/text()').extract()
+        store_code = re.search(r".*/s/(\d*)$", response.url).group(1)
+        payload = {
+            "operationName": "StoreByCode",
+            "variables": {"storeCode": f"{store_code}"},
+            "query": "query StoreByCode($storeCode: String!) {\n  store {\n    byCode(storeCode: $storeCode) {\n      ...myStoreFields\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment myStoreFields on StoreData {\n  code\n  address {\n    country {\n      isocode\n      name\n      __typename\n    }\n    email\n    line1\n    line2\n    phone\n    postalCode\n    region {\n      isocodeShort\n      name\n      __typename\n    }\n    town\n    __typename\n  }\n  winterStore\n  baseStore\n  description\n  displayName\n  isBopisTurnedOff: bopisTurnedOff\n  distance\n  legacyStoreCode\n  geoPoint {\n    latitude\n    longitude\n    __typename\n  }\n  rating {\n    rating\n    numberOfReviews\n    __typename\n  }\n  weekDays {\n    closed\n    formattedDate\n    dayOfWeek\n    __typename\n  }\n  __typename\n}\n",
+        }
 
-        for url in urls:
-            store_code = re.search(r".+/(.+?)/?(?:\.html|$)", url).group(1)
-            payload = {
-                "operationName": "StoreByCode",
-                "variables": {"storeCode": f"{store_code}"},
-                "query": "query StoreByCode($storeCode: String!) {\n  store {\n    byCode(storeCode: $storeCode) {\n      ...myStoreFields\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment myStoreFields on StoreData {\n  code\n  address {\n    country {\n      isocode\n      name\n      __typename\n    }\n    email\n    line1\n    line2\n    phone\n    postalCode\n    region {\n      isocodeShort\n      name\n      __typename\n    }\n    town\n    __typename\n  }\n  winterStore\n  baseStore\n  description\n  displayName\n  isBopisTurnedOff: bopisTurnedOff\n  distance\n  legacyStoreCode\n  geoPoint {\n    latitude\n    longitude\n    __typename\n  }\n  rating {\n    rating\n    numberOfReviews\n    __typename\n  }\n  weekDays {\n    closed\n    formattedDate\n    dayOfWeek\n    __typename\n  }\n  __typename\n}\n",
-            }
-
-            yield scrapy.Request(
-                url=URL,
-                method="POST",
-                body=json.dumps(payload),
-                callback=self.parse_stores,
-                meta={"website": url},
-            )
+        yield scrapy.Request(
+            url=URL,
+            method="POST",
+            body=json.dumps(payload),
+            callback=self.parse_stores,
+            meta={"website": response.url},
+        )
 
     def parse_stores(self, response):
         store_data = json.loads(response.text)
