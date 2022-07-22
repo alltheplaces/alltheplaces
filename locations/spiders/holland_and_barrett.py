8b33708566b280ee653546c0ba30251966aa159a
Cj Malone <CjMalone@mail.com>
2022-07-22 13:20:17 +0100

    Add Holland & Barrett spider

---
+++ locations/spiders/holland_and_barrett.py
@@ -0,0 +1,22 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.linked_data_parser import LinkedDataParser
+
+
+class HollandAndBarrettSpider(SitemapSpider):
+    name = "holland_and_barrett"
+    item_attributes = {
+        "brand": "Holland & Barrett",
+        "brand_wikidata": "Q5880870",
+    }
+    sitemap_urls = ["https://www.hollandandbarrett.com/sitemap-stores.xml"]
+    sitemap_rules = [
+        (
+            r"https:\/\/www\.hollandandbarrett\.com\/stores\/([-\w]+)\/$",
+            "parse",
+        )
+    ]
+    custom_settings = {"REDIRECT_ENABLED": False}
+
+    def parse(self, response):
+        yield LinkedDataParser.parse(response, "LocalBusiness")
