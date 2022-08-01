434d4d086b8873b135d98d2145f58b579e59c361
Cj Malone <CjMalone@mail.com>
2022-08-01 21:45:25 +0100

    Add Oak Furnitureland spider

---
+++ locations/spiders/oak_furnitureland.py
@@ -0,0 +1,31 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.linked_data_parser import LinkedDataParser
+
+
+class OakFurnitureland(SitemapSpider):
+    name = "oak_furnitureland"
+    item_attributes = {
+        "brand": "Oak Furnitureland",
+        "brand_wikidata": "Q16959724",
+        "country": "GB",
+    }
+    sitemap_urls = ["https://www.oakfurnitureland.co.uk/sitemaps/sitemap.xml"]
+    sitemap_rules = [
+        (
+            r"https:\/\/www\.oakfurnitureland\.co\.uk\/showrooms\/([\w-]+).html$",
+            "parse",
+        ),
+    ]
+
+    def parse(self, response):
+        if "PERMANENTLY CLOSED" in "".join(
+            response.xpath('//div[@id="title"]/descendant::*/text()').getall()
+        ):
+            return
+
+        item = LinkedDataParser.parse(response, "LocalBusiness")
+
+        item["ref"] = response.url
+
+        return item
