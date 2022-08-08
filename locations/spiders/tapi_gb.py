b9f6461c2c4de9e17ee53a4cc5c8344c57b3882d
Joe Dixon <mjoe999@gmail.com>
2022-08-08 16:20:13 +0100

    add Tapi (GB) spider

---
+++ locations/spiders/tapi_gb.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from locations.linked_data_parser import LinkedDataParser
+
+
+class TapiGBSpider(scrapy.spiders.SitemapSpider):
+    name = "tapi_gb"
+    item_attributes = {
+        "brand": "Tapi",
+        "brand_wikidata": "Q79223951",
+    }
+    allowed_domains = ["tapi.co.uk"]
+    sitemap_urls = ["https://www.tapi.co.uk/sitemap.xml"]
+    sitemap_rules = [("/stores/", "parse_store")]
+
+    def parse_store(self, response):
+        item = LinkedDataParser.parse(response, "HomeGoodsStore")
+        if item:
+            item["ref"] = response.url
+            yield item
