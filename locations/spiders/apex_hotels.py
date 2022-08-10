700eda5342fd14407ae59e40b16e0728ae93e10a
Cj Malone <CjMalone@mail.com>
2022-08-10 09:10:09 +0100

    Add Apex Hotels spider

---
+++ locations/spiders/apex_hotels.py
@@ -0,0 +1,26 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.linked_data_parser import LinkedDataParser
+
+
+class ApexHotelsSpider(SitemapSpider):
+    name = "apex_hotels"
+    item_attributes = {
+        "brand": "Apex Hotels",
+        "brand_wikidata": "Q4779426",
+    }
+    sitemap_urls = ["https://www.apexhotels.co.uk/sitemapxml/"]
+    sitemap_rules = [
+        (
+            r"https:\/\/www\.apexhotels\.co\.uk\/destinations\/([-\w]+)\/([-\w]+)\/$",
+            "parse_item",
+        )
+    ]
+    custom_settings = {"REDIRECT_ENABLED": "False"}
+
+    def parse_item(self, response):
+        item = LinkedDataParser.parse(response, "Hotel")
+        if item:
+            item["ref"] = response.url.split("/")[5]
+
+            return item
