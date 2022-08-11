075c640ccc7f15fd7aecb0860c720925be07860e
Joe Dixon <mjoe999@gmail.com>
2022-08-11 19:47:03 +0100

    fix lint riverisland_gb

--- locations/spiders/riverisland_gb.py
+++ locations/spiders/riverisland_gb.py
@@ -11,9 +11,15 @@ class RiverIslandGBSpider(CrawlSpider):
         "brand": "RIVER ISLAND",
         "brand_wikidata": "Q2670328",
     }
-    allowed_domains = ['riverisland.com']
-    start_urls = ['https://www.riverisland.com/river-island-stores']
-    rules = [Rule(LinkExtractor(allow='how-can-we-help/find-a-store'), callback='parse_func', follow=False)]
+    allowed_domains = ["riverisland.com"]
+    start_urls = ["https://www.riverisland.com/river-island-stores"]
+    rules = [
+        Rule(
+            LinkExtractor(allow="how-can-we-help/find-a-store"),
+            callback="parse_func",
+            follow=False,
+        )
+    ]
 
     def parse_func(self, response):
         ld = LinkedDataParser.find_linked_data(response, "ClothingStore")
