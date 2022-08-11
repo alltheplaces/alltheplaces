ee909ecc86d04122be6cde589e88c3662990c350
Cj Malone <CjMalone@mail.com>
2022-08-11 11:25:49 +0100

    Make Claires global
    Utilise PandoraSpider as they share the same mistakes

--- locations/spiders/pandora.py
+++ locations/spiders/pandora.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
+from locations.items import GeojsonPointItem
 from locations.linked_data_parser import LinkedDataParser
 
 
@@ -12,12 +14,17 @@ class PandoraSpider(scrapy.spiders.SitemapSpider):
     download_delay = 0.2
     allowed_domains = ["pandora.net"]
     sitemap_urls = ["https://stores.pandora.net/sitemap.xml"]
-    sitemap_rules = [("\.html", "parse_store")]
+    sitemap_rules = [
+        (r"https:\/\/stores\.pandora\.net\/.+-([\w\d]+)\.html$", "parse_store")
+    ]
 
     def parse_store(self, response):
+        yield self.parse_item(response)
+
+    @staticmethod
+    def parse_item(response) -> GeojsonPointItem:
         item = LinkedDataParser.parse(response, "JewelryStore")
         if item:
-            item["name"] = "Pandora"
             item["ref"] = response.url
             # In many countries "state" is set to "country-region", unpick and discard region
             splits = item["state"].split("-")
