614b9631f31a53c79ff5a8b48bcf82757430b8e6
Joe Dixon <mjoe999@gmail.com>
2022-08-12 16:24:03 +0100

    Add jdsports_gb (400 sports goods stores)

---
+++ locations/spiders/jdsports_gb.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from locations.linked_data_parser import LinkedDataParser
+
+
+class JDSportsGBSpider(CrawlSpider):
+    name = "jdsports_gb"
+    item_attributes = {
+        "brand": "JD Sports",
+        "brand_wikidata": "Q6108019",
+    }
+    start_urls = ["https://www.jdsports.co.uk/store-locator/all-stores/"]
+    rules = [
+        Rule(
+            LinkExtractor(allow="store-locator/"), callback="parse_store", follow=False
+        )
+    ]
+    download_delay = 0.2
+
+    def parse_store(self, response):
+        if "-soon" in response.url:
+            self.logger.warning("ignoring store opening soon %s", response.url)
+        else:
+            item = LinkedDataParser.parse(response, "Store")
+            if item:
+                item["ref"] = response.url
+                yield item
