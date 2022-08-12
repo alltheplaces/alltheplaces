01c64254c36f76f00c4894d95fb2b68d51b21ef7
Joe Dixon <mjoe999@gmail.com>
2022-08-12 14:36:16 +0100

    Add petsathome_gb + vets4pets_gb (850 sites)

---
+++ locations/spiders/petsathome_gb.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+import csv
+from locations.linked_data_parser import LinkedDataParser
+from scrapy.http import FormRequest
+
+
+# This method could be moved into library code and documented, tested etc, supports calls of the form:
+# get_locations("eu_centroids_40km_radius_country.csv")
+# get_locations("eu_centroids_40km_radius_country.csv", ["GB", "IE"])
+# get_locations("us_centroids_50mile_radius_state.csv", "NY")
+def get_locations(areas_csv_file, area_field_filter=None):
+    def get_key(row, keys):
+        for key in keys:
+            if row.get(key):
+                return row[key]
+        return None
+
+    if area_field_filter and type(area_field_filter) is not list:
+        area_field_filter = [area_field_filter]
+    with open("./locations/searchable_points/{}".format(areas_csv_file)) as points:
+        for row in csv.DictReader(points):
+            lat = get_key(row, ["latitude", "lat"])
+            lon = get_key(row, ["longitude", "lng"])
+            if not lat or not lon:
+                raise Exception("missing lat/lon in file")
+            area = get_key(row, ["country", "territory", "state"])
+            if area_field_filter:
+                if not area:
+                    raise Exception(
+                        "trying to perform area filter on file with no area support"
+                    )
+                if area not in area_field_filter:
+                    continue
+            yield lat, lon
+
+
+class PetsAtHomeGBSpider(scrapy.Spider):
+    name = "petsathome_gb"
+    item_attributes = {
+        "brand": "Pets at Home",
+        "brand_wikidata": "Q7179258",
+    }
+    download_delay = 0.2
+
+    def start_requests(self):
+        headers = {"X-Requested-With": "XMLHttpRequest"}
+        url = "https://community.petsathome.com/models/utils.cfc"
+        for (lat, lon) in get_locations("eu_centroids_20km_radius_country.csv", "UK"):
+            form_data = {
+                "method": "functionhandler",
+                "returnFormat": "json",
+                "event": "webproperty.storelocator",
+                "lat": str(lat),
+                "lng": str(lon),
+                "radius": "50",
+                "companyID": "any",
+                "active": "true",
+            }
+            yield FormRequest(
+                url, callback=self.parse_func, formdata=form_data, headers=headers
+            )
+
+    def parse_func(self, response):
+        for node in json.loads(response.body)["data"]:
+            if "groom-room" not in node["slug"]:
+                yield scrapy.Request("https://community.petsathome.com" + node["slug"])
+
+    def parse(self, response):
+        ld = LinkedDataParser.find_linked_data(response, "PetStore")
+        if ld:
+            # Our LD parser is reasonably strict, help petaathome meet its requirements.
+            ld["address"]["@type"] = "PostalAddress"
+            return LinkedDataParser.parse_ld(ld)
---
+++ locations/spiders/vets4pets_gb.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from locations.linked_data_parser import LinkedDataParser
+from locations.spiders.petsathome_gb import PetsAtHomeGBSpider
+from locations.google_url import url_to_coords
+
+
+def extract_google_position(item, response):
+    for link in response.xpath("//iframe/@src").extract():
+        if "google.com" in link:
+            item["lat"], item["lon"] = url_to_coords(link)
+            return
+
+
+def set_located_in(item, location):
+    item["located_in"] = location["brand"]
+    item["located_in_wikidata"] = location["brand_wikidata"]
+
+
+class Vets4PetsGBSpider(CrawlSpider):
+    name = "vets4pets_gb"
+    item_attributes = {
+        "brand": "Vets4Pets",
+        "brand_wikidata": "Q16960196",
+    }
+    start_urls = ["https://www.vets4pets.com/practices/"]
+    rules = [
+        Rule(LinkExtractor(allow="/practices/"), callback="parse_func", follow=True)
+    ]
+    allowed_domains = ["vets4pets.com"]
+    download_delay = 0.2
+
+    def parse_func(self, response):
+        ld = LinkedDataParser.find_linked_data(response, "VeterinaryCare")
+        if ld:
+            # Should we support lists of addresses in the common parser code and take the first?
+            ld["address"] = ld["address"][0]
+            item = LinkedDataParser.parse_ld(ld)
+            item["ref"] = response.url
+            extract_google_position(item, response)
+            if "petsathome" in item["street_address"].lower().replace(" ", ""):
+                self.logger.info("%s located in Pets at Home store", item["name"])
+                set_located_in(item, PetsAtHomeGBSpider.item_attributes)
+            return item
