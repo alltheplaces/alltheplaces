ab767bca659431a7117f9cb9f3db6c86db4cfca3
Cj Malone <CjMalone@mail.com>
2022-08-16 10:57:39 +0100

    Add Generic Dict Parser

---
+++ locations/dict_parser.py
@@ -0,0 +1,62 @@
+from locations.items import GeojsonPointItem
+
+
+class DictParser(object):
+    @staticmethod
+    def parse(obj) -> GeojsonPointItem:
+        item = GeojsonPointItem()
+
+        location = DictParser.get_first_key(obj, ["location", "geolocation"])
+
+        # Check for a location object, if not use the parent
+        if location and not isinstance(location, dict):
+            location = obj
+
+        item["lat"] = DictParser.get_first_key(location, ["latitude", "lat"])
+        item["lon"] = DictParser.get_first_key(location, ["longitude", "lon", "long", "lng"])
+
+        item["name"] = DictParser.get_first_key(
+            obj, ["name", "storeName", "displayName"]
+        )
+
+        address = DictParser.get_first_key(obj, ["address", "addr"])
+
+        if address and isinstance(address, str):
+            item["addr_full"] = address
+
+        if not address or not isinstance(address, dict):
+            address = obj
+
+        item["housenumber"] = DictParser.get_first_key(
+            address, ["houseNumber", "houseNo", "streetNumber"]
+        )
+        item["street"] = DictParser.get_first_key(address, ["street", "streetName"])
+        item["street_address"] = DictParser.get_first_key(
+            address, ["streetAddress", "street_address", "line1", "line"]
+        )
+
+        if isinstance(item["street_address"], list):
+            item["street_address"] = ", ".join(item["street_address"])
+
+        item["city"] = DictParser.get_first_key(address, ["city", "town"])
+        item["state"] = DictParser.get_first_key(address, ["state"])
+        item["postcode"] = DictParser.get_first_key(
+            address, ["postCode", "post_code", "postalCode"]
+        )
+        item["country"] = DictParser.get_first_key(address, ["country", "countryCode"])
+
+        item["phone"] = DictParser.get_first_key(obj, ["phone", "telephone", "tel"])
+
+        item["ref"] = DictParser.get_first_key(obj, ["ref", "id", "store_id", "shopNumber", "slug"])
+
+        return item
+
+    @staticmethod
+    def get_first_key(obj, keys):
+        for key in keys:
+            if obj.get(key):
+                return obj[key]
+            elif obj.get(key.lower()):
+                return obj[key.lower()]
+            elif obj.get(key.upper()):
+                return obj[key.upper()]
