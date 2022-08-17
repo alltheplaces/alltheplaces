1f6514097466834d5e8723523ac3de825e6047e3
Ian Dees <ian.dees@gmail.com>
2022-08-17 10:51:31 -0500

    Extract location from google URL

--- locations/spiders/chickfila.py
+++ locations/spiders/chickfila.py
@@ -2,12 +2,13 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.linked_data_parser import LinkedDataParser
+from locations.google_url import url_to_coords
 
 
 class ChickFilASpider(SitemapSpider):
     name = "chickfila"
     item_attributes = {"brand": "Chick-Fil-A", "brand_wikidata": "Q491516"}
-    allowed_domains = ["chick-fil-a.com", "yext-cdn.com"]
+    allowed_domains = ["chick-fil-a.com"]
     sitemap_urls = ["https://www.chick-fil-a.com/sitemap.xml"]
     sitemap_rules = [
         (
@@ -19,10 +20,11 @@ class ChickFilASpider(SitemapSpider):
     def parse(self, response):
         item = LinkedDataParser.parse(response, "Restaurant")
 
+        # Note that their opening hours specification doesn't include closing times, so no opening hours for now
         item["ref"] = "-".join(response.url.rsplit("/", 2)[-2:])
         item["phone"] = response.xpath(
             "//a[@id='LocationDetail-PhoneNumber']/a/text()"
         ).extract_first()
-        # Note that their opening hours specification doesn't include closing times, so no opening hours for now
+        item["lat"], item["lon"] = url_to_coords(response.xpath("//div[@id='map-modal']/div/div/a/@href").extract_first())
 
         yield item
