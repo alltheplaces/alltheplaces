ea2bad6d63ebe3f57009daca93c090ff8e6c5ad9
Josh Lee <jleedev@gmail.com>
2022-08-21 19:01:48 +0000

    Appears to be a more up to date part of their site

--- locations/spiders/forever21_us.py
+++ locations/spiders/forever21_us.py
@@ -1,30 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from locations.microdata_parser import MicrodataParser
+from locations.linked_data_parser import LinkedDataParser
 from locations.items import GeojsonPointItem
 
 
-class Forever21UsSpider(scrapy.Spider):
+class Forever21UsSpider(scrapy.spiders.SitemapSpider):
     name = "forever21_us"
     item_attributes = {
         "brand": "Forever 21",
         "brand_wikidata": "Q1060537",
     }
     allowed_domains = ["forever21.com"]
-    start_urls = [
-        "https://www.forever21.com/on/demandware.store/Sites-forever21-Site/en_US/Stores-FindStores?showMap=false&radius=10000&lat=45&long=-104",
+    sitemap_urls = [
+        "https://locations.forever21.com/robots.txt",
+    ]
+    sitemap_rules = [
+        (r"/stores/", "parse"),
     ]
 
     def parse(self, response):
-        for row in response.json()["stores"]:
-            properties = {
-                "ref": row["ID"],
-                "lat": row["latitude"],
-                "lon": row["longitude"],
-                "name": row["name"],
-                "street_address": row["address1"],
-                "city": row["city"],
-                "state": row["stateCode"],
-                "country": row["countryCode"],
-            }
-            yield GeojsonPointItem(**properties)
+        for city in response.css('[itemprop="address"] .Address-city'):
+            city.root.set("itemprop", "addressLocality")
+        MicrodataParser.convert_to_json_ld(response)
+        item = LinkedDataParser.parse(response, "ClothingStore")
+        yield item
