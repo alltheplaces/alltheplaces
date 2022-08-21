5abbb0790ff5eea0a53d3e42e855e243d3780e38
Josh Lee <jleedev@gmail.com>
2022-08-21 03:41:59 +0000

    Fix brookdale

--- locations/spiders/brookdale.py
+++ locations/spiders/brookdale.py
@@ -1,86 +1,27 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
+import urllib.parse
 import scrapy
-from locations.items import GeojsonPointItem
 
-URL = "https://www.brookdale.com/bin/brookdale/community-search?care_type_category=resident&loc=&finrpt=&state="
-
-US_STATES = (
-    "AL",
-    "AZ",
-    "AR",
-    "CA",
-    "CO",
-    "CT",
-    "DE",
-    "FL",
-    "GA",
-    "ID",
-    "IL",
-    "IN",
-    "IA",
-    "KS",
-    "KY",
-    "LA",
-    "MD",
-    "MA",
-    "MI",
-    "MN",
-    "MS",
-    "MO",
-    "MT",
-    "NE",
-    "NV",
-    "NH",
-    "NJ",
-    "NM",
-    "NY",
-    "NC",
-    "OH",
-    "OK",
-    "OR",
-    "PA",
-    "RI",
-    "SC",
-    "TN",
-    "TX",
-    "UT",
-    "VT",
-    "VA",
-    "WA",
-    "WV",
-    "WI",
-    "WY",
-)
+from locations.linked_data_parser import LinkedDataParser
 
 
-class BrookdaleSpider(scrapy.Spider):
+class BrookdaleSpider(scrapy.spiders.SitemapSpider):
     name = "brookdale"
     item_attributes = {"brand": "Brookdale"}
     allowed_domains = ["www.brookdale.com"]
+    sitemap_urls = [
+        "https://www.brookdale.com/robots.txt",
+    ]
+    sitemap_rules = [
+        (r"/communities/[^/]+$", "parse"),
+    ]
 
-    def start_requests(self):
-        for state in US_STATES:
-            url = "".join([URL, state])
-            yield scrapy.Request(url, callback=self.parse_info)
-
-    def parse_info(self, response):
-
-        data = response.json()
-
-        for row in data:
-
-            properties = {
-                "ref": row["community_id"],
-                "name": row["name"],
-                "lat": row["latitude"],
-                "lon": row["longitude"],
-                "addr_full": row["address1"],
-                "city": row["city"],
-                "state": row["state"],
-                "country": row["country_code"],
-                "postcode": row["zip_postal_code"],
-                "website": row["website"],
-                "phone": row["contact_center_phone"],
-            }
-
-            yield GeojsonPointItem(**properties)
+    def parse(self, response):
+        if response.request.meta.get("redirect_reasons") == [301]:
+            # Returning 301 when should be 404
+            return
+        item = LinkedDataParser.parse(response, "Residence")
+        path = urllib.parse.urlsplit(response.url).path
+        item["ref"] = Path(path).stem.removeprefix("brookdale-")
+        yield item
