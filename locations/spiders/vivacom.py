eeb91bacb4ae247a51b2804d65bbf9549d814e1b
Cj Malone <CjMalone@mail.com>
2022-08-26 14:54:05 +0100

    Add Vivacom spider

---
+++ locations/spiders/vivacom.py
@@ -0,0 +1,26 @@
+from scrapy import Spider
+
+from locations.items import GeojsonPointItem
+
+
+class VivacomSpider(Spider):
+    name = "vivacom"
+    item_attributes = {"brand": "Vivacom", "brand_wikidata": "Q7937522"}
+    start_urls = ["https://www.vivacom.bg/bg/stores/xhr?method=getJSON"]
+
+    def parse(self, response):
+        for store in response.json():
+            item = GeojsonPointItem()
+
+            item["ref"] = store["store_id"]
+            item["lat"], item["lon"] = store["latlng"].split(",")
+            item["image"] = (
+                "https://www.vivacom.bg/web/images/stores/" + store["store_img"]
+            )
+            item["name"] = store["store_name"]
+            item["phone"] = store["store_phone"]
+
+            item["extras"] = {}
+            item["extras"]["type"] = store["store_type"]
+
+            yield item
