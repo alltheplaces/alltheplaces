e09fbc15576ca55c61fa6931a0a290f29f4f8b5f
Cj Malone <CjMalone@mail.com>
2022-08-26 14:37:41 +0100

    Add char.gy spider

---
+++ locations/spiders/chargy_gb.py
@@ -0,0 +1,26 @@
+from scrapy import Spider
+
+from locations.items import GeojsonPointItem
+
+
+class ChargyGBSpider(Spider):
+    name = "chargy_gb"
+    item_attributes = {
+        "brand": "char.gy",
+        "brand_wikidata": "Q113465698",
+        "country": "GB",
+    }
+    start_urls = ["https://char.gy/charge_points.geojson?bounds=48,-10,60.716907,2"]
+
+    def parse(self, response):
+        for charger in response.json()["features"]:
+            item = GeojsonPointItem()
+
+            item["lon"], item["lat"] = charger["geometry"]["coordinates"]
+
+            item["ref"] = charger["properties"]["slug"]
+            item["website"] = "https://char.gy/" + charger["properties"]["slug"]
+            item["street_address"] = charger["properties"]["description"]
+            item["postcode"] = charger["properties"]["postcode"]
+
+            yield item
