712a51eca3d75867b23ef081950acda0bf8887f9
Cj Malone <CjMalone@mail.com>
2022-08-29 10:14:47 +0100

    Add opening_hours

--- locations/spiders/vivacom.py
+++ locations/spiders/vivacom.py
@@ -20,6 +20,18 @@ class VivacomSpider(Spider):
             item["lat"], item["lon"] = store["latlng"].split(",")
             item["name"] = store["store_name"]
             item["phone"] = store["store_phone"]
+            item["opening_hours"] = (
+                store["store_time"]
+                .strip()
+                .replace("почивен ден", "off")
+                .replace("пон.", "Mo")
+                .replace("пт.", "Fr")
+                .replace("пет.", "Fr")
+                .replace("съб.", "Sa")
+                .replace("съб", "Sa")
+                .replace("нед.", "Su")
+                .replace("нд.", "Su")
+            )
 
             item["extras"] = {}
             item["extras"]["type"] = store["store_type"]
