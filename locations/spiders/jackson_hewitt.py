fd486d15013bef062da7ccbc651c976d27ba647e
Josh Lee <jleedev@gmail.com>
2022-08-29 01:27:47 +0000

    Fix jackson_hewitt

--- locations/spiders/jackson_hewitt.py
+++ locations/spiders/jackson_hewitt.py
@@ -1,60 +1,20 @@
 # -*- coding: utf-8 -*-
-import re
-
 import scrapy
 
-from locations.items import GeojsonPointItem
-from locations.hours import OpeningHours
+from locations.linked_data_parser import LinkedDataParser
+from locations.microdata_parser import MicrodataParser
 
 
-class JacksonHewittSpider(scrapy.Spider):
+class JacksonHewittSpider(scrapy.spiders.SitemapSpider):
     name = "jackson_hewitt"
-    item_attributes = {"brand": "Jackson Hewitt"}
-    allowed_domains = ["https://www.jacksonhewitt.com/"]
-
-    def start_requests(self):
-        base_url = "https://www.jacksonhewitt.com/api/offices/search?customerAddress%5Blatitude%5D={lat}&customerAddress%5Blongitude%5D={lon}"
-
-        with open(
-            "./locations/searchable_points/us_centroids_25mile_radius.csv"
-        ) as points:
-            next(points)
-            for point in points:
-                _, lat, lon = point.strip().split(",")
-                url = base_url.format(lat=lat, lon=lon)
-                yield scrapy.Request(url=url, callback=self.parse)
+    item_attributes = {"brand": "Jackson Hewitt", "brand_wikidata": "Q6117132"}
+    allowed_domains = ["jacksonhewitt.com"]
+    sitemap_urls = ["https://office.jacksonhewitt.com/robots.txt"]
+    sitemap_rules = [
+        (r"/\d+$", "parse"),
+    ]
 
     def parse(self, response):
-        xml = scrapy.selector.Selector(response)
-        xml.remove_namespaces()
-
-        places = xml.xpath("//OfficeLocation").extract()
-
-        for place in places:
-            properties = {
-                "ref": re.search(r"<OfficeNumber>(.*)<\/OfficeNumber>", place).groups()[
-                    0
-                ],
-                "addr_full": re.search(r"<Address1>(.*)<\/Address1>", place).groups()[
-                    0
-                ],
-                "city": re.search(r"<City>(.*)<\/City>", place).groups()[0],
-                "state": re.search(r"<State>(.*)<\/State>", place).groups()[0],
-                "postcode": re.search(r"<ZipCode>(.*)<\/ZipCode>", place).groups()[0],
-                "country": "US",
-                "lat": re.search(r"<Latitude>(.*)<\/Latitude>", place).groups()[0],
-                "lon": re.search(r"<Longitude>(.*)<\/Longitude>", place).groups()[0],
-                "phone": re.search(r"<Phone>(.*)<\/Phone>", place).groups()[0],
-                "extras": {
-                    "location_type": re.search(
-                        r"<Seasonal>(.*)<\/Seasonal>", place
-                    ).groups()[0],
-                    # Is this a permanent or seasonal location
-                    "type_name": re.search(
-                        r"<TypeName>(.*)<\/TypeName>", place
-                    ).groups()[0]
-                    # Is this a storefront or walmart location
-                },
-            }
-
-            yield GeojsonPointItem(**properties)
+        MicrodataParser.convert_to_json_ld(response)
+        item = LinkedDataParser.parse(response, "AccountingService")
+        yield item
