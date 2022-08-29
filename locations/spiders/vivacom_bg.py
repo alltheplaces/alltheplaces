2000e4c5410b9e9a9605a879f5094f3834517c6f
Cj Malone <CjMalone@mail.com>
2022-08-29 11:31:05 +0100

    Fix opening hours parsing

--- locations/spiders/vivacom_bg.py
+++ locations/spiders/vivacom_bg.py
@@ -1,3 +1,5 @@
+import re
+
 from scrapy import Spider
 
 from locations.items import GeojsonPointItem
@@ -24,7 +26,8 @@ class VivacomBGSpider(Spider):
             item["lat"], item["lon"] = store["latlng"].split(",")
             item["name"] = store["store_name"]
             item["phone"] = store["store_phone"]
-            item["opening_hours"] = (
+
+            opening_hours = (
                 store["store_time"]
                 .strip()
                 .replace("почивен ден", "off")
@@ -35,6 +38,22 @@ class VivacomBGSpider(Spider):
                 .replace("съб", "Sa")
                 .replace("нед.", "Su")
                 .replace("нд.", "Su")
+                .replace("  ", " ")
             )
+            oh = []
+            for rule in re.findall(
+                r"(\w{2})\s?-?\s?(\w{2})?:?\s?(\d{2}(\.|:)\d{2})\s?-\s?(\d{2}(\.|:)\d{2})",
+                opening_hours,
+            ):
+                start_day, end_day, start_time, _, end_time, _ = rule
+                start_time = start_time.replace(".", ":")
+                end_time = end_time.replace(".", ":")
+
+                if end_day:
+                    oh.append(f"{start_day}-{end_day} {start_time}-{end_time}")
+                else:
+                    oh.append(f"{start_day} {start_time}-{end_time}")
+
+            item["opening_hours"] = "; ".join(oh)
 
             yield item
