0c6b96a8102e89deb6d3c6bf43c2dd885d3a16df
Joe Dixon <mjoe999@gmail.com>
2022-08-29 16:06:24 +0100

    Introducing the OpenGraphParser, londis_gb and premier_gb example uses give +5K stores
    Makes its debug with the Londis and Premier GB symbol group convenience stores.

---
+++ locations/open_graph_parser.py
@@ -0,0 +1,22 @@
+from locations.items import GeojsonPointItem
+from locations.dict_parser import DictParser
+
+
+class OpenGraphParser(object):
+
+    @staticmethod
+    def parse(response) -> GeojsonPointItem:
+        keys = response.xpath("/html/head/meta/@property").getall()
+        src = {}
+        for key in keys:
+            if key.startswith("og:") or key.startswith("place:location:"):
+                content = response.xpath(
+                    '//meta[@property="{}"]/@content'.format(key)
+                ).get()
+                if content:
+                    src[key.split(":")[-1]] = content
+        item = DictParser.parse(src)
+        item["website"] = response.url
+        if not item.get("ref"):
+            item["ref"] = response.url
+        return item
---
+++ locations/spiders/londis_gb.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from locations.open_graph_parser import OpenGraphParser
+
+
+class LondisGBSpider(scrapy.spiders.SitemapSpider):
+    name = "londis_gb"
+    item_attributes = {
+        "brand": "Londis",
+        "brand_wikidata": "Q21008564",
+        "country": "GB",
+    }
+    allowed_domains = ["londis.co.uk"]
+    sitemap_urls = ["https://www.londis.co.uk/sitemap.xml"]
+    sitemap_rules = [("/our-stores/", "parse_store")]
+    download_delay = 1.0
+
+    def parse_store(self, response):
+        return OpenGraphParser.parse(response)
