10d58171115b987edf2de69b30eb4cda877e414d
Cj Malone <CjMalone@mail.com>
2022-08-31 08:46:51 +0100

    Add Kutchenhaus Spider

---
+++ locations/spiders/kutchenhaus_gb.py
@@ -0,0 +1,42 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.items import GeojsonPointItem
+
+
+class KutchenhausGBSpider(SitemapSpider):
+    name = "kutchenhaus_gb"
+    item_attributes = {"brand": "Kutchenhaus", "country": "GB"}
+    sitemap_urls = ["https://uk.kutchenhaus.com/sitemap.xml"]
+    sitemap_follow = ["Store"]
+    sitemap_rules = [
+        (r"https:\/\/uk\.kutchenhaus\.com\/store-finder\/[-\w]+$", "parse")
+    ]
+    custom_settings = {"REDIRECT_ENABLED": False}
+
+    def parse(self, response, **kwargs):
+        if label := response.xpath('//div[@class="store-details--label"]/text()').get():
+            if label.strip() == "COMING SOON":
+                return
+
+        item = GeojsonPointItem()
+
+        item["lat"] = response.xpath(
+            '//div[@id="storeDetailsMap"]/@data-store-geopoint-latitude'
+        ).get()
+        item["lon"] = response.xpath(
+            '//div[@id="storeDetailsMap"]/@data-store-geopoint-longitude'
+        ).get()
+        item["name"] = response.xpath(
+            '//div[@class="store-details--caption"]/h1/text()'
+        ).get()
+        item["addr_full"] = (
+            response.xpath('//div[@class="store-details--info-item col-10"]/text()')
+            .getall()[0]
+            .strip()
+        )
+        item["phone"] = response.xpath(
+            '//div[@class="store-details--info-item col-10"]/a/text()'
+        ).get()
+        item["ref"] = item["website"] = response.url
+
+        return item
