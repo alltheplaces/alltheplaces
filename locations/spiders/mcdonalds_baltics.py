0f734a2a365d973106affa0f00e1c8b35226fc7b
Joe Dixon <mjoe999@gmail.com>
2022-09-01 14:49:26 +0100

    forgot to stage work in previous commit!

--- locations/spiders/mcdonalds_baltics.py
+++ locations/spiders/mcdonalds_baltics.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from locations.open_graph_parser import OpenGraphParser
+
 
 def country_from_url(response):
     return response.url.split("/")[2].split(".")[-1].upper()
@@ -12,15 +14,14 @@ class McDonaldsBalticsSpider(scrapy.spiders.SitemapSpider):
     item_attributes = {"brand": "McDonald's", "brand_wikidata": "Q38076"}
     sitemap_urls = [
         "https://mcdonalds.ee/location-sitemap.xml",
-#        "https://mcd.lt/location-sitemap.xml",
-#        "https://mcdonalds.lv/location-sitemap.xml",
+        "https://mcd.lt/location-sitemap.xml",
+        "https://mcdonalds.lv/location-sitemap.xml",
     ]
+    download_delay = 1.0
 
     def parse(self, response):
-        print(response.url)
-
-
-#        item = Brand.MCDONALDS.item(response)
-#        item["country"] = country_from_url(response)
-#        item["lat"] = response.xpath("//@data-lat").extract_first()
-#        item["lon"] = response.xpath("//@data-lng").extract_first()
+        item = OpenGraphParser.parse(response)
+        item["country"] = country_from_url(response)
+        item["lat"] = response.xpath("//@data-lat").extract_first()
+        item["lon"] = response.xpath("//@data-lng").extract_first()
+        yield item
