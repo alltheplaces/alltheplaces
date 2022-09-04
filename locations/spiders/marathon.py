2adcf22522af9a9a1127da1cb86f3a349e47738b
Joe Dixon <mjoe999@gmail.com>
2022-09-04 23:36:17 +0100

    fix lint

--- locations/spiders/marathon.py
+++ locations/spiders/marathon.py
@@ -5,7 +5,11 @@ from locations.dict_parser import DictParser
 
 class MarathonSpider(scrapy.spiders.CSVFeedSpider):
     name = "marathon"
-    item_attributes = {"brand": "Marathon Petroleum", "brand_wikidata": "Q458363", "country": "US"}
+    item_attributes = {
+        "brand": "Marathon Petroleum",
+        "brand_wikidata": "Q458363",
+        "country": "US",
+    }
     start_urls = [
         "https://www.marathonbrand.com/content/includes/mpc-brand-stations/SiteList.csv"
     ]
