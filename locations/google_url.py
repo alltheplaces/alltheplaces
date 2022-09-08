be7d190c28a268bad8466c720aa2021d5669f987
Joe Dixon <mjoe999@gmail.com>
2022-09-08 18:21:19 +0100

    Add wrenkitchens_gb (100+ showrooms)
    Required tweak to static embedded map parse. We are a bit in the dark with this.

--- locations/google_url.py
+++ locations/google_url.py
@@ -17,19 +17,20 @@ def url_to_coords(url: str) -> (float, float):
     if url.startswith("https://www.google.com/maps/embed?pb="):
         # https://andrewwhitby.com/2014/09/09/google-maps-new-embed-format/
         params = url[38:].split("!")
-
+        maps_keys = {}
         for i in range(0, len(params)):
-            if params[i] == "1m3":
-                lat = 0
-                lon = 0
-
-                for k in range(i + 1, i + 4):
-                    if params[k][:2] == "2d":
-                        lon = float(params[k][2:])
-                    elif params[k][:2] == "3d":
-                        lat = float(params[k][2:])
-
-                return lat, lon
+            splits = params[i].split("d")
+            if len(splits) == 2:
+                maps_keys[splits[0]] = splits[1]
+        lat_index = lon_index = None
+        if maps_keys.keys() == {"1", "2", "3"}:
+            lon_index = "2"
+            lat_index = "3"
+        if maps_keys.keys() == {"1", "2"}:
+            lon_index = "2"
+            lat_index = "1"
+        if lat_index and lon_index:
+            return float(maps_keys[lat_index]), float(maps_keys[lon_index])
     elif url.startswith("https://maps.googleapis.com/maps/api/staticmap"):
         ll = get_query_param(url, "center")
         if ll:
