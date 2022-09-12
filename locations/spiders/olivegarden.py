cbf392b1e6a09ccf5954f105d1f18e1b1ea22f96
Josh Lee <jleedev@gmail.com>
2022-09-12 18:55:18 +0000

    Fix olivegarden

--- locations/spiders/olivegarden.py
+++ locations/spiders/olivegarden.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-import json
 import scrapy
-import re
 
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
 
 
@@ -10,93 +9,45 @@ class OliveGardenSpider(scrapy.Spider):
     name = "olivegarden"
     item_attributes = {"brand": "Olive Garden", "brand_wikidata": "Q3045312"}
     allowed_domains = ["olivegarden.com"]
-    download_delay = 0.5
-    start_urls = ("http://www.olivegarden.com/en-locations-sitemap.xml",)
-
-    def address(self, address):
-        if not address:
-            return None
-
-        addr_tags = {
-            "addr_full": address[0].split('value="')[1].split('"')[0].split(",")[0],
-            "city": address[0].split('value="')[1].split('"')[0].split(",")[1],
-            "state": address[0].split('value="')[1].split('"')[0].split(",")[-2],
-            "postcode": address[0].split('value="')[1].split('"')[0].split(",")[-1],
-        }
-
-        return addr_tags
+    start_urls = [
+        "https://www.olivegarden.com/en-locations-sitemap.xml",
+    ]
 
     def parse(self, response):
         response.selector.remove_namespaces()
-        city_urls = response.xpath("//url/loc/text()").extract()
-        locationURL = re.compile(r"http[s]:/(/|/www.)olivegarden.com/locations/\S+")
-        for path in city_urls:
-            if not re.search(locationURL, path):
-                pass
-            else:
-                yield scrapy.Request(
-                    path.strip(),
-                    callback=self.parse_store,
-                )
+        for loc in response.xpath("//loc/text()").extract():
+            ref = int(loc.split("/")[-1])
+            url = f"https://www.olivegarden.com/web-api/restaurant/{ref}?locale=en_US&restNumFlag=true&restaurantNumber={ref}"
+            yield scrapy.Request(url, self.parse_restaurant, meta={"website": loc})
 
-    def parse_store(self, response):
-        if response.url == "https://www.olivegarden.com/home":
-            return
-
-        data = json.loads(
-            response.xpath(
-                '//script[@type="application/ld+json"]/text()'
-            ).extract_first()
-        )
-        fallback = False
-
-        try:
-            properties = {
-                "ref": response.url.split("/")[-1],
-                "name": data["name"],
-                "addr_full": data["address"].get("streetAddress", "").strip(),
-                "city": data["address"]["addressLocality"],
-                "state": data["address"]["addressRegion"],
-                "postcode": data["address"]["postalCode"],
-                "country": data["address"]["addressCountry"],
-                "phone": data.get("telephone", None),
-                "lat": float(data["geo"]["latitude"]),
-                "lon": float(data["geo"]["longitude"]),
-                "website": response.url,
-            }
-
-        except KeyError:  # a few location pages don't have the complete ld+json data
-            properties = {}
-            fallback = True
-
-        if not properties.get("city", False) or fallback:
-            properties = {
-                "name": response.xpath(
-                    'normalize-space(//input[@id="isLocationDetails"]/../h1/text())'
-                ).extract_first(),
-                "website": response.url,
-                "ref": response.url.split("/")[-1],
-                "country": "US",
-                "lon": float(
-                    response.xpath('//input[@id="restLatLong"]')
-                    .extract_first()
-                    .split('value="')[1]
-                    .split('"')[0]
-                    .split(",")[1]
-                ),
-                "lat": float(
-                    response.xpath('//input[@id="restLatLong"]')
-                    .extract_first()
-                    .split('value="')[1]
-                    .split('"')[0]
-                    .split(",")[0]
-                ),
-            }
-
-            address = self.address(
-                response.xpath('//input[@id="restAddress"]').extract()
-            )
-            if address:
-                properties.update(address)
-
+    def parse_restaurant(self, response):
+        data = response.json()["successResponse"]["restaurantDetails"]
+        # Field is named longitudeLatitude and contains latitude, longitude.
+        lat, lon = data["address"]["longitudeLatitude"].split(",")
+        properties = {
+            "lon": lon,
+            "lat": lat,
+            "website": response.meta["website"],
+            "ref": data["restaurantNum"],
+            "name": data["restaurantName"],
+            "street_address": data["address"]["address1"],
+            "city": data["address"]["city"],
+            "state": data["address"]["state"],
+            "postcode": data["address"]["zipCode"],
+            "country": data["address"]["country"],
+            "phone": data["restPhoneNumber"][0]["Phone"],
+            "opening_hours": self.parse_hours(data),
+        }
         yield GeojsonPointItem(**properties)
+
+    @staticmethod
+    def parse_hours(data):
+        oh = OpeningHours()
+        # Sun = 1, Sat = 7
+        day = lambda d: data["daysOfWeeks"][d - 1][:2].title()
+        for row in data["weeklyHours"]:
+            if row["hourTypeDesc"] == "Hours of Operations":
+                oh.add_range(
+                    day(row["dayOfWeek"]), row["startTime"], row["endTime"], "%I:%M%p"
+                )
+        return oh.as_opening_hours()
