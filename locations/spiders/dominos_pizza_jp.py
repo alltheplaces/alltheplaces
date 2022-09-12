cd36067c0e3b3f411e03fdbc5817a0789573204d
Cj Malone <CjMalone@mail.com>
2022-09-12 08:39:06 +0100

    Fix xpaths

--- locations/spiders/dominos_pizza_jp.py
+++ locations/spiders/dominos_pizza_jp.py
@@ -8,7 +8,11 @@ from locations.items import GeojsonPointItem
 
 class DominosPizzaJPSpider(scrapy.Spider):
     name = "dominos_pizza_jp"
-    item_attributes = {"brand": "Domino's", "brand_wikidata": "Q839466"}
+    item_attributes = {
+        "brand": "Domino's",
+        "brand_wikidata": "Q839466",
+        "country": "JP",
+    }
     allowed_domains = ["dominos.jp"]
     start_urls = [
         "https://www.dominos.jp/sitemap.aspx",
@@ -27,18 +31,17 @@ class DominosPizzaJPSpider(scrapy.Spider):
         properties = {
             "ref": ref,
             "name": response.xpath(
-                'normalize-space(//div[@class="storetitle"][1]/text())'
+                'normalize-space(//h1[@class="storetitle"][1]/text())'
             ).extract_first(),
             "addr_full": response.xpath(
-                'normalize-space(//span[@id="store-address-info"]/p/a/text())'
+                'normalize-space(//span[@id="store-address-info"]/p/text()[4])'
             ).extract_first(),
             "postcode": re.search(
                 r"([\d-]*)$",
                 response.xpath(
-                    'normalize-space(//div[@class="store-details-text"][1]/p/text())'
+                    'normalize-space(//div[@class="store-details-text"]/span/p/text()[2])'
                 ).extract_first(),
             ).group(1),
-            "country": "JP",
             "lat": response.xpath(
                 'normalize-space(//input[@id="store-lat"]/@value)'
             ).extract_first(),
