7bca3a627f94a98eb16584136ae5b182db5970fb
Cj Malone <CjMalone@mail.com>
2022-09-14 08:51:12 +0100

    Don't complain on opening_hours == 24/7

--- locations/pipelines.py
+++ locations/pipelines.py
@@ -175,7 +175,10 @@ class CheckItemPropertiesPipeline(object):
         if opening_hours := item.get("opening_hours"):
             if not isinstance(opening_hours, str):
                 spider.crawler.stats.inc_value("atp/field/opening_hours/wrong_type")
-            elif not self.opening_hours_regex.match(opening_hours):
+            elif (
+                not self.opening_hours_regex.match(opening_hours)
+                and opening_hours != "24/7"
+            ):
                 spider.crawler.stats.inc_value("atp/field/opening_hours/invalid")
         else:
             spider.crawler.stats.inc_value("atp/field/opening_hours/missing")
