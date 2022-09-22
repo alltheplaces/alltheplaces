cc76d050c5e63695be247ac43918aa11679bc8c6
Cj Malone <CjMalone@mail.com>
2022-09-22 17:46:53 +0100

    Add Harvester Spider

---
+++ locations/spiders/harvester_gb.py
@@ -0,0 +1,13 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class HarvesterGB(SitemapSpider, StructuredDataSpider):
+    name = "harvester_gb"
+    item_attributes = {"brand": "Harvester", "brand_wikidata": "Q5676915"}
+    sitemap_urls = ["https://www.harvester.co.uk/sitemap.xml"]
+    sitemap_rules = [
+        (r"https:\/\/www\.harvester\.co\.uk\/restaurants\/[\w]+\/[\w]+$", "parse_sd")
+    ]
+    wanted_types = ["Restaurant"]
