c26b89220fbbe4acdaed3244d79fedb107d43e7c
Cj Malone <CjMalone@mail.com>
2022-09-22 17:57:03 +0100

    Fix Homebase spider

--- locations/spiders/homebase.py
+++ locations/spiders/homebase.py
@@ -1,48 +1,13 @@
-import scrapy
-import re
-import json
+from scrapy.spiders import SitemapSpider
 
-from locations.items import GeojsonPointItem
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class HomebaseSpider(scrapy.Spider):
+class Homebase(SitemapSpider, StructuredDataSpider):
     name = "homebase"
     item_attributes = {"brand": "Homebase", "brand_wikidata": "Q9293447"}
-    allowed_domains = ["www.homebase.co.uk"]
-    download_delay = 0.5
-    start_urls = ("https://www.homebase.co.uk/stores",)
-
-    def parse_stores(self, response):
-        data = re.findall(
-            r"var com_bunnings_locations_mapLocations = [^;]+",
-            response.text,
-        )
-        json_data = json.loads(
-            data[0].replace("var com_bunnings_locations_mapLocations = ", "")
-        )
-        properties = {
-            "addr_full": json_data[0]["Store"]["Address"]["Address"]
-            + json_data[0]["Store"]["Address"]["AddressLineTwo"],
-            "phone": json_data[0]["Store"]["Phone"],
-            "city": json_data[0]["Store"]["Address"]["Suburb"],
-            "state": json_data[0]["Store"]["Address"]["State"],
-            "postcode": json_data[0]["Store"]["Address"]["Postcode"],
-            "country": json_data[0]["Store"]["Address"]["Country"],
-            "ref": json_data[0]["Store"]["StoreID"],
-            "website": response.url,
-            "lat": float(json_data[0]["Store"]["Location"]["Latitude"]),
-            "lon": float(json_data[0]["Store"]["Location"]["Longitude"]),
-        }
-
-        hours = response.xpath('//time[@itemprop="openingHours"]/@datatime').extract()
-        if hours != []:
-            properties["opening_hours"] = "; ".join(x for x in hours)
-
-        yield GeojsonPointItem(**properties)
-
-    def parse(self, response):
-        urls = response.xpath(
-            '//div[@class="store-listing__state__list alpha"]/ul/li/a/@href'
-        ).extract()
-        for path in urls:
-            yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
+    sitemap_urls = ["https://store.homebase.co.uk/robots.txt"]
+    sitemap_rules = [
+        (r"https:\/\/store\.homebase\.co\.uk\/[-\w]+\/[-.\w]+$", "parse_sd")
+    ]
+    wanted_types = ["HardwareStore"]
