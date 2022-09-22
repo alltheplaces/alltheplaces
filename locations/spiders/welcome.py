4036e22e3d456e0a8ec3f1e45dc820a6984ec0f5
Cj Malone <CjMalone@mail.com>
2022-09-22 10:45:45 +0100

    Convert welcome to StructuredDataSpider

--- locations/spiders/welcome.py
+++ locations/spiders/welcome.py
@@ -1,27 +1,16 @@
 from scrapy.spiders import SitemapSpider
 
-from locations.linked_data_parser import LinkedDataParser
-from locations.microdata_parser import MicrodataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class WelcomeSpider(SitemapSpider):
+class WelcomeGB(SitemapSpider, StructuredDataSpider):
     name = "welcome"
     item_attributes = {"brand": "Welcome"}
     sitemap_urls = ["https://stores.welcome-stores.co.uk/sitemap.xml"]
     sitemap_rules = [
         (
             "https:\/\/stores\.welcome-stores\.co\.uk\/[-\w]+\/[-\w]+\/[-\w]+\.html$",
-            "parse_item",
+            "parse_sd",
         )
     ]
-
-    def parse_item(self, response):
-        MicrodataParser.convert_to_json_ld(response)
-        item = LinkedDataParser.parse(response, "GroceryStore")
-
-        if item is None:
-            return
-
-        item["ref"] = item["website"]
-
-        return item
+    wanted_types = ["GroceryStore"]
