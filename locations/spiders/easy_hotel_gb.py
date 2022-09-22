63edfd566d6cf495b68176562448ce3dfea0a813
Cj Malone <CjMalone@mail.com>
2022-09-22 17:06:58 +0100

    Add Easy Hotel Spider

---
+++ locations/spiders/easy_hotel_gb.py
@@ -0,0 +1,25 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class EasyHotelGB(SitemapSpider, StructuredDataSpider):
+    name = "easy_hotel_gb"
+    item_attributes = {"brand": "Easy Hotel", "brand_wikidata": "Q17011598"}
+    sitemap_urls = ["https://www.easyhotel.com/sitemap.xml"]
+    sitemap_rules = [
+        (
+            r"https:\/\/www\.easyhotel\.com\/hotels\/([-\w]+\/[-\w]+\/[-\w]+)$",
+            "parse_sd",
+        )
+    ]
+    wanted_types = ["Hotel"]
+
+    def sitemap_filter(self, entries):
+        for entry in entries:
+            entry["loc"] = entry["loc"].replace("www.www.", "www.")
+            yield entry
+
+    def inspect_item(self, item, response):
+        item["state"] = None
+        yield item
