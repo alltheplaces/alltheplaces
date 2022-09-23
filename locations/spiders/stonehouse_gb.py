f50ef69bf95a1bc3831c6f5575d8fbbf852435f8
Cj Malone <CjMalone@mail.com>
2022-09-23 09:56:27 +0100

    Add stonehouse_gb spider

---
+++ locations/spiders/stonehouse_gb.py
@@ -0,0 +1,16 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class StonehouseGB(SitemapSpider, StructuredDataSpider):
+    name = "stonehouse_gb"
+    item_attributes = {"brand": "Stonehouse", "brand_wikidata": "Q78192049"}
+    sitemap_urls = ["https://www.stonehouserestaurants.co.uk/sitemap.xml"]
+    sitemap_rules = [
+        (
+            r"https:\/\/www\.stonehouserestaurants\.co\.uk\/nationalsearch\/[-\w]+\/[-\w]+$",
+            "parse_sd",
+        )
+    ]
+    wanted_types = ["Restaurant"]
