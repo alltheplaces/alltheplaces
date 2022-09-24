36c2b13e0cc59df374afb975dac4c7026ea6f3f4
Ian Dees <ian.dees@gmail.com>
2022-09-24 12:31:19 -0500

    Correct brand name

--- locations/spiders/ulta_beauty.py
+++ locations/spiders/ulta_beauty.py
@@ -4,7 +4,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class UltaBeautySpider(SitemapSpider, StructuredDataSpider):
     name = "ulta-beauty"
-    item_attributes = {"brand": "Ulta-Beauty", "brand_wikidata": "Q7880076"}
+    item_attributes = {"brand": "Ulta Beauty", "brand_wikidata": "Q7880076"}
     allowed_domains = ["www.ulta.com"]
     sitemap_urls = ("https://www.ulta.com/stores/sitemap.xml",)
     sitemap_rules = [
