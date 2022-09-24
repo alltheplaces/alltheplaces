611815633135b45694ab0efd6f368597201435d5
Ian Dees <ian.dees@gmail.com>
2022-09-24 10:59:24 -0500

    Famous Footwear update

--- locations/spiders/famous_footwear.py
+++ locations/spiders/famous_footwear.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import re
-
-from locations.linked_data_parser import LinkedDataParser
+from scrapy.spiders import SitemapSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class FamousFootwearSpider(scrapy.spiders.SitemapSpider):
+class FamousFootwearSpider(SitemapSpider, StructuredDataSpider):
     name = "famous_footwear"
     item_attributes = {"brand": "Famous Footwear", "brand_wikidata": "Q5433457"}
     sitemap_urls = ["https://www.famousfootwear.com/sitemap.xml"]
-    sitemap_rules = [("/stores/", "parse")]
-    download_delay = 2.0
+    sitemap_rules = [("/stores/", "parse_sd")]
+    download_delay = 5.0
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"
     }
+    wanted_types = ["Store"]
 
-    def parse(self, response):
-        item = LinkedDataParser.parse(response, "Store")
-        item["ref"] = item["website"]
-        pattern = re.compile('location: \["(.*)", "(.*)"\],')
-        matches = pattern.search(response.text)
-        item["lat"], item["lon"] = matches.group(1), matches.group(2)
+    def inspect_item(self, item, response):
+        item["lat"] = response.xpath('//div[@class="CoveoStaticPositionProvider" and @data-latitude]/@data-latitude').extract_first()
+        item["lon"] = response.xpath('//div[@class="CoveoStaticPositionProvider" and @data-longitude]/@data-longitude').extract_first()
         yield item
