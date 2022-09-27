5e14d015a737d488190a5e033c9e8fbd8c8a2b2b
Cj Malone <CjMalone@mail.com>
2022-09-27 16:28:21 +0100

    Convert B&M to StructuredDataSpider

--- locations/spiders/b_and_m.py
+++ locations/spiders/b_and_m.py
@@ -1,60 +1,17 @@
+from locations.spiders.vapestore_gb import clean_address
+from locations.structured_data_spider import StructuredDataSpider
+
 from scrapy.spiders import SitemapSpider
-from locations.items import GeojsonPointItem
 
 
-class BAndMSpider(SitemapSpider):
+class BAndMSpider(SitemapSpider, StructuredDataSpider):
     name = "b_and_m"
-    item_attributes = {"brand": "B&M", "brand_wikidata": "Q4836931"}
+    item_attributes = {"brand": "B&M", "brand_wikidata": "Q4836931", "country": "GB"}
     allowed_domains = ["www.bmstores.co.uk"]
     sitemap_urls = ["https://www.bmstores.co.uk/hpcstores/storessitemap"]
+    sitemap_rules = [("", "parse_sd")]
+    wanted_types = ["LocalBusiness"]
 
-    def parse(self, response):
-        root = response.xpath(
-            '//*[@itemscope][@itemtype="http://schema.org/LocalBusiness"]'
-        )
-        properties = {
-            "ref": root.xpath('.//*[@itemprop="branchCode"]/text()').get(),
-            "website": response.request.url,
-            "name": root.xpath('.//*[@itemprop="name"]/text()').get(),
-            "phone": root.xpath('.//*[@itemprop="telephone"]/text()').get(),
-            "lat": root.xpath(
-                './/*[@itemscope][@itemprop="geo"][@itemtype="http://schema.org/GeoCoordinates"]/meta[@itemprop="latitude"]/@content'
-            ).get(),
-            "lon": root.xpath(
-                './/*[@itemscope][@itemprop="geo"][@itemtype="http://schema.org/GeoCoordinates"]/meta[@itemprop="longitude"]/@content'
-            ).get(),
-            "street_address": ", ".join(
-                map(
-                    lambda x: x.strip(),
-                    root.xpath(
-                        './/*[@itemscope][@itemprop="address"][@itemtype="http://schema.org/PostalAddress"]/*[@itemprop="streetAddress"]/text()'
-                    ).getall(),
-                )
-            ).replace("\n", ", "),
-            "city": root.xpath(
-                './/*[@itemscope][@itemprop="address"][@itemtype="http://schema.org/PostalAddress"]/*[@itemprop="addressLocality"]/text()'
-            ).get(),
-            "postcode": root.xpath(
-                './/*[@itemscope][@itemprop="address"][@itemtype="http://schema.org/PostalAddress"]/*[@itemprop="postalCode"]/text()'
-            ).get(),
-            "country": "GB",
-            "brand": root.xpath('.//*[@class="bm-line-compact"]/text()').get(),
-        }
-
-        properties["phone"] = "+44 " + properties["phone"][1:]
-        properties["addr_full"] = ", ".join(
-            filter(
-                None,
-                (
-                    properties["street_address"],
-                    properties["city"],
-                    root.xpath(
-                        './/*[@itemscope][@itemprop="address"][@itemtype="http://schema.org/PostalAddress"]/*[@itemprop="addressRegion"]/text()'
-                    ).get(),
-                    properties["postcode"],
-                    "United Kingdom",
-                ),
-            )
-        )
-
-        yield GeojsonPointItem(**properties)
+    def inspect_item(self, item, response):
+        item["street_address"] = clean_address(item["street_address"])
+        yield item
