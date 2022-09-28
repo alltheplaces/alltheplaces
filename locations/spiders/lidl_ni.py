9d6d09be990da77dfaa0e6e3c28b34596eb2087e
Cj Malone <CjMalone@mail.com>
2022-09-28 11:17:52 +0100

    Add OpeningHours to Lidl Spiders

--- locations/spiders/lidl_ni.py
+++ locations/spiders/lidl_ni.py
@@ -1,5 +1,8 @@
+import re
+
 import scrapy
 
+from locations.hours import day_range, OpeningHours, sanitise_day
 from locations.items import GeojsonPointItem
 
 
@@ -12,27 +15,24 @@ class LidlNISpider(scrapy.Spider):
         "?key=Asz4OJrOqSHy-1xEWYGLbFhH4TnVP0LL1xgj0YBkewA5ZrtHRB2nlpfqzm1lqKPK"
         "&$filter=Adresstyp Eq 1"
         "&$select=EntityID,ShownStoreName,AddressLine,Locality,PostalCode,CountryRegion,CityDistrict,Latitude,"
-        "Longitude,INFOICON17"
+        "Longitude,INFOICON17,OpeningTimes"
+        "&$format=json"
     )
+    start_urls = [base_url + "&$inlinecount=allpages"]
 
-    def start_requests(self):
-        yield scrapy.Request(
-            self.base_url + "&$inlinecount=allpages" + "&$format=json",
-            callback=self.get_pages,
-        )
-
-    def get_pages(self, response):
+    def parse(self, response):
         total_count = int(response.json()["d"]["__count"])
         offset = 0
         page_size = 250
 
         while offset < total_count:
             yield scrapy.Request(
-                self.base_url + f"&$top={page_size}&$skip={offset}&$format=json"
+                self.base_url + f"&$top={page_size}&$skip={offset}",
+                callback=self.parse_stores,
             )
             offset += page_size
 
-    def parse(self, response):
+    def parse_stores(self, response):
         stores = response.json()["d"]["results"]
 
         for store in stores:
@@ -63,5 +63,27 @@ class LidlNISpider(scrapy.Spider):
             if store["INFOICON17"] == "customerToilet":
                 properties["extras"]["toilets"] = "yes"
                 properties["extras"]["toilets:access"] = "customers"
+
+            if matches := re.findall(
+                r"(\w{3} ?- ?\w{3}|\w{3}) (\d{2}:\d{2})\*?-(\d{2}:\d{2})",
+                store["OpeningTimes"],
+            ):
+                oh = OpeningHours()
+                for rule in matches:
+                    day = rule[0]
+                    start_time = rule[1]
+                    end_time = rule[2]
+                    if "-" in day:
+                        start_day, end_day = day.split("-")
+
+                        start_day = sanitise_day(start_day)
+                        end_day = sanitise_day(end_day)
+
+                        for day in day_range(start_day, end_day):
+                            oh.add_range(day, start_time, end_time)
+                    else:
+                        oh.add_range(day, start_time, end_time)
+
+                properties["opening_hours"] = oh.as_opening_hours()
 
             yield GeojsonPointItem(**properties)
