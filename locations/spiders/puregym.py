4ab84096f2a86e7d9823761ab4d6e75aaa924642
Cj Malone <CjMalone@mail.com>
2022-09-29 16:23:24 +0100

    Convert PureGym to StructuredDataSpider

--- locations/spiders/puregym.py
+++ locations/spiders/puregym.py
@@ -1,58 +1,28 @@
-import json
-
-from locations.google_url import url_to_coords
-from locations.items import GeojsonPointItem
+from locations.google_url import extract_google_position
 from scrapy.spiders import SitemapSpider
 
+from locations.structured_data_spider import StructuredDataSpider
 
-class PureGymSpider(SitemapSpider):
+
+class PureGymSpider(SitemapSpider, StructuredDataSpider):
     name = "puregym"
-    item_attributes = {"brand": "PureGym", "brand_wikidata": "Q18345898"}
+    item_attributes = {
+        "brand": "PureGym",
+        "brand_wikidata": "Q18345898",
+        "country": "GB",
+    }
     allowed_domains = ["www.puregym.com"]
     sitemap_urls = ["https://www.puregym.com/sitemap.xml"]
     sitemap_rules = [
         (
             "https:\/\/www\.puregym\.com\/gyms\/([\w-]+)\/$",
-            "parse_location",
+            "parse_sd",
         ),
     ]
+    wanted_types = ["HealthClub"]
 
-    def parse_location(self, response):
-        page_title = response.xpath("/html/head/title/text()").get().lower()
-        if "coming soon" in page_title:
-            return
+    def inspect_item(self, item, response):
+        item["ref"] = response.xpath('//meta[@itemprop="gymId"]/@content').get()
+        extract_google_position(item, response)
 
-        ld = json.loads(
-            response.xpath('//script[@type="application/ld+json"]/text()').get()
-        )
-
-        properties = {
-            "ref": response.xpath('//meta[@itemprop="gymId"]/@content').get(),
-            "website": response.request.url,
-            "name": ld["name"],
-            "phone": ld["telephone"],
-            "street_address": ld["location"]["address"]["streetAddress"],
-            "city": ld["location"]["address"]["addressLocality"],
-            "postcode": ld["location"]["address"]["postalCode"],
-            "country": "GB",
-            "addr_full": ", ".join(
-                filter(
-                    None,
-                    (
-                        ld["location"]["address"]["streetAddress"],
-                        ld["location"]["address"]["addressLocality"],
-                        ld["location"]["address"]["postalCode"],
-                        "United Kingdom",
-                    ),
-                )
-            ),
-            "email": ld["email"],
-        }
-
-        properties["lat"], properties["lon"] = url_to_coords(
-            response.xpath(
-                '//*[@class="gym-map static-map"]/img[@id="view-location-bottom_map"]/@src'
-            ).get()
-        )
-
-        yield GeojsonPointItem(**properties)
+        yield item
