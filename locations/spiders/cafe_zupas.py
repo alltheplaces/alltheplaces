24bcd67a5795766134d4dc0fb8c859e88820caf4
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2022-10-04 15:31:34 +0530

    Add spider for Cafe Zupas

---
+++ locations/spiders/cafe_zupas.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from locations.items import GeojsonPointItem
+
+
+class CSpider(scrapy.Spider):
+    name = "cafe_zupas"
+    item_attributes = {"brand": "Cafe Zupas"}
+    allowed_domains = ["cafezupas.com"]
+    start_urls = [
+        "https://cafezupas.com/server.php?url=https://api.controlcenter.zupas.com/api/markets/listing"
+    ]
+
+    def parse(self, response):
+        data = response.json()
+        for i in data["data"]["data"]:
+            for location in i["locations"]:
+                properties = {
+                    "ref": "https://cafezupas.com/locationcopy/info/"
+                    + location["name"].lower().replace(" ", "-"),
+                    "website": "https://cafezupas.com/",
+                    "name": location["name"],
+                    "image": "https://cafezupas.com" + location["image"]
+                    if location["image"] is not None
+                    else None,
+                    "phone": location["phone"],
+                    "lat": location["lat"],
+                    "lon": location["long"],
+                    "addr_full": location["address"],
+                    "city": location["city"],
+                    "state": location["state"],
+                    "postcode": location["zip"],
+                    "facebook": location["facebook_url"],
+                    "opening_hours": "Fr-Sa "
+                    + location["fri_sat_timings_open"]
+                    + "-"
+                    + location["fri_sat_timings_close"]
+                    + "; Mo-Th "
+                    + location["mon_thurs_timings_open"]
+                    + "-"
+                    + location["mon_thurs_timings_close"]
+                    + "; Su "
+                    + location["sunday_timings"],
+                }
+                yield GeojsonPointItem(**properties)
