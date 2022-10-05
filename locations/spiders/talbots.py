7d4c08146c7b4773c7197f3451619e7a3a48a1b7
Amit Kumar <akumar@indeed.com>
2022-10-05 17:39:38 +0530

    1059: Fixing linting issue using black

--- locations/spiders/talbots.py
+++ locations/spiders/talbots.py
@@ -7,21 +7,29 @@ from locations.items import GeojsonPointItem
 class TalbotsSpider(scrapy.Spider):
     name = "talbots"
     allowed_domains = ["www.talbots.com"]
-    start_urls = [
-        "https://www.talbots.com/view-all-stores/"
-    ]
+    start_urls = ["https://www.talbots.com/view-all-stores/"]
 
     def parse(self, response):
         all_data = response.xpath('//*[contains(@class, "store-row")]').getall()
         for data in all_data:
             resp = scrapy.Selector(text=data)
             properties = {
-                "name": self.sanitize_name(resp.xpath('//a[contains(@class, "store-details-link")]//text()').get()),
+                "name": self.sanitize_name(
+                    resp.xpath(
+                        '//a[contains(@class, "store-details-link")]//text()'
+                    ).get()
+                ),
                 "phone": resp.xpath('//a[contains(@href, "tel")]/text()').get(),
                 "addr_full": self.get_address(
-                    resp.xpath('//div[contains(@class, "store-name")]/following-sibling::text()').getall()),
+                    resp.xpath(
+                        '//div[contains(@class, "store-name")]/following-sibling::text()'
+                    ).getall()
+                ),
                 "opening_hours": self.sanitize_time(
-                    resp.xpath('//*[contains(@class, "store-hours storeCol")]//text()').getall()),
+                    resp.xpath(
+                        '//*[contains(@class, "store-hours storeCol")]//text()'
+                    ).getall()
+                ),
                 "ref": resp.xpath('//a[contains(@href, "store")]/@href').get(),
             }
             yield GeojsonPointItem(**properties)
@@ -32,7 +40,7 @@ class TalbotsSpider(scrapy.Spider):
             if "Phone:" in line:
                 break
             address.append(line.replace("\n", ""))
-        return ' '.join(address)
+        return " ".join(address)
 
     def sanitize_time(self, response):
         sanitized_data = []
@@ -40,7 +48,7 @@ class TalbotsSpider(scrapy.Spider):
             if "\n" == line:
                 continue
             sanitized_data.append(line.replace("\n", ""))
-        return ' '.join(sanitized_data)
+        return " ".join(sanitized_data)
 
     def sanitize_name(self, response):
         return response.replace("\n", "")
