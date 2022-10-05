73a37d7c8bff9a3ad511f6911b5660d6b2aca15f
stephinson <40458699+stephinson@users.noreply.github.com>
2022-10-05 14:41:48 +0530

    PR Changes
    * Corrects brand name
    * Adds brand wikidata
    * Adds image url
    * Rename street address field name

--- locations/spiders/farmerboys.py
+++ locations/spiders/farmerboys.py
@@ -9,7 +9,7 @@ from scrapy.selector import Selector
 
 class FarmerBoys(scrapy.Spider):
     name = "farmerboys"
-    item_attributes = {"brand": "Farmer Boy's"}
+    item_attributes = {"brand": "Farmer Boys", "brand_wikidata": "Q5435711"}
     allowed_domains = ["farmerboys.com"]
     start_urls = ["https://www.farmerboys.com/locations/"]
 
@@ -23,18 +23,22 @@ class FarmerBoys(scrapy.Spider):
             properties = {
                 "name": location["location_name"],
                 "ref": location["location_url"],
-                "street": location["address_1"],
+                "street_address": location["address_1"],
                 "city": location["city"],
                 "postcode": location["postal_code"],
                 "state": location["state"],
                 "country": "US",
                 "phone": location["phone"],
                 "website": "https://www.farmerboys.com/locations/location-detail.php?loc="
-                + location["location_url"],
+                + location["location_url"].strip(),
+                "image": "https://www.farmerboys.com/images/locations/"
+                + location["location_pic"].strip()
+                if location["location_pic"]
+                else None,
                 "lat": float(location["lat"]) if location["lat"] else None,
                 "lon": float(location["lng"]) if location["lng"] else None,
                 "opening_hours": " ".join(
                     Selector(text=location["location_hours"]).xpath("//text()").getall()
-                ),
+                ).replace("\r\n", ""),
             }
             yield GeojsonPointItem(**properties)
