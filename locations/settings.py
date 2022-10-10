8a19959d2bc269617aed26a18f69a8ba71bbab11
Cj Malone <CjMalone@mail.com>
2022-10-11 00:42:44 +0100

    Add a default download delay of 1 second

--- locations/settings.py
+++ locations/settings.py
@@ -41,7 +41,7 @@ FEED_EXPORTERS = {
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
