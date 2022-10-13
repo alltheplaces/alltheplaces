2fb302788fe3113d61d2b587383cd93485c71763
Boomer Yancey, Location Analyst <8587049+boomerwv1@users.noreply.github.com>
2022-10-13 12:42:45 -0400

    Update miller_carter.py

--- locations/spiders/miller_carter.py
+++ locations/spiders/miller_carter.py
@@ -2,9 +2,7 @@
 import re
 import json
 import scrapy
-from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
-from locations.items import GeojsonPointItem
 
 class MillerCarterSpider(StructuredDataSpider):
     # download_delay = 0.2
@@ -17,4 +15,3 @@ class MillerCarterSpider(StructuredDataSpider):
         urls = response.xpath('//div[@class="accordion parbase section"]//a/@href').extract()
         for url in urls:
             yield scrapy.Request(response.urljoin(url), callback=self.parse_sd)
-            
