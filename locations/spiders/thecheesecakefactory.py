e17e4b7ef863026e78dd849bc61ffd619b84fabf
Tony Garcia <tonysk8@gmx.net>
2022-10-13 07:40:16 -0500

    Update class name

--- locations/spiders/thecheesecakefactory.py
+++ locations/spiders/thecheesecakefactory.py
@@ -5,7 +5,7 @@ from locations.hours import OpeningHours
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class UrbanOutfitters(SitemapSpider, StructuredDataSpider):
+class TheCheesecakeFactorySpider(SitemapSpider, StructuredDataSpider):
     download_delay = 0.1
     name = "thecheesecakefactory"
     item_attributes = {"brand": "The Cheesecake Factory", "brand_wikidata": "Q1045842"}
