b4cf083caedcce3cac2bb9ae24e9b5f857f433bc
Boomer Yancey, Location Analyst <8587049+boomerwv1@users.noreply.github.com>
2022-10-13 12:35:11 -0400

    Update miller_carter.py

--- locations/spiders/miller_carter.py
+++ locations/spiders/miller_carter.py
@@ -14,8 +14,7 @@ class MillerCarterSpider(StructuredDataSpider):
     name = "millercarter"
     item_attributes = {"brand": "Miller and Carter"}
     allowed_domains = ["millerandcarter.co.uk"]
-    start_urls = ("https://www.millerandcarter.co.uk/ourvenues#/",)
-    sitemap_rules = [("", "parse_sd")]
+    start_urls = ["https://www.millerandcarter.co.uk/ourvenues#/"]
 
     def parse(self, response):
         urls = response.xpath('//div[@class="accordion parbase section"]//a/@href').extract()
