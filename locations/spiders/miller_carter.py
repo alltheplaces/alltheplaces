8f0780a77929251c85f74e15e2be7d9c02cce144
Boomer Yancey, Location Analyst <8587049+boomerwv1@users.noreply.github.com>
2022-10-13 12:46:05 -0400

    Update miller_carter.py

--- locations/spiders/miller_carter.py
+++ locations/spiders/miller_carter.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-import re
-import json
 import scrapy
 from locations.structured_data_spider import StructuredDataSpider
 
+
 class MillerCarterSpider(StructuredDataSpider):
-    # download_delay = 0.2
     name = "millercarter"
-    item_attributes = {"brand": "Miller and Carter", "brand_wikidata": "Q87067401"}
+    item_attributes = {"brand": "Miller and Carter"}
     allowed_domains = ["millerandcarter.co.uk"]
     start_urls = ["https://www.millerandcarter.co.uk/ourvenues#/"]
 
     def parse(self, response):
-        urls = response.xpath('//div[@class="accordion parbase section"]//a/@href').extract()
+        urls = response.xpath(
+            '//div[@class="accordion parbase section"]//a/@href'
+        ).extract()
         for url in urls:
             yield scrapy.Request(response.urljoin(url), callback=self.parse_sd)
