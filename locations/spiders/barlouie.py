1a36f97dc89104ea2c706bfda37b63613389223d
Amit Kumar <akumar@indeed.com>
2022-10-15 16:05:34 +0530

    3524: Addressing review comments

--- locations/spiders/barlouie.py
+++ locations/spiders/barlouie.py
@@ -1,48 +1,36 @@
 import json
 
-import scrapy
+from scrapy.spiders import SitemapSpider
 
-from locations.items import GeojsonPointItem
+from locations.linked_data_parser import LinkedDataParser
 
 
-class BarlouieSpider(scrapy.Spider):
+class BarlouieSpider(SitemapSpider):
     name = "barlouie"
     allowed_domains = ["www.barlouie.com"]
-    start_urls = ["https://www.barlouie.com/locations"]
     item_attributes = {"brand": "Barlouie", "brand_wikidata": "Q16935493"}
+    sitemap_urls = ["https://www.barlouie.com/sitemap.xml"]
+    sitemap_rules = [("/locations/", "parse")]
 
     def parse(self, response):
-        for link in response.xpath("//a[contains(@href, 'locations')]/@href").getall():
-            yield scrapy.Request(
-                "https://www.barlouie.com" + link, self.parse_each_website
-            )
-
-    def parse_each_website(self, response):
         for ldjson in response.xpath(
-            '//script[@type="application/ld+json"]/text()'
+            "//script[@type='application/ld+json' and contains(text(), 'Restaurant')]/text()"
         ).extract():
-            sanitized_data = self.sanitize_json(ldjson)
-            data = json.loads(sanitized_data)
-            if data["@type"] == "Restaurant":
-                yield self.parse_store(response, data)
+            try:
+                data = json.loads(ldjson)
+            except:
+                sanitized_data = self.sanitize_json(ldjson)
+                data = json.loads(sanitized_data)
+            item = LinkedDataParser.parse_ld(data)
+            print("Item: ", item)
+            item["ref"] = item["website"] = response.url
+            item["image"] = item.get("image", "").replace(
+                "https://www.barlouie.comhttps://images.prismic.io",
+                "https://images.prismic.io",
+            )
+            yield item
 
     def sanitize_json(self, data):
         start_index = data.find('"TimeZoneOffsetSeconds"')
         end_index = data.find("]", start_index)
         return data[: start_index - 7] + data[end_index - 1 :]
-
-    def parse_store(self, response, data):
-        properties = {
-            "name": data["name"],
-            "ref": response.url.split("/")[-2],
-            "lat": data["geo"]["latitude"],
-            "lon": data["geo"]["longitude"],
-            "addr_full": data["address"]["streetAddress"],
-            "city": data["address"]["addressLocality"],
-            "state": data["address"]["addressRegion"],
-            "postcode": data["address"]["postalCode"],
-            "phone": data["telephone"],
-            "opening_hours": "".join(data["openingHours"]),
-            "website": response.url,
-        }
-        return GeojsonPointItem(**properties)
