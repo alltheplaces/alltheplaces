33943eae5f88c876d739a03488f9d20ec6fcef6c
Amit Kumar <akumar@indeed.com>
2022-10-16 00:42:50 +0530

    1055: Addressed review comments

--- locations/spiders/sprouts.py
+++ locations/spiders/sprouts.py
@@ -1,7 +1,8 @@
-import json
+import re
 
 from scrapy.spiders import SitemapSpider
 
+from locations.hours import OpeningHours, sanitise_day, day_range
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -10,9 +11,27 @@ class SproutsFarmersMarket(SitemapSpider, StructuredDataSpider):
     allowed_domains = ["www.sprouts.com"]
     item_attributes = {"brand": "Sprouts Farmer Market", "brand_wikidata": "Q7581369"}
     sitemap_urls = ["https://www.sprouts.com/store-sitemap.xml"]
-    sitemap_rules = [(r"^https:\/\/www.sprouts.com\/store\/fl\/*\/*\/", "parse_sd")]
+    sitemap_rules = [(r"^https:\/\/www.sprouts.com\/store\/\w\w\/*\/*\/", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["website"] = response.url
         item["opening_hours"] = ld_data["openingHours"].replace(",", ";")
         yield item
+
+    def pre_process_data(self, ld_data, **kwargs):
+        oh = OpeningHours()
+        for days, open_time, open_time_ampm, close_time, close_time_ampm in re.findall(
+            r"(\w+-\w+) (\d+:\d+)\s?(AM|am|PM|pm) (\d+:\d+)\s?(AM|am|PM|pm)",
+            ld_data["openingHours"],
+        ):
+            start_day, end_day = days.split("-")
+            start_day = sanitise_day(start_day)
+            end_day = sanitise_day(end_day)
+            for day in day_range(start_day, end_day):
+                oh.add_range(
+                    day,
+                    open_time + open_time_ampm,
+                    close_time + close_time_ampm,
+                    time_format="%I:%M%p",
+                )
+        ld_data["openingHours"] = oh.as_opening_hours()
