de5f667f35705c8e101256e76853c87ccefbd85e
Cj Malone <CjMalone@mail.com>
2022-10-19 12:04:33 +0100

    Fix aldi_nord_nl

--- locations/spiders/aldi_nord_nl.py
+++ locations/spiders/aldi_nord_nl.py
@@ -1,56 +1,31 @@
 # -*- coding: utf-8 -*-
-import re
-
 import scrapy
 
-from locations.items import GeojsonPointItem
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours, DAYS
 
 
 class AldiNordNLSpider(scrapy.Spider):
     name = "aldi_nord_nl"
     item_attributes = {"brand": "ALDI", "brand_wikidata": "Q41171373"}
-    allowed_domains = ["www.aldi.nl"]
     start_urls = [
-        "https://www.aldi.nl/service/winkels-en-openingstijden.html",
+        "https://uberall.com/api/storefinders/ALDINORDNL_8oqeY3lnn9MTZdVzFn4o0WCDVTauoZ/locations/all"
     ]
 
-    def parse(self, response):
-        urls = response.xpath(
-            '//div[@class="mod-stores__multicolumn"]/p/a/@href'
-        ).extract()
-        is_store_list = response.xpath(
-            '//div[@class="mod mod-stores"]//div[@class="mod-stores__overview-company-tools"]/a/@href'
-        ).extract()
+    def parse(self, response, **kwargs):
+        for store in response.json()["response"]["locations"]:
 
-        if not urls and is_store_list:
-            for store_url in is_store_list:
-                yield scrapy.Request(
-                    response.urljoin(store_url), callback=self.parse_store
-                )
-        else:
-            for url in urls:
-                yield scrapy.Request(response.urljoin(url))
+            store["street_address"] = ", ".join(
+                filter(None, [store.pop("streetAndNumber", store.pop("addressExtra"))])
+            )
 
-    def parse_store(self, response):
-        ref = re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1)
-        country = re.search(r"aldi\.(\w{2}?)\/", response.url).group(1)
+            item = DictParser.parse(store)
 
-        properties = {
-            "ref": ref,
-            "name": response.xpath(
-                '//div[@class="mod-overview-intro__content"]/h1/text()'
-            ).extract_first(),
-            "addr_full": response.xpath(
-                'normalize-space(//span[@itemprop="streetAddress"]//text())'
-            ).extract_first(),
-            "city": response.xpath(
-                'normalize-space(//span[@itemprop="addressLocality"]//text())'
-            ).extract_first(),
-            "postcode": response.xpath(
-                'normalize-space(//span[@itemprop="postalCode"]//text())'
-            ).extract_first(),
-            "country": country,
-            "website": response.url,
-        }
+            oh = OpeningHours()
+            for rule in store["openingHours"]:
+                if rule.get("closed", False):
+                    continue
+                oh.add_range(DAYS[rule["dayOfWeek"] - 1], rule["from1"], rule["to1"])
+            item["opening_hours"] = oh.as_opening_hours()
 
-        yield GeojsonPointItem(**properties)
+            yield item
