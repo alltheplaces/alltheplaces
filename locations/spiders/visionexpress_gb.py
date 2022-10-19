edc80245c4ab2087338861d85454dbc40f12ef58
Cj Malone <CjMalone@mail.com>
2022-10-19 22:06:10 +0100

    Fix Vision Express GB Spider
    Google URL Parser was failing on a "markers" value

--- locations/spiders/visionexpress_gb.py
+++ locations/spiders/visionexpress_gb.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from locations.linked_data_parser import LinkedDataParser
+from scrapy.spiders import SitemapSpider
+
 from locations.google_url import extract_google_position
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class VisionExpressGBSpider(scrapy.spiders.SitemapSpider):
+class VisionExpressGBSpider(SitemapSpider, StructuredDataSpider):
     name = "visionexpress_gb"
-    item_attributes = {
-        "brand": "VisionExpress",
-        "brand_wikidata": "Q7936150",
-    }
+    item_attributes = {"brand": "VisionExpress", "brand_wikidata": "Q7936150"}
     sitemap_urls = ["https://www.visionexpress.com/sitemap.xml"]
-    sitemap_rules = [("/opticians/", "parse")]
+    sitemap_rules = [("/opticians/", "parse_sd")]
     download_delay = 0.2
 
-    def parse(self, response):
-        item = LinkedDataParser.parse(response, "Store")
-        if item:
-            item["street_address"] = item["street_address"].replace(" undefined", "")
-            # TODO: when there is an agreed solution to this.
-            # if "-tesco" in response.url:
-            #    set_located_in(item, "Tesco Extra", "Q25172225")
-            extract_google_position(item, response)
-            return item
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["street_address"] = item["street_address"].replace(" undefined", "")
+        # TODO: when there is an agreed solution to this.
+        # if "-tesco" in response.url:
+        #    set_located_in(item, "Tesco Extra", "Q25172225")
+        extract_google_position(item, response)
+        yield item
