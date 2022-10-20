26aaa623ce043d9254dac5ad00f575748bb68d7e
Romeo <romeo@echo-analytics.io>
2022-10-20 17:07:43 +0200

    fix: opening hours

--- locations/spiders/duffys.py
+++ locations/spiders/duffys.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+
 import scrapy
 
 from locations.hours import DAYS_EN
@@ -22,16 +24,24 @@ class Duffys(scrapy.Spider):
 
         for store in results:
             address_results = store.get("address")
+
+            oh = store.get("hoursOfOperation")
+            oh = [format_hours(o) for o in oh.split(" ")]
+            # print(" ".join(m))
             oh = (
-                store.get("hoursOfOperation")
-                .replace(":", "")
+                " ".join(oh)
                 .replace(",", ";")
                 .replace(" - ", "-")
                 .replace("Daily", "Mo-Su")
-                .replace("am", ":00")
-                .replace("pm", ":00")
                 .lstrip(" ")
             )
+
+            formated = [
+                oh_string.replace("am", "").replace("pm", "")
+                if re.search(r"\d", oh_string) and len(oh_string) >= 7
+                else oh_string.replace("am", ":00").replace("pm", ":00")
+                for oh_string in oh.split(" ")
+            ]
             for k, v in DAYS_EN.items():
                 oh = oh.replace(k, v)
 
@@ -50,3 +60,32 @@ class Duffys(scrapy.Spider):
             }
 
             yield GeojsonPointItem(**properties)
+
+
+def format_hours(oh_string):
+    has_comma = "," in oh_string
+    oh_string = oh_string.replace(",", "")
+    if any(x in oh_string for x in ["am", "pm"]):
+        if any(x in oh_string for x in ["pm", "12"]):
+            oh_string = oh_string.replace("pm", "").replace("am", "")
+            if ":" in oh_string:
+                oh_string_s = oh_string.split(":")
+                oh_string_h = int(oh_string_s[0]) + 12
+                oh_string = (
+                    str(oh_string_h) + ":".join(oh_string_s[1:])
+                    if oh_string_h < 24
+                    else "00:" + "".join(oh_string_s[1:])
+                )
+            else:
+                oh_string_h = int(oh_string) + 12
+                oh_string = str(oh_string_h) + ":00" if oh_string_h < 24 else "00:00"
+        else:
+            oh_string = (
+                oh_string.replace("am", "")
+                if ":" in oh_string
+                else oh_string.replace("am", ":00")
+            )
+        oh_string = oh_string + "," if has_comma else oh_string
+    else:
+        return oh_string.replace(":", "")
+    return oh_string
