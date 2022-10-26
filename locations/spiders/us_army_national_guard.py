31e341dab97cf78d41b1248d517a89ab34abdb26
Cj Malone <CjMalone@mail.com>
2022-10-26 19:11:36 +0100

    Add back extra states

--- locations/spiders/us_army_national_guard.py
+++ locations/spiders/us_army_national_guard.py
@@ -17,6 +17,8 @@ class USArmyNationalGuardSpider(scrapy.Spider):
     def start_requests(self):
         for state in GeonamesCache().get_us_states():
             yield scrapy.Request(url="https://www.nationalguard.com/api/state/" + state)
+        for state in ["USPR", "USGU", "USVI"]:
+            yield scrapy.Request(url="https://www.nationalguard.com/api/state/" + state)
 
     def parse(self, response, **kwargs):
         data = response.json()
