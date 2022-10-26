ee76f2930edfa9a9c90d57c1eea662b23af5d2ce
Romeo <romeo@echo-analytics.io>
2022-10-26 18:39:07 +0200

    fix: HSBC brand fix

--- locations/spiders/hsbc_uk.py
+++ locations/spiders/hsbc_uk.py
@@ -7,7 +7,7 @@ from locations.linked_data_parser import LinkedDataParser
 class HSBC_UK(CrawlSpider):
     name = "hsbc_uk"
     item_attributes = {
-        "brand": "HSBC UK",
+        "brand": "HSBC",
         "brand_wikidata": "Q64767453",
     }
     start_urls = ["https://www.hsbc.co.uk/branch-list/"]
