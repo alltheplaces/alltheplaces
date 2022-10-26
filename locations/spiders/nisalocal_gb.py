ea28d9c4c5abf769806887fe6d30fda7361633d6
Joe Dixon <mjoe999@gmail.com>
2022-10-26 13:39:57 +0100

    lint

--- locations/spiders/nisalocal_gb.py
+++ locations/spiders/nisalocal_gb.py
@@ -9,5 +9,7 @@ class NisaLocalGBSpider(CrawlSpider, StructuredDataSpider):
     item_attributes = {"brand": "Nisa", "brand_wikidata": "Q16999069"}
     allowed_domains = ["nisalocally.co.uk"]
     start_urls = ["https://www.nisalocally.co.uk/stores/index.html"]
-    rules = [Rule(LinkExtractor(allow=".*/stores/.*"), callback="parse_sd", follow=True)]
+    rules = [
+        Rule(LinkExtractor(allow=".*/stores/.*"), callback="parse_sd", follow=True)
+    ]
     download_delay = 0.5
