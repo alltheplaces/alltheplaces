c57ad750719a75dc84229c7d6c17be1e82a695e7
Cj Malone <CjMalone@mail.com>
2022-11-02 10:25:16 +0000

    Fix extended_stay_america

--- locations/spiders/extended_stay_america.py
+++ locations/spiders/extended_stay_america.py
@@ -1,52 +1,16 @@
-# -*- coding: utf-8 -*-
-import json
-import re
+from scrapy.spiders import SitemapSpider
 
-import scrapy
-
-from locations.items import GeojsonPointItem
-from locations.hours import OpeningHours
+from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROSWER_DEFAULT
 
 
-class ExtendedStayAmericaSpider(scrapy.Spider):
+class ExtendedStayAmericaSpider(SitemapSpider, StructuredDataSpider):
     name = "extended_stay_america"
     item_attributes = {
         "brand": "Extended Stay America",
         "brand_wikidata": "Q5421850",
         "country": "US",
     }
-    allowed_domains = ["extendedstayamerica.com"]
-    start_urls = [
-        "https://www.extendedstayamerica.com/hotels/",
-    ]
-    user_agent = "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
-
-    def parse(self, response):
-        urls = response.xpath(
-            '//ul[@class="esa-locations-markets-list"]/li/a/@href'
-        ).extract()
-        for url in urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_hotel_list)
-
-    def parse_hotel_list(self, response):
-        script = response.xpath(
-            "//script[contains(text(), 'hotelsData')]/text()"
-        ).extract_first()
-        hotels = json.loads(script[58:-3])
-
-        for hotel in hotels:
-            properties = {
-                "name": hotel["title"],
-                "ref": hotel["siteId"],
-                "street_address": hotel["address"]["street"],
-                "city": hotel["address"]["city"],
-                "state": hotel["address"]["region"],
-                "postcode": hotel["address"]["postalCode"],
-                "website": response.urljoin(hotel["urlMap"]),
-                "image": response.urljoin(hotel["featuredImages"][0]["url"]),
-                "phone": hotel["phone"],
-                "lat": hotel["latitude"],
-                "lon": hotel["longitude"],
-            }
-
-            yield GeojsonPointItem(**properties)
+    sitemap_urls = ["https://api.prod.bws.esa.com/cms-proxy-api/sitemap/property"]
+    sitemap_rules = [("/hotels/", "parse_sd")]
+    custom_settings = {"AUTOTHROTTLE_ENABLED": True, "USER_AGENT": BROSWER_DEFAULT}
