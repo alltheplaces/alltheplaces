2645b8af3440a23ed78e863775f7740b8a528943
Cj Malone <CjMalone@mail.com>
2022-11-09 15:09:16 +0000

    Convert JDSports GB Spider StructuredDataSpider

--- locations/spiders/jdsports_gb.py
+++ locations/spiders/jdsports_gb.py
@@ -1,28 +1,13 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-from locations.linked_data_parser import LinkedDataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class JDSportsGBSpider(CrawlSpider):
+class JDSportsGBSpider(CrawlSpider, StructuredDataSpider):
     name = "jdsports_gb"
-    item_attributes = {
-        "brand": "JD Sports",
-        "brand_wikidata": "Q6108019",
-    }
+    item_attributes = {"brand": "JD Sports", "brand_wikidata": "Q6108019"}
     start_urls = ["https://www.jdsports.co.uk/store-locator/all-stores/"]
     rules = [
-        Rule(
-            LinkExtractor(allow="store-locator/"), callback="parse_store", follow=False
-        )
+        Rule(LinkExtractor(allow="store-locator/", deny="-soon"), callback="parse_sd")
     ]
-    download_delay = 0.2
-
-    def parse_store(self, response):
-        if "-soon" in response.url:
-            self.logger.warning("ignoring store opening soon %s", response.url)
-        else:
-            item = LinkedDataParser.parse(response, "Store")
-            if item:
-                item["ref"] = response.url.strip("/").split("/")[-1]
-                yield item
