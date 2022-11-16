78d2e16b8eb8550660823960c4937d52ad1abec9
jancallewaert <jan.callewaert@gmail.com>
2022-11-16 08:40:28 +0100

    Add additional Decathlon hosts

--- locations/spiders/decathlon.py
+++ locations/spiders/decathlon.py
@@ -10,14 +10,21 @@ class Decathlon(CrawlSpider):
     name = "decathlon"
     item_attributes = {"brand": "Decathlon", "brand_wikidata": "Q509349"}
     start_urls = [
+        "https://www.decathlon.be/nl/store-locator",
+        "https://www.decathlon.ch/fr/store-locator",
+        "https://www.decathlon.cz/store-locator",
         "https://www.decathlon.co.uk/store-locator",
         "https://www.decathlon.de/store-locator",
         "https://www.decathlon.es/es/store-locator",
+        "https://www.decathlon.com.hk/en/store-locator",
         "https://www.decathlon.it/store-locator",
         "https://www.decathlon.fr/store-locator",
         "https://www.decathlon.hu/store-locator",
+        "https://www.decathlon.nl/store-locator",
         "https://www.decathlon.pl/store-locator",
         "https://www.decathlon.pt/store-locator",
+        "https://www.decathlon.ro/store-locator",
+        "https://www.decathlon.com.tr/store-locator",
         # TODO: more domains no doubt
     ]
     rules = [Rule(LinkExtractor(allow="/store-view/"), callback="parse", follow=False)]
