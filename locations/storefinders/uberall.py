dcdd7562951639396583adc62c451046706a0b2f
Cj Malone <CjMalone@mail.com>
2022-11-23 22:42:53 +0000

    Add Uberall base spider

---
+++ locations/storefinders/uberall.py
@@ -0,0 +1,56 @@
+import logging
+
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS, OpeningHours
+
+
+class UberallSpider(Spider):
+    key = ""
+    business_id_filter = None
+
+    def start_requests(self):
+        yield JsonRequest(
+            url=f"https://uberall.com/api/storefinders/{self.key}/locations/all"
+        )
+
+    def parse(self, response, **kwargs):
+        if response.json()["status"] != "SUCCESS":
+            logging.warning("Request failed")
+
+        for feature in response.json()["response"]["locations"]:
+            if self.business_id_filter:
+                if feature["businessId"] != self.business_id_filter:
+                    continue
+
+            feature["street_address"] = ", ".join(
+                filter(None, [feature["streetAndNumber"], feature["addressExtra"]])
+            )
+            feature["ref"] = feature.get("identifier")
+
+            item = DictParser.parse(feature)
+
+            item["image"] = ";".join(
+                filter(None, [p.get("publicUrl") for p in feature["photos"] or []])
+            )
+
+            oh = OpeningHours()
+            for rule in feature["openingHours"]:
+                if rule.get("closed"):
+                    continue
+                # I've only seen from1 and from2, but I guess it could any length
+                for i in range(1, 3):
+                    if rule.get(f"from{i}") and rule.get(f"to{i}"):
+                        oh.add_range(
+                            DAYS[rule["dayOfWeek"] - 1],
+                            rule[f"from{i}"],
+                            rule[f"to{i}"],
+                        )
+            item["opening_hours"] = oh.as_opening_hours()
+
+            yield from self.parse_item(item, feature)
+
+    def parse_item(self, item, feature, **kwargs):
+        yield item
