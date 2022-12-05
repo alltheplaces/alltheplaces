dbfbb40f6436eca1ef4468fdf5baed51d473217f
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-05 14:36:48 +0100

    Fix academy Spider

--- locations/spiders/academy.py
+++ locations/spiders/academy.py
@@ -3,7 +3,6 @@ import re
 import scrapy
 from scrapy.utils.gz import gunzip
 
-from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
 
 
@@ -23,56 +22,41 @@ class AcademySpider(scrapy.Spider):
         body = scrapy.Selector(text=body)
         body.remove_namespaces()
         urls = body.xpath("//url/loc/text()").extract()
+        store_url = re.compile(
+            r"https://www.academy.com/storelocator/.+?/.+?/store-\d+"
+        )
         for path in urls:
-            store_url = re.compile(
-                r"https://www.academy.com/shop/storelocator/.+?/.+?/store-\d+"
-            )
             if re.search(store_url, path):
                 yield scrapy.Request(path.strip(), callback=self.parse_store)
-
-    def parse_hours(self, hours):
-        opening_hours = OpeningHours()
-
-        for elem in hours:
-            day, open_time, close_time = re.search(
-                r"([A-Za-z]+)\s([\d:]+)\s-\s([\d:]+)", elem
-            ).groups()
-            opening_hours.add_range(
-                day=day[:2], open_time=open_time, close_time=close_time
-            )
-
-        return opening_hours.as_opening_hours()
 
     def parse_store(self, response):
         properties = {
             "ref": re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1),
             "name": response.xpath(
-                'normalize-space(//h1[@itemprop="name"]//text())'
+                'normalize-space(//*[@class="Hero-name"]//text())'
             ).extract_first(),
             "addr_full": response.xpath(
-                'normalize-space(//span[@itemprop="streetAddress"]//text())'
+                'normalize-space(//*[@class="c-address-street-1"]//text())'
             ).extract_first(),
             "city": response.xpath(
-                'normalize-space(//span[@itemprop="addressLocality"]//text())'
+                'normalize-space(//*[@class="c-address-city"]//text())'
             ).extract_first(),
             "state": response.xpath(
-                'normalize-space(//span[@itemprop="addressRegion"]//text())'
+                'normalize-space(//*[@class="c-address-state"]//text())'
             ).extract_first(),
             "postcode": response.xpath(
                 'normalize-space(//span[@itemprop="postalCode"]//text())'
             ).extract_first(),
-            "phone": response.xpath('//a[@id="storePhone"]/text()').extract_first(),
+            "phone": response.xpath('normalize-space(//*[@class="Phone-link"]//text())').extract_first(),
             "website": response.url,
             "lat": float(
-                response.xpath('//input[@id="params"]/@data-lat').extract_first()
-            ),
+                    response.xpath('normalize-space(//*[@itemprop="latitude"]/@content)').extract_first()
+                ),
             "lon": float(
-                response.xpath('//input[@id="params"]/@data-lng').extract_first()
-            ),
+                    response.xpath('normalize-space(//*[@itemprop="longitude"]/@content)').extract_first()
+                )
         }
 
-        properties["opening_hours"] = self.parse_hours(
-            response.xpath('//*[@itemprop="openingHours"]/@datetime').extract()
-        )
+        properties["opening_hours"] = "; ".join(response.xpath('//*[@itemprop="openingHours"]/@content').extract())
 
         yield GeojsonPointItem(**properties)
