d7b928222a35682eb5dffbff00e450ebe52627ad
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-05 16:21:46 +0100

    Fix anatomie Spider

--- locations/spiders/anatomie.py
+++ locations/spiders/anatomie.py
@@ -9,6 +9,7 @@ class AnatomieSpider(scrapy.Spider):
     name = "anatomie"
     item_attributes = {"brand": "Anatomie"}
     allowed_domains = ["stores.boldapps.net"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
     start_urls = (
         "https://stores.boldapps.net/front-end/get_surrounding_stores.php?shop=anatomie-store.myshopify.com&latitude=38.331258&longitude=-121.54841799999997&max_distance=100000&limit=10000&calc_distance=1",
     )
