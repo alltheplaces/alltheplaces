98a4bac1c7d9c72059ffd0fa4927b38378555f7d
Cj Malone <CjMalone@mail.com>
2022-12-05 14:22:43 +0000

    Fix PNC Spider

--- locations/spiders/pnc.py
+++ locations/spiders/pnc.py
@@ -1,45 +1,43 @@
 import scrapy
 
-from locations.items import GeojsonPointItem
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.geo import point_locations
 
 
-class PncSpider(scrapy.Spider):
+class PNCSpider(scrapy.Spider):
     name = "pnc"
-    item_attributes = {"brand": "PNC", "brand_wikidata": "Q38928"}
+    item_attributes = {"brand": "PNC", "brand_wikidata": "Q38928", "country": "US"}
     allowed_domains = ["www.pnc.com"]
-    start_urls = [
-        "https://apps.pnc.com/locator/search",
-    ]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def start_requests(self):
-        base_url = "https://apps.pnc.com/locator-api/locator/api/v2/location/?t=1619625397878&latitude={lat}&longitude={lng}&radius=100&radiusUnits=mi&branchesOpenNow=false"
-
-        with open(
-            "./locations/searchable_points/us_centroids_100mile_radius.csv"
-        ) as points:
-            next(points)
-            for point in points:
-                _, lat, lon = point.strip().split(",")
-                url = base_url.format(lat=lat, lng=lon)
-                yield scrapy.Request(url=url, callback=self.parse)
+        for lat, lon in point_locations("us_centroids_100mile_radius.csv"):
+            yield scrapy.Request(
+                f"https://apps.pnc.com/locator-api/locator/api/v2/location/?latitude={lat}&longitude={lon}&radius=100&radiusUnits=mi&branchesOpenNow=false"
+            )
 
     def parse(self, response):
-        data = response.json()
-        branch_data = data["locations"]
-
-        for branch in branch_data:
-            if "ATM" in branch["locationType"]["locationTypeDesc"]:
-                pass
+        for branch in response.json()["locations"]:
+            branch["ref"] = branch.pop("locationId")
+            branch["name"] = branch.pop("locationName")
+            branch["street_address"] = ", ".join(
+                filter(
+                    None,
+                    [
+                        branch["address"].pop("address1"),
+                        branch["address"].pop("address2"),
+                    ],
+                )
+            )
+            branch["location"] = {
+                "latitude": branch["address"].pop("latitude"),
+                "longitude": branch["address"].pop("longitude"),
+            }
+            item = DictParser.parse(branch)
+            if "BRANCH" in branch["locationType"]["locationTypeDesc"]:
+                apply_category(Categories.BANK, item)
             else:
-                properties = {
-                    "ref": branch["locationId"],
-                    "name": branch["locationName"],
-                    "addr_full": branch["address"]["address1"],
-                    "city": branch["address"]["city"],
-                    "state": branch["address"]["state"],
-                    "postcode": branch["address"]["zip"],
-                    "lat": branch["address"]["latitude"],
-                    "lon": branch["address"]["longitude"],
-                }
+                apply_category(Categories.ATM, item)
 
-                yield GeojsonPointItem(**properties)
+            yield item
