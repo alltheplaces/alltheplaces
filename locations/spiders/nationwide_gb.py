e9c3bf0b311c21d69396f86ab1e763b9ae69eb1f
Cj Malone <CjMalone@mail.com>
2022-12-06 18:26:02 +0000

    Parse more Nationwide locations

--- locations/spiders/nationwide_gb.py
+++ locations/spiders/nationwide_gb.py
@@ -8,17 +8,8 @@ class NationwideGB(CrawlSpider, StructuredDataSpider):
     name = "nationwide_gb"
     item_attributes = {"brand": "Nationwide", "brand_wikidata": "Q846735"}
     start_urls = ["https://www.nationwide.co.uk/branches/index.html"]
-    rules = [
-        Rule(
-            LinkExtractor(
-                allow=r"https:\/\/www\.nationwide\.co\.uk\/branches\/[-()\w]+\/[-\w]+$"
-            ),
-            callback="parse_sd",
-        ),
-        Rule(
-            LinkExtractor(
-                allow=r"https:\/\/www\.nationwide\.co\.uk\/branches\/[-()\w]+$"
-            )
-        ),
-    ]
-    wanted_types = ["BankOrCreditUnion"]
+    rules = [Rule(LinkExtractor(allow=r"/branches/"), callback="parse_sd", follow=True)]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if "permanently closed" not in item["name"].lower():
+            yield item
