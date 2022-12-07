870a9c02734d7ef29e7a0e917c093c5d13e0f3d8
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-07 02:32:14 +0100

    Fix andpizza Spider

--- locations/spiders/andpizza.py
+++ locations/spiders/andpizza.py
@@ -1,47 +1,53 @@
 import re
-
 import scrapy
-
 from locations.items import GeojsonPointItem
 
 
 class AndPizzaSpider(scrapy.Spider):
     name = "andpizza"
-    item_attributes = {"brand": "&pizza"}
-    allowed_domains = ["andpizza.com"]
-    start_urls = ("https://andpizza.com",)
+    item_attributes = {"brand": "&pizza", "brand_wikidata": "Q21189222"}
+    start_urls = ("https://api.andpizza.com/webapi/v100/partners/shops",)
+
+    def start_requests(self):
+
+        url = self.start_urls[0]
+
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Referer": "https://order.andpizza.com/",
+            "Connection": "keep-alive",
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIsInBhcnRuZXIiOiJpNzIiLCJpc3MiOiJodHRwczovL2FwaS5hbmRwaXp6YS5jb20iLCJpYXQiOjE1OTgyOTEwNzUsImV4cCI6MTkxMzY1MTA3NSwibmJmIjoxNTk4MjkxMDc1LCJqdGkiOiI4VWh1VnhJRjFwZFhrSXplIn0.rXJL4rt5YbT4XRk21sSrkoGffJ5ttowV3UbHInZcnMs",
+        }
+
+        yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def normalize_time(sef, time_str):
-        match = re.search(r"([0-9]{1,2}) (a|p)m$", time_str)
+        match = re.search(r"([0-9]{1,2}):([0-9]{2}) (a|p)m$", time_str)
         if not match:
-            match = re.search(r"([0-9]{1,2})(a|p)m$", time_str)
-            h, am_pm = match.groups()
+            match = re.search(r"([0-9]{1,2}):([0-9]{2})(a|p)m$", time_str)
+            h, m, am_pm = match.groups()
         else:
-            h, am_pm = match.groups()
+            h, m, am_pm = match.groups()
 
         return "%02d:%02d" % (
-            int(h) + 12 if am_pm == "p." else int(h),
-            int("0"),
+            int(h) + 12 if am_pm == "p" else int(h),
+            int(m),
         )
-        return ""
 
-    def hours(self, data):
+    def parse_hours(self, data):
         opening_hours = ""
 
-        if "&bar" in data:
-            data = data[: data.index("&bar")]
-
-        for i in range(len(data) // 2):
-            days = data[i * 2].split(" ")
-            hours = data[i * 2 + 1]
+        for i in range(len(data)):
+            days = data[i]["label"].split(" - ")
+            hours = data[i]["value"]
             if len(days) == 1:
                 day = days[0][:2]
-            elif len(days) == 3:
-                day = days[0][:2] + "-" + days[2][:2]
+            elif len(days) == 2:
+                day = days[0][:2] + "-" + days[1][:2]
             else:
                 day = "Mo-Su"
 
-            hours = hours.split("-")
+            hours = hours.split(" - ")
             open = self.normalize_time(hours[0].strip())
             close = self.normalize_time(hours[1].strip())
 
@@ -52,29 +58,22 @@ class AndPizzaSpider(scrapy.Spider):
         return opening_hours
 
     def parse(self, response):
-        selector = scrapy.Selector(response)
-        stores = selector.css("div.location")
 
+        stores = response.json().get("data")
         for store in stores:
-            ref = store.css("div.location::attr(class)").extract_first().split(" ")[1]
-            name = store.css("a.knockout *::text").extract_first()
-            address = store.css("address>a *::text").extract()
-            address1 = address[0]
-            address2 = address[len(address) - 1].split(",")
-            hours = store.css("div.hours")
+            props = {}
+            props["name"] = store["name"]
+            props["ref"] = store["id"]
+            props["addr_full"] = store["location"]["address1"]
+            props["city"] = store["location"]["city"]
+            props["state"] = store["location"]["state"]
+            props["lat"] = store["location"]["latitude"]
+            props["lon"] = store["location"]["longitude"]
+            props["postcode"] = store["location"]["zipcode"]
+            props["phone"] = store["location"]["phone"]
 
-            store_hours = ""
-            if not hours.css("span>a"):
-                store_hours = self.hours(store.css("div.hours *::text").extract())
+            props["opening_hours"] = self.parse_hours(
+                store["service_schedule"]["general"]
+            )
 
-            properties = {
-                "ref": ref,
-                "name": name,
-                "street": address1,
-                "city": address2[0],
-                "state": address2[1].split(" ")[1],
-                "postcode": address2[1].split(" ")[2],
-                "opening_hours": store_hours,
-            }
-
-            yield GeojsonPointItem(**properties)
+            yield GeojsonPointItem(**props)
