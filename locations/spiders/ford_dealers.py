6d9e5190451e02523917f4f02b807214bf86a0cc
Ian Dees <ian.dees@gmail.com>
2022-12-10 18:02:46 -0600

    Add Ford dealerships

---
+++ locations/spiders/ford_dealers.py
@@ -0,0 +1,26 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class FordDealersSpider(CrawlSpider, StructuredDataSpider):
+    name = "ford_dealers"
+    item_attributes = {
+        "brand": "Ford Motor Company",
+        "brand_wikidata": "Q44294",
+    }
+    start_urls = ["https://www.ford.com/dealerships/dealer-directory/browse-all/"]
+    wanted_types = ["AutoDealer"]
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow=r"^https://www.ford.com/dealerships/dealer-directory/"
+            ),
+        ),
+        Rule(
+            LinkExtractor(
+                allow=r"^https://www.ford.com/content/brand_ford/en_us/brand/dealerships/dealer-details/.*.html$",
+            ),
+            callback="parse_sd",
+        )
+    ]
