7c4f41687c6bda2b7c629f7f3ddf5f6926d86635
Cj Malone <CjMalone@mail.com>
2022-12-12 12:39:55 +0000

    Collect location data

--- locations/spiders/popeyes.py
+++ locations/spiders/popeyes.py
@@ -1,3 +1,4 @@
+from chompjs import chompjs
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
@@ -8,3 +9,11 @@ class PopeyesSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Popeyes", "brand_wikidata": "Q1330910"}
     sitemap_urls = ["https://locations.popeyes.com/sitemap.xml"]
     sitemap_rules = [(r"https://locations.popeyes.com/\w\w/.+/.+$", "parse_sd")]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if data := response.xpath('//script[@class="js-map-data"]/text()').get():
+            if loc := chompjs.parse_js_object(data):
+                item["lat"] = loc["latitude"]
+                item["lon"] = loc["longitude"]
+
+        yield item
