c44653e73d1a2a3ed9f19ba3ba4bbcfc0720b04a
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-13 17:21:46 +0100

    Add spider for bmw_es

---
+++ locations/spiders/bmw_es.py
@@ -0,0 +1,26 @@
+import scrapy
+
+from locations.dict_parser import DictParser
+
+
+class BeerBmwEsSpider(scrapy.Spider):
+    name = "bmw_es"
+    item_attributes = {
+        "brand": "bmw",
+        "brand_wikidata": "Q26678",
+    }
+    allowed_domains = ["bmw.es"]
+    start_urls = [
+        "https://www.bmw.es/c2b-localsearch/services/api/v4/clients/BMWSTAGE2_DLO/ES/pois?brand=BMW_BMWI_BMWM&cached=off&category=BM&country=ES&language=es&lat=0&lng=0&maxResults=700&showAll=true&unit=km"
+    ]
+
+    def parse(self, response):
+        results = response.json()["data"]["pois"]
+        for data in results:
+            item = DictParser.parse(data)
+            item["ref"] = data.get("key")
+            item["phone"] = data.get("attributes", {}).get("phone")
+            item["email"] = data.get("attributes", {}).get("mail")
+            item["website"] = data.get("attributes", {}).get("homepage")
+
+            yield item
