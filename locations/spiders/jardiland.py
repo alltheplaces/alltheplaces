034bca4b522bf7fb7a881f01d123d538ebea32d4
Romeo <romeo@echo-analytics.io>
2022-12-13 11:51:20 +0100

    feat: Jardiland website

--- locations/spiders/jardiland.py
+++ locations/spiders/jardiland.py
@@ -6,3 +6,7 @@ class JardilandSpider(WoosmapSpider):
     item_attributes = {"brand": "Jardiland", "brand_wikidata": "Q3162276"}
     key = "jardiland-woos-staging"
     custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Origin": "https://www.jardiland.com/"}}
+
+    def parse_item(self, item, feature, **kwargs):
+        item[
+            "website"] = f'https://www.jardiland.com/storelocator/store/view/name/{feature["properties"]["contact"]["website"]}/'
