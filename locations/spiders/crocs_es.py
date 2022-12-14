f4a0c1252ff955bc219fe5d236b22438805ac0a1
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-14 17:26:27 +0100

    update crocs to crocs_es

---
+++ locations/spiders/crocs_es.py
@@ -0,0 +1,34 @@
+import xmltodict
+
+import scrapy
+
+from locations.items import GeojsonPointItem
+
+
+class CrocsEsDeSpider(scrapy.Spider):
+    name = "crocs_es"
+    item_attributes = {
+        "brand": "Crocs",
+        "brand_wikidata": "Q926699",
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url="https://crocs.es/storefinder?ajax=1&all=1",
+            method="POST",
+        )
+
+    def parse(self, response):
+        json_data = xmltodict.parse(response.text)
+        for data in json_data["markers"]["marker"]:
+            properties = {
+                "name": data["@name"],
+                "addr_full": data["@addressNoHtml"],
+                "email": data["@email"],
+                "ref": data["@id_store"],
+                "lat": data["@lat"],
+                "lon": data["@lng"],
+                "website": data["@link"],
+            }
+
+            yield GeojsonPointItem(**properties)
