d9dad6709ab63b4685e78ae2cb6581909b612948
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-17 11:23:25 +0100

    update maserati

--- locations/spiders/maserati.py
+++ locations/spiders/maserati.py
@@ -6,12 +6,12 @@ from locations.items import GeojsonPointItem
 class MaseratiSpider(scrapy.Spider):
     name = "maserati"
     item_attributes = {
-        "brand": "maserati",
+        "brand": "Maserati",
         "brand_wikidata": "Q26678",
     }
     allowed_domains = ["maserati.com"]
     start_urls = [
-        "https://api.onthemap.io/server/v1/api/location?query=([sales]=[true] OR [assistance]=[true])&language=en&sort=dealername&key=6e0b94fb-7f95-11ec-9c36-eb25f50f4870&channel=www.maserati.com",
+        "https://api.onthemap.io/server/v1/api/location?query=([sales]=[true] OR [assistance]=[true])&language=en&key=6e0b94fb-7f95-11ec-9c36-eb25f50f4870&channel=www.maserati.com",
     ]
 
     def parse(self, response):
