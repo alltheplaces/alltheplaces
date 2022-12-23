023bcbce2f325b29e8d192578c0b47eadadbe8b2
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-23 23:31:17 +0100

    fix and update spider bannerhealth

--- locations/spiders/bannerhealth.py
+++ locations/spiders/bannerhealth.py
@@ -1,65 +1,73 @@
 import json
 import re
+import time
 
-import scrapy
-
+from scrapy.spiders import SitemapSpider
+from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
 
 
-class BannerHealthSpider(scrapy.Spider):
+class BannerHealthSpider(SitemapSpider):
     name = "bannerhealth"
     item_attributes = {"brand": "Banner Health", "brand_wikidata": "Q4856918"}
     allowed_domains = ["bannerhealth.com"]
-    start_urls = ("https://www.bannerhealth.com/locations?PageNo=ALL",)
-
-    def parse(self, response):
-        urls = response.xpath('//div[@class="location-link"][2]/a/@href').extract()
-
-        for url in urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_location)
+    sitemap_urls = ["https://www.bannerhealth.com/sitemap-BH.xml"]
+    sitemap_rules = [(r"https://www.bannerhealth.com/locations/[-\w]+", "parse_location")]
 
     def parse_location(self, response):
-        try:
-            locs = response.xpath('//div[@class="text-card-location-image-content"]/p[1]/text()[2]').extract_first()
-            city, state_postalcode = locs.split(",")
-            state_postalcode = state_postalcode.strip()
-            jsondata = json.loads(response.xpath('//div[@data-js="map_canvas-v2"]/@data-map-config').extract_first())
-            data = jsondata["markerList"]
-            name = re.search(
-                r".+/(.+)",
-                response.xpath('//meta[@property="og:url"]/@content').extract_first(),
-            ).group(1)
-            ref = re.search(r".+/(.+)", response.url).group(1)
 
-            if " " in state_postalcode:
-                state, postcode = state_postalcode.split(" ")
-                state = state.strip()
-                postcode = postcode.strip()
-            else:
-                state = state_postalcode
-                postcode = None
+        other = response.xpath("/html/body/div[1]/div[2]/div/div[2]/div").get()
+        if not (data := response.xpath('//div[@data-js="map_canvas"]/@data-map-config').get()):
+            data = response.xpath('//div[@data-js="map_canvas-v2"]/@data-map-config').get()
+        if data and not other:
+            if not (
+                cty_ste_pde := response.xpath(
+                    '//div[contains(@class,"text-card-location-image-content")]/p[1]/text()[3]'
+                ).get()
+            ):
+                cty_ste_pde = response.xpath(
+                    '//div[contains(@class,"text-card-location-image-content")]/p[1]/text()[2]'
+                ).get()
+            if not cty_ste_pde:
+                cty_ste_pde = response.xpath("//address/text()[3]").get()
+            if not cty_ste_pde:
+                cty_ste_pde = response.xpath("//address/text()[2]").get()
 
-            for locations in data:
-                location = json.dumps(locations)
-                location_data = json.loads(location)
+            days = response.xpath('//div[@class="card-hours-row"]')
+            oh = OpeningHours()
+            for day in days:
+                hours = day.xpath("./div[2]/text()").get().replace("\n", "").strip()
+                if not hours == "Closed":
+                    for hour in hours.split(","):
+                        open_time = re.split(".to|to", hour)[0].replace(".", "").strip()
+                        close_time = re.split(".to|to", hour)[1].replace(".", "").strip()
+                        oh.add_range(
+                            day=day.xpath("./div[1]/text()").get().replace("\n", "").strip(),
+                            open_time=time.strftime(
+                                "%H:%M", time.strptime(open_time, "%I:%M %p" if ":" in open_time else "%I %p")
+                            ),
+                            close_time=time.strftime(
+                                "%H:%M", time.strptime(close_time, "%I:%M %p" if ":" in close_time else "%I %p")
+                            ),
+                        )
+
+            data = json.loads(data)
+            cty_ste_pde = cty_ste_pde.strip().strip("\n").replace("\xa0", "")
+            postcode = re.findall("[0-9]{5}-[0-9]{4}|[0-9]{5}", cty_ste_pde)[0]
+            state = (re.findall("[A-Z]{2}", cty_ste_pde)[0:1] or (None,))[0]
+            city = cty_ste_pde.replace(str(state), "").replace(postcode, "").replace(",", "").strip()
 
             properties = {
-                "ref": ref,
-                "name": name,
-                "addr_full": response.xpath(
-                    '//div[@class="text-card-location-image-content"]/p[1]/text()'
-                ).extract_first(),
-                "city": city,
-                "state": state,
+                "ref": response.url,
                 "postcode": postcode,
-                "phone": response.xpath('//li[@class="text-card-location-image-content-action-list-item"][1]/a/text()')
-                .extract_first()
-                .strip(),
-                "lat": float(location_data["Latitude"]),
-                "lon": float(location_data["Longitude"]),
-                "website": response.url,
+                "state": state,
+                "city": city,
+                "lat": data.get("markerList")[0].get("Latitude"),
+                "lon": data.get("markerList")[0].get("Longitude"),
+                "name": data.get("markerList")[0].get("Locations")[0].get("Name"),
+                "street_address": data.get("markerList")[0].get("Locations")[0].get("Address").replace("<br/>", ""),
+                "phone": data.get("markerList")[0].get("Locations")[0].get("PhoneNumber"),
+                "opening_hours": oh.as_opening_hours(),
             }
 
             yield GeojsonPointItem(**properties)
-        except:
-            pass
