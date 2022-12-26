bf57dda4794ba753bd88334f51f4a1596b4fdafe
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-26 14:25:03 +0100

    update spider coffeetime

--- locations/spiders/coffee_time.py
+++ locations/spiders/coffee_time.py
@@ -1,31 +1,41 @@
 import scrapy
 
 from locations.dict_parser import DictParser
+from scrapy.spiders import SitemapSpider
 from locations.hours import DAYS, OpeningHours
 
 
-class CoffeeTimeSpider(scrapy.Spider):
+class CoffeeTimeSpider(SitemapSpider):
     name = "coffeetime"
     item_attributes = {"brand": "Coffee Time"}
-    allowed_domains = ["www.coffeetime.com"]
-    start_urls = ["https://api.momentfeed.com/v1/analytics/api/llp.json?auth_token=YDGUJSNDOUAFKPRL"]
+    allowed_domains = ["locations.coffeetime.com", "api.momentfeed.com"]
+    sitemap_urls = ["https://locations.coffeetime.com/sitemap.xml"]
+    sitemap_rules = [("", "parse")]
 
     def parse(self, response):
-        for data in response.json():
-            item = DictParser.parse(data.get("store_info"))
-            item["ref"] = data.get("store_info", {}).get("corporate_id")
-            item["website"] = f'https://{self.allowed_domains[0]}{data.get("llp_url")}'
+        headers = {"Host": "api.momentfeed.com", "Authorization": "YDGUJSNDOUAFKPRL"}
+        url = "https://api.momentfeed.com/v1/analytics/api/llp.json?address={}"
+        yield scrapy.Request(
+            url=url.format(response.url.split("/")[-1].replace("-", "+").replace("_", ".")),
+            headers=headers,
+            callback=self.parse_store,
+        )
 
-            oh = OpeningHours()
-            for day in data.get("store_info", {}).get("store_hours").split(";")[:-1]:
-                info_day = day.split(",")
-                oh.add_range(
-                    day=DAYS[int(info_day[0]) - 1],
-                    open_time=info_day[1],
-                    close_time=info_day[2],
-                    time_format="%H%M",
-                )
+    def parse_store(self, response):
+        item = DictParser.parse(response.json()[0].get("store_info"))
+        item["ref"] = response.json()[0].get("store_info", {}).get("corporate_id")
+        item["website"] = f'https://{self.allowed_domains[0]}{response.json()[0].get("llp_url")}'
 
-            item["opening_hours"] = oh.as_opening_hours()
+        oh = OpeningHours()
+        for day in response.json()[0].get("store_info", {}).get("store_hours").split(";")[:-1]:
+            info_day = day.split(",")
+            oh.add_range(
+                day=DAYS[int(info_day[0]) - 1],
+                open_time=info_day[1],
+                close_time=info_day[2],
+                time_format="%H%M",
+            )
 
-            yield item
+        item["opening_hours"] = oh.as_opening_hours()
+
+        yield item
