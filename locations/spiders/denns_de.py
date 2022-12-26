a54b8084a7a91b3fe47e15248fa4e851e1c02a3a
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-27 00:10:49 +0100

    Fix and update spider denns_de

--- locations/spiders/denns_de.py
+++ locations/spiders/denns_de.py
@@ -1,62 +1,19 @@
-import json
-
 import scrapy
 
-from locations.hours import OpeningHours
-from locations.items import GeojsonPointItem
+from scrapy.spiders import SitemapSpider
 
-DAY_MAPPING = {
-    "Montag": "Mo",
-    "Dienstag": "Tu",
-    "Mittwoch": "We",
-    "MIttwoch": "We",
-    "Donnerstag": "Th",
-    "Donnertag": "Th",
-    "Freitag": "Fr",
-    "Samstag": "Sa",
-    "Sonntag": "Su",
-}
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class DennsDeSpider(scrapy.Spider):
+class DennsDeSpider(SitemapSpider, StructuredDataSpider):
     name = "denns_de"
     allowed_domains = ["www.biomarkt.de"]
-    start_urls = [
-        "https://www.biomarkt.de/api/es/market/_search/?source=%7B%22from%22:0,%22size%22:1000,%22sort%22:[],%22query%22:%7B%22bool%22:%7B%22must%22:[]%7D%7D%7D&source_content_type=application%2Fjson"
-    ]
-    download_delay = 0.2
+    sitemap_urls = ["https://www.biomarkt.de/sitemap.xml"]
+    sitemap_rules = [(r"(.+)/marktseite$", "parse_sd")]
+    wanted_types = ["Store"]
 
-    def parse_hours(self, store_hours):
-        opening_hours = OpeningHours()
-        if store_hours is None:
-            return
-
-        for store_day in store_hours:
-            opening_hours.add_range(
-                day=DAY_MAPPING[store_day["weekday"].strip()],
-                open_time=store_day["open_from"],
-                close_time=store_day["open_until"],
-                time_format="%H:%M",
+    def _parse_sitemap(self, response):
+        for row in super()._parse_sitemap(response):
+            yield scrapy.Request(
+                row.url.replace("http://localhost:8000/", "https://www.biomarkt.de/"), callback=self.parse_sd
             )
-        return opening_hours.as_opening_hours()
-
-    def parse(self, response):
-        data = json.loads(response.text)
-        stores = data["hits"]["hits"]
-        for store in stores:
-            properties = {
-                "ref": store["_id"],
-                "street": store["_source"]["address.street"],
-                "city": store["_source"]["address.city"],
-                "postcode": store["_source"]["address.zip"],
-                "country": store["_source"].get("countryCode", ""),
-                "lat": store["_source"].get("address.lat", ""),
-                "lon": store["_source"].get("address.lon", ""),
-                "phone": store["_source"].get("contact.phone", ""),
-            }
-
-            hours = self.parse_hours(store["_source"]["openingHoursMarket"])
-            if hours:
-                properties["opening_hours"] = hours
-
-            yield GeojsonPointItem(**properties)
