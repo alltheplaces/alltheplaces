9ccc909fabdd23e2d55c1df0a3f06f71f8c3103f
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-26 14:45:43 +0100

    Fix and update spider completecash

--- locations/spiders/completecash.py
+++ locations/spiders/completecash.py
@@ -1,49 +1,12 @@
-import json
+from scrapy.spiders import SitemapSpider
 
-import scrapy
-
-from locations.hours import OpeningHours
-from locations.items import GeojsonPointItem
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CompleteCashSpider(scrapy.Spider):
+class CompleteCashSpider(SitemapSpider, StructuredDataSpider):
     name = "completecash"
     item_attributes = {"brand": "Complete Cash"}
     allowed_domains = ["locations.completecash.net"]
-    cc_url = "https://locations.completecash.net/api/5c1bc42410e9b07c77f0fab5/locations-details"
-    base_url = "https://locations.completecash.net/"
-
-    start_urls = (cc_url,)
-
-    def get_opening_hours(self, days):
-        o = OpeningHours()
-        for day, hours in days.items():
-            short_day = day[:2]
-            if hours:
-                hours = hours[0]
-                o.add_range(short_day, hours[0], hours[1])
-        return o.as_opening_hours()
-
-    def parse_location(self, location):
-        properties = location["properties"]
-        opening_hours = self.get_opening_hours(properties["hoursOfOperation"])
-        coordinates = location["geometry"]["coordinates"]
-
-        props = {
-            "addr_full": properties["addressLine1"] + "\n" + properties["addressLine2"],
-            "lat": coordinates[1],
-            "lon": coordinates[0],
-            "city": properties["city"],
-            "postcode": properties["postalCode"],
-            "state": properties["province"],
-            "phone": properties["phoneNumber"],
-            "ref": self.base_url + properties["slug"],
-            "website": self.base_url + properties["slug"],
-            "opening_hours": opening_hours,
-        }
-        return GeojsonPointItem(**props)
-
-    def parse(self, response):
-        locations = json.loads(response.text)["features"]
-        for location in locations:
-            yield self.parse_location(location)
+    sitemap_urls = ["https://locations.completecash.net/sitemap.xml"]
+    sitemap_rules = [("", "parse_sd")]
+    wanted_types = ["Place"]
