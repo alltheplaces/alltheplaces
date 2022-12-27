c141a9dbb02fba63c105a1c4e594501cd12de472
Abdessalem BENSIDI AISSA <119605917+abbensid2@users.noreply.github.com>
2022-12-27 18:04:36 +0100

    Fix and update spider circle_k (#4438)
    * Fix and update spider circle_k
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/circle_k.py
+++ locations/spiders/circle_k.py
@@ -1,84 +1,30 @@
-import re
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-import scrapy
-
-from locations.items import GeojsonPointItem
-
-WIKIBRANDS = {
-    "Circle K": "Q3268010",
-    "Corner Store": "Q16959310",
-    "Couche-Tard": "Q2836957",
-    "Flash Foods": "Q16959310",
-    "Kangaroo Express": "Q61747408",
-    "Mac's": "Q4043527",
-    "On The Run": "Q16931259",
-}
+from locations.hours import OpeningHours
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CircleKSpider(scrapy.Spider):
-
+class CircleKSpider(CrawlSpider, StructuredDataSpider):
     name = "circle_k"
     item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010"}
     allowed_domains = ["www.circlek.com"]
+    start_urls = ["https://www.circlek.com/list-united-states-stores?lang=en"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+    rules = [Rule(LinkExtractor(allow="/store-locator/"), callback="parse_sd")]
+    wanted_types = ["ConvenienceStore"]
 
-    start_urls = (
-        "https://www.circlek.com/stores_new.php?lat=30.352998399999997&lng=-97.79609599999999&distance=80000&services=&region=global",
-    )
-
-    def parse(self, response):
-        results = response.json()
-
-        for storeid, store in results["stores"].items():
-            services = store["services"]
-
-            properties = {
-                "ref": store["cost_center"],
-                "name": store["display_brand"],
-                "addr_full": store["address"],
-                "city": store["city"],
-                "country": store["country"],
-                "lat": re.sub(r"[^\d\-\.]", "", store["latitude"]),
-                "lon": re.sub(r"[^\d\-\.]", "", store["longitude"]),
-                "website": f"https://www.circlek.com{store['url']}" if store["url"] else None,
-                "brand": store["display_brand"],
-                "brand_wikidata": WIKIBRANDS.get(store["display_brand"], WIKIBRANDS["Circle K"]),
-                "extras": {
-                    "amenity:fuel": any("gas" == s["name"] for s in services) or None,
-                    "fuel:diesel": any("diesel" == s["name"] for s in services) or None,
-                    "car_wash": any("car_wash" == s["name"] for s in services) or None,
-                    "shop": "convenience",
-                },
-            }
-
-            yield scrapy.Request(
-                url=response.urljoin(store["url"]),
-                callback=self.parse_state,
-                meta={"properties": properties},
+    def inspect_item(self, item, response):
+        days = response.xpath('//div[contains(@class,"hours-item")]')
+        oh = OpeningHours()
+        for day in days:
+            hours = day.xpath("./span[2]/text()").get().strip("\n").strip()
+            oh.add_range(
+                day=day.xpath("./span[1]/text()").get().strip(),
+                open_time="12:00 AM" if hours == "Open 24h" else hours.split(" to ")[0],
+                close_time="12:00 AM" if hours == "Open 24h" else hours.split(" to ")[1],
+                time_format="%I:%M %p",
             )
+        item["opening_hours"] = oh.as_opening_hours()
 
-    def parse_state(self, response):
-        properties = response.meta["properties"]
-        # Not all countries/store pages follow the same format
-        if properties["country"] == "US":
-            state = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
-            postal = response.xpath('//*[@class="heading-small"]/span[3]/text()').extract_first()
-        elif properties["country"] in ["CA", "Canada"]:
-            extracted = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
-            if extracted and len(extracted) < 4:
-                state = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
-                postal = response.xpath('//*[@class="heading-small"]/span[3]/text()').extract_first()
-            else:
-                state = ""
-                postal = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
-        else:
-            state = ""
-            postal = response.xpath('//*[@class="heading-small"]/span[2]/text()').extract_first()
-
-        properties.update(
-            {
-                "state": state,
-                "postcode": postal,
-            }
-        )
-
-        yield GeojsonPointItem(**properties)
+        yield item
