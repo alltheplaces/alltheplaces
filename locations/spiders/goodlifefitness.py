fa05db877cedf7750e8dff824130d2dfa9ca21fe
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-27 22:41:25 +0100

    Fix and update spider goodlifefitness

--- locations/spiders/goodlifefitness.py
+++ locations/spiders/goodlifefitness.py
@@ -1,36 +1,36 @@
+import re
 import scrapy
 
-from locations.items import GeojsonPointItem
+from scrapy.spiders import SitemapSpider
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours, DAYS
 
 
-class GoodLifeFitnessSpider(scrapy.Spider):
+class GoodLifeFitnessSpider(SitemapSpider):
     name = "goodlifefitness"
     item_attributes = {"brand": "GoodLife Fitness", "brand_wikidata": "Q3110654"}
-    allowed_domains = ["www.goodlifefitness.com"]
-    start_urls = ("https://www.goodlifefitness.com/locations",)
-
-    def start_requests(self):
-        template = "https://www.goodlifefitness.com/api/club/filterclubsforcitypage"
-
-        headers = {
-            "Accept": "application/json",
-        }
-
-        yield scrapy.http.FormRequest(url=template, method="GET", headers=headers, callback=self.parse)
+    allowed_domains = ["goodlifefitness.com"]
+    sitemap_urls = ["https://www.goodlifefitness.com/sitemap.xml"]
+    sitemap_rules = [(r"/clubs/club.", "parse")]
 
     def parse(self, response):
-        store_data = response.json()
-        for store in store_data:
-            properties = {
-                "ref": store["ClubNo"],
-                "name": store["ClubName"],
-                "addr_full": store["Address1"],
-                "city": store["City"],
-                "state": store["Province"],
-                "postcode": store["PostalCode"],
-                "phone": store["Phone"],
-                "lat": float(store["Lat"]),
-                "lon": float(store["Long"]),
-            }
+        template = "https://www.goodlifefitness.com/content/experience-fragments/goodlife/header/master/jcr:content/root/responsivegrid/header.GetClubsWithDetails.{}.false.true.20221127.json"
+        url_id = re.findall("[0-9]+", response.url)[0]
+        yield scrapy.Request(url=template.format(url_id), callback=self.parse_club)
 
-            yield GeojsonPointItem(**properties)
+    def parse_club(self, response):
+        data = response.json().get("map", {}).get("response")[0]
+        item = DictParser.parse(data)
+        item["name"] = data.get("ClubName")
+        item["website"] = data.get("pagePath")
+        item["ref"] = data.get("ClubNumber")
+        oh = OpeningHours()
+        for day in data.get("OperatingHours"):
+            oh.add_range(
+                day=DAYS[day.get("Day")],
+                open_time=day.get("StartTime").strip("Z")[-8:][:5],
+                close_time=day.get("EndTime").strip("Z")[-8:][:5],
+            )
+        item["opening_hours"] = oh.as_opening_hours()
+
+        yield item
