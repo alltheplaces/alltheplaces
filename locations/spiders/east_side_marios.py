8ca1041c6322bbb60e844311248b93b6be021acc
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-27 14:07:30 +0100

    Fix and update spider east_side_marios

--- locations/spiders/east_side_marios.py
+++ locations/spiders/east_side_marios.py
@@ -1,40 +1,14 @@
-import scrapy
+from scrapy.spiders import SitemapSpider
 
-from locations.items import GeojsonPointItem
+from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROSWER_DEFAULT
 
 
-class EastSideMariosSpider(scrapy.Spider):
+class EastSideMariosSpider(SitemapSpider, StructuredDataSpider):
     name = "east_side_marios"
     item_attributes = {"brand": "East Side Mario's"}
     allowed_domains = ["eastsidemarios.com"]
-    start_urls = [
-        "https://iosapi.eastsidemarios.com/CaraAPI/servlet/VESBCmdServlet?application=VECOMV1&service=AuthenticationService&command=createSession&reqJSON=%7B%22request%22%3A%7B%22requestHeader%22%3A%7B%22caller%22%3A%22Mobile%22%7D%2C%22requestContent%22%3A%7B%22@class%22%3A%22createSessionRqstModel%22%2C%22lang%22%3A%22en%22%2C%22version%22%3A%221.0.1%22%2C%22appType%22%3A%22web%22%7D%7D%7D"
-    ]
-
-    def parse(self, response):
-        base_url = "https://iosapi.eastsidemarios.com/CaraAPI/servlet/VESBCmdServlet?application=VECOMV1&service=OrganizationService&command=getStoreList&reqJSON=%7B%22request%22%3A%7B%22requestHeader%22%3A%7B%22caller%22%3A%22Mobile%22%2C%22sessionId%22%3A%22{session_id}%22%7D%2C%22requestContent%22%3A%7B%22@class%22%3A%22storeListRqstModel%22%2C%22eCommOnly%22%3A%22N%22%2C%22fromLatitude%22%3A90.000%2C%22toLatitude%22%3A0.0000%2C%22fromLongitude%22%3A-180.0000%2C%22toLongitude%22%3A-1.61336%7D%7D%7D"
-
-        info = response.json()
-        session_id = info["response"]["responseContent"]["veSessionID"]
-
-        url = base_url.format(session_id=session_id)
-        yield scrapy.Request(url=url, callback=self.parse_places)
-
-    def parse_places(self, response):
-        places = response.json()
-
-        for place in places["response"]["responseContent"]["storeModel"]:
-            properties = {
-                "ref": place["storeNumber"],
-                "name": place["storeName"],
-                "addr_full": str(place["streetNumber"]) + " " + place["street"],
-                "city": place["city"],
-                "state": place["province"],
-                "postcode": place["postalCode"],
-                "country": "CA",
-                "lat": place["latitude"],
-                "lon": place["longitude"],
-                "phone": place["phoneNumber"],
-            }
-
-            yield GeojsonPointItem(**properties)
+    sitemap_urls = ["https://www.eastsidemarios.com/en/locations/sitemap.xml"]
+    sitemap_rules = [(r"/locations/[-\w]+/[-\w]+", "parse_sd")]
+    wanted_types = ["Restaurant"]
+    user_agent = BROSWER_DEFAULT
