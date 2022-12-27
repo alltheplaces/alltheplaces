9cbe0d14a55b0d10c80f1e8177e329c52281e4ec
Abdessalem BENSIDI AISSA <119605917+abbensid2@users.noreply.github.com>
2022-12-27 18:12:43 +0100

    Fix and update spider canadapost (#4436)
    * Fix and update spider canadapost
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/canada_post.py
+++ locations/spiders/canada_post.py
@@ -1,99 +1,67 @@
-import json
 import re
 
 import scrapy
 
+from locations.geo import point_locations
 from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
-
-DAY_MAPPING = {
-    "Sunday": "Su",
-    "Monday": "Mo",
-    "Tuesday": "Tu",
-    "Wednesday": "We",
-    "Thursday": "Th",
-    "Friday": "Fr",
-    "Saturday": "Sa",
-}
 
 
 class CanadaPostSpider(scrapy.Spider):
     name = "canadapost"
     item_attributes = {"brand": "Canada Post", "brand_wikidata": "Q1032001"}
-    allowed_domains = ["canadapost.ca"]
-    start_urls = (
-        "https://maps.googleapis.com/maps/api/js/KmlOverlayService.GetOverlays?1shttp%3A%2F%2Fwww.canadapost.ca%2Fcpo%2Fmc%2Fapp%2Fpersonal%2Ffpo%2Fgeodata%2Fcpc-outlets-en.kmz%3F14&callback=_xdc_._vjm4rb&client=gme-innovapost1&token=61596",
-    )
+    allowed_domains = ["canadapost-postescanada.ca"]
 
-    def parse(self, response):
-        pdata = json.dumps(response.text)
-        postids = re.findall('(00.+?)\\\\"]', pdata)
+    def start_requests(self):
+        url = (
+            "https://www.canadapost-postescanada.ca/information/app/fpo/personal/findpostofficelist?lat={lat}&lng={lon}"
+        )
+        for lat, lon in point_locations("ca_centroids_25mile_radius_territory.csv"):
+            yield scrapy.Request(url.format(lat=lat, lon=lon), callback=self.parse_list)
 
-        url = "https://www.canadapost.ca/cpotools/apps/fpo/personal/findPostOfficeDetail?outletId={}"
-
-        for postid in postids:
-            try:
-                yield scrapy.Request(url.format(postid), callback=self.parse_office)
-            except ValueError:
-                continue
-
-    def parse_hours(self, days, hours):
-        opening_hours = OpeningHours()
-
-        # Some listed hours do not follow a consistent scrape-able pattern, so they've been excluded.
-        for d, h in zip(days, hours):
-            d = DAY_MAPPING[d]
-            h = "".join(h).replace(" ", "")
-            if h == "-":
-                continue
-            try:
-                open_time, close_time = h.split("-")
-            except ValueError:
-                continue
-            try:
-                opening_hours.add_range(
-                    day=d,
-                    open_time=open_time,
-                    close_time=close_time,
-                    time_format="%H:%M",
-                )
-            except ValueError:
-                continue
-
-        return opening_hours.as_opening_hours()
+    def parse_list(self, response):
+        urls = response.xpath('//a[contains(@href,"outletId")]/@href')
+        for url in urls:
+            yield scrapy.Request(url=f"https://www.{self.allowed_domains[0]}{url.get()}", callback=self.parse_office)
 
     def parse_office(self, response):
-        try:
-            addr_parts = response.xpath('//*[@id="results"]/div/div[1]/address/p[1]/text()').extract()
-            addr_parts = list(filter(None, [line.strip().replace("\xa0", " ") for line in addr_parts]))
-            city_state = addr_parts.pop()
-            name = addr_parts.pop(0)
-            addr = " ".join(addr_parts)
-            match = re.search(r"([\w\s]+)\s([A-Z]{2})\s+(\w{3}\s\w{3})", city_state)
-            try:
-                city, state, postcode = match.groups()
-            except AttributeError:
-                return
-        except IndexError:
-            return
+
+        address = response.xpath('//div[@id="results"]//address/p[1]/text()[3]').get()
+        state = re.findall("[A-Z]{2}", address)[0]
+        postcode = re.findall("[0-9]{5}|[A-Z0-9]{3} [A-Z0-9]{3}", address)[0]
+        city = address.replace(state, "").replace(postcode, "").strip().replace(",", "")
+
+        oh = OpeningHours()
+        days = response.xpath('//div[@id="hoursOperation"]//tr')
+        for day in days:
+            if day.xpath("./td[2]/text()").get() == " - ":
+                continue
+            hours = day.xpath("./td[2]/text()").get()
+            if len(re.findall("-", hours)) == 1:
+                oh.add_range(
+                    day=day.xpath("./td[1]/text()").get(),
+                    open_time=hours.split(" - ")[0],
+                    close_time=hours.split(" - ")[1],
+                )
+                continue
+            for hour in hours.split(" - "):
+                oh.add_range(
+                    day=day.xpath("./td[1]/text()").get(),
+                    open_time=hour.split("-")[0],
+                    close_time=hour.split("-")[1],
+                )
 
         properties = {
-            "ref": response.xpath('//*[@id="results"]/div/div[1]/address/p[1]/text()[1]').extract_first(),
-            "name": name,
-            "addr_full": addr,
+            "ref": re.findall("[0-9]+", response.url)[0],
+            "name": response.xpath('//div[@id="results"]//address/p[1]/text()[1]').get(),
+            "street_address": response.xpath('//div[@id="results"]//address/p[1]/text()[2]').get().strip("\n").strip(),
             "city": city,
             "state": state,
             "postcode": postcode,
-            "lat": response.xpath('//*[@id="fpoDetailForm:latitude"]/@value').extract_first(),
-            "lon": response.xpath('//*[@id="fpoDetailForm:longitude"]/@value').extract_first(),
-            "website": response.xpath('//*[@id="selectme"]/text()').extract_first(),
+            "lat": response.xpath('//input[@id="fpoDetailForm:latitude"]/@value').get(),
+            "lon": response.xpath('//input[@id="fpoDetailForm:longitude"]/@value').get(),
+            "website": response.url,
+            "opening_hours": oh.as_opening_hours(),
         }
-        hours = self.parse_hours(
-            days=response.xpath("//table//tbody//tr//td[1]/text()").extract(),
-            hours=response.xpath("//table//tbody//tr//td[2]/text()").extract(),
-        )
-
-        if hours:
-            properties["opening_hours"] = hours
 
         yield GeojsonPointItem(**properties)
