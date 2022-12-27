d3529d0149cfca388f1326aad392028c4c61550b
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-27 12:18:47 +0100

    Fix and update spider duluthtrading

--- locations/spiders/duluthtrading.py
+++ locations/spiders/duluthtrading.py
@@ -1,33 +1,38 @@
 import json
+import re
 
 import scrapy
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
 
-from locations.items import GeojsonPointItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class DuluthTradingSpider(scrapy.Spider):
+class DuluthTradingSpider(CrawlSpider):
     name = "duluthtrading"
     item_attributes = {"brand": "Duluth Trading", "brand_wikidata": "Q48977107"}
     allowed_domains = ["duluthtrading.com"]
-    start_urls = ["https://www.duluthtrading.com/find-stores/?brand=duluth"]
+    start_urls = ["https://www.duluthtrading.com/our-stores"]
+    rules = [Rule(LinkExtractor(allow="/locations/"), callback="parse", follow=True)]
 
     def parse(self, response):
-        locations = json.loads(response.xpath("//div/@data-storejson").extract_first())
-        for data in locations:
-            street = data.get("address1")
-            city = data.get("city")
-            ref = data.get("id")
-            properties = {
-                "name": data.get("name"),
-                "ref": ref,
-                "street": street,
-                "city": city,
-                "postcode": data.get("postalCode"),
-                "state": data.get("stateCode"),
-                "country": data.get("countryCode"),
-                "phone": data.get("phone"),
-                "website": f"https://www.duluthtrading.com/locations/?StoreID={ref}&street-address={street}&city={city}",
-                "lat": float(data.get("latitude", 0)),
-                "lon": float(data.get("longitude", 0)),
-            }
-            yield GeojsonPointItem(**properties)
+        url = "https://www.duluthtrading.com/mobify/proxy/ocapi/s/DTC/api/store/detail/?id={}"
+        id_store = re.findall("[0-9]+", response.url)[0]
+        yield scrapy.Request(url=url.format(id_store), method="POST", callback=self.parse_store)
+
+    def parse_store(self, response):
+        item = DictParser.parse(response.json())
+        days = json.loads(response.json().get("storeHoursData").replace("\n", "").replace("\t", "")).get("storeHours")
+        oh = OpeningHours()
+        for day in days:
+            if day.get("day") == "Holidays":
+                continue
+            oh.add_range(
+                day=day.get("day"),
+                open_time=day.get("hours24format")[0],
+                close_time=day.get("hours24format")[1],
+            )
+        item["opening_hours"] = oh.as_opening_hours()
+
+        yield item
