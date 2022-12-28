a5fc79f0bf076b9eeabb6032d03b7b820c19211d
Cj Malone <Cj-Malone@users.noreply.github.com>
2022-12-28 15:51:50 +0000

    Collect mulitiple Guzman y Gomez opening times (#4466)
    * Support multiple times
    
    * Collect extra details

--- locations/spiders/guzmany_gomez.py
+++ locations/spiders/guzmany_gomez.py
@@ -2,26 +2,31 @@ import scrapy
 
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
+from locations.spiders.costacoffee_gb import yes_or_no
 
 
 class GuzmanyGomezSpider(scrapy.Spider):
     name = "guzmany_gomez"
-    item_attributes = {"brand": "Guzman Y Gomez", "brand_wikidata": "Q23019759"}
-    allowed_domains = ["guzmanygomez.com.au"]
+    item_attributes = {"brand": "Guzman y Gomez", "brand_wikidata": "Q23019759"}
     start_urls = ["https://api-external.prod.apps.gyg.com.au/prod/store"]
 
     def parse(self, response):
         for data in response.json():
+            data["street_address"] = ", ".join(filter(None, [data["address1"], data["address2"]]))
             item = DictParser.parse(data)
             oh = OpeningHours()
-            for day in data.get("tradingHours"):
-                if not day.get("timePeriods"):
-                    continue
-                oh.add_range(
-                    day=day.get("dayOfWeek"),
-                    open_time=day.get("timePeriods")[0].get("openTime"),
-                    close_time=day.get("timePeriods")[0].get("endTime"),
-                )
+            for day in data["tradingHours"]:
+                for period in day["timePeriods"]:
+                    oh.add_range(
+                        day=day["dayOfWeek"],
+                        open_time=period["openTime"],
+                        close_time=period["endTime"],
+                    )
             item["opening_hours"] = oh.as_opening_hours()
 
+            item["website"] = data["orderLink"]
+            item["extras"] = {
+                "wheelchair": yes_or_no(any("Wheelchair accessible" == t["tag"] for t in data["tags"])),
+                "sells:alcohol": yes_or_no(any("Liquor" == t["tag"] for t in data["tags"])),
+            }
             yield item
