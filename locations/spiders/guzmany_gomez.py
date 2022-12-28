23ecd6dede7e2519f8ac8e972a174427c1c54ae5
Cj Malone <CjMalone@mail.com>
2022-12-28 11:09:50 +0000

    Collect extra details

--- locations/spiders/guzmany_gomez.py
+++ locations/spiders/guzmany_gomez.py
@@ -2,11 +2,12 @@ import scrapy
 
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
+from locations.spiders.costacoffee_gb import yes_or_no
 
 
 class GuzmanyGomezSpider(scrapy.Spider):
     name = "guzmany_gomez"
-    item_attributes = {"brand": "Guzman Y Gomez", "brand_wikidata": "Q23019759"}
+    item_attributes = {"brand": "Guzman y Gomez", "brand_wikidata": "Q23019759"}
     start_urls = ["https://api-external.prod.apps.gyg.com.au/prod/store"]
 
     def parse(self, response):
@@ -24,5 +25,8 @@ class GuzmanyGomezSpider(scrapy.Spider):
             item["opening_hours"] = oh.as_opening_hours()
 
             item["website"] = data["orderLink"]
-
+            item["extras"] = {
+                "wheelchair": yes_or_no(any("Wheelchair accessible" == t["tag"] for t in data["tags"])),
+                "sells:alcohol": yes_or_no(any("Liquor" == t["tag"] for t in data["tags"])),
+            }
             yield item
