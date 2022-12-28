e34775573c23b6c4f374a8d7537bb3cfe158e8a8
Cj Malone <Cj-Malone@users.noreply.github.com>
2022-12-28 11:29:42 +0000

    Add wikidata

--- locations/spiders/haggen.py
+++ locations/spiders/haggen.py
@@ -5,7 +5,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class HaggenSpider(SitemapSpider, StructuredDataSpider):
     name = "haggen"
-    item_attributes = {"brand": "Haggen"}
+    item_attributes = {"brand": "Haggen", "brand_wikidata": "Q5638683"}
     allowed_domains = ["haggen.com"]
     sitemap_urls = ["https://local.haggen.com/sitemap.xml"]
     sitemap_rules = [("", "parse_sd")]
