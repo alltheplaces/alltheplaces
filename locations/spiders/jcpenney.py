0c16e6a44aefe35adb2c40a372e6e3d26a4b116d
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-29 14:50:30 +0100

    set StructuredDataSpider

--- locations/spiders/jcpenney.py
+++ locations/spiders/jcpenney.py
@@ -1,27 +1,18 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-from locations.linked_data_parser import LinkedDataParser
-from locations.microdata_parser import MicrodataParser
 from locations.user_agents import BROSWER_DEFAULT
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class JCPenneySpider(CrawlSpider):
+class JCPenneySpider(CrawlSpider, StructuredDataSpider):
     name = "jcpenney"
     item_attributes = {"brand": "JCPenney", "brand_wikidata": "Q920037"}
     allowed_domains = ["jcpenney.com"]
     start_urls = ["https://jcpenney.com/locations/"]
     rules = [
         Rule(LinkExtractor(allow=r"/locations/[-\w]{2}$"), follow=True),
-        Rule(LinkExtractor(allow=r"/locations/[-\w]{2}/[-\w]+/[-\w]+.html$"), follow=True, callback="parse"),
+        Rule(LinkExtractor(allow=r"/locations/[-\w]{2}/[-\w]+/[-\w]+.html$"), follow=True, callback="parse_sd"),
     ]
     user_agent = BROSWER_DEFAULT
-
-    def parse(self, response):
-        MicrodataParser.convert_to_json_ld(response.selector)
-        if not (ld_item := LinkedDataParser.find_linked_data(response, "DepartmentStore")):
-            return
-
-        item = LinkedDataParser.parse_ld(ld_item)
-        item["ref"] = item["website"] = response.url
-        yield item
+    wanted_types = ["DepartmentStore"]
