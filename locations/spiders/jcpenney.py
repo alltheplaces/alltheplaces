5613e1b85692236478364b488987fcf017786784
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2022-12-29 17:21:12 +0100

    change filter sitemap

--- locations/spiders/jcpenney.py
+++ locations/spiders/jcpenney.py
@@ -1,8 +1,8 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-from locations.user_agents import BROSWER_DEFAULT
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROSWER_DEFAULT
 
 
 class JCPenneySpider(CrawlSpider, StructuredDataSpider):
@@ -12,7 +12,11 @@ class JCPenneySpider(CrawlSpider, StructuredDataSpider):
     start_urls = ["https://jcpenney.com/locations/"]
     rules = [
         Rule(LinkExtractor(allow=r"/locations/[-\w]{2}$"), follow=True),
-        Rule(LinkExtractor(allow=r"/locations/[-\w]{2}/[-\w]+/[-\w]+.html$"), follow=True, callback="parse_sd"),
+        Rule(
+            LinkExtractor(allow=r"/locations/[-\w]{2}/[-\w]+/clothing-stores-[-\w]+.html$"),
+            follow=True,
+            callback="parse_sd",
+        ),
     ]
     user_agent = BROSWER_DEFAULT
     wanted_types = ["DepartmentStore"]
