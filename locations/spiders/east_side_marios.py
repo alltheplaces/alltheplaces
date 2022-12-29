0366af8129a654ef108743e2e80be31c4c4cb426
Cj Malone <CjMalone@mail.com>
2022-12-29 14:23:11 +0000

    Add East Side Marios brand

--- locations/spiders/east_side_marios.py
+++ locations/spiders/east_side_marios.py
@@ -6,9 +6,10 @@ from locations.user_agents import BROSWER_DEFAULT
 
 class EastSideMariosSpider(SitemapSpider, StructuredDataSpider):
     name = "east_side_marios"
-    item_attributes = {"brand": "East Side Mario's"}
+    item_attributes = {"brand": "East Side Mario's", "brand_wikidata": "Q5329375"}
     allowed_domains = ["eastsidemarios.com"]
     sitemap_urls = ["https://www.eastsidemarios.com/en/locations/sitemap.xml"]
     sitemap_rules = [(r"/locations/[-\w]+/[-\w]+", "parse_sd")]
     wanted_types = ["Restaurant"]
     user_agent = BROSWER_DEFAULT
+    requires_proxy = True
