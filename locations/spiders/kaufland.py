8c167c90a1f1e7505113e1b2d5fa3d6ee3966cf1
Cj Malone <CjMalone@mail.com>
2022-12-30 16:34:10 +0000

    Expand KauflandSpider to more countries

--- locations/spiders/kaufland.py
+++ locations/spiders/kaufland.py
@@ -1,15 +1,22 @@
 import scrapy
 
+from locations.categories import Categories
 from locations.hours import OpeningHours
 from locations.items import GeojsonPointItem
 
 
 class KauflandSpider(scrapy.Spider):
     name = "kaufland"
-    item_attributes = {"brand": "Kaufland", "brand_wikidata": "Q685967"}
+    item_attributes = {"brand": "Kaufland", "brand_wikidata": "Q685967", "extras": Categories.SHOP_SUPERMARKET.value}
     website_formats = {
-        "https://filiale.kaufland.de/.klstorefinder.json": "https://filiale.kaufland.de/service/filiale/frankfurt-oder-spitzkrug-multi-center-{}.html",
         "https://www.kaufland.bg/.klstorefinder.json": "https://www.kaufland.bg/moyat-kaufland/uslugi/filiali/{}.html",
+        "https://www.kaufland.cz/.klstorefinder.json": "https://www.kaufland.cz/aktualne/servis/prodejna/{}.html",
+        "https://www.kaufland.hr/.klstorefinder.json": "https://www.kaufland.hr/usluge/poslovnica/{}.html",
+        "https://www.kaufland.md/ro/.klstorefinder.json": "https://www.kaufland.md/ro/utile/magazin/{}.html",
+        "https://www.kaufland.pl/.klstorefinder.json": "https://www.kaufland.pl/dla-klienta/sklepy/{}.html",
+        "https://www.kaufland.ro/.klstorefinder.json": "https://www.kaufland.ro/utile/magazin/{}.html",
+        "https://www.kaufland.sk/.klstorefinder.json": "https://www.kaufland.sk/servis/predajne/{}.html",
+        "https://filiale.kaufland.de/.klstorefinder.json": "https://filiale.kaufland.de/service/filiale/frankfurt-oder-spitzkrug-multi-center-{}.html",
     }
     start_urls = website_formats.keys()
 
@@ -24,6 +31,7 @@ class KauflandSpider(scrapy.Spider):
             item["phone"] = location["p"].replace("/", "")
             item["postcode"] = location["pc"]
             item["street_address"] = location["sn"]
+            item["city"] = location["t"]
 
             oh = OpeningHours()
             for rule in location["wod"]:
