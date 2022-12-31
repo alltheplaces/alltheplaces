190f3615d87dd333e0a7907f4df301f97a89759b
Sascha Brawer <sascha@brawer.ch>
2022-12-31 17:37:22 +0100

    Count categories of harvested items (#4510)
    The `missing` counter is particularly interesting, but why not count them all.

--- locations/pipelines.py
+++ locations/pipelines.py
@@ -343,3 +343,16 @@ class ApplyNSICategoriesPipeline:
         item["extras"] = extras
 
         return item
+
+
+class CountCategoriesPipeline:
+    def process_item(self, item, spider):
+        if categories := get_category_tags(item):
+            for k, v in sorted(categories.items()):
+                spider.crawler.stats.inc_value("atp/category/%s/%s" % (k, v))
+                break
+            if len(categories) > 1:
+                spider.crawler.stats.inc_value("atp/category/multiple")
+        else:
+            spider.crawler.stats.inc_value("atp/category/missing")
+        return item
