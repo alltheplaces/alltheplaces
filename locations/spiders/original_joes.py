fa548d59eb89aca773154b80a6db4388889f0bff
Abdessalem BENSIDI AISSA <abdessalem@echo-analytics.io>
2023-01-02 20:33:03 +0100

    Fix and update spider original_joes (#4543)
    Co-authored-by: abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>

--- locations/spiders/original_joes.py
+++ locations/spiders/original_joes.py
@@ -1,50 +1,21 @@
-import json
-import re
+from scrapy.spiders import SitemapSpider
 
-import scrapy
-
-from locations.items import GeojsonPointItem
+from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROSWER_DEFAULT
 
 
-class OriginalJoesSpider(scrapy.Spider):
+class OriginalJoesSpider(SitemapSpider, StructuredDataSpider):
     name = "original_joes"
     item_attributes = {"brand": "Original Joes"}
     allowed_domains = ["originaljoes.ca"]
-    start_urls = [
-        "https://www.originaljoes.ca/",
-    ]
-
-    def parse(self, response):
-        data = response.xpath('//script[contains(text(),"ojlocations")]/text()').extract_first()
-
-        ## Fix lots of formatting issues
-        json_data = data.replace("var ojlocations = ", "")
-        json_data = re.sub(r'"hours":\s{((?s).*?)},', "", json_data)
-        json_data = re.sub(r"var currentLocation\s=\s{((?s).*?)};", "", json_data)
-        json_data = json_data.replace(";", "")
-        json_data = json_data[:-8]
-        json_data = json_data + "}"
-
-        places = json.loads(json_data)
-
-        for place in places:
-            try:
-                coords = places[place]["geo"]
-                lat, lng = coords.split(",")
-
-                properties = {
-                    "ref": place,
-                    "name": "Original Joe's",
-                    "addr_full": places[place]["streetAddress"],
-                    "city": places[place]["city"],
-                    "state": places[place]["province"],
-                    "postcode": places[place]["postalCode"],
-                    "country": "CA",
-                    "lat": lat,
-                    "lon": lng,
-                    "phone": places[place]["telephone"],
-                }
-
-                yield GeojsonPointItem(**properties)
-            except:
-                pass
+    sitemap_urls = ["https://www.originaljoes.ca/en/locations/sitemap.xml"]
+    sitemap_rules = [(r"/[0-9]+.", "parse_sd")]
+    wanted_types = ["Restaurant"]
+    custom_settings = {
+        "DEFAULT_REQUEST_HEADERS": {
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept": "*/*",
+            "User-Agent": BROSWER_DEFAULT,
+            "Connection": "keep-alive",
+        }
+    }
