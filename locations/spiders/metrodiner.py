a620e5f794bf68312b46ffd501463b8ded44b209
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-01-02 16:33:53 +0000

    Utilise LinkedData opening hours parser (#4536)

--- locations/spiders/metrodiner.py
+++ locations/spiders/metrodiner.py
@@ -1,5 +1,3 @@
-import json
-
 from scrapy.spiders import SitemapSpider
 
 from locations.google_url import extract_google_position
@@ -11,25 +9,14 @@ class MetroDinerSpider(SitemapSpider, StructuredDataSpider):
     name = "metrodiner"
     item_attributes = {"brand": "Metro Diner", "brand_wikidata": "Q104870732"}
     allowed_domains = ["metrodiner.com"]
-    sitemap_urls = [
-        "https://metrodiner.com/locations-sitemap.xml",
-    ]
-    sitemap_rules = [("", "parse_sd")]
+    sitemap_urls = ["https://metrodiner.com/locations-sitemap.xml"]
     wanted_types = ["Restaurant"]
 
     def post_process_item(self, item, response, ld_data):
-        ldjson = json.loads(
-            response.xpath('//script[@type="application/ld+json"]/text()[contains(.,\'"Restaurant"\')]').get()
-        )
         oh = OpeningHours()
-        for day in ldjson.get("openingHoursSpecification"):
-            oh.add_range(
-                day=day.get("dayOfWeek").replace("https://schema.org/", ""),
-                open_time=day.get("opens")[:5],
-                close_time=day.get("closes")[:5],
-            )
-
+        oh.from_linked_data(ld_data, time_format="%H:%M:%S")
         item["opening_hours"] = oh.as_opening_hours()
+
         extract_google_position(item, response)
 
         yield item
