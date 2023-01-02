57295e6b552769ccb6eead2ec2b80189d16d5b54
Abdessalem BENSIDI AISSA <abdessalem@echo-analytics.io>
2023-01-02 05:00:06 +0100

    Fix and update spider habit_burger (#4462)
    * Fix and update spider habit_burger
    
    * change inspect_item to post_process_item
    
    * use from_linked_data to handle opening_hours
    
    Co-authored-by: abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>

--- locations/spiders/habit_burger.py
+++ locations/spiders/habit_burger.py
@@ -1,80 +1,28 @@
-import scrapy
-from scrapy.selector import Selector
+import json
 
-from locations.items import GeojsonPointItem
+from scrapy.spiders import SitemapSpider
+
+from locations.hours import OpeningHours
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class HabitBurgerSpider(scrapy.Spider):
+class HabitBurgerSpider(SitemapSpider, StructuredDataSpider):
     name = "habit-burger"
     allowed_domains = ["habitburger.com"]
-    start_urls = ("https://www.habitburger.com/locations/all/",)
+    sitemap_urls = ["https://www.habitburger.com/locations-sitemap.xml"]
+    sitemap_rules = [("", "parse_sd")]
+    wanted_types = ["Restaurant"]
 
-    def parse(self, response):
-        regions = response.xpath("//h2").xpath("@id").extract()
-        non_us_cities = ["phnom penh", "shanghai", "hangzhou"]
+    def post_process_item(self, item, response, ld_data):
+        openingHour = json.loads(response.xpath('//*[@id="content"]/script[1]/text()').get())
+        openingHour = openingHour.get("openingHours")[0].split()
+        days = [openingHour[i] for i in range(0, len(openingHour), 2)]
+        hours = [openingHour[i] for i in range(1, len(openingHour), 2)]
+        openhours = []
+        for i in range(len(openingHour) // 2):
+            openhours.extend(f"{day} {hours[i]}" for day in days[i].split(","))
+        oh = OpeningHours()
+        oh.from_linked_data({"openingHours": openhours}, "%I:%M%p")
+        item["opening_hours"] = oh.as_opening_hours()
 
-        for region in regions:
-            region_body = response.xpath(
-                '//h2[@id = "{region}"]/following-sibling::ul[@class = "reglist"]'.format(region=region)
-            ).get()
-            pois = Selector(text=region_body).css(".loc").extract()
-
-            for poi in pois:
-                ref = Selector(text=poi).xpath("//a/@href").extract()[0].split("/")[-1]
-                name = Selector(text=poi).xpath("//h4/text()").extract()
-                if name == []:
-                    name = Selector(text=poi).xpath("//h3/text()").extract()
-                name = "".join(name)
-
-                map_link = Selector(text=poi).xpath('//div[@class = "locaddress"]/a').xpath("@href").extract_first()
-                lat, long = None, None
-                if "daddr" in map_link:
-                    coords = map_link.split("daddr=")[1].split(",")
-                    lat = coords[0]
-                    long = coords[1]
-
-                addr = Selector(text=poi).xpath('//div[@class = "locaddress"]/a').extract_first()
-                addr = Selector(text=addr).xpath("//a/text()").extract()
-                addr = [a.strip() for a in addr]
-
-                addr_full = ", ".join(addr)
-                street = ", ".join(addr[:-1])
-                city, state, postcode = None, None, None
-
-                if region in ["cambodia", "china"]:
-                    for c in non_us_cities:
-                        if c in poi.lower():
-                            city = c.capitalize()
-                    country = region.capitalize()
-                else:
-                    city = addr[-1].split(", ")[0]
-                    state_postcode = addr[-1].split(", ")[1].split(" ")
-                    if len(state_postcode) > 1:
-                        state = state_postcode[0]
-                        postcode = state_postcode[1]
-                    country = "US"
-
-                phone = Selector(text=poi).xpath('//div[@class="locinfo"]/text()').get()
-                phone = phone.strip() if phone else None
-                opening_hours = Selector(text=poi).xpath('//div[@class="lochrs"]/text()').extract()
-                opening_hours = opening_hours = (
-                    ", ".join([hours.strip() for hours in opening_hours]) if opening_hours else None
-                )
-
-                properties = {
-                    "ref": ref,
-                    "website": "https://www.habitburger.com/locations/" + ref,
-                    "name": name,
-                    "addr_full": addr_full,
-                    "street": street,
-                    "city": city,
-                    "state": state,
-                    "postcode": postcode,
-                    "country": country,
-                    "phone": phone,
-                    "opening_hours": opening_hours,
-                    "lat": lat,
-                    "lon": long,
-                }
-
-                yield GeojsonPointItem(**properties)
+        yield item
