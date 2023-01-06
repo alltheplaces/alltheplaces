aa31d9af36bd252b13a098cf4e1c86b4630c0683
Romeo-PHILLIPS <92714362+Romeo-PHILLIPS@users.noreply.github.com>
2023-01-06 16:42:46 +0100

    feat: Franprix (#4170)
    * feat: Franprix
    
    * fix: country rename
    
    * Flag requires_proxy
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Cj Malone <CjMalone@mail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/franprix_fr.py
@@ -0,0 +1,43 @@
+from scrapy import Spider
+
+from locations.hours import OpeningHours
+from locations.items import GeojsonPointItem
+
+
+class FranprixFRSpider(Spider):
+    name = "franprix_fr"
+    item_attributes = {"brand": "Franprix", "brand_wikidata": "Q2420096"}
+    start_urls = ["https://www.franprix.fr/xhr-cache/resource/stores"]
+    requires_proxy = True
+
+    def parse(self, response):
+        data = response.json()
+        store_data = data.get("list")
+
+        for store in store_data:
+            oh = OpeningHours()
+            for day in store.get("hours"):
+                day_str = day.get("day").title()[:2]
+                opening_hours = list(day.get("hours"))
+                if not opening_hours:
+                    continue
+                formatted_hours = opening_hours[0].lstrip("T").split("/")
+                oh.add_range(
+                    day=day_str,
+                    open_time=formatted_hours[0],
+                    close_time=formatted_hours[1],
+                    time_format="%H:%M",
+                )
+            properties = {
+                "ref": store.get("id"),
+                "name": f"Franprix - {store.get('store_name')}" if store.get("store_name") else "Franprix",
+                "addr_full": store.get("street"),
+                "city": store.get("city"),
+                "phone": store.get("phone"),
+                "postcode": store.get("postcode"),
+                "lat": store.get("lat"),
+                "lon": store.get("lon"),
+                "website": f"https://www.franprix.fr/magasins/{store.get('id')}",
+                "opening_hours": oh.as_opening_hours(),
+            }
+            yield GeojsonPointItem(**properties)
