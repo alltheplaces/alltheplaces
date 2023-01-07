cb1091d117973b65bdd2ba7bf59efae027f0f2b1
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2023-01-07 19:19:49 +0100

    add dhl_express_canada

---
+++ locations/spiders/dhl_express_us_ca.py
@@ -0,0 +1,24 @@
+import re
+
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class DhlExpressUsSpider(SitemapSpider, StructuredDataSpider):
+    name = "dhl_express_us_ca"
+    item_attributes = {
+        "brand": "DHL",
+        "brand_wikidata": "Q489815",
+    }
+    sitemap_urls = [
+        "https://locations.us.express.dhl.com/sitemap.xml",
+        "https://locations.ca.express.dhl.com/sitemap.xml",
+    ]
+    sitemap_rules = [(r"/[0-9]+", "parse_sd")]
+    wanted_types = ["Store"]
+
+    def post_process_item(self, item, response, ld_data):
+        item["country"] = re.findall(r"\.ca|\.us", response.url)[0][1:].upper()
+
+        yield item
