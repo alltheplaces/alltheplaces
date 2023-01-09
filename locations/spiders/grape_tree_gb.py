c4383d5e289129991b30284e6aa4d5aaaaff81e3
Cj Malone <CjMalone@mail.com>
2023-01-09 15:40:52 +0000

    Add Grape Tree Spider

---
+++ locations/spiders/grape_tree_gb.py
@@ -0,0 +1,7 @@
+from locations.storefinders.uberall import UberallSpider
+
+
+class GrapeTreeGBSpider(UberallSpider):
+    name = "grape_tree_gb"
+    item_attributes = {"brand": "Grape Tree", "brand_wikidata": "Q87071497"}
+    key = "N2WuycvqRWwy4Hd0bYpVzqN7jllSkA"
