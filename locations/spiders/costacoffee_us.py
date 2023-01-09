061b7a8d18d724062c5eae4b27d806485d1ea6fa
jancallewaert <jan.callewaert@gmail.com>
2023-01-09 15:39:50 +0100

    Fix Costa Coffee US (532 places) (#4616)
    * Fix Costa Coffee US
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/costacoffee_us.py
+++ locations/spiders/costacoffee_us.py
@@ -1,8 +1,10 @@
+import json
 import re
 
 import scrapy
 from scrapy import Selector
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 
@@ -13,7 +15,15 @@ class CostaCoffeeUSSpider(scrapy.Spider):
     start_urls = ["https://us.costacoffee.com/amlocator/index/ajax"]
 
     def parse(self, response):
-        for store in response.json()["items"]:
+        script = response.xpath('//script[contains(text(), "amLocator")]/text()').extract_first()
+
+        start = script.index("jsonLocations: ") + len("jsonLocations: ")
+        stop = script.index("imageLocations")
+
+        locations = script[start:stop].strip().strip(",")
+        items = json.loads(locations)["items"]
+
+        for store in items:
             item = Feature()
             item["ref"] = store["id"]
             item["lat"] = store["lat"]
@@ -33,5 +43,7 @@ class CostaCoffeeUSSpider(scrapy.Spider):
                     item["street_address"] = m.group(1)
                 elif m := re.match(r"State: (.*)", line):
                     item["state"] = m.group(1)
+
+            apply_category(Categories.COFFEE_SHOP, item)
 
             yield item
