b8e6b8b4d19cf0da5828f8a96b7913d6d277ccdf
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-01-10 15:01:17 +0000

    Add categories (#4627)

--- locations/spiders/circle_k.py
+++ locations/spiders/circle_k.py
@@ -1,20 +1,23 @@
+import html
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class CircleKSpider(CrawlSpider, StructuredDataSpider):
     name = "circle_k"
-    item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010"}
+    item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010", "country": "US"}
     allowed_domains = ["www.circlek.com"]
     start_urls = ["https://www.circlek.com/list-united-states-stores?lang=en"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
     rules = [Rule(LinkExtractor(allow="/store-locator/"), callback="parse_sd")]
     wanted_types = ["ConvenienceStore"]
 
-    def inspect_item(self, item, response):
+    def post_process_item(self, item, response, ld_data, **kwargs):
         days = response.xpath('//div[contains(@class,"hours-item")]')
         oh = OpeningHours()
         for day in days:
@@ -26,5 +29,12 @@ class CircleKSpider(CrawlSpider, StructuredDataSpider):
                 time_format="%I:%M %p",
             )
         item["opening_hours"] = oh.as_opening_hours()
+
+        item["name"] = html.unescape(item["name"])
+
+        if "Convenience Store" in item["name"]:
+            apply_category(Categories.SHOP_CONVENIENCE, item)
+        if "Gas Station" in item["name"]:
+            apply_category(Categories.FUEL_STATION, item)
 
         yield item
