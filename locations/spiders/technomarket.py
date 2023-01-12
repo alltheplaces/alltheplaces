f95675b6df1f6a99c806afd1c5f09ad02c9459f1
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-01-12 19:59:50 +0200

    Add Technomarket spider
    Solves #4507

---
+++ locations/spiders/technomarket.py
@@ -0,0 +1,44 @@
+import scrapy
+
+from locations.hours import OpeningHours
+from locations.items import Feature
+
+DAY_MAPPING = {
+    0: "Mo-Fr",
+    1: "Sa",
+    2: "Su"
+}
+
+
+class TechnomarketSpider(scrapy.Spider):
+    name = "Техномаркет"
+    item_attributes = {"brand": "Техномаркет", "brand_wikidata": "Q7692633", "country": "BG"}
+    allowed_domains = ["www.technomarket.bg"]
+    start_urls = ["https://www.technomarket.bg/api/frontend/stores"]
+
+    def parse(self, response):
+        data = response.json()
+
+        for store in data["response"]:
+            item = Feature()
+            item["ref"] = store["code"]
+            item["name"] = store["name"]
+            item["addr_full"] = store["address"].strip()
+            item["lat"] = store["latLng"]["lat"]
+            item["lon"] = store["latLng"]["lng"]
+            item["phone"] = store["tel"]
+            item["email"] = store["email"]
+            item["url"] = "https://www.technomarket.bg/" + store["url"]["city"] + "/" + store["url"]["store"]
+
+            oh = OpeningHours()
+            for index, day in store["wh"]:
+                oh.add_range(
+                    DAY_MAPPING[index],
+                    day.split(' до ')[0],
+                    day.split(' до ')[1],
+                    "%H:%M",
+                )
+
+            item["opening_hours"] = oh.as_opening_hours()
+
+            yield item
