0d40b6caab47846bad2c3b3145efcbfd3af029a7
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-01-12 15:22:45 +0000

    Reuse linked data dict (#4541)
    * Reuse linked data dict
    
    * Try minus sign to allow non-zero-padding hour
    
    * That didn't help
    
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/mccoys.py
+++ locations/spiders/mccoys.py
@@ -1,5 +1,3 @@
-import json
-
 from scrapy.spiders import SitemapSpider
 
 from locations.hours import OpeningHours
@@ -8,10 +6,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class McCoysSpider(SitemapSpider, StructuredDataSpider):
     name = "mccoys"
-    item_attributes = {
-        "brand": "McCoy's Building Supply",
-        "brand_wikidata": "Q27877295",
-    }
+    item_attributes = {"brand": "McCoy's Building Supply", "brand_wikidata": "Q27877295"}
     allowed_domains = ["www.mccoys.com"]
     sitemap_urls = ["https://www.mccoys.com/sitemap.xml"]
     sitemap_rules = [(r"/stores/[-\w]+", "parse_sd")]
@@ -19,17 +14,8 @@ class McCoysSpider(SitemapSpider, StructuredDataSpider):
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def post_process_item(self, item, response, ld_data):
-        data = json.loads(response.xpath('//script[@type="application/ld+json"]/text()[contains(.,\'"Store"\')]').get())
         oh = OpeningHours()
-        for day in data.get("OpeningHoursSpecification"):
-            if day.get("opens") == "Closed":
-                continue
-            oh.add_range(
-                day=day.get("dayOfWeek"),
-                open_time=day.get("opens"),
-                close_time=day.get("closes"),
-                time_format="%I:%M %p",
-            )
+        oh.from_linked_data(ld_data, time_format="%I:%M %p")
         item["opening_hours"] = oh.as_opening_hours()
 
         yield item
