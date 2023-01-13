c0c67f1a9e7d1e5b90d72bbc403a8bac9e5d6da4
Ian Dees <ian.dees@gmail.com>
2023-01-12 21:47:39 -0600

    Try cleaning off whitespace after split

--- locations/spiders/technomarket.py
+++ locations/spiders/technomarket.py
@@ -26,14 +26,14 @@ class TechnomarketSpider(scrapy.Spider):
 
             oh = OpeningHours()
             for day in DAYS[0:5]:
-                from_str, to_str = store["wh"][0].split(" до ")
-                oh.add_range(day, from_str, to_str, "%H:%M")
+                from_str, to_str = store["wh"][0].split("до")
+                oh.add_range(day, from_str.strip(), to_str.strip(), "%H:%M")
             for day in DAYS[5:6]:
-                from_str, to_str = store["wh"][1].split(" до ")
-                oh.add_range(day, from_str, to_str, "%H:%M")
+                from_str, to_str = store["wh"][1].split("до")
+                oh.add_range(day, from_str.strip(), to_str.strip(), "%H:%M")
             for day in DAYS[6:7]:
-                from_str, to_str = store["wh"][2].split(" до ")
-                oh.add_range(day, from_str, to_str, "%H:%M")
+                from_str, to_str = store["wh"][2].split("до")
+                oh.add_range(day, from_str.strip(), to_str.strip(), "%H:%M")
 
             item["opening_hours"] = oh.as_opening_hours()
 
