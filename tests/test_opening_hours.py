ca054e2ef1f1c941be346187a4373c47c8ac4b4b
Sascha Brawer <sascha@brawer.ch>
2023-01-13 15:22:58 +0100

    Sort same-day opening hours (#4639)
    * Sort same-day opening hours
    
    There had already been a unit test for sorting multiple opening hours
    on the same day, but the implementation had not actually sorted the hours.
    Before this change, the results thus got formatted in pseudo-random order
    depending on the value of Python’s built-in hash function. Accidentally,
    the existing test case happened to give the expected sorting order.
    
    * Add unit test that inserts opening hours in reverse order

--- tests/test_opening_hours.py
+++ tests/test_opening_hours.py
@@ -99,6 +99,16 @@ def test_multiple_times():
     o.add_range("Mo", "13:00", "17:30")
     assert o.as_opening_hours() == "Mo 08:00-12:00,13:00-17:30"
 
+    o2 = OpeningHours()
+    o2.add_range("Tu", "09:00", "12:00")
+    o2.add_range("Tu", "15:00", "17:00")
+    assert o2.as_opening_hours() == "Tu 09:00-12:00,15:00-17:00"
+
+    o3 = OpeningHours()
+    o3.add_range("Tu", "15:00", "17:00")
+    o3.add_range("Tu", "09:00", "12:00")
+    assert o3.as_opening_hours() == "Tu 09:00-12:00,15:00-17:00"
+
 
 def test_sanitise_days():
     assert sanitise_day("Mo") == "Mo"
