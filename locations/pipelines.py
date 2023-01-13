b3a54fae30ebc7f001ecad44b2de11b8cfa8e0fc
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-01-13 18:15:04 +0000

    Improve opening_hours regex to allow for multiple time ranges (#4647)
    eg "Mo 08:00-12:00,13:00-17:30"

--- locations/pipelines.py
+++ locations/pipelines.py
@@ -195,7 +195,7 @@ class CheckItemPropertiesPipeline:
         r"^Q[0-9]+$",
     )
     opening_hours_regex = re.compile(
-        r"^(?:(?:Mo|Tu|We|Th|Fr|Sa|Su)(?:-(?:Mo|Tu|We|Th|Fr|Sa|Su))? [0-9]{2}:[0-9]{2}-[0-9]{2}:[0-9]{2}(?:; )?)+$"
+        r"^(?:(?:Mo|Tu|We|Th|Fr|Sa|Su)(?:-(?:Mo|Tu|We|Th|Fr|Sa|Su))? (?:,?[0-9]{2}:[0-9]{2}-[0-9]{2}:[0-9]{2})+(?:; )?)+$"
     )
     min_lat = -90.0
     max_lat = 90.0
