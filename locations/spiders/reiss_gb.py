e3d09861bb2fd5372b4e55e04d4a2ecc7fbb06bf
Cj Malone <CjMalone@mail.com>
2023-01-15 15:53:47 +0000

    Add Reiss GB Spider

---
+++ locations/spiders/reiss_gb.py
@@ -0,0 +1,17 @@
+import re
+
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class ReissGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "reiss_gb"
+    item_attributes = {"brand": "Reiss", "brand_wikidata": "Q7310479"}
+    sitemap_urls = ["https://www.reiss.com/robots.txt"]
+    sitemap_rules = [(r"/storelocator/.+/\d+$", "parse_sd")]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if m := re.search(r"\"&daddr=\"\s*\+\s*(-?\d+\.\d+)\s*\+\s*\",\"\s*\+\s*(-?\d+\.\d+);", response.text):
+            item["lat"], item["lon"] = m.groups()
+        yield item
