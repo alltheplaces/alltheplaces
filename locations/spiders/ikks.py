364c5296f5883860d27d224b0139c478b5afcffb
abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
2023-01-16 14:48:48 +0100

    scrape all countries

--- locations/spiders/ikks.py
+++ locations/spiders/ikks.py
@@ -3,20 +3,14 @@ import re
 import scrapy
 
 from locations.dict_parser import DictParser
-from locations.geo import point_locations
 from locations.hours import DAYS_FR, OpeningHours
 
 
-class DhlExpressFrSpider(scrapy.Spider):
-    name = "ikks_fr"
+class IkksSpider(scrapy.Spider):
+    name = "ikks"
     item_attributes = {"brand": "IKKS", "brand_wikidata": "Q3146711"}
     allowed_domains = ["stores.ikks.com"]
-
-    def start_requests(self):
-        point_files = "eu_centroids_120km_radius_country.csv"
-        for lat, lon in point_locations(point_files, "FR"):
-            url = f"https://stores.ikks.com/mobify/proxy/ocapi/s/IKKS_COM/dw/shop/v21_3/stores?latitude={lat}&longitude={lon}&client_id=1c53da3a-3640-4a31-adad-0f43be6c0904&max_distance=120&count=200"
-            yield scrapy.Request(url=url)
+    start_urls = ["https://stores.ikks.com/mobify/proxy/ocapi/s/IKKS_COM/dw/shop/v21_3/stores?latitude=24.3356791&longitude=23.769953599999997&max_distance=20000&delivery_method=delivery&client_id=1c53da3a-3640-4a31-adad-0f43be6c0904&start=0&count=200"]
 
     def parse(self, response):
         if not response.json().get("count"):
@@ -41,3 +35,7 @@ class DhlExpressFrSpider(scrapy.Spider):
                 item["opening_hours"] = oh.as_opening_hours()
 
                 yield item
+
+        start = int(re.findall('start=[0-9]+', response.url)[0][6:]) + 200
+        url = f"https://stores.ikks.com/mobify/proxy/ocapi/s/IKKS_COM/dw/shop/v21_3/stores?latitude=24.3356791&longitude=23.769953599999997&max_distance=20000&delivery_method=delivery&client_id=1c53da3a-3640-4a31-adad-0f43be6c0904&start={start}&count=200"
+        yield scrapy.Request(url=url, callback=self.parse)
