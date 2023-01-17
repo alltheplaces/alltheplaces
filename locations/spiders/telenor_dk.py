7c2b36ec0e476fb7d18d87e9b67d2dba5718b2d4
Cj Malone <CjMalone@mail.com>
2023-01-17 20:30:07 +0000

    Add opening hours

--- locations/spiders/telenor_dk.py
+++ locations/spiders/telenor_dk.py
@@ -1,5 +1,6 @@
 from scrapy import Selector, Spider
 
+from locations.hours import DAYS_DK, OpeningHours, day_range, sanitise_day
 from locations.items import Feature
 
 
@@ -32,9 +33,31 @@ class TelenorDKSpider(Spider):
 
             item["phone"] = html.xpath('//p[contains(text(), "Telefon:")]/text()').get().replace("Telefon:", "")
             if slug := html.xpath('//a[contains(@href, "find-butik")]/@href').get():
-                item["ref"] = slug
-                item["website"] = f"https://www.telenor.dk{slug}"
+                item["website"] = item["ref"] = f"https://www.telenor.dk{slug}"
             if email := html.xpath('//p[contains(text(), "@telenor.dk")]/text()').get():
                 item["email"] = email.strip()
+
+            item["opening_hours"] = OpeningHours()
+            for rule in html.xpath(
+                '//div[@class="border--bottom padding-trailer"]/div[@class="margin-trailer--small"][2]/p/text()'
+            ).getall():
+                rule = rule.strip()
+                if not rule:
+                    continue
+                day, times = rule.split(": ")
+                start_time, end_time = times.split(" - ")
+                if day == "Hverdage":  # Weekdays
+                    for d in day_range("Mo", "Fr"):
+                        item["opening_hours"].add_range(d, start_time, end_time)
+                elif " - " in day:  # Day range
+                    start_day, end_day = day.split(" - ")
+                    start_day = sanitise_day(start_day, DAYS_DK)
+                    end_day = sanitise_day(end_day, DAYS_DK)
+                    if start_day and end_day:
+                        for d in day_range(start_day, end_day):
+                            item["opening_hours"].add_range(d, start_time, end_time)
+                else:  # Single day
+                    if d := sanitise_day(day, DAYS_DK):
+                        item["opening_hours"].add_range(d, start_time, end_time)
 
             yield item
