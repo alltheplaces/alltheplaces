07a08e19961466b919969489a76cdbaf3130c84a
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-01-19 00:36:19 +0000

    Fix Kroger Spider (#4663)

--- locations/pipelines.py
+++ locations/pipelines.py
@@ -373,3 +373,12 @@ class CountCategoriesPipeline:
         else:
             spider.crawler.stats.inc_value("atp/category/missing")
         return item
+
+
+class CountBrandsPipeline:
+    def process_item(self, item, spider):
+        if brand := item.get("brand"):
+            spider.crawler.stats.inc_value(f"atp/brand/{brand}")
+        if wikidata := item.get("brand_wikidata"):
+            spider.crawler.stats.inc_value(f"atp/brand_wikidata/{wikidata}")
+        return item
