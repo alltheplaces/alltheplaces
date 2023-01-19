186d1af7e50730bcbd4f90a22b9c5f82d1e62b2f
Abdessalem BENSIDI AISSA <abdessalem@echo-analytics.io>
2023-01-19 01:31:40 +0100

    Add spider for ikks (#4652)
    * Add spider for ikks_fr
    
    * update link in start_request
    
    * scrape all countries
    
    * update opening_hours
    
    * fix opening_hours
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: abdessalem bensidi aissa <abdessalem@abdessalems-MacBook-Air.local>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/ikks.py
@@ -0,0 +1,47 @@
+import json
+import re
+
+import scrapy
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_FR, OpeningHours
+
+
+class IkksSpider(scrapy.Spider):
+    name = "ikks"
+    item_attributes = {"brand": "IKKS", "brand_wikidata": "Q3146711"}
+    allowed_domains = ["stores.ikks.com"]
+    start_urls = [
+        "https://stores.ikks.com/mobify/proxy/ocapi/s/IKKS_COM/dw/shop/v21_3/stores?latitude=24.3356791&longitude=23.769953599999997&max_distance=20000&delivery_method=delivery&client_id=1c53da3a-3640-4a31-adad-0f43be6c0904&start=0&count=200"
+    ]
+
+    def parse(self, response):
+        if not response.json().get("count"):
+            return
+        for data in response.json().get("data"):
+            if "IKKS" in data.get("c_commercialSign"):
+                item = DictParser.parse(data)
+                oh = OpeningHours()
+                for day in json.loads(data.get("c_workingSchedule")):
+                    if not day.get("amBegin"):
+                        continue
+                    if day.get("amEnd") and day.get("pmBegin"):
+                        oh.add_range(
+                            day=DAYS_FR[day.get("title")[:2]], open_time=day.get("amBegin"), close_time=day.get("amEnd")
+                        )
+                        oh.add_range(
+                            day=DAYS_FR[day.get("title")[:2]], open_time=day.get("pmBegin"), close_time=day.get("pmEnd")
+                        )
+                    else:
+                        oh.add_range(
+                            day=DAYS_FR[day.get("title")[:2]],
+                            open_time=day.get("pmBegin") or day.get("amBegin"),
+                            close_time=day.get("pmEnd"),
+                        )
+                item["opening_hours"] = oh.as_opening_hours()
+
+                yield item
+
+        start = int(re.findall("start=[0-9]+", response.url)[0][6:]) + 200
+        url = f"https://stores.ikks.com/mobify/proxy/ocapi/s/IKKS_COM/dw/shop/v21_3/stores?latitude=24.3356791&longitude=23.769953599999997&max_distance=20000&delivery_method=delivery&client_id=1c53da3a-3640-4a31-adad-0f43be6c0904&start={start}&count=200"
+        yield scrapy.Request(url=url, callback=self.parse)
