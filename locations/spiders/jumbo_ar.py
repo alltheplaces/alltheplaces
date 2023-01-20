1d7bdde2da8783aac65ca300db3226499bd58a70
Jan Callewaert <72855158+JanCallewaert-TomTom@users.noreply.github.com>
2023-01-20 13:44:24 +0100

    Fix Jumbo Argentina spider (#4668)
    * Fix Jumbo Argentina spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add wikidata reference
    
    * Update wikidata
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>

--- locations/spiders/jumbo_ar.py
+++ locations/spiders/jumbo_ar.py
@@ -1,46 +1,27 @@
-import json
-
 import scrapy
 
-from locations.items import Feature
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
 
 
 class JumboARSpider(scrapy.Spider):
-    name = "jumbo_argentina"
-    item_attributes = {"brand": "Jumbo"}
-    allowed_domains = ["www.jumbo.com.ar"]
+    name = "jumbo_ar"
+    item_attributes = {"brand": "Jumbo", "brand_wikidata": "Q6310864"}
 
     def start_requests(self):
-        url = "https://www.jumbo.com.ar/Login/PreHomeService.aspx/TraerLocales"
-        headers = {
-            "Content-Type": "application/json",
-        }
-        form_data = {}
+        url = "https://www.jumbo.com.ar/api/dataentities/NT/search?_fields=name,grouping,image_maps,geocoordinates,SellerName,id,country,city,neighborhood,number,postalCode,state,street,schedule,services,paymentMethods,opening,hasPickup,hasDelivery,address,url_image,phone&an=jumboargentina"
+        headers = {"Content-Type": "application/json", "Accept": "application/json", "rest-range": "resources=0-999"}
 
-        yield scrapy.http.FormRequest(
-            url=url,
-            method="POST",
-            formdata=form_data,
-            headers=headers,
-            callback=self.parse,
-        )
+        yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def parse(self, response):
-        result = response.text.replace("\\", "")
-        result = result[:5] + result[6:-2] + result[-1:]
-        data = json.loads(result)
-        ref = 0
-        for store in data["d"]["Locales"]:
-            properties = {
-                "ref": ref,
-                "name": store["Local"]["Nombre"].strip(),
-                "addr_full": store["Local"]["Direccion"].strip(),
-                "city": store["Local"]["Localidad"].strip(),
-                "state": store["Local"]["Provincia"].strip(),
-                "postcode": store["Local"]["CodigoPostal"].strip(),
-                "lat": float(store["Local"]["Latitud"].strip()),
-                "lon": float(store["Local"]["Longitud"].strip()),
-                "phone": store["Local"]["Telefono"].strip(),
-            }
-            ref += 1
-            yield Feature(**properties)
+        for data in response.json():
+            item = DictParser.parse(data)
+
+            (lat, lon) = data["geocoordinates"].split(",")
+            item["lat"] = lat
+            item["lon"] = lon
+
+            apply_category(Categories.SHOP_SUPERMARKET, item)
+
+            yield item
