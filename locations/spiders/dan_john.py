28d595f7892f5e3b2ae8b501397ef5952a0746f2
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-01-23 15:08:01 +0000

    Add Dan John Spider (#4676)

---
+++ locations/spiders/dan_john.py
@@ -0,0 +1,20 @@
+from scrapy import Spider
+
+from locations.dict_parser import DictParser
+
+
+class DanJohnSpider(Spider):
+    name = "dan_john"
+    item_attributes = {"brand": "Dan John", "brand_wikidata": "Q116304215"}
+    start_urls = ["https://stores.danjohn.com/js_db/stores-json-en.json"]
+
+    def parse(self, response, **kwargs):
+        for location in response.json():
+            if location["storeTypeLabels"] != ["DANJOHN"]:
+                continue
+            location["street_address"] = ", ".join(filter(None, [location.pop("address1"), location.pop("address2")]))
+            location["country"] = location["country"]["tagISO31661Alpha2"]
+            item = DictParser.parse(location)
+            item["image"] = ";".join(location["miniPhotos"])
+
+            yield item
