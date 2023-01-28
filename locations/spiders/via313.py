93fc5a53035ab52d5f865dedb2b19cdac6d58782
rjw62 <rjw62@cantab.net>
2023-01-28 11:42:12 +0000

    Amend code for compatibilitywith Python 3.8

--- locations/spiders/via313.py
+++ locations/spiders/via313.py
@@ -10,7 +10,7 @@ class Via313Spider(SitemapSpider, StructuredDataSpider):
         "brand": "Via 313",
         "brand_wikidata": "Q115699944",
         "nsi_id": -1,
-        "extras": Categories.RESTAURANT.value | {"cuisine": "pizza"},
+        "extras": { **Categories.RESTAURANT.value, "cuisine": "pizza" },
     }
     sitemap_urls = ["https://locations.via313.com/robots.txt"]
     sitemap_rules = [(r"/locations/\d+-[-\w]+/", "parse_sd")]
