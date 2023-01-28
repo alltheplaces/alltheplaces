f4baff1d788098087aa2aa274a3831b5e33cbf22
Cj Malone <CjMalone@mail.com>
2023-01-28 10:02:19 +0000

    Convert Currys Spider to a StructuredDataSpider

--- locations/spiders/currys.py
+++ locations/spiders/currys.py
@@ -1,35 +1,22 @@
-import json
-
 from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CurrysSpider(SitemapSpider):
-    name = "currys"
+class CurrysSpider(SitemapSpider, StructuredDataSpider):
+    name = "currys_gb"
     item_attributes = {"brand": "currys", "brand_wikidata": "Q3246464"}
-    allowed_domains = ["www.currys.co.uk"]
-    sitemap_urls = ["https://www.currys.co.uk/sitemap-stores.xml"]
+    sitemap_rules = ["https://www.currys.co.uk/sitemap-stores-curryspcworlduk.xml"]
 
-    def parse(self, response, **kwargs):
-        ld = json.loads(response.xpath('//script[@type="application/ld+json"]').get())
+    def pre_process_data(self, ld_data, **kwargs):
+        if isinstance(ld_data["openingHoursSpecification"], dict):
+            ld_data["openingHoursSpecification"] = [ld_data["openingHoursSpecification"]]
+        for rule in ld_data["openingHoursSpecification"]:
+            rule["opens"] = rule["opens"].strip()
+            rule["closes"] = rule["closes"].strip()
 
-        oh = OpeningHours()
-        for rule in ld["openingHoursSpecification"]:
-            for day in rule["dayOfWeek"]:
-                oh.add_range(day[:2], rule["opens"], rule["closes"])
-
-        yield Feature(
-            lat=response.xpath('//input[@class="storeDetailLat"]/@value').get(),
-            lon=response.xpath('//input[@class="storeDetailLong"]/@value').get(),
-            name=response.xpath('//h1[@class="store-information-page-title"]/text()').get(),
-            addr_full=response.xpath('//span[@class="storeinfo-addres storeinfowidth"]/text()').get()
-            + ", United Kingdom",
-            street_address=ld["address"]["streetAddress"],
-            postcode=ld["address"]["postalCode"],
-            country="GB",
-            website=response.request.url,
-            opening_hours=oh.as_opening_hours(),
-            ref=ld["@id"],
-        )
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["lat"] = response.xpath('//input[@class="storeDetailLat"]/@value').get()
+        item["lon"] = response.xpath('//input[@class="storeDetailLong"]/@value').get()
+        item["name"] = response.xpath('//h1[@class="store-information-page-title"]/text()').get().strip()
+        yield item
