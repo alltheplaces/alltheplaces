af5cfee4b032b43c2dc8517703b676c5401ac403
Sky <19364673+Dimitar5555@users.noreply.github.com>
2023-01-29 14:02:46 +0200

    Update yettel_rs.py

--- locations/spiders/yettel_rs.py
+++ locations/spiders/yettel_rs.py
@@ -14,7 +14,7 @@ class YettelRSSpider(scrapy.Spider):
 
     def start_requests(self):
         url = "https://www.yettel.rs/stores/latlong/"
-        yield scrapy.http.FormRequest(url=url, formdata={"initial": "true"}, method="POST")
+        yield scrapy.http.FormRequest(url=url, formdata={"lat": "0", "lng": "0", "dist": "10000000000"}, method="POST")
 
     def parse(self, response):
         for index, store in enumerate(response.json()["data"]["stores"]):
@@ -25,26 +25,26 @@ class YettelRSSpider(scrapy.Spider):
             item["lon"] = store["lng"]
 
             item["city"] = store["city"]
-            item["postal_code"] = store["post_number"]
+            item["postcode"] = store["post_number"]
             item["street_address"] = store["address"]
 
             oh = OpeningHours()
 
             for wh in store["workingHoursFormated"]:
-                if wh.day == "Ponedeljak":
-                    oh.addRange("Mo", wh.startTime, wh.endTime)
-                if wh.day == "Utorak":
-                    oh.addRange("Tu", wh.startTime, wh.endTime)
-                if wh.day == "Sreda":
-                    oh.addRange("We", wh.startTime, wh.endTime)
-                if wh.day == "Četvrtak":
-                    oh.addRange("Th", wh.startTime, wh.endTime)
-                if wh.day == "Petak":
-                    oh.addRange("Fr", wh.startTime, wh.endTime)
-                if wh.day == "Subota":
-                    oh.addRange("Sa", wh.startTime, wh.endTime)
-                if wh.day == "Nedelja":
-                    oh.addRange("Su", wh.startTime, wh.endTime)
+                if wh["day"] == "Ponedeljak":
+                    oh.add_range("Mo", wh["startTime"], wh["endTime"])
+                if wh["day"] == "Utorak":
+                    oh.add_range("Tu", wh["startTime"], wh["endTime"])
+                if wh["day"] == "Sreda":
+                    oh.add_range("We", wh["startTime"], wh["endTime"])
+                if wh["day"] == "Četvrtak":
+                    oh.add_range("Th", wh["startTime"], wh["endTime"])
+                if wh["day"] == "Petak":
+                    oh.add_range("Fr", wh["startTime"], wh["endTime"])
+                if wh["day"] == "Subota":
+                    oh.add_range("Sa", wh["startTime"], wh["endTime"])
+                if wh["dat"] == "Nedelja":
+                    oh.add_range("Su", wh["startTime"], wh["endTime"])
 
             item["opening_hours"] = oh
             yield item
