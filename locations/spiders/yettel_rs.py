24cd6b11dadba7f4adfe457fcce20a4fb74c46c5
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-01-29 13:21:06 +0200

    Create yettel_rs.py

---
+++ locations/spiders/yettel_rs.py
@@ -0,0 +1,38 @@
+import scrapy
+
+class YettelRSSpider(scrapy.Spider):
+    name = "yettel_rs"
+
+    def start_requests(self):
+        url = "https://www.yettel.rs/stores/latlong/"
+        yield scrapy.http.FormRequest(url=url, formdata={"initial": "true"}, method="POST")
+
+    def parse(self, response):
+        for index, store in enumerate(response.json()["data"]["stores"]):
+            item["lat"] = store["lat"]
+            item["lon"] = store["lng"]
+
+            item["city"] = store["city"]
+            item["postal_code"] = store["post_number"]
+            item["street_address"] = store["address"]
+
+            oh = opening_hours()
+
+            for wh in store["workingHoursFormated"]:
+                if wh.day=="Ponedeljak":
+                    oh.add("Mo", wh.startTime, wh.endTime)
+                if wh.day=="Utorak":
+                    oh.add("Tu", wh.startTime, wh.endTime)
+                if wh.day=="Sreda":
+                    oh.add("We", wh.startTime, wh.endTime)
+                if wh.day=="Četvrtak":
+                    oh.add("Th", wh.startTime, wh.endTime)
+                if wh.day=="Petak":
+                    oh.add("Fr", wh.startTime, wh.endTime)
+                if wh.day=="Subota":
+                    oh.add("Sa", wh.startTime, wh.endTime)
+                if wh.day=="Nedelja":
+                    oh.add("Su", wh.startTime, wh.endTime)
+
+            item["opening_hours"] = oh
+            yield item
