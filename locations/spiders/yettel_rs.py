70b7a60c03b8fd8bb3590934e5e5ad8488f0785e
Cj Malone <CjMalone@mail.com>
2023-01-29 12:16:38 +0000

    Compress opening hours

--- locations/spiders/yettel_rs.py
+++ locations/spiders/yettel_rs.py
@@ -1,16 +1,12 @@
 import scrapy
 
-from locations.hours import OpeningHours
+from locations.hours import DAYS_RS, OpeningHours, sanitise_day
 from locations.items import Feature
 
 
 class YettelRSSpider(scrapy.Spider):
     name = "yettel_rs"
-    item_attributes = {
-        "brand": "Yettel",
-        "brand_wikidata": "Q1780171",
-        "country": "RS",
-    }
+    item_attributes = {"brand": "Yettel", "brand_wikidata": "Q1780171"}
 
     def start_requests(self):
         url = "https://www.yettel.rs/stores/latlong/"
@@ -20,7 +16,6 @@ class YettelRSSpider(scrapy.Spider):
         for index, store in enumerate(response.json()["data"]["stores"]):
             item = Feature()
             item["ref"] = store["id"]
-
             item["lat"] = store["lat"]
             item["lon"] = store["lng"]
 
@@ -31,20 +26,9 @@ class YettelRSSpider(scrapy.Spider):
             oh = OpeningHours()
 
             for wh in store["workingHoursFormated"]:
-                if wh["day"] == "Ponedeljak":
-                    oh.add_range("Mo", wh["startTime"], wh["endTime"])
-                if wh["day"] == "Utorak":
-                    oh.add_range("Tu", wh["startTime"], wh["endTime"])
-                if wh["day"] == "Sreda":
-                    oh.add_range("We", wh["startTime"], wh["endTime"])
-                if wh["day"] == "Četvrtak":
-                    oh.add_range("Th", wh["startTime"], wh["endTime"])
-                if wh["day"] == "Petak":
-                    oh.add_range("Fr", wh["startTime"], wh["endTime"])
-                if wh["day"] == "Subota":
-                    oh.add_range("Sa", wh["startTime"], wh["endTime"])
-                if wh["day"] == "Nedelja":
-                    oh.add_range("Su", wh["startTime"], wh["endTime"])
+                if day := sanitise_day(wh["day"], DAYS_RS):
+                    if wh.get("startTime") and wh.get("endTime"):
+                        oh.add_range(day, wh["startTime"], wh["endTime"])
 
             item["opening_hours"] = oh
             yield item
