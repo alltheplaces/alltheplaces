db13e40517aff93adf8ed8e0d7c787c5a8123632
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-01-29 17:45:18 +0200

    Yettel Serbia (#4695)
    * Create yettel_rs.py
    
    * Update yettel_rs.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update yettel_rs.py
    
    * Add DAYS_RS
    
    * Update yettel_rs.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update locations/spiders/yettel_rs.py
    
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    
    * Update yettel_rs.py
    
    * Update yettel_rs.py
    
    * Compress opening hours
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    Co-authored-by: Cj Malone <CjMalone@mail.com>

---
+++ locations/spiders/yettel_rs.py
@@ -0,0 +1,34 @@
+import scrapy
+
+from locations.hours import DAYS_RS, OpeningHours, sanitise_day
+from locations.items import Feature
+
+
+class YettelRSSpider(scrapy.Spider):
+    name = "yettel_rs"
+    item_attributes = {"brand": "Yettel", "brand_wikidata": "Q1780171"}
+
+    def start_requests(self):
+        url = "https://www.yettel.rs/stores/latlong/"
+        yield scrapy.http.FormRequest(url=url, formdata={"lat": "0", "lng": "0", "dist": "10000000000"}, method="POST")
+
+    def parse(self, response):
+        for index, store in enumerate(response.json()["data"]["stores"]):
+            item = Feature()
+            item["ref"] = store["id"]
+            item["lat"] = store["lat"]
+            item["lon"] = store["lng"]
+
+            item["city"] = store["city"]
+            item["postcode"] = store["post_number"]
+            item["street_address"] = store["address"]
+
+            oh = OpeningHours()
+
+            for wh in store["workingHoursFormated"]:
+                if day := sanitise_day(wh["day"], DAYS_RS):
+                    if wh.get("startTime") and wh.get("endTime"):
+                        oh.add_range(day, wh["startTime"], wh["endTime"])
+
+            item["opening_hours"] = oh
+            yield item
