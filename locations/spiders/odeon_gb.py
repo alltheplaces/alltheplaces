a1b6307726d607fe3b54e33a48000d5dc6a035ff
Cj Malone <CjMalone@mail.com>
2023-01-30 09:33:26 +0000

    Flag as needing a proxy

--- locations/spiders/odeon_gb.py
+++ locations/spiders/odeon_gb.py
@@ -9,6 +9,7 @@ class OdeonGBSpider(Spider):
     name = "odeon_gb"
     item_attributes = {"brand": "Odeon", "brand_wikidata": "Q6127470"}
     start_urls = ["https://www.odeon.co.uk/cinemas/"]
+    requires_proxy = True
 
     def parse(self, response, **kwargs):
         data = json.loads(response.xpath("//@data-v-site-list").get())
