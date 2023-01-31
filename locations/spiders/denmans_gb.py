792d61ff42bde02d7962d39ab5aa41eef62f8571
Peter Newman <peternewman@users.noreply.github.com>
2023-01-31 16:02:11 +0000

    Recreate the DenmansGB spider using our new base Rexel one

---
+++ locations/spiders/denmans_gb.py
@@ -0,0 +1,12 @@
+from locations.storefinders.rexel import RexelSpider
+
+
+class DenmansGBSpider(RexelSpider):
+    name = "denmans_gb"
+    item_attributes = {"brand": "Denmans", "brand_wikidata": "Q116508855"}
+    base_url = "www.denmans.co.uk/den"
+    search_lat = 51
+    search_lon = -0
+
+    def parse_item(self, item, feature, **kwargs):
+        yield item
