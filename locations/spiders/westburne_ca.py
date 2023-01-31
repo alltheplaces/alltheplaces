735f9a68052995b0c7d35bcb93e79444179a11f3
Peter Newman <peternewman@users.noreply.github.com>
2023-01-31 17:01:08 +0000

    Add westburne_ca spider

---
+++ locations/spiders/westburne_ca.py
@@ -0,0 +1,12 @@
+from locations.storefinders.rexel import RexelSpider
+
+
+class WestburneCASpider(RexelSpider):
+    name = "westburne_ca"
+    item_attributes = {"brand": "Westburne (Canada)", "brand_wikidata": "Q"}
+    base_url = "www.westburne.ca/cwr"
+    search_lat = 51
+    search_lon = 85
+
+    def parse_item(self, item, feature, **kwargs):
+        yield item
