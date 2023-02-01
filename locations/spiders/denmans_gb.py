2d47606621dc524acb5bad1828b1c5ff68463f74
Peter Newman <peternewman@users.noreply.github.com>
2023-02-01 15:07:24 +0000

    Convert DenmansGBSpider to our base RexelSpider (#4708)
    * Convert DenmansGBSpider to our base RexelSpider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Recreate the DenmansGB spider using our new base Rexel one
    
    * Correctly use the new URL
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add the missing import
    
    * Unify the code
    
    * Ensure storeImages isn't null before we try to process it
    
    * Move more code into the template storefinder
    
    * Finish moving code into the template storefinder
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/denmans_gb.py
+++ locations/spiders/denmans_gb.py
@@ -1,49 +1,9 @@
-from scrapy import Spider
-
-from locations.dict_parser import DictParser
-from locations.hours import OpeningHours
+from locations.storefinders.rexel import RexelSpider
 
 
-class DenmansGBSpider(Spider):
+class DenmansGBSpider(RexelSpider):
     name = "denmans_gb"
     item_attributes = {"brand": "Denmans", "brand_wikidata": "Q116508855"}
-    # This seems to return all stores regardless of lat-long; as long as it's in the UK?
-    start_urls = ["https://www.denmans.co.uk/den/store-finder/findNearbyStores?latitude=51&longitude=-0"]
-
-    def parse(self, response):
-        for store in response.json()["results"]:
-            store["address"]["street_address"] = ", ".join(
-                filter(
-                    None,
-                    [
-                        store["address"].pop("line1"),
-                        store["address"].pop("line2"),
-                        store["address"].pop("line3"),
-                    ],
-                )
-            )
-            store["ref"] = store.pop("name")
-            item = DictParser.parse(store)
-            item["phone"] = store["address"]["phone"]
-            # e.g. https://www.denmans.co.uk/den/Bradley-Stoke-Bristol/store/1AR
-            item[
-                "website"
-            ] = f'https://www.denmans.co.uk/den/{store["address"]["town"].replace(" ", "-")}/store/{store["ref"]}'
-            item["opening_hours"] = self.decode_hours(store)
-            # We could also fall back to cartIcon here...
-            storeImages = filter(lambda x: (x["format"] == "store" and x["url"]), store["storeImages"])
-            if storeImages:
-                item["image"] = next(storeImages)["url"]
-            yield item
-
-    @staticmethod
-    def decode_hours(store):
-        oh = OpeningHours()
-        for r in filter(lambda x: (not x["closed"]), store["openingHours"]["rexelWeekDayOpeningList"]):
-            oh.add_range(
-                r["weekDay"],
-                r["openingTime"]["formattedHour"],
-                r["closingTime"]["formattedHour"],
-                time_format="%I:%M %p",
-            )
-        return oh
+    base_url = "www.denmans.co.uk/den"
+    search_lat = 51
+    search_lon = -0
