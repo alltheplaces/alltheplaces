d3584c11fcf0e06bddd82e3d209b43db8476ed25
Cj Malone <CjMalone@mail.com>
2023-02-02 10:38:40 +0000

    Add Paperchase spider

---
+++ locations/storefinders/storemapper.py
@@ -0,0 +1,21 @@
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+
+
+class StoremapperSpider(Spider):
+    key = ""
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def start_requests(self):
+        yield JsonRequest(url=f"https://storemapper-herokuapp-com.global.ssl.fastly.net/api/users/{self.key}/stores.js")
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["stores"]:
+            item = DictParser.parse(location)
+
+            yield from self.parse_item(item, location) or []
+
+    def parse_item(self, item, location, **kwargs):
+        yield item
