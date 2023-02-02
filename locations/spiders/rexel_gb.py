65cc153c3388d0088e6de6bd0e11f92a58791ba6
Peter Newman <peternewman@users.noreply.github.com>
2023-02-02 00:42:46 +0000

    Create rexel_gb.py - Take 2 (#4713)
    * Re-add rexel_gb spider
    
    * Only try and add rexel opening hours if they're present

---
+++ locations/spiders/rexel_gb.py
@@ -0,0 +1,9 @@
+from locations.storefinders.rexel import RexelSpider
+
+
+class RexelGBSpider(RexelSpider):
+    name = "rexel_gb"
+    item_attributes = {"brand": "Rexel", "brand_wikidata": "Q962489"}
+    base_url = "www.rexel.co.uk/uki"
+    search_lat = 51
+    search_lon = -0
