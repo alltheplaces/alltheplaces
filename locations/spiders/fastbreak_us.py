a2739aa408dd4d7034c9ae81bf7accaca1c59297
Cj Malone <CjMalone@mail.com>
2023-02-08 16:04:26 +0000

    Add Fastbreak Spider

---
+++ locations/spiders/fastbreak_us.py
@@ -0,0 +1,13 @@
+from locations.items import Feature
+from locations.storefinders.storerocket import StorerocketSpider
+
+
+class FastbreakUSSpider(StorerocketSpider):
+    name = "fastbreak_us"
+    item_attributes = {"brand": "Fastbreak", "brand_wikidata": "Q116731804"}
+    id = "WwzpABj4dD"
+
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        item["website"] = f'https://www.myfastbreak.com/locations?location={location["slug"]}'
+
+        yield item
