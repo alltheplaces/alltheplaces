266836d204c8657247e43f237be874ad3b89a0c1
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-02-08 16:57:42 +0000

    Add Fastbreak Spider (#4738)
    * Add Fastbreak Spider
    
    * Rename id

---
+++ locations/storefinders/storerocket.py
@@ -0,0 +1,35 @@
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours, sanitise_day
+from locations.items import Feature
+
+
+class StorerocketSpider(Spider):
+    storerocket_id = ""
+
+    def start_requests(self):
+        yield JsonRequest(url=f"https://storerocket.io/api/user/{self.storerocket_id}/locations")
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["results"]["locations"]:
+            item = DictParser.parse(location)
+
+            item["street_address"] = ", ".join(filter(None, [location["address_line_1"], location["address_line_2"]]))
+
+            item["facebook"] = location.get("facebook")
+            item["extras"]["instagram"] = location.get("instagram")
+            item["twitter"] = location.get("twitter")
+
+            item["opening_hours"] = OpeningHours()
+            for day, times in location["hours"].items():
+                day = sanitise_day(day)
+                if day and times:
+                    start_time, end_time = times.split("-")
+                    item["opening_hours"].add_range(day, start_time, end_time)
+
+            yield from self.parse_item(item, location) or []
+
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        yield item
