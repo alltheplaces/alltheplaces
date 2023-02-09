39b77d42387004f5f6c54a3a97ae11fed65f23f4
jancallewaert <jan.callewaert@tomtom.com>
2023-02-09 15:13:55 +0100

    Crawl locations from JSON API

--- locations/spiders/edwardjones.py
+++ locations/spiders/edwardjones.py
@@ -1,5 +1,3 @@
-import json
-
 import requests
 from scrapy.spiders import SitemapSpider
 
@@ -17,4 +15,28 @@ class EdwardJonesSpider(SitemapSpider, StructuredDataSpider):
         oh = OpeningHours()
         oh.from_linked_data(ld_data, time_format="%H:%M:%S")
         item["opening_hours"] = oh.as_opening_hours()
+
+        flag = True
+        page = 1
+        while flag:
+            url = "https://www.edwardjones.com/api/v3/financial-advisor/results?q={postcode}&distance=75&distance_unit=mi&page={page}&searchtype=2".format(
+                postcode=item["postcode"], page=page)
+
+            headers = {"Host": "www.edwardjones.com",
+                       "Accept": "application/json",
+                       "Referer": "https://www.edwardjones.com/us-en/search/financial-advisor/results"}
+            response = requests.get(url, headers=headers, data={}).json()
+
+            for x in response["results"]:
+                if item["website"].endswith(x["faUrl"]):
+                    item["lat"] = x["lat"]
+                    item["lon"] = x["lon"]
+                    flag = False
+
+            count = len(response["results"])
+            if count < response["itemsPerPage"] or count == 0:
+                flag = False
+            else:
+                page = page + 1
+
         yield item
