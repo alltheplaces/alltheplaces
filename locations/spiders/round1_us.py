f350c4ecfd6816d258beecaa2db8813f6cba283a
David Hicks <david@hicks.id.au>
2023-02-09 23:35:47 +1100

    Add Round1 (US) (54 stores)
    Closes issue #4758

---
+++ locations/spiders/round1_us.py
@@ -0,0 +1,11 @@
+from locations.storefinders.storepoint import StorepointSpider
+
+
+class Round1USSpider(StorepointSpider):
+    name = "round1_us"
+    item_attributes = {"brand": "Round One Entertainment", "brand_wikidata": "Q11346634"}
+    key = "16026f2c5ac3c7"
+
+    def parse_item(self, item, location: {}, **kwargs):
+        item["name"] = " ".join(item["name"].replace("<br>", "").split())
+        yield item
