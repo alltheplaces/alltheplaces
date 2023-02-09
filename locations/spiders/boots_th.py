9e5cbc9ee9bcab898f0b49c54cfe2192097282d5
jancallewaert <jan.callewaert@tomtom.com>
2023-02-09 15:36:09 +0100

    Fix Boots TH spider

--- locations/spiders/boots_th.py
+++ locations/spiders/boots_th.py
@@ -4,38 +4,34 @@ import re
 import scrapy
 from scrapy import Selector
 
+from locations.dict_parser import DictParser
 from locations.items import Feature
 
 
 class BootsTHSpider(scrapy.Spider):
     name = "boots_th"
     item_attributes = {"brand": "Boots", "brand_wikidata": "Q6123139"}
-    allowed_domains = ["www.intl.boots.com"]
     download_delay = 0.5
-    start_urls = [
-        "http://www.intl.boots.com/storelocator/Results.aspx?country_id=4&s=",
-    ]
+
+    def start_requests(self):
+        yield self.get_page(1)
+
+    def get_page(self, n):
+        return scrapy.Request(
+            f"https://store.boots.co.th/api/v1/branches/web?locale=en&latitude=0&longitude=0&filter_type=1&page={n}",
+            meta={"page": n},
+        )
 
     def parse(self, response):
-        data = response.xpath('//script[@type="text/javascript" and contains(text(), "stores")]/text()').extract_first()
-        stores = json.loads(re.search(r"stores = (.*);", data).group(1))
+        data = response.json()
+        stores = data["entities"]
 
-        for i, store_data in enumerate(stores, start=1):
-            store_div_content = Selector(text=store_data[4])
-            ref = re.search(r"store_id=(.*)$", store_div_content.xpath("//a/@href").extract_first()).group(1)
-            website = response.urljoin(store_div_content.xpath("//a/@href").extract_first())
-            address = response.xpath("normalize-space(//li[{index}]/a/div/text()[2])".format(index=i)).extract_first()
-            phone = store_div_content.xpath('//p[@class="telephone"]/text()').extract_first()
+        for store in stores:
+            item = DictParser.parse(store)
+            item["ref"] = store["store_code"]
+            item["phone"] = store["phone_list"][0]
+            item["postcode"] = store["zipcode"]
+            yield item
 
-            properties = {
-                "name": store_data[0].strip(),
-                "ref": ref,
-                "addr_full": address,
-                "country": "TH",
-                "phone": phone,
-                "website": website,
-                "lat": store_data[1],
-                "lon": store_data[2],
-            }
-
-            yield Feature(**properties)
+        if data["page_information"]["page"] < data["page_information"]["number_of_page"]:
+            yield self.get_page(1 + response.meta["page"])
