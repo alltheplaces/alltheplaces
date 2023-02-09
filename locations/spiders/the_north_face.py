701a9aba372c463dc05d7c9269a31b0e6b610bc0
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-02-09 17:09:47 +0000

    Fix The North Face Spider (#4741)
    * Add Where2GetIt base spider
    
    * Rename fields
    
    * Fix The North Face Spider
    
    * Drop bad North Face states

--- locations/spiders/the_north_face.py
+++ locations/spiders/the_north_face.py
@@ -1,96 +1,19 @@
-import json
-
-import scrapy
-
 from locations.items import Feature
+from locations.storefinders.where2getit import Where2GetItSpider
+
+# The spider gets Stores and Outlets
+# A single query for CA collects both CA and US
 
 
-class TheNorthFaceSpider(scrapy.Spider):
+class TheNorthFaceSpider(Where2GetItSpider):
     name = "the_north_face"
     item_attributes = {"brand": "The North Face", "brand_wikidata": "Q152784"}
-    allowed_domains = ["hosted.where2getit.com"]
+    w2gi_id = "C1907EFA-14E9-11DF-8215-BBFCBD236D0E"
+    w2gi_filter = {"or": {"northface": {"eq": "1"}, "outletstore": {"eq": "1"}}}
+    w2gi_query = "CA"
 
-    def start_requests(self):
-        url = "https://hosted.where2getit.com/northface/2015/rest/locatorsearch?like=0.8704346432892842&lang=en_EN"
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        if item["state"].upper() == "TNF RETAIL":
+            item["state"] = None
 
-        headers = {
-            "origin": "https://hosted.where2getit.com",
-            "Referer": "https://hosted.where2getit.com/northface/2015/index.html",
-            "content-type": "application/json",
-        }
-
-        current_state = json.dumps(
-            {
-                "request": {
-                    "appkey": "C1907EFA-14E9-11DF-8215-BBFCBD236D0E",
-                    "formdata": {
-                        "geoip": "false",
-                        "dataview": "store_default",
-                        "limit": 1000,
-                        "order": "rank, _DISTANCE",
-                        "geolocs": {
-                            "geoloc": [
-                                {
-                                    "addressline": "Austin TX 78754",
-                                    "country": "US",
-                                    "latitude": "",
-                                    "longitude": "",
-                                }
-                            ]
-                        },
-                        "searchradius": "5000",
-                        "where": {
-                            "visiblelocations": {"eq": ""},
-                            "or": {
-                                "northface": {"eq": "1"},
-                                "outletstore": {"eq": "1"},
-                                "retailstore": {"eq": ""},
-                                "summit": {"eq": ""},
-                            },
-                            "and": {
-                                "youth": {"eq": ""},
-                                "apparel": {"eq": ""},
-                                "footwear": {"eq": ""},
-                                "equipment": {"eq": ""},
-                                "mt": {"eq": ""},
-                                "access_pack": {"eq": ""},
-                                "steep_series": {"eq": ""},
-                            },
-                        },
-                        "false": "0",
-                    },
-                }
-            }
-        )
-
-        yield scrapy.Request(
-            url,
-            method="POST",
-            body=current_state,
-            headers=headers,
-            callback=self.parse,
-        )
-
-    def parse(self, response):
-        stores = response.json()
-
-        for store in stores["response"]["collection"]:
-            state = store["state"]
-            if state is None:
-                state = store["province"]
-
-            properties = {
-                "ref": store["clientkey"],
-                "name": store["name"],
-                "addr_full": store["address1"],
-                "city": store["city"],
-                "state": state,
-                "postcode": store["postalcode"],
-                "country": store["country"],
-                "lat": store["latitude"],
-                "lon": store["longitude"],
-                "phone": store["phone"],
-                "website": store["url"],
-            }
-
-            yield Feature(**properties)
+        yield item
---
+++ locations/storefinders/where2getit.py
@@ -0,0 +1,50 @@
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.items import Feature
+
+
+class Where2GetItSpider(Spider):
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    w2gi_api = "https://hosted.where2getit.com/rest/locatorsearch"
+    w2gi_id = ""
+    w2gi_query = "US"
+    w2gi_filter = None
+    request_size = 1000
+
+    def make_request(self, offset: int) -> JsonRequest:
+        return JsonRequest(
+            url=self.w2gi_api,
+            data={
+                "request": {
+                    "appkey": self.w2gi_id,
+                    "formdata": {
+                        "dataview": "store_default",
+                        "limit": self.request_size,
+                        "offset": offset,
+                        "geolocs": {"geoloc": [{"addressline": self.w2gi_query}]},
+                        "searchradius": "5000",
+                        "where": self.w2gi_filter,
+                    },
+                }
+            },
+            meta={"offset": offset},
+        )
+
+    def start_requests(self):
+        yield self.make_request(0)
+
+    def parse(self, response, **kwargs):
+        if response.json()["response"]["collectioncount"] == self.request_size:
+            yield self.make_request(response.meta["offset"] + self.request_size)
+
+        for location in response.json()["response"]["collection"]:
+            item = DictParser.parse(location)
+            item["ref"] = location["clientkey"]
+
+            yield from self.parse_item(item, location) or []
+
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        yield item
