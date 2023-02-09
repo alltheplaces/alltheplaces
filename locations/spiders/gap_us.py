2d8b380cb9c73b56c57ceff8766aa0679084eaaf
Cj Malone <CjMalone@mail.com>
2023-02-09 13:42:18 +0000

    Fix Gap clothes and brand output

---
+++ locations/spiders/gap_us.py
@@ -0,0 +1,46 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, Clothes, apply_category, apply_yes_no
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+def apply_clothes(clothes: [str], item: Feature):
+    for c in clothes:
+        apply_yes_no(f"clothes:{c}", item, True)
+    item["extras"]["clothes"] = ";".join(clothes)
+
+
+class GapUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "gap_us"
+    item_attributes = {"brand": "Gap", "brand_wikidata": "Q420822"}
+    allowed_domains = ["www.gap.com"]
+    sitemap_urls = ["https://www.gap.com/stores/sitemap.xml"]
+    sitemap_rules = [(r"https://www\.gap\.com/stores/\w\w/\w+/gap-(\d+)\.html$", "parse_sd")]
+    requires_proxy = True
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["name"] = response.xpath('normalize-space(//div[@class="location-name"]/text())').get()
+
+        types = response.xpath('normalize-space(//div[@class="store-carries"]/text())').get()
+
+        if types == "Gap Factory Store":
+            item["brand"] = "Gap Factory Store"
+        else:
+            item["brand"] = "Gap"
+            clothes = []
+            if "GapBody" in types:
+                clothes.append(Clothes.UNDERWEAR.value)
+            if "GapMaternity" in types:
+                clothes.append(Clothes.MATERNITY.value)
+            if "babyGap" in types:
+                clothes.append(Clothes.BABY.value)
+            if "GapKids" in types:
+                clothes.append(Clothes.CHILDREN.value)
+            apply_clothes(clothes, item)
+
+        item["image"] = None
+
+        apply_category(Categories.SHOP_CLOTHES, item)
+
+        yield item
