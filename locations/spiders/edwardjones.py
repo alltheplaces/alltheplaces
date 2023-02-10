44c71aa06c0e5ce7d4d72e25dca1813fae2d6a08
jancallewaert <jan.callewaert@tomtom.com>
2023-02-10 13:28:06 +0100

    Use scrapy.Request

--- locations/spiders/edwardjones.py
+++ locations/spiders/edwardjones.py
@@ -1,4 +1,4 @@
-import requests
+import scrapy
 from scrapy.spiders import SitemapSpider
 
 from locations.hours import OpeningHours
@@ -16,30 +16,25 @@ class EdwardJonesSpider(SitemapSpider, StructuredDataSpider):
         oh.from_linked_data(ld_data, time_format="%H:%M:%S")
         item["opening_hours"] = oh.as_opening_hours()
 
-        flag = True
-        page = 1
-        while flag:
-            url = "https://www.edwardjones.com/api/v3/financial-advisor/results?q={postcode}&distance=75&distance_unit=mi&page={page}&searchtype=2".format(
-                postcode=item["postcode"], page=page
-            )
+        yield from self.get_location(item, 1)
 
-            headers = {
-                "Host": "www.edwardjones.com",
-                "Accept": "application/json",
-                "Referer": "https://www.edwardjones.com/us-en/search/financial-advisor/results",
-            }
-            response = requests.get(url, headers=headers, data={}).json()
+    def get_location(self, item, page):
+        url = "https://www.edwardjones.com/api/v3/financial-advisor/results?q={postcode}&distance=75&distance_unit=mi&page={page}&searchtype=2".format(
+            postcode=item["postcode"], page=page
+        )
+        yield scrapy.Request(url=url, cb_kwargs={"item": item, "page": 1}, callback=self.parse_location)
 
-            for x in response["results"]:
-                if item["website"].endswith(x["faUrl"]):
-                    item["lat"] = x["lat"]
-                    item["lon"] = x["lon"]
-                    flag = False
+    def parse_location(self, response, item, page):
+        json = response.json()
+        results = json["results"]
 
-            count = len(response["results"])
-            if count < response["itemsPerPage"] or count == 0:
-                flag = False
-            else:
-                page = page + 1
+        for x in results:
+            if item["website"].endswith(x["faUrl"]):
+                item["lat"] = x["lat"]
+                item["lon"] = x["lon"]
+                break
 
-        yield item
+        if ("lat" in item and "lon" in item) or json["resultCount" == 0] or len(results) < json["itemsPerPage"]:
+            yield item
+        else:
+            yield from self.get_location(item, page + 1)
