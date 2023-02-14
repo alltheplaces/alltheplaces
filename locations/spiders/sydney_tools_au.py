ac69fe2844f9232ec99733ad8bd50e197fc7ef0c
David Hicks <david@hicks.id.au>
2023-02-15 00:20:42 +1100

    Add Sydney Tools (AU) (69 stores)

---
+++ locations/spiders/sydney_tools_au.py
@@ -0,0 +1,33 @@
+import scrapy
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_EN, OpeningHours
+
+
+class SydneyToolsAUSpider(scrapy.Spider):
+    name = "sydney_tools_au"
+    item_attributes = {"brand": "Sydney Tools", "brand_wikidata": "Q116779309"}
+    allowed_domains = ["sydneytools.com.au"]
+
+    def start_requests(self):
+        graphql_query = "query routes_StoreList_Query { viewer { ...StoreList_viewer id } } fragment StoreList_viewer on Customer { id email myStore { id } stores(first: 10000) { edges { node { id name address city postcode state description fax phone lat lng canPickup hours { monday { open close } tuesday { open close } wednesday { open close } thursday { open close } friday { open close } saturday { open close } sunday { open close } } } } } }"
+        data = {"query": graphql_query, "variables": {}}
+        yield JsonRequest(url="https://sydneytools.com.au/graphql", data=data)
+
+    def parse(self, response):
+        stores = response.json()["data"]["viewer"]["stores"]["edges"]
+        for store in stores:
+            item = DictParser.parse(store["node"])
+            item["street_address"] = item.pop("addr_full")
+            item["website"] = (
+                "https://sydneytools.com.au/stores/"
+                + item["state"].lower()
+                + "/"
+                + item["name"].lower().replace(" ", "-")
+            )
+            oh = OpeningHours()
+            for day_name, day_hours in store["node"]["hours"].items():
+                oh.add_range(DAYS_EN[day_name.title()], day_hours["open"], day_hours["close"], "%H:%M")
+            item["opening_hours"] = oh.as_opening_hours()
+            yield item
