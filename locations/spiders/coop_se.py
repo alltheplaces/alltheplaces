f1b1f883225188a62e9e9e14d4aafaf42e8440a5
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-02-16 17:56:15 +0000

    Collect Coop SE city and postcode (#4822)
    * Clean up opening hours
    
    * Bump API versions to get postcode and city

--- locations/spiders/coop_se.py
+++ locations/spiders/coop_se.py
@@ -1,25 +1,19 @@
 import scrapy
 
-from locations.hours import DAYS_SE, OpeningHours
+from locations.hours import DAYS_SE, OpeningHours, day_range, sanitise_day
 from locations.items import Feature
 
 
-class CoopNOSpider(scrapy.Spider):
+class CoopSESpider(scrapy.Spider):
     name = "coop_se"
-    item_attributes = {"brand": "Coop", "brand_wikidata": "Q15229319", "country": "SE"}
-    start_urls = ["https://proxy.api.coop.se/external/store/stores/?api-version=v1"]
-
-    custom_settings = {
-        "ROBOTSTXT_OBEY": False,
-        "DEFAULT_REQUEST_HEADERS": {
-            "ocp-apim-subscription-key": "990520e65cc44eef89e9e9045b57f4e9",
-        },
-    }
+    item_attributes = {"brand": "Coop", "brand_wikidata": "Q15229319"}
+    start_urls = ["https://proxy.api.coop.se/external/store/stores?api-version=v2"]
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"ocp-apim-subscription-key": "990520e65cc44eef89e9e9045b57f4e9"}}
 
     def parse(self, response, **kwargs):
         for store in response.json().get("stores"):
             yield scrapy.Request(
-                f"https://proxy.api.coop.se/external/store/stores/{store.get('ledgerAccountNumber')}?api-version=v1",
+                f"https://proxy.api.coop.se/external/store/stores/{store.get('ledgerAccountNumber')}?api-version=v3",
                 callback=self.parse_store,
             )
 
@@ -27,31 +21,28 @@ class CoopNOSpider(scrapy.Spider):
         store = response.json()
         oh = OpeningHours()
         for opening_hour in store.get("openingHours"):
-            day = opening_hour.get("text")
-            if "-" not in day:
-                oh.add_range(DAYS_SE.get(day), opening_hour.get("openFrom")[:5], opening_hour.get("openTo")[:5])
+            if "-" in opening_hour["text"]:
+                start_day, end_day = opening_hour["text"].split("-", maxsplit=1)
             else:
-                split_day = day.split("-")
-                found_first_day = False
-                for se_day in DAYS_SE:
-                    if found_first_day or se_day in split_day:
-                        oh.add_range(
-                            DAYS_SE.get(se_day), opening_hour.get("openFrom")[:5], opening_hour.get("openTo")[:5]
-                        )
-                        found_first_day = True
-                        if split_day[1] == se_day:
-                            break
+                start_day = end_day = opening_hour["text"]
+            start_day = sanitise_day(start_day, DAYS_SE)
+            end_day = sanitise_day(end_day, DAYS_SE)
+            if start_day and end_day:
+                for day in day_range(start_day, end_day):
+                    oh.add_range(day, opening_hour["openFrom"], opening_hour["openTo"], time_format="%H:%M:%S")
 
         yield Feature(
             {
-                "ref": store.get("id"),
+                "ref": str(store.get("id")),
                 "name": store.get("name"),
                 "street_address": store.get("address"),
+                "postcode": store["postalCode"],
+                "city": store["city"],
                 "phone": store.get("phone"),
                 "website": f"https://www.coop.se{store.get('url')}" if store.get("url") else None,
                 "lat": store.get("latitude"),
                 "lon": store.get("longitude"),
-                "opening_hours": oh.as_opening_hours(),
+                "opening_hours": oh,
                 "extras": {"store_type": store.get("concept").get("name")},
             }
         )
