0da839993c62c5376c693bcc28d5fad33883ea78
Cj Malone <CjMalone@mail.com>
2023-02-16 17:32:19 +0000

    Clean up opening hours

--- locations/spiders/coop_se.py
+++ locations/spiders/coop_se.py
@@ -1,12 +1,12 @@
 import scrapy
 
-from locations.hours import DAYS_SE, OpeningHours
+from locations.hours import DAYS_SE, OpeningHours, day_range, sanitise_day
 from locations.items import Feature
 
 
-class CoopNOSpider(scrapy.Spider):
+class CoopSESpider(scrapy.Spider):
     name = "coop_se"
-    item_attributes = {"brand": "Coop", "brand_wikidata": "Q15229319", "country": "SE"}
+    item_attributes = {"brand": "Coop", "brand_wikidata": "Q15229319"}
     start_urls = ["https://proxy.api.coop.se/external/store/stores/?api-version=v1"]
 
     custom_settings = {
@@ -27,20 +27,15 @@ class CoopNOSpider(scrapy.Spider):
         store = response.json()
         oh = OpeningHours()
         for opening_hour in store.get("openingHours"):
-            day = opening_hour.get("text")
-            if "-" not in day:
-                oh.add_range(DAYS_SE.get(day), opening_hour.get("openFrom")[:5], opening_hour.get("openTo")[:5])
+            if "-" in opening_hour["text"]:
+                start_day, end_day = opening_hour["text"].split("-", maxsplit=1)
             else:
-                split_day = day.split("-")
-                found_first_day = False
-                for se_day in DAYS_SE:
-                    if found_first_day or se_day in split_day:
-                        oh.add_range(
-                            DAYS_SE.get(se_day), opening_hour.get("openFrom")[:5], opening_hour.get("openTo")[:5]
-                        )
-                        found_first_day = True
-                        if split_day[1] == se_day:
-                            break
+                start_day = end_day = opening_hour["text"]
+            start_day = sanitise_day(start_day, DAYS_SE)
+            end_day = sanitise_day(end_day, DAYS_SE)
+            if start_day and end_day:
+                for day in day_range(start_day, end_day):
+                    oh.add_range(day, opening_hour["openFrom"], opening_hour["openTo"], time_format="%H:%M:%S")
 
         yield Feature(
             {
@@ -51,7 +46,7 @@ class CoopNOSpider(scrapy.Spider):
                 "website": f"https://www.coop.se{store.get('url')}" if store.get("url") else None,
                 "lat": store.get("latitude"),
                 "lon": store.get("longitude"),
-                "opening_hours": oh.as_opening_hours(),
+                "opening_hours": oh,
                 "extras": {"store_type": store.get("concept").get("name")},
             }
         )
