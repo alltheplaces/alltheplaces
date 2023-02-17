9f9379e6bdc749fbfbd56c7b4ecd9a68340f2ba3
jancallewaert <jan.callewaert@tomtom.com>
2023-02-17 16:09:49 +0100

    Fix waterstones

--- locations/spiders/waterstones.py
+++ locations/spiders/waterstones.py
@@ -2,6 +2,7 @@ import scrapy
 
 from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
 class WaterstonesSpider(scrapy.Spider):
@@ -9,6 +10,8 @@ class WaterstonesSpider(scrapy.Spider):
     item_attributes = {"brand": "Waterstones", "brand_wikidata": "Q151779"}
     allowed_domains = ["www.waterstones.com"]
     start_urls = ("https://www.waterstones.com/bookshops/viewall",)
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
     def parse(self, response):
         stores = response.xpath('//div[contains(@class, "shops-directory-list")]//a/@href').extract()
@@ -21,8 +24,14 @@ class WaterstonesSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
     def parse_store(self, response):
+        try:
+            ref = response.url.split("/")[4]
+        except IndexError:
+            # No bookshop
+            return
+
         properties = {
-            "ref": response.url.split("/")[4],
+            "ref": ref,
             "name": self.get_meta_property(response, "og:title"),
             "addr_full": self.get_meta_property(response, "business:contact_data:street_address"),
             "city": self.get_meta_property(response, "business:contact_data:locality"),
@@ -39,14 +48,18 @@ class WaterstonesSpider(scrapy.Spider):
         return response.xpath(f'//meta[@property="{property}"]/@content').extract_first()
 
     def get_opening_hours(self, response):
-        days = response.xpath('//meta[@property="business:hours:day"]/@content').extract()
-        starts = response.xpath('//meta[@property="business:hours:start"]/@content').extract()
-        ends = response.xpath('//meta[@property="business:hours:end"]/@content').extract()
-        o = OpeningHours()
-        for i in range(len(days)):
-            day = days[i][0].upper() + days[i][1]
-            start = starts[i].replace(".", ":")
-            end = ends[i].replace(".", ":")
-            if day and start and end:
-                o.add_range(day, start, end)
-        return o.as_opening_hours()
+        try:
+            days = response.xpath('//meta[@property="business:hours:day"]/@content').extract()
+            starts = response.xpath('//meta[@property="business:hours:start"]/@content').extract()
+            ends = response.xpath('//meta[@property="business:hours:end"]/@content').extract()
+            o = OpeningHours()
+            for i in range(len(days)):
+                day = days[i][0].upper() + days[i][1]
+                start = starts[i].replace(".", ":")
+                end = ends[i].replace(".", ":")
+                if day and start and end:
+                    o.add_range(day, start, end)
+            return o.as_opening_hours()
+        except IndexError:
+            # No opening hours provided
+            pass
