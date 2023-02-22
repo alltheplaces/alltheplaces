3fcafa800d46439feec5d8522b11fcf97687d15d
Romeo-PHILLIPS <92714362+Romeo-PHILLIPS@users.noreply.github.com>
2023-02-22 05:51:14 +0100

    feat: Ekoplaza (#4850)

---
+++ locations/spiders/ekoplaza_nl.py
@@ -0,0 +1,61 @@
+import scrapy
+
+from locations.hours import OpeningHours, sanitise_day
+from locations.items import Feature
+
+
+class EkoplazaNLSpider(scrapy.Spider):
+    name = "ekoplaza_nl"
+    item_attributes = {"brand": "EkoPlaza", "brand_wikidata": "Q47017915"}
+    start_urls = [
+        "https://www.ekoplaza.nl/api/aspos/stores?distanceFromLatitude=48.8619029&distanceFromLongitude=2.3730383&limit=3000&storeTags=Webshop",
+    ]
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Accept": "*/*"}}
+
+    def parse(self, response):
+        for store in response.json().get("Stores"):
+            coordinates = store.get("Location")
+            oh = OpeningHours()
+            for ohs in store.get("OpeningTimes").get("Weekdays"):
+                day, opens, closes = (
+                    sanitise_day(ohs.get("Day")),
+                    ohs.get("OpeningTime").lstrip("PT"),
+                    ohs.get("ClosingTime").lstrip("PT"),
+                )
+                opens = opens if "M" in opens else opens + "00M"
+                closes = closes if "M" in closes else closes + "00M"
+                if opens == closes:
+                    continue
+                oh.add_range(
+                    day=sanitise_day(day),
+                    open_time=opens,
+                    close_time=closes,
+                    time_format="%Hh%Mm",
+                )
+
+            props = {
+                "ref": store.get("BranchNumber"),
+                "name": store.get("Name"),
+                "housenumber": store.get("streetNumber"),
+                "street": store.get("Street"),
+                "street_address": " ".join([store.get("HouseNumber"), store.get("Street")]),
+                "country": store.get("CountryCode"),
+                "phone": store.get("PhoneNumber"),
+                "email": store.get("Email"),
+                "postcode": store.get("PostalCode"),
+                "city": store.get("City"),
+                "addr_full": " ".join(
+                    [
+                        store.get("HouseNumber"),
+                        store.get("Street"),
+                        store.get("PostalCode"),
+                        store.get("City"),
+                    ]
+                ),
+                "lat": coordinates.get("Latitude"),
+                "lon": coordinates.get("Longitude"),
+                "opening_hours": oh,
+            }
+            if store.get("FormulaCode") == "EWA":
+                props["brand"] = "Bio Drôme Goes"
+            yield Feature(**props)
