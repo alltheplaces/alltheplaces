395893a50874ab04d81095a7fb9cc425eefb93cd
rjw62 <rjw62@cantab.net>
2023-02-23 20:23:53 +0000

    Use default download delay

--- locations/spiders/screwfix_gb.py
+++ locations/spiders/screwfix_gb.py
@@ -9,7 +9,7 @@ class ScrewfixGBSpider(CrawlSpider):
     name = "screwfix_gb"
     item_attributes = {"brand": "Screwfix", "brand_wikidata": "Q7439115"}
     allowed_domains = ["www.screwfix.com"]
-    download_delay = 0.1
+    download_delay = 1
     start_urls = ["https://www.screwfix.com/stores/all"]
     rules = [Rule(LinkExtractor(allow=r"\/stores\/([A-Z][A-Z][0-9])\/.+$"), callback="parse")]
     wanted_types = ["HardwareStore"]
