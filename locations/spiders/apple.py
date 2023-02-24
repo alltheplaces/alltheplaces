efb069638968cd6b64a2907c99f8bb2109e9fb48
Romeo <romeo@echo-analytics.io>
2023-02-25 00:09:42 +0100

    feat: Apple Update

--- locations/spiders/apple.py
+++ locations/spiders/apple.py
@@ -6,5 +6,4 @@ from locations.structured_data_spider import StructuredDataSpider
 class AppleSpider(SitemapSpider, StructuredDataSpider):
     name = "apple"
     item_attributes = {"brand": "Apple", "brand_wikidata": "Q421253"}
-    allowed_domains = ["apple.com"]
     sitemap_urls = ("https://www.apple.com/retail/sitemap/sitemap.xml",)
