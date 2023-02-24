91986d8f064840f09997733cd8e6ce5bbd527072
David Hicks <david@hicks.id.au>
2023-02-25 08:40:28 +1100

    albert_heijn: fix spider (#4866)
    * albert_heijn: fix spider
    
    1120 stores returned of which 73 are BE and 1047 are NL.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * albert_heijn: code improvement
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/albert_heijn.py
+++ locations/spiders/albert_heijn.py
@@ -1,48 +1,142 @@
-import json
-import re
-
 import scrapy
+from scrapy.http import JsonRequest
 
-from locations.hours import OpeningHours
-from locations.items import Feature
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_NL, OpeningHours
 
 
 class AlbertHeijnSpider(scrapy.Spider):
     name = "albert_heijn"
     item_attributes = {"brand": "Albert Heijn", "brand_wikidata": "Q1653985"}
     allowed_domains = ["www.ah.nl", "www.ah.be"]
-    start_urls = (
-        "https://www.ah.nl/sitemaps/entities/stores/stores.xml",
-        "https://www.ah.be/sitemaps/entities/stores/stores.xml",
-    )
+    start_urls = ["https://www.ah.nl/gql", "https://www.ah.be/gql"]
+
+    def get_page(self, gql_url, page_number):
+        gql_query = """
+query stores($filter: StoreFilterInput, $size: PageSize!, $start: Int) {
+  stores(filter: $filter, size: $size, start: $start) {
+    result {
+      ...storeList
+      __typename
+    }
+    page {
+      total
+      hasNextPage
+      __typename
+    }
+    __typename
+  }
+}
+
+fragment storeList on Store {
+  ...storeDetails
+  distance
+  __typename
+}
+
+fragment storeDetails on Store {
+  id
+  name
+  storeType
+  phone
+  address {
+    ...storeAddress
+    __typename
+  }
+  geoLocation {
+    latitude
+    longitude
+    __typename
+  }
+  services {
+    ...serviceInfo
+    __typename
+  }
+  openingDays {
+    ...openingDaysInfo
+    __typename
+  }
+  __typename
+}
+
+fragment storeAddress on StoreAddress {
+  city
+  street
+  houseNumber
+  houseNumberExtra
+  postalCode
+  countryCode
+  __typename
+}
+
+fragment serviceInfo on StoreService {
+  code
+  description
+  shortDescription
+  __typename
+}
+
+fragment openingDaysInfo on StoreOpeningDay {
+  dayName
+  type
+  date
+  nextWeekDate
+  openingHour {
+    ...storeOpeningHour
+    __typename
+  }
+  nextWeekOpeningHour {
+    ...storeOpeningHour
+    __typename
+  }
+  __typename
+}
+
+fragment storeOpeningHour on StoreOpeningHour {
+  date
+  openFrom
+  openUntil
+  __typename
+}
+"""
+        query = [
+            {
+                "operationName": "stores",
+                "query": gql_query,
+                "variables": {
+                    "filter": {},
+                    "size": 100,
+                    "start": page_number * 100,
+                },
+            }
+        ]
+        headers = {
+            "client-name": "alltheplaces",
+            "client-version": "1",
+        }
+        yield JsonRequest(url=gql_url, data=query, headers=headers, meta={"page_number": page_number})
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield from self.get_page(url, 0)
 
     def parse(self, response):
-        response.selector.remove_namespaces()
-        for url in response.xpath("//loc/text()").extract():
-            if re.search("/winkel/albert-heijn/", url):
-                yield scrapy.Request(url, callback=self.parse_store)
-
-    def parse_store(self, response):
-        for ldjson in response.xpath('//script[@type="application/ld+json"]/text()').extract():
-            data = json.loads(ldjson)
-            if data["@type"] != "GroceryStore":
-                continue
-
-            opening_hours = OpeningHours()
-            for spec in data["openingHoursSpecification"]:
-                opening_hours.add_range(spec["dayOfWeek"][:2], spec["opens"], spec["closes"])
-
-            properties = {
-                "ref": response.url,
-                "website": response.url,
-                "name": data["name"],
-                "phone": data["telephone"],
-                "lat": data["geo"]["latitude"],
-                "lon": data["geo"]["longitude"],
-                "addr_full": data["address"]["streetAddress"],
-                "city": data["address"]["addressLocality"],
-                "postcode": data["address"]["postalCode"],
-                "country": data["address"]["addressCountry"],
-                "opening_hours": opening_hours.as_opening_hours(),
-            }
-            yield Feature(**properties)
+        for location in response.json()[0]["data"]["stores"]["result"]:
+            item = DictParser.parse(location)
+            if ".nl/" in response.url:
+                item["website"] = "https://www.ah.nl/winkels?storeId=" + str(item["ref"])
+            elif ".be/" in response.url:
+                item["website"] = "https://www.ah.be/winkels?storeId=" + str(item["ref"])
+            oh = OpeningHours()
+            for day in location["openingDays"]:
+                if day["dayName"].title() in DAYS_NL and day.get("openingHour"):
+                    oh.add_range(
+                        DAYS_NL[day["dayName"].title()],
+                        day["openingHour"]["openFrom"],
+                        day["openingHour"]["openUntil"],
+                        "%H:%M",
+                    )
+            item["opening_hours"] = oh.as_opening_hours()
+            yield item
+        if response.json()[0]["data"]["stores"]["page"]["hasNextPage"]:
+            yield from self.get_page(response.url, response.meta["page_number"] + 1)
