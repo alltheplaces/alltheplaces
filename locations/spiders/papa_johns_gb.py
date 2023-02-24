e9933711e88104ba998cd3ab2d9691d393a97533
Robert Whittaker <rjw62@cantab.net>
2023-02-24 21:40:59 +0000

    Fix broken papa_johns_gb spider (#4872)
    * Correct store page regex, update deprecated function, remove broken position extraction (Google URL no longer contains lat/lon).
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/papa_johns_gb.py
+++ locations/spiders/papa_johns_gb.py
@@ -11,12 +11,13 @@ class PapaJohnsGBSpider(SitemapSpider, StructuredDataSpider):
         "brand_wikidata": "Q2759586",
         "country": "GB",
     }
+    download_delay = 1
     sitemap_urls = ["https://www.papajohns.co.uk/sitemap.xml"]
-    sitemap_rules = [(r"https:\/\/www\.papajohns\.co\.uk\/stores\/(.+)\/home\.aspx$", "parse_sd")]
+    sitemap_rules = [(r"https:\/\/www\.papajohns\.co\.uk\/stores\/([-\w]+)$", "parse_sd")]
     wanted_types = ["LocalBusiness"]
 
-    def inspect_item(self, item, response):
-        extract_google_position(item, response)
+    def post_process_item(self, item, response, ld_data):
+        # extract_google_position(item, response)
         item["website"] = response.url
 
         yield item
