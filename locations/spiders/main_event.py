ab8004e87019b853dd101f0c84f885385ec8b2be
Romeo-PHILLIPS <92714362+Romeo-PHILLIPS@users.noreply.github.com>
2023-02-25 03:18:54 +0100

    feat: Main Event Update (#4880)
    * feat: Main Event Update
    
    * feat: change id
    
    * fix: lat/lon

--- locations/spiders/main_event.py
+++ locations/spiders/main_event.py
@@ -1,72 +1,25 @@
-import json
 import re
 
-import scrapy
+from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class MainEventSpider(scrapy.Spider):
+class MainEventSpider(SitemapSpider, StructuredDataSpider):
     name = "main_event"
     item_attributes = {
         "brand": "Main Event Entertainment",
         "brand_wikidata": "Q56062981",
     }
-    allowed_domains = ["mainevent.com"]
     download_delay = 0.2
-    start_urls = ("https://www.mainevent.com/sitemap.xml",)
+    sitemap_urls = ["https://www.mainevent.com/sitemap.xml"]
+    sitemap_rules = [(r"\/locations\/", "parse_sd")]
 
-    def parse(self, response):
-        response.selector.remove_namespaces()
-        urls = response.xpath("//url/loc/text()").extract()
-        for url in urls:
-            if re.match(r".*/locations/.+$", url):
-                yield scrapy.Request(url=url, callback=self.parse_location, meta={"url": url})
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["ref"] = item["website"]
+        latitude = re.search(r'\\"latitude\\":(-?\d+\.\d+)', response.text).group(1)
+        longitude = re.search(r'\\"longitude\\":(-?\d+\.\d+)', response.text).group(1)
+        item["lat"] = float(latitude)
+        item["lon"] = float(longitude)
 
-    def parse_location(self, response):
-        store_data = response.xpath('//script[@type="application/ld+json"]/text()').extract_first()
-
-        # Stores pending "Grand Opening" won't have this
-        if not store_data:
-            return
-
-        store_json = json.loads(store_data)
-
-        # \\"latitude\\":33.318367794546674,\\"longitude\\":-111.74067152513123,
-        map_data = response.xpath("//script[contains(text(), 'googleInfo')]/text()").extract_first()
-
-        yield Feature(
-            ref=response.url.split("/")[-2],
-            name=store_json["name"],
-            lat=re.search('.*latitude\\\\":(-?\\d+\\.\\d+),.*', map_data).group(1),
-            lon=re.search('.*longitude\\\\":(-?\\d+\\.\\d+),.*', map_data).group(1),
-            addr_full=store_json["address"]["streetAddress"],
-            city=store_json["address"]["addressLocality"],
-            state=store_json["address"]["addressRegion"],
-            postcode=store_json["address"]["postalCode"],
-            country=store_json["address"]["addressCountry"],
-            phone=store_json["telephone"],
-            website=response.url,
-            opening_hours=self.parse_hours(store_json["openingHoursSpecification"]),
-        )
-
-    def parse_hours(self, hours):
-        opening_hours = OpeningHours()
-
-        for spec in hours:
-            if isinstance(spec["dayOfWeek"], list):
-                for day in spec["dayOfWeek"]:
-                    opening_hours.add_range(
-                        day=day[0:2],
-                        open_time=spec["opens"],
-                        close_time=spec["closes"],
-                    )
-            else:
-                opening_hours.add_range(
-                    day=spec["dayOfWeek"][0:2],
-                    open_time=spec["opens"],
-                    close_time=spec["closes"],
-                )
-
-        return opening_hours.as_opening_hours()
+        yield item
