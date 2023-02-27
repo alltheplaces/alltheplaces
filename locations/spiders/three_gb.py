562b81d84728f40623d67f877d9d9d9763be3bf6
Robert Whittaker <rjw62@cantab.net>
2023-02-27 14:56:08 +0000

    New spider for Three (UK mobile phone stores) (#4886)
    * New spider for Three (UK mobile phone stores)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/three_gb.py
@@ -0,0 +1,26 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class ThreeGB(SitemapSpider, StructuredDataSpider):
+    name = "three_gb"
+    item_attributes = {
+        "brand": "Three",
+        "brand_wikidata": "Q407009",
+        "country": "GB",
+    }
+    sitemap_urls = ["https://locator.three.co.uk/sitemap.xml"]
+    sitemap_rules = [
+        (
+            r"^https:\/\/locator\.three\.co\.uk\/(london|london-&-ni|midlands|north|south)\/([-a-z]+)\/([-0-9a-z']+)$",
+            "parse_sd",
+        )
+    ]
+    wanted_types = ["MobilePhoneStore"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if " closed " in item["name"].lower():
+            # Some closed stores still have pages
+            return
+        yield item
