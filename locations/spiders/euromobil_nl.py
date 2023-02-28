8995a6d10e1e4736ba8054dac9ddace309c0560e
Romeo-PHILLIPS <92714362+Romeo-PHILLIPS@users.noreply.github.com>
2023-02-28 19:42:28 +0100

    feat: Euromobil NL (#4894)

---
+++ locations/spiders/euromobil_nl.py
@@ -0,0 +1,35 @@
+import json
+import re
+
+import scrapy
+
+from locations.items import Feature
+
+
+class EuroMobilNLSpider(scrapy.Spider):
+    name = "euromobil_nl"
+    start_urls = ["https://euromobil.nl/contact/"]
+
+    item_attributes = {"brand": "Euromobil"}
+
+    def parse(self, response, **kwargs):
+        pattern = r"var\s+partners\s*=\s*(\[.*?\]);\s*var"
+        raw = response.xpath("//script[contains(., 'var stores =')]/text()")
+        stores_json = json.loads(re.search(pattern, raw.extract_first(), re.DOTALL).group(1))
+        for store in stores_json:
+            coordinates = store.get("geo")
+            yield Feature(
+                {
+                    "ref": store.get("nummer"),
+                    "name": store.get("title"),
+                    "street_address": store.get("adres"),
+                    "addr_full": coordinates.get("address"),
+                    "phone": store.get("phone"),
+                    "email": store.get("email"),
+                    "postcode": store.get("zip"),
+                    "city": store.get("plaats"),
+                    "website": store.get("website"),
+                    "lat": coordinates.get("lat"),
+                    "lon": coordinates.get("lng"),
+                }
+            )
