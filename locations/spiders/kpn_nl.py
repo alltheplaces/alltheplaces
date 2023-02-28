f01b6bfbc57cf2262dd5984dcb9758afae3c5999
Romeo-PHILLIPS <92714362+Romeo-PHILLIPS@users.noreply.github.com>
2023-02-28 19:43:17 +0100

    feat: KPN NL (#4895)

---
+++ locations/spiders/kpn_nl.py
@@ -0,0 +1,40 @@
+import scrapy
+
+from locations.hours import DAYS_NL, OpeningHours
+from locations.items import Feature
+
+
+class KpnNLSpider(scrapy.Spider):
+    name = "kpn_nl"
+    start_urls = ["https://www.kpn.com/w3/rest/storelocator/stores"]
+
+    item_attributes = {"brand": "KPN", "brand_wikidata": "Q338633"}
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"referer": "https://www.kpn.com/w3/vind-een-winkel/"}}
+
+    def parse(self, response, **kwargs):
+        for store in response.json().get("stores"):
+            oh = OpeningHours()
+            for day in DAYS_NL.keys():
+                if not store.get(f"{day.lower()}_open"):
+                    continue
+                open = store.get(f"{day.lower()}_open")
+                close = store.get(f"{day.lower()}_dicht")
+                oh.add_range(day=DAYS_NL.get(day), open_time=open, close_time=close, time_format="%H:%M")
+            coordinates = store.get("locatie")
+            yield Feature(
+                {
+                    "ref": store.get("winkelnummer"),
+                    "name": store.get("formulenaam"),
+                    "street": store.get("straat"),
+                    "housenumber": store.get("huisnummer"),
+                    "street_address": ", ".join([store.get("huisnummer"), store.get("straat")]),
+                    "phone": store.get("phone"),
+                    "email": store.get("email"),
+                    "postcode": store.get("postcode"),
+                    "city": store.get("plaats"),
+                    "website": f"https://www.kpn.com{store.get('link')}",
+                    "lat": coordinates.get("lat"),
+                    "lon": coordinates.get("lng"),
+                    "opening_hours": oh,
+                }
+            )
