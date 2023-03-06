c0a587cd7c96bc4518e346a4636915dda14bb328
David Hicks <david@hicks.id.au>
2023-03-07 03:48:40 +1100

    specsavers_fi: clean up OpeningHours code

--- locations/spiders/specsavers_fi.py
+++ locations/spiders/specsavers_fi.py
@@ -35,7 +35,7 @@ class SpecsaversFISpider(CrawlSpider):
             "website": response.url,
         }
 
-        properties["opening_hours"] = OpeningHours()
+        oh = OpeningHours()
         hours_raw = (
             (" ".join(response.xpath('//tr[@itemprop="openingHours"]/@content').getall()))
             .replace("Suljettu", "0:00 - 0:00")
@@ -46,6 +46,7 @@ class SpecsaversFISpider(CrawlSpider):
         for day in hours_raw:
             if day[1] == "0:00" and day[2] == "0:00":
                 continue
-            properties["opening_hours"].add_range(DAYS_FI[day[0].title()], day[1].upper(), day[2].upper())
+            oh.add_range(DAYS_FI[day[0].title()], day[1].upper(), day[2].upper())
+        properties["opening_hours"] = oh.as_opening_hours()
 
         yield Feature(**properties)
