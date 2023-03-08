9db3d38c3a7ae94d6f10fbe4b54be2fb2cfed133
Charles Dassonville <35760973+charlesdassonville@users.noreply.github.com>
2023-03-08 16:03:22 +0100

    feat: petit bateau (#4947)
    * feat: petit bateau
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: pr review 1
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: charlesdassonville <charlesdassonville@yahoo.fr>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/petit_bateau.py
@@ -0,0 +1,11 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class PetitBateauSpider(SitemapSpider, StructuredDataSpider):
+    name = "petit_bateau"
+    item_attributes = {"brand": "Petit Bateau", "brand_wikidata": "Q3377090"}
+    sitemap_urls = ["https://stores.petit-bateau.com/sitemap.xml"]
+    sitemap_rules = [(r"https://stores\.petit-bateau\.com/(?:\w+)/(?:\w+)/(?:\w+)/(?:\w+)", "parse_sd")]
+    wanted_types = ["ClothingStore"]
