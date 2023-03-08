bcefdd9008ce6a3f981a18cee39fe0743232d8a6
David Hicks <david@hicks.id.au>
2023-03-09 08:20:34 +1100

    Add Amasty Store Locator store finder and some example store spiders (#4950)
    * Add Amasty Store Locator store finder
    
    * Add Leyland SDM (GB) (33 stores)
    
    * Add Munch's Supply (US) (17 stores)
    
    * Add Nick Scali Furniture (AU/NZ) (71 stores)
    
    * Add The Coffee Bean & Tea Leaf (SG) (70 stores)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/storefinders/amasty_store_locator.py
@@ -0,0 +1,48 @@
+from scrapy import Request, Selector, Spider
+
+from locations.dict_parser import DictParser
+
+# This store finder is provided as an extension either for Magento
+# v1 or Magento v2. Some limited documentation on the store finder
+# is provided as:
+# v1: https://amasty.com/docs/doku.php?id=magento_1:store_locator
+# v2: https://amasty.com/docs/doku.php?id=magento_2:store_locator
+#
+# To use this spider, specify one or more allowed_domain, and the
+# default path /amlocator/index/ajax/ will be requested. If for
+# some reason the path is not default, it can be overridden by
+# supplying full paths in start_urls.
+#
+# JSON blobs of location are returned but are highly variable as
+# to how many fields are provided. There is often a popup_html
+# field in this JSON blob that contains HTML data relating to the
+# location and whilst some of this is parsed by default, often there
+# are fields such as addresses and phone numbers that need to be
+# manually extracted by overriding the parse_item function of this
+# store finder and then updating item fields using
+# popup_html.xpath(...) queries.
+
+
+class AmastyStoreLocatorSpider(Spider):
+    def start_requests(self):
+        if len(self.start_urls) == 0:
+            for domain in self.allowed_domains:
+                yield Request(url=f"https://{domain}/amlocator/index/ajax/")
+        else:
+            for url in self.start_urls:
+                yield Request(url=url)
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["items"]:
+            item = DictParser.parse(location)
+            popup_html = Selector(text=location["popup_html"])
+            if not item["name"]:
+                item["name"] = " ".join(
+                    popup_html.xpath('//div[contains(@class, "amlocator-title")]//text()').get().split()
+                )
+            if not item["website"]:
+                item["website"] = popup_html.xpath('//a[contains(@class, "amlocator-link")]/@href').get()
+            yield from self.parse_item(item, location, popup_html)
+
+    def parse_item(self, item, location, popup_html):
+        yield item
