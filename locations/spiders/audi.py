1c8f089c5a966b2b757d68e09aaf996c818ebc68
charlesdassonville <charlesdassonville@yahoo.fr>
2023-03-08 17:43:03 +0100

    feat: audi improved world wide

--- locations/spiders/audi.py
+++ locations/spiders/audi.py
@@ -1,8 +1,6 @@
+import requests
 from scrapy import Spider
 from scrapy.http import JsonRequest
-
-from locations.country_utils import CountryUtils
-from locations.geo import point_locations
 from locations.items import Feature
 
 
@@ -13,117 +11,44 @@ class AudiSpider(Spider):
         "brand_wikidata": "Q23317",
     }
     graphql_url = "https://dev-dealer-graphql.apps.emea.vwapps.io/"
-    countries = [
-        "ALB",
-        "ARE",
-        "ARG",
-        "ARM",
-        "AUS",
-        "AUT",
-        "AZE",
-        "BEL",
-        "BGR",
-        "BHR",
-        "BIH",
-        "BLR",
-        "BRA",
-        "BRN",
-        "CAN",
-        "CHE",
-        "CHL",
-        "CHN",
-        "CYP",
-        "CZE",
-        "DEU",
-        "DNK",
-        "ESP",
-        "EST",
-        "FIN",
-        "FRA",
-        "GBR",
-        "GEO",
-        "GRC",
-        "HKG",
-        "HRV",
-        "HUN",
-        "IDN",
-        "IND",
-        "IRL",
-        "ISL",
-        "ISR",
-        "ITA",
-        "JOR",
-        "JPN",
-        "KAZ",
-        "KOR",
-        "KWT",
-        "LBN",
-        "LTU",
-        "LUX",
-        "LVA",
-        "MDA",
-        "MEX",
-        "MKD",
-        "MLT",
-        "MNE",
-        "MYS",
-        "NLD",
-        "NOR",
-        "NZL",
-        "OMN",
-        "PHL",
-        "POL",
-        "PRT",
-        "QAT",
-        "ROU",
-        "RUS",
-        "SAU",
-        "SGP",
-        "SRB",
-        "SVK",
-        "SVN",
-        "SWE",
-        "THA",
-        "TUR",
-        "TWN",
-        "UKR",
-        "USA",
-        "UZB",
-        "ZAF",
-    ]
 
     def start_requests(self):
-        for iso_3 in self.countries:
-            iso_2 = CountryUtils().to_iso_alpha2_country_code(country_str=iso_3)
-            for lat, lon in point_locations("eu_centroids_120km_radius_country.csv", iso_2):
-                yield JsonRequest(
-                    url=self.graphql_url,
-                    method="POST",
-                    data={
-                        "operationName": "Dealer",
-                        "variables": {"country": iso_3, "lat": float(lat), "lng": float(lon)},
-                        "query": "query Dealer($country: String, $services: String, $lng: Float!, $lat: Float!) {\n  dealersByGeoLocation(\n  limit: 100\n    country: $country\n    serviceFilter: $services\n  lat: $lat\n lng: $lng\n) {\n    dealers {\n      ...FragmentDealerFields\n      __typename\n    }\n    meta {\n      resultCount\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment FragmentDealerFields on Dealer {\n  dealerId\n  vCardUrl\n matPrimaryCode\n  kvpsPartnerKey\n  name\n  address\n  latitude \n  longitude\n houseNumber\n  street\n region\n  city\n  zipCode\n  services\n  distance\n  phone\n  additionalData {\n    displayName\n    locationName\n    __typename\n  }\n  openingHours {\n    departments {\n      id\n      departmentName\n      openingHours {\n        id\n        open\n        timeRanges {\n          openTime\n           \n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n",
-                    },
-                    meta={"country_code": iso_2},
-                )
+        market_request = requests.post(
+            url=self.graphql_url,
+            json={
+                "query": "query Dealer {\n  marketInfo {\n    markets {\n      market\n      scope\n    }\n  }\n}\n",
+            }
+        )
+        market_data = market_request.json().get('data').get('marketInfo').get('markets')
+
+        for market in market_data:
+            yield JsonRequest(
+                url=self.graphql_url,
+                method="POST",
+                data={
+                    "operationName": "Dealer",
+                    "variables": {"market": market.get('market')},
+                    "query": "query Dealer ($market: Market!) {\n    dealersByMarket(market: $market) {\n        dealers {\n            dealerId\n            name\n            services\n            latitude\n            longitude\n            phone: phoneInternational\n            fax: faxInternational\n            email\n            url\n            operator: chainId\n            country\n            openingHours {\n                departments {\n                    departmentName\n                    openingHours {\n                        timeRanges {\n                            openTime\n                            closeTime\n                        }\n                    }\n                }\n            }\n            address\n            houseNumber\n            street\n            city\n            zipCode\n        }\n    }\n}",
+                }
+            )
 
     def parse(self, response):
         data = response.json()
-        store_data = data.get("data").get("dealersByGeoLocation").get("dealers")
-        for store in store_data:
-            properties = {
-                "ref": store.get("dealerId"),
-                "name": store.get("name"),
-                "addr_full": ", ".join(store.get("address")),
-                "housenumber": store.get("houseNumber"),
-                "street": store.get("street"),
-                "street_address": store.get("address")[0],
-                "city": store.get("city"),
-                "state": store.get("region"),
-                "phone": store.get("phone"),
-                "postcode": store.get("zipCode"),
-                "country": response.meta.get("country_code"),
-                "lat": store.get("latitude"),
-                "lon": store.get("longitude"),
-            }
-            yield Feature(**properties)
+        store_data = data.get("data").get("dealersByMarket").get("dealers")
+        if store_data:
+            for store in store_data:
+                properties = {
+                    "ref": store.get("dealerId"),
+                    "name": store.get("name"),
+                    "addr_full": ", ".join(store.get("address")),
+                    "housenumber": store.get("houseNumber"),
+                    "street": store.get("street"),
+                    "street_address": store.get("address")[0],
+                    "city": store.get("city"),
+                    "state": store.get("region"),
+                    "phone": store.get("phone"),
+                    "postcode": store.get("zipCode"),
+                    "lat": store.get("latitude"),
+                    "lon": store.get("longitude"),
+                }
+                yield Feature(**properties)
