4a368683ec719b801ace54229b97134e13eba6fd
charlesdassonville <charlesdassonville@yahoo.fr>
2023-03-09 08:28:09 +0100

    feat: petit bateau improved

--- locations/spiders/petit_bateau.py
+++ locations/spiders/petit_bateau.py
@@ -7,5 +7,5 @@ class PetitBateauSpider(SitemapSpider, StructuredDataSpider):
     name = "petit_bateau"
     item_attributes = {"brand": "Petit Bateau", "brand_wikidata": "Q3377090"}
     sitemap_urls = ["https://stores.petit-bateau.com/sitemap.xml"]
-    sitemap_rules = [(r"https://stores.petit-bateau.com/", "parse_sd")]
+    sitemap_rules = [(r"https:\/\/stores\.petit-bateau\.com\/[a-z\-]{3,}\/.+", "parse_sd")]
     wanted_types = ["ClothingStore"]
