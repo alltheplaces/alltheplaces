886c688c4dc7faf20ff0fca25a603d2f46bde25b
David Hicks <david@hicks.id.au>
2023-03-09 17:27:43 +1100

    Add Freedom Furniture (AU/NZ) (61 stores)

---
+++ locations/spiders/freedom_furniture.py
@@ -0,0 +1,50 @@
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_EN, OpeningHours
+
+
+class FreedomFurnitureSpider(Spider):
+    name = "freedom_furniture"
+    item_attributes = {"brand": "Freedom Furniture", "brand_wikidata": "Q5500546"}
+    allowed_domains = ["api-prod.freedom.com.au"]
+    start_urls = [
+        "https://api-prod.freedom.com.au/greenlitrest/v2/freedomfurniture/stores/country/AU?fields=FULL&lang=en&curr=AUD",
+        "https://api-prod.freedom.com.au/greenlitrest/v2/freedomnewzealand/stores/country/NZ?fields=FULL&lang=en&curr=NZD",
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield JsonRequest(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for location in response.json()["pointOfServices"]:
+            item = DictParser.parse(location)
+            item["ref"] = location["name"]
+            item["name"] = location["displayName"]
+            if item["country"] == "NZ":
+                item.pop("state")
+                item["website"] = "https://www.freedomfurniture.co.nz/store-finder/stores/" + item["ref"]
+            elif item["country"] == "AU":
+                item["state"] = location["address"]["region"]["name"]
+                item["website"] = "https://www.freedom.com.au/store-finder/stores/" + item["ref"]
+            item["phone"] = location["address"]["phone"]
+            if "storeEmail" in location:
+                item["email"] = location["storeEmail"]
+            
+            if "openingHours" in location:
+                oh = OpeningHours()
+                for day in location["openingHours"]["weekDayOpeningList"]:
+                    if day["closed"]:
+                        continue
+                    open_time = day["openingTime"]["formattedHour"].upper()
+                    if ":" not in open_time:
+                        open_time = open_time.replace(" AM", ":00 AM").replace(" PM", ":00 PM")
+                    close_time = day["closingTime"]["formattedHour"].upper()
+                    if ":" not in close_time:
+                        close_time = close_time.replace(" AM", ":00 AM").replace(" PM", ":00 PM")
+                    oh.add_range(DAYS_EN[day["weekDay"]], open_time, close_time, "%I:%M %p")
+                item["opening_hours"] = oh.as_opening_hours()
+
+            yield item
