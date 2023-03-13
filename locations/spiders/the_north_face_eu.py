4bcd3ee8f037f7c1f9acf5fec1fe27bb647249c3
Romeo <romeo@echo-analytics.io>
2023-03-13 18:35:03 +0100

    feat: The North Face Europe

---
+++ locations/spiders/the_north_face_eu.py
@@ -0,0 +1,26 @@
+from locations.storefinders.where2getit import Where2GetItSpider
+
+# The spider gets Stores and Outlets in Europe
+# A single query for CA collects both CA and US
+
+
+class TheNorthFaceSpider(Where2GetItSpider):
+    name = "the_north_face_eu"
+    item_attributes = {"brand": "The North Face", "brand_wikidata": "Q152784"}
+    w2gi_id = "3A992F50-193E-11E5-91BC-C90E919C4603"
+    w2gi_filter = {
+        "or": {"northface": {"eq": ""}, "retailstore": {"eq": ""}, "outletstore": {"eq": ""}},
+        "and": {
+            "or": {
+                "youth": {"eq": ""},
+                "apparel": {"eq": ""},
+                "mountain_athletics": {"eq": ""},
+                "footwear": {"eq": ""},
+                "equipment": {"eq": ""},
+                "summit": {"eq": ""},
+                "mt": {"eq": ""},
+            }
+        },
+    }
+    w2gi_query = "Brussels"
+    w2gi_country_code = "BE"
