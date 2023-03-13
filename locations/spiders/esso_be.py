58454f5754e44d9f9a3708c8dabe313fc92eea39
charlesdassonville <charlesdassonville@yahoo.fr>
2023-03-13 18:37:39 +0100

    feat: esso

--- locations/spiders/esso_be.py
+++ locations/spiders/esso_be.py
@@ -16,7 +16,6 @@ class EssoSpider(Spider):
 
     def start_requests(self):
         for lat, lng in points(grid_size=5, a2_country_codes=["BE"]):
-            print(lat, lng)
             be_url = f"https://www.esso.be/fr-be/api/RetailLocator/GetRetailLocations?DataSource=RetailGasStations&country=BE&Latitude1={lat-0.5}&Longitude1={lng-1}&Latitude2={lat+0.5}&Longitude2={lng+1}"
             market_request = JsonRequest(url=be_url, method="GET")
             yield market_request
