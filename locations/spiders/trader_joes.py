41cbf7fcf574c9b66a736219e61fd58dd8ca3c74
Romeo <romeo@echo-analytics.io>
2023-03-13 12:59:34 +0100

    feat: Convert Trader Joes to Where2GetItSpider

--- locations/spiders/trader_joes.py
+++ locations/spiders/trader_joes.py
@@ -1,26 +1,9 @@
-import re
-
-import scrapy
-
-from locations.linked_data_parser import LinkedDataParser
+from locations.storefinders.where2getit import Where2GetItSpider
 
 
-class TraderJoesSpider(scrapy.Spider):
+class TraderJoesSpider(Where2GetItSpider):
     name = "trader_joes"
     item_attributes = {"brand": "Trader Joe's", "brand_wikidata": "Q688825"}
-    allowed_domains = ["traderjoes.com"]
-    start_urls = [
-        "https://locations.traderjoes.com/",
-    ]
-
-    def parse(self, response):
-        yield from response.follow_all(css="a.listitem")
-        for script in response.xpath('//script[@type="application/ld+json"]'):
-            text = script.root.text
-            # json with two kinds of comments
-            text = re.sub(r" +//.*", "", text)
-            text = re.sub(r"/\*.*?\*/", "", text, flags=re.S)
-            script.root.text = text
-        item = LinkedDataParser.parse(response, "GroceryStore")
-        if item is not None and item["ref"]:
-            yield item
+    w2gi_id = "8559C922-54E3-11E7-8321-40B4F48ECC77"
+    w2gi_query = "CA"
+    w2gi_filter = {"or": {"wine": {"eq": ""}, "beer": {"eq": ""}, "liquor": {"eq": ""}, "comingsoon": {"eq": ""}}}
