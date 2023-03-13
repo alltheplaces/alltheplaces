11166df200615105e791d0bbaa262d8f6adaf7c2
Romeo <romeo@echo-analytics.io>
2023-03-13 12:47:08 +0100

    feat: MAN Truck & Bus

---
+++ locations/spiders/man_truck_and_bus.py
@@ -0,0 +1,32 @@
+import scrapy
+
+from locations.items import Feature
+
+
+class ManTruckAndBusSpider(scrapy.Spider):
+    name = "man_truck_and_bus"
+    start_urls = ["https://settlement.man.eu/settlement/public/mui/world.js?"]
+
+    item_attributes = {"brand": "MAN Truck & Bus", "brand_wikidata": "Q708667"}
+
+    def parse(self, response, **kwargs):
+        for store in response.json():
+            name_data = store[1]
+            address_data = store[2]
+            coordinates_data = store[3]
+            phone_data = store[4]
+            name = name_data[1] if name_data[1] != "" else name_data[0]
+            if len(coordinates_data) > 0:
+                yield Feature(
+                    {
+                        "ref": store[0],
+                        "name": name,
+                        "street_address": address_data[0],
+                        "phone": phone_data[0],
+                        "postcode": address_data[1],
+                        "city": address_data[2],
+                        "country": address_data[3],
+                        "lat": coordinates_data[1],
+                        "lon": coordinates_data[0],
+                    }
+                )
