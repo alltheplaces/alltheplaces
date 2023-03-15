2c1edd9e6a5779a329d43e4102ea1511e7291083
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-03-15 02:38:55 +0000

    Add Lidl Spiders (#4981)

---
+++ locations/spiders/lidl_hr.py
@@ -0,0 +1,11 @@
+from locations.spiders.lidl_gb import LidlGBSpider
+from locations.storefinders.virtualearth import VirtualEarthSpider
+
+
+class LidlHRSpider(VirtualEarthSpider):
+    name = "lidl_hr"
+    item_attributes = LidlGBSpider.item_attributes
+
+    dataset_id = "d82c19ca83104facab354f376bf4312b"
+    dataset_name = "Filialdaten-HR/Filialdaten-HR"
+    key = "AoX_2ZTHnC3kIskrKATT7ZcheF4L7vHMCaHTqcTpjmaOm3kIGwASRnEqLpeH7_-S"
