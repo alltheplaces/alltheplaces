e8b0e8a0bca213ef895492076d05ae46e85da773
David Hicks <david@hicks.id.au>
2023-03-16 04:11:07 +1100

    Add Reece brands (AU/NZ) (561 stores)

---
+++ locations/spiders/reece.py
@@ -0,0 +1,38 @@
+from scrapy import Selector
+from scrapy.spiders import SitemapSpider
+
+from locations.hours import OpeningHours
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class ReeceSpider(SitemapSpider, StructuredDataSpider):
+    name = "reece"
+    item_attributes = {"brand": "Reece", "brand_wikidata": "Q29025524"}
+    allowed_domains = [
+        "www.reece.com.au",
+        "www.reece.co.nz",
+    ]
+    sitemap_urls = [
+        "https://www.reece.com.au/sitemaps/store_sitemap.xml",
+        "https://www.reece.co.nz/sitemaps/store_sitemap.xml",
+    ]
+    wanted_types = ["Organization"]
+
+    def post_process_item(self, item, response, ld_data):
+        item["ref"] = response.xpath('//input/@data-brcode').get()
+        item["name"] = response.xpath('//input/@data-bname').get()
+        item["brand"] = Selector(text=response.xpath('//input/@data-cname').get()).xpath("//text()").get()
+        item["lat"] = response.xpath('//input/@data-lat').get()
+        item["lon"] = response.xpath('//input/@data-lon').get()
+        if "www.reece.co.nz" in response.url:
+            item.pop("state")
+        if response.xpath('//input/@data-phone'):
+            item["phone"] = response.xpath('//input/@data-phone').get()
+        item["website"] = response.url
+        item.pop("image")
+
+        item["opening_hours"] = OpeningHours()
+        hours_string = " ".join(response.xpath('//tr[contains(@class, "branch-hours")]/td/text()').getall())
+        item["opening_hours"].add_ranges_from_string(hours_string)
+
+        yield item
