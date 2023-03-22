f96961309015849cdd0476bb1f28d20195bab6de
Cj Malone <CjMalone@mail.com>
2023-03-22 13:45:42 +0000

    Fix Coen opening hours

--- locations/spiders/coen_markets.py
+++ locations/spiders/coen_markets.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.selector.unified import Selector
 
+from locations.hours import OpeningHours, day_range, sanitise_day
 from locations.items import Feature
 
 
@@ -20,7 +21,7 @@ class CoenMarketsSpider(scrapy.Spider):
 
     def parse_store(self, response, js):
         props = {}
-        props["addr_full"] = Selector(text=js["address"]).xpath("//p/text()").get()
+        props["street_address"] = Selector(text=js["address"]).xpath("//p/text()").get()
         props["ref"] = js["url_title"]
         props["lat"] = js["coordinates"][0]
         props["lon"] = js["coordinates"][1]
@@ -28,6 +29,30 @@ class CoenMarketsSpider(scrapy.Spider):
         props["state"] = js["state"]
         props["postcode"] = js["zip"]
         props["phone"] = js["phone_number"]
-        hours = response.css(".hours p:not(:empty)").xpath("text()").get()
-        props["opening_hours"] = hours
+        if hours := response.css(".hours p:not(:empty)").xpath("text()").get():
+            props["opening_hours"] = self.parse_hours(hours)
         return Feature(**props)
+
+    @staticmethod
+    def parse_hours(rules: str) -> OpeningHours():
+        rules = rules.replace("HOURS", "Mo-Su").replace(" to ", " - ")
+
+        days, times = rules.split(":", maxsplit=1)
+        start_day, end_day = days.split("-")
+        start_day = sanitise_day(start_day)
+        end_day = sanitise_day(end_day)
+        start_time, end_time = times.split("-")
+        start_time = CoenMarketsSpider.sanitise_time(start_time)
+        end_time = CoenMarketsSpider.sanitise_time(end_time)
+
+        if start_day and end_day and start_time and end_time:
+            oh = OpeningHours()
+            oh.add_days_range(day_range(start_day, end_day), start_time, end_time, time_format="%I:%M%p")
+            return oh
+
+    @staticmethod
+    def sanitise_time(time: str) -> str:
+        time = time.upper().replace("A ", "AM").strip()
+        if ":" not in time:
+            time = time[:-2] + ":00" + time[-2:]
+        return time
