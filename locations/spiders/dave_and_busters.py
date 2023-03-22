30be041a0b81af21b5f166d3301a1a595e32b17e
David Hicks <david@hicks.id.au>
2023-03-22 22:43:29 +1100

    dave_and_busters: fix spider

--- locations/spiders/dave_and_busters.py
+++ locations/spiders/dave_and_busters.py
@@ -6,12 +6,13 @@ from locations.dict_parser import DictParser
 class DaveAndBustersSpider(Spider):
     name = "dave_and_busters"
     item_attributes = {"brand": "Dave and Busters", "brand_wikidata": "Q5228205"}
-    start_urls = ["https://www.daveandbusters.com/bin/courses.json?mode=location"]
+    allowed_domains = ["www.daveandbusters.com"]
+    start_urls = ["https://www.daveandbusters.com/content/dnb-request/datadetails.json?mode=location"]
 
-    def parse(self, response, **kwargs):
+    def parse(self, response):
         for location in response.json()["locations"]:
-            if not location["websiteUrl"]:
-                continue  # "Test" entry
+            if "COMING SOON" in location["name"].upper():
+                continue
 
             location["address"]["street_address"] = ", ".join(
                 filter(None, [location["address"].pop("line1"), location["address"].pop("line2")])
