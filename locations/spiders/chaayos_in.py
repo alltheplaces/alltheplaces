bf0de1d8fddbede957553789e321ccfa2f1091f3
David Hicks <david@hicks.id.au>
2023-03-22 12:28:36 +1100

    Add Chaayos (IN) (109 stores) (#5013)
    * Add Chaayos (IN) (109 stores)
    
    * chaayos_in: add missing brand_wikidata

---
+++ locations/spiders/chaayos_in.py
@@ -0,0 +1,16 @@
+from locations.storefinders.storerocket import StoreRocketSpider
+
+
+class ChaayosINSpider(StoreRocketSpider):
+    name = "chaayos_in"
+    item_attributes = {"brand": "Chaayos", "brand_wikidata": "Q117235235"}
+    storerocket_id = "wgprwvz4XB"
+
+    def parse_item(self, item, location):
+        # remove unused/non-store-specific-value fields
+        item.pop("email")
+        item.pop("phone")
+        item.pop("facebook")
+        item.pop("twitter")
+        item["extras"].pop("instagram")
+        yield item
