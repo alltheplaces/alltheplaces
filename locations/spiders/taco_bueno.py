fb0a6b284de0d7d1c029f1b2be39fa5625cc6500
Cj Malone <CjMalone@mail.com>
2023-03-22 13:11:23 +0000

    Fix Taco Bueno opening hours

--- locations/spiders/taco_bueno.py
+++ locations/spiders/taco_bueno.py
@@ -1,65 +1,26 @@
-import re
-
 import scrapy
 from geonamescache import GeonamesCache
 
+from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
-
-regex_am = r"\s?([Aa][Mm])"
-regex_pm = r"\s?([Pp][Mm])"
 
 
 class TacobuenoSpider(scrapy.Spider):
     name = "bueno"
-    item_attributes = {
-        "brand": "Taco Bueno",
-        "brand_wikidata": "Q7673958",
-        "country": "US",
-    }
-    allowed_domains = ["buenoonthego.com"]
-    download_delay = 0.1
+    item_attributes = {"brand": "Taco Bueno", "brand_wikidata": "Q7673958"}
 
     def start_requests(self):
         for state in GeonamesCache().get_us_states():
             yield scrapy.Request(f"https://buenoonthego.com/mp/ndXTAL/searchByStateCode_JSON?stateCode='{state}'")
 
-    def convert_hours(self, hours):
-        converted_times = ""
-        if not hours:
-            return ""
-
-        if hours != "Closed":
-            from_hr, to_hr = (hr.strip() for hr in hours.split("-"))
-            if re.search(regex_am, from_hr):
-                from_hr = re.sub(regex_am, "", from_hr)
-                hour_min = from_hr.split(":")
-                if len(hour_min[0]) < 2:
-                    hour_min[0].zfill(2)
-                converted_times += (":".join(hour_min)) + " - "
-            else:
-                from_hr = re.sub(regex_pm, "", from_hr)
-                hour_min = from_hr.split(":")
-                if int(hour_min[0]) < 12:
-                    hour_min[0] = str(12 + int(hour_min[0]))
-                converted_times += (":".join(hour_min)) + " - "
-
-            if re.search(regex_am, to_hr):
-                to_hr = re.sub(regex_am, "", to_hr)
-                hour_min = to_hr.split(":")
-                if len(hour_min[0]) < 2:
-                    hour_min[0].zfill(2)
-                if int(hour_min[0]) == 12:
-                    hour_min[0] = "00"
-                converted_times += ":".join(hour_min)
-            else:
-                to_hr = re.sub(regex_pm, "", to_hr)
-                hour_min = to_hr.split(":")
-                if int(hour_min[0]) < 12:
-                    hour_min[0] = str(12 + int(hour_min[0]))
-                converted_times += ":".join(hour_min)
-        else:
-            converted_times += "off"
-        return converted_times
+    @staticmethod
+    def convert_hours(times: dict) -> OpeningHours:
+        if times == "Closed":
+            return
+        oh = OpeningHours()
+        start_time, end_time = times.split(" - ")
+        oh.add_days_range(DAYS, start_time, end_time, time_format="%I:%M %p")
+        return oh
 
     def parse(self, response):
         results = response.json()
@@ -67,16 +28,17 @@ class TacobuenoSpider(scrapy.Spider):
             for i in results:
                 ref = i["storeid"]
                 name = i["restaurantname"]
-                street = i["address1"]
+                street = ", ".join(filter(None, [i["address1"], i["address2"], i["address3"]]))
                 city = i["city"]
                 state = i["statecode"]
                 postcode = i["zipcode"]
+                country = i["country"]
                 phone = i["phone"]
                 lon = i["longitude"]
                 lat = i["latitude"]
+                # business_hours seems to hold bad data
                 hours = self.convert_hours(i["businesshours"])
-                addr_full = "{} {}, {} {}".format(street, city, state, postcode)
-
+                addr_full = ", ".join([street, city, state, postcode, country])
                 yield Feature(
                     ref=ref,
                     name=name,
@@ -84,6 +46,7 @@ class TacobuenoSpider(scrapy.Spider):
                     city=city,
                     state=state,
                     postcode=postcode,
+                    country=country,
                     addr_full=addr_full,
                     phone=phone,
                     lon=lon,
