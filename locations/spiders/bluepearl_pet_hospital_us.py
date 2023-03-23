9ee386769ff70c013934dc82e94c33568238fafc
David Hicks <david@hicks.id.au>
2023-03-24 08:01:48 +1100

    blue_pearl_pet_hospital: fix and rename to bluepearl_pet_hospital_us (#5041)
    * blue_pearl_pet_hospital: fix and rename to bluepearl_pet_hospital_us
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/bluepearl_pet_hospital_us.py
@@ -0,0 +1,28 @@
+from scrapy import Selector
+from scrapy.spiders import SitemapSpider
+
+from locations.hours import DAYS, OpeningHours
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class BluePearlPetHospitalUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "bluepearl_pet_hospital_us"
+    item_attributes = {"brand": "BluePearl Pet Hospital", "brand_wikidata": "Q4928764"}
+    allowed_domains = ["bluepearlvet.com"]
+    sitemap_urls = ["https://bluepearlvet.com/hospital-sitemap.xml"]
+    sitemap_rules = [(r"\/hospital\/[\w\-]+\/$", "parse_sd")]
+    wanted_types = ["LocalBusiness"]
+
+    def post_process_item(self, item, response, ld_data):
+        name_html = Selector(text=ld_data["name"])
+        item["name"] = " ".join((" ".join(name_html.xpath("//text()").getall())).split()).replace(
+            " - BluePearl Pet Hospital", ""
+        )
+        item.pop("twitter")
+        item.pop("facebook")
+        if "24/7" in ld_data["openingHours"]:
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_days_range(DAYS, "00:00", "23:59")
+        # else: not worth parsing because not even humans can interpret the
+        # unintelligible hours information (example: "Sun 7am-Thu 6 am")
+        yield item
