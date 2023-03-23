144680ee5cb60c30d951f4594fcefd6b4c4c87c3
David Hicks <david@hicks.id.au>
2023-03-24 07:49:21 +1100

    Add Yext storefinder (#5049)
    * Add Yext store finder
    
    * locations/dict_parser: add geocodedCoordinate as dict to query
    
    geocodedCoordinates is used by the Yext store finder.
    
    * Convert Specsavers AU and NZ to a single combined Yext spider
    
    * builders_firstsource: fix and rename to builders_firstsource_us
    
    The previous spider was broken. The new store locater uses Yext.
    
    * storefinders/yext: allow custom filter to be specified
    
    * storefinder/yext: don't overwrite custom API version
    
    * pet_suites: convert to YextSpider and rename to pet_suites_us
    
    Now returns a correct and higher number of locations: 73. Previously
    returned only 45 locations as the Yext API only ever returns a maximum
    of 50 results per query.
    
    * storefinders/yext: add check for closed stores and skip them
    
    * storefinders/yext: hours are optionally provided
    
    Some store locators such as Seven Eleven (7/11) Canada have some stores
    without opening hours specified at all (not even a placeholder to denote
    the store is completely closed). Modify the Yext spider to handle such
    situations where location["hours"] is undefined.
    
    * seven_eleven_ca: convert to YextSpider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/builders_firstsource_us.py
@@ -0,0 +1,28 @@
+from locations.storefinders.yext import YextSpider
+
+
+class BuildersFirstSourceUSSpider(YextSpider):
+    name = "builders_firstsource_us"
+    item_attributes = {"brand": "Builders FirstSource", "brand_wikidata": "Q27686100"}
+    api_key = "75d03100e13baa02575a046100dc4a15"
+
+    def parse_item(self, item, location):
+        if "c_locationStoreName" in location:
+            item["name"] = location["c_locationStoreName"]
+        if "c_websiteLocationName" in location:
+            item["website"] = (
+                "https://www.bldr.com/location/"
+                + location["c_websiteLocationName"].lower().replace(" ", "-")
+                + "/"
+                + location["meta"]["id"]
+            )
+        elif "c_locationStoreName" in location:
+            item["website"] = (
+                "https://www.bldr.com/location/"
+                + location["c_locationStoreName"].lower().replace(" ", "-")
+                + "/"
+                + location["meta"]["id"]
+            )
+        item.pop("twitter")
+        item["extras"].pop("contact:instagram")
+        yield item
