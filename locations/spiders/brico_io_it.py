95f95adef09ac0b5e206624e41ced2a5cb076789
David Hicks <david@hicks.id.au>
2023-03-27 18:49:59 +1100

    brico_io_it: simplify spider based on review feedback

--- locations/spiders/brico_io_it.py
+++ locations/spiders/brico_io_it.py
@@ -1,5 +1,3 @@
-import collections
-
 from scrapy import Spider
 
 from locations.dict_parser import DictParser
@@ -20,12 +18,10 @@ class Brico_ioITSpider(Spider):
             item = DictParser.parse(location)
             item["ref"] = location["code"]
             item["name"] = location["alias"]
-            address_fields = {k: v for k, v in DictParser.parse(location["address"]).items() if v}
-            print(address_fields)
             item["street_address"] = " ".join(
                 filter(None, [location["address"]["street1"], location["address"]["street2"]])
             )
-            item.update(address_fields)
+            item["country"] = location["address"]["idCountry"]
             for contact_method in location["contacts"]:
                 if contact_method["type"] == 1:
                     item["email"] = contact_method["destination"]
@@ -33,12 +29,10 @@ class Brico_ioITSpider(Spider):
                     item["phone"] = contact_method["destination"]
             item["website"] = "https://www.bricoio.it/it/it/brit/storelocator/store/" + item["ref"]
 
-            day_list = collections.deque(DAYS.copy())
-            day_list.rotate(1)
             item["opening_hours"] = OpeningHours()
             for timeslot in location["hours"]["timeslots"]:
                 open_time = str(timeslot["from"]["hours"]) + ":" + str(timeslot["from"]["minutes"]).zfill(2)
                 close_time = str(timeslot["to"]["hours"]) + ":" + str(timeslot["to"]["minutes"]).zfill(2)
-                item["opening_hours"].add_range(day_list[timeslot["dayOfWeek"]], open_time, close_time)
+                item["opening_hours"].add_range(DAYS[timeslot["dayOfWeek"] - 1], open_time, close_time)
 
             yield item
