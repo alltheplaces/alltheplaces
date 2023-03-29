92e4394875b4ea41aa356eb1c858e542ef1d687b
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-03-29 14:27:47 +0100

    Fix more opening hours (#5071)
    * Fix Michael Kors opening hours
    
    * Fix Lee's Famous Recipe Chicken opening hours
    
    * Fix Coto opening hours
    
    * Fix The Fresh Grocer opening hours
    
    * Fix Club Milanesa opening hours
    
    * Fix Quality Dairy opening hours

--- locations/spiders/lees_famous_recipe.py
+++ locations/spiders/lees_famous_recipe.py
@@ -1,144 +1,38 @@
 import re
 
-import scrapy
+from scrapy.spiders import SitemapSpider
 
+from locations.categories import Extras, apply_yes_no
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
-
-daysKey = {
-    "Monday": "Mo",
-    "Tuesday": "Tu",
-    "Wednesday": "We",
-    "Thursday": "Th",
-    "Friday": "Fr",
-    "Saturday": "Sa",
-    "Sunday": "Su",
-}
+from locations.spiders.vapestore_gb import clean_address
 
 
-class LeesFamousRecipeSpider(scrapy.Spider):
+class LeesFamousRecipeSpider(SitemapSpider):
     name = "lees_famous_recipe"
-    item_attributes = {
-        "brand": "Lee's Famous Recipe Chicken",
-        "brand_wikidata": "Q6512810",
-    }
+    item_attributes = {"brand": "Lee's Famous Recipe Chicken", "brand_wikidata": "Q6512810"}
     allowed_domains = ["www.leesfamousrecipe.com"]
-    start_urls = ("https://www.leesfamousrecipe.com/locations/all",)
+    sitemap_urls = ["https://www.leesfamousrecipe.com/sitemap.xml"]
+    sitemap_rules = [(r"/locations/.+/.+$", "parse")]
 
-    def parse_phone(self, phone):
-        phone = phone.replace(".", "")
-        phone = phone.replace(")", "")
-        phone = phone.replace("(", "")
-        phone = phone.replace("_", "")
-        phone = phone.replace("-", "")
-        phone = phone.replace("+", "")
-        phone = phone.replace(" ", "")
-        return phone
+    def parse(self, response, **kwargs):
+        item = Feature()
+        item["website"] = item["ref"] = response.url
+        item["phone"] = response.xpath('//div[@class="tel"]/text()').get()
+        item["name"] = "".join(response.xpath('//h1[@class="node-title"]//text()').getall())
+        item["street_address"] = clean_address(response.xpath('//div[@class="street-address"]/text()').get())
+        item["city"] = response.xpath('//span[@class="locality"]/text()').get()
+        item["state"] = response.xpath('//span[@class="region"]/text()').get()
+        item["postcode"] = response.xpath('//span[@class="postal-code"]/text()').get()
 
-    def store_hours(self, hours):
-        try:
-            days = hours.split(": ")[0].strip()
-            if "-" in days:
-                start_day = daysKey[days.split("-")[0]]
-                end_day = daysKey[days.split("-")[1]]
-                day_output = start_day + "-" + end_day
-            else:
-                day_output = daysKey[days]
+        item["opening_hours"] = OpeningHours()
+        for rule in response.xpath('//div[@class="group-hours"]//p/text()').getall():
+            if m := re.search(r"(\w+): (\d+:\d\d [ap]m) - (\d+:\d\d [ap]m)", rule):
+                item["opening_hours"].add_range(m.group(1), m.group(2), m.group(3), time_format="%I:%M %p")
 
-            both_hours = hours.split(": ")[1].replace(" ", "")
-            open_hours = both_hours.split("-")[0]
-            close_hours = both_hours.split("-")[1]
+        apply_yes_no(Extras.DRIVE_THROUGH, item, response.xpath('//li[@class="drive-thru"]').get())
 
-            if "am" in open_hours:
-                open_hours = open_hours.replace("am", "")
-                if ":" in open_hours:
-                    open_h = open_hours.split(":")[0]
-                    open_m = open_hours.split(":")[1]
-                else:
-                    open_h = open_hours
-                    open_m = "00"
-                open_hours = open_h + ":" + open_m
+        extract_google_position(item, response)
 
-            if "pm" in open_hours:
-                open_hours = open_hours.replace("pm", "")
-                if ":" in open_hours:
-                    open_h = open_hours.split(":")[0]
-                    open_m = open_hours.split(":")[1]
-                else:
-                    open_h = open_hours
-                    open_m = "00"
-                open_h = str(int(open_h) + 12)
-                open_hours = open_h + ":" + open_m
-
-            if "am" in close_hours:
-                close_hours = close_hours.replace("am", "")
-                if ":" in close_hours:
-                    close_h = close_hours.split(":")[0]
-                    close_m = close_hours.split(":")[1]
-                else:
-                    close_h = close_hours
-                    close_m = "00"
-                close_hours = close_h + ":" + close_m
-
-            if "pm" in close_hours:
-                close_hours = close_hours.replace("pm", "")
-                if ":" in close_hours:
-                    close_h = close_hours.split(":")[0]
-                    close_m = close_hours.split(":")[1]
-                else:
-                    close_h = close_hours
-                    close_m = "00"
-                close_h = str(int(close_h) + 12)
-                close_hours = close_h + ":" + close_m
-            return day_output + " " + open_hours.replace(" ", "") + "-" + close_hours + ";"
-        except (KeyError, IndexError):
-            return ""
-
-    def parse(self, response):
-        if "https://www.leesfamousrecipe.com/locations/all" == response.url:
-            for match in response.xpath("//div[contains(@class,'field-content')]/a/@href"):
-                request = scrapy.Request(match.extract())
-                yield request
-        else:
-            name_string = response.xpath("//h1[@class='node-title']/text()").extract_first().strip()
-            short_string = response.xpath("//h1[@class='node-title']/small/text()").extract_first()
-            if short_string is None:
-                short_string = ""
-            name_string = name_string + " " + short_string
-            name_string = name_string.strip()
-
-            google_map_src = response.xpath("//*[@id='block-system-main']/div/div/iframe").extract_first()
-            [lat_string, lon_string] = re.findall('center=(.*?)"', google_map_src)[0].split(",")
-
-            opening_hours_string = ""
-            first_hour_block = response.xpath(
-                "//div[contains(@class,'field-name-field-hours-summer')]/div/div/p/br/parent::p/text()"
-            )
-            for hourLine in first_hour_block:
-                opening_hours_string = opening_hours_string + " " + self.store_hours(hourLine.extract())
-            opening_hours_string = opening_hours_string.strip(";").strip()
-
-            if "british-columbia" in response.url:
-                country_string = "CA"
-                state_string = "BC"
-            else:
-                country_string = "US"
-                state_string = response.xpath("//div[contains(@class,'adr')]/div[2]/span[2]/text()").extract_first()
-
-            yield Feature(
-                ref=name_string,
-                addr_full=response.xpath("//div[@class='street-address']/text()").extract_first().strip(),
-                city=response.xpath("//div[@class='city-state-zip']/span[@class='locality']/text()")
-                .extract_first()
-                .strip(),
-                opening_hours=opening_hours_string,
-                state=state_string,
-                postcode=response.xpath("//div[@class='city-state-zip']/span[@class='postal-code']/text()")
-                .extract_first()
-                .strip(),
-                phone=self.parse_phone(
-                    response.xpath("//div[contains(@class,'adr')]/div[3]/text()").extract_first().strip()
-                ),
-                country=country_string,
-                lat=lat_string,
-                lon=lon_string,
-            )
+        yield item
--- locations/spiders/michaelkors.py
+++ locations/spiders/michaelkors.py
@@ -1,76 +1,10 @@
-import re
+from scrapy.spiders import SitemapSpider
 
-import scrapy
-
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class MichaelkorsSpider(scrapy.Spider):
+class MichaelkorsSpider(SitemapSpider, StructuredDataSpider):
     name = "michaelkors"
     item_attributes = {"brand": "Michael Kors", "brand_wikidata": "Q19572998"}
-    allowed_domains = ["locations.michaelkors.com"]
-    download_delay = 0.5
-    start_urls = ("https://locations.michaelkors.com/index.html",)
-
-    def parse_stores(self, response):
-        ref = re.findall(r"[^(\/)]+$", response.url)
-        if len(ref) > 0:
-            ref = ref[0].split(".")[0]
-        properties = {
-            "addr_full": response.xpath('//meta[@itemprop="streetAddress"]/@content').extract_first(),
-            "phone": response.xpath('normalize-space(//span[@itemprop="telephone"]/text())').extract_first(),
-            "city": response.xpath('//meta[@itemprop="addressLocality"]/@content').extract_first(),
-            "state": response.xpath('normalize-space(//abbr[@itemprop="addressRegion"]/text())').extract_first(),
-            "country": response.xpath('//abbr[@itemprop="addressCountry"]/text()').extract_first(),
-            "postcode": response.xpath('normalize-space(//span[@itemprop="postalCode"]/text())').extract_first(),
-            "ref": ref,
-            "website": response.url,
-            "lat": float(response.xpath('normalize-space(//meta[@itemprop="latitude"]/@content)').extract_first()),
-            "lon": float(response.xpath('normalize-space(//meta[@itemprop="longitude"]/@content)').extract_first()),
-        }
-        hours = response.xpath('//tr[@itemprop="openingHours"]/@content').extract()
-        if hours != []:
-            hours = "; ".join(hours)
-            properties["opening_hours"] = hours
-        yield Feature(**properties)
-
-    def parse_city_stores(self, response):
-        stores = response.xpath('//a[@class="LocationCard-storeLink"]/@href').extract()
-        if stores == []:
-            yield scrapy.Request(response.url, callback=self.parse_stores)
-        else:
-            for store in stores:
-                yield scrapy.Request(response.urljoin(store), callback=self.parse_stores)
-
-    def parse_state(self, response):
-        urls = response.xpath('//div[@class="c-directory-list-content-wrapper"]/ul/li/a/@href').extract()
-        for path in urls:
-            path_split = len(path.split("/"))
-            if path_split == 4:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
-            else:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
-
-    def parse_country(self, response):
-        urls = response.xpath('//div[@class="c-directory-list-content-wrapper"]/ul/li/a/@href').extract()
-        for path in urls:
-            path_split = len(path.split("/"))
-            if path_split == 2:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_state)
-            elif path_split == 3:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
-            else:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
-
-    def parse(self, response):
-        urls = response.xpath('//div[@class="c-directory-list-content-wrapper"]/ul/li/a/@href').extract()
-        for path in urls:
-            path_split = len(path.split("/"))
-            if path_split == 1:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_country)
-            elif path_split == 2:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_state)
-            elif path_split == 3:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_city_stores)
-            else:
-                yield scrapy.Request(response.urljoin(path), callback=self.parse_stores)
+    sitemap_urls = ["https://locations.michaelkors.com/sitemap.xml"]
+    sitemap_rules = [(r"https://locations.michaelkors.com/.+/.+/.+/.+.html$", "parse_sd")]
