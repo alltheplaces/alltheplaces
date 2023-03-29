a420bca2a4f077668dc32626fd52a75db3f82fd5
mickaelabdealy <mickael@echo-analytics.io>
2023-03-29 15:49:06 +0200

    fix: remove website

--- locations/spiders/united_colors_of_benetton.py
+++ locations/spiders/united_colors_of_benetton.py
@@ -6,3 +6,8 @@ class UnitedColorsOfBenettonSpider(WoosmapSpider):
     item_attributes = {"brand": "United Colors of Benetton", "brand_wikidata": "Q817139"}
     key = "woos-77ab54a0-3d40-3188-8f64-58f02485a654"
     custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Origin": "https://world.benetton.com/"}}
+
+
+    def parse_item(self, item, feature, **kwargs):
+        item.pop('website')
+        yield item
