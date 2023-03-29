1783000b20969624fe3e0c2fe0872edbbbf13d8b
mickaelabdealy <mickael@echo-analytics.io>
2023-03-29 17:20:12 +0200

    feat: mephisto be

---
+++ locations/spiders/mephisto_be.py
@@ -0,0 +1,14 @@
+from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
+
+
+class MephistoBESpider(AmastyStoreLocatorSpider):
+    name = "mephisto_be"
+    item_attributes = {"brand": "Mephisto", "brand_wikidata": "Q822975"}
+    allowed_domains = ["www.mephisto.com/be-fr"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+
+    def parse_item(self, item, location, popup_html):
+        # We don't want resellers. Only brand shop.
+        if item["name"] == "MEPHISTO-SHOP":
+            yield item
