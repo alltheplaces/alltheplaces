a95efe3dd11f7791af4f7700c9921cdbebf35d64
mickaelabdealy <mickael@echo-analytics.io>
2023-03-30 15:12:48 +0200

    feat: use pagination and AmastyStoreLocatorSpider

--- locations/spiders/mephisto_be.py
+++ locations/spiders/mephisto_be.py
@@ -1,13 +1,25 @@
+from scrapy import Request
+
 from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
-class MephistoBESpider(AmastyStoreLocatorSpider):
-    name = "mephisto_be"
+class MephistoSpider(AmastyStoreLocatorSpider):
+    name = "mephisto"
     item_attributes = {"brand": "Mephisto", "brand_wikidata": "Q822975"}
-    allowed_domains = ["www.mephisto.com/be-fr"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    start_urls = ["https://www.mephisto.com/be-fr/points-de-vente/"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url=url, callback=self.parse_website)
+
+    def parse_website(self, response, **kwargs):
+        page = "https://www.mephisto.com/be-fr/amlocator/index/ajax/"
+        count_page = int(response.xpath('//*[@id="am-page-count"]/text()').get())
+        for p in range(1, count_page + 1):
+            yield Request(url=page + f"?p={p}", callback=self.parse)
 
     def parse_item(self, item, location, popup_html):
         # We don't want resellers. Only brand shop.
         if item["name"] == "MEPHISTO-SHOP":
+            item["addr_full"] = f"{item['addr_full']}, {item['postcode']} {item['city']}"
             yield item
