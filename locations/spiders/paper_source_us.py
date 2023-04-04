d6a7d55f877051d8c11616380cb48a44233555c6
David Hicks <david@hicks.id.au>
2023-04-04 11:04:48 +1000

    paper_source_us: fix spider (#5094)
    * paper_source_us: fix spider
    
    Paper Source returns 125 stores, with partial information returned via
    Amasty Store Finder API and the bulk of information returned via XPath
    query of the website of each store.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/paper_source_us.py
+++ locations/spiders/paper_source_us.py
@@ -1,39 +1,55 @@
-import re
+from scrapy import Request
+from scrapy.http import FormRequest
 
-from scrapy import Selector, Spider
+from locations.hours import OpeningHours
+from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
-from locations.items import Feature
 
-
-class PaperSourceUSSpider(Spider):
+class PaperSourceUSSpider(AmastyStoreLocatorSpider):
     name = "paper_source_us"
     item_attributes = {"brand": "Paper Source", "brand_wikidata": "Q25000269"}
     start_urls = ["https://www.papersource.com/amlocator/index/ajax/"]
 
-    def parse(self, response, **kwargs):
-        for store in response.json()["items"]:
-            item = Feature()
-            item["ref"] = store["id"]
-            item["lat"] = store["lat"]
-            item["lon"] = store["lng"]
+    def start_requests(self):
+        formdata = {
+            "lat": "0",
+            "lng": "0",
+            "radius": "",
+            "product": "0",
+            "category": "0",
+            "attributes[0][name]": "3",
+            "attributes[0][value]": "",
+            "attributes[1][name]": "6",
+            "attributes[1][value]": "",
+            "sortByDistance": "1",
+        }
+        for url in self.start_urls:
+            yield FormRequest(url=url, formdata=formdata, headers={"X-Requested-With": "XMLHttpRequest"}, method="POST")
 
-            html = Selector(text=store["popup_html"])
+    def parse_item(self, item, location, popup_html):
+        item["website"] = popup_html.xpath('//a[contains(@class, "amlocator-link")]/@href').get()
+        yield Request(url=item["website"], meta={"item": item}, callback=self.add_location_details)
 
-            item["name"] = html.xpath('//div[@class="amlocator-title"]/a/text()').get()
-            item["website"] = html.xpath('//div[@class="amlocator-title"]/a/@href').get()
-
-            for line in html.xpath('//div[@class="amlocator-info-popup"]/text()').getall():
-                line = line.strip()
-                if match := re.match(r"(.*): (.*)", line):
-                    if match.group(1) == "Phone":
-                        item["phone"] = match.group(2)
-                    elif match.group(1) == "Address":
-                        item["street_address"] = match.group(2)
-                    elif match.group(1) == "City":
-                        item["city"] = match.group(2)
-                    elif match.group(1) == "State":
-                        item["state"] = match.group(2)
-                    elif match.group(1) == "Zip":
-                        item["postcode"] = match.group(2)
-
-            yield item
+    def add_location_details(self, response):
+        item = response.meta["item"]
+        item["ref"] = response.xpath(
+            '//div[contains(@data-amlocator-js, "location-attributes")]/div[2]/div[3]/div/span/text()'
+        ).get()
+        item["street_address"] = response.xpath(
+            '//div[contains(@class, "amlocator-location-info")]/div[4]/span[2]/text()'
+        ).get()
+        item["city"] = response.xpath('//div[contains(@class, "amlocator-location-info")]/div[3]/span[2]/text()').get()
+        item["postcode"] = response.xpath(
+            '//div[contains(@class, "amlocator-location-info")]/div[1]/span[2]/text()'
+        ).get()
+        item["phone"] = response.xpath(
+            '//div[contains(@class, "amlocator-location-info")]/div[contains(@class, "-contact")]/div[1]/a/text()'
+        ).get()
+        item["email"] = response.xpath(
+            '//div[contains(@class, "amlocator-location-info")]/div[contains(@class, "-contact")]/div[2]/a/text()'
+        ).get()
+        item["image"] = response.xpath('//a[contains(@data-amlocator-js, "location-image")]/@href').get()
+        hours_string = " ".join(response.xpath('//div[contains(@class, "amlocator-schedule-table")]//text()').getall())
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_string)
+        yield item
