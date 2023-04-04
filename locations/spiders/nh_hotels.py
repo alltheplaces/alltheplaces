b2492f9dd17f989733c10b965063af97b0be23c5
Mickael Abdealy <123941209+mickaelabdealy@users.noreply.github.com>
2023-04-04 02:43:14 +0200

    Feat/nh hotels (#5086)
    * feat: nh hotels v1
    
    * feat: add nh_hotels
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply code review
    
    * add website from sitemap and not datalayer url
    
    * add website from sitemap and not datalayer url
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Merge conflict
    
    * add category
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/nh_hotels.py
@@ -0,0 +1,37 @@
+from scrapy.spiders import Request, SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.spiders.vapestore_gb import clean_address
+from locations.user_agents import BROWSER_DEFAULT
+
+
+class NHHotelsSpider(SitemapSpider):
+    name = "nh_hotels"
+    item_attributes = {"brand": "NH Hotel Group", "brand_wikidata": "Q1604631"}
+    allowed_domains = ["nh-hotels.com"]
+    sitemap_urls = ["https://www.nh-hotels.com/hotels-sitemap.xml"]
+    user_agent = BROWSER_DEFAULT
+
+    def parse(self, response):
+        if store_id := response.xpath("//@data-id").get():
+            url = "https://www.nh-hotels.com/rest/datalayer/hotelPage/" + store_id
+            yield Request(url=url, callback=self.parse_site, cb_kwargs={"website": response.url})
+
+    def parse_site(self, response, website):
+        data = response.json()
+        item = Feature()
+        item["ref"] = data["id"]
+        item["name"] = data["name"]
+        item["city"] = data["city"]
+        item["country"] = data["country"]
+        item["street_address"] = clean_address(data["address"]["street"])
+        item["postcode"] = data["address"]["postalCode"]
+        item["lat"] = data["address"]["latitud"]
+        item["lon"] = data["address"]["longitud"]
+        item["email"] = data["contact"]["mail"]
+        item["phone"] = data["contact"]["phone"]
+        item["website"] = website
+        apply_category(Categories.HOTEL, item)
+
+        yield item
