5188190fd93696cb638eb71464b396193b0e32ad
Cj Malone <CjMalone@mail.com>
2023-04-04 15:04:25 +0100

    Add Black Sheep Coffee Spider

---
+++ locations/spiders/black_sheep_coffee.py
@@ -0,0 +1,12 @@
+from locations.storefinders.yext import YextSpider
+
+
+class BlackSheepCoffeeSpider(YextSpider):
+    name = "black_sheep_coffee"
+    item_attributes = {"brand": "Black Sheep Coffee", "brand_wikidata": "Q109745011"}
+    api_key = "88a4c90cba781a9bee544c96a3f87ff7"
+
+    def parse_item(self, item, location, **kwargs):
+        item["website"] = item["website"].split("?")[0]  # Strip yext trackers
+
+        yield item
