7d28d9c93f38e5847f65714b4de923705b797668
David Hicks <david@hicks.id.au>
2023-04-04 21:47:40 +1000

    ross_dress: fix and split into ross_dress_us and ross_dress_gu
    Ross Dress is now using Where2GetIt. As there are locations in
    mainland USA (1701) and Guam (3), two spiders are needed as the
    Where2GetIt spider only finds locations within a radius of 5000
    (probably km) and Guam is much further from mainland USA than 5000km.
    
    Fixes #4750

---
+++ locations/spiders/ross_dress_gu.py
@@ -0,0 +1,28 @@
+from locations.hours import DAYS_FULL, OpeningHours
+from locations.storefinders.where2getit import Where2GetItSpider
+
+class RossDressGUSpider(Where2GetItSpider):
+    name = "ross_dress_gu"
+    item_attributes = {"brand": "Ross Dress for Less"}
+    w2gi_id = "097D3C64-7006-11E8-9405-6974C403F339"
+    w2gi_filter = {
+        "clientkey": {"eq": ""},
+        "opendate": {"eq": ""},
+        "shopping_spree": {"eq": ""}
+    }
+    # Unclear why this query works, perhaps the radius=5000 starts from Guam
+    # because one of the first results for addressline=US is in Guam. Then
+    # nothing in mainland USA is within radius=5000 of Guam. Oddly, a query
+    # for "Guam" or "GU" returns mainland USA locations.
+    w2gi_query = "US"
+    w2gi_country = "US"
+
+    def parse_item(self, item, location):
+        item["country"] = "GU"
+        item.pop("state")
+        hours_string = ""
+        for day_name in DAYS_FULL:
+            hours_string = hours_string + " " + day_name + ": " + location[day_name.lower()]
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_string)
+        yield item
---
+++ locations/spiders/ross_dress_us.py
@@ -0,0 +1,26 @@
+from locations.hours import DAYS_FULL, OpeningHours
+from locations.storefinders.where2getit import Where2GetItSpider
+
+class RossDressUSSpider(Where2GetItSpider):
+    name = "ross_dress_us"
+    item_attributes = {"brand": "Ross Dress for Less"}
+    w2gi_id = "097D3C64-7006-11E8-9405-6974C403F339"
+    w2gi_filter = {
+        "clientkey": {"eq": ""},
+        "opendate": {"eq": ""},
+        "shopping_spree": {"eq": ""}
+    }
+    # A query of "CA" (California) with radius=5000 captures the
+    # entirity of mainland USA because the most extreme points of
+    # mainland USA are less than 5000km apart. It should also
+    # capture Hawaii which is less than 5000km from California.
+    w2gi_query = "CA"
+    w2gi_country = "US"
+
+    def parse_item(self, item, location):
+        hours_string = ""
+        for day_name in DAYS_FULL:
+            hours_string = hours_string + " " + day_name + ": " + location[day_name.lower()]
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_string)
+        yield item
