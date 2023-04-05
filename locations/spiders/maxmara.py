07e08b51bf09ba669db6359c4fa0f597802c6cb0
Elsa Monteiro <elsa@echo-analytics.io>
2023-04-06 00:23:16 +0200

    feat: add maxmara spider

---
+++ locations/spiders/maxmara.py
@@ -0,0 +1,44 @@
+import scrapy
+from geonamescache import GeonamesCache
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_EN, OpeningHours
+from locations.user_agents import BROWSER_DEFAULT
+
+
+class MaxmaraSpider(scrapy.Spider):
+    name = "maxmara"
+    item_attributes = {"brand": "Max Mara", "brand_wikidata": "Q1151774"}
+    gc = GeonamesCache()
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def start_requests(self):
+        for country_code in self.gc.get_countries().keys():
+            url = f"https://world.maxmara.com/store-locator?listJson=true&withoutRadius=false&country={country_code}"
+            headers = {
+                "authority": "world.maxmara.com",
+                "referer": "https://world.maxmara.com/store-locator",
+                "user-agent": BROWSER_DEFAULT,
+                "x-requested-with": "XMLHttpRequest",
+            }
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
+
+    def parse(self, response):
+        stores = response.json().get("features")
+        for store in stores:
+            store_info = store["properties"]
+            item = DictParser.parse(store_info)
+            item["ref"] = store_info["name"]
+            item["name"] = store_info["displayName"]
+            item["phone"] = store_info["phone1"]
+            item["addr_full"] = store_info["formattedAddress"]
+
+            oh = OpeningHours()
+            for day, hours in store_info.get("openingHours").items():
+                for chunk in hours:
+                    open_at = chunk.split(" - ")[0].replace(".", ":")
+                    close_at = chunk.split(" - ")[1].replace(".", ":")
+                    oh.add_range(day=DAYS_EN[day], open_time=open_at, close_time=close_at, time_format="%H:%M")
+            item["opening_hours"] = oh
+
+            yield item
