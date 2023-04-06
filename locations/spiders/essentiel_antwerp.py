c71d67e5192037fe09a35e169bf678c361ce2b4f
Mickael <mickael@echo-analytics.io>
2023-04-06 14:31:48 +0200

    Apply code review

--- locations/spiders/essentiel_antwerp.py
+++ locations/spiders/essentiel_antwerp.py
@@ -8,12 +8,12 @@ from locations.items import Feature
 
 class EssentielAntwerpSpider(scrapy.Spider):
     name = "essentiel_antwerp"
-    item_attributes = {"brand": "Essentiel-Antwerp"}
+    item_attributes = {"brand": "Essentiel Antwerp", "brand_wikidata": "Q117456339"}
     start_urls = ["https://www.essentiel-antwerp.com/be_fr/storelocator"]
 
     def parse(self, response):
-        data = response.xpath(".").get()
-        stores = json.loads("[" + re.findall(r"(stores:\[)(.*?)(])", data)[0][1] + "]")
+        data = response.text
+        stores = json.loads(re.search(r"stores:(\[.*\])", data).group(1))
         for store in stores:
             item = Feature()
             item["ref"] = store["storelocator_id"]
@@ -25,5 +25,6 @@ class EssentielAntwerpSpider(scrapy.Spider):
             item["lon"] = float(store["longtitude"])
             item["street_address"] = store["address"]
             item["city"] = store["city"]
-            item["name"] = "Essentiel-Antwerp" + " ".join(store["rewrite_request_path"].split("-"))
+            item["name"] = "Essentiel Antwerp" + " " + store["rewrite_request_path"].replace("-", " ").title()
+            item["website"] = f'https://www.essentiel-antwerp.com/eu/{store["rewrite_request_path"]}/'
             yield item
