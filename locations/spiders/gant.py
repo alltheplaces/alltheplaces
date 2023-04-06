6a82032f91134d1344be8364f445303bc3e14ac3
Mickael <mickael@echo-analytics.io>
2023-04-06 11:27:10 +0200

    feat: add gant for list of country

---
+++ locations/spiders/gant.py
@@ -0,0 +1,56 @@
+from locations.items import Feature
+from locations.hours import *
+import scrapy
+
+
+class GantSpider(scrapy.Spider):
+    name = "gant"
+    item_attributes = {
+        "brand": "GANT",
+        "brand_wikidata": "Q1493667",
+    }
+    start_urls = [
+        ("https://www.gant.be/stores", DAYS_NL),
+        ("https://dk.gant.com/stores", DAYS_DK),
+        ("https://fi.gant.com/stores", DAYS_FI),
+        ("https://fr.gant.com/stores", DAYS_FR),
+        ("https://de.gant.com/stores", DAYS_DE),
+        ("https://it.gant.com/stores", DAYS_IT),
+        ("https://nl.gant.com/stores", DAYS_NL),
+        ("https://pt.gant.com/stores", DAYS_ES),
+        ("https://www.gant.es/stores", DAYS_ES),
+        ("https://www.gant.se/stores", DAYS_SE),
+        ("https://ch.gant.com/stores", DAYS_DE),
+        ("https://www.gant.co.uk/stores", DAYS_EN),
+    ]
+
+    def start_requests(self):
+        for url, language in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse, cb_kwargs={"language": language})
+
+    def parse(self, response, language):
+        if (part_url := response.xpath("//*[@data-lat]/@data-action-url").get()) is not None:
+            complete_url = response.url.replace("/stores", "") + part_url
+            yield scrapy.Request(url=complete_url, callback=self.parse_website, cb_kwargs={"language": language})
+
+    def parse_website(self, response, language):
+        stores = response.json()["stores"]
+        for store in stores:
+            item = Feature()
+            item["ref"] = store.get("ID", None)
+            item["name"] = store.get("name", None)
+            item["city"] = store.get("city", None)
+            item["lat"] = store.get("latitude", None)
+            item["lon"] = store.get("longitude", None)
+            item["phone"] = store.get("phone", None)
+            item["country"] = store.get("countryCode", None)
+            item["postcode"] = store.get("postalCode", None)
+            item["email"] = store.get("email", None)
+            item["street_address"] = " ".join([store.get("address1", None) or "", store.get("address2", None) or ""])
+            oh = OpeningHours()
+            for opening_hour in store["storeHours"]:
+                oh.add_ranges_from_string(
+                    ranges_string=opening_hour["day"] + " " + opening_hour["hours"], days=language, delimiters=" - "
+                )
+            item["opening_hours"] = oh.as_opening_hours()
+            yield item
