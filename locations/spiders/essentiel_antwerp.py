92637b337c26e3ffcdf512801de4022c959b7a23
Mickael <mickael@echo-analytics.io>
2023-04-06 12:02:40 +0200

    feat: add essentiel-antwerp

---
+++ locations/spiders/essentiel_antwerp.py
@@ -0,0 +1,27 @@
+import scrapy
+import re
+from locations.items import Feature
+import json
+
+
+class EssentielAntwerpSpider(scrapy.Spider):
+    name = "essentiel_antwerp"
+    item_attributes = {"brand": "Essentiel-Antwerp"}
+    start_urls = ["https://www.essentiel-antwerp.com/be_fr/storelocator"]
+
+    def parse(self, response):
+        data = response.xpath(".").get()
+        stores = json.loads("[" + re.findall("(stores:\[)(.*?)(])", data)[0][1] + "]")
+        for store in stores:
+            item = Feature()
+            item["ref"] = store["storelocator_id"]
+            item["phone"] = store["phone"]
+            item["country"] = store["country"]
+            item["state"] = store["state"]
+            item["postcode"] = store["zipcode"]
+            item["lat"] = float(store["latitude"])
+            item["lon"] = float(store["longtitude"])
+            item["street_address"] = store["address"]
+            item["city"] = store["city"]
+            item["name"] = "Essentiel-Antwerp" + " ".join(store["rewrite_request_path"].split("-"))
+            yield item
