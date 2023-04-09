0753242632c7c4a73ea7df4362993c681eed599d
Cj Malone <CjMalone@mail.com>
2023-04-09 11:16:17 +0100

    Allow AmastyStoreLocatorSpider to be used with none API data

--- locations/spiders/eldi_be.py
+++ locations/spiders/eldi_be.py
@@ -1,34 +1,28 @@
 import json
 
-from scrapy import Selector, Spider
-
-from locations.items import Feature
+from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
-class EldiBESpider(Spider):
+class EldiBESpider(AmastyStoreLocatorSpider):
     name = "eldi_be"
     item_attributes = {"brand": "Eldi", "brand_wikidata": "Q3050500"}
     start_urls = ["https://www.eldi.be/nl/winkels"]
 
     def parse(self, response, **kwargs):
-        data = json.loads(
-            response.xpath('//script[contains(text(), "Amasty_Storelocator")]/text()').re_first(
-                r"jsonLocations: ({.+}),"
-            )
+        yield from self.parse_items(
+            json.loads(
+                response.xpath('//script[contains(text(), "Amasty_Storelocator")]/text()').re_first(
+                    r"jsonLocations: ({.+}),"
+                )
+            )["items"]
         )
-        for location in data["items"]:
-            item = Feature()
-            item["ref"] = location["id"]
-            item["lat"] = location["lat"]
-            item["lon"] = location["lng"]
-            popup_html = Selector(text=location["popup_html"])
-            item["name"] = popup_html.xpath('//a[@class="amlocator-link"]/@title').get()
-            item["website"] = popup_html.xpath('//a[@class="amlocator-link"]/@href').get()
-            for line in popup_html.xpath('//div[@class="amlocator-info-popup"]/text()').getall():
-                line = line.strip()
-                if line.startswith("Adres: "):
-                    item["street_address"] = line.replace("Adres: ", "")
-                elif line.startswith("Postcode: "):
-                    item["postcode"] = line.replace("Postcode: ", "")
 
-            yield item
+    def parse_item(self, item, location, popup_html):
+        for line in popup_html.xpath('//div[@class="amlocator-info-popup"]/text()').getall():
+            line = line.strip()
+            if line.startswith("Adres: "):
+                item["street_address"] = line.replace("Adres: ", "")
+            elif line.startswith("Postcode: "):
+                item["postcode"] = line.replace("Postcode: ", "")
+
+        yield item
