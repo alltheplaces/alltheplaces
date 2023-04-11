197b94b30a2d7eb189a8c3acf9e5d6274fec3a56
David Hicks <david@hicks.id.au>
2023-04-12 02:17:29 +1000

    dollartree: convert to Where2GetIt store finder

--- locations/spiders/dollartree.py
+++ locations/spiders/dollartree.py
@@ -1,15 +1,14 @@
+from locations.hours import OpeningHours
 from locations.storefinders.where2getit import Where2GetItSpider
 
 
 class DollarTreeSpider(Where2GetItSpider):
     name = "dollartree"
     item_attributes = {"brand": "Dollar Tree", "brand_wikidata": "Q5289230"}
-    w2gi_id = "134E9A7A-AB8F-11E3-80DE-744E58203F82"
-    w2gi_filter = {
-        "icon": {"eq": ""},
-        "ebt": {"eq": ""},
-        "crafters_square": {"eq": ""},
-        "freezers": {"eq": ""},
-        "snack_zone": {"eq": ""},
-    }
-    w2gi_query = "CA"
+    api_brand_name = "dollartree"
+    api_key = "134E9A7A-AB8F-11E3-80DE-744E58203F82"
+
+    def parse_item(self, item, location):
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(location.get("hours"))
+        yield item
