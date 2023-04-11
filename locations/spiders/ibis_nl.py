11f3956c7005f2e45b7e655cffa8efe2a63cbc98
Haoming <haoming@echo-analytics.io>
2023-04-11 17:36:41 +0200

    feat: Ibis NL

---
+++ locations/spiders/ibis_nl.py
@@ -0,0 +1,10 @@
+from locations.storefinders.yext import YextSpider
+
+
+class IbisNLSpider(YextSpider):
+    name = "ibis_nl"
+    item_attributes = {"brand": "Ibis", "brand_wikidata": "Q920166"}
+    api_key = "f60a800cdb7af0904b988d834ffeb221"
+    wanted_type = "hotel"
+    search_filter = {"name": {"$contains": "Ibis"}, "countryCode": "NL"}
+
