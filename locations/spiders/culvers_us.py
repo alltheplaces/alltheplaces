1bf9caadafbe3d27f47006beaa6164dd9b90cba6
David Hicks <david@hicks.id.au>
2023-04-12 14:25:13 +1000

    Where2GetIt store finder rewrite to use getlist API call (#5141)
    * storefinders/where2getit: rewrite to use getlist API call
    
    The Where2GetIt store finder was using an inefficient geolocation search
    API call that can be significantly improved and simplified by use of a
    different getlist API call that returns all locations, not just those in
    a geographic radius of a point.
    
    For particularly large brands, this rewritten store finder allows
    locations to be returned on a country-by-country basis to avoid the
    server timing out in its response to the getlist API call.
    
    Fixes #5119
    
    * culvers: convert to Where2GetIt store finder and rename to culvers_us
    
    Fixed #4751
    
    * joann_fabrics: convert to Where2GetIt store finder and rename to joann_us
    
    Fixes #4754
    
    * dollartree: convert to Where2GetIt store finder
    
    * michaels: convert to Where2GetIt store finder
    
    Fixed #4753
    
    * moneygram: convert to Where2GetIt store finder
    
    Now returns 322332 locations (much more than the old spider) which
    aligns approximately with the figure of 350000 locations cited on the
    MoneyGram website.
    
    Fixes #4749
    
    * ross_dress: convert to Where2GetIt store finder and rename to
    ross_dress_for_less_us
    
    Fixed #4750
    
    * the_north_face: convert to Where2GetIt store finder
    
    * timberland: convert to Where2GetIt store finder
    
    Replaces timberland_za spider as these South African locations are
    returned by this global spider.
    
    Fixes #4748
    
    * trader_joes: convert to Where2GetIt store finder and rename to
    trader_joes_us
    
    Fixes #4745
    
    * Add The Cheesecake Factory (US/CA) (206 stores)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * storefinders/where2getit: remove print statement accidentally remaining
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/culvers_us.py
@@ -0,0 +1,26 @@
+from locations.categories import Extras, PaymentMethods, apply_yes_no
+from locations.hours import DAYS, OpeningHours
+from locations.storefinders.where2getit import Where2GetItSpider
+
+
+class CulversUSSpider(Where2GetItSpider):
+    name = "culvers_us"
+    item_attributes = {"brand": "Culver's", "brand_wikidata": "Q1143589"}
+    api_brand_name = "culvers"
+    api_key = "1099682E-D719-11E6-A0C4-347BDEB8F1E5"
+    api_filter = {"number": {"ne": "P"}}
+
+    def parse_item(self, item, location):
+        item["opening_hours"] = OpeningHours()
+        for day_index in range(-1, 5, 1):
+            item["opening_hours"].add_range(
+                DAYS[day_index], location["bho"][day_index + 1][0], location["bho"][day_index + 1][1], "%H%M"
+            )
+        apply_yes_no(Extras.INDOOR_SEATING, item, location["dine_in"], False)
+        apply_yes_no(Extras.TAKEAWAY, item, location["takeout"], False)
+        apply_yes_no(Extras.DELIVERY, item, location["delivery"], False)
+        apply_yes_no(Extras.DRIVE_THROUGH, item, location["drivethru"], False)
+        apply_yes_no(Extras.TOILETS, item, location["restroom"], False)
+        apply_yes_no(PaymentMethods.VISA, item, location["visa"], False)
+        apply_yes_no(PaymentMethods.MASTER_CARD, item, location["mastercard"], False)
+        yield item
--- locations/spiders/the_north_face.py
+++ locations/spiders/the_north_face.py
@@ -1,19 +1,23 @@
-from locations.items import Feature
+from locations.hours import DAYS, OpeningHours
 from locations.storefinders.where2getit import Where2GetItSpider
-
-# The spider gets Stores and Outlets
-# A single query for CA collects both CA and US
 
 
 class TheNorthFaceSpider(Where2GetItSpider):
     name = "the_north_face"
     item_attributes = {"brand": "The North Face", "brand_wikidata": "Q152784"}
-    w2gi_id = "C1907EFA-14E9-11DF-8215-BBFCBD236D0E"
-    w2gi_filter = {"or": {"northface": {"eq": "1"}, "outletstore": {"eq": "1"}}}
-    w2gi_query = "CA"
+    api_brand_name = "northface"
+    api_key = "C1907EFA-14E9-11DF-8215-BBFCBD236D0E"
+    api_filter = {"or": {"northface": {"eq": "1"}, "outletstore": {"eq": "1"}}}
 
-    def parse_item(self, item: Feature, location: dict, **kwargs):
+    def parse_item(self, item, location):
+        if location.get("enterprise_store_identifier"):
+            item["ref"] = location.get("enterprise_store_identifier")
         if item["state"].upper() == "TNF RETAIL":
             item["state"] = None
-
+        hours_string = ""
+        for day in list(zip(["m", "t", "w", "thu", "f", "sa", "su"], DAYS)):
+            if location.get(day[0]):
+                hours_string = f"{hours_string} {day[1]}: " + location.get(day[0]).replace("/", ",")
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_string)
         yield item
