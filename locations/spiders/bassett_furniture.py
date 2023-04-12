9ed9ba1ba17fccb0b5bada7d5c573b30ddcf06df
Cj Malone <CjMalone@mail.com>
2023-04-12 15:06:38 +0100

    Clean up addr_full

--- locations/spiders/bassett_furniture.py
+++ locations/spiders/bassett_furniture.py
@@ -1,54 +1,13 @@
-import json
-import re
+from scrapy.spiders import SitemapSpider
 
-import scrapy
-from scrapy.utils.gz import gunzip
-
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class BassettFurnitureSpider(scrapy.Spider):
+class BassettFurnitureSpider(SitemapSpider, StructuredDataSpider):
     name = "bassett_furniture"
     item_attributes = {"brand": "Bassett Furniture", "brand_wikidata": "Q4868109"}
-    allowed_domains = ["www.bassettfurniture.com", "stores.bassettfurniture.com"]
-    start_urls = [
-        "https://stores.bassettfurniture.com/sitemap.xml.gz",
-    ]
-    download_delay = 0.3
-
-    def parse(self, response):
-        body = gunzip(response.body)
-        body = scrapy.Selector(text=body)
-        body.remove_namespaces()
-        urls = body.xpath("//url/loc/text()").extract()
-
-        for url in urls:
-            if url.count("/") >= 6:
-                yield scrapy.Request(url=url, callback=self.parse_store)
-
-    def parse_store(self, response):
-        data = response.xpath(
-            '//script[@type="application/ld+json" and contains(text(), "streetAddress")]/text()'
-        ).extract_first()
-
-        if data:
-            parsed_data = re.search(r'(.*),\s*"branchOf":\s{', data, flags=re.MULTILINE | re.DOTALL).group(1) + "}"
-            store_data = json.loads(parsed_data)
-            ref = re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1)
-            phone = re.search(r'"telephone":\s*"([0-9.]+)"', data).group(1)
-
-            properties = {
-                "ref": ref,
-                "name": response.xpath('//div[@class="address_inner"]/h1/text()').extract_first(),
-                "addr_full": store_data["address"]["streetAddress"],
-                "city": store_data["address"]["addressLocality"],
-                "state": store_data["address"]["addressRegion"],
-                "postcode": store_data["address"]["postalCode"],
-                "country": store_data["address"]["addressCountry"],
-                "phone": phone,
-                "website": response.url,
-                "lat": float(store_data["geo"]["latitude"]),
-                "lon": float(store_data["geo"]["longitude"]),
-            }
-
-            yield Feature(**properties)
+    allowed_domains = ["stores.bassettfurniture.com"]
+    sitemap_urls = ["https://stores.bassettfurniture.com/robots.txt"]
+    sitemap_rules = [("https://stores\.bassettfurniture\.com/[-\w]+/[-\w]+/\d+/$", "parse_sd")]
+    wanted_types = ["FurnitureStore"]
+    json_parser = "json5"
