67202ccaeae9779c586fe92c367ac712697fb33d
David Hicks <david@hicks.id.au>
2023-04-12 14:25:13 +1000

    Where2GetIt store finder rewrite to use getlist API call (#5141)
    * storefinders/where2getit: rewrite to use getlist API call
    
    The Where2GetIt store finder was using an inefficient geolocation search
    API call that can be significantly improved and simplified by use of a
    different getlist API call that returns all locations, not just those in
    a geographic radius of a point.
    
    For particularly large brands, this rewritten store finder allows
    locations to be returned on a country-by-country basis to avoid the
    server timing out in its response to the getlist API call.
    
    Fixes #5119
    
    * culvers: convert to Where2GetIt store finder and rename to culvers_us
    
    Fixed #4751
    
    * joann_fabrics: convert to Where2GetIt store finder and rename to joann_us
    
    Fixes #4754
    
    * dollartree: convert to Where2GetIt store finder
    
    * michaels: convert to Where2GetIt store finder
    
    Fixed #4753
    
    * moneygram: convert to Where2GetIt store finder
    
    Now returns 322332 locations (much more than the old spider) which
    aligns approximately with the figure of 350000 locations cited on the
    MoneyGram website.
    
    Fixes #4749
    
    * ross_dress: convert to Where2GetIt store finder and rename to
    ross_dress_for_less_us
    
    Fixed #4750
    
    * the_north_face: convert to Where2GetIt store finder
    
    * timberland: convert to Where2GetIt store finder
    
    Replaces timberland_za spider as these South African locations are
    returned by this global spider.
    
    Fixes #4748
    
    * trader_joes: convert to Where2GetIt store finder and rename to
    trader_joes_us
    
    Fixes #4745
    
    * Add The Cheesecake Factory (US/CA) (206 stores)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * storefinders/where2getit: remove print statement accidentally remaining
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/storefinders/where2getit.py
+++ locations/storefinders/where2getit.py
@@ -4,50 +4,114 @@ from scrapy.http import JsonRequest
 from locations.dict_parser import DictParser
 from locations.items import Feature
 
+# To use this storefinder, supply an api_brand_name value as would
+# be found in the following URL template:
+# https://hosted.where2getit.com/{api_brand_name}/rest/...
+# The api_key value needs to be supplied too and is found in the
+# request parameters for a POST to this URL. It is formatted as a
+# UUID (https://en.wikipedia.org/wiki/Universally_unique_identifier).
+
+# The api_filter value is optional and allows server-side
+# filtering of store locations which will be returned. This is useful
+# for spiders where you may want to only return official stores and
+# not resellers of a brand's products. It is also useful for
+# filtering out new stores not yet open, or stores which are closed
+# but which haven't been removed from the database.
+
+# Filters are a dictionary of nested rules that can include the
+# following types of rules:
+# * and     - all nested rules must return true
+# * or      - at least one nested rule must return true
+# * like    - a string must contain the specified string
+#             (case sensitive)
+# * ilike   - a string must contain the specified string
+#           (case insensitive)
+# * notlike - a string must not contain the specified string
+# * in      - a string must match one of the strings specified in a
+#             comma-separated list
+# * notin   - a string must not match any of the strings specified
+#             in a comma-separated list
+# * eq      - a string must equal the specified string
+# * ne      - a string must not equal the specified string
+# * gt      - a number must be greater than the specified number
+# * lt      - a number must be less than the specified number
+# * ge      - a number must be greater than or equal to the specified
+#             number
+# * le      - a number must be less than or equal to the specified
+#             number
+# * between - a number must be between (including lower and upper
+#             bounds) the number range specified as a
+#             comma-separated list
+#
+# As an example:
+# {"or": {"field1": {"in", "value1,value2,value3"},
+#         "field2": {"eq", "value4"}
+#        }
+# }
+#
+# As filters are complex to understand, it is best to search spiders
+# using "api_query" to see other examples of filters being used.
+
+# If there are many store locations to return, it is possible the
+# response will time out. If this is the case, try setting the
+# value of the separate_api_call_per_country parameter to True and
+# an individual request will be sent for each country, hopefully
+# allowing all requests to avoid timing out. If this approach fails,
+# you will need to overwrite the make_request function to use
+# a more granular form of filtering (e.g. by states of a country) to
+# avoid requests timing out.
+
 
 class Where2GetItSpider(Spider):
     dataset_attributes = {"source": "api", "api": "where2getit.com"}
-
     custom_settings = {"ROBOTSTXT_OBEY": False}
+    api_brand_name = ""
+    api_key = ""
+    api_filter = {}
+    separate_api_call_per_country = False
 
-    w2gi_api = "https://hosted.where2getit.com/rest/locatorsearch"
-    w2gi_id = ""
-    w2gi_query = "US"
-    w2gi_country_code = ""
-    w2gi_filter = None
-    request_size = 1000
-
-    def make_request(self, offset: int) -> JsonRequest:
-        return JsonRequest(
-            url=self.w2gi_api,
+    def make_request(self, country_code: str = None) -> JsonRequest:
+        where_clause = {}
+        if country_code and self.api_filter:
+            where_clause = {"and": {"country": {"eq": country_code}}}
+            where_clause["and"].update(self.api_filter)
+        elif country_code:
+            where_clause = {"country": {"eq": country_code}}
+        elif self.api_filter:
+            where_clause = self.api_filter
+        yield JsonRequest(
+            url=f"https://hosted.where2getit.com/{self.api_brand_name}/rest/getlist",
             data={
-                "request": {
-                    "appkey": self.w2gi_id,
-                    "formdata": {
-                        "dataview": "store_default",
-                        "limit": self.request_size,
-                        "offset": offset,
-                        "geolocs": {"geoloc": [{"addressline": self.w2gi_query, "country": self.w2gi_country_code}]},
-                        "searchradius": "5000",
-                        "where": self.w2gi_filter,
-                    },
-                }
+                "request": {"appkey": self.api_key, "formdata": {"objectname": "Locator::Store", "where": where_clause}}
             },
-            meta={"offset": offset},
+            method="POST",
+            callback=self.parse_locations,
         )
 
     def start_requests(self):
-        yield self.make_request(0)
+        if self.separate_api_call_per_country:
+            yield JsonRequest(
+                url=f"https://hosted.where2getit.com/{self.api_brand_name}/rest/getlist",
+                data={"request": {"appkey": self.api_key, "formdata": {"objectname": "Account::Country"}}},
+                method="POST",
+                callback=self.parse_country_list,
+            )
+        else:
+            yield from self.make_request()
 
-    def parse(self, response, **kwargs):
-        if response.json()["response"]["collectioncount"] == self.request_size:
-            yield self.make_request(response.meta["offset"] + self.request_size)
+    def parse_country_list(self, response, **kwargs):
+        for country in response.json()["response"]["collection"]:
+            yield from self.make_request(country["name"])
 
+    def parse_locations(self, response, **kwargs):
         for location in response.json()["response"]["collection"]:
             item = DictParser.parse(location)
-            item["ref"] = location["clientkey"]
-
-            yield from self.parse_item(item, location) or []
+            if not item["ref"]:
+                item["ref"] = location["clientkey"]
+            item["street_address"] = ", ".join(
+                filter(None, [location.get("address1"), location.get("address2"), location.get("address3")])
+            )
+            yield from self.parse_item(item, location)
 
     def parse_item(self, item: Feature, location: dict, **kwargs):
         yield item
