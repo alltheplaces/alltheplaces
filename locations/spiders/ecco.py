0a1b259af7be6cfb5b9ba7c0fcb17e0d9f005f55
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-04-14 13:28:38 +0100

    Directly add Ecco opening times (#5148)

--- locations/spiders/ecco.py
+++ locations/spiders/ecco.py
@@ -1,8 +1,6 @@
-import json
-
 import scrapy
 
-from locations.hours import DAYS_EN, DAYS_FULL, OpeningHours
+from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
 
 
@@ -15,15 +13,20 @@ class EccoSpider(scrapy.Spider):
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def parse(self, response):
-        stores = json.loads(response.text)
-        for store in stores:
-            if "ecco" in store["n"].lower():
+        for store in response.json():
+            if store["t"] in [
+                # 0,  # PARTNER # Just sell the stock?
+                1,  # ECCO
+                2,  # Outlet
+            ]:
                 yield scrapy.Request(
                     url="https://se.ecco.com/api/store/finder/" + store["i"], callback=self.parse_store
                 )
 
     def parse_store(self, response):
-        store = json.loads(response.text)
+        store = response.json()
+        if store["StoreType"] == "FullPrice":
+            return  # Online/virtual store
         item = Feature()
         item["ref"] = store["StoreId"]
         item["name"] = store["Name"]
@@ -36,10 +39,10 @@ class EccoSpider(scrapy.Spider):
         item["email"] = store["Email"]
         item["lat"] = store["Latitude"]
         item["lon"] = store["Longitude"]
+        item["extras"]["store_type"] = store["StoreType"]
         oh = OpeningHours()
         for day in DAYS_FULL:
-            ranges_string = day + " " + store[day + "Open"] + "-" + store[day + "Close"]
-            oh.add_ranges_from_string(ranges_string=ranges_string, days=DAYS_EN, delimiters=["-"])
+            oh.add_range(day, store[f"{day}Open"], store[f"{day}Close"], time_format="%H:%M:%S")
 
-        item["opening_hours"] = oh.as_opening_hours()
+        item["opening_hours"] = oh
         yield item
