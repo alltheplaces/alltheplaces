c5c5de91309ce640ee3b96e1c04b94f195a185f9
David Hicks <david@hicks.id.au>
2023-04-17 02:57:23 +1000

    Add Toyworld (AU) (122 stores)

---
+++ locations/spiders/toyworld_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class ToyworldAUSpider(StockInStoreSpider):
+    name = "toyworld_au"
+    item_attributes = {"brand": "Toyworld", "brand_wikidata": "Q95923071"}
+    api_site_id = "10046"
+    api_widget_id = "53"
+    api_widget_type = "storelocator"
+    api_origin = "https://www.toyworld.com.au"
