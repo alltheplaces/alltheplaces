2b64d34ed68379f9a27a1b8738c3e641523b1360
David Hicks <david@hicks.id.au>
2023-04-17 01:19:58 +1000

    Add Carla Zampatti (AU/NZ) (13 stores)

---
+++ locations/spiders/carla_zampatti.py
@@ -0,0 +1,14 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class CarlaZampattiSpider(StockInStoreSpider):
+    name = "carla_zampatti"
+    item_attributes = {"brand": "Carla Zampatti", "brand_wikidata": "Q87377342"}
+    api_site_id = "10131"
+    api_widget_id = "139"
+    api_widget_type = "storelocator"
+    api_origin = "https://www.carlazampatti.com.au"
+
+    def parse_item(self, item, location):
+        if "David Jones" in item["name"].title():
+            return
+        yield item
