4388cfe6d1a63816330613008ae6570e928004f1
David Hicks <david@hicks.id.au>
2023-04-17 02:04:36 +1000

    Add Manning Cartell (AU) (6 stores)

---
+++ locations/spiders/manning_cartell_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class ManningCartellAUSpider(StockInStoreSpider):
+    name = "manning_cartell_au"
+    item_attributes = {"brand": "Manning Cartell", "brand_wikidata": "Q117746697"}
+    api_site_id = "10045"
+    api_widget_id = "52"
+    api_widget_type = "sis"
+    api_origin = "https://www.manningcartell.com.au"
