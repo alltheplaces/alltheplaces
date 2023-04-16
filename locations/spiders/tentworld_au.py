416470c6ab68e9095a2135ce39c590c2ebd11f2b
David Hicks <david@hicks.id.au>
2023-04-17 02:52:29 +1000

    Add Tentworld (AU) (15 stores)

---
+++ locations/spiders/tentworld_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class TentworldAUSpider(StockInStoreSpider):
+    name = "tentworld_au"
+    item_attributes = {"brand": "Tentworld", "brand_wikidata": "Q117747711"}
+    api_site_id = "30"
+    api_widget_id = "7"
+    api_widget_type = "sis"
+    api_origin = "https://www.tentworld.com.au"
