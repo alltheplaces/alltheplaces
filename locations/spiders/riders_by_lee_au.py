3562bd623e7e2c39001c34c774ba3cf5c4461895
David Hicks <david@hicks.id.au>
2023-04-17 02:32:36 +1000

    Add Riders by Lee (AU) (3 stores)

---
+++ locations/spiders/riders_by_lee_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class RidersByLeeAUSpider(StockInStoreSpider):
+    name = "riders_by_lee_au"
+    item_attributes = {"brand": "Riders by Lee", "brand_wikidata": "Q117747538"}
+    api_site_id = "10057"
+    api_widget_id = "64"
+    api_widget_type = "sis"
+    api_origin = "https://ridersbylee.com.au"
