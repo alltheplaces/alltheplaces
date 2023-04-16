f830eba147a467647d4c31a89e74e51c007efd9c
David Hicks <david@hicks.id.au>
2023-04-17 01:18:05 +1000

    Add Bags To Go (AU) (4 stores)

---
+++ locations/spiders/bags_to_go_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class BagsToGoAUSpider(StockInStoreSpider):
+    name = "bags_to_go_au"
+    item_attributes = {"brand": "Bags To Go", "brand_wikidata": "Q117745930"}
+    api_site_id = "10017"
+    api_widget_id = "25"
+    api_widget_type = "sis"
+    api_origin = "https://bagstogo.com.au"
