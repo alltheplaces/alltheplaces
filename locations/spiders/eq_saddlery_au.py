1ad9ac30f67e680ca75f4fb09088d005b1af87ba
David Hicks <david@hicks.id.au>
2023-04-17 01:37:06 +1000

    Add EQ Saddlery (AU) (6 stores)

---
+++ locations/spiders/eq_saddlery_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class EQSaddleryAUSpider(StockInStoreSpider):
+    name = "eq_saddlery_au"
+    item_attributes = {"brand": "EQ Saddlery", "brand_wikidata": "Q117746041"}
+    api_site_id = "10040"
+    api_widget_id = "47"
+    api_widget_type = "sis"
+    api_origin = "https://www.eqsaddlery.com.au"
