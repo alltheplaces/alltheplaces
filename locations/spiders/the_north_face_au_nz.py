45c43591a386356bc1ded2f45a592dedc5ad7483
David Hicks <david@hicks.id.au>
2023-04-17 02:53:24 +1000

    Add The North Face (AU/NZ) (15 stores)

---
+++ locations/spiders/the_north_face_au_nz.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class TheNorthFaceAUNZSpider(StockInStoreSpider):
+    name = "the_north_face_au_nz"
+    item_attributes = {"brand": "The North Face", "brand_wikidata": "Q152784"}
+    api_site_id = "10063"
+    api_widget_id = "70"
+    api_widget_type = "sis"
+    api_origin = "https://thenorthface.com.au"
