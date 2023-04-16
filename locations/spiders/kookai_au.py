d82baaff3188927bcad9e0c9beb7b5ae47eebcdd
David Hicks <david@hicks.id.au>
2023-04-17 01:58:32 +1000

    Add Kookai (AU) (43 stores)

---
+++ locations/spiders/kookai_au.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class KookaiAUSpider(StockInStoreSpider):
+    name = "kookai_au"
+    item_attributes = {"brand": "Kookai", "brand_wikidata": "Q1783759"}
+    api_site_id = "10003"
+    api_widget_id = "11"
+    api_widget_type = "sis"
+    api_origin = "https://www.kookai.com.au"
