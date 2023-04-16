c79f4a26d2a0e0648e550776775fc1559cf656d6
David Hicks <david@hicks.id.au>
2023-04-17 01:54:04 +1000

    Add Kate Spade (AU/NZ) (10 stores)

---
+++ locations/spiders/kate_spade_au_nz.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class KateSpadeAUNZSpider(StockInStoreSpider):
+    name = "kate_spade_au_nz"
+    item_attributes = {"brand": "Kate Spade New York", "brand_wikidata": "Q6375797"}
+    api_site_id = "10076"
+    api_widget_id = "83"
+    api_widget_type = "sis"
+    api_origin = "https://katespade.com.au"
