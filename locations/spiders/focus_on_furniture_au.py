64f38c3166e88ce8d125726ae0e41f93ec713034
David Hicks <david@hicks.id.au>
2023-04-17 01:40:04 +1000

    Add Focus on Furniture (AU) (23 stores)

---
+++ locations/spiders/focus_on_furniture_au.py
@@ -0,0 +1,14 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class FocusOnFurnitureAUSpider(StockInStoreSpider):
+    name = "focus_on_furniture_au"
+    item_attributes = {"brand": "Focus on Furniture", "brand_wikidata": "Q117746060"}
+    api_site_id = "10100"
+    api_widget_id = "107"
+    api_widget_type = "product"
+    api_origin = "https://focusonfurniture.com.au"
+
+    def parse_item(self, item, location):
+        if item.get("email"):
+            item["email"] = item["email"].strip()
+        yield item
