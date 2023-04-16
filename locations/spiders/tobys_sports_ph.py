e7908fe0299956c4a2640f23a8ba5b54c374c608
David Hicks <david@hicks.id.au>
2023-04-17 02:56:09 +1000

    Add Toby's Sports (PH) (35 stores)

---
+++ locations/spiders/tobys_sports_ph.py
@@ -0,0 +1,9 @@
+from locations.storefinders.stockinstore import StockInStoreSpider
+
+class TobysSportsPHSpider(StockInStoreSpider):
+    name = "tobys_sports_ph"
+    item_attributes = {"brand": "Toby's Sports", "brand_wikidata": "Q117747741"}
+    api_site_id = "10083"
+    api_widget_id = "90"
+    api_widget_type = "cnc"
+    api_origin = "https://www.tobys.com"
