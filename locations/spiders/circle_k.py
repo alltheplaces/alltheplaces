78fc442f27f3e4e5e64ba42c7dc3872a5f303c85
David Hicks <david@hicks.id.au>
2023-04-18 00:57:08 +1000

    circle_k: fix spider and make international
    Use a new Circle API which returns global results. The last page which
    can be requested is
    https://www.circlek.com/stores_master.php?lat=0&lng=0&page=959 and with
    10 locations returned per page, and page 0 being the first, that equals
    a count of 9597 locations returned by this API.
    
    Previously before this spider broke, approximately 6600 results were
    returned from the USA only.

--- locations/spiders/circle_k.py
+++ locations/spiders/circle_k.py
@@ -1,40 +1,38 @@
-import html
+import re
 
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Spider
+from scrapy.http import JsonRequest
 
-from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
 
 
-class CircleKSpider(CrawlSpider, StructuredDataSpider):
+class CircleKSpider(Spider):
     name = "circle_k"
-    item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010", "country": "US"}
+    item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010"}
     allowed_domains = ["www.circlek.com"]
-    start_urls = ["https://www.circlek.com/list-united-states-stores?lang=en"]
+    start_urls = ["https://www.circlek.com/stores_master.php?lat=0&lng=0&page=0"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
-    rules = [Rule(LinkExtractor(allow="/store-locator/"), callback="parse_sd")]
-    wanted_types = ["ConvenienceStore"]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        days = response.xpath('//div[contains(@class,"hours-item")]')
-        oh = OpeningHours()
-        for day in days:
-            hours = day.xpath("./span[2]/text()").get().strip("\n").strip()
-            oh.add_range(
-                day=day.xpath("./span[1]/text()").get().strip(),
-                open_time="12:00 AM" if hours == "Open 24h" else hours.split(" to ")[0],
-                close_time="12:00 AM" if hours == "Open 24h" else hours.split(" to ")[1],
-                time_format="%I:%M %p",
-            )
-        item["opening_hours"] = oh.as_opening_hours()
+    def start_requests(self):
+        for url in self.start_urls:
+            yield JsonRequest(url=url, meta={"page": 0})
 
-        item["name"] = html.unescape(item["name"])
+    def parse(self, response):
+        if response.json()["count"] == 0:
+            # crawl completed
+            return
 
-        if "Convenience Store" in item["name"]:
-            apply_category(Categories.SHOP_CONVENIENCE, item)
-        if "Gas Station" in item["name"]:
-            apply_category(Categories.FUEL_STATION, item)
+        for location_id, location in response.json()["stores"].items():
+            item = DictParser.parse(location)
+            item["ref"] = location_id
+            item["street_address"] = item.pop("addr_full")
+            item["website"] = "https://www.circlek.com" + item["website"]
+            yield item
 
-        yield item
+        if response.json()["count"] < 10:
+            # crawl completed
+            return
+
+        next_page = response.meta["page"] + 1
+        next_url = re.sub(r"\d+$", str(next_page), response.url)
+        yield JsonRequest(url=next_url, meta={"page": next_page})
