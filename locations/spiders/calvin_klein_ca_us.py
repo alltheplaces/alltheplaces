25dc3ee4c293d939e6cd8403d7c608e1f77978e5
David Hicks <david@hicks.id.au>
2023-04-18 01:02:02 +1000

    calvin_klein_ca_us: fix spider (#5178)
    Commit 1bf9caadafbe3d27f47006beaa6164dd9b90cba6 should have updated this
    spider, but it was forgotten.
    
    Also fix the name of the spider to match the filename.

--- locations/spiders/calvin_klein_ca_us.py
+++ locations/spiders/calvin_klein_ca_us.py
@@ -3,8 +3,8 @@ from locations.storefinders.where2getit import Where2GetItSpider
 
 
 class CalvinKleinCAUSSpider(Where2GetItSpider):
-    name = "calvin_klein_us"
+    name = "calvin_klein_ca_us"
     item_attributes = CalvinKleinSpider.item_attributes
-    w2gi_id = "F5C66E24-E944-11EA-A1DF-53CCF48ECC77"
-    w2gi_filter = {"or": {"icon": {"like": ""}}}
-    w2gi_query = "CA"
+    api_brand_name = "calvinklein"
+    api_key = "F5C66E24-E944-11EA-A1DF-53CCF48ECC77"
+    api_filter = {"or": {"icon": {"like": ""}}}
