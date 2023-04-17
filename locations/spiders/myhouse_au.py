439e7744aff1fad504e2a4f6c3318fb0ecb3ed68
David Hicks <david@hicks.id.au>
2023-04-17 11:19:57 +1000

    Add MyHouse (AU) (30 stores) (#5169)

---
+++ locations/spiders/myhouse_au.py
@@ -0,0 +1,26 @@
+from scrapy import Request, Spider
+
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+
+
+class MyHouseAUSpider(Spider):
+    name = "myhouse_au"
+    item_attributes = {"brand": "MyHouse", "brand_wikidata": "Q28854270"}
+    allowed_domains = ["myhouse.com.au"]
+    start_urls = ["https://myhouse.com.au/api/get-stores"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url=url, method="POST")
+
+    def parse(self, response):
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["geometry"] = location["location"]
+            item["street_address"] = ", ".join(filter(None, [location["address1"], location["address2"]]))
+            item["website"] = "https://myhouse.com.au/stores/" + location["slug"]
+            item["opening_hours"] = OpeningHours()
+            for day_name, day_hours in location["storeHours"].items():
+                item["opening_hours"].add_range(day_name.title(), day_hours["open"], day_hours["close"])
+            yield item
