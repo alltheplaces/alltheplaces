ce5a24291cb8bfc4f3b1176a1516bfed515ec18f
David Hicks <david@hicks.id.au>
2023-04-17 23:18:13 +1000

    evyve_gb: fix spider

--- locations/spiders/evyve_gb.py
+++ locations/spiders/evyve_gb.py
@@ -1,3 +1,5 @@
+import json
+
 from scrapy import Spider
 
 from locations.dict_parser import DictParser
@@ -6,12 +8,17 @@ from locations.dict_parser import DictParser
 class EvyveGBSpider(Spider):
     name = "evyve_gb"
     item_attributes = {"brand": "evyve", "brand_wikidata": "Q116698197"}
-    start_urls = ["https://evyve.co.uk/umbraco/surface/EvyveCharger/GetChargePointData"]
+    start_urls = ["https://evyve.co.uk/locations/"]
 
-    def parse(self, response, **kwargs):
-        for location in response.json()["data"]:
+    def parse(self, response):
+        locations = json.loads(response.xpath('//main[@id="content"]/script').get().split("var propertyLocations = JSON.parse('", 2)[1].split("');", 2)[0])
+        for location in locations:
+            print(location)
             if not location["publish"]:
-                continue  # "coming soon"
-
-            location["street_address"] = location.pop("address")
-            yield DictParser.parse(location)
+                continue # location not open (coming soon or other reason)
+            item = DictParser.parse(location)
+            # Address fields are all jumbled up it's best to combine them together
+            item["addr_full"] = ", ".join(filter(None, [location["address"], location["address_line_2"], location["city"]]))
+            item.pop("city")
+            item["extras"]["access"] = location["access_type"].lower()
+            yield item
