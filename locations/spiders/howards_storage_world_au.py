1a9c59a0888b36a8ef1d11637f5fc139a79f697f
David Hicks <david@hicks.id.au>
2023-04-29 21:23:53 +1000

    howards_storage_world_au: fix spider class name

--- locations/spiders/howards_storage_world_au.py
+++ locations/spiders/howards_storage_world_au.py
@@ -3,7 +3,7 @@ import re
 from locations.storefinders.storelocatorwidgets import StoreLocatorWidgetsSpider
 
 
-class XtremeCommunicationsAUSpider(StoreLocatorWidgetsSpider):
+class HowardsStorageWorldAUSpider(StoreLocatorWidgetsSpider):
     name = "howards_storage_world_au"
     item_attributes = {"brand": "Howards Storage World", "brand_wikidata": "Q106283555"}
     key = "5ca7e612c7df4447ec76bf314efb8f52"
