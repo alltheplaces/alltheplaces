fba63d946847e4d95bef1b53206a3e97fc715da0
Joe Dixon <mjoe999@users.noreply.github.com>
2023-05-02 02:26:09 +0100

    fix some exceptions when forming street_address, general refresh (#5243)

--- locations/spiders/williamhill_gb.py
+++ locations/spiders/williamhill_gb.py
@@ -9,25 +9,18 @@ from locations.dict_parser import DictParser
 
 class WilliamHillGBSpider(CrawlSpider):
     name = "williamhill_gb"
-    item_attributes = {
-        "brand": "William Hill",
-        "brand_wikidata": "Q4053147",
-        "country": "GB",
-    }
+    item_attributes = {"brand": "William Hill", "brand_wikidata": "Q4053147"}
     allowed_domains = ["shoplocator.williamhill"]
     start_urls = ["https://shoplocator.williamhill/directory"]
     rules = [Rule(LinkExtractor(), callback="parse_func", follow=False)]
-    download_delay = 0.5
 
     def parse_func(self, response):
         pattern = re.compile(r"window.lctr.results.push\((.*?)\);", re.MULTILINE | re.DOTALL)
-        s = response.xpath('//script[contains(., "lctr")]/text()').re(pattern)
-        if s:
+        if s := response.xpath('//script[contains(., "lctr")]/text()').re(pattern):
             store = json.loads(s[0])
             item = DictParser.parse(store)
             item["website"] = response.url
-            street_no = store.get("street_no", "").strip()
-            street = store.get("street", "").strip()
-            item["street_address"] = (street_no + " " + street).strip()
+            components = list(filter(None, [store.get("street_no"), store.get("street")]))
+            item["street_address"] = " ".join(map(str.strip, components)).strip()
             item["city"] = store.get("county")
             return item
