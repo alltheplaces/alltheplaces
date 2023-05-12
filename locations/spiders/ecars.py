31557b094b9150f0d639351097d00370368c04ca
Cj Malone <CjMalone@mail.com>
2023-05-12 10:42:55 +0100

    Add ecars Spider

---
+++ locations/spiders/ecars.py
@@ -0,0 +1,33 @@
+import pprint
+
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+
+
+class EcarsSpider(Spider):
+    name = "ecars"
+    item_attributes = {"brand": "ecars", "brand_wikidata": "Q3050566"}
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def start_requests(self):
+        yield JsonRequest(
+            url="https://myaccount.esbecars.com/stationFacade/findSitesInBounds",
+            data={
+                "filterByBounds": {"northEastLat": 90, "northEastLng": 180, "southWestLat": -90, "southWestLng": -180}
+            },
+        )
+
+    def parse(self, response, **kwargs):
+        if not response.json()["success"]:
+            pprint.pp(response.json()["errors"])
+            return
+        for location in response.json()["data"][1]:
+            if location["deleted"]:
+                continue
+
+            item = DictParser.parse(location)
+            item["addr_full"] = location["dn"]
+
+            yield item
