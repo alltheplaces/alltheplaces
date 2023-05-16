efc467a929dc5ce87ef4e6f6df6ea43f4a20cfa9
David Hicks <david@hicks.id.au>
2023-05-17 00:14:19 +1000

    Add Ally Fashion (AU) (145 stores)

---
+++ locations/spiders/ally_fashion_au.py
@@ -0,0 +1,14 @@
+import re
+
+from locations.hours import OpeningHours
+from locations.storefinders.stockist import StockistSpider
+
+
+class AllyFashionAUSpider(StockistSpider):
+    name = "ally_fashion_au"
+    item_attributes = {"brand": "Ally Fashion", "brand_wikidata": "Q19870623"}
+    key = "u13251"
+
+    def parse_item(self, item, location):
+        item.pop("website")
+        yield item
