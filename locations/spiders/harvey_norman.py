893d6699de316ec7f979b63f36c43b0a6ebf2522
David Hicks <david@hicks.id.au>
2023-05-18 04:47:16 +1000

    harvey_norman: fix spider (#5293)

--- locations/spiders/harvey_norman.py
+++ locations/spiders/harvey_norman.py
@@ -1,47 +1,34 @@
 import json
 
-import scrapy
+from scrapy import Spider
 
-from locations.hours import DAYS_EN, OpeningHours
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
+from locations.hours import DAYS, OpeningHours
 
 
-class HarveyNormanSpider(StructuredDataSpider):
+class HarveyNormanSpider(Spider):
     name = "harvey_norman"
     item_attributes = {"brand": "Harvey Norman", "brand_wikidata": "Q4040441"}
     allowed_domains = ["stores.harveynorman.com.au", "stores.harveynorman.co.nz"]
     start_urls = ["https://stores.harveynorman.com.au/", "https://stores.harveynorman.co.nz/"]
+    # There is no robots.txt, instead the store finder page (HTML)
+    # is returned and this confuses Scrapy.
+    custom_settings = {"ROBOTSTXT_OBEY": False}
     requires_proxy = "AU"
 
     def parse(self, response):
-        data_raw = response.xpath('//script[@type="application/ld+json"]/text()').get()
-        for store in json.loads(data_raw):
-            yield scrapy.Request(url=store["url"], callback=self.parse_sd)
-
-    def pre_process_data(self, ld_data, **kwargs):
-        # openingHoursSpecification data exists but needs to first
-        # be corrected prior to use.
-        oh_spec = ld_data.pop("openingHoursSpecification")
-        for day_range in oh_spec:
-            day_range["opens"] = "".join(day_range["opens"].upper().split())
-            if ":" not in day_range["opens"]:
-                day_range["opens"] = day_range["opens"].replace("AM", ":00AM").replace("PM", ":00PM")
-            day_range["closes"] = "".join(day_range["closes"].upper().split())
-            if ":" not in day_range["closes"]:
-                day_range["closes"] = day_range["closes"].replace("AM", ":00AM").replace("PM", ":00PM")
-            if "dayOfWeek" in day_range:
-                for day_name in day_range["dayOfWeek"]:
-                    day_name = day_name.title()
-        ld_data["openingHoursSpecification"] = oh_spec
-
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item.pop("image")
-        if "stores.harveynorman.com.au" in response.url:
-            item["country"] = "AU"
-        else:
-            item["country"] = "NZ"
-            item.pop("state")
-        oh = OpeningHours()
-        oh.from_linked_data(ld_data, time_format="%I:%M%p")
-        item["opening_hours"] = oh.as_opening_hours()
-        yield item
+        data_raw = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
+        for location in json.loads(data_raw)["props"]["pageProps"]["locations"]:
+            item = DictParser.parse(location)
+            item["ref"] = location["storeCode"]
+            item["street_address"] = ", ".join(filter(None, location["addressLines"]))
+            if ".co.nz" in response.url:
+                item.pop("state")
+            if len(location["phoneNumbers"]) > 0:
+                item["phone"] = location["phoneNumbers"][0]
+            item["opening_hours"] = OpeningHours()
+            for day_number, hours in enumerate(location["businessHours"]):
+                if len(hours) < 2:
+                    continue
+                item["opening_hours"].add_range(DAYS[day_number - 1], hours[0], hours[1])
+            yield item
