277bb95bcc08f17f06e0be113befd9a027881f39
David Hicks <david@hicks.id.au>
2023-05-17 23:34:16 +1000

    house_au: fix spider

--- locations/spiders/house_au.py
+++ locations/spiders/house_au.py
@@ -1,39 +1,28 @@
 from scrapy import Request, Spider
-from scrapy.http import JsonRequest
 
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_FULL, OpeningHours
+from locations.hours import OpeningHours
 
 
 class HouseAUSpider(Spider):
     name = "house_au"
     item_attributes = {"brand": "House", "brand_wikidata": "Q117921987"}
     allowed_domains = ["www.house.com.au"]
-    start_urls = ["https://www.house.com.au/service/storeLocator/findNearestStore"]
-    custom_settings = {
-        "COOKIES_ENABLED": True,
-    }
+    start_urls = ["https://www.house.com.au/api/get-stores"]
 
     def start_requests(self):
-        yield Request(url="https://www.house.com.au/", callback=self.start_requests_with_cookie)
-
-    def start_requests_with_cookie(self, response):
         for url in self.start_urls:
-            yield JsonRequest(url=url, data={}, method="POST")
+            yield Request(url=url, method="POST")
 
     def parse(self, response):
-        for location in response.json()["data"]:
+        for location in response.json():
             item = DictParser.parse(location)
-            item["ref"] = location["WarehouseCode"]
-            if "House Bed and Bath" in item["name"]:
-                item["brand"] = "House Bed & Bath"
-            item["street_address"] = ", ".join(
-                filter(None, [location.get("AddressLine1"), location.get("AddressLine2")])
-            )
-            item["website"] = "https://www.house.com.au/store-locator/" + item["name"].lower().replace(" ", "-")
+            item["geometry"] = location["location"]
+            item["street_address"] = ", ".join(filter(None, [location["address1"], location["address2"]]))
+            item["website"] = "https://www.house.com.au/stores/" + location["slug"]
             item["opening_hours"] = OpeningHours()
-            for day_name in DAYS_FULL:
-                open_time = location[f"{day_name}Open"].strip().replace(".", ":")
-                close_time = location[f"{day_name}Close"].strip().replace(".", ":")
-                item["opening_hours"].add_range(day_name, open_time, close_time)
+            for day_name, hours in location["storeHours"].items():
+                if hours["open"] == "-" or hours["close"] == "-":
+                    continue
+                item["opening_hours"].add_range(day_name.title(), hours["open"].replace(".", ":"), hours["close"].replace(".", ":"))
             yield item
