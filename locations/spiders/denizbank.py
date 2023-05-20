6674c86c68e6f65f8e0dcf2550572336348e2bca
kwaigonjinn <kolosov.yv@gmail.com>
2023-05-20 13:21:32 +0300

    Add country code

--- locations/spiders/denizbank.py
+++ locations/spiders/denizbank.py
@@ -1,16 +1,15 @@
 import scrapy
-from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
-from locations.hours import DAYS
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
-class DenizbankSpider(scrapy.Spider):
-    name = "denizbank"
+
+class DenizbankTrSpider(scrapy.Spider):
+    name = "denizbank_tr"
     allowed_domains = ["denizbank.com"]
-    item_attributes = { 'brand': "Denizbank", "brand_wikidata": "Q1115064" }
+    item_attributes = {"brand": "Denizbank", "brand_wikidata": "Q1115064"}
     start_urls = [
-        'https://www.denizbank.com/api/branch-atm?isAtmSearch=true&isBranchSearch=true&isVisuallyImparied=false&isOrthopedicalHandicapped=false&isCurrencySupport=false&keyword=&latitude=&longitude=&cityCode=&townCode=',
+        "https://www.denizbank.com/api/branch-atm?isAtmSearch=true&isBranchSearch=true&isVisuallyImparied=false&isOrthopedicalHandicapped=false&isCurrencySupport=false&keyword=&latitude=&longitude=&cityCode=&townCode=",
     ]
 
     def parse(self, response):
@@ -18,10 +17,10 @@ class DenizbankSpider(scrapy.Spider):
         for poi in data:
             item = DictParser.parse(poi)
             # TODO: map "is-visually-imparied", "is-orthopedical-handicapped", "is-exchange-support" to OSM tags if possible
-            item['ref'] = poi.get('code')
-            item['phone'] = poi.get('phone').strip() if poi.get('phone') else None
-            if poi['record-type'] == 'atm':
+            item["ref"] = poi.get("code")
+            item["phone"] = poi.get("phone").strip() if poi.get("phone") else None
+            if poi["record-type"] == "atm":
                 apply_category(Categories.ATM, item)
-            if poi['record-type'] == 'branch':
+            if poi["record-type"] == "branch":
                 apply_category(Categories.BANK, item)
             yield item
