9d56cccbe76628f5fd6206c48ecea805307ffd0c
kwaigonjinn <kolosov.yv@gmail.com>
2023-05-21 14:56:37 +0300

    Cleaned parse a bit

--- locations/spiders/kuveyt_turk_tr.py
+++ locations/spiders/kuveyt_turk_tr.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urljoin
 
 from scrapy import Spider
 from scrapy.http import JsonRequest, Request
@@ -11,29 +11,27 @@ class KuveytTurkTRSpider(Spider):
     name = "kuveyt_turk_tr"
     item_attributes = {"brand": "Kuveyt Türk", "brand_wikidata": "Q6036058"}
     allowed_domains = ["kuveytturk.com.tr"]
+    base_url = "https://www.kuveytturk.com.tr/"
 
     def start_requests(self):
-        url = "https://www.kuveytturk.com.tr/en/branches-and-atms"
-        yield Request(url)
+        yield Request(urljoin(self.base_url, "/en/branches-and-atms"))
 
     def parse(self, response, **kwargs):
-        bnh = response.xpath('//script[contains(text(), "addresses")]/text()').re_first(r'"bnh":"(.+?)"')
-        base_url = "https://www.kuveytturk.com.tr/"
-        yield JsonRequest(url=base_url + bnh + "&" + urlencode({"p6": 0}), callback=self.parse_pois)  # Branches
-        yield JsonRequest(url=base_url + bnh + "&" + urlencode({"p6": 1}), callback=self.parse_pois)  # ATMs
-        yield JsonRequest(
-            url=base_url + bnh + "&" + urlencode({"p6": 2}), callback=self.parse_pois  # XTMs (a special type of ATMs)
-        )
+        endpoint = response.xpath('//script[contains(text(), "addresses")]/text()').re_first(r'"bnh":"(.+?)"')
+        params = {"p6": 0} # Branches
+        yield JsonRequest(urljoin(self.base_url, endpoint + "&" + urlencode(params)), callback=self.parse_pois)  
+        params["p6"] = 1 # ATMs
+        yield JsonRequest(urljoin(self.base_url, endpoint + "&" + urlencode(params)), callback=self.parse_pois)  
+        params["p6"] = 2 # XTMs (a special type of ATMs)
+        yield JsonRequest(urljoin(self.base_url, endpoint + "&" + urlencode(params)), callback=self.parse_pois)  
 
     def parse_pois(self, response, **kwargs):
-        data = response.json()
-        for poi in data:
+        pois = response.json()
+        for poi in pois:
             item = DictParser.parse(poi)
             item["ref"] = poi.get("MapKey")
             if poi.get("LocationType") == "Branch":
                 apply_category(Categories.BANK, item)
-            elif poi.get("LocationType") == "ATM":
-                apply_category(Categories.ATM, item)
-            elif poi.get("LocationType") == "XTM":
+            elif poi.get("LocationType") in ["ATM", "XTM"]:
                 apply_category(Categories.ATM, item)
             yield item
