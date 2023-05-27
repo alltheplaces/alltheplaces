5beb2040586e3015725f32e574009227aab11fae
David Hicks <david@hicks.id.au>
2023-05-27 10:43:56 +1000

    timhortons: fix spider and rename to tim_hortons (#5356)
    * timhortons: fix spider and rename to tim_hortons
    
    GraphQL is now being used for the store locator.
    
    Returns 4568 locations vs. 4805 previously returned.
    
    Locations in the USA and Canada are both returned.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/tim_hortons.py
@@ -0,0 +1,44 @@
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.categories import Extras, apply_yes_no
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+
+
+class TimHortonsSpider(Spider):
+    name = "tim_hortons"
+    item_attributes = {"brand": "Tim Hortons", "brand_wikidata": "Q175106"}
+    allowed_domains = ["czqk28jt.apicdn.sanity.io"]
+
+    def start_requests(self):
+        graphql_query = "query GetRestaurants($filter:RestaurantFilter,$limit:Int){allRestaurants(where:$filter,limit:$limit){...RestaurantFragment}} fragment RestaurantFragment on Restaurant{environment diningRoomHours{...HoursFragment} email hasBreakfast hasBurgersForBreakfast hasCurbside hasDineIn hasCatering hasDelivery hasDriveThru hasTableService hasMobileOrdering hasParking hasPlayground hasTakeOut hasWifi hasLoyalty latitude longitude  name number phoneNumber physicalAddress{address1 address2 city country postalCode stateProvince} status} fragment HoursFragment on HoursOfOperation{friClose friOpen monClose monOpen satClose satOpen sunClose sunOpen thrClose thrOpen tueClose tueOpen wedClose wedOpen}"
+        data = {
+            "query": graphql_query,
+            "variables": {"filter": {"status": "Open", "environment": "prod"}, "limit": 10000},
+        }
+        yield JsonRequest(url="https://czqk28jt.apicdn.sanity.io/v1/graphql/prod_th_us/default", data=data)
+
+    def parse(self, response):
+        locations = response.json()["data"]["allRestaurants"]
+        for location in locations:
+            item = DictParser.parse(location)
+            item["ref"] = location["number"]
+
+            apply_yes_no(Extras.TAKEAWAY, item, location["hasTakeOut"] or location["hasDriveThru"], False)
+            apply_yes_no(Extras.DRIVE_THROUGH, item, location["hasDriveThru"], False)
+            apply_yes_no(Extras.INDOOR_SEATING, item, location["hasDineIn"], False)
+            apply_yes_no(Extras.DELIVERY, item, location["hasDelivery"], False)
+            apply_yes_no(Extras.WIFI, item, location["hasWifi"], False)
+
+            item["opening_hours"] = OpeningHours()
+            for day in ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]:
+                if not location["diningRoomHours"].get(f"{day}Open") or not location["diningRoomHours"].get(
+                    f"{day}Close"
+                ):
+                    continue
+                open_time = location["diningRoomHours"][f"{day}Open"].split(" ", 1)[1]
+                close_time = location["diningRoomHours"][f"{day}Close"].split(" ", 1)[1]
+                item["opening_hours"].add_range(day.title(), open_time, close_time, "%H:%M:%S")
+
+            yield item
