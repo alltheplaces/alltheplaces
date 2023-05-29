837e34f27fdcbc7ed772df4f83e8c01d94e95404
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-05-29 16:47:23 +0100

    Fix Condado Tacos US Spider regex (#5372)

--- locations/spiders/condado_tacos_us.py
+++ locations/spiders/condado_tacos_us.py
@@ -10,7 +10,7 @@ class CondadoTacosUSSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Condado Tacos", "brand_wikidata": "Q118640152"}
     allowed_domains = ["locations.condadotacos.com"]
     sitemap_urls = ["https://locations.condadotacos.com/sitemap.xml"]
-    sitemap_rules = [(r"https://locations.condadotacos.com\/([-\w]+)\/([-\w]+)$", "parse_sd")]
+    sitemap_rules = [(r"https://locations.condadotacos.com\/\w\w\/[-.\w]+$", "parse_sd")]
 
     def inspect_item(self, item, response):
         description = response.css("meta[name=description]").attrib["content"]
