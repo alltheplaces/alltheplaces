562293073df84122b8d85df5ae1ec6b1cce916bb
David Hicks <david@hicks.id.au>
2023-06-07 02:16:09 +1000

    Add All Secure Self Storage (US) (31 locations) (#5411)
    * Add All Secure Self Storage (US) (31 locations)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/all_secure_self_storage_us.py
@@ -0,0 +1,21 @@
+from locations.hours import DAYS_FULL, OpeningHours
+from locations.storefinders.storelocatorwidgets import StoreLocatorWidgetsSpider
+
+
+class AllSecureSelfStorageUSSpider(StoreLocatorWidgetsSpider):
+    name = "all_secure_self_storage_us"
+    item_attributes = {"brand": "All Secure Self Storage", "brand_wikidata": "Q119141885"}
+    key = "27e9068ee54b279b61f3d6dbf0fbdb74"
+
+    def parse_item(self, item, location):
+        if "COMING SOON" in item["name"].upper():
+            return
+        item.pop("website")
+        hours_string = ""
+        for day_name in DAYS_FULL:
+            if location["data"].get(f"hours_{day_name}"):
+                time_range = location["data"].get(f"hours_{day_name}")
+                hours_string = f"{hours_string} {day_name}: {time_range}"
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_string)
+        yield item
