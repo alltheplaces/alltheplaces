b1f33cb998ef86b70e72dc86238d05adef6b77a1
Cj Malone <CjMalone@mail.com>
2023-06-06 14:03:02 +0100

    Update Billa CZ wikidata

--- locations/spiders/billa.py
+++ locations/spiders/billa.py
@@ -2,7 +2,7 @@ from scrapy import Spider
 from scrapy.http import JsonRequest
 
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_BG, DAYS_CZ, DAYS_SK, OpeningHours
+from locations.hours import DAYS_BG, DAYS_CZ, DAYS_EN, DAYS_SK, OpeningHours, sanitise_day
 
 
 class BILLASpider(Spider):
@@ -21,25 +21,29 @@ class BILLASpider(Spider):
             yield JsonRequest(url=url)
 
     def parse(self, response):
+        days = DAYS_EN
+        wikidata = "Q537781"
+        if "billa.bg" in response.url:
+            days = DAYS_BG
+        elif "billa.sk" in response.url:
+            days = DAYS_SK
+        elif "billa.cz" in response.url:
+            days = DAYS_CZ
+            wikidata = "Q42969493"
+
         for location in response.json():
             if not location.get("open") and not location.get("openingTimes"):
                 continue
             item = DictParser.parse(location)
+            item["brand_wikidata"] = wikidata
             item["lon"] = location["coordinate"]["x"]
             item["lat"] = location["coordinate"]["y"]
             item["opening_hours"] = OpeningHours()
             for day_hours in location["openingTimes"]:
                 if len(day_hours.get("times", [])) != 2:
                     continue
-                if "billa.bg" in response.url:
-                    day_name = DAYS_BG[day_hours["dayOfWeek"][:2].title()]
-                elif "billa.sk" in response.url:
-                    day_name = DAYS_SK[day_hours["dayOfWeek"][:2].title()]
-                elif "billa.cz" in response.url:
-                    day_name = DAYS_CZ[day_hours["dayOfWeek"][:2].title()]
-                else:
-                    day_name = day_hours["dayOfWeek"]
-                item["opening_hours"].add_range(day_name, day_hours["times"][0], day_hours["times"][1])
+                if day := sanitise_day(day_hours["dayOfWeek"], days):
+                    item["opening_hours"].add_range(day, day_hours["times"][0], day_hours["times"][1])
             if "parking" in location and "spotCount" in location["parking"]:
                 item["extras"]["capacity:motorcar"] = location["parking"]["spotCount"]
             yield item
