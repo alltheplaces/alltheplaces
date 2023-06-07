b337535ec299bb01109bf55ba60890dd5eef9ae3
David Hicks <david@hicks.id.au>
2023-06-07 23:57:09 +1000

    Add Black Rooster Peri Peri (GB) (17 stores)

---
+++ locations/spiders/black_rooster_peri_peri_gb.py
@@ -0,0 +1,15 @@
+import re
+
+from locations.storefinders.storelocatorwidgets import StoreLocatorWidgetsSpider
+
+
+class BlackRoosterPeriPeriGBSpider(StoreLocatorWidgetsSpider):
+    name = "black_rooster_peri_peri_gb"
+    item_attributes = {"brand": "Black Rooster Peri Peri", "brand_wikidata": "Q119158787"}
+    key = "d9fa8573a1f38aee44860f511a67e368"
+
+    def parse_item(self, item, location):
+        item["name"] = item["name"].strip()
+        item["addr_full"] = re.sub(r"\s+", " ", item["addr_full"])
+        item.pop("website")
+        yield item
