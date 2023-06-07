5e0879671b0beab96c96019884459de6dfb2fdcb
David Hicks <david@hicks.id.au>
2023-06-08 00:06:09 +1000

    Add Sal's Pizza (US) (15 stores) (#5425)

---
+++ locations/spiders/sals_pizza_us.py
@@ -0,0 +1,16 @@
+import re
+
+from locations.storefinders.storelocatorwidgets import StoreLocatorWidgetsSpider
+
+
+class SalsPizzaUSSpider(StoreLocatorWidgetsSpider):
+    name = "sals_pizza_us"
+    item_attributes = {"brand": "Sal's Pizza", "brand_wikidata": "Q7403249"}
+    key = "178ecac2bff5d20c4041ada5f817d79e"
+
+    def parse_item(self, item, location):
+        if "Sals" not in location["filters"]:
+            return
+        item["addr_full"] = re.sub(r"\s+", " ", item["addr_full"])
+        item.pop("website")
+        yield item
