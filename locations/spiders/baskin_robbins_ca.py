2b95ae0f52c2358ac06fd7495c3abbf22a1271a9
David Hicks <david@hicks.id.au>
2023-06-07 23:18:31 +1000

    baskin_robbins_ca: fix styling of brand name

--- locations/spiders/baskin_robbins_ca.py
+++ locations/spiders/baskin_robbins_ca.py
@@ -3,5 +3,5 @@ from locations.storefinders.storelocatorwidgets import StoreLocatorWidgetsSpider
 
 class BaskinRobbinsCASpider(StoreLocatorWidgetsSpider):
     name = "baskin_robbins_ca"
-    item_attributes = {"brand": "Baskin Robbins", "brand_wikidata": "Q584601"}
+    item_attributes = {"brand": "Baskin-Robbins", "brand_wikidata": "Q584601"}
     key = "4a009290a87670e408c8d77aada5c556"
