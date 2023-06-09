a18500a8dde8a6c62fbde30b4fff82db4f7be108
David Hicks <david@hicks.id.au>
2023-06-10 00:35:10 +1000

    Add Alive Pharmacy Warehouse (AU) (11 stores)

---
+++ locations/spiders/alive_pharmacy_warehouse_au.py
@@ -0,0 +1,11 @@
+from locations.storefinders.stockist import StockistSpider
+
+
+class AlivePharmacyWarehouseAUSpider(StockistSpider):
+    name = "alive_pharmacy_warehouse_au"
+    item_attributes = {"brand": "Alive Pharmacy Warehouse", "brand_wikidata": "Q119258489"}
+    key = "u6442"
+
+    def parse_item(self, item, location):
+        item.pop("website")
+        yield item
