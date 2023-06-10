11a10aaa14376f4f434b40137bfcef59638811d2
David Hicks <david@hicks.id.au>
2023-06-10 12:49:56 +1000

    Add AGnVET (AU) (71 stores) (#5445)
    * Add AGnVET (AU) (71 stores)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/agnvet_au.py
@@ -0,0 +1,24 @@
+import chompjs
+from scrapy import Selector, Spider
+
+from locations.dict_parser import DictParser
+
+
+class AGnVETAUSpider(Spider):
+    name = "agnvet_au"
+    item_attributes = {"brand": "AGnVET", "brand_wikidata": "Q119263284"}
+    allowed_domains = ["agnvet.com.au"]
+    start_urls = ["https://agnvet.com.au/group/locations/"]
+
+    def parse(self, response):
+        data_raw = response.xpath("//script[contains(text(), '},\"places\":[{')]/text()").get()
+        data_raw = data_raw.split('},"places":', 1)[1]
+        locations = chompjs.parse_js_object(data_raw)
+        for location in locations:
+            item = DictParser.parse(location)
+            item["city"] = location["location"]["city"]
+            item["state"] = location["location"]["state"]
+            item["postcode"] = location["location"]["postal_code"]
+            phone_html = Selector(text=location["location"]["extra_fields"]["phone"])
+            item["phone"] = phone_html.xpath('//a[contains(@href, "tel:")]/@href').get().replace("tel:", "")
+            yield item
