9b3aa0d5c5645c969fc7eaef63b77ccd8883d95d
Dhiraj Tourani <dhiraj.tourani@gmail.com>
2023-06-14 10:47:02 +0100

    Updated with suggested changes after code review

--- locations/spiders/delhaize_be.py
+++ locations/spiders/delhaize_be.py
@@ -8,8 +8,6 @@ class DelhaizeBESpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {
         "brand": "Delhaize",
         "brand_wikidata": "Q1184173",
-        "country": "BE",
     }
     sitemap_urls = ["https://stores.delhaize.be/sitemap.xml"]
-    sitemap_rules = [(".*/fr/.*", "parse_sd")]
-    download_delay = 0.5
+    sitemap_rules = [("/fr/", "parse_sd")]
