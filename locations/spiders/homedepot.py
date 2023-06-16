24364003b4f856d9f90f51fba378a3f347680043
Ian Dees <ian.dees@gmail.com>
2023-06-16 10:39:35 -0500

    Post process hours and location

--- locations/spiders/homedepot.py
+++ locations/spiders/homedepot.py
@@ -1,6 +1,9 @@
+import json
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.hours import OpeningHours
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -16,3 +19,31 @@ class HomeDepotSpider(CrawlSpider, StructuredDataSpider):
         Rule(LinkExtractor(allow=r"^https:\/\/www.homedepot.com\/l\/.*\/\d*$"), callback="parse_sd"),
     ]
     requires_proxy = "US"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        # There's a JSON blob in the HTML that has a "nearby stores" query that includes lat/lon for this store
+        data = json.loads(
+            response.xpath('//script[contains(text(), "__APOLLO_STATE__")]/text()').extract_first().strip()[24:-1]
+        )["ROOT_QUERY"]
+
+        # The key with store info in it seems to change based on store ID, so look for the one we care about
+        # It starts with "storeSearch".
+        store_info = None
+        for k, v in data.items():
+            if k.startswith("storeSearch"):
+                store_info = v
+                break
+
+        store_info = store_info["stores"][0]
+
+        item["lat"] = store_info["coordinates"]["lat"]
+        item["lon"] = store_info["coordinates"]["lng"]
+
+        item["opening_hours"] = OpeningHours()
+        for day, info in store_info["storeHours"].items():
+            if day == "__typename":
+                continue
+
+            item["opening_hours"].add_range(day[:2].title(), info["open"], info["close"])
+
+        yield item
