3c9d332474d0e18e5479fd6958cf6c23727da9ea
Ian Dees <ian.dees@gmail.com>
2023-06-16 09:39:47 -0500

    Switch to a crawl spider because the sitemap is broken

--- locations/spiders/homedepot.py
+++ locations/spiders/homedepot.py
@@ -1,27 +1,18 @@
-import urllib.parse
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-from scrapy.spiders import SitemapSpider
-
-from locations.linked_data_parser import LinkedDataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class HomeDepotSpider(SitemapSpider):
+class HomeDepotSpider(CrawlSpider, StructuredDataSpider):
     name = "homedepot"
     item_attributes = {"brand": "The Home Depot", "brand_wikidata": "Q864407"}
     allowed_domains = ["www.homedepot.com"]
-    download_delay = 0.2
-    sitemap_urls = ("https://www.homedepot.com/sitemap/d/store.xml",)
-    sitemap_rules = [
-        (r"^https:\/\/www.homedepot.com\/l\/.*\/\d*$", "parse_store"),
+    start_urls = [
+        "https://www.homedepot.com/l/storeDirectory",
+    ]
+    rules = [
+        Rule(LinkExtractor(allow=r"^https:\/\/www.homedepot.com\/l\/..$")),
+        Rule(LinkExtractor(allow=r"^https:\/\/www.homedepot.com\/l\/.*\/\d*$"), callback="parse_sd"),
     ]
     requires_proxy = "US"
-
-    def parse_store(self, response):
-        item = LinkedDataParser.parse(response, "LocalBusiness")
-        item["ref"] = item["website"].split("/")[-1]
-        mapurl = urllib.parse.urlsplit(response.css('img[alt="map preview"]').attrib["src"])
-        lat_lon = next(p for p in mapurl.path.split("/") if "," in p)
-        lat, lon = lat_lon.split(",")
-        item["lat"] = lat
-        item["lon"] = lon
-        yield item
