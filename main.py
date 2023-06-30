e38dd3448d1a39bc648873647d6b0fbc3a6c96d4
Jan Callewaert <72855158+JanCallewaert-TomTom@users.noreply.github.com>
2023-06-30 08:33:17 +0200

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from scrapy import cmdline
-
-spider = "bestbuy_ca"
-
-# cmdline.execute("rm {spider}".format(spider=spider).split())
-cmdline.execute("scrapy crawl {spider} -o {spider}.ndgeojson".format(spider=spider).split())
