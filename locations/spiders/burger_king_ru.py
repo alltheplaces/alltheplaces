04f46b0ab38feb7bf33e5084a54b6ff20f11d048
Yuriy Kolosov <35237910+panasonic-mafia@users.noreply.github.com>
2023-07-01 22:36:46 +0300

    Add Burger King (RU) (812 locations) (#5489)
    * Add a spider
    
    * fix
    
    * Fix phones
    
    * lint
    
    * changed applying of wifi
    
    * fix

---
+++ locations/spiders/burger_king_ru.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.http import JsonRequest
+
+from locations.categories import apply_yes_no
+from locations.dict_parser import DictParser
+from locations.hours import DAYS, OpeningHours
+
+
+class BurgerKingRUSpider(scrapy.Spider):
+    name = "burger_king_ru"
+    item_attributes = {"brand": "Бургер Кинг", "brand_wikidata": "Q177054"}
+    allowed_domains = ["burgerkingrus.ru"]
+
+    def start_requests(self):
+        yield JsonRequest("https://orderapp.burgerkingrus.ru/api/v1/restaurants/search")
+
+    def parse(self, response):
+        for poi in response.json().get("response"):
+            item = DictParser.parse(poi)
+            item["phone"] = poi.get("phone", "").replace(":", "")
+            self.parse_hours(item, poi)
+            apply_yes_no("internet_access=wlan", item, poi.get("wifi") == "1")
+            apply_yes_no("drive_through", item, poi.get("king_drive") == "1")
+            yield item
+
+    def parse_hours(self, item, poi):
+        oh = OpeningHours()
+        open, close = poi.get("open_time"), poi.get("close_time")
+        if open and close:
+            oh.add_days_range(DAYS, open, close)
+            item["opening_hours"] = oh.as_opening_hours()
