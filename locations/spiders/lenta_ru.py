8d16f812a6a60306f2e1245a1e21d5ebe48610d0
kwaigonjinn <kolosov.yv@gmail.com>
2023-07-02 17:22:26 +0300

    fix

--- locations/spiders/lenta_ru.py
+++ locations/spiders/lenta_ru.py
@@ -1,15 +1,15 @@
 import scrapy
 from scrapy.http import JsonRequest
-from locations.categories import Categories, apply_category
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
 
 
 class LentaRUSpider(scrapy.Spider):
     name = "lenta_ru"
-    item_attributes = {'brand': 'Лента', 'brand_wikidata': 'Q4258608'}
-    custom_settings = {'ROBOTSTXT_OBEY': False}
+    item_attributes = {"brand": "Лента", "brand_wikidata": "Q4258608"}
+    custom_settings = {"ROBOTSTXT_OBEY": False}
     allowed_domains = ["lenta.com"]
 
     def start_requests(self):
@@ -20,17 +20,14 @@ class LentaRUSpider(scrapy.Spider):
             item = DictParser.parse(poi)
             self.parse_hours(item, poi)
             # A few stores are marked as pet shops
-            if poi.get('type') == 'zoo':
+            if poi.get("type") == "zoo":
                 apply_category(Categories.SHOP_PET, item)
             yield item
 
     def parse_hours(self, item, poi):
-        if poi.get('is24hStore'):
+        if poi.get("is24hStore"):
             item["opening_hours"] = "24/7"
         else:
             oh = OpeningHours()
-            oh.add_days_range(DAYS, poi.get('opensAt'), poi.get('closesAt'))
+            oh.add_days_range(DAYS, poi.get("opensAt"), poi.get("closesAt"))
             item["opening_hours"] = oh.as_opening_hours()
-
-
-
