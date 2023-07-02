55351d1b2ee14029c17c85c6f8ed0c03753e0969
Yuriy Kolosov <35237910+panasonic-mafia@users.noreply.github.com>
2023-07-02 15:04:58 +0300

    Update locations/spiders/opendata_mos_household_services_ru.py
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/opendata_mos_household_services_ru.py
+++ locations/spiders/opendata_mos_household_services_ru.py
@@ -156,6 +156,6 @@ class OpendataMosHouseholdServicesRUSpider(scrapy.Spider):
                             open, close = times[0], times[1]
                             oh.add_range(day, open, close)
                 item["opening_hours"] = oh.as_opening_hours()
-            except Exception as e:
+            except Exception:
                 self.logger.warning(f"Parse hours failed: {hours}")
                 self.crawler.stats.inc_value("atp/opendata_mos_ru/hours/failed")
