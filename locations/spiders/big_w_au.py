bd4b6abf8a6a8fbc9e85214ce30434834ac3c269
David Hicks <david@hicks.id.au>
2023-07-12 02:44:13 +1000

    big_w_au: fix spider (#5516)
    A new JSON blob format is now used.

--- locations/spiders/big_w_au.py
+++ locations/spiders/big_w_au.py
@@ -1,13 +1,13 @@
 import json
 
-import scrapy
+from scrapy import Spider
 
-from locations.dict_parser import DictParser
-from locations.hours import DAYS_EN, OpeningHours
+from locations.hours import OpeningHours
+from locations.items import Feature
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class BigWAUSpider(scrapy.Spider):
+class BigWAUSpider(Spider):
     name = "big_w_au"
     item_attributes = {"brand": "Big W", "brand_wikidata": "Q4906646"}
     allowed_domains = ["www.bigw.com.au"]
@@ -20,16 +20,19 @@ class BigWAUSpider(scrapy.Spider):
     def parse(self, response):
         data_raw = response.xpath('//script[@id="__NEXT_DATA__" and @type="application/json"]/text()').get()
         stores = json.loads(data_raw)["props"]["pageProps"]["serializedData"]["store"]
-        for store_id, store in stores.items():
-            item = DictParser.parse(store)
-            item["street_address"] = item.pop("street")
-            item["website"] = "https://www.bigw.com.au/store/" + item["ref"] + "/" + item["name"].replace(" ", "-")
-            oh = OpeningHours()
-            for day, hours in store["tradingHours"][0]["hours"].items():
-                if day not in DAYS_EN:
-                    continue
-                if hours.upper() == "CLOSED":
-                    continue
-                oh.add_range(day, hours.split(" - ", 1)[0], hours.split(" - ", 1)[1], "%I:%M %p")
-            item["opening_hours"] = oh.as_opening_hours()
-            yield item
+        for store in stores:
+            properties = {
+                "ref": store["id"],
+                "name": store["n"],
+                "phone": store["ph"],
+                "street_address": store["a"]["street"],
+                "city": store["a"]["suburb"],
+                "state": store["a"]["state"],
+                "postcode": store["a"]["postcode"],
+                "lat": store["l"]["lat"],
+                "lon": store["l"]["lng"],
+            }
+            properties["opening_hours"] = OpeningHours()
+            hours_string = " ".join([f"{day} {hours}" for day, hours in store["hr"][0]["hours"].items()])
+            properties["opening_hours"].add_ranges_from_string(hours_string)
+            yield Feature(**properties)
