1c8bbad35110081676c2738269c78d6bc60b6f8d
David Hicks <david@hicks.id.au>
2023-07-12 02:45:16 +1000

    Add Agile Store Locator store finder (#5514)
    * Add Agile Store Locator store finder
    
    * Convert ag_warehouse_au to AgileStoreLocatorSpider
    
    * Convert speedee to AgileStoreLocatorSpider and rename
    
    * Convert ultra_tune_au to AgileStoreLocatorSpider
    
    * Add Petrol (BG) as AgileStoreLocatorSpider
    
    Fixes #5455
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * storefinders/agile_store_locator: fix typo in documentation
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/ultra_tune_au.py
+++ locations/spiders/ultra_tune_au.py
@@ -1,15 +1,7 @@
-from scrapy import Spider
-
-from locations.dict_parser import DictParser
+from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
 
 
-class UltraTuneAUSpider(Spider):
+class UltraTuneAUSpider(AgileStoreLocatorSpider):
     name = "ultra_tune_au"
     item_attributes = {"brand": "Ultra Tune", "brand_wikidata": "Q29025649"}
-    start_urls = ["https://www.ultratune.com.au/wp-admin/admin-ajax.php?action=asl_load_stores"]
-
-    def parse(self, response, **kwargs):
-        for location in response.json():
-            location["street_address"] = location.pop("street")
-
-            yield DictParser.parse(location)
+    allowed_domains = ["www.ultratune.com.au"]
