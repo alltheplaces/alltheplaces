e0a653403d03bbb0062fe1211b4a5438ce488eec
David Hicks <david@hicks.id.au>
2023-07-15 22:11:05 +1000

    Add NOVO (AU) (101 stores)

---
+++ locations/spiders/novo_au.py
@@ -0,0 +1,8 @@
+from locations.storefinders.metalocator import MetaLocatorSpider
+
+
+class NOVOAUSpider(MetaLocatorSpider):
+    name = "novo_au"
+    item_attributes = {"brand": "NOVO", "brand_wikidata": "Q120669012"}
+    brand_id = "9067"
+    country_list = ["Australia"]
