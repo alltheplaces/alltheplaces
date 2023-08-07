4f2db8b1e7cbd969e4713987b183d82d542f3666
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-08-07 19:10:32 +0100

    Fix J Crew spiders (#5620)

---
+++ locations/spiders/j_crew_us.py
@@ -0,0 +1,21 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class JCrewUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "j_crew_us"
+    item_attributes = {"brand": "J. Crew", "brand_wikidata": "Q5370765"}
+    allowed_domains = ["jcrew.com"]
+    sitemap_urls = ["https://stores.jcrew.com/robots.txt", "https://stores.factory.jcrew.com/robots.txt"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["extras"]["branch"] = item.pop("name")
+        if "stores.factory.jcrew.com" in response.url:
+            item["name"] = "J.Crew Factory"
+        else:
+            item["name"] = "J.Crew"
+
+        item["phone"] = response.xpath('//div[@id="phone-main"]/a/@href').get()
+
+        yield item
---
+++ locations/spiders/madewell_us.py
@@ -0,0 +1,16 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class MadewellUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "madewell_us"
+    item_attributes = {"brand": "Madewell", "brand_wikidata": "Q64026213"}
+    sitemap_urls = ["https://stores.madewell.com/robots.txt"]
+    sitemap_rules = [(r"^https://stores\.madewell\.com/\w\w/\w\w/[-.\w]+/[-.'\w]+$", "parse")]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["extras"]["branch"] = item.pop("name")
+        item["phone"] = response.xpath('//div[@id="phone-main"]/a/@href').get()
+
+        yield item
