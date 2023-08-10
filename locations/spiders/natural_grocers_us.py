e237737a7bcb6961aa09292ca73d894f743a5cec
Cj Malone <CjMalone@mail.com>
2023-08-10 12:24:25 +0100

    Fix Natural Grocers US Spider

--- locations/spiders/natural_grocers_us.py
+++ locations/spiders/natural_grocers_us.py
@@ -1,20 +1,32 @@
-from scrapy.spiders import SitemapSpider
+from scrapy import Request
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Spider
 
 from locations.hours import OpeningHours, day_range, sanitise_day
 from locations.items import Feature
 
 
-class NaturalGrocersSpider(SitemapSpider):
-    name = "natural_grocers"
+# Paging links are added by js, so this can't currently be a CrawlSpider
+class NaturalGrocersUSSpider(Spider):
+    name = "natural_grocers_us"
     item_attributes = {"brand": "Natural Grocers", "brand_wikidata": "Q17146520"}
     allowed_domains = ["naturalgrocers.com"]
-    sitemap_urls = ["https://www.naturalgrocers.com/sitemap.xml"]
-    sitemap_rules = [
-        (
-            r"/store/",
-            "parse_store",
-        ),
-    ]
+    link_extractor = LinkExtractor(allow="/store/")
+
+    def start_requests(self):
+        yield Request("https://www.naturalgrocers.com/store-directory?page=0", meta={"page": 0})
+
+    def parse(self, response: Response, **kwargs):
+        links = self.link_extractor.extract_links(response)
+        if len(links) == 1:
+            return
+        else:
+            next_page = response.meta["page"] + 1
+            yield Request(f"https://www.naturalgrocers.com/store-directory?page={next_page}", meta={"page": next_page})
+
+            for link in links:
+                yield Request(link.url, callback=self.parse_store)
 
     def parse_store(self, response):
         item = Feature(
@@ -23,7 +35,7 @@ class NaturalGrocersSpider(SitemapSpider):
             name=response.css(".node-title span::text").get().strip(),
             lat=response.xpath('//div[@id="main"]//meta[@property="latitude"]').attrib["content"],
             lon=response.xpath('//div[@id="main"]//meta[@property="longitude"]').attrib["content"],
-            addr_full=response.xpath('//div[@id="main"]').css(".address-line1::text").get(),
+            street_address=response.xpath('//div[@id="main"]').css(".address-line1::text").get(),
             city=response.xpath('//div[@id="main"]').css(".locality::text").get(),
             state=response.xpath('//div[@id="main"]').css(".administrative-area::text").get(),
             postcode=response.xpath('//div[@id="main"]').css(".postal-code::text").get(),
