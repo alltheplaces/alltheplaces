1d9e9bae9a1e46ff2e997bb297322b2fa6fea0b3
David Hicks <david@hicks.id.au>
2023-08-12 07:23:22 +1000

    sixt: fix spider (#5652)

--- locations/spiders/sixt.py
+++ locations/spiders/sixt.py
@@ -1,13 +1,15 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class SixtSpider(SitemapSpider, StructuredDataSpider):
     name = "sixt"
     item_attributes = {"brand": "SIXT", "brand_wikidata": "Q705664"}
-    sitemap_urls = ["https://www.sixt.co.uk/car-hire/xml-sitemaps/branches.xml"]
+    sitemap_urls = ["https://www.sixt.co.uk/xml-sitemaps/branch.xml"]
     sitemap_rules = [(r"\/car-hire\/[-\w]+\/[-\w]+\/[-\w]+\/$", "parse_sd")]
+    user_agent = BROWSER_DEFAULT
 
     def pre_process_data(self, ld_data, **kwargs):
         if not ld_data["address"].get("addressCountry"):
