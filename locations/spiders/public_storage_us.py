ee5b33339d8b71781540027bcb9fe2c5a17cbf6b
David Hicks <david@hicks.id.au>
2023-08-12 06:30:05 +1000

    public_storage_us: fix spider

--- locations/spiders/public_storage_us.py
+++ locations/spiders/public_storage_us.py
@@ -7,8 +7,8 @@ from locations.structured_data_spider import StructuredDataSpider
 class PublicStorageUSSpider(CrawlSpider, StructuredDataSpider):
     name = "public_storage_us"
     item_attributes = {"brand": "Public Storage", "brand_wikidata": "Q1156045"}
-    allowed_domains = ["www1.publicstorage.com"]
-    start_urls = ["https://www1.publicstorage.com/site-map-states"]
+    allowed_domains = ["www.publicstorage.com"]
+    start_urls = ["https://www.publicstorage.com/site-map-states"]
     rules = [
         Rule(LinkExtractor(allow=r"/site-map-states-[-\w]+$")),
         Rule(LinkExtractor(allow=r"/self-storage-.+/\d+.html$"), callback="parse_sd"),
