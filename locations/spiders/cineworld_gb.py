6ae4eb73ec3f5b9a8d316a3e2b77caad146d3c40
Cj Malone <CjMalone@mail.com>
2023-08-15 15:08:43 +0100

    Add Cineworld GB Spider

---
+++ locations/spiders/cineworld_gb.py
@@ -0,0 +1,30 @@
+import json
+import re
+
+from scrapy import Spider
+
+from locations.dict_parser import DictParser
+from locations.spiders.vapestore_gb import clean_address
+
+
+class CineworldGBSpider(Spider):
+    name = "cineworld_gb"
+    item_attributes = {"brand": "Cineworld", "brand_wikidata": "Q5120901"}
+    start_urls = ["https://www.cineworld.co.uk/"]
+    requires_proxy = True
+
+    def parse(self, response, **kwargs):
+        for location in json.loads(re.search(r"apiSitesList = (\[.+\]),", response.text).group(1)):
+            item = DictParser.parse(location)
+            item["street_address"] = clean_address(
+                [
+                    location["address"]["address1"],
+                    location["address"]["address2"],
+                    location["address"]["address3"],
+                    location["address"]["address4"],
+                ]
+            )
+            item["ref"] = location["externalCode"]
+            item["website"] = response.urljoin("{}/{}".format(location["uri"], location["externalCode"]))
+
+            yield item
