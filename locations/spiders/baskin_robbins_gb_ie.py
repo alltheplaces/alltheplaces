f85defecdc56c7c937297ac34b319c687f83a5a0
David Hicks <david@hicks.id.au>
2023-08-15 22:32:43 +1000

    baskin_robbins_gb: rename to baskin_robbins_gb_ie
    There is a single location in Dublin which this API account also
    returns.

--- locations/spiders/baskin_robbins_gb_ie.py
+++ locations/spiders/baskin_robbins_gb_ie.py
@@ -1,7 +1,7 @@
 from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
 
 
-class BaskinRobbinsGBSpider(AgileStoreLocatorSpider):
-    name = "baskin_robbins_gb"
+class BaskinRobbinsGBIESpider(AgileStoreLocatorSpider):
+    name = "baskin_robbins_gb_ie"
     item_attributes = {"brand": "Baskin-Robbins", "brand_wikidata": "Q584601"}
     allowed_domains = ["baskinrobbins.co.uk"]
