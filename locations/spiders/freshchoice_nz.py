95c6e6bcfe4eaae03a50717433590c86d1ab8c8f
David Hicks <david@hicks.id.au>
2023-08-17 23:53:18 +1000

    Add FreshChoice (NZ) (40 stores) (#5697)
    * Add FreshChoice (NZ) (40 stores)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/freshchoice_nz.py
@@ -0,0 +1,46 @@
+import re
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.hours import OpeningHours
+from locations.items import Feature
+
+
+class FreshChoiceNZSpider(CrawlSpider):
+    name = "freshchoice_nz"
+    item_attributes = {"brand": "FreshChoice", "brand_wikidata": "Q22271877"}
+    allowed_domains = ["store.freshchoice.co.nz"]
+    start_urls = ["https://store.freshchoice.co.nz/multipage"]
+    rules = [Rule(LinkExtractor(restrict_xpaths='//a[contains(@class, "StoreLink--Default")]'), callback="parse")]
+    custom_settings = {"ROBOTSTXT_OBEY": False}  # Avoid many robots.txt queries as each store is a subdomain
+
+    def parse(self, response):
+        properties = {
+            "ref": response.url,
+            "name": response.xpath('//meta[@property="og:title"]/@content').get("").strip(),
+            "addr_full": re.sub(
+                r"\s+",
+                " ",
+                " ".join(
+                    response.xpath(
+                        '//div[@class="Footer__Column"]//a[contains(@href, "maps.google.com")]/text()'
+                    ).getall()
+                ),
+            ).strip(),
+            "phone": response.xpath('//div[@class="Footer__Column"]//a[contains(@href, "tel:")]/@href')
+            .get("")
+            .replace("tel:", "")
+            .strip(),
+            "email": response.xpath('//div[@class="Footer__Column"]//a[contains(@href, "mailto:")]/@href')
+            .get("")
+            .replace("mailto:", "")
+            .strip(),
+            "website": response.url,
+        }
+        hours_string = " ".join(
+            response.xpath('//div[@class="Footer__Column"]//dl[@class="TradingHours__Days"]//text()').getall()
+        ).replace("day ", "day: ")
+        properties["opening_hours"] = OpeningHours()
+        properties["opening_hours"].add_ranges_from_string(hours_string)
+        yield Feature(**properties)
