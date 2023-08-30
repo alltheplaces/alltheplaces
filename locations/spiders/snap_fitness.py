1bcbb55cfcf06f2c714465570711f6e83f205c22
David Hicks <david@hicks.id.au>
2023-08-31 03:51:54 +1000

    Add Snap Fitness (worldwide) (1032 locations) (#5762)
    * Add Snap Fitness (worldwide) (397 locations)
    
    Fixes #690
    
    * snap_fitness: add missing countries
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * snap_fitness: remove unwanted print statement
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/snap_fitness.py
@@ -0,0 +1,64 @@
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS, OpeningHours
+from locations.structured_data_spider import clean_facebook, clean_instagram
+
+
+class SnapFitnessSpider(Spider):
+    name = "snap_fitness"
+    item_attributes = {"brand": "Snap Fitness", "brand_wikidata": "Q7547254"}
+    allowed_domains = ["www.snapfitness.com"]
+    country_codes = [
+        "ae",
+        "au",
+        "be",
+        "ca",
+        "es",
+        "hk",
+        "id",
+        "ie",
+        "mx",
+        "nl",
+        "nz",
+        "ph",
+        "sa_en",
+        "sg",
+        "tr",
+        "tw",
+        "uk",
+        "us",
+    ]
+
+    def start_requests(self):
+        for country_code in self.country_codes:
+            yield JsonRequest(url=f"https://www.snapfitness.com/{country_code}/api/location-finder-edge")
+
+    def parse(self, response):
+        for location in response.json()["items"]:
+            item = DictParser.parse(location["customProperties"]["contactDetails"])
+            item["ref"] = location["id"]
+            item["name"] = location["name"]
+            item["lat"] = location["customProperties"]["latitude"]
+            item["lon"] = location["customProperties"]["longitude"]
+            item["street_address"] = ", ".join(
+                filter(
+                    None,
+                    [
+                        location["customProperties"]["contactDetails"].get("address"),
+                        location["customProperties"]["contactDetails"].get("address2"),
+                    ],
+                )
+            )
+            item["website"] = response.url.replace("/api/location-finder-edge", location["urlPath"])
+            if not item.get("extras"):
+                item["extras"] = {}
+            item["extras"]["contact:instagram"] = clean_instagram(
+                location["customProperties"]["contactDetails"].get("instagramUrl", "")
+            )
+            item["facebook"] = clean_facebook(location["customProperties"]["contactDetails"].get("facebookUrl", ""))
+            if location["customProperties"]["contactDetails"].get("open24Hours"):
+                item["opening_hours"] = OpeningHours()
+                item["opening_hours"].add_days_range(DAYS, "00:00", "23:59")
+            yield item
