97185dc40aa5adf286e9097e613f25c89f360804
David Hicks <david@hicks.id.au>
2023-08-30 23:37:27 +1000

    canadiantire: fix and rename to canadian_tire_ca

--- locations/spiders/canadian_tire_ca.py
+++ locations/spiders/canadian_tire_ca.py
@@ -1,18 +1,19 @@
 import re
 
-import scrapy
+from scrapy.http import JsonRequest
+from scrapy.spiders import SitemapSpider
 
+from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
-from locations.items import Feature
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class CanadianTireSpider(scrapy.spiders.SitemapSpider):
-    name = "canadiantire"
+class CanadianTireSpiderCA(SitemapSpider):
+    name = "canadian_tire_ca"
     item_attributes = {"brand": "Canadian Tire", "brand_wikidata": "Q1032400"}
     allowed_domains = ["canadiantire.ca"]
     sitemap_urls = [
-        "https://www.canadiantire.ca/sitemap_store_en_01.xml",
+        "https://www.canadiantire.ca/sitemap_Store-en_CA-CAD.xml"
     ]
     sitemap_rules = [("", "parse_store")]
     custom_settings = {
@@ -32,34 +33,28 @@ class CanadianTireSpider(scrapy.spiders.SitemapSpider):
         }
         id_url = re.findall("[0-9]+", response.url)[0]
         template = "https://apim.canadiantire.ca/v1/store/store/{id_url}?lang=en_CA"
-
-        yield scrapy.Request(url=template.format(id_url=id_url), headers=headers, callback=self.parse_store_details)
+        yield JsonRequest(url=template.format(id_url=id_url), headers=headers, callback=self.parse_store_details)
 
     def parse_store_details(self, response):
-        oh = OpeningHours()
+        item = DictParser.parse(response.json())
+        item["lat"] = response.json().get("geoPoint", {}).get("latitude")
+        item["lon"] = response.json().get("geoPoint", {}).get("longitude")
+        item["street_address"] = ", ".join(filter(None, [response.json().get("address", {}).get("line1"), response.json().get("address", {}).get("line2")]))
+        item["city"] = response.json().get("address", {}).get("town")
+        item["state"] = response.json().get("address", {}).get("region", {}).get("name")
+        item["country"] = response.json().get("address", {}).get("country", {}).get("isocode")
+        item["phone"] = response.json().get("address", {}).get("phone")
+        item["email"] = response.json().get("address", {}).get("email")
+        item["website"] = "https://www.canadiantire.ca" + response.json().get("url")
+        item["opening_hours"] = OpeningHours()
         if response.json().get("storeServices"):
             for day in response.json().get("storeServices", {})[0].get("weekDayOpeningList"):
                 if day.get("closed"):
                     continue
-                oh.add_range(
+                item["opening_hours"].add_range(
                     day=day.get("weekDay"),
                     open_time=day.get("openingTime", {}).get("formattedHour").replace(".", ""),
                     close_time=day.get("closingTime", {}).get("formattedHour").replace(".", ""),
                     time_format="%I:%M %p",
                 )
-
-        properties = {
-            "ref": response.json().get("id"),
-            "name": response.json().get("name"),
-            "postcode": response.json().get("address", {}).get("postalCode"),
-            "country": response.json().get("address", {}).get("country", {}).get("isocode"),
-            "lat": response.json().get("geoPoint", {}).get("latitude"),
-            "lon": response.json().get("geoPoint", {}).get("longitude"),
-            "phone": response.json().get("address", {}).get("phone"),
-            "email": response.json().get("address", {}).get("email"),
-            "street_address": response.json().get("address", {}).get("line1"),
-            "website": f'https://www.canadiantire.ca{response.json().get("url")}',
-            "opening_hours": oh.as_opening_hours(),
-        }
-
-        yield Feature(**properties)
+        yield item
