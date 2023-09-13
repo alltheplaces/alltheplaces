901d6a2cf473b161b3bc26047ec427c99d4ff9c1
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-09-13 23:17:03 +0300

    Create lidl_at.py (#5258)
    * Create lidl_at.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added DAYS_AT
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update lidl_at.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/hours.py
+++ locations/hours.py
@@ -13,6 +13,8 @@ DAYS_FULL = [
     "Sunday",
 ]
 
+DAYS_AT = {"Mo": "Mo", "Di": "Tu", "Mi": "We", "Do": "Th", "Fr": "Fr", "Sa": "Sa", "So": "Su"}
+
 DAYS_EN = {
     "Monday": "Mo",
     "Mon": "Mo",
