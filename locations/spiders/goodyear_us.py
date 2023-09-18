c7fb67f6ac571654398546da7530a6e7ea215db8
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-09-18 17:44:21 +0100

    Goodyear improvments (#5884)
    * Add NZ to Goodyear AU Spider
    
    * Fix Goodyear US Spider
    
    * Remove Goodyear spider

--- locations/spiders/goodyear_us.py
+++ locations/spiders/goodyear_us.py
@@ -1,27 +1,16 @@
-import json
-
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class GoodYearSpider(SitemapSpider, StructuredDataSpider):
+class GoodYearUSSpider(SitemapSpider, StructuredDataSpider):
     name = "goodyear_us"
     item_attributes = {"brand": "Goodyear", "brand_wikidata": "Q620875"}
-    sitemap_urls = [
-        "https://www.goodyear.com/sitemap-retail-detail.xml",
-        "https://www.goodyearautoservice.com/sitemap-retail-detail.xml",
-    ]
-    sitemap_rules = [("/tire-shop/", "parse_sd"), ("/shop/", "parse_sd")]
-    time_format = "%I:%M %p"
+    sitemap_urls = ["https://www.goodyearautoservice.com/sitemap-retail-locations-sitemap-GAS.xml"]
+    sitemap_rules = [("/location/", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data):
         item["lat"] = ld_data.get("areaServed", {}).get("geoMidpoint", {}).get("latitude")
         item["lon"] = ld_data.get("areaServed", {}).get("geoMidpoint", {}).get("longitude")
-
-        if not item["lat"]:
-            coords = json.loads(response.xpath("//@data-location").get())
-            item["lat"] = coords.get("latitude")
-            item["lon"] = coords.get("longitude")
 
         yield item
