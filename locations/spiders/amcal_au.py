0a3faa10f15c661728855c49c829ad3e1567fc51
David Hicks <david@hicks.id.au>
2023-09-18 12:21:09 +1000

    amcal_au: fix spider (#5874)
    * amcal_au: fix spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/amcal_au.py
+++ locations/spiders/amcal_au.py
@@ -1,22 +1,62 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Spider
+from scrapy.http import JsonRequest
 
-from locations.google_url import extract_google_position
-from locations.spiders.vapestore_gb import clean_address
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_FULL, OpeningHours
 
 
-class AmcalAUSpider(CrawlSpider, StructuredDataSpider):
+class AmcalAUSpider(Spider):
     name = "amcal_au"
     item_attributes = {"brand": "Amcal", "brand_wikidata": "Q63367373"}
-    start_urls = ["https://www.amcal.com.au/store-locator"]
-    rules = [
-        Rule(LinkExtractor(allow=r"/store\-locator/\w{2,3}$")),
-        Rule(LinkExtractor(allow=r"/store\-locator/\w{2,3}/\w+"), callback="parse_sd"),
-    ]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["street_address"] = clean_address(ld_data["address"]["streetAddress"])
-        extract_google_position(item, response)
+    def start_requests(self):
+        data = {
+            "businessid": "4",
+            "latitude": "0",
+            "longitude": "0",
+            "session_id": "0000000000000000000000000000000000000000",
+        }
+        headers = {"Origin": "https://amcal.com.au"}
+        yield JsonRequest(
+            url="https://app.medmate.com.au/connect/api/get_locations", data=data, headers=headers, method="POST"
+        )
 
+    def parse(self, response):
+        for location in response.json():
+            if location["openstatus"] != "Open":
+                continue
+            item = DictParser.parse(location)
+            item["name"] = location["locationname"]
+            item["website"] = (
+                "https://amcal.com.au/store/"
+                + str(location["locationid"])
+                + "/"
+                + location["state"]
+                + "/"
+                + location["slug"]
+                + "/"
+            )
+            data = {
+                "businessid": "4",
+                "include_services": True,
+                "locationid": str(location["locationid"]),
+                "session_id": "0000000000000000000000000000000000000000",
+            }
+            headers = {"Origin": "https://amcal.com.au"}
+            yield JsonRequest(
+                url="https://app.medmate.com.au/connect/api/get_pharmacy",
+                data=data,
+                headers=headers,
+                method="POST",
+                meta={"item": item},
+                callback=self.add_opening_hours,
+            )
+
+    def add_opening_hours(self, response):
+        item = response.meta["item"]
+        item["opening_hours"] = OpeningHours()
+        for day_name, day_hours in response.json()["trading_hours"].items():
+            if day_name not in DAYS_FULL:
+                continue
+            item["opening_hours"].add_range(day_name, day_hours["open"], day_hours["closed"], "%I:%M %p")
         yield item
