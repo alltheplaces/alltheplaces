66e28101dc44b7f7641b550a8d8f1e6cdce6b4ca
Cj Malone <CjMalone@mail.com>
2023-09-20 10:56:38 +0100

    Add GB train spiders

---
+++ locations/spiders/southeastern_railway_gb.py
@@ -0,0 +1,27 @@
+import re
+
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.google_url import extract_google_position
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class SoutheasternRailwayGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "southeastern_railway_gb"
+    item_attributes = {"extras": {"operator": "Southeastern", "operator:wikidata": "Q1696490"}}
+    sitemap_urls = ["https://www.southeasternrailway.co.uk/robots.txt"]
+    sitemap_rules = [("/station-information/stations/", "parse")]
+    search_for_facebook = False
+    search_for_twitter = False
+    search_for_email = False
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        extract_google_position(item, response)
+
+        if m := re.search(r"Station Code:</b> (\w{3})", response.text):
+            item["ref"] = item["extras"]["ref:crs"] = m.group(1)
+
+        apply_category(Categories.TRAIN_STATION, item)
+
+        yield item
