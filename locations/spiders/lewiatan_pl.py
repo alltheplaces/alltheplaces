349fdb02b62a3d12d18078996407143226c02d21
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-09-21 03:16:59 +0100

    Add Lewiatan PL Spider (#5896)
    * Add Lewiatan PL Spider
    
    * Move name to operator

---
+++ locations/spiders/lewiatan_pl.py
@@ -0,0 +1,20 @@
+import html
+
+from scrapy import Spider
+
+from locations.dict_parser import DictParser
+
+
+class LewiatanPLSpider(Spider):
+    name = "lewiatan_pl"
+    item_attributes = {"brand": "Lewiatan", "brand_wikidata": "Q11755396"}
+    start_urls = ["https://www.lewiatan.pl/api/stores"]
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["data"]:
+            item = DictParser.parse(location)
+            item["extras"]["operator"] = html.unescape(item.pop("name"))
+            item["street_address"] = item.pop("addr_full")
+            item["website"] = response.urljoin(location["url"])
+
+            yield item
