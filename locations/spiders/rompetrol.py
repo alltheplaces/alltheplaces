db48bb5f3e837b792b92098166fedc772cc69c82
Cj Malone <CjMalone@mail.com>
2023-09-25 10:56:41 +0100

    [Rompetrol] Drop duplicate BG locations

--- locations/spiders/rompetrol.py
+++ locations/spiders/rompetrol.py
@@ -8,16 +8,14 @@ class RompetrolSpider(Spider):
     name = "rompetrol"
     item_attributes = {"brand": "Rompetrol", "brand_wikidata": "Q1788862"}
     allowed_domains = [
-        "www.rompetrol.bg",
         "www.rompetrol.ge",
         "www.rompetrol.md",
         "www.rompetrol.ro",
     ]
     start_urls = [
-        "https://www.rompetrol.bg/routeplanner/stations?language_id=1",
         "https://www.rompetrol.ge/routeplanner/stations?language_id=1",
         "https://www.rompetrol.md/routeplanner/stations?language_id=1",
-        "https://www.rompetrol.ro/routeplanner/stations?language_id=1",
+        "https://www.rompetrol.ro/routeplanner/stations?language_id=1",  # BG and RO locations
     ]
 
     def parse(self, response):
@@ -25,26 +23,17 @@ class RompetrolSpider(Spider):
             location["street_address"] = location.pop("address", None)
             item = DictParser.parse(location)
 
-            if "www.rompetrol.bg" in response.url:
-                item["country"] = "BG"
-            elif "www.rompetrol.ge" in response.url:
-                item["country"] = "GE"
-            elif "www.rompetrol.md" in response.url:
-                item["country"] = "MD"
-            elif "www.rompetrol.ro" in response.url:
-                item["country"] = "RO"
+            item["ref"] = "{}:{}".format(response.url, item["ref"])
 
-            item["ref"] = "{}-{}".format(item["country"], item["ref"])
-
-            apply_yes_no(Fuel.OCTANE_98, item, "27" in location["services"], False)
-            apply_yes_no(Fuel.OCTANE_95, item, "28" in location["services"], False)
-            apply_yes_no(Fuel.GTL_DIESEL, item, "30" in location["services"], False)
-            apply_yes_no(Fuel.DIESEL, item, "31" in location["services"], False)
-            apply_yes_no(Fuel.ADBLUE, item, "14" in location["services"], False)
-            apply_yes_no(Fuel.LPG, item, "5" in location["services"], False)
-            apply_yes_no(Extras.VACUUM_CLEANER, item, "21" in location["services"], False)
-            apply_yes_no(Extras.SHOWERS, item, "15" in location["services"], False)
-            apply_yes_no(Extras.WIFI, item, "7" in location["services"], False)
+            apply_yes_no(Fuel.OCTANE_98, item, "27" in location["services"])
+            apply_yes_no(Fuel.OCTANE_95, item, "28" in location["services"])
+            apply_yes_no(Fuel.GTL_DIESEL, item, "30" in location["services"])
+            apply_yes_no(Fuel.DIESEL, item, "31" in location["services"])
+            apply_yes_no(Fuel.ADBLUE, item, "14" in location["services"])
+            apply_yes_no(Fuel.LPG, item, "5" in location["services"])
+            apply_yes_no(Extras.VACUUM_CLEANER, item, "21" in location["services"])
+            apply_yes_no(Extras.SHOWERS, item, "15" in location["services"])
+            apply_yes_no(Extras.WIFI, item, "7" in location["services"])
 
             apply_category(Categories.FUEL_STATION, item)
 
