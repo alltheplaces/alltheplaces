5d1c82280aacb1b7958e5f98fb7ccdcedc8a988e
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-09-26 01:33:35 +0100

    [CanadianTireCA] Skip fetching html page (#5937)
    * [CanadianTireCA] Skip fetching html page
    
    * [CanadianTireCA] Add category

--- locations/spiders/canadian_tire_ca.py
+++ locations/spiders/canadian_tire_ca.py
@@ -1,37 +1,39 @@
 import re
 
-from scrapy.http import JsonRequest
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class CanadianTireSpiderCA(SitemapSpider):
+class CanadianTireCASpider(SitemapSpider):
     name = "canadian_tire_ca"
     item_attributes = {"brand": "Canadian Tire", "brand_wikidata": "Q1032400"}
     allowed_domains = ["canadiantire.ca"]
     sitemap_urls = ["https://www.canadiantire.ca/sitemap_Store-en_CA-CAD.xml"]
-    sitemap_rules = [("", "parse_store")]
-    user_agent = BROWSER_DEFAULT
+    sitemap_rules = [("", "parse_store_details")]
     requires_proxy = True  # Data centre IP ranges appear to be blocked (time out)
-
-    def parse_store(self, response):
-        headers = {
+    custom_settings = {
+        "DEFAULT_REQUEST_HEADERS": {
             "User-Agent": BROWSER_DEFAULT,
-            "Host": "apim.canadiantire.ca",
             "baseSiteId": "CTR",
             "Ocp-Apim-Subscription-Key": "c01ef3612328420c9f5cd9277e815a0e",
         }
-        id_url = re.findall("[0-9]+", response.url)[0]
-        template = "https://apim.canadiantire.ca/v1/store/store/{id_url}?lang=en_CA"
-        yield JsonRequest(url=template.format(id_url=id_url), headers=headers, callback=self.parse_store_details)
+    }
+
+    def sitemap_filter(self, entries):
+        for entry in entries:
+            # Exclude bad pages eg https://www.canadiantire.ca/en/store-details/null/-979.html
+            if ref := re.search(r"\w\-(\d+).html$", entry["loc"]):
+                if ref.group(1) in ["949"]:  # Test store
+                    continue
+                entry["loc"] = "https://apim.canadiantire.ca/v1/store/store/{}?lang=en_CA".format(ref.group(1))
+                yield entry
 
     def parse_store_details(self, response):
         item = DictParser.parse(response.json())
-        item["lat"] = response.json().get("geoPoint", {}).get("latitude")
-        item["lon"] = response.json().get("geoPoint", {}).get("longitude")
         item["street_address"] = ", ".join(
             filter(
                 None, [response.json().get("address", {}).get("line1"), response.json().get("address", {}).get("line2")]
@@ -54,4 +56,7 @@ class CanadianTireSpiderCA(SitemapSpider):
                     close_time=day.get("closingTime", {}).get("formattedHour").replace(".", ""),
                     time_format="%I:%M %p",
                 )
+
+        apply_category(Categories.SHOP_DEPARTMENT_STORE, item)
+
         yield item
