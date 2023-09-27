2a544d48abcecaf8524dd916c41e8c2c1daefe02
Cj Malone <CjMalone@mail.com>
2023-09-27 09:18:48 +0100

    Clean up opening hours

--- locations/spiders/mcdonalds_cz.py
+++ locations/spiders/mcdonalds_cz.py
@@ -18,12 +18,10 @@ class McDonaldsCZSpider(scrapy.Spider):
         if worktime := poi.get("worktime"):
             oh = OpeningHours()
             try:
-                i = 0
-                for day in worktime:
-                    open, close = day.split(" - ")
-                    oh.add_range(DAYS[i], open.strip(), close.strip())
-                    i += 1
-                item["opening_hours"] = oh.as_opening_hours()
+                for day, times in zip(DAYS, worktime):
+                    open, close = times.split(" - ")
+                    oh.add_range(day, open.strip(), close.strip())
+                item["opening_hours"] = oh
             except:
                 self.logger.warning(f"Couldn't parse opening hours: {worktime}")
 
