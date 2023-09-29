355762a9e60555a33f1fbbf2107e4798a75ddbee
panasonic-mafia <kolosov.yv@gmail.com>
2023-09-29 11:35:50 +0200

    add categories to some spiders

--- locations/spiders/dia_es.py
+++ locations/spiders/dia_es.py
@@ -3,6 +3,7 @@ from gzip import decompress
 
 from scrapy import Request, Spider
 from scrapy.http import JsonRequest
+from locations.categories import Categories, apply_category
 
 from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
@@ -48,4 +49,7 @@ class DiaESSpider(Spider):
                     )
             else:
                 properties["opening_hours"].add_range(DAYS[int(day_number) - 1], *day_hours.split(" - ", 1), "%H:%M")
-        yield Feature(**properties)
+        
+        item = Feature(**properties)
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+        yield item
