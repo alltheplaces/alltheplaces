2efaaa5cc4408dbc3de530f61400900b8d7e3f4e
panasonic-mafia <kolosov.yv@gmail.com>
2023-10-02 14:27:45 +0200

    fix

--- locations/spiders/carrefour_ar.py
+++ locations/spiders/carrefour_ar.py
@@ -10,14 +10,14 @@ class CarrefourARSpider(scrapy.Spider):
     # TODO: I suspect other Carrefour domains have this very same interface
     # TODO: Figure out how to handle change of sha256Hash of persistedQuery.
     start_urls = [
-        '''https://www.carrefour.com.ar/_v/public/graphql/v1?workspace=master&maxAge=short&appsEtag=remove&domain=store&locale=es-AR&operationName=getStoreLocations&extensions={"persistedQuery":{"version":1,"sha256Hash":"81646d6f8cea93c5500fb78cce9a7e282b0a8585bb77079f6085d8e7f5036127","sender":"valtech.carrefourar-store-locator@0.x","provider":"vtex.store-graphql@2.x"},"variables":"eyJhY2NvdW50IjoiY2FycmVmb3VyYXIifQ=="}'''
+        """https://www.carrefour.com.ar/_v/public/graphql/v1?workspace=master&maxAge=short&appsEtag=remove&domain=store&locale=es-AR&operationName=getStoreLocations&extensions={"persistedQuery":{"version":1,"sha256Hash":"81646d6f8cea93c5500fb78cce9a7e282b0a8585bb77079f6085d8e7f5036127","sender":"valtech.carrefourar-store-locator@0.x","provider":"vtex.store-graphql@2.x"},"variables":"eyJhY2NvdW50IjoiY2FycmVmb3VyYXIifQ=="}"""
     ]
 
     brands = {
         "Express": CARREFOUR_EXPRESS,
         "Hipermercado": CARREFOUR_SUPERMARKET,
         "Market": CARREFOUR_MARKET,
-        "Maxi": CARREFOUR_SUPERMARKET
+        "Maxi": CARREFOUR_SUPERMARKET,
     }
 
     def parse(self, response):
