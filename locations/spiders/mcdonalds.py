33825021260fb64736f174a1d6afa3e8c582b07f
Yuriy Kolosov <35237910+panasonic-mafia@users.noreply.github.com>
2023-10-03 00:16:21 +0300

    [mcdonalds] Add Taiwan. (#6013)
    * add mcdonalds for Taiwan
    
    * fix

--- locations/spiders/mcdonalds.py
+++ locations/spiders/mcdonalds.py
@@ -34,6 +34,7 @@ class McDonaldsSpider(scrapy.Spider):
             "en-sa",
             "uk-ua",
             "hu-hu",
+            "zh-tw",
         ]:
             country = locale.split("-")[1]
             for city in city_locations(country.upper(), 20000):
