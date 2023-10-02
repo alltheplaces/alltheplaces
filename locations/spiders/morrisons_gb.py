9d2f6b7832afc5fcf0342ffe38c85eb08bec45a5
Cj Malone <CjMalone@mail.com>
2023-10-02 19:21:58 +0100

    Fix categories

--- locations/spiders/morrisons_gb.py
+++ locations/spiders/morrisons_gb.py
@@ -2,104 +2,68 @@ import string
 
 from scrapy import Spider
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_EN, OpeningHours
+from locations.hours import OpeningHours
+from locations.items import Feature
 
 
-class MorrisonsSpider(Spider):
+def set_operator(brand: {}, item: Feature):
+    item["extras"]["operator"] = brand.get("brand")
+    item["extras"]["operator:wikidata"] = brand.get("brand_wikidata")
+
+
+class MorrisonsGBSpider(Spider):
     name = "morrisons_gb"
-    item_attributes = {"nsi_id": -1}  # Skip NSI matching as it
-    # conflates stores and
-    # fuel stations.
     allowed_domains = ["api.morrisons.com"]
-    start_urls = ["https://api.morrisons.com/location/v2//stores?apikey=kxBdM2chFwZjNvG2PwnSn3sj6C53dLEY&limit=20000"]
+    start_urls = ["https://api.morrisons.com/location/v2/stores?apikey=kxBdM2chFwZjNvG2PwnSn3sj6C53dLEY&limit=20000"]
 
-    MARTINS = {
-        "brand": "Martin's",
-        "brand_wikidata": "Q116779207",
-        "extras": Categories.SHOP_CONVENIENCE.value,
-    }
-
-    MCCOLLS = {
-        "brand": "McColl's",
-        "brand_wikidata": "Q16997477",
-        "extras": Categories.SHOP_CONVENIENCE.value,
-    }
-
-    MORRISONS_FUEL_STATION = {
-        "brand": "Morrisons",
-        "brand_wikidata": "Q922344",
-        "extras": Categories.FUEL_STATION.value,
-    }
-
-    MORRISONS_SUPERMARKET = {
-        "brand": "Morrisons",
-        "brand_wikidata": "Q922344",
-        "extras": Categories.SHOP_SUPERMARKET.value,
-    }
-
-    MORRISONS_DAILY_CONVENIENCE = {
-        "brand": "Morrisons Daily",
-        "brand_wikidata": "Q99752411",
-        "extras": Categories.SHOP_CONVENIENCE.value,
-    }
-
-    MORRISONS_DAILY_FUEL_STATION = {
-        "brand": "Morrisons Daily",
-        "brand_wikidata": "Q99752411",
-        "extras": Categories.FUEL_STATION.value,
-    }
-
-    MORRISONS_SELECT = {
-        "brand": "Morrisons Select",
-        "brand_wikidata": "Q105221633",
-        "extras": Categories.FUEL_STATION.value,
-    }
-
-    RS_MCCOLL = {
-        "brand": "RS McColl",
-        "brand_wikidata": "Q7277785",
-        "extras": Categories.SHOP_CONVENIENCE.value,
-    }
+    MCCOLLS = {"brand": "McColl's", "brand_wikidata": "Q16997477"}
+    MARTINS = {"brand": "Martin's", "brand_wikidata": "Q116779207"}
+    RS_MCCOLL = {"brand": "RS McColl", "brand_wikidata": "Q7277785"}
+    MORRISONS = {"brand": "Morrisons", "brand_wikidata": "Q922344"}
+    MORRISONS_DAILY = {"brand": "Morrisons Daily", "brand_wikidata": "Q99752411"}
+    MORRISONS_SELECT = {"brand": "Morrisons Select", "brand_wikidata": "Q105221633"}
 
     def parse(self, response):
         for location in response.json()["stores"]:
+            location["id"] = str(location.pop("name"))
             item = DictParser.parse(location)
-            item["ref"] = location["name"]
-            item["name"] = location["storeName"]
             item["street_address"] = ", ".join(
                 filter(None, [location["address"].get("addressLine1"), location["address"].get("addressLine2")])
             )
-            item["website"] = (
-                "https://my.morrisons.com/storefinder/"
-                + str(location["name"])
-                + "/"
-                + location["storeName"].lower().translate(str.maketrans("", "", string.punctuation)).replace(" ", "-")
+            item["website"] = "https://my.morrisons.com/storefinder/{}/{}".format(
+                item["ref"],
+                location["storeName"].lower().translate(str.maketrans("", "", string.punctuation)).replace(" ", "-"),
             )
 
             item["opening_hours"] = OpeningHours()
             for day_abbrev, day_hours in location["openingTimes"].items():
-                item["opening_hours"].add_range(
-                    DAYS_EN[day_abbrev.title()], day_hours["open"], day_hours["close"], "%H:%M:%S"
-                )
+                item["opening_hours"].add_range(day_abbrev, day_hours["open"], day_hours["close"], "%H:%M:%S")
 
             if location["storeFormat"] == "supermarket" and location["category"] == "Supermarket":
-                item.update(self.MORRISONS_SUPERMARKET)
+                item.update(self.MORRISONS)
+                apply_category(Categories.SHOP_SUPERMARKET, item)
             elif location["storeFormat"] == "supermarket" and location["category"] == "McColls":
                 if "Morrisons Daily" in location["storeName"]:
-                    item.update(self.MORRISONS_DAILY_CONVENIENCE)
+                    item.update(self.MORRISONS_DAILY)
                 elif "McColl's" in location["storeName"]:
                     item.update(self.MCCOLLS)
                 elif "Rs Mccoll" in location["storeName"]:
                     item.update(self.RS_MCCOLL)
+                elif "Martin's" in location["storeName"]:
+                    item.update(self.MARTINS)
+                apply_category(Categories.SHOP_CONVENIENCE, item)
+                set_operator(self.MCCOLLS, item)
             elif location["storeFormat"] == "supermarket" and location["category"] == "Gas Station":
                 if "Morrisons Daily" in location["storeName"]:
-                    item.update(self.MORRISONS_DAILY_FUEL_STATION)
+                    item.update(self.MORRISONS_DAILY)
                 elif "Morrisons Select" in location["storeName"]:
                     item.update(self.MORRISONS_SELECT)
+                apply_category(Categories.SHOP_CONVENIENCE, item)
             elif location["storeFormat"] == "pfs" and location["category"] == "Gas Station":
-                item.update(self.MORRISONS_FUEL_STATION)
+                item.update(self.MORRISONS)
+                apply_category(Categories.FUEL_STATION, item)
 
             if not item.get("brand"):
                 continue
