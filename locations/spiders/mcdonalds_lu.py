ae3a385cce4d80247ce8f0e07aef1cf34aacb9f4
Cj Malone <CjMalone@mail.com>
2023-10-03 12:48:02 +0100

    Fix html name

--- locations/spiders/mcdonalds_lu.py
+++ locations/spiders/mcdonalds_lu.py
@@ -1,25 +1,16 @@
 import re
 
-import scrapy
+from scrapy import Request
 
-from locations.items import Feature
 from locations.spiders.mcdonalds import McDonaldsSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class McDonaldsLUSpider(scrapy.Spider):
+class McDonaldsLUSpider(StructuredDataSpider):
     name = "mcdonalds_lu"
     item_attributes = McDonaldsSpider.item_attributes
     allowed_domains = ["mcd.lu"]
     start_urls = ["https://mcd.lu/"]
-
-    # TODO: There is opening hours, but no good way to grab it as there is a lot of commented out hours around it.
-
-    def parse_name(self, name):
-        match = re.search(r'<h1 itemprop="name">(.*)</h1>', name)
-        if match:
-            return match.groups()[0].strip()
-        else:
-            return None
 
     def parse_latlon(self, data):
         match = re.search(r"sll=(.*),(.*)&amp;ss", data)
@@ -40,32 +31,16 @@ class McDonaldsLUSpider(scrapy.Spider):
         match = re.sub("<[^<]+?>", "", address)
         return " ".join(match.split())
 
-    def parse_store(self, response):
-        data = response.text
-        name = self.parse_name(data)
-        address = self.parse_address(data)
-        phone = self.parse_phone(data)
-        lat, lon = self.parse_latlon(data)
-        properties = {
-            "ref": response.meta["ref"],
-            "phone": phone,
-            "lon": lon,
-            "lat": lat,
-            "name": name,
-            "addr_full": address,
-            "country": "lu",
-        }
-
-        yield Feature(**properties)
-
-    def parse(self, response):
-        matches = re.finditer(r"<a id=\"snav_(1_\d+)\">", response.text)
-        for matchNum, match in enumerate(matches):
-            ref = match.groups()[0]
-            headers = {"User-Agent": "PostmanRuntime/7.29.0"}
-            yield scrapy.Request(
-                "https://mcd.lu/content.php?r=" + ref + "&lang=de",
-                meta={"ref": ref},
-                headers=headers,
-                callback=self.parse_store,
+    def parse(self, response, **kwargs):
+        for ref in response.xpath('//a[starts-with(@id, "snav_1_")]/@id').getall():
+            yield Request(
+                "https://mcd.lu/content.php?r={}&lang=de".format(ref.replace("snav_", "")), callback=self.parse_sd
             )
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["website"] = None
+        item["addr_full"] = self.parse_address(response.text)
+        item["phone"] = self.parse_phone(response.text)
+        item["lat"], item["lon"] = self.parse_latlon(response.text)
+
+        yield item
