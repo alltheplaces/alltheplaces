64d8e5cf632e90c115cb53dfb0d01e34c77ef15b
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-10-05 10:19:10 +0300

    petrol_bg: add services data
    Resolves #5939

--- locations/spiders/petrol_bg.py
+++ locations/spiders/petrol_bg.py
@@ -1,7 +1,7 @@
 import re
 
 from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
-
+from locations.categories import Categories, Fuel, Extras
 
 class PetrolBGSpider(AgileStoreLocatorSpider):
     name = "petrol_bg"
@@ -12,4 +12,18 @@ class PetrolBGSpider(AgileStoreLocatorSpider):
         if m := re.match(r"^(\d+) (.+)$", item["name"]):
             item["ref"] = m.group(1)
             item["name"] = m.group(2)
+        
+        categories = location["categories"].split(",")
+        apply_yes_no(Fuel.DIESEL, item, ("19" in categories || "20" in categories))
+        apply_yes_no(Fuel.OCTANE_100, item, "21" in categories)
+        apply_yes_no(Fuel.OCTANE_95, item, "22" in categories)
+        apply_yes_no(Fuel.LPG, item, "23" in categories)
+        apply_yes_no(Fuel.CNG, item, "24" in categories)
+        apply_yes_no(Extras.CAR_WASH, item, "25" in categories)
+        apply_yes_no(Extras.ATM, item, "26" in categories)
+        apply_yes_no("restaurant", item, "27" in categories)
+        apply_yes_no("self_service", item, "28" in categories)
+        apply_yes_no(Fuel.ADBLUE, item, "29" in categories)
+        apply_yes_no("amenity:chargingstation", item, "31" in categories)
+        
         yield item
