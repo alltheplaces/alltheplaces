5a5050bfa4ebc75bbc508432d55f5b059c45dddd
Cj Malone <CjMalone@mail.com>
2023-10-05 22:23:10 +0100

    [WilkoGB] Flag closed POIs

--- locations/spiders/wilko_gb.py
+++ locations/spiders/wilko_gb.py
@@ -8,10 +8,17 @@ class WilkoGBSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Wilko", "brand_wikidata": "Q8002536"}
     allowed_domains = ["stores.wilko.com"]
     sitemap_urls = ["https://stores.wilko.com/sitemap.xml"]
-    sitemap_rules = [
-        (
-            r"https:\/\/stores\.wilko\.com\/gb\/([\w-]+\/[-\w\/]+)$",
-            "parse_sd",
-        ),
-    ]
+    sitemap_rules = [(r"https:\/\/stores\.wilko\.com\/gb\/([\w-]+\/[-\w\/]+)$", "parse_sd")]
     wanted_types = ["DepartmentStore"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if closed := response.xpath(
+            '//span[@class="AlertBanner-text"][contains(text(), "The store will permanently close on ")]/text()'
+        ).get():
+            closed_date = closed.replace("The store will permanently close on ", "")
+            item["extras"]["confidence:feature"] = 0.0
+            item["extras"]["end_date"] = closed_date
+        elif item["name"].upper().endswith("CLOSED") or item["name"].upper().endswith("(CLOSED)"):
+            item["extras"]["confidence:feature"] = 0.0
+
+        yield item
