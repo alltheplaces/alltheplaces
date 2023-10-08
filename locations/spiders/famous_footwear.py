b68726d1baf2ddc5ccc9dd6aad0bbe1c6948b84e
Ian Dees <ian.dees@gmail.com>
2023-10-08 09:58:44 -0500

    Update Famous Footwear

--- locations/spiders/famous_footwear.py
+++ locations/spiders/famous_footwear.py
@@ -3,7 +3,6 @@ import re
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
-from locations.user_agents import BROWSER_DEFAULT
 
 
 class FamousFootwearSpider(SitemapSpider, StructuredDataSpider):
@@ -12,8 +11,7 @@ class FamousFootwearSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://ecomprdsharedstorage.blob.core.windows.net/sitemaps/20000/stores-sitemap.xml"]
     sitemap_rules = [("", "parse_sd")]
     wanted_types = ["Store"]
-    user_agent = BROWSER_DEFAULT
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    requires_proxy = "US"
 
     def inspect_item(self, item, response):
         matches = re.search(r'location: \["(.*)", "(.*)"\],', response.text)
