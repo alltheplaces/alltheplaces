228816259dc303a0a516406658640bc3fa7ba1b7
Ian Dees <ian.dees@gmail.com>
2023-10-08 10:33:40 -0500

    Add Gorjana

---
+++ locations/spiders/gorjana_us.py
@@ -0,0 +1,13 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class GorjanaUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "gorjana_us"
+    item_attributes = {"brand": "Massage Envy", "brand_wikidata": "Q115200885"}
+    allowed_domains = ["www.gorjana.com"]
+    sitemap_urls = ("https://www.gorjana.com/sitemap_pages_1.xml",)
+    sitemap_rules = [
+        (r"^https://www.gorjana.com/pages/[^/]+-store-details$", "parse_sd"),
+    ]
