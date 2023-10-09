d239c1b36068cd3a35d6587e9f5f92dc854650ac
Ian Dees <ian.dees@gmail.com>
2023-10-09 17:00:32 -0500

    Snake case for method names

--- locations/spiders/millennium_bank_pl.py
+++ locations/spiders/millennium_bank_pl.py
@@ -15,10 +15,10 @@ class MillenniumBankPLSpider(Spider):
 
     def parse(self, response, **kwargs):
         mapConfig = json.loads(response.xpath("//wc-facilities-map/@config").get())
-        yield JsonRequest(url=response.urljoin(mapConfig["branchesUrl"]), callback=self.parseBranches)
-        yield JsonRequest(url=response.urljoin(mapConfig["atmsUrl"]), callback=self.parseATMs)
+        yield JsonRequest(url=response.urljoin(mapConfig["branchesUrl"]), callback=self.parse_branches)
+        yield JsonRequest(url=response.urljoin(mapConfig["atmsUrl"]), callback=self.parse_atms)
 
-    def parseBranches(self, response, **kwargs):
+    def parse_branches(self, response, **kwargs):
         for branch in response.json():
             item = DictParser.parse(branch)
             item["lat"] = branch["branchLat"]
@@ -26,12 +26,12 @@ class MillenniumBankPLSpider(Spider):
             item["ref"] = branch["branch"]
             item["street_address"] = branch["address_Street"]
             item["city"] = branch["address_City"]
-            item["opening_hours"] = self.parseBranchOpeningHours(branch)
+            item["opening_hours"] = self.parse_branch_opening_hours(branch)
             apply_yes_no(Extras.WHEELCHAIR, item, branch["disabledPersonsFriendly_Movement"] == "1")
             apply_category(Categories.BANK, item)
             yield item
 
-    def parseBranchOpeningHours(self, data):
+    def parse_branch_opening_hours(self, data):
         openingHours = OpeningHours()
         for key in data.keys():
             if key.startswith("openHours_"):
@@ -39,7 +39,7 @@ class MillenniumBankPLSpider(Spider):
                 openingHours.add_ranges_from_string(f"{day} {data[key]}")
         return openingHours
 
-    def parseATMs(self, response, **kwargs):
+    def parse_atms(self, response, **kwargs):
         for atm in response.json():
             item = DictParser.parse(atm)
             item["ref"] = atm["name"]
