ecb04a27251faa3054e98126e494bd60cc7d4fbf
Filip Czaplicki <github@starsep.com>
2023-10-09 03:47:19 +0200

    Add T-Mobile (PL) (809 stores) (#6058)
    * Add T-Mobile (PL) (809 stores)
    
    * T-Mobile PL: use street_prefix
    
    * Update locations/spiders/tmobile_pl.py
    
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>

---
+++ locations/spiders/tmobile_pl.py
@@ -0,0 +1,29 @@
+from scrapy import Spider
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_PL, OpeningHours
+
+
+class TMobilePLSpider(Spider):
+    name = "tmobile_pl"
+    item_attributes = {"brand": "T-Mobile", "brand_wikidata": "Q327634"}
+    start_urls = ["https://www.t-mobile.pl/c/_bffapi/sdr-shops/v1/shops"]
+
+    def parse(self, response, **kwargs):
+        streetPrefixMapping = {
+            "Os.": "Osiedle",
+            "Al.": "Aleja",
+            "Pl.": "Plac",
+            "Ul.": None,  # ulica (street) is omitted - OSM convention in Poland
+        }
+        for feature in response.json():
+            item = DictParser.parse(feature)
+            item["lat"] = feature["coordinates"]["y"]
+            item["lon"] = feature["coordinates"]["x"]
+            if len(feature["contact"]["email"]) > 0:
+                item["email"] = feature["contact"]["email"]
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_ranges_from_string(ranges_string=feature["hours"]["txt"], days=DAYS_PL)
+            streetPrefix = streetPrefixMapping.get(feature["address"]["street_prefix"], None)
+            item["street"] = " ".join(filter(None, [streetPrefix, feature["address"]["street"]]))
+            yield item
