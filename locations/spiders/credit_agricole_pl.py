d09b33a09d3b27ba3839290d974b83fb01c2b6fa
panasonic-mafia <kolosov.yv@gmail.com>
2023-10-10 15:36:57 +0200

    parse hours for atms

--- locations/spiders/credit_agricole_pl.py
+++ locations/spiders/credit_agricole_pl.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -26,10 +27,10 @@ class CreditAgricolePLSpider(SitemapSpider, StructuredDataSpider):
     def parse_atm(self, response):
         item = Feature()
         item["street_address"] = response.xpath('//*[@class="street-address"]//text()').get()
-        # TODO: opening hours
         item["ref"] = item["website"] = response.url
         item["lat"] = response.xpath('//script[contains(., "branchLat")]').re_first(r"var branchLat = '([\d.]+)';")
         item["lon"] = response.xpath('//script[contains(., "branchLng")]').re_first(r"var branchLng = '([\d.]+)';")
+        self.parse_atm_hours(item, response)
 
         atm_type = (
             response.xpath("//h1[@class='section-title title--secondary title--contact-map']/small[1]/text()")
@@ -44,3 +45,13 @@ class CreditAgricolePLSpider(SitemapSpider, StructuredDataSpider):
 
         apply_category(Categories.ATM, item)
         yield item
+
+    def parse_atm_hours(self, item, response):
+        if hours := response.xpath("//p[@datetime]/@datetime").getall():
+            try:
+                oh = OpeningHours()
+                for hour in hours:
+                    oh.add_ranges_from_string(hour.replace('.', ''))
+                item["opening_hours"] = oh.as_opening_hours()
+            except Exception as e:
+                self.logger.warning(f"Failed to parse hours {hours}: {e}")
