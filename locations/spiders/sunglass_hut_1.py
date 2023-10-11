a5c5d61fb9f46fbbba4875156ca3f02cc36b4e83
Arshad-Ali-1a <111346755+Arshad-Ali-1a@users.noreply.github.com>
2023-10-11 07:04:02 +0530

    added spider for sunglass_hut_2 (#6043)
    * added spider for sunglass_hut_2
    
    * made the changes as requested in https://github.com/alltheplaces/alltheplaces/pull/6043#pullrequestreview-1661542276
    
    * added "ref" and "website" to item
    
    * renamed sunglass_hut.py to sunglass_hut_1.py
    
    * sunglass_hut_gb now imports from sunglass_hut_1.py as sunglass_hut.py was renamed.
    
    * changed spider name and class name in sunglass_hut_1.py to match the file name.

--- locations/spiders/sunglass_hut_1.py
+++ locations/spiders/sunglass_hut_1.py
@@ -4,8 +4,8 @@ from locations.structured_data_spider import StructuredDataSpider
 
 
 # AU, CA, GU, NZ, PR, US, VI
-class SunglassHutSpider(SitemapSpider, StructuredDataSpider):
-    name = "sunglass_hut"
+class SunglassHut1Spider(SitemapSpider, StructuredDataSpider):
+    name = "sunglass_hut_1"
     item_attributes = {"brand": "Sunglass Hut", "brand_wikidata": "Q136311"}
     allowed_domains = ["stores.sunglasshut.com"]
     sitemap_urls = ["https://stores.sunglasshut.com/robots.txt"]
