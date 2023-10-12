ae1dd956261b1eeadf71d12e602c3e33c37b0299
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-10-12 21:44:11 +0300

    Update mtexx_bg.py

--- locations/spiders/mtexx_bg.py
+++ locations/spiders/mtexx_bg.py
@@ -2,6 +2,7 @@ from scrapy import Spider
 
 from locations.items import Feature
 
+import re
 
 class MtexxBGSpider(Spider):
     name = "mtexx_bg"
@@ -13,9 +14,10 @@ class MtexxBGSpider(Spider):
         for locations in response.xpath("//div[@data-ux='ContentText']"):
             for location in locations.xpath("//li"):
                 text = location.get()
+                coords = re.findall(r'(\d+\.\d+,\ \d+\.\d+|\d+°\d+\'\d+\.\d+"[NS]\s\d+°\d+\'\d+\.\d+"[EW])', text)
                 properties = {
                     "name": text.rsplit("-")[0],
-                    "lat": text.rsplit("-", 1)[1].rsplit(" ")[0],
-                    "lon": text.rsplit(" ", 1)[1],
+                    "lat": coords[0],
+                    "lon": coords[1],
                 }
                 yield Feature(**properties)
