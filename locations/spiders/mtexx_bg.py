d9a3ae6c52c7007d0433116c46610f1fb965c956
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-10-12 19:43:50 +0300

    Add M-Texx (BG) (362 locations)

---
+++ locations/spiders/mtexx_bg.py
@@ -0,0 +1,20 @@
+from scrapy import Spider
+
+from locations.items import Feature
+
+
+class MtexxBGSpider(Spider):
+    name = "mtexx_bg"
+    item_attributes = {"brand": "M-texx", "brand_wikidata": "Q122947768"}
+    allowed_domains = ["www.m-texx.com"]
+    start_urls = ["https://m-texx.com/локации"]
+
+    def parse(self, response):
+        for locations in response.xpath('//div[@"data-ux"="GridCell"]'):
+          for location in locations.xpath('//li')
+            properties = {
+                "name": store.xpath('.//span[@class="name"]/text()').get().rsplit('-')[0],
+                "lat": store.xpath(".//@data-lat").get().rsplit(',', 1)[0].rsplit(' ')[1],
+                "lon": store.xpath(".//@data-lng").get().rsplit(',', 1)[1],
+            }
+            yield Feature(**properties)
