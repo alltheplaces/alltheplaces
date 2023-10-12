844d2642f2cbe7b869b386da2b979f5d520e9c44
panasonic-mafia <kolosov.yv@gmail.com>
2023-10-12 17:20:20 +0200

    add category to fnb_us spider

--- locations/spiders/fnb_us.py
+++ locations/spiders/fnb_us.py
@@ -1,4 +1,5 @@
 import scrapy
+from locations.categories import Categories, apply_category
 
 from locations.linked_data_parser import LinkedDataParser
 from locations.microdata_parser import MicrodataParser
@@ -21,4 +22,7 @@ class FnbUSSpider(scrapy.spiders.SitemapSpider):
         MicrodataParser.convert_to_json_ld(response)
         item = LinkedDataParser.parse(response, "BankOrCreditUnion")
         item["country"] = "US"
+        apply_category(Categories.BANK, item)
+        # TODO: more data at:
+        # https://www.fnb-online.com/atm-branch-locator?=&query=&facetFilters=%7B%7D&filters=%7B%7D&referrerPageUrl=https%3A%2F%2Fwww.fnb-online.com%2F&verticalUrl=branches.html
         yield item
