79d325723bf471704d622b9c49511532d2a4e3f2
Cj Malone <CjMalone@mail.com>
2023-10-13 14:55:26 +0100

    [CentralEnglandCooperative] Improve brands and operator tagging

--- locations/spiders/central_england_cooperative.py
+++ locations/spiders/central_england_cooperative.py
@@ -1,24 +1,46 @@
 from scrapy.spiders import SitemapSpider
 
-from locations.linked_data_parser import LinkedDataParser
-from locations.microdata_parser import MicrodataParser
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CentralEnglandCooperativeSpider(SitemapSpider):
+def set_operator(brand: dict, item: Feature):
+    item["extras"]["operator"] = brand["brand"]
+    item["extras"]["operator:wikidata"] = brand["brand_wikidata"]
+
+
+COOP_FOOD = {"brand": "The Co-operative Food", "brand_wikidata": "Q107617274"}
+COOP_FUNERALCARE = {"brand": "The Co-operative Funeralcare", "brand_wikidata": "Q7726521"}
+
+CENTRAL_COOP = {"brand": "Central England Co-operative", "brand_wikidata": "Q16986583"}
+
+
+class CentralEnglandCooperativeSpider(SitemapSpider, StructuredDataSpider):
     name = "central_england_cooperative"
-    item_attributes = {
-        "brand": "Central England Co-operative",
-        "brand_wikidata": "Q16986583",
-        "country": "GB",
-    }
     sitemap_urls = ["https://stores.centralengland.coop/sitemap.xml"]
-    sitemap_rules = [
-        (
-            r"https:\/\/stores\.centralengland\.coop\/[-\w]+\/.*$",
-            "parse_item",
-        )
-    ]
+    sitemap_rules = [(r"https:\/\/stores\.centralengland\.coop\/[-\w]+\/.*$", "parse")]
 
-    def parse_item(self, response):
-        MicrodataParser.convert_to_json_ld(response)
-        return LinkedDataParser.parse(response, "LocalBusiness")
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["image"] = None
+
+        if "logo-food" in ld_data["logo"]:
+            apply_category(Categories.SHOP_CONVENIENCE, item)
+            set_operator(CENTRAL_COOP, item)
+
+            item.update(COOP_FOOD)
+            item["brand"], item["branch"] = item.pop("name").split(" - ", 1)
+        elif "logo-funeral" in ld_data["logo"]:
+            apply_category(Categories.SHOP_FUNERAL_DIRECTORS, item)
+            set_operator(CENTRAL_COOP, item)
+
+            if "Central Co-op Funeral" in item["name"]:
+                item.update(COOP_FUNERALCARE)
+                item["branch"] = item.pop("name").replace("Central Co-op Funeral", "").strip(" -")
+        else:
+            apply_category(Categories.SHOP_FLORIST, item)
+            set_operator(CENTRAL_COOP, item)
+
+            item["brand"], item["branch"] = item.pop("name").split(" - ", 1)
+
+        yield item
