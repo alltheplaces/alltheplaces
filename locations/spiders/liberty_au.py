60f4a633a3b5dd7ed59e56dbf687291a6c88b469
Oscar <71343264+0scvr@users.noreply.github.com>
2023-10-15 16:37:44 +0200

    Add liberty_au spider (92 locations) (#6116)
    * add liberty_au spider (92 locations)
    
    Signed-off-by: 0scvr <71343264+0scvr@users.noreply.github.com>
    
    * Improve liberty_au spider
    
    Refactored parse function to use DictParser & added parsing of opening hours.
    
    Signed-off-by: 0scvr <71343264+0scvr@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: 0scvr <71343264+0scvr@users.noreply.github.com>

---
+++ locations/spiders/liberty_au.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy import Selector
+
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+
+
+class LibertyAUSpider(scrapy.Spider):
+    name = "liberty_au"
+    item_attributes = {"brand": "Liberty Oil", "brand_wikidata": "Q106687078"}
+    allowed_domains = ["libertyconvenience.com.au"]
+    start_urls = ["https://www.libertyconvenience.com.au/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
+
+    def parse(self, response):
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["street_address"] = item.pop("addr_full", None)
+            hours_string = " ".join(filter(None, Selector(text=location["hours"]).xpath("//text()").getall()))
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_ranges_from_string(hours_string)
+            yield item
