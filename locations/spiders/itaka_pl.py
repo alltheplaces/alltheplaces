5a48b464dbe8515c02f0c371767687497a756c91
Filip Czaplicki <github@starsep.com>
2023-10-15 13:18:04 +0200

    Add Itaka PL (186 offices)

---
+++ locations/spiders/itaka_pl.py
@@ -0,0 +1,37 @@
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_PL, OpeningHours
+
+
+class ItakaPLSpider(Spider):
+    name = "itaka_pl"
+    item_attributes = {"brand": "Itaka", "brand_wikidata": "Q16560452"}
+    start_urls = ["https://www.itaka.pl/biura/ajax/get/all/?q=data"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for items in response.json()["data"]:
+            # ignoring "agent-prestizowy" and "agent-zwykly"
+            # which are travel agencies cooperating but not branded by Itaka
+            offices = []
+            if "salon-firmowy" in items:
+                offices.extend(items["salon-firmowy"])
+            if "agent-franchising" in items:
+                offices.extend(items["agent-franchising"])
+            for office in offices:
+                details = office["showroom"]["library"]
+                item = DictParser.parse(details)
+                item["ref"] = office["showroom"]["id"]
+                if "fotos" in details:
+                    item["image"] = ";".join([f"https://www.itaka.pl{url}" for url in details["fotos"]])
+                item["website"] = f"https://www.itaka.pl/{details['www']}"
+                openingHours = OpeningHours()
+                for hours in details["opening_hours"]:
+                    days, hoursRange = hours
+                    hoursRange = hoursRange.removesuffix("*")
+                    openingHours.add_ranges_from_string(ranges_string=f"{days} {hoursRange}", days=DAYS_PL)
+                item["opening_hours"] = openingHours
+                yield item
