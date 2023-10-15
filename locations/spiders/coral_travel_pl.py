349a7b7416f353fd55155a33c3f79477beb9f637
Filip Czaplicki <github@starsep.com>
2023-10-15 13:41:47 +0200

    Add Coral Travel PL (134 offices)

---
+++ locations/spiders/coral_travel_pl.py
@@ -0,0 +1,29 @@
+import json
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.items import Feature
+
+
+class CoralTravelPLSpider(Spider):
+    name = "coral_travel_pl"
+    start_urls = ["https://www.coraltravel.pl/rwd_map_container/index/"]
+    item_attributes = {"brand": "Coral Travel", "brand_wikidata": "Q58011479"}
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        script = response.xpath("//script/text()[contains(., 'initMap')]").get()
+        content = script.split("var markers = ")[1].split("];")[0].replace("'", '"').strip().removesuffix(",") + "]"
+        content = content.replace('["", ,, "autorized"],', "")
+        data = json.loads(content)
+        for index, office in enumerate(data):
+            name, lat, lon, officeType = office
+            # TODO: more data in infoWindowContent (HTML in JavaScript)
+            yield Feature(
+                {
+                    "ref": index,
+                    "lat": lat,
+                    "lon": lon,
+                }
+            )
