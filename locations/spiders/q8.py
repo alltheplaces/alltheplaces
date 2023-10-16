12e14bb2585a6058650004cb1ba89423d6968eb2
panasonic-mafia <kolosov.yv@gmail.com>
2023-10-16 15:08:36 +0200

    fix

--- locations/spiders/q8.py
+++ locations/spiders/q8.py
@@ -31,9 +31,7 @@ class Q8Spider(scrapy.Spider):
                     "ref": store.get("StationId"),
                     "name": store.get("Name"),
                     "country": store.get("Country"),
-                    "addr_full": ", ".join(
-                        filter(None, [store.get("Address_line_1"), store.get("Address_line_2")])
-                    ),
+                    "addr_full": ", ".join(filter(None, [store.get("Address_line_1"), store.get("Address_line_2")])),
                     "phone": store.get("Phone"),
                     "email": store.get("Email"),
                     "website": f"https://www.q8.be/{website}" if website else None,
