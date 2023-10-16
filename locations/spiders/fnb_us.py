12f05c56c9e9f32a661018345b1c3bf932d9e0a3
panasonic-mafia <kolosov.yv@gmail.com>
2023-10-16 18:29:20 +0200

    fix

--- locations/spiders/fnb_us.py
+++ locations/spiders/fnb_us.py
@@ -1,8 +1,4 @@
-import scrapy
-
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
-from locations.linked_data_parser import LinkedDataParser
-from locations.microdata_parser import MicrodataParser
 from locations.storefinders.yext import YextSpider
 
 
@@ -10,15 +6,17 @@ class FnbUSSpider(YextSpider):
     name = "fnb_us"
     item_attributes = {"brand": "First National Bank", "brand_wikidata": "Q5426765"}
     api_key = "10f82fdf7a37ee369b154241c59dade1"
-    api_version = '20190101'
-    wanted_types = ["location", 'atm']
+    api_version = "20190101"
+    wanted_types = ["location", "atm"]
 
     def parse_item(self, item, location, **kwargs):
-        entity_type = location['meta']['entityType']
-        if entity_type == 'location':
+        entity_type = location["meta"]["entityType"]
+        if entity_type == "location":
             apply_category(Categories.BANK, item)
-            apply_yes_no(Extras.ATM, item, any(s in ['ATM', 'ATM with Teller Chat'] for s in location.get('c_branchFilters', [])))
-            apply_yes_no(Extras.DRIVE_THROUGH, item, 'Drive-Thru' in location.get('c_branchFilters', []))   
-        elif entity_type == 'atm':
+            apply_yes_no(
+                Extras.ATM, item, any(s in ["ATM", "ATM with Teller Chat"] for s in location.get("c_branchFilters", []))
+            )
+            apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive-Thru" in location.get("c_branchFilters", []))
+        elif entity_type == "atm":
             apply_category(Categories.ATM, item)
         yield item
