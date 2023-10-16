bff414036b8b1ed3aabc490d3c5839d29b9066a6
panasonic-mafia <kolosov.yv@gmail.com>
2023-10-16 18:29:02 +0200

    improve fnb_us spider

--- locations/spiders/fnb_us.py
+++ locations/spiders/fnb_us.py
@@ -1,28 +1,24 @@
 import scrapy
 
-from locations.categories import Categories, apply_category
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.linked_data_parser import LinkedDataParser
 from locations.microdata_parser import MicrodataParser
+from locations.storefinders.yext import YextSpider
 
 
-class FnbUSSpider(scrapy.spiders.SitemapSpider):
+class FnbUSSpider(YextSpider):
     name = "fnb_us"
     item_attributes = {"brand": "First National Bank", "brand_wikidata": "Q5426765"}
-    allowed_domains = ["fnb-online.com"]
-    sitemap_urls = [
-        "https://locations.fnb-online.com/robots.txt",
-    ]
-    sitemap_rules = [
-        (r"^https://locations\.fnb-online\.com/[^/]+/[^/]+/[^/]+$", "parse"),
-    ]
+    api_key = "10f82fdf7a37ee369b154241c59dade1"
+    api_version = '20190101'
+    wanted_types = ["location", 'atm']
 
-    def parse(self, response):
-        for city in response.css('[itemprop="address"] .Address-city'):
-            city.root.set("itemprop", "addressLocality")
-        MicrodataParser.convert_to_json_ld(response)
-        item = LinkedDataParser.parse(response, "BankOrCreditUnion")
-        item["country"] = "US"
-        apply_category(Categories.BANK, item)
-        # TODO: more data at:
-        # https://www.fnb-online.com/atm-branch-locator?=&query=&facetFilters=%7B%7D&filters=%7B%7D&referrerPageUrl=https%3A%2F%2Fwww.fnb-online.com%2F&verticalUrl=branches.html
+    def parse_item(self, item, location, **kwargs):
+        entity_type = location['meta']['entityType']
+        if entity_type == 'location':
+            apply_category(Categories.BANK, item)
+            apply_yes_no(Extras.ATM, item, any(s in ['ATM', 'ATM with Teller Chat'] for s in location.get('c_branchFilters', [])))
+            apply_yes_no(Extras.DRIVE_THROUGH, item, 'Drive-Thru' in location.get('c_branchFilters', []))   
+        elif entity_type == 'atm':
+            apply_category(Categories.ATM, item)
         yield item
