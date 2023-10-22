2c6ead0ae99571e67a8a770bc0e8ddd442c9ffa8
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2023-10-22 22:29:35 +0300

    Create spider for cashterminal_bg

---
+++ locations/spiders/cashterminal_bg.py
@@ -0,0 +1,20 @@
+from scrapy import Spider
+
+from locations.items import Feature
+
+
+class CashterminalBGSpider(Spider):
+    name = "cashterminal_bg"
+    item_attributes = {"brand": "Cashterminal", "brand_wikidata": "Q115668697"}
+    allowed_domains = ["www.cashterminal.eu"]
+    start_urls = ["https://www.cashterminal.eu/places"]
+    no_refs = True
+
+    def parse(self, response):
+        for location in response.xpath("//div[@class='markerList']/a[@data-lat]"):
+            properties = {
+                "name": location.get(),
+                "lat": location.xpath("./@data-lat"),
+                "lon": data.xpahth("./@data-long"),
+            }
+            yield Feature(**properties)
