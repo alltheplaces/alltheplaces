b7dc85173ed8b46f4d9595098f8a4d92866327bd
Colton Piper <pipercolton@gmail.com>
2023-10-24 15:46:58 -0400

    Added Category and Brand Wikidata for Kontanten

--- locations/spiders/kontanten_se.py
+++ locations/spiders/kontanten_se.py
@@ -1,12 +1,13 @@
 import scrapy
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 
 class KontantenSESpider(scrapy.Spider):
     name = "kontanten_se"
 
-    item_attributes = {"brand": "Kontanten"}
+    item_attributes = {"brand": "Kontanten", "brand_wikidata": "Q122831839", "country": "GB"}
 
     def start_requests(self):
         yield scrapy.FormRequest(
@@ -21,7 +22,7 @@ class KontantenSESpider(scrapy.Spider):
     def parse(self, response, **kwargs):
         for store in response.json():
             coordinates = store.get("geometry").get("coordinates")
-            yield Feature(
+            item = Feature(
                 {
                     "ref": store.get("id"),
                     "name": store.get("place"),
@@ -32,3 +33,5 @@ class KontantenSESpider(scrapy.Spider):
                     "lon": coordinates[1],
                 }
             )
+            apply_category(Categories.ATM, item)
+            yield item
