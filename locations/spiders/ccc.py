69efb3c6c6ac795ed64c80fd1469b8b879d13631
Ian Dees <ian.dees@gmail.com>
2023-10-23 19:15:58 -0500

    Update ccc.py

--- locations/spiders/ccc.py
+++ locations/spiders/ccc.py
@@ -31,16 +31,17 @@ class CCCSpider(Spider):
     def parse(self, response, **kwargs):
         shop_data = response.xpath("//div[@id='pos-list-json']/text()").get()
         country = response.url.split("/")[-2].upper()
-        days = DAYS_EN
-        if country in self.days_mapping:
-            days = self.days_mapping[country]
+        days = self.day_mapping.get(country) or DAYS_EN
+
         for shop in json.loads(shop_data):
             item = DictParser.parse(shop)
             item["website"] = f"https://ccc.eu{shop['link']}"
-            item["opening_hours"] = OpeningHours()
             item["country"] = country
-            if "workingHours" in shop:
-                for oh in shop["workingHours"]:
+
+            item["opening_hours"] = OpeningHours()
+            if working_hours := shop.get("workingHours"):
+                for oh in working_hours:
                     for hours_range, days_range in oh.items():
                         item["opening_hours"].add_ranges_from_string(f"{days_range} {hours_range}", days=days)
+
             yield item
