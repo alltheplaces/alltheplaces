e7cd13df101d488b3ae790bbaf1ffb049631e5d8
Exterminator11 <rachit1031@gmail.com>
2023-10-24 21:05:08 +0530

    Added spider for Ron Jon Surf Shop

---
+++ locations/spiders/ron_jon_surf_shop.py
@@ -0,0 +1,20 @@
+from scrapy import Spider
+
+from locations.dict_parser import DictParser
+
+
+class RonJonSurfShopSpider(Spider):
+    name = "ron_jon_surf_shop"
+    item_attributes = {"brand": "Ron Jon Surf Shop", "brand_wikidata": "Q7363993"}
+    allowed_domains = ["www.ronjonsurfshop.com"]
+    start_urls = ["https://www.ronjonsurfshop.com/ajax/store-locator?searchText=&pagesize=15&pageNumber=1"]
+
+    def parse(self, response):
+        for data in response.json()["results"]:
+            item = DictParser.parse(data)
+            if not data["address1"]:
+                item["addr_full"] = data["address2"]
+            else:
+                item["addr_full"] = data["address1"]
+            item["website"] = "www.ronjonsurfshop.com/" + data["url"]
+            yield item
