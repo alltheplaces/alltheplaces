eb16218b61084fe42ed31e0d47c470b3d55c50d3
David Hicks <david@hicks.id.au>
2023-10-30 21:16:36 +1100

    jdwetherspoon: fix spider and rename to j_d_wetherspoon
    Now uses SitemapSpider + StructuredDataSpider.

---
+++ locations/spiders/j_d_wetherspoon.py
@@ -0,0 +1,12 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class JDWetherspoonSpider(SitemapSpider, StructuredDataSpider):
+    name = "j_d_wetherspoon"
+    item_attributes = {"brand": "J D Wetherspoon", "brand_wikidata": "Q6109362"}
+    allowed_domains = ["www.jdwetherspoon.com"]
+    sitemap_urls = ["https://www.jdwetherspoon.com/sitemap.xml"]
+    sitemap_rules = [(r"https:\/\/www\.jdwetherspoon\.com\/pubs\/all-pubs(?:\/[\w\-]+){3}", "parse_sd")]
+    custom_settings = {"REDIRECT_ENABLED": False} # Numerous location pages don't exist.
