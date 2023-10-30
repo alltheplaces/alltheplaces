855d6af32fdb6b7c92b9a69d20f0ad915c352f39
David Hicks <david@hicks.id.au>
2023-10-31 02:54:00 +1100

    reiss_gb: fix spider (#6238)
    * reiss_gb: fix spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/reiss_gb.py
+++ locations/spiders/reiss_gb.py
@@ -1,17 +1,46 @@
 import re
 
+from scrapy import Request
+from scrapy.http import JsonRequest
 from scrapy.spiders import SitemapSpider
 
+from locations.spiders.johnlewis import JohnLewisSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class ReissGBSpider(SitemapSpider, StructuredDataSpider):
+class ReissGBSpider(StructuredDataSpider):
     name = "reiss_gb"
     item_attributes = {"brand": "Reiss", "brand_wikidata": "Q7310479"}
-    sitemap_urls = ["https://www.reiss.com/robots.txt"]
-    sitemap_rules = [(r"/storelocator/.+/\d+$", "parse_sd")]
+    start_urls = ["https://www.reiss.com/storelocator/data/stores"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield JsonRequest(url=url)
+
+    def parse(self, response):
+        for location in response.json()["Stores"]:
+            yield Request(
+                url="https://www.reiss.com/storelocator/{}/{}".format(
+                    location["NA"].lower().replace(" ", ""), location["BR"]
+                ),
+                meta={"id": location["BR"]},
+                callback=self.parse_sd,
+            )
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         if m := re.search(r"\"&daddr=\"\s*\+\s*(-?\d+\.\d+)\s*\+\s*\",\"\s*\+\s*(-?\d+\.\d+);", response.text):
             item["lat"], item["lon"] = m.groups()
+        item["ref"] = response.meta["id"]
+        item["website"] = response.url
+        item.pop("facebook", None)
+        item.pop("twitter", None)
+        if "John Lewis" in item["name"]:
+            item["located_in"] = JohnLewisSpider.item_attributes["brand"]
+            item["located_in_wikidata"] = JohnLewisSpider.item_attributes["brand_wikidata"]
+        elif "Fenwick" in item["name"]:
+            item["located_in"] = "Fenwick"
+            item["located_in_wikidata"] = "Q5443673"
+        elif "Selfridges" in item["name"]:
+            item["located_in"] = "Selfridges"
+            item["located_in_wikidata"] = "Q1475656"
         yield item
