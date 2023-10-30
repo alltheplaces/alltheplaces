7eff291e4e91021971bfd569b5c09e79a905e158
Cj Malone <CjMalone@mail.com>
2023-10-30 17:05:13 +0000

    [JSWethersoon] Switch to crawl spider
    Sitemap is out of date

--- locations/spiders/j_d_wetherspoon.py
+++ locations/spiders/j_d_wetherspoon.py
@@ -1,12 +1,12 @@
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class JDWetherspoonSpider(SitemapSpider, StructuredDataSpider):
+class JDWetherspoonSpider(CrawlSpider, StructuredDataSpider):
     name = "j_d_wetherspoon"
-    item_attributes = {"brand": "J D Wetherspoon", "brand_wikidata": "Q6109362"}
+    item_attributes = {"brand": "Wetherspoon", "brand_wikidata": "Q6109362"}
     allowed_domains = ["www.jdwetherspoon.com"]
-    sitemap_urls = ["https://www.jdwetherspoon.com/sitemap.xml"]
-    sitemap_rules = [(r"https:\/\/www\.jdwetherspoon\.com\/pubs\/all-pubs(?:\/[\w\-]+){3}", "parse_sd")]
-    custom_settings = {"REDIRECT_ENABLED": False}  # Numerous location pages don't exist.
+    start_urls = ["https://www.jdwetherspoon.com/pubs/all-pubs"]
+    rules = [Rule(LinkExtractor(allow="/pubs/all-pubs/"), callback="parse_sd")]
