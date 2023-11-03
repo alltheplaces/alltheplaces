5ca2e3c7f5fb9894c8e48295192baad494184c3d
Cj Malone <CjMalone@mail.com>
2023-11-03 12:20:31 +0000

    Fix spider

--- locations/spiders/stonegate_gb.py
+++ locations/spiders/stonegate_gb.py
@@ -1,21 +1,40 @@
-import scrapy
+from urllib.parse import urlparse
 
-from locations.linked_data_parser import LinkedDataParser
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.categories import Categories, apply_category
+from locations.spiders.morrisons_gb import set_operator
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class StonegateGBSpider(scrapy.spiders.SitemapSpider):
+class StonegateGBSpider(CrawlSpider, StructuredDataSpider):
     name = "stonegate_gb"
-    item_attributes = {
-        "brand": "Stonegate",
-        "brand_wikidata": "Q7619176",
-    }
-    sitemap_urls = ["https://great-british-pubs.co.uk/sitemap_index.xml"]
-    sitemap_rules = [("/locations/", "parse")]
     download_delay = 0.2
+    STONEGATE = {"brand": "Stonegate", "brand_wikidata": "Q7619176"}
+    start_urls = ["https://www.stonegategroup.co.uk/find-your-local/page/1/"]
+    rules = [
+        Rule(LinkExtractor(allow=r"/page/\d+/$")),
+        Rule(LinkExtractor(restrict_xpaths='//a[contains(., "View Site")]'), callback="parse_sd"),
+    ]
 
-    def parse(self, response):
-        item = LinkedDataParser.parse(response, "BarOrPub")
-        if item:
-            item["ref"] = response.url
-            item["website"] = response.url
-            return item
+    brands = {
+        "www.feverbars.co.uk": {"brand": "Fever"},
+        "www.walkaboutbars.co.uk": {"brand": "Walkabout", "brand_wikidata": "Q7962149"},
+        "www.beatone.co.uk": {"brand": "Be At One", "brand_wikidata": "Q110016786"},
+        "www.popworldparty.co.uk": {"brand": "Popworld"},
+        "www.slugandlettuce.co.uk": {"brand": "Slug & Lettuce", "brand_wikidata": "Q7542224"},
+        "www.crafted-social.co.uk": {"brand": "Crafted Social"},
+        "www.greatukpubs.co.uk": {"brand": "Great UK Pubs"},
+        "www.craftunionpubs.com": {"brand": "Craft Union"},
+    }
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        set_operator(self.STONEGATE, item)
+
+        if brand := self.brands.get(urlparse(response.url).netloc):
+            item.update(brand)
+        else:
+            apply_category(Categories.PUB, item)
+
+        yield item
