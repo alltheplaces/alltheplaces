f08cb27265c6acb841173554f18198aac38cf09b
Cj Malone <CjMalone@mail.com>
2023-11-16 11:03:57 +0000

    Switch to point_locations

--- locations/spiders/first_cash.py
+++ locations/spiders/first_cash.py
@@ -1,26 +1,20 @@
 import scrapy
+from scrapy.http import JsonRequest
 
+from locations.geo import point_locations
 from locations.items import Feature
-from locations.searchable_points import open_searchable_points
 
 
 class FirstCashSpider(scrapy.Spider):
     name = "first_cash"
-    item_attributes = {
-        "brand": "First Cash",
-        "brand_wikidata": "Q5048636",
-    }
+    item_attributes = {"brand": "First Cash", "brand_wikidata": "Q5048636"}
     allowed_domains = ["find.cashamerica.us"]
 
     def start_requests(self):
         base_url = "http://find.cashamerica.us/api/stores?p=1&s=100&lat={lat}&lng={lng}&d=2019-10-14T17:43:05.914Z&key=D21BFED01A40402BADC9B931165432CD"
 
-        with open_searchable_points("us_centroids_100mile_radius.csv") as points:
-            next(points)
-            for point in points:
-                _, lat, lon = point.strip().split(",")
-                url = base_url.format(lat=lat, lng=lon)
-                yield scrapy.Request(url=url, callback=self.parse)
+        for lat, lon in point_locations("us_centroids_100mile_radius.csv"):
+            yield JsonRequest(url=base_url.format(lat=lat, lng=lon))
 
     def parse(self, response):
         data = response.json()
