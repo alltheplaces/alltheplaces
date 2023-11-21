19c94b481e57153f23c220fbb30b302259c77b57
Adam Zambrzycki <adam@zambrzycki.net>
2023-11-21 18:56:04 +0100

    Fix caffe nero

--- locations/spiders/caffe_nero.py
+++ locations/spiders/caffe_nero.py
@@ -1,39 +1,45 @@
-import chompjs
 from scrapy import Request, Spider
 
-from locations.categories import Categories, apply_category
+from locations.categories import apply_yes_no, Extras
 from locations.dict_parser import DictParser
+from locations.hours import OpeningHours, DAYS_EN
 
 
 class CaffeNeroSpider(Spider):
     name = "caffe_nero"
     item_attributes = {"brand": "Caffe Nero", "brand_wikidata": "Q675808"}
-    allowed_domains = ["caffenero.com", "greencaffenero.pl"]
     start_urls = ["https://caffenero.com/us/stores/"]
 
     def parse(self, response, **kwargs):
-        for region in response.xpath('//li[@role="menuitem"]/a[not(@class="global")]/@href').getall():
-            # GB stores use a different store locator
-            if ".com/uk" in region:
-                continue
-            region = region.replace("http://", "https://")
-            yield Request(url=f"{region}/stores/", callback=self.parse_country)
+        for region in response.xpath("//link[@rel='alternate'][starts-with(@hreflang, 'en-')]/@hreflang").getall():
+            country_code = region.split('-')[1].lower()
+            yield Request(url=f"https://caffenerowebsite.blob.core.windows.net/production/data/stores/stores-{country_code}.json", callback=self.parse_country)
 
-    def parse_country(self, response, **kwargs):
-        for location in chompjs.parse_js_object(response.xpath('//script[contains(., "storesData")]/text()').get())[
-            "stores"
-        ]:
-            item = DictParser.parse(location)
-            item["ref"] = location["store_id"]
-            item["website"] = f'https://caffenero.com{location["permalink"]}'
+    def parse_country(self, response):
+        geojson = response.json()
+        for location in geojson["features"]:
+            item = DictParser.parse(location["properties"])
 
-            if location["type"] == "EXP":
-                item["brand"] = "Nero Express"
-            elif location["type"] == "HOF":
-                item["located_in"] = "House of Fraser"
-                item["located_in_wikidata"] = "Q5928422"
-            # else normal store
+            lon, lat = location["geometry"]["coordinates"]
+            item["lat"] = lat
+            item["lon"] = lon
 
-            apply_category(Categories.COFFEE_SHOP, item)
+            amenities = location["properties"]["amenities"]
+            apply_yes_no(Extras.TOILETS, item, amenities["toilet"])
+            apply_yes_no(Extras.TOILETS_WHEELCHAIR, item, amenities["disabled_toilet"])
+            apply_yes_no(Extras.AIR_CONDITIONING, item, amenities["air_conditioned"])
+            apply_yes_no(Extras.BABY_CHANGING_TABLE, item, amenities["baby_change"])
+            apply_yes_no(Extras.OUTDOOR_SEATING, item, amenities["outside_seating"])
+            apply_yes_no(Extras.SMOKING_AREA, item, amenities["smoking_area"])
+            apply_yes_no(Extras.WHEELCHAIR, item, amenities["disabled_access"])
+            apply_yes_no(Extras.WIFI, item, amenities["wifi"])
+
+            opening_days = location["properties"]["hoursRegular"]
+            item["opening_hours"] = OpeningHours()
+            for day, day_details in opening_days.items():
+                day = day.capitalize()
+                if day not in DAYS_EN:
+                    continue
+                item["opening_hours"].add_range(DAYS_EN.get(day), day_details["open"], day_details["close"])
 
             yield item
