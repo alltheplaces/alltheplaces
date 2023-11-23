948611c99164a722159550ac3df58fa44000fcd4
Cj Malone <CjMalone@mail.com>
2023-11-23 16:00:19 +0000

    [Lifetime] Switch to StructuredDataSpider

--- locations/spiders/lifetime.py
+++ locations/spiders/lifetime.py
@@ -1,42 +1,11 @@
-import json
+from scrapy.spiders import SitemapSpider
 
-import scrapy
-
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class LifetimeSpider(scrapy.Spider):
+class LifetimeSpider(SitemapSpider, StructuredDataSpider):
     name = "lifetimefitness"
     item_attributes = {"brand": "Lifetime", "brand_wikidata": "Q6545004"}
-    allowed_domains = ["lifetime.life"]
-    start_urls = ("https://www.lifetime.life/view-all-locations.html",)
-
-    def parse(self, response):
-        response.selector.remove_namespaces()
-        city_urls = response.xpath(
-            '//a[@class="btn btn-link btn-xs m-b-sm p-x-0 b-x-xs-0 pull-xs-right"]/@href'
-        ).extract()
-        for path in city_urls:
-            yield scrapy.Request(
-                "https://www.lifetime.life" + path.strip(),
-                callback=self.parse_store,
-            )
-
-    def parse_store(self, response):
-        json_data = response.xpath('//script[@type="application/ld+json"]/text()').extract_first()
-        data = json.loads(json_data)
-
-        properties = {
-            "name": data["name"],
-            "ref": data["name"],
-            "street_address": data["address"]["streetAddress"],
-            "city": data["address"]["addressLocality"],
-            "state": data["address"]["addressRegion"],
-            "postcode": data["address"]["postalCode"],
-            "phone": data["telephone"],
-            "website": data["url"],
-            "lat": data["geo"]["latitude"],
-            "lon": data["geo"]["longitude"],
-        }
-
-        yield Feature(**properties)
+    sitemap_urls = ["https://www.lifetime.life/robots.txt"]
+    sitemap_rules = [(r"/life-time-locations/[-\w]+\.html$", "parse_sd")]
+    wanted_types = ["ExerciseGym"]
