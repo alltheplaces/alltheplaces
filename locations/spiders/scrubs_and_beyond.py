639484006fd935565afc0940692e407606eb507d
Karen Kerr <karenkerrgithub@gmail.com>
2023-11-23 10:52:33 +0000

    [ScrubsandBeyond] Add category

--- locations/spiders/scrubs_and_beyond.py
+++ locations/spiders/scrubs_and_beyond.py
@@ -1,11 +1,16 @@
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class ScrubsAndBeyondSpider(SitemapSpider, StructuredDataSpider):
     name = "scrubs_and_beyond"
-    item_attributes = {"brand": "Scrubs and Beyond"}
+    item_attributes = {
+        "brand": "Scrubs and Beyond",
+        "brand_wikidata": "Q119972011",
+        "extras": Categories.SHOP_CLOTHES.value,
+    }
     allowed_domains = ["locations.scrubsandbeyond.com"]
     sitemap_urls = ("https://locations.scrubsandbeyond.com/robots.txt",)
     sitemap_rules = [
