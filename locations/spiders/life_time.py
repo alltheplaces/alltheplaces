ac405edfdbe7ff8d7d21e785d1a0d2c9bf8c2f47
Cj Malone <CjMalone@mail.com>
2023-11-23 16:36:59 +0000

    Rename again
    They have 3 locations in CA

--- locations/spiders/life_time.py
+++ locations/spiders/life_time.py
@@ -3,8 +3,8 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class LifeTimeUSSpider(SitemapSpider, StructuredDataSpider):
-    name = "life_time_us"
+class LifeTimeSpider(SitemapSpider, StructuredDataSpider):
+    name = "life_time"
     item_attributes = {"brand": "Life Time", "brand_wikidata": "Q6545004"}
     sitemap_urls = ["https://www.lifetime.life/robots.txt"]
     sitemap_rules = [(r"/life-time-locations/[-\w]+\.html$", "parse_sd")]
