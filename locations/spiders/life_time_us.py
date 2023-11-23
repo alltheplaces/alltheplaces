761939163bad29bec8e93cc00e2c1595e4a4222a
Cj Malone <CjMalone@mail.com>
2023-11-23 16:04:49 +0000

    Rename spider

--- locations/spiders/life_time_us.py
+++ locations/spiders/life_time_us.py
@@ -3,9 +3,9 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class LifetimeSpider(SitemapSpider, StructuredDataSpider):
-    name = "lifetimefitness"
-    item_attributes = {"brand": "Lifetime", "brand_wikidata": "Q6545004"}
+class LifeTimeUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "life_time_us"
+    item_attributes = {"brand": "Life Time", "brand_wikidata": "Q6545004"}
     sitemap_urls = ["https://www.lifetime.life/robots.txt"]
     sitemap_rules = [(r"/life-time-locations/[-\w]+\.html$", "parse_sd")]
     wanted_types = ["ExerciseGym"]
