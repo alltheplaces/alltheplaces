522459f5ec914b7e754b4453ecb34f816ced3b42
panasonic-mafia <kolosov.yv@gmail.com>
2023-11-24 13:37:51 +0100

    correct name of the spider

--- locations/spiders/burger_king_dk.py
+++ locations/spiders/burger_king_dk.py
@@ -1,7 +1,7 @@
 from locations.spiders.burger_king_se import BurgerKingSESpider
 
 
-class BurgerKingDkSpider(BurgerKingSESpider):
+class BurgerKingDKSpider(BurgerKingSESpider):
     name = "burger_king_dk"
     start_urls = [
         "https://bk-dk-ordering-api.azurewebsites.net/api/v2/restaurants?latitude=59.330311012767446&longitude=18.068330468145753&radius=99900000&top=100000"
