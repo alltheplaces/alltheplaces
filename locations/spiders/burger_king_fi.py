4daa22524d021650bbff142620a5fd7738846370
panasonic-mafia <kolosov.yv@gmail.com>
2023-11-24 14:17:36 +0100

    fix

--- locations/spiders/burger_king_fi.py
+++ locations/spiders/burger_king_fi.py
@@ -1,16 +1,17 @@
 import scrapy
-from locations.categories import Extras, apply_yes_no
 
+from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
 from locations.spiders.burger_king import BurgerKingSpider
 
 FEATURES_MAPPING = {
-    'bk_delivery': Extras.DELIVERY,
-    'drive_through': Extras.DRIVE_THROUGH,
-    'take_away': Extras.TAKEAWAY,
+    "bk_delivery": Extras.DELIVERY,
+    "drive_through": Extras.DRIVE_THROUGH,
+    "take_away": Extras.TAKEAWAY,
 }
 
-class BurgerKingFiSpider(scrapy.Spider):
+
+class BurgerKingFISpider(scrapy.Spider):
     name = "burger_king_fi"
     allowed_domains = ["burgerking.fi"]
     item_attributes = BurgerKingSpider.item_attributes
@@ -18,14 +19,13 @@ class BurgerKingFiSpider(scrapy.Spider):
 
     def parse(self, response):
         for poi in response.json():
-            poi['street_address'] = poi.pop('address')
+            poi["street_address"] = poi.pop("address")
             item = DictParser.parse(poi)
-            item['ref'] = item['name']
+            item["ref"] = item["name"]
             # 'features' might be an empty list or filled dict
-            if features := poi.get('features'):
+            if features := poi.get("features"):
                 for feature, value in features.items():
                     if tags := FEATURES_MAPPING.get(feature):
                         apply_yes_no(tags, item, value, True)
             # TODO: parse opening hours
             yield item
-            
