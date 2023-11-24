9b0097c0900a99a68d052352387ade867f9fda7f
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-11-24 15:41:09 +0000

    [AutoZoneUS] Fix spider (#6414)

--- locations/spiders/auto_zone_us.py
+++ locations/spiders/auto_zone_us.py
@@ -6,22 +6,14 @@ from locations.structured_data_spider import StructuredDataSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class AutoZoneSpider(SitemapSpider, StructuredDataSpider):
-    name = "auto_zone"
+class AutoZoneUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "auto_zone_us"
     item_attributes = {"brand": "AutoZone", "brand_wikidata": "Q4826087"}
     allowed_domains = ["www.autozone.com"]
     sitemap_urls = ["https://www.autozone.com/locations/sitemap.xml"]
     sitemap_rules = [(r"https://www.autozone.com/locations/([-\w]+)\/([-\w]+)\/([-\w]+).html$", "parse")]
     wanted_types = ["AutoPartsStore"]
-    custom_settings = {
-        "DEFAULT_REQUEST_HEADERS": {
-            "Host": "www.autozone.com",
-            "Accept": "*/*",
-            "User-Agent": BROWSER_DEFAULT,
-            "Connection": "keep-alive",
-            "Accept-Encoding": "gzip, deflate, br",
-        }
-    }
+    user_agent = BROWSER_DEFAULT
 
     def inspect_item(self, item, response):
         name = response.xpath('//h1[@id="location-name"]/span[2]/text()').get()
