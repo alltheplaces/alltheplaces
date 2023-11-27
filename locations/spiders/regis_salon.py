3bcdc142d24b9a13a3648465f7526d7f8531e05d
Cj Malone <Cj-Malone@users.noreply.github.com>
2023-11-27 15:52:17 +0000

    Sync brand strings with NSI (#6421)

--- locations/spiders/regis_salon.py
+++ locations/spiders/regis_salon.py
@@ -8,7 +8,7 @@ from locations.items import Feature
 
 class RegisSalonSpider(scrapy.Spider):
     name = "regis"
-    item_attributes = {"brand": "Regis Salon", "brand_wikidata": "Q7309325"}
+    item_attributes = {"brand": "Regis", "brand_wikidata": "Q7309325"}
     download_delay = 0.1
     allowed_domains = ["www.regissalons.com"]
     start_urls = ["https://www.regissalons.com/salon-locator.html"]
