e08afb3005dea8c73c92d749e0a09d3657af66e2
David Hicks <david@hicks.id.au>
2023-11-28 02:45:47 +1100

    AutomaticSpiderGenerator: enable for Closeby

--- locations/storefinders/closeby.py
+++ locations/storefinders/closeby.py
@@ -1,10 +1,13 @@
-from scrapy import Spider
-from scrapy.http import JsonRequest
+import re
 
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
+
+from locations.automatic_spider_generator import AutomaticSpiderGenerator
 from locations.dict_parser import DictParser
 
 
-class ClosebySpider(Spider):
+class ClosebySpider(Spider, AutomaticSpiderGenerator):
     dataset_attributes = {"source": "api", "api": "closeby.co"}
     api_key = ""
 
@@ -16,3 +19,26 @@ class ClosebySpider(Spider):
             item = DictParser.parse(location)
             item["addr_full"] = location.get("address_full")
             yield item
+
+    @staticmethod
+    def storefinder_exists(response: Response) -> bool:
+        if len(response.xpath('//iframe[contains(@src, "https://www.closeby.co/embed/")]')) > 0:
+            return True
+        elif len(response.xpath('//script[contains(@src, "https://embed.closeby.co/v1.js")]')) > 0:
+            return True
+        return False
+
+    @staticmethod
+    def extract_spider_attributes(response: Response) -> dict:
+        api_key = response.xpath('//iframe[contains(@src, "https://www.closeby.co/embed/")]/@src').get()
+        if api_key and "/" in api_key:
+            api_key = api_key.split("/")[-1]
+        if not api_key:
+            if api_key_match := re.search(r"closeby\.mapKey=([0-9a-f]{32})(?![0-9a-f])", response.xpath('//script[contains(@src, "https://embed.closeby.co/v1.js")]').get()):
+                api_key = api_key_match.group(1)
+        if api_key:
+            return {
+                "api_key": api_key,
+            }
+        else:
+            return {}
