1b66def92c861bdf1cbd925cff04206ddbfd95ed
Colton Piper <pipercolton@gmail.com>
2023-12-11 09:11:08 -0500

    Changed to one file.

--- locations/spiders/indigo.py
+++ locations/spiders/indigo.py
@@ -6,20 +6,14 @@ import scrapy
 from locations.dict_parser import DictParser
 
 
-class IndigoESSpider(scrapy.Spider):
-    name = "indigo_es"
+class IndigoCASpider(scrapy.Spider):
+    name = "indigo"
     item_attributes = {"brand": "Indigo", "brand_wikidata": "Q3559970"}
     url = "https://salesforce.parkindigo.com/locations?"
-    bbox = (-9.39288367353, 35.946850084, 3.03948408368, 43.7483377142)
 
     def start_requests(self):
         params = {
             "location.language": "en",
-            "location.address.countries": self.name[-2:].upper(),
-            "box.first.x": self.bbox[3],
-            "box.first.y": self.bbox[0],
-            "box.second.x": self.bbox[1],
-            "box.second.y": self.bbox[2],
             "page": 0,
         }
         yield scrapy.Request(self.url + urlencode(params))
@@ -37,11 +31,6 @@ class IndigoESSpider(scrapy.Spider):
         if not data.get("last"):
             params = {
                 "location.language": "en",
-                "location.address.countries": self.name[-2:].upper(),
-                "box.first.x": self.bbox[3],
-                "box.first.y": self.bbox[0],
-                "box.second.x": self.bbox[1],
-                "box.second.y": self.bbox[2],
                 "page": int(data.get("number")) + 1,
             }
             yield scrapy.Request(self.url + urlencode(params), callback=self.parse)
