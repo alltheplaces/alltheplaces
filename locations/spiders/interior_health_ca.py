840dd2cc2ffe4c8d0476f6a2bd910b052d36a12e
DivyaB1 <dbandapalle10@gmail.com>
2023-12-11 14:23:48 -0500

    adding category to interior_health_ca

--- locations/spiders/interior_health_ca.py
+++ locations/spiders/interior_health_ca.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.items import Feature
+from locations.categories import Categories, apply_category
 
 
 class InteriorHealthCASpider(SitemapSpider):
@@ -27,4 +28,6 @@ class InteriorHealthCASpider(SitemapSpider):
             properties["phone"] = properties["phone"].replace("tel:", "").strip()
         else:
             properties.pop("phone", None)
+            
+        apply_category(Categories.HOSPITAL, properties)
         yield Feature(**properties)
