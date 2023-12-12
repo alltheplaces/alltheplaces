dbb88ee6437f02ec84dbd9d70ca5c8da3d24459b
DivyaB1 <dbandapalle10@gmail.com>
2023-12-12 11:10:56 -0500

    [betts_au] add category

--- locations/spiders/betts_au.py
+++ locations/spiders/betts_au.py
@@ -1,8 +1,10 @@
 from locations.storefinders.metalocator import MetaLocatorSpider
-
+from locations.categories import Categories, apply_category
 
 class BettsAU(MetaLocatorSpider):
     name = "betts_au"
     item_attributes = {"brand": "Betts", "brand_wikidata": "Q118555401"}
     brand_id = "6176"
     country_list = ["Australia"]
+
+    apply_category(Categories.SHOP_SHOES, item_attributes)
