d6d0686d0652dc4bce65929906f8ad450be1e10f
DivyaB1 <dbandapalle10@gmail.com>
2023-12-12 12:34:36 -0500

    [goodlife_pharmacy_ke] add category

--- locations/spiders/goodlife_pharmacy_ke.py
+++ locations/spiders/goodlife_pharmacy_ke.py
@@ -1,7 +1,9 @@
 from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
-
+from locations.categories import Categories, apply_category
 
 class GoodlifePharmacyKESpider(AgileStoreLocatorSpider):
     name = "goodlife_pharmacy_ke"
     item_attributes = {"brand": "Goodlife Pharmacy", "brand_wikidata": "Q120783615"}
     allowed_domains = ["www.goodlife.co.ke"]
+
+    apply_category(Categories.PHARMACY, item_attributes)
