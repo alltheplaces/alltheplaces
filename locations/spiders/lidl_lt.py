11068a117e7881a5e2496c896aa9b6250eb71a24
Colton Piper <pipercolton@gmail.com>
2023-12-15 15:58:13 -0500

    As many are similar to one another, I used one of the spiders as a parent spider so the others are cleaner. (#6518)

--- locations/spiders/lidl_lt.py
+++ locations/spiders/lidl_lt.py
@@ -1,27 +1,11 @@
-import re
-
-from locations.hours import DAYS_ES, OpeningHours, sanitise_day
-from locations.spiders.lidl_gb import LidlGBSpider
-from locations.storefinders.virtualearth import VirtualEarthSpider
+from locations.hours import DAYS_ES
+from locations.spiders.lidl_at import LidlATSpider
 
 
-class LidlLTSpider(VirtualEarthSpider):
+class LidlLTSpider(LidlATSpider):
     name = "lidl_lt"
-    item_attributes = LidlGBSpider.item_attributes
 
     dataset_id = "8a2167d4bd8a47d9930fc73f5837f0bf"
     dataset_name = "Filialdaten-LT/Filialdaten-LT"
     key = "AkEdcFBe-gxNmSmCIpOKE_KuLBZv-NHRKY_ndbJKiVvc0ramz4hZKta-rZRpuNZS"
-
-    def parse_item(self, item, feature, **kwargs):
-        item["name"] = feature["ShownStoreName"]
-
-        item["opening_hours"] = OpeningHours()
-        for day, start_time, end_time in re.findall(
-            r"(\w+) (\d{2}:\d{2})-(\d{2}:\d{2})",
-            feature["OpeningTimes"],
-        ):
-            if day := sanitise_day(day, DAYS_ES):
-                item["opening_hours"].add_range(day, start_time, end_time)
-
-        yield item
+    days = DAYS_ES
--- locations/spiders/lidl_se.py
+++ locations/spiders/lidl_se.py
@@ -1,41 +1,11 @@
-import re
-
-from locations.hours import DAYS_FR, OpeningHours, day_range, sanitise_day
-from locations.spiders.lidl_gb import LidlGBSpider
-from locations.storefinders.virtualearth import VirtualEarthSpider
+from locations.hours import DAYS_FR
+from locations.spiders.lidl_be import LidlBESpider
 
 
-class LidlSESpider(VirtualEarthSpider):
+class LidlSESpider(LidlBESpider):
     name = "lidl_se"
-    item_attributes = LidlGBSpider.item_attributes
 
     dataset_id = "b340d487953044ba8e2b20406ce3fcc6"
     dataset_name = "Filialdaten-SE/Filialdaten-SE"
     key = "AiHIKQCACRaaOyOJQjGEGl5uxp7KOTXwae435wJqW3jBo_HLpRWmOVrhOI-eI-Rj"
-
-    def parse_item(self, item, feature, **kwargs):
-        item["name"] = feature["ShownStoreName"]
-
-        oh = OpeningHours()
-        for day, start_time, end_time in re.findall(
-            r"(\w+ - \w+|\w+) (\d{2}:\d{2})-(\d{2}:\d{2})",
-            feature["OpeningTimes"],
-        ):
-            if "-" in day:
-                start_day, end_day = day.split("-")
-
-                start_day = sanitise_day(start_day, DAYS_FR)
-                end_day = sanitise_day(end_day, DAYS_FR)
-            else:
-                start_day = sanitise_day(day, DAYS_FR)
-                end_day = None
-
-            if start_day and end_day:
-                for d in day_range(start_day, end_day):
-                    oh.add_range(d, start_time, end_time)
-            elif start_day:
-                oh.add_range(start_day, start_time, end_time)
-
-        item["opening_hours"] = oh.as_opening_hours()
-
-        yield item
+    days = DAYS_FR
