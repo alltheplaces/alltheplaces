alltheplaces/alltheplaces

commit fd3a6c24df32d7f9a2b06f8d5fb68bfe5b87f753

Implement top-level operator and operator_wikidata fields (#6449)
* Implement top-level operator and operator_wikidata fields

Many locations of interest to AllThePlaces are known primarily by their
operator. Examples include fire stations, police stations and hospitals
which may be operated by the same public or private organisation on
behalf of a government agency. Many other locations are known primarily
by their brand, but due to their franchise arrangements, are also known
by their franchise owner (operator). For numerous franchises, hundreds
of locations may be operated by the same franchise owner.

Treating `operator` and `operator_wikidata` as top-level fields of the
`Feature` class makes sense because these fields often carry equal
weight ot he existing `brand` and `brand_wikidata` top-level fields.
Benefits include better statistics collection on operators, better
name-suggestion-index interoperability (where operators and brands are
equally weighted) and simplifed spider code.

* winterthur_ch: fix incorrect merge (should not overwrite new changes)

* Add little test

* Add information about operator to the data format doc.

---------

Co-authored-by: Cj Malone <CjMalone@mail.com>
Co-authored-by: Ian Dees <ian.dees@gmail.com>

locations/spiders/fastned.py
@@ -14,7 +14,8 @@ class FastnedSpider(Spider):
             item = DictParser.parse(location)
 
             apply_category(Categories.CHARGING_STATION, item)
-            item["extras"]["operator"] = "Fastned"
+            item["operator"] = self.item_attributes["brand"]
+            item["operator:wikidata"] = self.item_attributes["brand_wikidata"]
 
             # TODO: connector data available in location["connectors"]
             yield item

locations/spiders/fire_and_rescue_nsw_au.py
@@ -7,6 +7,7 @@ from locations.items import Feature
 
 class FireAndRescueNSWAUSpider(SitemapSpider):
     name = "fire_and_rescue_nsw_au"
+    item_attributes = {"operator": "Fire and Rescue New South Wales", "operator:wikidata": "Q5451532"}
     allowed_domains = ["www.fire.nsw.gov.au"]
     sitemap_urls = ["https://www.fire.nsw.gov.au/feeds/sitemap.xml"]
     sitemap_rules = [(r"^https:\/\/www\.fire\.nsw\.gov\.au\/page\.php\?id=9210&station=\d+", "parse")]
@@ -34,6 +35,4 @@ class FireAndRescueNSWAUSpider(SitemapSpider):
         }
         extract_google_position(properties, response)
         apply_category({"amenity": "fire_station"}, properties)
-        properties["extras"]["operator"] = "Fire and Rescue New South Wales"
-        properties["extras"]["operator:wikidata"] = "Q5451532"
         yield Feature(**properties)

locations/spiders/great_western_railway_gb.py
@@ -7,7 +7,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class GreatWesternRailwayGBSpider(StructuredDataSpider):
     name = "great_western_railway_gb"
-    item_attributes = {"extras": {"operator": "Great Western Railway", "operator:wikidata": "Q1419438"}}
+    item_attributes = {"operator": "Great Western Railway", "operator:wikidata": "Q1419438"}
     start_urls = ["https://www.gwr.com/api/stations"]
     wanted_types = ["TrainStation"]
     search_for_twitter = False

locations/spiders/marks_and_spencer.py
@@ -41,8 +41,8 @@ class MarksAndSpencerSpider(scrapy.Spider):
             }
 
             if store["storeType"] == "mands":
-                properties["extras"]["operator"] = "Marks and Spencer"
-                properties["extras"]["operator:wikidata"] = "Q714491"
+                properties["operator"] = "Marks and Spencer"
+                properties["operator:wikidata"] = "Q714491"
 
             name = store["name"].lower()
             if name.endswith("foodhall"):

locations/spiders/midcounties_cooperative_gb.py
@@ -9,7 +9,8 @@ class MidcountiesCooperativeGBSpider(Spider):
     name = "midcounties_cooperative_gb"
     item_attributes = {
         "brand": "Your Co-op",
-        "extras": {"operator": "Midcounties Co-operative", "operator:wikidata": "Q6841138"},
+        "operator": "Midcounties Co-operative",
+        "operator:wikidata": "Q6841138",
     }
     start_urls = ["https://www.midcounties.coop/static/js/stores.json"]
     no_refs = True

locations/spiders/national_rail_gb.py
@@ -59,7 +59,7 @@ class NationalRailGBSpider(Spider):
             item["lon"] = location["longitude"]
             item["postcode"] = location["postcode"]
             item["website"] = "https://www.nationalrail.co.uk/stations/{}/".format(location["crsCode"].lower())
-            item["extras"]["operator"], item["extras"]["operator:wikidata"] = self.operators.get(
+            item["operator"], item["operator:wikidata"] = self.operators.get(
                 location["operator"], (location["operator"], None)
             )
 

locations/spiders/northern_railway_gb.py
@@ -8,7 +8,7 @@ from locations.items import Feature
 
 class NorthernRailwayGBSpider(Spider):
     name = "northern_railway_gb"
-    item_attributes = {"extras": {"operator": "Northern", "operator:wikidata": "Q85789775"}}
+    item_attributes = {"operator": "Northern", "operator:wikidata": "Q85789775"}
     start_urls = ["https://www.northernrailway.co.uk/api/northern_station_list_auto_complete"]
 
     def parse(self, response, **kwargs):

locations/spiders/nsw_ambulance_au.py
@@ -7,6 +7,7 @@ from locations.items import Feature
 
 class NSWAmbulanceAUSpider(Spider):
     name = "nsw_ambulance_au"
+    item_attributes = {"operator": "New South Wales Ambulance", "operator_wikidata": "Q4741948"}
     allowed_domains = ["portal.spatial.nsw.gov.au"]
     start_urls = [
         "https://portal.spatial.nsw.gov.au/server/rest/services/NSW_FOI_Health_Facilities/FeatureServer/0/query?f=geojson"
@@ -48,9 +49,6 @@ class NSWAmbulanceAUSpider(Spider):
                 "SOUTHCARE HELO AMBULANCE STATION",
             ]:
                 properties["state"] = "ACT"
-                properties["extras"]["operator"] = "Australian Capital Territory Ambulance Service"
-                properties["extras"]["operator:wikidata"] = "Q4823922"
-            else:
-                properties["extras"]["operator"] = "New South Wales Ambulance"
-                properties["extras"]["operator:wikidata"] = "Q4741948"
+                properties["operator"] = "Australian Capital Territory Ambulance Service"
+                properties["operator:wikidata"] = "Q4823922"
             yield Feature(**properties)

commit 00da9c5a83fab02ab758375753b1dcd1c924d7e0

Fix incorrect operator:wikidata field typos
Commit fd3a6c24df32d7f9a2b06f8d5fb68bfe5b87f753 accidentally used
"operator:wikidata" as a field of locations.items.Feature, instead of
"operator_wikidata" (correct field name).

locations/spiders/fastned.py
@@ -15,7 +15,7 @@ class FastnedSpider(Spider):
 
             apply_category(Categories.CHARGING_STATION, item)
             item["operator"] = self.item_attributes["brand"]
-            item["operator:wikidata"] = self.item_attributes["brand_wikidata"]
+            item["operator_wikidata"] = self.item_attributes["brand_wikidata"]
 
             # TODO: connector data available in location["connectors"]
             yield item

locations/spiders/fire_and_rescue_nsw_au.py
@@ -7,7 +7,7 @@ from locations.items import Feature
 
 class FireAndRescueNSWAUSpider(SitemapSpider):
     name = "fire_and_rescue_nsw_au"
-    item_attributes = {"operator": "Fire and Rescue New South Wales", "operator:wikidata": "Q5451532"}
+    item_attributes = {"operator": "Fire and Rescue New South Wales", "operator_wikidata": "Q5451532"}
     allowed_domains = ["www.fire.nsw.gov.au"]
     sitemap_urls = ["https://www.fire.nsw.gov.au/feeds/sitemap.xml"]
     sitemap_rules = [(r"^https:\/\/www\.fire\.nsw\.gov\.au\/page\.php\?id=9210&station=\d+", "parse")]

locations/spiders/great_western_railway_gb.py
@@ -7,7 +7,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class GreatWesternRailwayGBSpider(StructuredDataSpider):
     name = "great_western_railway_gb"
-    item_attributes = {"operator": "Great Western Railway", "operator:wikidata": "Q1419438"}
+    item_attributes = {"operator": "Great Western Railway", "operator_wikidata": "Q1419438"}
     start_urls = ["https://www.gwr.com/api/stations"]
     wanted_types = ["TrainStation"]
     search_for_twitter = False

locations/spiders/marks_and_spencer.py
@@ -42,7 +42,7 @@ class MarksAndSpencerSpider(scrapy.Spider):
 
             if store["storeType"] == "mands":
                 properties["operator"] = "Marks and Spencer"
-                properties["operator:wikidata"] = "Q714491"
+                properties["operator_wikidata"] = "Q714491"
 
             name = store["name"].lower()
             if name.endswith("foodhall"):

locations/spiders/midcounties_cooperative_gb.py
@@ -10,7 +10,7 @@ class MidcountiesCooperativeGBSpider(Spider):
     item_attributes = {
         "brand": "Your Co-op",
         "operator": "Midcounties Co-operative",
-        "operator:wikidata": "Q6841138",
+        "operator_wikidata": "Q6841138",
     }
     start_urls = ["https://www.midcounties.coop/static/js/stores.json"]
     no_refs = True

locations/spiders/national_rail_gb.py
@@ -59,7 +59,7 @@ class NationalRailGBSpider(Spider):
             item["lon"] = location["longitude"]
             item["postcode"] = location["postcode"]
             item["website"] = "https://www.nationalrail.co.uk/stations/{}/".format(location["crsCode"].lower())
-            item["operator"], item["operator:wikidata"] = self.operators.get(
+            item["operator"], item["operator_wikidata"] = self.operators.get(
                 location["operator"], (location["operator"], None)
             )
 

locations/spiders/northern_railway_gb.py
@@ -8,7 +8,7 @@ from locations.items import Feature
 
 class NorthernRailwayGBSpider(Spider):
     name = "northern_railway_gb"
-    item_attributes = {"operator": "Northern", "operator:wikidata": "Q85789775"}
+    item_attributes = {"operator": "Northern", "operator_wikidata": "Q85789775"}
     start_urls = ["https://www.northernrailway.co.uk/api/northern_station_list_auto_complete"]
 
     def parse(self, response, **kwargs):

locations/spiders/nsw_ambulance_au.py
@@ -50,5 +50,5 @@ class NSWAmbulanceAUSpider(Spider):
             ]:
                 properties["state"] = "ACT"
                 properties["operator"] = "Australian Capital Territory Ambulance Service"
-                properties["operator:wikidata"] = "Q4823922"
+                properties["operator_wikidata"] = "Q4823922"
             yield Feature(**properties)

locations/spiders/scotrail_gb.py
@@ -9,7 +9,7 @@ from locations.items import Feature
 
 class ScotrailGBSpider(CrawlSpider):
     name = "scotrail_gb"
-    item_attributes = {"operator": "ScotRail", "operator:wikidata": "Q18356161"}
+    item_attributes = {"operator": "ScotRail", "operator_wikidata": "Q18356161"}
     start_urls = ["https://www.scotrail.co.uk/plan-your-journey/stations-and-facilities"]
     rules = [Rule(LinkExtractor(allow=r"/plan-your-journey/stations-and-facilities/\w{3}$"), callback="parse")]
     requires_proxy = True

locations/spiders/southeastern_railway_gb.py
@@ -9,7 +9,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class SoutheasternRailwayGBSpider(SitemapSpider, StructuredDataSpider):
     name = "southeastern_railway_gb"
-    item_attributes = {"operator": "Southeastern", "operator:wikidata": "Q1696490"}
+    item_attributes = {"operator": "Southeastern", "operator_wikidata": "Q1696490"}
     sitemap_urls = ["https://www.southeasternrailway.co.uk/robots.txt"]
     sitemap_rules = [("/station-information/stations/", "parse")]
     search_for_facebook = False

locations/spiders/transport_for_wales_gb.py
@@ -10,7 +10,7 @@ from locations.items import Feature
 
 class TransportForWalesGB(Spider):
     name = "transport_for_wales_gb"
-    item_attributes = {"operator": "Transport for Wales", "operator:wikidata": "Q104878180"}
+    item_attributes = {"operator": "Transport for Wales", "operator_wikidata": "Q104878180"}
     start_urls = ["https://tfw.wales/api/stations/links/{}".format(letter) for letter in string.ascii_uppercase]
 
     def parse(self, response, **kwargs):

locations/spiders/winterthur_ch.py
@@ -56,7 +56,7 @@ class WinterthurCHSpider(scrapy.Spider):
                 "city": "Winterthur",
                 "country": "CH",
                 "operator": "Stadtgrün Winterthur",
-                "operator:wikidata": "Q56825906",
+                "operator_wikidata": "Q56825906",
             }
             apply_category(Categories.LEISURE_PLAYGROUND, item)
             if name_words := props.get("name", "").split():
@@ -80,7 +80,7 @@ class WinterthurCHSpider(scrapy.Spider):
                     "city": "Winterthur",
                     "country": "CH",
                     "operator": "Stadtgrün Winterthur",
-                    "operator:wikidata": "Q56825906",
+                    "operator_wikidata": "Q56825906",
                     "extras": {
                         "backrest": self.parse_bench_backrest(btype),
                         "colour": self.parse_bench_colour(btype),