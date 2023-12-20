ef323b3e8f550cbb7037040acfa41acf44d5a94a
Cj Malone <CjMalone@mail.com>
2023-12-20 15:04:48 +0000

    Clean up strings

--- locations/spiders/taco_bell_in.py
+++ locations/spiders/taco_bell_in.py
@@ -5,6 +5,7 @@ from scrapy.http import Response
 
 from locations.google_url import extract_google_position
 from locations.items import Feature
+from locations.spiders.vapestore_gb import clean_address
 
 
 class TacoBellINSpider(Spider):
@@ -16,8 +17,8 @@ class TacoBellINSpider(Spider):
         for location in response.xpath('//div[@class="find-Us-Branches"]'):
             item = Feature()
             item["image"] = location.xpath('.//img[@class="find-Us-Branch-Img"]/@src').get()
-            item["branch"] = location.xpath('.//div[@class="findus-heading"]/text()').get()
-            item["addr_full"] = location.xpath('.//p[@class="findus_addr"]/text()').get()
+            item["branch"] = location.xpath('normalize-space(.//div[@class="findus-heading"]/text())').get()
+            item["addr_full"] = clean_address(location.xpath('.//p[@class="findus_addr"]/text()').getall())
             item["phone"] = location.xpath('.//a[@class="mobile-no"]/@rel').get()
             extract_google_position(item, location)
             item["ref"] = item["website"] = location.xpath('.//a[@class="restro-detail"]/@href').get()
