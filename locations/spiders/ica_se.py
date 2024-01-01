fba854d124b09595be814562207567d471957b3a
Matthias Feist <matze@matf.de>
2024-01-01 10:05:00 +0100

    added spider for ICA supermarkets in Sweden

---
+++ locations/spiders/ica_se.py
@@ -0,0 +1,12 @@
+from locations.structured_data_spider import StructuredDataSpider
+from scrapy.spiders import SitemapSpider
+from locations.categories import Categories, apply_category
+
+
+class IcaSESpider(SitemapSpider, StructuredDataSpider):
+    name = "ica_se"
+    item_attributes = {"brand": "ICA", "brand_wikidata": "Q1663776", "extras": Categories.SHOP_SUPERMARKET.value}
+    sitemap_urls = ["https://www.ica.se/butiker/sitemaps/1/", "https://www.ica.se/butiker/sitemap.xml"]
+    sitemap_rules = [
+        (r"/butiker/.+\d+/$", "parse_sd"),
+    ]
