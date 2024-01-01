5a27a6412e8d2943ae48cab52d15faf8ef505576
Matthias Feist <github.com@matf.de>
2024-01-01 11:26:37 +0100

    Update locations/spiders/ica_se.py
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>

--- locations/spiders/ica_se.py
+++ locations/spiders/ica_se.py
@@ -7,7 +7,7 @@ from locations.structured_data_spider import StructuredDataSpider
 class IcaSESpider(SitemapSpider, StructuredDataSpider):
     name = "ica_se"
     item_attributes = {"brand": "ICA", "brand_wikidata": "Q1663776", "extras": Categories.SHOP_SUPERMARKET.value}
-    sitemap_urls = ["https://www.ica.se/butiker/sitemaps/1/", "https://www.ica.se/butiker/sitemap.xml"]
+    sitemap_urls = ["https://www.ica.se/butiker/sitemaps/", "https://www.ica.se/butiker/sitemap.xml"]
     sitemap_rules = [
         (r"/butiker/.+\d+/$", "parse_sd"),
     ]
