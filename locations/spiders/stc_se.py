7c6e4bff6c0cce885c478673f060921b2e2957c6
Matthias Feist <matze@matf.de>
2024-01-02 18:14:55 +0100

    removed comment and redundant command

--- locations/spiders/stc_se.py
+++ locations/spiders/stc_se.py
@@ -3,9 +3,6 @@ import scrapy
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
-
-# Sats (Gym chain in the nordics) is active in SE, NO, DK, FI.
-# This spider is scraping the gym centers in all countries.
 class StcSESpider(scrapy.Spider):
     name = "stc_se"
     item_attributes = {"brand": "STC", "brand_wikidata": "Q124061743"}
@@ -14,7 +11,6 @@ class StcSESpider(scrapy.Spider):
     def parse(self, response):
         for club in response.json():
             item = DictParser.parse(club)
-            item["branch"] = item["name"]
             item["branch"] = item.pop("name")
             item["city"] = item["city"].title()
 
