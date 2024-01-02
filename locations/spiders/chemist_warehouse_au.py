7d617ef227ef1b8384e81aff6ddc80921a6488b3
David Hicks <david@hicks.id.au>
2024-01-03 02:16:20 +1100

    chemist_warehouse_au: fix spider

--- locations/spiders/chemist_warehouse_au.py
+++ locations/spiders/chemist_warehouse_au.py
@@ -1,11 +1,10 @@
-import scrapy
-import xmltodict
+from scrapy.spiders import XMLFeedSpider
 
-from locations.hours import DAYS_FULL, OpeningHours
+from locations.hours import OpeningHours
 from locations.items import Feature
 
 
-class ChemistWarehouseAUSpider(scrapy.Spider):
+class ChemistWarehouseAUSpider(XMLFeedSpider):
     name = "chemist_warehouse_au"
     item_attributes = {"brand": "Chemist Warehouse", "brand_wikidata": "Q48782120"}
     allowed_domains = ["www.chemistwarehouse.com.au"]
@@ -13,30 +12,31 @@ class ChemistWarehouseAUSpider(scrapy.Spider):
         "https://www.chemistwarehouse.com.au/ams/webparts/Google_Map_SL_files/storelocator_data.ashx?searchedPoint=(0,%200)&TrafficSource=1&TrafficSourceState=0"
     ]
     requires_proxy = True  # Residential IP addresses appear to be required.
+    iterator = "xml"
+    itertag = "marker"
 
-    def parse(self, response):
-        for data in xmltodict.parse(response.text).get("markers").get("marker"):
-            item = Feature()
-            item["ref"] = data.get("@id")
-            item["name"] = data.get("@storename")
-            item["lat"] = data.get("@lat")
-            item["lon"] = data.get("@lng")
-            item["street_address"] = " ".join(data.get("@storeaddress").split())
-            item["city"] = data.get("@storesuburb")
-            item["state"] = data.get("@storestate")
-            item["postcode"] = data.get("@storepostcode")
-            item["country"] = "AU"
-            item["phone"] = data.get("@storephone")
-            item["email"] = data.get("@storeemail")
-            oh = OpeningHours()
-            for day in DAYS_FULL:
-                rule = (
-                    data["@store" + day[:3].lower()].replace(".", ":").replace("00:00AM - 00:00AM", "12:00AM - 12:00AM")
-                )
-                open_time, close_time = rule.split(" - ")
-                if open_time == "0" or close_time == "0":
-                    continue
-                oh.add_range(day, open_time, close_time, time_format="%I:%M%p")
-            item["opening_hours"] = oh.as_opening_hours()
-
-            yield item
+    def parse_node(self, response, node):
+        properties = {
+            "ref": node.xpath('@id').get(),
+            "name": node.xpath('@storename').get(),
+            "lat": node.xpath('@lat').get(),
+            "lon": node.xpath('@lon').get(),
+            "street_address": node.xpath('@storeaddress').get(),
+            "city": node.xpath('@storesuburb').get(),
+            "state": node.xpath('@storestate').get(),
+            "postcode": node.xpath('@storepostcode').get(),
+            "phone": node.xpath('@storephone').get(),
+            "email": node.xpath('@storeemail').get(),
+        }
+        hours_string = " ".join([
+            "Mon: ", node.xpath('@storemon').get(),
+            "Tue: ", node.xpath('@storetue').get(),
+            "Wed: ", node.xpath('@storewed').get(),
+            "Thu: ", node.xpath('@storethu').get(),
+            "Fri: ", node.xpath('@storefri').get(),
+            "Sat: ", node.xpath('@storesat').get(),
+            "Sun: ", node.xpath('@storesun').get(),
+        ])
+        properties["opening_hours"] = OpeningHours()
+        properties["opening_hours"].add_ranges_from_string(hours_string)
+        yield Feature(**properties)
