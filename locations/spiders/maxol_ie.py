078d4a8202922e15c173e1d5479df4f838da85d1
Cj Malone <CjMalone@mail.com>
2024-01-10 11:25:23 +0000

    [MaxolIE] Collect coords

--- locations/spiders/maxol_ie.py
+++ locations/spiders/maxol_ie.py
@@ -1,3 +1,5 @@
+import re
+
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories
@@ -9,3 +11,9 @@ class MaxolIESpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Maxol", "brand_wikidata": "Q3302837", "extras": Categories.FUEL_STATION.value}
     sitemap_urls = ["https://stations.maxol.ie/sitemap.xml"]
     sitemap_rules = [(r"\.ie\/[-\w]+\/[-.\w]+\/[-\/\w]+$", "parse_sd")]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if m := re.search(r"\"latitude\":(-?\d+\.\d+),\"longitude\":(-?\d+\.\d+)", response.text):
+            item["lat"], item["lon"] = m.groups()
+
+        yield item
