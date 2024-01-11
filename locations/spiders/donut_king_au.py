88fa9b00f9b3a8086d970ea90a527d509221c68f
David Hicks <david@hicks.id.au>
2024-01-12 01:28:11 +1100

    donut_king_au: set time format for opening hours

--- locations/spiders/donut_king_au.py
+++ locations/spiders/donut_king_au.py
@@ -7,6 +7,7 @@ class DonutKingAUSpider(WPStoreLocatorSpider):
     name = "donut_king_au"
     item_attributes = {"brand_wikidata": "Q5296921", "brand": "Donut King"}
     start_urls = ["https://www.donutking.com.au/wp/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
+    time_format = "%I:%M %p"
 
     def parse_item(self, item, location):
         if "CLOSED" in re.split(r"\W+", item.get("name", "").upper()):
