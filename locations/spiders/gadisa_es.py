1b9e4fd68400747f7476e7a32563a29d1ac4dc5a
Ian Dees <ian.dees@gmail.com>
2024-01-12 10:10:52 -0600

    Add autoflake to remove unused imports (#6693)
    * Add autoflake to remove unused imports
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/gadisa_es.py
+++ locations/spiders/gadisa_es.py
@@ -1,7 +1,6 @@
 from scrapy import Spider
 from scrapy.http import JsonRequest
 
-from locations.dict_parser import DictParser
 from locations.hours import DAYS_ES, DELIMITERS_ES, OpeningHours
 from locations.items import Feature
 
