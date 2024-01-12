2ecd4ce52bdf26cbc013027ae7d2b30e1482e3c1
David Hicks <david@hicks.id.au>
2024-01-13 02:08:12 +1100

    five_guys_hk: fix sitemap_rules regex

--- locations/spiders/five_guys_hk.py
+++ locations/spiders/five_guys_hk.py
@@ -8,4 +8,4 @@ class FiveGuysHKSpider(SitemapSpider, StructuredDataSpider):
     name = "five_guys_hk"
     item_attributes = FiveGuysUSSpider.item_attributes
     sitemap_urls = ["https://restaurants.fiveguys.com.hk/sitemap.xml"]
-    sitemap_rules = [("^https://restaurants\.fiveguys\.com\.hk\/en_hk\/(?!search$)[^/]+$", "parse_sd")]
+    sitemap_rules = [(r"^https:\/\/restaurants\.fiveguys\.com\.hk\/en_hk\/(?!search$)[^/]+$", "parse_sd")]
