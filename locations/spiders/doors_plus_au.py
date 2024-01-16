80e9224b8703cf9a299659a5ee14456be327cfa1
Karen Kerr <138442998+karen-kerr@users.noreply.github.com>
2024-01-16 19:49:44 +0000

    [DoorsPlusAU] Add category (#6730)

--- locations/spiders/doors_plus_au.py
+++ locations/spiders/doors_plus_au.py
@@ -3,6 +3,10 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 class DoorsPlusAUSpider(WPStoreLocatorSpider):
     name = "doors_plus_au"
-    item_attributes = {"brand": "Doors Plus", "brand_wikidata": "Q78945358"}
+    item_attributes = {
+        "brand": "Doors Plus",
+        "brand_wikidata": "Q78945358",
+        "extras": {"shop": "doors"},
+    }
     allowed_domains = ["www.doorsplus.com.au"]
     time_format = "%I:%M %p"
