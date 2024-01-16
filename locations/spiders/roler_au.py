2096835878be28a4ae42c819e530c1759c6e86f1
Karen Kerr <138442998+karen-kerr@users.noreply.github.com>
2024-01-16 20:21:30 +0000

    [RolerAU] Add category (#6741)

--- locations/spiders/roler_au.py
+++ locations/spiders/roler_au.py
@@ -1,3 +1,4 @@
+from locations.categories import Categories, apply_category
 from locations.storefinders.stockinstore import StockInStoreSpider
 
 
@@ -12,4 +13,5 @@ class RolerAUSpider(StockInStoreSpider):
     def parse_item(self, item, location):
         if "INDUSTRIE" in item["name"].upper():
             return
+        apply_category(Categories.SHOP_CLOTHES, item)
         yield item
