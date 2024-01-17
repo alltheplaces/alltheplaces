1d0fec31cd4ca225192760642683c587b767d8a4
Mateusz Konieczny <matkoniecz@gmail.com>
2024-01-17 04:49:06 +0100

    remove unused import

--- locations/spiders/western_union.py
+++ locations/spiders/western_union.py
@@ -4,7 +4,6 @@ from scrapy import Spider
 from scrapy.downloadermiddlewares.retry import get_retry_request
 from scrapy.http import JsonRequest
 
-from locations.categories import Categories
 from locations.dict_parser import DictParser
 from locations.geo import point_locations
 from locations.hours import OpeningHours
