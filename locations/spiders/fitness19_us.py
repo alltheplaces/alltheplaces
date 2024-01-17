e8607b7f55f4c3a875122a50ce139f07ddd61715
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-01-17 14:32:43 +0000

    [Fitness19US] Add spider (#6764)

---
+++ locations/spiders/fitness19_us.py
@@ -0,0 +1,18 @@
+import re
+
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class Fitness19USSpider(SitemapSpider, StructuredDataSpider):
+    name = "fitness19_us"
+    item_attributes = {"brand": "Fitness 19", "brand_wikidata": "Q121787953"}
+    sitemap_urls = ["https://www.fitness19.com/location-sitemap.xml"]
+    wanted_types = ["LocalBusiness"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if m := re.search(r"LatLng\((-?\d+\.\d+) ,(-?\d+\.\d+)\);", response.text):
+            item["lat"], item["lon"] = m.groups()
+
+        yield item
