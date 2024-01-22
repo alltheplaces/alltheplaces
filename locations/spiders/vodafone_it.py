b46aa53b5f693b873f9360e7ea0d6ce1aa7ba8a7
Joe Dixon <mjoe999@gmail.com>
2024-01-22 17:44:28 +0000

    new command to show spiders sharing hard coded wikidata information

--- locations/spiders/vodafone_it.py
+++ locations/spiders/vodafone_it.py
@@ -1,8 +1,9 @@
+from locations.spiders.vodafone_de import VODAFONE_SHARED_ATTRIBUTES
 from locations.storefinders.yext import YextSpider
 
 
 class VodafoneITSpider(YextSpider):
     name = "vodafone_it"
-    item_attributes = {"brand": "Vodafone", "brand_wikidata": "Q122141"}
+    item_attributes = VODAFONE_SHARED_ATTRIBUTES
     api_key = "07377ddb3ff87208d4fb4d14fed7c6ff"
     api_version = "20220511"
