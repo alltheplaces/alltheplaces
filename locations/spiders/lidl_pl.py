da5856047d6a675d0c955311acdf8cadb1bb3251
Colton Piper <pipercolton@gmail.com>
2024-01-24 13:32:34 -0500

    Deleted getting name from ShownStoreName

--- locations/spiders/lidl_pl.py
+++ locations/spiders/lidl_pl.py
@@ -1,6 +1,4 @@
-import re
-
-from locations.hours import DAYS_PL, OpeningHours, sanitise_day
+from locations.hours import DAYS_PL
 from locations.spiders.lidl_at import LidlATSpider
 
 
@@ -11,14 +9,3 @@ class LidlPLSpider(LidlATSpider):
     dataset_name = "Filialdaten-PL/Filialdaten-PL"
     key = "AnZ7UrM33kcHeNxFJsJ6McC4-iAx6Mv55FfsAzmlImV6eJ1n6OX4zfhe2rsut6CD"
     days = DAYS_PL
-
-    def parse_item(self, item, feature, **kwargs):
-        item["opening_hours"] = OpeningHours()
-        for day, start_time, end_time in re.findall(
-            r"(\w+) (\d{2}:\d{2})-(\d{2}:\d{2})",
-            feature["OpeningTimes"],
-        ):
-            if day := sanitise_day(day, self.days):
-                item["opening_hours"].add_range(day, start_time, end_time)
-
-        yield item
