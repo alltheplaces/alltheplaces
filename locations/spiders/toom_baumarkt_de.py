88e167b8c34e5134cab036df8e3a3aef88fef7b4
Josef Sterovsky <104327272+sterojos@users.noreply.github.com>
2024-01-29 23:31:09 +0100

    [ToomBaumarktDe] Fix sitemap URL
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>

--- locations/spiders/toom_baumarkt_de.py
+++ locations/spiders/toom_baumarkt_de.py
@@ -6,5 +6,6 @@ from locations.structured_data_spider import StructuredDataSpider
 class ToomBaumarktDeSpider(SitemapSpider, StructuredDataSpider):
     name = "toom_baumarkt_de"
     item_attributes = {"brand": "Toom Baumarkt", "brand_wikidata": "Q2442970"}
-    sitemap_urls = ["https://static.toom.de/sitemap/cms/newmarkets-0.xml"]
+    sitemap_urls = ["https://static.toom.de/sitemap/cms.xml"]
+    sitemap_follow = ["newmarkets"]
     json_parser = "chompjs"
