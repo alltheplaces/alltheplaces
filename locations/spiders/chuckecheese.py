71a16b52150a6d0584f9ed20c3e6485b2ad6a5f6
Cj Malone <CjMalone@mail.com>
2024-02-08 17:02:08 +0000

    [ChuckECheese] Fix spider

--- locations/spiders/chuckecheese.py
+++ locations/spiders/chuckecheese.py
@@ -9,6 +9,7 @@ class ChuckECheeseSpider(SitemapSpider, StructuredDataSpider):
     allowed_domains = ["locations.chuckecheese.com"]
     sitemap_urls = ["https://locations.chuckecheese.com/sitemap.xml"]
     sitemap_rules = [(r"locations\.chuckecheese\.com(?:\/[a-z]{2}){1,2}(?:\/[\w\-.]{3,}){2}", "parse_sd")]
+    wanted_types = ["AmusementPark"]
 
     def post_process_item(self, item, response, ld_data):
         item.pop("image", None)
