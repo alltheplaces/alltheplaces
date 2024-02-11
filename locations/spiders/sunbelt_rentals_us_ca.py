c140bd2d5cb328156b03ff4d7c0d80f31de772e8
Ian Dees <ian.dees@gmail.com>
2024-02-11 10:35:13 -0600

    Update for review comments

--- locations/spiders/sunbelt_rentals_us_ca.py
+++ locations/spiders/sunbelt_rentals_us_ca.py
@@ -4,13 +4,19 @@ import re
 
 import scrapy
 
+from locations.categories import Categories
+from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 from locations.items import Feature
 
 
 class SunbeltRentalsUsCaSpider(scrapy.Spider):
     name = "sunbelt_rentals_us_ca"
-    item_attributes = {"brand": "Sunbelt Rentals", "brand_wikidata": "Q102396721"}
+    item_attributes = {
+        "brand": "Sunbelt Rentals",
+        "brand_wikidata": "Q102396721",
+        "extras": Categories.SHOP_TOOL_HIRE.value,
+    }
     allowed_domains = ["sunbeltrentals.com"]
 
     def start_requests(self):
@@ -19,16 +25,7 @@ class SunbeltRentalsUsCaSpider(scrapy.Spider):
             headers={
                 "Client_id": "0oa56ipgl8SAfB1kE5d7",
                 "Client_secret": "6yNzPOIbav3xJ0XMFRI9cCKjEmqcKXiPVPhQS7eo",
-                "Logintoken": "",
-                "Request-Id": "12323sunbelt",
-                "X-Correlation-Id": "qjElWEoUHtD5oK0uqMbCivbwL632fc23bef67923b234f8c19",
-                "Origin": "https://www.sunbeltrentals.com",
-                "Referer": "https://www.sunbeltrentals.com/",
-                "Sec-Ch-Ua-Platform": "macOS",
-                "Authorization": "Bearer",
-                "Channel": "WEBAPP",
                 "Companyid": 0,
-                "Host": "api.sunbeltrentals.com",
             },
             method="GET",
             callback=self.parse,
@@ -40,37 +37,26 @@ class SunbeltRentalsUsCaSpider(scrapy.Spider):
         for hour in hours:
             day = hour["day"][:2]
             if hour["hours"] == "Closed":
-                pass
-            else:
-                try:
-                    o = hour["open"].split("T")
-                    open = o[1]
-                    c = hour["close"].split("T")
-                    close = c[1]
-                    opening_hours.add_range(day=day, open_time=open, close_time=close, time_format="%H:%M:%S")
-                except:
-                    o = hour["hours"].split(" - ")
-                    open = o[0]
-                    close = o[1]
-                    opening_hours.add_range(day=day, open_time=open, close_time=close, time_format="%I:%M %p")
+                continue
+
+            try:
+                o = hour["open"].split("T")
+                open = o[1]
+                c = hour["close"].split("T")
+                close = c[1]
+                opening_hours.add_range(day=day, open_time=open, close_time=close, time_format="%H:%M:%S")
+            except:
+                o = hour["hours"].split(" - ")
+                open = o[0]
+                close = o[1]
+                opening_hours.add_range(day=day, open_time=open, close_time=close, time_format="%I:%M %p")
 
         return opening_hours.as_opening_hours()
 
     def parse(self, response):
-        data = response.json()
-
-        for store in data["data"]["pcList"]:
-            properties = {
-                "ref": store["pc"],
-                "name": store["name"],
-                "street_address": store["street"],
-                "city": store["city"],
-                "state": store["state"],
-                "postcode": store["zip"],
-                "lat": store["latitude"],
-                "lon": store["longitude"],
-                "phone": store["phone"],
-            }
+        for store in response.json()["data"]["pcList"]:
+            item = DictParser.parse(location)
+            item["street_address"] = item.pop("street", None)
 
             hours = self.parse_hours(store["operatingHours"])
             if hours:
