f9fd615888b133a182a90bcb57689c5cc43fbfa7
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-11 21:07:54 +0000

    Rename and add multiple countries

--- locations/spiders/benefit_cosmetics.py
+++ locations/spiders/benefit_cosmetics.py
@@ -2,8 +2,8 @@ from locations.categories import Categories
 from locations.storefinders.metalocator import MetaLocatorSpider
 
 
-class BenefitCosmeticsAUSpider(MetaLocatorSpider):
-    name = "benefit_cosmetics_au"
+class BenefitCosmeticsSpider(MetaLocatorSpider):
+    name = "benefit_cosmetics"
     item_attributes = {"brand_wikidata": "Q2895769", "brand": "Benefit", "extras": Categories.SHOP_BEAUTY.value}
     brand_id = "17147"
-    country_list = ["Australia"]
+    country_list = ["Australia", "Canada", "United States"]
