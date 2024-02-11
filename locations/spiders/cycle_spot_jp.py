a5a4ba356ada1a2f8e89c28b63e4c7f3a3e8d8ba
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-11 06:14:04 +0000

    Add URLs in attempt to get more than 5 stores

--- locations/spiders/cycle_spot_jp.py
+++ locations/spiders/cycle_spot_jp.py
@@ -1,13 +1,17 @@
 
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
-
+from locations.categories import Categories
 
 class CycleSpotJPSpider(WPStoreLocatorSpider):
-        name = "cycle_spot_jp"
-        item_attributes = {
-                "brand_wikidata": "Q93620124",
-                "brand": "サイクルスポット",
-        }
-        allowed_domains = [
-                "www.cyclespot.net",
-        ]
+    name = "cycle_spot_jp"
+    item_attributes = {
+        "brand_wikidata": "Q93620124",
+        "brand": "サイクルスポット",
+        "extras": Categories.SHOP_BICYCLE.value,
+    }
+    allowed_domains = [
+        "www.cyclespot.net",
+    ]
+    start_urls = (
+        "https://www.cyclespot.net/wp-admin/admin-ajax.php?action=store_search&autoload=1&max_results=1000&search_radius=10000",
+    )
