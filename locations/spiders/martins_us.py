fc1f525b25e12d80f0546bd6a408a2015b562b6d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-12 21:32:12 +0000

    Add attributes

--- locations/spiders/martins_us.py
+++ locations/spiders/martins_us.py
@@ -1,6 +1,12 @@
 from locations.storefinders.freshop import FreshopSpider
+from locations.categories import Categories
 
 
-class MartinUSSpider(FreshopSpider):
-    name = "martin_us"
+class MartinsUSSpider(FreshopSpider):
+    name = "martins_us"
     app_key = "martins"
+    item_attributes = {
+        "brand": "Martin's Super Markets",
+        "brand_wikidata": "Q7573912",
+        "extras": Categories.SHOP_SUPERMARKET.values,
+    }
