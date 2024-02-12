9ce3b832d037db1c43e20434af01542908d2d21c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-12 08:57:45 +0000

    Add wikidata

--- locations/spiders/goodfeet.py
+++ locations/spiders/goodfeet.py
@@ -5,6 +5,6 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class GoodfeetSpider(SitemapSpider, StructuredDataSpider):
     name = "goodfeet"
-    item_attributes = {"brand": "The Good Feet Store"}
+    item_attributes = {"brand": "The Good Feet Store", "brand_wikidata": "Q122031157"}
     sitemap_urls = ["https://www.goodfeet.com/sitemap.xml"]
     sitemap_rules = [(r"/locations/(.*)$", "parse_sd")]
