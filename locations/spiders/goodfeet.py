62f1079e1c410e70bf7ef6fc021a48b12fccb3f3
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-12 08:55:45 +0000

    Remove commented out

--- locations/spiders/goodfeet.py
+++ locations/spiders/goodfeet.py
@@ -5,9 +5,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class GoodfeetSpider(SitemapSpider, StructuredDataSpider):
     name = "goodfeet"
-    item_attributes = {"brand": "Good Feet"}
+    item_attributes = {"brand": "The Good Feet Store"}
     sitemap_urls = ["https://www.goodfeet.com/sitemap.xml"]
     sitemap_rules = [(r"/locations/(.*)$", "parse_sd")]
-    # wanted_types = ["Store"]
-    # is_playwright_spider = True
-    # custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
+
