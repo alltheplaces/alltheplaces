dde66b98cc90cd4a40598f6f851b5dc8d063f873
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-12 21:50:53 +0000

    Fromatting

---
+++ locations/spiders/spa_world_au_nz.py
@@ -0,0 +1,18 @@
+from locations.categories import Categories
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class SpawWorldAUNZSpider(SitemapSpider, StructuredDataSpider):
+    name = "spa_world_au_nz"
+    item_attributes = {
+        "brand": "Spa World",
+        "brand_wikidata": "Q124003802",
+        "extras": {"shop": "swimming_pool"},
+    }
+    sitemap_urls = [
+        "https://www.spaworld.com.au/sitemap/sitemap-index.xml",
+        "https://www.spaworld.co.nz/sitemap/sitemap-index.xml",
+    ]
+    sitemap_rules = [("find-a-showroom/(.*)", "parse_sd")]
