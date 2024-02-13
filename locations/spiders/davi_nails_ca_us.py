cfcba3cd61c653d28260fea724698525fcd587a1
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-13 12:31:03 +0000

    Clean city and names

--- locations/spiders/davi_nails_ca_us.py
+++ locations/spiders/davi_nails_ca_us.py
@@ -1,6 +1,10 @@
-from locations.categories import Categories
-from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+import re
 
+from scrapy import Request
+
+from locations.categories import Categories
+from locations.items import Feature
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 class DaViNailsCAUSSpider(WPStoreLocatorSpider):
     name = "davi_nails_ca_us"
@@ -12,3 +16,12 @@ class DaViNailsCAUSSpider(WPStoreLocatorSpider):
     allowed_domains = [
         "davinails.com",
     ]
+    searchable_points_files = {
+        "us_centroids_100mile_radius.csv"
+    }
+    search_radius = 100
+
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        item["city"] = item["city"].strip(",")
+        item["name"] = re.sub(" inside WM \#\d+", "", item["name"])
+        yield item
