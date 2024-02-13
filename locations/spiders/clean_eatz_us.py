e82100d2d148ec85d2556f89a6e6355b4061100e
Karen Kerr <karenkerrgithub@gmail.com>
2024-02-13 08:44:45 +0000

    [CleanEatzUS] Add category

--- locations/spiders/clean_eatz_us.py
+++ locations/spiders/clean_eatz_us.py
@@ -1,11 +1,12 @@
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class CleanEatzUSSpider(SitemapSpider, StructuredDataSpider):
     name = "clean_eatz_us"
-    item_attributes = {"brand": "Clean Eatz", "brand_wikidata": "Q124412638"}
+    item_attributes = {"brand": "Clean Eatz", "brand_wikidata": "Q124412638", "extras": Categories.FAST_FOOD.value}
     sitemap_urls = ["https://locations.cleaneatz.com/robots.txt"]
     sitemap_rules = [(r"com/\w\w/[^/]+/meal-prep-(\d+).html$", "parse")]
     wanted_types = ["Restaurant"]
