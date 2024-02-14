f01d99dc96b3ae33e560d0db4fc735d2fae733ec
jhutton1 <jaimehutton01@gmail.com>
2024-02-14 10:17:03 -0500

    fix access token

--- locations/spiders/pizza_hut_tr.py
+++ locations/spiders/pizza_hut_tr.py
@@ -1,31 +1,37 @@
+import re
+
 import scrapy
 from scrapy.http import JsonRequest
 
-from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from locations.spiders.pizza_hut_us import PizzaHutUSSpider
 
 
 class PizzaHutTRSpider(scrapy.Spider):
     name = "pizza_hut_tr"
-    item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
+    item_attributes = PizzaHutUSSpider.PIZZA_HUT
+    start_urls = ["https://www.pizzahut.com.tr/static/js/main.c39af298.chunk.js"]
 
-    def start_requests(self):
+    def parse(self, response, **kwargs):
+        if match := re.search(r"Username[:\s]+\"(.+?)\"[\s,]+Password[:\s]+\"(.+?)\"", response.text):
+            yield JsonRequest(
+                url="https://auth.pizzahut.com.tr/api/auth/AuthService",
+                data={"Username": match.group(1), "Password": match.group(2)},
+                callback=self.parse_token,
+            )
+
+    def parse_token(self, response):
+        access_token = response.json()["access_token"]
         yield JsonRequest(
             url="https://api.pizzahut.com.tr/api/web/Restaurants/GetRestaurants?getAll=true",
-            headers={
-                "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjM4NUI0QjAyRENBMzQxMDMwNDhBN0EyNzQxRDc3NTA1MjlGM0RDMkYiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPRnRMQXR5alFRTUVpbm9uUWRkMUJTbnozQzgifQ.eyJuYmYiOjE3MDY2MDQwMjksImV4cCI6MTcwNzIwODgyOSwiaXNzIjoiaHR0cDovL2F1dGgucGl6emFodXQuY29tLnRyIiwiYXVkIjpbImh0dHA6Ly9hdXRoLnBpenphaHV0LmNvbS50ci9yZXNvdXJjZXMiLCJwaXp6YWh1dGFwaSJdLCJjbGllbnRfaWQiOiJ3ZWIiLCJzdWIiOiJyb2J1c2VyQGNsb2Nrd29yay5jb20udHIiLCJhdXRoX3RpbWUiOjE3MDY2MDQwMjksImlkcCI6ImxvY2FsIiwiZW1haWwiOiJyb2J1c2VyQGNsb2Nrd29yay5jb20udHIiLCJ1aWQiOiIyNCIsInBlcnNvbmlkIjoiMzAwMDMyOTM5MDc5IiwibmFtZXN1cm5hbWUiOiJQSCBEVU1NWSIsImxjdG9rZW4iOiJWclE0Rk81WTlwSktNZk9LbW9PSGZTTDE2OTdlc3FfajdIMEZGYU01Z25XMzVyTXpCdzRQMmkzS1pVcWF4Vy1reWVHcjZNYkhIc3Z2VkxQTG1KTkdqY29IdEQ1WUtJT0FiemRxQ1VaOEpHM2w5bHptRlJfQkdYbjlCTUxHUTcwaG5GdXhPTVRoeEd4RFV3bmJfbHprU1NnSE14eWFhQTUtSURxelNOSUZCY2Y3R2NuQ1BOSlgxUlBJR3h0WFN3N1Bjcmk4dkZVQ2hYS2trVEIxTEhrdi1rX0ZicGlOQVFfOFZFOGlvSWlyOEVYTEpZV2U0VWM5RXFoUDU3Tm5CbjBNYmlNNDFXcnBqSFNiRVNtZ3pUZ0NPWGt2ZEZ3WHhiRVdjdXU1Y0tHMG5NQXFOQmdWTGtwbHZRT0VmX1YyZzdMdkhGeVVSeC1YamsxUDIyRVlzTl9tOEpyaEVKWVNXOElLbXhLOUd0X1UzOHBNRnpvTUVLdXNrUWdiM2VWZVhPY1ZteUlDNlEiLCJsY3JlZnJlc2h0b2tlbiI6IjczN2U3MjBkMjI1NjQyNDM4N2UwYTNlZmMzOGU4ZWZmIiwicGVyc29uZW1haWwiOiJkdW1teUBtYXdhcmlkZ2lkYS5jb20iLCJzY29wZSI6WyJwaXp6YWh1dGFwaSIsIm9mZmxpbmVfYWNjZXNzIl0sImFtciI6WyI3NjU2QkFGM0YxNUE2NTA0QkJGM0NFRTgyOTA5MkRGQSJdfQ.N2hwVhTm31_RgXRu2_lV1CU0KWqiGGbzG2UVWUWge8aYMJDMhEK_oQmsh8mEvrZq_pGnlb3PTIdO2L4Jx08ohu0675pKlKl1BBDiappMzy2sjsAoxMI49B9EQKIKpdJAzPoiMhV2jpFNkrIyXouJzUrTpTowqkl45TOLZLBxGH7tc-kfwkYac8vOii81__fTq2_xFQaiv_wloA5avktYU8n68WHjVbROvFiM8_cb8xMFEpWE51bIqLZpk51yuCMUHOlEaj2orzptHRJjvWIFIwEpwENtOMjG4L54x7LUpHMJ-VeVa7JRK0B2G9FEQJrhqoh9G_Sf556On0SSfZ9iJQ"
-            },
+            headers={"Authorization": f"Bearer {access_token}"},
+            callback=self.parse_stores,
         )
 
-    def parse(self, response):
-        for poi in response.json()["result"]:
-            item = DictParser.parse(poi)
-            apply_category(Categories.RESTAURANT, item)
-            latitude = poi.get("latitude")
-            longitude = poi.get("longitude")
-
-            if latitude is not None and longitude is not None:
-                item["lat"] = latitude.replace(",", ".")
-                item["lon"] = longitude.replace(",", ".")
-
+    def parse_stores(self, response):
+        for store in response.json()["result"]:
+            item = DictParser.parse(store)
+            if item["lat"] and item["lon"]:
+                item["lat"] = item["lat"].replace(",", ".")
+                item["lon"] = item["lon"].replace(",", ".")
             yield item
