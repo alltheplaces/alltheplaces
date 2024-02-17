f26caf7ad8d64ab68a382bd833c361d574b5c130
tif-calin <76538249+tif-calin@users.noreply.github.com>
2024-02-16 20:32:24 -0500

    Attempt to fix `costco` spider with proxy (#7249)
    * adds requires_proxy
    
    * Collect ref
    
    * Drop facebook link
    
    * Collect name and branch
    
    * Rename spider
    
    * Rerename spider
    
    Includes PR
    
    ---------
    
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/costco.py
+++ locations/spiders/costco.py
@@ -17,4 +17,16 @@ class CostcoSpider(SitemapSpider, StructuredDataSpider):
     custom_settings = {"ROBOTSTXT_OBEY": False}
     sitemap_urls = ["https://www.costco.com/sitemap_lw_index.xml"]
     sitemap_follow = ["lw_l"]
-    sitemap_rules = [("/warehouse-locations/", "parse_sd")]
+    sitemap_rules = [(r"/warehouse-locations/[^.]+-(\d+)\.html$", "parse_sd")]
+    requires_proxy = True
+    search_for_facebook = False
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        name = response.xpath('//h1[@automation-id="warehouseNameOutput"]/text()').get()
+        item["branch"] = name.removesuffix(" Warehouse").removesuffix(" Business Center")
+        if "Business Center" in name:
+            item["name"] = "Costco Business Center"
+        else:
+            item["name"] = "Costco"
+
+        yield item
