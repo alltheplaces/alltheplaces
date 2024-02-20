1959d5a7bde4f09db7721a245d9919763b3e7c66
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-20 22:23:19 +0000

    Naming

--- locations/spiders/sanders_backstube_de.py
+++ locations/spiders/sanders_backstube_de.py
@@ -2,7 +2,7 @@ from locations.hours import DAYS_DE
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class sandersbackstubeDESpider(WPStoreLocatorSpider):
+class SandersBackstubeDESpider(WPStoreLocatorSpider):
     name = "sanders_backstube_de"
     item_attributes = {
         "brand_wikidata": "Q66207337",
