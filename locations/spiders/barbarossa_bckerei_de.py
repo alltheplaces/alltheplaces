6bf5b61755f932d48a5e4bddaf44086adb756f4e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-24 02:01:31 +0000

    naming

--- locations/spiders/barbarossa_bckerei_de.py
+++ locations/spiders/barbarossa_bckerei_de.py
@@ -2,8 +2,8 @@ from locations.hours import DAYS_DE
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class BarbarossaBckereiDESpider(WPStoreLocatorSpider):
-    name = "barbarossa_bckerei_de"
+class BarbarossaBaeckereiDESpider(WPStoreLocatorSpider):
+    name = "barbarossa_baeckerei_de"
     item_attributes = {
         "brand_wikidata": "Q807766",
         "brand": "Barbarossa Bäckerei",
