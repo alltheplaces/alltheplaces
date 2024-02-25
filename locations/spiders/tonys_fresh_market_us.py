3b6b1b35c00047daeda27abb0619c0fac313ca00
David Hicks <david@hicks.id.au>
2024-02-25 02:22:07 +0000

    Add Tony's Fresh Market (US) (21 stores) (#7528)
    * Add Tony's Fresh Market (US) (21 stores)
    
    Fixes #7526
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/tonys_fresh_market_us.py
@@ -0,0 +1,16 @@
+from locations.categories import Categories
+from locations.storefinders.freshop import FreshopSpider
+
+
+class TonysFreshMarketSpider(FreshopSpider):
+    name = "tonys_fresh_market_us"
+    item_attributes = {
+        "brand": "Tony's Fresh Market",
+        "brand_wikidata": "Q118594253",
+        "extras": Categories.SHOP_SUPERMARKET.value,
+    }
+    app_key = "tony_s_fresh_market"
+
+    def parse_item(self, item, location):
+        item["phone"] = item["phone"].split("\n", 1)[0]
+        yield item
