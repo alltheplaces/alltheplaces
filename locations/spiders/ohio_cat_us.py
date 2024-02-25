da841bc07cb1fd4e40e9b9f3455d061ac0114dff
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-25 21:18:34 +0000

    Add

---
+++ locations/spiders/ohio_cat_us.py
@@ -0,0 +1,12 @@
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+
+
+class OhioCATUSSpider(WPStoreLocatorSpider):
+    name = "ohio_cat_us"
+    item_attributes = {
+        "brand_wikidata": "Q115486235",
+        "brand": "Ohio CAT",
+    }
+    allowed_domains = [
+        "ohiocat.com",
+    ]
