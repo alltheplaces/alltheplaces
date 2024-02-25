3a4a98d31b20bb195ae1de214befcdce018a25a1
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-25 04:55:43 +0000

    Add basic detection of uberall

--- locations/storefinders/uberall.py
+++ locations/storefinders/uberall.py
@@ -1,13 +1,16 @@
 import logging
 
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from urllib.parse import urlparse
 
+from scrapy import Spider
+from scrapy.http import JsonRequest, Request, Response
+
+from locations.automatic_spider_generator import AutomaticSpiderGenerator
 from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
 
 
-class UberallSpider(Spider):
+class UberallSpider(Spider, AutomaticSpiderGenerator):
     dataset_attributes = {"source": "api", "api": "uberall.com"}
 
     key = ""
@@ -48,5 +51,23 @@ class UberallSpider(Spider):
 
             yield from self.parse_item(item, feature)
 
-    def parse_item(self, item, feature, **kwargs):
-        yield item
+    def storefinder_exists(response: Response) -> bool | Request:
+        # Example: https://www.yves-rocher.it/trova-il-tuo-negozio#!
+        # <script src="https://uberall.com/assets/storeFinderWidget-v2.js"></script>
+        if response.xpath('//script[contains(@src, "https://uberall.com/assets/storeFinderWidget-v2.js")]').get():
+            return True
+
+        # Example: https://www.aldi.pl/informacje-dla-klienta/wyszukiwarka-sklepu.html
+        # Example: https://www.hypovereinsbank.de/hvb/kontaktwege/filiale
+        # TODO: Needs playwright to detect this properly in most cases
+        # Loads into the DOM:
+        # <script type="module" src="https://locator.uberall.com/locator-assets/store-finder-widget-bundle-v2-modern.js?b=My4xODQuMjU="></script>
+        if response.xpath('//script[contains(@src, "https://locator.uberall.com/locator-assets/store-finder-widget-bundle-v2-modern.js")]').get():
+            return True
+
+        return False
+
+    def extract_spider_attributes(response: Response) -> dict | Request:
+        return {
+            "allowed_domains": [urlparse(response.url).netloc],
+        }
