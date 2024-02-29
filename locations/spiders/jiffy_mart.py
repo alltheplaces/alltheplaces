723123041d19d44624a6feeb9bb948b38298d9aa
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-29 11:12:22 +0000

    Add jiffy mart

---
+++ locations/spiders/jiffy_mart.py
@@ -0,0 +1,15 @@
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from locations.hours import DAYS_EN
+
+
+class JiffyMartSpider(WPStoreLocatorSpider):
+    name = "jiffy_mart"
+    item_attributes = {
+        "brand_wikidata": "Q119592174",
+        "brand": "Jiffy Mart",
+    }
+    allowed_domains = [
+        "jiffymartstores.com",
+    ]
+    time_format = "%I:%M %p"
+    days = DAYS_EN
