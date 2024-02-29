a9ead79b4815305b2763bafa851f397928689b5c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-29 11:25:44 +0000

    Hours

--- locations/spiders/leader_br.py
+++ locations/spiders/leader_br.py
@@ -1,8 +1,10 @@
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from locations.hours import DAYS_EN
 
 
 class LeaderBRSpider(WPStoreLocatorSpider):
     name = "leader_br"
+    days = DAYS_EN
     item_attributes = {
         "brand_wikidata": "Q10316732",
         "brand": "Leader",
