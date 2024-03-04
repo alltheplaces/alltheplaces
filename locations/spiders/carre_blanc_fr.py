f524e48d63905179e78fc93ee2f7fa6abf48c8e2
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-04 21:03:06 +0000

    Add Carre blanc

---
+++ locations/spiders/carre_blanc_fr.py
@@ -0,0 +1,14 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class CarreBlancFRSpider(CrawlSpider, StructuredDataSpider):
+    name = "carre_blanc_fr"
+    item_attributes = {"brand": "Carré blanc", "brand_wikidata": "Q55596025"}
+    start_urls = ["http://boutiques.carreblanc.com/"]
+    rules = [
+        # Example: https://boutiques.carreblanc.com/linge-de-maison/montlucon/MON
+        Rule(LinkExtractor(allow=r"https://boutiques.carreblanc.com/[\w-]+/[\w-]+/[\w\w\w]"), callback="parse_sd"),
+    ]
