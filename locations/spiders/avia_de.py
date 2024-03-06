3fc8ac96848364b30a50cf55b364d5d8666f74ea
panasonic-mafia <kolosov.yv@gmail.com>
2024-03-06 19:10:55 +0100

    improve avia_de

--- locations/spiders/avia_de.py
+++ locations/spiders/avia_de.py
@@ -1,6 +1,16 @@
 import scrapy
 
-from locations.categories import Categories, Fuel, FuelCards, PaymentMethods, apply_category, apply_yes_no, map_payment
+from locations.categories import (
+    Access,
+    Categories,
+    Extras,
+    Fuel,
+    FuelCards,
+    PaymentMethods,
+    apply_category,
+    apply_yes_no,
+    map_payment,
+)
 from locations.dict_parser import DictParser
 
 AVIA_SHARED_ATTRIBUTES = {
@@ -23,6 +33,32 @@ FUEL_TYPES_MAPPING = {
     "Super Plus": Fuel.OCTANE_98,
 }
 
+SERVICES_MAPPING = {
+    "Anhängervermietung": None,
+    "Backshop": None,
+    "Bistro": Extras.FAST_FOOD,
+    "Dusche": Extras.SHOWERS,
+    "Erdgas": None,
+    "Geldautomat": Extras.ATM,
+    "Getränkemarkt": None,
+    "LKW-Hochleistungssäule": Access.HGV,
+    "LOTTO": None,
+    "Portalwaschanlage": Extras.CAR_WASH,
+    "Prima Bistro": Extras.FAST_FOOD,
+    "Reinigungsannahme": None,
+    "SB-Sauger": Extras.VACUUM_CLEANER,
+    "SB-Waschbox": None,
+    "SB-Öltheke": None,
+    "Segafredo Kaffee": None,
+    "Shop": None,
+    "Stromladesäule": Fuel.ELECTRIC,
+    "Tankautomat": "automated",
+    "TÜV / AU": None,
+    "Unbemannte Automatenstation": None,
+    "Waschstrasse": Extras.CAR_WASH,
+    "Werkstatt": None,
+}
+
 
 class AviaDESpider(scrapy.Spider):
     name = "avia_de"
@@ -36,6 +72,9 @@ class AviaDESpider(scrapy.Spider):
     }
 
     def parse(self, response):
+        def dict_to_list(d):
+            return d.values() if d else []
+
         for poi in response.json():
             poi.update(poi.pop("addressData"))
             poi.update(poi.pop("contactData"))
@@ -43,23 +82,30 @@ class AviaDESpider(scrapy.Spider):
             item = DictParser.parse(poi)
             item.update(self.BRANDS_MAPPING.get(poi.get("facilityTitle"), {}))
 
-            if payment := poi.get("optionalData", {}).get("paymentMethods", {}):
-                for paymentMethod in payment.values():
-                    if not map_payment(item, paymentMethod, PaymentMethods):
-                        self.crawler.stats.inc_value(f"atp/avia_de/payment/fail/{paymentMethod}")
+            payments = dict_to_list(poi.get("optionalData", {}).get("paymentMethods"))
+            fuel_cards = dict_to_list(poi.get("optionalData", {}).get("fuelCards", {}))
+            gas_types = dict_to_list(poi.get("optionalData", {}).get("gasTypes", {}))
+            services = dict_to_list(poi.get("optionalData", {}).get("services", {}))
 
-            if fuel_cards := poi.get("optionalData", {}).get("fuelCards", {}):
-                for fuelCard in fuel_cards.values():
-                    if not map_payment(item, fuelCard, FuelCards):
-                        self.crawler.stats.inc_value(f"atp/avia_de/fuelCard/fail/{fuelCard}")
+            for payment in payments:
+                if not map_payment(item, payment, PaymentMethods):
+                    self.crawler.stats.inc_value(f"atp/avia_de/payment/fail/{payment}")
 
-            for fuel in poi.get("optionalData", {}).get("fuelTypes", []):
-                if tag := FUEL_TYPES_MAPPING.get(fuel):
-                    apply_yes_no(tag, item, True)
-                else:
-                    self.crawler.stats.inc_value(f"atp/avia_de/fuel/fail/{fuel}")
+            for fuel_card in fuel_cards:
+                if not map_payment(item, fuel_card, FuelCards):
+                    self.crawler.stats.inc_value(f"atp/avia_de/fuelCard/fail/{fuel_card}")
+
+            self.parse_attribute(item, gas_types, "gasTypes", FUEL_TYPES_MAPPING)
+            self.parse_attribute(item, services, "services", SERVICES_MAPPING)
 
             apply_category(Categories.FUEL_STATION, item)
 
             # TODO: Opening hours
             yield item
+
+    def parse_attribute(self, item, values: list, attribute_name, mapping: dict):
+        for value in values:
+            if tag := mapping.get(value):
+                apply_yes_no(tag, item, True)
+            else:
+                self.crawler.stats.inc_value(f"atp/avia_de/{attribute_name}/fail/{value}")
