706f410a6fb6efe960a3cf41ad4c8c87382ed949
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-09 06:55:22 +0000

    Add second cup

---
+++ locations/spiders/second_cup_ca.py
@@ -0,0 +1,26 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import SitemapSpider, Rule
+
+from locations.hours import DAYS_FR
+from locations.categories import Categories
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class SecondCupCASpider(SitemapSpider, StructuredDataSpider):
+    name = "second_cup_ca"
+    item_attributes = {
+        "brand": "Second Cup",
+        "brand_wikidata": "Q862180",
+    }
+    sitemap_urls = ["https://secondcup.com/sitemap_index.xml"]
+    sitemap_rules = [
+        (r"^https://secondcup\.com/location/.*/$", "parse_sd"),
+    ]
+    # wanted_types = ["Restaurant"]
+    # time_format = "%I:%M %p"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if "openingHours" in ld_data:
+            item["opening_hours"].add_ranges_from_string(ld_data["openingHours"], days=DAYS_FR)
+
+        return super().post_process_item(item, response, ld_data, **kwargs)
