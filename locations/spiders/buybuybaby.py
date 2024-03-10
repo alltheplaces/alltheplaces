a02e6fe45f28be2793b4fb44ab0cfe40709b9655
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-10 04:48:50 +0000

    Fix

--- locations/spiders/buybuybaby.py
+++ locations/spiders/buybuybaby.py
@@ -1,12 +1,7 @@
-import json
-import re
-
-import scrapy
-
 from locations.storefinders.stockist import StockistSpider
+
 
 class BuyBuyBabySpider(StockistSpider):
     name = "buybuybaby"
     item_attributes = {"brand": "Buy Buy Baby", "brand_wikidata": "Q5003352"}
     key = "u21140"
-
