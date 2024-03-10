40351c0dec012c525b1646f5b8bdf27b03747abf
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-10 07:35:44 +0000

    Ignore robots

--- locations/spiders/tractorsupply.py
+++ locations/spiders/tractorsupply.py
@@ -23,6 +23,7 @@ class TractorSupplySpider(scrapy.Spider):
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "ROBOTSTXT_OBEY": False,
     }
 
     def start_requests(self):
