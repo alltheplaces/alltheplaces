6d62f80d10292e25476edb66ec2556ad97dd9d40
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-10 09:07:02 +0000

    Fix spider

--- locations/spiders/simply_self_storage.py
+++ locations/spiders/simply_self_storage.py
@@ -5,31 +5,17 @@ import scrapy
 from locations.hours import OpeningHours
 from locations.linked_data_parser import LinkedDataParser
 from locations.microdata_parser import MicrodataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class SimplySelfStorageSpider(scrapy.spiders.SitemapSpider):
+class SimplySelfStorageSpider(scrapy.spiders.SitemapSpider, StructuredDataSpider):
     name = "simply_self_storage"
-    item_attributes = {"brand": "Simply Self Storage"}
-    allowed_domains = ["simplyss.com"]
-    sitemap_urls = ["https://www.simplyss.com/robots.txt"]
+    item_attributes = {"brand": "Public Storage"}
+    allowed_domains = ["www.publicstorage.com"]
+    sitemap_urls = ["https://www.publicstorage.com/sitemap_index.xml"]
 
     sitemap_rules = [
-        (r"-self-storage/$", "parse"),
+        (r"https://www.publicstorage.com/self-storage-.*\.html", "parse_sd"),
     ]
+    wanted_types = ["SelfStorage"]
 
-    def parse(self, response):
-        item = LinkedDataParser.parse(response, "SelfStorage")
-        item["ref"] = re.search(r"location_id=(\d+)", response.text)[1]
-
-        response.selector.root.set("itemscope")
-        response.selector.root.set("itemtype", "http://schema.org/Thing")
-        ld = MicrodataParser.convert_to_graph(MicrodataParser.extract_microdata(response))
-        oh = OpeningHours()
-        oh.from_linked_data(ld, "%I:%M %p")
-        item["opening_hours"] = oh.as_opening_hours()
-
-        if isinstance(ld["telephone"], list):
-            item["phone"] = ";".join(ld["telephone"])
-        else:
-            item["phone"] = ld["telephone"]
-        yield item
