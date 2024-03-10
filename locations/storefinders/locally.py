c2b938aef6e48ec048be026b7663ff1f1ac2a150
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-10 03:58:59 +0000

    Refactor and add Columbia US

--- locations/storefinders/locally.py
+++ locations/storefinders/locally.py
@@ -7,7 +7,7 @@ from locations.hours import DAYS_FULL, OpeningHours
 class LocallySpider(scrapy.Spider):
     allowed_domains = []
     start_urls = []
-    api_key = None # Later, will refactor to this
+    api_key = None  # Later, will refactor to this
     custom_settings = {"ROBOTSTXT_OBEY": False}
     # "https://www.locally.com/stores/conversion_data?has_data=true&company_id=1682&store_mode=&style=&color=&upc=&category=&inline=1&show_links_in_list=&parent_domain=&map_center_lat=31.945163222857545&map_center_lng=-96.352774663203&map_distance_diag=2692.1535387671056&sort_by=proximity&no_variants=0&only_retailer_id=&dealers_company_id=&only_store_id=false&uses_alt_coords=false&q=false&zoom_level=5"
     # "https://crocs.locally.com/stores/conversion_data?has_data=true&company_id=1762&category=Store&inline=1&map_center_lat=46.661219&map_center_lng=2.587603&map_distance_diag=3000&sort_by=proximity&lang=en-gb",
