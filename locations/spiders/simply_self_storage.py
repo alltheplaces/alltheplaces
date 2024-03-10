45d5340002e165b39c293fc83476f4037197596b
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-10 09:09:11 +0000

    Add wikidata

--- locations/spiders/simply_self_storage.py
+++ locations/spiders/simply_self_storage.py
@@ -5,7 +5,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class SimplySelfStorageSpider(scrapy.spiders.SitemapSpider, StructuredDataSpider):
     name = "simply_self_storage"
-    item_attributes = {"brand": "Public Storage"}
+    item_attributes = {"brand": "Public Storage", "brand_wikidata": "Q28228611"}
     allowed_domains = ["www.publicstorage.com"]
     sitemap_urls = ["https://www.publicstorage.com/sitemap_index.xml"]
 
