60708eff5d6db636bbaaadf5eb53df496c4f1e51
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-10 05:14:18 +0000

    Fix Spider

--- locations/spiders/premierurgentcare.py
+++ locations/spiders/premierurgentcare.py
@@ -1,6 +1,5 @@
-import re
-
 from scrapy.spiders import SitemapSpider
+
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -9,9 +8,7 @@ class PremierurgentcareSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Premier Urgent Care"}
     allowed_domains = ["www.premier.care"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
-    sitemap_urls = [
-        "https://www.premier.care/sitemap.xml"
-    ]
+    sitemap_urls = ["https://www.premier.care/sitemap.xml"]
     # Example:     # https://www.premier.care/locations/thumbs-up-temple/
     sitemap_rules = [(r"/locations/[\w-]+/$", "parse_sd")]
     wanted_types = ["MedicalOrganization"]
