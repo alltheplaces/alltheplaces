7542b37d10f1f8f8f00532c7277bf0a6a9d79a97
Sascha Brawer <sascha@brawer.ch>
2024-03-12 10:38:43 +0100

    Add benches operated by the City of Zurich

--- locations/spiders/stadt_zuerich_ch.py
+++ locations/spiders/stadt_zuerich_ch.py
@@ -55,6 +55,7 @@ class StadtZuerichCHSpider(scrapy.Spider):
         url_pattern % ("Schulanlagen", "poi_kinderhort_view"),
         url_pattern % ("Schulanlagen", "poi_sonderschule_view"),
         url_pattern % ("Schulanlagen", "poi_volksschule_view"),
+        url_pattern % ("Sitzbankkataster_OGD", "bankstandorte_ogd"),
         url_pattern % ("Stadtpolizei", "poi_stadtpolizei_view"),
         url_pattern % ("Zueri_WC", "poi_zueriwc_rs_view"),
         url_pattern % ("Zweiradparkierung", "zweiradabstellplaetze_p"),
@@ -89,6 +90,7 @@ class StadtZuerichCHSpider(scrapy.Spider):
         # The type-specific parsers should come after the general ones,
         # so they can overwrite tags.
         if parser := {
+            "bankstandorte_ogd": self.parse_bench,
             "ewz_brennstelle_p": self.parse_street_lamp,
             "poi_kindergarten_view": self.parse_kindergarten,
             "poi_kinderhort_view": self.parse_after_school,
@@ -139,7 +141,14 @@ class StadtZuerichCHSpider(scrapy.Spider):
                 "nohousenumber": "yes" if not housenumber else None,
             }
         if addr := p.get("adresse"):
-            return {"addr:full": f"{addr}, Zürich"}
+            if m := re.match(r"^(.+) (\d+\s?[a-kA-K]?)$", addr):
+                return {
+                    "addr:street": m.group(1),
+                    "addr:housenumber": m.group(2),
+                    "addr:postcode": p.get("plz"),
+                }
+            else:
+                return {"addr:full": f"{addr}, Zürich"}
         return {}
 
     def parse_after_school(self, p):
@@ -161,6 +170,35 @@ class StadtZuerichCHSpider(scrapy.Spider):
             "name": name.split("(")[0].strip().removesuffix(" MoT"),
             "opening_hours": oh.as_opening_hours(),
         }
+
+    bench_models = {
+        "mit Rückenlehne": {"backrest": "yes"},
+        "mit Rückenlehne und 2 Armlehnen": {
+            "armrest": "yes",
+            "backrest": "yes",
+        },
+        "ohne Rückenlehne": {"backrest": "no"},
+        "mit Rückenlehne und Armlehne links": {
+            "armrest": "left",
+            "backrest": "yes",
+        },
+        "mit Rückenlehne und Armlehne rechts": {
+            "armrest": "right",
+            "backrest": "yes",
+        },
+        "Tisch-Bank-Kombination": {"picnic_table": "yes"},
+        "Tisch": {"picnic_table": "yes"},
+    }
+
+    def parse_bench(self, p):
+        operator, operator_wikidata = self.operators["Grün Stadt Zürich"]
+        tags = {
+            "amenity": "bench",
+            "operator": operator,
+            "operator:wikidata": operator_wikidata,
+        }
+        tags.update(self.bench_models.get(p.get("sitzbankmodelle"), {}))
+        return tags
 
     def parse_bicycle_parking(self, p):
         # For bicycle and motorcycle parkings, the data feed puts the
