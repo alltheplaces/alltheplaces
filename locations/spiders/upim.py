7b21ecc8d5061265bc0816214f4a8e621df5c580
Cj Malone <CjMalone@mail.com>
2024-03-12 16:35:22 +0000

    Brand Blukids POIs

--- locations/spiders/upim.py
+++ locations/spiders/upim.py
@@ -4,12 +4,14 @@ import chompjs
 from scrapy import Spider
 from scrapy.http import Response
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 
 class UpimSpider(Spider):
     name = "upim"
     UPIM = {"brand": "Upim", "brand_wikidata": "Q1414836"}
+    BLUKIDS = {"brand": "Blukids", "name": "Blukids"}
     start_urls = ["https://stores.upim.com/js_db/locations-1.js"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
@@ -28,7 +30,11 @@ class UpimSpider(Spider):
             item["phone"] = location["phone"]
 
             if item["name"].upper().startswith("UPIM"):
-                item.update(self.UPIM)
                 item["branch"] = item.pop("name").split(" ", 1)[1]
+                item.update(self.UPIM)
+            elif item["name"].upper().startswith("BLUKIDS"):
+                item["branch"] = item.pop("name").split(" ", 1)[1].removesuffix(" Bk")
+                item.update(self.BLUKIDS)
+                apply_category(Categories.SHOP_CLOTHES, item)
 
             yield item
