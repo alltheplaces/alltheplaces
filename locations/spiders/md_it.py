4a9bc1468363d1e29ea9778f00248071ce9b5b42
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-13 09:43:30 +0000

    Add spider MD (IT)

---
+++ locations/spiders/md_it.py
@@ -0,0 +1,11 @@
+from locations.storefinders.woosmap import WoosmapSpider
+
+
+class MDITSpider(WoosmapSpider):
+    name = "md_it"
+    item_attributes = {
+        "brand_wikidata": "Q3841263",
+        "brand": "MD",
+    }
+    key = "woos-5a59cdb1-7aa1-3545-86dc-55b3527563b2"
+    origin = "https://www.mdspa.it"
