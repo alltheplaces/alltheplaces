93f8b22a10d15d635fd1a97bac82576144e6ad2c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-13 21:09:06 +0000

    Add Mercator (SL)

--- locations/spiders/mercator_sl.py
+++ locations/spiders/mercator_sl.py
@@ -7,7 +7,10 @@ class MercatorSLSpider(SitemapSpider, StructuredDataSpider):
     name = "mercator_sl"
     item_attributes = {"brand": "Mercator", "brand_wikidata": "Q738412"}
     allowed_domains = ["www.mercator.si"]
-    sitemap_urls = ["https://www.mercator.si/sitemap.xml/sitemap/Store/1", "https://www.mercator.si/sitemap.xml/sitemap/Store/2"]
+    sitemap_urls = [
+        "https://www.mercator.si/sitemap.xml/sitemap/Store/1",
+        "https://www.mercator.si/sitemap.xml/sitemap/Store/2",
+    ]
     sitemap_rules = [
         (r"^https:\/\/www\.mercator\.si\/prodajna-.*/.*/$", "parse_sd"),
     ]
