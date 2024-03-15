b1ee92ed4b90e3f7ffa5ab96de9b6f29e41686db
Cj Malone <CjMalone@mail.com>
2024-03-15 14:38:05 +0000

    [McDonalds FR] Collect more attributes

--- locations/spiders/mcdonalds_fr.py
+++ locations/spiders/mcdonalds_fr.py
@@ -10,8 +10,19 @@ class McDonaldsFRSpider(WoosmapSpider):
     origin = "https://www.mcdonalds.fr"
 
     def parse_item(self, item, feature, **kwargs):
-        item["website"] = (
-            f'https://www.mcdonalds.fr/restaurants{feature["properties"]["contact"]["website"]}/{feature["properties"]["store_id"]}'
-        )
+        item[
+            "website"
+        ] = f'https://www.mcdonalds.fr/restaurants{feature["properties"]["contact"]["website"]}/{feature["properties"]["store_id"]}'
+        item["postcode"] = feature["properties"]["user_properties"]["displayPostCode"]
+        item["extras"]["check_date"] = feature["properties"]["last_updated"]
+        item["branch"] = item.pop("name").title()
+
+        apply_yes_no(Extras.DELIVERY, item, "mcdelivery" in feature["properties"]["tags"])
         apply_yes_no(Extras.DRIVE_THROUGH, item, "mcdrive" in feature["properties"]["tags"])
+        apply_yes_no(Extras.INDOOR_SEATING, item, "table-service" in feature["properties"]["tags"])
+        apply_yes_no(Extras.TAKEAWAY, item, "take-away" in feature["properties"]["tags"])
+        apply_yes_no(Extras.SELF_CHECKOUT, item, "terminal" in feature["properties"]["tags"])
+        apply_yes_no(Extras.OUTDOOR_SEATING, item, "terrace" in feature["properties"]["tags"])
+        apply_yes_no(Extras.WIFI, item, "wireless" in feature["properties"]["tags"])
+
         yield item
