22c0786cc1c9430cdda4ddc378fa6e7c75b2ad76
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-16 03:18:18 +0000

    Fix spiders

--- locations/spiders/pizza_hut_fr.py
+++ locations/spiders/pizza_hut_fr.py
@@ -1,7 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
-from locations.spiders.vapestore_gb import clean_address
+from locations.pipelines.address_clean_up import merge_address_lines
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -13,7 +13,7 @@ class PizzaHutFR(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [(r"https:\/\/www\.pizzahut\.fr\/huts\/[-\w]+\/([-.\w]+)\/$", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["street_address"] = clean_address(item["street_address"])
+        item["street_address"] = merge_address_lines(item["street_address"])
 
         if item["website"].startswith("https://www.pizzahut.fr/huts/"):
             item.update(self.PIZZA_HUT_DELIVERY)
