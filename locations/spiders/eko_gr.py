add59fdf2956b7e4944290f75a3a0b55691664ec
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-17 04:12:08 +0000

    Add initial Lighthouse storefinder and EKO (GR)

---
+++ locations/spiders/eko_gr.py
@@ -0,0 +1,8 @@
+from locations.storefinders.lighthouse import LighthouseSpider
+from locations.categories import Categories
+
+class EkoGRSpider(LighthouseSpider):
+    name = "eko_gr"
+    item_attributes = {"brand": "EKO", "brand_wikidata": "Q31283948", "extras": Categories.FUEL_STATION.value}
+    allowed_domains = ["www.eko.gr"]
+    start_urls = ["https://www.eko.gr/pratiria/katastimata/"]
