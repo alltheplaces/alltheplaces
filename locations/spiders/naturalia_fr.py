6008945040da1681ee86588427fbc373298b3ae1
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-17 01:46:26 +0000

    Add Naturalia (FR)

--- locations/spiders/naturalia_fr.py
+++ locations/spiders/naturalia_fr.py
@@ -8,4 +8,3 @@ class NaturaliaFRSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Naturalia", "brand_wikidata": "Q3337081"}
     sitemap_urls = ["https://magasins.naturalia.fr/sitemap.xml"]
     sitemap_rules = [(r"https://magasins.naturalia.fr/naturalia/fr/store/.*", "parse_sd")]
-
