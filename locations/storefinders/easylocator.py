1a3cd5a0465770e0c9c98a8ed5dfc4bf5875225a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-17 07:17:16 +0000

    Add Easylocator and Gigi's Cupcakes

--- locations/storefinders/easylocator.py
+++ locations/storefinders/easylocator.py
@@ -2,7 +2,7 @@ from scrapy import Spider
 from scrapy.http import JsonRequest
 
 from locations.dict_parser import DictParser
-
+from locations.geo import point_locations
 
 # To use this spider, either specify the start_urls or
 # pass in a coordinates file.
