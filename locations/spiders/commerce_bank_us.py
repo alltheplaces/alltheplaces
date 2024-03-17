e8123871f66e97abca4f9fa311ae1e2d04b2976e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-17 07:36:55 +0000

    Add Commerce Bank (US)

--- locations/spiders/commerce_bank_us.py
+++ locations/spiders/commerce_bank_us.py
@@ -1,12 +1,14 @@
-from locations.storefinders.rio_seo_spider import RioSeoSpider
 from locations.categories import Categories
+from locations.storefinders.rio_seo_spider import RioSeoSpider
 
 
 class CommerceBankUSSpider(RioSeoSpider):
     name = "commerce_bank_us"
     item_attributes = {"brand": "Commerce Bank", "brand_wikidata": "Q5152411"}
     allowed_domains = ["locations.commercebank.com"]
-    start_urls = ["https://maps.locations.commercebank.com/api/getAsyncLocations?template=domain&level=domain&radius=10000&limit=3000"]
+    start_urls = [
+        "https://maps.locations.commercebank.com/api/getAsyncLocations?template=domain&level=domain&radius=10000&limit=3000"
+    ]
 
     def post_process_feature(self, feature, location):
         # TODO: Is name reliable?
