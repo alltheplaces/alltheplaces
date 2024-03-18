1467e5150f6d798d070435a48469f7e1a7423e84
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-03-18 20:41:48 +0000

    [Liberty AU] Sync brand string with NSI (#7971)

--- locations/spiders/liberty_au.py
+++ locations/spiders/liberty_au.py
@@ -8,13 +8,14 @@ from locations.hours import OpeningHours
 
 class LibertyAUSpider(scrapy.Spider):
     name = "liberty_au"
-    item_attributes = {"brand": "Liberty Oil", "brand_wikidata": "Q106687078"}
+    item_attributes = {"brand": "Liberty", "brand_wikidata": "Q106687078"}
     allowed_domains = ["libertyconvenience.com.au"]
     start_urls = ["https://www.libertyconvenience.com.au/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
 
     def parse(self, response):
         for location in response.json():
             item = DictParser.parse(location)
+            item["branch"] = location["store"]
             item["street_address"] = item.pop("addr_full", None)
             hours_string = " ".join(filter(None, Selector(text=location["hours"]).xpath("//text()").getall()))
             item["opening_hours"] = OpeningHours()
