c3a006535bfb6f672d3248525065bed295db4a96
David Hicks <david@hicks.id.au>
2024-03-20 21:55:08 +1100

    peavey_mart_ca: use api_filter parameter to filter out non-stores

--- locations/spiders/peavey_mart_ca.py
+++ locations/spiders/peavey_mart_ca.py
@@ -12,10 +12,8 @@ class PeaveyMartCASpider(KiboSpider):
     }
     start_urls = ["https://www.peaveymart.com/api/commerce/storefront/locationUsageTypes/SP/locations"]
     user_agent = BROWSER_DEFAULT
+    api_filter = "locationType.Code eq STORE"
 
     def parse_item(self, item, location):
-        location_types = [location_type["code"] for location_type in location["locationTypes"]]
-        if "STORE" not in location_types:
-            return
         item["website"] = "https://www.peaveymart.com/store-details?locationCode=" + item["ref"]
         yield item
