883da7b27b817174fdabbb0ab70766fa5c7dcae1
David Hicks <david@hicks.id.au>
2024-03-20 20:01:26 +1100

    Add Peavey Mart (CA) (94 locations)
    Fixes #7503

---
+++ locations/spiders/peavey_mart_ca.py
@@ -0,0 +1,18 @@
+from locations.storefinders.kibo import KiboSpider
+from locations.user_agents import BROWSER_DEFAULT
+
+class PeaveyMartCASpider(KiboSpider):
+    name = "peavey_mart_ca"
+    item_attributes = {
+        "brand": "Peavey Mart",
+        "brand_wikidata": "Q7158483",
+    }
+    start_urls = ["https://www.peaveymart.com/api/commerce/storefront/locationUsageTypes/SP/locations"]
+    user_agent = BROWSER_DEFAULT
+
+    def parse_item(self, item, location):
+        location_types = [location_type["code"] for location_type in location["locationTypes"]]
+        if "STORE" not in location_types:
+            return
+        item["website"] = "https://www.peaveymart.com/store-details?locationCode=" + item["ref"]
+        yield item
