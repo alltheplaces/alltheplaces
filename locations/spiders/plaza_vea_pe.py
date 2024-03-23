e49a786dabd60f8f694cba29cdebedabf034aeb2
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-23 02:29:42 +0000

    Add Vea Plaza (PE)

--- locations/spiders/plaza_vea_pe.py
+++ locations/spiders/plaza_vea_pe.py
@@ -13,17 +13,17 @@ class PlazaVeaPESpider(Spider):
         for location in response.json():
             # This doesn't pick up much at the moment, but I'm assuming it may turn multi lingual down the track
             item = DictParser.parse(location)
-            item['ref'] = location['TIE_CODIGO']
-            item['name'] = location['TIE_NOMBRE']
-            item['lat'] = location['TIE_GMAP_LONGITUD']
-            item['lon'] = location['TIE_GMAP_LATITUD']
-            item['phone'] = location['TIE_TELEFONO']
-            item['street_address'] = location['TIE_DIRECCION']
-            item['opening_hours'] = OpeningHours()
+            item["ref"] = location["TIE_CODIGO"]
+            item["name"] = location["TIE_NOMBRE"]
+            item["lat"] = location["TIE_GMAP_LONGITUD"]
+            item["lon"] = location["TIE_GMAP_LATITUD"]
+            item["phone"] = location["TIE_TELEFONO"]
+            item["street_address"] = location["TIE_DIRECCION"]
+            item["opening_hours"] = OpeningHours()
 
             # 'TIE_HORARIO': 'L-D 08:00 - 22:00 hrs. '
-            cleaned_hours = location['TIE_HORARIO'].replace("L", "Lunes").replace("D", "Domingo")
-            item['opening_hours'].add_ranges_from_string(cleaned_hours, DAYS_ES)
+            cleaned_hours = location["TIE_HORARIO"].replace("L", "Lunes").replace("D", "Domingo")
+            item["opening_hours"].add_ranges_from_string(cleaned_hours, DAYS_ES)
 
             # Some locations include services:
             # "TIE_SERVICIOS": "Banco Interbank, Farmacia Inkafarma y óptica Econolentes",
