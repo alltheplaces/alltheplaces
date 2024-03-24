3f695bfff01f0b2eda0de80d708b8c4859e31a92
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-25 00:53:16 +1030

    Add Cobasi (BR) (#8045)
    * Add Cobasi
    
    * Add Cobasi

---
+++ locations/spiders/cobasi_br.py
@@ -0,0 +1,23 @@
+from scrapy import Spider
+
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+
+
+class CobasiBR(Spider):
+    name = "cobasi_br"
+    item_attributes = {"brand_wikidata": "Q86739236"}
+    start_urls = ["https://mid-back.cobasi.com.br/stores"]
+
+    def parse(self, response):
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["ref"] = location["_id"]
+            # 'shopBusinessHours': {'weekdays': '10h00 às 21h45 ', 'saturday': '10h00 às 21h45 ', 'sunday': '12h00 às 21h00', 'holiday': '12h00 às 21h00'},
+            item["opening_hours"] = OpeningHours()
+            for days, hours in location["shopBusinessHours"].items():
+                item["opening_hours"].add_ranges_from_string(
+                    days.replace("weekdays", "Mo-Fr") + " " + hours.replace(" às ", "-").replace("h", ":"),
+                )
+
+            yield item
