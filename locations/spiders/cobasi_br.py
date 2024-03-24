e6783432c905b21a6373007f073af93b18406b3c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-24 13:21:08 +0000

    Add Cobasi

--- locations/spiders/cobasi_br.py
+++ locations/spiders/cobasi_br.py
@@ -1,5 +1,4 @@
 from scrapy import Spider
-from scrapy.http import JsonRequest
 
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
@@ -13,7 +12,7 @@ class CobasiBR(Spider):
     def parse(self, response):
         for location in response.json():
             item = DictParser.parse(location)
-            item['ref'] = location["_id"]
+            item["ref"] = location["_id"]
             # 'shopBusinessHours': {'weekdays': '10h00 às 21h45 ', 'saturday': '10h00 às 21h45 ', 'sunday': '12h00 às 21h00', 'holiday': '12h00 às 21h00'},
             item["opening_hours"] = OpeningHours()
             for days, hours in location["shopBusinessHours"].items():
