64a5aee1a57149f8b194534136b0067f87a626a3
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-24 12:16:45 +0000

    Add spider

---
+++ locations/spiders/la_anonima_ar.py
@@ -0,0 +1,21 @@
+import json
+
+from scrapy import Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+
+
+class LaAnonimaAR(Spider):
+    name = 'la_anonima_ar'
+    start_urls = ["https://www.laanonima.com.ar/contents/themes/evolucionamos/bin/get_all_sucursales.php"]
+    item_attributes = {"brand_wikidata": "Q6135985"}
+
+    def parse(self, response):
+        for location in response.json():
+            item = DictParser.parse(location)
+            item['phone'] = location['telefono']
+            item['addr_full'] = location['direccion']
+            item['name'] = location['nombre']
+            item['website'] = "https://www.laanonima.com.ar/" + location['url']
+            yield item
