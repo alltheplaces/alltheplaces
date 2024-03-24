7851f9011ffa60874cee26909e46b7238737d0b5
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-24 13:35:26 +0000

    Add Mix Markt (GR)

--- locations/spiders/mix_markt_gr.py
+++ locations/spiders/mix_markt_gr.py
@@ -1,5 +1,5 @@
-from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 from locations.hours import DAYS_GR
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
 class MixMarktGRSpider(WPStoreLocatorSpider):
