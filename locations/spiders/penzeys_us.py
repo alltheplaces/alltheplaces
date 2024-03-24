1ddd5011fa16d7bb1e9862d2147ae9d6ea79261a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-24 12:57:47 +0000

    Add spider

--- locations/spiders/penzeys_us.py
+++ locations/spiders/penzeys_us.py
@@ -13,19 +13,19 @@ class PenzeysUS(Spider):
         url = "https://www.penzeys.com/api/GetLocations"
         headers = {
             "Referer": "https://www.penzeys.com/locations/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         }
         yield JsonRequest(url=url, headers=headers, callback=self.parse)
 
-
     def parse(self, response):
-        for location in response.json()['Locations']:
+        for location in response.json()["Locations"]:
             item = DictParser.parse(location)
-            item['state'] = location['State']['Name']
-            item['opening_hours'] = OpeningHours()
+            item["state"] = location["State"]["Name"]
+            item["opening_hours"] = OpeningHours()
             if "HoursOfOperation" in location:
                 for hours in location["HoursOfOperation"]:
-                    
-                    item['opening_hours'].add_ranges_from_string(hours['daysOfWeek'] + " " + hours["timeOpen"] + "-" + hours["timeClose"])
+                    item["opening_hours"].add_ranges_from_string(
+                        hours["daysOfWeek"] + " " + hours["timeOpen"] + "-" + hours["timeClose"]
+                    )
 
             yield item
