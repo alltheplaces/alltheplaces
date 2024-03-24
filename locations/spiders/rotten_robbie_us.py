a8fe6e2411e7254b4a6875927e7f71fb35bc8138
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-24 05:19:13 +0000

    Refactor

--- locations/spiders/rotten_robbie_us.py
+++ locations/spiders/rotten_robbie_us.py
@@ -5,6 +5,5 @@ from locations.storefinders.wp_go_maps import WpGoMapsSpider
 class RottenRobbieUSSpider(WpGoMapsSpider):
     name = "rotten_robbie_us"
     item_attributes = {"brand": "Rotten Robbie", "brand_wikidata": "Q87378070", "extras": Categories.FUEL_STATION.value}
-    start_urls = [
-        "https://www.rottenrobbie.com/wp-json/wpgmza/v1/features/base64eJyrVkrLzClJLVKyUqqOUcpNLIjPTIlRsopRMoxRqlWqBQCnUQoG"
-    ]
+    allowed_domains = ["www.rottenrobbie.com"]
+    map_id = 1
