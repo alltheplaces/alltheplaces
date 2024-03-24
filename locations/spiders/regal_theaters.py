1b651b0d00d64c8e584449d5b4041dd3cb95f6d1
Ian Dees <ian.dees@gmail.com>
2024-03-23 19:58:17 -0500

    Use a different user agent

--- locations/spiders/regal_theaters.py
+++ locations/spiders/regal_theaters.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class RegalTheatersSpider(SitemapSpider, StructuredDataSpider):
@@ -8,3 +9,4 @@ class RegalTheatersSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Regal Theaters", "brand_wikidata": "Q835638"}
     sitemap_urls = ["https://www.regmovies.com/sitemap.xml"]
     sitemap_rules = [("/theatres/", "parse_sd")]
+    user_agent = BROWSER_DEFAULT
