6d7c973a0bc9957d8aedd084560caaac7b4d282b
Karen Kerr <karenkerrgithub@gmail.com>
2024-03-28 13:43:30 +0000

    [SparNO] Add category

--- locations/spiders/spar_no.py
+++ locations/spiders/spar_no.py
@@ -1,8 +1,13 @@
+from locations.categories import Categories
 from locations.storefinders.sylinder import SylinderSpider
 
 
 class SparNoSpider(SylinderSpider):
     name = "spar_no"
-    item_attributes = {"brand": "Spar", "brand_wikidata": "Q610492"}
+    item_attributes = {
+        "brand": "Spar",
+        "brand_wikidata": "Q610492",
+        "extras": Categories.SHOP_SUPERMARKET.value,
+    }
     app_key = "1210"
     base_url = "https://spar.no/Finn-butikk/"
