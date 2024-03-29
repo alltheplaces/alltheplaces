e097e3abaa9b2215a150550f2a108eb9d2e77391
David Hicks <david@hicks.id.au>
2024-03-30 00:05:43 +1100

    Add Shoprite (ZA)
    Fixes #8069

--- locations/spiders/shoprite_za.py
+++ locations/spiders/shoprite_za.py
@@ -1,17 +1,18 @@
 from scrapy import Request, Selector
 
-from locations.categories import Categories, apply_category
+from locations.categories import Categories
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
 
-# Shoprite Holdings owns the Checkers brand, as well as another brand,
-# Shoprite. Refer to shoprite_za for an almost identical spider
-# addressing the Shoprite brand.
+# Shoprite Holdings owns the Shoprite brand, as well as another brand,
+# Checkers. Refer to checkers_za for an almost identical spider
+# addressing the Checkers brand.
 
-class CheckersZASpider(StructuredDataSpider):
-    name = "checkers_za"
-    start_urls = ["https://www.checkers.co.za/sitemap/medias/Store-checkersZA-0.xml"]
-    sitemap_rules = [(r"s^http:\/\/www\.checkers\.co\.za\/store\/\d+$", "parse_sd")]
+class ShopriteZASpider(StructuredDataSpider):
+    name = "shoprite_za"
+    item_attributes = {"brand": "Shoprite", "brand_wikidata": "Q1857639", "extras": Categories.SHOP_SUPERMARKET.value}
+    start_urls = ["https://www.shoprite.co.za/sitemap/medias/Store-shopriteZA-0.xml"]
+    sitemap_rules = [(r"s^http:\/\/www\.shopriteza\.prod\.shopritelabs\.co\.za\/store\/\d+$", "parse_sd")]
     # AWS WAF bot protection appears to be used but can be bypassed with Playwright.
     is_playwright_spider = True
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"CONCURRENT_REQUESTS": 1, "ROBOTSTXT_OBEY": False}
@@ -36,7 +37,7 @@ class CheckersZASpider(StructuredDataSpider):
         await playwright_page.close()
         sitemap_locations = Selector(text=sitemap).xpath("//loc/text()").getall()
         for sitemap_location in sitemap_locations:
-            yield Request(url=sitemap_location.replace("http://", "https://").split("?", 1)[0], callback=self.parse_sd)
+            yield Request(url=sitemap_location.replace("shopriteza.prod.shopritelabs.co.za", "www.shoprite.co.za").replace("http://", "https://").split("?", 1)[0], callback=self.parse_sd)
 
     def pre_process_data(self, ld_data):
         # One openingHourSpecification mixes hours for a
@@ -52,20 +53,9 @@ class CheckersZASpider(StructuredDataSpider):
             ld_data["openingHoursSpecification"] = list(newohspec.values())
 
     def post_process_item(self, item, response, ld_data):
-        if "Checkers Hyper " in item["name"]:
-            item["brand"] = "Checkers Hyper"
-            item["brand_wikidata"] = "Q116518886"
-            item["name"] = item["name"].replace("Checkers Hyper ", "")
-        elif "Checkers " in item["name"]:
-            item["brand"] = "Checkers"
-            item["brand_wikidata"] = "Q5089126"
-            item["name"] = item["name"].replace("Checkers ", "")
-        else:
-            self.logger.warning("Unknown brand from store name: {}".format(item["name"]))
-            return
-        apply_category(Categories.SHOP_SUPERMARKET, item)
         item["ref"] = response.url.split("/")[-1]
-        item["website"] = response.xpath('//link[@rel="canonical"]/@href').get()
+        item["city"] = item.pop("state")
+        item["state"] = response.url.split("/")[3].replace("-", " ").replace("Kwazulu Natal", "KwaZulu-Natal")
         item.pop("facebook", None)
         item.pop("twitter", None)
         yield item
