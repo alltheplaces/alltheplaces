82e84e625f1d2077211ed3ca558d03a40ec790d6
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-30 23:40:26 +0000

    Add Strack & Van Til

--- locations/spiders/strack_and_van_til_us.py
+++ locations/spiders/strack_and_van_til_us.py
@@ -7,7 +7,7 @@ class StrackAndVanTilUSSpider(StoreLocatorPlusSelfSpider):
         "brand_wikidata": "Q17108969",
         "brand": "Strack & Van Til",
     }
-    allowed_domains = ['strackandvantil.com']
+    allowed_domains = ["strackandvantil.com"]
     searchable_points_files = ["us_centroids_iseadgg_458km_radius.csv"]
     search_radius = 500
     max_results = 100
