61955541df1859173560c89ed53e229a2f0e0d06
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-31 00:12:56 +0000

    Add New Seasons Market (US)

--- locations/spiders/new_seasons_market_us.py
+++ locations/spiders/new_seasons_market_us.py
@@ -7,5 +7,5 @@ class NewSeasonsMarketUSSpider(SitemapSpider, StructuredDataSpider):
     name = "new_seasons_market_us"
     item_attributes = {"brand": "New Seasons Market", "brand_wikidata": "Q7011463"}
     sitemap_urls = ["https://www.newseasonsmarket.com/robots.txt"]
-    sitemap_rules = [(r"/find-a-store/\w+$", "parse_sd")]
+    sitemap_rules = [(r"/find-a-store/[\w-]+$", "parse_sd")]
     wanted_types = ["GroceryStore"]
