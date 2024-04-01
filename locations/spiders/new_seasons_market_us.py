d750caf88e4bf3c63c7bf6d7f03e10f26c0630bf
Cj Malone <CjMalone@mail.com>
2024-04-01 15:31:18 +0100

    Collect branch

--- locations/spiders/new_seasons_market_us.py
+++ locations/spiders/new_seasons_market_us.py
@@ -9,3 +9,9 @@ class NewSeasonsMarketUSSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://www.newseasonsmarket.com/robots.txt"]
     sitemap_rules = [(r"/find-a-store/[\w-]+$", "parse_sd")]
     wanted_types = ["GroceryStore"]
+    search_for_facebook = False
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["branch"] = response.xpath('//h1[@class="h2"]/text()').get()
+
+        yield item
