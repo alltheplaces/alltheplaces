1a2d2c57ae9c36439cd329eaaa7ec5a83a5e5d28
David Hicks <david@hicks.id.au>
2024-04-02 01:32:40 +1100

    bluerhino: fix spider and rename to blue_rhino_pr_us
    Queries were timing out for an unlimited search radius, but a 200km
    radius search appears to be fine instead.
    
    Use "PR" ISO-3166-1 alpha 2 code for Puerto Rico locations.
    
    Fix extraction of fax numbers (where they are listed).

--- locations/spiders/blue_rhino_pr_us.py
+++ locations/spiders/blue_rhino_pr_us.py
@@ -1,21 +1,23 @@
-import scrapy
+from scrapy import Spider
+from scrapy.http import JsonRequest
 
+from locations.geo import point_locations
 from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class BlueRhinoSpider(scrapy.Spider):
-    name = "bluerhino"
+class BlueRhinoPRUSSpider(Spider):
+    name = "blue_rhino_pr_us"
     item_attributes = {
         "brand": "Blue Rhino",
         "brand_wikidata": "Q65681213",
-        "country": "US",
     }
     allowed_domains = ["bluerhino.com"]
-    start_urls = [
-        "https://bluerhino.com/api/propane/GetRetailersNearPoint?latitude=0&longitude=0&radius=10000&name=&type=&top=500000&cache=false"
-    ]
-    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Accept": "application/json"}}
+
+    def start_requests(self):
+        # The server times out at a 500km search radius, so use 200km instead.
+        for lat, lon in point_locations("pr_us_centroids_iseadgg_175km_radius.csv"):
+            yield JsonRequest(url=f"https://bluerhino.com/api/propane/GetRetailersNearPoint?latitude={lat}&longitude={lon}&radius=200&name=&type=&top=100000&cache=false")
 
     def parse(self, response):
         for row in response.json():
@@ -30,6 +32,12 @@ class BlueRhinoSpider(scrapy.Spider):
                 "postcode": row["Zip"],
                 "country": row["Country"],
                 "phone": row["Phone"],
-                "extras": {"fax": row["Fax"], "check_date": row["LastDeliveryDate"].split("T", 1)[0]},
+                "extras": {
+                    "check_date": row["LastDeliveryDate"].split("T", 1)[0]
+                },
             }
+            if properties["state"] == "PR":
+                properties["country"] = properties.pop("state")
+            if fax_number := row["Fax"].strip():
+                properties["extras"]["fax"] = fax_number
             yield Feature(**properties)
