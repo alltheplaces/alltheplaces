1af1fa37ceb9188ebc9b0078874ad2c467abf9aa
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-04-10 13:37:33 +0100

    [FiveGuys AE AT AU BE BH CH KW NL] Fix coords and urls (#8155)
    * [FiveGuys AE AT AU BE BH CH KW NL] Fix coords and urls
    
    * Clean up Five Guys Microdata branch

--- locations/spiders/five_guys_kr.py
+++ locations/spiders/five_guys_kr.py
@@ -1,10 +1,8 @@
-from scrapy.spiders import SitemapSpider
-
+from locations.spiders.five_guys_ca import FiveGuysCASpider
 from locations.spiders.five_guys_us import FiveGuysUSSpider
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class FiveGuysKRSpider(SitemapSpider, StructuredDataSpider):
+class FiveGuysKRSpider(FiveGuysCASpider):
     name = "five_guys_kr"
     item_attributes = FiveGuysUSSpider.item_attributes
     sitemap_urls = ["https://restaurants.fiveguys.co.kr/sitemap.xml"]
--- locations/spiders/five_guys_sg.py
+++ locations/spiders/five_guys_sg.py
@@ -1,11 +1,9 @@
-from scrapy.spiders import SitemapSpider
-
+from locations.spiders.five_guys_ca import FiveGuysCASpider
 from locations.spiders.five_guys_us import FiveGuysUSSpider
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class FiveGuysSGSpider(SitemapSpider, StructuredDataSpider):
-    name = "five_guys_sh"
+class FiveGuysSGSpider(FiveGuysCASpider):
+    name = "five_guys_sg"
     item_attributes = FiveGuysUSSpider.item_attributes
     sitemap_urls = ["https://restaurants.fiveguys.sg/sitemap.xml"]
     sitemap_rules = [(r"^https://restaurants\.fiveguys\.sg\/[^/]+$", "parse_sd")]
