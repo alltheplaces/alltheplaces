alltheplaces/alltheplaces

commit 1e14402677f05b46b3d19f89cc3c83cc9a974442

western_union: fix spider (#8150)
* western_union: fix spider

point_locations was treating coordinates of +/-0.0000000 as invalid
coordinates, when these are valid coordinates. Instead of using a loose
if(lat) and if(lon) check that fails for floating point values of 0,
only consider coordinates to be invalid if they can't be converted to a
floating point type.

* geo/point_locations: fix formatted strings syntax for error messages

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

locations/geo.py
@@ -65,13 +65,22 @@ def point_locations(areas_csv_file: str, area_field_filter: list[str] = None) ->
     for csv_file in areas_csv_file:
         with open_searchable_points("{}".format(csv_file)) as file:
             for row in csv.DictReader(file):
-                lat, lon = float(row["latitude"]), float(row["longitude"])
-                if not lat or not lon:
-                    raise Exception("missing lat/lon in file")
+                try:
+                    lat, lon = float(row["latitude"]), float(row["longitude"])
+                except ValueError:
+                    raise Exception(
+                        "Invalid latitude/longitude in searchable points file {} where latitude = {} and longitude = {}.".format(
+                            csv_file, row["latitude"], row["longitude"]
+                        )
+                    )
                 area = get_key(row, ["country", "territory", "state"])
                 if area_field_filter:
                     if not area:
-                        raise Exception("trying to perform area filter on file with no area support")
+                        raise Exception(
+                            "Searchable points file {} does support support area field filters (columns named 'country', 'territory' and 'state').".format(
+                                csv_file
+                            )
+                        )
                     if area not in area_field_filter:
                         continue
                 yield lat, lon

commit 7f2a19fe9f8e1dc34c302ae71da997a0907bfecd

locations/geo: fix typo in error message
1e14402677f05b46b3d19f89cc3c83cc9a974442 introduced a typo in a modified
error message string.

locations/geo.py
@@ -77,7 +77,7 @@ def point_locations(areas_csv_file: str, area_field_filter: list[str] = None) ->
                 if area_field_filter:
                     if not area:
                         raise Exception(
-                            "Searchable points file {} does support support area field filters (columns named 'country', 'territory' and 'state').".format(
+                            "Searchable points file {} does not support area field filters (columns named 'country', 'territory' and 'state').".format(
                                 csv_file
                             )
                         )