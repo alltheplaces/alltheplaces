2da88faf90db7ec13fe3727f9914779a7b2e5144
Colton Piper <pipercolton@gmail.com>
2024-04-12 11:58:59 -0400

    Added hungary to nespresso spider

--- locations/spiders/nespresso.py
+++ locations/spiders/nespresso.py
@@ -9,7 +9,7 @@ class NespressoSpider(scrapy.Spider):
     item_attributes = {"brand": "Nespresso", "brand_wikidata": "Q301301"}
 
     def start_requests(self):
-        countries = ["AT", "CA", "CH", "DE", "DK", "FR", "GB", "IT", "NL", "US", "ZA"]
+        countries = ["AT", "CA", "CH", "DE", "DK", "FR", "GB", "HU", "IT", "NL", "US", "ZA"]
 
         base_url = "https://www.nespresso.com/storelocator/app/find_poi-v4.php?country={country}&lang=EN"
 
