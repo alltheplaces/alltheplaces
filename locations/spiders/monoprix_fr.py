c72d565c1c4f806598b8f89941f5833dbd7110c8
David Hicks <david@hicks.id.au>
2024-04-13 10:13:51 +1000

    Add Monoprix (FR) (592 stores)
    Fixes #7877

---
+++ locations/spiders/monoprix_fr.py
@@ -0,0 +1,10 @@
+from locations.storefinders.woosmap import WoosmapSpider
+
+from locations.categories import Categories
+
+
+class MonoprixFRSpider(WoosmapSpider):
+    name = "monoprix_fr"
+    item_attributes = {"brand": "Monoprix", "brand_wikidata": "Q3321241", "extras": Categories.SHOP_SUPERMARKET.value}
+    key = "woos-ef21433b-45e1-3752-851f-6653279c035a"
+    origin = "https://www.monoprix.fr"
