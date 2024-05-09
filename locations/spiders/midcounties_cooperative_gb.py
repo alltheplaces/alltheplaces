46778fb1a3a158f0e9b4bf14fbdffb1bd35b489f
David Hicks <david@hicks.id.au>
2024-05-09 16:53:31 +0000

    Use clean_address to join and clean addresses (10) (#8329)
    * Use clean_address to join and clean addresses (10)
    
    Fixes #8309
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/midcounties_cooperative_gb.py
+++ locations/spiders/midcounties_cooperative_gb.py
@@ -3,6 +3,7 @@ from scrapy import Spider
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_FULL, OpeningHours
+from locations.pipelines.address_clean_up import clean_address
 
 
 class MidcountiesCooperativeGBSpider(Spider):
@@ -18,9 +19,8 @@ class MidcountiesCooperativeGBSpider(Spider):
     def parse(self, response):
         for store in response.json()["stores"]:
             item = DictParser.parse(store)
-
-            item["street_address"] = ", ".join(
-                filter(None, [store.get("addressLine1"), store.get("addressLine2"), store.get("addressLine3")])
+            item["street_address"] = clean_address(
+                [store.get("addressLine1"), store.get("addressLine2"), store.get("addressLine3")]
             )
 
             item["opening_hours"] = OpeningHours()
