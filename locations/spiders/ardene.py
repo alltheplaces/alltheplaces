2facda9d65ace5745a6f90e87f59eb700dec7ad9
David Hicks <david@hicks.id.au>
2024-05-10 00:40:53 +1000

    ardene: fix category specification

--- locations/spiders/ardene.py
+++ locations/spiders/ardene.py
@@ -4,5 +4,5 @@ from locations.storefinders.uberall import UberallSpider
 
 class ArdeneSpider(UberallSpider):
     name = "ardene"
-    item_attributes = {"brand": "Ardene", "brand_wikidata": "Q2860764", "extras": Categories.SHOP_CLOTHES}
+    item_attributes = {"brand": "Ardene", "brand_wikidata": "Q2860764", "extras": Categories.SHOP_CLOTHES.value}
     key = "APLOifTJjDLpXnd0K1bTlQmbKKM3mt"
