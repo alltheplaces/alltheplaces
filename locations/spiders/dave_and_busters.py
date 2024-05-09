4b178d8b73cd4f3015a341f05912693ee789fe13
David Hicks <david@hicks.id.au>
2024-05-10 02:11:57 +1000

    dave_and_busters: fix spider

--- locations/spiders/dave_and_busters.py
+++ locations/spiders/dave_and_busters.py
@@ -1,4 +1,7 @@
+from chompjs import parse_js_object
+
 from scrapy import Spider
+from scrapy.http import JsonRequest
 
 from locations.dict_parser import DictParser
 from locations.pipelines.address_clean_up import clean_address
@@ -9,10 +12,16 @@ class DaveAndBustersSpider(Spider):
     item_attributes = {"brand": "Dave and Busters", "brand_wikidata": "Q5228205"}
     allowed_domains = ["www.daveandbusters.com"]
     start_urls = ["https://www.daveandbusters.com/content/dnb-request/datadetails.json?mode=location"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield JsonRequest(url=url)
 
     def parse(self, response):
-        for location in response.json()["locations"]:
+        # Returned JSON object is malformed
+        for location in parse_js_object(response.text)["locations"]:
+            if location["isComingSoon"]:
+                continue
             if "COMING SOON" in location["name"].upper():
                 continue
 
