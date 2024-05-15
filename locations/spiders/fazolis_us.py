b60870f8122f26c09e7d0d8c3a444f8fcf8a2b82
David Hicks <david@hicks.id.au>
2024-05-15 17:03:04 +0000

    fazolis_us: fix spider (#8359)
    Partially fixes #8340

--- locations/spiders/fazolis_us.py
+++ locations/spiders/fazolis_us.py
@@ -1,20 +1,17 @@
-from locations.categories import Extras, apply_yes_no
-from locations.storefinders.yext import YextSpider
+from locations.categories import Categories, Extras, apply_yes_no
+from locations.storefinders.yext_answers import YextAnswersSpider
 
 
-class FazolisUSSpider(YextSpider):
+class FazolisUSSpider(YextAnswersSpider):
     name = "fazolis_us"
-    item_attributes = {"brand": "Fazoli's", "brand_wikidata": "Q1399195"}
+    item_attributes = {"brand": "Fazoli's", "brand_wikidata": "Q1399195", "extras": Categories.FAST_FOOD.value}
     api_key = "e6cda871db03e0fc9eb2211470649126"
+    experience_key = "locator"
 
-    def parse_item(self, item, location):
-        if location.get("c_pagesURL"):
-            item["website"] = location["c_pagesURL"]
-        item.pop("twitter")
-        item["extras"].pop("contact:instagram", None)
-        if location.get("products"):
-            apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive-thru" in location["products"], False)
-            apply_yes_no(Extras.TAKEAWAY, item, "Take-out" in location["products"], False)
-            apply_yes_no(Extras.WIFI, item, "Free Wi-Fi" in location["products"], False)
-            apply_yes_no(Extras.WHEELCHAIR, item, "Handicap Accessible" in location["products"], False)
+    def parse_item(self, location, item):
+        if amenities := location["data"].get("products"):
+            apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive-thru" in amenities, False)
+            apply_yes_no(Extras.TAKEAWAY, item, "Take-out" in amenities, False)
+            apply_yes_no(Extras.WIFI, item, "Free Wi-Fi" in amenities, False)
+            apply_yes_no(Extras.WHEELCHAIR, item, "Handicap Accessible" in amenities, False)
         yield item
