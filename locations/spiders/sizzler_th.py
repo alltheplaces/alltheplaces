33fc85e9e7b63413707ea3b30ffa94b61e9185fa
Colton Piper <pipercolton@gmail.com>
2024-05-16 11:38:54 -0400

    Deleted the accidental ndgeojson addition

--- locations/spiders/sizzler_th.py
+++ locations/spiders/sizzler_th.py
@@ -13,99 +13,14 @@ class SizzlerTHSpider(scrapy.Spider):
     }
     custom_settings = {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
 
-    thailand_provinces = [
-        "Bangkok",
-        "Amnat Charoen",
-        "Ang Thong",
-        "Bueng Kan",
-        "Buriram",
-        "Chachoengsao",
-        "Chai Nat",
-        "Chaiyaphum",
-        "Chanthaburi",
-        "Chiang Mai",
-        "Chiang Rai",
-        "Chonburi",
-        "Chumphon",
-        "Kalasin",
-        "Kamphaeng Phet",
-        "Kanchanaburi",
-        "Khon Kaen",
-        "Krabi",
-        "Lampang",
-        "Lamphun",
-        "Loei",
-        "Lopburi",
-        "Mae Hong Son",
-        "Maha Sarakham",
-        "Mukdahan",
-        "Nakhon Nayok",
-        "Nakhon Pathom",
-        "Nakhon Phanom",
-        "Nakhon Ratchasima",
-        "Nakhon Sawan",
-        "Nakhon Si Thammarat",
-        "Nan",
-        "Narathiwat",
-        "Nong Bua Lamphu",
-        "Nong Khai",
-        "Nonthaburi",
-        "Pathum Thani",
-        "Pattani",
-        "Phang Nga",
-        "Phatthalung",
-        "Phayao",
-        "Phetchabun",
-        "Phetchaburi",
-        "Phichit",
-        "Phitsanulok",
-        "Phra Nakhon Si Ayutthaya",
-        "Phrae",
-        "Phuket",
-        "Prachin Buri",
-        "Prachuap Khiri Khan",
-        "Ranong",
-        "Ratchaburi",
-        "Rayong",
-        "Roi Et",
-        "Sa Kaeo",
-        "Sakon Nakhon",
-        "Samut Prakan",
-        "Samut Sakhon",
-        "Samut Songkhram",
-        "Saraburi",
-        "Satun",
-        "Sing Buri",
-        "Sisaket",
-        "Songkhla",
-        "Sukhothai",
-        "Suphan Buri",
-        "Surat Thani",
-        "Surin",
-        "Tak",
-        "Trang",
-        "Trat",
-        "Ubon Ratchathani",
-        "Udon Thani",
-        "Uthai Thani",
-        "Uttaradit",
-        "Yala",
-        "Yasothon",
-    ]
-
     def start_requests(self):
-        for province in self.thailand_provinces:
-            yield Request(
-                url=f"https://www.sizzler.co.th/api/search/location?location=&province={province}&lang=en",
-                cb_kwargs={"province": province},
-            )
+        yield Request(url="https://www.sizzler.co.th/api/search/location?location=&province=&lang=en")
 
-    def parse(self, response, province):
+    def parse(self, response):
         locations = response.json()
         if len(locations) > 0:
             for location in locations:
                 item = DictParser.parse(location)
-                item["state"] = province
                 item["addr_full"] = location.get("address_th")
                 item["extras"]["addr:full:en"] = location.get("address_en")
                 item["extras"]["check_date"] = location.get("updated_at")
