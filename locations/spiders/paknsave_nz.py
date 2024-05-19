a2a5ead51b0f875757ade2163e50b6a975bc1a57
Cj Malone <CjMalone@mail.com>
2024-05-19 11:40:08 +0100

    [PakNSave NZ] Add spider

---
+++ locations/spiders/paknsave_nz.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+from locations.user_agents import BROWSER_DEFAULT
+
+
+class PakNSaveNZSpider(Spider):
+    name = "paknsave_nz"
+    start_urls = ["https://www.paknsave.co.nz/BrandsApi/BrandsStore/GetBrandStores"]
+    item_attributes = {"brand": "PAK'nSAVE", "brand_wikidata": "Q7125339"}
+    user_agent = BROWSER_DEFAULT
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["website"] = response.urljoin(location["url"])
+
+            item["opening_hours"] = OpeningHours()
+            for rule in location["openingHours"].split(";"):
+                item["opening_hours"].add_range(*rule.replace(".", ":").split("-"), time_format="%I:%M%p")
+
+            yield item
