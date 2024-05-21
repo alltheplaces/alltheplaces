86b0096095bb19f36ba80afe36497f6bfaf1168a
David Hicks <david@hicks.id.au>
2024-05-21 13:09:44 +0000

    Add Kia (AU) (143 dealers) (#8390)

---
+++ locations/spiders/kia_au.py
@@ -0,0 +1,35 @@
+from typing import Iterable
+
+from scrapy import Spider
+
+from locations.categories import Categories
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+from locations.items import Feature
+
+
+class KiaAUSpider(Spider):
+    name = "kia_au"
+    item_attributes = {"brand": "Kia", "brand_wikidata": "Q35349", "extras": Categories.SHOP_CAR.value}
+    allowed_domains = ["www.kia.com"]
+    start_urls = ["https://www.kia.com/api/kia_australia/base/fd01/findDealer.selectFindDealerAllList?isAll=true"]
+
+    def parse(self, response):
+        for location in response.json()["dataInfo"]:
+            if location["displayYn"] == "N":
+                continue  # Dealer is permanently closed.
+            if location["delYn"] == "Y":
+                continue  # Dealer deleted (probably franchised under a new name/dealer ID).
+            item = DictParser.parse(location)
+            item["ref"] = location["dealerCode"]
+            item["name"] = location["dealerNm"]
+            if item["website"] and item["website"].startswith("www."):
+                item["website"] = "https://" + item["website"]
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_ranges_from_string(location.get("openHours", "").replace("<br>", " "))
+            yield from self.post_process_feature(item, location) or []
+
+    def post_process_feature(self, item: Feature, feature: dict) -> Iterable[Feature]:
+        if feature["dealerNm"] == "testtest":
+            return
+        yield item
