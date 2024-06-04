2c298a6af0ff047ce001560d384aa31dbcef1d4b
David Hicks <david@hicks.id.au>
2024-06-04 22:26:24 +1000

    Add Ollies Place (AU) (45 locations)
    Fixes #8502

---
+++ locations/spiders/ollies_place_au.py
@@ -0,0 +1,11 @@
+from locations.categories import Categories
+from locations.storefinders.stockist import StockistSpider
+
+
+class OlliesPlaceAUSpider(StockistSpider):
+    name = "ollies_place_au"
+    item_attributes = {"brand": "Ollies Place", "brand_wikidata": "Q126165914", "extras": Categories.SHOP_BABY_GOODS.value}
+    key = "u20939"
+
+    def parse_item(self, item, location):
+        yield item
