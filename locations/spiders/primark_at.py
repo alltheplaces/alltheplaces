5afb51322b8ce917c430d461a9f875b0e3b29a55
David Hicks <david@hicks.id.au>
2024-06-05 21:07:26 +1000

    Add Primark (AT) (5 locations)

---
+++ locations/spiders/primark_at.py
@@ -0,0 +1,8 @@
+from locations.categories import Categories
+from locations.spiders.penneys_ie import PenneysIESpider
+from locations.spiders.primark_gb import PrimarkGBSpider
+
+class PrimarkATSpider(PenneysIESpider):
+    name = "primark_at"
+    item_attributes = PrimarkGBSpider.item_attributes
+    locale = "de-at"
