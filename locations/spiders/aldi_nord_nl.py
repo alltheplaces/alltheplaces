c2360b0b9857b430121be2d7acc7bb231c74a13a
Cj Malone <CjMalone@mail.com>
2024-06-07 14:06:29 +0100

    Aldi brand updates

--- locations/spiders/aldi_nord_nl.py
+++ locations/spiders/aldi_nord_nl.py
@@ -1,7 +1,9 @@
+from locations.categories import Categories
 from locations.storefinders.uberall import UberallSpider
 
 
 class AldiNordNLSpider(UberallSpider):
     name = "aldi_nord_nl"
-    item_attributes = {"brand": "ALDI", "brand_wikidata": "Q41171373"}
+    item_attributes = {"brand_wikidata": "Q41171373", "extras": Categories.SHOP_SUPERMARKET.value}
+    drop_attributes = {"name"}
     key = "ALDINORDNL_8oqeY3lnn9MTZdVzFn4o0WCDVTauoZ"
