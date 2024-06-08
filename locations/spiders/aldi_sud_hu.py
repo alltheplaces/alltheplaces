36a99dc0de8d79832ad07962d32d10d7ac1dc3f0
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-06-08 04:29:13 +0100

    Aldi brand updates (#8584)

--- locations/spiders/aldi_sud_hu.py
+++ locations/spiders/aldi_sud_hu.py
@@ -1,12 +1,13 @@
 from scrapy import Spider
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_HU, OpeningHours, sanitise_day
 
 
 class AldiSudHUSpider(Spider):
     name = "aldi_sud_hu"
-    item_attributes = {"brand": "ALDI", "brand_wikidata": "Q41171672"}
+    item_attributes = {"brand_wikidata": "Q41171672"}
     start_urls = [
         "https://www.aldi.hu/hu/hu/.get-stores-in-radius.json?latitude=47.162494&longitude=19.503304&radius=2500000"
     ]
@@ -18,6 +19,7 @@ class AldiSudHUSpider(Spider):
                 continue
             if item["country"] != "HU":
                 continue
+            item["name"] = None  # ref
 
             item["website"] = store.get("url")
 
@@ -27,5 +29,7 @@ class AldiSudHUSpider(Spider):
                 if not rule.get("closed", False):
                     oh.add_range(day, rule["openFormatted"], rule["closeFormatted"])
             item["opening_hours"] = oh.as_opening_hours()
-            item["name"] = "ALDI"
+
+            apply_category(Categories.SHOP_SUPERMARKET, item)
+
             yield item
