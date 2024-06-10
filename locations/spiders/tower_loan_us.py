eb0e5599468905d6854b52e5bdc29a5e0f83dfa0
Karen Kerr <karenkerrgithub@gmail.com>
2024-06-10 13:53:55 +0100

    [TowerLoanUS] Add Category

--- locations/spiders/tower_loan_us.py
+++ locations/spiders/tower_loan_us.py
@@ -5,12 +5,17 @@ from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.categories import Categories
 from locations.dict_parser import DictParser
 
 
 class TowerLoanUSSpider(CrawlSpider):
     name = "tower_loan_us"
-    item_attributes = {"brand": "Tower Loan", "brand_wikidata": "Q126195732"}
+    item_attributes = {
+        "brand": "Tower Loan",
+        "brand_wikidata": "Q126195732",
+        "extras": Categories.OFFICE_FINANCIAL.value,
+    }
     start_urls = ["https://www.towerloan.com/branch-locations/alabama-locations/"]
     rules = [Rule(LinkExtractor(r"/branch-locations/[^/]+/$"), "parse", follow=True)]
 
