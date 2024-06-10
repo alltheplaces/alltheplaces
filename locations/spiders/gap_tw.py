03b94f704e963781dff05ff421f2dbbb29eb1c32
Colton Piper <pipercolton@gmail.com>
2024-06-10 09:46:07 -0400

    Added Gap TW (#8605)

---
+++ locations/spiders/gap_tw.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+
+
+class GapTWSpider(scrapy.Spider):
+    name = "gap_tw"
+    item_attributes = {"brand": "Gap", "brand_wikidata": "Q420822"}
+
+    def start_requests(self):
+        url = "https://api.gap.tw/store/queryStoreLocation"
+        yield JsonRequest(
+            url=url,
+            method="POST",
+            headers={"Content-Type": "application/json;charset=UTF-8"},
+            data={"id": 1, "locationIdentifier": "Gap"},
+        )
+
+    def parse(self, response, **kwargs):
+        for city in response.json()["data"][0]["cityList"]:
+            for store in city["storeLocations"]:
+                item = DictParser.parse(store)
+                item["country"] = "TW"
+                item["website"] = "https://www.gap.tw/"
+                yield item
