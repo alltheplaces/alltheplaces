9b9253e83ea3c0678492d7fe25259a947f1e0ad1
Colton Piper <pipercolton@gmail.com>
2024-06-12 11:04:56 -0400

    Finished Opening Hours

--- locations/spiders/sushi_express_tw.py
+++ locations/spiders/sushi_express_tw.py
@@ -1,7 +1,7 @@
 from scrapy import Spider
 
 from locations.dict_parser import DictParser
-from locations.hours import OpeningHours
+from locations.hours import DAYS, OpeningHours
 
 
 class SushiExpressTW(Spider):
@@ -13,16 +13,18 @@ class SushiExpressTW(Spider):
         stores = response.json()
         for store in stores:
             item = DictParser.parse(store)
-            item["state"] = store.get("District")
-            item["fax"] = store.get("faxNumber")
+            item["state"] = store.get("district")
+            item["extras"]["fax"] = store.get("faxNumber")
             self.parse_opening_hours(item, store.get("openingHours"))
 
             yield item
 
-    def parse_opening_hours(self, item, hours):
+    def parse_opening_hours(self, item, full_hours):
         try:
+            full_hours = full_hours.replace("~", "-")
             item["opening_hours"] = OpeningHours()
-            hours = hours.split("(")[0]
+            hours = full_hours.split("（")[0]  # Has weird character in string. Shows hours and last order hour?
+            item["opening_hours"].add_days_range(DAYS, hours.split("-")[0], hours.split("-")[1])
 
         except Exception:
             self.crawler.stats.inc_value("failed_hours_parse")
