637a8eb885af4b55de5d6675931a5499a4e6199d
Cj Malone <CjMalone@mail.com>
2024-06-12 14:35:29 +0100

    Drop debug stats

--- locations/spiders/texaco_central_america.py
+++ locations/spiders/texaco_central_america.py
@@ -6,13 +6,13 @@ from typing import Any
 from scrapy import Spider
 from scrapy.http import Response
 
-from locations.categories import Categories, Fuel, apply_yes_no
+from locations.categories import Categories, Fuel, apply_category, apply_yes_no
 from locations.items import Feature
 
 
 class TexacoCentralAmericaSpider(Spider):
     name = "texaco_central_america"
-    item_attributes = {"brand": "Texaco", "brand_wikidata": "Q775060", "extras": Categories.FUEL_STATION.value}
+    item_attributes = {"brand": "Texaco", "brand_wikidata": "Q775060"}
     start_urls = [
         "https://www.texacocontechron.com/estaciones-de-servicio/",
         "https://www.texacocontechron.com/gt/estaciones-de-servicio/",
@@ -31,8 +31,8 @@ class TexacoCentralAmericaSpider(Spider):
                 item["lat"] = location["ubicacion"]["lat"]
                 item["lon"] = location["ubicacion"]["lng"]
                 item["city"] = location["ciudad"]
-                for t in location["tipo"]:
-                    self.crawler.stats.inc_value("z/{}".format(t))
+
+                apply_category(Categories.FUEL_STATION, item)
 
                 apply_yes_no(Fuel.DIESEL, item, "diesel" in location["tipo"])
                 yield item
