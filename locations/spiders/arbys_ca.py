01b7629f93fce6b809935e7570e922a7158cd1cc
Cj Malone <CjMalone@mail.com>
2024-06-21 17:29:59 +0100

    Fix regex

--- locations/spiders/arbys_ca.py
+++ locations/spiders/arbys_ca.py
@@ -8,5 +8,5 @@ class ArbysCASpider(SitemapSpider, StructuredDataSpider):
     name = "arbys_ca"
     item_attributes = ArbysUSSpider.item_attributes
     sitemap_urls = ["https://locations.arbys.ca/sitemap.xml"]
-    sitemap_rules = [("ca/\w\w/[^/]+/[^/]+.html", "parse")]
+    sitemap_rules = [(r"ca/\w\w/[^/]+/[^/]+.html", "parse")]
     wanted_types = ["Restaurant"]
