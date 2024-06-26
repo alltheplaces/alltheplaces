9253edfe62a8665227f0a08eb280bee7e45402a1
IreuN <gatkowski.michal@gmail.com>
2024-06-26 17:38:51 +0200

    Update rossmann_pl.py

--- locations/spiders/rossmann_pl.py
+++ locations/spiders/rossmann_pl.py
@@ -1,6 +1,7 @@
 from scrapy import Spider
 
 from locations.items import Feature
+from locations.hours import OpeningHours, DAYS_FULL
 
 
 class RossmannPLSpider(Spider):
@@ -11,14 +12,33 @@ class RossmannPLSpider(Spider):
     def parse(self, response):
         data = response.json()["data"]
         for shop in data:
+
+            hours = self.parse_hours(shop.get("openHours"))
+
             properties = {
                 "ref": shop["shopNumber"],
                 "street_address": shop["address"]["street"],
                 "city": shop["address"]["city"],
                 "postcode": shop["address"]["postCode"],
                 "country": "PL",
+                "opening_hours": hours.as_opening_hours(),
                 "lat": shop["shopLocation"]["latitude"],
                 "lon": shop["shopLocation"]["longitude"],
                 "website": "https://www.rossmann.pl/drogerie" + shop["navigateUrlV2"],
             }
-            yield Feature(**properties)
+
+            item = Feature(**properties)
+
+            yield item
+
+    def parse_hours(self, data):
+
+        hours = OpeningHours()
+        for day in [x.lower() for x in DAYS_FULL]:
+            if day + "OpenFrom" not in data:
+                continue
+            start_time = data[day + "OpenFrom"]
+            end_time = data[day + "OpenTo"]
+            hours.add_range(day[:2].capitalize(), start_time, end_time, "%H:%M")
+
+        return hours
