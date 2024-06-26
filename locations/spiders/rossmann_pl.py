4ae6c3426fc61414da68a0461e3ed7254edf910c
IreuN <gatkowski.michal@gmail.com>
2024-06-26 18:09:48 +0200

    Change rossmann_pl url, much more keys can be now extracted (#8711)
    * Change rossmann_pl url, much more keys can be now extracted
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update rossmann_pl.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/rossmann_pl.py
+++ locations/spiders/rossmann_pl.py
@@ -1,14 +1,44 @@
-from scrapy.spiders import SitemapSpider
+from scrapy import Spider
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.hours import DAYS_FULL, OpeningHours
+from locations.items import Feature
 
 
-class RossmannPLSpider(SitemapSpider, StructuredDataSpider):
+class RossmannPLSpider(Spider):
     name = "rossmann_pl"
     item_attributes = {"brand": "Rossmann", "brand_wikidata": "Q316004"}
-    sitemap_urls = ["https://www.rossmann.pl/sitemap_shops.xml"]
-    sitemap_rules = [("/drogerie/Drogeria-Rossmann-", "parse_sd")]
+    start_urls = ["https://www.rossmann.pl/shops/api/Shops"]
 
-    def post_process_item(self, item, response, ld_data):
-        item["name"] = ld_data["description"]
-        yield item
+    def parse(self, response):
+        data = response.json()["data"]
+        for shop in data:
+
+            hours = self.parse_hours(shop.get("openHours"))
+
+            properties = {
+                "ref": shop["shopNumber"],
+                "street_address": shop["address"]["street"],
+                "city": shop["address"]["city"],
+                "postcode": shop["address"]["postCode"],
+                "country": "PL",
+                "opening_hours": hours.as_opening_hours(),
+                "lat": shop["shopLocation"]["latitude"],
+                "lon": shop["shopLocation"]["longitude"],
+                "website": "https://www.rossmann.pl/drogerie" + shop["navigateUrlV2"],
+            }
+
+            item = Feature(**properties)
+
+            yield item
+
+    def parse_hours(self, data):
+
+        hours = OpeningHours()
+        for day in [x.lower() for x in DAYS_FULL]:
+            if day + "OpenFrom" not in data:
+                continue
+            start_time = data[day + "OpenFrom"]
+            end_time = data[day + "OpenTo"]
+            hours.add_range(day[:2].capitalize(), start_time, end_time, "%H:%M")
+
+        return hours
