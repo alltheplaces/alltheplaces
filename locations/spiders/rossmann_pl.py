10e08ce49462ccb165638c75ee76db479399c453
IreuN <gatkowski.michal@gmail.com>
2024-06-26 16:37:47 +0200

    Change rossmann_pl url, much more keys can be now extracted

--- locations/spiders/rossmann_pl.py
+++ locations/spiders/rossmann_pl.py
@@ -1,14 +1,24 @@
-from scrapy.spiders import SitemapSpider
+from scrapy import Spider
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.items import Feature
 
 
-class RossmannPLSpider(SitemapSpider, StructuredDataSpider):
+class RossmannPLSpider(Spider):
     name = "rossmann_pl"
     item_attributes = {"brand": "Rossmann", "brand_wikidata": "Q316004"}
-    sitemap_urls = ["https://www.rossmann.pl/sitemap_shops.xml"]
-    sitemap_rules = [("/drogerie/Drogeria-Rossmann-", "parse_sd")]
+    start_urls = ["https://www.rossmann.pl/shops/api/Shops"]
 
-    def post_process_item(self, item, response, ld_data):
-        item["name"] = ld_data["description"]
-        yield item
+    def parse(self, response):
+        data = response.json()['data']
+        for shop in data:
+            properties = {
+                "ref": shop['shopNumber'],
+                "street_address": shop['address']['street'],
+                "city": shop['address']['city'],
+                "postcode": shop['address']["postCode"],
+                "country": "PL",
+                "lat": shop['shopLocation']['latitude'],
+                "lon": shop['shopLocation']['longitude'],
+                "website": "https://www.rossmann.pl/drogerie" + shop['navigateUrlV2'],
+            }
+            yield Feature(**properties)
