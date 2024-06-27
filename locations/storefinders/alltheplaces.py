78ddfcc3089c81ad3243444e62acf8b1c575d806
Cj Malone <CjMalone@mail.com>
2024-06-27 12:29:31 +0100

    Add ATP ingest code

---
+++ locations/storefinders/alltheplaces.py
@@ -0,0 +1,31 @@
+from typing import Any, Iterable
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.exporters.geojson import mapping
+from locations.items import Feature
+
+
+class AllThePlacesSpider(Spider):
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for feature in response.json().get("features", []):
+            properties = feature.get("properties", {})
+
+            item = Feature()
+
+            for atp_key, json_key in mapping:
+                if value := properties.pop(json_key, None):
+                    item[atp_key] = value
+
+            item["ref"] = properties.pop("ref", None)
+            item["geometry"] = feature.get("geometry", {})
+            item["extras"] = properties
+
+            yield from self.post_process_feature(item, feature, response) or []
+
+    def post_process_feature(
+        self, item: Feature, source_feature: dict, response: Response, **kwargs
+    ) -> Iterable[Feature]:
+        yield item
