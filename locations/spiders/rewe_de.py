7cc21960e1b6404b5e003939253dbea9c57d9fca
Cj Malone <CjMalone@mail.com>
2024-06-28 12:38:09 +0100

    [REWE DE] Fix spider

--- locations/spiders/rewe_de.py
+++ locations/spiders/rewe_de.py
@@ -1,17 +1,24 @@
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class REWEDESpider(SitemapSpider, StructuredDataSpider):
     name = "rewe_de"
-    item_attributes = {"brand": "REWE", "brand_wikidata": "Q16968817", "extras": Categories.SHOP_SUPERMARKET.value}
+    item_attributes = {"name": "REWE", "brand": "REWE", "brand_wikidata": "Q16968817"}
     allowed_domains = ["www.rewe.de"]
     sitemap_urls = ["https://www.rewe.de/sitemaps/sitemap-maerkte.xml"]
-    sitemap_rules = [(r"^https:\/\/www\.rewe\.de\/marktseite\/[\w\-]+\/\d+\/[\w\-]+\/$", "parse_sd")]
+    sitemap_rules = [(r"/marktseite/[^/]+/(\d+)/[^/]+/$", "parse_sd")]
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
     def post_process_item(self, item, response, ld_data):
+        item["name"] = None
         item["street_address"] = item["street_address"].removesuffix(" null")
         item.pop("image", None)
+
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+
         yield item
