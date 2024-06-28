0e7f744d3e653a9668eb14eec604287fb6bc73bf
Cj Malone <CjMalone@mail.com>
2024-06-28 15:42:01 +0100

    [BrowArt23 US] Add spider

--- locations/spiders/brow_art_23_us.py
+++ locations/spiders/brow_art_23_us.py
@@ -1,11 +1,31 @@
-from locations.categories import Categories
-from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
+from typing import Any
+
+from scrapy import Selector, Spider
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class BrowArt23USSpider(AgileStoreLocatorSpider):
+class BrowArt23USSpider(Spider):
     name = "brow_art_23_us"
-    item_attributes = {"brand_wikidata": "Q115675881", "brand": "Brow Art 23", "extras": Categories.SHOP_BEAUTY.value}
-    allowed_domains = [
-        "browart23.com",
+    item_attributes = {"brand": "Brow Art 23", "brand_wikidata": "Q115675881"}
+    start_urls = [
+        "https://browart23.com/wp-admin/admin-ajax.php?action=yith_sl_get_results&context=frontend&filters[radius][]=500"
     ]
     requires_proxy = True
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["markers"]:
+            item = Feature()
+            item["ref"] = location["id"]
+            item["branch"] = location["name"]
+            item["lat"] = location["latitude"]
+            item["lon"] = location["longitude"]
+            item["website"] = "https://browart23.com/store-locator/{}/".format(location["slug"])
+
+            sel = Selector(text=location["pin_modal"])
+            item["addr_full"] = merge_address_lines(sel.xpath('//p[@class="store-address"]/text()').getall())
+            item["phone"] = sel.xpath('//li[@class="store-phone"]/a/text()').get()
+
+            yield item
