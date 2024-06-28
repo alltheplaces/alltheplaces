b4b54f58fe9849e95eb4b4def76ec1007f7a77d5
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-06-28 15:16:43 +0100

    Add contacts attribute on spider (#8734)

--- locations/spiders/tomtom.py
+++ locations/spiders/tomtom.py
@@ -4,3 +4,4 @@ from locations.storefinders.alltheplaces import AllThePlacesSpider
 class TomTomSpider(AllThePlacesSpider):
     name = "tomtom"
     start_urls = ["https://download.tomtom.com/open/fcd/pois.json"]
+    contacts = ["office-pois@groups.tomtom.com"]
