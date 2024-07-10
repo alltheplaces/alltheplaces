7b129b57d103820f4ab88d71a0abf583d3eefe11
panasonic-mafia <kolosov.yv@gmail.com>
2024-07-10 11:24:11 +0200

    adjust delay and sitemap regex

--- locations/spiders/upsstore.py
+++ locations/spiders/upsstore.py
@@ -8,4 +8,5 @@ class UpsStoreSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "UPS Store", "brand_wikidata": "Q7771029"}
     allowed_domains = ["locations.theupsstore.com"]
     sitemap_urls = ["https://locations.theupsstore.com/sitemap.xml"]
-    sitemap_rules = [(r"\/[a-z]{2}\/[a-z]+\/\d+-[a-z-]+$", "parse_sd")]
+    sitemap_rules = [(r"https:\/\/locations.theupsstore.com\/[a-z]{2}\/[a-z]+\/\d+-[a-z-]+$", "parse_sd")]
+    download_delay = 0.5
