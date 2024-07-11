5b543e14f55e18501f61448c916147950d972ea8
jhutton1 <jaimehutton01@gmail.com>
2024-07-11 11:01:38 -0400

    fix spider

--- locations/spiders/joules.py
+++ locations/spiders/joules.py
@@ -6,9 +6,9 @@ from locations.structured_data_spider import StructuredDataSpider
 class JoulesSpider(SitemapSpider, StructuredDataSpider):
     name = "joules"
     item_attributes = {"brand": "Joules", "brand_wikidata": "Q25351738"}
-    sitemap_urls = ["https://www.joules.com/sitemap.xml"]
-    sitemap_follow = ["Store-en-GBP"]
-    sitemap_rules = [("/store-locator/store/", "parse_sd")]
+    sitemap_urls = ["https://www.joules.com/Joules-GB-EN-Store.xml.gz"]
+    sitemap_rules = [("/storelocator/", "parse_sd")]
 
     def pre_process_data(self, ld_data, **kwargs):
         ld_data["url"] = None
+
