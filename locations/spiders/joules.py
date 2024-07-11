88437aaa445c4cbe88acfdd79276ddf6806a1df4
jhutton1 <jaimehutton01@gmail.com>
2024-07-11 16:38:42 -0400

    update sitemap

--- locations/spiders/joules.py
+++ locations/spiders/joules.py
@@ -6,7 +6,8 @@ from locations.structured_data_spider import StructuredDataSpider
 class JoulesSpider(SitemapSpider, StructuredDataSpider):
     name = "joules"
     item_attributes = {"brand": "Joules", "brand_wikidata": "Q25351738"}
-    sitemap_urls = ["https://www.joules.com/Joules-GB-EN-Store.xml.gz"]
+    sitemap_urls = ["https://www.joules.com/sitemap-index.xml"]
+    sitemap_follow = ["Joules-GB-EN-Store"]
     sitemap_rules = [("/storelocator/", "parse_sd")]
 
     def pre_process_data(self, ld_data, **kwargs):
