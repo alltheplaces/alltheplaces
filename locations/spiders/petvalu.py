56fa88e89dcd2197ecc554800f69608efabc4ebd
Karen Kerr <138442998+karen-kerr@users.noreply.github.com>
2024-07-11 03:55:32 +0100

    [PetValu] Fix non running spider (#8804)
    * [PetValu] Fix non running spider
    
    * Flip lat/lon
    
    ---------
    
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/petvalu.py
+++ locations/spiders/petvalu.py
@@ -9,14 +9,16 @@ class PetValuSpider(scrapy.Spider):
     item_attributes = {"brand": "Pet Valu", "brand_wikidata": "Q58009635"}
     allowed_domains = ["store.petvalu.ca"]
     start_urls = (
-        "https://store.petvalu.ca/modules/multilocation/?near_location=toronto&threshold=40000000000000&geocoder_components=country:CA&distance_unit=km&limit=20000000000&services__in=&language_code=en-us&published=1&within_business=true",
+        "https://store.petvalu.ca/stat/api/locations/search?lng=-73.9818&lat=40.7263&kilometers=3000&limit=200&fields=servicetags_bosleys,servicetags_paulmacs,servicetags_tisol_2,servicetags_total_pet_2,servicetags_petvalu_2,displayname_location_name",
     )
 
     def parse(self, response):
         data = response.json()
-        for i in data["objects"]:
-            i["street_address"] = i.pop("street")
-            item = DictParser.parse(i)
-            item["addr_full"] = i["formatted_address"]
-            item["website"] = i["location_url"]
+        for store in data["locations"]:
+            store.update(store.pop("businessAddress"))
+            item = DictParser.parse(store)
+            item["ref"] = store["clientLocationId"]
+            item["lon"], item["lat"] = store["coordinates"]
+            item["phone"] = store["primaryPhone"]
+            item["website"] = "https://store.petvalu.ca/store/" + store["link"]
             yield item
