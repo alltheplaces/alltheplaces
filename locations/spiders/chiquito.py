ceba05be22faa28233690d25f5897757a1ce49ff
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-16 14:18:55 +0200

    Update chiquito.py
    Updating chiquito spider

--- locations/spiders/chiquito.py
+++ locations/spiders/chiquito.py
@@ -1,71 +1,41 @@
-import scrapy
+from typing import Any
+from urllib.parse import urljoin
+
+from scrapy.http import Response
+from scrapy.spiders import Spider
 
 from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
-
+from locations.pipelines.address_clean_up import merge_address_lines
 
 class ChiquitoSpider(scrapy.Spider):
     name = "chiquito"
     item_attributes = {"brand": "Chiquito", "brand_wikidata": "Q5101775"}
     allowed_domains = ["www.chiquito.co.uk"]
     start_urls = [
-        "https://www.chiquito.co.uk/api/content/restaurants",
+        "https://api.bigtablegroup.com/cdg/allRestaurants/chiquito",
     ]
 
-    def parse(self, response):
-        stores = response.json()["restaurants"]["fields"]["list"]
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json():
+            item = Feature()
+            item["ref"] = location["storeId"]
+            item["lat"] = location["addressLocation"]["lat"]
+            item["lon"] = location["addressLocation"]["lon"]
+            item["branch"] = location["title"]
+            item["street_address"] = merge_address_lines([location["addressLine1"], location["addressLine2"]])
+            item["city"] = location["addressCity"]
+            item["postcode"] = location["postcode"]
+            item["website"] = urljoin("https://www.chiquito.co.uk/restaurants/", location["slug"])
+            item["phone"] = location["phoneNumber"]
+            item["email"] = location["email"]
 
-        for store in stores:
-            properties = {
-                "lat": store["fields"]["latitude"],
-                "lon": store["fields"]["longitude"],
-                "ref": store["fields"]["siteId"],
-                "name": store["fields"]["name"],
-                "street_address": store["fields"]["street"],
-                "city": store["fields"]["city"],
-                "postcode": store["fields"]["postalCode"],
-                "country": store["fields"]["country"].upper(),
-                "addr_full": ", ".join(
-                    filter(
-                        None,
-                        (
-                            store["fields"]["street"],
-                            store["fields"]["city"],
-                            store["fields"]["region"],
-                            store["fields"]["postalCode"],
-                            "United Kingdom",
-                        ),
-                    )
-                ),
-                "phone": "+44 " + store["fields"]["telephone"][1:],
-                "website": "https://" + store["fields"]["url"],
-                "extras": {
-                    "delivery": "yes" if store["fields"]["enableDeliveries"] else "no",
-                    "takeaway": "yes" if store["fields"]["enableClickAndCollect"] else "no",
-                    "contact:deliveroo": store["fields"].get("deliverooDirectLink"),
-                    "contact:uberEats": store["fields"].get("uberEatsDirectLink"),
-                    "contact:justEat": store["fields"].get("justEatDirectLink"),
-                },
-            }
-            oh = OpeningHours()
+            item["opening_hours"] = OpeningHours()
+            for day in map(str.lower, DAYS_FULL):
+                item["opening_hours"].add_range(
+                    day,
+                    location["hours"]["{}Open".format(day)].strip(),
+                    location["hours"]["{}Close".format(day)].strip(),
+                )
 
-            for day in DAYS_FULL:
-                open_time = self.parse_time(store["fields"]["open" + day].split("-")[0])
-                close_time = self.parse_time(store["fields"]["open" + day].split("-")[1])
-
-                oh.add_range(day, open_time, close_time, "%I:%M%p")
-
-            properties["opening_hours"] = oh
-
-            yield Feature(**properties)
-
-    def parse_time(self, time):
-        time = time.strip()
-        if "." in time:
-            time = time.replace(".", ":")
-
-        if len(time) == 3:
-            time = "0" + time
-        if ":" not in time:
-            time = time[0:2] + ":00" + time[2:]
-        return time
+            yield item   
