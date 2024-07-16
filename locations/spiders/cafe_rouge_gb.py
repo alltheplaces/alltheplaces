180f82b518b8a6f9a56d15bba4a7fe72b0f17ec0
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-16 18:57:27 +0200

    Update cafe_rouge_gb.py
    Already updated to this.

--- locations/spiders/cafe_rouge_gb.py
+++ locations/spiders/cafe_rouge_gb.py
@@ -1,39 +1,15 @@
-from typing import Any
-from urllib.parse import urljoin
+from typing import Iterable
 
-from scrapy.http import Response
-from scrapy.spiders import Spider
-
-from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
-from locations.pipelines.address_clean_up import merge_address_lines
+from locations.spiders.frankie_and_bennys_gb import FrankieAndBennysGBSpider
 
 
-class CafeRougeGBSpider(Spider):
+class CafeRougeGBSpider(FrankieAndBennysGBSpider):
     name = "cafe_rouge_gb"
-    item_attributes = {"brand": "Cafe Rouge", "brand_wikidata": "Q5017261"}
+    item_attributes = {"brand": "Café Rouge", "brand_wikidata": "Q5017261"}
     start_urls = ["https://api.bigtablegroup.com/cdg/allRestaurants/rouge"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json():
-            item = Feature()
-            item["ref"] = location["storeId"]
-            item["lat"] = location["addressLocation"]["lat"]
-            item["lon"] = location["addressLocation"]["lon"]
-            item["branch"] = location["title"]
-            item["street_address"] = merge_address_lines([location["addressLine1"], location["addressLine2"]])
-            item["city"] = location["addressCity"]
-            item["postcode"] = location["postcode"]
-            item["website"] = urljoin("https://www.caferouge.com/restaurants/", location["slug"])
-            item["phone"] = location["phoneNumber"]
-            item["email"] = location["email"]
+    def parse_item(self, item: Feature, location: dict, **kwargs) -> Iterable[Feature]:
+        item["website"] = "https://www.caferouge.com/restaurants/{}/{}/".format(location["city"], location["slug"])
 
-            item["opening_hours"] = OpeningHours()
-            for day in map(str.lower, DAYS_FULL):
-                item["opening_hours"].add_range(
-                    day,
-                    location["hours"]["{}Open".format(day)].strip(),
-                    location["hours"]["{}Close".format(day)].strip(),
-                )
-
-            yield item
+        yield item
