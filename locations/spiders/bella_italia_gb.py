02bded91ea3d08ad840966c957660eec5d216d89
Cj Malone <CjMalone@mail.com>
2024-07-16 14:22:49 +0100

    [BellaItalia GB] Fix website

--- locations/spiders/bella_italia_gb.py
+++ locations/spiders/bella_italia_gb.py
@@ -1,40 +1,15 @@
-from typing import Any
-from urllib.parse import urljoin
+from typing import Iterable
 
-from scrapy.http import Response
-from scrapy.spiders import Spider
-
-from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
-from locations.pipelines.address_clean_up import merge_address_lines
+from locations.spiders.frankie_and_bennys_gb import FrankieAndBennysGBSpider
 
 
-class BellaItaliaGBSpider(Spider):
+class BellaItaliaGBSpider(FrankieAndBennysGBSpider):
     name = "bella_italia_gb"
     item_attributes = {"brand": "Bella Italia", "brand_wikidata": "Q4883362"}
     start_urls = ["https://api.bigtablegroup.com/cdg/allRestaurants/bella"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json():
-            item = Feature()
-            item["ref"] = location["storeId"]
-            item["lat"] = location["addressLocation"]["lat"]
-            item["lon"] = location["addressLocation"]["lon"]
-            item["branch"] = location["title"]
-            item["street_address"] = merge_address_lines([location["addressLine1"], location["addressLine2"]])
-            item["city"] = location["addressCity"]
-            item["postcode"] = location["postcode"]
-            slug = urljoin(location["city"], "/", location["slug"])
-            item["website"] = urljoin("https://www.bellaitalia.co.uk/restaurants/", slug)
-            item["phone"] = location["phoneNumber"]
-            item["email"] = location["email"]
-            if location["hours"]:
-                item["opening_hours"] = OpeningHours()
-                for day in map(str.lower, DAYS_FULL):
-                    item["opening_hours"].add_range(
-                        day,
-                        location["hours"]["{}Open".format(day)].strip(),
-                        location["hours"]["{}Close".format(day)].strip(),
-                    )
+    def parse_item(self, item: Feature, location: dict, **kwargs) -> Iterable[Feature]:
+        item["website"] = "https://www.bellaitalia.co.uk/restaurants/{}/{}/".format(location["city"], location["slug"])
 
-            yield item
+        yield item
