00ab7be7c1f5eef8488193a019e7f8f18661e795
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-16 11:53:20 +0200

    Update las_iguanas_gb.py
    Updated url to json file of full list of branches.

--- locations/spiders/las_iguanas_gb.py
+++ locations/spiders/las_iguanas_gb.py
@@ -1,23 +1,39 @@
-from scrapy.spiders import SitemapSpider
+from typing import Any
+from urllib.parse import urljoin
 
-from locations.google_url import extract_google_position
+from scrapy.http import Response
+from scrapy.spiders import Spider
+
+from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
-from locations.structured_data_spider import extract_email, extract_phone
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class LasIguanasGBSpider(SitemapSpider):
     name = "las_iguanas_gb"
     item_attributes = {"brand": "Las Iguanas", "brand_wikidata": "Q19875012"}
-    sitemap_urls = ["https://www.iguanas.co.uk/sitemap.xml"]
-    sitemap_rules = [(r"/restaurants/[-\w]+/[-\w]+$", "parse")]
+    start_urls = ["https://api.bigtablegroup.com/cdg/allRestaurants/iguanas"]
 
-    def parse(self, response, **kwargs):
-        item = Feature()
-        item["ref"] = item["website"] = response.url
-        item["addr_full"] = response.xpath('//div[@class="address"]/text()').get()
-        item["branch"] = response.xpath('//h1[@class="restaurant-title"]/text()').get()
-        extract_google_position(item, response)
-        extract_email(item, response)
-        extract_phone(item, response)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json():
+            item = Feature()
+            item["ref"] = location["storeId"]
+            item["lat"] = location["addressLocation"]["lat"]
+            item["lon"] = location["addressLocation"]["lon"]
+            item["branch"] = location["title"]
+            item["street_address"] = merge_address_lines([location["addressLine1"], location["addressLine2"]])
+            item["city"] = location["addressCity"]
+            item["postcode"] = location["postcode"]
+            item["website"] = urljoin("hhttps://www.iguanas.co.uk/restaurants/", location["slug"])
+            item["phone"] = location["phoneNumber"]
+            item["email"] = location["email"]
 
-        yield item
+            item["opening_hours"] = OpeningHours()
+            for day in map(str.lower, DAYS_FULL):
+                item["opening_hours"].add_range(
+                    day,
+                    location["hours"]["{}Open".format(day)].strip(),
+                    location["hours"]["{}Close".format(day)].strip(),
+                )
+
+            yield item
