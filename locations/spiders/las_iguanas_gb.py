72cdc53c56005b463988a84a9bdb73080fa60d0a
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-16 13:39:27 +0200

    Update las_iguanas_gb.py
    Fixed typo in website.

--- locations/spiders/las_iguanas_gb.py
+++ locations/spiders/las_iguanas_gb.py
@@ -24,7 +24,7 @@ class LasIguanasGBSpider(Spider):
             item["street_address"] = merge_address_lines([location["addressLine1"], location["addressLine2"]])
             item["city"] = location["addressCity"]
             item["postcode"] = location["postcode"]
-            item["website"] = urljoin("hhttps://www.iguanas.co.uk/restaurants/", location["slug"])
+            item["website"] = urljoin("https://www.iguanas.co.uk/restaurants/", location["slug"])
             item["phone"] = location["phoneNumber"]
             item["email"] = location["email"]
 
