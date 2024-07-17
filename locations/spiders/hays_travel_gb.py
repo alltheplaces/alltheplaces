e01b46c3b76b0e6bb705e0878fb30b24cdced3a5
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-17 20:34:36 +0200

    Update hays_travel_gb.py

--- locations/spiders/hays_travel_gb.py
+++ locations/spiders/hays_travel_gb.py
@@ -1,9 +1,11 @@
+import json
+
 from typing import Any
 from urllib.parse import urljoin
 
 from scrapy.http import Response
 from scrapy.spiders import Spider
-
+from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
 
 
@@ -13,9 +15,7 @@ class HaysTravelGBSpider(Spider):
     start_urls = ["       https://branches.haystravel.co.uk/api/branches"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        data = response.json()
-        print(data["data"])
-        items = data["data"]
+        items = response.json()["data"]
 
         for location in items:
             item = Feature()
@@ -26,11 +26,11 @@ class HaysTravelGBSpider(Spider):
             item["street_address"] = location["address"]
             item["city"] = location["name"]
             item["postcode"] = location["postcode"]
-            item["website"] = urljoin("https://www.americangolf.co.uk/stores?store=", location["ID"])
+            item["website"] = urljoin("https://www.haystravel.co.uk/branches/", location["name"])
             item["phone"] = location["phone"]
             item["email"] = location["email"]
 
-            item["opening_hours"] = OpeningHours()
+
             for day in map(str.lower, DAYS_FULL):
                 item["opening_hours"].add_range(
                     day,
