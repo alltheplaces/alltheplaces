1636246ec6cc98f002f86cd659e40df243b45635
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-18 14:30:05 +0200

    Update hays_travel_gb.py
    Fixing opening hours in 2 different formats

--- locations/spiders/hays_travel_gb.py
+++ locations/spiders/hays_travel_gb.py
@@ -4,6 +4,7 @@ from typing import Any
 from scrapy.http import Response
 from scrapy.spiders import Spider
 
+from urllib.parse import urljoin
 from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
 
@@ -11,7 +12,7 @@ from locations.items import Feature
 class HaysTravelGBSpider(Spider):
     name = "hays_travel_gb"
     item_attributes = {"brand": "Hays Travel", "brand_wikidata": "Q70250954"}
-    start_urls = ["       https://branches.haystravel.co.uk/api/branches"]
+    start_urls = ["https://branches.haystravel.co.uk/api/branches"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         items = response.json()["data"]
@@ -25,15 +26,30 @@ class HaysTravelGBSpider(Spider):
             item["street_address"] = location["address"]
             item["city"] = location["name"]
             item["postcode"] = location["postcode"]
-            # This is only true for cases where name is a single word
-            #            item["website"] = urljoin("https://www.haystravel.co.uk/branches/", location["name"])
+            # This is not true for all cases - example Hull Northpoint
+            item["website"] = urljoin("https://www.haystravel.co.uk/branches/", "-".join(location["name"].split()))
             item["phone"] = location["phone"]
             item["email"] = location["email"]
 
-            item["opening_hours"] = OpeningHours()
+            oh = OpeningHours()
             for day in map(str.lower, DAYS_FULL):
                 if rule := location.get(f"{day}_hours"):
-                    for start_time, end_time in re.findall(r"(\d\d:\d\d)a?m?\s*-\s*(\d\d:\d\d)p?m?", rule):
-                        item["opening_hours"].add_range(day, start_time, end_time)
+                    for start_hour, start_minute, start_zone, end_hour, end_minute, end_zone in re.findall(r"(\d*)[:.](\d\d)\s*(am|pm)?\s*-\s*(\d*)[:.](\d\d)\s*(am|pm)?", rule):
+                        start=''
+                        end=''
+                        if start_zone:
+                           start_hour = int(start_hour)
+                           if start_zone == 'pm' and start_hour != 12:
+                                start_hour += 12
+                           start = f'{start_hour:02d}:{start_minute}'
+                           end_hour=int(end_hour)
+                           if end_zone == 'pm' and end_hour != 12:
+                                end_hour += 12
+                           end = f'{end_hour:02d}:{end_minute}'
+                        else:
+                           start=f'{start_hour}:{start_minute}'
+                           end=f'{end_hour}:{end_minute}'
+                        oh.add_range(day, start, end)
+            item["opening_hours"] = oh.as_opening_hours()
 
             yield item
