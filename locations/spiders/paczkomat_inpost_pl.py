de608d87e43db78ff6ad461b4152de81e1fb782f
IreuN <gatkowski.michal@gmail.com>
2024-07-19 13:04:02 +0200

    Update paczkomat_inpost_pl.py

--- locations/spiders/paczkomat_inpost_pl.py
+++ locations/spiders/paczkomat_inpost_pl.py
@@ -42,7 +42,7 @@ class PaczkomatInpostPLSpider(Spider):
             # poi["g"]
             # poi["p"]  # payment
 
-            item["website"] = f"https://inpost.pl/{self.parse_slug(item, poi["r"])}"
+            item["website"] = f'https://inpost.pl/{self.parse_slug(item, poi["e"], poi["r"])}'
             if poi["h"] == "24/7":
                 item["opening_hours"] = "24/7"
             else:
@@ -50,8 +50,8 @@ class PaczkomatInpostPLSpider(Spider):
                 item["opening_hours"].add_ranges_from_string(poi["h"], days=DAYS_PL)
             yield item
 
-    def parse_slug(self, item, state):
-        slug_parts = ["paczkomat", item["city"], item["ref"], item["street"], "paczkomaty", state]
+    def parse_slug(self, item, street, state):
+        slug_parts = ["paczkomat", item["city"], item["ref"], street, "paczkomaty", state]
         slug = "-".join(map(lambda x: unidecode(x.lower().strip()), slug_parts))
         slug = re.sub(r"[·/_:; ]", "-", slug)
         slug = re.sub(r"[^a-z0-9 -]", "", slug)
