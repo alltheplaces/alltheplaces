294ae20c6908d874c192a5aac663cdde692031ee
Spencer Alves <impiaaa@gmail.com>
2024-07-18 17:34:54 -0700

    [Barrys] New spider (93 locations)

---
+++ locations/spiders/barrys.py
@@ -0,0 +1,14 @@
+from locations.storefinders.storemapper import StoremapperSpider
+
+
+class BarrysSpider(StoremapperSpider):
+    name = "barrys"
+    item_attributes = {
+        "brand_wikidata": "Q96373178",
+        "brand": "Barry's",
+    }
+    key = "5809"
+
+    def parse_item(self, item, location):
+        item["branch"] = item.pop("name")
+        yield item
