7ce429884c484ab4c2e4c9b68911e49b02bd3a03
IreuN <gatkowski.michal@gmail.com>
2024-07-24 16:24:51 +0200

    [allegro_one_box_pl] filter for only PL points, try to extract more data (#8928)
    * [allegro_one_box_pl] filter for only PL points, try to extract more data
    
    Not sure if it will be fast enough..
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update allegro_one_box_pl.py
    
    * Street contains also housenumber
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/allegro_one_box_pl.py
+++ locations/spiders/allegro_one_box_pl.py
@@ -1,7 +1,7 @@
 from typing import Iterable
 
 from scrapy import Request, Spider
-from scrapy.http import Response
+from scrapy.http import JsonRequest, Response
 
 from locations.dict_parser import DictParser
 
@@ -14,7 +14,7 @@ class AllegroOneBoxPLSpider(Spider):
     def start_requests(self) -> Iterable[Request]:
         yield Request(
             method="GET",
-            url="https://edge.allegro.pl/general-deliveries?nwLat=56.04770689192265&nwLon=2.5567803015399404&seLat=48.224919937057095&seLon=38.32826467653994&brandKeys=ALLEGRO_APM&clusterThreshold=900000000",
+            url="https://edge.allegro.pl/general-deliveries?nwLat=56.04770689192265&nwLon=2.5567803015399404&seLat=48.224919937057095&seLon=38.32826467653994&brandKeys=ALLEGRO_APM&deliveryMethods=0b257488-c85d-4507-b967-9b45ffbfa2e8&deliveryMethods=0aafb43c-e66a-46ec-9cc4-29bb39ebb483&sellerId=95826291&clusterThreshold=900000000",
             headers={"Accept": "application/vnd.allegro.public.v3+json"},
         )
 
@@ -22,7 +22,21 @@ class AllegroOneBoxPLSpider(Spider):
         data = response.json()
 
         for box in data["points"]:
-            properties = DictParser.parse(box)
-            del properties["name"]
-            properties["website"] = f"https://allegro.pl/kampania/one/znajdz-nas?pointId={box['id']}"
-            yield properties
+            yield JsonRequest(
+                url=f"https://edge.allegro.pl/general-deliveries/{box['id']}",
+                headers={"Accept": "application/vnd.allegro.public.v6+json"},
+                callback=self.parse2,
+            )
+
+    def parse2(self, response):
+        data = response.json()
+        properties = DictParser.parse(data)
+        del properties["name"]
+        properties["website"] = f"https://allegro.pl/kampania/one/znajdz-nas?pointId={data['id']}"
+        properties["ref"] = data["carrierPointId"]
+        properties["extras"]["description"] = data["description"]
+        properties["street_address"] = properties["street"]
+        properties["street"] = None
+        if data["openingTimesLabels"][0]["days"] == "całodobowo":
+            properties["opening_hours"] = "24/7"
+        yield properties
