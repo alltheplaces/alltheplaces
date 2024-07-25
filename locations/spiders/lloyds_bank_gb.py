17330912cce9c81d4d1107f0358a69a323381a81
Mateusz Konieczny <matkoniecz@gmail.com>
2024-07-25 14:52:01 +0200

    lloyds_bank_gb - skip call centers

--- locations/spiders/lloyds_bank_gb.py
+++ locations/spiders/lloyds_bank_gb.py
@@ -17,4 +17,6 @@ class LloydsBankGBSpider(SitemapSpider, StructuredDataSpider):
     def sitemap_filter(self, entries):
         for entry in entries:
             if "event" not in entry["loc"]:
+                if not entry["phone"].replace(" ", "").startswith("+443"):
+                    entry.pop("name", None)
                 yield entry
