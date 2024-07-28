b8bb4f0763b74233fbe9b093abd5d433a6d9e3bc
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-07-28 22:03:10 +0100

    Update hays_travel_gb.py

--- locations/spiders/hays_travel_gb.py
+++ locations/spiders/hays_travel_gb.py
@@ -1,13 +1,15 @@
+import re
 from typing import Any
-from urllib.parse import urljoin
+from datetime import datetime
 
 from scrapy.http import Response
 from scrapy.spiders import Spider
 
-# from locations.hours import DAYS_FULL, OpeningHours
+from urllib.parse import urljoin
+#from locations.hours import DAYS_FULL, OpeningHours
 from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
-
+from locations.pipelines.address_clean_up import clean_address
 
 class HaysTravelGBSpider(Spider):
     name = "hays_travel_gb"
@@ -15,17 +17,13 @@ class HaysTravelGBSpider(Spider):
     start_urls = ["https://www.haystravel.co.uk/umbraco/api/branches/getbranchlocatorcontent"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        locations = response.xpath('//li[contains(@class,"py-8")]')
-        for location in locations:
-            item = Feature()
-            url = location.xpath('//a[contains(@href,"branches")]/@href').get()
-            item["website"] = urljoin("https://www.haystravel.co.uk/", url)
-            item["name"] = location.xpath("@data-branch-title").get()
-            item["ref"] = url
-            coords = location.xpath("@data-branch-lat-lng").get()
-            item["lat"], item["lon"] = [c.strip() for c in coords.split(",")]
-            item["phone"] = location.xpath('//a[contains(@href, "tel:")]/@href').get().replace("tel:", "")
-            item["addr_full"] = merge_address_lines(
-                location.xpath('.//div[contains(@class,"rich-text")]//p//text()').getall()
-            )
+        for location in response.xpath('//li[contains(@class,"py-8")]'):
+            item=Feature()
+            item["ref"]=url=location.xpath('.//a[contains(@href,"branches")]/@href').get()
+            item["website"]=urljoin('https://www.haystravel.co.uk/',url)
+            item["name"]=location.xpath('@data-branch-title').get()
+            coords=location.xpath('@data-branch-lat-lng').get()
+            item["lat"],item["lon"]=[c.strip() for c in coords.split(",")]
+            item["phone"]=location.xpath('.//a[contains(@href, "tel:")]/@href').get().replace("tel:", "")
+            item["addr_full"] = merge_address_lines(location.xpath('.//div[contains(@class,"rich-text")]//p//text()').getall())
             yield item
