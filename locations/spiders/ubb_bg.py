74b301e11c917c549a530d3932207be670826f0e
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-07-28 21:02:44 +0300

    Added default Accept header

--- locations/spiders/ubb_bg.py
+++ locations/spiders/ubb_bg.py
@@ -14,7 +14,7 @@ class UbbBGSpider(Spider):
     item_attributes = {"brand": "Обединена българска банка", "brand_wikidata": "Q7887555"}
     start_urls = ["https://www.ubb.bg/offices/pins"]
     user_agent = FIREFOX_LATEST
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    custom_settings = {"ROBOTSTXT_OBEY": False, "DEFAULT_REQUEST_HEADERS": {"Accept": "application/json"}}
 
     def parse(self, response, **kwargs):
         logging.info(response.body)
