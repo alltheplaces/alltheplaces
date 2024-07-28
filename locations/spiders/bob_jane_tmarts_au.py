201087e848c67d1a1d00c6823a911f98aa9f4285
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-07-28 03:39:51 +0000

    Parse coords

--- locations/spiders/bob_jane_tmarts_au.py
+++ locations/spiders/bob_jane_tmarts_au.py
@@ -1,3 +1,5 @@
+import re
+
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
@@ -11,3 +13,17 @@ class BobJaneTmartsAUSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [
         (r"shop/.*$", "parse_sd"),
     ]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["lat"], item["lon"] = self.parse_lat_lon(response)
+        yield from self.inspect_item(item, response)
+
+    @staticmethod
+    def parse_lat_lon(response):
+        # Raw text is: location&quot;:&quot;-37.99739838,145.22099304&quot
+        props = response.xpath('//div[@data-react-class="ShopHeader"]/@data-react-props').get().replace("&quot;", '"')
+        match = re.search(r'"location":"([\-\d\.]+),([\d\.]+)"', props)
+        if match:
+            return (float(match.group(1)), float(match.group(2)))
+        else:
+            return (None, None)
