0bca430366536abe12c317c53db2967d9910d7d6
Miha Frangež <miha.frangez@gmail.com>
2024-07-29 15:06:41 +0200

    [posta_si] Use the data instead of search endpoint

--- locations/spiders/posta_si.py
+++ locations/spiders/posta_si.py
@@ -1,7 +1,7 @@
 import scrapy
-from locations.categories import Categories, apply_category, apply_yes_no
-from locations.hours import DAYS_FULL, OpeningHours
 
+from locations.categories import Categories, apply_category, apply_yes_no
+from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
 
 
@@ -12,8 +12,9 @@ class PostaSpider(scrapy.Spider):
 
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
+    # There is no "show all" option, but we can search for every letter to get all of them
     start_urls = [
-        "https://www.posta.si/_vti_bin/PostaSI/PostOffices/PostOfficesService.svc/SearchPostOffices/" + c + "/"
+        "https://www.posta.si/_vti_bin/PostaSI/PostOffices/PostOfficesService.svc/PostalOffice/" + c
         for c in "abcčdefghijklmnoprsštuvzž"
     ]
 
@@ -21,43 +22,50 @@ class PostaSpider(scrapy.Spider):
 
     def parse(self, response):
         for posta in response.json():
-            if posta["PostOfficeId"] in self.ids_seen:
+
+            # Skip everything that isn't a post office (newsagents, petrol stations...)
+            # 0 = Pošta, 1 = BS petrol, 2 = PS Paketomat, 4 = BS MOL, 7 = TRAFIKA 3DVA
+            if posta["VrstaEnote"] != 0:
                 continue
-            self.ids_seen.add(posta["PostOfficeId"])
+
+            # Units with no physical location, only used for administrative purposes
+            if "Pin" not in posta:
+                continue
+
+            # Skip already-seen post offices
+            if posta["PostalId"] in self.ids_seen:
+                continue
+            self.ids_seen.add(posta["PostalId"])
 
             feature = Feature(
-                ref=posta["PostOfficeId"],
-                name=posta.get("Title"),
-                lat=posta["Geo"]["Lat"],
-                lon=posta["Geo"]["Lng"],
-                street_address=posta["Address"],
+                ref=posta["PostalId"],
+                name=posta["PostalClassNumAndTitle"],
+                lat=posta["Pin"]["GPSx"],
+                lon=posta["Pin"]["GPSy"],
+                street_address=posta["PostalAddress"],
                 country="SI",
                 phone=posta.get("FirstPhone"),
-                opening_hours=self.parse_open_hours(posta["WorkTimes"] or {}),
+                opening_hours=self.parse_open_hours(posta["FormatWorkingDays"]),
             )
 
             apply_category(Categories.POST_OFFICE, feature)
 
-            services = [service["ServiceId"] for service in posta["Services"]]
-
-            apply_yes_no("sells:lottery", feature, 8 in services)
-            apply_yes_no("services:copy", feature, 9 in services)
-
-            if 2 in services:
-                apply_category(Categories.BUREAU_DE_CHANGE, feature)
+            # Some post offices offer additional services. Descriptions are next to the IDs in the JSON reposne ("ServiceTitle")
+            service_ids = [service["PostalServiceId"] for service in posta["PostalOfficeServices"]]
+            apply_yes_no("sells:lottery", feature, 11 in service_ids)  # 11 = Igre na srečo Loterije Slovenije
+            apply_yes_no("services:copy", feature, 12 in service_ids)  # 12 = Fotokopiranje
 
             yield feature
 
-    def parse_open_hours(self, obj):
+    def parse_open_hours(self, lst):
         opening_hours = OpeningHours()
 
-        for day in DAYS_FULL:
-            times = obj.get(day + "WorkTime")
-            if not times:
-                continue
-            if times["From1"]:
-                opening_hours.add_range(day=day[:2], open_time=times["From1"], close_time=times["To1"])
-            if times["From2"]:
-                opening_hours.add_range(day=day[:2], open_time=times["From2"], close_time=times["To2"])
+        for times in lst:
+            day = DAYS[times["Day"] - 1]
+
+            if times["WorkingTimeFrom"]:
+                opening_hours.add_range(day=day, open_time=times["WorkingTimeFrom"], close_time=times["WorkingTimeTo"])
+            if times["OtherTimeFrom"]:
+                opening_hours.add_range(day=day, open_time=times["OtherTimeFrom"], close_time=times["OtherTimeTo"])
 
         return opening_hours.as_opening_hours()
