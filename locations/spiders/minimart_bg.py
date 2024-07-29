126ffd6c39fc41766ec3fcd7bee08bb7bddaf907
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-07-29 16:20:56 +0300

    Update minimart_bg.py

--- locations/spiders/minimart_bg.py
+++ locations/spiders/minimart_bg.py
@@ -1,4 +1,4 @@
-from scrapy import Request, Selector, Spider
+from scrapy import Request, Spider
 
 from locations.google_url import url_to_coords
 from locations.items import Feature
@@ -8,22 +8,22 @@ class MinimartBGSpider(Spider):
     name = "minimart_bg"
     item_attributes = {"brand": "Minimart", "brand_wikidata": "Q119168386"}
     allowed_domains = ["mini-mart.bg", "goo.gl"]
-    start_urls = ["https://mini-mart.bg/nameri-magazin"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}  # Ignore www.google.com robots.txt
+    start_urls = ["https://mini-mart.bg/nameri-magazin/"]
+    custom_settings = {"REDIRECT_ENABLED": False}
+    handle_httpstatus_list = [302]
     no_refs = True
 
-    def parse(self, response):
-        locations = response.xpath("//div[contains(@class, 'et_pb_with_border')]").getall()
-        for location in locations:
-            htmlLocation = Selector(text=location)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.xpath("//div[contains(@class, 'et_pb_with_border')]"):
             item = Feature()
-            item["addr_full"] = htmlLocation.xpath(".//p//text()").get()
-            location_url = htmlLocation.xpath(".//a/@href").get()
+            item["addr_full"] = location.xpath(".//p//text()").get()
+            location_url = location.xpath(".//a/@href").get()
             if location_url is not None:
-                yield Request(url=location_url, meta={"item": item}, callback=self.parse_maps_url)
+                yield Request(method="HEAD", url=location_url, meta={"item": item}, callback=self.parse_maps_url)
 
     # Follow shortened URL to find full Google Maps URL containing coordinates
     def parse_maps_url(self, response):
         item = response.meta["item"]
-        item["lat"], item["lon"] = url_to_coords(response.url)
+        item["lat"], item["lon"] = url_to_coords(str(response.headers["Location"]))
         yield item
+
