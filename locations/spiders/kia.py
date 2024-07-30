cd62345091d76739b8f51c032fa207ffcf4c59b0
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-07-31 00:46:15 +0530

    Code clean up

--- locations/spiders/kia.py
+++ locations/spiders/kia.py
@@ -7,24 +7,27 @@ class KiaSpider(scrapy.Spider):
     name = "kia"
     item_attributes = {"brand": "Kia", "brand_wikidata": "Q35349"}
     start_urls = [
-        "https://www.kia.com/api/bin/dealer?locale=at-de&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=fr-fr&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=de-de&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=es-es&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=nl-nl&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=be-nl&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=cz-cz&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=dk-da&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=fi-fi&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=gr-el&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=hu-hu&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=ie-en&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=it-it&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=lu-fr&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=no-nn&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=se-sv&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=pl-pl&program=dealerLocatorSearch",
-        "https://www.kia.com/api/bin/dealer?locale=sk-sk&program=dealerLocatorSearch",
+        f"https://www.kia.com/api/bin/dealer?locale={locale}&program=dealerLocatorSearch"
+        for locale in [
+            "at-de",
+            "fr-fr",
+            "de-de",
+            "es-es",
+            "nl-nl",
+            "be-nl",
+            "cz-cz",
+            "dk-da",
+            "fi-fi",
+            "gr-el",
+            "hu-hu",
+            "ie-en",
+            "it-it",
+            "lu-fr",
+            "no-nn",
+            "se-sv",
+            "pl-pl",
+            "sk-sk",
+        ]
     ]
 
     def parse(self, response, **kwargs):
