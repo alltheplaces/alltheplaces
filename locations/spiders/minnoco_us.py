04e6107ce425a2bdc8e93c6cf2ce94536a83b207
Ian Dees <ian.dees@gmail.com>
2024-07-31 17:53:19 -0500

    Fix category

--- locations/spiders/minnoco_us.py
+++ locations/spiders/minnoco_us.py
@@ -4,7 +4,7 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 class MinnocoUSSpider(WPStoreLocatorSpider):
     name = "minnoco_us"
-    item_attributes = {"brand": "Minnoco", "extras": Categories.FUEL_STATION}
+    item_attributes = {"brand": "Minnoco", "extras": Categories.FUEL_STATION.value}
     allowed_domains = ["minnoco.com"]
     start_urls = ["https://www.minnoco.com/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
     time_format = "%I:%M %p"
