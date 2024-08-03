482b6264d5d6a4fbf605b8fec1755eb3ecc2e91d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-03 12:27:22 +0000

    Add first pass of EZ Mart

--- locations/spiders/ez_mart_us.py
+++ locations/spiders/ez_mart_us.py
@@ -1,7 +1,9 @@
 from locations.storefinders.wp_go_maps import WpGoMapsSpider
+from locations.categories import Categories
 
 
 class EzMartUSSpider(WpGoMapsSpider):
+    item_attributes = {"brand": "EZ Mart", "category": Categories.SHOP_CONVENIENCE}
     name = "ez_mart_us"
     allowed_domains = [
         "blarneycastleoil.com",
