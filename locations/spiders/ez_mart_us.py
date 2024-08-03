ed971124c57a17d56023df63f5303c47c9c0eea7
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-03 22:28:13 +0930

    Update locations/spiders/ez_mart_us.py

--- locations/spiders/ez_mart_us.py
+++ locations/spiders/ez_mart_us.py
@@ -3,7 +3,7 @@ from locations.storefinders.wp_go_maps import WpGoMapsSpider
 
 
 class EzMartUSSpider(WpGoMapsSpider):
-    item_attributes = {"brand": "EZ Mart", "category": Categories.SHOP_CONVENIENCE}
+    item_attributes = {"brand": "EZ Mart", "extras": Categories.SHOP_CONVENIENCE.value}
     name = "ez_mart_us"
     allowed_domains = [
         "blarneycastleoil.com",
