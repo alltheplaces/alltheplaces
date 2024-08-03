8690891c05fa5749ef63dfaa1337507ca1344530
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-03 12:54:55 +0000

    Add Suzuki Marine

--- locations/spiders/suzuki_marine_au.py
+++ locations/spiders/suzuki_marine_au.py
@@ -1,21 +1,17 @@
 from scrapy import Spider
 from scrapy.http import JsonRequest
 
-from locations.hours import OpeningHours
 from locations.categories import Categories
 from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+
 
 class SuzukiMarineAU(Spider):
     name = "suzuki_marine_au"
-    item_attributes = {
-        "brand": "Suzuki",
-        "brand_wikidata": "Q181642",
-        "extras": Categories.SHOP_BOAT_REPAIR.value
-    }
+    item_attributes = {"brand": "Suzuki", "brand_wikidata": "Q181642", "extras": Categories.SHOP_BOAT_REPAIR.value}
 
     def start_requests(self):
         yield JsonRequest(url="https://www.suzukimarine.com.au/find-dealers/dealersByState?state=all")
-
 
     def parse(self, response, **kwargs):
         for feature in response.json():
@@ -25,7 +21,7 @@ class SuzukiMarineAU(Spider):
 
             if "ServiceHours" in feature:
                 item["opening_hours"] = OpeningHours()
-                
+
                 # "ServiceHours": "<p>Mon - Fri: 8:00am - 5:00pm<br>Sat: 8:00am - 12:00pm<br>Sun: Closed</p>",
                 # item["opening_hours"].add_ranges_from_string(feature["ServiceHours"].replace("<br>", ", "))
             if "LatLong" in feature:
