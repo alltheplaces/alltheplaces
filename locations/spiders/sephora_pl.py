86e7dc13b205def3063c6d15224767f7980c45a6
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2024-08-05 03:28:58 +0530

    [SephoraUSCAPL] Add PL locations and apply country code fixes (1651 locations) (#9121)
    * Add PL and fix country for CA
    
    * Add Spephora PL Spider
    
    * rename sephora_us_ca_pl to sephora_us_ca and fixes country code
    
    * removed redundant code
    
    * Fix class name
    
    ---------
    
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/sephora_pl.py
+++ locations/spiders/sephora_pl.py
@@ -1,5 +1,3 @@
-import json
-
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
@@ -8,18 +6,15 @@ from locations.structured_data_spider import StructuredDataSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class SephoraUSCASpider(SitemapSpider, StructuredDataSpider):
-    name = "sephora_us_ca"
+class SephoraPLSpider(SitemapSpider, StructuredDataSpider):
+    name = "sephora_pl"
     item_attributes = {"brand": "Sephora", "brand_wikidata": "Q2408041"}
     sitemap_urls = [
-        "https://www.sephora.com/sephora-store-sitemap.xml",
-        "https://www.sephora.com/sephora-store-sitemap_en-CA.xml",
+        "https://www.sephora.pl/sitemap-store-locator.xml",
     ]
-    sitemap_rules = [
-        (r"\/happening\/stores\/(?!kohls).+", "parse_sd"),
-        (r"\/ca\/en\/happening\/stores\/(?!kohls).+", "parse_sd"),
-    ]
+
     user_agent = BROWSER_DEFAULT
+    require_proxy = True
 
     def post_process_item(self, item, response, ld_data):
         item.pop("image")
@@ -27,8 +22,4 @@ class SephoraUSCASpider(SitemapSpider, StructuredDataSpider):
         item["opening_hours"] = OpeningHours()
         item["opening_hours"].add_ranges_from_string(hours_string)
         apply_category(Categories.SHOP_COSMETICS, item)
-        if "sephora.com" in response.url:
-            item["country"] = json.loads(response.xpath('//*[@id="linkStore"]/text()').get())["ssrProps"][
-                "ErrorBoundary(ReduxProvider(StoreDetail))"
-            ]["stores"][0]["address"]["country"]
         yield item
