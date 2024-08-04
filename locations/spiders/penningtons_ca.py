f0f6fc0242c6346dad9b47d2c493838b4224a9da
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-04 03:57:17 +0000

    Add Penningtons (CA)

---
+++ locations/spiders/penningtons_ca.py
@@ -0,0 +1,16 @@
+from locations.storefinders.sweetiq import SweetIQSpider
+
+
+class PenningtonsCASpider(SweetIQSpider):
+    name = "penningtons_ca"
+    item_attributes = {
+        "brand_wikidata": "Q16956527",
+        "brand": "Penningtons",
+    }
+    allowed_domains = [
+        "locations.penningtons.com",
+        "sls-api-service.sweetiq-sls-production-east.sweetiq.com"
+    ]
+    start_urls = [
+        "https://locations.penningtons.com/en",
+    ]
