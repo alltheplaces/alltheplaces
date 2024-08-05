cf78b51446146aeda274ffde3d9408ee3c4f0206
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-05 10:23:21 +0000

    Add Gong Cha (US)

--- locations/spiders/gong_cha_us.py
+++ locations/spiders/gong_cha_us.py
@@ -3,8 +3,5 @@ from locations.storefinders.storepoint import StorepointSpider
 
 class GongChaUSSpider(StorepointSpider):
     name = "gong_cha_us"
-    item_attributes = {
-        "brand": "Gong Cha",
-        "brand_wikidata": "Q5581670"
-    }
+    item_attributes = {"brand": "Gong Cha", "brand_wikidata": "Q5581670"}
     key = "161d1d5abc0b7c"
