5dfe8fba678702694dc50ce2fbe00325c48ac74f
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-07 18:00:12 +0930

    Update and rename soneczko_pl.py to sloneczko_pl.py

--- locations/spiders/sloneczko_pl.py
+++ locations/spiders/sloneczko_pl.py
@@ -4,8 +4,8 @@ from locations.geo import point_locations
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class SoneczkoPLSpider(WPStoreLocatorSpider):
-    name = "soneczko_pl"
+class SloneczkoPLSpider(WPStoreLocatorSpider):
+    name = "sloneczko_pl"
     item_attributes = {
         "brand_wikidata": "Q113230439",
         "brand": "Słoneczko",
