e43e09dcba9525418ea77358109c4fce338b6a4d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-07 23:26:48 +0000

    Add WIP

--- locations/spiders/bonita.py
+++ locations/spiders/bonita.py
@@ -1,9 +1,7 @@
-from chompjs import parse_js_object
 from scrapy import Spider
 
-from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_SI, OpeningHours
+from locations.hours import OpeningHours
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
@@ -16,7 +14,6 @@ class BonitaSpider(Spider):
         "https://www.bonita.de/de/de/shop_api/app/store_finder/search.json?country=NL&distance=10000",
         "https://www.bonita.de/de/de/shop_api/app/store_finder/search.json?country=AT&distance=10000",
         "https://www.bonita.de/de/de/shop_api/app/store_finder/search.json?country=CH&distance=10000",
-
     ]
 
     def parse(self, response):
@@ -25,7 +22,7 @@ class BonitaSpider(Spider):
             item["ref"] = location["id"]
             item["name"] = None
 
-            item["street"] =  merge_address_lines([location["address_line_1"], location["address_line_2"]])
+            item["street"] = merge_address_lines([location["address_line_1"], location["address_line_2"]])
             item["opening_hours"] = OpeningHours()
             if location["opening_hours_monday"]:
                 item["opening_hours"].add_ranges_from_string("Monday " + location["opening_hours_monday"])
