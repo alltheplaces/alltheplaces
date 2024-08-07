5f4937261fa03e187881a1b9b21c4be49b04f15c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-07 08:23:14 +0000

    Add Nasz Sklep (PL)

---
+++ locations/spiders/nasz_sklep_pl.py
@@ -0,0 +1,38 @@
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from scrapy import Selector, Spider
+from scrapy.http import JsonRequest
+
+from locations.dict_parser import DictParser
+from locations.geo import point_locations
+from locations.hours import DAYS_BY_FREQUENCY, OpeningHours, sanitise_day
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
+
+
+class NaszSklepPLSpider(WPStoreLocatorSpider):
+    name = "nasz_sklep_pl"
+    item_attributes = {
+        "brand_wikidata": "Q62070369",
+        "brand": "Nasz Sklep",
+    }
+    allowed_domains = [
+        "nasz-sklep.pl",
+    ]
+    searchable_points_files = [
+        "eu_centroids_20km_radius_country.csv",
+    ]
+    time_format = "%I:%M %p"
+    max_results = 50
+    search_radius = 20
+
+    def start_requests(self):
+        if len(self.start_urls) == 0 and hasattr(self, "allowed_domains"):
+            for domain in self.allowed_domains:
+                if len(self.searchable_points_files) > 0 and self.search_radius != 0 and self.max_results != 0:
+                    for searchable_points_file in self.searchable_points_files:
+                        for lat, lon in point_locations(searchable_points_file, "PL"):
+                            yield JsonRequest(
+                                url=f"https://{domain}/wp-admin/admin-ajax.php?action=store_search&lat={lat}&lng={lon}&max_results={self.max_results}&search_radius={self.search_radius}"
+                            )
+                else:
+                    yield JsonRequest(url=f"https://{domain}/wp-admin/admin-ajax.php?action=store_search&autoload=1")
