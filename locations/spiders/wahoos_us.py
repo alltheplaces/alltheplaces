1c14a278c35fa50f32499243504d8f4d446e317a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-07 02:44:11 +0000

    Add Wahoo's

---
+++ locations/spiders/wahoos_us.py
@@ -0,0 +1,18 @@
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
+from locations.items import set_closed
+
+class WahoosUSSpider(AgileStoreLocatorSpider):
+    name = "wahoos_us"
+    item_attributes = {"brand": "Wahoo's Fish Tacos", "brand_wikidata": "Q7959827"}
+    start_urls = [
+        "https://www.wahoos.com/wp-admin/admin-ajax.php?action=asl_load_stores&nonce=33daf6dd9c&load_all=1&layout=1"
+    ]
+
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        if "closed" in item["name"]:
+            set_closed(item)
+        if " (" in item["name"]:
+            item["name"] = item["name"].split(" (")[0]
+        yield item
