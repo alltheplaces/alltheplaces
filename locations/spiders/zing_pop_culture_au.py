d21e0e9398a44656abd95ca72d3b4cf561a08f99
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-07 18:17:33 +0930

    Update zing_pop_culture_au.py

--- locations/spiders/zing_pop_culture_au.py
+++ locations/spiders/zing_pop_culture_au.py
@@ -5,10 +5,10 @@ from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class EbGamesAUSpider(SitemapSpider, StructuredDataSpider):
-    name = "eb_games_au"
-    item_attributes = {"brand": "EB Games", "brand_wikidata": "Q5322604"}
-    sitemap_urls = ["https://www.ebgames.com.au/sitemap-stores.xml"]
+class ZingPopCultureAUSpider(SitemapSpider, StructuredDataSpider):
+    name = "zing_pop_culture_au"
+    item_attributes = {"brand": "Zing Pop Culture Australia", "brand_wikidata": "Q23023711"}
+    sitemap_urls = ["https://www.zingpopculture.com.au/sitemap-stores.xml"]
     sitemap_rules = [(r"/stores/store/(\d+)-[-\w]+$", "parse_sd")]
     is_playwright_spider = True
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
