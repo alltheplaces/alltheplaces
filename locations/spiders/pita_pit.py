6c4809c187577b6117a6a8dad49b68a8425d92ae
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-07 06:18:05 +0000

    Add Pita Pit (CA)

---
+++ locations/spiders/pita_pit.py
@@ -0,0 +1,25 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class PitaPitSpider(SitemapSpider, StructuredDataSpider):
+    name = "pita_pit"
+    item_attributes = {
+        "brand_wikidata": "Q7757289",
+        "brand": "Pita Pit",
+    }
+    allowed_domains = [
+        "pitapit.ca",
+    ]
+
+    sitemap_urls = ["https://pitapit.ca/sitemap_index.xml"]
+    sitemap_rules = [(r"https://pitapit.ca/restaurants/\w+/$", "parse_sd")]
+    wanted_types = ["Restaurant"]
+
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if "| " in name:
+            item["name"] = item["name"].split(" | ")[0].replace("Health Food Restaurant at ", "")
+
+        yield item
