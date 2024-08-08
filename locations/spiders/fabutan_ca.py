77935d9227be569c2be9216873a0f2511ebb5f7d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-08 22:46:47 +0000

    Add Fabutan (CA)

---
+++ locations/spiders/fabutan_ca.py
@@ -0,0 +1,16 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class FabutanCASpider(SitemapSpider, StructuredDataSpider):
+    name = "fabutan_ca"
+    item_attributes = {"brand_wikidata": "Q120765494"}
+    sitemap_urls = ["https://fabutan.com/sitemap.xml"]
+    sitemap_rules = [
+        (
+            r"/locations/[\w-]+/[\w-]+/[\w-]+/$",
+            "parse_sd",
+        )
+    ]
