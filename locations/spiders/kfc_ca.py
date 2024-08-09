b5764931e173bd62e40612ffb71ac11ee7448ac1
arrival-spring <lnnunns+git@gmail.com>
2024-08-09 17:17:33 +0200

    Fix TH name, remove  proxy requirements

--- locations/spiders/kfc_ca.py
+++ locations/spiders/kfc_ca.py
@@ -7,3 +7,4 @@ class KfcCASpider(KfcAUSpider):
     region_code = "na"
     tenant_id = "a087813cef074625a8341e162356a1e5"
     web_root = None
+    requires_proxy = False
--- locations/spiders/kfc_th.py
+++ locations/spiders/kfc_th.py
@@ -2,8 +2,9 @@ from locations.spiders.kfc_au import KfcAUSpider
 
 
 class KfcTHSpider(KfcAUSpider):
-    name = "kfc_za"
+    name = "kfc_th"
 
     region_code = "apac"
     tenant_id = "59dhhptudcn7hk1ogssvsb4cujvbcnh6o"
     web_root = "https://www.kfc.co.th/restaurants/"
+    requires_proxy = False
