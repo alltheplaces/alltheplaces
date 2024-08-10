52a1bab93296e01863b51fd57f5e3fae8751ffca
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-10 04:02:55 +0000

    Refactor to OpenGraphSpider

--- locations/spiders/mcdonalds_baltics.py
+++ locations/spiders/mcdonalds_baltics.py
@@ -1,10 +1,10 @@
 import scrapy
 
-from locations.open_graph_spider import OpenGraphParser
+from locations.open_graph_spider import OpenGraphSpider
 from locations.spiders.mcdonalds import McdonaldsSpider
 
 
-class McdonaldsBalticsSpider(scrapy.spiders.SitemapSpider, OpenGraphParser):
+class McdonaldsBalticsSpider(scrapy.spiders.SitemapSpider, OpenGraphSpider):
     name = "mcdonalds_baltics"
     custom_settings = {"ROBOTSTXT_OBEY": False}
     item_attributes = McdonaldsSpider.item_attributes
