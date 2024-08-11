d8c6be01ed911f81c1a2c1ac0a47b234d94c9b46
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-11 03:40:47 +0000

    Add post_process_item

--- locations/storefinders/closeby.py
+++ locations/storefinders/closeby.py
@@ -22,7 +22,7 @@ class ClosebySpider(Spider):
 
     def post_process_item(self, item, response, location):
         """Override with any post-processing on the item."""
-        return item
+        yield item
 
     def pre_process_data(self, location, **kwargs):
         """Override with any pre-processing on the item."""
