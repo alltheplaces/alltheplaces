0bef134c3ab402e9d4bb91bf52a38cc295c7895b
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-11 19:38:29 +0930

    Update storemapper.py

--- locations/storefinders/storemapper.py
+++ locations/storefinders/storemapper.py
@@ -8,6 +8,8 @@ class StoremapperSpider(Spider):
     """
     Storemapper (https://www.storemapper.com/) is an medded map based store locator.
 
+    API docs are available via https://help.storemapper.com/category/4313-advanced-settings-and-customisation
+
     Use by specifying a `key`
     """
     dataset_attributes = {"source": "api", "api": "storemapper.com"}
