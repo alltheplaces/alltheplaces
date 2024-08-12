91d1c1bedd256b45783d6528f997351a338ba902
Spencer Alves <impiaaa@gmail.com>
2024-08-12 14:52:49 -0700

    Make regex a raw string

--- locations/spiders/best_buy.py
+++ locations/spiders/best_buy.py
@@ -9,7 +9,7 @@ class BestBuySpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand_wikidata": "Q533415", "extras": Categories.SHOP_ELECTRONICS.value}
     allowed_domains = ["stores.bestbuy.com"]
     sitemap_urls = ["https://stores.bestbuy.com/sitemap.xml"]
-    sitemap_rules = [("/[a-z]{2}/[\w-]+/[\w-]+.html", "parse_sd")]
+    sitemap_rules = [(r"/[a-z]{2}/[\w-]+/[\w-]+.html", "parse_sd")]
     search_for_image = False
 
     def post_process_item(self, item, response, ld_data, **kwargs):
