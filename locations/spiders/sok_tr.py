08e645b74138a248ff115cd9d1f83fe8dd03a07f
Oğuz Kurt <kurt.oguz@outlook.com>
2024-08-12 11:34:23 +0300

    add sok_tr spider

--- locations/spiders/sok_tr.py
+++ locations/spiders/sok_tr.py
@@ -11,33 +11,29 @@ PROVINCES_URL = "https://localhost:8080/ajax/servis/sehirler"
 DISTRICTS_URL = "https://localhost:8080/ajax/servis/ilceler?city={province}"
 STORES_URL = "https://localhost:8080/ajax/servis/magazalarimiz?city={province}&district={district}"
 
+
 class SokTRSpider(scrapy.Spider):
     name = "sok_tr"
     item_attributes = {"brand": "ŞOK", "brand_wikidata": "Q19613992"}
 
     def start_requests(self) -> Iterable[scrapy.Request]:
-        yield scrapy.Request(
-            url=PROVINCES_URL,
-            callback=self.parse_provinces
-        )
-    
+        yield scrapy.Request(url=PROVINCES_URL, callback=self.parse_provinces)
+
     def parse_provinces(self, response):
         for province in response.json()["cities"]:
             yield scrapy.Request(
-                url=DISTRICTS_URL.format(province=province),
-                callback=self.parse_districts,
-                meta={"province": province}
+                url=DISTRICTS_URL.format(province=province), callback=self.parse_districts, meta={"province": province}
             )
-   
+
     def parse_districts(self, response):
         province = response.meta["province"]
         for district in response.json()["districts"]:
             yield scrapy.Request(
                 url=STORES_URL.format(province=province, district=district),
                 callback=self.parse_stores,
-                meta={"province": province, "district": district}
+                meta={"province": province, "district": district},
             )
-    
+
     def parse_stores(self, response):
         for store in response.json()["response"]["subeler"]:
             item = DictParser.parse(store)
@@ -65,7 +61,7 @@ class SokTRSpider(scrapy.Spider):
             return float(n.replace(",", "."))
         except ValueError:
             return None
-         
+
     @staticmethod
     def parse_branch_name(branch_name: str) -> tuple[str, str]:
         mini_re = re.compile(r"ŞOK.[\s]*MİNİ")
@@ -87,9 +83,9 @@ class SokTRSpider(scrapy.Spider):
 
         except ValueError:
             name = "ŞOK"
-            
+
         return name, branch_name
-    
+
 
 def cut(text: str, start: int, end: int) -> str:
     part1 = text[:start].strip()
