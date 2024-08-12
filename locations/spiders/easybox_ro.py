4267cae9e14859fc59f06463285e7e88734f1dce
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-08-12 11:36:52 +0300

    Update easybox_ro.py

--- locations/spiders/easybox_ro.py
+++ locations/spiders/easybox_ro.py
@@ -4,4 +4,4 @@ from locations.spiders.easybox_bg import EasyboxBGSpider
 class EasyboxROSpider(EasyboxBGSpider):
     name = "easybox_ro"
     allowed_domains = ["sameday.ro"]
-    start_urls = ["sameday.ro/wp/wp-admin/admin-ajax.php?action=get_ooh_lockers_request&country=Romania"]
+    start_urls = ["https://sameday.ro/wp/wp-admin/admin-ajax.php?action=get_ooh_lockers_request&country=Romania"]
