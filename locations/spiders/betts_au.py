b629654cd8b7af7efd89e6ecf7c5872dd64e2920
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-12 10:19:36 +0000

    Remove country_list

--- locations/spiders/betts_au.py
+++ locations/spiders/betts_au.py
@@ -6,6 +6,5 @@ class BettsAUSpider(MetaLocatorSpider):
     name = "betts_au"
     item_attributes = {"brand": "Betts", "brand_wikidata": "Q118555401"}
     brand_id = "6176"
-    country_list = ["Australia"]
 
     apply_category(Categories.SHOP_SHOES, item_attributes)
