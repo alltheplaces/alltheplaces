0d6b601ab7172f63a4131b80a0028c77f928f4ae
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-08-12 11:51:57 +0300

    Update praktiker_bg.py

--- locations/spiders/praktiker_bg.py
+++ locations/spiders/praktiker_bg.py
@@ -3,6 +3,6 @@ from locations.spiders.technopolis_bg import TechnopolisBGSpider
 
 class PraktikerBGSpider(TechnopolisBGSpider):
     name = "praktiker_bg"
-    item_attributes = {"brand": "Praktiker", "brand_wikidata": "Q110399491"}
+    item_attributes = {"brand": "Praktiker", "brand_wikidata": "Q110399491", "shop": "doityourself"}
     allowed_domains = ["praktiker.bg"]
     start_urls = ["https://api.praktiker.bg/videoluxcommercewebservices/v2/praktiker/mapbox/customerpreferedstore"]
