ee303893869d6271e04db2e783f6ecbcdbc84c4d
David Hicks <david@hicks.id.au>
2024-08-12 13:44:36 +1000

    rosauers_us: reintroduce map_id to filter unrelated features

--- locations/spiders/rosauers_us.py
+++ locations/spiders/rosauers_us.py
@@ -5,3 +5,4 @@ class RosauersUSSpider(WpGoMapsSpider):
     name = "rosauers_us"
     item_attributes = {"brand": "Rosauers Supermarkets", "brand_wikidata": "Q7367458"}
     allowed_domains = ["www.rosauers.com"]
+    map_id = 1
