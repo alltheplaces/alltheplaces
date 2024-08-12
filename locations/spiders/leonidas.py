f695e0e77ce7fa06f4eb83c40c3f89289b79ea41
MerylStreet <e.kergrene@gmail.com>
2024-08-12 21:49:57 +0200

    [Leonidas] localised url (#9534)
    * Update leonidas.py localised url
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove unused website variable
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/leonidas.py
+++ locations/spiders/leonidas.py
@@ -12,12 +12,26 @@ class LeonidasSpider(scrapy.Spider):
         "https://www.leonidas.com/en/jsonapi/node/shops?1=1&filter[lat][condition][path]=field_shop_address_latitude&filter[lat][condition][operator]=BETWEEN&filter[lat][condition][value][]=50&filter[lat][condition][value][]=90&filter[lng][condition][path]=field_shop_address_longitude&filter[lng][condition][operator]=BETWEEN&filter[lng][condition][value][]=-179&filter[lng][condition][value][]=179&fields[node--shops]=status,title,drupal_internal__nid,field_shop_address_city,field_shop_address_country,field_shop_address_street1,field_shop_address_street2,field_shop_address_postal,field_shop_address_latitude,field_shop_address_longitude,field_shop_address_country,field_shop_address_latitude,field_shop_days,field_shop_email_2,field_shop_tel,path",
     ]
 
+    language_url_map = {
+        "FR": ("fr_fr", "/boutiques/"),
+        "NL": ("nl_nl", "/winkels/"),
+        "DE": ("de_de", "/boutiquen/"),
+    }
+
+    def construct_website_url(self, country, alias):
+        if not alias:
+            return None
+
+        lang, path_replacement = self.language_url_map.get(country, ("en", "/shops/"))
+        alias = alias.replace("/shops/", path_replacement)
+        return f"https://www.leonidas.com/{lang}{alias}"
+
     def parse(self, response, **kwargs):
         for store in response.json().get("data"):
             attributes = store.get("attributes")
+
             opening_hours = OpeningHours()
             ohs = attributes.get("field_shop_days").rstrip("|").split("|")
-            website = attributes.get("path").get("alias")
             for i, oh in enumerate(ohs):
                 if "0::'" in oh:
                     continue
@@ -33,6 +47,7 @@ class LeonidasSpider(scrapy.Spider):
                     opening_hours.add_range(DAYS[i], opening_mid_hour, closing_hour)
                 else:
                     opening_hours.add_range(DAYS[i], opening_hour, closing_hour)
+
             yield Feature(
                 {
                     "ref": attributes.get("path").get("pid"),
@@ -43,7 +58,9 @@ class LeonidasSpider(scrapy.Spider):
                     "postcode": store.get("field_shop_address_postal"),
                     "phone": attributes.get("field_shop_tel"),
                     "email": attributes.get("field_shop_email_2"),
-                    "website": f"https://www.leonidas.com/en{website}" if website else None,
+                    "website": self.construct_website_url(
+                        attributes.get("field_shop_address_country"), attributes.get("path").get("alias")
+                    ),
                     "lat": attributes.get("field_shop_address_latitude"),
                     "lon": attributes.get("field_shop_address_longitude"),
                     "opening_hours": opening_hours.as_opening_hours(),
