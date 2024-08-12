1d737dc4c74cbb77ea00397011fa4c26219fcf77
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-13 01:34:00 +0930

    Refactoring: Backport EasyLocator changes plus clean up naming (Autospidergen branch) (#9520)
    * Refactoring: Backport EasyLocator changes plus clean up naming
    
    * Refactoring: Backport EasyLocator changes plus clean up naming
    
    * Update locations/spiders/gigis_cupcakes_us.py

--- locations/spiders/gigis_cupcakes_us.py
+++ locations/spiders/gigis_cupcakes_us.py
@@ -1,15 +1,12 @@
 from locations.categories import Categories
 from locations.hours import OpeningHours
-from locations.storefinders.easylocator import EasyLocatorSpider
+from locations.storefinders.easy_locator import EasyLocatorSpider
 
 
 class GigisCupcakesUSSpider(EasyLocatorSpider):
     name = "gigis_cupcakes_us"
     item_attributes = {"brand": "Gigi's Cupcakes", "extras": Categories.SHOP_BAKERY.value}
-    api_key = "gigiscupcakesusa"
-    start_urls = [
-        "https://easylocator.net/ajax/search_by_lat_lon_geojson/gigiscupcakesusa/-37.86/144.9717/0/1000/null/null"
-    ]
+    api_brand_name = "gigiscupcakesusa"
 
     def parse_item(self, item, location):
         item["opening_hours"] = OpeningHours()
