5e7365c82232e735d776cba25fe2d14c770aa92b
David Hicks <david@hicks.id.au>
2024-08-12 23:22:58 +1000

    betts_au: fix spider (#9509)
    * betts_au: fix spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/betts_au.py
+++ locations/spiders/betts_au.py
@@ -1,11 +1,24 @@
-from locations.categories import Categories, apply_category
-from locations.storefinders.metalocator import MetaLocatorSpider
+import csv
+from io import StringIO
+
+from scrapy import Spider
+
+from locations.categories import Categories
+from locations.dict_parser import DictParser
 
 
-class BettsAUSpider(MetaLocatorSpider):
+class BettsAUSpider(Spider):
     name = "betts_au"
-    item_attributes = {"brand": "Betts", "brand_wikidata": "Q118555401"}
-    brand_id = "6176"
-    country_list = ["Australia"]
+    item_attributes = {"brand": "Betts", "brand_wikidata": "Q118555401", "extras": Categories.SHOP_SHOES.value}
+    start_urls = ["https://www.betts.com.au/pages/store-locator"]
+    allowed_domains = ["www.betts.com.au"]
+    no_refs = True
 
-    apply_category(Categories.SHOP_SHOES, item_attributes)
+    def parse(self, response):
+        csv_raw = response.xpath('//script[@id="store-locations"]/text()').get()
+        rows = csv.DictReader(StringIO(csv_raw))
+        for row in rows:
+            item = DictParser.parse(row)
+            item["branch"] = item.pop("name").removeprefix("Betts ")
+            item["extras"]["ref:google"] = row["place_id"]
+            yield item
