3290fbf92cead1776e07856c61837580c72fa987
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-08-12 12:04:05 +0300

    Update praktiker_bg.py

--- locations/spiders/praktiker_bg.py
+++ locations/spiders/praktiker_bg.py
@@ -1,8 +1,13 @@
 from locations.spiders.technopolis_bg import TechnopolisBGSpider
+from locations.categories import Categories, apply_category
 
 
 class PraktikerBGSpider(TechnopolisBGSpider):
     name = "praktiker_bg"
-    item_attributes = {"brand": "Praktiker", "brand_wikidata": "Q110399491", "shop": "doityourself"}
+    item_attributes = {"brand": "Praktiker", "brand_wikidata": "Q110399491"}
     allowed_domains = ["praktiker.bg"]
     start_urls = ["https://api.praktiker.bg/videoluxcommercewebservices/v2/praktiker/mapbox/customerpreferedstore"]
+
+    def post_process_feature(self, item, feature):
+        apply_category(Categories.SHOP_DOITYOURSELF, item)
+        yield item
