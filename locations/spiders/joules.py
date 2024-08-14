6610050a12f161b6716162af2713dc8753707695
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-08-14 19:27:32 +0100

    Update joules.py

--- locations/spiders/joules.py
+++ locations/spiders/joules.py
@@ -1,11 +1,10 @@
-import re
+from typing import Any
+from scrapy.http import Response
+from scrapy.spiders import Spider
+from locations.items import Feature
+from urllib.parse import urljoin
 
-from scrapy.spiders import SitemapSpider
-
-from locations.structured_data_spider import StructuredDataSpider
-
-
-class JoulesSpider(SitemapSpider, StructuredDataSpider):
+class JoulesSpider(Spider):
     name = "joules"
     item_attributes = {"brand": "Joules", "brand_wikidata": "Q25351738"}
     start_urls = [
@@ -13,12 +12,14 @@ class JoulesSpider(SitemapSpider, StructuredDataSpider):
     ]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json():
+        for location in response.json()["Stores"]:
             item = Feature()
             item["ref"] = location["BR"]
             item["lat"] = location["LT"]
             item["lon"] = location["LN"]
             item["branch"] = location["NA"]
-            name=location["NA"].join(s.split()).lower()+"/"+location["BR"]
+            s=location["NA"]
+            name=''.join(s.split()).lower()+"/"+location["BR"]
             item["website"] = urljoin("https://www.joules.com/storelocator/", name)
             yield item
+
