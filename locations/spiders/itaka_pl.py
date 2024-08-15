0ceb1d5a5bff23d1b199332a0e84ffd76b326bac
Ian Dees <ian.dees@gmail.com>
2024-08-15 15:57:24 -0500

    Protect against blank opening hours

--- locations/spiders/itaka_pl.py
+++ locations/spiders/itaka_pl.py
@@ -33,12 +33,13 @@ class ItakaPLSpider(Spider):
                     item["image"] = ";".join([f"https://www.itaka.pl{url}" for url in details["fotos"]])
                 item["website"] = f"https://www.itaka.pl/{details['www']}"
 
-                opening_hours = OpeningHours()
-                for hours in details["opening_hours"]:
-                    days, hours_range = hours
-                    hours_range = hours_range.removesuffix("*")
-                    opening_hours.add_ranges_from_string(ranges_string=f"{days} {hours_range}", days=DAYS_PL)
-                item["opening_hours"] = opening_hours
+                if opening_hours_text := details.get("opening_hours"):
+                    opening_hours = OpeningHours()
+                    for hours in opening_hours_text:
+                        days, hours_range = hours
+                        hours_range = hours_range.removesuffix("*")
+                        opening_hours.add_ranges_from_string(ranges_string=f"{days} {hours_range}", days=DAYS_PL)
+                    item["opening_hours"] = opening_hours
 
                 item.pop("name", None)
 
