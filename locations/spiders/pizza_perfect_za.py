f34412e5d853f535957363d125d6ee221cda7e84
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-16 11:38:19 +0930

    Add Pizza Perfect (ZA) (#9591)
    * Add Pizza Perfect (ZA)
    
    * Add Pizza Perfect (ZA)
    
    * Add ISEADGG centroid generator script with 48km radius centroids
    
    Automates the process of regenerating a common set of ISEADGG centroid
    CSV files for each country of the world.
    
    Further improvements possible in the future:
    * Additional CSV files can be generated for other radiuses.
    * Output CSV files can be merged together to generate common groupings
      of countries/territories such as gg_gb_im_je_centroids_iseadgg_48km_radius.csv
    * Worldwide CSV files can be automatically generated for various
      radiuses.
    
    * Update pizza_perfect_za.py
    
    * Update locations/spiders/pizza_perfect_za.py
    
    * Fix spider
    
    * Fix name
    
    ---------
    
    Co-authored-by: David Hicks <david@hicks.id.au>

---
+++ locations/spiders/pizza_perfect_za.py
@@ -0,0 +1,39 @@
+from scrapy import Selector
+
+from locations.hours import OpeningHours, sanitise_day
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+
+
+class PizzaPerfectZASpider(WPStoreLocatorSpider):
+    name = "pizza_perfect_za"
+    item_attributes = {
+        "brand_wikidata": "Q116619227",
+        "brand": "Pizza Perfect",
+    }
+    allowed_domains = [
+        "pizzaperfect.co.za",
+    ]
+    max_results = 100
+    search_radius = 50
+    searchable_points_files = ["za_centroids_iseadgg_48km_radius.csv"]
+
+    def parse_opening_hours(self, location: dict, days: dict, **kwargs) -> OpeningHours:
+        if not location.get("hours"):
+            return
+        sel = Selector(text=location["hours"])
+        oh = OpeningHours()
+        for rule in sel.xpath("//tr"):
+            day = sanitise_day(rule.xpath("./td/text()").get(), days)
+            times = rule.xpath("./td/time/text()").get()
+            if not day or not times:
+                continue
+            if times.lower() in ["closed"]:
+                continue
+            start_time, end_time = times.split("-")
+            # TODO: There must be a better way than this
+            try:
+                oh.add_range(day, start_time.strip(), end_time.strip(), time_format="%I:%M %p")
+            except ValueError:
+                oh.add_range(day, start_time.strip(), end_time.strip(), time_format="%H:%M %p")
+
+        return oh
