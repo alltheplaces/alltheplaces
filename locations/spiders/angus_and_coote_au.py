08b4741dc17b03a5f07a7a33f71e11800713390e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-17 06:39:14 +0000

    Fix spider

--- locations/spiders/angus_and_coote_au.py
+++ locations/spiders/angus_and_coote_au.py
@@ -14,31 +14,37 @@ class AngusAndCooteAUSpider(JSONBlobSpider):
 
     def extract_json(self, response):
         stores_js = response.xpath('//script[contains(text(), "var go_stores = ")]/text()').get()
-        return parse_js_object("{" + stores_js.split("{", 1)[1].split("};", 1)[0] + "}")
+        locations = parse_js_object("{" + stores_js.split("{", 1)[1].split("};", 1)[0] + "}")
+        for location_id, location in locations.items():
+            if location.get("rawdata"):
+                raw_data = parse_js_object(location.get("rawdata"))
+                location["street_address"] = re.sub(
+                    r"\s+",
+                    " ",
+                    ", ".join(
+                        filter(
+                            None,
+                            [
+                                raw_data.get("Address line 1"),
+                                raw_data.get("Address line 2"),
+                                raw_data.get("Address line 3"),
+                                raw_data.get("Address line 4"),
+                                raw_data.get("Address line 5"),
+                            ],
+                        )
+                    ).strip(),
+                )
+                location["city"] = raw_data.get("Locality")
+                location["postcode"] = raw_data.get("Postcode")
+        return locations.values()
+
 
     def post_process_item(self, item, response, location):
         item["addr_full"] = re.sub(r"\s+", " ", item.get("addr_full", ""))
         item["website"] = "https://www.anguscoote.com.au/stores/" + location.get("url")
         if location.get("rawdata"):
             raw_data = parse_js_object(location.get("rawdata"))
-            item["street_address"] = re.sub(
-                r"\s+",
-                " ",
-                ", ".join(
-                    filter(
-                        None,
-                        [
-                            raw_data.get("Address line 1"),
-                            raw_data.get("Address line 2"),
-                            raw_data.get("Address line 3"),
-                            raw_data.get("Address line 4"),
-                            raw_data.get("Address line 5"),
-                        ],
-                    )
-                ).strip(),
-            )
-            item["city"] = raw_data.get("Locality")
-            item["postcode"] = raw_data.get("Postcode")
+
             item["opening_hours"] = OpeningHours()
             hours_text = ""
             for day_name in DAYS_FULL:
@@ -46,4 +52,5 @@ class AngusAndCooteAUSpider(JSONBlobSpider):
                     hours_range = raw_data.get("{} hours".format(day_name.title()))
                     hours_text = f"{hours_text} {day_name}: {hours_range}"
             item["opening_hours"].add_ranges_from_string(hours_text)
+
         yield item
