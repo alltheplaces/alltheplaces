90d0415ea8ca0c31571a4075d9ab9e7fd7d6b28a
arrival-spring <lnnunns+git@gmail.com>
2024-08-17 11:03:25 +0200

    footgear_za move name to branch

--- locations/spiders/footgear_za.py
+++ locations/spiders/footgear_za.py
@@ -5,3 +5,7 @@ class FootgearZASpider(WPStoreLocatorSpider):
     name = "footgear_za"
     item_attributes = {"brand": "Footgear", "brand_wikidata": "Q116290280"}
     allowed_domains = ["www.footgear.co.za"]
+
+    def parse_item(self, item, location):
+        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
+        yield item
