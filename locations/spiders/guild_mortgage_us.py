c1f71dce7e821fa10597c2c110a4b754680a77ff
Ian Dees <ian.dees@gmail.com>
2024-08-17 10:17:29 -0500

    Adjust spider to follow merge

--- locations/spiders/guild_mortgage_us.py
+++ locations/spiders/guild_mortgage_us.py
@@ -4,4 +4,4 @@ from locations.storefinders.rio_seo import RioSeoSpider
 class GuildMortgageUSSpider(RioSeoSpider):
     name = "guild_mortgage_us"
     item_attributes = {"brand": "Guild Mortgage Company", "brand_wikidata": "Q122074693"}
-    domain = "maps-r1.guildmortgage.com"
+    end_point = "https://maps-r1.guildmortgage.com"
