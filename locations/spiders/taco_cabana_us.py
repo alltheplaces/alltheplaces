068a8073d21ecb7f8f1f661b3e9fbefb2edbdc7e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-18 01:23:49 +0000

    Revert

--- locations/spiders/taco_cabana_us.py
+++ locations/spiders/taco_cabana_us.py
@@ -10,11 +10,9 @@ class TacoCabanaUSSpider(Spider):
     allowed_domains = ["www.tacocabana.com"]
     start_urls = ["https://www.tacocabana.com/locations/"]
 
-    def extract_json(self, response):
-        return parse_js_object(response.xpath('//script[contains(text(), "var locations_meta = [")]/text()').get())
-
     def parse(self, response):
-        for location in self.extract_json(response):
+        js_blob = response.xpath('//script[contains(text(), "var locations_meta = [")]/text()').get()
+        for location in parse_js_object(js_blob):
             item = DictParser.parse(location["map_pin"])
             item["name"] = item["street"] = None
             item["website"] = location["order_now_link"]
