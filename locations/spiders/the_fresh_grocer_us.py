37f4dbf2b3c89ac1539444ffab078d23356af84c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-18 01:04:55 +0000

    Refactor TheFreshGrocer to JSONBlobSpider

--- locations/spiders/the_fresh_grocer_us.py
+++ locations/spiders/the_fresh_grocer_us.py
@@ -4,12 +4,12 @@ import chompjs
 import scrapy
 
 from locations.categories import Categories
-from locations.dict_parser import DictParser
+from locations.json_blob_spider import JSONBlobSpider
 from locations.hours import OpeningHours, day_range
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class TheFreshGrocerUSSpider(scrapy.Spider):
+class TheFreshGrocerUSSpider(JSONBlobSpider):
     name = "the_fresh_grocer_us"
     item_attributes = {
         "brand": "The Fresh Grocer",
@@ -19,33 +19,37 @@ class TheFreshGrocerUSSpider(scrapy.Spider):
     start_urls = ["https://www.thefreshgrocer.com/"]
     requires_proxy = True
 
-    def parse(self, response, **kwargs):
-        for location in chompjs.parse_js_object(
-            response.xpath('//script[contains(text(), "__PRELOADED_STATE__")]/text()').get()
-        )["stores"]["availablePlanningStores"]["items"]:
-            location["street_address"] = merge_address_lines(
-                [location["addressLine1"], location["addressLine2"], location["addressLine3"]]
-            )
-            location["state"] = location["countyProvinceState"]
-            item = DictParser.parse(location)
-            item["ref"] = location["retailerStoreId"]
-            item["website"] = f'https://www.thefreshgrocer.com/sm/planning/rsid/{item["ref"]}'
+    def extract_json(self, response):
+        return chompjs.parse_js_object(
+                    response.xpath('//script[contains(text(), "__PRELOADED_STATE__")]/text()').get()
+                )["stores"]["availablePlanningStores"]["items"]
 
-            if m := re.search(
-                r"(\w+)(?: (?:-|thru) (\w+))?: (\d+)\s*([ap]m) (?:-|to) (\d+)\s*([ap]m)",
-                location["openingHours"],
-                re.IGNORECASE,
-            ):
-                start_day, end_day, start_time, start_zone, end_time, end_zone = m.groups()
-                if not end_day:
-                    end_day = start_day
-                if start_day and end_day:
-                    item["opening_hours"] = OpeningHours()
-                    item["opening_hours"].add_days_range(
-                        day_range(start_day, end_day),
-                        f"{start_time}{start_zone}",
-                        f"{end_time}{end_zone}",
-                        time_format="%I%p",
-                    )
+    def pre_process_data(self, location):
+        location["street_address"] = merge_address_lines(
+            [location["addressLine1"], location["addressLine2"], location["addressLine3"]]
+        )
+        location["state"] = location["countyProvinceState"]
 
-            yield item
+
+    def post_process_item(self, item, response, location):
+        item["ref"] = location["retailerStoreId"]
+        item["website"] = f'https://www.thefreshgrocer.com/sm/planning/rsid/{item["ref"]}'
+
+        if m := re.search(
+            r"(\w+)(?: (?:-|thru) (\w+))?: (\d+)\s*([ap]m) (?:-|to) (\d+)\s*([ap]m)",
+            location["openingHours"],
+            re.IGNORECASE,
+        ):
+            start_day, end_day, start_time, start_zone, end_time, end_zone = m.groups()
+            if not end_day:
+                end_day = start_day
+            if start_day and end_day:
+                item["opening_hours"] = OpeningHours()
+                item["opening_hours"].add_days_range(
+                    day_range(start_day, end_day),
+                    f"{start_time}{start_zone}",
+                    f"{end_time}{end_zone}",
+                    time_format="%I%p",
+                )
+
+        yield item
