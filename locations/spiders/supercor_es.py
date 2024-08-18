2d5e846eda18b8c699fa9008b0cde5b6408cb68d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-18 11:52:27 +0930

    Refactor Supercore, The Big Biscuit, The Fresh Grocer to JSON Blob Spider (#9645)
    * Refactor TheFreshGrocer to JSONBlobSpider
    
    * Refactor TheFreshGrocer to JSONBlobSpider
    
    * Refactor TheBigBiscuit to JSONBlobSpider
    
    * Refactor TheBigBiscuit to JSONBlobSpider
    
    * Refactor to common pattern
    
    * Refactor Supercore to JSONBlobSpider
    
    * Refactor Supercore to JSONBlobSpider
    
    * Revert
    
    * Fix exception
    
    * Update locations/spiders/the_fresh_grocer_us.py

--- locations/spiders/supercor_es.py
+++ locations/spiders/supercor_es.py
@@ -1,36 +1,35 @@
 from hashlib import sha1
 
 from chompjs import parse_js_object
-from scrapy import Spider
 
 from locations.categories import Categories
-from locations.dict_parser import DictParser
 from locations.hours import DAYS_ES, OpeningHours
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class SupercorESSpider(Spider):
+class SupercorESSpider(JSONBlobSpider):
     name = "supercor_es"
     item_attributes = {"brand": "Supercor", "brand_wikidata": "Q6135841", "extras": Categories.SHOP_SUPERMARKET.value}
     allowed_domains = ["www.supercor.es"]
     start_urls = ["https://www.supercor.es/tiendas/"]
 
-    def parse(self, response):
+    def extract_json(self, response):
         js_blob = response.xpath('//script[contains(text(), "var tiendas = `")]/text()').get()
         js_blob = js_blob.split("var tiendas = `", 1)[1].split("`;", 1)[0]
-        locations = parse_js_object(js_blob, unicode_escape=True)
-        for location in locations:
-            # Ignore closed stores
-            if location["abierto"] != "1":
-                continue
+        return parse_js_object(js_blob, unicode_escape=True)
 
-            item = DictParser.parse(location)
-            # Create a reference ID from the store address as this is the most
-            # permanent identifier available to be used.
-            item["ref"] = sha1(location["direccion"].encode("UTF-8")).hexdigest()
-            item["postcode"] = location["cp"]
+    def post_process_item(self, item, response, location):
+        # Ignore closed stores
+        if location["abierto"] != "1":
+            return
 
-            hours_text = " ".join(location["horario"]).replace(":|", ": ")
-            item["opening_hours"] = OpeningHours()
-            item["opening_hours"].add_ranges_from_string(hours_text, days=DAYS_ES)
+        # Create a reference ID from the store address as this is the most
+        # permanent identifier available to be used.
+        item["ref"] = sha1(location["direccion"].encode("UTF-8")).hexdigest()
+        item["postcode"] = location["cp"]
 
-            yield item
+        hours_text = " ".join(location["horario"]).replace(":|", ": ")
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_text, days=DAYS_ES)
+
+        yield item
