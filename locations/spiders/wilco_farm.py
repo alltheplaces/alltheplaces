972035b6d1b1cae05b2dec67bead4204017b9f57
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-18 00:48:37 +0000

    Refactor Wilco Farm to JSON Blob

--- locations/spiders/wilco_farm.py
+++ locations/spiders/wilco_farm.py
@@ -1,10 +1,9 @@
 import re
 
 import chompjs
-import scrapy
 
-from locations.json_blob_spider import JSONBlobSpider
 from locations.hours import OpeningHours, day_range
+from locations.json_blob_spider import JSONBlobSpider
 
 
 class WilcoFarmSpider(JSONBlobSpider):
@@ -15,15 +14,12 @@ class WilcoFarmSpider(JSONBlobSpider):
     requires_proxy = True
 
     def extract_json(self, response):
-        return chompjs.parse_js_object(
-            response.xpath('//script[contains(text(), "var markers")]/text()').get()
-        )
+        return chompjs.parse_js_object(response.xpath('//script[contains(text(), "var markers")]/text()').get())
 
     def pre_process_data(self, location):
         for k in list(location.keys()):
             location[k.replace("store", "")] = location.pop(k)
         location["street_address"] = location.pop("Street")
-
 
     def post_process_item(self, item, response, location):
         item["opening_hours"] = OpeningHours()
