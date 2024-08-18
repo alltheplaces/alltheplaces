eb46a1eb1f2396c7719dc8232ec26a7f91b1581c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-18 11:52:52 +0000

    Waynes Coffee (SE)

--- locations/spiders/waynes_coffee_se.py
+++ locations/spiders/waynes_coffee_se.py
@@ -3,9 +3,12 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class WaynesCoffeeSESpider(SitemapSpider, StructuredDataSpider):
-    name = "waynes_coffee_se"
-    sitemap_urls = ["https://www.waynescoffee.se/sitemap.xml"]
+class WaynesCoffeeSpider(SitemapSpider, StructuredDataSpider):
+    name = "waynes_coffee"
+    sitemap_urls = [
+        "https://www.waynescoffee.se/sitemap.xml",
+        "https://www.waynescoffee.jo/sitemap.xml"
+    ]
     sitemap_rules = [(r"/kafe/[\w-]+/", "parse_sd")]
     item_attributes = {"brand": "Waynes Coffee", "brand_wikidata": "Q10397787"}
     wanted_types = ["CafeOrCoffeeShop"]
