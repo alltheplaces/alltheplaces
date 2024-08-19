5e506a8b7486fcc3c2655aaab83d2177b8db26c0
David Hicks <david@hicks.id.au>
2024-08-20 01:34:40 +1000

    ISEADGG geographic search refactor (#9685)
    * ISEADGG geographic search refactor
    
    * Move ISEADGG centroids to a dedicated directory.
    * Overhaul script for automatically generating ISEADGG centroids.
    * Add 24km ISEADGG centroids.
    * Add missing 48km ISEADGG centroids and missing shapes for
      territories/regions of countries which share ISO-3166 alpha-2 country
      codes.
    * Add 79km ISEADGG centroids.
    * Add 94km ISEADGG centroids.
    * Add 158km ISEADGG centroids.
    * Add 315km ISEADGG centroids.
    * Add 458km ISEADGG centroids.
    * Introduce new function country_iseadgg_centroids for easy geographic
      searches with ISEADGG centroids (including multiple countries searched
      at the same time).
    * Update existing ISEADGG-using spiders to use new
      country_iseadgg_centroids function. Fix errors in these spiders and
      modernise the spiders.
    * Add unit tests for the country_iseadgg_centroids function.
    * Use country_iseadgg_centroids function in WPStoreLocatorSpider store
      finder.
    * Use country_iseadgg_centroids function in StoreLocatorPlusSelfSpider
      store finder.
    * Add statistics for hits, misses and maximum features returned in a
      single query for geographic search spiders using ISEADGG centroids.
    * Include "branch" as a new field to generate field statistics for.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix CI warnings
    
    * Remove f-strings when not required
    * Simplify start_requests function of WPStoreLocatorSpider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/save_a_lot_us.py
+++ locations/spiders/save_a_lot_us.py
@@ -1,45 +1,62 @@
-import re
-
 from scrapy import Spider
 from scrapy.http import JsonRequest
 
 from locations.categories import Categories
 from locations.dict_parser import DictParser
-from locations.geo import point_locations, vincenty_distance
+from locations.geo import country_iseadgg_centroids
+from locations.hours import DAYS, OpeningHours
 
 
 class SaveALotUSSpider(Spider):
     name = "save_a_lot_us"
     item_attributes = {"brand": "Save-A-Lot", "brand_wikidata": "Q7427972", "extras": Categories.SHOP_SUPERMARKET.value}
     allowed_domains = ["savealot.com"]
+    download_delay = 0.2
 
     def start_requests(self):
-        for lat, lon in point_locations("us_centroids_iseadgg_48km_radius.csv"):
-            # Create a bounding box that fully encloses a circle of the given
-            # search radius.
-            circle_north = vincenty_distance(lat, lon, 50, 0)
-            circle_east = vincenty_distance(lat, lon, 50, 90)
-            circle_south = vincenty_distance(lat, lon, 50, 180)
-            circle_west = vincenty_distance(lat, lon, 50, 270)
-            box_sw = (circle_south[0], circle_west[1])
-            box_ne = (circle_north[0], circle_east[1])
-            yield JsonRequest(
-                url=f"https://savealot.com/wp-json/locator/v1/search/{lat}/{lon}/{box_sw[0]}/{box_sw[1]}/{box_ne[0]}/{box_ne[1]}/"
-            )
+        # API returns stores within 25 miles of a provided WGS84 coordinate.
+        for lat, lon in country_iseadgg_centroids(["US"], 79):
+            yield JsonRequest(url=f"https://savealot.com/?lat={lat}&lng={lon}&_data=root")
 
     def parse(self, response):
-        locations = response.json()
-        if len(locations) == 50:
-            raise RuntimeError(
-                "Locations have probably been truncated due to 50 (or more) locations being returned by a single geographic radius search. Use a more granular searchable points file."
-            )
-        for location in locations:
-            item = DictParser.parse(location)
-            item["ref"] = location["store"]
+        locations = response.json()["storeList"]["_embedded"]["matches"]
 
-            city_uri_safe = re.sub(r"[^\w\-]", "", item["city"].lower().replace(" ", "-"))
-            postcode = item["postcode"]
-            store_number = item["ref"]
-            item["website"] = f"https://savealot.com/grocery-stores/{city_uri_safe}-{postcode}-{store_number}/"
+        # A maximum of 50 locations are returned at once. The search radius is
+        # set to avoid receiving 50 locations in a single response. If 50
+        # locations were to be returned, it is a sign that some locations have
+        # most likely been truncated.
+        if len(locations) >= 50:
+            raise RuntimeError(
+                "Locations have probably been truncated due to 50 (or more) locations being returned by a single geographic radius search, and the API restricts responses to 50 results only. Use a smaller search radius."
+            )
+
+        if len(locations) > 0:
+            self.crawler.stats.inc_value("atp/geo_search/hits")
+        else:
+            self.crawler.stats.inc_value("atp/geo_search/misses")
+        self.crawler.stats.max_value("atp/geo_search/max_features_returned", len(locations))
+
+        for location in locations:
+            location = location["details"]
+            item = DictParser.parse(location)
+            item["ref"] = location["number"]
+            item["branch"] = item.pop("name", None)
+            item["website"] = "https://savealot.com/stores/" + location["number"]
+            item["street_address"] = location["location"]["address1"]
+            item["city"] = location["location"]["city"]
+            item["state"] = location["location"]["state"]
+            item["postcode"] = location["location"]["postalCode"]
+            item["country"] = location["location"]["country"]
+
+            for phone_number in location["phoneNumbers"]:
+                if phone_number["description"] == "Main":
+                    item["phone"] = phone_number["value"]
+                    break
+
+            item["opening_hours"] = OpeningHours()
+            for day_hours in location["hours"]:
+                item["opening_hours"].add_range(
+                    DAYS[day_hours["day"]], day_hours["hours"]["open"], day_hours["hours"]["close"], "%H:%M:%S"
+                )
 
             yield item
