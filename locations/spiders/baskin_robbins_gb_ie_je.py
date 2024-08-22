d7b009b835c0a4a1e761d2cbff6477a5faabb114
David Hicks <david@hicks.id.au>
2024-08-23 00:42:52 +1000

    AgileStoreLocatorSpider: tidy up storefinder and associated spiders

--- locations/spiders/baskin_robbins_gb_ie_je.py
+++ locations/spiders/baskin_robbins_gb_ie_je.py
@@ -1,3 +1,8 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
 from locations.spiders.cineworld_gb_je import CineworldGBJESpider
 from locations.spiders.millies_gb import MilliesGBSpider
 from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
@@ -8,7 +13,7 @@ class BaskinRobbinsGBIEJESpider(AgileStoreLocatorSpider):
     item_attributes = {"brand": "Baskin-Robbins", "brand_wikidata": "Q584601"}
     allowed_domains = ["baskinrobbins.co.uk"]
 
-    def parse_item(self, item, location):
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         if "(Cineworld)" in item["name"]:
             item["located_in"] = CineworldGBJESpider.item_attributes["brand"]
             item["located_in_wikidata"] = CineworldGBJESpider.item_attributes["brand_wikidata"]
