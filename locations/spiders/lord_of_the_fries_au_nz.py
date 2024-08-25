2848b056984cb11999fd8c24d92f4f4c0334204d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 07:44:10 +0000

    Add lord_of_the_fries_au_nz

---
+++ locations/spiders/lord_of_the_fries_au_nz.py
@@ -0,0 +1,12 @@
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+
+
+class LordoftheFriesAUNZSpider(WPStoreLocatorSpider):
+    name = "lord_of_the_fries_au_nz"
+    item_attributes = {
+        "brand_wikidata": "Q104088629",
+        "brand": "Lord of the Fries",
+    }
+    allowed_domains = [
+        "www.lordofthefries.com.au",
+    ]
