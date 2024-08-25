68d13ffb69983b8bac02172146b0353463363797
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 03:46:48 +0000

    Improve

--- contrib/new_spider.sh
+++ contrib/new_spider.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider -t basic -d $1 > locations/spiders/$1.py && git add locations/spiders/$1.py && code locations/spiders/$1.py"
+echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider -t $2 -d $1 > locations/spiders/$1.py && git add locations/spiders/$1.py && code locations/spiders/$1.py"
 echo "pipenv run scrapy crawl $1 && bash contrib/hooks/pre-commit && git commit -m 'Add $1' locations/spiders/$1.py && git push -u origin $1"
 
