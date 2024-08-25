69057472d87aad9e79896e28a0d16d372f32078e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 04:28:55 +0000

    Refactor to try vscode, nano, vim if EDITOR is not set

--- contrib/new_spider.sh
+++ contrib/new_spider.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+if [ -z "${EDITOR}" ]; then 
+if command -v code &> /dev/null
+then
+    EDITOR='code'
+elif command -v nano &> /dev/null
+    EDITOR='nano'
+fi
+
 echo "Generate your new spider. Tip: Do this a new terminal"
-echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider $1 $1 && git add locations/spiders/$1.py && code locations/spiders/$1.py"
+echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider $1 $1 && git add locations/spiders/$1.py && $EDITOR locations/spiders/$1.py"
 
 echo ""
 echo "Crawl and commit"
