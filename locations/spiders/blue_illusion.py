2e7318682ba013115d9ca043789084ea0e0d2396
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 10:08:13 +0000

    Fix blue_illusion_au_nz

--- locations/spiders/blue_illusion.py
+++ locations/spiders/blue_illusion.py
@@ -1,20 +1,10 @@
-import re
-
-import json5
-from scrapy import Spider
-
-from locations.dict_parser import DictParser
+from locations.storefinders.stockist import StockistSpider
 
 
-class BlueIllusionSpider(Spider):
-    name = "blue_illusion"
-    item_attributes = {"brand": "Blue Illusion", "brand_wikidata": "Q118464703"}
-    start_urls = ["https://www.blueillusion.com/stores"]
-
-    def parse(self, response, **kwargs):
-        script = response.xpath('//script[contains(text(), "stores")]/text()').get()
-        for location in json5.loads(re.search(r"stores:\s*(\[.+?\])", script, re.DOTALL).group(1)):
-            item = DictParser.parse(location)
-            item["website"] = response.urljoin(location["url"])
-
-            yield item
+class BlueIllusionAUNZSpider(StockistSpider):
+    name = "blue_illusion_au_nz"
+    item_attributes = {
+        "brand_wikidata": "Q118464703",
+        "brand": "Blue Illusion",
+    }
+    key = "u17747"
