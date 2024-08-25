6ea73adedb68e7ce6d156fa53cfe734f610bd995
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 03:55:52 +0000

    Add hero_certified_burgers_ca

---
+++ locations/spiders/hero_certified_burgers_ca.py
@@ -0,0 +1,12 @@
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+
+
+class HeroCertifiedBurgersCASpider(WPStoreLocatorSpider):
+    name = "hero_certified_burgers_ca"
+    item_attributes = {
+        "brand_wikidata": "Q5742641",
+        "brand": "Hero Certified Burgers",
+    }
+    allowed_domains = [
+        "heroburgers.com",
+    ]
