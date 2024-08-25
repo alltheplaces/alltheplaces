754e3a718e891412e8f3214cea6fcb2c69913a01
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 14:56:52 +0930

    Update and rename cuisinella.py to cuisinella_fr.py

--- locations/spiders/cuisinella_fr.py
+++ locations/spiders/cuisinella_fr.py
@@ -3,8 +3,8 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class CuisinellaSpider(SitemapSpider, StructuredDataSpider):
-    name = "cuisinella"
+class CuisinellaFRSpider(SitemapSpider, StructuredDataSpider):
+    name = "cuisinella_fr"
     item_attributes = {"brand": "Cuisinella", "brand_wikidata": "Q3007012"}
     sitemap_urls = ["https://www.ma.cuisinella/robots.txt"]
     sitemap_rules = [
