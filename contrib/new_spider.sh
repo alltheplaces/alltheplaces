144a959a5ac864d83b272e8957ca208673e5c673
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 03:28:43 +0000

    Improve commands

--- contrib/new_spider.sh
+++ contrib/new_spider.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+echo "git fetch upstream"
+echo "git checkout upstream/master"
 echo "git checkout -b $1"
 echo "touch locations/spiders/$1.py"
 echo "git add locations/spiders/$1.py"
-echo "black locations/spiders/$1.py"
+echo "pipenv run scrapy crawl $1"
+echo "bash .git/hooks/pre-commit"
+echo "git commit -m 'Add $1' locations/spiders/$1.py"
+echo "git push -u origin $1"
+
