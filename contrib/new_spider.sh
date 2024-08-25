0ba19d0e290dc44da17716b48d2f9e4201a32ed1
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 14:11:39 +0930

    Update new_spider.sh

--- contrib/new_spider.sh
+++ contrib/new_spider.sh
@@ -9,8 +9,8 @@ if [ -z "${EDITOR}" ]; then
 fi
 
 echo "Generate your new spider. Tip: Do this a new terminal"
-echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider $1 $3 -t $2 && git add locations/spiders/$1.py && $EDITOR locations/spiders/$1.py"
+echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider $1 $3 -t $2 && git add locations/spiders/$1.py && $EDITOR locations/spiders/$1.py && pipenv run scrapy crawl $1"
 
 echo ""
-echo "Crawl and commit"
-echo "pipenv run scrapy crawl $1 && bash contrib/hooks/pre-commit && git commit -m 'Add $1' locations/spiders/$1.py && git push -u origin $1"
+echo "Commit and push"
+echo "bash contrib/hooks/pre-commit && git commit -m 'Add $1' locations/spiders/$1.py && git push -u origin $1"
