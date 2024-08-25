85f7d88de02217ab79b4135e6da7c65a95383618
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-25 03:59:12 +0000

    Add hints

--- contrib/new_spider.sh
+++ contrib/new_spider.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider -t $2 -d $1 > locations/spiders/$1.py && git add locations/spiders/$1.py && code locations/spiders/$1.py"
+echo "Generate your new spider. Tip: Do this a new terminal"
+echo "git fetch upstream && git checkout upstream/master && git checkout -b $1 && pipenv run scrapy genspider $1 $1 && git add locations/spiders/$1.py && code locations/spiders/$1.py"
+
+echo ""
+echo "Crawl and commit"
 echo "pipenv run scrapy crawl $1 && bash contrib/hooks/pre-commit && git commit -m 'Add $1' locations/spiders/$1.py && git push -u origin $1"
 
