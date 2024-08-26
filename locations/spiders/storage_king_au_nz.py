630832446fe16c1603eee38b051c4effb029f797
Cj Malone <CjMalone@mail.com>
2024-08-26 13:48:19 +0100

    Collect NZ locations

--- locations/spiders/storage_king_au_nz.py
+++ locations/spiders/storage_king_au_nz.py
@@ -7,10 +7,13 @@ from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
-class StorageKingAUSpider(JSONBlobSpider):
-    name = "storage_king_au"
+class StorageKingAUNZSpider(JSONBlobSpider):
+    name = "storage_king_au_nz"
     item_attributes = {"brand": "Storage King", "brand_wikidata": "Q114353097"}
-    start_urls = ["https://www.storageking.com.au/apps/checkout/api/facility/all"]
+    start_urls = [
+        "https://www.storageking.com.au/apps/checkout/api/facility/all",
+        "https://www.storageking.co.nz/apps/checkout/api/facility/all",
+    ]
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def post_process_item(self, item: Feature, response: Response, feature: dict):
@@ -20,7 +23,14 @@ class StorageKingAUSpider(JSONBlobSpider):
         item["street_address"] = item.pop("addr_full")
         item["branch"] = feature.get("short_name")
         item["geometry"] = feature["Location"]
-        item["website"] = urljoin("https://www.storageking.com.au/collections/", feature["collection_handle"])
+        item["website"] = urljoin(
+            (
+                "https://www.storageking.com.au/collections/"
+                if ".com.au" in response.url
+                else "https://www.storageking.co.nz/collections/"
+            ),
+            feature["collection_handle"],
+        )
 
         item["opening_hours"] = self.parse_oh(feature.get("trading_hours", {}).get("accessHours", {}))
         item["extras"]["opening_hours:office"] = self.parse_oh(
