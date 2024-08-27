e37d0062799a6e5289b4ac8ae7078d1f92270a3e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-27 22:59:01 +0930

    Update and rename eriks_delicafe.py to eriks_deli_cafe.py

--- locations/spiders/eriks_deli_cafe.py
+++ locations/spiders/eriks_deli_cafe.py
@@ -2,7 +2,7 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
 class EriksDeliCafeSpider(WPStoreLocatorSpider):
-    name = "eriks_delicafe"
+    name = "eriks_deli_cafe"
     item_attributes = {
         "brand_wikidata": "Q116922917",
         "brand": "Erik's DeliCafé",
