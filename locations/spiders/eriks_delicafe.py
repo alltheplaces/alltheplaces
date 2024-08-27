11f22edad8b7ad0ea158be035cd6e48b237a6777
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-27 13:24:56 +0000

    Add eriks_delicafe

---
+++ locations/spiders/eriks_delicafe.py
@@ -0,0 +1,14 @@
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+
+
+class EriksDeliCafeSpider(WPStoreLocatorSpider):
+    name = "eriks_delicafe"
+    item_attributes = {
+        "brand_wikidata": "Q116922917",
+        "brand": "Erik's DeliCafé",
+    }
+    allowed_domains = [
+        "eriksdelicafe.com",
+    ]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+    time_format = "%I:%M %p"
