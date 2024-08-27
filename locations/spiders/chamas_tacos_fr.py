f22ddd2a718ee6877f485a7cfa868656fad5fc7b
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-27 13:18:19 +0000

    Add chamas_tacos_fr

---
+++ locations/spiders/chamas_tacos_fr.py
@@ -0,0 +1,10 @@
+from locations.storefinders.wp_go_maps import WpGoMapsSpider
+
+
+class ChamasTacosFRSpider(WpGoMapsSpider):
+    name = "chamas_tacos_fr"
+    item_attributes = {
+        "brand_wikidata": "Q127411207",
+        "brand": "Chamas Tacos",
+    }
+    allowed_domains = ["chamas-tacos.com"]
