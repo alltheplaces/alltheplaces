7c1674c8bd92e31a0e7cd8548a4c9669fe3210da
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-28 07:14:17 +0930

    Update lord_of_the_fries_au_nz.py

--- locations/spiders/lord_of_the_fries_au_nz.py
+++ locations/spiders/lord_of_the_fries_au_nz.py
@@ -1,3 +1,5 @@
+from locations.categories import Categories
+from locations.hours import DAYS_EN
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
@@ -6,7 +8,9 @@ class LordOfTheFriesAUNZSpider(WPStoreLocatorSpider):
     item_attributes = {
         "brand_wikidata": "Q104088629",
         "brand": "Lord of the Fries",
+        "extras": Categories.FAST_FOOD.value
     }
     allowed_domains = [
         "www.lordofthefries.com.au",
     ]
+    days = DAYS_EN
