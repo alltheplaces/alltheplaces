f4c12787799c0188be943025be8fb0fdd3a34fa6
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-28 07:23:37 +0930

    Update pretzelmaker_us.py

--- locations/spiders/pretzelmaker_us.py
+++ locations/spiders/pretzelmaker_us.py
@@ -1,4 +1,6 @@
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from locations.hours import DAYS_EN
+from locations.categories import Categories
 
 
 class PretzelmakerUSSpider(WPStoreLocatorSpider):
@@ -6,8 +8,10 @@ class PretzelmakerUSSpider(WPStoreLocatorSpider):
     item_attributes = {
         "brand_wikidata": "Q7242321",
         "brand": "Pretzelmaker",
+        "extras": Categories.FAST_FOOD.value
     }
     allowed_domains = [
         "www.pretzelmaker.com",
     ]
     time_format = "%I:%M %p"
+    days = DAYS_EN
