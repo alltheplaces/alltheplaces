c0f719d861ecceaa580d0f14a17efb7ddd4cedd6
Ian Dees <ian.dees@gmail.com>
2024-08-26 20:50:01 -0500

    Fix class name

--- locations/spiders/fresh_thyme.py
+++ locations/spiders/fresh_thyme.py
@@ -6,7 +6,7 @@ from locations.items import Feature
 from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
 
 
-class FreshThymeSpider(StorefrontgatewaySpider):
+class FreshThymeUSSpider(StorefrontgatewaySpider):
     name = "fresh_thyme_us"
     item_attributes = {"brand": "Fresh Thyme", "brand_wikidata": "Q64132791"}
     start_urls = ["https://storefrontgateway.freshthyme.com/api/stores"]
