cfe610c952c394fb1b7f4677918470149cf491a5
Spencer Alves <impiaaa@gmail.com>
2024-08-28 14:36:04 -0700

    New spider: LesComptoirsDeLaBioFR (78 locations) (fixes #7786)

---
+++ locations/spiders/lescomptoirsdelabio_fr.py
@@ -0,0 +1,40 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.pipelines.address_clean_up import clean_address
+from locations.storefinders.algolia import AlgoliaSpider
+
+
+class LesComptoirsDeLaBioFRSpider(AlgoliaSpider):
+    name = "lescomptoirsdelabio_fr"
+    item_attributes = {"brand": "Les Comptoirs de la Bio", "brand_wikidata": "Q120801428"}
+    api_key = "7a46160ed01bb0af2c2af8d14b97f3c5"
+    app_id = "0KNEMTBXX3"
+    index_name = "CDLB_fr"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["addr_full"] = clean_address(feature["formatted_address"])
+        item["branch"] = item.pop("name").removeprefix("Les Comptoirs de La Bio ")
+        item["city"] = feature["city"]["name"]
+        item["country"] = feature["country"]["code"]
+        item["image"] = feature["pictures"][0]
+        item["lat"] = feature["_geoloc"]["lat"]
+        item["lon"] = feature["_geoloc"]["lng"]
+        item["opening_hours"] = self.extract_opening_hours(feature)
+        item["street_address"] = clean_address([feature.pop("street1"), feature.pop("street2")])
+        item["website"] = f"https://magasins.lescomptoirsdelabio.fr/fr/{feature['url_location']}"
+        yield item
+
+    def extract_opening_hours(self, feature: dict) -> OpeningHours:
+        if hours := feature.get("formatted_opening_hours"):
+            try:
+                oh = OpeningHours()
+                for day, hour in hours.items():
+                    for times in hour:
+                        oh.add_range(day, times.split("-")[0], times.split("-")[1], time_format="%H:%M")
+                return oh
+            except Exception as e:
+                self.logger.warning(f"Failed to parse hours {hours}: {e}")
