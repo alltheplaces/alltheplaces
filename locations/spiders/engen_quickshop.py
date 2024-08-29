8fe0face4d459cb84e923506c7e3f2cdc7f2b9a7
arrival-spring <lnnunns+git@gmail.com>
2024-08-29 16:40:30 +0200

    EngenQuickshopSpider and move name to branch for EngenSpider

--- locations/spiders/engen_quickshop.py
+++ locations/spiders/engen_quickshop.py
@@ -1,20 +1,23 @@
-import scrapy
+from scrapy import Spider
 
 from locations.categories import Categories
 from locations.dict_parser import DictParser
 
 
-class EngenSpider(scrapy.Spider):
-    name = "engen"
-    item_attributes = {"brand": "Engen", "brand_wikidata": "Q3054251", "extras": Categories.FUEL_STATION.value}
-    allowed_domains = ["engen.co.za"]
+class EngenQuickshopSpider(Spider):
+    name = "engen_quickshop"
+    item_attributes = {
+        "brand": "Quickshop",
+        "brand_wikidata": "Q122764368",
+        "extras": Categories.SHOP_CONVENIENCE.value,
+    }
     start_urls = ["https://engen-admin.engen.co.za/api/service-stations/all"]
 
     def parse(self, response):
         data = response.json()
         for i in data["response"]["data"]["stations"]:
-            i["branch"] = i.pop("company_name").replace(self.item_attributes["brand"], "").strip()
-            i["phone"] = i.pop("mobile")
+            if "Quickshop" not in i.get("rental_units", ""):
+                continue
             postcode = i.pop("street_postal_code")
             if postcode and postcode != "0":
                 i["postcode"] = postcode
