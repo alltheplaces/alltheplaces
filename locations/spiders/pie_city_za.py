44a739d9da5d043a51db0225897f5d208b58b4f7
arrival-spring <lnnunns+git@gmail.com>
2024-08-31 09:30:55 +0200

    Refactor PieCityZASpider

--- locations/spiders/pie_city_za.py
+++ locations/spiders/pie_city_za.py
@@ -1,17 +1,7 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import Rule
-
-from locations.spiders.tiger_wheel_and_tyre_za import TigerWheelAndTyreZASpider
+from locations.storefinders.go_review import GoReviewSpider
 
 
-class PieCityZASpider(TigerWheelAndTyreZASpider):
+class PieCityZASpider(GoReviewSpider):
     name = "pie_city_za"
     item_attributes = {"brand": "Pie City", "brand_wikidata": "Q116619195"}
     start_urls = ["https://pcsl.goreview.co.za/store-locator"]
-    rules = [
-        Rule(
-            LinkExtractor(allow=r"^https:\/\/piecity\d+\.goreview\.co\.za\/store-information\?store-locator=pcsl$"),
-            callback="parse",
-        )
-    ]
-    url_prefix = "piecity"
