3031a1f0c90d3806313fe9e678afcd8bd93e8768
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-31 12:09:15 +0930

    Update locations/structured_data_spider.py

--- locations/structured_data_spider.py
+++ locations/structured_data_spider.py
@@ -206,7 +206,6 @@ class StructuredDataSpider(Spider):
         "HobbyShop",
         "HomeAndConstructionBusiness",
         "HomeGoodsStore",
-        "Hospital",
         "IceCreamShop",
         "JewelryStore",
         "LegalService",
