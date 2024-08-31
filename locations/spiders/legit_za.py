c4d501fada70fac2b0f1a3d8dab6149ba9c697ac
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-31 13:06:28 +0930

    Add LEGiT (ZA) (#10013)
    * Add Legit (ZA)
    
    * Add Legit (ZA)

---
+++ locations/spiders/legit_za.py
@@ -0,0 +1,12 @@
+import chompjs
+
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class LegitZASpider(JSONBlobSpider):
+    name = "legit_za"
+    start_urls = ["https://amaicdn.com/storelocator-prod/wtb/legit-za-1724151185.js"]
+
+    def extract_json(self, response):
+        chunks = response.text.split("SCASLWtb=")
+        return chompjs.parse_js_object(chunks[1])["locations"]
