d57c7d1e1548ff4a3f7dbd8219a19b198b0f27a2
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-31 01:50:39 +0000

    Add pre_process_data hook for StoreRocket

--- locations/storefinders/storerocket.py
+++ locations/storefinders/storerocket.py
@@ -26,6 +26,7 @@ class StoreRocketSpider(Spider):
             return
 
         for location in response.json()["results"]["locations"]:
+            self.pre_process_data(location)
             item = DictParser.parse(location)
 
             item["street_address"] = ", ".join(filter(None, [location["address_line_1"], location["address_line_2"]]))
@@ -48,3 +49,6 @@ class StoreRocketSpider(Spider):
 
     def parse_item(self, item: Feature, location: dict, **kwargs):
         yield item
+
+    def pre_process_data(self, location, **kwargs):
+        """Override with any pre-processing on the item."""
