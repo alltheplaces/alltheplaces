d6381743a9391ae8889c4875d55b9b20057a6a3e
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-08-31 05:17:48 +0100

    [AutoNation US] Fix and rename spider (#9993)
    * [AutoNation US] Fix and rename spider
    
    * See what happens without proxy
    
    ---------
    
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

---
+++ locations/spiders/auto_nation_us.py
@@ -0,0 +1,19 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class AutoNationUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "auto_nation_us"
+    allowed_domains = ["autonation.com"]
+    item_attributes = {"brand": "AutoNation", "brand_wikidata": "Q784804"}
+    sitemap_urls = ["https://www.autonation.com/robots.txt"]
+    sitemap_rules = [(r"https://www.autonation.com/dealers/[^/]+$", "parse")]
+    wanted_types = ["AutoDealer"]
+    time_format = "%I:%M %p"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["lat"] = response.xpath('//input[@id="storeLatitude"]/@value').get()
+        item["lon"] = response.xpath('//input[@id="storeLongitude"]/@value').get()
+
+        yield item
