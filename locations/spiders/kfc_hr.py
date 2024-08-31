07041f7f861466d8ced0fb9e6da13ba93046b791
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-31 02:53:34 +0000

    Apply refactoring from autospidergen branch

---
+++ locations/spiders/kfc_hr.py
@@ -0,0 +1,22 @@
+from unidecode import unidecode
+
+from locations.spiders.kfc_us import KFC_SHARED_ATTRIBUTES
+from locations.storefinders.amrest_eu import AmrestEUSpider
+
+
+class KfcHRSpider(AmrestEUSpider):
+    name = "kfc_hr"
+    item_attributes = KFC_SHARED_ATTRIBUTES
+    api_brand_key = "KFC"
+    api_brand_country_key = "KFC_HR"
+    api_source = "WEB"
+    api_auth_source = "WEB_KFC"
+
+    def parse_item(self, item, location):
+        item["branch"] = item.pop("name").removeprefix("KFC ")
+        item["website"] = (
+            "https://kfc.hr/en/restaurants/"
+            + unidecode(location["name"]).lower().replace(" - ", "-").replace(" ", "-")
+            + "-"
+            + item["ref"]
+        )
