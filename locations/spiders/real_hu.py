1f0350a805d5a62c1ad702bf104467082eff4c58
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-31 04:36:41 +0000

    Backport refactoring from autospidergen branch

--- locations/spiders/real_hu.py
+++ locations/spiders/real_hu.py
@@ -5,6 +5,6 @@ from locations.storefinders.maps_marker_pro import MapsMarkerProSpider
 
 class RealHUSpider(MapsMarkerProSpider):
     name = "real_hu"
+    item_attributes = {"brand": "Real", "brand_wikidata": "Q100741414", "extras": Categories.SHOP_CONVENIENCE.value}
     allowed_domains = ["real.hu"]
     days = DAYS_HU
-    item_attributes = {"brand": "Real", "brand_wikidata": "Q100741414", "extras": Categories.SHOP_CONVENIENCE.value}
