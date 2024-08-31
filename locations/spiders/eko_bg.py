5e05388e80623250f368441a3dc30e85ba2fadb7
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-31 04:27:35 +0000

    Backport changes to lighthouse

--- locations/spiders/eko_bg.py
+++ locations/spiders/eko_bg.py
@@ -7,3 +7,5 @@ class EkoBGSpider(LighthouseSpider):
     item_attributes = {"brand": "EKO", "brand_wikidata": "Q111603199", "extras": Categories.FUEL_STATION.value}
     allowed_domains = ["www.eko.bg"]
     start_urls = ["https://www.eko.bg/stations/karta-na-obektite/"]
+
+    
