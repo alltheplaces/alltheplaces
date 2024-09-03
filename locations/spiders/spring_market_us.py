b3e06e4fd91e5b721ce720effcce1948c4023f68
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-03 23:24:22 +0000

    Add Spring-Market

---
+++ locations/spiders/spring_market_us.py
@@ -0,0 +1,8 @@
+from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
+
+
+class SpringMarketUSSpider(StorefrontgatewaySpider):
+    name = "spring_market_us"
+    item_attributes = {"brand": "Spring Market"}
+    start_urls = ["https://storefrontgateway.spring-market.com/api/stores"]
+    requires_proxy = "US"  # Cloudflare geoblocking in use
