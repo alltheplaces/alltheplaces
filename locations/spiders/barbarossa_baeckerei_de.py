8ee2b5267604ebdc54b99cc8400ba9fa01e95c1c
Cj Malone <CjMalone@mail.com>
2024-09-03 15:45:28 +0100

    Remove explicit category, NSI has us

--- locations/spiders/barbarossa_baeckerei_de.py
+++ locations/spiders/barbarossa_baeckerei_de.py
@@ -1,16 +1,9 @@
-from locations.categories import Categories
 from locations.hours import DAYS_DE
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
 class BarbarossaBaeckereiDESpider(WPStoreLocatorSpider):
     name = "barbarossa_baeckerei_de"
-    item_attributes = {
-        "brand": "Barbarossa Bäckerei",
-        "brand_wikidata": "Q807766",
-        "extras": Categories.SHOP_BAKERY.value,
-    }
-    allowed_domains = [
-        "www.barbarossa-baeckerei.de",
-    ]
+    item_attributes = {"brand": "Barbarossa Bäckerei", "brand_wikidata": "Q807766"}
+    allowed_domains = ["www.barbarossa-baeckerei.de"]
     days = DAYS_DE
