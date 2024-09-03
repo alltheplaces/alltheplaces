b3c201b1afb2f0b0d98761537792603b448f41d9
Cj Malone <CjMalone@mail.com>
2024-09-03 15:49:03 +0100

    Drip html unescaped name

--- locations/spiders/barbarossa_baeckerei_de.py
+++ locations/spiders/barbarossa_baeckerei_de.py
@@ -5,5 +5,6 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 class BarbarossaBaeckereiDESpider(WPStoreLocatorSpider):
     name = "barbarossa_baeckerei_de"
     item_attributes = {"brand": "Barbarossa Bäckerei", "brand_wikidata": "Q807766"}
+    drop_attributes = {"name"}
     allowed_domains = ["www.barbarossa-baeckerei.de"]
     days = DAYS_DE
