140a87114dd407a82c34466f227086269d5f3641
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-04 08:48:39 +0930

    Update locations/spiders/cub.py

--- locations/spiders/cub.py
+++ locations/spiders/cub.py
@@ -2,7 +2,7 @@ from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
 
 
 class CubSpider(StorefrontgatewaySpider):
-    name = "cub"
+    name = "cub_foods_us"
     item_attributes = {"brand": "Cub Foods", "brand_wikidata": "Q5191916"}
     start_urls = ["https://storefrontgateway.cub.com/api/stores"]
     requires_proxy = "US"  # Cloudflare geoblocking in use
