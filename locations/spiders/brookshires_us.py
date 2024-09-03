de0001e8f07fc03470315ddab4424558e1d1bd28
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-04 08:45:31 +0930

    Update brookshires_us.py

--- locations/spiders/brookshires_us.py
+++ locations/spiders/brookshires_us.py
@@ -5,3 +5,4 @@ class BrookshiresUSSpider(StorefrontgatewaySpider):
     name = "brookshires_us"
     item_attributes = {"brand": "Brookshires", "brand_wikidata": "Q7427974"}
     start_urls = ["https://storefrontgateway.brookshires.com/api/stores"]
+    requires_proxy = "US" # Cloudflare geoblocking
