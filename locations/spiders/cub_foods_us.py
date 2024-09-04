80dbfd57b978620901386777941492fe12667dd5
Ian Dees <ian.dees@gmail.com>
2024-09-03 19:57:34 -0500

    Try without proxy

--- locations/spiders/cub_foods_us.py
+++ locations/spiders/cub_foods_us.py
@@ -5,4 +5,3 @@ class CubFoodsUSSpider(StorefrontgatewaySpider):
     name = "cub_foods_us"
     item_attributes = {"brand": "Cub Foods", "brand_wikidata": "Q5191916"}
     start_urls = ["https://storefrontgateway.cub.com/api/stores"]
-    requires_proxy = "US"  # Cloudflare geoblocking in use
