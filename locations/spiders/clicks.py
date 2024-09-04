40a61bba88b4c136b6e0ecee15374a9b0260609b
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-04 15:44:22 +0200

    Skip country from domain

--- locations/spiders/clicks.py
+++ locations/spiders/clicks.py
@@ -14,6 +14,7 @@ class ClicksSpider(CrawlSpider, SitemapSpider):
     allowed_domains = ["clicks.co.za"]
     sitemap_urls = ["https://clicks.co.za/sitemap.xml"]
     sitemap_rules = [(r"/store/", "parse")]
+    skip_auto_cc_domain = True
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
