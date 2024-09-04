f4627ce6a2fc69da3a7a16dfac655529055049a1
Ian Dees <ian.dees@gmail.com>
2024-09-03 19:58:15 -0500

    Try without proxy first

--- locations/spiders/spring_market_us.py
+++ locations/spiders/spring_market_us.py
@@ -5,4 +5,3 @@ class SpringMarketUSSpider(StorefrontgatewaySpider):
     name = "spring_market_us"
     item_attributes = {"brand": "Spring Market"}
     start_urls = ["https://storefrontgateway.spring-market.com/api/stores"]
-    requires_proxy = "US"  # Cloudflare geoblocking in use
