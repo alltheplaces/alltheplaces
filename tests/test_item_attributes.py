35b6e81fb74b7735e6f2d4151dab21b6e4e36860
panasonic-mafia <kolosov.yv@gmail.com>
2024-09-04 12:23:52 +0200

    [TESTS] speed up item_attributes test

--- tests/test_item_attributes.py
+++ tests/test_item_attributes.py
@@ -1,17 +1,15 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
+from scrapy.utils.misc import walk_modules
+from scrapy.utils.spider import iter_spider_classes
 
-from locations.exporters.geojson import find_spider_class
+from locations.settings import SPIDER_MODULES
 
 
 def test_item_attributes_type():
-    settings = get_project_settings()
-    process = CrawlerProcess(settings)
+    for mod in SPIDER_MODULES:
+        for module in walk_modules(mod):
+            for spider_class in iter_spider_classes(module):
+                item_attributes = getattr(spider_class, "item_attributes", {})
+                assert isinstance(item_attributes, dict)
 
-    for name in process.spider_loader.list():
-        spider = find_spider_class(name)
-        item_attributes = getattr(spider, "item_attributes", {})
-        assert isinstance(item_attributes, dict)
-
-        if extras := item_attributes.get("extras"):
-            assert isinstance(extras, dict)
+                if extras := item_attributes.get("extras"):
+                    assert isinstance(extras, dict)
