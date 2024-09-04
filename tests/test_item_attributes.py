706b29be3bc7b138559602b94fa08b34f6e1d6f4
panasonic-mafia <kolosov.yv@gmail.com>
2024-09-04 13:54:43 +0200

    wrap iteration of spider classes for all modules into a function

--- tests/test_item_attributes.py
+++ tests/test_item_attributes.py
@@ -1,15 +1,10 @@
-from scrapy.utils.misc import walk_modules
-from scrapy.utils.spider import iter_spider_classes
-
-from locations.settings import SPIDER_MODULES
+from locations.exporters.geojson import iter_spider_classes_in_all_modules
 
 
 def test_item_attributes_type():
-    for mod in SPIDER_MODULES:
-        for module in walk_modules(mod):
-            for spider_class in iter_spider_classes(module):
-                item_attributes = getattr(spider_class, "item_attributes", {})
-                assert isinstance(item_attributes, dict)
+    for spider_class in iter_spider_classes_in_all_modules():
+        item_attributes = getattr(spider_class, "item_attributes", {})
+        assert isinstance(item_attributes, dict)
 
-                if extras := item_attributes.get("extras"):
-                    assert isinstance(extras, dict)
+        if extras := item_attributes.get("extras"):
+            assert isinstance(extras, dict)
