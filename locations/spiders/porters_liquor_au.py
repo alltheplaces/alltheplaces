4b4c1de5b87032bfe522dc3470e044064119d375
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-04 08:34:59 +0000

    Add Porters Liquor

--- locations/spiders/porters_liquor_au.py
+++ locations/spiders/porters_liquor_au.py
@@ -1,13 +1,10 @@
 from locations.categories import Categories
 from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
 
+
 class PortersLiquorAuSpider(StorefrontgatewaySpider):
     name = "porters_liquor_au"
     start_urls = [
         "https://storefrontgateway.portersliquor.com.au/api/stores",
     ]
-    item_attributes = {
-        "brand": "Porters Liquor",
-        "extras": Categories.SHOP_ALCOHOL.value
-    }
-
+    item_attributes = {"brand": "Porters Liquor", "extras": Categories.SHOP_ALCOHOL.value}
