d3b484ff240f4c5eb1694e87455b70992ba01910
David Hicks <david@hicks.id.au>
2024-09-04 10:11:32 +1000

    hubo_nl: fix spider

--- locations/spiders/hubo_nl.py
+++ locations/spiders/hubo_nl.py
@@ -1,21 +1,40 @@
-import html
+from typing import Iterable
 
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Response
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories
+from locations.hours import DAYS_EN, DAYS_FULL, OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class HuboNLSpider(CrawlSpider, StructuredDataSpider):
+class HuboNLSpider(JSONBlobSpider):
     name = "hubo_nl"
-    item_attributes = {"brand": "Hubo", "brand_wikidata": "Q5473953"}
-    start_urls = ["https://www.hubo.nl/winkels"]
-    rules = [
-        Rule(LinkExtractor(allow=r"https://www\.hubo\.nl/[-\w]+/contact$"), callback="parse_sd"),
-    ]
+    item_attributes = {"brand": "Hubo", "brand_wikidata": "Q5473953", "extras": Categories.SHOP_DOITYOURSELF.value}
+    allowed_domains = ["www.hubo.nl"]
+    start_urls = ["https://www.hubo.nl/api/storelocator/stores"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["branch"] = html.unescape(item.pop("name").removeprefix("Hubo "))
-        item["city"] = html.unescape(item["city"])
-        item["website"] = response.url
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = feature["title"]
+        item.pop("name", None)
+        item["lat"] = feature["address_data"]["location_lat"]
+        item["lon"] = feature["address_data"]["location_lng"]
+        item["street_address"] = feature["address_data"]["address"]
+        item["city"] = feature["address_data"]["city"]
+        item["postcode"] = feature["address_data"]["postal_code"]
+        item["phone"] = feature["address_data"]["phone_number"]
+
+        if feature.get("url"):
+            item["website"] = "https://www.hubo.nl" + feature["url"]
+
+        item["opening_hours"] = OpeningHours()
+        for day_name, day_hours in feature.get("opening_hours", {}).items():
+            if day_name.title() not in DAYS_FULL:
+                continue
+            if isinstance(day_hours, dict):
+                item["opening_hours"].add_range(day_name.title(), day_hours["from"], day_hours["until"])
+            elif day_hours == "Gesloten":  # Closed
+                item["opening_hours"].set_closed(DAYS_EN[day_name.title()])
+
         yield item
