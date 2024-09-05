4bdbd2f044394db988ab9c73a01f9bf073a71bb9
arrival-spring <lnnunns+git@gmail.com>
2024-09-05 21:30:50 +0200

    Refactor BrowArt23USSpider to JSONBlobSpider and collect opening hours

--- locations/spiders/brow_art_23_us.py
+++ locations/spiders/brow_art_23_us.py
@@ -1,31 +1,33 @@
-from typing import Any
+from scrapy import Selector
 
-from scrapy import Selector, Spider
-from scrapy.http import Response
-
+from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class BrowArt23USSpider(Spider):
+class BrowArt23USSpider(JSONBlobSpider):
     name = "brow_art_23_us"
     item_attributes = {"brand": "Brow Art 23", "brand_wikidata": "Q115675881"}
     start_urls = [
         "https://browart23.com/wp-admin/admin-ajax.php?action=yith_sl_get_results&context=frontend&filters[radius][]=500"
     ]
     requires_proxy = True
+    locations_key = "markers"
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json()["markers"]:
-            item = Feature()
-            item["ref"] = location["id"]
-            item["branch"] = location["name"]
-            item["lat"] = location["latitude"]
-            item["lon"] = location["longitude"]
-            item["website"] = "https://browart23.com/store-locator/{}/".format(location["slug"])
+    def post_process_item(self, item, response, feature):
+        item = Feature()
+        item["ref"] = feature["id"]
+        item["branch"] = feature["name"]
+        item["website"] = f"https://browart23.com/store-locator/{feature["slug"]}/"
 
-            sel = Selector(text=location["pin_modal"])
-            item["addr_full"] = merge_address_lines(sel.xpath('//p[@class="store-address"]/text()').getall())
-            item["phone"] = sel.xpath('//li[@class="store-phone"]/a/text()').get()
+        sel = Selector(text=feature["pin_modal"])
+        item["addr_full"] = merge_address_lines(sel.xpath('//p[@class="store-address"]/text()').getall())
+        item["phone"] = sel.xpath('//li[@class="store-phone"]/a/text()').get()
 
-            yield item
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(
+            " ".join(sel.xpath('//div[@class="store-description"]/p/text()').getall())
+        )
+
+        yield item
