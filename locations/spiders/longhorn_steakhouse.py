3306ef5651ec339828ed28b498009f87e50484ec
jhutton1 <jaimehutton01@gmail.com>
2024-09-05 11:41:56 -0400

    fix spider

--- locations/spiders/longhorn_steakhouse.py
+++ locations/spiders/longhorn_steakhouse.py
@@ -1,69 +1,30 @@
-import json
-import re
+from typing import Any
 
-import scrapy
+from scrapy import Spider
+from scrapy.http import Response, JsonRequest
 
-from locations.hours import OpeningHours
-from locations.items import Feature
+from locations.dict_parser import DictParser
 
-
-class LonghornSteakhouseSpider(scrapy.Spider):
+class LonghornSteakhouseSpider(Spider):
     name = "longhorn_steakhouse"
     item_attributes = {"brand": "LongHorn Steakhouse", "brand_wikidata": "Q3259007"}
-    allowed_domains = []
-    start_urls = [
-        "https://www.longhornsteakhouse.com/locations-sitemap.xml",
-    ]
-    custom_settings = {
-        "ROBOTSTXT_OBEY": False,
-        "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36 RuxitSynthetic/1.0 v2946028852165593646 t2919217341348717815",
-    }
-    download_delay = 0.3
 
-    def parse_hours(self, hours):
-        opening_hours = OpeningHours()
+    def start_requests(self):
+        url = "https://m.longhornsteakhouse.com/api/restaurants?"
+        yield JsonRequest(url=url, headers={"X-Source-Channel": "WEB"})
 
-        for hour in hours:
-            day, open_close = hour.split(" ")
-            open_time, close_time = open_close.split("-")
-            opening_hours.add_range(day=day, open_time=open_time, close_time=close_time, time_format="%H:%M")
-        return opening_hours.as_opening_hours()
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["restaurants"]:
+            location.update(location.pop("contactDetail"))
+            item = DictParser.parse(location)
+            item["ref"] = location["restaurantNumber"]
+            item["branch"] = location["restaurantName"]
+            item["phone"] = location["phoneDetail"][0].get("phoneNumber")
+            item["street_address"] =  location["address"].get("street1")
+            item["lat"] = location["address"]["coordinates"]["latitude"]
+            item["lon"] = location["address"]["coordinates"]["longitude"]
+ 
+            yield item
 
-    def parse(self, response):
-        response.selector.remove_namespaces()
-        urls = response.xpath("//url/loc/text()").extract()
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_store)
 
-    def parse_store(self, response):
-        store_data = response.xpath(
-            '//script[@type="application/ld+json" and contains(text(), "streetAddress")]/text()'
-        ).extract_first()
-        if store_data:
-            data = json.loads(store_data)
-            ref = re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1)
-
-            # Handle store pages that are missing the application/ld+json data
-            addr, city_state_zip, phone = response.xpath('//p[@id="info-link-webhead"]/text()').extract()
-            city, state, postcode = re.search(r"(.*?),\s([A-Z]{2})\s([\d-]+)$", city_state_zip).groups()
-
-            properties = {
-                "name": data.get("name") or response.xpath('//h1[@class="style_h1"]/text()').extract_first().strip(),
-                "ref": data["branchCode"] or ref,
-                "addr_full": data["address"]["streetAddress"].strip() or addr.strip(),
-                "city": data["address"]["addressLocality"] or city,
-                "state": data["address"]["addressRegion"] or state,
-                "postcode": data["address"]["postalCode"] or postcode,
-                "country": data["address"]["addressCountry"],
-                "phone": data.get("telephone") or phone.strip(),
-                "website": data.get("url") or response.url,
-                "lat": float(data["geo"]["latitude"]),
-                "lon": float(data["geo"]["longitude"]),
-            }
-
-            hours = data.get("openingHours")
-            if hours:
-                store_hours = self.parse_hours(hours)
-                properties["opening_hours"] = store_hours
-
-            yield Feature(**properties)
+   
