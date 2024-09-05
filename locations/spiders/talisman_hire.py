0025f96dd25753c0690aa5b0f84f0f818644c239
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-05 14:55:47 +0200

    Update talisman_hire.py

--- locations/spiders/talisman_hire.py
+++ locations/spiders/talisman_hire.py
@@ -2,6 +2,6 @@ from locations.storefinders.location_bank import LocationBankSpider
 
 
 class TalismanHireSpider(LocationBankSpider):
-    name = "talisman_hire_company"
+    name = "talisman_hire"
     client_id = "e291ff7a-451d-4eee-a25b-a834c9fe6eeb"
     item_attributes = {"brand": "Talisman Hire", "brand_wikidata": "Q120885726"}
