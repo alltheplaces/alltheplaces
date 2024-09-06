c1d72127c033dee17fe13e55ac51c9dbf3ae01ec
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-07 09:28:48 +0930

    Update freshslice_pizza_ca.py

--- locations/spiders/freshslice_pizza_ca.py
+++ locations/spiders/freshslice_pizza_ca.py
@@ -3,6 +3,7 @@ from locations.hours import DAYS_EN
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
+# NOTE: Produces ~50 results; however coordinate based search only produes 45 results.
 class FreshslicePizzaCASpider(WPStoreLocatorSpider):
     name = "freshslice_pizza_ca"
     item_attributes = {"brand_wikidata": "Q5503082", "brand": "Freshslice Pizza", "extras": Categories.FAST_FOOD.value}
