b8db85905ea0fc790657aa24bb450aa0cb6cf770
Spencer Alves <impiaaa@gmail.com>
2024-09-06 14:11:30 -0700

    Refactor all Wakefern brands to a shared parent class; Add Gourmet Garage (3 locations), as well as Smart & Final (255 locations) (#10061)
    * Refactor all Wakefern brands to a shared parent class; Add Gourmet Garage (3 locations), as well as Smart & Final (255 locations)
    
    * Switch all Wakefern spiders to Storefrontgateway
    
    * Switch Smart & Final to StorefrontgatewaySpider
    
    * Remove JSON blob Wakefern base

--- locations/spiders/gourmet_garage_us.py
+++ locations/spiders/gourmet_garage_us.py
@@ -4,21 +4,21 @@ from locations.categories import Categories
 from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
 
 
-class FairwayMarketUSSpider(StorefrontgatewaySpider):
-    name = "fairway_market_us"
+class GourmetGarageUSSpider(StorefrontgatewaySpider):
+    name = "gourmet_garage_us"
     item_attributes = {
-        "brand": "Fairway Market",
-        "brand_wikidata": "Q5430910",
-        "name": "Fairway Market",
+        "brand": "Gourmet Garage",
+        "brand_wikidata": "Q16994340",
+        "name": "Gourmet Garage",
         "extras": Categories.SHOP_SUPERMARKET.value,
     }
     start_urls = ["https://storefrontgateway.brands.wakefern.com/api/stores"]
-    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"X-Site-Host": "https://www.fairwaymarket.com/"}}
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"X-Site-Host": "https://www.gourmetgarage.com/"}}
 
     def post_process_item(self, item, response, location):
         if location["type"] != "Regular":
             return
-        item["website"] = f"https://www.fairwaymarket.com/sm/planning/rsid/{item['ref']}"
+        item["website"] = f"https://www.gourmetgarage.com/sm/planning/rsid/{item['ref']}"
         split_name = re.split(r"\s+of\s+", item["name"], flags=re.IGNORECASE)
         if len(split_name) == 2:
             item["name"], item["branch"] = split_name
