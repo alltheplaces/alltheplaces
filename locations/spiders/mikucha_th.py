d4850541ffa5207bf010102709cc114e79b339cf
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-07 06:24:43 +0000

    Refactor to common storefinder

--- locations/spiders/mikucha_th.py
+++ locations/spiders/mikucha_th.py
@@ -1,47 +1,17 @@
 import scrapy
 
 from locations.items import Feature
+from locations.storefinders.wordpress_heron_foods_spider import WordpressHeronFoodsSpider
 
 
-class MikuchaTHSpider(scrapy.Spider):
+class MikuchaTHSpider(WordpressHeronFoodsSpider):
     name = "mikucha_th"
     item_attributes = {
         "brand_wikidata": "Q118640408",
         "brand": "Mikucha",
     }
-    allowed_domains = [
-        "mikucha.co.th",
-    ]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    radius = 1000
+    domain = "mikucha.co.th"
+    lat = 13.7563309
+    lon = 100.5017651
 
-    # Similar to HerronFoodsSpider
-    def start_requests(self):
-        yield scrapy.FormRequest(
-            url="https://mikucha.co.th/wp-admin/admin-ajax.php",
-            formdata={
-                "action": "get_stores",
-                "lat": "13.7563309",
-                "lng": "100.5017651",
-                "radius": "1000",
-            },
-            callback=self.parse,
-        )
-
-    def parse(self, response):
-        stores = response.json()
-        for i in range(0, len(stores)):
-            store = stores[str(i)]
-
-            properties = {
-                "lat": store["lat"],
-                "lon": store["lng"],
-                "name": store["na"],
-                "street_address": store["st"].strip(" ,"),
-                "city": store["ct"].strip(),
-                "postcode": store["zp"].strip(),
-                "country": "TH",
-                "website": store["gu"],
-                "ref": store["ID"],
-            }
-
-            yield Feature(**properties)
