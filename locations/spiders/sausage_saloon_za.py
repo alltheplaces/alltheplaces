3759ff0ae64a9a7dafd5ccb0cb9582f53be70238
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-07 06:30:53 +0000

    Refactor to common storefinder

--- locations/spiders/sausage_saloon_za.py
+++ locations/spiders/sausage_saloon_za.py
@@ -3,30 +3,12 @@ from typing import Any
 import scrapy
 from scrapy import Spider
 
-from locations.dict_parser import DictParser
+from locations.storefinders.wordpress_heron_foods_spider import WordpressHeronFoodsSpider
 
-
-class SausageSaloonZASpider(Spider):
+class SausageSaloonZASpider(WordpressHeronFoodsSpider):
     name = "sausage_saloon_za"
     item_attributes = {"brand": "Sausage Saloon", "brand_wikidata": "Q116619342"}
-
-    def start_requests(self):
-        form_data = {"action": "get_stores", "lat": "-29", "lng": "24", "radius": "10000000"}
-        yield scrapy.http.FormRequest(
-            url="https://www.sausagesaloon.co.za/wp-admin/admin-ajax.php",
-            method="POST",
-            formdata=form_data,
-            callback=self.parse,
-        )
-
-    def parse(self, response, **kwargs: Any) -> Any:
-        for index, location in response.json().items():
-            item = DictParser.parse(location)
-            item["name"] = location["na"]
-            item["phone"] = location.get("te", "")
-            item["street_address"] = location["st"]
-            item["city"] = location["ct"]
-            item["postcode"] = location["zp"]
-            item["website"] = location["gu"]
-
-            yield item
+    domain = "www.sausagesaloon.co.za"
+    radius = 10000000
+    lat = -29
+    lon = 24
