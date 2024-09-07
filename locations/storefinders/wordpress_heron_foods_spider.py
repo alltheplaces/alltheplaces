764021a4db8f2f550957b8471a10dd3ca9ef0014
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-07 16:12:28 +0930

    Update wordpress_heron_foods_spider.py

--- locations/storefinders/wordpress_heron_foods_spider.py
+++ locations/storefinders/wordpress_heron_foods_spider.py
@@ -20,6 +20,7 @@ class WordpressHeronFoodsSpider(Spider):
     radius = 600
     lat = None
     lon = None
+    detected_rules = [] # DetectionRequestRule(https://{self.domain}/wp-admin/admin-ajax.php, but its a POST and has get_stores), or DetectionResponseRule(jsblob with na zp lng lon ID)
 
     def start_requests(self):
         yield scrapy.FormRequest(
