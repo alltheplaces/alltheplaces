86b41dae7dd2698983faa20246160fa184ae6cf7
arrival-spring <lnnunns+git@gmail.com>
2024-09-07 19:31:40 +0200

    Docs and variable name change

--- locations/name_suggestion_index.py
+++ locations/name_suggestion_index.py
@@ -96,18 +96,18 @@ class NSI(metaclass=Singleton):
                 if s in NSI.normalise(label):
                     yield k, v
 
-    def iter_country(self, country_code=None):
+    def iter_country(self, location_code=None):
         """
         Lookup by country code match in the NSI.
-        :param country_code: country code or NSI location to search for
+        :param location_code: country code or NSI location to search for
         :return: iterator of matching NSI wikidata.json entries
         """
         self._ensure_loaded()
         for v in self.nsi_json.values():
             for item in v["items"]:
-                if not country_code:
+                if not location_code:
                     yield item
-                elif country_code.lower() in item["locationSet"].get("include"):
+                elif location_code.lower() in item["locationSet"].get("include"):
                     yield item
 
     def iter_nsi(self, wikidata_code=None):
