f0f6931fe0227847e33dac606269f40fdf449f3d
arrival-spring <lnnunns+git@gmail.com>
2024-09-08 13:17:05 +0200

    US only

--- locations/spiders/medexpress_us.py
+++ locations/spiders/medexpress_us.py
@@ -4,8 +4,8 @@ from locations.items import set_closed
 from locations.json_blob_spider import JSONBlobSpider
 
 
-class MedexpressSpider(JSONBlobSpider):
-    name = "medexpress"
+class MedexpressUSSpider(JSONBlobSpider):
+    name = "medexpress_us"
     item_attributes = {"brand": "MedExpress", "brand_wikidata": "Q102183820"}
     start_urls = ["https://www.medexpress.com/bin/optum3/medexserviceCallToYEXT2"]
     locations_key = "locations"
