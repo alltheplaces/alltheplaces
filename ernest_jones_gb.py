302f76142831ad7bd9edeb6f0b108da53bf87882
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-09 22:16:46 +0200

    Create ernest_jones_gb.py

---
+++ ernest_jones_gb.py
@@ -0,0 +1,15 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from locations.structured_data_spider import StructuredDataSpider
+
+class ErnestJonesGBSpider(CrawlSpider, StructuredDataSpider):
+    name = "ernest_jones_gb"
+    item_attributes = {
+        "brand": "Ernest Jones",
+        "brand_wikidata": "Q5393358",
+        "country": "GB",
+    }
+    start_urls = ["https://www.ernestjones.co.uk/store-finder/view-stores/GB%20Region"]
+    rules = [Rule(LinkExtractor(allow="/store/"), callback="parse", follow=True)]
+    
