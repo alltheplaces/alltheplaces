6980e98ba23593a9237bb3fa150ef9bb477106b9
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-09 22:56:08 +0200

    Update ernest_jones_gb.py

--- ernest_jones_gb.py
+++ ernest_jones_gb.py
@@ -11,5 +11,6 @@ class ErnestJonesGBSpider(CrawlSpider, StructuredDataSpider):
         "brand_wikidata": "Q5393358",
         "country": "GB",
     }
+    allowed_domains = ["www.ernestjones.co.uk"]
     start_urls = ["https://www.ernestjones.co.uk/store-finder/view-stores/GB%20Region"]
     rules = [Rule(LinkExtractor(allow="/store/"), callback="parse", follow=True)]
