66b11a1524276e72851bd00207f9ab82b9546c97
Joe Dixon <mjoe999@gmail.com>
2024-09-11 13:08:26 +0100

    [Vanden Borre] add 72 Belgian electronic shops

---
+++ locations/spiders/vandenborre_be.py
@@ -0,0 +1,15 @@
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class VandenBorreBESpider(StructuredDataSpider):
+    name = "vanden_borre_be"
+    item_attributes = {"brand": "Vanden Borre", "brand_wikidata": "Q3554485"}
+    start_urls = ["https://www.vandenborre.be/web/json/shops_NL.json"]
+
+    def parse(self, response, **kwargs):
+        for store in response.json()["markers"]:
+            yield response.follow(store["url"], self.parse_sd)
+
+
+#
+#            print(store["url"])
