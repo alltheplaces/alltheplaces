0df87501a54c4117757c6b592403b8e9ff3a8f0c
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-12 02:58:34 +0200

    Add TymebankZASpider (1053 locations) and refactor LocationBank storefinder for pre and post processing (#10180)
    * Add TymebankZASpider (1035 locations) and refactor LocationBank storefinder for pre and post procssing
    
    * Fix filename
    
    * Add category and located_in

--- locations/spiders/african_bank_za.py
+++ locations/spiders/african_bank_za.py
@@ -7,7 +7,7 @@ class AfricanBankZASpider(LocationBankSpider):
     client_id = "6ef8638f-af8c-409d-ad24-cf3422269370"
     item_attributes = {"brand": "African Bank", "brand_wikidata": "Q4689703", "extras": Categories.BANK.value}
 
-    def parse_item(self, item, location):
+    def post_process_item(self, item, response, location):
         apply_yes_no(Extras.ATM, item, "ATM" in location["slAttributes"])
         # Unhandled: "Business Services", "Specialized Services"
         yield item
