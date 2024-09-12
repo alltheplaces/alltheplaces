57616190c8f407fcf27322cd5e1bd27c60e14031
panasonic-mafia <kolosov.yv@gmail.com>
2024-09-12 12:31:05 +0200

    minor clean

--- locations/spiders/fairprice_sg.py
+++ locations/spiders/fairprice_sg.py
@@ -1,4 +1,4 @@
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
@@ -9,7 +9,6 @@ class FairpriceSGSpider(JSONBlobSpider):
     item_attributes = {
         "brand": "NTUC Fairprice",
         "brand_wikidata": "Q6955519",
-        "extras": Categories.SHOP_SUPERMARKET.value,
     }
     start_urls = ["https://public-api.omni.fairprice.com.sg/stores"]
 
@@ -25,18 +24,21 @@ class FairpriceSGSpider(JSONBlobSpider):
             item["name"] = location.get("storeType")
             item["street_address"] = None
             self.parse_hours(item, location)
+            apply_category(Categories.SHOP_SUPERMARKET, item)
             yield item
 
     def parse_hours(self, item: Feature, location: dict):
+        sngBusinessHours = location.get("sngBusinessHours", {}).get("Valid")
+
         if location.get("is24Hour"):
             item["opening_hours"] = "24/7"
             return
-        elif location.get("sngBusinessHours", {}).get("Valid") == False:
+        elif not sngBusinessHours:
             # Store works daily
             oh = OpeningHours()
             oh.add_days_range(DAYS, location.get("fromTime"), location.get("toTime"), time_format="%H:%M:%S")
             item["opening_hours"] = oh
             return
-        elif location.get("sngBusinessHours", {}).get("Valid") == True:
+        elif sngBusinessHours:
             # Only 17 stores have this format, it's not worth to implement
             return
