b422be60b46f8263db9ade5c28a5044ba24ad963
panasonic-mafia <kolosov.yv@gmail.com>
2024-09-13 12:37:10 +0200

    [lidl spiders] fixes after virtualearth spider refactoring

--- locations/spiders/lidl_sk.py
+++ locations/spiders/lidl_sk.py
@@ -7,5 +7,5 @@ class LidlSKSpider(LidlATSpider):
 
     dataset_id = "018f9e1466694a03b6190cb8ccc19272"
     dataset_name = "Filialdaten-SK/Filialdaten-SK"
-    key = "AqN50YiXhDtZtWqXZcb7nWvF-4Xc9rg9IXd6YWepqk4WnlmbvD-NV3KHA3A0dOtw"
+    api_key = "AqN50YiXhDtZtWqXZcb7nWvF-4Xc9rg9IXd6YWepqk4WnlmbvD-NV3KHA3A0dOtw"
     days = DAYS_SK
