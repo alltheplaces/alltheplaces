1b113565cca3e584dd02ec68e6f7efa6dd705516
Ian Dees <ian.dees@gmail.com>
2024-09-13 10:25:17 -0500

    Try without proxy first.

--- locations/spiders/jd_sports_es.py
+++ locations/spiders/jd_sports_es.py
@@ -9,4 +9,3 @@ class JdSportsESSpider(CrawlSpider, StructuredDataSpider):
     item_attributes = {"brand": "JD Sports", "brand_wikidata": "Q6108019"}
     start_urls = ["https://www.jdsports.es/store-locator/all-stores/"]
     rules = [Rule(LinkExtractor(allow="store-locator/", deny="-soon"), callback="parse_sd")]
-    requires_proxy = True
--- locations/spiders/jd_sports_it.py
+++ locations/spiders/jd_sports_it.py
@@ -9,4 +9,3 @@ class JdSportsITSpider(CrawlSpider, StructuredDataSpider):
     item_attributes = {"brand": "JD Sports", "brand_wikidata": "Q6108019"}
     start_urls = ["https://www.jdsports.it/store-locator/all-stores/"]
     rules = [Rule(LinkExtractor(allow="store-locator/", deny="-soon"), callback="parse_sd")]
-    requires_proxy = True
