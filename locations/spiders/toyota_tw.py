b573703a2d6fb84198321d77398af190136f9e1a
Joe Dixon <mjoe999@gmail.com>
2024-09-13 07:34:50 +0100

    clean up a little

--- locations/spiders/toyota_tw.py
+++ locations/spiders/toyota_tw.py
@@ -1,26 +1,21 @@
-import scrapy
-
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
+from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.toyota_us import ToyotaUSSpider
 
 
-class ToyotaTWSpider(scrapy.Spider):
+class ToyotaTWSpider(JSONBlobSpider):
     name = "toyota_tw"
-    item_attributes = {
-        "brand": "Toyota",
-        "brand_wikidata": "Q53268",
-    }
+    item_attributes = ToyotaUSSpider.item_attributes
     start_urls = ["https://www.toyota.com.tw/api/location.ashx"]
+    locations_key = "DATA"
 
-    def parse(self, response, **kwargs):
-        for store in response.json()["DATA"]:
-            item = DictParser.parse(store)
-            item["website"] = "https://www.toyota.com.tw/"
-            if store["TYPE"] == "1":
-                item["ref"] = "-".join([store["KEY"], "Dealer"])
-                apply_category(Categories.SHOP_CAR, item)
-            elif store["TYPE"] == "2":
-                item["ref"] = "-".join([store["KEY"], "Service"])
-                apply_category(Categories.SHOP_CAR_REPAIR, item)
+    def post_process_item(self, item, response, location: dict):
+        item["website"] = "https://www.toyota.com.tw/"
+        if location["TYPE"] == "1":
+            item["ref"] = "-".join([location["KEY"], "Dealer"])
+            apply_category(Categories.SHOP_CAR, item)
+        elif location["TYPE"] == "2":
+            item["ref"] = "-".join([location["KEY"], "Service"])
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
 
-            yield item
+        yield item
