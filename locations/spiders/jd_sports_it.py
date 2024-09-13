3da9c045cf8f2fea0fb8962eb8d2ae4efacb3547
ebadm <ebadm.work@gmail.com>
2024-09-13 14:47:58 +0500

    [jd_sports] add locations for de, es, fr, ie, it, pt

---
+++ locations/spiders/jd_sports_it.py
@@ -0,0 +1,11 @@
+from locations.structured_data_spider import StructuredDataSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class JdSportsITSpider(CrawlSpider, StructuredDataSpider):
+    name = "jd_sports_it"
+    item_attributes = {"brand": "JD Sports", "brand_wikidata": "Q6108019"}
+    start_urls = ["https://www.jdsports.it/store-locator/all-stores/"]
+    rules = [Rule(LinkExtractor(allow="store-locator/", deny="-soon"), callback="parse_sd")]
+    requires_proxy = True
---
+++ locations/spiders/jd_sports_pt.py
@@ -0,0 +1,11 @@
+from locations.structured_data_spider import StructuredDataSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class JdSportsPTSpider(CrawlSpider, StructuredDataSpider):
+    name = "jd_sports_pt"
+    item_attributes = {"brand": "JD Sports", "brand_wikidata": "Q6108019"}
+    start_urls = ["https://www.jdsports.pt/store-locator/all-stores/"]
+    rules = [Rule(LinkExtractor(allow="store-locator/", deny="-soon"), callback="parse_sd")]
+    requires_proxy = True
