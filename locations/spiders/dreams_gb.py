a836b0a3e93a929b688a78391f8a67b38fa96d0a
Cj Malone <CjMalone@mail.com>
2024-09-15 11:02:56 +0100

    Switch to Yext spider

--- locations/spiders/dreams_gb.py
+++ locations/spiders/dreams_gb.py
@@ -1,16 +1,20 @@
-import scrapy
+from locations.spiders.central_england_cooperative import set_operator
+from locations.storefinders.yext import YextSpider
 
-from locations.linked_data_parser import LinkedDataParser
 
-
-class DreamsGBSpider(scrapy.spiders.SitemapSpider):
+class DreamsGBSpider(YextSpider):
     name = "dreams_gb"
-    item_attributes = {
-        "brand": "Dreams",
-        "brand_wikidata": "Q5306688",
-    }
-    sitemap_urls = ["https://www.dreams.co.uk/sitemap-store.xml"]
+    item_attributes = {"brand": "Dreams", "brand_wikidata": "Q5306688"}
+    api_key = "653b917ffb5e9db1dc81b9756563b7c3"
+    api_version = "20220927"
 
-    def parse(self, response):
-        if "store-finder" not in response.url:
-            return LinkedDataParser.parse(response, "FurnitureStore")
+    def parse_item(self, item, location, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Dreams").strip(" -")
+        if item["branch"].endswith(" - FRANCHISEE MAINTAINS"):
+            item["branch"] = item["branch"].removesuffix(" - FRANCHISEE MAINTAINS")
+        else:
+            set_operator(self.item_attributes, item)
+
+        item["website"] = item["website"].split("?", 1)[0]
+
+        yield item
