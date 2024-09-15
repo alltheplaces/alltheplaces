f5dab777975c12910bf980706520f4ad1790484b
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-15 20:10:00 +0930

    Update jimmy_the_greek_ae_ca.py

--- locations/spiders/jimmy_the_greek_ae_ca.py
+++ locations/spiders/jimmy_the_greek_ae_ca.py
@@ -1,6 +1,6 @@
 from locations.storefinders.storerocket import StoreRocketSpider
 
 
-class JimmytheGreekAECASpider(StoreRocketSpider):
+class JimmyTheGreekAECASpider(StoreRocketSpider):
     name = "jimmy_the_greek_ae_ca"
     storerocket_id = "BrJq9MkJqE"
