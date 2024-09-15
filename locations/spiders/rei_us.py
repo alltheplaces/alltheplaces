29f0d8a6e0652b81226bb0e15686c6d33b72e4cb
Ian Dees <ian.dees@gmail.com>
2024-09-15 14:01:40 -0500

    Update use agents and tweak REI

--- locations/spiders/rei_us.py
+++ locations/spiders/rei_us.py
@@ -8,7 +8,6 @@ from locations.user_agents import FIREFOX_LATEST
 class ReiUSSpider(SitemapSpider, StructuredDataSpider):
     name = "rei_us"
     item_attributes = {"brand": "REI", "brand_wikidata": "Q3414933", "country": "US"}
-    allowed_domains = ["www.rei.com"]
     sitemap_urls = ["https://www.rei.com/sitemap-stores.xml"]
     sitemap_rules = [(r"^https://www.rei.com/stores/([^/]+)$", "parse_sd")]
     user_agent = FIREFOX_LATEST
