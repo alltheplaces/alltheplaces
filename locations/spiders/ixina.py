9b66c98b23143eb72f29dc16ef463f3050e6761a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-15 10:22:10 +0000

    Fix pattern

---
+++ locations/spiders/ixina.py
@@ -0,0 +1,16 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class IxinaSpider(SitemapSpider, StructuredDataSpider):
+    name = "ixina"
+    #item_attributes = {"brand_wikidata": "Add if known"}
+    sitemap_urls = ["https://magasins.ixina.fr/sitemap.xml"]
+    sitemap_rules = [
+        (r"https://magasins.ixina.fr/fr/magasins/[\w-]+/[\w-]+/[\w-]+", "parse"),
+    ]
+    # wanted_types = ["GroceryStore"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        yield item
