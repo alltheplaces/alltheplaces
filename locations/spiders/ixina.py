527844813593e4a8481f2e1be5413707d79283c5
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-15 10:22:39 +0000

    Fix pattern

--- locations/spiders/ixina.py
+++ locations/spiders/ixina.py
@@ -5,7 +5,7 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class IxinaSpider(SitemapSpider, StructuredDataSpider):
     name = "ixina"
-    #item_attributes = {"brand_wikidata": "Add if known"}
+    item_attributes = {"brand": "Ixina", "brand_wikidata": "Q3156424"}
     sitemap_urls = ["https://magasins.ixina.fr/sitemap.xml"]
     sitemap_rules = [
         (r"https://magasins.ixina.fr/fr/magasins/[\w-]+/[\w-]+/[\w-]+", "parse"),
