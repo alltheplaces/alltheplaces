065de8a44da76f1def5b2b154f6d4e5342159cab
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-15 19:54:34 +0930

    Update and rename ixina.py to ixina_fr.py

--- locations/spiders/ixina_fr.py
+++ locations/spiders/ixina_fr.py
@@ -3,14 +3,10 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class IxinaSpider(SitemapSpider, StructuredDataSpider):
-    name = "ixina"
+class IxinaFRSpider(SitemapSpider, StructuredDataSpider):
+    name = "ixina_fr"
     item_attributes = {"brand": "Ixina", "brand_wikidata": "Q3156424"}
     sitemap_urls = ["https://magasins.ixina.fr/sitemap.xml"]
     sitemap_rules = [
         (r"https://magasins.ixina.fr/fr/magasins/[\w-]+/[\w-]+/[\w-]+", "parse"),
     ]
-    # wanted_types = ["GroceryStore"]
-
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        yield item
