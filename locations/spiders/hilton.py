18a556bd1f125e4ecb1ce594662c6cd2aa82d406
panasonic-mafia <kolosov.yv@gmail.com>
2024-09-15 20:55:18 +0200

    [hilton] rewamp and fix the spider (8k locations)

--- locations/spiders/hilton.py
+++ locations/spiders/hilton.py
@@ -1,106 +1,183 @@
-from pathlib import Path
-from urllib.parse import urlparse
+import json
 
-import geonamescache
-import scrapy
-from scrapy.spiders import SitemapSpider
+import requests
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.dict_parser import DictParser
 from locations.user_agents import CHROME_LATEST
 
 
-class HiltonSpider(SitemapSpider, StructuredDataSpider):
+class HiltonSpider(Spider):
     name = "hilton"
-    sitemap_urls = ["https://www.hilton.com/sitemap.xml"]
+
     custom_settings = {
-        "USER_AGENT": CHROME_LATEST,
-        "DOWNLOAD_DELAY": 0.2,
+        "ROBOTSTXT_OBEY": False,
+        "DEFAULT_REQUEST_HEADERS": {
+            "User-Agent": CHROME_LATEST,
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+        },
+        "CONCURRENT_REQUESTS": 1,
+        "DOWNLOAD_DELAY": 2,
     }
-    visited_pages = set()
+    token = None
 
-    HILTON_DOUBLETREE = ["DoubleTree by Hilton", "Q2504643"]
-    HILTON_HOTELS = ["Hilton Hotels & Resorts", "Q598884"]
-    # Each hotel has a 7 digit alpha code, the last two letters indicate the brand.
-    my_brands = {
-        "ci": ["Conrad Hotels & Resorts", "Q855525"],
-        "di": HILTON_DOUBLETREE,
-        "dt": HILTON_DOUBLETREE,
-        "es": ["Embassy Suites", "Q5369524"],
-        "gi": ["Hilton Garden Inn", "Q1162859"],
-        "he": HILTON_HOTELS,
-        "hf": HILTON_HOTELS,
-        "hh": HILTON_HOTELS,
-        "hi": HILTON_HOTELS,
-        "hn": HILTON_HOTELS,
-        "hs": HILTON_HOTELS,
-        "ht": ["Home2 Suites by Hilton", "Q5887912"],
-        "hw": ["Homewood Suites by Hilton", "Q5890701"],
-        "hx": ["Hampton by Hilton", "Q5646230"],
-        "ol": ["LXR Hotels & Resorts", "Q64605184"],
-        "on": HILTON_HOTELS,
-        "pe": HILTON_HOTELS,
-        "po": ["Tempo by Hilton", "Q112144357"],
-        "pr": "Hilton Hotels & Resorts",
-        "py": ["Canopy by Hilton", "Q30632909"],
-        "qq": "Curio Collection",
-        "ru": ["Tru by Hilton", "Q24907770"],
-        "tw": HILTON_HOTELS,
-        "ua": ["Motto by Hilton", "Q112144350"],
-        "up": "Tapestry Collection",
-        "wa": ["Waldorf Astoria", "Q3239392"],
+    # More details about brand codes is in filters section of https://www.hilton.com/en/locations/hilton-hotels/
+    BRANDS_MAPPING = {
+        "CH": ("Conrad", "Q855525"),
+        "DT": ("DoubleTree", "Q2504643"),
+        "ES": ("Embassy Suites", "Q5369524"),
+        "GI": ("Hilton Garden Inn", "Q1162859"),
+        "GU": ("Graduate", "Q85846030"),
+        "GV": ("Hilton Grand Vacations", "Q13636126"),
+        "HI": ("Hilton", "Q598884"),
+        "HP": ("Hampton", "Q5646230"),
+        "HT": ("Home2 Suites", "Q5887912"),
+        "HW": ("Homewood Suites", "Q5890701"),
+        "LX": ("Small Luxury Hotels of the World", None),
+        "ND": ("NoMad", None),
+        "OL": ("LXR Hotels & Resorts", "Q64605184"),
+        "OU": ("AutoCamp", None),
+        "PE": ("Spark by Hilton", None),
+        "PO": ("Tempo", "Q112144357"),
+        "PY": ("Canopy", "Q30632909"),
+        "QQ": ("Curio Collection", "Q19903229"),
+        "RU": ("Tru", "Q24907770"),
+        "SA": ("Signia", "Q112144335"),
+        "UA": ("Motto", "Q112144350"),
+        "UP": ("Tapestry Collection", "Q109275422"),
+        "WA": ("Waldorf Astoria", "Q3239392"),
     }
-    gc = geonamescache.GeonamesCache()
-    requires_proxy = True
 
-    def _parse_sitemap(self, response):
-        for x in super()._parse_sitemap(response):
-            if x.url.endswith(".xml"):
-                yield x
-            elif x.url.endswith("/hotel-info/"):
-                hotel_url = x.url.replace("/hotel-info/", "/")
-                hotel_name = Path(urlparse(hotel_url).path).name
+    # TODO: map more services
+    SERVICES_MAPPING = {
+        "adjoiningRooms": None,
+        "adventureStays": None,
+        "airportShuttle": None,
+        "allInclusive": None,
+        "beach": None,
+        "boutique": None,
+        "businessCenter": None,
+        "casino": None,
+        "concierge": None,
+        "digitalKey": None,
+        "dining": None,
+        "evCharging": None,
+        "eveningReception": None,
+        "executiveLounge": None,
+        "fitnessCenter": None,
+        "freeBreakfast": None,
+        "freeParking": None,
+        "freeWifi": Extras.WIFI,
+        "golf": None,
+        "hotelResidence": None,
+        "inRoomKitchen": None,
+        "indoorPool": Extras.SWIMMING_POOL,
+        "luxury": None,
+        "meetingRooms": None,
+        "newHotel": None,
+        "nonSmoking": "smoking=no",
+        "outdoorPool": None,
+        "petsAllowed": None,
+        "petsNotAllowed": None,
+        "resort": None,
+        "roomService": None,
+        "ski": None,
+        "spa": None,
+        "streamingTv": None,
+        "tennisCourt": None,
+    }
 
-                if hotel_name in self.visited_pages:
-                    # There are localized pages for each hotel, don't scrape same hotel twice.
-                    continue
+    def start_requests(self):
+        url = "https://www.hilton.com/dx-customer/auth/applications/token?appName=dx_shop_search_app"
+        headers = {
+            "x-dtpc": "ignore",
+        }
+        data = {"app_id": "096a02c6-e844-41b4-bebf-ec74e3ca3cd4"}
+        yield JsonRequest(url, headers=headers, data=data, callback=self.parse_token)
 
-                yield scrapy.Request(hotel_url, callback=self.parse_sd)
-                self.visited_pages.add(hotel_name)
+    def parse_token(self, response: Response):
+        self.token = response.json()["access_token"]
+        yield from self.get_quadrants()
 
-    def lookup_brand(self, response):
-        if "-dt-doubletree-" in response.url:
-            # Catch the XXXXX-DT rather than XXXXXDT case
-            return self.HILTON_DOUBLETREE
-        splits = response.url.split("/")[-2]
-        code = splits.split("-")[0][-2:]
-        return self.my_brands.get(code)
+    def get_quadrants(self):
+        url = "https://www.hilton.com/graphql/customer?appName=dx_shop_search_app&operationName=hotelQuadrants&originalOpName=hotelQuadrants&bl=en"
+        data = {
+            "query": "query hotelQuadrants {hotelQuadrants{ id } }",
+            "operationName": "hotelQuadrants",
+            "variables": {},
+        }
+        yield JsonRequest(
+            url,
+            headers={
+                "Authorization": f"Bearer {self.token}",
+            },
+            data=data,
+            callback=self.parse_quadrants,
+        )
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if brand := self.lookup_brand(response):
-            if isinstance(brand, str):
-                return
+    def parse_quadrants(self, response: Response):
+        quadrants = response.json()["data"]["hotelQuadrants"]
+        for quadrant in quadrants:
+            yield from self.parse_hotels(quadrant["id"])
 
-            # Last part of url is unique
-            item["ref"] = Path(urlparse(response.url).path).name
-            # Website provided in structured data does not work, so replace it with working url
-            item["website"] = response.url
+    def parse_hotels(self, quadrant_id):
+        response = self.get_hotels(quadrant_id)
 
-            item["brand"], item["brand_wikidata"] = brand
-            # In many cases the street address is set by Hilton to be the full address
-            # of the property. A certain amount of fixup can be attempted.
-            street_address = item["street_address"]
-            splits = street_address.split(", {},".format(item["city"]))
-            if len(splits) == 2:
-                item["addr_full"] = street_address
-                item["street_address"] = splits[0]
-            else:
-                # If we find the country name in the street address treat it as a full address.
-                # Otherwise for those few remaining countries we will leave as a street address
-                # which at time of writing is totally correct.
-                country = self.gc.get_countries().get(item["country"])
-                if country and country["name"].lower() in street_address.lower():
-                    item["addr_full"] = street_address
-                    item["street_address"] = None
+        for poi in response.json()["data"]["hotelSummaryOptions"]["hotels"]:
+            poi.update(poi.pop("address"))
+            poi.update(poi.pop("contactInfo"))
+            item = DictParser.parse(poi)
+            item["ref"] = poi["ctyhocn"]
+            item["website"] = poi.get("facilityOverview", {}).get("homeUrlTemplate")
+            item["lat"] = poi.get("localization", {}).get("coordinate", {}).get("latitude")
+            item["lon"] = poi.get("localization", {}).get("coordinate", {}).get("longitude")
+
+            if brand_code := poi.get("brandCode"):
+                if match := self.BRANDS_MAPPING.get(brand_code):
+                    item["brand"], item["brand_wikidata"] = match
+                else:
+                    self.crawler.stats.inc_value(f"atp/{self.name}/unknown_brand/{brand_code}")
+
+            for service in poi.get("amenityIds", []):
+                if match := self.SERVICES_MAPPING.get(service):
+                    apply_yes_no(match, item, True)
+                else:
+                    self.crawler.stats.inc_value(f"atp/{self.name}/unknown_service/{service}")
+
+            apply_category(Categories.HOTEL, item)
             yield item
-        else:
-            self.logger.error("unable to lookup brand: %s", response.url)
+
+    def get_hotels(self, quadrant_id):
+        # TODO: we should use JsonRequest here, but it doesn't work with this endpoint for some reason!
+        url = "https://www.hilton.com/graphql/customer?appName=dx_shop_search_app&operationName=hotelSummaryOptions&originalOpName=hotelSummaryOptions&bl=en"
+        data = {
+            "operationName": "hotelSummaryOptions",
+            "query": """
+                query 
+                    hotelSummaryOptions($language: String!, $input: HotelSummaryOptionsInput) { 
+                        hotelSummaryOptions(language: $language, input: $input) { 
+                            hotels { 
+                                ctyhocn 
+                                amenityIds 
+                                brandCode 
+                                facilityOverview { allowAdultsOnly homeUrlTemplate } 
+                                name 
+                                display { open openDate preOpenMsg resEnabled resEnabledDate treatments } 
+                                contactInfo { phoneNumber } 
+                                address { addressLine1 city country countryName state stateName _id } 
+                                localization { currencyCode coordinate { latitude longitude } } 
+                                images { master(ratios: [threeByTwo]) { altText ratios { size url } } carousel(ratios: [threeByTwo]) { altText ratios { url size } } } } } }
+                    """
+            ,
+            "variables": {"input": {"guestLocationCountry": "HU", "quadrantId": f"{quadrant_id}"}, "language": "en"},
+        }
+        headers = {
+            "User-Agent": CHROME_LATEST,
+            "Content-Type": "application/json",
+        }
+        return requests.post(url, headers=headers, data=json.dumps(data))
