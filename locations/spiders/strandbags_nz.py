5bcd0c0b041565ac942da910452396cb84cd42d1
Ian Dees <ian.dees@gmail.com>
2024-09-15 10:57:43 -0500

    Class name typo

--- locations/spiders/strandbags_nz.py
+++ locations/spiders/strandbags_nz.py
@@ -1,7 +1,7 @@
 from locations.spiders.strandbags_au import StrandbagsAUSpider
 
 
-class StandbagsNZSpider(StrandbagsAUSpider):
+class StrandbagsNZSpider(StrandbagsAUSpider):
     name = "strandbags_nz"
     key = "u9317"
     drop_attributes = {"state"}
