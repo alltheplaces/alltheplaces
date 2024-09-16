1c045336511984e501f5604187e15dc47ad3ae84
David Hicks <david@hicks.id.au>
2024-09-16 21:54:36 +1000

    Add Hyundai (AU) (543 features) (#10303)
    * Add Hyundai (KR) (681 dealers)
    
    Fixes #8432
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add Hyundai (AU) (543 features)
    
     'atp/brand/Hyundai': 543,
     'atp/brand_wikidata/Q55931': 543,
     'atp/category/shop/car': 179,
     'atp/category/shop/car_parts': 180,
     'atp/category/shop/car_repair': 184,
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/hyundai_au.py
@@ -0,0 +1,40 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.hyundai_kr import HYUNDAI_SHARED_ATTRIBUTES
+
+
+class HyundaiAUSpider(JSONBlobSpider):
+    name = "hyundai_au"
+    item_attributes = HYUNDAI_SHARED_ATTRIBUTES
+    allowed_domains = ["www.hyundai.com"]
+    start_urls = ["https://www.hyundai.com/au/services/v1/findadealer?postcode=0"]
+    locations_key = ["allDealers", "dealers"]
+
+    def parse(self, response: Response) -> Iterable[Feature]:
+        yield from self.parse_feature_array(response, response.json()["allDealers"]["dealers"]) or []
+        yield from self.parse_feature_array(response, response.json()["allDealers"]["serviceDealers"]) or []
+        yield from self.parse_feature_array(response, response.json()["allDealers"]["partsDealers"]) or []
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if feature.get("closed"):
+            return
+
+        item["ref"] = feature.get("code", feature.get("dealerCode"))
+        item["branch"] = feature.get("tradingName")
+
+        if "testDriveModels" in feature.keys():
+            apply_category(Categories.SHOP_CAR, item)
+            item["ref"] = item["ref"] + "_Sales"
+        elif "newWinBkAServiceMobile" in feature.keys():
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
+            item["ref"] = item["ref"] + "_Service"
+        else:
+            apply_category(Categories.SHOP_CAR_PARTS, item)
+            item["ref"] = item["ref"] + "_Parts"
+
+        yield item
