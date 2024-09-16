7167d2053929ee8daced6dd8c5108690a2240245
David Hicks <david@hicks.id.au>
2024-09-16 17:33:23 +1000

    Add Hyundai (NZ) (122 features)
     'atp/brand/Hyundai': 122,
     'atp/brand_wikidata/Q55931': 122,
     'atp/category/shop/boat': 15,
     'atp/category/shop/car': 29,
     'atp/category/shop/car_repair': 44,
     'atp/category/shop/truck': 17,
     'atp/category/shop/truck_repair': 17,

---
+++ locations/spiders/hyundai_nz.py
@@ -0,0 +1,53 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import apply_category, Categories
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.hyundai_kr import HYUNDAI_SHARED_ATTRIBUTES
+
+
+class HyundaiNZSpider(JSONBlobSpider):
+    name = "hyundai_nz"
+    item_attributes = HYUNDAI_SHARED_ATTRIBUTES
+    allowed_domains = ["www.hyundai.co.nz"]
+    start_urls = ["https://www.hyundai.co.nz/hyundai/locations"]
+
+    def parse(self, response: Response) -> Iterable[Feature]:
+        for region in response.json()["Regions"]:
+            yield from self.parse_feature_array(response, region["Dealers"])
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature["Value"])
+        item["branch"] = item.pop("name", None)
+        item.pop("email", None)  # E-mail of primary contact person only. Ignore.
+
+        if isinstance(feature.get("Type"), list) and len(feature["Type"]) > 0:
+            if feature["Type"][0] == "Passenger sales and service":
+                service_feature = item.copy()
+                apply_category(Categories.SHOP_CAR, item)
+                item["ref"] = item["ref"] + "_Sales"
+                apply_category(Categories.SHOP_CAR_REPAIR, service_feature)
+                service_feature["ref"] = service_feature["ref"] + "_Service"
+                yield item
+                yield service_feature
+            elif feature["Type"][0] == "Service only agents":
+                apply_category(Categories.SHOP_CAR_REPAIR, item)
+                item["ref"] = item["ref"] + "_Service"
+                yield item
+            elif feature["Type"][0] == "Truck sales and service":
+                service_feature = item.copy()
+                apply_category(Categories.SHOP_TRUCK, item)
+                item["ref"] = item["ref"] + "_Sales"
+                apply_category(Categories.SHOP_TRUCK_REPAIR, service_feature)
+                service_feature["ref"] = service_feature["ref"] + "_Service"
+                yield item
+                yield service_feature
+            elif feature["Type"][0] == "Marine":
+                apply_category(Categories.SHOP_BOAT, item)
+                apply_category({"boat:repair": "yes"}, item)
+                apply_category({"boat:parts": "yes"}, item)
+                yield item
+            else:
+                raise ValueError("Unknown feature type: {}".format(feature["Type"][0]))
