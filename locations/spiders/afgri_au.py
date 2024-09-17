d6808f6f2875d8d7c217db79d3aa7e1d2af89d1e
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-17 21:13:01 +0200

    Move name to branch

--- locations/spiders/afgri_au.py
+++ locations/spiders/afgri_au.py
@@ -5,3 +5,6 @@ class AfgriAUSpider(StorepointSpider):
     name = "afgri_au"
     item_attributes = {"brand": "AFGRI Equipment", "brand_wikidata": "Q119264464"}
     key = "15f4466ee99d2b"
+
+    def parse_item(self, item, location):
+        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").replace(" - ", "")
