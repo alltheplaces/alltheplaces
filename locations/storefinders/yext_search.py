c12a34f10a64a8a8af7cd6ecf04f4f135b557b6e
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-17 21:56:32 +0200

    Don't always apply payments

--- locations/storefinders/yext_search.py
+++ locations/storefinders/yext_search.py
@@ -65,13 +65,13 @@ class YextSearchSpider(Spider):
                 item["extras"]["ref:google"] = location["profile"].get("googlePlaceId")
 
             if payment_methods := location["profile"].get("paymentOptions"):
-                apply_yes_no(PaymentMethods.CASH, item, "Cash" in payment_methods, False)
-                apply_yes_no(PaymentMethods.CHEQUE, item, "Check" in payment_methods, False)
-                apply_yes_no(PaymentMethods.MASTER_CARD, item, "MasterCard" in payment_methods, False)
-                apply_yes_no(PaymentMethods.VISA, item, "Visa" in payment_methods, False)
-                apply_yes_no(PaymentMethods.AMERICAN_EXPRESS, item, "American Express" in payment_methods, False)
-                apply_yes_no(PaymentMethods.DISCOVER_CARD, item, "Discover" in payment_methods, False)
-                apply_yes_no(PaymentMethods.CONTACTLESS, item, "Contactless Payment" in payment_methods, False)
+                apply_yes_no(PaymentMethods.CASH, item, "Cash" in payment_methods)
+                apply_yes_no(PaymentMethods.CHEQUE, item, "Check" in payment_methods)
+                apply_yes_no(PaymentMethods.MASTER_CARD, item, "MasterCard" in payment_methods)
+                apply_yes_no(PaymentMethods.VISA, item, "Visa" in payment_methods)
+                apply_yes_no(PaymentMethods.AMERICAN_EXPRESS, item, "American Express" in payment_methods)
+                apply_yes_no(PaymentMethods.DISCOVER_CARD, item, "Discover" in payment_methods)
+                apply_yes_no(PaymentMethods.CONTACTLESS, item, "Contactless Payment" in payment_methods)
 
             item["opening_hours"] = self.parse_opening_hours(location)
 
