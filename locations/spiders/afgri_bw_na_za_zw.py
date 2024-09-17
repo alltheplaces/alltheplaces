26e66db0483a96e9f255a41f4a0d8acf6b4f138a
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-17 21:15:48 +0200

    Move name to branch

--- locations/spiders/afgri_bw_na_za_zw.py
+++ locations/spiders/afgri_bw_na_za_zw.py
@@ -5,3 +5,7 @@ class AfgriBWNAZAZWSpider(AgileStoreLocatorSpider):
     name = "afgri_bw_na_za_zw"
     item_attributes = {"brand": "AFGRI Equipment", "brand_wikidata": "Q119264464"}
     allowed_domains = ["afgriequipment.co.za"]
+
+    def post_process_item(self, item, response, feature):
+        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "")
+        yield item
