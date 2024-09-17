fa513fceef8abf0a4272a8011fd821ee0732877b
arrival-spring <lnnunns+git@gmail.com>
2024-09-17 17:34:05 +0200

    Add Amai Promap storefinder and Tekkie Town spider ( 447 locations)

--- locations/spiders/dunns.py
+++ locations/spiders/dunns.py
@@ -1,7 +1,11 @@
-from locations.spiders.legit_za import LegitZASpider
+from locations.storefinders.amai_promap import AmaiPromapSpider
 
 
-class DunnsSpider(LegitZASpider):
+class DunnsSpider(AmaiPromapSpider):
     name = "dunns"
     start_urls = ["https://www.dunns.co.za/pages/store-locator-dunns"]
     item_attributes = {"brand": "Dunns", "brand_wikidata": "Q116619823"}
+
+    def post_process_item(self, item, response, feature):
+        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
+        yield item
--- locations/spiders/legit_za.py
+++ locations/spiders/legit_za.py
@@ -1,29 +1,10 @@
-from chompjs import parse_js_object
-from scrapy.http import JsonRequest, Request
-
-from locations.json_blob_spider import JSONBlobSpider
+from locations.storefinders.amai_promap import AmaiPromapSpider
 
 
-class LegitZASpider(JSONBlobSpider):
+class LegitZASpider(AmaiPromapSpider):
     name = "legit_za"
     start_urls = ["https://www.legit.co.za/pages/store-locator"]
     item_attributes = {"brand": "LEGiT", "brand_wikidata": "Q122959274"}
-
-    def start_requests(self):
-        for url in self.start_urls:
-            yield Request(url=url, callback=self.fetch_js)
-
-    def fetch_js(self, response):
-        urls = parse_js_object(
-            response.xpath('.//script[contains(text(), "var urls =")]/text()').get().split("var urls =")[1]
-        )
-        js_url = [u for u in urls if "amaicdn.com/storelocator-prod/wtb/" in u]  # should be only one
-        for url in js_url:
-            yield JsonRequest(url=url, callback=self.parse)
-
-    def extract_json(self, response):
-        chunks = response.text.split("SCASLWtb=")
-        return parse_js_object(chunks[1])["locations"]
 
     def post_process_item(self, item, response, feature):
         item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
