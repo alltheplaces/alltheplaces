2b1d43e5d03a38441838e35007f1d929ab2e22e4
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-17 23:36:05 +0000

    Add hirebase_gb

---
+++ locations/spiders/hirebase_gb.py
@@ -0,0 +1,16 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class HirebaseGbSpider(SitemapSpider, StructuredDataSpider):
+    name = "hirebase_gb"
+    item_attributes = {
+        "brand": "Hirebase",
+        "brand_wikidata": "Q100297859",
+        # "extras": Categories.SHOP_XYZ.value
+    }
+    sitemap_urls = ["https://www.hirebase.uk/robots.txt"]
+    sitemap_rules = [
+        (r"https://www.hirebase.uk/storefinder/store/[\w-]+-\d+", "parse"),
+    ]
