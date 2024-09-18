934510aa63daf1af172dfafbc2511002ad7fd8af
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-18 05:58:03 +0200

    Align name with AU

--- locations/spiders/afgri-equipment_bw_na_za_zw.py
+++ locations/spiders/afgri-equipment_bw_na_za_zw.py
@@ -1,8 +1,8 @@
 from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
 
 
-class AfgriBWNAZAZWSpider(AgileStoreLocatorSpider):
-    name = "afgri_bw_na_za_zw"
+class AfgriEquipmentBWNAZAZWSpider(AgileStoreLocatorSpider):
+    name = "afgri_equipment_bw_na_za_zw"
     item_attributes = {"brand": "AFGRI Equipment", "brand_wikidata": "Q119264464"}
     allowed_domains = ["afgriequipment.co.za"]
 
