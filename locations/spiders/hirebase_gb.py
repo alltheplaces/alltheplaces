b094df97cc2777899d0933cc058ca1dfd3ba6d3c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-18 12:33:10 +0000

    Add coords

--- locations/spiders/hirebase_gb.py
+++ locations/spiders/hirebase_gb.py
@@ -9,7 +9,17 @@ class HirebaseGBSpider(SitemapSpider, StructuredDataSpider):
         "brand": "Hirebase",
         "brand_wikidata": "Q100297859",
     }
+
+    # "DefaultLat":"54.072815","DefaultLng":"-4.421120"
     sitemap_urls = ["https://www.hirebase.uk/robots.txt"]
     sitemap_rules = [
         (r"https://www.hirebase.uk/storefinder/store/[\w-]+-\d+", "parse"),
     ]
+    def post_process_item(self, item, response, ld_data, **kwargs): 
+        """Override with any post-processing on the item."""
+        js_blob = response.xpath("//script[contains(text(),'DefaultLat')]/text()").get()
+        if js_blob:
+            item['lat'] = js_blob.split('DefaultLat":"')[1].split('","')[0]
+            item['lon'] = js_blob.split('DefaultLng":"')[1].split('","')[0]
+
+        yield item
