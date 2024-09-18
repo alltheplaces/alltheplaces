f4a7036fa6efdc374e1889be563804b31511fb42
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-18 12:21:08 +0000

    Add coords

--- locations/spiders/hss.py
+++ locations/spiders/hss.py
@@ -13,3 +13,12 @@ class HssSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [
         (r"https://www.hss.com/hire/find-a-branch/[\w-]+/[\w-]+", "parse"),
     ]
+
+    def post_process_item(self, item, response, ld_data, **kwargs): 
+        """Override with any post-processing on the item."""
+        js_blob = response.xpath("//div[@class='google_map']/script/text()").get()
+        if js_blob:
+            item['lat'] = js_blob.split("latitude = ")[1].split(";")[0]
+            item['lon'] = js_blob.split("longitude = ")[1].split(";")[0]
+
+        yield item
