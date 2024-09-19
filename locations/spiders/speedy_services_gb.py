c0778a436e791772ffacfcdab455ab73a7095a03
arrival-spring <lnnunns+git@gmail.com>
2024-09-19 16:24:10 +0200

    [speedy_services_gb] Refine spider

--- locations/spiders/speedy_services_gb.py
+++ locations/spiders/speedy_services_gb.py
@@ -1,6 +1,7 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.linked_data_parser import LinkedDataParser
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -17,3 +18,20 @@ class SpeedyServicesGBSpider(CrawlSpider, StructuredDataSpider):
             callback="parse",
         ),
     ]
+    search_for_twitter = False
+    search_for_facebook = False
+
+    def pre_process_data(self, ld_data, **kwargs):
+        filtered_times = []
+        for spec in LinkedDataParser.get_case_insensitive(ld_data, "OpeningHoursSpecification"):
+            if spec.get("Opens") == "00:00" and spec.get("Closes") == "00:00":
+                spec["Opens"] = "closed"
+                spec["Closes"] = "closed"
+            elif spec.get("opens") == "00:00" and spec.get("closes") == "00:00":
+                spec["opens"] = "closed"
+                spec["closes"] = "closed"
+            filtered_times.append(spec)
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["branch"] = item.pop("name")
+        yield item
