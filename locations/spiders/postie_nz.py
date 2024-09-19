a59f939ba909cb85ca37e732fd4baaee22c60ab6
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-19 09:00:37 +0200

    [postie_nz] Add spider

---
+++ locations/spiders/postie_nz.py
@@ -0,0 +1,23 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class PostieNZSpider(CrawlSpider, StructuredDataSpider):
+    name = "postie_nz"
+    item_attributes = {
+        "brand": "Postie",
+        "brand_wikidata": "Q110299434",
+    }
+    start_urls = ["https://www.postie.co.nz/stores/all"]
+    rules = [
+        Rule(
+            LinkExtractor(allow=r"https://www.postie.co.nz/store-details/southern/[\w-]+"),
+            callback="parse",
+        ),
+        Rule(
+              LinkExtractor(allow=r"https://www.postie.co.nz/store-details/northern/[\w-]+"),
+              callback="parse",
+          ),
+    ]
