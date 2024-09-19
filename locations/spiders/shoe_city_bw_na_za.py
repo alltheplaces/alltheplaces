55c080bc47d4a399b7f28d8b87ef73902affbe70
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-19 08:42:24 +0200

    [shoe_city_bw_na_za] Add spider

---
+++ locations/spiders/shoe_city_bw_na_za.py
@@ -0,0 +1,17 @@
+from locations.storefinders.amai_promap import AmaiPromapSpider
+from locations.hours import OpeningHours
+
+
+class ShoeCityBWNAZASpider(AmaiPromapSpider):
+    name = "shoe_city_bw_na_za"
+    start_urls = ["https://www.shoecity.co.za/pages/sca-store-locator"]
+    item_attributes = {"brand": "Shoe City", "brand_wikidata": "Q116620945"}
+
+    def post_process_item(self, item, response, feature):
+        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
+        item.pop("email")
+        if description := feature.get("description"):
+            item["opening_hours"] = OpeningHours()
+            for day in schedule.split("<br>"):
+                item["opening_hours"].add_ranges_from_string(day)
+        yield item
