3ec28a78954059ba729695d7a345a3089737814e
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-20 08:49:23 +0200

    Also LS and SZ and move name to branch

---
+++ locations/spiders/power_fashion_ls_sz_za.py
@@ -0,0 +1,10 @@
+from locations.storefinders.storepoint import StorepointSpider
+
+
+class PowerFashionLSSZZASpider(StorepointSpider):
+    name = "power_fashion_ls_sz_za"
+    key = "162e1380619248"
+    item_attributes = {"brand": "Power", "brand_wikidata": "Q118185713"}
+
+    def parse_item(self, item, location):
+        item["branch"] = item.pop("name")
--- locations/spiders/power_fashion_za.py
+++
@@ -1,7 +0,0 @@
-from locations.storefinders.storepoint import StorepointSpider
-
-
-class PowerFashionZASpider(StorepointSpider):
-    name = "power_fashion_za"
-    key = "162e1380619248"
-    item_attributes = {"brand": "Power", "brand_wikidata": "Q118185713"}
