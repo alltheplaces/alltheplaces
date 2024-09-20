910fdac26a032f17e42379420a4f16fd4f15518f
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 02:02:30 +0930

    [Micro Center] Modernise spider (#10383)
    * Refactor
    
    * Refactor
    
    * Update micro_center.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Convert to CrawlSpider, drop duplicates and clean up oh parsing
    
    * Rename spider
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Cj Malone <CjMalone@mail.com>

---
+++ locations/spiders/micro_center_us.py
@@ -0,0 +1,25 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class MicroCenterUSSpider(CrawlSpider, StructuredDataSpider):
+    name = "micro_center_us"
+    item_attributes = {"brand": "Micro Center", "brand_wikidata": "Q6839153"}
+    allowed_domains = ["www.microcenter.com"]
+    start_urls = ["https://www.microcenter.com/site/stores/default.aspx"]
+    rules = [Rule(LinkExtractor("/site/stores/"), "parse")]
+    wanted_types = ["ComputerStore"]
+    time_format = "%H"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if item["ref"] == "https://www.microcenter.com/site/stores/default.aspx":
+            return None
+        if (item.get("name") or "").startswith("Micro Center - "):
+            item["branch"] = item.pop("name").removeprefix("Micro Center - ")
+            apply_category(Categories.SHOP_COMPUTER, item)
+            yield item
+        else:
+            pass  # Duplicate Micro data on every page
