99aa45e95de428d0c69365ae78f7899db382a327
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-20 11:44:34 +0000

    Refactor pk_equipment to StructuredDataSpider

--- locations/spiders/pk_equipment.py
+++ locations/spiders/pk_equipment.py
@@ -3,10 +3,10 @@ import re
 
 import scrapy
 
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class PkEquipmentSpider(scrapy.Spider):
+class PkEquipmentSpider(StructuredDataSpider):
     name = "pk_equipment"
     item_attributes = {"brand": "P&K Equipment", "extras": {"shop": "tractor"}}
     allowed_domains = ["pkequipment.com"]
@@ -16,29 +16,15 @@ class PkEquipmentSpider(scrapy.Spider):
         urls = response.xpath('//div[@class="li-text"]/h2/a/@href').extract()
 
         for url in urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_location)
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_sd)
 
-    def parse_location(self, response):
+    def post_process_item(self, item, response, ld_data):
         ref = re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1)
-        data = json.loads(
-            response.xpath(
-                '//script[@type="application/ld+json" and contains(text(), "streetAddress")]/text()'
-            ).extract_first()
-        )
         mapdata = response.xpath('//div[@class="clearfix map_equipment"]/script[2]').extract_first()
         lat = re.search(r'(?:lat":)(-?\d+\.\d+),.*(?:long":)(-?\d*.\d*)', mapdata).group(1)
         lon = re.search(r'(?:lat":)(-?\d+\.\d+),.*(?:long":)(-?\d*.\d*)', mapdata).group(2)
-        properties = {
-            "ref": ref,
-            "name": data["name"],
-            "street_address": data["address"]["streetAddress"],
-            "city": data["address"]["addressLocality"],
-            "state": data["address"]["addressRegion"],
-            "postcode": data["address"]["postalCode"],
-            "phone": data.get("telephone"),
-            "lat": float(lat),
-            "lon": float(lon),
-            "website": data.get("url"),
-        }
+        item["ref"] = ref
+        item["lat"] = float(lat)
+        item["lon"] = float(lon)
 
-        yield Feature(**properties)
+        yield item
