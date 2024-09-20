b37141740dbe38cf7dfd2bea29f06cbc29c10529
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-20 12:12:39 +0000

    Refactor

--- locations/spiders/micro_center.py
+++ locations/spiders/micro_center.py
@@ -4,46 +4,30 @@ import scrapy
 
 from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class MicroCenterSpider(scrapy.Spider):
+class MicroCenterSpider(StructuredDataSpider):
     name = "micro_center"
     item_attributes = {"brand": "Micro Center", "brand_wikidata": "Q6839153"}
     allowed_domains = ["www.microcenter.com"]
-    start_urls = ("https://www.microcenter.com/site/stores/default.aspx",)
+    start_urls = ["https://www.microcenter.com/site/stores/default.aspx"]
+    wanted_types = ["ComputerStore"]
 
     def parse(self, response):
         for url in response.xpath('//div[@class="location-container"]//a/@href').extract():
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_sd)
 
-    def parse_store(self, response):
-        for ldjson in response.xpath('//script[@type="application/ld+json"]/text()').extract():
-            data = json.loads(ldjson)
-            if data["@type"] == "ComputerStore":
-                break
-        else:
-            raise ValueError
+    def post_process_item(self, item, response, ld_data):
+        if "openingHoursSpecification" in ld_data:
+            opening_hours = OpeningHours()
+            for spec in ld_data["openingHoursSpecification"]:
+                day = spec["dayOfWeek"][:2]
+                open_time = spec["opens"] + ":00"
+                close_time = spec["closes"] + ":00"
+                opening_hours.add_range(day, open_time, close_time)
 
-        opening_hours = OpeningHours()
-        for spec in data["openingHoursSpecification"]:
-            day = spec["dayOfWeek"][:2]
-            open_time = spec["opens"] + ":00"
-            close_time = spec["closes"] + ":00"
-            opening_hours.add_range(day, open_time, close_time)
-
-        properties = {
-            "ref": response.url,
-            "name": data["name"],
-            "website": response.url,
-            "phone": data["telephone"],
-            "street_address": data["address"]["streetAddress"],
-            "city": data["address"]["addressLocality"],
-            "state": data["address"]["addressRegion"],
-            "postcode": data["address"]["postalCode"],
-            "country": data["address"]["addressCountry"],
-            "lat": data["geo"]["latitude"],
-            "lon": data["geo"]["longitude"],
-            "opening_hours": opening_hours.as_opening_hours(),
-        }
-
-        yield Feature(**properties)
+            item["opening_hours"] = opening_hours
+        item['ref'] = response.url
+        
+        yield item
