022bde744233873ad5ada041bc679ba2c63c792e
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 02:00:59 +0930

    Refactor eatn park to StructuredDataSpider (#10381)
    * Refactor to StructuredDataSpider
    
    * Refactor to StructuredDataSpider
    
    * Update locations/spiders/eatn_park.py
    
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    
    * Update locations/spiders/eatn_park.py
    
    * Update eatn_park.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update locations/spiders/eatn_park.py
    
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/eatn_park.py
+++ locations/spiders/eatn_park.py
@@ -1,40 +1,19 @@
-import json
 import re
 
 from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 HOURS_RE = re.compile(r"(?P<day>\w+) (?P<open_time>\S+) - (?P<close_time>\S+)")
 
 
-class EatnParkSpider(SitemapSpider):
+class EatnParkSpider(SitemapSpider, StructuredDataSpider):
     name = "eatn_park"
     item_attributes = {"brand": "Eat'n Park", "brand_wikidata": "Q5331211"}
     sitemap_urls = ["https://locations.eatnpark.com/robots.txt"]
-    sitemap_rules = [(r"/restaurants-", "parse")]
+    sitemap_rules = [("/restaurants-", "parse_sd")]
 
-    def parse(self, response):
-        ldjson = response.xpath('//script[@type="application/ld+json"]/text()')
-        [data] = json.loads(ldjson.get())
-
-        opening_hours = OpeningHours()
-        for m in HOURS_RE.finditer(data["openingHours"]):
-            g = m.groupdict()
-            opening_hours.add_range(g["day"], g["open_time"], g["close_time"])
-
-        properties = {
-            "ref": re.search(r"-(\d+)\.html", response.url).group(1),
-            "website": response.url,
-            "name": response.css("span.location-name::text").get(),
-            "lat": data["geo"]["latitude"],
-            "lon": data["geo"]["longitude"],
-            "street_address": data["address"]["streetAddress"],
-            "city": data["address"]["addressLocality"],
-            "state": data["address"]["addressRegion"],
-            "postcode": data["address"]["postalCode"],
-            "opening_hours": opening_hours.as_opening_hours(),
-            "phone": data["address"]["telephone"],
-        }
-        yield Feature(**properties)
+    def post_process_item(self, item, response, ld_data):
+        item["branch"] = response.css("span.location-name::text").get()
+        item["ref"] = re.search(r"-(\d+)\.html", response.url).group(1)
+        yield item
