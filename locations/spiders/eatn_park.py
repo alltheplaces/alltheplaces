009ece096c37e8d44dc9cb4f81d35ac69e68a670
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-20 11:32:20 +0000

    Refactor to StructuredDataSpider

--- locations/spiders/eatn_park.py
+++ locations/spiders/eatn_park.py
@@ -3,38 +3,25 @@ import re
 
 from scrapy.spiders import SitemapSpider
 
+from locations.structured_data_spider import StructuredDataSpider
 from locations.hours import OpeningHours
 from locations.items import Feature
 
 HOURS_RE = re.compile(r"(?P<day>\w+) (?P<open_time>\S+) - (?P<close_time>\S+)")
 
 
-class EatnParkSpider(SitemapSpider):
+class EatnParkSpider(SitemapSpider, StructuredDataSpider):
     name = "eatn_park"
     item_attributes = {"brand": "Eat'n Park", "brand_wikidata": "Q5331211"}
     sitemap_urls = ["https://locations.eatnpark.com/robots.txt"]
-    sitemap_rules = [(r"/restaurants-", "parse")]
+    sitemap_rules = [(r"/restaurants-", "parse_sd")]
 
-    def parse(self, response):
-        ldjson = response.xpath('//script[@type="application/ld+json"]/text()')
-        [data] = json.loads(ldjson.get())
-
+    def post_process_item(self, item, response, ld_data):
         opening_hours = OpeningHours()
-        for m in HOURS_RE.finditer(data["openingHours"]):
+        for m in HOURS_RE.finditer(ld_data["openingHours"]):
             g = m.groupdict()
             opening_hours.add_range(g["day"], g["open_time"], g["close_time"])
-
-        properties = {
-            "ref": re.search(r"-(\d+)\.html", response.url).group(1),
-            "website": response.url,
-            "name": response.css("span.location-name::text").get(),
-            "lat": data["geo"]["latitude"],
-            "lon": data["geo"]["longitude"],
-            "street_address": data["address"]["streetAddress"],
-            "city": data["address"]["addressLocality"],
-            "state": data["address"]["addressRegion"],
-            "postcode": data["address"]["postalCode"],
-            "opening_hours": opening_hours.as_opening_hours(),
-            "phone": data["address"]["telephone"],
-        }
-        yield Feature(**properties)
+        item["opening_hours"] = opening_hours
+        item["name"] = response.css("span.location-name::text").get(),
+        item["ref"] = re.search(r"-(\d+)\.html", response.url).group(1)
+        yield item
