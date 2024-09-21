fca7d207bb43f52cf190d27b4ea6f8aa09ed4026
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 03:05:52 +0000

    Refactor to StructureDataSpider, CrawlSpider

--- locations/spiders/kindercare.py
+++ locations/spiders/kindercare.py
@@ -22,3 +22,6 @@ class KindercareSpider(CrawlSpider, StructuredDataSpider):
             follow=True,
         )
     ]
+
+    # TODO: Technically, phone is available but not presented in the structured data - ie https://www.kindercare.com/our-centers/brentwood/ca/301830
+    # This could be extracted and added
