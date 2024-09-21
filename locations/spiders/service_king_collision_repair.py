0a6465657507cbeb3a1a07bf80215244fe0e6c95
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 04:37:44 +0000

    Add hours

--- locations/spiders/service_king_collision_repair.py
+++ locations/spiders/service_king_collision_repair.py
@@ -2,13 +2,13 @@ import re
 
 from scrapy.spiders import SitemapSpider
 
-from locations.items import Feature
 from locations.google_url import extract_google_position
+from locations.items import Feature
+from locations.hours import OpeningHours
 
 
-class ServiceKingCollisionRepairSpider(SitemapSpider):
-    name = "service_king_collision_repair"
-    # allowed_domains = ["serviceking.com"]
+class CrashChampionsUSSpider(SitemapSpider):
+    name = "crash_champions_us"
     sitemap_urls = [
         "https://www.crashchampions.com/sitemap.xml",
     ]
@@ -16,22 +16,19 @@ class ServiceKingCollisionRepairSpider(SitemapSpider):
     sitemap_rules = [(r"/locations/", "parse_store")]
 
     def parse_store(self, response):
-        info = response.xpath("//*[@class='about-center__address']/text()").extract_first()
-        # city, state, postal = info.split(",")
         properties = {
             "ref": re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1),
             "name": response.xpath("//*[@class='about-center__center']/text()").extract_first(),
             "addr_full": response.xpath("//*[@class='about-center__address']/text()").extract_first().strip(),
-            # "city": city.strip(),
-            # "state": state.strip(),
-            # "postcode": postal.strip(),
-            "country": "US",
-            "phone": response.xpath("//*[@class='about-center__phoneno']/a/text()")
-            .extract_first()
-            .strip(),
+            "phone": response.xpath("//*[@class='about-center__phoneno']/a/text()").extract_first().strip(),
             "website": response.url,
         }
 
         item = Feature(**properties)
+        item["opening_hours"] = OpeningHours()
+        for day_range in response.xpath("//*[@class='about-center__timings']/p/text()").getall():
+            item["opening_hours"].add_ranges_from_string(day_range)
+        
+
         extract_google_position(item, response)
         yield item
