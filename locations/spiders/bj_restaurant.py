51fcb2ee1e33fc560653f37351b0b3ad49d971b1
Ian Dees <ian.dees@gmail.com>
2024-09-21 09:06:39 -0700

    Try extracting the linked data from the page JSON

--- locations/spiders/bj_restaurant.py
+++ locations/spiders/bj_restaurant.py
@@ -15,33 +15,12 @@ class BjRestaurantSpider(SitemapSpider):
     sitemap_rules = [
         (
             r"https://www.bjsrestaurants.com/locations/\w\w/\w+",
-            "parse_store",
+            "parse",
         )
     ]
 
-    def parse_store(self, response):
-        # Store data is in two locations.
-        # 1. <script type="application/ld+json">
-        # 2. A data-location attribute
-        # Some data exists in both locations
-
-        schema_string = response.xpath('//*/script[@type="application/ld+json"]/text()').extract_first().strip(";\n")
-        schema_dict = json.loads(schema_string)
-        data_location_string = response.xpath("//@data-location").extract_first()
-        data_location_dict = json.loads(data_location_string)
-        return Feature(
-            ref=data_location_dict["id"],
-            lat=data_location_dict["latitude"],
-            lon=data_location_dict["longitude"],
-            name=schema_dict["name"],
-            addr_full=schema_dict["address"]["streetAddress"],
-            city=schema_dict["address"]["addressLocality"],
-            state=schema_dict["address"]["addressRegion"],
-            postcode=schema_dict["address"]["postalCode"],
-            phone=schema_dict["telephone"],
-            website=schema_dict["url"],
-            opening_hours=self.parse_store_hours(schema_dict),
-        )
-
-    def parse_store_hours(self, store_dict):
-        return ";".join(store_dict["openingHours"])
+    def parse(self, response):
+        f = json.loads(response.xpath('//script[@id="__NEXT_DATA__"]/text()').extract_first())
+        nested_json = json.loads(f['props']['pageProps']['model'][':items']['root'][':items']['responsivegrid'][':items']['restaurantdetails']['restaurant']['seoScript'])
+        item = LinkedDataParser.parse(nested_json)
+        yield item
