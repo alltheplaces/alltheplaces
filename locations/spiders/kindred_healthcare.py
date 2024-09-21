c61bf84aa04e866af26fb5d576a64cc26261f693
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 05:31:11 +0000

    Refactor to StructuredDataSpider

--- locations/spiders/kindred_healthcare.py
+++ locations/spiders/kindred_healthcare.py
@@ -1,51 +1,32 @@
 import json
 import re
 
-from scrapy.spiders import SitemapSpider 
+from scrapy.spiders import SitemapSpider
 
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class KindredHealthcareSpider(SitemapSpider):
+class KindredHealthcareSpider(SitemapSpider, StructuredDataSpider):
     name = "kindred_healthcare"
     item_attributes = {"brand": "Kindred Healthcare", "brand_wikidata": "Q921363"}
     allowed_domains = ["www.kindredhospitals.com"]
     sitemap_urls = [
         "https://www.kindredhospitals.com/sitemap/sitemap.xml",
     ]
-    sitemap_rules = [(r"https://www.kindredhospitals.com/locations/ltac/[\w-]+/contact-us", "parse_location")]
+    sitemap_rules = [(r"https://www.kindredhospitals.com/locations/ltac/[\w-]+/contact-us", "parse_sd")]
+    wanted_types = ["Hospital"]
 
-    def parse_location(self, response):
+    def post_process_item(self, item, response, ld_data):
         is_location_page = response.xpath(
             '//script[@type="application/ld+json" and contains(text(), "streetAddress")]/text()'
         )
 
-        if is_location_page:
-            data = json.loads(
-                response.xpath(
-                    '//script[@type="application/ld+json" and contains(text(), "streetAddress")]/text()'
-                ).extract_first()
-            )
+        facility_type = ld_data["@type"]
+        if facility_type == "Hospital":  # Keep refs consistent
+            ref = re.search(r".+/(.+?)/(.+?)/?(?:\.html|$)", response.url).group(1)
+        else:
+            ref = re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1)
 
-            facility_type = data["@type"]
-            if facility_type == "Hospital":  # Keep refs consistent
-                ref = re.search(r".+/(.+?)/(.+?)/?(?:\.html|$)", response.url).group(1)
-            else:
-                ref = re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1)
+        item["ref"] = ref
 
-            properties = {
-                "name": data["name"],
-                "ref": ref,
-                "street_address": data["address"]["streetAddress"],
-                "city": data["address"]["addressLocality"],
-                "state": data["address"]["addressRegion"],
-                "postcode": data["address"]["postalCode"],
-                "country": data["address"]["addressCountry"],
-                "phone": data.get("contactPoint", {}).get("telephone"),
-                "website": data.get("url") or response.url,
-                "lat": float(data["geo"]["latitude"]),
-                "lon": float(data["geo"]["longitude"]),
-                "extras": {"facility_type": facility_type},
-            }
-
-            yield Feature(**properties)
+        yield item
