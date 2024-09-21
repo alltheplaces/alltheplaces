c9ef6b5e9228b67ab2b57e396db689d141fe8085
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 04:34:23 +0000

    Fix spider

--- locations/spiders/service_king_collision_repair.py
+++ locations/spiders/service_king_collision_repair.py
@@ -1,41 +1,37 @@
 import re
 
-import scrapy
+from scrapy.spiders import SitemapSpider
 
 from locations.items import Feature
+from locations.google_url import extract_google_position
 
 
-class ServiceKingCollisionRepairSpider(scrapy.Spider):
+class ServiceKingCollisionRepairSpider(SitemapSpider):
     name = "service_king_collision_repair"
-    allowed_domains = ["serviceking.com"]
-    start_urls = [
-        "https://www.serviceking.com/sitemap.xml",
+    # allowed_domains = ["serviceking.com"]
+    sitemap_urls = [
+        "https://www.crashchampions.com/sitemap.xml",
     ]
 
-    def parse(self, response):
-        response.selector.remove_namespaces()
-        for url in response.xpath("//loc/text()").extract():
-            if "/locations/" in url and "request-an-appointment-faq" not in url:
-                yield scrapy.Request(url, callback=self.parse_store)
+    sitemap_rules = [(r"/locations/", "parse_store")]
 
     def parse_store(self, response):
-        info = response.xpath("//*[@class='address']/p/text()").extract_first()
-        city, state, postal = info.split(",")
+        info = response.xpath("//*[@class='about-center__address']/text()").extract_first()
+        # city, state, postal = info.split(",")
         properties = {
             "ref": re.search(r".+/(.+?)/?(?:\.html|$)", response.url).group(1),
-            "name": response.xpath("//*[@class='location-dtls__title']/span/text()").extract_first(),
-            "addr_full": response.xpath("//*[@class='address']/text()").extract_first().strip(),
-            "city": city.strip(),
-            "state": state.strip(),
-            "postcode": postal.strip(),
+            "name": response.xpath("//*[@class='about-center__center']/text()").extract_first(),
+            "addr_full": response.xpath("//*[@class='about-center__address']/text()").extract_first().strip(),
+            # "city": city.strip(),
+            # "state": state.strip(),
+            # "postcode": postal.strip(),
             "country": "US",
-            "lat": response.xpath("//@data-latitude").extract_first(),
-            "lon": response.xpath("//@data-longitude").extract_first(),
-            "phone": response.xpath("//*[@class='location-dtls__phone']/text()")
+            "phone": response.xpath("//*[@class='about-center__phoneno']/a/text()")
             .extract_first()
-            .replace("T: ", "")
             .strip(),
             "website": response.url,
         }
 
-        yield Feature(**properties)
+        item = Feature(**properties)
+        extract_google_position(item, response)
+        yield item
