0eb66048bb31fc262d196fbda8e24c5ab1293494
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 01:41:08 +0000

    Refactor to Sitemap, StructuredDataSpider

--- locations/spiders/jimmy_johns.py
+++ locations/spiders/jimmy_johns.py
@@ -1,35 +1,13 @@
 import json
 
-import scrapy
+from scrapy.spiders import SitemapSpider
 
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class JimmyJohnsSpider(scrapy.Spider):
+class JimmyJohnsSpider(SitemapSpider, StructuredDataSpider):
     name = "jimmy_johns"
     item_attributes = {"brand": "Jimmy John's", "brand_wikidata": "Q1689380"}
     allowed_domains = ["locations.jimmyjohns.com"]
-    start_urls = ("https://locations.jimmyjohns.com/sitemap.xml",)
-
-    def parse(self, response):
-        stores = response.xpath('//url/loc[contains(text(),"sandwiches")]/text()').extract()
-        for store in stores:
-            yield scrapy.Request(response.urljoin(store), callback=self.parse_store)
-
-    def parse_store(self, response):
-        data = json.loads(response.xpath('//script[@type="application/ld+json"]//text()').extract_first())
-
-        properties = {
-            "ref": data[0]["url"],
-            "street_address": data[0]["address"]["streetAddress"],
-            "city": data[0]["address"]["addressLocality"],
-            "state": data[0]["address"]["addressRegion"],
-            "postcode": data[0]["address"]["postalCode"],
-            "website": response.url,
-            "lat": data[0]["geo"]["latitude"],
-            "lon": data[0]["geo"]["longitude"],
-        }
-        if data[0]["address"]["telephone"]:
-            properties["phone"] = data[0]["address"]["telephone"]
-
-        yield Feature(**properties)
+    sitemap_urls = ["https://locations.jimmyjohns.com/sitemap.xml"]
+    sitemap_rules = [(r"sandwiches", "parse_sd")]
