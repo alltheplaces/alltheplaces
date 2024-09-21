b6695dec4d83bb60077e12ef8b93bd4be563e2f4
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 11:21:54 +0930

    Update and rename jimmy_johns.py to jimmy_johns_us.py

--- locations/spiders/jimmy_johns_us.py
+++ locations/spiders/jimmy_johns_us.py
@@ -3,8 +3,8 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class JimmyJohnsSpider(SitemapSpider, StructuredDataSpider):
-    name = "jimmy_johns"
+class JimmyJohnsUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "jimmy_johns_us"
     item_attributes = {"brand": "Jimmy John's", "brand_wikidata": "Q1689380"}
     allowed_domains = ["locations.jimmyjohns.com"]
     sitemap_urls = ["https://locations.jimmyjohns.com/sitemap.xml"]
