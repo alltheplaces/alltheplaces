2095d6fb6c1359cb2b84ba4a1af8391a0854db99
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-22 14:02:01 +0000

    Add domain

--- locations/spiders/ufs_au.py
+++ locations/spiders/ufs_au.py
@@ -5,3 +5,4 @@ class UfsAUSpider(StoreifySpider):
     name = "ufs_au"
     item_attributes = {"brand": "UFS", "brand_wikidata": "Q63367573"}
     api_key = "ufs-pharmacies.myshopify.com"
+    domain = "https://ufs.com.au/"
