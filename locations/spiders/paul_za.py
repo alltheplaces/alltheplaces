0497e48f33f94c4166736ea753e54e55460a13e7
arrival-spring <lnnunns+git@gmail.com>
2024-09-23 22:03:33 +0200

    Add Paul spiders

---
+++ locations/spiders/paul_za.py
@@ -0,0 +1,8 @@
+from locations.spiders.paul_fr import PAUL_SHARED_ATTRIBUTES
+from locations.storefinders.yext_search import YextSearchSpider
+
+
+class PaulZASpider(YextSearchSpider):
+    name = "paul_za"
+    item_attributes = PAUL_SHARED_ATTRIBUTES
+    host = "https://location.paulsa.co.za"
