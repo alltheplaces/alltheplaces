73fe58bec0cb881a40a98f5c5c3f4167c9c38673
David Hicks <david@hicks.id.au>
2024-09-23 23:17:52 +1000

    closeby: tidy up storefinder and associated spiders

--- locations/spiders/patisserie_valerie_gb.py
+++ locations/spiders/patisserie_valerie_gb.py
@@ -1,3 +1,10 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories
+from locations.items import Feature
+from locations.spiders.sainsburys import SainsburysSpider
 from locations.storefinders.closeby import ClosebySpider
 
 
@@ -6,5 +13,16 @@ class PatisserieValerieGBSpider(ClosebySpider):
     item_attributes = {
         "brand_wikidata": "Q22101966",
         "brand": "Patisserie Valerie",
+        "extras": Categories.CAFE.value
     }
     api_key = "f312dbbc1e1036a6e7b395fbd18aaf1f"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if branch_name := item.pop("name", None):
+            if branch_name.startswith("Patisserie Valerie, "):
+                item["branch"] = branch_name.removeprefix("Patisserie Valerie, ")
+            elif branch_name.startswith("Sainsbury's, "):
+                item["branch"] = branch_name.removeprefix("Sainsbury's, ")
+                item["located_in"] = SainsburysSpider.SAINSBURYS["brand"]
+                item["located_in_wikidata"] = SainsburysSpider.SAINSBURYS["brand_wikidata"]
+        yield item
