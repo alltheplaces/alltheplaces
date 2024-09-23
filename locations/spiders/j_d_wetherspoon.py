b0296cb90de5f9c3af19c01eec1e41982a3e071b
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-09-23 15:33:01 +0100

    [Wetherspoon] Fix spider (Fixes #10477) (#10527)

--- locations/spiders/j_d_wetherspoon.py
+++ locations/spiders/j_d_wetherspoon.py
@@ -1,17 +1,25 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+import html
+from typing import Any
 
-from locations.structured_data_spider import StructuredDataSpider
+import chompjs
+from scrapy.http import Response
+from scrapy.spiders import Spider
+
+from locations.dict_parser import DictParser
 
 
-class JDWetherspoonSpider(CrawlSpider, StructuredDataSpider):
+class JDWetherspoonSpider(Spider):
     name = "j_d_wetherspoon"
     item_attributes = {"brand": "Wetherspoon", "brand_wikidata": "Q6109362"}
-    allowed_domains = ["www.jdwetherspoon.com"]
-    start_urls = ["https://www.jdwetherspoon.com/pubs/all-pubs"]
-    rules = [Rule(LinkExtractor(allow="/pubs/all-pubs/"), callback="parse_sd")]
+    start_urls = ["https://www.jdwetherspoon.com/pub-search/"]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if item.get("postcode") and item.get("postcode") in item.get("street_address"):
-            item["addr_full"] = item.pop("street_address")
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in chompjs.parse_js_object(
+            response.xpath('//script[@id="filter-google-maps-js-after"]/text()').get()
+        ):
+            item = DictParser.parse(location)
+            item["name"] = html.unescape(item["name"])
+            item["image"] = location["featured_image"]
+            item["ref"] = item["website"]
+
+            yield item
