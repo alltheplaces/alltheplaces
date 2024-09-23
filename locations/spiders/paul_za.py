a89ca0cc6bbb1ed40e26f9c5aa41f0c68c64d0c7
arrival-spring <lnnunns+git@gmail.com>
2024-09-23 22:24:34 +0200

    [paul_za] Fix website

--- locations/spiders/paul_za.py
+++ locations/spiders/paul_za.py
@@ -6,3 +6,7 @@ class PaulZASpider(YextSearchSpider):
     name = "paul_za"
     item_attributes = PAUL_SHARED_ATTRIBUTES
     host = "https://location.paulsa.co.za"
+
+    def parse_item(self, location, item):
+        item["website"] = location["profile"]["c_pagesURL"]
+        yield item
