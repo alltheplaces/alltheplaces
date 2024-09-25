6ec3bdfbe59c600224820bb71b8434e12c84638d
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-25 14:51:35 +0100

    Create beaverbrooks_gb.py

---
+++ locations/spiders/beaverbrooks_gb.py
@@ -0,0 +1,17 @@
+import scrapy
+from urllib.parse import urljoin
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class BeaverbrooksGBSpider(StructuredDataSpider):
+    name = "beaverbrooks_gb"
+    item_attributes = {"brand": "Beaverbrooks", "brand_wikidata": "Q4878226"}
+    start_urls = [
+        "https://www.beaverbrooks.co.uk/stores",
+    ]
+
+    def parse(self, response):
+        locations = response.xpath('//li[contains(@class, "stores-list__store  ")]/@data-store-link').getall()
+        for location in locations:
+            url = urljoin("https://www.beaverbrooks.co.uk", location)
+            yield scrapy.Request(url=url, callback=self.parse_sd)
