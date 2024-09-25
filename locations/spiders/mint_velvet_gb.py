1f7a2d93759c1c183ea0f819315fc2b313bcfaa3
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-25 12:30:15 +0100

    Update mint_velvet_gb.py
    Removed stockists which are not Mint Velvet stores.

--- locations/spiders/mint_velvet_gb.py
+++ locations/spiders/mint_velvet_gb.py
@@ -6,3 +6,7 @@ class MintVelvetGBSpider(StockistSpider):
     name = "mint_velvet_gb"
     item_attributes = {"brand": "Mint Velvet", "brand_wikidata": "Q104901572", "extras": Categories.SHOP_CLOTHES.value}
     key = "u10125"
+
+    def parse_item(self, item, location):
+        if "Boutique" in item["name"]:
+            yield item
