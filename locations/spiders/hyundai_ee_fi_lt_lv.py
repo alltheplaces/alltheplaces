3e9f4515e70cefefa5ac4bc7957692f6ef64cc81
David Hicks <david@hicks.id.au>
2024-09-27 00:51:40 +1000

    hyundai_ee_fi_lt_lv: rename ET to EE as correct Estonian country code

--- locations/spiders/hyundai_ee_fi_lt_lv.py
+++ locations/spiders/hyundai_ee_fi_lt_lv.py
@@ -8,8 +8,8 @@ from locations.json_blob_spider import JSONBlobSpider
 from locations.spiders.hyundai_kr import HYUNDAI_SHARED_ATTRIBUTES
 
 
-class HyundaiETFILTLVSpider(JSONBlobSpider):
-    name = "hyundai_et_fi_lt_lv"
+class HyundaiEEFILTLVSpider(JSONBlobSpider):
+    name = "hyundai_ee_fi_lt_lv"
     item_attributes = HYUNDAI_SHARED_ATTRIBUTES | {"extras": Categories.SHOP_CAR.value}
     allowed_domains = ["locator.maplet.com"]
     start_urls = ["https://locator.maplet.com/api/public/v3/hyundai/places?type=place&layer=default"]
@@ -24,4 +24,8 @@ class HyundaiETFILTLVSpider(JSONBlobSpider):
         item["postcode"] = feature["postcode"]
         item["state"] = feature["region"]["fi"] or None
         item["country"] = feature["country"]["fi"]
+        if item["country"] == "ET":
+            # ISO 639 language code for Estonian is "ET"
+            # ISO 3166-1 alpha-2 country code for Estonia is "EE"
+            item["country"] = "EE"
         yield item
