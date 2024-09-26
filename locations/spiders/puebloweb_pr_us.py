16c4f36b182d092d9c6da71ec3021b2c7128b8c8
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-26 12:20:50 +0000

    Add spider, fix https://github.com/alltheplaces/alltheplaces/issues/7974

---
+++ locations/spiders/puebloweb_pr_us.py
@@ -0,0 +1,8 @@
+from locations.storefinders.elfsight import ElfsightSpider
+
+
+class PueblowebPRUSSpider(ElfsightSpider):
+    name = "puebloweb_pr_us"
+    item_attributes = {"brand": "Pueblo Supermarkets", "brand_wikidata": "Pueblo Supermarkets"}
+    host = "core.service.elfsight.com"
+    api_key = "cf19e95f-dbcc-4213-a471-329628e371f9"
