f1c6d6dd1b31c5b8b4eda61a1cde9f9ca3dbb529
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-26 12:34:55 +0000

    Add categories

--- locations/spiders/puebloweb_pr_us.py
+++ locations/spiders/puebloweb_pr_us.py
@@ -1,8 +1,9 @@
 from locations.storefinders.elfsight import ElfsightSpider
+from locations.categories import Categories
 
 
 class PueblowebPRUSSpider(ElfsightSpider):
     name = "puebloweb_pr_us"
-    item_attributes = {"brand": "Pueblo Supermarkets", "brand_wikidata": "Pueblo Supermarkets"}
+    item_attributes = {"brand": "Pueblo Supermarkets", "brand_wikidata": "Pueblo Supermarkets", "extras": Categories.SHOP_SUPERMARKET.value}
     host = "core.service.elfsight.com"
     api_key = "cf19e95f-dbcc-4213-a471-329628e371f9"
