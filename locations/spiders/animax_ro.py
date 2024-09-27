0c3ab29868c70efc7af51ec41a181ed6d8795eb9
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-27 23:09:47 +0930

    Add Elfsight storefinder (#10609)
    * Reactor to extract elfsight
    
    * Reactor to extract elfsight
    
    * Reactor to extract elfsight
    
    * Refactor
    
    * Add Evergreen - fix #10283
    
    * Add animax, fix https://github.com/alltheplaces/alltheplaces/issues/7664
    
    * Cleanup
    
    * Add spider, fix https://github.com/alltheplaces/alltheplaces/issues/7974
    
    * Fix no_refs
    
    * Fix no_refs
    
    * Add categories
    
    * Add categories
    
    * Add placeholders for auto detection
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Update locations/spiders/bootlegger_coffee_company.py
    
    * Update locations/storefinders/elfsight.py
    
    * Update locations/spiders/bootlegger_coffee_company.py
    
    * Update locations/spiders/bootlegger_coffee_company.py

---
+++ locations/spiders/animax_ro.py
@@ -0,0 +1,18 @@
+from locations.storefinders.elfsight import ElfsightSpider
+
+
+class AnimaxROSpider(ElfsightSpider):
+    name = "animax_ro"
+    item_attributes = {"brand": "Animax", "brand_wikidata": "Q119440709"}
+    host = "shy.elfsight.com"
+    shop = "animaxro.myshopify.com"
+    api_key = "73d5edfc-9672-4567-b102-02ef8265ef7e"
+    no_refs = True
+
+    def post_process_item(self, item, response, location):
+        item["street_address"] = location.pop("position")
+        # TODO: Parse hours from:
+        #  'infoDescription': 'Drumul Ciorogârla 295-291&nbsp;Jud. Ilfov\nPetshop &amp; punct farmaceutic<div><br></div><div>☎&nbsp;0725.888.814</div><div>✉ chiajna@animax.ro </div><div><br></div><div>Luni 09:00 AM - 09:00 PM</div><div>Marti 09:00 AM -
+        #  09:00 PM</div><div>Miercuri 09:00 AM - 09:00 PM</div><div>Joi 09:00 AM - 09:00 PM</div>
+        #  <div>Vineri 09:00 AM - 09:00 PM</div><div>Sambata 09:00 AM - 09:00 PM</div><div>Duminica 09:00 AM - 07:00 PM</div>',
+        yield item
