e4d3a54f24299c8004823309ccb2529072982256
Ian Dees <ian.dees@gmail.com>
2024-09-27 08:45:56 -0500

    Rename some spiders to allow CO country code.

--- locations/spiders/aloha_poke_company_us.py
+++ locations/spiders/aloha_poke_company_us.py
@@ -3,8 +3,8 @@ from locations.hours import DAYS_EN
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class AlohaPokeCoUSSpider(WPStoreLocatorSpider):
-    name = "aloha_poke_co_us"
+class AlohaPokeCompanyUSSpider(WPStoreLocatorSpider):
+    name = "aloha_poke_company_us"
     item_attributes = {"brand_wikidata": "Q111231031", "brand": "Aloha Pokē Co", "extras": Categories.FAST_FOOD.value}
     allowed_domains = [
         "www.alohapokeco.com",
