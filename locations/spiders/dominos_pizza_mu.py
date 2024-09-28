67afb77440c0472567901f8ec7315d5f40fa6598
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-09-28 13:10:02 +0200

    Inherit from DominosPizzaSAQASpider

--- locations/spiders/dominos_pizza_mu.py
+++ locations/spiders/dominos_pizza_mu.py
@@ -1,7 +1,14 @@
-from locations.storefinders.go_review import GoReviewSpider
+from locations.spiders.dominos_pizza_sa_qa import DominosPizzaSAQASpider
 
 
 class DominosPizzaMUSpider(GoReviewSpider):
     name = "dominos_pizza_mu"
-    item_attributes = {"brand": "Domino's", "brand_wikidata": "Q839466"}
-    start_urls = ["https://dominosmau.goreview.co.za/store-locator"]
+    
+    def start_requests(self):
+        for country_code, name, lat, lon in [
+            ("MU", "MAURITIUS", "-20.3484", "57.5522"),
+        ]:
+            url = f"https://order.golo03.dominos.com/store-locator-international/locate/store?regionCode={country_code}&latitude={lat}&longitude={lon}"
+            headers = {"DPZ-Language": "en", "DPZ-Market": name}
+
+            yield JsonRequest(url=url, headers=headers, callback=self.parse, cb_kwargs={"country_code": country_code})
