92eb4dc96b9f1078047cd0a2b00198501ee8271a
arrival-spring <lnnunns+git@gmail.com>
2024-09-28 11:17:19 +0200

    Fix hours and uncomment next page fetching

--- locations/spiders/burger_king_sg.py
+++ locations/spiders/burger_king_sg.py
@@ -29,8 +29,8 @@ class BurgerKingSGSpider(Spider):
             item["phone"] = location.xpath('//span[@class="phone"]/text()').get().replace("Phone:", "")
             item["website"] = f"{self.website_root}/Locator/Details/" + item["ref"]
             yield Request(url=item["website"], callback=self.parse_location, meta={"item": item})
-        # if response.xpath('//a[contains(@class, "bk-btn-next")]').get() is not None:
-        #     yield from self.request_page(response.meta["page"] + 1)
+        if response.xpath('//a[contains(@class, "bk-btn-next")]').get() is not None:
+            yield from self.request_page(response.meta["page"] + 1)
 
     def parse_location(self, response):
         item = response.meta["item"]
@@ -38,9 +38,7 @@ class BurgerKingSGSpider(Spider):
 
         if (f_hours := response.xpath('string(//div[@class="fh-secondary-list"])').get()) != "":
             item["opening_hours"] = OpeningHours()
-            item["opening_hours"].add_ranges_from_string(
-                response.xpath('string(//div[@class="fh-secondary-list"])').get()
-            )
+            item["opening_hours"].add_ranges_from_string(f_hours)
         elif hours := response.xpath('//*[contains(text(), "Operation Hours")]/text()').get():
             item["opening_hours"] = OpeningHours()
             item["opening_hours"].add_ranges_from_string("Mo-Su " + hours.replace("Operation Hours", ""))
