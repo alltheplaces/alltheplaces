fba184d2d6802151e745a903445be639bc34e522
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-29 21:19:35 +0100

    Update the_fragrance_shop_gb.py
    Adding opening hours.

--- locations/spiders/the_fragrance_shop_gb.py
+++ locations/spiders/the_fragrance_shop_gb.py
@@ -5,7 +5,17 @@ from scrapy.spiders import Spider
 
 from locations.dict_parser import DictParser
 from locations.pipelines.address_clean_up import merge_address_lines
+from locations.hours import OpeningHours
 
+DAYS_MAPPING = {
+    1: "Mo",
+    2: "Tu",
+    3: "We",
+    4: "Th",
+    5: "Fr",
+    6: "Sa",
+    7: "Su",
+}
 
 class TheFragranceShopGBSpider(Spider):
     name = "the_fragrance_shop_gb"
@@ -18,6 +28,20 @@ class TheFragranceShopGBSpider(Spider):
             item["branch"] = item.pop("name")
             item["street_address"] = merge_address_lines([location["address1"], location["address2"]])
 
-            # TODO openingHours
-
+            self.parse_hours(item, location)
             yield item
+
+    def parse_hours(self, item, location):
+        times = location.get("openingHours")
+        oh = OpeningHours()
+        hours=times.split(",")
+        i=1
+        for times in hours:
+            # Days off
+            if times == "CLOSED":
+                continue
+            day = DAYS_MAPPING.get(i)
+            open, close = times.split("-")
+            oh.add_range(day, open, close)
+            i=i+1
+        item["opening_hours"] = oh.as_opening_hours()
