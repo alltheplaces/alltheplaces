7e65044f257d8f7c54dd9812c8174c1f20b06c18
arrival-spring <lnnunns+git@gmail.com>
2024-09-30 17:53:54 +0200

    [burger_king_hr] Add spider

---
+++ locations/spiders/burger_king_hr.py
@@ -0,0 +1,30 @@
+from scrapy import Spider
+from locations.spiders.burger_king import BURGER_KING_SHARED_ATTRIBUTES
+from locations.items import Feature
+from locations.categories import Extras, apply_yes_no, PaymentMethods
+from locations.hours import OpeningHours, DAYS_HR
+
+class BurgerKingHRSpider(Spider):
+    name = "burger_king_hr"
+    item_attributes = BURGER_KING_SHARED_ATTRIBUTES
+    start_urls = ["https://burgerking.hr/restorani"]
+    no_refs = True
+    
+    def parse(self, response):
+        for location in response.xpath('//div[@class="marker"]'):
+            item = Feature()
+            item["lat"] = location.xpath('.//@data-latitude').get()
+            item["lon"] = location.xpath('.//@data-longitude').get()
+            item["branch"] = location.xpath('.//div[@class="name"]/text()').get()
+            item["street_address"] = location.xpath('.//div[@class="address"]/text()').get()
+            item["city"] = location.xpath('.//div[@class="city"]/text()').get()
+            properties = location.xpath('.//div[@class="properties"]/img/@title').getall()
+            apply_yes_no(Extras.WIFI, item, "Wifi" in properties)
+            apply_yes_no(PaymentMethods.CARDS, item, "Kartično plaćanje" in properties)
+            apply_yes_no(Extras.DELIVERY, item, "Dostava" in properties)
+
+            if hours := location.xpath('string(.//div[@class="working-hours"])').get():
+                item["opening_hours"] = OpeningHours()
+                item["opening_hours"].add_ranges_from_string(hours, days=DAYS_HR)
+        
+            yield item
