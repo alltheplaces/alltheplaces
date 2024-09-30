f00a81d5c615fe5f13489442f5c46f7e331186db
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-30 19:35:37 +0100

    Create tag_heuer.py

---
+++ locations/spiders/tag_heuer.py
@@ -0,0 +1,21 @@
+from locations.storefinders.algolia import AlgoliaSpider
+
+class TagHeuerSpider(AlgoliaSpider):
+    name = "tag_heuer"
+    item_attributes = {
+        "brand_wikidata": "Q645984",
+        "brand": "TAG Heuer",
+    }
+    api_key = "8cf40864df513111d39148923f754024"
+    app_id = "6OBGA4VJKI"
+    index_name = "stores"
+#{"params":"filters=products.mechanicalWatches:true AND country:GB&attributesToRetrieve=address,Latitude,zip,country,phone,type,id,image,address2,email,name,description,services,payments,city,openingHours,exceptionalHours,_geoloc,i18nAddress,image1,timezone,sfccUrl","aroundRadius":"15000","hitsPerPage":"1000","getRankingInfo":"1","attributesToRetrieve":"address,Latitude,zip,country,phone,type,id,image,address2,email,name,description,services,payments,city,openingHours,exceptionalHours,_geoloc,i18nAddress,image1,timezone,sfccUrl","attributesToHighlight":"name"}
+
+    def post_process_item(self, item, response, feature):
+        item["branch"] = item.pop("name")
+        item["lat"] = feature["_geoloc"]["lat"]
+        item["lon"] = feature["_geoloc"]["lng"]
+        item["image"] = feature["image"]
+        slug = feature["sfccUrl"]
+        item["website"] = f"https://www.tagheuer.com/{slug}"
+        yield item
