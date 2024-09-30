874754481da4ac955e919f777fa0e1c4c92648b0
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-09-30 16:50:12 +0100

    [VirginiaHealthInspectionBlueRidge] Fix pageing (#10721)
    * [VirginiaHealthInspectionBlueRidge] Fix pageing
    
    Response is capped at 500 inspections
    
    * Use more OSM styled ref since ref:myhealthdepartment.com:inspectionID isn't used yet
    
    https://wiki.openstreetmap.org/wiki/Key:ref#Extensions
    
    * Use osm formmated check_date
    
    * Count unmapped categories
    
    * Prep for parsing all areas

--- locations/spiders/virginia_health_inspection_blue_ridge.py
+++ locations/spiders/virginia_health_inspection_blue_ridge.py
@@ -1,5 +1,9 @@
+import json
+from datetime import datetime, timedelta
+from typing import Any
+
 from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy.http import JsonRequest, Response
 
 from locations.dict_parser import DictParser
 
@@ -8,20 +12,30 @@ class VirginiaHealthInspectionBlueRidgeSpider(Spider):
     name = "virginia_health_inspection_blue_ridge"
     item_attributes = {}
     start_urls = ["https://inspections.myhealthdepartment.com"]
+    custom_settings = {"DOWNLOAD_TIMEOUT": 30}
 
-    def start_requests(self):
-        for url in self.start_urls:
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for area in response.xpath('//a[@title="View Inspections"]/@href').getall():
+            if area != "/va-blue-ridge":
+                continue
             yield JsonRequest(
-                url=url,
+                url=response.url,
                 callback=self.parse_location_list,
                 data={
                     "data": {
-                        "path": "va-blue-ridge",
+                        "path": area.split("/")[-1],
                         "programName": "",
-                        # TODO: make the date dynamic
-                        "filters": {"date": "2024-01-01 to 2024-09-27", "purpose": "", "city": "", "zip": ""},
+                        "filters": {
+                            "date": "{} to {}".format(
+                                (datetime.now() - timedelta(weeks=4 * 6)).strftime("%Y-%m-%d"),
+                                datetime.now().strftime("%Y-%m-%d"),
+                            ),
+                            "purpose": "",
+                            "city": "",
+                            "zip": "",
+                        },
                         "start": 0,
-                        "count": 2000,
+                        "count": 500,
                         "searchQueryOverride": None,
                         "searchStr": "",
                         "lat": 0,
@@ -31,15 +45,17 @@ class VirginiaHealthInspectionBlueRidgeSpider(Spider):
                 },
             )
 
-    def parse_location_list(self, response):
+    def parse_location_list(self, response: Response, **kwargs: Any) -> Any:
         for inspection in response.json():
             item = DictParser.parse(inspection)
 
             item["ref"] = inspection["inspectionID"]
-            item["extras"]["ref:myhealthdepartment.com:inspectionID"] = inspection["inspectionID"]
+            item["extras"]["ref:US:myhealthdepartment"] = inspection["inspectionID"]
             item["name"] = inspection["establishmentName"]
 
-            item["extras"]["checked_date"] = inspection["inspectionDate"]
+            item["extras"]["check_date"] = datetime.strptime(inspection["inspectionDate"], "%m/%d/%Y").strftime(
+                "%Y-%m-%d"
+            )
 
             # categorize the inspection to OSM standards
             if inspection["inspectionType"] == "Fast Food":
@@ -51,6 +67,12 @@ class VirginiaHealthInspectionBlueRidgeSpider(Spider):
             elif inspection["inspectionType"] == "Educational Facility Food Service":
                 item["extras"]["amenity"] = "school"
             else:
+                self.crawler.stats.inc_value("{}/unmapped_category/{}".format(self.name, inspection["inspectionType"]))
                 continue
 
             yield item
+
+        if len(response.json()) == 500:
+            req = json.loads(response.request.body)
+            req["data"]["start"] += req["data"]["count"]
+            yield response.request.replace(data=req)
