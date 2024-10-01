6f97104311ec1ee825b581673e89ca18465cc72a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-01 20:49:00 +0930

    Update superdry.py

--- locations/spiders/superdry.py
+++ locations/spiders/superdry.py
@@ -9,10 +9,10 @@ class SuperdrySpider(scrapy.spiders.SitemapSpider):
     item_attributes = {"brand": "Superdry", "brand_wikidata": "Q1684445"}
     sitemap_urls = ["https://stores.superdry.com/sitemap.xml"]
     download_delay = 0.5
+    drop_attributes = {"image"}
 
     def parse(self, response):
         MicrodataParser.convert_to_json_ld(response.selector)
         item = LinkedDataParser.parse(response, "ClothingStore")
         if item:
             yield item
-    drop_attributes = {"image"}
