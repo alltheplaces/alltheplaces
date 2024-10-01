abfbed6775813a9c9a2ea746b50d4fd66f3d8251
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-01 21:03:57 +0930

    Update mr_gattis_pizza_us.py

--- locations/spiders/mr_gattis_pizza_us.py
+++ locations/spiders/mr_gattis_pizza_us.py
@@ -5,9 +5,9 @@ class MrGattisPizzaUSSpider(StoreLocatorWidgetsSpider):
     name = "mr_gattis_pizza_us"
     item_attributes = {"brand": "Mr Gatti's Pizza", "brand_wikidata": "Q5527509"}
     key = "583c916beabda4913ceabbdd59df7689"
+    drop_attributes = {"image"}
 
     def parse_item(self, item, location: {}, **kwargs):
         if "COMING SOON" in item["name"].upper():
             return
         yield item
-    drop_attributes = {"image"}
