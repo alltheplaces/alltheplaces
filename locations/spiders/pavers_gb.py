8e304fd164a71d888be57abaeea45f32b0c6edcd
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-10-01 20:27:59 +0100

    Update pavers_gb.py

--- locations/spiders/pavers_gb.py
+++ locations/spiders/pavers_gb.py
@@ -1,10 +1,11 @@
-
 import scrapy
 from scrapy import Spider
 
+from locations.hours import OpeningHours
 from locations.dict_parser import DictParser
 from locations.geo import city_locations
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class PaversGBSpider(Spider):
@@ -23,6 +24,15 @@ class PaversGBSpider(Spider):
 
     def parse(self, response):
         for location in response.json()["result"]["response"]["results"]:
-            item = Feature()
             item = DictParser.parse(location["data"])
+            if "line2" in location["data"]["address"]:
+                item["street_address"] = merge_address_lines([location["data"]["address"]["line1"], location["data"]["address"]["line2"]])
+
+            hours = OpeningHours()
+            for day, intervals in location["data"]["hours"].items():
+                if "isClosed" in intervals:
+                    continue
+                for interval in intervals["openIntervals"]:
+                    hours.add_range(day[:2].capitalize(), interval["start"], interval["end"])
+            item["opening_hours"] = hours.as_opening_hours()
             yield item
