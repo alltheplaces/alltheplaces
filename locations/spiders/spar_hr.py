1d5d11a26d952dc6de1d870b4df6279a2bf24e6a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-01 20:53:01 +0930

    Update spar_hr.py

--- locations/spiders/spar_hr.py
+++ locations/spiders/spar_hr.py
@@ -8,14 +8,12 @@ from locations.structured_data_spider import StructuredDataSpider
 class SparHRSpider(SitemapSpider, StructuredDataSpider):
     name = "spar_hr"
     item_attributes = {"brand": "Spar", "brand_wikidata": "Q610492"}
-
     sitemap_urls = ["https://www.spar.hr/index.sitemap.lokacije-sitemap.xml"]
     sitemap_rules = [("/lokacije/", "parse_sd")]
-
     is_playwright_spider = True
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
-
     time_format = "%H:%M:%S"
+    drop_attributes = {"image"}
 
     def post_process_item(self, item, response, ld_data):
         if "INTERSPAR" in ld_data["name"].upper():
@@ -23,4 +21,3 @@ class SparHRSpider(SitemapSpider, StructuredDataSpider):
             item["brand_wikidata"] = "Q15820339"
         apply_category(Categories.SHOP_SUPERMARKET, item)
         yield item
-    drop_attributes = {"image"}
