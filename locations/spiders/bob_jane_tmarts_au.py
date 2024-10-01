94e1e31aaf7c7d0dcdce8ea73cfaf66bc64509f1
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-01 21:36:32 +0930

    Update bob_jane_tmarts_au.py

--- locations/spiders/bob_jane_tmarts_au.py
+++ locations/spiders/bob_jane_tmarts_au.py
@@ -13,6 +13,7 @@ class BobJaneTmartsAUSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [
         (r"shop/.*$", "parse_sd"),
     ]
+    drop_attributes = {"image"}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["lat"], item["lon"] = self.parse_lat_lon(response)
@@ -27,4 +28,3 @@ class BobJaneTmartsAUSpider(SitemapSpider, StructuredDataSpider):
             return (float(match.group(1)), float(match.group(2)))
         else:
             return (None, None)
-    drop_attributes = {"image"}
