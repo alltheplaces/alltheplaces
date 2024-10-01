c181d985124e2c2f48dafafe83b0c9761ff6a176
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-01 22:02:19 +0930

    Update and rename homesense.py to homesense_us.py

--- locations/spiders/homesense_us.py
+++ locations/spiders/homesense_us.py
@@ -3,8 +3,8 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class HomesenseSpider(SitemapSpider, StructuredDataSpider):
-    name = "homesense"
+class HomesenseUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "homesense_us"
     item_attributes = {"brand": "HomeSense", "brand_wikidata": "Q16844433"}
     sitemap_urls = ["https://locations.us.homesense.com/sitemap.xml"]
     sitemap_rules = [(r"https:\/\/locations\.us\.homesense\.com\/\w{2}\/[-\w]+$", "parse_sd")]
