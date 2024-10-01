d86f1b907c1166066f26cf33f8e7bf497fea9769
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-10-01 20:04:35 +0100

    Create pavers_gb.py

---
+++ locations/spiders/pavers_gb.py
@@ -0,0 +1,29 @@
+import json
+import scrapy
+from scrapy import Spider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.geo import city_locations
+from locations.dict_parser import DictParser
+
+
+class PaversGBSpider(Spider):
+    name = "pavers_gb"
+    item_attributes = {
+        "brand": "Pavers",
+        "brand_wikidata": "Q7155843",
+        "country": "GB",
+    }
+    allowed_domains = ["pavers.co.uk"]
+
+    def start_requests(self):
+        url_template = "https://www.pavers.co.uk/api/storeLocation/search?query={city}&location={lat},{lon}&page=1"
+        for city in city_locations("GB",10000):
+            yield scrapy.Request(url_template.format(city=city["name"],lat=city["latitude"],lon=city["longitude"]))
+
+    def parse(self, response):
+        for location in response.json()["result"]["response"]["results"]:
+            item = Feature()
+            item = DictParser.parse(location["data"])
+            yield item
