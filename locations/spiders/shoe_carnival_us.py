e394972a3043ac754357e0ca4858a384a52348f7
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-01 20:54:21 +0930

    Update shoe_carnival_us.py

--- locations/spiders/shoe_carnival_us.py
+++ locations/spiders/shoe_carnival_us.py
@@ -9,9 +9,10 @@ class ShoeCarnivalUSSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://stores.shoecarnival.com/robots.txt"]
     sitemap_rules = [(r"com/\w\w/[^/]+/shoe-store-.+-(\d+)\.html$", "parse")]
     wanted_types = ["ShoeStore"]
+    drop_attributes = {"image"}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item.pop("name")
         item["branch"] = response.xpath('//span[@class="loc-name"]/a/text()').get()
         yield item
-    drop_attributes = {"image"}
+
