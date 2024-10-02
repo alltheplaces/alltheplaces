00d6e5d151378e7ea252f82d0817280d93622724
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-10-02 20:37:23 +0100

    Update booker_gb.py

--- locations/spiders/booker_gb.py
+++ locations/spiders/booker_gb.py
@@ -1,8 +1,20 @@
 from locations.storefinders.yext_answers import YextAnswersSpider
 
+brands = {
+       "Booker" : {"brand": "Booker", "brand_wikidata": "Q4943180"},
+       "Makro" : {"brand": "Makro", "brand_wikidata": "Q704606"}
+}
+
 
 class BookerGBSpider(YextAnswersSpider):
     name = "booker_gb"
     item_attributes = {"brand": "Booker", "brand_wikidata": "Q4943180"}
     api_key = "9ca92999bf86e368065b5018e3f82c74"
     experience_key = "yext-pages-locator-search"
+
+    def parse_item(self, location, item):
+        for brand_key in brands.keys():
+            if brand_key in item["name"]:
+                item.update(brands[brand_key])
+
+        yield item
