f9a4f036e065b0fca27198800717c6a0e48b778d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-04 08:46:06 +0930

    Update iren_it.py

--- locations/spiders/iren_it.py
+++ locations/spiders/iren_it.py
@@ -8,13 +8,12 @@ class IrenITSpider(JSONBlobSpider):
     name = "iren_it"
     item_attributes = {"brand": "Iren", "brand_wikidata": "Q3801865", "extras": {"office": "energy_supplier"}}
     start_urls = ["https://www.irenlucegas.it/assistenza/sportelli"]
-
+    no_refs = True
+    
     def extract_json(self, response):
         return chompjs.parse_js_object(response.xpath('//div[@id="store-pin"]/@data-store').get())
 
     def post_process_item(self, item, response, location):
-        item["ref"] = location["cap"]  # Or, is this postcode?
-
         # TODO: Category... door? Shopfront?
         #  'categoria': 'Sportello'
 
