8e4caab2e6ffc4aa342560e049e2ca87320cba48
arrival-spring <lnnunns+git@gmail.com>
2024-10-04 11:58:30 +0200

    [virgin_active_au] Add spider

--- locations/spiders/virgin_active_sg.py
+++ locations/spiders/virgin_active_sg.py
@@ -1,23 +1,29 @@
 from chompjs import parse_js_object
 from scrapy.http import JsonRequest
 
-from locations.hours import OpeningHours
+from locations.hours import DAYS_3_LETTERS, OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
 from locations.spiders.virgin_active_bw_na_za import VIRGIN_ACTIVE_SHARED_ATTRIBUTES
 
 
+# Also used by virgin_active_au
 class VirginActiveSGSpider(JSONBlobSpider):
     name = "virgin_active_sg"
     item_attributes = VIRGIN_ACTIVE_SHARED_ATTRIBUTES
     start_urls = ["https://www.virginactive.com.sg/locations"]
 
-    def extract_json(self, response):
+    def parse(self, response):
         json_data = parse_js_object(response.xpath('//script[@id="__NEXT_DATA__"]/text()').get())
-        return json_data["props"]["pageProps"]["clubGroups"][0]["clubs"]
+        club_groups = json_data["props"]["pageProps"]["clubGroups"]
+        for group in club_groups:
+            features = group["clubs"]
+            yield from self.parse_feature_array(response, features) or []
 
     def post_process_item(self, item, response, location):
         yield JsonRequest(
-            url="https://www.virginactive.com.sg/" + location["path"], meta={"item": item}, callback=self.parse_location
+            url=self.start_urls[0].replace("/locations", "") + location["path"],
+            meta={"item": item},
+            callback=self.parse_location,
         )
 
     def parse_location(self, response):
@@ -27,5 +33,8 @@ class VirginActiveSGSpider(JSONBlobSpider):
         item["email"] = location["email"]
         item["opening_hours"] = OpeningHours()
         for day in location["openingHours"]:
-            item["opening_hours"].add_ranges_from_string(day["label"] + " " + day["value"])
+            if day["value"].lower() == "closed" and day["label"] in DAYS_3_LETTERS:
+                item["opening_hours"].set_closed(day["label"])
+            else:
+                item["opening_hours"].add_ranges_from_string(day["label"] + " " + day["value"])
         yield item
