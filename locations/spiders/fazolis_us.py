c930160952c449612a241885803bc7dfe8ba11d9
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-05 13:59:37 +0000

    Refactor to WPStoreLocator

--- locations/spiders/fazolis_us.py
+++ locations/spiders/fazolis_us.py
@@ -1,17 +1,11 @@
 from locations.categories import Categories, Extras, apply_yes_no
-from locations.storefinders.yext_answers import YextAnswersSpider
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class FazolisUSSpider(YextAnswersSpider):
+class FazolisUSSpider(WPStoreLocatorSpider):
     name = "fazolis_us"
     item_attributes = {"brand": "Fazoli's", "brand_wikidata": "Q1399195", "extras": Categories.FAST_FOOD.value}
-    api_key = "e6cda871db03e0fc9eb2211470649126"
-    experience_key = "locator"
+    allowed_domains = [
+        "fazolis.com",
+    ]
 
-    def parse_item(self, location, item):
-        if amenities := location["data"].get("products"):
-            apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive-thru" in amenities, False)
-            apply_yes_no(Extras.TAKEAWAY, item, "Take-out" in amenities, False)
-            apply_yes_no(Extras.WIFI, item, "Free Wi-Fi" in amenities, False)
-            apply_yes_no(Extras.WHEELCHAIR, item, "Handicap Accessible" in amenities, False)
-        yield item
