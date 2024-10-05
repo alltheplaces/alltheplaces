28ea4a96363d535f269506945728a5ce14d69c12
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-05 10:57:43 +0000

    Category

--- locations/spiders/american_mattress_us.py
+++ locations/spiders/american_mattress_us.py
@@ -1,7 +1,8 @@
+from locations.categories import Categories
 from locations.storefinders.amai_promap import AmaiPromapSpider
 
 
 class AmericanMattressUSSpider(AmaiPromapSpider):
     name = "american_mattress_us"
     start_urls = ["https://www.americanmattress.com/pages/store-locator"]
-    item_attributes = {"brand": "American Mattress", "brand_wikidata": "Q126896153"}
+    item_attributes = {"brand": "American Mattress", "brand_wikidata": "Q126896153", "extras": Categories.SHOP_BED.value}
