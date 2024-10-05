927599a6ea24b4024ed335aed9f4a011dd1e3bca
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-05 20:08:21 +0930

    Delete locations/spiders/waterdrop_de.py

--- locations/spiders/waterdrop_de.py
+++
@@ -1,7 +0,0 @@
-from locations.storefinders.amai_promap import AmaiPromapSpider
-
-
-class WaterdropDESpider(AmaiPromapSpider):
-    name = "waterdrop_de"   
-    item_attributes = {"brand": "Waterdrop", "brand_wikidata": "Q104178991"}
-    start_urls = ["https://www.waterdrop.de/pages/stores"]
