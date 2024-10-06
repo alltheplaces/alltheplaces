32e047529c4421e23e3ab590487eec2d967e9432
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-07 10:26:12 +1030

    Update marugame_jp.py

--- locations/spiders/marugame_jp.py
+++ locations/spiders/marugame_jp.py
@@ -7,3 +7,4 @@ class MarugameJPSpider(YextSpider):
     api_version = "20220615"
     search_filter = "%7B%22$and%22%20:%20%5B%0A%20%20%20%20%20%20%20%20%20%20%7B%22c_%E5%BA%97%E8%88%97%E9%96%8B%E5%BA%97%E6%97%A5%22:%7B%22$ge%22:%222019-02-15%22%7D%7D,%0A%20%20%20%20%20%20%20%20%20%20%7B%22name%22%20:%7B%22$startsWith%22:%20%22%E4%B8%B8%E4%BA%80%22%20%7D%7D%0A%20%20%20%20%20%20%20%20%5D%7D"
     wanted_types = ["Restaurant"]
+    requires_proxy = True
