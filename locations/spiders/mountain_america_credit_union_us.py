779ef151406cc73586df4f7f494af423c50414e7
Spencer Alves <impiaaa@gmail.com>
2024-10-07 14:07:24 -0700

    Skip entry for internal service team; refactor to JSONBlobSpider

--- locations/spiders/mountain_america_credit_union_us.py
+++ locations/spiders/mountain_america_credit_union_us.py
@@ -1,15 +1,13 @@
 import json
 from collections import defaultdict
 
-from scrapy import Spider
-
 from locations.categories import Categories
 from locations.hours import OpeningHours
-from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class MountainAmericaCreditUnionUSSpider(Spider):
+class MountainAmericaCreditUnionUSSpider(JSONBlobSpider):
     name = "mountain_america_credit_union_us"
     item_attributes = {
         "brand": "Mountain America Credit Union",
@@ -17,28 +15,29 @@ class MountainAmericaCreditUnionUSSpider(Spider):
         "extras": Categories.BANK.value,
     }
     start_urls = ["https://www.macu.com/page-data/branch-locations/page-data.json"]
+    locations_key = ["result", "pageContext", "frontmatter", "tableEntries", "branches"]
 
-    def parse(self, response):
-        for location in response.json()["result"]["pageContext"]["frontmatter"]["tableEntries"]["branches"].values():
-            item = Feature()
-            item["ref"] = location["branch_id"]
-            item["state"], _ = location["name"].split(" - ")
-            item["branch"] = location["headline"]
-            item["street_address"] = location["address_line_1"]
-            item["addr_full"] = merge_address_lines([location["address_line_1"], location["address_line_2"]])
-            item["phone"] = location["phone"]
-            item["lat"] = location["latitude"]
-            item["lon"] = location["longitude"]
-            item["website"] = response.urljoin(location["details_url"])
+    def post_process_item(self, item, response, location):
+        if location["id"] == "member_service_team":
+            return
 
-            open_close_times = json.loads(location["open_close_times"])
-            opening_hours = defaultdict(OpeningHours)
-            for day, open_types in open_close_times.items():
-                for open_type, open_close in open_types.items():
-                    opening_hours[open_type].add_range(
-                        day, open_close["open"], open_close["closed"], time_format="%H:%M:%S"
-                    )
-            item["opening_hours"] = opening_hours["lobby"]
-            item["extras"]["opening_hours:drive_through"] = opening_hours["driveUp"].as_opening_hours()
+        del item["name"]
+        item["ref"] = location["branch_id"]
+        item["state"], _ = location["name"].split(" - ")
+        item["branch"] = location["headline"]
+        item["street_address"] = location["address_line_1"]
+        item["addr_full"] = merge_address_lines([location["address_line_1"], location["address_line_2"]])
+        item["phone"] = location["phone"]
+        item["website"] = response.urljoin(location["details_url"])
 
-            yield item
+        open_close_times = json.loads(location["open_close_times"])
+        opening_hours = defaultdict(OpeningHours)
+        for day, open_types in open_close_times.items():
+            for open_type, open_close in open_types.items():
+                opening_hours[open_type].add_range(
+                    day, open_close["open"], open_close["closed"], time_format="%H:%M:%S"
+                )
+        item["opening_hours"] = opening_hours["lobby"]
+        item["extras"]["opening_hours:drive_through"] = opening_hours["driveUp"].as_opening_hours()
+
+        yield item
