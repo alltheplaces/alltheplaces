8f1388fea1d1c9ae621770c1e25c7f28fdeeb366
jhutton1 <jaimehutton01@gmail.com>
2024-10-07 14:08:11 -0400

    fix moncler spider

--- locations/spiders/moncler.py
+++ locations/spiders/moncler.py
@@ -1,7 +1,9 @@
+from typing import Any
+from requests_cache import Response
 import scrapy
 
-from locations.items import Feature
-
+from locations.dict_parser import DictParser
+from locations.user_agents import BROWSER_DEFAULT
 
 class MonclerSpider(scrapy.Spider):
     name = "moncler"
@@ -9,20 +11,12 @@ class MonclerSpider(scrapy.Spider):
     allowed_domains = ["moncler.com"]
     start_urls = ["https://www.moncler.com/on/demandware.store/Sites-MonclerEU-Site/it_IT/StoresApi-FindAll"]
     requires_proxy = True
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+    user_agent = BROWSER_DEFAULT
 
-    def parse(self, response):
-        data = response.json()
-        for i in data["stores"]:
-            properties = {
-                "ref": i["ID"],
-                "name": i["name"],
-                "street_address": i["address1"],
-                "city": i["city"],
-                "postcode": i.get("postalCode"),
-                "country": i["countryCode"]["value"],
-                "phone": i["phone"],
-                "lat": i["latitude"],
-                "lon": i["longitude"],
-            }
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["stores"]:
+            item = DictParser.parse(location)
+            item["state"] = location.get("stateCode")
+            yield item
 
-            yield Feature(**properties)
