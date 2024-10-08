96ac56130286895ed8b7d15fb75c18d72fe6515c
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-10-08 09:35:14 +0200

    [vodacom_za] Add category

--- locations/spiders/vodacom_za.py
+++ locations/spiders/vodacom_za.py
@@ -1,7 +1,8 @@
 from locations.storefinders.location_bank import LocationBankSpider
+from locations.categories import Categories
 
 
 class VodacomZASpider(LocationBankSpider):
     name = "vodacom_za"
     client_id = "19ced8ad-ae12-440c-abb5-423a1d49a002"
-    item_attributes = {"brand": "Vodacom", "brand_wikidata": "Q1856518"}
+    item_attributes = {"brand": "Vodacom", "brand_wikidata": "Q1856518", "extras": Categories.SHOP_MOBILE_PHONE}
