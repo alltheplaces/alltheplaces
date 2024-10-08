4bf1c4529fbeb85f3023f60a69f03bce18353c7e
Cj Malone <CjMalone@mail.com>
2024-10-08 11:56:23 +0100

    [Mcdonalds Baltics] Fix spider

--- locations/spiders/mcdonalds_baltics.py
+++ locations/spiders/mcdonalds_baltics.py
@@ -1,22 +1,31 @@
-import scrapy
+from typing import Any
 
-from locations.open_graph_spider import OpenGraphSpider
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 from locations.spiders.mcdonalds import McdonaldsSpider
 
 
-class McdonaldsBalticsSpider(scrapy.spiders.SitemapSpider, OpenGraphSpider):
+class McdonaldsBalticsSpider(SitemapSpider):
     name = "mcdonalds_baltics"
-    custom_settings = {"ROBOTSTXT_OBEY": False}
     item_attributes = McdonaldsSpider.item_attributes
     sitemap_urls = [
         "https://mcdonalds.ee/location-sitemap.xml",
-        "https://mcd.lt/location-sitemap.xml",
+        "https://mcd.lt/wp-sitemap-posts-location-1.xml",
         "https://mcdonalds.lv/location-sitemap.xml",
     ]
-    wanted_types = ["article"]
-    requires_proxy = True
+    requires_proxy = "US"
 
-    def post_process_item(self, item, response, **kwargs):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["ref"] = item["website"] = response.url
+        item["branch"] = response.xpath("//h1/text()").get()
+        item["addr_full"] = merge_address_lines(
+            response.xpath('//p/a[contains(@href, "tel:")]/parent::p/text()').getall()
+        )
+        item["phone"] = response.xpath('//p/a[contains(@href, "tel:")]/text()').get()
         item["lat"] = response.xpath("//@data-lat").extract_first()
         item["lon"] = response.xpath("//@data-lng").extract_first()
         yield item
