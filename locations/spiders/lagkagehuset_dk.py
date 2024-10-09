8b20203ef0e8e9532cc26b07759e818addf05359
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-10 01:41:30 +0530

    Fix Lagkagehuset DK Spider

--- locations/spiders/lagkagehuset_dk.py
+++ locations/spiders/lagkagehuset_dk.py
@@ -1,23 +1,24 @@
-import scrapy
+import re
+from typing import Iterable
 
-from locations.google_url import url_to_coords
+import chompjs
+from scrapy.http import Response
+
 from locations.items import Feature
-from locations.pipelines.address_clean_up import merge_address_lines
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class LagkagehusetDKSpider(scrapy.Spider):
+class LagkagehusetDKSpider(JSONBlobSpider):
     name = "lagkagehuset_dk"
     item_attributes = {"brand": "Lagkagehuset", "brand_wikidata": "Q12323572"}
-    start_urls = ["https://lagkagehuset.dk/butikker"]
-    no_refs = True
+    start_urls = ["https://lagkagehuset.dk/select-shop"]
 
-    def parse(self, response, **kwargs):
-        for store in response.xpath("//*[@region]"):
-            item = Feature()
-            item["name"] = store.xpath('.//*[@class="store-headline"]/text()').get()
-            item["addr_full"] = merge_address_lines(store.xpath('.//*[@class="store-content"]/p[1]/text()').getall())
-            if map_url := store.xpath(".//a[contains(@href, 'google')]/@href").get():
-                item["lat"], item["lon"] = url_to_coords(map_url.replace("google.dk", "google.com"))
-            item["email"] = store.xpath('.//a[contains(@href, "mailto")]/text()').get()
-            item["phone"] = store.xpath('.//a[contains(@href, "tel")]/text()').get()
-            yield item
+    def extract_json(self, response: Response) -> list:
+        return chompjs.parse_js_object(
+            re.search(r"\\\"shops\\\":(\[.+?\])}", response.text).group(1), unicode_escape=True
+        )
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = item.pop("addr_full")
+        item["branch"] = item.pop("name")
+        yield item
