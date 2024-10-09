de6b3f5c1906575a10812e74d1e83b909893fc63
Cj Malone <CjMalone@mail.com>
2024-10-09 13:13:21 +0100

    Don't honour robots.txt

--- locations/spiders/ampm_us.py
+++ locations/spiders/ampm_us.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from scrapy import Spider
+from scrapy.http import Response
 
 from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
@@ -9,8 +12,9 @@ class AmpmUSSpider(Spider):
     item_attributes = {"brand": "ampm", "brand_wikidata": "Q306960"}
     start_urls = ["https://www.ampm.com/handlers/getlocation.ashx?loc=45,-104&sl=true&isll=true&ns=1000000"]
     no_refs = True
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json():
             item = DictParser.parse(location)
             item["street_address"] = item.pop("addr_full")
