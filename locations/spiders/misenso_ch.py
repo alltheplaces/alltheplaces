73446ca9df55f62b647cffc99163b43cfa9137a2
Cj Malone <CjMalone@mail.com>
2024-10-10 14:59:16 +0100

    Restore coords

--- locations/spiders/misenso_ch.py
+++ locations/spiders/misenso_ch.py
@@ -1,31 +1,36 @@
+import json
 from typing import Any
 
 from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+from scrapy.spiders import Spider
 
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS_DE, OpeningHours
+from locations.hours import OpeningHours
 from locations.items import Feature
 
 
-class MisensoCHSpider(SitemapSpider):
+class MisensoCHSpider(Spider):
     name = "misenso_ch"
-    item_attributes = {"brand": "Misenso", "brand_wikidata": "Q116151325"}
-    sitemap_urls = ["https://www.misenso.ch/de/wp-sitemap.xml"]
-    sitemap_rules = [(r"/filialen/[a-zA-Z0-9]+", "parse")]
+    item_attributes = {"name": "Misenso", "brand": "Misenso", "brand_wikidata": "Q116151325"}
+    start_urls = ["https://www.misenso.ch/de/filialen/"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        item = Feature()
-        item["name"] = response.xpath("//h1/text()").get()
-        item["addr_full"] = response.xpath("//p").xpath("normalize-space()").get()
-        item["ref"] = item["website"] = response.url
-        item["phone"] = response.xpath('//*[@class="rtc"]//*[contains(@href,"tel:")]/text()').get()
-        item["email"] = response.xpath('//*[contains(@href,"mailto:")]/text()').get()
-        apply_category(Categories.SHOP_OPTICIAN, item)
-        item["opening_hours"] = OpeningHours()
-        for day_time in response.xpath(
-            '//*[@class = "type--module name--text moix--1 moix-wowr--1 moix-mona--text--0 container number_of_cols--two"]//li'
-        ):
-            day_time = day_time.xpath("normalize-space()").get()
-            item["opening_hours"].add_ranges_from_string(day_time, days=DAYS_DE)
-        yield item
+        for location in json.loads(response.xpath("//@data-stores").get()):
+            data = json.loads(location["google_place_data"])
+            item = Feature()
+            item["ref"] = str(location["id"])
+            item["lat"] = data["location"]["lat"]
+            item["lon"] = data["location"]["lng"]
+            item["branch"] = location["title"].removeprefix("Misenso ")
+
+            item["website"] = item["extras"]["website:de"] = location["page_link_de"]
+            item["extras"]["website:fr"] = location["page_link_fr"]
+            item["extras"]["ref:google"] = location["place_id"]
+
+            item["opening_hours"] = OpeningHours()
+            for day, times in location["amparex"]["opening_hours"].items():
+                item["opening_hours"].add_range(day, times["from"], times["to"])
+
+            apply_category(Categories.SHOP_OPTICIAN, item)
+
+            yield item
