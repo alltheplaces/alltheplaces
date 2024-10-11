521e8a9448e5f3720d22dc26fdb3657d890e2e97
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-11 15:11:35 +0530

    Use NL version of API to get JSON data

--- locations/spiders/krefel_be.py
+++ locations/spiders/krefel_be.py
@@ -7,17 +7,19 @@ from locations.user_agents import BROWSER_DEFAULT
 class KrefelBESpider(Spider):
     name = "krefel_be"
     item_attributes = {"brand": "Krëfel", "brand_wikidata": "Q3200093"}
-    start_urls = ["https://api.krefel.be/api/v2/krefel/stores?pageSize=100&lang=fr"]
+    start_urls = ["https://api.krefel.be/api/v2/krefel/stores?pageSize=100&lang=nl"]
     custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
 
     def parse(self, response, **kwargs):
         for location in response.json()["stores"]:
             location["ref"] = location.pop("name")
-            location["website"] = f'https://www.krefel.be/fr/magasins/{location["ref"]}'
+            location["website"] = f'https://www.krefel.be/nl/winkels/{location["ref"]}'
             location["phone"] = ";".join(
                 filter(None, [location["address"].get("phone"), location["address"].get("phone2")])
             ).replace("/", "")
             location["address"]["house_number"] = location["address"].pop("line2", "")
             location["address"]["street"] = location["address"].pop("line1")
 
-            yield DictParser.parse(location)
+            item = DictParser.parse(location)
+            item["extras"]["website:fr"] = f'https://www.krefel.be/fr/magasins/{location["ref"]}'
+            yield item
