1647251680ad9de0a72eb05e3ba899f67755a6fe
arrival-spring <lnnunns+git@gmail.com>
2024-10-11 22:57:44 +0200

    Fix brand name

--- locations/spiders/chris_willemse_cycles_za.py
+++ locations/spiders/chris_willemse_cycles_za.py
@@ -4,7 +4,7 @@ from locations.spiders.cycle_lab_za import CycleLabZASpider
 
 class ChrisWillemseCyclesZASpider(CycleLabZASpider):
     name = "chris_willemse_cycles_za"
-    item_attributes = {"brand": "Chris Willemse Cyycles", "brand_wikidata": "Q130488816"}
+    item_attributes = {"brand": "Chris Willemse Cycles", "brand_wikidata": "Q130488816"}
     start_urls = ["https://cwcycles.co.za/store-location"]
 
     def post_process_item(self, item, response, location):
