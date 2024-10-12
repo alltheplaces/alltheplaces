f960e3ca264f2989cd0b4e6788a700855dc9259b
Ian Dees <ian.dees@gmail.com>
2024-10-12 09:09:13 -0500

    Don't drop ref and don't lower the download_delay from default

--- locations/spiders/benugo_gb.py
+++ locations/spiders/benugo_gb.py
@@ -7,8 +7,7 @@ class BenugoGBSpider(SitemapSpider, StructuredDataSpider):
     name = "benugo_gb"
     item_attributes = {"operator": "Benugo", "operator_wikidata": "Q20746208"}
     sitemap_urls = ["https://benugo.com/site-sitemap.xml"]
-    drop_attributes = {"twitter", "facebook", "ref"}
-    download_delay = 0.5
+    drop_attributes = {"twitter", "facebook"}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         amenityType = response.url.split("/")[4]
