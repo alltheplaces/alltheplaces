c4e0affd8d65507b9a3dc00bbb5bf2fa15512726
Mateusz Konieczny <matkoniecz@gmail.com>
2024-10-13 11:51:21 +0200

    drop name in jewson_gb.py
    maybe, maybe these should be put into branch key

--- locations/spiders/jewson_gb.py
+++ locations/spiders/jewson_gb.py
@@ -8,7 +8,7 @@ class JewsonGBSpider(scrapy.spiders.SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Jewson", "brand_wikidata": "Q6190226", "country": "GB"}
     sitemap_urls = ["https://www.jewson.co.uk/sitemap/sitemap_branches_jewson.xml"]
     download_delay = 0.5
-    drop_attributes = {"image"}
+    drop_attributes = {"image", "name"}
     wanted_types = ["LocalBusiness"]
 
     def post_process_item(self, item, response, ld_data):
