4022b17ddeab5d3c2f636113eda21416b429cf21
Mateusz Konieczny <matkoniecz@gmail.com>
2024-10-13 09:30:43 +0200

    fjallraven - set SHOP_OUTDOOR
    also based on OpenStreetMap tagging

--- locations/spiders/fjallraven.py
+++ locations/spiders/fjallraven.py
@@ -7,7 +7,7 @@ class FjallravenSpider(Where2GetItSpider):
     item_attributes = {
         "brand": "Fjällräven",
         "brand_wikidata": "Q1422481",
-        "extras": Categories.SHOP_CLOTHES.value,
+        "extras": Categories.SHOP_OUTDOOR.value,
     }
     api_brand_name = "fjallraven"
     api_key = "FE424754-8D89-11E7-A07A-F839407E493E"
