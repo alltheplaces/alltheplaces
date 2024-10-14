5143d49815778cb017bc43641a8f3c2fc8d59578
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-14 12:23:40 +0530

    Add services

--- locations/spiders/ikes.py
+++ locations/spiders/ikes.py
@@ -1,6 +1,7 @@
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Extras, apply_yes_no
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -21,4 +22,10 @@ class IkesSpider(SitemapSpider, StructuredDataSpider):
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["branch"] = response.xpath('//*[@class="location-name"]/text()').get()
+        services = [
+            service.strip().lower() for service in response.xpath('//*[@class="location-services"]/li/text()').getall()
+        ]
+        apply_yes_no(Extras.DELIVERY, item, "delivery" in services)
+        apply_yes_no(Extras.INDOOR_SEATING, item, "dine-in" in services)
+        apply_yes_no(Extras.OUTDOOR_SEATING, item, "patio" in services)
         yield item
