e0b1d073cd322c72500375a9f43ae56b82be1512
Cj Malone <CjMalone@mail.com>
2024-10-14 08:54:15 +0100

    Fix name and branch

--- locations/spiders/ikes.py
+++ locations/spiders/ikes.py
@@ -8,20 +8,12 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class IkesSpider(SitemapSpider, StructuredDataSpider):
     name = "ikes"
-    item_attributes = {
-        "brand": "Ike's Love & Sandwiches",
-        "brand_wikidata": "Q112028897",
-    }
+    item_attributes = {"brand": "Ike's Love & Sandwiches", "brand_wikidata": "Q112028897"}
     sitemap_urls = ["https://locations.ikessandwich.com/robots.txt"]
-    sitemap_rules = [
-        (
-            r"https://locations.ikessandwich.com/[a-z]{2}/[-\w]+/[-\w]+",
-            "parse_sd",
-        ),
-    ]
+    sitemap_rules = [(r"https://locations.ikessandwich.com/[a-z]{2}/[-\w]+/[-\w]+", "parse_sd")]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
-        item["branch"] = response.xpath('//*[@class="location-name"]/text()').get()
+        item["branch"] = item.pop("name").removeprefix("IKE's Sandwiches").strip()
         services = [
             service.strip().lower() for service in response.xpath('//*[@class="location-services"]/li/text()').getall()
         ]
