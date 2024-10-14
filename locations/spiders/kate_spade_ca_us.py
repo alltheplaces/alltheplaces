69a577d69c28ea674bbb05f5178f05258271b7b8
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-14 17:02:04 +0530

    Add branch

--- locations/spiders/kate_spade_ca_us.py
+++ locations/spiders/kate_spade_ca_us.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.spiders.kate_spade import KateSpadeSpider
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -9,3 +11,7 @@ class KateSpadeCAUSSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = KateSpadeSpider.item_attributes
     sitemap_urls = ["https://www.katespade.com/robots.txt"]
     sitemap_rules = [(r"/stores/\w\w/\w\w/[-\w]+/[-\w]+$", "parse_sd")]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Kate Spade ")
+        yield item
