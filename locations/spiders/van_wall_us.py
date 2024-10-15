0c65c719b9ad457a94dc3acad38ba62ea389cc5e
Cj Malone <CjMalone@mail.com>
2024-10-15 17:52:16 +0100

    Restore spider name

--- locations/spiders/van_wall_us.py
+++ locations/spiders/van_wall_us.py
@@ -3,10 +3,8 @@ from locations.hours import DAYS_EN
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class VanWallEquipmentCarrollSpider(WPStoreLocatorSpider):
-    name = "van_wall_equipment_carroll"
+class VanWallUSSpider(WPStoreLocatorSpider):
+    name = "van_wall_us"
     item_attributes = {"brand": "Van Wall", "extras": Categories.SHOP_TOOL_HIRE.value}
-    allowed_domains = [
-        "vanwall.com",
-    ]
+    allowed_domains = ["vanwall.com"]
     days = DAYS_EN
