17cb97953081785495cbe0764b7f8b9b1f04b0d2
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-15 20:38:08 +0530

    Use CrawlSpider and Structured Data to fix Korian FR Spider

--- locations/spiders/korian_fr.py
+++ locations/spiders/korian_fr.py
@@ -1,58 +1,16 @@
-import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class KorianFRSpider(scrapy.Spider):
+class KorianFRSpider(CrawlSpider, StructuredDataSpider):
     name = "korian_fr"
     item_attributes = {"brand": "Korian", "brand_wikidata": "Q3198944"}
-    allowed_domains = ["api-www.korian.fr"]
-
-    def start_requests(self):
-        types = [
-            "maison-retraite",
-            "clinique-ssr",
-            "residence-seniors",
-            "hospitalisation-a-domicile",
-        ]
-
-        regions = [
-            "bretagne",
-            "occitanie",
-            "ile-de-france",
-            "normandie",
-            "auvergne-rhone-alpes",
-            "nouvelle-aquitaine",
-            "hauts-de-france",
-            "provence-alpes-cote-dazur",
-            "centre-val-de-loire",
-            "pays-de-la-loire",
-            "bourgogne-franche-comte",
-            "grand-est",
-        ]
-
-        for type in types:
-            for region in regions:
-                url = "https://api-www.korian.fr/api-front/FR/{type}/{region}".format(type=type, region=region)
-                yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        result = response.json()
-
-        stores = result["data"]["content"][0]["results"]
-
-        for store in stores:
-            properties = {
-                "ref": store["id"],
-                "name": store["name"],
-                "street_address": store["address"],
-                "city": store["city"],
-                "state": store["region"],
-                "postcode": store["zipcode"],
-                "country": "FR",
-                "lat": store["latitude"],
-                "lon": store["longitude"],
-                "website": "https://www.korian.fr/" + store["redirecturl"],
-            }
-
-            yield Feature(**properties)
+    start_urls = ["https://www.korian.fr/maisons-retraite"]
+    rules = [
+        Rule(
+            LinkExtractor(allow=r"https://www.korian.fr/maisons-retraite/[-\w]+/[-\w]+/[-\w]+/[-\w]+$"),
+            callback="parse_sd",
+        )
+    ]
