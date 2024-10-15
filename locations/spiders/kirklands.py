f061ec7bf6ff1838a726824f35d97b0ab7517c16
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-15 15:38:48 +0530

    Switch to Stuctured Data Spider

--- locations/spiders/kirklands.py
+++ locations/spiders/kirklands.py
@@ -1,38 +1,15 @@
-import re
-import urllib.parse
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-from scrapy import Spider
-
-from locations.dict_parser import DictParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class KirklandsSpider(Spider):
+class KirklandsSpider(CrawlSpider, StructuredDataSpider):
     name = "kirklands"
     item_attributes = {
         "brand": "Kirkland's",
         "brand_wikidata": "Q6415714",
     }
-    start_urls = ["https://www.kirklands.com/js/dyn/store_locator_all_stores.js"]
-    ROBOTSTXT_OBEY = False
+    start_urls = ["https://www.kirklands.com/custserv/locate_store.cmd"]
+    rules = [Rule(LinkExtractor(allow="/store.jsp?"), callback="parse_sd")]
     requires_proxy = True
-
-    def parse(self, response):
-        location_objects = re.split(r"allStores\[\d+\] = ", response.text)[1:]
-        location_list = []
-        for location_obj in location_objects:
-            loc_info = {}
-            matches = re.findall(r"store\.(.*?) = (.*?);", location_obj)
-            for key, value in matches:
-                loc_info[key.strip()] = value.strip().strip("'").replace('"', "")
-            location_list.append(loc_info)
-
-        for location in location_list:
-            if location:
-                item = DictParser.parse(location)
-                item["branch"] = item.pop("name")
-                item["street_address"] = " ".join([location["ADDRESS_LINE_1"], location["ADDRESS_LINE_2"]])
-                item["website"] = "https://www.kirklands.com/custserv/store.jsp?storeName=" + urllib.parse.quote(
-                    location["STORE_NAME"]
-                )
-
-                yield item
