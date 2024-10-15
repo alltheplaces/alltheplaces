3b5290eb73c1e6fdc2830ccb6d5103acd77be0e1
Abhinav S <87542795+AbhiKamp@users.noreply.github.com>
2024-10-15 21:25:08 +0530

    Update van_wall_us.py
    name changed to Van Wall Equipment – Carroll also categories changed to SHOP_TOOL_HIRE

--- locations/spiders/van_wall_us.py
+++ locations/spiders/van_wall_us.py
@@ -4,8 +4,8 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
 class VanWallUSSpider(WPStoreLocatorSpider):
-    name = "van_wall_us"
-    item_attributes = {"brand": "Van Wall", "extras": Categories.SHOP_PLANT_HIRE.value}
+    name = "Van Wall Equipment – Carroll"
+    item_attributes = {"brand": "Van Wall", "extras": Categories.SHOP_TOOL_HIRE.value}
     allowed_domains = [
         "vanwall.com",
     ]
