645099f44c3cb223df047f3c88bf856a5719d58b
arrival-spring <lnnunns+git@gmail.com>
2024-10-15 17:03:47 +0200

    Remove generic facebook

--- locations/spiders/papa_johns_gb.py
+++ locations/spiders/papa_johns_gb.py
@@ -15,6 +15,9 @@ class PapaJohnsGBSpider(SitemapSpider, StructuredDataSpider):
 
     def post_process_item(self, item, response, ld_data):
         item["website"] = response.url
+        if item["extras"]["facebook"] == "https://www.facebook.com/papajohnsuk":
+            item["extras"].pop("facebook")
+
         item["branch"] = item.pop("name")
         if m := re.search(r"\((-?\d+\.\d+),(-?\d+\.\d+)\)", response.xpath('.//*[@class="map col"]/img/@src').get()):
             item["lon"], item["lat"] = m.groups()
