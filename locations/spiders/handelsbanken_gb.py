a822eec4c9c1160c73b155d4d1edb98552c867a3
Cj Malone <CjMalone@mail.com>
2024-10-15 10:23:17 +0100

    apply_category

--- locations/spiders/handelsbanken_gb.py
+++ locations/spiders/handelsbanken_gb.py
@@ -1,28 +1,27 @@
 import scrapy
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
 
 
 class HandelsbankenGBSpider(scrapy.Spider):
     name = "handelsbanken_gb"
-    item_attributes = {"brand": "Handelsbanken", "brand_wikidata": "Q1421630", "extras": Categories.BANK.value}
+    item_attributes = {"brand": "Handelsbanken", "brand_wikidata": "Q1421630"}
     start_urls = ["https://www.handelsbanken.co.uk/rsoia/parg/bu/branches/v3/country/GB/en"]
 
     def parse(self, response, **kwargs):
         for location in response.json():
             item = DictParser.parse(location)
-            item["name"] = "Handelsbanken"
-            item["branch"] = location["name"]
+            item["branch"] = item.pop("name")
             if location["branchUrl"]:
                 item["website"] = "https://" + location["branchUrl"]
-            item["street_address"] = location["streetAddress"]
 
             oh = OpeningHours()
-            days = location["openingHours"]
-            for day in days:
+            for day in location["openingHours"]:
                 oh.add_range(DAYS[int(day.get("weekday"))], day.get("openTime"), day.get("closeTime"))
-            item["opening_hours"] = oh.as_opening_hours()
+            item["opening_hours"] = oh
+
+            apply_category(Categories.BANK, item)
 
             yield item
