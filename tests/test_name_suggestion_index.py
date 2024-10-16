b7f4fd85bf5e4de99c5fc7ce0ce824d6744bfec1
Ian Dees <ian.dees@gmail.com>
2024-10-16 08:19:54 -0500

    Fix McDonald's lookup test for NSI

--- tests/test_name_suggestion_index.py
+++ tests/test_name_suggestion_index.py
@@ -4,7 +4,7 @@ from locations.name_suggestion_index import NSI
 def test_nsi_lookup_wikidata():
     nsi = NSI()
     data = nsi.lookup_wikidata("Q38076")
-    assert data["identities"]["facebook"] == "mcdonalds"
+    assert data["identities"]["website"] == "https://www.mcdonalds.com"
 
 
 def test_iter_wikidata():
