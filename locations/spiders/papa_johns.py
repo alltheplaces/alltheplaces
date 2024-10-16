37ea8498eaa2c47c1950625161a602814d390223
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-10-16 14:15:24 +0100

    [Papa John's] Fix spider (#11149)
    * [Papa John's] Fix spider
    
    * Use old ref
    
    Number in url is postcode
    
    * Clean ref

--- locations/spiders/papa_johns.py
+++ locations/spiders/papa_johns.py
@@ -8,22 +8,17 @@ class PapaJohnsSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Papa John's", "brand_wikidata": "Q2759586"}
     allowed_domains = ["papajohns.com"]
     sitemap_urls = ["https://locations.papajohns.com/sitemap.xml"]
-    sitemap_rules = [
-        (
-            r"https:\/\/locations\.papajohns\.com\/(?:united\-states|canada)\/\w{2}\/[-\w]+\/[-\w]+\/.+$",
-            "parse_sd",
-        )
-    ]
+    sitemap_rules = [(r"com/(?:united\-states|canada)/\w{2}/[-\w]+/[^/]+/[^/]+$", "parse_sd")]
     wanted_types = ["FastFoodRestaurant"]
-    drop_attributes = {"image"}
+    search_for_twitter = False
+    search_for_facebook = False
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        if name := item.get("name", "").lower():
-            if name.startswith("coming soon - "):
-                return
-            else:
-                item["branch"] = item.pop("name").removeprefix("Papa Johns Pizza ")
+        item["ref"] = item["ref"].strip("/.")
+        if item.get("name").startswith("Coming Soon - "):
+            item["opening_hours"] = "off"
+        item["name"] = item["image"] = None
 
-        item["website"] = response.urljoin(item["website"])
+        item["website"] = response.url
 
         yield item
