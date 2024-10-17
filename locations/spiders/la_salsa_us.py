40b98fb7aac389c39ff5473c9be39bbf04528c01
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-17 18:10:24 +0530

    Rename spider

--- locations/spiders/la_salsa_us.py
+++ locations/spiders/la_salsa_us.py
@@ -4,8 +4,8 @@ from scrapy.spiders import CrawlSpider, Rule
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class LaSalsaSpider(CrawlSpider, StructuredDataSpider):
-    name = "la_salsa"
+class LaSalsaUSSpider(CrawlSpider, StructuredDataSpider):
+    name = "la_salsa_us"
     item_attributes = {"brand": "La Salsa", "brand_wikidata": "Q48835190"}
     start_urls = ["https://www.lasalsa.com/locator/index.php?brand=26&pagesize=1000&q=us"]
     rules = [
