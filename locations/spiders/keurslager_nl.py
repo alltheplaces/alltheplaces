a4dcf9ed9c909affb6751f084ef1b280106552e7
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-10-17 12:36:44 +0530

    code refactored to fix spider

--- locations/spiders/keurslager_nl.py
+++ locations/spiders/keurslager_nl.py
@@ -1,30 +1,53 @@
-from typing import Any
-
+import scrapy
+from chompjs import chompjs
 from scrapy import Spider
-from scrapy.http import Response
 
-from locations.dict_parser import DictParser
-from locations.hours import DAYS, OpeningHours
+from locations.hours import OpeningHours
+from locations.items import Feature
 
 
 class KeurslagerNLSpider(Spider):
     name = "keurslager_nl"
     item_attributes = {"brand": "Keurslager", "brand_wikidata": "Q114637402"}
-    start_urls = ["https://www.keurslager.nl/map-locations?hubId=41"]
+    start_urls = ["https://www.keurslager.nl/winkels/"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for ref, location in response.json().items():
-            if not isinstance(location, dict):
+    def parse(self, response):
+        for location_id in (chompjs.parse_js_object(response.xpath('//*[contains(text(),"FWP_JSON")]/text()').get()))[
+            "preload_data"
+        ]["settings"]["map"]["locations"]:
+            payload = f'action=facetwp_map_marker_content&facet_name=map&post_id={location_id["post_id"]}'
+            url = "https://www.keurslager.nl/wp/wp-admin/admin-ajax.php"
+            yield scrapy.Request(
+                url=url,
+                method="POST",
+                body=payload,
+                headers={"Content-Type": "application/x-www-form-urlencoded"},
+                cb_kwargs={
+                    "id": location_id["post_id"],
+                    "lat": location_id["position"]["lat"],
+                    "lon": location_id["position"]["lng"],
+                },
+                callback=self.parse_details,
+            )
+
+    def parse_details(self, response, **kwargs):
+        item = Feature()
+        item["name"] = response.xpath("//h2/text()").get()
+        item["street_address"] = response.xpath("//text()[1]").get()
+        item["addr_full"] = ",".join([response.xpath("//text()[1]").get(), response.xpath("//text()[2]").get()])
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["email"] = response.xpath('//*[contains(@href,"mailto:")]/text()').get()
+        item["website"] = response.xpath("//a[3]/@href").get()
+        item["ref"] = kwargs["id"]
+        item["lat"] = kwargs["lat"]
+        item["lon"] = kwargs["lon"]
+        item["opening_hours"] = OpeningHours()
+        for day_time in response.xpath("//table//tr"):
+            day = day_time.xpath(".//td/text()").get()
+            if day_time.xpath(".//td[2]/text()").get() in ["gesloten", None]:
                 continue
-            item = DictParser.parse(location)
-            item["street_address"] = item.pop("addr_full")
-            item["ref"] = ref
-            item["extras"]["ref:google"] = location["placeid"]
-
-            item["opening_hours"] = OpeningHours()
-            for rule in location["openTimes"]:
-                if rule["closed"]:
-                    continue
-                item["opening_hours"].add_range(DAYS[rule["day"]], rule["openTime"], rule["closeTime"])
-
-            yield item
+            open_time, close_time = day_time.xpath(".//td[2]/text()").get().split("-")
+            item["opening_hours"].add_range(
+                day=day.replace(":", ""), open_time=open_time.strip(), close_time=close_time.strip()
+            )
+        yield item
