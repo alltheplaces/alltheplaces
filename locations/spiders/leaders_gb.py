8a426591c1d5496e0e0fa30d91ca20233cdb043a
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-18 15:23:04 +0530

    Fix Leaders GB Spider

--- locations/spiders/leaders_gb.py
+++ locations/spiders/leaders_gb.py
@@ -1,32 +1,20 @@
-from urllib.parse import urljoin
+import json
+from typing import Iterable
 
-from scrapy.spiders import Spider
+from scrapy.http import Response
 
 from locations.items import Feature
-from locations.structured_data_spider import extract_email, extract_phone
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class LeadersGBSpider(Spider):
+class LeadersGBSpider(JSONBlobSpider):
     name = "leaders_gb"
     item_attributes = {"brand": "Leaders", "brand_wikidata": "Q111522674"}
     start_urls = ["https://www.leaders.co.uk/contact-us"]
 
-    def parse(self, response, **kwargs):
-        for location in response.xpath(
-            '//div[contains(@class, "branch-dept-Sales") or contains(@class, "branch-dept-Lettings")]'
-        ):
-            item = Feature()
-            item["addr_full"] = location.xpath("./p/text()").get()
+    def extract_json(self, response: Response) -> list:
+        return json.loads(response.xpath("//@data-branches").get())
 
-            if "Covering" in item["addr_full"]:
-                continue
-
-            item["ref"] = location.xpath("./@data-history-node-id").get()
-            item["image"] = urljoin(response.url, location.xpath("./img/@src").get())
-            item["lat"], item["lon"] = location.xpath(".//@data-location").get().split(",")
-            item["name"] = location.xpath("./h5/span/text()").get()
-            extract_email(item, location)
-            extract_phone(item, location)
-            item["website"] = urljoin(response.url, location.xpath('.//p[@class="branch-link"]/a/@href').get())
-
-            yield item
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = item["website"]
+        yield item
