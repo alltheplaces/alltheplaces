07c3f06b13175d1672e8e08f7cbe79348ca17d1f
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-10-20 16:29:54 +0100

    Update tag_heuer.py

--- locations/spiders/tag_heuer.py
+++ locations/spiders/tag_heuer.py
@@ -10,6 +10,8 @@ class TagHeuerSpider(AlgoliaSpider):
     api_key = "8cf40864df513111d39148923f754024"
     app_id = "6OBGA4VJKI"
     index_name = "stores"
+    myfilter = "type:TAGSTORE"
+
     # {"params":"filters=products.mechanicalWatches:true AND country:GB&attributesToRetrieve=address,Latitude,zip,country,phone,type,id,image,address2,email,name,description,services,payments,city,openingHours,exceptionalHours,_geoloc,i18nAddress,image1,timezone,sfccUrl","aroundRadius":"15000","hitsPerPage":"1000","getRankingInfo":"1","attributesToRetrieve":"address,Latitude,zip,country,phone,type,id,image,address2,email,name,description,services,payments,city,openingHours,exceptionalHours,_geoloc,i18nAddress,image1,timezone,sfccUrl","attributesToHighlight":"name"}
 
     def post_process_item(self, item, response, feature):
@@ -20,8 +22,5 @@ class TagHeuerSpider(AlgoliaSpider):
         item["image"] = feature["image"]
         slug = feature["sfccUrl"]
         item["website"] = f"https://www.tagheuer.com/{slug}"
-        if "boutique-tag-heuer" in slug:
-            yield item
-
-        if len(feature) == 1000:
-            yield self.make_request(response.meta["page"] + 1)
+        #if "boutique-tag-heuer" in slug:
+        yield item
