e0e0833c563ac6680d1cbdccd9357351ab2beb2c
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-21 16:57:13 +0530

    Fix Mavis US Spuder

--- locations/spiders/mavis_us.py
+++ locations/spiders/mavis_us.py
@@ -1,31 +1,30 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from typing import Iterable
+
+import chompjs
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.structured_data_spider import StructuredDataSpider
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class MavisUSSpider(CrawlSpider, StructuredDataSpider):
+class MavisUSSpider(JSONBlobSpider):
     name = "mavis_us"
     item_attributes = {"brand": "Mavis", "brand_wikidata": "Q65058420"}
     start_urls = ["https://www.mavis.com/locations/all-stores/"]
-    rules = [
-        Rule(
-            LinkExtractor(allow=r"/locations/[^/]+/$", restrict_xpaths=['//a[starts-with(text(), "Mavis")]']),
-            callback="parse",
-        )
-    ]
-    wanted_types = ["Store"]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["addr_full"] = item.pop("street_address")
+    def extract_json(self, response: Response) -> list:
+        stores = []
+        for state_wise_stores_list in chompjs.parse_js_object(
+            (chompjs.parse_js_object(response.xpath('//script[contains(text(),"fullAddress")]/text()').get())[-1])
+        )[-1]["children"][-1]["stores"].values():
+            stores.extend(state_wise_stores_list)
+        return stores
 
-        label = item.pop("name")
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        label = feature.get("storeHeader", {}).get("fields", {}).get("myStoreLabel")
         if label.startswith("Mavis Tire"):
             item["name"] = "Mavis Tires & Brakes"
         elif label.startswith("Mavis Discount"):
             item["name"] = "Mavis Discount Tire"
-
-        apply_category(Categories.SHOP_TYRES, item)
-
         yield item
