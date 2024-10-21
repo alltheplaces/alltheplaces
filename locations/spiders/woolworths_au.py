2b1ee43b00b1a46fb5b888de060ca23aa18dbb52
Cj Malone <CjMalone@mail.com>
2024-10-21 12:26:38 +0100

    [Woolworths AU] Improve Woolworths Metro branding

--- locations/spiders/woolworths_au.py
+++ locations/spiders/woolworths_au.py
@@ -1,38 +1,42 @@
-import scrapy
+from typing import Any
 
+import scrapy
+from scrapy.http import Response
+
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
-from locations.pipelines.address_clean_up import clean_address
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class WoolworthsAUSpider(scrapy.Spider):
     name = "woolworths_au"
+    METRO = {"brand": "Woolworths Metro", "brand_wikidata": "Q111772555"}
     item_attributes = {"brand": "Woolworths", "brand_wikidata": "Q3249145"}
     allowed_domains = ["woolworths.com.au"]
     start_urls = [
-        "https://www.woolworths.com.au/apis/ui/StoreLocator/Stores?Max=10000&Division=SUPERMARKETS,PETROL,CALTEXWOW,AMPOLMETRO,AMPOL&Facility=&postcode=*"
+        "https://www.woolworths.com.au/apis/ui/StoreLocator/Stores?Max=10000&Division=SUPERMARKETS&Facility=&postcode=*"
     ]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
     requires_proxy = "AU"
 
-    def parse(self, response):
-        data = response.json()
-
-        for i in data["Stores"]:
-            if not i["IsOpen"]:
-                continue
-
-            i["street_address"] = clean_address([i["AddressLine1"], i["AddressLine2"]])
-            i["ref"] = i.pop("StoreNo")
-            i["city"] = i.pop("Suburb")
-
-            item = DictParser.parse(i)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["Stores"]:
+            item = DictParser.parse(location)
+            item["branch"] = item.pop("name")
+            item["street_address"] = merge_address_lines([location["AddressLine1"], location["AddressLine2"]])
+            item["ref"] = location["StoreNo"]
+            item["city"] = location["Suburb"]
 
             item["website"] = (
                 "https://www.woolworths.com.au/shop/storelocator/"
-                + "-".join([item["state"], item["city"], item["ref"], i["Division"]]).lower()
+                + "-".join([item["state"], item["city"], item["ref"], location["Division"]]).lower()
             )
 
-            # TODO: types needs some work, NSI seems out of date too
-            item["extras"] = {"type": i["Division"]}
+            if "Metro" in item["branch"]:
+                item["branch"] = item["branch"].replace("Metro", "").strip(" ()")
+                item.update(self.METRO)
+
+            apply_yes_no(Extras.WIFI, item, "WiFi" in location["Facilities"])
+
+            apply_category(Categories.SHOP_SUPERMARKET, item)
 
             yield item
