74d21dd07b530be505103115d54a05d27eec3002
panasonic-mafia <kolosov.yv@gmail.com>
2024-10-23 00:58:57 +0200

    more work on hours and address

--- locations/spiders/starbucks_eu.py
+++ locations/spiders/starbucks_eu.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import scrapy
 
 from locations.categories import Categories, Extras, PaymentMethods, apply_category, apply_yes_no
@@ -5,6 +7,7 @@ from locations.dict_parser import DictParser
 from locations.geo import point_locations
 from locations.hours import DAYS_EN, OpeningHours
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 from locations.spiders.starbucks_us import STARBUCKS_SHARED_ATTRIBUTES
 
 # Full list of features: https://www.starbucks.co.uk/api/v2/storeFeatures/
@@ -27,35 +30,45 @@ class StarbucksEUSpider(scrapy.Spider):
         for lat, lon in point_locations("eu_centroids_20km_radius_country.csv"):
             yield scrapy.Request(base_url.format(lat, lon))
 
-    def parse(self, response):
+    def parse(self, response) -> Iterable[Feature]:
         for poi in response.json().get("data", []):
-            # move most of the attributes to the top level
+            # Help to DictParser a bit
             attributes = poi.pop("attributes", {})
             coordinates = poi.pop("coordinates", {})
             address = attributes.pop("address", {})
             poi = {**poi, **attributes, **coordinates, **address}
+
             item = DictParser.parse(poi)
             item["ref"] = poi["storeNumber"]
             item["branch"] = item.pop("name")
+            item["street_address"] = merge_address_lines(
+                [poi.get("streetAddressLine1"), poi.get("streetAddressLine2"), poi.get("streetAddressLine3")]
+            )
             self.parse_hours(item, poi)
             self.parse_features(item, poi)
             apply_category(Categories.COFFEE_SHOP, item)
             yield item
 
     def parse_hours(self, item: Feature, poi: dict):
-        if poi.get("open24x7") is True:
+        hours = poi.get("openHours") or {}
+
+        if poi.get("open24x7") is True or hours.get("open24x7") is True:
             item["opening_hours"] = "24/7"
             return
 
-        if not poi.get("openHours"):
+        if not hours:
             return
 
         try:
-            hours = poi.get("openHours")
             oh = OpeningHours()
             for day_name, data in hours.items():
+                if day_name == "open24x7":
+                    # Already handled above
+                    continue
+
                 day = DAYS_EN.get(day_name.title())
-                if data.get("open") is False:
+
+                if data is None or data.get("open") is False:
                     oh.set_closed(day)
                     continue
 
@@ -67,7 +80,7 @@ class StarbucksEUSpider(scrapy.Spider):
                 oh.add_range(day, open_time, close_time, time_format="%H:%M:%S")
             item["opening_hours"] = oh
         except Exception as e:
-            self.logger.warning(f"Failed to parse hours: {e}")
+            self.logger.warning(f"Failed to parse hours: {e}, {poi.get('openHours')}, {poi.get('storeNumber')}")
             self.crawler.stats.inc_value(f"atp/{self.name}/hours/fail")
 
     def parse_features(self, item, poi):
