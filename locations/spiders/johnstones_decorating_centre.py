f911d04e242b67320ba093c76ed8459ac0e07424
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2024-10-22 16:11:27 +0530

    [Johnstone's Decorating Centre] Fix Spider (#11187)
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/johnstones_decorating_centre.py
+++ locations/spiders/johnstones_decorating_centre.py
@@ -1,49 +1,30 @@
 from typing import Any
+from urllib.parse import urljoin
 
+import scrapy
+from scrapy import Selector
 from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address
 
 
-class JohnstonesDecoratingCentreSpider(SitemapSpider):
+class JohnstonesDecoratingCentreSpider(scrapy.Spider):
     name = "johnstones_decorating_centre"
     item_attributes = {"brand": "Johnstone's Decorating Centre", "brand_wikidata": "Q121742106"}
-    sitemap_urls = ["https://www.johnstonesdc.com/sitemap.xml"]
-
-    # https://www.johnstonesdc.com/united-kingdom-region-norwich-17634
-    # https://www.johnstonesdc.com/republic-of-ireland-region-dublin-rathmines-17621
-    sitemap_rules = [(r"/(united-kingdom-region|republic-of-ireland-region)-[-\w]+\d+$", "parse")]
+    start_urls = ["https://www.johnstonesdc.com/StoreLocator/AjaxStores"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        if "https://www.johnstonesdc.com/stores" in response.url:
-            return
-        store_locator = response.xpath("//section[contains(@class, 'store-locator')]")
-        address = store_locator.xpath(".//div[contains(@class, 'card-address')]/div/div/div/text()").getall()
-        hours = store_locator.xpath(
-            ".//div[contains(@class, 'card-opening-hours')]//div[contains(@class, 'info-details')]/span/text()"
-        )
+        for store in response.json()["markers"]:
+            data = Selector(text=store["popupHTML"])
+            item = Feature()
+            item["branch"] = data.xpath("//h5/text()").get()
+            item["street_address"] = data.xpath("//*[@class='address-holder']/text()").get()
+            item["addr_full"] = data.xpath("//*[@class='address-holder']").xpath("normalize-space()").get()
+            item["lat"], item["lon"] = store["location"]
+            item["ref"] = store["id"]
 
-        contact = store_locator.xpath(
-            ".//div[contains(@class, 'card-contact')]//div[contains(@class, 'info-details')]/span/text()"
-        ).getall()
-        map = store_locator.xpath(".//*[@id='map']")
-
-        oh = OpeningHours()
-        oh.add_ranges_from_string(" ".join(hours.getall()))
-
-        properties = {
-            "lat": map.xpath("@data-lat").get(0),
-            "lon": map.xpath("@data-lng").get(0),
-            "ref": response.url,
-            "website": response.url,
-            "opening_hours": oh,
-            "addr_full": clean_address([address[1], address[2]]),
-            "phone": contact[0],
-            "email": contact[1],
-            "name": address[0],
-        }
-
-        yield Feature(**properties)
+            sel = Selector(text=store["card"])
+            item["website"] = urljoin(
+                "https://www.johnstonesdc.com", sel.xpath('//a[text() = "Shop detail"]/@href').get()
+            )
+            yield item
