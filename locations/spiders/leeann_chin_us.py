87d649b3a54f90ee7f43eb781e5162a175193264
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-10-22 07:39:32 +0530

    Fix Leeann Chin US Spider

--- locations/spiders/leeann_chin_us.py
+++ locations/spiders/leeann_chin_us.py
@@ -1,25 +1,18 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+import chompjs
+from scrapy.http import Response
 
-from locations.hours import DAYS_EN
-from locations.structured_data_spider import StructuredDataSpider
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class LeeannChinUSSpider(CrawlSpider, StructuredDataSpider):
+class LeeannChinUSSpider(JSONBlobSpider):
     name = "leeann_chin_us"
     item_attributes = {
         "brand": "Leeann Chin",
         "brand_wikidata": "Q6515716",
     }
     start_urls = ["https://www.leeannchin.com/locations"]
-    wanted_types = ["Restaurant"]
-    rules = [
-        Rule(LinkExtractor(allow=r"restaurant/"), callback="parse_sd"),
-    ]
-    time_format = "%I:%M %p"
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if "openingHours" in ld_data:
-            item["opening_hours"].add_ranges_from_string(ld_data["openingHours"], days=DAYS_EN)
-
-        return super().post_process_item(item, response, ld_data, **kwargs)
+    def extract_json(self, response: Response) -> list:
+        return chompjs.parse_js_object(response.xpath('//script[contains(text(), "var jsonContent")]/text()').get())[
+            "data"
+        ]
