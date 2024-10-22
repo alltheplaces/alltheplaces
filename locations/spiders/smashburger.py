3a11a53affb88c8eae58b19b25b6d942fafc5ddb
Cj Malone <CjMalone@mail.com>
2024-10-22 15:27:28 +0100

    [Smashburger] Remove duplicates

--- locations/spiders/smashburger.py
+++ locations/spiders/smashburger.py
@@ -1,6 +1,8 @@
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -10,18 +12,12 @@ class SmashburgerSpider(CrawlSpider, StructuredDataSpider):
     allowed_domains = ["smashburger.com"]
     start_urls = ["https://smashburger.com/locations/index.html"]
     rules = [
-        Rule(
-            LinkExtractor(allow=r"^https:\/\/smashburger\.com\/locations\/[a-z]{2}\/[a-z]{2}(?:\/(?:[^/]+\/?))?$"),
-            follow=True,
-        ),
-        Rule(
-            LinkExtractor(allow=r"^https:\/\/smashburger\.com\/locations\/[a-z]{2}\/[a-z]{2}\/[^/]+\/.+"),
-            callback="parse_sd",
-            follow=False,
-        ),
+        Rule(LinkExtractor(allow=r"^https://smashburger\.com/locations/\w\w(?:/\w\w)?(?:/[^/]+)?$")),
+        Rule(LinkExtractor(allow=r"^https://smashburger\.com/locations/\w\w/\w\w/[^/]+/[^/]+$"), callback="parse_sd"),
     ]
+    wanted_types = ["Restaurant"]
 
-    def post_process_item(self, item, response, ld_data):
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["website"] = response.url
         item.pop("facebook")
         item.pop("twitter")
