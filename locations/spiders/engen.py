43f3ef36669b909e35bb6cd55947ccd5293ec035
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2024-10-23 02:24:50 +0200

    [engen] Fix CD and SZ countries and fix shop category (#11214)
    * [engen] Fix CD country and shop category
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Use internal id for ref
    
    * Fix SZ country
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/engen.py
+++ locations/spiders/engen.py
@@ -9,11 +9,19 @@ class EngenSpider(JSONBlobSpider):
     locations_key = ["response", "data", "stations"]
 
     def post_process_item(self, item, response, location):
+        item["ref"] = location["internal_id"]
         item["branch"] = location.pop("company_name").replace(self.item_attributes["brand"], "").strip()
         item["phone"] = location.pop("mobile")
+
+        if location.get("country_name") == "DRC":  # has incorrect country_code of ZA
+            item["country"] = "CD"
+        if location.get("country_name") == "eSwatini":  # has incorrect country_code of ZA
+            item["country"] = "SZ"
+
         postcode = location.pop("street_postal_code")
         if postcode and postcode != "0":
             location["postcode"] = postcode
+
         item["street_address"] = item.pop("street")
         try:
             int(item["street_address"].split(" ", 1)[0])
@@ -21,6 +29,7 @@ class EngenSpider(JSONBlobSpider):
             item["street"] = item["street_address"].split(" ", 1)[1]
         except ValueError:
             pass
+
         if "Quickshop" in location.get("rental_units", ""):
             shop_item = item.deepcopy()
             shop_item["ref"] = str(item.get("ref")) + "-attached-shop"
@@ -32,13 +41,17 @@ class EngenSpider(JSONBlobSpider):
                     "brand_wikidata": "Q122764368",
                 }
             )
-            apply_category(Categories.SHOP_CONVENIENCE, item)
+            apply_category(Categories.SHOP_CONVENIENCE, shop_item)
             yield shop_item
+
         apply_category(Categories.FUEL_STATION, item)
+
         if location["station_types"] == "Truck Stop":
             item["name"] = "Truck Stop"
             apply_yes_no(Access.HGV, item, True)
         else:
             item["name"] = "Engen"
+
         apply_yes_no(Fuel.ADBLUE, item, "AdBlue" in location["rental_units"])
+
         yield item
