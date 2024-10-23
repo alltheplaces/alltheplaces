c154a706ea509d8300136f8a29cb63297e52e8ff
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-10-23 17:32:12 +0530

    code refactored to fix spider

--- locations/spiders/kruidvat.py
+++ locations/spiders/kruidvat.py
@@ -1,58 +1,42 @@
-from typing import Any, Iterable
-from urllib.parse import urlencode
+from typing import Any
 
 import scrapy
-from scrapy import Request
-from scrapy.http import JsonRequest, Response
+from scrapy.http import Response
 
-from locations.hours import DAYS_NL, OpeningHours, sanitise_day
+from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class KruidvatSpider(scrapy.Spider):
     name = "kruidvat"
     item_attributes = {"brand": "Kruidvat", "brand_wikidata": "Q2226366"}
-    allowed_domains = ["kruidvat.nl"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-
-    def make_request(self, api: str, page: int) -> JsonRequest:
-        return JsonRequest(
-            url="{}?{}".format(
-                api, urlencode({"fields": "FULL", "radius": "100000", "pageSize": "100", "currentPage": str(page)})
-            ),
-            meta={"api": api, "page": page},
-        )
-
-    def start_requests(self) -> Iterable[Request]:
-        yield self.make_request("https://www.kruidvat.be/api/v2/kvb/stores", 0)
-        yield self.make_request("https://www.kruidvat.nl/api/v2/kvn/stores", 0)
+    start_urls = [
+        "https://www.kruidvat.be/api/v2/kvb/stores?lang=nl_BE&radius=100000&pageSize=10000&fields=FULL",
+        "https://www.kruidvat.nl/api/v2/kvn/stores?lang=nl&radius=100000&pageSize=10000&fields=FULL",
+    ]
+    custom_settings = {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        pagination = response.json()["pagination"]
-        if pagination["currentPage"] < pagination["totalPages"]:
-            yield self.make_request(response.meta["api"], pagination["currentPage"] + 1)
+        for store in response.xpath("//stores"):
+            item = Feature()
+            item["name"] = store.xpath(".//displayName/text()").get()
+            item["state"] = store.xpath(".//province/text()").get()
+            item["postcode"] = store.xpath(".//postalCode/text()").get()
+            item["street"] = store.xpath(".//line1/text()").get()
+            item["lat"] = store.xpath(".//latitude/text()").get()
+            item["lon"] = store.xpath(".//longitude/text()").get()
+            item["addr_full"] = store.xpath(".//formattedAddress/text()").get()
+            item["opening_hours"] = OpeningHours()
+            for day_time in store.xpath(".//weekDayOpeningList"):
+                open_time = day_time.xpath("./openingTime/formattedHour/text()").get()
+                close_time = day_time.xpath("./closingTime/formattedHour/text()").get()
+                day = day_time.xpath(".//shortenedWeekDay/text()").get()
+                if day:
+                    item["opening_hours"].add_range(day=day, open_time=open_time, close_time=close_time)
 
-        for store in response.json().get("stores"):
-            oh = OpeningHours()
-            for day in store.get("openingHours", {}).get("weekDayOpeningList"):
-                oh.add_range(
-                    day=sanitise_day(day.get("weekDay")[:2], DAYS_NL),
-                    open_time=day.get("openingTime", {}).get("formattedHour"),
-                    close_time=day.get("closingTime", {}).get("formattedHour"),
-                )
-
-            properties = {
-                "ref": store.get("externalId"),
-                "addr_full": store.get("address", {}).get("formattedAddress"),
-                "street_address": store.get("address", {}).get("line1"),
-                "country": store.get("address", {}).get("country").get("isocode"),
-                "city": store.get("address").get("town"),
-                "state": store["address"].get("province"),
-                "postcode": store.get("address", {}).get("postalCode"),
-                "lat": store.get("geoPoint", {}).get("latitude"),
-                "lon": store.get("geoPoint", {}).get("longitude"),
-                "website": response.urljoin(store.get("url")),
-                "opening_hours": oh.as_opening_hours(),
-            }
-
-            yield Feature(**properties)
+            if ".nl" in response.url:
+                item["ref"] = item["website"] = "https://www.kruidvat.nl/" + store.xpath(".//url/text()").get()
+            else:
+                item["ref"] = item["website"] = "https://www.kruidvat.be/" + store.xpath(".//url/text()").get()
+            yield item
