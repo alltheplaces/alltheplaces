748a06b6a0e0cf8e71ce2a1f982df07834e98c9b
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-10-24 15:02:38 +0100

    [Pizza Hut US] Fix spider (#11227)

--- locations/spiders/pizza_hut_us.py
+++ locations/spiders/pizza_hut_us.py
@@ -1,75 +1,33 @@
-import re
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
-import scrapy
+from locations.categories import Categories, apply_category
+from locations.items import Feature, set_closed
+from locations.structured_data_spider import StructuredDataSpider
 
-from locations.categories import Categories
-from locations.items import Feature
 
-
-class PizzaHutUSSpider(scrapy.Spider):
+class PizzaHutUSSpider(SitemapSpider, StructuredDataSpider):
     name = "pizza_hut_us"
+    item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
     PIZZA_HUT = {"brand": "Pizza Hut", "brand_wikidata": "Q191615", "extras": Categories.RESTAURANT.value}
-    PIZZA_HUT_EXPRESS = {
-        "brand": "Pizza Hut Express",
-        "brand_wikidata": "Q191615",
-        "extras": Categories.FAST_FOOD.value,
-    }
-    PIZZA_HUT_DELIVERY = {
-        "brand": "Pizza Hut Delivery",
-        "brand_wikidata": "Q191615",
-        "extras": Categories.FAST_FOOD.value,
-    }
-    item_attributes = PIZZA_HUT
-    allowed_domains = ["pizzahut.com"]
-    start_urls = ("https://locations.pizzahut.com/",)
+    sitemap_urls = ["https://locations.pizzahut.com/robots.txt"]
+    sitemap_rules = [
+        (
+            r"^https://locations\.pizzahut\.com/\w\w/[^/]+/(?!pizzeria|food-places|takeout|pizza-deals|pizza-delivery|wings)[^/]+$",
+            "parse_sd",
+        )
+    ]
+    wanted_types = ["FastFoodRestaurant"]
 
-    def parse(self, response):
-        urls = response.xpath('//li[@class="Directory-listItem"]/a/@href').extract()
-
-        for url in urls:
-            if len(url.split("/")) == 2:
-                yield scrapy.Request(response.urljoin(url), callback=self.parse_stores)
-            else:
-                yield scrapy.Request(response.urljoin(url), callback=self.parse_cities)
-
-    def parse_cities(self, response):
-        urls = response.xpath('//a[@class="Directory-listLink"]/@href').extract()
-
-        for url in urls:
-            if len(url.split("/")) == 2:
-                yield scrapy.Request(response.urljoin(url), callback=self.parse_stores)
-            else:
-                yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
-
-    def parse_stores(self, response):
-        urls = response.xpath('//div[@class="Directory-content"]//li//a[@class="Teaser-titleLink"]/@href').extract()
-        for url in urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_store)
-
-    def parse_store(self, response):
-        ref = re.findall(r".com/(.+?)/(.+?)/(.+)", response.url)[0]
-        ref = "_".join(ref)
-        properties = {
-            "street_address": response.xpath('//span[@class="c-address-street-1"]/text()').extract_first(),
-            "phone": response.xpath('//span[@itemprop="telephone"]/text()').extract_first(),
-            "city": response.xpath('//span[@class="c-address-city"]/text()').extract_first(),
-            "state": response.xpath('//abbr[@class="c-address-state"]/text()').extract_first(),
-            "postcode": response.xpath('//span[@class="c-address-postal-code"]/text()').extract_first(),
-            "ref": ref,
-            "website": response.url,
-            "lat": float(response.xpath('//meta[@itemprop="latitude"]/@content').extract_first()),
-            "lon": float(response.xpath('//meta[@itemprop="longitude"]/@content').extract_first()),
-        }
-
-        brand = response.xpath(
-            '//h1[@class="HeroRedesign-title Heading-head--redesign"]/span[@itemprop="name"]/text()'
-        ).get()
-        if brand == "Pizza Hut":
-            properties.update(self.PIZZA_HUT)
-        elif brand == "Pizza Hut Express":
-            properties.update(self.PIZZA_HUT_EXPRESS)
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["image"] = None
+        label = response.xpath('//h1[@class="H1"]/text()').get()
+        if not label:
+            set_closed(item)
+        if label == "Pizza Hut Express":
+            item["name"] = "Pizza Hut Express"
+            apply_category(Categories.FAST_FOOD, item)
         else:
-            properties["brand"] = brand
-            self.crawler.stats.inc_value(f"atp/pizza_hut_us/unmapped_brand/{brand}")
+            apply_category(Categories.RESTAURANT, item)
 
-        yield Feature(**properties)
+        yield item
