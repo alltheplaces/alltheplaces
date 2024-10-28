a25050579acb7016b57534fca5f9752311f7d3af
Spencer Alves <impiaaa@gmail.com>
2024-10-28 15:53:08 -0700

    Workaround to fix Guitar Center

--- locations/spiders/guitar_center.py
+++ locations/spiders/guitar_center.py
@@ -1,3 +1,5 @@
+from scrapy.http import JsonRequest
+
 from locations.storefinders.rio_seo import RioSeoSpider
 
 
@@ -5,6 +7,9 @@ class GuitarCenterSpider(RioSeoSpider):
     name = "guitar_center"
     item_attributes = {"brand": "Guitar Center", "brand_wikidata": "Q3622794"}
     end_point = "https://maps.stores.guitarcenter.com"
+
+    def start_requests(self):
+        yield JsonRequest(f"https://stores.guitarcenter.com/api/getAutocompleteData", callback=self.parse_autocomplete)
 
     def post_process_feature(self, feature, location):
         feature["branch"] = feature.pop("name")
