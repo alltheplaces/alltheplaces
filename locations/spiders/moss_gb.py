6ce557bbe1567c4736c44d042e5ecd4f49580aa6
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-10-30 14:14:15 +0000

    [Moss GB] Add spider

---
+++ locations/spiders/moss_gb.py
@@ -0,0 +1,18 @@
+import json
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.structured_data_spider import StructuredDataSpider
+
+class MossGBSpider(CrawlSpider, StructuredDataSpider):
+    name = "moss_gb"
+    item_attributes = {
+        "brand": "Moss Bros",
+        "brand_wikidata": "Q6916538",
+        "country": "GB",
+    }
+
+    start_urls = ["https://www.moss.co.uk/store/finder"]
+    rules = [Rule(LinkExtractor(allow=r"/store/([^/]+)$"), callback="parse_sd")]
+    wanted_types = ["Store"]
