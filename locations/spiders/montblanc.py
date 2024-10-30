40a3573edf0e0be0193bf3a6baf4eb1ec108314f
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-10-30 21:23:47 +0530

    opening_hours has been removed to fix spider

--- locations/spiders/montblanc.py
+++ locations/spiders/montblanc.py
@@ -4,7 +4,6 @@ from scrapy import Request, Spider
 from scrapy.http import JsonRequest, Response
 
 from locations.dict_parser import DictParser
-from locations.hours import OpeningHours, day_range
 
 
 class MontblancSpider(Spider):
@@ -51,17 +50,9 @@ class MontblancSpider(Spider):
         for location in response.json()["data"]["publicBoutiques"]:
             if location["type"]["defaultName"] not in ["Boutique", "Outlet"]:
                 continue
-
             item = DictParser.parse(location)
             item["city"] = None
             item["state"] = (location.get("state") or {}).get("defaultName")
             item["country"] = ((location.get("city") or {}).get("country") or {}).get("defaultName")
             item["website"] = "https://stores.montblanc.com/boutique/{}".format(location["id"])
-
-            item["opening_hours"] = OpeningHours()
-            for rule in location["openTimes"]:
-                item["opening_hours"].add_days_range(
-                    day_range(rule["startDay"], rule["endDay"]), rule["startTime"], rule["endTime"]
-                )
-
             yield item
