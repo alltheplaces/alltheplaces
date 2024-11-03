2c9eb793ff6ee98c05cfca50df79da0f71e20acb
Petr Zahradník <junior@zahradnik.cz>
2024-11-04 00:50:14 +0100

    [fio_banka_atm] Drop Google Maps coordinates extraction (#11298)
    * [fio_banka_atm] Drop Google Maps coordinates extraction
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/fio_banka_atm.py
+++ locations/spiders/fio_banka_atm.py
@@ -4,7 +4,6 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
-from locations.google_url import extract_google_position
 from locations.hours import DAYS, DAYS_CZ, DAYS_SK, OpeningHours
 from locations.items import Feature
 
@@ -35,15 +34,13 @@ class FioBankaAtmSpider(CrawlSpider):
             item["city"] = (
                 header.replace("(bankomat i vkladomat)", "").replace("(bankomat aj vkladomat)", "").strip(" -")
             )
-            extract_google_position(item, line1)
             item["street_address"] = line2.xpath(".//strong/text()").get()
 
             apply_category(Categories.ATM, item)
 
-            if "lat" not in item:
-                lat, lon = line1.xpath(".//td[contains(text(), 'GPS')]/text()").get().removeprefix("GPS:").split(",")
-                item["lat"] = self.parse_coordinates(lat)
-                item["lon"] = self.parse_coordinates(lon)
+            lat, lon = line1.xpath(".//td[contains(text(), 'GPS')]/text()").get().removeprefix("GPS:").split(",")
+            item["lat"] = self.parse_coordinates(lat)
+            item["lon"] = self.parse_coordinates(lon)
 
             if "fio.cz" in response.url:
                 item["country"] = "CZ"
