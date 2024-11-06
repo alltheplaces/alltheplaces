e44f82e5f3bced0d36962c76f6b30b3bd115347b
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-11-06 09:34:20 +0530

    Add Lacoste Spider

---
+++ locations/spiders/lacoste.py
@@ -0,0 +1,24 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.user_agents import FIREFOX_LATEST
+
+
+class LacosteSpider(JSONBlobSpider):
+    name = "lacoste"
+    item_attributes = {"brand": "Lacoste", "brand_wikidata": "Q309031"}
+    start_urls = [
+        "https://www.lacoste.com/fr/stores?maxLatitude=90&minLatitude=-90&maxLongitude=180&minLongitude=-180&json=true"
+    ]
+    user_agent = FIREFOX_LATEST
+
+    def extract_json(self, response: Response) -> list:
+        return response.json()["stores"]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = item.pop("addr_full")
+        item["website"] = f'https://www.lacoste.com/fr/stores{feature["url"]}'
+        yield item
