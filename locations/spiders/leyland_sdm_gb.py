4faa8de5d8c0d0986d88fb3a3a4bf3bcc2c84f0d
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-11-08 18:54:06 +0530

    updated amasty_store_locator & updated code for leyland_sdm_gb spider

--- locations/spiders/leyland_sdm_gb.py
+++ locations/spiders/leyland_sdm_gb.py
@@ -1,32 +1,21 @@
-import json
-import re
-from typing import Any
+from typing import Iterable
 
-import scrapy
 from scrapy import Selector
-from scrapy.http import Response
 
-from locations.dict_parser import DictParser
+from locations.items import Feature
+from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
-class LeylandSdmGBSpider(scrapy.Spider):
+class LeylandSdmGBSpider(AmastyStoreLocatorSpider):
     name = "leyland_sdm_gb"
     item_attributes = {"brand": "Leyland SDM", "brand_wikidata": "Q110437963"}
-    start_urls = ["https://leylandsdm.co.uk/amlocator/"]
+    allowed_domains = ["leylandsdm.co.uk"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        data = json.loads(
-            re.search(
-                r"items\":(\[.*\]),\"totalRecords", response.xpath('//*[contains(text(),"jsonLocations")]/text()').get()
-            ).group(1)
-        )
-        for store in data:
-            html_data = Selector(text=store["popup_html"])
-            item = DictParser.parse(store)
-            item["name"] = html_data.xpath("//h3//text()").get()
-            item["street_address"] = html_data.xpath("//text()[4]").get().replace("Address:", "")
-            item["city"] = html_data.xpath("//text()[5]").get().replace("City:", "")
-            item["postcode"] = html_data.xpath("//text()[6]").get().replace("Postcode:", "")
-            item["phone"] = html_data.xpath("//text()[7]").get()
-            item["website"] = html_data.xpath("//@href").get().replace("Maida Vale/", "Maida%20Vale/")
-            yield item
+    def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
+        item["name"] = item["name"].strip()
+        item["street_address"] = popup_html.xpath("//text()[4]").get().replace("Address:", "")
+        item["city"] = popup_html.xpath("//text()[5]").get().replace("City:", "")
+        item["postcode"] = popup_html.xpath("//text()[6]").get().replace("Postcode:", "")
+        item["phone"] = popup_html.xpath("//text()[7]").get()
+        item["website"] = popup_html.xpath("//@href").get().replace("Maida Vale/", "Maida%20Vale/")
+        yield item
