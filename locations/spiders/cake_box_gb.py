a80b5df6899de3055bae89999f3623739b594de2
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-11-10 03:04:45 +0100

    [Cake Box GB] Add spider (#11330)
    * [Cake Box GB] Add spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Category already mapped
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/cake_box_gb.py
@@ -0,0 +1,30 @@
+import json
+import re
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.items import Feature
+
+
+class CakeBoxGBSpider(Spider):
+    name = "cake_box_gb"
+    item_attributes = {"brand": "Cake Box", "brand_wikidata": "Q110057905"}
+    start_urls = ["https://www.cakebox.com/storelocator"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        data = response.xpath('//script[contains(text(), "jsonLocations")]/text()').get()
+        data = re.sub(r"^.*jsonLocations: ", "", data, flags=re.DOTALL)
+        data = re.sub(r",\s+imageLocations.*$", "", data, flags=re.DOTALL)
+        jsondata = json.loads(data)
+        for location in jsondata["items"]:
+            item = Feature()
+            item["ref"] = location["id"]
+            item["lat"] = location["lat"]
+            item["lon"] = location["lng"]
+            sel = location["popup_html"]
+            item["city"] = re.search(r"(?<=City: )[^<]+", sel).group(0)
+            item["street_address"] = re.search(r"(?<=Address: )[^<]+", sel).group(0)
+            item["postcode"] = re.search(r"(?<=Zip: )[^<]+", sel).group(0)
+            yield item
