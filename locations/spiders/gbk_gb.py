96711eb035272e0e37a07b0f57726a74fc572014
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-11-10 19:00:12 +0000

    Update gbk_gb.py

--- locations/spiders/gbk_gb.py
+++ locations/spiders/gbk_gb.py
@@ -1,7 +1,7 @@
 from locations.spiders.slim_chickens_gb import SlimChickensGBSpider
 
 
-class GBKGBSpider(SlimChickensGBSpider):
+class GbkGBSpider(SlimChickensGBSpider):
     name = "gbk_gb"
     item_attributes = {"brand": "GBK", "brand_wikidata": "Q5588445"}
     start_urls = ["https://www.gbk.co.uk/find-your-gbk"]
