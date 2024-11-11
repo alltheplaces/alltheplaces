c37312bb20e86042599422f3f5b5e08ab9ef70fe
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-11-11 13:50:48 +0100

    [Popeyes GB] Add spider (#11338)
    * [Popeyes GB] Add spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add coordinates
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add website
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Use `pop` to remove name
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

---
+++ locations/spiders/popeyes_gb.py
@@ -0,0 +1,22 @@
+from typing import Any
+from urllib.parse import urljoin
+
+from scrapy.http import Response
+from scrapy.spiders import Spider
+
+from locations.dict_parser import DictParser
+
+
+class PopeyesGBSpider(Spider):
+    name = "popeyes_gb"
+    item_attributes = {"brand": "Popeyes", "brand_wikidata": "Q1330910"}
+
+    start_urls = ["https://pe-uk-ordering-api-fd-eecsdkg6btfeg0cc.z01.azurefd.net/api/v2/restaurants"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["data"]:
+            item = DictParser.parse(location)
+            item["geometry"] = location["storeLocation"]["coordinates"]
+            item["branch"] = item.pop("name")
+            item["website"] = urljoin("https://popeyesuk.com/restaurants/", location["slug"])
+            yield item
