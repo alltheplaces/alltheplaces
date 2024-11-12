2642e7550174b550f4f4645f781c454f023f3c81
David Hicks <david@hicks.id.au>
2024-11-13 02:26:17 +1100

    woolworths_nz: add category

--- locations/spiders/woolworths_nz.py
+++ locations/spiders/woolworths_nz.py
@@ -1,6 +1,7 @@
 from scrapy import Spider
 from scrapy.http import JsonRequest
 
+from locations.categories import Categories
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_FULL, OpeningHours
 from locations.user_agents import BROWSER_DEFAULT
@@ -9,7 +10,7 @@ from locations.user_agents import BROWSER_DEFAULT
 class WoolworthsNZSpider(Spider):
     name = "woolworths_nz"
     METRO = {"brand": "Woolworths Metro", "brand_wikidata": "Q123699264"}
-    item_attributes = {"brand": "Woolworths", "brand_wikidata": "Q5176845"}
+    item_attributes = {"brand": "Woolworths", "brand_wikidata": "Q5176845", "extras": Categories.SHOP_SUPERMARKET.value}
     allowed_domains = ["api.cdx.nz"]
     start_urls = ["https://api.cdx.nz/site-location/api/v1/sites?latitude=-42&longitude=174&maxResults=10000"]
     user_agent = BROWSER_DEFAULT
