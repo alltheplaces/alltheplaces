d2ec31aabbd1b022a0e619377e97750ba20e0c3b
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-11-12 17:09:28 +0000

    [Cancer Research UK] Fix spider (#11355)

--- locations/spiders/cancer_research_uk_gb.py
+++ locations/spiders/cancer_research_uk_gb.py
@@ -1,29 +1,61 @@
-from typing import Any
+from typing import Any, Iterable
 
-from scrapy.http import Response
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
+from scrapy.spiders import Spider
 
 from locations.items import Feature
 
 
 # Sitemap is incomplete - 534 shops but only 218 on sitemap
 # No location available
-class CancerResearchUkGBSpider(CrawlSpider):
+class CancerResearchUkGBSpider(Spider):
     name = "cancer_research_uk_gb"
     item_attributes = {"brand": "Cancer Research UK", "brand_wikidata": "Q326079"}
-    allowed_domains = ["cancerresearchuk.org"]
-    start_urls = ["https://www.cancerresearchuk.org/get-involved/find-a-shop"]
-    rules = [
-        Rule(LinkExtractor(r"/find-a-shop/[^/]+"), callback="parse"),
-        Rule(LinkExtractor(r"/find-a-shop\?page=\d+$"), follow=True),
-    ]
+    aallowed_domains = ["cancerresearchuk.org"]
+
+    def make_request(self, page: int) -> JsonRequest:
+        return JsonRequest(
+            url="https://xss-content.ent.eu-west-1.aws.found.io/api/as/v1/engines/shops/search.json",
+            data={
+                "query": "",
+                "result_fields": {
+                    "title": {"raw": {}},
+                    "url": {"raw": {}},
+                    "postcode": {"raw": {}},
+                    "shop_address": {"raw": {}},
+                    "shop_phone_number": {"raw": {}},
+                    "document_type": {"raw": {}},
+                    "content_type": {"raw": {}},
+                    "postcode_location": {"raw": {}},
+                },
+                "search_fields": {
+                    "postcode": {},
+                    "title": {},
+                    "shop_address": {},
+                    "shop_phone_number": {},
+                    "postcode_location": {},
+                },
+                "page": {"size": 100, "current": page},
+            },
+            headers={"Authorization": "Bearer search-yxrzepvos3f259zqovdykpmf"},
+        )
+
+    def start_requests(self) -> Iterable[Request]:
+        yield self.make_request(1)
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        if addr_full := response.xpath('//div[@class="panel-pane pane-custom pane-1 address"]/p/text()').get():
+        for location in response.json()["results"]:
             item = Feature()
-            item["addr_full"] = addr_full
-            item["postcode"] = addr_full.split(", ")[-1]
-            item["country"] = "GB"
-            item["ref"] = item["website"] = response.url
+            item["addr_full"] = location["shop_address"]["raw"]
+            item["phone"] = location.get("shop_phone_number", {}).get("raw")
+            item["postcode"] = location["postcode"]["raw"]
+            item["branch"] = location["title"]["raw"]
+            item["website"] = location["url"]["raw"]
+            item["ref"] = location["id"]["raw"]
+
             yield item
+
+        page = response.json()["meta"]["page"]
+        if page["current"] <= page["total_pages"]:
+            yield self.make_request(page["current"] + 1)
