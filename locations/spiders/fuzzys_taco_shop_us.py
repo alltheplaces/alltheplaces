5addcd7df6f943a6e5b57bbbdb81658555434ad1
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-11-13 14:29:08 +0530

    Fix Fuzzys Taco Shop US Spider

--- locations/spiders/fuzzys_taco_shop_us.py
+++ locations/spiders/fuzzys_taco_shop_us.py
@@ -1,3 +1,7 @@
+import re
+from typing import Any
+
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
@@ -11,13 +15,19 @@ class FuzzysTacoShopUSSpider(CrawlSpider, StructuredDataSpider):
     allowed_domains = ["fuzzystacoshop.com"]
     start_urls = ["https://fuzzystacoshop.com/locations/list/"]
     rules = [
-        Rule(LinkExtractor(allow=r"https:\/\/fuzzystacoshop\.com\/locations\/list\/.+")),
         Rule(
-            LinkExtractor(allow=r"https:\/\/fuzzystacoshop\.com\/locations\/(?!list\/).+"),
-            callback="parse_sd",
-            follow=False,
+            LinkExtractor(allow=r"/list/[a-z]{2}/$"),
+            callback="parse",
         ),
     ]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        # POI page url is generated dynamically through javascript code, not available for CrawlSpider.
+        for link in set(re.findall(r"onclick=\"window\.open\(\'(.+?)\'", response.text)):
+            if "list" in link:  # city page having multiple locations
+                yield response.follow(link, callback=self.parse)
+            else:  # POI page
+                yield response.follow(link, callback=self.parse_sd)
 
     def post_process_item(self, item, response, ld_data):
         item["opening_hours"] = LinkedDataParser.parse_opening_hours(ld_data, time_format="%H:%M:%S")
