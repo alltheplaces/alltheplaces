32c5b89bb0e1dadb53d50ebdf57e6d22246bf4e7
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2024-11-14 14:15:01 +0530

    [Debra GB] Fix Spider (#11383)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/debra_gb.py
+++ locations/spiders/debra_gb.py
@@ -5,24 +5,26 @@ from scrapy.spiders import SitemapSpider
 
 from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.pipelines.address_clean_up import clean_address
 
 
 class DebraGBSpider(SitemapSpider):
     name = "debra_gb"
     item_attributes = {"brand": "Debra", "brand_wikidata": "Q104535435"}
     sitemap_urls = ["https://www.debra.org.uk/sitemap.xml"]
-    sitemap_rules = [(r"uk/charity-shop/[-\w]+", "parse")]
+    sitemap_rules = [(r"/charity-shop/\w+", "parse")]
+    sitemap_follow = ["shop"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
-        item["branch"] = response.xpath('//meta[@property="og:title"]/@content').get()
-        item["addr_full"] = response.xpath("//address/text()").get()
+        item["branch"] = response.xpath("//h1/text()").get()
+        item["addr_full"] = clean_address(response.xpath('//*[contains(@class,"details-address")]/text()').get())
+        item["phone"] = response.xpath('//*[contains(@class,"details-phone")]/text()').get()
+        item["email"] = response.xpath('//*[contains(@class,"details-email")]/text()').get()
         item["lat"] = response.xpath("//@data-lat").get()
         item["lon"] = response.xpath("//@data-lng").get()
         item["ref"] = item["website"] = response.url
-        item["email"] = response.xpath('//a[contains(@href,"mailto:")]/text()').get()
-        item["phone"] = response.xpath('//a[contains(@href,"tel:")]/text()').get()
-        if hours := response.xpath('//*[contains(@class,"details-opening-times")]/text()').get():
-            item["opening_hours"] = OpeningHours()
-            item["opening_hours"].add_ranges_from_string(hours)
+        item["opening_hours"] = OpeningHours()
+        day_time_string = response.xpath('//*[@class="single-shop__details-opening-times"]/text()').get()
+        item["opening_hours"].add_ranges_from_string(day_time_string)
         yield item
