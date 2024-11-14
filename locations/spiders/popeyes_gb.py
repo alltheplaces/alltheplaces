2ba9470dab337ccb2dfb80ee5bc5127ef84ce4c9
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-11-14 16:16:14 +0000

    [Popeyes GB] Fix coords (#11385)

--- locations/spiders/popeyes_gb.py
+++ locations/spiders/popeyes_gb.py
@@ -4,6 +4,7 @@ from urllib.parse import urljoin
 from scrapy.http import Response
 from scrapy.spiders import Spider
 
+from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
 
 
@@ -16,7 +17,14 @@ class PopeyesGBSpider(Spider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["data"]:
             item = DictParser.parse(location)
-            item["geometry"] = location["storeLocation"]["coordinates"]
+            item["lat"] = location["storeLocation"]["coordinates"]["latitude"]
+            item["lon"] = location["storeLocation"]["coordinates"]["longitude"]
             item["branch"] = item.pop("name")
             item["website"] = urljoin("https://popeyesuk.com/restaurants/", location["slug"])
+
+            apply_yes_no(Extras.DRIVE_THROUGH, item, location["hasDriveThru"] is True)
+            apply_yes_no(Extras.TOILETS, item, location["hasToilets"] is True or location["hasDisabledToilets"] is True)
+            apply_yes_no(Extras.TOILETS_WHEELCHAIR, item, location["hasDisabledToilets"] is True)
+            apply_yes_no(Extras.WHEELCHAIR, item, location["hasDisabledAccess"] is True)
+            apply_yes_no(Extras.WIFI, item, location["hasWiFi"] is True)
             yield item
