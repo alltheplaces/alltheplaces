43e6d67759c5f189bc1e34a7eef9a7c03a1629b2
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-11-15 20:29:09 +0000

    Add category

--- locations/spiders/change_group.py
+++ locations/spiders/change_group.py
@@ -4,6 +4,8 @@ from typing import Any
 from scrapy.http import Response
 from scrapy.spiders import Spider
 
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_FULL, OpeningHours
 
@@ -14,37 +16,38 @@ class ChangeGroupGBSpider(Spider):
     start_urls = ["https://uk.changegroup.com/slatwall/?slatAction=changeGroup:main.globalBranchData"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        data = response.text.encode("utf-8")
+        data=response.text.encode("utf-8")
         for country in json.loads(data)["countries"]:
             for region in country["regions"]:
                 for location in region["branches"]:
                     item = DictParser.parse(location)
+                    apply_category(Categories.BUREAU_DE_CHANGE, item)
                     oh = OpeningHours()
                     for day in DAYS_FULL:
-                        open = location["openingHours"][day.lower()]["open"].replace(" am", "").strip()
-                        close = location["openingHours"][day.lower()]["close"].replace(" am", "").strip()
-                        if open == "24/7":
+                        open = location["openingHours"][day.lower()]["open"].replace(" am","").strip()
+                        close = location["openingHours"][day.lower()]["close"].replace(" am","").strip()
+                        if open == '24/7':
                             item["opening_hours"] = "24/7"
                         elif open == "24 Hours":
-                            oh = "24/7"
+                            oh="24/7"
                         elif "to" in open:
-                            open1, close1 = open.split(" to ")
+                            open1,close1=open.split(" to ")
                             if not ("." or ":") in open1:
                                 open1 = open1 + ":00"
                             if not ("." or ":") in close1:
                                 close1 = close1 + ":00"
-                            oh.add_range(day, open1.replace(".", ":"), close1.replace(".", ":"))
+                            oh.add_range(day, open1.replace(".",":"), close1.replace(".",":"))
                             if "to" in close:
-                                open2, close2 = close.split(" to ")
+                                open2,close2=close.split(" to ")
                                 if not ("." or ":") in open2:
                                     open2 = open2 + ":00"
                                     close2 = close2 + ":00"
-                                oh.add_range(day, open2.replace(".", ":"), close2.replace(".", ":"))
+                                oh.add_range(day, open2.replace(".",":"), close2.replace(".",":"))
                         elif close:
                             if "closed" in open.lower():
                                 continue
-                            openhour, openminutes = open.replace(".", ":").replace(" pm", "").split(":")
-                            closehour, closeminutes = close.replace(".", ":").replace(" pm", "").split(":")
+                            openhour,openminutes=open.replace(".",":").replace(" pm","").split(":")
+                            closehour,closeminutes=close.replace(".",":").replace(" pm","").split(":")
                             if "pm" in open:
                                 openhour = int(openhour) + 12
                             if "pm" in close:
@@ -53,10 +56,8 @@ class ChangeGroupGBSpider(Spider):
                                 continue
                             if int(openhour) > 23 or int(closehour) > 23:
                                 continue
-                            oh.add_range(
-                                day, str(openhour) + ":" + str(openminutes), str(closehour) + ":" + str(closeminutes)
-                            )
+                            oh.add_range(day, str(openhour) + ":" + str(openminutes), str(closehour) + ":" + str(closeminutes))
                         else:
                             continue
-                    item["opening_hours"] = oh
+                    item["opening_hours"]=oh
                     yield item
