7f8ed4763ecb0490275c3ce012f95daad124da27
hfs <538109+hfs@users.noreply.github.com>
2024-11-18 21:22:30 +0100

    [hair_express_de] Fix spider name
    to match the brand and the class name.

--- locations/spiders/hair_express_de.py
+++ locations/spiders/hair_express_de.py
@@ -2,6 +2,6 @@ from locations.storefinders.klier_hair_group import KlierHairGroupSpider
 
 
 class HairExpressDESpider(KlierHairGroupSpider):
-    name = "hairexpress_de"
+    name = "hair_express_de"
     start_urls = ["https://www.hairexpress-friseur.de/salons/"]
     item_attributes = {"brand": "Hair Express", "brand_wikidata": "Q57550814"}
