498c6d4656bcdc3e1032f92a0dddeb9180a02109
Ian Dees <ian.dees@gmail.com>
2024-11-19 07:35:10 -0600

    Try without proxy first, and fix pre-commit check

--- locations/spiders/busy_bees_gb.py
+++ locations/spiders/busy_bees_gb.py
@@ -8,9 +8,8 @@ class BusyBeesGBSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Busy Bees", "brand_wikidata": "Q28134563"}
     sitemap_urls = ["https://www.busybeeschildcare.co.uk/sitemap.xml"]
     sitemap_rules = [("", "parse_sd")]
-    requires_proxy = True
 
     def sitemap_filter(self, entries):
         for entry in entries:
-            if "/nursery" in entry["loc"] and not "meet-the-team" in entry["loc"]:
+            if "/nursery" in entry["loc"] and "meet-the-team" not in entry["loc"]:
                 yield entry
