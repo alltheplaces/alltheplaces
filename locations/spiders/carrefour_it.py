6329258c18fb870970dff20912db1108c64ec23a
Marco Ciotola <848222@stud.unive.it>
2024-11-22 23:34:12 +0100

    chore: make carrefour_it a JSONBlobSpider

--- locations/spiders/carrefour_it.py
+++ locations/spiders/carrefour_it.py
@@ -1,7 +1,5 @@
-from scrapy import Spider
-
-from locations.dict_parser import DictParser
 from locations.hours import CLOSED_IT, DAYS_IT, OpeningHours
+from locations.json_blob_spider import JSONBlobSpider
 from locations.spiders.carrefour_fr import (
     CARREFOUR_EXPRESS,
     CARREFOUR_MARKET,
@@ -10,9 +8,10 @@ from locations.spiders.carrefour_fr import (
 )
 
 
-class CarrefourITSpider(Spider):
+class CarrefourITSpider(JSONBlobSpider):
     name = "carrefour_it"
     start_urls = ["https://www.carrefour.it/on/demandware.store/Sites-carrefour-IT-Site/it_IT/StoreLocator-GetAll"]
+    locations_key = "stores"
 
     brands = {
         "iper": CARREFOUR_SUPERMARKET,
@@ -20,26 +19,24 @@ class CarrefourITSpider(Spider):
         "express": CARREFOUR_EXPRESS,
     }
 
-    def parse(self, response):
-        for location in response.json()["stores"]:
-            item = DictParser.parse(location)
-            item["name"] = location["Insegna"] + " " + location["Descrizione"]
-            item["street_address"] = location["Indirizzo"]
-            item["city"] = location["Citta"]
-            item["state"] = location["Provincia"]
-            item["postcode"] = location["CAP"]
-            item["website"] = "https://www.carrefour.it" + location["Url"]
-            if not self.brands.get(location["Type"]):
-                self.crawler.stats.inc_value(f'atp/carrefour_it/unknown_brand/{location["Type"]}')
-                continue
+    def post_process_item(self, item, response, location):
+        item["name"] = location["Insegna"] + " " + location["Descrizione"]
+        item["street_address"] = location["Indirizzo"]
+        item["city"] = location["Citta"]
+        item["state"] = location["Provincia"]
+        item["postcode"] = location["CAP"]
+        item["website"] = "https://www.carrefour.it" + location["Url"]
+        if not self.brands.get(location["Type"]):
+            self.crawler.stats.inc_value(f'atp/carrefour_it/unknown_brand/{location["Type"]}')
+            return
 
-            parse_brand_and_category_from_mapping(item, location["Type"], self.brands)
+        parse_brand_and_category_from_mapping(item, location["Type"], self.brands)
 
-            item["opening_hours"] = OpeningHours()
-            for day_name, day_hours in location["Orari"].items():
-                item["opening_hours"].add_ranges_from_string(
-                    f"{day_name} {day_hours}",
-                    days=DAYS_IT,
-                    closed=CLOSED_IT,
-                )
-            yield item
+        item["opening_hours"] = OpeningHours()
+        for day_name, day_hours in location["Orari"].items():
+            item["opening_hours"].add_ranges_from_string(
+                f"{day_name} {day_hours}",
+                days=DAYS_IT,
+                closed=CLOSED_IT,
+            )
+        yield item
