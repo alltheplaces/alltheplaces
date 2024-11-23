2b07abf41c02bb0195ccea8c4eb9bb90d262da02
Joe Dixon <mjoe999@gmail.com>
2024-11-23 14:42:47 +0000

    [utah_dot_us] add 1901 cameras in Utah

--- locations/spiders/nevada_dot_us.py
+++ locations/spiders/nevada_dot_us.py
@@ -1,26 +1,25 @@
 from typing import Iterable
+from urllib.parse import urlparse
 
 from scrapy.http import Response
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
 class NevadaDotUSSpider(JSONBlobSpider):
     name = "nevada_dot_us"
-    item_attributes = {
-        "operator": "Nevada DOT",
-        "operator_wikidata": "Q886390",
-        "extras": Categories.SURVEILLANCE_CAMERA.value,
-    }
+    item_attributes = {"operator": "Nevada DOT", "operator_wikidata": "Q886390"}
     start_urls = ["https://www.nvroads.com/map/mapIcons/Cameras"]
     locations_key = "item2"
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["lat"], item["lon"] = feature["location"]
-        item["image"] = "https://www.nvroads.com/map/Cctv/" + item["ref"]
-        item["website"] = "https://www.nvroads.com/map"
+        url = urlparse(self.start_urls[0])
+        item["website"] = url.scheme + "://" + url.netloc
+        item["image"] = item["website"] + "/map/Cctv/" + item["ref"]
         item["name"] = "Traffic camera"
         item["extras"]["camera:type"] = "fixed"
+        apply_category(Categories.SURVEILLANCE_CAMERA, item)
         yield item
