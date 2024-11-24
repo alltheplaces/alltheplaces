886d30430bdbc16438a58fa45ae84532bb1e5477
Joe Dixon <mjoe999@gmail.com>
2024-11-24 16:30:38 +0000

    [DOT spiders] refactor to ise common code

--- locations/spiders/georgia_dot_us.py
+++ locations/spiders/georgia_dot_us.py
@@ -1,26 +1,7 @@
-from typing import Iterable
-
-from scrapy.http import Response
-
-from locations.categories import Categories
-from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.nevada_dot_us import NevadaDotUSSpider
 
 
-class GeorgiaDotUSSpider(JSONBlobSpider):
+class GeorgiaDotUSSpider(NevadaDotUSSpider):
     name = "georgia_dot_us"
-    item_attributes = {
-        "operator": "Georgia DOT",
-        "operator_wikidata": "Q944993",
-        "extras": Categories.SURVEILLANCE_CAMERA.value,
-    }
+    item_attributes = {"operator": "Georgia DOT", "operator_wikidata": "Q944993"}
     start_urls = ["https://511ga.org/map/mapIcons/Cameras"]
-    locations_key = "item2"
-
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["lat"], item["lon"] = feature["location"]
-        item["image"] = "https://511ga.org/map/Cctv/" + item["ref"]
-        item["website"] = "https://511ga.org/map"
-        item["name"] = "Traffic camera"
-        item["extras"]["camera:type"] = "fixed"
-        yield item
