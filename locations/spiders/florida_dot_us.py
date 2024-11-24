9c4e53416bbb743c13a41274f7423aca7a3f6a02
Joe Dixon <mjoe999@users.noreply.github.com>
2024-11-24 18:01:59 +0000

    [DOT spiders] refactor to use common code (#11480)
    * [DOT spiders] refactor to ise common code
    
    * Update Wisconsin too
    
    ---------
    
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/florida_dot_us.py
+++ locations/spiders/florida_dot_us.py
@@ -1,26 +1,7 @@
-from typing import Iterable
-
-from scrapy.http import Response
-
-from locations.categories import Categories
-from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.nevada_dot_us import NevadaDotUSSpider
 
 
-class FloridaDotUSSpider(JSONBlobSpider):
+class FloridaDotUSSpider(NevadaDotUSSpider):
     name = "florida_dot_us"
-    item_attributes = {
-        "operator": "Florida DOT",
-        "operator_wikidata": "Q3074270",
-        "extras": Categories.SURVEILLANCE_CAMERA.value,
-    }
+    item_attributes = {"operator": "Florida DOT", "operator_wikidata": "Q3074270"}
     start_urls = ["https://www.fl511.com/map/mapIcons/Cameras"]
-    locations_key = "item2"
-
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["lat"], item["lon"] = feature["location"]
-        item["image"] = "https://www.fl511.com/map/Cctv/" + item["ref"]
-        item["website"] = "https://www.fl511.com/map"
-        item["name"] = "Traffic camera"
-        item["extras"]["camera:type"] = "fixed"
-        yield item
--- locations/spiders/wisconsin_dot_us.py
+++ locations/spiders/wisconsin_dot_us.py
@@ -1,26 +1,7 @@
-from typing import Iterable
-
-from scrapy.http import Response
-
-from locations.categories import Categories
-from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.nevada_dot_us import NevadaDotUSSpider
 
 
-class WisconsinDotUSSpider(JSONBlobSpider):
+class WisconsinDotUSSpider(NevadaDotUSSpider):
     name = "wisconsin_dot_us"
-    item_attributes = {
-        "operator": "Wisconsin DOT",
-        "operator_wikidata": "Q8027162",
-        "extras": Categories.SURVEILLANCE_CAMERA.value,
-    }
+    item_attributes = {"operator": "Wisconsin DOT", "operator_wikidata": "Q8027162"}
     start_urls = ["https://511wi.gov/map/mapIcons/Cameras"]
-    locations_key = "item2"
-
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["lat"], item["lon"] = feature["location"]
-        item["image"] = "https://511wi.gov/map/Cctv/" + item["ref"]
-        item["website"] = "https://511wi.gov/"
-        item["name"] = "Traffic camera"
-        item["extras"]["camera:type"] = "fixed"
-        yield item
