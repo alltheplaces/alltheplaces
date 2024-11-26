3e829175166cb4072951660b536768db6dca9e84
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-11-26 17:58:11 +0530

    Add more location details

--- locations/spiders/davids_tea.py
+++ locations/spiders/davids_tea.py
@@ -26,6 +26,11 @@ class DavidsTeaSpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         store_info = Selector(text=item.pop("addr_full"))
+        item["branch"] = store_info.xpath('//*[@class="name"]/text()').get("").strip()
         item["street_address"] = clean_address(store_info.xpath('//*[contains(@class, "address")]/text()').getall())
+        item["city"] = store_info.xpath('//*[@class="city"]/text()').get()
+        item["state"] = store_info.xpath('//*[@class="prov_state"]/text()').get()
+        item["postcode"] = store_info.xpath('//*[@class="postal_zip"]/text()').get()
+        item["country"] = store_info.xpath('//*[@class="country"]/text()').get()
         item["website"] = response.url
         yield item
