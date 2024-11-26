ca4d869d4d07b3cd34d9198462b2e785f83d0ea7
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-11-26 17:37:27 +0530

    Fix Davids Tea spider

--- locations/spiders/davids_tea.py
+++ locations/spiders/davids_tea.py
@@ -1,17 +1,31 @@
-from scrapy.spiders import SitemapSpider
+import json
+import re
+from typing import Iterable
 
-from locations.structured_data_spider import StructuredDataSpider
+from scrapy import Selector
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.pipelines.address_clean_up import clean_address
 
 
-class DavidsTeaSpider(SitemapSpider, StructuredDataSpider):
+class DavidsTeaSpider(JSONBlobSpider):
     name = "davids_tea"
     item_attributes = {"brand": "David's Tea", "brand_wikidata": "Q3019129"}
-    allowed_domains = [
-        "locations.davidstea.com",
-    ]
-    sitemap_urls = ["https://locations.davidstea.com/robots.txt"]
-    sitemap_rules = [("", "parse_sd")]
+    start_urls = ["https://davidstea.com/apps/store-locator/"]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
+    def extract_json(self, response: Response) -> list:
+        return [
+            json.loads(store)
+            for store in re.findall(
+                r"markersCoords.push\(({\".+?)\);",
+                response.xpath('//script[contains(text(),"markersCoords")]/text()').get(""),
+            )
+        ]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        store_info = Selector(text=item.pop("addr_full"))
+        item["street_address"] = clean_address(store_info.xpath('//*[contains(@class, "address")]/text()').getall())
         item["website"] = response.url
         yield item
