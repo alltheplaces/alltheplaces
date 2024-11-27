3a1042e8a2a2b6633f8f8c75e2b73f767ccce0c2
Marco Ciotola <848222@stud.unive.it>
2024-11-27 02:38:28 +0100

    fix: simplify and improve fit_active
    Fixes #10574

--- locations/spiders/fit_active.py
+++ locations/spiders/fit_active.py
@@ -1,42 +1,36 @@
-import json
+from locations.hours import DAYS_IT, CLOSED_IT, OpeningHours
+from locations.items import set_social_media
+from locations.json_blob_spider import JSONBlobSpider
 
-import scrapy
-
-from locations.items import Feature
-
-
-class FitActiveSpider(scrapy.Spider):
+class FitActiveSpider(JSONBlobSpider):
     name = "fit_active"
     item_attributes = {"brand": "FitActive", "brand_wikidata": "Q123807531"}
-    start_urls = ["https://www.fitactive.it/Club/Club"]
+    start_urls = ["https://www.fitactive.it/Club/ProvaDellaMappa"]
+    skip_auto_cc_domain = True # all URLs are .it, but not all places are in Italy
 
-    def parse(self, response):
-        # Website and phone number are in a separate section of the website
-        website = {}
-        phone = {}
-        for item in response.xpath('//ul[@class="menu-club"]/li'):
-            link = item.xpath('descendant::a[not(contains(@href, "tel:"))]')
-            name = link.xpath("descendant::text()").extract_first()
-            website[name] = link.attrib["href"]
-            phone[name] = item.xpath('descendant::a[contains(@href, "tel:")]/text()').extract_first()
+    def pre_process_data(self, location):
+        del location["region"]
+        del location["state"]
+        website_path = location["title"].removeprefix("FitActive").strip().replace(" ", "")
+        location["website"] = f"https://www.fitactive.it/i-club/{website_path}.php"
+        location["branch"] = location["title"]
+        location["title"] = "FitActive"
 
-        # The main information is in a JavaScript call
-        javascript = response.xpath(
-            '//script[@type="text/javascript" and contains(text(), "mainMap(")]/text()'
-        ).re_first(r"mainMap\((.*)\);")
-        map_data = json.loads(javascript)
-        for shop in map_data:
-            name = shop["title"]
-            yield Feature(
-                {
-                    "ref": shop["id"],
-                    "name": name,
-                    "addr_full": shop["address"].strip(),
-                    "country": shop["state"],
-                    "lat": shop["latitude"],
-                    "lon": shop["longitude"],
-                    "email": shop["email"],
-                    "phone": phone[name],
-                    "website": response.urljoin(website[name]),
-                }
-            )
+    def post_process_item(self, item, response, location):
+        if img_path := location.get("img"):
+            item["image"] = f"https://www.fitactive.it/{img_path.lstrip('/')}"
+        item["branch"] = location["branch"]
+        if fb := location.get("fbLinkString"):
+            set_social_media(item, "facebook", fb.strip())
+        if insta := location.get("linkInsta"):
+            set_social_media(item, "instagram", insta.strip())
+        if start := location.get("dataApertura"):
+            if start != "SOON":
+                item["extras"]["start_date"] = start.strip()
+        item["opening_hours"] = "24/7"
+        if oph := location["orari"]:
+            oh = OpeningHours()
+            oh.add_ranges_from_string(oph, days=DAYS_IT, closed=CLOSED_IT)
+            item["extras"]["opening_hours:office"] = oh.as_opening_hours()
+        yield item
+
