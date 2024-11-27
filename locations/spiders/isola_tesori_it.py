eb11e95bf8d3834fc9a31d6a0c135485ee0dc445
Marco Ciotola <848222@stud.unive.it>
2024-11-27 15:01:52 +0100

    feat: add opening hours parsing

--- locations/spiders/isola_tesori_it.py
+++ locations/spiders/isola_tesori_it.py
@@ -1,5 +1,10 @@
 from locations.categories import Categories
+from locations.hours import CLOSED_IT, DAYS_IT, NAMED_DAY_RANGES_IT, NAMED_TIMES_IT, OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
+
+
+def clean_strings(iterator):
+    return list(filter(bool, map(str.lower, map(str.strip, iterator))))
 
 
 class IsolaTesoriITSpider(JSONBlobSpider):
@@ -17,4 +22,20 @@ class IsolaTesoriITSpider(JSONBlobSpider):
         item["branch"] = item["name"]
         item["name"] = self.item_attributes["brand"]
         item["website"] = f"https://www.isoladeitesori.it/store/{item['website']}.html"
+        yield response.follow(item["website"], cb_kwargs={"item": item}, callback=self.parse_store_oph)
+
+    def parse_store_oph(self, response, item):
+        hours = clean_strings(response.css(".store-hours td::text").getall())
+        oh = OpeningHours()
+        for day, hour in zip(hours[0::2], hours[1::2]):
+            oh.add_ranges_from_string(
+                f"{day} {hour}",
+                days=DAYS_IT,
+                named_day_ranges=NAMED_DAY_RANGES_IT,
+                named_times=NAMED_TIMES_IT,
+                closed=CLOSED_IT,
+            )
+        if oh:
+            item["opening_hours"] = oh
+
         yield item
