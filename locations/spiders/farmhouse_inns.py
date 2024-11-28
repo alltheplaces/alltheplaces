69d2c8e24a3d3b6e857a212086c5ff56a0434d44
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-11-28 13:08:17 +0530

    updated sitemap_url and sitemap_rules to fix spider

--- locations/spiders/farmhouse_inns.py
+++ locations/spiders/farmhouse_inns.py
@@ -1,17 +1,15 @@
 from scrapy.spiders import SitemapSpider
 
-from locations.linked_data_parser import LinkedDataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class FarmhouseInnsSpider(SitemapSpider):
+class FarmhouseInnsSpider(SitemapSpider, StructuredDataSpider):
     name = "farmhouse_inns"
     item_attributes = {
         "brand": "Farmhouse Inns",
         "brand_wikidata": "Q105504972",
         "country": "GB",
     }
-    sitemap_urls = ["https://www.farmhouseinns.co.uk/xml-sitemap"]
-    sitemap_rules = [(r"https:\/\/www\.farmhouseinns\.co\.uk\/pubs\/([-\w]+)\/([-\w]+)\/$", "parse")]
-
-    def parse(self, response):
-        yield LinkedDataParser.parse(response, "BarOrPub")
+    sitemap_urls = ["https://www.farmhouseinns.co.uk/sitemap.xml"]
+    sitemap_rules = [(r"https:\/\/www\.farmhouseinns\.co\.uk\/pubs\/([-\w]+)\/([-\w]+)$", "parse_sd")]
+    wanted_types = ["BarOrPub"]
