02f091d2501740eddec98e9a7229e0d9bcf8bd90
Cj Malone <CjMalone@mail.com>
2024-11-28 14:43:45 +0000

    Rename spider

--- locations/spiders/farmhouse_inns_gb.py
+++ locations/spiders/farmhouse_inns_gb.py
@@ -4,12 +4,8 @@ from locations.structured_data_spider import StructuredDataSpider
 
 
 class FarmhouseInnsSpider(SitemapSpider, StructuredDataSpider):
-    name = "farmhouse_inns"
-    item_attributes = {
-        "brand": "Farmhouse Inns",
-        "brand_wikidata": "Q105504972",
-        "country": "GB",
-    }
+    name = "farmhouse_inns_gb"
+    item_attributes = {"brand": "Farmhouse Inns", "brand_wikidata": "Q105504972"}
     sitemap_urls = ["https://www.farmhouseinns.co.uk/sitemap.xml"]
-    sitemap_rules = [(r"https:\/\/www\.farmhouseinns\.co\.uk\/pubs\/([-\w]+)\/([-\w]+)$", "parse_sd")]
+    sitemap_rules = [(r"/pubs/[^/]+/[^/]+$", "parse_sd")]
     wanted_types = ["BarOrPub"]
