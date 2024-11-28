db9c3e02ff5f1818639a790bc5049017887728e1
Cj Malone <CjMalone@mail.com>
2024-11-28 15:30:56 +0000

    apply_category

--- locations/spiders/isola_tesori_it.py
+++ locations/spiders/isola_tesori_it.py
@@ -1,4 +1,4 @@
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.hours import CLOSED_IT, DAYS_IT, NAMED_DAY_RANGES_IT, NAMED_TIMES_IT, OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -9,11 +9,7 @@ def clean_strings(iterator):
 
 class IsolaTesoriITSpider(JSONBlobSpider):
     name = "isola_tesori_it"
-    item_attributes = {
-        "brand": "L'Isola dei Tesori",
-        "brand_wikidata": "Q108578003",
-        "extras": Categories.SHOP_PET.value,
-    }
+    item_attributes = {"brand": "L'Isola dei Tesori", "brand_wikidata": "Q108578003"}
     start_urls = ["https://www.isoladeitesori.it/on/demandware.store/Sites-idt-it-Site/it_IT/Stores-FindStores"]
     locations_key = "stores"
 
@@ -22,6 +18,9 @@ class IsolaTesoriITSpider(JSONBlobSpider):
         item["branch"] = item["name"]
         item["name"] = self.item_attributes["brand"]
         item["website"] = f"https://www.isoladeitesori.it/store/{item['website']}.html"
+
+        apply_category(Categories.SHOP_PET, item)
+
         yield response.follow(item["website"], cb_kwargs={"item": item}, callback=self.parse_store_oph)
 
     def parse_store_oph(self, response, item):
