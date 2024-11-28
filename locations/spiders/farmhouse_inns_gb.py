162517e3267e40791216d6cfc56189c3cbb1bc47
Cj Malone <CjMalone@mail.com>
2024-11-28 14:48:59 +0000

    Rename class

--- locations/spiders/farmhouse_inns_gb.py
+++ locations/spiders/farmhouse_inns_gb.py
@@ -3,7 +3,7 @@ from scrapy.spiders import SitemapSpider
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class FarmhouseInnsSpider(SitemapSpider, StructuredDataSpider):
+class FarmhouseInnsGBSpider(SitemapSpider, StructuredDataSpider):
     name = "farmhouse_inns_gb"
     item_attributes = {"brand": "Farmhouse Inns", "brand_wikidata": "Q105504972"}
     sitemap_urls = ["https://www.farmhouseinns.co.uk/sitemap.xml"]
