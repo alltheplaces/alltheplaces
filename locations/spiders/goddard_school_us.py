8494852a521ccb6c0bd801c5b07c5ff0f1b6d7b2
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-11-29 18:43:12 +0530

    Add branch

--- locations/spiders/goddard_school_us.py
+++ locations/spiders/goddard_school_us.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -8,3 +10,7 @@ class GoddardSchoolUSSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Goddard School", "brand_wikidata": "Q5576260"}
     sitemap_urls = ["https://www.goddardschool.com/sitemap.xml"]
     sitemap_rules = [(r"/schools/[a-z]{2}/[-\w]+/[-\w]+$", "parse_sd")]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("The Goddard School of ")
+        yield item
