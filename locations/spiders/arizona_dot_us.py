11acc620011547d4c66c183981b4db5962770a9a
David Hicks <david@hicks.id.au>
2024-12-02 02:58:00 +1100

    TravelIQ: add/convert more spiders to TravelIQSpider

--- locations/spiders/arizona_dot_us.py
+++
@@ -1,7 +0,0 @@
-from locations.spiders.nevada_dot_us import NevadaDotUSSpider
-
-
-class ArizonaDotUSSpider(NevadaDotUSSpider):
-    name = "arizona_dot_us"
-    item_attributes = {"operator": "Arizona DOT", "operator_wikidata": "Q807704"}
-    start_urls = ["https://www.az511.com/map/mapIcons/Cameras"]
--- locations/spiders/utah_dot_us.py
+++
@@ -1,7 +0,0 @@
-from locations.spiders.nevada_dot_us import NevadaDotUSSpider
-
-
-class UtahDotUSSpider(NevadaDotUSSpider):
-    name = "utah_dot_us"
-    item_attributes = {"operator": "Utah DOT", "operator_wikidata": "Q2506783"}
-    start_urls = ["https://prod-ut.ibi511.com/map/mapIcons/Cameras"]
