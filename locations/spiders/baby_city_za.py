46f065421460d32d0a603f5bfc8b51a279180239
David Hicks <david@hicks.id.au>
2024-12-03 01:46:22 +1100

    baby_city_za: extract coordinates

--- locations/spiders/baby_city_za.py
+++ locations/spiders/baby_city_za.py
@@ -5,6 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories
+from locations.google_url import extract_google_position
 from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
@@ -28,6 +29,7 @@ class BabyCityZASpider(CrawlSpider):
             "website": response.url,
             "opening_hours": OpeningHours(),
         }
+        extract_google_position(properties, response)
         hours_text = " ".join(
             filter(
                 None,
