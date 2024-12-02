714e7cf6024788584b7aa81c38b05c0357116fdc
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-02 10:24:17 +0530

    code refactored to fix spider

--- locations/spiders/hangerclinic.py
+++ locations/spiders/hangerclinic.py
@@ -1,120 +1,33 @@
-import re
+from typing import Any
 
-import scrapy
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
 
 
-class HangerclinicSpider(scrapy.Spider):
+class HangerclinicSpider(SitemapSpider):
     name = "hangerclinic"
     item_attributes = {"brand": "Hanger Clinic"}
-    allowed_domains = ["hangerclinic.com"]
-    start_urls = ("http://www.hangerclinic.com/locations/Pages/by-state.aspx",)
+    sitemap_urls = [
+        "https://hangerclinic.com/locations-sitemap.xml",
+    ]
+    sitemap_rules = [(r"/clinics/[^/]+/[^/]+/[^/]+/$", "parse")]
 
-    def format_hours(self, datestring):
-        split_date = datestring.replace(";", ",").split(",")
-
-        if "ppointment" in datestring:
-            return "By Appointment"
-
-        if len(split_date) == 2:
-            matches = re.search(r"([\d]{1,2}).*?-.*?(\d{1,2})", split_date[1])
-
-            if matches:
-
-                def fmt_hr(hour):
-                    hour = str(hour)
-
-                    if len(hour) == 1:
-                        hour = "0" + hour + ":00"
-                    elif len(hour) == 2:
-                        hour = hour + ":00"
-                    elif len(hour) == 4:
-                        hour = "0" + hour
-                    return hour
-
-                try:
-                    am = int(matches.group(1))
-                    pm = int(matches.group(2))
-
-                    if pm != 12:
-                        pm = int(pm) + 12
-                except:
-                    return
-
-                hourstring = fmt_hr(am) + "-" + fmt_hr(pm)
-                datestring = str(split_date[0]) + ", " + hourstring
-
-        day_dict = {
-            "Mon": "Mo",
-            "Tue": "Tu",
-            "Wed": "We",
-            "Thu": "Th",
-            "Fri": "Fr",
-            "Sat": "Sa",
-            "Sun": "Su",
-            "Monday": "Mo",
-            "Tuesday": "Tu",
-            "Wednesday": "We",
-            "Thursday": "Th",
-            "Thurs": "Th",
-            "Thur": "Th",
-            "Friday": "Fr",
-            "Saturday": "Sa",
-            "Sunday": "Su",
-            "24 hours/7 days a week": "24/7",
-            "Please contact store for hours": "N/A",
-            "Open only on": "",
-            "Only": "",
-            "A.M.": "",
-            "P.M.": "",
-        }
-        pattern = re.compile(r"\b(" + "|".join(day_dict.keys()) + r")\b")
-        datestring = pattern.sub(lambda x: day_dict[x.group()], datestring.replace(" - ", "-"))
-
-        return datestring
-
-    def parse(self, response):
-        states = response.xpath("//select[@class='locationsByStateSelect']/option/text()")
-
-        for state in states:
-            link = "http://www.hangerclinic.com/locations/Pages/by-state.aspx?state=" + state.extract()
-            request = scrapy.Request(link, callback=self.parse_state)
-            yield request
-
-    def parse_state(self, response):
-        store_links = response.xpath("//td[@class='byStateLocation']/a/@href")
-
-        for link in store_links:
-            store_link = "http://www.hangerclinic.com" + link.extract()
-            store_link_request = scrapy.Request(store_link, callback=self.parse_store)
-            yield store_link_request
-
-    def parse_store(self, response):
-        store_info = response.xpath("//div[@class='intro']")
-        city, statezip = (
-            store_info.xpath("//span[@id='locCityState']/text()")
-            .extract()[0]
-            .replace(" ", "")
-            .replace("\n", "")
-            .replace("\r", "")
-            .split(",")
-        )
-        opening_hours = store_info.xpath("//div[@id='locHours']/span[@class='locContent']/text()").extract()[0].strip()
-        street = store_info.xpath("//span[@id='locStreet']/text()").extract()[0].strip()
-
-        properties = {
-            "name": street.replace(" ", "_").replace(",", ""),
-            "addr_full": street + ", " + city + " " + statezip[:2] + " " + statezip[2:],
-            "ref": city + "_" + street.replace(" ", "_").replace(",", ""),
-            "street": street.split(",")[0],
-            "city": city,
-            "state": statezip[:2],
-            "postcode": statezip[2:],
-            "opening_hours": self.format_hours(opening_hours),
-            "phone": store_info.xpath("//div[@id='locPhone']/span[@class='locContent']/text()").extract()[0].strip(),
-            "lat": float(store_info.xpath("//div[@id='locLat']/text()").extract()[0].strip()),
-            "lon": float(store_info.xpath("//div[@id='locLong']/text()").extract()[0].strip()),
-        }
-
-        yield (Feature(**properties))
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["name"] = response.xpath('//*[@class = "c-page-header__title"]/span/text()').get()
+        item["street_address"] = response.xpath('//*[@class="c-map__info--address"]/text()').get().strip()
+        item["addr_full"] = response.xpath('//*[@class="c-map__info--address"]').xpath("normalize-space()").get()
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["ref"] = item["website"] = response.url
+        apply_category(Categories.CLINIC, item)
+        extract_google_position(item, response)
+        hours_string = response.xpath('//*[@class="c-map__info--hours"]/p/text()').get()
+        item["opening_hours"] = OpeningHours()
+        if hours_string:
+            item["opening_hours"].add_ranges_from_string(hours_string)
+        yield item
