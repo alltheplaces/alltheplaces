25f87534cf4ab9bef542a4db6433bb62cacc1528
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2024-12-03 03:32:44 +0530

    code refactored to use StorepointSpider to fix spider (#11553)

--- locations/spiders/donut_king_au.py
+++ locations/spiders/donut_king_au.py
@@ -1,25 +1,13 @@
-import re
-from typing import Iterable
-
-from scrapy.http import Response
-
-from locations.categories import Categories
-from locations.hours import DAYS_EN
-from locations.items import Feature
-from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from locations.categories import Categories, apply_category
+from locations.storefinders.storepoint import StorepointSpider
 
 
-class DonutKingAUSpider(WPStoreLocatorSpider):
+class DonutKingAUSpider(StorepointSpider):
     name = "donut_king_au"
-    item_attributes = {"brand_wikidata": "Q5296921", "brand": "Donut King", "extras": Categories.FAST_FOOD.value}
-    start_urls = ["https://www.donutking.com.au/wp/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
-    days = DAYS_EN
+    item_attributes = {"brand_wikidata": "Q5296921", "brand": "Donut King"}
+    key = "167209e72db19e"
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        if "CLOSED" in re.split(r"\W+", item.get("name", "").upper()):
-            return
-        if "CLOSED" in re.split(r"\W+", item.get("addr_full", "").upper()):
-            return
-        if "CLOSED" in re.split(r"\W+", item.get("street_address", "").upper()):
-            return
+    def parse_item(self, item, location):
+        item["website"] = "https://donutking.com.au/"
+        apply_category(Categories.FAST_FOOD, item)
         yield item
