48c6dd95bbe3c87f25cd1e81af5aa0378a4bc447
Karen Kerr <karenkerrgithub@gmail.com>
2024-12-03 11:04:14 +0000

    [NettoSalling] Fix non running PL

--- locations/spiders/netto_salling.py
+++ locations/spiders/netto_salling.py
@@ -8,15 +8,16 @@ from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
 
 
-class NettoSallingDESpider(Spider):
-    name = "netto_salling_de"
+class NettoSallingSpider(Spider):
+    name = "netto_salling"
     item_attributes = {"brand": "Netto", "brand_wikidata": "Q552652"}
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def start_requests(self):
-        url = "https://api.sallinggroup.com/v2/stores?&country=DE&geo?&radius=100&per_page=1000"
-        auth_token = "b1832498-0e22-436c-bd18-9ffa325dd846"
-        yield JsonRequest(url=url, headers={"Authorization": f"Bearer {auth_token}"})
+        for country in ["DE", "DK", "PL"]:
+            url = f"https://api.sallinggroup.com/v2/stores?&country={country}&geo?&radius=100&per_page=1000"
+            auth_token = "b1832498-0e22-436c-bd18-9ffa325dd846"
+            yield JsonRequest(url=url, headers={"Authorization": f"Bearer {auth_token}"})
 
     def parse(self, response, **kwargs):
         for location in response.json():
@@ -32,5 +33,4 @@ class NettoSallingDESpider(Spider):
                     day = DAYS[datetime.strptime(opening_hours["date"], "%Y-%m-%d").weekday()]
                     oh.add_ranges_from_string(f"{day} {open}-{close}")
                 item["opening_hours"] = oh
-
             yield item
