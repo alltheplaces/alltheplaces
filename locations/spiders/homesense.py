a786eab1339a5e8cb9dd8cf22aab39fdb43656c7
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-03 21:42:12 +0530

    code refactored to fix spider

--- locations/spiders/homesense.py
+++ locations/spiders/homesense.py
@@ -1,11 +1,44 @@
-from scrapy.spiders import SitemapSpider
+import re
+from typing import Any
 
-from locations.structured_data_spider import StructuredDataSpider
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.items import Feature
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class HomesenseSpider(SitemapSpider, StructuredDataSpider):
+class HomesenseSpider(CrawlSpider):
     name = "homesense"
     item_attributes = {"brand": "HomeSense", "brand_wikidata": "Q16844433"}
-    sitemap_urls = ["https://locations.us.homesense.com/sitemap.xml"]
-    sitemap_rules = [(r"https:\/\/locations\.us\.homesense\.com\/\w{2}\/[-\w]+$", "parse_sd")]
-    wanted_types = ["DepartmentStore"]
+    start_urls = ["https://us.homesense.com/all-stores"]
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow="/store-details/",
+                deny=[
+                    "yonkers-ny-10710",
+                    "totowa-nj-07512",
+                    "paramus-nj-07652",
+                    "mohegan-lake-ny-10547",
+                ],
+            ),
+            callback="parse",
+        )
+    ]
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["name"] = "-".join(
+            [
+                response.xpath('//*[@id="store-details-hero"]//h1/text()').get(),
+                response.xpath('//*[@id="store-details-hero"]//h2/text()').get(),
+            ]
+        )
+        item["addr_full"] = response.xpath('//*[@id="store-details-amenities"]//p').xpath("normalize-space()").get()
+        item["ref"] = item["website"] = response.url
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["lat"], item["lon"] = re.search(r"initMap\(([0-9-\.]+),\s*([0-9-\.]+)\);", response.text).groups()
+        yield item
