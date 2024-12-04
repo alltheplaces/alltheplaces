bb6fdf2cd8b5280a764a1f8ab12d0fff401e071c
Cj Malone <Cj-Malone@users.noreply.github.com>
2024-12-04 15:10:03 +0000

    [Auchan HU] Fix spider (#11565)

--- locations/spiders/auchan_hu.py
+++ locations/spiders/auchan_hu.py
@@ -1,39 +1,37 @@
-import chompjs
-from scrapy.spiders import SitemapSpider
+from typing import Any, Iterable
+from urllib.parse import urljoin
+
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
+from scrapy.spiders import Spider
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address
+from locations.google_url import url_to_coords
 
 
-class AuchanHUSpider(SitemapSpider):
+class AuchanHUSpider(Spider):
     name = "auchan_hu"
     item_attributes = {"brand": "Auchan", "brand_wikidata": "Q758603"}
-    sitemap_urls = ["https://www.auchan.hu/sitemap.xml"]
-    sitemap_rules = [
-        (r"/aruhazak/auchan-", "parse_store"),
-        (r"/benzinkutak/auchan-", "parse_gas_station"),
-    ]
 
-    def parse_store(self, response, **kwargs):
-        location = chompjs.parse_js_object(response.xpath('//script[contains(text(), "shopsmap")]/text()').get())
-        item = DictParser.parse(location[0])
-        item["ref"] = item["website"] = response.url
-        address_info = response.xpath('//*[contains(@class,"shopCard")]//p/text()').get()
-        item["addr_full"], item["phone"] = (
-            address_info.split("Központi szám:") if "Központi szám:" in address_info else (address_info, None)
-        )
-        apply_category(Categories.SHOP_SUPERMARKET, item)
-        yield item
+    def start_requests(self) -> Iterable[Request]:
+        yield JsonRequest(url="https://auchan.hu/api/v2/cache/petrol/list")
+        yield JsonRequest(url="https://auchan.hu/api/v2/cache/store/list")
 
-    def parse_gas_station(self, response, **kwargs):
-        # coordinates etc. in page's js_object belong to shops instead of gas station, hence ignored.
-        if address := response.xpath('//*[contains(@class,"petrolAddress")]/text()').get():
-            item = Feature()
-            item["ref"] = item["website"] = response.url
-            item["name"] = clean_address(response.xpath('//*[contains(@class,"selectedStation")]/text()').getall())
-            item["addr_full"] = clean_address(address)
-            item["nsi_id"] = "N/A"
-            apply_category(Categories.FUEL_STATION, item)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["lat"], item["lon"] = url_to_coords(location["googleMapsLink"])
+
+            if response.url == "https://auchan.hu/api/v2/cache/petrol/list":
+                item["branch"] = item.pop("name").removeprefix("MH Auchan ").removesuffix(" benzinkút")
+                item["name"] = "Auchan"
+                item["website"] = urljoin("https://auchan.hu/petrol/", location["slug"])
+                apply_category(Categories.FUEL_STATION, item)
+                item["nsi_id"] = "N/A"
+            else:
+                item["branch"] = item.pop("name").removeprefix("Auchan ")
+                item["website"] = urljoin("https://auchan.hu/stores/", location["slug"])
+                apply_category(Categories.SHOP_SUPERMARKET, item)
+
             yield item
