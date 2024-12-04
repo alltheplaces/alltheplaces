2823f229af284e51e2438f900c6487c133c597b1
Ian Dees <ian.dees@gmail.com>
2024-12-04 09:14:05 -0600

    Try without proxy

--- locations/spiders/alabama_department_of_transportation_us.py
+++ locations/spiders/alabama_department_of_transportation_us.py
@@ -17,7 +17,6 @@ class AlabamaDepartmentOfTransportationUSSpider(JSONBlobSpider):
     }
     allowed_domains = ["api.algotraffic.com"]
     start_urls = ["https://api.algotraffic.com/v3.0/Cameras"]
-    requires_proxy = "US"
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["name"] = " ".join(
