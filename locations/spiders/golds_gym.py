4054ded896a948543a9b5eafe3e92f429d7dc77b
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-05 16:08:29 +0530

    code refactored to fix spider

--- locations/spiders/golds_gym.py
+++ locations/spiders/golds_gym.py
@@ -1,15 +1,24 @@
+from typing import Any
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.items import Feature
 
 
-class GoldsGymSpider(SitemapSpider, StructuredDataSpider):
+class GoldsGymSpider(SitemapSpider):
     name = "golds_gym"
     item_attributes = {"brand": "Gold's Gym", "brand_wikidata": "Q1536234"}
-    sitemap_urls = ["https://www.goldsgym.com/gym_index-sitemap.xml"]
-    wanted_types = ["ExerciseGym"]
+    sitemap_urls = ["https://www.goldsgym.com/gym-sitemap.xml"]
+    sitemap_rules = [(r"/locations/[^/]+/[^/]+/$", "parse")]
 
-    def sitemap_filter(self, entries):
-        for entry in entries:
-            entry["loc"] = entry["loc"].replace("sitemap_index.xml", "")
-            yield entry
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["name"] = response.xpath("//h1").xpath("normalize-space()").get()
+        item["addr_full"] = response.xpath('//*[@class = "block-content__address"]//text()').get()
+        item["website"] = item["ref"] = response.url
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["email"] = response.xpath('//*[contains(@href,"mailto:")]/text()').get()
+        item["lat"] = response.xpath("//@data-map-lat").get()
+        item["lon"] = response.xpath("//@data-map-lng").get()
+        yield item
