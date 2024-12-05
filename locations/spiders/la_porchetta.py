6d308ade33ad5832cfa62bcb67bc8ce1eea485f3
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-12-05 19:22:02 +0530

    Fix La Porchetta Spider by using  WPStoreLocator

--- locations/spiders/la_porchetta.py
+++ locations/spiders/la_porchetta.py
@@ -1,7 +1,7 @@
-from locations.storefinders.storepoint import StorepointSpider
+from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class LaPorchettaSpider(StorepointSpider):
+class LaPorchettaSpider(WPStoreLocatorSpider):
     name = "la_porchetta"
     item_attributes = {"brand": "La Porchetta", "brand_wikidata": "Q6464528"}
-    key = "16156af5878536"
+    start_urls = ["https://laporchetta.com.au/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
