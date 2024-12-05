599a1b360e9d6f7d14db637affa317e3a7436fa2
Cj Malone <CjMalone@mail.com>
2024-12-05 13:59:02 +0000

    Rename spider

--- locations/spiders/john_lewis_gb.py
+++ locations/spiders/john_lewis_gb.py
@@ -8,8 +8,8 @@ from locations.structured_data_spider import StructuredDataSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class JohnLewisSpider(SitemapSpider, StructuredDataSpider):
-    name = "john_lewis"
+class JohnLewisGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "john_lewis_gb"
     item_attributes = {"brand": "John Lewis", "brand_wikidata": "Q1918981"}
     sitemap_urls = ["https://www.johnlewis.com/shops-services.xml"]
     sitemap_rules = [("/our-shops/", "parse_sd")]
