b6a405fdd97ad3c0670c99a29107c4339702b447
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-12-09 11:04:49 +0530

    Refactor using Sitemap and SD to fix Five Guys CN Spider

--- locations/spiders/five_guys_cn.py
+++ locations/spiders/five_guys_cn.py
@@ -1,13 +1,12 @@
-from locations.spiders.five_guys_au import FiveGuysAUSpider
+from scrapy.spiders import SitemapSpider
+
+from locations.spiders.five_guys_us import FIVE_GUYS_SHARED_ATTRIBUTES
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class FiveGuysCNSpider(FiveGuysAUSpider):
+class FiveGuysCNSpider(SitemapSpider, StructuredDataSpider):
     name = "five_guys_cn"
-    experience_key = "search-backend-cn"
-    locale = (
-        "en-CN"  # Using en because it has google attributes which gives lots of extra details (not present in zh-Hans)
-    )
-
-    def process_websites(self, item) -> None:
-        item["extras"]["website:en"] = item["website"].replace("fiveguys.cn/", "fiveguys.cn/en_cn/")
-        item["extras"]["website:zh"] = item["website"]
+    item_attributes = FIVE_GUYS_SHARED_ATTRIBUTES
+    sitemap_urls = ["https://restaurants.fiveguys.cn/sitemap.xml"]
+    sitemap_rules = [(r"https://restaurants.fiveguys.cn/en_cn/[-\w]+$", "parse_sd")]
+    wanted_types = ["FastFoodRestaurant"]  # Explicitly mention the type to ignore duplicate linked data
