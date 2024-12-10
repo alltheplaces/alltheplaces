2f57e9de333c3f46289f313d5eed7ce9a86eea56
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-10 12:14:30 +0530

    code refactored to fix spider

--- locations/spiders/juice_press_us.py
+++ locations/spiders/juice_press_us.py
@@ -1,7 +1,26 @@
-from locations.storefinders.storepoint import StorepointSpider
+import re
+from typing import Any
+
+import chompjs
+import scrapy
+from scrapy.http import Response
+
+from locations.dict_parser import DictParser
 
 
-class JuicePressUSSpider(StorepointSpider):
+class JuicePressUSSpider(scrapy.Spider):
     name = "juice_press_us"
     item_attributes = {"brand": "Juice Press", "brand_wikidata": "Q27150131"}
-    key = "15d4897d596fbd"
+    start_urls = ["https://www.juicepress.com/pages/location"]
+    no_refs = True
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        raw_data = list(
+            chompjs.parse_js_objects(
+                re.search(r"JPSTORELOCATOR\.locations\.push\((.*)\);Object", response.text, re.DOTALL).group(1)
+            )
+        )
+        for store in raw_data:
+            item = DictParser.parse(store)
+            item["street_address"] = item.pop("name")
+            yield item
