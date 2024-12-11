fb8465c677ae19220e1e09ce212196a5bbe50e21
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-12-11 17:49:17 +0530

    Ignore robotstxt file

--- locations/spiders/dusk_nz.py
+++ locations/spiders/dusk_nz.py
@@ -12,6 +12,9 @@ class DuskNZSpider(AmastyStoreLocatorSpider):
     name = "dusk_nz"
     item_attributes = {"brand": "dusk", "brand_wikidata": "Q120669167", "extras": Categories.SHOP_HOUSEWARE.value}
     allowed_domains = ["www.duskcandles.co.nz"]
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+    }
 
     def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
         address_string = re.sub(r"\s+", " ", " ".join(filter(None, popup_html.xpath("//text()").getall())))
