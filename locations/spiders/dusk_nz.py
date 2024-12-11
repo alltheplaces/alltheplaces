0ec2c1ae4d57c67003f337e22594fa0acb6e0e41
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-12-11 18:05:55 +0530

    Fix error due to change in address format

--- locations/spiders/dusk_nz.py
+++ locations/spiders/dusk_nz.py
@@ -1,10 +1,10 @@
-import re
 from typing import Iterable
 
 from scrapy import Selector
 
 from locations.categories import Categories
 from locations.items import Feature
+from locations.pipelines.address_clean_up import clean_address
 from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
@@ -17,8 +17,5 @@ class DuskNZSpider(AmastyStoreLocatorSpider):
     }
 
     def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
-        address_string = re.sub(r"\s+", " ", " ".join(filter(None, popup_html.xpath("//text()").getall())))
-        item["city"] = address_string.split("City: ", 1)[1].split(" Postcode: ", 1)[0]
-        item["postcode"] = address_string.split("Postcode: ", 1)[1].split(" Address: ", 1)[0]
-        item["street_address"] = address_string.split("Address: ", 1)[1].split(" Region: ", 1)[0]
+        item["addr_full"] = clean_address(popup_html.xpath("//text()").getall())
         yield item
