4a0adbdccc265e2e70290a08cde6efb9e403a875
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-11 12:42:41 +0530

    code refactored to use sitemap to fix spider

--- locations/spiders/lagerhaus_at.py
+++ locations/spiders/lagerhaus_at.py
@@ -1,22 +1,36 @@
-import scrapy
-import xmltodict
+from typing import Any
 
-from locations.dict_parser import DictParser
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.google_url import extract_google_position
+from locations.hours import DAYS_DE, OpeningHours, sanitise_day
+from locations.items import Feature
 
 
-class LagerhausATSpider(scrapy.Spider):
+class LagerhausATSpider(SitemapSpider):
     name = "lagerhaus_at"
     item_attributes = {"brand": "Lagerhaus", "brand_wikidata": "Q1232873"}
-    start_urls = [
-        "https://static.lagerhaus.at/loc/v1/find/locations/full?lat=47.2&lon=16.2&distance=1660&divisions=Fachhandel%2FShop"
-    ]
+    sitemap_urls = ["https://lagerhaus.at/sitemap/index.xml"]
+    sitemap_rules = [("/standort/", "parse")]
+    sitemap_follow = ["locations"]
 
-    def parse(self, response):
-        obj = xmltodict.parse(response.text)
-        for store in DictParser.get_nested_key(obj, "item"):
-            item = DictParser.parse(store)
-            item["branch"] = item.pop("name")
-            item.pop("country")
-            item["street_address"] = store["address"]["line"]
-            item["website"] = "https://lagerhaus.at/standort/" + store["id"]
-            yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["name"] = response.xpath("//h1").xpath("normalize-space()").get()
+        item["addr_full"] = response.xpath('//*[@class="A5SKTxfddRWFz7lYLFXx"]').xpath("normalize-space()").get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
+        item["opening_hours"] = OpeningHours()
+        for day_time in response.xpath('//*[@class="crIzOQn7PNuYkyfoUq9P"]//*[@class="m4VniD5GavaCh3bqMUKm"]'):
+            day = sanitise_day(
+                day_time.xpath('.//*[@class="HUI8SAWj6xc116qIMHGC GtBm40epTsrl8DTPJIA_"]/text()').get(), DAYS_DE
+            )
+            for time_string in day_time.xpath(".//li"):
+                time = time_string.xpath(".//text()").get()
+                if "geschlossen" in time:
+                    item["opening_hours"].set_closed(day)
+                else:
+                    open_time, close_time = time.replace("Uhr", "").split("-")
+                    item["opening_hours"].add_range(day=day, open_time=open_time.strip(), close_time=close_time.strip())
+        yield item
