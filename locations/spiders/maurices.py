43a3aeefd688ba7ceb7e05bd4f7c8da0a9fc53c8
Cj Malone <CjMalone@mail.com>
2024-12-12 15:10:42 +0000

    [Maurices] Improve brand and branch

--- locations/spiders/maurices.py
+++ locations/spiders/maurices.py
@@ -1,12 +1,17 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class MauricesSpider(SitemapSpider, StructuredDataSpider):
     name = "maurices"
-    item_attributes = {"brand": "maurices", "brand_wikidata": "Q6793571"}
+    item_attributes = {"brand": "Maurices", "brand_wikidata": "Q6793571"}
     sitemap_urls = ["https://locations.maurices.com/sitemap.xml"]
-    sitemap_rules = [("", "parse_sd")]
-    download_delay = 0.2
+    sitemap_rules = [("com/\w\w/\w\w/[^/]+/[^/]+$", "parse_sd")]
     drop_attributes = {"image"}
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("maurices ")
+        yield item
