faa47ae5d6999faddb74c27596db53deffba2200
Cj Malone <CjMalone@mail.com>
2024-12-12 15:13:53 +0000

    Fix re string

--- locations/spiders/maurices.py
+++ locations/spiders/maurices.py
@@ -9,7 +9,7 @@ class MauricesSpider(SitemapSpider, StructuredDataSpider):
     name = "maurices"
     item_attributes = {"brand": "Maurices", "brand_wikidata": "Q6793571"}
     sitemap_urls = ["https://locations.maurices.com/sitemap.xml"]
-    sitemap_rules = [("com/\w\w/\w\w/[^/]+/[^/]+$", "parse_sd")]
+    sitemap_rules = [(r"com/\w\w/\w\w/[^/]+/[^/]+$", "parse_sd")]
     drop_attributes = {"image"}
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
