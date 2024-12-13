12ad62e2ab08b8239ef09239aa64a92b8fc7e4e4
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-12-13 14:50:46 +0530

    Fix Nordsee Spider

--- locations/spiders/nordsee.py
+++ locations/spiders/nordsee.py
@@ -1,31 +1,23 @@
-from scrapy import Selector, Spider
-from scrapy.http import JsonRequest
+from typing import Iterable
 
-from locations.dict_parser import DictParser
-from locations.hours import OpeningHours
+import chompjs
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class NordseeSpider(Spider):
+class NordseeSpider(JSONBlobSpider):
     name = "nordsee"
     item_attributes = {"brand": "Nordsee", "brand_wikidata": "Q74866"}
-    allowed_domains = ["www.nordsee.com"]
-    start_urls = ["https://www.nordsee.com/en/?type=2001"]
+    start_urls = ["https://www.nordsee.com/de/filialen"]
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield JsonRequest(url=url)
+    def extract_json(self, response: Response) -> list:
+        return chompjs.parse_js_object(
+            response.xpath('//script[contains(text(),"latitude")]/text()').get(), unicode_escape=True
+        )["stores"]
 
-    def parse(self, response):
-        for location in response.json()["stores"]:
-            item = DictParser.parse(location)
-            item["ref"] = location["uid"]
-            item["street_address"] = item.pop("housenumber", None)
-            item["opening_hours"] = OpeningHours()
-            for day_abbrev, hours_range in location.get("opening").items():
-                if isinstance(hours_range, str) and "------" not in hours_range:
-                    item["opening_hours"].add_range(day_abbrev.title(), *hours_range.split(" - ", 1), "%H:%M")
-            item["website"] = (
-                "https://www.nordsee.com"
-                + Selector(text=location["listItem"]).xpath('//a[@class="storeListItem__link"]/@href').get()
-            )
-            yield item
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if item.get("state") in ["AT", "DE"]:
+            item["country"] = item.pop("state")
+        yield item
