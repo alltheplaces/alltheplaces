22998bad4d36477246d5752791b681669d15e875
Colton Piper <pipercolton@gmail.com>
2024-12-16 16:10:43 -0500

    [healthhub_sg] Added some Hospital Data for Singapore (32 items)

---
+++ locations/spiders/healthhub_sg.py
@@ -0,0 +1,39 @@
+import re
+
+from scrapy import Request, Spider
+
+from locations.dict_parser import DictParser
+
+
+class HealthhubSGSpider(Spider):
+    name = "healthhub_sg"
+    start_urls = [
+        "https://eservices.healthhub.sg/healthhubng/public/services/directory/56",
+        "https://eservices.healthhub.sg/healthhubng/public/services/directory/58",
+    ]
+
+    def parse(self, response, **kwargs):
+        url_for_api_key = response.xpath("//script[@type='module']/@src").get()
+        if url_for_api_key:
+            url_for_api_key = f"https://eservices.healthhub.sg{url_for_api_key}"
+            yield Request(url_for_api_key, callback=self.parse_api_key)
+
+    def parse_api_key(self, response, **kwargs):
+        for m in re.findall(r'''VITE_REACT_APP_API_KEY:\s*"([^"\s\n]+)"''', response.text):
+            api_key = m
+
+        if api_key:
+            locations_urls = [
+                "https://api.healthhub.sg/healthhub/prod/v2/Directory/LocationList?pageNum=1&pageSize=100&categoryId=56",
+                "https://api.healthhub.sg/healthhub/prod/v2/Directory/LocationList?pageNum=1&pageSize=100&categoryId=58",
+            ]
+            for url in locations_urls:
+                yield Request(url, callback=self.parse_locations, headers={"X-Api-Key": api_key})
+
+    def parse_locations(self, response, **kwargs):
+        for location in response.json().get("Results", []):
+            item = DictParser.parse(location)
+            if location.get("Open24H") == "Y":
+                item["opening_hours"] = "Mo-Su 00:00-24:00"
+
+            yield item
