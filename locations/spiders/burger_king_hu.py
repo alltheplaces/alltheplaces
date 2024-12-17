49b599903fa920df9c465ead260bebf2309ca114
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-17 10:31:33 +0530

    code refactored to fix spider

--- locations/spiders/burger_king_hu.py
+++ locations/spiders/burger_king_hu.py
@@ -1,8 +1,24 @@
-from locations.spiders.burger_king_bs import BurgerKingBSSpider
+from typing import Any
+import chompjs
+import scrapy
+from scrapy.http import Response
+from locations.dict_parser import DictParser
+from locations.spiders.burger_king import BURGER_KING_SHARED_ATTRIBUTES
 
 
-class BurgerKingHUSpider(BurgerKingBSSpider):
+class BurgerKingHUSpider(scrapy.Spider):
     name = "burger_king_hu"
-    allowed_domains = ["burgerking.hu"]
-    host = "https://burgerking.hu"
-    country_code = "HU"
+    item_attributes = BURGER_KING_SHARED_ATTRIBUTES
+    start_urls = [
+        "https://burgerking.hu/ettermeink/",
+    ]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        bk_locations = response.xpath('//*[contains(text(),"bkLocations")]/text()').get()
+        stores = chompjs.parse_js_object(bk_locations)["restaurants"]
+        for store in stores:
+            item = DictParser.parse(store)
+            item["addr_full"] = store["info"]
+            item["street_address"] = store["address"]
+            item["website"] = "https://burgerking.hu/"
+            yield item
