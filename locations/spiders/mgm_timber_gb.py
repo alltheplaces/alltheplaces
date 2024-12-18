f1b54fc37311125f3aed9ca8cd5ed7b9e41c5de6
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-12-18 15:31:03 +0000

    Fix spider name

--- locations/spiders/mgm_timber_gb.py
+++ locations/spiders/mgm_timber_gb.py
@@ -1,7 +1,7 @@
 from locations.storefinders.yext_answers import YextAnswersSpider
 
 
-class MGMTimberGBSpider(YextAnswersSpider):
+class MgmTimberGBSpider(YextAnswersSpider):
     name = "mgm_timber_gb"
     item_attributes = {"brand": "MGM Timber", "brand_wikidata": ""}
     api_key = "db9fb251f6697c5529b02e93d68f6e33"
