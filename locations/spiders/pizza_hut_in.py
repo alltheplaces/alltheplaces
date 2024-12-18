3eb0e1f03db39d8b5432623ba0a8e8299a0c9bc6
ehsan-firdaus <ehsanfirdaus@gmail.com>
2024-12-18 19:34:18 +0530

    Fix Pizza Hut IN Spider

--- locations/spiders/pizza_hut_in.py
+++ locations/spiders/pizza_hut_in.py
@@ -1,31 +1,38 @@
+import json
 from typing import Any
 
 from scrapy.http import Response
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Spider
 
-from locations.categories import Categories, apply_category
-from locations.items import Feature
-from locations.pipelines.address_clean_up import merge_address_lines
+from locations.dict_parser import DictParser
+from locations.linked_data_parser import LinkedDataParser
 
 
-class PizzaHutINSpider(CrawlSpider):
+# https://restaurants.pizzahut.co.in/sitemap.xml gives lesser count, hence ignored.
+class PizzaHutINSpider(Spider):
     name = "pizza_hut_in"
     item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
     start_urls = ["https://restaurants.pizzahut.co.in/?page=1"]
-    rules = [Rule(LinkExtractor(r"/\?page=\d+$"), callback="parse", follow=True)]
+    final_page = 0
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.xpath('//div[@class="store-info-box"]'):
-            item = Feature()
-            item["ref"] = item["website"] = location.xpath('.//a[contains(@href, "/Home")]/@href').get()
-            item["lat"] = location.xpath('input[@class="outlet-latitude"]/@value').get()
-            item["lon"] = location.xpath('input[@class="outlet-longitude"]/@value').get()
-            item["addr_full"] = merge_address_lines(
-                location.xpath('.//li[@class="outlet-address"]/div[@class="info-text"]/span/text()').getall()
+        if not self.final_page:
+            self.final_page = int(
+                response.xpath(
+                    '//button[contains(@class,"mantine-Pagination-control")][@data-with-padding="true"]/text()'
+                ).getall()[-1]
             )
-            item["phone"] = location.xpath('.//li[@class="outlet-phone"]/div[@class="info-text"]/a/text()').get()
+        current_page = int(response.xpath('//button[@aria-current="page"]/text()').get())
+        linked_data = json.loads(response.xpath('//script[@type="application/ld+json"]/text()').get())
+        if isinstance(linked_data, list):
+            for ld in linked_data:
+                if not DictParser.get_nested_key(ld, "itemListElement"):
+                    continue
+                for item_element in DictParser.get_nested_key(ld, "itemListElement"):
+                    if ld_item := item_element.get("item"):
+                        item = LinkedDataParser.parse_ld(ld_item)
+                        item["ref"] = item["website"] = ld_item["url"].split("?utm_source")[0]
+                        yield item
 
-            apply_category(Categories.RESTAURANT, item)
-
-            yield item
+        if current_page < self.final_page:
+            yield response.follow(f"/?page={current_page + 1}")
