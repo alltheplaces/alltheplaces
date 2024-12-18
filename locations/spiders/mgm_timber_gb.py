bf92061646d3b77d6775198b068b35d3a101c701
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-12-18 15:39:40 +0000

    Add category

--- locations/spiders/mgm_timber_gb.py
+++ locations/spiders/mgm_timber_gb.py
@@ -1,4 +1,5 @@
 from locations.storefinders.yext_answers import YextAnswersSpider
+from locations.categories import Categories, apply_category
 
 
 class MgmTimberGBSpider(YextAnswersSpider):
@@ -7,3 +8,7 @@ class MgmTimberGBSpider(YextAnswersSpider):
     api_key = "db9fb251f6697c5529b02e93d68f6e33"
     experience_key = "donaldson-locator"
     locale = "en_GB"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.TRADE_BUILDING_SUPPLIES, item)
+        yield item
