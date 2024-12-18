6341b0841b716184443ad12efbdff63b7796ec43
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-12-18 15:28:20 +0000

    [MGM Timber GB] Add Spider

---
+++ locations/spiders/mgm_timber_gb.py
@@ -0,0 +1,9 @@
+from locations.storefinders.yext_answers import YextAnswersSpider
+
+
+class MGMTimberGBSpider(YextAnswersSpider):
+    name = "mgm_timber_gb"
+    item_attributes = {"brand": "MGM Timber", "brand_wikidata": ""}
+    api_key = "db9fb251f6697c5529b02e93d68f6e33"
+    experience_key = "donaldson-locator"
+    locale = "en_GB"
