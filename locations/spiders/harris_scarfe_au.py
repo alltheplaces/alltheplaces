97491e73eca7fed3c8e79e8573373265641e91c9
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-26 19:44:35 +0530

    code updated to parse token dynamically

--- locations/spiders/harris_scarfe_au.py
+++ locations/spiders/harris_scarfe_au.py
@@ -1,6 +1,10 @@
 import html
 import re
+from typing import Any
+
+import scrapy
 import unicodedata
+from scrapy.http import Response
 
 from scrapy.spiders import SitemapSpider
 
@@ -8,14 +12,34 @@ from locations.hours import OpeningHours
 from locations.items import Feature
 
 
-class HarrisScarfeAUSpider(SitemapSpider):
+class HarrisScarfeAUSpider(scrapy.Spider):
     name = "harris_scarfe_au"
     item_attributes = {"brand": "Harris Scarfe", "brand_wikidata": "Q5665029"}
-    sitemap_urls = ["https://www.harrisscarfe.com.au/sitemap/store/store-sitemap.xml?queueittoken=e_harrisscarfe~ts_1734950356~ce_true~rt_safetynet~h_03afd3fd079b81a4185484a4329694c58554f14cefb91dd5a1ae291868e652e4"]
-    sitemap_rules = [(r"^https://www\.harrisscarfe\.com\.au/store/(?!online)[^/]+/[^/]+/\d+$", "parse")]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    start_urls = ["https://www.harrisscarfe.com.au/sitemap/store/store-sitemap.xml"]
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+        "REDIRECT_ENABLED": True
+    }
+    handle_httpstatus_list = [302]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        token_request_url = str(response.headers.getlist("Location")[0].decode("utf-8"))
+        yield scrapy.Request(url=token_request_url, callback=self.parse_token_request)
+
+    def parse_token_request(self, response):
+        token_request_url = str(response.headers.getlist("Location")[0].decode("utf-8"))
+        yield scrapy.Request(url=token_request_url, callback=self.parse_token)
+
+    def parse_token(self, response):
+        url = str(response.headers.getlist("Location")[0].decode("utf-8"))
+        token = str(response.headers.getlist("Set-Cookie")[0].decode("utf-8"))
+        yield scrapy.Request(url=url, headers={"cookie": token}, callback=self.parse_store_urls)
+
+    def parse_store_urls(self, response, **kwargs):
+        for link in response.xpath('//urlset//url//loc/text()').getall():
+            yield scrapy.Request(url=link, callback=self.parse_store)
+
+    def parse_store(self, response):
         properties = {
             "ref": response.xpath('//div[@id="maps_canvas"]/@data-storeid').get(),
             "name": response.xpath('//div[@id="maps_canvas"]/@data-storename').get(),
