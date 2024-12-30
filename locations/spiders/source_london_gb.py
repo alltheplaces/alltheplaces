43b7c76703f60f15e3e6056dc269a5b15d3e232c
mohit-sharma2194 <sharmaji2194@gmail.com>
2024-12-30 15:44:18 +0530

    added start request to fix spider

--- locations/spiders/source_london_gb.py
+++ locations/spiders/source_london_gb.py
@@ -1,4 +1,8 @@
-from scrapy import Spider
+from typing import Iterable
+
+import scrapy
+from scrapy import Spider, Request
+from scrapy.http import JsonRequest
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
@@ -7,10 +11,14 @@ from locations.dict_parser import DictParser
 class SourceLondonGBSpider(Spider):
     name = "source_london_gb"
     item_attributes = {"brand": "Source London", "brand_wikidata": "Q7565133"}
-    start_urls = ["https://www.sourcelondon.net/api/infra/location"]
+
+    def start_requests(self) -> Iterable[Request]:
+        yield JsonRequest(
+            url="https://www.sourcelondon.net/api/infra/location", headers={"X-API-VERSION": "2"}, callback=self.parse
+        )
 
     def parse(self, response, **kwargs):
-        for location in response.json():
+        for location in response.json()["internalLocations"]:
             item = DictParser.parse(location)
             apply_category(Categories.CHARGING_STATION, item)
             yield item
