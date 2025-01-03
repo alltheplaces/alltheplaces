9e47ef0776b15435817f074daa2ad77243673447
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-01-03 12:51:00 +0000

    Saved in correct place

---
+++ locations/spiders/english_heritage_gb.py
@@ -0,0 +1,10 @@
+#from urllib.parse import urljoin
+
+from locations.json_blob_spider import JSONBlobSpider
+
+class EnglishHeritageGBSpider(JSONBlobSpider):
+    name = "english_heritage_gb"
+    item_attributes = {"brand": "English Heritage", "brand_wikidata": "Q936287"}
+    start_urls = ["https://www.english-heritage.org.uk/api/PropertySearch/GetAll"]
+    no_refs = True
+    locations_key = "Results"
