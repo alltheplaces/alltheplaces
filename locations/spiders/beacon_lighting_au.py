f0848395e7f4bed734c12506b93b57471e04f521
David Hicks <david@hicks.id.au>
2025-01-03 23:55:40 +1100

    beacon_lighting_au: fix spider

--- locations/spiders/beacon_lighting_au.py
+++ locations/spiders/beacon_lighting_au.py
@@ -1,13 +1,16 @@
+from json import loads
+
 from scrapy import Spider
 from scrapy.http import JsonRequest
 
+from locations.categories import Categories
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 
 
 class BeaconLightingAUSpider(Spider):
     name = "beacon_lighting_au"
-    item_attributes = {"brand": "Beacon Lighting", "brand_wikidata": "Q109626941"}
+    item_attributes = {"brand": "Beacon Lighting", "brand_wikidata": "Q109626941", "extras": Categories.SHOP_LIGHTING.value}
     allowed_domains = ["www.beaconlighting.com.au"]
     start_urls = ["https://www.beaconlighting.com.au/beaconlocator/index/ajax/"]
 
@@ -18,12 +21,15 @@ class BeaconLightingAUSpider(Spider):
     def parse(self, response):
         for location in response.json()["items"]:
             item = DictParser.parse(location)
+            item["ref"] = str(location["id"])
             item["street_address"] = item.pop("addr_full")
+            item["state"] = location["state_data"]["code"]
             item["image"] = location["location_image"]
             item["website"] = "https://www.beaconlighting.com.au/storelocator/" + location["url_key"] + "/"
 
             item["opening_hours"] = OpeningHours()
-            for day_name, day_details in location["schedule_array"].items():
+            schedule_dict = loads(location["schedule_string"])
+            for day_name, day_details in schedule_dict.items():
                 if day_details[f"{day_name}_status"] != "1":
                     continue
                 open_time_1 = day_details["from"]["hours"] + ":" + day_details["from"]["minutes"]
