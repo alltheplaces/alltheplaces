80c5bf46b344fa72b4a6937b2f130454bd3d1b52
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-01-03 15:48:32 +0530

    code refactored to fix spider

--- locations/spiders/bunnings.py
+++ locations/spiders/bunnings.py
@@ -1,76 +1,27 @@
-import json
-
-from scrapy import Request, Spider
+import scrapy
+from scrapy import Spider
 
 from locations.dict_parser import DictParser
-from locations.hours import OpeningHours
 
 
 class BunningsSpider(Spider):
     name = "bunnings"
-    allowed_domains = ["bunnings.com.au"]
-    start_urls = [
-        "https://api.prod.bunnings.com.au/v1/stores?latitude=-23.12&longitude=132.13&currentPage=0&fields=FULL&pageSize=10000&radius=9000000",
-    ]
     item_attributes = {"brand": "Bunnings", "brand_wikidata": "Q4997829"}
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    client_id = "mHPVWnzuBkrW7rmt56XGwKkb5Gp9BJMk"  # Fixed value of APIGEE_CLIENT_ID variable from store locator page.
-    requires_proxy = "AU"  # Requires AU or NZ proxy, possibly residential IP addresses only.
 
     def start_requests(self):
-        yield Request(url="https://www.bunnings.com.au/", callback=self.parse_cookies)
-
-    def parse_cookies(self, response):
-        cookies = response.headers.getlist("Set-Cookie")
-        for cookie in cookies:
-            if "guest-token-storage" not in str(cookie):
-                continue
-            token_dict = json.loads("{" + str(cookie).split("={", 1)[1].split("}", 1)[0] + "}")
-            auth_token = token_dict["token"]
-            headers = {
-                "Authorization": "Bearer " + auth_token,
-                "clientId": self.client_id,
+        yield scrapy.Request(
+            url="https://api.prod.bunnings.com.au/v1/stores/country/AU?fields=FULL",
+            headers={
+                "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkJGRTFEMDBBRUZERkVDNzM4N0E1RUFFMzkxNjRFM0MwMUJBNzVDODciLCJ4NXQiOiJ2LUhRQ3VfZjdIT0hwZXJqa1dUandCdW5YSWMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2J1bm5pbmdzLmNvbS5hdS8iLCJuYmYiOjE3MzU4ODUzNjIsImlhdCI6MTczNTg4NTM2MiwiZXhwIjoxNzM2MzE3MzYyLCJhdWQiOlsiQ2hlY2tvdXQtQXBpIiwiY3VzdG9tZXJfYnVubmluZ3MiLCJodHRwczovL2J1bm5pbmdzLmNvbS5hdS9yZXNvdXJjZXMiXSwic2NvcGUiOlsiY2hrOmV4ZWMiLCJjbTphY2Nlc3MiLCJlY29tOmFjY2VzcyIsImNoazpwdWIiXSwiYW1yIjpbImV4dGVybmFsIl0sImNsaWVudF9pZCI6ImJ1ZHBfZ3Vlc3RfdXNlcl9hdSIsInN1YiI6IjI5ODRkYWNmLWU2YjEtNDYyNS1hNjM4LTdkYTU1YThlYTQ3MCIsImF1dGhfdGltZSI6MTczNTg4NTM2MiwiaWRwIjoibG9jYWxsb29wYmFjayIsImItaWQiOiIyOTg0ZGFjZi1lNmIxLTQ2MjUtYTYzOC03ZGE1NWE4ZWE0NzAiLCJiLXJvbGUiOiJndWVzdCIsImItdHlwZSI6Imd1ZXN0IiwibG9jYWxlIjoiZW5fQVUiLCJiLWNvdW50cnkiOiJBVSIsImFjdGl2YXRpb25fc3RhdHVzIjoiRmFsc2UiLCJ1c2VyX25hbWUiOiIyOTg0ZGFjZi1lNmIxLTQ2MjUtYTYzOC03ZGE1NWE4ZWE0NzAiLCJiLXJiYWMiOlt7ImFzYyI6IjgwYjM5NGM1LThiOGItNGE3My1hMTVkLTljYmNmMTE1NGU2OCIsInR5cGUiOiJDIiwicm9sIjpbIkNISzpHdWVzdCJdfV0sInNpZCI6IjNGMThGMDk2NEEwRTc5ODI0NDczMzhGOURGRjJERDA5IiwianRpIjoiMDRBM0UyQjkzRTBBRUI2QTkyNjFGNTE2MUUxQjk5NTAifQ.FqWatu2TJth5LVW9gMiy9_0Ki5t6uYh9L837wj-zqPX_xbtoHGZUJ8QkEy3oe5u2IXm0TCJZSRzAAoG3JALIogN9NIfqrkxeDBat0EPMXUDsh4seFReDZPylzbSxJEnWRVxApCzbMBl6dIFCJOGl1xcoikmLdGgP1zSiB7ugE-d7DxQf9btNnyRUhsbyE_kgEi7yWO0FlCzHoKdDZyfWbQgQEcmVzp8U7eWmRdEueCNl99SJK9coOAYGpjF-f4FB5u364xMHaKqR_SfhB2_dAPGdeU7_lSzr4hhaQ4iJuvIBe4ZKJd2lcI0MqM3F0X40p19Mzgg-Ln-p0uyIhI1NLg",
+                "clientId": "mHPVWnzuBkrW7rmt56XGwKkb5Gp9BJMk",
                 "country": "AU",
                 "locale": "en_AU",
-            }
-            for url in self.start_urls:
-                yield Request(url=url, headers=headers, callback=self.parse)
-            return
+            },
+        )
 
     def parse(self, response):
-        if response.json()["statusDetails"]["state"] != "SUCCESS":
-            return
-        for location in response.json()["data"]["stores"]:
-            if not location["isActiveLocation"]:
-                continue
-            item = DictParser.parse(location)
-            item["ref"] = location["name"]
-            item["name"] = location["displayName"]
-            item["phone"] = location["address"].get("phone")
-            item["email"] = location["address"].get("email")
-            if item["country"] == "NZ":
-                item.pop("state")
-                website_prefix = "https://www.bunnings.co.nz/stores/"
-            else:
-                item["state"] = location["address"]["region"]["isocode"]
-                website_prefix = "https://www.bunnings.com.au/stores/"
-            if "urlRegion" in location:
-                item["website"] = website_prefix + location["urlRegion"] + "/" + item["name"].lower().replace(" ", "-")
-            item["extras"]["website:map"] = location.get("mapUrl")
-            oh = OpeningHours()
-            for day in location["openingHours"]["weekDayOpeningList"]:
-                if (
-                    not day["closed"]
-                    and day.get("openingTime")
-                    and day["openingTime"].get("formattedHour")
-                    and day.get("closingTime")
-                    and day["closingTime"].get("formattedHour")
-                ):
-                    oh.add_range(
-                        day=day["weekDay"],
-                        open_time=day["openingTime"]["formattedHour"],
-                        close_time=day["closingTime"]["formattedHour"],
-                        time_format="%I:%M %p",
-                    )
-            item["opening_hours"] = oh.as_opening_hours()
+        for store in response.json()["data"]["pointOfServices"]:
+            store.update(store.pop("address"))
+            item = DictParser.parse(store)
+            item["name"] = store["displayName"]
             yield item
