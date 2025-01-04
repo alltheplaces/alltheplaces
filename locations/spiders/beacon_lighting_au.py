d3e9c787fe6c4657411877a68dc861c9d26436c0
David Hicks <david@hicks.id.au>
2025-01-05 01:17:54 +1100

    beacon_lighting_au: rewrite as AmastyStoreLocatorSpider

--- locations/spiders/beacon_lighting_au.py
+++ locations/spiders/beacon_lighting_au.py
@@ -1,50 +1,48 @@
 from json import loads
+from typing import Iterable
 from urllib.parse import quote
 
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy import Selector
 
-from locations.categories import Categories
-from locations.dict_parser import DictParser
+from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
-class BeaconLightingAUSpider(Spider):
+class BeaconLightingAUSpider(AmastyStoreLocatorSpider):
     name = "beacon_lighting_au"
-    item_attributes = {
-        "brand": "Beacon Lighting",
-        "brand_wikidata": "Q109626941",
-        "extras": Categories.SHOP_LIGHTING.value,
-    }
+    item_attributes = {"brand": "Beacon Lighting", "brand_wikidata": "Q109626941"}
     allowed_domains = ["www.beaconlighting.com.au"]
     start_urls = ["https://www.beaconlighting.com.au/beaconlocator/index/ajax/"]
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield JsonRequest(url=url)
+    def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
+        item["ref"] = feature["branch_code"]
+        item["branch"] = item.pop("name").removeprefix("Beacon Lighting ")
+        item["street_address"] = item.pop("addr_full")
+        item["state"] = feature["state_data"]["code"]
+        item["website"] = "https://www.beaconlighting.com.au/storelocator/" + quote(feature["url_key"])
+        item["image"] = feature["location_image"]
 
-    def parse(self, response):
-        for location in response.json()["items"]:
-            item = DictParser.parse(location)
-            item["ref"] = str(location["id"])
-            item["street_address"] = item.pop("addr_full")
-            item["state"] = location["state_data"]["code"]
-            item["image"] = location["location_image"]
-            item["website"] = "https://www.beaconlighting.com.au/storelocator/" + quote(location["url_key"]) + "/"
-
-            item["opening_hours"] = OpeningHours()
-            schedule_dict = loads(location["schedule_string"])
-            for day_name, day_details in schedule_dict.items():
-                if day_details[f"{day_name}_status"] != "1":
+        item["opening_hours"] = OpeningHours()
+        if feature.get("schedule_string"):
+            schedule = loads(feature["schedule_string"])
+            for day_name, day in schedule.items():
+                if day[f"{day_name}_status"] != "1":
                     continue
-                open_time_1 = day_details["from"]["hours"] + ":" + day_details["from"]["minutes"]
-                close_time_1 = day_details["break_to"]["hours"] + ":" + day_details["break_to"]["minutes"]
-                open_time_2 = day_details["break_from"]["hours"] + ":" + day_details["break_from"]["minutes"]
-                close_time_2 = day_details["to"]["hours"] + ":" + day_details["to"]["minutes"]
-                if close_time_1 == "00:00" and open_time_2 == "00:00":
-                    item["opening_hours"].add_range(day_name.title(), open_time_1, close_time_2)
+                open_time = day["from"]["hours"] + ":" + day["from"]["minutes"]
+                break_start = day["break_from"]["hours"] + ":" + day["break_from"]["minutes"]
+                break_end = day["break_to"]["hours"] + ":" + day["break_to"]["minutes"]
+                close_time = day["to"]["hours"] + ":" + day["to"]["minutes"]
+                if break_start == break_end:
+                    if open_time == close_time == "00:00":
+                        item["opening_hours"].set_closed(day_name)
+                    else:
+                        item["opening_hours"].add_range(day_name, open_time, close_time)
                 else:
-                    item["opening_hours"].add_range(day_name.title(), open_time_1, close_time_1)
-                    item["opening_hours"].add_range(day_name.title(), open_time_2, close_time_2)
+                    item["opening_hours"].add_range(day_name.title(), open_time, break_start)
+                    item["opening_hours"].add_range(day_name.title(), break_end, close_time)
 
-            yield item
+        apply_category(Categories.SHOP_LIGHTING, item)
+
+        yield item
