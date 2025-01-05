d0312415e150157bf7abbc1c1e77c708499b8bc8
Andrew Belt <andrewpbelt@gmail.com>
2025-01-05 07:54:38 -0500

    Rewrite Terminix with StructuredDataSpider.

--- locations/spiders/terminix.py
+++ locations/spiders/terminix.py
@@ -1,45 +1,26 @@
-import scrapy
+from scrapy.spiders import SitemapSpider
 
-from locations.items import Feature
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class TerminixSpider(scrapy.Spider):
-    # download_delay = 0.2
+class TerminixSpider(SitemapSpider, StructuredDataSpider):
     name = "terminix"
-    item_attributes = {"brand": "Terminix", "brand_wikidata": "Q7702831"}
-    allowed_domains = ["terminix.com"]
-    start_urls = ("https://www.terminix.com/exterminators/",)
+    item_attributes = {
+        "brand": "Terminix",
+        "brand_wikidata": "Q7702831",
+    }
+    sitemap_urls = ["https://www.terminix.com/sitemap.xml"]
+    sitemap_rules = [
+        (r"/exterminators/\w+/[\w\-]+-(\d+)/$", "parse_sd"),
+    ]
 
-    def parse(self, response):
-        urls = response.xpath('//*[@id="state-list"]/div[3]/div[3]/div//ul//a/@href').extract()
+    def pre_process_data(self, ld_data, **kwargs):
+        ld_data.pop("image", None)
 
-        for url in urls:
-            st = url[:2]
-            yield scrapy.Request(response.urljoin(st), callback=self.parse_state)
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        # Remove brand social links
+        item.pop("facebook", None)
 
-    def parse_state(self, response):
-        cityurls = response.xpath('//*[@id="content"]/div/div/div//a/@href').extract()
-
-        for city in cityurls:
-            yield scrapy.Request(response.urljoin(city), callback=self.parse_store)
-
-    def parse_store(self, response):
-        try:
-            phone = response.xpath('//span[@class="Phone Phone--desktop"]/text()').extract()[0]
-        except:
-            phone = ""
-
-        properties = {
-            "ref": response.url,
-            "name": response.xpath('//h3[@class="NAP-locationName"]/text()').extract()[0],
-            "addr_full": response.xpath('//span[@itemprop="streetAddress"]/text()').extract()[0],
-            "city": response.xpath('//span[@itemprop="addressLocality"]/text()').extract()[0],
-            "state": response.xpath('//span[@itemprop="addressRegion"]/text()').extract()[0],
-            "postcode": response.xpath('//span[@itemprop="postalCode"]/text()').extract()[0],
-            "country": response.xpath('//span[@itemprop="addressCountry"]/text()').extract()[0],
-            "phone": phone,
-            "lat": float(response.xpath('//span[@class="coordinates"]/meta[1]/@content').extract()[0]),
-            "lon": float(response.xpath('//span[@class="coordinates"]/meta[2]/@content').extract()[0]),
-        }
-
-        yield Feature(**properties)
+        apply_category(Categories.SHOP_PEST_CONTROL, item)
+        yield item
