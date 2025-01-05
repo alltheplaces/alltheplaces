b83d9688b8c6039be299f55f4c119eda28405af7
Andrew Belt <andrewpbelt@gmail.com>
2025-01-05 03:39:40 -0500

    Add Hobby Lobby spider

---
+++ locations/spiders/hobby_lobby.py
@@ -0,0 +1,44 @@
+from urllib.parse import urlparse
+
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class HobbyLobbySpider(SitemapSpider, StructuredDataSpider):
+    name = "hobby_lobby"
+    allowed_domains = ["hobbylobby.com"]
+    item_attributes = {
+        "brand": "Hobby Lobby",
+        "brand_wikidata": "Q5874938",
+    }
+    sitemap_urls = ["https://www.hobbylobby.com/sitemap-Store.xml"]
+    sitemap_rules = [
+        (r"/stores/search/(\d+)$", "parse_sd"),
+    ]
+    time_format = "%H:%M %p"
+    custom_settings = {
+        # Site has no rate limiting
+        "DOWNLOAD_DELAY": 0.1,
+    }
+    # There are only social links for branch, not location
+    search_for_twitter = False
+    search_for_facebook = False
+
+    def pre_process_data(self, ld_data, **kwargs):
+        ld_data.pop("name", None)
+        # Broken image link
+        ld_data.pop("image", None)
+
+        # ID/ref should be store number, not main website URL
+        url = urlparse(ld_data["url"])
+        ref = url.path.split("/")[-1]
+        ld_data["@id"] = ref
+        # Store URLs have the wrong domain
+        url = url._replace(scheme="https", netloc="www.hobbylobby.com")
+        ld_data["url"] = url.geturl()
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.SHOP_CRAFT, item)
+        yield item
