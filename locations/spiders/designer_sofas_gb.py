3777ff697ce237976248630fb72a23d4925dce3f
Joe Dixon <mjoe999@gmail.com>
2025-01-07 12:09:25 +0000

    [designer_sofas_gb] furniture chain (33 items)

---
+++ locations/spiders/designer_sofas_gb.py
@@ -0,0 +1,21 @@
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.stockist import StockistSpider
+
+
+class DesignerSofasGBSpider(StockistSpider):
+    name = "designer_sofas_gb"
+    item_attributes = {"brand": "Designer Sofas", "brand_wikidata": "Q131695219"}
+    key = "u19314"
+
+    def parse_item(self, item: Feature, location: dict):
+        for custom_field in location.get("custom_fields", []):
+            if custom_field["id"] == 7543:
+                item["website"] = custom_field["value"]
+            item["name"] = self.item_attributes["brand"]
+        if addr := item.pop("street_address"):
+            if postcode := item.get("postcode"):
+                addr += ", " + postcode
+            item["addr_full"] = addr
+        apply_category(Categories.SHOP_FURNITURE, item)
+        yield item
