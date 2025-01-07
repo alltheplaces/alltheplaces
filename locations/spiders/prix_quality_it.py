5cb9a9327100d2a1af68b67fc24c490f348c6faa
Cj Malone <CjMalone@mail.com>
2025-01-07 14:14:43 +0000

    Fix brand

--- locations/spiders/prix_quality_it.py
+++ locations/spiders/prix_quality_it.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 import scrapy
+from scrapy.http import Response
 
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_IT, OpeningHours
@@ -6,15 +9,10 @@ from locations.hours import DAYS_IT, OpeningHours
 
 class PrixQualityITSpider(scrapy.Spider):
     name = "prix_quality_it"
-    start_urls = [
-        # Utilises Wordpress' Advanced Custom Fields. Unfortunately this means little commonality in types or fields,
-        # beyond using a DictParser in the "acf" record and some of the url patterns
-        "https://www.prixquality.com/wp-json/acf/v3/shop?per_page=999&orderby=title",
-    ]
-    time_format = "%H:%M"
-    item_attributes = {"brand": "Prix Quality", "brand_wikidata": "Q61994819"}
+    item_attributes = {"brand": "Prix", "brand_wikidata": "Q61994819"}
+    start_urls = ["https://www.prixquality.com/wp-json/acf/v3/shop?per_page=999&orderby=title"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for record in response.json():
             store = record["acf"]
             item = DictParser.parse(store)
@@ -28,7 +26,5 @@ class PrixQualityITSpider(scrapy.Spider):
                         item["opening_hours"].set_closed(DAYS_IT[day])
                     else:
                         for time in times.split("|"):
-                            item["opening_hours"].add_range(
-                                DAYS_IT[day], *time.split("-"), time_format=self.time_format
-                            )
+                            item["opening_hours"].add_range(DAYS_IT[day], *time.split("-"), time_format="%H:%M")
             yield item
