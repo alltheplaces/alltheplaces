ded6186bce8ab0e4321f7e50558a30083f38e0f6
Andrew Belt <andrewpbelt@gmail.com>
2025-01-07 04:42:11 -0500

    Fix sitemap and HTML scraper from updated website

--- locations/spiders/texas_roadhouse.py
+++ locations/spiders/texas_roadhouse.py
@@ -1,9 +1,11 @@
-import json
+import re
+from urllib.parse import urljoin, urlparse
 
 import scrapy
 
 from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class TexasRoadhouseSpider(scrapy.Spider):
@@ -15,49 +17,46 @@ class TexasRoadhouseSpider(scrapy.Spider):
     allowed_domains = ["www.texasroadhouse.com"]
     start_urls = ("https://www.texasroadhouse.com/sitemap.xml",)
 
-    def parse_hours(self, store_hours):
-        opening_hours = OpeningHours()
-
-        for weekday in store_hours:
-            # convert day from full Monday to Mo, etc
-            day = weekday.get("day")[:2]
-            open_time = weekday.get("hours").get("openTime")
-            close_time = weekday.get("hours").get("closeTime")
-            opening_hours.add_range(
-                day=day,
-                open_time=open_time,
-                close_time=close_time,
-                time_format="%I:%M%p",
-            )
-
-        return opening_hours.as_opening_hours()
-
     def parse(self, response):
         response.selector.remove_namespaces()
-        city_urls = response.xpath("//url/loc/text()").extract()
-        for path in city_urls:
-            if path.startswith("https://www.texasroadhouse.com/locations/"):
+        location_urls = response.xpath("//url/loc/text()").getall()
+        for path in location_urls:
+            url = urlparse(path)
+            m = re.match(r"/locations/([-\w]+)", url.path)
+            if m:
+                # IDs starting with "null" are probably closed or future locations
+                if m.group(1).startswith("null"):
+                    continue
+                # Change domain from incorrect http://live-texas-roadhouse-marketing.pantheonsite.io
+                url = url._replace(scheme="https", netloc="www.texasroadhouse.com")
                 yield scrapy.Request(
-                    path.strip(),
-                    callback=self.parse_store,
+                    url.geturl(),
+                    callback=self.parse_location,
                 )
 
-    def parse_store(self, response):
-        data = json.loads(response.xpath("//script/text()").extract_first()[22:-1])
+    def parse_location(self, response):
+        item = Feature()
 
-        properties = {
-            "lat": data["latitude"],
-            "lon": data["longitude"],
-            "ref": data["url"],
-            "name": data["name"],
-            "addr_full": data["address1"],
-            "city": data["city"],
-            "state": data["state"],
-            "postcode": data["postalCode"],
-            "country": data["countryCode"],
-            "phone": data["telephone"],
-            "website": response.urljoin(data["url"]),
-            "opening_hours": self.parse_hours(data["schedule"]),
-        }
+        branch = response.css(".vendor-block-content-top h2::text").get().strip()
+        # Reject future locations
+        if "(Opening" in branch:
+            return
+        item["branch"] = branch
+        item["ref"] = response.url.split("/")[-1]
+        item["addr_full"] = merge_address_lines(response.css(".store-address::text").getall())
+        item["website"] = response.url
+        item["facebook"] = response.css(".store-facebookurl a::attr(href)").get().strip()
 
-        yield Feature(**properties)
+        oh = OpeningHours()
+        for line in response.css(".open-dine-hours ul li").getall():
+            oh.add_ranges_from_string(line)
+        item["opening_hours"] = oh.as_opening_hours()
+
+        item["extras"]["website:orders"] = urljoin(
+            response.url, response.css(".cta-ordering a[href$='/start-order']::attr(href)").get()
+        )
+        item["extras"]["website:menu"] = urljoin(
+            response.url, response.css(".cta-ordering a[href$='/digital-menu']::attr(href)").get()
+        )
+
+        yield item
