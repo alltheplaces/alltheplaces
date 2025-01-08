5d44666aa415048182c0756a4c42ebb0ebfb6acd
Cj Malone <CjMalone@mail.com>
2025-01-08 13:31:30 +0000

    Clean name

--- locations/spiders/okta_mk.py
+++ locations/spiders/okta_mk.py
@@ -1,9 +1,21 @@
-from locations.categories import Categories
+from typing import Iterable
+
+from scrapy import Selector
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.storefinders.lighthouse import LighthouseSpider
 
 
 class OktaMKSpider(LighthouseSpider):
     name = "okta_mk"
-    item_attributes = {"brand": "Окта", "brand_wikidata": "Q3350105", "extras": Categories.FUEL_STATION.value}
+    item_attributes = {"brand": "Окта", "brand_wikidata": "Q3350105"}
     allowed_domains = ["www.okta-elpe.com"]
     start_urls = ["https://www.okta-elpe.com/service-stations/find-a-station/"]
+
+    def parse_item(self, item: Feature, location: Selector) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("ОКТА ")
+
+        apply_category(Categories.FUEL_STATION, item)
+
+        yield item
