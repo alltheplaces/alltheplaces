3aea32347c90ff5e177d3790bcb2c40074d8339e
David Hicks <david@hicks.id.au>
2025-01-11 00:45:57 +1100

    alertcalifornia_us: properly use JSONBlobSpider helper functions

--- locations/spiders/alertcalifornia_us.py
+++ locations/spiders/alertcalifornia_us.py
@@ -12,32 +12,31 @@ class AlertcaliforniaUSSPider(JSONBlobSpider):
     item_attributes = {"operator": "University of California, San Diego", "operator_wikidata": "Q622664"}
     allowed_domains = ["cameras.alertcalifornia.org"]
     start_urls = ["https://cameras.alertcalifornia.org/public-camera-data/all_cameras-v3.json"]
+    locations_key = "features"
 
-    def parse(self, response: Response) -> Iterable[Feature]:
-        for camera in response.json()["features"]:
-            if coordinates := camera["geometry"].get("coordinates"):
-                if coordinates[0] is None:
-                    # Unused (?) cameras appear to exist as cameras lacking coordinates
-                    continue
+    def pre_process_data(self, feature: dict) -> None:
+        feature.update(feature["properties"])
+        del feature["properties"]
 
-            properties = {
-                "ref": camera["properties"]["id"],
-                "name": camera["properties"]["name"],
-                "geometry": camera["geometry"],
-                "website": "https://cameras.alertcalifornia.org/?id={}".format(camera["properties"]["id"]),
-            }
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if coordinates := feature["geometry"].get("coordinates"):
+            if coordinates[0] is None:
+                # Unused (?) cameras appear to exist as cameras lacking coordinates
+                return
 
-            apply_category(Categories.SURVEILLANCE_CAMERA, properties)
+            item["website"] = "https://cameras.alertcalifornia.org/?id={}".format(item["ref"])
+
+            apply_category(Categories.SURVEILLANCE_CAMERA, item)
 
             # Azimuths can be changed over time so the camera direction is not
             # always a fixed/stable attribute. This extracted value for
             # panning cameras could possibly just be a centre of the field of
             # view the camera pans across.
-            if direction := camera["properties"].get("az_current"):
-                properties["extras"]["camera:direction"] = str(direction)
+            if direction := feature.get("az_current"):
+                item["extras"]["camera:direction"] = str(direction)
 
-            properties["extras"]["contact:webcam"] = (
-                "https://cameras.alertcalifornia.org/public-camera-data/{}/latest-frame.jpg".format(properties["ref"])
+            item["extras"]["contact:webcam"] = (
+                "https://cameras.alertcalifornia.org/public-camera-data/{}/latest-frame.jpg".format(item["ref"])
             )
 
-            yield Feature(**properties)
+            yield item
