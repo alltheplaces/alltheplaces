2b681f9579e2c98ccf313f4bee2200c87fd69b4b
David Hicks <david@hicks.id.au>
2025-01-11 02:31:33 +1100

    settings: fix typo in pipeline import

--- locations/settings.py
+++ locations/settings.py
@@ -115,7 +115,7 @@ ITEM_PIPELINES = {
     "locations.pipelines.closed.ClosePipeline": 650,
     "locations.pipelines.apply_nsi_categories.ApplyNSICategoriesPipeline": 700,
     "locations.pipelines.check_item_properties.CheckItemPropertiesPipeline": 750,
-    "locations.pipelines.geojson_multipoint_simplication.GeoJSONMultiPointSimplificationPipeline": 760,
+    "locations.pipelines.geojson_multipoint_simplification.GeoJSONMultiPointSimplificationPipeline": 760,
     "locations.pipelines.count_categories.CountCategoriesPipeline": 800,
     "locations.pipelines.count_brands.CountBrandsPipeline": 810,
     "locations.pipelines.count_operators.CountOperatorsPipeline": 820,
