5e3be35826a3abc2be13dd48b7ae789fc85e5000
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-01-13 18:18:01 +0530

    code refactored to fix spider

--- locations/spiders/kbp_foods_us.py
+++ locations/spiders/kbp_foods_us.py
@@ -5,10 +5,14 @@ from scrapy import Selector, Spider
 from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
 from locations.items import Feature
+from locations.spiders.arbys_us import ArbysUSSpider
 from locations.spiders.aw_restaurants import AwRestaurantsSpider
 from locations.spiders.kfc_us import KFC_SHARED_ATTRIBUTES
 from locations.spiders.ljsilvers import LjsilversSpider
+from locations.spiders.sonic_drivein import SonicDriveinSpider
+from locations.spiders.walmart_us import WalmartUSSpider
 from locations.spiders.taco_bell_us import TACO_BELL_SHARED_ATTRIBUTES
 
 brands_map = {
@@ -16,59 +20,32 @@ brands_map = {
     "KB": [KFC_SHARED_ATTRIBUTES, TACO_BELL_SHARED_ATTRIBUTES],
     "KFC": [KFC_SHARED_ATTRIBUTES],
     "KL": [KFC_SHARED_ATTRIBUTES, LjsilversSpider.item_attributes],
+    "KT": [KFC_SHARED_ATTRIBUTES, TACO_BELL_SHARED_ATTRIBUTES],
+    "KW": [KFC_SHARED_ATTRIBUTES, WalmartUSSpider.item_attributes],
+    "Sonic": [SonicDriveinSpider.item_attributes],
+    "Arbys": [
+        ArbysUSSpider.item_attributes,
+    ],
 }
 
 
 class KbpFoodsUSSpider(Spider):
     name = "kbp_foods_us"
-    start_urls = ["https://kbp-foods.com/wp-admin/admin-ajax.php?action=get_all_locations"]
+    start_urls = ["https://kbpbrands.com/api/locations"]
     item_attributes = {"operator": "KBP Foods"}
     brands = {}
-    start_dates = {}
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json()["locations"]:
-            self.brands[location["title"]] = location["brand"]
-            self.start_dates[location["title"]] = datetime.strptime(location["date_opened"], "%m/%d/%Y").strftime(
-                "%Y-%m-%d"
-            )
-
-            yield self.make_request(1)
-
-    def make_request(self, page: int) -> JsonRequest:
-        return JsonRequest(
-            url="https://kbp-foods.com/wp-json/facetwp/v1/refresh",
-            data={
-                "action": "facetwp_refresh",
-                "data": {"facets": {"proximity": [], "map": []}, "template": "locations", "paged": page},
-            },
-            cb_kwargs={"page": page},
-            callback=self.parse_locations,
-        )
-
-    def parse_locations(self, response: Response, **kwargs: Any) -> Any:
-        if data := response.json()["settings"]["map"]["locations"]:
-            for store in data:
-                item = Feature()
-                html_data = Selector(text=store["content"])
-                item["ref"] = html_data.xpath("//h2/text()").get()
-                item["lat"] = store["position"]["lat"]
-                item["lon"] = store["position"]["lng"]
-                item["addr_full"] = html_data.xpath('//*[@class="fwpl-item"]/text()').get()
-                item["phone"] = html_data.xpath('//*[contains(@href, "tel:")]/text()').get()
-
-                apply_category(Categories.FAST_FOOD, item)
-
-                item["extras"]["start_date"] = self.start_dates[item["ref"]]
-
-                if brands := brands_map.get(self.brands[item["ref"]]):
-                    for b in brands:
-                        i = item.deepcopy()
-                        i["ref"] = "{}-{}".format(item["ref"], b["brand"])
-                        i.update(b)
-                        yield i
-                else:
-                    self.logger.error("Unmapped brand: {}".format(self.brands[item["ref"]]))
-                    yield item
-
-            yield self.make_request(kwargs["page"] + 1)
+        for store in response.json():
+            item = DictParser.parse(store)
+            item["street_address"] = item.pop("addr_full")
+            apply_category(Categories.FAST_FOOD, item)
+            if brands := brands_map.get(store["type"].strip()):
+                for b in brands:
+                    i = item.deepcopy()
+                    i["brand"] = "{}".format(b["brand"])
+                    i.update(b)
+                    yield i
+            else:
+                self.logger.error("Unmapped brand: {}".format(store["type"]))
+                yield item
