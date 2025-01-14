bb35500612e71b6cb65a84a64829994294ebe585
Karen Kerr <karenkerrgithub@gmail.com>
2025-01-14 15:20:16 +0000

    [Vitalia] Fix non running spider

--- locations/spiders/vitalia.py
+++ locations/spiders/vitalia.py
@@ -1,70 +1,29 @@
-import json
-import re
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-import scrapy
-
-from locations.hours import OpeningHours
 from locations.items import Feature
 
-DAY_MAPPING = {
-    "monday": "Mo",
-    "tuesday": "Tu",
-    "wednesday": "We",
-    "thursday": "Th",
-    "friday": "Fr",
-    "saturday": "Sa",
-    "sunday": "Su",
-}
 
-
-class VitaliaSpider(scrapy.Spider):
+class VitaliaSpider(CrawlSpider):
     name = "vitalia"
     item_attributes = {"brand": "Vitalia Reformhaus", "brand_wikidata": "Q2528558"}
     allowed_domains = ["www.vitalia-reformhaus.de"]
     start_urls = ["https://www.vitalia-reformhaus.de/marktfinder"]
-
-    def parse_hours(self, store_hours):
-        opening_hours = OpeningHours()
-        if store_hours is None:
-            return
-
-        for store_day in store_hours:
-            opening_hours.add_range(
-                day=DAY_MAPPING[store_day],
-                open_time=f"{store_hours[store_day]['from']['hours']}:" f"{store_hours[store_day]['from']['minutes']}",
-                close_time=f"{store_hours[store_day]['to']['hours']}:" f"{store_hours[store_day]['to']['minutes']}",
-                time_format="%H:%M",
-            )
-        return opening_hours.as_opening_hours()
+    rules = [
+        Rule(
+            LinkExtractor(allow=".*/marktfinder/vitalia.*"),
+            callback="parse",
+            follow=False,
+        )
+    ]
 
     def parse(self, response):
-        match = re.search(r"jsonLocations: (.+),", response.text)
-        if match:
-            data = match.group(1)
-            stores = json.loads(data)
-
-            for store in stores["items"]:
-                properties = {
-                    "ref": store["id"],
-                    "name": store["name"],
-                    "street": store["address"],
-                    "city": store["city"],
-                    "postcode": store["zip"],
-                    "country": store["country"],
-                    "lat": store["lat"],
-                    "lon": store["lng"],
-                    "phone": store["phone"],
-                    "email": store["email"],
-                    "website": store["website"],
-                    "extras": {
-                        "category": store["category"],
-                    },
-                }
-
-                if store["schedule_string"]:
-                    opening_hours = json.loads(store["schedule_string"])
-                    hours = self.parse_hours(opening_hours)
-                    if hours:
-                        properties["opening_hours"] = hours
-
-                yield Feature(**properties)
+        item = Feature()
+        item["ref"] = item["website"] = response.url
+        item["city"] = response.xpath('//span[contains(., "Stadt:")]/following-sibling::span/text()').get()
+        item["street_address"] = response.xpath(
+            '//span[contains(., "Anschrift:")]/following-sibling::span/text()'
+        ).get()
+        item["postcode"] = response.xpath('//span[contains(., "PLZ:")]/following-sibling::span/text()').get()
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/@href').get().replace("tel:", "")
+        yield item
