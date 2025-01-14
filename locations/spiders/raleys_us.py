a4e59deb8bac0f21cb21b3fe06cd552c511268cf
Cj Malone <CjMalone@mail.com>
2025-01-14 14:03:45 +0000

    Match Raley's ONE branding with OSM

--- locations/spiders/raleys_us.py
+++ locations/spiders/raleys_us.py
@@ -1,14 +1,14 @@
 from scrapy.http import JsonRequest, Request
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
 
-BRAND_WIKIDATA = {
-    "Bel Air": "Q112922067",
-    "Nob Hill Foods": "Q121816894",
-    "Raley's": "Q7286970",
-    "Raley's ONE Market": "Q7286970",
+BRAND_MAP = {
+    "Bel Air": {"brand": "Bel Air", "brand_wikidata": "Q112922067"},
+    "Nob Hill Foods": {"name": "Nob Hill Foods", "brand": "Nob Hill Foods", "brand_wikidata": "Q121816894"},
+    "Raley's": {"brand": "Raley's", "brand_wikidata": "Q7286970"},
+    "Raley's ONE Market": {"name": "Raley's O-N-E Market", "brand": "Raley's", "brand_wikidata": "Q7286970"},
 }
 
 
@@ -30,8 +30,11 @@ class RaleysUSSpider(JSONBlobSpider):
     def post_process_item(self, item, response, location):
         item["ref"] = location["number"]
         item["website"] = response.urljoin(f"/store/{location['number']}")
-        item["brand"] = item["name"] = location["brand"]["name"]
-        item["brand_wikidata"] = BRAND_WIKIDATA.get(location["brand"]["name"])
+        if brand := BRAND_MAP.get(location["brand"]["name"]):
+            item["name"] = None
+            item.update(brand)
+        else:
+            self.logger.error("Unexpected brand: {}".format(location["brand"]["name"]))
         item["street_address"] = item.pop("street")
 
         oh = OpeningHours()
