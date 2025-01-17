37434344260716cfb5e588d3fcaeed87ebadc807
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-01-17 19:42:07 +0530

    Use Sitemap and SD to fix Captain D Spider

--- locations/spiders/captain_d.py
+++ locations/spiders/captain_d.py
@@ -1,7 +1,10 @@
-from locations.storefinders.momentfeed import MomentFeedSpider
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CaptainDSpider(MomentFeedSpider):
+class CaptainDSpider(SitemapSpider, StructuredDataSpider):
     name = "captain_d"
     item_attributes = {"brand": "Captain D's", "brand_wikidata": "Q5036616"}
-    api_key = "AJXCZOENNNXKHAKZ"
+    sitemap_urls = ["https://locations.captainds.com/robots.txt"]
+    sitemap_rules = [(r"https://locations.captainds.com/ll/US/\w{2}/[-\w]+/[-\w]+", "parse_sd")]
