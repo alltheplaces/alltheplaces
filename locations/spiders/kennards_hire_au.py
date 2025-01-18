19b5dec41ae7f0c044bed96696488879ff03c6e1
David Hicks <david@hicks.id.au>
2025-01-18 20:23:03 +1100

    Add Kennards Hire (shop/plant_hire) (AU/NZ) (218 locations)
    Fixes #6544

---
+++ locations/spiders/kennards_hire_au.py
@@ -0,0 +1,41 @@
+from typing import Iterable
+from urllib.parse import urlparse
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours, DAYS_FULL
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.pipelines.address_clean_up import merge_address_lines
+
+
+class KennardsHireAUSpider(JSONBlobSpider):
+    name = "kennards_hire_au"
+    item_attributes = {"brand": "Kennards Hire", "brand_wikidata": "Q63557330"}
+    allowed_domains = ["www.kennards.com.au"]
+    start_urls = ["https://www.kennards.com.au/api/data/branches"]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature["code"])
+        item["branch"] = item.pop("name", None)
+        item["street_address"] = merge_address_lines([feature["address"].get("streetLine1"), feature["address"].get("streetLine2")])
+        item["city"] = feature["address"].get("suburb")
+        item["state"] = feature["address"].get("state")
+        item["postcode"] = feature["address"].get("postcode")
+        item["country"] = feature["address"].get("country")
+        if website_path := feature.get("url"):
+            item["website"] = "https://" + urlparse(response.url).netloc + website_path
+
+        item["opening_hours"] = OpeningHours()
+        for day_hours in feature.get("workingHours", []):
+            if day_hours["title"] not in DAYS_FULL:
+                continue
+            if not day_hours.get("openingTime") or not day_hours.get("closingTime"):
+                item["opening_hours"].set_closed(day_hours["title"])
+                continue
+            item["opening_hours"].add_range(day_hours["title"], day_hours["openingTime"], day_hours["closingTime"], "%I:%M%p")
+
+        apply_category(Categories.SHOP_PLANT_HIRE, item)
+
+        yield item
