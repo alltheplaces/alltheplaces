d995c28da6388e220c3e68dc699456b67e946d16
Ian Dees <ian.dees@gmail.com>
2025-01-18 12:51:27 -0600

    Simplify website URL handling

--- locations/spiders/handelsbanken_se.py
+++ locations/spiders/handelsbanken_se.py
@@ -29,9 +29,8 @@ class HandelsbankenSESpider(scrapy.Spider):
                 oh_json = json.loads(options.get("value"))
                 for day in oh_json:
                     oh.add_range(DAYS[int(day.get("Weekday"))], day.get("Open"), day.get("Close"))
-            if website := store.get("url"):
-                if website.startswith("www."):
-                    website = website.replace("www.", "https://www.")
+            if website := store.get("url") and website.startswith("www."):
+                website = website.replace("www.", "https://www.")
             properties = {
                 "ref": str(store.get("id")),
                 "name": store.get("name"),
