8563bd0f1397ba31c616fdd48ed08d2142151638
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-01-21 19:36:27 +0530

    [Insta Volt GB] Fix Spider (#12028)
    * updated start_urls and parse method to fix spider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/insta_volt_gb.py
+++ locations/spiders/insta_volt_gb.py
@@ -4,6 +4,7 @@ from scrapy import FormRequest, Spider
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import clean_address
 
 
 class InstaVoltGBSpider(Spider):
@@ -13,9 +14,8 @@ class InstaVoltGBSpider(Spider):
 
     def parse(self, response, **kwargs):
         self.ajax_nonce = re.search(r'"ajax_nonce":"(\w+)"', response.text).group(1)
-
         yield FormRequest(
-            url="https://instavolt.co.uk/wp-json/rouge-location-hub/v1/location-hubs",
+            url="https://instavolt.co.uk/wp-json/instavolt-location/v1/location",
             formdata={"security": self.ajax_nonce},
             callback=self.parse_locations,
         )
@@ -23,14 +23,15 @@ class InstaVoltGBSpider(Spider):
     def parse_locations(self, response, **kwargs):
         for location in response.json():
             item = DictParser.parse(location)
-
-            item["addr_full"] = location["address_lines_string"]
-            item["ref"] = location["alias"]
-            item["website"] = f'https://instavolt.co.uk/location-hub/{location["alias"]}'
-
-            apply_yes_no(Extras.TOILETS, item, "facilities^toilet" in location.get("amenities", []))
-            apply_yes_no(Extras.WIFI, item, "facilities^free_wifi" in location.get("amenities", []))
-
+            item["branch"] = (
+                item.pop("name").removeprefix(f'{location["location_id"]} &#8211; ').replace("&#8211;", ",")
+            )
+            item["name"] = self.item_attributes["brand"]
+            item["addr_full"] = clean_address(location["address_lines"])
+            item["ref"] = location["location_id"]
+            item["website"] = location["permalink"]
+            extras_list = [extras["name"] for extras in location["amenities"]]
+            apply_yes_no(Extras.WIFI, item, True if "Free WIFI" in extras_list else False)
+            apply_yes_no(Extras.TOILETS, item, True if "Toilet" in extras_list else False)
             apply_category(Categories.CHARGING_STATION, item)
-
             yield item
