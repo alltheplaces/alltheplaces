ff36283546e35e3ee2198e94d56e202ffc4838f3
Cj Malone <CjMalone@mail.com>
2025-01-21 10:18:29 +0000

    Collect social media

--- locations/spiders/lounges_gb.py
+++ locations/spiders/lounges_gb.py
@@ -4,6 +4,7 @@ from scrapy.spiders import SitemapSpider
 
 from locations.open_graph_spider import OpenGraphSpider
 from locations.pipelines.address_clean_up import merge_address_lines
+from locations.structured_data_spider import extract_email, extract_facebook, extract_instagram
 
 
 class LoungesGBSpider(SitemapSpider, OpenGraphSpider):
@@ -18,5 +19,9 @@ class LoungesGBSpider(SitemapSpider, OpenGraphSpider):
         lounge_header = merge_address_lines(response.xpath('//section[@class="lounge-header"]/p//text()').getall())
         if m := re.match(r"(.+)(?: •|,) ([\d ]+) •", lounge_header):
             item["addr_full"], item["phone"] = m.groups()
+
+        extract_email(item, response)
+        extract_facebook(item, response)
+        extract_instagram(item, response)
 
         yield item
