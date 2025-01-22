3d67a6ccc137935b2512cb2c5072d9a14569b508
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-01-22 09:50:00 +0530

    updated sitemap_rules to fix spider

--- locations/spiders/qdoba.py
+++ locations/spiders/qdoba.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -8,5 +10,9 @@ class QdobaSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Qdoba", "brand_wikidata": "Q1363885"}
     allowed_domains = ["qdoba.com"]
     sitemap_urls = ["https://locations.qdoba.com/sitemap.xml"]
-    sitemap_rules = [(r"com/\w\w/\w\w/[^/]+/[^/]+\.html", "parse")]
-    wanted_types = ["Restaurant"]
+    sitemap_rules = [(r"com/[^/]+/[^/]+/[^/]+/.*", "parse")]
+    wanted_types = ["Organization"]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        if ld_data.get("geo"):
+            yield item
