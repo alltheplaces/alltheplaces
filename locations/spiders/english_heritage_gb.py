10cd4e5fbf9071fdcd86d2c19d27de0df3bc8378
Cj Malone <CjMalone@mail.com>
2025-01-23 14:55:59 +0000

    Skip NSI matching

--- locations/spiders/english_heritage_gb.py
+++ locations/spiders/english_heritage_gb.py
@@ -6,15 +6,11 @@ from locations.user_agents import BROWSER_DEFAULT
 
 class EnglishHeritageGBSpider(JSONBlobSpider):
     name = "english_heritage_gb"
-    item_attributes = {"brand": "English Heritage", "brand_wikidata": "Q936287"}
+    item_attributes = {"brand": "English Heritage", "brand_wikidata": "Q936287", "nsi_id": "N/A"}
     start_urls = ["https://www.english-heritage.org.uk/api/PropertySearch/GetAll"]
     no_refs = True
     locations_key = "Results"
-    custom_settings = {
-        "DEFAULT_REQUEST_HEADERS": {
-            "user-agent": BROWSER_DEFAULT,
-        },
-    }
+    user_agent = BROWSER_DEFAULT
 
     def post_process_item(self, item, response, location):
         item["website"] = urljoin("https://www.english-heritage.org.uk", location["Path"])
