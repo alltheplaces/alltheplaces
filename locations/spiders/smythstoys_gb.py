1b06049b87a805379bd7a68e725efe2d340e19f3
Mateusz Konieczny <matkoniecz@gmail.com>
2025-01-27 18:36:12 +0100

    [smythstoys_gb] brand=Smyths (#12078)
    To match their website, Wikipedia, NSI, OpenStreetMap

--- locations/spiders/smythstoys_gb.py
+++ locations/spiders/smythstoys_gb.py
@@ -3,6 +3,6 @@ from locations.storefinders.woosmap import WoosmapSpider
 
 class SmythstoysGBSpider(WoosmapSpider):
     name = "smythstoys_gb"
-    item_attributes = {"brand": "SmythsToys", "brand_wikidata": "Q7546779"}
+    item_attributes = {"brand": "Smyths", "brand_wikidata": "Q7546779"}
     key = "woos-49066dab-d11d-3614-98c7-ab241da1565b"
     origin = "https://www.smythstoys.com/"
