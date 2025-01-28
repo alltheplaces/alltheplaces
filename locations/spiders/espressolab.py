e551df8b6b21bcc8c1ce5c8c7e310a2493dac520
Spencer Alves <impiaaa@gmail.com>
2025-01-28 11:25:35 -0800

    Espressolab: Rewrite to use CrawlSpider

--- locations/spiders/espressolab.py
+++ locations/spiders/espressolab.py
@@ -1,61 +1,31 @@
 import re
-from typing import Iterable
 
-import scrapy
-from lxml import etree
 from scrapy import http
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-from locations.categories import Categories, apply_category
 from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address
-from locations.user_agents import BROWSER_DEFAULT
+from locations.pipelines.address_clean_up import merge_address_lines
 
-MAP_SCRIPT_REGEX = re.compile(r"google\.maps\.LatLng\([ ]*([-0-9.]*)[ ]*,[ ]*([-0-9.]*)[ ]*\);")
+MAP_SCRIPT_REGEX = re.compile(r"google\.maps\.LatLng\(\s*([-\d.]*)\s*,\s*([-\d.]*)\s*\);")
 
 
-class EspressolabSpider(scrapy.Spider):
+class EspressolabSpider(CrawlSpider):
     name = "espressolab"
     item_attributes = {"brand": "Espressolab", "brand_wikidata": "Q97599059"}
+    allowed_domains = ["espressolab.com"]
+    start_urls = ["https://espressolab.com/subeler/"]
+    rules = [Rule(LinkExtractor(allow=[r"/subeler/[\w-]+/"]), callback="parse_item")]
 
-    user_agent = BROWSER_DEFAULT
-    require_proxy = True
-
-    def start_requests(self) -> Iterable[scrapy.Request]:
-        yield scrapy.Request(url="https://espressolab.com/subeler/", callback=self.parse_stores_page)
-
-    def parse_stores_page(self, response: http.HtmlResponse):
-        for url_path in response.xpath(
-            "/html/body/form/div[4]/div[4]/div[1]/div/div/div/div[1]/div/div/div[2]/ul/li/a/@href"
-        ).getall():
-            yield scrapy.Request(url="https://espressolab.com" + url_path, callback=self.parse_store_page)
-
-    def parse_store_page(self, response: http.HtmlResponse):
-        name = response.xpath("//h1/text()").get()
-        address = extract_text(response.xpath("//div[contains(@class, 'address')]").get())
-        latitude, longitude = extract_coords(response.xpath("/html/head/script[7]/text()").get())
-
-        item = Feature(
-            **{
-                "ref": response.url.split("/")[-2],
-                "name": name,
-                "addr_full": address,
-                "lat": latitude,
-                "lon": longitude,
-            }
+    def parse_item(self, response: http.HtmlResponse):
+        item = Feature()
+        item["ref"] = response.url.split("/")[-2]
+        item["name"] = response.xpath("//h1/text()").get()
+        item["addr_full"] = merge_address_lines(response.css(".address ::text").getall())
+        item["lat"], item["lon"] = map(
+            float,
+            MAP_SCRIPT_REGEX.search(
+                response.xpath("//script[contains(text(), 'google.maps.LatLng')]/text()").get()
+            ).groups(),
         )
-
-        apply_category(Categories.COFFEE_SHOP, item)
-
         yield item
-
-
-def extract_text(address_element: str | None) -> str | None:
-    if address_element is None:
-        return None
-    node = etree.fromstring(address_element)
-    return clean_address("".join(node.itertext()).strip())
-
-
-def extract_coords(script: str) -> tuple[float, float] | None:
-    coords = MAP_SCRIPT_REGEX.findall(script)[0]
-    return float(coords[0]), float(coords[1])
