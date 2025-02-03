3417433148e99f539b6e21efa5b1a300af25529b
Spencer Alves <impiaaa@gmail.com>
2025-02-03 13:03:47 -0800

    Best Buy: Flag closed stores

--- locations/spiders/best_buy.py
+++ locations/spiders/best_buy.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories
+from locations.items import set_closed
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -11,3 +12,8 @@ class BestBuySpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://stores.bestbuy.com/sitemap.xml"]
     sitemap_rules = [(r"/[a-z]{2}/[\w-]+/[\w-]+.html", "parse_sd")]
     search_for_image = False
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if response.css(".closed-banner-header-shop-online"):
+            set_closed(item)
+        yield item
