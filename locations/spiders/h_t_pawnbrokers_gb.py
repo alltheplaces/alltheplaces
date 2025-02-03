b01c40be6630daecbe02467f6ba43c8885e85915
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-02-03 14:36:43 +0000

    Fix name of spider

--- locations/spiders/h_t_pawnbrokers_gb.py
+++ locations/spiders/h_t_pawnbrokers_gb.py
@@ -6,7 +6,7 @@ from locations.json_blob_spider import JSONBlobSpider
 
 
 class HTPawnbrokersGBSpider(JSONBlobSpider):
-    name = "h_t_pawnbrokers"
+    name = "h_t_pawnbrokers_gb"
     item_attributes = {"brand": "H&T Pawnbrokers", "brand_wikidata": "Q105672451"}
     start_urls = ["https://as-handt-store-address-service.azurewebsites.net/api/AllStoreData"]
 
