3457f37f04a01dd8ea1a143445c541903154ca79
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-02-03 12:18:03 +0000

    Update h_t_pawnbrokers_gb.py

--- locations/spiders/h_t_pawnbrokers_gb.py
+++ locations/spiders/h_t_pawnbrokers_gb.py
@@ -6,3 +6,9 @@ class HTPawnbrokersGBSpider(JSONBlobSpider):
     name = "h_t_pawnbrokers"
     item_attributes = {"brand": "H&T Pawnbrokers", "brand_wikidata": "Q105672451"}
     start_urls = ["https://as-handt-store-address-service.azurewebsites.net/api/AllStoreData"]
+
+    def post_process_item(self, item, response, location):
+        item["branch"]=item.pop("name")
+        item["name"]="H&T Pawnbrokers"
+        if item["lat"]:
+            yield item
