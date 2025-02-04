ba019767a5297f1815f66cd4b782cd57450fb03b
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-02-04 16:29:04 +0000

    [Radley GB] Add Spider

---
+++ locations/spiders/radley_gb.py
@@ -0,0 +1,42 @@
+from typing import Any
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations import google_url
+
+
+class RadleyGBSpider(CrawlSpider):
+    name = "radley_gb"
+    item_attributes = {
+        "brand": "Radley",
+        "brand_wikidata": "Q7281436",
+        "country": "GB",
+    }
+    allowed_domains = ["radley.co.uk"]
+    start_urls = ["https://www.radley.co.uk/stores"]
+    rules = [
+        Rule(
+            LinkExtractor(allow=r"/stores/outlets/([-\w]+)$"),
+            callback="parse",
+            follow=False,
+        ),
+        Rule(
+            LinkExtractor(allow=r"/stores/shops/([-\w]+)$"),
+            callback="parse",
+            follow=False,
+        )
+
+    ]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        print (response)
+        item = Feature()
+
+        item["website"] = response.url
+        item["ref"] = response.url
+        item["lat"] = response.xpath("//script/@latitude").get()
+        item["lon"] = response.xpath("//script/@longitude").get()
+        yield item
