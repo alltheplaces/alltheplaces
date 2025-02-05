6a9fb09e418ff1fe7e0d833ac48d111629f1c81e
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-05 13:33:54 +0530

    Add coordinates

--- locations/spiders/fcbanking.py
+++ locations/spiders/fcbanking.py
@@ -1,5 +1,9 @@
+import json
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -9,3 +13,9 @@ class FcbankingSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://www.fcbanking.com/robots.txt"]
     sitemap_rules = [(r"/branch-locations/[-\w]+/[-\w]+/[-\w]+/[-\w]+", "parse_sd")]
     wanted_types = ["BankOrCreditUnion"]  # Capture only url specific linked data
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        location = json.loads(response.xpath("//@data-location").get(""))
+        item["lat"] = location.get("latLng", {}).get("latitude")
+        item["lon"] = location.get("latLng", {}).get("longitude")
+        yield item
