8f8f829bd4cbd421da98fa96590ad0ce6347e164
Karen Kerr <karenkerrgithub@gmail.com>
2025-02-05 13:55:58 +0000

    [Brookshires US] Add category

--- locations/spiders/brookshires_us.py
+++ locations/spiders/brookshires_us.py
@@ -1,3 +1,4 @@
+from locations.categories import Categories, apply_category
 from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
 
 
@@ -6,3 +7,7 @@ class BrookshiresUSSpider(StorefrontgatewaySpider):
     item_attributes = {"brand": "Brookshire's", "brand_wikidata": "Q4975085"}
     start_urls = ["https://storefrontgateway.brookshires.com/api/stores"]
     requires_proxy = "US"  # Cloudflare geoblocking
+
+    def post_process_item(self, item, response, location):
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+        yield item
