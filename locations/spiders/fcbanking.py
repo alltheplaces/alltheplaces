6b75cd4056ccdb1a5f33085ff9330da7b24475db
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-05 13:07:27 +0530

    Fix Fcbanking Spider

--- locations/spiders/fcbanking.py
+++ locations/spiders/fcbanking.py
@@ -1,37 +1,11 @@
-import json
-import re
-import urllib.parse
-
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories, apply_category
-from locations.linked_data_parser import LinkedDataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class FcbankingSpider(SitemapSpider):
+class FcbankingSpider(SitemapSpider, StructuredDataSpider):
     name = "fcbanking"
     item_attributes = {"brand": "First Commonwealth Bank", "brand_wikidata": "Q5452773"}
-    allowed_domains = ["www.fcbanking.com"]
     sitemap_urls = ["https://www.fcbanking.com/robots.txt"]
-    sitemap_follow = ["branch-locations"]
-    sitemap_rules = [(r"branch-locations/.", "parse")]
-
-    def parse(self, response):
-        map_script = response.xpath('//script/text()[contains(., "setLat")]').get()
-        map_script = map_script.replace("\r", "\n")
-        lat = re.search(r'setLat\("(.*)"\)', map_script)[1]
-        lon = re.search(r'setLon\("(.*)"\)', map_script)[1]
-
-        ldjson = response.xpath('//script[@type="application/ld+json"]/text()').get()
-        ldjson = ldjson.replace("\r", "\n")
-        data = json.loads(re.sub(r"^//.*$", "", ldjson, flags=re.M))
-        item = LinkedDataParser.parse_ld(data)
-        item["lat"] = lat
-        item["lon"] = lon
-        item["website"] = response.url
-        path = urllib.parse.urlsplit(response.url).path
-        item["ref"] = path.removeprefix("/branch-locations")
-        hours_fixed = [re.sub(r"([ap])\.m\.?", r"\1m", row).replace("\u2013", "-") for row in data["openingHours"]]
-        item["opening_hours"] = LinkedDataParser.parse_opening_hours({"openingHours": hours_fixed}, "%I:%M %p")
-        apply_category(Categories.BANK, item)
-        yield item
+    sitemap_rules = [(r"/branch-locations/[-\w]+/[-\w]+/[-\w]+/[-\w]+", "parse_sd")]
+    wanted_types = ["BankOrCreditUnion"]  # Capture only url specific linked data
