1ee8abf7ac977afbbd9978228f12d5f016097587
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-02-12 01:39:34 +0530

    [Pizza Hut VN] Fix  Spider (#12221)
    * Fix Pizza Hut VN Spider
    
    * Add branch

--- locations/spiders/pizza_hut_vn.py
+++ locations/spiders/pizza_hut_vn.py
@@ -12,11 +12,15 @@ class PizzaHutVNSpider(scrapy.Spider):
     item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
 
     def start_requests(self):
+        # Token is generated using a timestamp and other parameters, but the timestamp-token pair remains consistent.
+        # Skipping complex JavaScript token generation implementation.
         yield JsonRequest(
-            url="https://api2.pizzahut.vn/api-core/api/store/GetAllStoreList",
+            url="https://rwapi.pizzahut.vn/api/store/GetAllStoreList",
             headers={
-                "Authorization": "Bearer T7RJyFN5ZY/2S7axVLhzLUd6SSBr8kVzvTwp4KdEk9qbcmQN1Zyz0F7fWQoRz6Jz7GjLA6TiFzmvdOhwCKkOCA==",
-                "PROJECT_ID": "WEB",
+                "Authorization": "Bearer 5EGv68487qV+zXxlX73CFsL8L8PPBmzKEoI9AzHvrvVm95T4KpV/Ggu9zyruZvkHLFwc69R+tqoOnP5epRXRWQ==",
+                "deviceuid": "47dcdf29-8d34-4beb-ac1f-8b91537cea35",
+                "project_id": "WEB",
+                "timestamp": "1739260740720",
             },
         )
 
@@ -25,8 +29,8 @@ class PizzaHutVNSpider(scrapy.Spider):
             item = Feature()
             item["ref"] = store.get("store_code")
             item["lat"], item["lon"] = store.get("location", "").split(",")
-            item["name"] = store.get("name_vi")
-            item["extras"]["name:en"] = store.get("name_en")
+            item["branch"] = store.get("name_vi").removeprefix("Pizza Hut ")
+            item["extras"]["branch:en"] = store.get("name_en").removeprefix("Pizza Hut ")
             item["addr_full"] = store.get("add_vn")
             item["extras"]["addr:full:en"] = clean_address(store.get("add_en"))
             if store.get("Open_Time") and store.get("Close_Time"):
