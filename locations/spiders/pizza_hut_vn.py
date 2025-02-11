02d1d5bcd783250771c59ded701f3a368ea5e350
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-11 20:06:43 +0530

    Fix Pizza Hut VN Spider

--- locations/spiders/pizza_hut_vn.py
+++ locations/spiders/pizza_hut_vn.py
@@ -12,11 +12,15 @@ class PizzaHutVNSpider(scrapy.Spider):
     item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
 
     def start_requests(self):
+        # Token is generated using a timestamp and other parameters, but the timestamp-token pair remains consistent.
+        # Skipping complex JavaScript token generation implementation.
         yield JsonRequest(
-            url="https://api2.pizzahut.vn/api-core/api/store/GetAllStoreList",
+            url="https://rwapi.pizzahut.vn/api/store/GetAllStoreList",
             headers={
-                "Authorization": "Bearer T7RJyFN5ZY/2S7axVLhzLUd6SSBr8kVzvTwp4KdEk9qbcmQN1Zyz0F7fWQoRz6Jz7GjLA6TiFzmvdOhwCKkOCA==",
-                "PROJECT_ID": "WEB",
+                "Authorization": "Bearer 5EGv68487qV+zXxlX73CFsL8L8PPBmzKEoI9AzHvrvVm95T4KpV/Ggu9zyruZvkHLFwc69R+tqoOnP5epRXRWQ==",
+                "deviceuid": "47dcdf29-8d34-4beb-ac1f-8b91537cea35",
+                "project_id": "WEB",
+                "timestamp": "1739260740720",
             },
         )
 
