d3833f211ecd1903e43fd2f5aba8087a079fde9b
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-02-11 13:03:06 +0530

    code refactored to fix spider

--- locations/spiders/burger_king_jp.py
+++ locations/spiders/burger_king_jp.py
@@ -1,54 +1,35 @@
-from scrapy.http import Request
+from typing import Iterable
 
-from locations.categories import Extras, apply_yes_no
-from locations.hours import DAYS_WEEKDAY, OpeningHours
-from locations.json_blob_spider import JSONBlobSpider
-from locations.pipelines.address_clean_up import clean_address
+import scrapy
+from scrapy.http import Request, Response
+from locations.items import Feature
+
 from locations.spiders.burger_king import BURGER_KING_SHARED_ATTRIBUTES
 
 
-class BurgerKingJPSpider(JSONBlobSpider):
-    download_timeout = 30
+class BurgerKingJPSpider(scrapy.Spider):
     name = "burger_king_jp"
     item_attributes = BURGER_KING_SHARED_ATTRIBUTES
-    start_urls = ["https://www.burgerking.co.jp/BKJ0001.json"]
-    locations_key = ["body", "storeList"]
 
     def start_requests(self):
-        for url in self.start_urls:
-            yield Request(
-                url=url,
+        yield Request(
+                url="https://www.burgerking.co.jp/burgerking/BKJ0302.json",
                 headers={
                     "Accept": "application/json, text/plain, */*",
                     "Content-Type": "application/x-www-form-urlencoded",
                 },
                 method="POST",
-                body='message={"header":{"error_code":"","error_text":"","info_text":"","login_session_id":"","message_version":"","ip_address":"","result":true,"trcode":"BKJ0001","platform":"02","id_member":"","auth_token":""},"body":{"state":"","city":"","dirveTh":"","dlvyn":"","kmonYn":"","kordYn":"","oper24Yn":"","parkingYn":"","wifiYn":"","powerYn":"","distance":"1000","sortType":"DISTANCE","storCoordX":"136","storCoordY":"35","storNm":""}}',
-            )
+                body='message=%7B%22header%22%3A%7B%22result%22%3Atrue%2C%22error_code%22%3A%22%22%2C%22error_text%22%3A%22%22%2C%22info_text%22%3A%22%22%2C%22message_version%22%3A%22%22%2C%22login_session_id%22%3A%22%22%2C%22trcode%22%3A%22BKJ0302%22%2C%22cdCallChnn%22%3A%2202%22%7D%2C%22body%22%3A%7B%22tpSearchStore%22%3A%2203%22%2C%22searchKeyword%22%3A%22%22%2C%22storeServiceCode%22%3A%5B%22%22%5D%2C%22sort%22%3A%2202%22%2C%22xCoordinates%22%3A%22%22%2C%22yCoordinates%22%3A%22%22%2C%22page%22%3A1%2C%22dataCount%22%3A300%7D%7D',
+            callback=self.parse)
 
-    def post_process_item(self, item, response, location):
-        item["ref"] = location["STOR_CD"]
-        item["lat"] = location["STOR_COORD_Y"]
-        item["lon"] = location["STOR_COORD_X"]
-        item["branch"] = location["STOR_NM"]
-        item["street_address"] = location["ADDR_1"]
-        item["addr_full"] = clean_address([location["ADDR_1"], location["ADDR_2"]])
-        apply_yes_no(Extras.WIFI, item, location["WIFI_YN"] == "1")
-        apply_yes_no(Extras.DRIVE_THROUGH, item, location["DIRVE_TH"] == "1", False)
-        apply_yes_no(Extras.DELIVERY, item, location["DLVYN"] == "1", False)
+    def parse(self, response: Response) -> Iterable[Feature]:
+        for location in response.json()["body"]["data"]:
+            item = Feature()
+            item["ref"] = location["storCd"]
+            item["lat"] = location["storCoordY"]
+            item["lon"] = location["storCoordX"]
+            item["branch"] = location["storNm"]
+            item["addr_full"] = location["storAddr"]
+            item["website"] = "https://www.burgerking.co.jp/"
 
-        item["opening_hours"] = OpeningHours()
-        try:
-            item["opening_hours"].add_days_range(
-                DAYS_WEEKDAY, location["WEEKDAY_START_HOUR"], location["WEEKDAY_FNSH_HOUR"], time_format="%H%M"
-            )
-            item["opening_hours"].add_range(
-                "Sat", location["WEEKEND_START_HOUR"], location["WEEKEND_FNSH_HOUR"], time_format="%H%M"
-            )
-            item["opening_hours"].add_range(
-                "Sun", location["HOLIDAY_START_HOUR"], location["HOLIDAY_FNSH_HOUR"], time_format="%H%M"
-            )
-        except ValueError:
-            self.crawler.stats.inc_value(f"atp/{self.name}/hours/failed")
-
-        yield item
+            yield item
