7b746d9e552232f6585fcf66a2a2dbef240643e0
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-12 19:41:30 +0530

    [Boost Mobile US] Apply custom settings to avoid http 429 error

--- locations/spiders/boost_mobile_us.py
+++ locations/spiders/boost_mobile_us.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class BoostMobileUSSpider(SitemapSpider, StructuredDataSpider):
@@ -11,3 +12,9 @@ class BoostMobileUSSpider(SitemapSpider, StructuredDataSpider):
     }
     sitemap_urls = ["https://www.boostmobile.com/locations/sitemap-business-main-pages.xml"]
     sitemap_rules = [(r"/locations/bd/boost-mobile-[a-z]{2}-[-\w]", "parse_sd")]
+    custom_settings = {
+        "USER_AGENT": BROWSER_DEFAULT,
+        "CONCURRENT_REQUESTS": 1,
+        "DOWNLOAD_DELAY": 5,
+        "ROBOTSTXT_OBEY": False,
+    }
