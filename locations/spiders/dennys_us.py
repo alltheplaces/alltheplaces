5491b1cab6e67c351ae2296354ebdfb320f684a4
Cj Malone <CjMalone@mail.com>
2025-02-12 11:43:16 +0000

    Clean opening hours code

--- locations/spiders/dennys_us.py
+++ locations/spiders/dennys_us.py
@@ -1,7 +1,6 @@
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -10,21 +9,19 @@ class DennysUSSpider(SitemapSpider, StructuredDataSpider):
     name = "dennys_us"
     item_attributes = {"brand": "Denny's", "brand_wikidata": "Q1189695"}
     sitemap_urls = ["https://locations.dennys.com/robots.txt"]
-    sitemap_rules = [(r"https://locations.dennys.com/[^/]+/[^/]+/\d+$", "parse_sd")]
+    sitemap_rules = [(r"https://locations.dennys.com/[^/]+/[^/]+/(\d+)$", "parse_sd")]
+    time_format = "%I:%M %p"
+
+    def pre_process_data(self, ld_data: dict, **kwargs):
+        for rule in ld_data["openingHoursSpecification"]:
+            if rule["opens"] == "Closed":
+                continue
+            if rule["opens"] == "Open 24 Hours":
+                rule["opens"] = "12:00 AM  to  11:59 PM"
+            rule["opens"], rule["closes"] = rule["opens"].split("  to  ", 1)
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["name"] = None
-        item["opening_hours"] = OpeningHours()
-        for day_time in ld_data["openingHoursSpecification"]:
-            day = day_time["dayOfWeek"].split("/")[-1]
-            time = day_time["opens"]
-            if "Open 24 Hours" in time.strip():
-                item["opening_hours"].add_range(day, "00:00", "23:59")
-            elif "Closed" in time:
-                item["opening_hours"].set_closed(day)
-            else:
-                open_time, close_time = time.split("  to  ")
-                item["opening_hours"].add_range(day, open_time.strip(), close_time.strip(), "%I:%M %p")
 
         item["extras"]["website:menu"] = response.xpath('//a[@data-event-name="loc_view_menu"]/@href').get()
         item["extras"]["website:orders"] = response.xpath('//a[@data-event-name="loc_order_online"]/@href').get()
