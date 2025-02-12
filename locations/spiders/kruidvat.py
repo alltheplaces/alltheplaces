7c6409db4aa40f9cd28e851fea6313b685c6fb4d
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-02-12 18:30:39 +0000

    [Kruidvat] Clean up opening hours and name

--- locations/spiders/kruidvat.py
+++ locations/spiders/kruidvat.py
@@ -21,25 +21,29 @@ class KruidvatSpider(scrapy.Spider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.xpath("//stores"):
             item = Feature()
-            item["name"] = store.xpath(".//displayName/text()").get()
+            item["ref"] = store.xpath("code/text()").get()
+
+            if item["ref"] == "8000":
+                continue
+
             item["state"] = store.xpath(".//province/text()").get()
             item["postcode"] = store.xpath(".//postalCode/text()").get()
             item["street"] = store.xpath(".//line1/text()").get()
             item["lat"] = store.xpath(".//latitude/text()").get()
             item["lon"] = store.xpath(".//longitude/text()").get()
             item["addr_full"] = store.xpath(".//formattedAddress/text()").get()
-            item["opening_hours"] = OpeningHours()
-            for day_time in store.xpath(".//weekDayOpeningList"):
-                open_time = day_time.xpath("./openingTime/formattedHour/text()").get()
-                close_time = day_time.xpath("./closingTime/formattedHour/text()").get()
-                day = day_time.xpath(".//shortenedWeekDay/text()").get()
-                if day:
-                    item["opening_hours"].add_range(day=day, open_time=open_time, close_time=close_time)
 
-            if ".nl" in response.url:
-                item["ref"] = item["website"] = "https://www.kruidvat.nl/" + store.xpath(".//url/text()").get()
-            else:
-                item["ref"] = item["website"] = "https://www.kruidvat.be/" + store.xpath(".//url/text()").get()
+            item["opening_hours"] = OpeningHours()
+            for rule in store.xpath(".//weekDayOpeningList"):
+                if rule.xpath("./closed/text()").get() == "true":
+                    continue
+                item["opening_hours"].add_range(
+                    rule.xpath("shortenedWeekDay/text()").get(),
+                    rule.xpath("openingTime/formattedHour/text()").get(),
+                    rule.xpath("closingTime/formattedHour/text()").get(),
+                )
+
+            item["website"] = response.urljoin(store.xpath("url/text()").get())
 
             apply_category(Categories.SHOP_CHEMIST, item)
 
