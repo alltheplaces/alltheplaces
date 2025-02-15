6d8942642b478f36f9fe2001844e46e7b99ab336
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-02-15 12:45:02 +0000

    [Mind GB] Add spider

--- locations/spiders/mind_gb.py
+++ locations/spiders/mind_gb.py
@@ -2,9 +2,9 @@ import json
 import re
 
 from locations.items import Feature
-
-# from locations.hours import DAYS_FULL, OpeningHours
+from locations.hours import DAYS_FULL, OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
+
 
 
 class MindGBSpider(JSONBlobSpider):
@@ -13,30 +13,34 @@ class MindGBSpider(JSONBlobSpider):
     start_urls = ["https://www.mind.org.uk/mind-charity-shops/find-our-local-mind-shops/"]
     requires_proxy = True
 
+
     def parse(self, response):
-        match = re.search(r"const locations = ([^\n]+)", response.text)
+        match = re.search(r"const locations = ([^\n]+)",response.text)
         data = match.group(1)[:-1]
-        print(data)
         json_data = json.loads(data)
         for location in json_data:
-            item = Feature()
-            item["name"] = "Mind Charity Shop"
-            item["branch"] = location["name"].replace(" shop", "").replace(" Mind", "").replace(" Shop", "")
+            item=Feature()
+            item["name"] = 'Mind Charity Shop'
+            item["branch"] = location["name"].replace(" shop","").replace(" Mind","").replace(" Shop","")
             item["ref"] = item["branch"]
-            item["lat"], item["lon"] = location["position"]["lat"], location["position"]["lng"]
-            temp = location["content"].replace("\n", "")
-            temp = re.sub(r"</?(?:(p|ul|li|span|div))[^>]*>", ",", temp).replace(",,", ",")
-            print(temp)
+            item["lat"],item["lon"] = location["position"]["lat"],location["position"]["lng"]
+            temp = location["content"].replace("\n","")
+            temp = re.sub(r"</?(?:(p|ul|li|span|div))[^>]*>",",",temp).replace(",,",",")
+            #print(temp)
             if "Opening hours" in temp:
-                result = re.search(r"Opening hours(.*),,(.*?)(?:(Phone|Email))", temp)
+                result = re.search(r"Opening hours(.*),,(.*?)(?:(Phone|Email))",temp)
                 hours = result.group(1)
-                address = result.group(2).replace("<br />", "")
+                address = result.group(2).replace("<br />","")
+                #print("hours" + hours)
                 item["opening_hours"] = hours
+                #print("address" + address)
                 item["addr_full"] = address
             if "Phone:" in temp:
-                phone = re.search(r"Phone: <a href=\"tel:([^\"]*)\"", temp).group(1)
+                phone = re.search(r"Phone: <a href=\"tel:([^\"]*)\"",temp).group(1)
+                #print("phone" + phone)
                 item["phone"] = phone
             if "Email:" in temp:
-                email = re.search(r"Email: <a href=\"mailto:(.*)\"", temp).group(1)
+                email = re.search(r"Email: <a href=\"mailto:(.*)\"",temp).group(1)
+                #print("email" + email)
                 item["email"] = email
             yield item
