d513942f9fc083bc5847c3c88c44ceca506f590f
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-02-15 13:26:13 +0000

    Update mind_gb.py

--- locations/spiders/mind_gb.py
+++ locations/spiders/mind_gb.py
@@ -14,6 +14,7 @@ class MindGBSpider(JSONBlobSpider):
     def parse(self, response):
         match = re.search(r"const locations = ([^\n]+)", response.text)
         data = match.group(1)[:-1]
+        print (data)
         json_data = json.loads(data)
         for location in json_data:
             item = Feature()
