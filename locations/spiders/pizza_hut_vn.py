2aa7cb02c4a687f4b26aa1ed7a61f1aea5ef8f5d
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-02-17 14:41:26 -0500

    Generate access token to fix Pizza Hut VN Spider (#12269)

--- locations/spiders/pizza_hut_vn.py
+++ locations/spiders/pizza_hut_vn.py
@@ -1,5 +1,11 @@
+import re
+import time
+import uuid
+from typing import Any
+
 import scrapy
-from scrapy.http import JsonRequest
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.hours import DAYS, OpeningHours
@@ -10,21 +16,37 @@ from locations.pipelines.address_clean_up import clean_address
 class PizzaHutVNSpider(scrapy.Spider):
     name = "pizza_hut_vn"
     item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
+    start_urls = ["https://pizzahut.vn/_next/static/chunks/700-ac6d47c5279a8d47.js"]
 
-    def start_requests(self):
-        # Token is generated using a timestamp and other parameters, but the timestamp-token pair remains consistent.
-        # Skipping complex JavaScript token generation implementation.
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        next_action_token = re.search(r"([a-f0-9]{40})", response.text).group(1)
+        timestamp = int(time.time() * 1000)
+        device_uid = uuid.uuid4()
+        yield Request(
+            url="https://pizzahut.vn/store-location?area=north",
+            method="POST",
+            body=f'[{{"url":"/store/GetAllStoreList","method":"get","bodyData":"","timeStamp":"{timestamp}","deviceUID":"{device_uid}"}}]',
+            headers={
+                "next-action": next_action_token,
+            },
+            callback=self.parse_token,
+            meta=dict(timestamp=timestamp, device_uid=device_uid),
+        )
+
+    def parse_token(self, response: Response, **kwargs: Any) -> Any:
+        access_token = re.search(r"1[:\s]+\"(.+)\"", response.text).group(1)
         yield JsonRequest(
             url="https://rwapi.pizzahut.vn/api/store/GetAllStoreList",
             headers={
-                "Authorization": "Bearer 5EGv68487qV+zXxlX73CFsL8L8PPBmzKEoI9AzHvrvVm95T4KpV/Ggu9zyruZvkHLFwc69R+tqoOnP5epRXRWQ==",
-                "deviceuid": "47dcdf29-8d34-4beb-ac1f-8b91537cea35",
+                "Authorization": f"Bearer {access_token}",
+                "deviceuid": str(response.meta["device_uid"]),
                 "project_id": "WEB",
-                "timestamp": "1739260740720",
+                "timestamp": str(response.meta["timestamp"]),
             },
+            callback=self.parse_stores,
         )
 
-    def parse(self, response, **kwargs):
+    def parse_stores(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json()["PZH_StoreList"]["StoreList"]:
             item = Feature()
             item["ref"] = store.get("store_code")
