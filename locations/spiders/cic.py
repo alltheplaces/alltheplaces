1c1448a566c728b5f058fca5e68352d7e541a4b6
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-02-19 14:55:08 +0530

    updated start_urls and parse method to fix spider

--- locations/spiders/cic.py
+++ locations/spiders/cic.py
@@ -1,47 +1,56 @@
+import re
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS_FR, OpeningHours
+from locations.hours import DAYS_FR, OpeningHours, sanitise_day
 from locations.items import Feature
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class CicSpider(CrawlSpider, StructuredDataSpider):
+class CicSpider(CrawlSpider):
     name = "cic"
     item_attributes = {"brand_wikidata": "Q746525"}
-    start_urls = ["https://www.cic.fr/fr/banques/entreprises/agences-et-distributeurs/BrowseSubdivision.aspx"]
+    start_urls = ["https://www.cic.fr/fr/agences-et-distributeurs/Regions.aspx"]
     allowed_domains = ["cic.fr"]
     rules = [
-        Rule(LinkExtractor(allow=r"SubdivisionId"), follow=True),
-        Rule(LinkExtractor(allow=r"type=branch&loca"), follow=True),
-        Rule(LinkExtractor(allow=r"/fr/agence/"), callback="parse", follow=True),
-        Rule(LinkExtractor(allow=r"/fr/agences"), callback="parse", follow=True),
+        Rule(
+            LinkExtractor(allow=r"/Departements\.aspx\?regionId="),
+        ),
+        Rule(
+            LinkExtractor(allow=r"/Localites\.aspx\?regionId="),
+        ),
+        Rule(
+            LinkExtractor(allow=r"/ResultatsRechercheGeographique\.aspx\?inseeCode="),
+        ),
+        Rule(
+            LinkExtractor(allow=r"/fr/agence/"),
+            callback="parse",
+        ),
     ]
 
     def parse(self, response):
         item = Feature()
-        item["website"] = response.url
-        item["ref"] = response.url
+        item["name"] = response.xpath("//title/text()").get()
+        item["website"] = item["ref"] = response.url
         item["phone"] = response.xpath('//*[@itemprop="telephone"]/text()').get()
         item["street_address"] = response.xpath('//*[@itemprop="streetAddress"]/text()').get()
         item["postcode"] = response.xpath('//*[@itemprop="postalCode"]/text()').get()
         item["city"] = response.xpath('//*[@itemprop="addressLocality"]/text()').get()
-        item["lat"] = response.xpath('//*[@itemprop="latitude"]/@content').get()
-        item["lon"] = response.xpath('//*[@itemprop="longitude"]/@content').get()
-
-        oh = OpeningHours()
-        for day in response.xpath("//tbody/tr[@class]"):
-            if day.xpath("./td[1]/text()").get().strip() == "Fermé":
-                continue
-            halfday = day.xpath("./td[2]/text()").get()
-            for i in range(1, 3 if halfday and halfday.strip() != "Fermé" else 2):
-                oh.add_range(
-                    day=DAYS_FR[day.xpath("./th/text()").get().strip()[:2]],
-                    open_time=day.xpath(f"./td[{i}]/text()").get().strip().replace("h", ":").split("-")[0],
-                    close_time=day.xpath(f"./td[{i}]/text()").get().strip().replace("h", ":").split("-")[1],
-                )
-        item["opening_hours"] = oh.as_opening_hours()
+        item["lat"] = re.search(r"Data\$Value2:\s*\"(-?\d+.\d+)\",", response.text).group(1)
+        item["lon"] = re.search(r"Data\$Value3:\s*\"(-?\d+.\d+)\"}", response.text).group(1)
+        item["opening_hours"] = OpeningHours()
+        for day_time in response.xpath('//*[@id="C:H1:D"]//tbody//tr'):
+            raw_data = day_time.xpath(".//td").xpath("normalize-space()").getall()
+            day = sanitise_day(raw_data[0], DAYS_FR)
+            for time in raw_data[1:3]:
+                if time in ["Ouvert sur RDV"]:
+                    continue
+                if "Fermé" in time:
+                    item["opening_hours"].set_closed(day)
+                else:
+                    open_time, close_time = time.replace("h", ":").split(" - ")
+                    item["opening_hours"].add_range(day=day, open_time=open_time, close_time=close_time)
         apply_category(Categories.BANK, item)
-
         yield item
+
