5a8e325c88837ec1542536b7f56ef3084b2f8b62
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-20 20:52:21 +0530

    Add opening hours

--- locations/spiders/burgerfi_us.py
+++ locations/spiders/burgerfi_us.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 from scrapy.http import Response
 
+from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -16,4 +17,7 @@ class BurgerfiUSSpider(JSONBlobSpider):
             return
         item["ref"] = item["website"].replace(" ", "-")
         item["branch"] = item.pop("name").replace(" - NOW OPEN", "")
+        item["opening_hours"] = OpeningHours()
+        for rule in feature.get("hours", []):
+            item["opening_hours"].add_ranges_from_string(f'{rule.get("label")} {rule.get("location_hours")}')
         yield item
