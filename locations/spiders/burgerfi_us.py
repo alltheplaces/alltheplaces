5c7d27da2e1d8b629265d935bcc5ec2f3a45d5ed
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-20 20:20:21 +0530

    Rename & refactor spider to use Brand API, instead of third party API

---
+++ locations/spiders/burgerfi_us.py
@@ -0,0 +1,16 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class BurgerfiUSSpider(JSONBlobSpider):
+    name = "burgerfi_us"
+    item_attributes = {"brand": "BurgerFi", "brand_wikidata": "Q39045496"}
+    start_urls = ["https://www.burgerfi.com/wp-json/burgerfi-locations/v1/locations"]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = item["website"]
+        yield item
