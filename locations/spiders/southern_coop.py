8826fc92e1924ad728aca888dcecc9ac2b3d7146
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-20 12:25:44 +0530

    Refactor code using Uberall API to fix Southern Coop Spider

--- locations/spiders/southern_coop.py
+++ locations/spiders/southern_coop.py
@@ -1,19 +1,20 @@
-from scrapy.spiders import SitemapSpider
+from typing import Iterable
+
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.spiders.central_england_cooperative import COOP_FOOD, set_operator
-from locations.structured_data_spider import StructuredDataSpider
+from locations.storefinders.uberall import UberallSpider
 
 SOUTHERN_COOP = {"brand": "The Southern Co-operative", "brand_wikidata": "Q7569773"}
 
 
-class SouthernCoopSpider(SitemapSpider, StructuredDataSpider):
+class SouthernCoopSpider(UberallSpider):
     name = "southern_coop"
-    sitemap_urls = ["https://stores.southern.coop/robots.txt"]
-    sitemap_rules = [(r"\.coop\/[-\w]+\/[-\w]+\/[-\w]+\.html$", "parse_sd")]
-    drop_attributes = {"image"}
+    key = "uvMckoaRcAUKR0LkkH03SVNyf7A4Lk"
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
+    def post_process_item(self, item: Feature, response: Response, location: dict) -> Iterable[Feature]:
         set_operator(SOUTHERN_COOP, item)
         item.update(COOP_FOOD)
         apply_category(Categories.SHOP_CONVENIENCE, item)
