f7a16a230a4262c3312481f5f8da8759a9d255c3
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-02-25 12:11:25 +0530

    code refactored to fix spider

--- locations/spiders/vakifbank_tr.py
+++ locations/spiders/vakifbank_tr.py
@@ -1,44 +1,52 @@
 import scrapy
 from scrapy.http import JsonRequest
-
 from locations.categories import Categories, apply_category
-from locations.items import Feature
+from locations.dict_parser import DictParser
 
 
 class VakifbankTRSpider(scrapy.Spider):
     name = "vakifbank_tr"
     item_attributes = {"brand": "Vakıfbank", "brand_wikidata": "Q1148521"}
-    allowed_domains = ["vakifbank.com.tr"]
     requires_proxy = "TR"
-    no_refs = True
 
     def start_requests(self):
-        url = "https://maps.vakifbank.com.tr/API/api/v1/Search/GetAllBranchAndAtmWithFiltersObj"
-        payload = {
-            "CountryId": 0,
-            "WhichOne": 1,  # Branches
-            "BranchNameOrCode": "",
-            "CityId": -1,
-            "TownId": -1,
-            "NeighborhoodId": -1,
-            "Options": [],
-        }
-        yield JsonRequest(url=url, data=payload, callback=self.parse)
-        payload["WhichOne"] = 2  # ATMs
-        yield JsonRequest(url=url, data=payload, callback=self.parse)
+        yield scrapy.Request(
+            url="https://apigw.vakifbank.com.tr:8443/auth/oauth/v2/token",
+            body="client_id=l7xx672ef83670e842b0bb2f13a539ddbfb5&client_secret=282ce53197da4c0cb8f7bd658a14a6c3&grant_type=client_credentials&scope=public",
+            headers={"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+            method="POST",
+            callback=self.parse_token,
+        )
 
-    def parse(self, response, **kwargs):
-        data = response.json()
-        for poi in data.get("ReturnObjectList"):
-            item = Feature()
-            item["ref"] = poi[7]
-            item["lat"] = poi[0]
-            item["lon"] = poi[1]
-            item["name"] = poi[2]
-            item["addr_full"] = poi[6]
-            item["phone"] = poi[4]
-            if poi[8] == "ATM":
+    def parse_token(self, response):
+        token = response.json()["access_token"]
+        for branch_type in ["Branch", "ATM"]:
+            url = f"https://apigw.vakifbank.com.tr:8443/vakifbank{branch_type}List"
+            payload = {"Status": 1} if branch_type == "Branch" else {"CurrencyCode": "1"}
+            yield JsonRequest(
+                url=url,
+                data=payload,
+                method="POST",
+                headers={
+                    "Authorization": f"Bearer {token}",
+                    "Content-Type": "application/json",
+                },
+                cb_kwargs={"branch_type": branch_type},
+                callback=self.parse_details,
+            )
+
+    def parse_details(self, response, **kwargs):
+        data_key = "Branch" if kwargs["branch_type"] == "Branch" else "ATM"
+        for item_data in response.json().get("Data").get(data_key):
+            item = DictParser.parse(item_data)
+            item["addr_full"] = item_data[f"{data_key}Address"]
+            item["branch"] = item.pop("name")
+            item["name"] = self.item_attributes["brand"]
+            if kwargs["branch_type"] == "ATM":
+                item["ref"] = item_data["ATMCode"]
+                item["lat"] = item_data["Latitude"].replace(",", ".")
+                item["lon"] = item_data["Longitude"].replace(",", ".")
                 apply_category(Categories.ATM, item)
-            if poi[8] == "SUBE" or poi[8] == "B_SUBE":
+            else:
                 apply_category(Categories.BANK, item)
             yield item
