0bb027c34abaeb5ad97784d5d9aa337f3f4a3354
David Hicks <david@hicks.id.au>
2025-02-25 22:36:38 +1100

    black_bear_diner_us: fix spider

--- locations/spiders/black_bear_diner_us.py
+++ locations/spiders/black_bear_diner_us.py
@@ -3,18 +3,19 @@ import re
 from scrapy import Spider
 from scrapy.selector import Selector
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 matcher = re.compile(
-    r"position: \{lat: ([0-9.-]+),\s*"
-    r"lng: ([0-9.-]+)\},\s*"
-    r"map: bbdMap,\s*"
-    r"icon: image,\s*"
-    r"title: '([^']+) Black Bear Diner'\s*"
-    r"\}\);\s*"
-    r"google\.maps\.event\.addListener\(marker, 'click', function \(\)\{\s*"
-    r"var contentString = '([^']+)';\s*"
-    r"contentString \+= '([^']+)';",
+    r"position:\s*\{\s*lat:\s*(-?\d{1,3}\.\d+)\s*,\s*"
+    r"lng:\s*(-?\d{1,3}\.\d+)\s*\}\s*,\s*"
+    r"map:\s*bbdMap\s*,\s*"
+    r"icon:\s*image\s*,\s*"
+    r"title:\s*'([^']+)'\s*"
+    r"\}\s*\)\s*;\s*"
+    r"google\.maps\.event\.addListener\(marker,\s*'click',\s*function\s*\(\)\s*\{\s*"
+    r"var contentString\s*=\s*'([^']+)'\s*;\s*"
+    r"contentString\s*\+=\s*'([^']+)'\s*;",
     flags=re.S,
 )
 
@@ -22,22 +23,28 @@ matcher = re.compile(
 class BlackBearDinerUSSpider(Spider):
     name = "black_bear_diner_us"
     item_attributes = {"brand": "Black Bear Diner", "brand_wikidata": "Q4920343"}
+    allowed_domains = ["blackbeardiner.com"]
     start_urls = ["https://blackbeardiner.com/locations/"]
 
     def parse(self, response):
         for match in matcher.findall(response.text):
+            branch = match[2].removeprefix("Black Bear Diner").strip("- ")
+            if not branch:
+                branch = None
             address = [line.removeprefix("\\") for line in Selector(text=match[3]).xpath("//address/text()").getall()]
             city, state = address[1].split(", ")
             link = Selector(text=match[4]).xpath("//@href").get()
-            yield Feature(
-                lat=match[0],
-                lon=match[1],
-                branch=match[2],
-                street_address=address[0],
-                city=city,
-                state=state,
-                postcode=address[2],
-                phone=address[3],
-                website=link,
-                ref=link,
-            )
+            properties = {
+                "lat": match[0],
+                "lon": match[1],
+                "branch": branch,
+                "street_address": address[0],
+                "city": city,
+                "state": state,
+                "postcode": address[2],
+                "phone": address[3],
+                "website": link,
+                "ref": link,
+            }
+            apply_category(Categories.RESTAURANT, properties)
+            yield Feature(**properties)
