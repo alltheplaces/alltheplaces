168245584751da8ed01e1290da70138812537625
Cj Malone <CjMalone@mail.com>
2025-02-27 12:57:00 +0000

    Drop old donation points

--- locations/spiders/oxfam_ireland.py
+++ locations/spiders/oxfam_ireland.py
@@ -8,7 +8,7 @@ class OxfamIrelandSpider(SitemapSpider):
     name = "oxfam_ireland"
     item_attributes = {"brand": "Oxfam", "brand_wikidata": "Q267941"}
     sitemap_urls = ["https://www.oxfamireland.org/sitemap.xml"]
-    sitemap_rules = [("/shops/", "parse"), ("/donation-point/", "parse")]
+    sitemap_rules = [("/shops/", "parse")]
     recycle_types = {
         "books": "recycling:books",
         "clothing": "recycling:clothes",
@@ -28,11 +28,11 @@ class OxfamIrelandSpider(SitemapSpider):
         item["name"] = response.xpath('//meta[@property="og:title"]/@content').get()
         item["email"] = response.xpath('//a[contains(@href, "mailto")]/text()').get()
         item["phone"] = response.xpath('//a[contains(@href, "tel")]/text()').get()
-        if "donation-point" in response.url:
-            apply_category({"amenity": "recycling", "recycling_type": "container"}, item)
-        else:
-            apply_category(Categories.SHOP_CHARITY, item)
+
+        apply_category(Categories.SHOP_CHARITY, item)
+
         for label in response.xpath('//*[contains(@class, "shop-accepts-item-label")]/text()').getall():
             if tag := self.recycle_types.get(label.strip().lower()):
                 apply_yes_no(tag, item, True)
+
         yield item
