bc8d4bb240930a1358bbb1d0f5c502c134ca7a3e
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-27 12:58:15 +0530

    Refactor using Yext API with coordinates available

--- locations/spiders/american_eagle_outfitters.py
+++ locations/spiders/american_eagle_outfitters.py
@@ -1,24 +1,20 @@
-from scrapy.spiders import SitemapSpider
-
 from locations.categories import Categories, apply_category
-from locations.structured_data_spider import StructuredDataSpider
+from locations.items import Feature
+from locations.storefinders.yext import YextSpider
 
 
-class AmericanEagleOutfittersSpider(SitemapSpider, StructuredDataSpider):
+class AmericanEagleOutfittersSpider(YextSpider):
     name = "american_eagle_outfitters"
     item_attributes = {"brand": "American Eagle Outfitters", "brand_wikidata": "Q2842931"}
     AERIE = {"brand": "Aerie", "brand_wikidata": "Q25351619"}
     OFFLINE = {"brand": "OFFLINE By Aerie", "brand_wikidata": ""}
-    sitemap_urls = ["https://stores.aeostores.com/sitemap.xml"]
-    sitemap_rules = [("", "parse_sd")]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    drop_attributes = {"image"}
+    api_key = "90d8e45747b6a14476d8f1a327f692fe"
+    drop_attributes = {"image", "twitter"}
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        brand = ld_data.get("brand", "").title()
-        if brand.startswith("Aerie"):
+    def parse_item(self, item: Feature, location: dict, **kwargs):
+        if item["name"].title().startswith("Aerie"):
             item.update(self.AERIE)
-        elif brand.startswith("Offline"):
+        elif item["name"].title().startswith("Offline"):
             item.update(self.OFFLINE)
             apply_category(Categories.SHOP_CLOTHES, item)
         yield item
