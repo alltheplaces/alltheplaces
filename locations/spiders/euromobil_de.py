e19dd73555e674a7f3e370787465324cc0e0d606
Karen Kerr <karenkerrgithub@gmail.com>
2025-02-27 12:57:47 +0000

    [Euromobil DE] Add spider

---
+++ locations/spiders/euromobil_de.py
@@ -0,0 +1,21 @@
+import scrapy
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+
+
+class EuroMobilDESpider(scrapy.Spider):
+    name = "euromobil_de"
+    item_attributes = {
+        "brand": "Euromobil",
+        "brand_wikidata": "Q1375118",
+    }
+    start_urls = ["https://euromobil.de/rest/search_emstations/50.1109221/8.6821267/10000/"]
+
+    def parse(self, response, **kwargs):
+        for station in response.json():
+            item = DictParser.parse(station)
+            item["street_address"] = item.pop("street")
+            item["ref"] = station["stationid"]
+            apply_category(Categories.CAR_RENTAL, item)
+            yield item
