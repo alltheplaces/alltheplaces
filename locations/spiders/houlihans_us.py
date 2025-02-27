19cb89aa2be3e12d12fea06e6deda7ad37d78a1f
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-02-27 02:44:37 +0000

    [Houlihan's] Fix wikidata (#12356)

--- locations/spiders/houlihans_us.py
+++ locations/spiders/houlihans_us.py
@@ -5,24 +5,28 @@ from scrapy import Request, Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
+from locations.linked_data_parser import LinkedDataParser
 
 
 class HoulihansUSSpider(Spider):
     name = "houlihans_us"
-    item_attributes = {"brand": "Houlihan's", "brand_wikidata": "Q5913100s"}
+    item_attributes = {"brand": "Houlihan's", "brand_wikidata": "Q5913100"}
     allowed_domains = ["houlihans.com"]
     start_urls = ["https://www.houlihans.com/store-locator/"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in json.loads(response.xpath('//*[@type="application/ld+json"]/text()').get())["subOrganization"]:
-            item = DictParser.parse(store)
+            item = LinkedDataParser.parse_ld(store)
+            item["branch"] = item.pop("name")
             item["ref"] = item["website"]
             apply_category(Categories.RESTAURANT, item)
             yield Request(url=item["website"], callback=self.parse_location, meta={"item": item})
 
     def parse_location(self, response, **kwargs):
         item = response.meta["item"]
+        item["facebook"] = response.xpath(
+            '//a[contains(@href, "https://www.facebook.com/")][not(contains(@href, "/houlihans/"))]/@href'
+        ).get()
         item["lat"] = response.xpath("//@data-gmaps-lat").get()
         item["lon"] = response.xpath("//@data-gmaps-lng").get()
         yield item
