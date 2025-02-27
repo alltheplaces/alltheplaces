f55721a2f2c5372c047e303ab65720c5fca5b6f0
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-02-27 12:03:49 +0530

    Improve branding for American Eagle Outfitters Spider

--- locations/spiders/american_eagle_outfitters.py
+++ locations/spiders/american_eagle_outfitters.py
@@ -6,11 +6,17 @@ from locations.structured_data_spider import StructuredDataSpider
 class AmericanEagleOutfittersSpider(SitemapSpider, StructuredDataSpider):
     name = "american_eagle_outfitters"
     item_attributes = {"brand": "American Eagle Outfitters", "brand_wikidata": "Q2842931"}
+    AERIE = {"brand": "Aerie", "brand_wikidata": "Q25351619"}
+    OFFLINE = {"brand": "OFFLINE By Aerie", "brand_wikidata": ""}
     sitemap_urls = ["https://stores.aeostores.com/sitemap.xml"]
     sitemap_rules = [("", "parse_sd")]
     custom_settings = {"ROBOTSTXT_OBEY": False}
     drop_attributes = {"image"}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["brand"] = response.xpath('//*[@class="LocationName-brand"]/text()').get()
+        brand = ld_data.get("brand", "").title()
+        if brand.startswith("Aerie"):
+            item.update(self.AERIE)
+        elif brand.startswith("Offline"):
+            item.update(self.OFFLINE)
         yield item
