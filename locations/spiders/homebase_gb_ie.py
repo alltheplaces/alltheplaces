9bdd5dc94e34bdd90bdc95dc6644a32f7b34668d
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-03-02 23:37:40 +0000

    [Homebase GB] Fix Spider

--- locations/spiders/homebase_gb_ie.py
+++ locations/spiders/homebase_gb_ie.py
@@ -1,12 +1,8 @@
-from scrapy.spiders import SitemapSpider
-
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class HomebaseGBIESpider(SitemapSpider, StructuredDataSpider):
+from locations.storefinders.uberall import UberallSpider
+
+class HomebaseGBIESpider(UberallSpider):
     name = "homebase_gb_ie"
     item_attributes = {"brand": "Homebase", "brand_wikidata": "Q9293447"}
-    sitemap_urls = ["https://store.homebase.co.uk/robots.txt"]
-    sitemap_rules = [(r"https:\/\/store\.homebase\.co\.uk\/[-.\w]+\/[-.\w]+$", "parse_sd")]
-    skip_auto_cc_domain = True
-    drop_attributes = {"image"}
+    key = "uryD0GjzSwZ7dbDQkmUpVvnwMkL4lh"
