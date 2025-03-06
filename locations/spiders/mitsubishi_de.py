cd3ddcbdb59643c73f183cbeea0745e3b3646b36
panasonic-mafia <kolosov.yv@gmail.com>
2025-03-06 12:58:37 +0100

    parse details, clean code

--- locations/spiders/mitsubishi_de.py
+++ locations/spiders/mitsubishi_de.py
@@ -1,6 +1,12 @@
+from copy import deepcopy
+from typing import Iterable
+
 import chompjs
+from scrapy import Request
+from scrapy.http import Response
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
@@ -12,18 +18,41 @@ class MitsubishiDESpider(JSONBlobSpider):
     }
     start_urls = ["https://www.mitsubishi-motors.de/haendlersuche"]
 
-    def extract_json(self, response):
+    def extract_json(self, response: Response) -> dict | list[dict]:
         return chompjs.parse_js_object(response.xpath("//partnersearch-root/@data-partners").get())
 
-    def post_process_item(self, item, response, location):
-        type_code = location.get("partnerTypID")
-        if type_code in ["1", "5"]:
-            apply_category(Categories.SHOP_CAR, item)
-            apply_yes_no(Extras.CAR_REPAIR, item, True)
-        elif type_code == "7":
-            apply_category(Categories.SHOP_CAR_REPAIR, item)
-        else:
-            self.logger.warning(f"Unknown type code: {type_code}, {item['name']}, {item['ref']}")
-        self.crawler.stats.inc_value(f"atp/{self.name}/type_code/{type_code}")
+    def post_process_item(self, item: Feature, response: Response, location: dict) -> Iterable[Feature]:
+        yield from self.get_details(item, location)
 
-        yield item
+    def get_details(self, item, location) -> Iterable[Request]:
+        yield Request(
+            url="https://www.mitsubishi-motors.de/api/psearchPartnerDetailedById?partnerId={}".format(
+                location.get("id")
+            ),
+            callback=self.parse_details,
+            meta={"item": item},
+        )
+
+    def parse_details(self, response: Response) -> Iterable[Feature]:
+        for poi in response.json().get("data", []):
+            item = deepcopy(response.meta["item"])
+            item["street_address"] = poi.get("street")
+            item["postcode"] = poi.get("zip")
+            item["city"] = poi.get("city")
+            item["phone"] = poi.get("phone")
+            item["email"] = poi.get("mail")
+            item["website"] = poi.get("baseUrl") or poi.get("parentBaseUrl")
+
+            type_id = poi.get("partnerTypID")
+            if type_id in ["1", "2", "5", "6"]:
+                apply_category(Categories.SHOP_CAR, item)
+                apply_yes_no(Extras.CAR_REPAIR, item, True)
+            elif type_id == "4":
+                apply_category(Categories.SHOP_CAR, item)
+            elif type_id in ["3", "7"]:
+                apply_category(Categories.SHOP_CAR_REPAIR, item)
+            else:
+                self.logger.warning(f"Unknown type code: {type_id}, {item['name']}, {item['ref']}")
+            self.crawler.stats.inc_value(f"atp/{self.name}/type_code/{type_id}")
+            # TODO: opening hours
+            yield item
