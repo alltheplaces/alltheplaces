a0547b492b74a18eb6959799ec27e1951897a7f2
David Hicks <david@hicks.id.au>
2025-03-06 19:22:30 +1100

    Add Niagara Falls City Council drain+sewer manholes (CA)
    sewer manholes: 6470 features
    drain manholes: 5605 features

---
+++ locations/spiders/niagara_falls_city_council_drain_manholes_ca.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.arcgis_feature_server import ArcGISFeatureServerSpider
+
+
+class NiagaraFallsCityCouncilDrainManholesCASpider(ArcGISFeatureServerSpider):
+    name = "niagara_falls_city_council_drain_manholes_ca"
+    item_attributes = {"operator": "Niagara Falls City Council", "operator_wikidata": "Q16941501", "state": "ON"}
+    host = "services9.arcgis.com"
+    context_path = "oMFQlUUrLd1Uh1bd/ArcGIS"
+    service_id = "Niagara_Falls_Storm_Sewer_Maintenance_Holes"
+    layer_id = "0"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if feature.get("Status") != "ACTIVE":
+            return
+        apply_category(Categories.MANHOLE, item)
+        if ref := feature.get("ID"):
+            item["ref"] = ref
+        else:
+            item["ref"] = str(feature["OBJECTID"])
+        apply_category(Categories.MANHOLE, item)
+        item["extras"]["manhole"] = "rainwater"
+        yield item
