46b22243a6cd9cde3337a374cb90f542afadbf91
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-07 17:28:09 +0530

    Switch to API to fix Radisson Hotels Spider

--- locations/spiders/radisson_hotels.py
+++ locations/spiders/radisson_hotels.py
@@ -1,18 +1,17 @@
-import json
-import re
-from typing import List
+from typing import Any, Iterable
 
 import scrapy
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
-from locations.items import Feature
+from locations.dict_parser import DictParser
 from locations.user_agents import BROWSER_DEFAULT
 
 
 class RadissonHotelsSpider(scrapy.Spider):
     name = "radisson_hotels"
     allowed_domains = ["www.radissonhotels.com"]
-    start_urls = ["https://www.radissonhotels.com/en-us/destination/"]
     brand_mapping = {
         "rdb": ["Radisson Blu", "Q7281341"],
         "rco": ["Radisson Collection", "Q60716706"],
@@ -28,42 +27,25 @@ class RadissonHotelsSpider(scrapy.Spider):
         "ri": ["Radisson Individuals", None],
     }
     custom_settings = {
-        "DEFAULT_REQUEST_HEADERS": {
-            "Accept": "*/*",
-            "User-Agent": BROWSER_DEFAULT,
-            "Connection": "keep-alive",
-            "Accept-Language": "fr-FR",
-        }
+        "USER_AGENT": BROWSER_DEFAULT,
+        "ROBOTSTXT_OBEY": False,
+        "DOWNLOAD_TIMEOUT": 300,
     }
 
-    def parse(self, response):
-        cities = response.xpath('//*[@class="list-destinations--link font-bold"]/@href').getall()
-        for city in cities:
-            url = "https://www.radissonhotels.com" + city
-            yield scrapy.Request(url=url, callback=self.parse_city)
+    def start_requests(self) -> Iterable[Request]:
+        yield JsonRequest(
+            url="https://www.radissonhotels.com/zimba-api/hotels?limit=1000", headers={"accept-language": "en-us"}
+        )
 
-    def parse_city(self, response):
-        try:
-            hotels: List[dict] = json.loads("[" + re.search(r'({"code":.*{.*})', response.text).group(1) + "]")
-        except AttributeError:
-            return
-        for hotel in hotels:
-            item = Feature()
-            item["ref"] = hotel.get("code", None)
-            item["name"] = hotel.get("name", None)
-            if brand := hotel.get("brand", None):
-                item["brand"], item["brand_wikidata"] = self.brand_mapping.get(brand, [None, None])
-            item["website"] = "https://www.radissonhotels.com" + hotel.get("overviewPath")
-
-            if full_address := hotel.get("fullAddress"):
-                item["street"] = full_address.get("street")
-                item["city"] = full_address.get("city")
-                item["country"] = full_address.get("countryCode")
-                item["postcode"] = full_address.get("postcode")
-
-            if coordinates := hotel.get("coordinates", None):
-                item["lat"] = coordinates.get("latitude", None)
-                item["lon"] = coordinates.get("longitude", None)
-            item["street_address"] = hotel.get("address", None)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for hotel in response.json()["hotels"]:
+            hotel.update(hotel.pop("contactInfo"))
+            item = DictParser.parse(hotel)
+            item["ref"] = hotel.get("code")
+            item["street_address"] = item.pop("addr_full")
+            if brand_info := self.brand_mapping.get(hotel.get("brand")):
+                item["brand"], item["brand_wikidata"] = brand_info
+            else:
+                self.crawler.stats.inc_value(f"atp/{self.name}/unknown_brand/{hotel['brandName']}")
             apply_category(Categories.HOTEL, item)
             yield item
