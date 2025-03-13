0ef5618367686de365f18b20b18dd71404304340
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-13 17:17:00 +0530

    Update API bearer token generation method to fix Pizza Hut VN Spider

--- locations/spiders/pizza_hut_vn.py
+++ locations/spiders/pizza_hut_vn.py
@@ -1,10 +1,12 @@
+import base64
+import hashlib
+import hmac
 import re
 import time
 import uuid
 from typing import Any
 
 import scrapy
-from scrapy import Request
 from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
@@ -16,32 +18,32 @@ from locations.pipelines.address_clean_up import clean_address
 class PizzaHutVNSpider(scrapy.Spider):
     name = "pizza_hut_vn"
     item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
-    start_urls = ["https://pizzahut.vn/_next/static/chunks/700-ac6d47c5279a8d47.js"]
+    start_urls = ["https://pizzahut.vn/_next/static/chunks/3070-0d2156c9136abbb1.js"]
+    api_url = "https://rwapi.pizzahut.vn/api/store/GetAllStoreList"
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        next_action_token = re.search(r"([a-f0-9]{40})", response.text).group(1)
         timestamp = int(time.time() * 1000)
         device_uid = uuid.uuid4()
-        yield Request(
-            url="https://pizzahut.vn/store-location?area=north",
-            method="POST",
-            body=f'[{{"url":"/store/GetAllStoreList","method":"get","bodyData":"","timeStamp":"{timestamp}","deviceUID":"{device_uid}"}}]',
-            headers={
-                "next-action": next_action_token,
-            },
-            callback=self.parse_token,
-            meta=dict(timestamp=timestamp, device_uid=device_uid),
-        )
 
-    def parse_token(self, response: Response, **kwargs: Any) -> Any:
-        access_token = re.search(r"1[:\s]+\"(.+)\"", response.text).group(1)
+        app_code, secret_key, access_key = re.findall(
+            r"[a-z][\s=]+\(0,[\n\s]*[a-z]\.[a-z]\)\(\"([a-z0-9]+)\"\)", response.text
+        )[:3]
+
+        query_string = f"TimeStamp={timestamp}&DeviceUID={device_uid}&AppCode={app_code}&AccessKey={access_key}&Method=GET&url={self.api_url}&Body="
+
+        key = base64.b64decode(secret_key)
+        message = query_string.encode("utf-8")
+        signature = hmac.new(key, message, hashlib.sha512)
+
+        access_token = base64.b64encode(signature.digest()).decode("utf-8")
+
         yield JsonRequest(
-            url="https://rwapi.pizzahut.vn/api/store/GetAllStoreList",
+            url=self.api_url,
             headers={
                 "Authorization": f"Bearer {access_token}",
-                "deviceuid": str(response.meta["device_uid"]),
+                "deviceuid": str(device_uid),
                 "project_id": "WEB",
-                "timestamp": str(response.meta["timestamp"]),
+                "timestamp": str(timestamp),
             },
             callback=self.parse_stores,
         )
