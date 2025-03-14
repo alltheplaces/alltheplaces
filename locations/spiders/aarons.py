b75cbecfa4c11777c5d6269135fbcae8bf64c1e9
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-14 13:58:26 +0530

    Skip location pages having no location data

--- locations/spiders/aarons.py
+++ locations/spiders/aarons.py
@@ -1,7 +1,9 @@
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -16,3 +18,8 @@ class AaronsSpider(CrawlSpider, StructuredDataSpider):
     drop_attributes = {"name", "image"}
     wanted_types = ["Store"]
     search_for_twitter = False
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        if not item.get("street_address"):
+            return
+        yield item
