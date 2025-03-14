54a497eccab94bc511d4c3ea7e9dc89fd32315b7
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-14 12:12:53 +0530

    Switch to CrawlSpider to fix Aarons Spider

--- locations/spiders/aarons.py
+++ locations/spiders/aarons.py
@@ -1,40 +1,18 @@
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class AaronsSpider(SitemapSpider, StructuredDataSpider):
+class AaronsSpider(CrawlSpider, StructuredDataSpider):
     name = "aarons"
-    sitemap_urls = ["https://locations.aarons.com/robots.txt"]
-    sitemap_rules = [(r"\.com/\w\w/\w\w/[^/]+/[^/]+$", "parse")]
     item_attributes = {"brand": "Aaron's", "brand_wikidata": "Q10397787", "extras": Categories.SHOP_FURNITURE.value}
+    start_urls = ["https://www.aarons.com/locations/us", "https://www.aarons.com/locations/ca"]
+    rules = [
+        Rule(LinkExtractor(allow=r"/[a-z]{2}/[a-z]{2}/?$")),
+        Rule(LinkExtractor(allow=r"/[a-z]{2}/[a-z]{2}/[-\w]+/[-\w]+"), callback="parse_sd"),
+    ]
     drop_attributes = {"name", "image"}
     wanted_types = ["Store"]
     search_for_twitter = False
-
-    def sitemap_filter(self, entries):
-        for entry in entries:
-            if any(
-                entry["loc"].endswith(s)
-                for s in [
-                    "/ashley-furniture",
-                    "/bedroom-furniture",
-                    "/computers",
-                    "/gaming",
-                    "/ge",
-                    "/hp",
-                    "/lg",
-                    "/living-room-furniture",
-                    "/mattresses",
-                    "/refrigerators",
-                    "/samsung",
-                    "/service-area",
-                    "/sony",
-                    "/televisions",
-                    "/washer-dryer",
-                    "/woodhaven",
-                ]
-            ):
-                continue
-            yield entry
