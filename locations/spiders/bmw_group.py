3102fea70607819d2150c33ab1e697346e721ea9
panasonic-mafia <kolosov.yv@gmail.com>
2025-03-16 16:07:13 +0100

    take all pois for all brands

--- locations/spiders/bmw_group.py
+++ locations/spiders/bmw_group.py
@@ -3,6 +3,62 @@ import scrapy
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
+# POI Types mapping found in https://dlo.api.bmw/main.js
+"""
+newCars: [{
+        type: "distributionBranches",
+        key: "F"
+    }],
+    usedCars: [{
+        type: "distributionBranches",
+        key: "G"
+    }],
+    repairServices: [{
+        type: "distributionBranches",
+        key: "T"
+    }],
+    mCertified: [{
+        type: "businessTypes",
+        key: "MC"
+    }],
+    classicCertified: [{
+        type: "businessTypes",
+        key: "CC"
+    }],
+    eRetail: [{
+        type: "requestServices",
+        key: "SON"
+    }],
+    highVoltageServices: [{
+        type: "services",
+        key: "HV"
+    }],
+    carbonServices: [{
+        type: "services",
+        key: "CR"
+    }],
+    bodyShop: [{
+        type: "requestServices",
+        key: "CBS"
+    }],
+    paintShop: [{
+        type: "requestServices",
+        key: "CPS"
+    }],
+    onlineBooking: [{
+        type: "testDriveBooking",
+        key: "TDB_OB"
+    }],
+    sendRequest: [{
+        type: "testDriveBooking",
+        key: "TDB_SR"
+    }],
+    bmwEmployeeDelivery: [{
+        type: "businessTypes",
+        key: "GE"
+    }]
+"""
+
 
 class BmwGroupSpider(scrapy.Spider):
     name = "bmw_group"
@@ -140,7 +196,7 @@ class BmwGroupSpider(scrapy.Spider):
         "BY",
         "LV",
     ]
-    brand_mapping = {
+    BRAND_MAPPING = {
         "BMW": {"brand": "BMW", "brand_wikidata": "Q26678"},
         "BMW_I": {"brand": "BMW i", "brand_wikidata": "Q796784"},
         "ROLLS_ROYCE": {"brand": "Rolls-Royce", "brand_wikidata": "Q243278"},
@@ -151,7 +207,7 @@ class BmwGroupSpider(scrapy.Spider):
 
     def start_requests(self):
         for country in self.available_countries:
-            url = f"https://www.bmw.com/c2b-localsearch/services/api/v4/clients/BMWSTAGE2_DLO/DE/pois?brand=BMW_BMWI_BMWM&cached=off&language=en&lat=0&lng=0&maxResults=7000&showAll=true&unit=km&country={country}"
+            url = f"https://c2b-services.bmw.com/c2b-localsearch/services/api/v4/clients/BMWSTAGE2_DLO/-/pois?cached=off&language=en&lat=0&lng=0&maxResults=10000&unit=km&showAll=true&country={country}"
             yield scrapy.Request(
                 url=url, callback=self.parse, headers={"Accept": "application/json", "Content-Type": "application/json"}
             )
@@ -160,19 +216,17 @@ class BmwGroupSpider(scrapy.Spider):
         response = response.json().get("data").get("pois")
         for data in response:
             data["street_address"] = data.pop("street")
-            occurrences = self.get_num_of_brands_per_location(response, data.get("key"))
+
             item = DictParser.parse(data)
-            item["ref"] = data.get("key")
+            item["ref"] = f"{data.get('key', '')}-{data.get('category','')}"
             item["phone"] = data.get("attributes", {}).get("phone")
             item["email"] = data.get("attributes", {}).get("mail")
             item["website"] = data.get("attributes", {}).get("homepage")
-            if occurrences > 1:
-                # If there are more than one brand per location, we set it to the mother brand, BMW
-                item["brand"] = self.brand_mapping["BMW"]["brand"]
-                item["brand_wikidata"] = self.brand_mapping["BMW"]["brand_wikidata"]
+
+            if match := self.BRAND_MAPPING.get(data.get("category")):
+                item.update(match)
             else:
-                item["brand"] = self.brand_mapping[data.get("category")]["brand"]
-                item["brand_wikidata"] = self.brand_mapping[data.get("category")]["brand_wikidata"]
+                self.crawler.stats.inc_value(f"atp/brand/fail/{data.get('category')}")
 
             if item["brand"] == "BMW Motorrad":
                 apply_category(Categories.SHOP_MOTORCYCLE, item)
@@ -180,7 +234,3 @@ class BmwGroupSpider(scrapy.Spider):
                 apply_category(Categories.SHOP_CAR, item)
 
             yield item
-
-    def get_num_of_brands_per_location(self, data, key):
-        # A dealer can have multiple brands, so we need to check if there are more than one brand per location
-        return sum(1 for pois in data if key == pois.get("key"))
