57dd489d20d02c9a71248492418f7eb0fcad5434
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-18 19:45:41 +0530

    Add category and services

--- locations/spiders/mitsubishi_jp.py
+++ locations/spiders/mitsubishi_jp.py
@@ -5,6 +5,7 @@ from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.items import Feature
 from locations.pipelines.address_clean_up import clean_address
 
@@ -34,4 +35,13 @@ class MitsubishiJPSpider(CrawlSpider):
         )
         item["phone"] = response.xpath('//*[contains(@class, "mod-link-tel")]//a/@href').get()
         item["extras"]["fax"] = response.xpath('//*[contains(@class, "mod-link-fax")]/text()').get()
+
+        services = response.xpath('//*[@class="dealer__icon__txt"]/text()').getall()
+        if "新車" or "中古車" in services:
+            apply_category(Categories.SHOP_CAR, item)
+            apply_yes_no(Extras.CAR_REPAIR, item, "メンテナンス" in services)
+        elif "メンテナンス" in services:
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
+        apply_yes_no(Extras.USED_CAR_SALES, item, "中古車" in services)
+        apply_yes_no(Extras.WIFI, item, "WiFi" in services)
         yield item
