43c2dc926cdc5605dadb9d94e76b93e23bfaf64b
Cj Malone <CjMalone@mail.com>
2025-03-19 12:16:36 +0000

    Try with proxy

--- locations/spiders/binnys.py
+++ locations/spiders/binnys.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 import chompjs
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.dict_parser import DictParser
@@ -9,11 +12,12 @@ class BinnysSpider(SitemapSpider):
     item_attributes = {"brand": "Binny's Beverage Depot", "brand_wikidata": "Q30687714"}
     allowed_domains = ["binnys.com"]
     sitemap_urls = ["https://www.binnys.com/robots.txt"]
-    sitemap_rules = [(r"/store-locator/", "parse")]
+    sitemap_rules = [(r"/store-locator/([^/]+)/", "parse")]
+    requires_proxy = True
 
-    def parse(self, response):
-        if script := response.xpath('//script/text()[contains(.,"var serverSideViewModel")]').get():
-            data = chompjs.parse_js_object(script)
-            item = DictParser.parse(data)
-            item["branch"] = item.pop("name")
-            yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        script = response.xpath('//script/text()[contains(.,"var serverSideViewModel")]').get()
+        data = chompjs.parse_js_object(script)
+        item = DictParser.parse(data)
+        item["branch"] = item.pop("name")
+        yield item
