43d0cfc7391daa06bcab081762788824b346a0be
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-20 16:54:44 +0530

    Add category

--- locations/spiders/mitsubishi_dk.py
+++ locations/spiders/mitsubishi_dk.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 from scrapy.http import Response
 
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -13,4 +14,11 @@ class MitsubishiDKSpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["street_address"] = item.pop("street", None)
+
+        categories = feature.get("categories") or ""
+        if "1" in categories:
+            apply_category(Categories.SHOP_CAR, item)
+            apply_yes_no(Extras.CAR_REPAIR, item, "2" in categories)
+        elif "2" in categories:
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
         yield item
