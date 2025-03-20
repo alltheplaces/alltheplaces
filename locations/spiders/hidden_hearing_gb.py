52e63e4e9837d554a950c8752a5c5769dafdd758
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-03-20 20:32:41 +0100

    [Hidden Hearing GB] Add spider

---
+++ locations/spiders/hidden_hearing_gb.py
@@ -0,0 +1,6 @@
+from locations.json_blob_spider import JSONBlobSpider
+
+class HiddenHearingGBSpider(JSONBlobSpider):
+    name = "hidden_hearing_gb"
+    item_attributes = {"brand": "Hidden Hearing", "brand_wikidata": ""}
+    start_urls = ["https://www.hiddenhearing.co.uk/api/clinics/getclinics/%7B22907258-EC4D-4701-BC3C-9F0A3A3204F9%7D"]
