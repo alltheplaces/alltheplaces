9a4d60a6420ace5de4bcc5d6d87fcd56f0e3d12c
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-03-20 21:09:09 +0100

    Fix phone, website and emails

--- locations/spiders/hidden_hearing_gb.py
+++ locations/spiders/hidden_hearing_gb.py
@@ -5,3 +5,10 @@ class HiddenHearingGBSpider(JSONBlobSpider):
     name = "hidden_hearing_gb"
     item_attributes = {"brand": "Hidden Hearing", "brand_wikidata": "Q107301478"}
     start_urls = ["https://www.hiddenhearing.co.uk/api/clinics/getclinics/%7B22907258-EC4D-4701-BC3C-9F0A3A3204F9%7D"]
+
+    def post_process_item(self, item, response, location):
+        item["website"] = "https://www.hiddenhearing.co.uk" + item["website"]
+        if item["phone"] == "+44 800 740 8706":
+            item.pop("phone")
+        if item["email"] == "info@hiddenhearing.co.uk":
+            item.pop("email")
