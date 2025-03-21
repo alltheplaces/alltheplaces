953d21f2b095803050cf5f5dd05d8aba918f7f64
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-21 20:30:13 +0530

    Fix category

--- locations/spiders/mitsubishi_ch.py
+++ locations/spiders/mitsubishi_ch.py
@@ -1,7 +1,8 @@
 import re
-from typing import Iterable
+from typing import Any
 
 import chompjs
+from scrapy import Request
 from scrapy.http import Response
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
@@ -28,14 +29,26 @@ class MitsubishiCHSpider(JSONBlobSpider):
         feature.update(feature.pop("marker", {}) | feature.pop("locationData", {}))
         feature["id"] = feature.get("externalId")
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Any:
         item["street_address"] = item.pop("addr_full", None)
         item["name"] = feature.get("organisation")
         item["website"] = response.urljoin(feature.get("www"))
 
-        if feature.get("hasSales"):
+        # hasSales, hasService fields are always false.Hence, we need to determine category from the POI pages.
+
+        yield Request(url=item["website"], callback=self.parse_location, cb_kwargs=dict(item=item))
+
+    def parse_location(self, response: Response, item: Feature) -> Any:
+        location_type = (
+            response.xpath('//*[@class="haendler-heading-details"]//*[@class="haendler-icon-text"]').get("").lower()
+        )
+
+        if "verkauf und service" in location_type:
             apply_category(Categories.SHOP_CAR, item)
-            apply_yes_no(Extras.CAR_REPAIR, item, feature.get("hasService"))
-        elif feature.get("hasService"):
+            apply_yes_no(Extras.CAR_REPAIR, item, True)
+        elif "verkauf" in location_type:
+            apply_category(Categories.SHOP_CAR, item)
+        elif "service" in location_type:
             apply_category(Categories.SHOP_CAR_REPAIR, item)
+
         yield item
