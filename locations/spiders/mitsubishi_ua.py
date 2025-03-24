a77f2fb8af6ccfd262b1b5df0c071fcf8f95d8d7
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-03-24 14:54:42 +0530

    Add Mitsubishi UA Spider

---
+++ locations/spiders/mitsubishi_ua.py
@@ -0,0 +1,18 @@
+import chompjs
+from scrapy.http import Response
+
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class MitsubishiUASpider(JSONBlobSpider):
+    name = "mitsubishi_ua"
+    item_attributes = {"brand": "Mitsubishi", "brand_wikidata": "Q36033"}
+    start_urls = ["https://mitsubishi-motors.com.ua/find-a-dealer"]
+
+    def extract_json(self, response: Response) -> list[dict]:
+        return chompjs.parse_js_object(
+            response.xpath('//script[contains(text(), "window.__NUXT__")]/text()').re_first(
+                r"dealers\s*=\s*\'(\[.+?\])\'"
+            ),
+            unicode_escape=True,
+        )
