6fad2170a9420234f0834d0d474a91a2b0aa9101
Cj Malone <CjMalone@mail.com>
2025-03-26 14:24:14 +0000

    clean name

--- locations/spiders/seven_eleven_se.py
+++ locations/spiders/seven_eleven_se.py
@@ -19,7 +19,15 @@ class SevenElevenSESpider(JSONBlobSpider):
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item.pop("addr_full", None)
         item["street_address"] = merge_address_lines(feature["address"])
-        item["branch"] = item.pop("name").removeprefix("7-Eleven ")
+
+        if feature["type"] == "seven_eleven":
+            item["branch"] = item.pop("name").removeprefix("7-Eleven ")
+        elif feature["type"] == "seven_eleven_express":
+            item["branch"] = item.pop("name").removeprefix("7-Eleven Express ")
+            item["name"] = "7-Eleven Express"
+        else:
+            self.logger.error("Unexpected store type: {}".format(feature["type"]))
+
         item["opening_hours"] = OpeningHours()
         for day_hours in feature["openhours"]["standard"]:
             item["opening_hours"].add_range(DAYS[day_hours["weekday"]], day_hours["hours"][0], day_hours["hours"][1])
