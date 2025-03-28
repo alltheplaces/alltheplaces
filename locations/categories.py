e3ab9ddd14fd06ae06a8005f286de0aed8ce1b54
David Hicks <david@hicks.id.au>
2025-03-28 12:47:18 +1100

    Add San Francisco MTA taxi ranks (US) (70 ranks) (#12515)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/categories.py
+++ locations/categories.py
@@ -305,6 +305,7 @@ class Categories(Enum):
     SAMPLE_COLLECTION = {"healthcare": "sample_collection"}
     SHARPS_WASTE_BASKET = {"amenity": "waste_basket", "waste": "sharps"}
     SPEECH_THERAPIST = {"healthcare": "speech_therapist"}
+    TAXI = {"amenity": "taxi"}
     TELEPHONE = {"amenity": "telephone"}
     TOILETS = {"amenity": "toilets"}
     RESTAURANT = {"amenity": "restaurant"}
