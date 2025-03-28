32b03b435051beb402c1e49aa3e18141d61e67c6
David Hicks <david@hicks.id.au>
2025-03-28 12:47:13 +1100

    Add San Francisco MTA traffic signals (US) (1507 signals) (#12514)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/infrastructure/san_francisco_mta_traffic_signals_us.py
@@ -0,0 +1,25 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.arcgis_feature_server import ArcGISFeatureServerSpider
+
+
+class SanFranciscoMtaTrafficSignalsUSSpider(ArcGISFeatureServerSpider):
+    name = "san_francisco_mta_traffic_signals_us"
+    item_attributes = {
+        "operator": "San Francisco Municipal Transportation Agency",
+        "operator_wikidata": "Q7414072",
+        "state": "CA",
+    }
+    host = "services.sfmta.com"
+    context_path = "arcgis"
+    service_id = "DataSF/master"
+    layer_id = "1"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature["CNN"])
+        apply_category(Categories.HIGHWAY_TRAFFIC_SIGNALS, item)
+        yield item
