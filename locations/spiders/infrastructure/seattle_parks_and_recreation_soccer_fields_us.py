91fadc48e4b9e0793fa27954eae94a82bc994486
David Hicks <david@hicks.id.au>
2025-04-01 00:36:25 +1100

    seattle_parks_and_recreation_soccer_fields_us: fix lighting status

--- locations/spiders/infrastructure/seattle_parks_and_recreation_soccer_fields_us.py
+++ locations/spiders/infrastructure/seattle_parks_and_recreation_soccer_fields_us.py
@@ -24,7 +24,7 @@ class SeattleParksAndRecreationSoccerFieldsUSSpider(ArcGISFeatureServerSpider):
         if lit := feature.get("E_LIGHTS"):
             if lit == "Yes":
                 item["extras"]["lit"] = "yes"
-            elif lit == "no":
+            elif lit == "No":
                 item["extras"]["lit"] = "no"
         match feature.get("E_SURFACE"):
             case "Grass":
