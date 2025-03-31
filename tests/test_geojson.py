5f4ae5baeebc8b6d9d1bc8b2896d14890da94e40
Ian Dees <ian.dees@gmail.com>
2025-03-31 10:24:58 -0500

    Add testing for item_to_properties

--- tests/test_geojson.py
+++ tests/test_geojson.py
@@ -1,9 +1,59 @@
-from locations.exporters.geojson import item_to_geometry
+from locations.exporters.geojson import item_to_geometry, item_to_properties
 from locations.items import Feature
 
 
 def test_item_to_properties():
-    assert False
+    # Test with a feature that has some properties
+    item = Feature()
+    item["name"] = "Test Name"
+    # We have manual mapping for some specific properties
+    item["addr_full"] = "123 Test St"
+    item["street"] = "Test St"
+    item["housenumber"] = "123"
+    item["postcode"] = "12345"
+    item["city"] = "Test City"
+    item["state"] = "Test State"
+    item["country"] = "Test Country"
+    item["twitter"] = "test_twitter"
+    item["facebook"] = "test_facebook"
+    item["brand_wikidata"] = "Q123456"
+    item["located_in_wikidata"] = "Q654321"
+    # Ref should get converted to string
+    item["ref"] = 123
+    # This should get added alongside the other properties. Empty and null values should be ignored
+    item["extras"] = {"key1": "value1", "key2": "value2", "key3": None, "key4": ""}
+    properties = item_to_properties(item)
+    assert properties == {
+        "name": "Test Name",
+        "addr:full": "123 Test St",
+        "addr:street": "Test St",
+        "addr:housenumber": "123",
+        "addr:postcode": "12345",
+        "addr:city": "Test City",
+        "addr:state": "Test State",
+        "addr:country": "Test Country",
+        "contact:twitter": "test_twitter",
+        "contact:facebook": "test_facebook",
+        "brand:wikidata": "Q123456",
+        "located_in:wikidata": "Q654321",
+        "key1": "value1",
+        "key2": "value2",
+        "ref": "123",
+    }
+
+    # Test that empty or null values are ignored
+    item = Feature()
+    item["name"] = None
+    item["addr_full"] = ""
+    item["street"] = "Test St"
+    assert item_to_properties(item) == {
+        "addr:street": "Test St",
+    }
+
+    # Test with an empty feature
+    item = Feature()
+    properties = item_to_properties(item)
+    assert properties == {}
 
 
 def test_item_to_geometry():
