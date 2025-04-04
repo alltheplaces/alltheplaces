81003f961650bd2300e5115162fca48f3221e4c6
Karen Kerr <karenkerrgithub@gmail.com>
2025-04-04 11:26:04 +0100

    [UFCGym US] Add spider

---
+++ locations/spiders/ufc_gym_us.py
@@ -0,0 +1,28 @@
+import chompjs
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+
+
+class UFCGymUSSpider(SitemapSpider):
+    name = "ufc_gym_us"
+    item_attributes = {"brand": "UFC GYM", "brand_wikidata": "Q122511683"}
+    sitemap_urls = ["https://www.ufcgym.com/sitemap.xml"]
+    sitemap_rules = [(r"/locations/[-\w]+$", "parse")]
+
+    def sitemap_filter(self, entries):
+        for entry in entries:
+            if "list" not in entry["loc"]:
+                yield entry
+
+    def parse(self, response, **kwargs):
+        if data := response.xpath('//script[@id="__NEXT_DATA__"]/text()').get():
+            if location := chompjs.parse_js_object(data)["props"]["pageProps"].get("location"):
+                location.update(location.pop("position"))
+                item = DictParser.parse(location)
+                item["street_address"] = item.pop("street", "")
+                item["branch"] = item.pop("name")
+                item["website"] = response.url
+                apply_category(Categories.GYM, item)
+                yield item
