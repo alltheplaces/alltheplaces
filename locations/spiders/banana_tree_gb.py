d3fed538a38803d9f69108ce6b3400d5a2b798d5
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-07 11:28:40 +0530

    Fix Banana Tree GB Spider

--- locations/spiders/banana_tree_gb.py
+++ locations/spiders/banana_tree_gb.py
@@ -7,7 +7,8 @@ from locations.spiders.frankie_and_bennys_gb import FrankieAndBennysGBSpider
 class BananaTreeGBSpider(FrankieAndBennysGBSpider):
     name = "banana_tree_gb"
     item_attributes = {"brand": "Banana Tree", "brand_wikidata": "Q123013837"}
-    start_urls = ["https://api.bigtablegroup.com/cdg/allRestaurants/banana"]
+    start_urls = ["https://netapi.bigtablegroup.com/api/v1/content/search-restaurants/"]
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"brandkey": "banana"}}
 
     def parse_item(self, item: Feature, location: dict, **kwargs) -> Iterable[Feature]:
         item["website"] = "https://bananatree.co.uk/restaurants/{}/".format(location["slug"])
