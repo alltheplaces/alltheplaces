3ec09e9036538797f3a328c56d9d3af84ffeebd3
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-04-07 11:09:22 +0530

    code refactored to use crawl spider to fix spider

--- locations/spiders/sprouts_farmers_market.py
+++ locations/spiders/sprouts_farmers_market.py
@@ -1,37 +1,33 @@
 import re
+from typing import Any
 
-from scrapy.spiders import SitemapSpider
+from requests import Response
+from scrapy.spiders import CrawlSpider, Rule
 
+from locations.google_url import extract_google_position
 from locations.hours import OpeningHours, day_range, sanitise_day
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 from locations.structured_data_spider import StructuredDataSpider
+from scrapy.linkextractors import LinkExtractor
 
 
-class SproutsFarmersMarketSpider(SitemapSpider, StructuredDataSpider):
+class SproutsFarmersMarketSpider(CrawlSpider):
     name = "sprouts_farmers_market"
-    allowed_domains = ["www.sprouts.com"]
     item_attributes = {"brand": "Sprouts Farmers Market", "brand_wikidata": "Q7581369"}
-    sitemap_urls = ["https://www.sprouts.com/store-sitemap.xml"]
-    sitemap_rules = [(r"^https:\/\/www.sprouts.com\/store\/\w\w\/*\/*\/", "parse_sd")]
+    start_urls = ["https://www.sprouts.com/stores/"]
+    rules = [
+        Rule(
+            LinkExtractor(allow=r"https://www.sprouts.com/stores/[^/]+/"),
+        ),
+        Rule(LinkExtractor(allow=r"https://www.sprouts.com/store/[^/]+/[^/]+/[^/]+/"), callback="parse"),
+    ]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["website"] = response.url
-        item["opening_hours"] = ld_data["openingHours"].replace(",", ";")
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath('//*[@class="cell store-details"]/h1/text()').get()
+        item["street_address"] = response.xpath('//*[@class="store-address"]/a/text()[1]').get()
+        item["addr_full"] = response.xpath('//*[@class="store-address"]/a').xpath("normalize-space()").get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
         yield item
-
-    def pre_process_data(self, ld_data, **kwargs):
-        oh = OpeningHours()
-        for days, open_time, open_time_ampm, close_time, close_time_ampm in re.findall(
-            r"(\w+-\w+) (\d+:\d+)\s?(AM|am|PM|pm) (\d+:\d+)\s?(AM|am|PM|pm)",
-            ld_data["openingHours"],
-        ):
-            start_day, end_day = days.split("-")
-            start_day = sanitise_day(start_day)
-            end_day = sanitise_day(end_day)
-            for day in day_range(start_day, end_day):
-                oh.add_range(
-                    day,
-                    open_time + open_time_ampm,
-                    close_time + close_time_ampm,
-                    time_format="%I:%M%p",
-                )
-        ld_data["openingHours"] = oh.as_opening_hours()
