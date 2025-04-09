35fcf03ff3fcef1a16a1184c8ef253804f55ed29
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-09 11:00:12 +0530

    Fix Magasin Vert FR Spider

--- locations/spiders/magasin_vert_fr.py
+++ locations/spiders/magasin_vert_fr.py
@@ -1,9 +1,17 @@
-from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class MagasinVertFRSpider(AmastyStoreLocatorSpider):
+class MagasinVertFRSpider(JSONBlobSpider):
     name = "magasin_vert_fr"
     allowed_domains = [
         "www.monmagasinvert.fr",
     ]
     item_attributes = {"brand": "Magasin Vert", "brand_wikidata": "Q16661975"}
+    start_urls = [
+        "https://www.monmagasinvert.fr/rest/mon_magasin_vert_lot1/V1/inventory/in-store-pickup/pickup-locations/?searchRequest[scopeCode]=mon_magasin_vert_lot1"
+    ]
+    locations_key = "items"
+    needs_json_request = True
+
+    def pre_process_data(self, feature: dict) -> None:
+        feature["ref"] = feature.get("pickup_location_code")
