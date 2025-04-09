1fc7cd459f1180fbee257513e320c2532fec7ad1
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-09 11:01:14 +0530

    Fix branding

--- locations/spiders/magasin_vert_fr.py
+++ locations/spiders/magasin_vert_fr.py
@@ -1,3 +1,8 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
@@ -6,7 +11,10 @@ class MagasinVertFRSpider(JSONBlobSpider):
     allowed_domains = [
         "www.monmagasinvert.fr",
     ]
-    item_attributes = {"brand": "Magasin Vert", "brand_wikidata": "Q16661975"}
+    BRANDS = {
+        "mv": {"brand": "Magasin Vert", "brand_wikidata": "Q16661975"},
+        "pv": {"brand": "Point Vert", "brand_wikidata": "Q16661975"},
+    }
     start_urls = [
         "https://www.monmagasinvert.fr/rest/mon_magasin_vert_lot1/V1/inventory/in-store-pickup/pickup-locations/?searchRequest[scopeCode]=mon_magasin_vert_lot1"
     ]
@@ -15,3 +23,9 @@ class MagasinVertFRSpider(JSONBlobSpider):
 
     def pre_process_data(self, feature: dict) -> None:
         feature["ref"] = feature.get("pickup_location_code")
+        feature.update(feature.pop("extension_attributes", {}))
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if brand_info := self.BRANDS.get(feature.get("store_type")):
+            item.update(brand_info)
+        yield item
