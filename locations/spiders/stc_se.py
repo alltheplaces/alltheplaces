012c8e441fe7982f70849b1bedae75b3e78643d3
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-04-15 11:45:54 +0530

    code updated

--- locations/spiders/stc_se.py
+++ locations/spiders/stc_se.py
@@ -6,11 +6,12 @@ import scrapy
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from urllib.parse import urljoin
 
 
 class StcSESpider(scrapy.Spider):
     name = "stc_se"
-    item_attributes = {"brand": "STC", "brand_wikidata": "Q124061743"}
+    item_attributes = {"brand": "STC", "brand_wikidata": "Q115114066"}
     start_urls = ["https://www.stc.se/gym"]
 
     def parse(self, response):
@@ -18,6 +19,7 @@ class StcSESpider(scrapy.Spider):
         for club in json.loads(re.search(r"clubsData\":(\[.*\]),\"clubTypeInformation", raw_data).group(1)):
             item = DictParser.parse(club)
             item["branch"] = item.pop("name")
-            item["website"] = "https://www.stc.se/gym/" + club["slug"]
+            item["street_address"] = item.pop("street")
+            item["website"] = urljoin("https://www.stc.se/gym/", club["slug"])
             apply_category(Categories.GYM, item)
             yield item
