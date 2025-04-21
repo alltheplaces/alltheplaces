f8fda12360bbccc9c4d9c6af83751973ca27dd5b
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-21 17:28:10 +0530

    Add headers to fix H R Block Spider

--- locations/spiders/h_r_block.py
+++ locations/spiders/h_r_block.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
@@ -13,10 +15,18 @@ class HRBlockSpider(SitemapSpider):
     custom_settings = {
         "DOWNLOADER_MIDDLEWARES": {"scrapy.downloadermiddlewares.redirect.MetaRefreshMiddleware": None},
         "USER_AGENT": BROWSER_DEFAULT,
+        "ROBOTSTXT_OBEY": False,
+        "DEFAULT_REQUEST_HEADERS": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9",
+            "sec-ch-ua": '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+        },
     }
     sitemap_follow = ["opp"]
 
-    def parse(self, response: Response, **kwargs):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
         item["lat"] = response.xpath("//@data-latitude").get()
         item["lon"] = response.xpath("//@data-longitude").get()
