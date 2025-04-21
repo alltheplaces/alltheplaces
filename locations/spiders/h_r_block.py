42e14c7ca8d1ad7c9d887ef2f4f8dad8e8a1365a
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-21 17:37:28 +0530

    Switch to Structured Data Spider

--- locations/spiders/h_r_block.py
+++ locations/spiders/h_r_block.py
@@ -1,17 +1,16 @@
-from typing import Any
-
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class HRBlockSpider(SitemapSpider):
+class HRBlockSpider(SitemapSpider, StructuredDataSpider):
     name = "h_r_block"
     item_attributes = {"brand": "H&R Block", "brand_wikidata": "Q5627799"}
     sitemap_urls = ["https://www.hrblock.com/sitemap.xml"]
-    sitemap_rules = [(r"https://www\.hrblock\.com/local-tax-offices/.+/.+/.+/(\d+)/$", "parse")]
+    sitemap_rules = [(r"https://www\.hrblock\.com/local-tax-offices/[^/]+/[^/]+/[^/]+/\d+/$", "parse_sd")]
     custom_settings = {
         "DOWNLOADER_MIDDLEWARES": {"scrapy.downloadermiddlewares.redirect.MetaRefreshMiddleware": None},
         "USER_AGENT": BROWSER_DEFAULT,
@@ -25,12 +24,10 @@ class HRBlockSpider(SitemapSpider):
         },
     }
     sitemap_follow = ["opp"]
+    drop_attributes = {"image"}
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        item = Feature()
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["ref"] = response.xpath("//@data-office-id").get()
         item["lat"] = response.xpath("//@data-latitude").get()
         item["lon"] = response.xpath("//@data-longitude").get()
-        item["addr_full"] = response.xpath('//*[@class="lbl-address"]').xpath("normalize-space()").get()
-        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
-        item["ref"] = item["website"] = response.url
         yield item
