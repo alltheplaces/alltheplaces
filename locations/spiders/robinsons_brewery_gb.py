3033c044f5a6165e55b2d9e6ea5f9f895e26f778
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-21 20:08:43 +0200

    [Robinsons Brewery GB] add spider

---
+++ locations/spiders/robinsons_brewery_gb.py
@@ -0,0 +1,22 @@
+import json
+
+from scrapy import Spider
+from locations.dict_parser import DictParser
+
+
+class RobinsonsBrewereyGBSpider(Spider):
+    name = "robinsons_brewery_gb"
+    item_attributes = {
+        "brand": "Robinsons Brewery",
+        "brand_wikidata": "Q7353116",
+    }
+    start_urls = [
+        "https://www.robinsonsbrewery.com/page-data/search-results/page-data.json"
+    ]
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["result"]["data"]["allWpPub"]["nodes"]:
+            item = DictParser.parse(location["PubData"])
+            item["ref"]=location["PubData"]["googlePlaceId"]
+            item["city"]=location["PubData"]["displayLocation"]
+            yield item
