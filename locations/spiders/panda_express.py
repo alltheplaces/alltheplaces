0b0c4c7757e5066dcd453b36042bef6fb6096372
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-04-21 15:15:00 +0530

    code refactored to use sitemap to fix spider

--- locations/spiders/panda_express.py
+++ locations/spiders/panda_express.py
@@ -1,37 +1,10 @@
-from scrapy import FormRequest
-from scrapy.http import JsonRequest
+from scrapy.spiders import SitemapSpider
 
-from locations.storefinders.nomnom import NomNomSpider, slugify
-from locations.user_agents import BROWSER_DEFAULT
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class PandaExpressSpider(NomNomSpider):
+class PandaExpressSpider(SitemapSpider, StructuredDataSpider):
     name = "panda_express"
     item_attributes = {"brand": "Panda Express", "brand_wikidata": "Q1358690"}
-    domain = "pandaexpress.com"
-    user_agent = BROWSER_DEFAULT
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    use_calendar = False
-
-    def start_requests(self):
-        # Fetch cookies to avoid DataDome captcha blockage
-        yield FormRequest(
-            url="https://api-js.datadome.co/js/",
-            headers={
-                "referer": "https://www.pandaexpress.com/",
-            },
-            formdata={"ddk": "988C29D6706800A8C3451C0AB0E93A"},
-            callback=self.parse_cookies,
-        )
-
-    def parse_cookies(self, response):
-        yield JsonRequest(
-            url="https://nomnom-prod-api.pandaexpress.com/restaurants",
-            cookies={"cookie": response.json()["cookie"]},
-        )
-
-    def post_process_item(self, item, response, feature):
-        item["website"] = (
-            f"https://www.pandaexpress.com/locations/{slugify(feature['state'])}/{slugify(feature['city'])}/{feature['extref']}"
-        )
-        yield item
+    sitemap_urls = ["https://www.pandaexpress.com/locations/sitemap.xml"]
+    sitemap_rules = [(r"https://www.pandaexpress.com/locations/[^/]+/[^/]+/\d+$", "parse_sd")]
