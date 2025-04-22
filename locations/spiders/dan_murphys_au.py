5151332adea1adf14a2b0ee956eddde5b4036f08
David Hicks <david@hicks.id.au>
2025-04-22 18:15:06 +1000

    dan_murphys_au: fix spider

--- locations/spiders/dan_murphys_au.py
+++ locations/spiders/dan_murphys_au.py
@@ -1,42 +1,29 @@
-from typing import Any, Iterable
-from urllib.parse import urljoin
+from typing import Iterable
 
-from scrapy import Request, Spider
-from scrapy.http import JsonRequest, Response
+from scrapy.http import Response
 
-from locations.categories import Categories
-from locations.dict_parser import DictParser
+from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 from locations.pipelines.address_clean_up import merge_address_lines
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class DanMurphysAUSpider(Spider):
+class DanMurphysAUSpider(JSONBlobSpider):
     name = "dan_murphys_au"
-    item_attributes = {"brand": "Dan Murphy's", "brand_wikidata": "Q5214075", "extras": Categories.SHOP_ALCOHOL.value}
+    item_attributes = {"brand": "Dan Murphy's", "brand_wikidata": "Q5214075"}
     allowed_domains = ["api.danmurphys.com.au"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    requires_proxy = True
+    start_urls = ["https://api.danmurphys.com.au/apis/ui/StoreLocator/Stores/danmurphys"]
+    user_agent = BROWSER_DEFAULT
+    locations_key = "Stores"
 
-    def start_requests(self) -> Iterable[Request]:
-        for state in ["ACT", "NSW", "QLD", "SA", "VIC", "TAS", "WA", "NT"]:
-            yield JsonRequest(
-                "https://api.danmurphys.com.au/apis/ui/StoreLocator/Stores/bws?state={}&type=allstores&Max=5000".format(
-                    state
-                )
-            )
-
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json()["Stores"]:
-            item = DictParser.parse(location)
-            item["ref"] = location["Id"]
-            if branch_name := item.pop("name", None):
-                item["branch"] = branch_name
-            item["street_address"] = merge_address_lines([location["AddressLine1"], location["AddressLine2"]])
-            slug = "{}-{}-{}".format(location["State"], location["Suburb"].replace(" ", "-"), item["ref"])
-            item["website"] = urljoin("https://danmurphys.com.au/stores/", slug)
-
-            item["opening_hours"] = OpeningHours()
-            for rule in location["OpeningHours"]:
-                item["opening_hours"].add_range(rule["Day"], *rule["Hours"].split(" - "), "%I:%M %p")
-
-            yield item
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name", None)
+        item["street_address"] = merge_address_lines([feature.get("AddressLine1"), feature.get("AddressLine2")])
+        item["website"] = "https://www.danmurphys.com.au/stores/{}-{}-{}".format(feature["State"], feature["Name"].replace(" ", "-"), feature["Id"])
+        item["opening_hours"] = OpeningHours()
+        hours_string = " ".join(["{}: {}".format(day_hours["Day"], day_hours["Hours"]) for day_hours in feature.get("OpeningHours", [])[:-1]])
+        item["opening_hours"].add_ranges_from_string(hours_string)
+        apply_category(Categories.SHOP_ALCOHOL, item)
+        yield item
