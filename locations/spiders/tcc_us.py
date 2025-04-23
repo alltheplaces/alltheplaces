fcd514a2fc41316316cf88798b21b0de0f8f75dd
Karen Kerr <karenkerrgithub@gmail.com>
2025-04-23 09:10:51 +0100

    [TCC US] Fix category

--- locations/spiders/tcc_us.py
+++ locations/spiders/tcc_us.py
@@ -1,6 +1,9 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from scrapy.http import Response
 
 
 class TccUSSpider(SitemapSpider, StructuredDataSpider):
@@ -15,3 +18,7 @@ class TccUSSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://locations.tccrocks.com/sitemap.xml"]
     sitemap_rules = [(r"https://locations.tccrocks.com/\w\w/.+/.+\.html", "parse_sd")]
     wanted_types = ["MobilePhoneStore"]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        apply_category(Categories.SHOP_MOBILE_PHONE, item)
+        yield item
