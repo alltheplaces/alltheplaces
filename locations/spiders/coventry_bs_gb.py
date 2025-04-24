c0cb3f11d7e04de9f46e92485602ee81b376c745
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-24 13:34:01 +0200

    [Coventry Building Society GB] Add spider

---
+++ locations/spiders/coventry_bs_gb.py
@@ -0,0 +1,20 @@
+import json
+
+from scrapy import Spider
+from locations.dict_parser import DictParser
+
+
+class CoventryBSGBSpider(Spider):
+    name = "coventry_bs_gb"
+    item_attributes = {
+        "brand": "Coventry Building Society",
+        "brand_wikidata": "Q5179055",
+    }
+    start_urls = [
+        "https://www.coventrybuildingsociety.co.uk/content/datasource/api/branch/allbranches.data.json"
+    ]
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["branches"]:
+            item = DictParser.parse(location)
+            yield item
