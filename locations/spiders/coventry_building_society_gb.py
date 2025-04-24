9e288f70e7a373449185c114f80eb5df308a1cd5
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-24 16:57:17 +0200

    Update coventry_building_society_gb.py

--- locations/spiders/coventry_building_society_gb.py
+++ locations/spiders/coventry_building_society_gb.py
@@ -1,6 +1,7 @@
 from scrapy import Spider
 
 from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class CoventryBuildingSocietyGBSpider(Spider):
@@ -14,6 +15,13 @@ class CoventryBuildingSocietyGBSpider(Spider):
     def parse(self, response, **kwargs):
         for location in response.json()["branches"]:
             item = DictParser.parse(location["address"])
-            item["ref"] = location["branchdetails"]["branchname"]
-            item["branch"] = location["branchdetails"]["branchname"]
+            item["ref"]=location["branchdetails"]["branchname"]
+            item["street_address"] = merge_address_lines([location["address"].get("address1"), location["address"].get("address2")])
+            if "html" in location["branchdetails"]["branchnumber"]:
+                item["branch"] = location["branchdetails"]["branchnumber"].replace("/content/cbs/member/en/our-branches/","").replace(".html","")
+                item["website"] = "https://www.coventrybuildingsociety.co.uk/member/our-branches/"+item["branch"]+".html"
+                item["name"] = "Coventry Building Society"
+            else:
+                item["name"] = location["branchdetails"]["branchname"]
             yield item
+
