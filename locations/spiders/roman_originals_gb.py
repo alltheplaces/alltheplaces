1a17ece40ae15552c22c66b1cd8a9d1440ea26f3
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-27 21:22:21 +0200

    [Roman Originals GB] Add spider

---
+++ locations/spiders/roman_originals_gb.py
@@ -0,0 +1,36 @@
+import json
+
+from typing import Any, Iterable
+from urllib.parse import urljoin
+from scrapy.http import Response
+from scrapy.spiders import Spider
+from locations.pipelines.address_clean_up import merge_address_lines
+from locations.dict_parser import DictParser
+
+
+class RomanOriginalsGBSpider(Spider):
+    name = "roman_originals_gb"
+    item_attributes = {"brand": "Roman Originals", "brand_wikidata": "Q94579553"}
+    start_urls = ["https://www.roman.co.uk/store-locator"]
+
+    def find_between(self, text, first, last):
+        start = text.index(first) + len(first)
+        end = text.index(last, start)
+        return text[start:end]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        data = self.find_between(response.text, '"@graph":', "}</script>")
+        json_data = json.loads(data)
+        for stores in json_data:
+            if stores["@type"]=='Store':
+                for store in stores["department"]:
+                    item = DictParser.parse(store)
+                    item["street_address"] = merge_address_lines([store["address"]["streetAddress"], store["address"]["addressLocality"]])
+                    item["website"] = urljoin("https://www.roman.co.uk",store["url"])
+                    item["geometry"] = store["location"]["geo"]
+#                    item["opening_hours"] = OpeningHours()
+                     #Opening hours are wrong
+#                    for day in store["openingHoursSpecification"][0]["dayOfWeek"]:
+#                        item["opening_hours"].add_range(day, store["openingHoursSpecification"][0]["opens"], store["openingHoursSpecification"][0]["closes"])
+
+                yield item
