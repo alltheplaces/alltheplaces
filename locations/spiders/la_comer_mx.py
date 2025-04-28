b80bcf3f41584237eec02a0e2309f560b2ec3cd0
David Hicks <david@hicks.id.au>
2025-04-29 01:27:21 +1000

    Add La Comer (MX) supermarket brands
    * La Comer (the supermarket chain, not the parent holding company)
    * City Market
    * Fresko
    * Sumesa
    
    Note the different Wikidata items for the parent holding company for all
    these supermarket brands (Q26765126) versus the brand name of the "La
    Comer" supermarket chain (Q134237561) within this group of brands.

---
+++ locations/spiders/la_comer_mx.py
@@ -0,0 +1,32 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours, DAYS
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class LaComerMXSpider(JSONBlobSpider):
+    name = "la_comer_mx"
+    item_attributes = {"brand": "La Comer", "brand_wikidata": "Q134237561"}
+    allowed_domains = ["www.lacomer.com.mx"]
+    start_urls = ["https://www.lacomer.com.mx/lacomer-api/api/v1/public/sucursal/sucursales?succFmt=200&succId=380&tipoNombre="]
+    locations_key = ["listLacomer"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature["succId"])
+        item["branch"] = feature["succDesOri"].removeprefix(self.item_attributes["brand"]).strip()
+        item["housenumber"] = feature["succNumext"]
+        item["street"] = feature["succCalle"]
+        item["city"] = feature["succCol"]
+        item["postcode"] = feature["succCp"]
+        item["state"] = feature["succEdo"]
+        item["lat"] = feature["succLatitud"]
+        item["lon"] = feature["succLongitud"]
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_days_range(DAYS, *feature["horario"].split(" a ", 1))
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+        yield item
