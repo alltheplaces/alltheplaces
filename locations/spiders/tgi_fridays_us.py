d6255dbb3e9cb50f37d41a0ac845ebdde575c567
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-29 17:46:45 +0530

    Add branch

--- locations/spiders/tgi_fridays_us.py
+++ locations/spiders/tgi_fridays_us.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -9,3 +11,7 @@ class TgiFridaysUSSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://locations.tgifridays.com/robots.txt"]
     sitemap_rules = [(r"locations\.tgifridays\.com/[a-z]{2}/[-\w]+/[-\w]+\.html$", "parse_sd")]
     drop_attributes = {"facebook", "twitter"}
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").split(",")[0].title()
+        yield item
