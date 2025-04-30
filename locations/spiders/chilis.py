275c37bcb42c07cae2f8da9bb4b9b94e660aec9f
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-04-30 20:09:53 +0530

    Fix Chilis Spider

--- locations/spiders/chilis.py
+++ locations/spiders/chilis.py
@@ -1,28 +1,45 @@
+from typing import Any
+
+import chompjs
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class ChilisSpider(CrawlSpider, StructuredDataSpider):
+class ChilisSpider(CrawlSpider):
     name = "chilis"
     item_attributes = {"brand": "Chili's", "brand_wikidata": "Q1072948"}
     allowed_domains = ["chilis.com"]
     download_delay = 0.5
-    start_urls = ["https://www.chilis.com/locations/us/all"]
-    rules = (
+    start_urls = ["https://www.chilis.com/locations"]
+    rules = [
         Rule(
-            LinkExtractor(restrict_xpaths='//div[contains(@class, "city-locations")]//a[@class="city-link"]'),
-            follow=True,
-            callback="parse_city",
+            LinkExtractor(allow=r"/locations/[a-z]{2}/[-\w]+$"),
         ),
         Rule(
-            LinkExtractor(restrict_xpaths='//a[text()="Details"]'),
-            follow=True,
-            callback="parse_sd",
+            LinkExtractor(allow=r"/locations/[a-z]{2}/[-\w]+/[-\w]+$"),
         ),
-    )
+        Rule(LinkExtractor(allow=r"/locations/[a-z]{2}/[-\w]+/[-\w]+/[-\w]+"), callback="parse"),
+    ]
 
-    def post_process_item(self, item, response, ld_data):
-        item["ref"] = ld_data["branchCode"]
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        location = chompjs.parse_js_object(
+            response.xpath('//script[contains(text(), "Coordinates")]/text()')
+            .re_first(r"Address\\\":\[.+?\]")
+            .replace("\\", "")
+        )[0]
+        item = DictParser.parse(location)
+        item["ref"] = item["website"] = response.url
+        item["street_address"] = merge_address_lines(
+            [
+                location.get("StreetAddressOne"),
+                location.get("StreetAddressTwo"),
+                location.get("StreetAddressThree"),
+                location.get("StreetAddressFour"),
+            ]
+        )
+        item["phone"] = response.xpath('//a[contains(@href, "tel:")]/@href').get()
         yield item
