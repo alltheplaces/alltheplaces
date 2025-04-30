7876a38c3a566992f57f30398cfc7a41faf84356
Karen Kerr <karenkerrgithub@gmail.com>
2025-04-30 11:53:05 +0100

    [DollarCurtainsAndBlinds AU] Fix data errors to generate more POIs

--- locations/spiders/dollar_curtains_and_blinds_au.py
+++ locations/spiders/dollar_curtains_and_blinds_au.py
@@ -18,19 +18,10 @@ class DollarCurtainsAndBlindsAUSpider(Spider):
 
     def parse(self, response):
         for location in response.json():
+            location.update(location.pop("map_location"))
             item = DictParser.parse(location)
-            item["ref"] = location["link"]
-            item["name"] = location["map_location"]["name"]
-            item["lat"] = location["map_location"]["lat"]
-            item["lon"] = location["map_location"]["lng"]
+            item["ref"] = item["website"] = location["link"]
             item["addr_full"] = location["address_formatted"]
-            item["housenumber"] = location["map_location"]["street_number"]
-            item["street"] = location["map_location"]["street_name"]
-            item["city"] = location["map_location"]["city"]
-            item["state"] = location["map_location"]["state"]
-            item["postcode"] = location["map_location"]["post_code"]
-            item["country"] = location["map_location"]["country"]
-            item["website"] = location["link"]
             item["opening_hours"] = OpeningHours()
             item["opening_hours"].add_ranges_from_string(location["open_hours"])
             apply_category(Categories.SHOP_HOUSEWARE, item)
