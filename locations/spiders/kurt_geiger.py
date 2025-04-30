664e4b34fffb34a43d5c108d16cdc9a0db8dcbdc
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-30 23:11:34 +0200

    Try website again

--- locations/spiders/kurt_geiger.py
+++ locations/spiders/kurt_geiger.py
@@ -10,6 +10,6 @@ class KurtGeigerSpider(YextAnswersSpider):
     api_key = "672f8261f70abeb66b9ce6c5c0572e7f"
     locale = "en-GB"
 
-    def parse_item(self, location, item, **kwargs):
-        # item["website"] = urljoin("https://www.kurtgeiger.com", location["data"]["slug"])
+    def parse_item(self, item, location, **kwargs):
+        item["website"] = urljoin("https://www.kurtgeiger.com", location["data"]["slug"])
         yield item
