729172d190a10cb6996138625e016745acbda32b
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-30 12:21:43 +0200

    [TOFS GB] Add spider

---
+++ locations/spiders/tofs_gb.py
@@ -0,0 +1,20 @@
+import json
+
+from scrapy import Spider
+from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
+
+
+class TofsGBSpider(Spider):
+    name = "tofs_gb"
+    item_attributes = {
+        "brand": "The Original Factory Shop",
+        "brand_wikidata": "Q7755366",
+    }
+    start_urls = ["https://api.storepoint.co/v1/15f4fbe5b10e3d/locations?rq"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def parse(self, response, **kwargs):
+        for location in response.json()["results"]["locations"]:
+            item = DictParser.parse(location)
+            yield item
