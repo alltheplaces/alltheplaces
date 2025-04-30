aec48896a9bb3982e70ca8959faa050e8a848316
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-04-30 21:59:48 +0200

    [Kurt Geiger] Add spider

---
+++ locations/spiders/kurt_geiger.py
@@ -0,0 +1,11 @@
+from locations.spiders.central_england_cooperative import set_operator
+from locations.storefinders.yext_answers import YextAnswersSpider
+
+
+class KurtGeigerSpider(YextAnswersSpider):
+    name = "kurt_geiger"
+    item_attributes = {"brand": "Kurt Geiger", "brand_wikidata": "Q15824663"}
+    endpoint = "https://prod-cdn.us.yextapis.com/v2/accounts/me/search/vertical/query"
+    experience_key = "kurt-geiger-search"
+    api_key = "672f8261f70abeb66b9ce6c5c0572e7f"
+    locale = "en-GB"
