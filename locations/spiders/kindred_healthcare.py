035e3118ce9cfe2240150f0f82f46eee07747100
Karen Kerr <karenkerrgithub@gmail.com>
2025-05-01 15:12:57 +0100

    [KindredHealthcare] Fix data errors

--- locations/spiders/kindred_healthcare.py
+++ locations/spiders/kindred_healthcare.py
@@ -19,7 +19,7 @@ class KindredHealthcareSpider(SitemapSpider):
         item = Feature()
         item["name"] = response.xpath("//h1/text()").get()
         item["addr_full"] = clean_address(response.xpath('//*[@class="cmp-text"]/p/text()').get())
-        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get().replace(".", "")
+        if phone := response.xpath('//*[contains(@href,"tel:")]/text()').get():
+            item["phone"] = phone.replace(".", "")
         item["ref"] = item["website"] = response.url
-        extract_google_position(item, response)
         yield item
