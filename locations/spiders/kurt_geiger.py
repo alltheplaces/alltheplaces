9d5969bd08eeb9209c3184b89449c83cf9603ad4
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-05-01 11:41:48 +0200

    Try website again

--- locations/spiders/kurt_geiger.py
+++ locations/spiders/kurt_geiger.py
@@ -1,4 +1,4 @@
-# from urllib.parse import urljoin
+from urllib.parse import urljoin
 
 from locations.categories import Categories, apply_category
 from locations.storefinders.yext_answers import YextAnswersSpider
@@ -13,6 +13,6 @@ class KurtGeigerSpider(YextAnswersSpider):
     locale = "en-GB"
 
     def parse_item(self, location, item, **kwargs):
-        # item["website"] = urljoin("https://www.kurtgeiger.com", location["data"]["slug"])
+        item["website"] = urljoin("https://www.kurtgeiger.com", location["data"]["slug"])
         apply_category(Categories.SHOP_SHOES, item)
         yield item
