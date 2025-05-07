633128fb093c95dc83f59f0c32a1a8d5174a8b39
David Riordan <dr@daveriordan.com>
2025-05-07 15:39:47 +0000

    Add spiders for Associated Supermarket Group brands

---
+++ locations/spiders/compare_foods.py
@@ -0,0 +1,9 @@
+"""Spider for Compare Foods stores."""
+from locations.storefinders.associated_supermarket_group import AssociatedSupermarketGroupSpider
+
+
+class CompareFoodsSpider(AssociatedSupermarketGroupSpider):
+    """Spider for Compare Foods stores."""
+    name = "compare_foods"
+    item_attributes = {"brand": "Compare Foods"}
+    start_urls = ["https://www.shopcomparefoods.com/locations/"]
---
+++ locations/spiders/met_foodmarket.py
@@ -0,0 +1,9 @@
+"""Spider for Met Foodmarket stores."""
+from locations.storefinders.associated_supermarket_group import AssociatedSupermarketGroupSpider
+
+
+class MetFoodmarketSpider(AssociatedSupermarketGroupSpider):
+    """Spider for Met Foodmarket stores."""
+    name = "met_foodmarket"
+    item_attributes = {"brand": "Met Foodmarket", "brand_wikidata": "Q6822231"}
+    start_urls = ["https://www.metfoods.com/locations"]
