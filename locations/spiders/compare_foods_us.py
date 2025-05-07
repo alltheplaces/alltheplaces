33a770abe5b33ece265c1b129f8000f21246bc3e
Cj Malone <CjMalone@mail.com>
2025-05-07 17:27:41 +0100

    Rename spider

--- locations/spiders/compare_foods_us.py
+++ locations/spiders/compare_foods_us.py
@@ -3,9 +3,9 @@
 from locations.storefinders.associated_supermarket_group import AssociatedSupermarketGroupSpider
 
 
-class CompareFoodsSpider(AssociatedSupermarketGroupSpider):
+class CompareFoodsUSSpider(AssociatedSupermarketGroupSpider):
     """Spider for Compare Foods stores."""
 
-    name = "compare_foods"
+    name = "compare_foods_us"
     item_attributes = {"brand": "Compare Foods"}
     start_urls = ["https://www.shopcomparefoods.com/locations/"]
