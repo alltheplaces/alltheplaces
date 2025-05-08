4d13dcd78f640cd8a6c934359d2f79d34234dd78
Cj Malone <CjMalone@mail.com>
2025-05-08 14:19:37 +0100

    apply_category

--- locations/spiders/sleep_number_us.py
+++ locations/spiders/sleep_number_us.py
@@ -1,17 +1,20 @@
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from typing import Any
 
-from locations.categories import Categories
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
+
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_FULL, OpeningHours
 
 
 class SleepNumberUSSpider(Spider):
     name = "sleep_number_us"
-    item_attributes = {"brand_wikidata": "Q7447640", "brand": "Sleep Number", "extras": Categories.SHOP_BED.value}
+    item_attributes = {"brand": "Sleep Number", "brand_wikidata": "Q7447640"}
+    custom_settings = {"DOWNLOAD_TIMEOUT": 60}
 
     def start_requests(self):
-        for city in [
+        for lat, lon in [
             (21.30694, -157.85833),
             (26.12231, -80.14338),
             (30.44332, -91.18747),
@@ -19,10 +22,12 @@ class SleepNumberUSSpider(Spider):
             (44.97997, -93.26384),
         ]:
             yield JsonRequest(
-                url=f"https://www.sleepnumber.com/api/storefront/store-locations?lat={city[0]}&lng={city[1]}&limit=190&radius=25000"
+                url="https://www.sleepnumber.com/api/storefront/store-locations?lat={}&lng={}&limit=190&radius=25000".format(
+                    lat, lon
+                )
             )
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json()["entities"]:
             item = DictParser.parse(store)
             item["ref"] = store["cid"]
@@ -34,5 +39,6 @@ class SleepNumberUSSpider(Spider):
                     continue
                 for time_period in day_hours["openIntervals"]:
                     item["opening_hours"].add_range(day_name.title(), time_period["start"], time_period["end"])
+            apply_category(Categories.SHOP_BED, item)
 
             yield item
