cd5e8cef2e3248fa26d58807f5ebfcadffc837d0
Cj Malone <CjMalone@mail.com>
2025-05-08 09:19:11 +0100

    Clean name

--- locations/spiders/the_body_shop_gb.py
+++ locations/spiders/the_body_shop_gb.py
@@ -1,3 +1,8 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
 from locations.storefinders.storeify import StoreifySpider
 
 
@@ -6,3 +11,7 @@ class TheBodyShopGBSpider(StoreifySpider):
     item_attributes = {"brand": "The Body Shop", "brand_wikidata": "Q837851"}
     api_key = "the-body-shop-uk.myshopify.com"
     domain = "https://www.thebodyshop.com/"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("The Body Shop ")
+        yield item
