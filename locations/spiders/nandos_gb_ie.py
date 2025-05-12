95ad1c915f49a7f40bd3c8d580a256efc7683697
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-05-12 21:19:27 +0200

    Update nandos_gb_ie.py

--- locations/spiders/nandos_gb_ie.py
+++ locations/spiders/nandos_gb_ie.py
@@ -22,5 +22,5 @@ class NandosGBIESpider(SitemapSpider, StructuredDataSpider):
         if "our Nino restaurant" in response.text:
             item.update(NINO_NANDOS)
             apply_category(Categories.FAST_FOOD, item)
-        if not "This restaurant is now closed." in response.text:
+        if "This restaurant is now closed." not in response.text:
             yield item
