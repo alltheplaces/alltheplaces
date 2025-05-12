f20a3a2efc696c24d86d7bdd867fba0afaf1406c
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-05-12 23:17:09 +0200

    Remove website

--- locations/spiders/kurt_geiger.py
+++ locations/spiders/kurt_geiger.py
@@ -13,6 +13,6 @@ class KurtGeigerSpider(YextAnswersSpider):
     locale = "en-GB"
 
     def parse_item(self, location, item, **kwargs):
-        item["website"] = urljoin("https://www.kurtgeiger.com", location["data"]["slug"])
+        #item["website"] = urljoin("https://www.kurtgeiger.com", location["data"]["slug"])
         apply_category(Categories.SHOP_SHOES, item)
         yield item
