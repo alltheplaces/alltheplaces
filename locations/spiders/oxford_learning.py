83c8e76fdb99a79708c5d9c19425cc670ec91428
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-05-13 14:26:15 +0530

    [Oxford Learning] Fix Spider (#13109)

--- locations/spiders/oxford_learning.py
+++ locations/spiders/oxford_learning.py
@@ -1,13 +1,11 @@
-import json
-import re
 from typing import Any
 
+import chompjs
 from scrapy import Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.items import SocialMedia, set_social_media
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
@@ -17,20 +15,14 @@ class OxfordLearningSpider(Spider):
     start_urls = ["https://www.oxfordlearning.com/locations/"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in json.loads(re.search(r"var olc_elements = ({.+});", response.text).group(1))[
-            "locationData"
-        ].values():
+        for location in chompjs.parse_js_object(response.xpath('//*[contains(text(),"kh_ld_locations")]/text()').get())[
+            0
+        ][0]:
+            location.update(location.pop("details"))
             item = DictParser.parse(location)
-            item["addr_full"] = None
+            item["website"] = location["url_en"]
+            item["ref"] = location["mis_location_id"]
             item["branch"] = item.pop("name")
-            item["street_address"] = merge_address_lines([location["address"], location["address2"]])
-            item["ref"] = item["website"]
-
-            if fb := location.get("facebook"):
-                set_social_media(item, SocialMedia.FACEBOOK, "https://www.facebook.com/{}".format(fb))
-            if twitter := location.get("twitter"):
-                set_social_media(item, SocialMedia.TWITTER, "https://twitter.com/{}".format(twitter))
-
+            item["street_address"] = merge_address_lines([location["address2"], location["address1"]])
             apply_category(Categories.PREP_SCHOOL, item)
-
             yield item
