41b73f8e8833bea459732c93db5a6eaf4b9c944a
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-05-13 13:35:26 +0530

    Clean & update older opening hours code

--- locations/spiders/vivacom_bg.py
+++ locations/spiders/vivacom_bg.py
@@ -1,4 +1,3 @@
-import re
 from typing import Any
 
 import chompjs
@@ -6,6 +5,7 @@ from scrapy import Spider
 from scrapy.http import Response
 
 from locations.dict_parser import DictParser
+from locations.hours import DAYS_BG, OpeningHours
 
 
 class VivacomBGSpider(Spider):
@@ -25,33 +25,7 @@ class VivacomBGSpider(Spider):
             item = DictParser.parse(store)
             item["lat"], item["lon"] = store["latlng"].split(",")
 
-            opening_hours = (
-                store["store_time"]
-                .strip()
-                .replace("почивен ден", "off")
-                .replace("пон.", "Mo")
-                .replace("пт.", "Fr")
-                .replace("пет.", "Fr")
-                .replace("съб.", "Sa")
-                .replace("съб", "Sa")
-                .replace("нед.", "Su")
-                .replace("нд.", "Su")
-                .replace("  ", " ")
-            )
-            oh = []
-            for rule in re.findall(
-                r"(\w{2})\s?-?\s?(\w{2})?:?\s?(\d{2}(\.|:)\d{2})\s?-\s?(\d{2}(\.|:)\d{2})",
-                opening_hours,
-            ):
-                start_day, end_day, start_time, _, end_time, _ = rule
-                start_time = start_time.replace(".", ":")
-                end_time = end_time.replace(".", ":")
-
-                if end_day:
-                    oh.append(f"{start_day}-{end_day} {start_time}-{end_time}")
-                else:
-                    oh.append(f"{start_day} {start_time}-{end_time}")
-
-            item["opening_hours"] = "; ".join(oh)
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_ranges_from_string(store["store_time"].replace(".", ""), days=DAYS_BG)
 
             yield item
