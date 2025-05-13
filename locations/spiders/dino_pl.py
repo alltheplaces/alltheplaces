2adf85498bf76c3b8d2a2baeb5a9079911a28377
David Hicks <david@hicks.id.au>
2025-05-13 18:05:01 +1000

    dino_pl: fix spider

--- locations/spiders/dino_pl.py
+++ locations/spiders/dino_pl.py
@@ -1,13 +1,14 @@
 import json
 import re
-from typing import Any
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from scrapy import Request, Spider
 from scrapy.http import Response
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
+from locations.items import Feature
 
 
 class DinoPLSpider(Spider):
@@ -18,31 +19,26 @@ class DinoPLSpider(Spider):
     start_urls = ["https://marketdino.pl/external/map/index.html"]
     no_refs = True
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
+    def parse(self, response: Response) -> Request:
         # Search for the desired JavaScript file
         yield response.follow(
             url=response.xpath('//script[contains(@src,"_next/static/chunks/pages/index-")]/@src').get(""),
-            callback=self.parse_decryption_params,
+            callback=self.parse_encrypted_geojson,
         )
 
-    def parse_decryption_params(self, response):
+    def parse_encrypted_geojson(self, response: Response) -> Feature:
         if m := re.search(r"""\.from\([\n ]*['"]([0-9a-f]{64})['"],[\n ]*['"]hex['"][\n ]*\)""", response.text):
             key = m.group(1)
         if m := re.search(r"""\.from\([\n ]*['"]([0-9a-f]{32})['"],[\n ]*['"]hex['"][\n ]*\)""", response.text):
             iv = m.group(1)
-        if m := re.search(r"[a-z]\s*=\s*\"([0-9a-f]{40})\",", response.text):
-            access_token = m.group(1)
-        yield Request(
-            url="https://api.marketdino.pl/api/v1/dino_content/geofile/",
-            headers={"Authorization": f"token {access_token}"},
-            meta={"key": key, "iv": iv},
-            callback=self.parse_encrypted_geojson,
-        )
+        if m := re.search(r"""=['"]([0-9a-f]{1024,})['"]""", response.text):
+            encrypted_data = m.group(1)
+        if not key or not iv or not encrypted_data:
+            raise RuntimeError("Could not extract encrypted store data, AES-256-GCM key and IV.")
 
-    def parse_encrypted_geojson(self, response):
-        aesgcm = AESGCM(bytes.fromhex(response.meta["key"]))
+        aesgcm = AESGCM(bytes.fromhex(key))
         geojson = json.loads(
-            aesgcm.decrypt(bytes.fromhex(response.meta["iv"]), bytes.fromhex(response.text), None).decode("utf-8")
+            aesgcm.decrypt(bytes.fromhex(iv), bytes.fromhex(encrypted_data), None).decode("utf-8")
         )
         for location in geojson["features"]:
             item = DictParser.parse(location["properties"])
@@ -52,4 +48,5 @@ class DinoPLSpider(Spider):
                 item["opening_hours"].add_days_range(["Mo", "Tu", "We", "Th", "Fr", "Sa"], *week_hours.split("-", 1))
             if sun_hours := location["properties"]["sundayHours"]:
                 item["opening_hours"].add_range("Su", *sun_hours.split("-", 1))
+            apply_category(Categories.SHOP_SUPERMARKET, item)
             yield item
