5df8816f891b3f6f2f5fb2e3375384023eb12c12
David Hicks <david@hicks.id.au>
2025-05-14 00:01:50 +1000

    la_porchetta: minor tagging improvements, rename spider

--- locations/spiders/la_porchetta_au_nz.py
+++ locations/spiders/la_porchetta_au_nz.py
@@ -2,15 +2,19 @@ from typing import Iterable
 
 from scrapy.http import Response
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class LaPorchettaSpider(WPStoreLocatorSpider):
-    name = "la_porchetta"
+class LaPorchettaAUNZSpider(WPStoreLocatorSpider):
+    name = "la_porchetta_au_nz"
     item_attributes = {"brand": "La Porchetta", "brand_wikidata": "Q6464528"}
-    start_urls = ["https://laporchetta.com.au/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
+    allowed_domains = ["laporchetta.com.au"]
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["branch"] = item.pop("name")
+        item["branch"] = item.pop("name", None)
+        if feature["country"] == "New Zealand":
+            item.pop("state", None)
+        apply_category(Categories.RESTAURANT, item)
         yield item
