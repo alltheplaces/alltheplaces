4c802a11e1bdcaee4c834e87fc2394bd7f7f81fc
Karen Kerr <karenkerrgithub@gmail.com>
2025-05-14 10:41:28 +0100

    [BeautySuccess] Add spider

---
+++ locations/spiders/beauty_success.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class BeautySuccessSpider(JSONBlobSpider):
+    name = "beauty_success"
+    item_attributes = {"brand": "Beauty Success", "brand_wikidata": "Q60964499"}
+    start_urls = ["https://www.beautysuccess.fr/storelocator/stockist/ajax"]
+
+    def extract_json(self, response: Response) -> dict | list:
+        return response.json()["store"]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = ", ".join(filter(None, [feature.get("address"), feature.get("address2")]))
+        item["ref"] = feature["store_code"]
+        yield item
