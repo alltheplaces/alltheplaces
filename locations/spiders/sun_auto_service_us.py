d692a4521a65aa744e07cf4bfb45c0aa7c8895c3
David Hicks <david@hicks.id.au>
2025-05-17 15:40:30 +1000

    sun_auto_service_us: fix spider

--- locations/spiders/sun_auto_service_us.py
+++ locations/spiders/sun_auto_service_us.py
@@ -1,10 +1,17 @@
-from scrapy.spiders import SitemapSpider
+from typing import Iterable
 
-from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.rio_seo import RioSeoSpider
 
 
-class SunAutoServiceUSSpider(SitemapSpider, StructuredDataSpider):
+class SunAutoServiceUSSpider(RioSeoSpider):
     name = "sun_auto_service_us"
     item_attributes = {"brand": "Sun Auto Service", "brand_wikidata": "Q118383798"}
-    sitemap_urls = ["https://www.sunautoservice.com/store-sitemap.xml"]
-    wanted_types = ["AutoRepair"]
+    end_point = "https://maps.locations.sun.auto.prod.rioseo.com"
+    template = "search"
+
+    def post_process_feature(self, feature: Feature, location: dict) -> Iterable[Feature]:
+        feature["branch"] = location["location_display_name"].removeprefix("{} ".format(location["location_name"]))
+        feature["website"] = feature["website"].replace("https://locations.sun.auto.prod.rioseo.com/", "https://locations.sun.auto.prod.rioseo.com/sunauto/")
+        yield feature
