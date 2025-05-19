6781f7ddd470d0009c04cf4d2b81b214a54ad3b7
Cj Malone <CjMalone@mail.com>
2025-05-19 12:42:32 +0100

    More selective sitemap_rules

--- locations/spiders/blank_street_coffee.py
+++ locations/spiders/blank_street_coffee.py
@@ -7,17 +7,10 @@ class BlankStreetCoffeeSpider(SitemapSpider, StructuredDataSpider):
     name = "blank_street_coffee"
     item_attributes = {"brand": "Blank Street Coffee", "brand_wikidata": "Q114792509"}
     sitemap_urls = ["https://www.blankstreet.com/sitemap.xml"]
-    sitemap_rules = [(r"com/locations/", "parse_sd")]
+    sitemap_rules = [(r"com/locations/[^/]+/([^-]+)[^/]+$", "parse_sd")]
     wanted_types = ["CafeOrCoffeeShop"]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        # Extract reference ID from URL
-        # URL format: /locations/city/ID-location-name
-        url_parts = response.url.split("/")
-        if len(url_parts) > 5:
-            ref = url_parts[5].split("-")[0]
-            item["ref"] = ref
-
         # Extract branch from the name if it has address info
         if item.get("name") and " at " in item["name"]:
             parts = item["name"].split(" at ")
