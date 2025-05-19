6ce51db8142ce99cb9ec88681e62669e21edcf58
Cj Malone <CjMalone@mail.com>
2025-05-19 13:14:45 +0100

    Clean up request

--- locations/spiders/tom_wahls_us.py
+++ locations/spiders/tom_wahls_us.py
@@ -1,30 +1,22 @@
+from typing import Any
+
 import scrapy
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.linked_data_parser import LinkedDataParser
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class TomWahlsUSSpider(scrapy.Spider):
     name = "tom_wahls_us"
     item_attributes = {"brand": "Tom Wahl's", "brand_wikidata": "Q7817965"}
     allowed_domains = ["tomwahls.com"]
+    start_urls = ["https://www.tomwahls.com/"]
+    user_agent = BROWSER_DEFAULT
 
-    # Use custom headers to mimic a real browser (learned from Bill Gray's)
-    custom_settings = {
-        "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "DEFAULT_REQUEST_HEADERS": {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1",
-        },
-    }
-
-    start_urls = ["https://www.tomwahls.com"]
-
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         # Tom Wahl's has all locations in JSON-LD structured data on the homepage
         for ld_obj in LinkedDataParser.iter_linked_data(response):
             # The locations are nested in the "department" array of the main object
