f217a003f0d04b300c68aed1a3a54441344da8c8
Cj Malone <CjMalone@mail.com>
2025-05-19 15:58:57 +0100

    Clean name, collect ref

--- locations/spiders/decathlon_ma.py
+++ locations/spiders/decathlon_ma.py
@@ -1,6 +1,9 @@
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.spiders.decathlon_fr import DecathlonFRSpider
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -10,11 +13,14 @@ class DecathlonMASpider(CrawlSpider, StructuredDataSpider):
     item_attributes = DecathlonFRSpider.item_attributes
     allowed_domains = ["www.decathlon.ma"]
     start_urls = ["https://www.decathlon.ma/content/154-filialen-decathlon"]
-    rules = [
-        Rule(LinkExtractor(allow=r"/content/\d+-store-"), "parse_sd"),
-    ]
+    rules = [Rule(LinkExtractor(allow=r"/content/(\d+)-store-"), "parse_sd")]
 
     def pre_process_data(self, ld_data: dict, **kwargs):
         for rule in ld_data.get("openingHoursSpecification", []):
             for key in ["opens", "closes"]:
                 rule[key] = rule.get(key, "").replace("H", ":")
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("DECATHLON ")
+        apply_category(Categories.SHOP_SPORTS, item)
+        yield item
