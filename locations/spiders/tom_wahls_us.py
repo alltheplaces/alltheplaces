d50100513a35556b48a690490b80ea20b3e0693a
Cj Malone <CjMalone@mail.com>
2025-05-19 13:14:57 +0100

    Use LinkedDataParser.parse_ld

--- locations/spiders/tom_wahls_us.py
+++ locations/spiders/tom_wahls_us.py
@@ -4,7 +4,6 @@ import scrapy
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.items import Feature
 from locations.linked_data_parser import LinkedDataParser
 from locations.user_agents import BROWSER_DEFAULT
 
@@ -31,46 +30,14 @@ class TomWahlsUSSpider(scrapy.Spider):
             elif ld_obj.get("@type") == "FastFoodRestaurant":
                 yield self.parse_location(ld_obj)
 
-    def parse_location(self, location):
-        item = Feature()
-
-        # Extract basic info from JSON-LD
-        item["name"] = location.get("name")
-        item["website"] = "https://www.tomwahls.com"
-
-        # Extract address info
-        address = location.get("address", {})
-        item["street_address"] = address.get("streetAddress")
-        item["city"] = address.get("addressLocality")
-        item["state"] = address.get("addressRegion")
-        item["postcode"] = address.get("postalCode")
-        item["country"] = address.get("addressCountry", "US")
-
-        # Extract contact info
-        phone = location.get("telephone")
-        if phone:
-            # Clean phone number format
-            item["phone"] = phone.replace("+1", "")
-
-        # Extract coordinates safely
-        location_data = location.get("location", {})
-        geo = location_data.get("geo", {})
-        if geo.get("latitude") and geo.get("longitude"):
-            item["lat"] = float(geo.get("latitude"))
-            item["lon"] = float(geo.get("longitude"))
-
-        # Extract branch name from the full name
-        # e.g., "Tom Wahl's Avon" -> branch = "Avon"
-        if item["name"] and item["name"].startswith("Tom Wahl's "):
-            item["branch"] = item["name"].replace("Tom Wahl's ", "")
+    def parse_location(self, location: dict):
+        item = LinkedDataParser.parse_ld(location)
+        item["branch"] = item.pop("name").removeprefix("Tom Wahl's ")
 
         # Set ref based on branch name or city
         item["ref"] = item.get("branch") or item.get("city")
 
-        # Apply category for fast food restaurants
         apply_category(Categories.FAST_FOOD, item)
-
-        # Add burger cuisine
-        item["extras"] = {"cuisine": "burger"}
+        item["extras"]["cuisine"] = "burger"
 
         return item
