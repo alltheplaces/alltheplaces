10e6fff7dd0854ec6f8f5c2039e56e513abfc5bc
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-05-19 13:01:13 +0530

    Fix Rema 1000 DK Spider

--- locations/spiders/rema_1000_dk.py
+++ locations/spiders/rema_1000_dk.py
@@ -1,38 +1,23 @@
+from typing import Any
+
 from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_NO, OpeningHours
 
 
 class Rema1000DKSpider(Spider):
     name = "rema_1000_dk"
     item_attributes = {"brand": "Rema 1000", "brand_wikidata": "Q28459"}
-    allowed_domains = ["www.rema1000.dk"]
-    start_urls = ["https://www.rema1000.dk/api/v2/stores"]
+    start_urls = ["https://cphapp.rema1000.dk/api/v3/stores?per_page=1000"]
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield JsonRequest(url)
-
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["data"]:
-            if location.get("permanently_closed") or location.get("temporarily_closed_until"):
-                continue
-
             item = DictParser.parse(location)
-            item["website"] = location.get("store_link")
-
-            hours_string = ""
-            for day_hours in location.get("opening_hours", {}).values():
-                hours_string = "{} {}: {} - {}".format(
-                    hours_string, day_hours.get("day"), day_hours.get("open"), day_hours.get("close")
-                )
-            item["opening_hours"] = OpeningHours()
-            item["opening_hours"].add_ranges_from_string(hours_string, days=DAYS_NO)
-            item["postcode"] = str(item["postcode"])
-
+            item["ref"] = location.get("internal_id")
+            item["branch"] = item.pop("name")
+            item["street_address"] = item.pop("addr_full", None)
+            item["website"] = f'https://rema1000.dk/find-butik-og-abningstider/{item["ref"]}'
             apply_category(Categories.SHOP_SUPERMARKET, item)
-
             yield item
