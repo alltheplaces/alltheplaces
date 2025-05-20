a5f0a81bd04e88dd731e5ea7093da3f5bab6540a
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-05-20 11:16:57 +0530

    Refactor using Structured Data to fix Decathlon SK Spider

--- locations/spiders/decathlon_sk.py
+++ locations/spiders/decathlon_sk.py
@@ -1,40 +1,21 @@
-from chompjs import parse_js_object
-from scrapy import Spider
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-from locations.dict_parser import DictParser
-from locations.hours import DAYS, OpeningHours
-from locations.pipelines.address_clean_up import clean_address
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.spiders.decathlon_fr import DecathlonFRSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class DecathlonSKSpider(Spider):
+class DecathlonSKSpider(CrawlSpider, StructuredDataSpider):
     name = "decathlon_sk"
     item_attributes = DecathlonFRSpider.item_attributes
+    allowed_domains = ["www.decathlon.sk"]
     start_urls = ["https://www.decathlon.sk/content/24-filialen-decathlon"]
+    rules = [Rule(LinkExtractor(allow=r"/content/(\d+)-store-"), "parse_sd")]
 
-    def parse(self, response):
-        js_blob = response.xpath('//script[contains(text(), ",stores=[{")]/text()').get()
-
-        js_blob_markers = (
-            js_blob.split(",store_marker=", 1)[1].split(",store_name=", 1)[0].replace('\\"', "'").replace("\\/", "/")
-        )
-        markers = parse_js_object(js_blob_markers, unicode_escape=True)
-        markers = {marker["store_number"]: marker for marker in markers}
-
-        js_blob_stores = js_blob.split(",stores=", 1)[1].split(",zoom=", 1)[0].replace('\\"', "'").replace("\\/", "/")
-        locations = parse_js_object(js_blob_stores, unicode_escape=True)
-
-        for location in locations:
-            location["hours"] = parse_js_object(location["hours"])
-            location["website"] = markers[location["store_number"]]["link"]
-            item = DictParser.parse(location)
-
-            item["ref"] = str(location["store_number"])
-            item["street_address"] = clean_address([location["address1"], location["address2"]])
-
-            item["opening_hours"] = OpeningHours()
-            for day_number, day_hours in enumerate(location["hours"]):
-                for time_range in day_hours:
-                    item["opening_hours"].add_range(DAYS[day_number], *time_range.split(" - "), "%H:%M")
-
-            yield item
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Decathlon ")
+        apply_category(Categories.SHOP_SPORTS, item)
+        yield item
