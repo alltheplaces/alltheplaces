161a00783d5c77ad2ebba90cce7720f343175383
Cj Malone <CjMalone@mail.com>
2025-05-20 12:49:09 +0100

    Simplfy opening hours

--- locations/spiders/esselunga_it.py
+++ locations/spiders/esselunga_it.py
@@ -3,7 +3,7 @@ from typing import Iterable
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS_IT, OpeningHours, sanitise_day
+from locations.hours import DAYS, OpeningHours
 from locations.items import Feature, set_closed
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -25,14 +25,12 @@ class EsselungaITSpider(JSONBlobSpider):
             item["branch"] = item["branch"].removeprefix("Esselunga di ")
 
         item["opening_hours"] = OpeningHours()
-        for day_time in feature.get("openingHours"):
-            day = sanitise_day(day_time.get("dayName"), DAYS_IT)
-            open_time = day_time.get("opening")
-            close_time = day_time.get("closing")
-            item["opening_hours"].add_range(day=day, open_time=open_time, close_time=close_time)
+        for rule in feature.get("openingHours"):
+            item["opening_hours"].add_range(DAYS[rule["weekDay"]], rule["opening"], rule["closing"])
 
         if feature["closed"] is True:
             set_closed(item)
+
         apply_category(Categories.SHOP_SUPERMARKET, item)
 
         yield item
