37f6e3cbd0c21bdd338bdebd7f08ee51643798ef
ehsan-firdaus <ehsanfirdaus@gmail.com>
2025-05-20 11:18:11 +0530

    Try with playwright to fix spider blockage

--- locations/spiders/decathlon_sk.py
+++ locations/spiders/decathlon_sk.py
@@ -4,8 +4,10 @@ from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.spiders.decathlon_fr import DecathlonFRSpider
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class DecathlonSKSpider(CrawlSpider, StructuredDataSpider):
@@ -14,6 +16,8 @@ class DecathlonSKSpider(CrawlSpider, StructuredDataSpider):
     allowed_domains = ["www.decathlon.sk"]
     start_urls = ["https://www.decathlon.sk/content/24-filialen-decathlon"]
     rules = [Rule(LinkExtractor(allow=r"/content/(\d+)-store-"), "parse_sd")]
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["branch"] = item.pop("name").removeprefix("Decathlon ")
