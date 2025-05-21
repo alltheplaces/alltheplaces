41013c05d0b832f20e3b84b19136041d62627db7
Cj Malone <CjMalone@mail.com>
2025-05-21 13:44:42 +0100

    Duplicate requests

--- locations/spiders/test_dups.py
+++ locations/spiders/test_dups.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from scrapy import Spider
+from scrapy import Request, Spider
 from scrapy.http import Response
 
 from locations.items import Feature
@@ -14,3 +14,8 @@ class TestDupsSpider(Spider):
         yield Feature(ref="a")
         for _ in range(100):
             yield Feature(ref="b")
+        for _ in range(10):
+            yield Request(url="data:,1", callback=self.parse_nop)
+
+    def parse_nop(self, response: Response, **kwargs: Any) -> Any:
+        pass
