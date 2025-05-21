59e3de48eb2480e08f6884e58ae107a2ad7b476b
David Hicks <david@hicks.id.au>
2025-05-21 23:45:30 +1000

    Add Los Angeles County Department of Public Works street lamps (123k)

---
+++ locations/spiders/infrastructure/los_angeles_county_department_of_public_works_street_lamps_us.py
@@ -0,0 +1,42 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.arcgis_feature_server import ArcGISFeatureServerSpider
+
+
+class LosAngelesCountyDepartmentOfPublicWorksStreetLampsUSSpider(ArcGISFeatureServerSpider):
+    name = "los_angeles_county_department_of_public_works_street_lamps_us"
+    item_attributes = {
+        "operator": "Los Angeles County Department of Public Works",
+        "operator_wikidata": "Q6682081",
+        "state": "CA",
+        "nsi_id": "N/A",
+    }
+    host = "dpw.gis.lacounty.gov"
+    context_path = "dpw"
+    service_id = "PW_Open_Data"
+    server_type = "MapServer"
+    layer_id = "5"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature["OBJECTID"])
+        apply_category(Categories.STREET_LAMP, item)
+        if sce_id := feature.get("SCE_POL_I"):
+            item["extras"]["alt_ref"] = feature["SCE_POL_I"]
+        match feature["POLE_TYPE"]:
+            case "C":
+                item["extras"]["material"] = "concrete"
+            case "F":
+                item["extras"]["material"] = "fiberglass"
+            case "S":
+                item["extras"]["material"] = "steel"
+            case "W":
+                item["extras"]["material"] = "wood"
+            case None:
+                pass
+            case _:
+                self.logger.warning("Unknown material type: {}".format(feature["POLE_TYPE"]))
+        yield item
