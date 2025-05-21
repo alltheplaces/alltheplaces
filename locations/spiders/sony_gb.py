c0825ad1d37a50c7564d6e303bc925643773f848
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-21 17:08:43 +0530

    Set browser default as user agent

--- locations/spiders/sony_gb.py
+++ locations/spiders/sony_gb.py
@@ -1,6 +1,7 @@
 from scrapy import Spider
 
 from locations.dict_parser import DictParser
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class SonyGBSpider(Spider):
@@ -9,6 +10,7 @@ class SonyGBSpider(Spider):
     start_urls = [
         "https://locator.sony/api/v1/dealers?flatten=true&minLat=47.41321842113969&maxLat=60.18523142888265&minLng=-15.303958203124997&maxLng=10.557858203124981&order=st_4k_tv%2Cst_accessories%2Cst_alpha_store%2Cst_hi_res&alternativeLanguages=en&orgLevel2=consumer&orgLevel3=smk&orgLevel4=eu&locale=en_GB"
     ]
+    user_agent = BROWSER_DEFAULT
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def parse(self, response, **kwargs):
