fe7a7af006fa2f85a613477f95c2c328c5ad5479
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-21 11:31:17 +0530

    Add coordinates

--- locations/spiders/einstein_bros_us.py
+++ locations/spiders/einstein_bros_us.py
@@ -1,5 +1,9 @@
+import re
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -9,3 +13,9 @@ class EinsteinBrosUSSpider(SitemapSpider, StructuredDataSpider):
     allowed_domains = ["einsteinbros.com"]
     sitemap_urls = ["https://locations.einsteinbros.com/sitemap.xml"]
     sitemap_rules = [(r"/us/[a-z]{2}/[-\w]+/[-\w]+$", "parse_sd")]
+    coordinates_pattern = re.compile(r"%7B%22latitude%22%3A([-.\d]+)%2C%22longitude%22%3A([-.\d]+)%7D")
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        if coordinates := re.search(self.coordinates_pattern, response.text):
+            item["lat"], item["lon"] = coordinates.groups()
+        yield item
