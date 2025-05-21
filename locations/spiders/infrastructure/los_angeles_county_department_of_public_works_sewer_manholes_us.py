8a59d058edbbc49ca109539d3c0d973f83830c13
David Hicks <david@hicks.id.au>
2025-05-22 00:49:44 +1000

    Add Los Angeles County Department of Public Works sewer manholes (108k)

---
+++ locations/spiders/infrastructure/los_angeles_county_department_of_public_works_sewer_manholes_us.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.arcgis_feature_server import ArcGISFeatureServerSpider
+
+
+class LosAngelesCountyDepartmentOfPublicWorksSewerPumpStationsUSSpider(ArcGISFeatureServerSpider):
+    name = "los_angeles_county_department_of_public_works_sewer_pump_stations_us"
+    item_attributes = {
+        "operator": "Los Angeles County Department of Public Works",
+        "operator_wikidata": "Q6682081",
+        "state": "CA",
+        "nsi_id": "N/A",
+    }
+    host = "dpw.gis.lacounty.gov"
+    context_path = "dpw"
+    service_id = "Sanitary_Sewer_Network"
+    layer_id = "0"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = feature["CW_ASSETID"]
+        apply_category(Categories.MANHOLE, item)
+        item["extras"]["manhole"] = "sewer"
+        item["extras"]["alt_ref"] = str(feature["MANHOLE_NUMBER"])
+        yield item
