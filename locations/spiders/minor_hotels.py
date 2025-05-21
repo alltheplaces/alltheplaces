58372f45c6af6d282516eec97855fe355764e39c
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-21 15:48:46 +0530

    Apply branding

--- locations/spiders/minor_hotels.py
+++ locations/spiders/minor_hotels.py
@@ -1,3 +1,4 @@
+import chompjs
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
@@ -11,7 +12,22 @@ class MinorHotelsSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"operator": "Minor Hotels", "operator_wikidata": "Q25108728"}
     sitemap_urls = ["https://www.minorhotels.com/en/sitemap_minor_en.xml"]
     sitemap_rules = [(r"/en/destinations/[-\w]+/[-\w]+/[-\w]+$", "parse_sd")]
+    BRANDS = {
+        "AN": ("Anantara", "Q112671781"),
+        "AV": ("Avani Hotels & Resorts", "Q48989823"),
+        "EW": ("Elewana", None),
+        "NC": ("NH Collection Hotels", "Q1604631"),
+        "NH": ("NH Hotels", "Q1604631"),
+        "NW": ("nhow", "Q1604631"),
+        "OH": ("Oaks Hotels, Resorts & Suites", "Q23073772"),
+        "TI": ("Tivoli Hotels & Resorts", "Q10382766"),
+    }
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        location_info = chompjs.parse_js_object(response.xpath('//script[contains(text(),"brandcode")]/text()').get())
+        item["ref"] = location_info.get("hotelcode")
+        item["brand"], item["brand_wikidata"] = self.BRANDS.get(
+            location_info.get("brandcode"), (location_info.get("brandname"), None)
+        )
         apply_category(Categories.HOTEL, item)
         yield item
