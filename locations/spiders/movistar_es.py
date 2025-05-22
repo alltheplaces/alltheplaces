be2c549c4b30d939ae1249a40f7a1d56d56bb687
Cj Malone <CjMalone@mail.com>
2025-05-22 14:50:28 +0100

    Clean up opening hours parsing

--- locations/spiders/movistar_es.py
+++ locations/spiders/movistar_es.py
@@ -3,7 +3,7 @@ from typing import Iterable
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS_EN, OpeningHours
+from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -17,9 +17,9 @@ class MovistarESSpider(JSONBlobSpider):
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name").replace("Tienda Movistar ", "")
         oh = OpeningHours()
-        for key, value in feature["hours"].items():
-            for time in value:
-                oh.add_range(DAYS_EN[key.title()], time["from"], time["to"])
+        for day, times in feature["hours"].items():
+            for time in times:
+                oh.add_range(day, time["from"], time["to"])
         item["opening_hours"] = oh
         item["website"] = feature["pages"]["store-page"]
         apply_category(Categories.SHOP_MOBILE_PHONE, item)
