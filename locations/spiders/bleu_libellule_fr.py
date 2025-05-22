9dcd4af04dee0a9e2c387e98f65595b5a4aeece0
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-22 11:59:43 +0530

    [BleuLibelluleFRSpider] Fix sitemap rule

--- locations/spiders/bleu_libellule_fr.py
+++ locations/spiders/bleu_libellule_fr.py
@@ -7,5 +7,5 @@ class BleuLibelluleFRSpider(SitemapSpider, StructuredDataSpider):
     name = "bleu_libellule_fr"
     item_attributes = {"brand": "Bleu Libellule", "brand_wikidata": "Q101830610"}
     sitemap_urls = ["https://magasins.bleulibellule.com/sitemap.xml"]
-    sitemap_rules = [(r"fr/shop/(.*)$", "parse_sd")]
+    sitemap_rules = [(r"magasins\.bleulibellule\.com/[-\w]+/[-\w]+/[-\w]+/[-\w]+", "parse_sd")]
     wanted_types = ["LocalBusiness"]
