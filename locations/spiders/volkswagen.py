61b9309e28f1e9d1c91e35d74f08510bd3996918
BelousDmitry <belous0219111999@gmail.com>
2025-05-22 13:56:52 -0400

    adding Volkswagen Commercial Vehicles

--- locations/spiders/volkswagen.py
+++ locations/spiders/volkswagen.py
@@ -1,34 +1,30 @@
 from typing import Iterable
 
-import pycountry
 from babel import Locale, UnknownLocaleError
 from scrapy.http import JsonRequest, Request
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.hours import DAYS, OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
+from geonamescache import GeonamesCache
 
-FEATURES_MAPPING = {
-    "VEHICLESERVICES": Extras.CAR_REPAIR,
-}
 
-SERVICES_MAPPING = {
-    "MOT": "service:vehicle:mot",
-}
+VOLKSWAGEN_SHARED_ATTRIBUTES = {"brand": "Volkswagen", "brand_wikidata": "Q246"}
+VOLKSWAGEN_COMMERCIAL_VEHICLES_SHARED_ATTRIBUTES = {"brand": "Volkswagen Commercial Vehicles", "brand_wikidata": "Q699709"}
 
 
 class VolkswagenSpider(JSONBlobSpider):
     download_timeout = 30
     name = "volkswagen"
-    item_attributes = {"brand": "Volkswagen", "brand_wikidata": "Q246"}
     locations_key = "dealers"
     start_urls = ["https://www.vw.com/en.global-config.json"]
 
     countries = {}
-    for country in pycountry.countries:
+
+    for country in GeonamesCache().get_countries().keys():
         try:
-            lang = str(Locale.parse("und_" + country.alpha_2))
-            countries[country.alpha_2] = lang.replace("_", "-")
+            lang = str(Locale.parse("und_" + country))
+            countries[country] = lang.replace("_", "-")
         except UnknownLocaleError:
             pass
 
@@ -48,33 +44,27 @@ class VolkswagenSpider(JSONBlobSpider):
         base_url = config["baseUrl"]
         api_key = config["featureAppApiKey"]
 
-        for country, language in self.countries.items():
-            url = (
-                base_url
-                + '/bff-search/dealers?serviceConfigEndpoint={"endpoint":{"type":"publish","country":"'
-                + country.lower()
-                + '","language":"'
-                + language.split("-")[0]
-                + '","content":"onehub_pkw","envName":"prod","testScenarioId":null},"signature":"'
-                + signature
-                + '"}&lufthansaApiKey='
-                + api_key
-                + '&query={"type":"DEALER","language":"'
-                + language
-                + '","countryCode":"'
-                + country
-                + '","dealerServiceFilter":[],"contentDealerServiceFilter":[],"usePrimaryTenant":true,"name":" "}'
-            )
-            yield JsonRequest(
-                url=url,
-                callback=self.check_status,
-                meta={
-                    "handle_httpstatus_list": [500],
-                    "dont_retry": True,  # If we try an unknown country we get a 500 error and it's pointless retrying
-                    "country": country,
-                    "language": language,
-                },
-            )
+        for content in ["onehub_pkw", "onehub_nfz"]:
+            for country, language in self.countries.items():
+                url = (
+                    f'{base_url}/bff-search/dealers?serviceConfigEndpoint={{"endpoint":{{"type":"publish",'
+                    f'"country":"{country.lower()}","language":"{language.split("-")[0]}","content":"{content}",'
+                    f'"envName":"prod","testScenarioId":null}},"signature":"{signature}"}}'
+                    f'&lufthansaApiKey={api_key}&query={{"type":"DEALER","language":"{language}",'
+                    f'"countryCode":"{country}","dealerServiceFilter":[],"contentDealerServiceFilter":[],"usePrimaryTenant":true,"name":" "}}'                                    
+                )
+
+                yield JsonRequest(
+                    url=url,
+                    callback=self.check_status,
+                    meta={
+                        "handle_httpstatus_list": [500],
+                        "dont_retry": True,  # If we try an unknown country we get a 500 error and it's pointless retrying
+                        "country": country,
+                        "language": language,
+                    },
+                )
+
 
     def check_status(self, response):
         if response.status == 500:
@@ -85,30 +75,49 @@ class VolkswagenSpider(JSONBlobSpider):
         else:
             yield from self.parse(response)
 
+
+    def pre_process_data(self, feature: dict):
+        feature["id"] = str(feature["coordinates"][0]) + str(feature["coordinates"][1]) + feature["brand"]
+
+        if feature.get("contact") and feature["contact"].get("website"):
+            if not feature["contact"]["website"].startswith("http"):
+                feature["contact"]["website"] = "".join(["https://", feature["contact"]["website"]])
+
+
     def post_process_item(self, item, response, location):
         item["lat"], item["lon"] = location["coordinates"]
         if item["country"] is None:
             item["country"] = response.meta["country"]
-        if location["brand"] != "V":
+        
+        if location["brand"] == "V":
+            item["brand"] = VOLKSWAGEN_SHARED_ATTRIBUTES["brand"]
+            item["brand_wikidata"] = VOLKSWAGEN_SHARED_ATTRIBUTES["brand_wikidata"]
+        elif location["brand"] == "N" or location["brand"] == "n":
+            item["brand"] = VOLKSWAGEN_COMMERCIAL_VEHICLES_SHARED_ATTRIBUTES["brand"]
+            item["brand_wikidata"] = VOLKSWAGEN_COMMERCIAL_VEHICLES_SHARED_ATTRIBUTES["brand_wikidata"]
+        else:
             self.crawler.stats.inc_value(f"atp/{self.name}/unknown_brand/{location['brand']}")
 
         # Some provided states are actually countries, where one country's search gives results for other nearby countries
         if item["state"] in ["Botswana", "Namibia"]:
             item["country"] = item.pop("state")
 
-        if "features" in location:
-            for feature in location["features"]:
-                if match := FEATURES_MAPPING.get(feature):
-                    apply_yes_no(match, item, True)
-                else:
-                    self.crawler.stats.inc_value(f"atp/{self.name}/unknown_feature/{feature}")
+
         if "rawServices" in location:
-            apply_yes_no("service:vehicle:mot", item, "MOT" in location["rawServices"])
-            for service in location["rawServices"]:
-                if match := SERVICES_MAPPING.get(service):
-                    apply_yes_no(match, item, True)
-                else:
+            if "DEALER_REVIEW" or "NEW_PC" in location["rawServices"]:
+                apply_category(Categories.SHOP_CAR, item)
+                apply_yes_no(Extras.CAR_REPAIR, item, "SERVICE" in location["rawServices"])
+                apply_yes_no(Extras.USED_CAR_SALES, item, "USED_PC" in location["rawServices"])
+            elif "SERVICE" in location["rawServices"]:
+                apply_category(Categories.SHOP_CAR_REPAIR, item)
+            elif "USED_PC" in location["rawServices"]:
+                apply_category(Categories.SHOP_CAR, item)
+                apply_yes_no(Extras.USED_CAR_SALES, item, True)
+            else:
+                for service in location["rawServices"]:
                     self.crawler.stats.inc_value(f"atp/{self.name}/unknown_service/{service}")
+                # self.crawler.stats.inc_value(f"atp/{self.name}/unknown_service/{str(location['coordinates']) + str(location['name'])}")
+
 
         if "businessHours" in location and len(location["businessHours"]) > 0:
             item["opening_hours"] = OpeningHours()
@@ -121,5 +130,4 @@ class VolkswagenSpider(JSONBlobSpider):
                     except ValueError:
                         pass
 
-        apply_category(Categories.SHOP_CAR, item)
         yield item
