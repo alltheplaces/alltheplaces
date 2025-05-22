fefc26835c690f230c90e941cba3086a8fdeb4ff
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-22 12:04:14 +0530

    [BleuLibelluleFRSpider] Add branch

--- locations/spiders/bleu_libellule_fr.py
+++ locations/spiders/bleu_libellule_fr.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -8,3 +10,7 @@ class BleuLibelluleFRSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Bleu Libellule", "brand_wikidata": "Q101830610"}
     sitemap_urls = ["https://magasins.bleulibellule.com/sitemap.xml"]
     sitemap_rules = [(r"magasins\.bleulibellule\.com/[-\w]+/[-\w]+/[-\w]+/[-\w]+", "parse_sd")]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Bleu Libellule ")
+        yield item
