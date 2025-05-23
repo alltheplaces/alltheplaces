29e0bb812c8ee4cbd4f856ec89dd300f46c503c4
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-23 12:12:48 +0530

    Refactor using StockInStore Spider to fix Kidstuff AU Spider

--- locations/spiders/kidstuff_au.py
+++ locations/spiders/kidstuff_au.py
@@ -1,8 +1,15 @@
-from locations.categories import Categories
-from locations.storefinders.stockist import StockistSpider
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.stockinstore import StockInStoreSpider
 
 
-class KidstuffAUSpider(StockistSpider):
+class KidstuffAUSpider(StockInStoreSpider):
     name = "kidstuff_au"
-    item_attributes = {"brand": "Kidstuff", "brand_wikidata": "Q117746407", "extras": Categories.SHOP_TOYS.value}
-    key = "u8053"
+    item_attributes = {"brand": "Kidstuff", "brand_wikidata": "Q117746407"}
+    api_site_id = "10041"
+    api_widget_id = "48"
+    api_origin = "https://www.kidstuff.com.au"
+
+    def parse_item(self, item: Feature, location: dict):
+        apply_category(Categories.SHOP_TOYS, item)
+        yield item
