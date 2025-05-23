0624b6db2dd1a763dfd6c8bdd508f703351a51b0
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-05-23 05:49:29 -0400

    [Supercut DE] Fix spider (#13231)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/cosmo_de.py
+++ locations/spiders/cosmo_de.py
@@ -3,5 +3,5 @@ from locations.storefinders.klier_hair_group import KlierHairGroupSpider
 
 class CosmoDESpider(KlierHairGroupSpider):
     name = "cosmo_de"
-    start_urls = ["https://www.cosmohairshop.de/shops/"]
+    start_urls = ["https://cosmohairshop.de/shops/"]
     item_attributes = {"brand": "Cosmo", "brand_wikidata": "Q121888284"}
