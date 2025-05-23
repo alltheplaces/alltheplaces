25cb3ecc0c8a25025f172c58e0d08455b7b01246
Cj Malone <CjMalone@mail.com>
2025-05-23 10:55:18 +0100

    Use more stable sitemap_urls

--- locations/spiders/noahs_ny_bagels.py
+++ locations/spiders/noahs_ny_bagels.py
@@ -8,11 +8,9 @@ from locations.structured_data_spider import StructuredDataSpider
 
 
 class NoahsNyBagelsSpider(SitemapSpider, StructuredDataSpider):
-    """Copy of Einstein Bros. Bagels - all brands of the same parent company Coffee & Bagels"""
-
     name = "noahs_ny_bagels"
     item_attributes = {"brand": "Noah's Bagels", "brand_wikidata": "Q64517373"}
-    sitemap_urls = ["https://locations.noahs.com/sitemap1.xml"]
+    sitemap_urls = ["https://locations.noahs.com/robots.txt"]
     sitemap_rules = [(r"/us/[^/]+/[^/]+/[0-9a-z-]+$", "parse_sd")]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
