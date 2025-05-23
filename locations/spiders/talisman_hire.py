8edba392b3efa3f9686a3e0287a83139596e99b4
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-23 16:59:29 +0530

    Fix country

--- locations/spiders/talisman_hire.py
+++ locations/spiders/talisman_hire.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -9,3 +11,13 @@ class TalismanHireSpider(SitemapSpider, StructuredDataSpider):
     allowed_domains = ["www.talisman.co.za"]
     sitemap_urls = ["https://www.talisman.co.za/googlesitemap"]
     sitemap_rules = [(r"talisman\.co\.za/[-\w]+/[-\w]+/talisman-hire-[-\w]+$", "parse_sd")]
+    skip_auto_cc_spider_name = True
+    skip_auto_cc_domain = True
+
+    def pre_process_data(self, ld_data: dict, **kwargs):
+        ld_data.get("address", {}).pop("addressCountry", None)  # It's always ZA, which isn't true
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        if item.get("state") == "Mpumalanga":  # country wrongly reverse geocoded as SZ
+            item["country"] = "ZA"
+        yield item
