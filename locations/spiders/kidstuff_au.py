b4b58c56df8c7df7b9966ed47e83e85b8c26ea2c
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-23 12:20:12 +0530

    Fix website

--- locations/spiders/kidstuff_au.py
+++ locations/spiders/kidstuff_au.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.stockinstore import StockInStoreSpider
@@ -11,5 +13,6 @@ class KidstuffAUSpider(StockInStoreSpider):
     api_origin = "https://www.kidstuff.com.au"
 
     def parse_item(self, item: Feature, location: dict):
+        item["website"] = urljoin(self.api_origin, item["website"])
         apply_category(Categories.SHOP_TOYS, item)
         yield item
