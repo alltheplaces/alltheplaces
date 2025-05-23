c9d20c9b842b7b49a41f8aa52786de7b28a58b9a
Cj Malone <CjMalone@mail.com>
2025-05-23 09:33:16 +0100

    Try with proxy

--- locations/spiders/sprinter.py
+++ locations/spiders/sprinter.py
@@ -5,27 +5,24 @@ from scrapy.http import JsonRequest, Response
 
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
-from locations.user_agents import FIREFOX_LATEST
 
 
 class SprinterSpider(JSONBlobSpider):
     name = "sprinter"
     SPRINTER = {"brand": "Sprinter", "brand_wikidata": "Q6133465"}
     SPORT_ZONE = {"brand": "Sport Zone", "brand_wikidata": "Q18485899"}
-    user_agent = FIREFOX_LATEST
+    requires_proxy = "ES"
     locations_key = "stores"
 
     def start_requests(self) -> Iterable[JsonRequest | Request]:
         yield JsonRequest(
             url="https://www.sprintersports.com/api/store/by_points",
-            method="POST",
-            body=b'{"p1x":-180,"p2y":90,"p2x":180,"p1y":-90,"clat":1,"clon":1}',
+            data={"p1x": -180, "p2y": 90, "p2x": 180, "p1y": -90, "clat": 1, "clon": 1},
             headers={"Referer": "https://www.sprintersports.com/tiendas"},
         )
         yield JsonRequest(
             url="https://www.sprintersports.com/pt/api/store/by_points",
-            method="POST",
-            body=b'{"p1x":-180,"p2y":90,"p2x":180,"p1y":-90,"clat":1,"clon":1}',
+            data={"p1x": -180, "p2y": 90, "p2x": 180, "p1y": -90, "clat": 1, "clon": 1},
             headers={"Referer": "https://www.sprintersports.com/pt/lojas"},
         )
 
