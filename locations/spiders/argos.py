b2bafc4cce2c5119bdb96f9e38853b26fac7a9a6
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-27 14:24:27 +0530

    Skip Collection Point

--- locations/spiders/argos.py
+++ locations/spiders/argos.py
@@ -1,3 +1,6 @@
+from typing import Iterable
+
+from locations.items import Feature
 from locations.storefinders.yext_answers import YextAnswersSpider
 
 
@@ -8,3 +11,8 @@ class ArgosSpider(YextAnswersSpider):
     experience_key = "argos-locator"
     api_key = "5295de161feec2b9e1ca6f483e9f77dd"
     locale = "en-GB"
+
+    def parse_item(self, location: dict, item: Feature) -> Iterable[Feature]:
+        if "Collection Point" in item["name"]:
+            return
+        yield item
