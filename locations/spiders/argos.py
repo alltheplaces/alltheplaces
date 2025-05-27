2ab4ab56300d9e515817f4b3b1ee402bd1999ed8
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-27 14:06:08 +0530

    Refactor using Yext API to get better POI data and get rid of proxy needs

--- locations/spiders/argos.py
+++ locations/spiders/argos.py
@@ -1,38 +1,10 @@
-import re
-
-from scrapy.spiders import SitemapSpider
-
-from locations.spiders.sainsburys import SainsburysSpider
-from locations.spiders.tesco_gb import set_located_in
-from locations.structured_data_spider import StructuredDataSpider
-from locations.user_agents import BROWSER_DEFAULT
+from locations.storefinders.yext_answers import YextAnswersSpider
 
 
-class ArgosSpider(SitemapSpider, StructuredDataSpider):
+class ArgosSpider(YextAnswersSpider):
     name = "argos"
     item_attributes = {"brand": "Argos", "brand_wikidata": "Q4789707"}
-    sitemap_urls = ["https://www.argos.co.uk/robots.txt"]
-    sitemap_rules = [(r"https://www.argos.co.uk/stores/([\d]+)-([\w-]+)", "parse")]
-    sitemap_follow = ["stores"]
-    user_agent = BROWSER_DEFAULT
-    requires_proxy = True
-
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if item["name"].startswith("Closed - "):
-            return
-
-        if m := re.search(r"((?:in|inside|) Sainsbury'?s?)", item["name"], flags=re.IGNORECASE):
-            item["name"] = item["name"].replace(m.group(1), "").strip()
-            set_located_in(SainsburysSpider.SAINSBURYS, item)
-        if item["name"].endswith(" Local"):
-            item["name"] = item["name"].removesuffix(" Local")
-            set_located_in(SainsburysSpider.SAINSBURYS_LOCAL, item)
-
-        if m := re.search(r'"type":(\d),"lat":(-?\d+\.\d+),"lng":(-?\d+\.\d+),', response.text):
-            store_type, item["lat"], item["lon"] = m.groups()
-            if store_type == "1" or "Collection Point" in item["name"] or "CP" in item["name"]:
-                return
-
-        item.pop("name", None)
-
-        yield item
+    endpoint = "https://prod-cdn.us.yextapis.com/v2/accounts/me/search/vertical/query"
+    experience_key = "argos-locator"
+    api_key = "5295de161feec2b9e1ca6f483e9f77dd"
+    locale = "en-GB"
