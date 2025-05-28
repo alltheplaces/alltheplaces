4ebed80a389ce843359481869a07001a35d68887
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-28 13:35:51 +0530

    Use Structured Data to refactor and fix Leeann Chin US Spider

--- locations/spiders/leeann_chin_us.py
+++ locations/spiders/leeann_chin_us.py
@@ -1,35 +1,14 @@
-from typing import Iterable
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-import chompjs
-from scrapy import Selector
-from scrapy.http import Response
-
-from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
-from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class LeeannChinUSSpider(JSONBlobSpider):
+class LeeannChinUSSpider(CrawlSpider, StructuredDataSpider):
     name = "leeann_chin_us"
     item_attributes = {
         "brand": "Leeann Chin",
         "brand_wikidata": "Q6515716",
     }
     start_urls = ["https://www.leeannchin.com/locations"]
-
-    def extract_json(self, response: Response) -> list:
-        return chompjs.parse_js_object(response.xpath('//script[contains(text(), "var jsonContent")]/text()').get())[
-            "data"
-        ]
-
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["street"] = feature.get("address_route")
-        item["addr_full"] = feature.get("location")
-        item["branch"] = item.pop("name")
-        item["name"] = self.item_attributes["brand"]
-        hours_info = Selector(text=feature.get("hours", ""))
-        item["opening_hours"] = OpeningHours()
-        item["opening_hours"].add_ranges_from_string(", ".join(hours_info.xpath("//li/text()").getall()))
-        apply_category(Categories.RESTAURANT, item)
-        yield item
+    rules = [Rule(LinkExtractor(allow=r"/restaurant/[-\w]+/[-\w]+/?$"), callback="parse_sd")]
