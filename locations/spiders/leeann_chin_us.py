5de395519045e0cedb4fb1ccd3d62466e7037fc3
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-28 13:49:35 +0530

    Add opening hours

--- locations/spiders/leeann_chin_us.py
+++ locations/spiders/leeann_chin_us.py
@@ -1,6 +1,11 @@
+from typing import Iterable
+
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.hours import OpeningHours
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -13,3 +18,12 @@ class LeeannChinUSSpider(CrawlSpider, StructuredDataSpider):
     start_urls = ["https://www.leeannchin.com/locations"]
     rules = [Rule(LinkExtractor(allow=r"/restaurant/[-\w]+/[-\w]+/?$"), callback="parse_sd")]
     json_parser = "chompjs"
+
+    def pre_process_data(self, ld_data: dict, **kwargs):
+        ld_data.pop("openingHoursSpecification", None)  # Current day is missing from ld_data
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["opening_hours"] = OpeningHours()
+        for rule in response.xpath('//*[@class="opening-hours"]//li'):
+            item["opening_hours"].add_ranges_from_string(rule.xpath("./text()").get())
+        yield item
