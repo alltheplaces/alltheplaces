d61371e45d152ba5c33c80b468b481761bc95718
David Hicks <david@hicks.id.au>
2025-05-29 01:56:07 +1000

    champion: fix spider

--- locations/spiders/champion.py
+++ locations/spiders/champion.py
@@ -1,22 +1,31 @@
-import re
-from json import loads
-from typing import Iterable
-
-from scrapy import Selector
-from scrapy.http import Response
-
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours, DAYS_EN, DAYS_FULL
 from locations.items import Feature
-from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
+from locations.storefinders.stockist import StockistSpider
 
 
-class ChampionSpider(AmastyStoreLocatorSpider):
+class ChampionSpider(StockistSpider):
     name = "champion"
     item_attributes = {"brand": "Champion", "brand_wikidata": "Q2948688"}
-    start_urls = ["https://www.championstore.com/en_gb/stores"]
+    key = "map_w3rk47yq"
 
-    def parse(self, response: Response) -> Iterable[Feature]:
-        yield from self.parse_features(loads(re.search(r"jsonLocations: ({.+}),", response.text).group(1))["items"])
-
-    def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
-        item["addr_full"] = popup_html.xpath('//span[contains(@class, "amlocator-address")]/text()').get()
+    def parse_item(self, item: Feature, location: dict):
+        print(location)
+        item["opening_hours"] = OpeningHours()
+        for custom_field in location["custom_fields"]:
+            if custom_field["name"] in DAYS_FULL:
+                if custom_field["value"] == "-":
+                    # Probably denotes unknown opening hours rather than
+                    # closure on a particular day.
+                    continue
+                if custom_field["value"] == "x":
+                    # Probably denotes a closed day.
+                    item["opening_hours"].set_closed(DAYS_EN[custom_field["name"]])
+                    continue
+                if "," in custom_field["value"]:
+                    for time_range in custom_field["value"].split(","):
+                        item["opening_hours"].add_range(DAYS_EN[custom_field["name"]], *time_range.strip().split(" - ", 1))
+                else:
+                    item["opening_hours"].add_range(DAYS_EN[custom_field["name"]], *custom_field["value"].split(" - ", 1))
+        apply_category(Categories.SHOP_CLOTHES, item)
         yield item
