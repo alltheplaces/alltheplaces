5cfd91c8e7931e3d2de365da079f2581d2125bad
panasonic-mafia <35237910+panasonic-mafia@users.noreply.github.com>
2025-05-29 13:56:22 +0200

    correct wikidata

--- locations/spiders/kurt_geiger.py
+++ locations/spiders/kurt_geiger.py
@@ -4,7 +4,7 @@ from locations.storefinders.yext_answers import YextAnswersSpider
 
 class KurtGeigerSpider(YextAnswersSpider):
     name = "kurt_geiger"
-    item_attributes = {"brand": "Kurt Geiger", "brand_wikidata": "Q15824663"}
+    item_attributes = {"brand": "Kurt Geiger", "brand_wikidata": "Q17063744"}
     endpoint = "https://prod-cdn.us.yextapis.com/v2/accounts/me/search/vertical/query"
     experience_key = "kurt-geiger-search"
     api_key = "672f8261f70abeb66b9ce6c5c0572e7f"
