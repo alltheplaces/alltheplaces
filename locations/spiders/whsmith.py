8ed65f4d790719c401500f26095c710b0210415c
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-29 19:18:37 +0530

    Add coordinates

--- locations/spiders/whsmith.py
+++ locations/spiders/whsmith.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any
 
 from scrapy.http import Response
@@ -21,10 +22,13 @@ class WhsmithSpider(SitemapSpider):
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {
         "ROBOTSTXT_OBEY": False,
     }
+    coordinates_pattern = re.compile(r"google\.maps\.LatLng\(\s*([-\d.]+)[,\s]+([-\d.]+)\s*\)")
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
         item["ref"] = item["website"] = response.url
         item["addr_full"] = clean_address(response.xpath('//*[@class="shop-styling__address"]/p/text()').getall())
+        if coordinates := re.search(self.coordinates_pattern, response.text):
+            item["lat"], item["lon"] = coordinates.groups()
         apply_category(Categories.SHOP_NEWSAGENT, item)
         yield item
