8d7c8c083c681a2b7cdaceddafce9e1d277e4edf
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-05-30 11:45:13 +0530

    code refactored to fix spider

--- locations/spiders/krefel_be.py
+++ locations/spiders/krefel_be.py
@@ -1,38 +1,25 @@
 from typing import Any, Iterable
 
-import xmltodict
 from scrapy import Request, Spider
 from scrapy.http import JsonRequest, Response
 
 from locations.dict_parser import DictParser
-from locations.user_agents import BROWSER_DEFAULT
+from locations.user_agents import FIREFOX_LATEST
 
 
 class KrefelBESpider(Spider):
     name = "krefel_be"
     item_attributes = {"brand": "Krëfel", "brand_wikidata": "Q3200093"}
-    user_agent = BROWSER_DEFAULT
-    requires_proxy = True
+    user_agent = FIREFOX_LATEST
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def start_requests(self) -> Iterable[Request]:
-        yield JsonRequest(url="https://api.krefel.be/api/v2/krefel/stores?pageSize=100&lang=nl")
+        yield JsonRequest(url="https://api.krefel.be/occ/v2/krefel/stores?fields=STORE_FINDER&pageSize=1000&lang=nl")
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        # API response's content-type is not consistent.
-        if response.headers["Content-Type"] == b"application/json":
-            locations = response.json()["stores"]
-        else:
-            locations = xmltodict.parse(response.text)["storeFinderSearchPage"]["stores"]
-        for location in locations:
-            location["ref"] = location.pop("name")
-            location["phone"] = "; ".join(
-                filter(None, [location["address"].get("phone"), location["address"].get("phone2")])
-            ).replace("/", "")
-            location["address"]["house_number"] = location["address"].pop("line2", "")
-            location["address"]["street"] = location["address"].pop("line1")
-
-            item = DictParser.parse(location)
-            item["website"] = item["extras"]["website:nl"] = f'https://www.krefel.be/nl/winkels/{location["ref"]}'
-            item["extras"]["website:fr"] = f'https://www.krefel.be/fr/magasins/{location["ref"]}'
+        for store in response.json()["stores"]:
+            item = DictParser.parse(store)
             item["branch"] = item.pop("name")
+            item["ref"] = store["krefelId"]
+            item["website"] = "/".join(["https://www.krefel.be/nl/winkels", store["name"].lower()])
             yield item
