28490a1b0068065bef5964eb60d41a1dfd341cac
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-05-30 17:14:20 +0530

    Use CrawlSpider

--- locations/spiders/planet_fitness.py
+++ locations/spiders/planet_fitness.py
@@ -1,9 +1,5 @@
-import re
-from typing import Any, Iterable
-
-from geonamescache import GeonamesCache
-from scrapy import Request
-from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
@@ -11,9 +7,14 @@ from locations.user_agents import BROWSER_DEFAULT
 
 
 # Sitemap https://www.planetfitness.com/sitemap.xml isn't accessible for the spider.
-class PlanetFitnessSpider(StructuredDataSpider):
+class PlanetFitnessSpider(CrawlSpider, StructuredDataSpider):
     name = "planet_fitness"
     item_attributes = {"brand": "Planet Fitness", "brand_wikidata": "Q7201095"}
+    start_urls = ["https://www.planetfitness.com/clubs"]
+    rules = [
+        Rule(LinkExtractor(allow=r"/clubs/[a-z]{2}/?$")),
+        Rule(LinkExtractor(allow="/gyms/[-\w]+/?$"), callback="parse_sd"),
+    ]
     requires_proxy = True
     user_agent = BROWSER_DEFAULT
     is_playwright_spider = True
@@ -23,11 +24,3 @@ class PlanetFitnessSpider(StructuredDataSpider):
         "DOWNLOAD_DELAY": 3,
         "RETRY_TIMES": 5,
     }
-
-    def start_requests(self) -> Iterable[Request]:
-        for state in GeonamesCache().get_us_states_by_names():
-            yield Request(f'https://www.planetfitness.com/gyms.data?limit=1000&q={state.lower().replace(" ", "-")}')
-
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for slug in set(re.findall(r"planetfitness\.com/gyms/([-\w]+)/offers", response.text)):
-            yield Request(url=f"https://www.planetfitness.com/gyms/{slug}", callback=self.parse_sd)
