57cb17522633222ff2068fe16f28fa6ba4755223
Cj Malone <CjMalone@mail.com>
2025-06-03 12:31:34 +0100

    [Distriktstandvården SE] Drop mobile location

--- locations/spiders/distriktstandvarden_se.py
+++ locations/spiders/distriktstandvarden_se.py
@@ -9,6 +9,7 @@ class DistriktstandvardenSESpider(SitemapSpider, StructuredDataSpider):
     name = "distriktstandvarden_se"
     item_attributes = {"brand": "Distriktstandvården", "brand_wikidata": "Q10474535"}
     sitemap_urls = ["https://distriktstandvarden.se/dtv_clinic-sitemap.xml"]
+    sitemap_rules = [(r"distriktstandvarden\.se/(?!mobil-)([^/]+)/", "parse")]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         if coords := response.xpath('//img[@class="clinic-static-map"]/@src').re(
