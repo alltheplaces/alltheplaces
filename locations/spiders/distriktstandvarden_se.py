f6a4f77f61fa386bd018025a7e02b800835330b9
Cj Malone <CjMalone@mail.com>
2025-06-03 12:31:21 +0100

    [Distriktstandvården SE] Collect coords

--- locations/spiders/distriktstandvarden_se.py
+++ locations/spiders/distriktstandvarden_se.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -7,3 +9,10 @@ class DistriktstandvardenSESpider(SitemapSpider, StructuredDataSpider):
     name = "distriktstandvarden_se"
     item_attributes = {"brand": "Distriktstandvården", "brand_wikidata": "Q10474535"}
     sitemap_urls = ["https://distriktstandvarden.se/dtv_clinic-sitemap.xml"]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        if coords := response.xpath('//img[@class="clinic-static-map"]/@src').re(
+            r"png%7C(-?\d+\.\d+)%2C(-?\d+\.\d+)%26style"
+        ):
+            item["lat"], item["lon"] = coords
+        yield item
