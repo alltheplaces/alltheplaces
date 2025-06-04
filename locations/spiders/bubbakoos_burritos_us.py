e92937032485fdf93c665a6112b95660dc7a1478
David Hicks <david@hicks.id.au>
2025-06-04 23:34:59 +1000

    [Bubbakoo's Burritos US] Fix spider

--- locations/spiders/bubbakoos_burritos_us.py
+++ locations/spiders/bubbakoos_burritos_us.py
@@ -1,11 +1,19 @@
-from locations.storefinders.where2getit import Where2GetItSpider
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class BubbakoosBurritosUSSpider(Where2GetItSpider):
+class BubbakoosBurritosUSSpider(SitemapSpider, StructuredDataSpider):
     name = "bubbakoos_burritos_us"
-    item_attributes = {
-        "brand_wikidata": "Q114619751",
-        "brand": "Bubbakoo's Burritos",
-    }
-    api_endpoint = "https://locations.bubbakoos.com/rest/getlist"
-    api_key = "DAD82608-FE61-11EE-B900-E7D4C42605F6"
+    item_attributes = {"brand": "Bubbakoo's Burritos", "brand_wikidata": "Q114619751"}
+    sitemap_urls = ["https://locations.bubbakoos.com/sitemap.xml"]
+    sitemap_rules = [(r"^https:\/\/locations\.bubbakoos\.com\/locations\/[a-z]{2}\/[\w\-]+$", "parse_sd")]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict):
+        item["branch"] = response.xpath('//h1/text()').get()
+        item.pop("facebook", None)
+        apply_category(Categories.FAST_FOOD, item)
+        yield item
