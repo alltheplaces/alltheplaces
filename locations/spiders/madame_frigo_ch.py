b659af5ae42cfaae3f11be7cf720d9ec0ef1934b
Karen Kerr <karenkerrgithub@gmail.com>
2025-06-04 08:45:57 +0100

    [MadameFrigo CH] Update start url to fix spider

--- locations/spiders/madame_frigo_ch.py
+++ locations/spiders/madame_frigo_ch.py
@@ -6,7 +6,7 @@ from locations.items import Feature
 class MadameFrigoCHSpider(Spider):
     name = "madame_frigo_ch"
     item_attributes = {"brand": "Madame Frigo", "brand_wikidata": "Q122664134"}
-    start_urls = ["https://www.madamefrigo.ch/en/towns/"]
+    start_urls = ["https://www.madamefrigo.ch/en/locations/"]
 
     def parse(self, response, **kwargs):
         for location in response.xpath('//div[@id="maps-pins"]/div[@class="fridge"]'):
