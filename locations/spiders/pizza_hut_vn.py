1fbd3628eb3b06267c7297799cb1f4cb2d0d8a99
BelousDmitry <belous0219111999@gmail.com>
2025-06-04 16:42:40 -0400

    fixing pizza_hut_vn spider

--- locations/spiders/pizza_hut_vn.py
+++ locations/spiders/pizza_hut_vn.py
@@ -1,7 +1,6 @@
 import base64
 import hashlib
 import hmac
-import re
 import time
 import uuid
 from typing import Any
@@ -19,31 +18,16 @@ from locations.user_agents import BROWSER_DEFAULT
 class PizzaHutVNSpider(scrapy.Spider):
     name = "pizza_hut_vn"
     item_attributes = {"brand": "Pizza Hut", "brand_wikidata": "Q191615"}
-    start_urls = ["https://pizzahut.vn/store-location?area=north"]
     api_url = "https://rwapi.pizzahut.vn/api/store/GetAllStoreList"
     user_agent = BROWSER_DEFAULT
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        # Search for the desired JavaScript file
-        yield response.follow(
-            url=response.xpath("//script/@src").re(r"/_next/static/chunks/3070-\w+\.js")[-1],
-            callback=self.parse_auth_token,
-        )
-
-    def parse_auth_token(self, response: Response, **kwargs: Any) -> Any:
+    def start_requests(self):
         timestamp = int(time.time() * 1000)
         device_uid = uuid.uuid4()
 
-        auth_data = {}
-        for auth_param in ["app_code", "access_key", "secret_key"]:
-            if identifier := self.find_identifier(response, auth_param.title().replace("_", "")):
-                pattern = re.compile(rf"{identifier}[\s=]+\(0,[\n\s]*[a-z]\.[a-z]\)\(\"([a-z0-9]+)\"\)")
-            else:  # secret_key
-                pattern = re.compile(r"s[\s=]+\(0,[\n\s]*[a-z]\.[a-z]\)\(\"([a-z0-9]+)\"\)")
+        auth_data = {"app_code": "REDWEB_DI1rpn3vHlyp", "access_key": "9GQ3cVW5Vqq4", "secret_key": "1YWkf7Rh0oJB"}
 
-            auth_data[auth_param] = re.search(pattern, response.text).group(1)
-
-        query_string = f"TimeStamp={timestamp}&DeviceUID={device_uid}&AppCode={auth_data['app_code']}&AccessKey={auth_data['access_key']}&Method=GET&url={self.api_url}&Body="
+        query_string = f"TimeStamp={timestamp}&DeviceUID={device_uid}&AppCode={auth_data['app_code']}&AccessKey={auth_data['access_key']}&Method=GET&url=/STORE/GETALLSTORELIST&Body="
 
         key = base64.b64decode(auth_data["secret_key"])
         message = query_string.encode("utf-8")
@@ -76,10 +60,3 @@ class PizzaHutVNSpider(scrapy.Spider):
                 item["opening_hours"].add_days_range(DAYS, store["Open_Time"], store["Close_Time"])
             apply_category(Categories.RESTAURANT, item)
             yield item
-
-    def find_identifier(self, response: Response, key: str) -> str | None:
-        pattern = re.compile(rf"\"&{key}=\"\)\.concat\(([a-z]),")
-        if match := re.search(pattern, response.text):
-            return match.group(1)
-        else:
-            return None
