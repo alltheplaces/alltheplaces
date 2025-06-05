c1a0a9992c5ab3d6ca093b427eba93c31b06e5d3
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-05 19:42:36 +0530

    Clean start url

--- locations/spiders/ottos_ch.py
+++ locations/spiders/ottos_ch.py
@@ -14,7 +14,7 @@ class OttosCHSpider(scrapy.Spider):
     name = "ottos_ch"
     item_attributes = {"brand": "Otto's", "brand_wikidata": "Q2041507"}
     start_urls = [
-        "https://api.ottos.ch/occ/v2/ottos/stores?fields=stores(additionalOpeningInformation%2Cname%2CdisplayName%2CformattedDistance%2CopeningHours(weekDayOpeningList(FULL)%2CspecialDayOpeningList(FULL))%2CgeoPoint(latitude%2Clongitude)%2Caddress(line1%2Cline2%2Ctown%2Cregion(FULL)%2CpostalCode%2Cphone%2Ccountry%2Cemail)%2Cfeatures%2CtodaySchedule(DEFAULT)%2CstoreFeatures(code%2Cname%2Ctooltip))%2Cpagination(DEFAULT)%2Csorts(DEFAULT)%2CselectableStoreFeatures%2CselectedStoreFeature%2CselectableStoreDistances%2CselectedStoreDistance&query=&radius=10000&lang=de&curr=CHF",
+        "https://api.ottos.ch/occ/v2/ottos/stores?fields=stores(additionalOpeningInformation,name,displayName,formattedDistance,openingHours(weekDayOpeningList(FULL),specialDayOpeningList(FULL)),geoPoint(latitude,longitude),address(line1,line2,town,region(FULL),postalCode,phone,country,email),features,todaySchedule(DEFAULT),storeFeatures(code,name,tooltip)),pagination(DEFAULT),sorts(DEFAULT),selectableStoreFeatures,selectedStoreFeature,selectableStoreDistances,selectedStoreDistance&query=&radius=10000&lang=de&curr=CHF"
     ]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
