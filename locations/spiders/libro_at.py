64335b381128e80a78493ebdc126e2dfc5308b5a
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-05 21:28:23 +0530

    Add full address

--- locations/spiders/libro_at.py
+++ locations/spiders/libro_at.py
@@ -5,6 +5,7 @@ from scrapy import Request, Selector
 from scrapy.http import Response
 
 from locations.items import Feature
+from locations.pipelines.address_clean_up import clean_address
 from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
@@ -20,5 +21,6 @@ class LibroATSpider(AmastyStoreLocatorSpider):
 
     def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
         item["ref"], item["street_address"] = item.pop("name").split(": ", 1)
+        item["addr_full"] = clean_address(popup_html.xpath("//span/text()").getall()).removesuffix(", Filiale wählen")
 
         yield item
