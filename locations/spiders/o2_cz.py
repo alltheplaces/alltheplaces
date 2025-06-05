55f6f9956ad2808b1f9afe5ac2b66c064fd3c46d
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-05 16:46:07 +0530

    Clean POI URLs in sitemap to prevent http 404 errors

--- locations/spiders/o2_cz.py
+++ locations/spiders/o2_cz.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Iterable
 
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
@@ -12,11 +12,16 @@ class O2CZSpider(SitemapSpider):
     name = "o2_cz"
     allowed_domains = ["www.o2.cz"]
     sitemap_urls = ["https://www.o2.cz/sitemap/prodejny.xml"]
-    sitemap_rules = [(r"^https://www.o2.cz/prodejny/.*$", "parse")]
+    sitemap_rules = [("/prodejny/", "parse")]
     item_attributes = {
         "brand": "O2",
         "brand_wikidata": "Q1759255",
     }
+
+    def sitemap_filter(self, entries: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]]:
+        for entry in entries:
+            entry["loc"] = entry["loc"].replace(".xml", "")
+            yield entry
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
