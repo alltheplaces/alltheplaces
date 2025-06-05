ff3e4736ac55d60eb5e8454a6269679dbeec2c5b
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-05 21:18:05 +0530

    Override deafult start request to fix Libro AT Spider

--- locations/spiders/libro_at.py
+++ locations/spiders/libro_at.py
@@ -1,7 +1,7 @@
 from json import loads
 from typing import Iterable
 
-from scrapy import Selector
+from scrapy import Request, Selector
 from scrapy.http import Response
 
 from locations.items import Feature
@@ -11,7 +11,9 @@ from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 class LibroATSpider(AmastyStoreLocatorSpider):
     name = "libro_at"
     item_attributes = {"brand": "Libro", "brand_wikidata": "Q1823138"}
-    start_urls = ["https://www.libro.at/rest/V1/mthecom/storelocator/locations"]
+
+    def start_requests(self) -> Iterable[Request]:
+        yield Request(url="https://www.libro.at/rest/V1/mthecom/storelocator/locations")
 
     def parse(self, response: Response) -> Iterable[Feature]:
         yield from self.parse_features(loads(response.xpath("/response/text()").get())["items"])
