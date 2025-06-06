9aee34ae6fecfb913e142ccdf489052c814d66b9
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-06 18:29:18 +0530

    code refactored to fix spider

--- locations/spiders/carls_jr_nz.py
+++ locations/spiders/carls_jr_nz.py
@@ -1,27 +1,35 @@
-import json
 from typing import Any
+from urllib.parse import urljoin
 
 from scrapy import Spider
-from scrapy.http import Response
+from scrapy.http import JsonRequest, Response
 
-from locations.items import Feature
-from locations.pipelines.address_clean_up import merge_address_lines
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_FULL, OpeningHours
 from locations.spiders.carls_jr_us import CarlsJrUSSpider
 
 
 class CarlsJrNZSpider(Spider):
     name = "carls_jr_nz"
     item_attributes = CarlsJrUSSpider.item_attributes
-    start_urls = ["https://www.carlsjr.co.nz/stores"]
-    no_refs = True
+    start_urls = ["https://api.carlsjr.co.nz/configurations"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.xpath("//@data-context").getall():
-            store = json.loads(location)["location"]
-            item = Feature()
-            item["name"] = store.get("addressTitle")
-            item["lat"] = store.get("mapLat")
-            item["lon"] = store.get("mapLng")
-            item["addr_full"] = merge_address_lines([store.get("addressLine1"), store.get("addressLine2")])
-            item["website"] = response.url
+        for key in response.json()["storeKeys"]:
+            yield JsonRequest(url="https://api.carlsjr.co.nz/stores/{}".format(key), callback=self.parse_details)
+
+    def parse_details(self, response):
+        for store in response.json()["Value"]:
+            item = DictParser.parse(store)
+            item["website"] = urljoin("https://carlsjr.co.nz/find-a-carls/", store["url"])
+            item["branch"] = item.pop("name").removeprefix(
+                "Carl's Jr ",
+            )
+            item["opening_hours"] = OpeningHours()
+            for day_time in store["operatingHoursStore"]:
+                open_time = day_time["start"]
+                close_time = day_time["end"]
+                day = DAYS_FULL[day_time["dayofweek"]]
+                item["opening_hours"].add_range(day=day, open_time=open_time, close_time=close_time)
+
             yield item
