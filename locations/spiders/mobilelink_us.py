aecbe415ebbbb54576ed35014c22402b492822c2
Karen Kerr <138442998+karen-kerr@users.noreply.github.com>
2025-06-06 12:13:45 +0100

    [Mobilelink] Add category (#13338)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/mobilelink_us.py
+++ locations/spiders/mobilelink_us.py
@@ -1,15 +1,17 @@
 from typing import Any
+from urllib.parse import urljoin
 
 from scrapy import Request, Spider
 from scrapy.http import Response
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
 
 
-class MobilelinkSpider(Spider):
-    name = "mobilelink"
-    item_attributes = {"brand": "Mobilelink"}
+class MobilelinkUSSpider(Spider):
+    name = "mobilelink_us"
+    item_attributes = {"brand": "Cricket Wireless", "brand_wikidata": "Q5184987", "operator": "Mobilelink"}
     allowed_domains = ["mobilelinkusa.com"]
 
     def start_requests(self):
@@ -22,6 +24,7 @@ class MobilelinkSpider(Spider):
         for location in response.json():
             item = DictParser.parse(location)
             item["branch"] = location["Store"]
+            item["website"] = urljoin("https://mobilelinkusa.com/stores/", location["StoreSEOTag"])
             item["phone"] = location["Contact"]
 
             oh = OpeningHours()
@@ -33,5 +36,5 @@ class MobilelinkSpider(Spider):
                 else:
                     oh.add_range(day, location["Opentime"], location["Closetime"], "%H:%M:%S")
             item["opening_hours"] = oh
-
+            apply_category(Categories.SHOP_MOBILE_PHONE, item)
             yield item
