104867fcc12c10c8a2c4efde02ac00b92b83ef68
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-09 17:54:53 +0530

    code refactored to use sitemap to fix spider

--- locations/spiders/laseraway_us.py
+++ locations/spiders/laseraway_us.py
@@ -1,25 +1,25 @@
+import re
 from typing import Iterable
-from urllib.parse import urljoin
 
 from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories
 from locations.hours import DAYS_EN
 from locations.items import Feature
-from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class LaserawayUSSpider(WPStoreLocatorSpider):
+class LaserawayUSSpider(SitemapSpider, StructuredDataSpider):
     name = "laseraway_us"
     item_attributes = {"brand_wikidata": "Q119982751", "brand": "LaserAway", "extras": Categories.SHOP_BEAUTY.value}
-    allowed_domains = [
-        "www.laseraway.com",
-    ]
+    sitemap_urls = ["https://www.laseraway.com/sitemap_index.xml"]
+    sitemap_rules = [(r"/locations/[^/]+/[^/]+/$", "parse_sd")]
+    wanted_types = ["HealthAndBeautyBusiness"]
     days = DAYS_EN
     requires_proxy = "US"  # Cloudflare captcha in use
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item.pop("addr_full", None)
         item["branch"] = item.pop("name").removeprefix("LaserAway ").removeprefix("– ")
-        item["website"] = urljoin("https://www.laseraway.com", feature["url"])
+        item["lat"], item["lon"] = re.search(r"lat\":\"(-?\d+\.\d+)\",\"lng\":\"(-?\d+\.\d+)\"", response.text).groups()
         yield item
