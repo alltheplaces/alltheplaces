c3dfbe3ff897497783da815c074ba068df6cf72e
Cj Malone <CjMalone@mail.com>
2025-06-09 19:36:09 +0100

    Collect located_in

--- locations/spiders/kuoni_gb.py
+++ locations/spiders/kuoni_gb.py
@@ -5,6 +5,8 @@ from scrapy.http import Response
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.john_lewis_gb import JohnLewisGBSpider
+from locations.spiders.tesco_gb import set_located_in
 
 
 class KuoniGBSpider(JSONBlobSpider):
@@ -13,9 +15,12 @@ class KuoniGBSpider(JSONBlobSpider):
     start_urls = ["https://www.kuoni.co.uk/api/appointment/get-stores/"]
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        if "Kuoni Partner" not in item["name"]:
+        if feature["isPartner"] is False:
             item["branch"] = feature["baseName"]
             item["name"] = None
+
+        if feature["isJohnLewis"] is True:
+            set_located_in(JohnLewisGBSpider.item_attributes, item)
 
         item["website"] = response.urljoin(feature["link"])
 
