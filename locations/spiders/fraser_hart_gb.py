d2b69f2ffdfedd1cc801b67a200e915193a7d127
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-09 14:59:40 +0200

    [Fraser Hart GB]
    This uses elfsight but isn't configured quite right to use the storefinder.

---
+++ locations/spiders/fraser_hart_gb.py
@@ -0,0 +1,27 @@
+from typing import Iterable
+from urllib.parse import unquote
+
+import chompjs
+from scrapy.http import JsonRequest, Request, Response
+
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class FraserHartGBSpider(JSONBlobSpider):
+    name = "fraser_hart_gb"
+    item_attributes = {"brand": "Fraser Hart", "brand_wikidata": "Q134776549"}
+    host = "core.service.elfsight.com"
+    shop = "www.fraserhart.co.uk"
+    api_key = "4a4f06f9-ddef-4c3e-a843-7ed8bb1de4c4"
+    no_refs = True
+
+
+    def start_requests(self) -> Iterable[JsonRequest | Request]:
+            yield JsonRequest(f"https://{self.host}/p/boot/?w={self.api_key}")
+
+    def extract_json(self, response: Response) -> list:
+            data = chompjs.parse_js_object(response.text)
+            return data["data"]["widgets"][self.api_key]["data"]["settings"]["locations"]
+
+    def pre_process_data(self, location: dict):
+        location["lat"], location["lon"] = location.get("place")["coordinates"]["lat"],location.get("place")["coordinates"]["lng"]
