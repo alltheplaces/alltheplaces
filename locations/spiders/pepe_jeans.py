c89c8cf7d774973356e37fe09b976ba4f8b41992
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-09 19:28:01 +0200

    Add branch and category

--- locations/spiders/pepe_jeans.py
+++ locations/spiders/pepe_jeans.py
@@ -1,7 +1,12 @@
 from locations.storefinders.yext import YextSpider
+from locations.categories import Categories, apply_category
 
 
 class PepeJeansSpider(YextSpider):
     name = "pepe_jeans"
     item_attributes = {"brand": "Pepe Jeans", "brand_wikidata": "Q426992"}
     api_key = "ed5d8ca6a191dbb8daeb12e8714a06c5"
+
+def parse_item(self, item, location, **kwargs):
+    item["branch"] = item.pop("name").removeprefix("Pepe Jeans ")
+    apply_category(Categories.SHOP_CLOTHES, item)
