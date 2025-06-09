0ed893a71db243db817495ea1484590491453417
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-09 18:00:04 +0200

    Update kuoni_gb.py

--- locations/spiders/kuoni_gb.py
+++ locations/spiders/kuoni_gb.py
@@ -1,6 +1,10 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
 from locations.categories import Categories, apply_category
-from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
+from locations.items import Feature
 
 
 class KuoniGBSpider(JSONBlobSpider):
@@ -8,8 +12,8 @@ class KuoniGBSpider(JSONBlobSpider):
     item_attributes = {"brand": "Kuoni", "brand_wikidata": "Q684355"}
     start_urls = ["https://www.kuoni.co.uk/api/appointment/get-stores/?r=20250609123615"]
 
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
 
-def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-    item["branch"] = item.pop("name").removeprefix("Kuoni ")
-    apply_category(Categories.SHOP_TRAVEL_AGENCY, item)
-    yield item
+        item["branch"] = item.pop("name").removeprefix("Kuoni ")
+        apply_category(Categories.SHOP_TRAVEL_AGENCY, item)
+        yield item
