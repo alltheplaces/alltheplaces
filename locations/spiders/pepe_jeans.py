e3f4ba54082ef900f2d29a347bcf2ee50ddea399
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-09 19:57:52 +0200

    Remove twitter

--- locations/spiders/pepe_jeans.py
+++ locations/spiders/pepe_jeans.py
@@ -6,10 +6,11 @@ class PepeJeansSpider(YextSpider):
     name = "pepe_jeans"
     item_attributes = {"brand": "Pepe Jeans", "brand_wikidata": "Q426992"}
     api_key = "ed5d8ca6a191dbb8daeb12e8714a06c5"
+        
 
-
-def parse_item(self, item, location, **kwargs):
-    if "Pepe Jeans" in item["name"]:
-        item["branch"] = item.pop("name").removeprefix("Pepe Jeans ")
-        apply_category(Categories.SHOP_CLOTHES, item)
-        yield item
+    def parse_item(self, item, location, **kwargs):
+        item.pop("twitter", None)
+        if "Pepe Jeans" in item["name"]:
+            item["branch"] = item.pop("name").removeprefix("Pepe Jeans ")
+            apply_category(Categories.SHOP_CLOTHES, item)
+            yield item
