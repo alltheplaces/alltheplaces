d67767d3a92e68702b2e63428e3cddd50b817aec
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-09 19:19:03 +0200

    [Pepe Jeans] Add Spider

---
+++ locations/spiders/pepe_jeans.py
@@ -0,0 +1,7 @@
+from locations.storefinders.yext import YextSpider
+
+
+class PepeJeansSpider(YextSpider):
+    name = "pepe_jeans"
+    item_attributes = {"brand": "Pepe Jeans", "brand_wikidata": "Q426992"}
+    api_key = "ed5d8ca6a191dbb8daeb12e8714a06c5"
