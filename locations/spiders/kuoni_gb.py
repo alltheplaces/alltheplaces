a2d738c3a85d47c2b1b5dc77f70a7699821829fb
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-09 17:04:09 +0200

    Add branch

--- locations/spiders/kuoni_gb.py
+++ locations/spiders/kuoni_gb.py
@@ -1,7 +1,13 @@
 from locations.json_blob_spider import JSONBlobSpider
+from locations.categories import Categories, apply_category
 
 
 class KuoniGBSpider(JSONBlobSpider):
     name = "kuoni_gb"
     item_attributes = {"brand": "Kuoni", "brand_wikidata": "Q684355"}
     start_urls = ["https://www.kuoni.co.uk/api/appointment/get-stores/?r=20250609123615"]
+
+def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+    item["branch"] = item.pop("name").removeprefix("Kuoni ")
+    apply_category(Categories.SHOP_TRAVEL_AGENCY, item)
+    yield item
