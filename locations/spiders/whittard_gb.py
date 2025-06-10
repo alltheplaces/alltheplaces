8ff340acb3ab561eaef68f83adb043d8c32890ad
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-10 17:04:23 +0200

    [Whittard GB] Add Spider

---
+++ locations/spiders/whittard_gb.py
@@ -0,0 +1,34 @@
+from scrapy import Spider
+from scrapy.http import Request
+
+from locations.items import Feature
+from locations.pipelines.address_clean_up import clean_address
+
+
+class WhittardGBSpider(Spider):
+    name = "whittard_gb"
+    item_attributes = {"brand": "Whittard", "brand_wikidata": "Q7996831"}
+
+    def request_page(self, next_offset):
+        yield Request(
+            url=f"https://www.whittard.co.uk/stores?ajax=true&start={next_offset}",
+            meta={"offset": next_offset},
+        )
+
+    def start_requests(self):
+        yield from self.request_page(0)
+
+    def parse(self, response):
+        for location in response.xpath('.//div[@class="store-row-container "]'):
+            item = Feature()
+            item["ref"] = location.xpath('.//div[@class="store-row grid-x location-cell"]/@data-storeid').get()
+            item["lat"],item["lon"] = location.xpath('.//span[@class="store-header"]/@data-lat').get(),location.xpath('.//span[@class="store-header"]/@data-lng').get()
+            item["branch"] = location.xpath('.//span[@class="store-header"]//text()').get().strip()
+            item["addr_full"] = clean_address(location.xpath('.//div[@class="cell small-12 store-details"]//text()').getall())
+            item["phone"] = location.xpath('.//div[@class="contacts contacts-desktop"]//text()')[2].get()
+            yield item
+
+
+        if "View More Stores" in response.text:
+            next_offset = response.meta["offset"] + 5
+            yield from self.request_page(next_offset)
