45c062ddda66abf76a0dc5ca6af8f325ecdd7678
Karen Kerr <karenkerrgithub@gmail.com>
2025-06-10 15:10:26 +0100

    [GoGrill BG] Add spider

---
+++ locations/spiders/go_grill_bg.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class GoGrillBGSpider(JSONBlobSpider):
+    name = "go_grill_bg"
+    item_attributes = {"brand": "Go Grill", "brand_wikidata": "Q122839782"}
+    start_urls = [
+        "https://gogrill.bg/wp-admin/admin-ajax.php?action=store_search&autoload=1",
+    ]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = feature.get("store").replace("GO GRILL • ", "")
+        apply_category(Categories.RESTAURANT, item)
+        yield item
