deaaa5bba10cfcca67fbb5ed0825e326b80ae23c
Cj Malone <CjMalone@mail.com>
2025-06-10 12:42:43 +0100

    Opening hours are for chemest, not pharmacy

--- locations/spiders/rite_aid_us.py
+++ locations/spiders/rite_aid_us.py
@@ -2,6 +2,7 @@ from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -19,5 +20,7 @@ class RiteAidUSSpider(CrawlSpider, StructuredDataSpider):
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["image"] = None
+
+        apply_category(Categories.SHOP_CHEMIST, item)
 
         yield item
