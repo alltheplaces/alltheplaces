38f52516139073639fe0813c60e57a7137137e7d
Cj Malone <CjMalone@mail.com>
2025-06-10 12:42:28 +0100

    Drop image

--- locations/spiders/rite_aid_us.py
+++ locations/spiders/rite_aid_us.py
@@ -1,6 +1,8 @@
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -8,11 +10,14 @@ class RiteAidUSSpider(CrawlSpider, StructuredDataSpider):
     name = "rite_aid_us"
     item_attributes = {"brand": "Rite Aid", "brand_wikidata": "Q3433273"}
     start_urls = ["https://www.riteaid.com/locations/index.html"]
-
     rules = [
-        Rule(
-            LinkExtractor(allow=r"/\w{2}\.html$"),
-        ),
+        Rule(LinkExtractor(allow=r"/\w{2}\.html$")),
         Rule(LinkExtractor(allow=r"/\w{2}/[^/]+\.html$")),
         Rule(LinkExtractor(allow=r"/\w{2}/[^/]+/[^/]+\.html$"), callback="parse_sd"),
     ]
+    wanted_types = ["Store"]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["image"] = None
+
+        yield item
