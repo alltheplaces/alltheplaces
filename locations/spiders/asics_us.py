8a8b0b3f15658f72a2818e99cefd403db8674353
Spencer Alves <impiaaa@gmail.com>
2025-06-11 15:02:16 -0700

    ASICS (US): Move addr_full to street_address (fix #11702)

--- locations/spiders/asics_us.py
+++ locations/spiders/asics_us.py
@@ -8,3 +8,7 @@ class AsicsUSSpider(LocallySpider):
     start_urls = [
         "https://www.locally.com/stores/conversion_data?has_data=true&company_id=1682&store_mode=&style=&color=&upc=&category=&inline=1&show_links_in_list=&parent_domain=&map_center_lat=31.945163222857545&map_center_lng=-96.352774663203&map_distance_diag=2692.1535387671056&sort_by=proximity&no_variants=0&only_retailer_id=&dealers_company_id=&only_store_id=false&uses_alt_coords=false&q=false&zoom_level=5"
     ]
+
+    def post_process_item(self, item, response, location):
+        item["street_address"] = item.pop("addr_full")
+        yield item
