1acc99b2d75b82c626cfebac6ebf01f51d67d7f3
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-11 14:52:27 +0530

    Add services

--- locations/spiders/arbys_ca.py
+++ locations/spiders/arbys_ca.py
@@ -4,6 +4,7 @@ from typing import Any, Iterable
 from scrapy.http import JsonRequest, Response
 from scrapy.spiders import Spider
 
+from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
 from locations.spiders.arbys_us import ArbysUSSpider
 
@@ -30,4 +31,9 @@ class ArbysCASpider(Spider):
             item = DictParser.parse(location)
             item["branch"] = item.pop("name").rsplit("-", 1)[0].removeprefix("Arby’s ")
             item["website"] = "https://arbys.ca"
+            apply_yes_no(Extras.DELIVERY, item, location.get("allowDelivery"))
+            apply_yes_no(Extras.TAKEAWAY, item, location.get("allowTakeWay"))
+            apply_yes_no(Extras.DRIVE_THROUGH, item, location.get("allowdriveThru"))
+            apply_yes_no(Extras.INDOOR_SEATING, item, location.get("allowDineIn"))
+            apply_yes_no(Extras.WIFI, item, location.get("allowWifi"))
             yield item
