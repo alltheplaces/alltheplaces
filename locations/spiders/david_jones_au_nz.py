43f89c14c8d9ec101d5db252518dc4867989811a
Cj Malone <CjMalone@mail.com>
2025-06-11 15:52:17 +0100

    Skip

--- locations/spiders/david_jones_au_nz.py
+++ locations/spiders/david_jones_au_nz.py
@@ -1,8 +1,7 @@
-import re
 from typing import Iterable
 
-from chompjs import parse_js_object
-from scrapy.http import Request, Response
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
@@ -10,21 +9,11 @@ from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class DavidJonesAUNZSpider(StructuredDataSpider):
+class DavidJonesAUNZSpider(SitemapSpider, StructuredDataSpider):
     name = "david_jones_au_nz"
     item_attributes = {"brand": "David Jones", "brand_wikidata": "Q5235753"}
     allowed_domains = ["www.davidjones.com"]
-    start_urls = ["https://www.davidjones.com/stores"]
-
-    def parse(self, response: Response) -> Iterable[Request]:
-        js_blob = response.xpath('//script[contains(text(), "stores: [")]/text()').get()
-        js_blob = "[" + js_blob.split("stores: [", 1)[1].split("// ]]>", 1)[0]
-        js_blob = re.sub(r"\s+", " ", js_blob)
-        js_blob = js_blob.removesuffix("}")
-        features = parse_js_object(js_blob)
-        for feature in features:
-            url = "https://www.davidjones.com" + feature["url"]
-            yield Request(url=url, callback=self.parse_sd)
+    sitemap_urls = ["https://www.davidjones.com/sitemaps/stores-sitemap.xml"]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name", None)
