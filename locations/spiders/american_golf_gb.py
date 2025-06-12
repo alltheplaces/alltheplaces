1b6c4c4ba011efddebe3a6f64c77b258798d24cd
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-12 17:43:29 +0530

    code refactored to fix spider

--- locations/spiders/american_golf_gb.py
+++ locations/spiders/american_golf_gb.py
@@ -1,10 +1,11 @@
+import json
+import re
 from typing import Any
 
 from scrapy.http import Response
 from scrapy.spiders import Spider
 
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_FULL, OpeningHours
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
@@ -13,26 +14,21 @@ class AmericanGolfGBSpider(Spider):
     item_attributes = {"brand": "American Golf", "brand_wikidata": "Q62657494"}
     custom_settings = {"ROBOTSTXT_OBEY": False}
     start_urls = [
-        "https://www.americangolf.co.uk/on/demandware.store/Sites-AmericanGolf-GB-Site/en_GB/Stores-GetAllStores"
+        "https://www.americangolf.co.uk/en/find-stores/",
     ]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json()["stores"]:
+        raw_data = json.loads(
+            re.search(
+                r"channel\":(\[.*\])}},\"content",
+                response.xpath('//*[contains(text(),"address1")]/text()').get().replace("\\", ""),
+            ).group(1)
+        )
+        for location in raw_data:
+            location.update(location.pop("resources"))
+            print(location)
             item = DictParser.parse(location)
             item["branch"] = item.pop("name")
+            item["addr_full"] = location["listAddress"]
             item["street_address"] = merge_address_lines([location["address1"], location["address2"]])
-            item["website"] = response.urljoin(location["detailsLink"])
-
-            if location.get("storeHours"):
-                item["opening_hours"] = OpeningHours()
-                for day in map(str.lower, DAYS_FULL):
-                    try:
-                        day_hours = location["storeHours"][day].strip()
-                        if "CLOSED" in day_hours.upper():
-                            item["opening_hours"].set_closed(day)
-                        else:
-                            item["opening_hours"].add_range(day, *day_hours.split(" - "))
-                    except:
-                        continue
-
             yield item
