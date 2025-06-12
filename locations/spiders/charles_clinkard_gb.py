aa263e93c296de310b6a342d23c696fa794b3827
Cj Malone <CjMalone@mail.com>
2025-06-12 14:23:01 +0100

    apply_category

--- locations/spiders/charles_clinkard_gb.py
+++ locations/spiders/charles_clinkard_gb.py
@@ -1,36 +1,31 @@
 import re
+from typing import Any
 
 import scrapy
+from scrapy.http import Response
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 
 class CharlesClinkardGBSpider(scrapy.Spider):
     name = "charles_clinkard_gb"
-    item_attributes = {
-        "brand": "Charles Clinkard",
-        "brand_wikidata": "Q134454710",
-        "extras": Categories.SHOP_SHOES.value,
-    }
-    allowed_domains = [
-        "www.charlesclinkard.co.uk",
-    ]
-    start_urls = ("https://www.charlesclinkard.co.uk/map",)
+    item_attributes = {"brand": "Charles Clinkard", "brand_wikidata": "Q134454710"}
+    allowed_domains = ["www.charlesclinkard.co.uk"]
+    start_urls = ["https://www.charlesclinkard.co.uk/map"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         urls = response.xpath('//a[@class="store-locator__store__link button"]/@href').extract()
         for path in urls:
             yield scrapy.Request(response.urljoin(path), callback=self.parse_store)
 
-    def parse_store(self, response):
+    def parse_store(self, response: Response, **kwargs: Any) -> Any:
         map_data = response.xpath('//script[contains(text(), "google.maps.LatLng")]/text()').extract_first()
         coordinates = re.search(r"var myLatlng = new google\.maps\.LatLng\((.*)\)", map_data).group(1)
         lat, lon = coordinates.split(",")
 
         properties = {
             "branch": response.xpath("//h1/text()").extract_first(),
-            "name": "Charles Clinkard",
             "addr_full": ", ".join(
                 response.xpath('//div[@class="col l-col-16 store-locator__store__col"]/div/p/span/text()').extract()
             ),
@@ -42,4 +37,7 @@ class CharlesClinkardGBSpider(scrapy.Spider):
             "lat": lat,
             "lon": lon,
         }
+
+        apply_category(Categories.SHOP_SHOES, properties)
+
         yield Feature(**properties)
