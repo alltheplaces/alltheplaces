1d137f65f74a09e8dfc190cd679a782934a49c3c
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-06-12 12:08:11 +0100

    Drop google url

--- locations/spiders/go_grill_bg.py
+++ locations/spiders/go_grill_bg.py
@@ -10,11 +10,12 @@ from locations.json_blob_spider import JSONBlobSpider
 class GoGrillBGSpider(JSONBlobSpider):
     name = "go_grill_bg"
     item_attributes = {"brand": "Go Grill", "brand_wikidata": "Q122839782"}
-    start_urls = [
-        "https://gogrill.bg/wp-admin/admin-ajax.php?action=store_search&autoload=1",
-    ]
+    start_urls = ["https://gogrill.bg/wp-admin/admin-ajax.php?action=store_search&autoload=1"]
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["branch"] = feature.get("store").replace("GO GRILL • ", "")
+        item["website"] = None
+        
         apply_category(Categories.RESTAURANT, item)
+        
         yield item
