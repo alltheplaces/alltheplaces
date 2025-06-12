3b390306e55c72dea98d789ebda870b14518bce1
Cj Malone <CjMalone@mail.com>
2025-06-12 13:27:15 +0100

    Clean name

--- locations/spiders/diesel.py
+++ locations/spiders/diesel.py
@@ -17,6 +17,31 @@ class DieselSpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["website"] = item["ref"] = "https://uk.diesel.com/en/store-detail?sid={}".format(feature["ID"])
-        item["branch"] = item.pop("name").removeprefix("DIESEL STORE ")
+
+        if item["name"].startswith("DIESEL ACCESSORIES STORE "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL ACCESSORIES STORE ")
+            item["name"] = "Diesel Accessories"
+        elif item["name"].startswith("DIESEL KID OUTLET "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL KID OUTLET ")
+            item["name"] = "Diesel Kid Outlet"
+        elif item["name"].startswith("DIESEL KID STORE "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL KID STORE ")
+            item["name"] = "Diesel Kid"
+        elif item["name"].startswith("DIESEL MENSWEAR STORE "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL MENSWEAR STORE ")
+            item["name"] = "Diesel Menswear"
+        elif item["name"].startswith("DIESEL OUTLET "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL OUTLET ")
+            item["name"] = "Diesel Outlet"
+        elif item["name"].startswith("DIESEL STORE "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL STORE ")
+            item["name"] = "Diesel"
+        elif item["name"].startswith("DIESEL WOMENSWEAR STORE "):
+            item["branch"] = item.pop("name").removeprefix("DIESEL WOMENSWEAR STORE ")
+            item["name"] = "Diesel Womenswear"
+        else:
+            item["branch"] = item.pop("name").removeprefix("DIESEL ")
+
         apply_category(Categories.SHOP_CLOTHES, item)
+
         yield item
