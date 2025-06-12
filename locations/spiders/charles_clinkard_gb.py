329d1515a06d572aa337c70b3c7ded8974ea45a1
Cj Malone <CjMalone@mail.com>
2025-06-12 14:26:13 +0100

    Switch to CrawlSpider

--- locations/spiders/charles_clinkard_gb.py
+++ locations/spiders/charles_clinkard_gb.py
@@ -1,25 +1,22 @@
 import re
 from typing import Any
 
-import scrapy
 from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 
-class CharlesClinkardGBSpider(scrapy.Spider):
+class CharlesClinkardGBSpider(CrawlSpider):
     name = "charles_clinkard_gb"
     item_attributes = {"brand": "Charles Clinkard", "brand_wikidata": "Q134454710"}
     allowed_domains = ["www.charlesclinkard.co.uk"]
     start_urls = ["https://www.charlesclinkard.co.uk/map"]
+    rules = [Rule(LinkExtractor(allow=r"/map/[^/]+$"), "parse")]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        urls = response.xpath('//a[@class="store-locator__store__link button"]/@href').extract()
-        for path in urls:
-            yield scrapy.Request(response.urljoin(path), callback=self.parse_store)
-
-    def parse_store(self, response: Response, **kwargs: Any) -> Any:
         map_data = response.xpath('//script[contains(text(), "google.maps.LatLng")]/text()').extract_first()
         coordinates = re.search(r"var myLatlng = new google\.maps\.LatLng\((.*)\)", map_data).group(1)
         lat, lon = coordinates.split(",")
