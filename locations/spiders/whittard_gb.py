8e6a2efaee37eef742039156567f3b49d1ed8343
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-12 20:13:05 +0200

    Add website

--- locations/spiders/whittard_gb.py
+++ locations/spiders/whittard_gb.py
@@ -30,7 +30,8 @@ class WhittardGBSpider(Spider):
             item["addr_full"] = clean_address(
                 location.xpath('.//div[contains(@class, "store-details")]//text()').getall()
             )
-            item["phone"] = location.xpath('.//div[@class="contacts contacts-desktop"]//text()').get()
+            item["phone"] = location.xpath('.//div[@class="contacts contacts-desktop"]//text()')[1].get()
+            item["website"] = location.xpath('.//a[contains(@href, "stores.whittard.co.uk")]//@href').get()
             yield item
 
         if "View More Stores" in response.text:
