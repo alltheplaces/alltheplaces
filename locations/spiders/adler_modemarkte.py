bce3a2a1be2129459622e0d822f8862d2b006bef
Karen Kerr <karenkerrgithub@gmail.com>
2025-06-13 10:27:45 +0100

    [Adler] Add spider

---
+++ locations/spiders/adler_modemarkte.py
@@ -0,0 +1,17 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class AdlerModemarkteSpider(SitemapSpider, StructuredDataSpider):
+    name = "adler_modemarkte"
+    item_attributes = {"brand": "Adler", "brand_wikidata": "Q358196"}
+    sitemap_urls = ["https://adler.de/wp-sitemap.xml"]
+    sitemap_follow = ["place"]
+    sitemap_rules = [(r"/standorte/[^/]+/$", "parse_sd")]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["branch"] = item.pop("name")
+        apply_category(Categories.SHOP_CLOTHES, item)
+        yield item
