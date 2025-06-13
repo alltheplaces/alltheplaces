90e61bb768fa7511e41fab80f8d0acec249762bf
Cj Malone <CjMalone@mail.com>
2025-06-13 09:56:50 +0100

    Clean name

--- locations/spiders/tommy_hr.py
+++ locations/spiders/tommy_hr.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from scrapy import Spider
 from scrapy.http import JsonRequest, Response
 
@@ -12,9 +14,20 @@ class TommyHRSpider(Spider):
     def start_requests(self):
         yield JsonRequest(url="https://spiza.tommy.hr/api/v2/shop/channels?itemsPerPage=500")
 
-    def parse(self, response: Response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json()["hydra:member"]:
             item = DictParser.parse(store["location"])
+            item["branch"] = (
+                item.pop("name")
+                .removeprefix("Tommy ")
+                .removeprefix("Hipermarket ")
+                .removeprefix("Supermarket ")
+                .removeprefix("supermarket ")
+                .removeprefix("Market ")
+                .removeprefix("Maximarket ")
+                .removeprefix("maximarket ")
+            )
+            item["ref"] = store["storeCode"]
             item["lat"] = store["position"]["latitude"]
             item["lon"] = store["position"]["longitude"]
             item["state"] = store["location"]["provinceName"]
