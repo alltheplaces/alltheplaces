45cd47ac0f0615cb03ceb8cb1ace700daf34aa26
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-13 10:48:17 +0530

    code updated to fix spider

--- locations/spiders/tommy_hr.py
+++ locations/spiders/tommy_hr.py
@@ -1,19 +1,22 @@
 from scrapy import Spider
-from scrapy.http import Response
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
 class TommyHRSpider(Spider):
-    # "businessHours" in the next data show incorrect opening hours for Sundays
     name = "tommy_hr"
     item_attributes = {"brand": "Tommy", "brand_wikidata": "Q12643718"}
-    start_urls = ["https://www.tommy.hr/_next/data/NQBnI1_5yBtg95innap3m/hr-HR/prodavaonice.json"]
+
+    def start_requests(self):
+        yield JsonRequest(url="https://spiza.tommy.hr/api/v2/shop/channels?itemsPerPage=500")
 
     def parse(self, response: Response):
-        for store in response.json()["pageProps"]["dehydratedState"]["queries"][1]["state"]["data"]["hydra:member"]:
-            store["address"]["street_address"] = store["address"].pop("street")
-            item = DictParser.parse(store)
+        for store in response.json()["hydra:member"]:
+            item = DictParser.parse(store["location"])
+            item["lat"] = store["position"]["latitude"]
+            item["lon"] = store["position"]["longitude"]
+            item["state"] = store["location"]["provinceName"]
             apply_category(Categories.SHOP_SUPERMARKET, item)
             yield item
