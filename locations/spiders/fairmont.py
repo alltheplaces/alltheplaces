3ea8dbeed3a8f0995d1d28d4f618d8709074c6ce
Cj Malone <CjMalone@mail.com>
2025-06-13 10:11:38 +0100

    Restore branding

--- locations/spiders/fairmont.py
+++ locations/spiders/fairmont.py
@@ -3,6 +3,7 @@ from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.spiders.central_england_cooperative import set_operator
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -13,5 +14,11 @@ class FairmontSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [("/hotels/[^/]+/[^/]+html$", "parse_sd")]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        if item["name"].startswith("Fairmont"):
+            item.update(self.FAIRMONT)
+        elif item["name"].endswith("A Fairmont Managed Hotel"):
+            set_operator(self.FAIRMONT, item)
+            item["nsi_id"] = "N/A"
+
         apply_category(Categories.HOTEL, item)
         yield item
