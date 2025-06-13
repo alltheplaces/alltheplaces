e3ad1a0d356a7e9bc1dd1451a9d9952080e6d6db
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-13 18:11:19 +0200

    Change to use Valero spider

--- locations/spiders/texaco_gb_ie.py
+++ locations/spiders/texaco_gb_ie.py
@@ -1,66 +1,14 @@
-import scrapy
+from locations.spiders.valero import ValeroSpider
 
-from locations.categories import Categories, Extras, Fuel, apply_category, apply_yes_no
-from locations.items import Feature
-
-uk_bbox = [-10, 49, 2, 61]
+uusa_bbox = [-10, 49, 2, 61]
 
 xstep = 5
 ystep = 3
 
 
-class TexacoGBIESpider(scrapy.Spider):
+class TexacoGBIESpider(ValeroSpider):
     name = "texaco_gb_ie"
     item_attributes = {"brand": "Texaco", "brand_wikidata": "Q775060"}
     allowed_domains = ["valero.com"]
 
-    def make_search(self, xmin, ymin, xmax, ymax):
-        return scrapy.FormRequest(
-            "https://locations.valero.com/en-us/Home/SearchForLocations",
-            formdata={
-                "NEBound_Lat": str(ymax),
-                "NEBound_Long": str(xmax),
-                "SWBound_Lat": str(ymin),
-                "SWBound_Long": str(xmin),
-            },
-            meta={
-                "dont_redirect": True,
-            },
-        )
-
-    def start_requests(self):
-        xs = list(range(uk_bbox[0], uk_bbox[2] + xstep, xstep))
-        ys = list(range(uk_bbox[1], uk_bbox[3] + ystep, ystep))
-        for xmin, xmax in zip(xs, xs[1:]):
-            for ymin, ymax in zip(ys, ys[1:]):
-                yield self.make_search(xmin, ymin, xmax, ymax)
-
-    def parse(self, response):
-        for row in response.json():
-            amenities = [detail["Description"] for detail in row["LocationDetails"]]
-            website = (
-                f"https://locations.valero.com/en-us/LocationDetails/Index/{row['DetailPageUrlID']}/{row['LocationID']}"
-            )
-            item = {
-                "ref": row["LocationID"],
-                "lat": row["Latitude"],
-                "lon": row["Longitude"],
-                "name": row["Name"],
-                "phone": row["Phone"],
-                "website": website,
-                "street_address": row["AddressLine1"],
-                "city": row["City"],
-                "state": row["State"],
-                "country": row["Country"],
-                "postcode": row["PostalCode"],
-                "opening_hours": "24/7" if "24 Hour" in amenities else None,
-            }
-
-            apply_category(Categories.FUEL_STATION, item)
-            apply_yes_no(Extras.ATM, item, "ATM" in amenities)
-            apply_yes_no(Extras.TOILETS, item, "Public Restroom" in amenities)
-            apply_yes_no(Extras.CAR_WASH, item, "Car Wash" in amenities)
-            apply_yes_no(Fuel.DIESEL, item, "Diesel" in amenities)
-            apply_yes_no(Fuel.E85, item, "E-85" in amenities)
-
-            yield Feature(**item)
+ 
