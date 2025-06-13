1c25e40e42c53fa45ba2dfd61677400e82d1a9c2
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-13 15:25:33 +0530

    Add opening hours

--- locations/spiders/rataplan_nl.py
+++ locations/spiders/rataplan_nl.py
@@ -5,6 +5,7 @@ from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.google_url import extract_google_position
+from locations.hours import DAYS_NL, OpeningHours, sanitise_day
 from locations.items import Feature
 from locations.pipelines.address_clean_up import clean_address
 
@@ -35,4 +36,13 @@ class RataplanNLSpider(SitemapSpider):
         item["name"] = self.item_attributes["brand"]
 
         apply_category(Categories.SHOP_SECOND_HAND, item)
+
+        item["opening_hours"] = OpeningHours()
+        for rule in response.xpath('//*[contains(text(),"Openingstijden")]/parent::*//table//tr'):
+            if day := sanitise_day(rule.xpath("./td[1]/text()").get(), DAYS_NL):
+                hours = rule.xpath("string(./td[2])").get("").replace("uur", "")
+                if "Gesloten" in hours.title():  # closed
+                    item["opening_hours"].set_closed(day)
+                else:
+                    item["opening_hours"].add_ranges_from_string(f"{day} {hours}")
         yield item
