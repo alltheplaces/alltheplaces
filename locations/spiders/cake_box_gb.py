908ff410f0d1833305688293e53c870ac710c8b6
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-13 15:01:51 +0200

    Update cake_box_gb.py

--- locations/spiders/cake_box_gb.py
+++ locations/spiders/cake_box_gb.py
@@ -1,23 +1,20 @@
-from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
+import re
+from typing import Iterable
 
+from scrapy import Selector
+
+from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
+from locations.items import Feature
 
 class CakeBoxGBSpider(AmastyStoreLocatorSpider):
     name = "cake_box_gb"
     item_attributes = {"brand": "Cake Box", "brand_wikidata": "Q110057905"}
     allowed_domains = ["www.cakebox.com"]
 
-    # def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
-    # data = response.xpath('//script[contains(text(), "jsonLocations")]/text()').get()
-    # data = re.sub(r"^.*jsonLocations: ", "", data, flags=re.DOTALL)
-    # data = re.sub(r",\s+imageLocations.*$", "", data, flags=re.DOTALL)
-    # jsondata = json.loads(data)
-    # for location in jsondata["items"]:
-    #    item = Feature()
-    #    item["ref"] = location["id"]
-    #    item["lat"] = location["lat"]
-    #    item["lon"] = location["lng"]
-    #    sel = location["popup_html"]
-    #    item["city"] = re.search(r"(?<=City: )[^<]+", sel).group(0)
-    #    item["street_address"] = re.search(r"(?<=Address: )[^<]+", sel).group(0)
-    #    item["postcode"] = re.search(r"(?<=Zip: )[^<]+", sel).group(0)
-    #    yield item
+    def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("Cake Box ")
+        sel = feature["popup_html"]
+        item["city"] = re.search(r"(?<=City: )[^<]+", sel).group(0)
+        item["street_address"] = re.search(r"(?<=Address: )[^<]+", sel).group(0)
+        item["postcode"] = re.search(r"(?<=Zip: )[^<]+", sel).group(0)
+        yield item
