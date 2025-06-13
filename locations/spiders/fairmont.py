983c29ac361a15afcf7692a9852f0a0777b0fdcc
Cj Malone <CjMalone@mail.com>
2025-06-13 10:06:46 +0100

    Fix import

--- locations/spiders/fairmont.py
+++ locations/spiders/fairmont.py
@@ -2,6 +2,7 @@ from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -11,6 +12,6 @@ class FairmontSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://www.fairmont.com/en.sitemap.xml"]
     sitemap_rules = [("/hotels/[^/]+/[^/]+html$", "parse_sd")]
 
-    def post_process_item(self, item, response: Response, ld_data: dict, **kwargs):
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         apply_category(Categories.HOTEL, item)
         yield item
