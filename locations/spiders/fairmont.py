cc76f60a38b71061f12fa3d788212440e7ef4d75
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-06-13 12:11:54 +0530

    Update fairmont.py

--- locations/spiders/fairmont.py
+++ locations/spiders/fairmont.py
@@ -9,7 +9,7 @@ class FairmontSpider(SitemapSpider, StructuredDataSpider):
     name = "fairmont"
     FAIRMONT = {"brand": "Fairmont", "brand_wikidata": "Q1393345"}
     sitemap_urls = ["https://www.fairmont.com/en.sitemap.xml"]
-    sitemap_rules = [("/hotels/[^/]+/[^/]+\.html$", "parse_sd")]
+    sitemap_rules = [("/hotels/[^/]+/[^/]+html$", "parse_sd")]
 
     def post_process_item(self, item, response: Response, ld_data: dict, **kwargs):
         apply_category(Categories.HOTEL, item)
