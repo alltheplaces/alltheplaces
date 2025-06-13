d1b1eac49ff1b775bb8a4f5b01bdf8cd10a6e203
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-13 15:35:47 +0530

    code updated to fix spider

--- locations/spiders/accenture.py
+++ locations/spiders/accenture.py
@@ -1,38 +1,33 @@
+import html
+import json
+
 import scrapy
 from scrapy.http import JsonRequest
 
-from locations.items import Feature
+from locations.dict_parser import DictParser
 
 
 class AccentureSpider(scrapy.Spider):
     name = "accenture"
     item_attributes = {"brand": "Accenture", "brand_wikidata": "Q29123313"}
-    allowed_domains = ["accenture.com"]
-    start_urls = ["https://www.accenture.com/us-en/about/location-index"]
+    start_urls = ["https://www.accenture.com/us-en/about/location"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
     no_refs = True
 
     def parse(self, response, **kwargs):
-        for country in response.xpath('//li[@class="cmp-link-teaser__list-item"]/a/text()').getall():
+        for country in response.xpath('//li[@class="rad-link-list__list-item"]/a/text()').getall():
             yield JsonRequest(
-                url=f"https://www.accenture.com/api/sitecore/LocationsHeroModule/GetLocation?query={country}&from=0&size=10000&language=en",
+                url=f"https://www.accenture.com/us-en/about/locations/office-details/jcr:content/root/container_main/locationhero_copy.result.html?query={country}&from=0&size=1500",
                 callback=self.parse_country,
             )
 
     def parse_country(self, response, **kwargs):
-        for store_data in response.json()["documents"]:
-            properties = {
-                "name": store_data["LocationName"],
-                "street_address": store_data["Address"],
-                "image": store_data["ExternalImageURL"],
-                "city": store_data["CityName"],
-                "state": store_data["StateCode"],
-                "postcode": store_data["PostalZipCode"],
-                "country": store_data["Country"],
-                "phone": store_data.get("ContactTel"),
-                "lat": float(store_data["Latitude"]),
-                "lon": float(store_data["Longitude"]),
-                "website": store_data.get("LocationURL"),
-            }
-
-            yield Feature(**properties)
+        for office in json.loads(html.unescape(response.text)):
+            item = DictParser.parse(office)
+            item.pop("state")
+            item["branch"] = item.pop("name")
+            item["name"] = self.item_attributes["brand"]
+            item["street_address"] = item.pop("addr_full")
+            item["postcode"] = office.get("postalZipCode", "")
+            item["phone"] = office.get("contactTel", "")
+            yield item
