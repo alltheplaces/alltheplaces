4a262420f0e2842af527b3c8fb85ac72f13bd0fa
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-13 18:27:00 +0200

    Try again

--- locations/spiders/texaco_gb_ie.py
+++ locations/spiders/texaco_gb_ie.py
@@ -1,12 +1,9 @@
 from locations.spiders.valero import ValeroSpider
 
-usa_bbox = [-10, 49, 2, 61]
-
-xstep = 5
-ystep = 3
-
 
 class TexacoGBIESpider(ValeroSpider):
     name = "texaco_gb_ie"
     item_attributes = {"brand": "Texaco", "brand_wikidata": "Q775060"}
     allowed_domains = ["valero.com"]
+
+    usa_bbox = [-10, 49, 2, 61]
