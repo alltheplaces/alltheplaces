879804cb64dd11e9fcc9676fe79f52a95f59c7cd
Ian Dees <ian.dees@gmail.com>
2025-06-14 20:49:08 -0500

    Switch back to proxy

--- locations/spiders/tesla.py
+++ locations/spiders/tesla.py
@@ -5,16 +5,13 @@ import scrapy
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.user_agents import FIREFOX_LATEST
 
 
 class TeslaSpider(scrapy.Spider):
     name = "tesla"
     item_attributes = {"brand": "Tesla", "brand_wikidata": "Q478214"}
     requires_proxy = True
-    custom_settings = {
-        "USER_AGENT": FIREFOX_LATEST,
-    }
+    download_timeout = 60
 
     def start_requests(self):
         yield scrapy.Request(
