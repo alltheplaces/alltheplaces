344ceede3ff4c4e7f35c6a770de7c4344d8d294c
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-15 15:03:36 +0200

    [Saltrock GB] Add Spider

---
+++ locations/spiders/saltrock_gb.py
@@ -0,0 +1,17 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class SaltrockGBSpider(JSONBlobSpider):
+    name = "saltrock_gb"
+    item_attributes = {"brand": "Saltrock", "brand_wikidata": "Q7406195"}
+    start_urls = ["https://stockist.co/api/v1/u24162/locations/all"]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        yield item
