9cb9d53002b5de91bfbd45201046d94c45d98404
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-16 17:09:50 +0530

    Convert spider into JSONBlob Spider

--- locations/spiders/mitsubishi_pl.py
+++ locations/spiders/mitsubishi_pl.py
@@ -1,41 +1,41 @@
 import re
-from typing import Any
+from typing import Iterable
 
 import chompjs
-from scrapy import Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
-from locations.dict_parser import DictParser
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class MitsubishiPLSpider(Spider):
+class MitsubishiPLSpider(JSONBlobSpider):
     name = "mitsubishi_pl"
     item_attributes = {"brand": "Mitsubishi", "brand_wikidata": "Q36033"}
     start_urls = ["https://www.mitsubishi.pl/dealerzy"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        raw_data = chompjs.parse_js_object(
+    def extract_json(self, response: Response) -> list[dict]:
+        return chompjs.parse_js_object(
             re.search(
                 r"dealerLocations\":(\[.*\])}\]\],\[\[\"\$\"",
                 response.xpath('//*[contains(text(),"latitude")]/text()').get().replace("\\", ""),
             ).group(1)
         )
-        for location in raw_data:
-            item = DictParser.parse(location)
-            item["street_address"] = item.pop("addr_full")
-            item["addr_full"] = location["full_address"]
-            item["name"] = location["dealer_name"]
 
-            services = location["services"]
-            if "SHOWROOM" in services:
-                apply_category(Categories.SHOP_CAR, item)
-                if "SERVICE" in services:
-                    apply_yes_no(Extras.CAR_REPAIR, item, True)
-            elif "SERVICE" in services:
-                apply_category(Categories.SHOP_CAR_REPAIR, item)
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = item.pop("addr_full")
+        item["addr_full"] = feature["full_address"]
+        item["name"] = feature["dealer_name"]
 
-            if isinstance(location["dealer_websites"], list):
-                item["website"] = location["dealer_websites"][0]["url"]
+        services = feature["services"]
+        if "SHOWROOM" in services:
+            apply_category(Categories.SHOP_CAR, item)
+            if "SERVICE" in services:
+                apply_yes_no(Extras.CAR_REPAIR, item, True)
+        elif "SERVICE" in services:
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
 
-            yield item
+        if isinstance(feature["dealer_websites"], list):
+            item["website"] = feature["dealer_websites"][0]["url"]
+
+        yield item
