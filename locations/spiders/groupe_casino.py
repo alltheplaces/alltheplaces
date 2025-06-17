dfb51be8ad5e128d34f3476cd49fcdd5f34dc9e6
Karen Kerr <karenkerrgithub@gmail.com>
2025-06-17 12:51:09 +0100

    [GroupeCasino] Update brands and categories

--- locations/spiders/groupe_casino.py
+++ locations/spiders/groupe_casino.py
@@ -13,16 +13,12 @@ from locations.pipelines.address_clean_up import merge_address_lines
 class GroupeCasinoSpider(Spider):
     name = "groupe_casino"
     brands = {
-        "30835": ("Casino Bio", "", None),
-        "30836": ("Casino Home", "", None),
-        "30837": ("Casino PauseDéj", "", None),
+        "30837": ("Casino PauseDéj", "Q89029249", Categories.SHOP_CONVENIENCE),
         "30838": ("Casino Shop", "Q89029601", Categories.SHOP_CONVENIENCE),
-        "30840": ("Casino Toutpres", "", None),
         "30842": ("Le Petit Casino", "Q89029249", Categories.SHOP_CONVENIENCE),
         "30843": ("Le Petit Casino", "Q89029249", Categories.SHOP_CONVENIENCE),  # Petit Casino
         "30844": ("Spar", "Q610492", Categories.SHOP_CONVENIENCE),
         "30845": ("Spar", "Q610492", Categories.SHOP_SUPERMARKET),
-        "30846": ("Casino", "Q89029184", Categories.SHOP_SUPERMARKET),
         "30847": ("Vival", "Q7937525", Categories.SHOP_CONVENIENCE),
     }
 
@@ -38,6 +34,8 @@ class GroupeCasinoSpider(Spider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json().get("businesses", []):
             if "Test" in location["name"].title():  # Dummy location data
+                continue
+            if "Camionnette" in location["name"].title():  # Mobile van store
                 continue
             item = DictParser.parse(location)
             item["street_address"] = merge_address_lines([item.pop("addr_full", ""), location.get("address2")])
