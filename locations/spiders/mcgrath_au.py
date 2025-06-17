cb4b6037b308ad1b4c84c996ce79dc77c4ce23a0
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-17 15:24:53 +0530

    Use playwright to fix http 429 error for Mcgrath AU Spider

--- locations/spiders/mcgrath_au.py
+++ locations/spiders/mcgrath_au.py
@@ -1,10 +1,14 @@
 import re
+from typing import Any
 
+import chompjs
 from scrapy import Request, Spider
+from scrapy.http import Response
 
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 from locations.react_server_components import parse_rsc
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
 class McgrathAUSpider(Spider):
@@ -14,9 +18,11 @@ class McgrathAUSpider(Spider):
         "brand": "McGrath",
     }
     start_urls = ["https://www.mcgrath.com.au/api/search/getOfficeSearchSuggestion"]
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
-    def parse(self, response):
-        for office in response.json()["data"]:
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for office in chompjs.parse_js_object(response.text)["data"]:
             slug = re.sub(r"\W+", "-", office["name"].strip()).lower()
             yield Request(
                 f"https://www.mcgrath.com.au/offices/{slug}-{office['id']}",
@@ -24,7 +30,7 @@ class McgrathAUSpider(Spider):
                 headers={"RSC": "1"},
             )
 
-    def parse_office(self, response):
+    def parse_office(self, response: Response, **kwargs: Any) -> Any:
         data = DictParser.get_nested_key(dict(parse_rsc(response.body)), "profile")
         if not data:
             return
