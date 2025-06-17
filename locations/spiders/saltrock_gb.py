23095424b665ec4e9ee8f248f738814047aa1153
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-06-17 14:05:33 +0100

    Convert to StockistSpider

--- locations/spiders/saltrock_gb.py
+++ locations/spiders/saltrock_gb.py
@@ -3,14 +3,14 @@ from typing import Iterable
 from scrapy.http import Response
 
 from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.storefinders.stockist import StockistSpider
 
 
-class SaltrockGBSpider(JSONBlobSpider):
+class SaltrockGBSpider(StockistSpider):
     name = "saltrock_gb"
     item_attributes = {"brand": "Saltrock", "brand_wikidata": "Q7406195"}
-    start_urls = ["https://stockist.co/api/v1/u24162/locations/all"]
+    key = "u24162"
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+    def parse_item(self, item: Feature, location: dict):
         item["branch"] = item.pop("name")
         yield item
