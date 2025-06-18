c123b70be7c2433a8104cb3c8c8e3c4cabfb4851
Cj Malone <CjMalone@mail.com>
2025-06-18 16:02:08 +0100

    Remove branch
    Just an address

--- locations/spiders/cuk_ubezpieczenia_pl.py
+++ locations/spiders/cuk_ubezpieczenia_pl.py
@@ -11,6 +11,6 @@ class CukUbezpieczeniaPLSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [(r"/placowki/([^/]+)$", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["branch"] = item.pop("name").removeprefix("CUK Ubezpieczenia - ")
+        item["name"] = None
         apply_category(Categories.OFFICE_INSURANCE, item)
         yield item
