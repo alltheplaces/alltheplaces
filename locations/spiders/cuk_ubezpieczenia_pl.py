1ad98d06608cf5945f0fd86f0e5d6a9541e76a60
Cj Malone <CjMalone@mail.com>
2025-06-18 16:00:25 +0100

    Scrape less pages

--- locations/spiders/cuk_ubezpieczenia_pl.py
+++ locations/spiders/cuk_ubezpieczenia_pl.py
@@ -7,10 +7,8 @@ from locations.structured_data_spider import StructuredDataSpider
 class CukUbezpieczeniaPLSpider(SitemapSpider, StructuredDataSpider):
     name = "cuk_ubezpieczenia_pl"
     item_attributes = {"brand": "CUK Ubezpieczenia", "brand_wikidata": "Q113230028"}
-    sitemap_urls = [
-        "https://cuk.pl/sitemap.offices.xml",
-    ]
-    sitemap_rules = [("/placowki/", "parse_sd")]
+    sitemap_urls = ["https://cuk.pl/sitemap.offices.xml"]
+    sitemap_rules = [(r"/placowki/([^/]+)$", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["branch"] = item["name"].replace("CUK Ubezpieczenia - ", "")
