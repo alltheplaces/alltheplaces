d9f7d957aedb961a7f2ef96c9e3a636616112bae
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-18 16:13:16 +0530

    Fix missing coordinates

--- locations/spiders/auto_nation_us.py
+++ locations/spiders/auto_nation_us.py
@@ -1,5 +1,10 @@
+import json
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.dict_parser import DictParser
+from locations.items import Feature
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -15,8 +20,8 @@ class AutoNationUSSpider(SitemapSpider, StructuredDataSpider):
     is_playwright_spider = True
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["lat"] = response.xpath('//input[@id="storeLatitude"]/@value').get()
-        item["lon"] = response.xpath('//input[@id="storeLongitude"]/@value').get()
-
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        store_details = json.loads(response.xpath('//script[@id="store-detail-state"]/text()').get())
+        item["lat"] = DictParser.get_nested_key(store_details, "latitude")
+        item["lon"] = DictParser.get_nested_key(store_details, "longitude")
         yield item
