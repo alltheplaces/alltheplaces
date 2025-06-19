67819238fb90fa08bcf865072c4c1d8cf32f3476
Karen Kerr <karenkerrgithub@gmail.com>
2025-06-19 08:17:04 +0100

    [Engbers] Add spider

---
+++ locations/spiders/engbers.py
@@ -0,0 +1,30 @@
+from typing import Iterable
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
+
+from locations.categories import Categories, apply_category
+from locations.hours import DAYS_DE, OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class EngbersSpider(JSONBlobSpider):
+    name = "engbers"
+    item_attributes = {"brand": "engbers", "brand_wikidata": "Q1290088"}
+    EMILIO_ADANI = {"brand": "emilio adani", "brand_wikidata": "Q123022474"}
+
+    def start_requests(self) -> Iterable[Request]:
+        yield JsonRequest(
+            url="https://engbersos-engbers.frontastic.live/frontastic/action/stores/getStoresByLocation",
+            data={"type": "storeFinder"},
+        )
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = feature.get("addInfo")
+        item["opening_hours"] = OpeningHours()
+        opening_hours = feature.get("openingHours", "").replace("<br>", " ").replace("Uhr<br>", ",")
+        item["opening_hours"].add_ranges_from_string(opening_hours, days=DAYS_DE)
+        if "Emilio Adani" in item["name"].title():
+            item.update(self.EMILIO_ADANI)
+        apply_category(Categories.SHOP_CLOTHES, item)
+        yield item
