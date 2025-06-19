59d90aa915fcd84646acbfcf1b076fd322a4ea74
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-19 12:26:05 +0530

    code updated to use jsonblob spider

--- locations/spiders/american_golf_gb.py
+++ locations/spiders/american_golf_gb.py
@@ -1,15 +1,15 @@
 import json
 import re
-from typing import Any
+from typing import Any, Iterable
 
 from scrapy.http import Response
-from scrapy.spiders import Spider
 
-from locations.dict_parser import DictParser
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class AmericanGolfGBSpider(Spider):
+class AmericanGolfGBSpider(JSONBlobSpider):
     name = "american_golf_gb"
     item_attributes = {"brand": "American Golf", "brand_wikidata": "Q62657494"}
     custom_settings = {"ROBOTSTXT_OBEY": False}
@@ -17,18 +17,17 @@ class AmericanGolfGBSpider(Spider):
         "https://www.americangolf.co.uk/en/find-stores/",
     ]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
+    def extract_json(self, response: Response, **kwargs: Any) -> Any:
         raw_data = json.loads(
             re.search(
                 r"channel\":(\[.*\])}},\"content",
                 response.xpath('//*[contains(text(),"address1")]/text()').get().replace("\\", ""),
             ).group(1)
         )
-        for location in raw_data:
-            location.update(location.pop("resources"))
-            print(location)
-            item = DictParser.parse(location)
-            item["branch"] = item.pop("name")
-            item["addr_full"] = location["listAddress"]
-            item["street_address"] = merge_address_lines([location["address1"], location["address2"]])
-            yield item
+        return raw_data
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        item["addr_full"] = feature["listAddress"]
+        item["street_address"] = merge_address_lines([feature["address1"], feature["address2"]])
+        yield item
