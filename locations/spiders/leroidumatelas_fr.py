5761a50095aafbef794cb9128a4837aff3b513ac
Karen Kerr <karenkerrgithub@gmail.com>
2025-06-23 10:08:19 +0100

    [LeRoiDuMatelas FR] Add spider

---
+++ locations/spiders/leroidumatelas_fr.py
@@ -0,0 +1,20 @@
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class LeroidumatelasFRSpider(CrawlSpider, StructuredDataSpider):
+    name = "leroidumatelas_fr"
+    item_attributes = {"brand": "Le Roi du Matelas", "brand_wikidata": "Q122931917"}
+    start_urls = ["https://www.leroidumatelas.fr/magasins/"]
+    rules = [Rule(LinkExtractor(allow=r"/magasin/"), callback="parse_sd")]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").replace("Mon magasin Le Roi du Matelas ","")
+        apply_category(Categories.SHOP_BED, item)
+        yield item
