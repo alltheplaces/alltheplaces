a26633398d3d879e11b2455519f966b5eb7aaaa3
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-06-23 14:50:01 +0100

    Rename class

--- locations/spiders/lolaliza_fr_be_lu.py
+++ locations/spiders/lolaliza_fr_be_lu.py
@@ -4,7 +4,7 @@ from locations.categories import Categories, apply_category
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class LolaLizaFRBELUSpider(SitemapSpider, StructuredDataSpider):
+class LolalizaFRBELUSpider(SitemapSpider, StructuredDataSpider):
     name = "lolaliza_fr_be_lu"
     item_attributes = {"brand": "LolaLiza", "brand_wikidata": "Q122840142"}
     sitemap_urls = ["https://stores.lolaliza.com/sitemap.xml"]
