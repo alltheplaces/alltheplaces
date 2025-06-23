26f7fcebb11b82c96456807551cca156f78b44e8
Cj Malone <CjMalone@mail.com>
2025-06-23 11:02:13 +0100

    Rename spider

--- locations/spiders/bertuccis_us.py
+++ locations/spiders/bertuccis_us.py
@@ -1,17 +1,19 @@
 import json
 import re
+from typing import Any
 
-import scrapy
+from scrapy import Spider
+from scrapy.http import Response
 
 from locations.dict_parser import DictParser
 
 
-class BertuccisSpider(scrapy.Spider):
-    name = "bertuccis"
+class BertuccisUSSpider(Spider):
+    name = "bertuccis_us"
     item_attributes = {"brand": "Bertucci's", "brand_wikidata": "Q4895917"}
     start_urls = ["https://www.bertuccis.com/locations/"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         data = json.loads(
             re.search(
                 r"features\":(\[.*\])};", response.xpath('//*[contains(text(),"address_line_1")]/text()').get()
