c73c236f77a185f677552d27de7e3049bd8e7019
Cj Malone <CjMalone@mail.com>
2025-06-23 10:53:12 +0100

    Rename spider

--- locations/spiders/big_lots_us.py
+++ locations/spiders/big_lots_us.py
@@ -1,16 +1,19 @@
-import scrapy
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
 
 from locations.dict_parser import DictParser
 
 
-class BigLotsSpider(scrapy.Spider):
-    name = "big_lots"
+class BigLotsUSSpider(Spider):
+    name = "big_lots_us"
     item_attributes = {"brand": "Big Lots", "brand_wikidata": "Q4905973"}
     start_urls = [
-        "https://core.service.elfsight.com/p/boot/?page=https%3A%2F%2Fbiglots.com%2Fstore-locator%2F&w=38636f5f-4115-4585-8c00-9e245fd92818",
+        "https://core.service.elfsight.com/p/boot/?page=https%3A%2F%2Fbiglots.com%2Fstore-locator%2F&w=38636f5f-4115-4585-8c00-9e245fd92818"
     ]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["data"]["widgets"]["38636f5f-4115-4585-8c00-9e245fd92818"]["data"]["settings"][
             "locations"
         ]:
