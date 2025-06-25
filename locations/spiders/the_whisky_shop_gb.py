0f8bfe65c64f744a31a96736b86a4e6bd5deecef
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-25 22:14:05 +0200

    [The Whisky Shop GB] Add Spider

---
+++ locations/spiders/the_whisky_shop_gb.py
@@ -0,0 +1,29 @@
+import json
+import re
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.json_blob_spider import JSONBlobSpider
+from locations.hours import DAYS_FULL, OpeningHours
+
+
+class TheWhiskyShopGBSpider(JSONBlobSpider):
+    name = "the_whisky_shop_gb"
+    item_attributes = {"brand": "The Whisky Shop", "brand_wikidata": "Q134400495"}
+    start_urls = ["https://www.whiskyshop.com/shops"]
+
+    def parse(self, response):
+        match = re.search(r'{"shops":([^\n]+),"styles":', response.text)
+        data = match.group(1)
+        json_data = json.loads(data)
+        for location in json_data.values():
+            item = DictParser.parse(location)
+            item["street_address"] = location["address_line_1"]
+            item["branch"] = item.pop("name").removeprefix("The Whisky Shop ")
+            oh = OpeningHours()
+            for day in DAYS_FULL:
+                myday=location["normal_hours"][day]
+                oh.add_range(day, myday["from"], myday["to"])
+            item["opening_hours"] = oh
+            apply_category(Categories.SHOP_ALCOHOL, item)
+            yield item
