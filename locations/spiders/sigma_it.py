32cad161c8b8c62e92b45529af23a6c5349c7e34
David Hicks <david@hicks.id.au>
2025-06-25 11:22:29 +1000

    [Sigma IT] Rewrite to improve tagging robustness (#13542)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/spiders/sigma_it.py
+++ locations/spiders/sigma_it.py
@@ -1,38 +1,56 @@
-from typing import Any
+import re
+from typing import Iterable
 
-from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+from scrapy.http import FormRequest, Request, Response
 
 from locations.categories import Categories, apply_category
-from locations.google_url import extract_google_position
 from locations.hours import CLOSED_IT, DAYS_IT, OpeningHours
 from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class SigmaITSpider(SitemapSpider):
+class SigmaITSpider(JSONBlobSpider):
     name = "sigma_it"
     item_attributes = {"brand": "Sigma", "brand_wikidata": "Q3977979"}
-    sitemap_urls = ["https://www.supersigma.com/store-sitemap.xml"]
-    sitemap_rules = [("/pdv/", "parse")]
+    allowed_domains = ["www.supersigma.com"]
+    start_urls = ["https://www.supersigma.com/wp-admin/admin-ajax.php"]
+    locations_key = ["map_args", "locations"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        item = Feature()
-        item["name"] = response.xpath("//title/text()").get()
-        item["street_address"] = response.xpath('//*[@class="fl fw fs16 "]/text()').get()
-        address2 = response.xpath('//*[@class="fl fw fs16 ml10 mt0 mb2"]').get()
-        if address2:
-            item["addr_full"] = ",".join([item["street_address"], address2])
-        item["ref"] = item["website"] = response.url
-        item["phone"] = response.xpath('//*[@class="fl fw fs16 "][2]/text()').get()
-        item["email"] = response.xpath('//*[@class="fl fw fs14 mb3 "]/text()').get()
-        extract_google_position(item, response)
+    def start_requests(self) -> Iterable[FormRequest]:
+        formdata = {
+            "action": "gmw_form_ajax_submission",
+            "submitted": "true",
+            "form_id": "2",
+            "form_values": "address%5B%5D=Napoli&distance=100000&units=metric&post%5B%5D=store&page=1&per_page=10000&lat=40.85177&lng=14.26812&swlatlng=&nelatlng=&form=2&action=fs",
+        }
+        yield FormRequest(url=self.start_urls[0], formdata=formdata)
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Request]:
+        item["ref"] = feature["location_id"]
+        branch_name = feature["location_name"]
+        if " di " in branch_name:
+            branch_name = branch_name.split(" di ", 1)[1]
+        elif " DI" in branch_name:
+            branch_name = branch_name.split(" DI ", 1)[1]
+        if " (" in branch_name:
+            branch_name = branch_name.split(" (", 1)[0]
+        item["branch"] = branch_name
+        item.pop("name", None)
+        item["street_address"] = item.pop("street")
+        item["website"] = "https://www.supersigma.com/?p=" + feature["object_id"]
         apply_category(Categories.SHOP_SUPERMARKET, item)
-        hours_string = " ".join(
-            filter(
-                None, map(str.strip, response.xpath('//span[contains(@class, "bkg_orari")]/span/span//text()').getall())
-            )
-        )
-        if hours_string:
+        yield Request(url=item["website"], meta={"item": item}, callback=self.parse_store_page)
+
+    def parse_store_page(self, response: Response) -> Iterable[Feature]:
+        item = response.meta["item"]
+        item["website"] = response.url  # Capture redirect destination
+        if m := re.findall(
+            r"\b\d{10}\b", " ".join(response.xpath('//span[contains(@class, "littleaddress")]/span[3]/text()').getall())
+        ):
+            item["phone"] = "; ".join(m)
+        item["email"] = response.xpath('//span[contains(@class, "littleaddress")]/span[4]/text()').get()
+        item["opening_hours"] = OpeningHours()
+        if hours_text := " ".join(response.xpath('//span[contains(@class, "bkg_orari")]/span/span//text()').getall()):
             item["opening_hours"] = OpeningHours()
-            item["opening_hours"].add_ranges_from_string(hours_string, days=DAYS_IT, closed=CLOSED_IT)
+            item["opening_hours"].add_ranges_from_string(hours_text, days=DAYS_IT, closed=CLOSED_IT)
         yield item
