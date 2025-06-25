7f5ca12718e96839718a3dea685c6cf4d419b3a0
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-06-24 21:37:22 -0400

    [Ace Hardware] Fix spider (#13055)
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/ace_hardware.py
+++ locations/spiders/ace_hardware.py
@@ -1,10 +1,9 @@
 from locations.storefinders.kibo import KiboSpider
-from locations.user_agents import BROWSER_DEFAULT
 
 
 class AceHardwareSpider(KiboSpider):
     name = "ace_hardware"
     item_attributes = {"brand": "Ace Hardware", "brand_wikidata": "Q4672981"}
     start_urls = ["https://www.acehardware.com/api/commerce/storefront/locationUsageTypes/SP/locations"]
-    user_agent = BROWSER_DEFAULT
+    user_agent = None
     requires_proxy = True
