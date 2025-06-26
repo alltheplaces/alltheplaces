44056188bba926ff0ceabbb511de67d209c1e65c
Cj Malone <CjMalone@mail.com>
2025-06-26 10:43:02 +0100

    Fix name type

--- locations/spiders/van_der_valk.py
+++ locations/spiders/van_der_valk.py
@@ -1,5 +1,7 @@
+from typing import Any
+
 from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
@@ -10,15 +12,17 @@ class VanDerValkSpider(Spider):
     item_attributes = {"brand": "Van der Valk", "brand_wikidata": "Q2802214"}
     start_urls = ["https://www.valk.com/ajax.cfm?event=ajax.get&type=content&name=proxy&action=apiToken"]
 
-    def parse(self, response, **kwargs):
-        token = response.json()["data"]["token"]
-        url = f"https://api.vandervalkonline.com/api/v2/hotels?token={token}"
-        yield JsonRequest(url=url, callback=self.parse_request)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        yield JsonRequest(
+            url="https://api.vandervalkonline.com/api/v2/hotels?token={}".format(response.json()["data"]["token"]),
+            callback=self.parse_request,
+        )
 
-    def parse_request(self, response, **kwargs):
+    def parse_request(self, response: Response) -> Any:
         for hotel in response.json().get("data"):
+            name = hotel.pop("name")
             item = DictParser.parse(hotel)
-            item["branch"] = hotel.get("name").get("full")
+            item["branch"] = name.get("full")
             if item["website"] and "http" not in item["website"]:
                 item["website"] = "https://" + item["website"]
             apply_category(Categories.HOTEL, item)
