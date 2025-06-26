f6348ee2a45bdae49f06316acdc7415d6436deac
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-06-26 18:22:48 +0530

    Use playwright and refactor code to fix Cbre Spider

--- locations/spiders/cbre.py
+++ locations/spiders/cbre.py
@@ -1,46 +1,69 @@
-import re
+from typing import Any
 
-import scrapy
+from scrapy.http import Response
+from scrapy.spiders import Spider
 
+from locations.google_url import extract_google_position
 from locations.items import Feature
+from locations.pipelines.address_clean_up import clean_address
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
-class CbreSpider(scrapy.Spider):
+class CbreSpider(Spider):
     name = "cbre"
     item_attributes = {"brand": "CBRE", "brand_wikidata": "Q1023013"}
-    allowed_domains = ["cbre.us"]
-    start_urls = ("https://www.cbre.us/people-and-offices",)
+    start_urls = ["https://www.cbre.com/offices"]
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
-    def parse(self, response):
-        urls = response.xpath('//li[@class="list-item list-item--second-level"]//a/@href').extract()
-        for url in urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_location)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for country_selector in response.xpath('//a[contains(@class, "countrySelector")]'):
+            country = country_selector.xpath(".//span/text()").get("").strip()
+            country_url = country_selector.xpath("./@href").get("").replace("people-and-offices", "offices")
+            yield response.follow(url=country_url, callback=self.parse_cities, cb_kwargs=dict(country=country))
 
-    def parse_location(self, response):
-        store_js = response.xpath('//div[@class="OfficeMapsWrapper google-map"]//script//text()').extract()
-        if store_js:
-            store = store_js[0]
-            storere = re.findall(r'\(("[^)]+")', store)
-            for item in storere:
-                item = item.replace("\\", "")
-                item = item.replace('"', "")
-                properties = {
-                    "ref": item.split(",")[-4].split("   ")[0] + "-" + item.split(",")[0],
-                    "name": item.split(",")[0],
-                    "addr_full": item.split(",")[-4].split("   ")[0],
-                    "state": item.split(", ")[-3].split(" ")[0],
-                    "city": item.split(",")[-4].split("   ")[-1],
-                    "postcode": item.split(", ")[-3].split(" ")[1],
-                    "country": "US",
-                    "phone": response.xpath(
-                        '//div[@class="numbers-wrapper__numbers numbers-wrapper__numbers--office tel"]//text()'
-                    )
-                    .extract_first()
-                    .replace("+", ""),
-                    "lat": float(item.split(",")[-2].replace('"', "")),
-                    "lon": float(item.split(",")[-1].replace('"', "")),
-                }
+    def parse_cities(self, response: Response, country: str) -> Any:
+        cities = response.xpath(
+            '//a[contains(@href, "offices") and not(contains(@class, "countrySelector"))]/@href'
+        ).getall()
+        if cities:
+            for city_url in cities:
+                yield response.follow(url=city_url, callback=self.parse_locations, cb_kwargs=dict(country=country))
+        else:
+            yield from self.parse_locations(response, country)
 
-                yield Feature(**properties)
-            else:
-                pass
+    def parse_locations(self, response: Response, country: str) -> Any:
+        primary_location = response.xpath('//*[contains(@class,"contactCardWrapper")]')
+        locations = response.xpath(
+            '//*[contains(@class,"listCards--office")]'
+        )  # multiple locations including primary one
+        if locations:
+            for location in locations:
+                item = Feature()
+                # Few duplicate locations from different urls are there, that's why we should make addr_full as ref.
+                item["ref"] = item["addr_full"] = clean_address(
+                    location.xpath('.//p[contains(@class, "officeAddress")]//text()').getall()
+                )
+                item["country"] = country
+                item["website"] = response.url
+                item["phone"] = location.xpath('.//a[contains(@href,"tel:")]/@href').get("")
+                item["extras"]["fax"] = (
+                    location.xpath('.//a[contains(@href,"fax:")]/@href').get("").replace("fax:", "").strip()
+                )
+                extract_google_position(item, location)
+                yield item
+        elif primary_location:  # only one location is present
+            item = Feature()
+            item["ref"] = item["addr_full"] = clean_address(
+                primary_location.xpath('.//p[contains(@class, "personDesignation")]//text()').getall()
+            )
+            item["country"] = country
+            item["website"] = response.url
+            item["phone"] = primary_location.xpath('.//a[contains(@href,"tel:")]/@href').get("")
+            item["extras"]["fax"] = (
+                primary_location.xpath('.//a[contains(@href,"fax:")]/@href').get("").replace("fax:", "").strip()
+            )
+            extract_google_position(item, primary_location)
+            yield item
+        else:
+            self.logger.warning(f"Failed to collect location info from url: {response.url}")
