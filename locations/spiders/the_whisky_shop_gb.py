7e8e2b8321de67ededf960927b84a8aca7de8639
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-26 10:06:23 +0200

    [The Whisky Shop GB] Add Spider (#13571)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: panasonic-mafia <35237910+panasonic-mafia@users.noreply.github.com>

---
+++ locations/spiders/the_whisky_shop_gb.py
@@ -0,0 +1,32 @@
+import json
+import re
+
+from scrapy import Spider
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_FULL, OpeningHours
+from locations.pipelines.address_clean_up import merge_address_lines
+
+
+class TheWhiskyShopGBSpider(Spider):
+    name = "the_whisky_shop_gb"
+    item_attributes = {"brand": "The Whisky Shop", "brand_wikidata": "Q134400495"}
+    start_urls = ["https://www.whiskyshop.com/shops"]
+
+    def parse(self, response):
+        match = re.search(r'{"shops":([^\n]+),"styles":', response.text)
+        data = match.group(1)
+        json_data = json.loads(data)
+        for location in json_data.values():
+            item = DictParser.parse(location)
+            item["street_address"] = merge_address_lines([location["address_line_1"], location["address_line_2"]])
+            item["branch"] = item.pop("name").removeprefix("The Whisky Shop ")
+            item["name"] = "The Whisky Shop"
+            oh = OpeningHours()
+            for day in DAYS_FULL:
+                myday = location["normal_hours"][day]
+                oh.add_range(day, myday["from"], myday["to"])
+            item["opening_hours"] = oh
+            apply_category(Categories.SHOP_ALCOHOL, item)
+            yield item
