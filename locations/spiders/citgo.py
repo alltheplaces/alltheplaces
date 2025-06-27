ad145d8a0dcf1d93c408646b8762d54bde6b5f37
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-06-27 10:31:18 +0000

    Refactor code using Sitemap and SD to fix Citgo Spider

--- locations/spiders/citgo.py
+++ locations/spiders/citgo.py
@@ -1,83 +1,22 @@
-import json
-import re
+from typing import Iterable
 
-import scrapy
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
 from locations.items import Feature
-
-SERVICE_VALUES = {"Yes": True, "No": False, "NR": None}
-TIME_PATTERN = re.compile(r"\d{2}:\d{2}")
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CitgoSpider(scrapy.Spider):
+class CitgoSpider(SitemapSpider, StructuredDataSpider):
     name = "citgo"
     item_attributes = {"brand": "Citgo", "brand_wikidata": "Q2974437"}
     allowed_domains = ["citgo.com"]
+    sitemap_urls = ["https://www.citgo.com/sitemap.xml"]
+    sitemap_rules = [
+        (r"/station-locator/locations/(\d+)", "parse_sd"),
+    ]
 
-    start_urls = ["https://citgo.com/locator/store-locators/store-locator"]
-
-    def parse(self, response):
-        if response.request.method == "GET":
-            # We first need to fetch the page normally and extract some required tokens from the form
-            yield scrapy.FormRequest(
-                self.start_urls[0],
-                method="POST",
-                formdata={
-                    "__CMSCsrfToken": response.css("#__CMSCsrfToken::attr(value)").get(),
-                    "__VIEWSTATE": response.css("#__VIEWSTATE::attr(value)").get(),
-                    "__CALLBACKID": "p$lt$WebPartZone3$PageContent$pageplaceholder$p$lt$WebPartZone3$Widgets$StoreLocator",
-                    "__CALLBACKPARAM": "66952|10000",
-                },
-            )
-        else:
-            # The first character of the response is `s`; after that it's JSON
-            result = json.loads(response.text[1:])
-
-            for location in result["Locations"]:
-                opening_hours = OpeningHours()
-                services = location["services"]
-
-                for hours_key in ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"]:
-                    open_time = location[f"hrs{hours_key}start"]
-                    close_time = location[f"hrs{hours_key}start"]
-
-                    if not (TIME_PATTERN.match(open_time) and TIME_PATTERN.match(close_time)):
-                        continue
-
-                    if int(open_time[0:2]) >= 24:
-                        open_time = f"{(int(open_time[0:2]) - 24):02d}{open_time[2:]}"
-
-                    if int(close_time[0:2]) >= 24:
-                        close_time = f"{(int(close_time[0:2]) - 24):02d}{close_time[2:]}"
-
-                    opening_hours.add_range(
-                        day=hours_key[:2].capitalize(),
-                        open_time=open_time,
-                        close_time=close_time,
-                        time_format="%H:%M",
-                    )
-
-                item = Feature(
-                    ref=location["number"],
-                    lon=location["longitude"],
-                    lat=location["latitude"],
-                    name=location["name"],
-                    addr_full=location["address"],
-                    city=location["city"],
-                    state=location["state"],
-                    postcode=location["zip"],
-                    country=location["country"],
-                    phone=location["phone"],
-                    opening_hours=opening_hours,
-                    extras={
-                        "atm": SERVICE_VALUES.get(services["atm"]),
-                        "car_wash": SERVICE_VALUES.get(services["carwash"]),
-                        "fuel:diesel": SERVICE_VALUES.get(services["diesel"]),
-                        "hgv": SERVICE_VALUES.get(services["truckstop"]),
-                        "wheelchair": SERVICE_VALUES.get(services["handicapaccess"]),
-                    },
-                )
-                apply_category(Categories.FUEL_STATION, item)
-                yield item
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        apply_category(Categories.FUEL_STATION, item)
+        yield item
