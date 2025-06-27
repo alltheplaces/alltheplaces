ce3d0e27f3fef8646867ca9b4a8f8062e3a34787
David Hicks <david@hicks.id.au>
2025-06-28 02:33:23 +1000

    [FDSN] Add spider for federated network of seismic stations
    Note: FDSN contains a list of "datacenters" which are repositories that
    different country/organisation seismic networks report to. Some of these
    repositories are dead (DNS timeout etc) or appear to refuse public
    connections, and thus will cause this spider to report some DNS and HTTP
    errors.

---
+++ locations/spiders/infrastructure/fdsn_seismic_stations.py
@@ -0,0 +1,58 @@
+from typing import Iterable
+from urllib.parse import urljoin
+
+from scrapy import Selector, Spider
+from scrapy.http import JsonRequest, Request, Response
+
+from locations.categories import Categories, MonitoringTypes, apply_category, apply_yes_no
+from locations.items import Feature
+
+
+class FdsnSeismicStationsSpider(Spider):
+    name = "fdsn_seismic_stations"
+    start_urls = ["https://www.fdsn.org/ws/datacenters/1/query"]
+    custom_settings = {"RETRY_ENABLED": False}
+
+    def start_requests(self) -> Iterable[JsonRequest]:
+        yield JsonRequest(url=self.start_urls[0], callback=self.parse_datacenters)
+
+    def parse_datacenters(self, response: Response) -> Iterable[JsonRequest]:
+        for datacenter in response.json()["datacenters"]:
+            for repository in datacenter["repositories"]:
+                for service in repository["services"]:
+                    if service["name"] == "fdsnws-station-1":
+                        yield Request(url=urljoin(service["url"], "query"), callback=self.parse_station_list)
+
+    def parse_station_list(self, response: Response) -> Iterable[Feature]:
+        document = Selector(response=response, type="xml")
+        document.register_namespace("s", "http://www.fdsn.org/xml/station/1")
+        for network in document.xpath('//s:Network'):
+            for station in network.xpath('./s:Station'):
+                if station.xpath('./@endDate').get():
+                    # Station has an end date and therefore no longer exists.
+                    # Ignore historical stations.
+                    continue
+                properties = {
+                    "ref": network.xpath('./@code').get() + "-" + station.xpath('./@code').get(),
+                    "name": station.xpath('./s:Site/s:Name/text()').get(),
+                    "lat": station.xpath('./s:Latitude/text()').get(),
+                    "lon": station.xpath('./s:Longitude/text()').get(),
+                }
+                if end_date := station.xpath('./@endDate').get():
+                    # Station has an end date and therefore has been shutdown/
+                    # removed. Extract the point as a historical feature as
+                    # there is historical data at this location which
+                    # continues to be a feature of interest.
+                    properties["extras"]["removed:man_made"] = "monitoring_station"
+                    properties["extras"]["removed:monitoring_station"] = "seismic_activity"
+                    properties["extras"]["end_date"] = station.xpath('./@endDate').get()
+                else:
+                    # Station has no end date and therefore is active and
+                    # continues to exist.
+                    apply_category(Categories.MONITORING_STATION, properties)
+                    apply_yes_no(MonitoringTypes.SEISMIC_ACTIVITY, properties, True)
+                properties["extras"]["ref:fdsn:network"] = network.xpath('./@code').get()
+                properties["extras"]["ref:fdsn:station"] = station.xpath('./@code').get()
+                properties["extras"]["ele"] = station.xpath('./s:Elevation/text()').get()
+                properties["extras"]["start_date"] = station.xpath('./@startDate').get()
+                yield Feature(**properties)
