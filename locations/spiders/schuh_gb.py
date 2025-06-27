79d026a7be4bc96626015d33eae637e9de68ae93
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-27 16:55:42 +0200

    [Schuh GB] Add Spider

---
+++ locations/spiders/schuh_gb.py
@@ -0,0 +1,21 @@
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
+
+
+class SchuhGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "schuh_gb"
+    item_attributes = {"brand": "Schuh", "brand_wikidata": "Q7432952"}
+    sitemap_urls = ["https://www.schuh.co.uk/googleSitemap.aspx"]
+    sitemap_rules = [(r"/stores/[^/]+/$", "parse")]
+    wanted_types = ["ShoeStore"]
+    requires_proxy = True
+    user_agent = BROWSER_DEFAULT
+    custom_settings = {
+        # somehow this actually seems to work
+        "RETRY_HTTP_CODES": [403],
+    }
