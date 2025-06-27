5ce02ff1ec88598d45229aebbd2c6cb9d44341aa
Cj Malone <CjMalone@mail.com>
2025-06-27 12:03:22 +0100

    Switch to StructuredDataSpider

--- locations/spiders/ats_euromaster_gb.py
+++ locations/spiders/ats_euromaster_gb.py
@@ -1,37 +1,30 @@
-import re
-
-import scrapy
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class AtsEuromasterGBSpider(scrapy.Spider):
+class AtsEuromasterGBSpider(SitemapSpider, StructuredDataSpider):
     name = "ats_euromaster_gb"
-    start_urls = ["https://www.atseuromaster.co.uk/centres"]
     item_attributes = {"brand": "ATS Euromaster", "brand_wikidata": "Q4654920"}
+    sitemap_urls = ["https://www.atseuromaster.co.uk/robots.txt"]
+    sitemap_rules = [(r"/centres/[^/]+/[^/]+/[^/]+$", "parse")]
+    wanted_types = ["AutoRepair"]
+    time_format = "%H:%M:%S"
 
-    def parse(self, response):
-        regions = response.xpath('//*[@class="list-group-item border-0 p-0"]//@href').getall()
-        for region in regions:
-            yield scrapy.Request(url=region, callback=self.parse_region)
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["facebook"] = None
+        item["branch"] = item.pop("name")
+        item["website"] = response.url
 
-    def parse_region(self, response):
-        cities = response.xpath('//*[@class="list-province w-100"]//li//@href').getall()
-        for shop in cities:
-            yield scrapy.Request(url=shop, callback=self.parse_shop)
-
-    def parse_shop(self, response):
-        item = Feature()
         item["ref"] = response.xpath('//*[@class="model-dealer-infos"]/@data-id').get()
-        item["branch"] = response.xpath('//*[@class="model-dealer-infos"]/@data-name').get()
-        item["street_address"] = response.xpath('//*[@class="model-dealer-infos"]/@data-address').get()
-        item["postcode"] = response.xpath('//*[@class="model-dealer-infos"]/@data-postcode').get()
-        item["city"] = response.xpath('//*[@class="model-dealer-infos"]/@data-city').get()
         item["state"] = response.xpath('//*[@class="model-dealer-infos"]/@data-region').get()
         item["extras"]["ref:google:place_id"] = response.xpath(
             '//*[@class="model-dealer-infos"]/@data-google-map-place-id'
         ).get()
-        item["lat"], item["lon"] = re.search(r"mapInit\('(.*)', '(.*)', true", response.text).groups()
+
         apply_category(Categories.SHOP_CAR_REPAIR, item)
+
         yield item
