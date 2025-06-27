317e947d3b2dca71d818f8418448eb902af7290c
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-27 14:26:20 +0530

    removed redundant line of code

--- locations/spiders/county_market.py
+++ locations/spiders/county_market.py
@@ -8,18 +8,12 @@ from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 
-SIEVE = ("View on Map", "Make My Preferred Store!")
-DAYS = r"M|Tue|Wed|Thu|Sat|Sun|Fri"
-AM_PM = r"am|pm|a.m.|p.m.|a|p"
-
 
 class CountyMarketSpider(SitemapSpider):
     name = "county_market"
     item_attributes = {"brand": "County Market", "brand_wikidata": "Q5177716"}
     sitemap_urls = ["https://www.mycountymarket.com/sitemap.xml"]
     sitemap_rules = [("/stores/", "parse")]
-
-    # start_urls = ("http://www.mycountymarket.com/shop/store-locator/",)
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         raw_data = list(chompjs.parse_js_objects(response.xpath('//*[@id="wpsl-js-js-extra"]/text()').get()))[1][
