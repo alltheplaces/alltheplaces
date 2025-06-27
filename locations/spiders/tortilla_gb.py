a88d1c3ae6d770b33f320973cc1040055c697245
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-27 15:55:26 +0200

    [Tortilla GB] Add Spider

---
+++ locations/spiders/tortilla_gb.py
@@ -0,0 +1,32 @@
+import re
+import json
+
+from typing import Iterable
+
+from locations.dict_parser import DictParser
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class TortillaGBSpider(JSONBlobSpider):
+    name = "tortilla_gb"
+    item_attributes = {"brand": "Tortilla", "brand_wikidata": "Q21006828"}
+    start_urls = ["https://www.tortilla.co.uk/restaurants"]
+
+    def parse(self, response):
+        match = re.search(r'\\"restaurants\\"\:([^\n]+)\}\],false\]\}\],\[\\"\$\\",', response.text)
+        data = match.group(1).replace('\\"','"')
+
+        json_data = json.loads(data)
+        for location in json_data:
+            print(location)
+            item = DictParser.parse(location["content"])
+            item["branch"]=item.pop("name")
+            item["ref"]=item["branch"]
+            if item["phone"]:
+                item["phone"].replace (" ", "")
+            item["website"] = "https://www.tortilla.co.uk/" + location["full_slug"]
+
+            apply_category(Categories.FAST_FOOD, item)
+
+            yield item
