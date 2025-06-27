549858eb310b3dad912c305671f10cc79ab9b22b
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-06-27 19:16:34 +0200

    Tidy up

--- locations/spiders/schuh_gb.py
+++ locations/spiders/schuh_gb.py
@@ -2,6 +2,7 @@ from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
 from locations.user_agents import BROWSER_DEFAULT
+from locations.hours import DAYS_EN, OpeningHours
 
 
 class SchuhGBSpider(SitemapSpider, StructuredDataSpider):
@@ -12,12 +13,26 @@ class SchuhGBSpider(SitemapSpider, StructuredDataSpider):
     wanted_types = ["ShoeStore"]
     requires_proxy = True
     user_agent = BROWSER_DEFAULT
-    custom_settings = {
-        # somehow this actually seems to work
-        "RETRY_HTTP_CODES": [403],
-    }
+
 
     def _get_sitemap_body(self, response):
         if response.url.split("?")[0].endswith(".aspx"):
             return response.body
         return super()._get_sitemap_body(response)
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["facebook"] = None
+        item["twitter"] = None
+        item["branch"] = item.pop("name")
+        item["ref"] = item["branch"]
+        oh = OpeningHours()
+        for opening_hour in ld_data["openingHours"]:
+            if "opens" in opening_hour:
+                oh.add_range(
+                    DAYS_EN[opening_hour["dayOfWeek"].removeprefix("https://schema.org/")],
+                    opening_hour["opens"][0:5],
+                    opening_hour["closes"][0:5],
+                )
+                item["opening_hours"] = oh
+        yield item
+    
