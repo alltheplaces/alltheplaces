dc9ae6174767549f8f5fa8076868e8d67f79df42
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-06-27 11:49:13 +0530

    code updated to fix spider

--- locations/spiders/coto_ar.py
+++ locations/spiders/coto_ar.py
@@ -1,33 +1,19 @@
 import scrapy
 
-from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
 
 
 class CotoARSpider(scrapy.Spider):
     name = "coto_ar"
     item_attributes = {"brand": "Coto", "brand_wikidata": "Q5175411"}
-    start_urls = ["http://www.coto.com.ar/mapassucursales/Sucursales/ListadoSucursalesPorDistancia.json.aspx"]
+    start_urls = ["https://www.coto.com.ar/sucursales/index.asp"]
 
     def parse(self, response, **kwargs):
-        results = response.json()
-        for data in results["results"]:
-            opening_hours = OpeningHours()
-            for days, field in [(DAYS[0:4], "hor_lu_a_ju"), (["Fr"], "hor_vi"), (["Sa"], "hor_sa"), (["Su"], "hor_do")]:
-                if times := data.get(field):
-                    if times == "Cerrado":
-                        continue
-                    times_split = times.split(" a ")
-                    if len(times_split) == 2:
-                        opening_hours.add_days_range(days, times_split[0], times_split[1])
-
-            yield Feature(
-                ref=data["id_suc"],
-                lat=data["latitud"],
-                lon=data["longitud"],
-                name="Coto " + data["desc_suc"],
-                country="AR",
-                phone=data["telefono"],
-                addr_full=data["direccion"],
-                opening_hours=opening_hours,
-            )
+        for store in response.xpath("//tbody/tr"):
+            item = Feature()
+            item["name"] = self.item_attributes["brand"]
+            item["branch"] = store.xpath('.//*[@data-label="Barrio"]/text()').get()
+            item["street_address"] = store.xpath('.//*[@data-label="Direccion"]/text()').get()
+            item["phone"] = store.xpath('.//*[@data-label="Telefono"]/text()').get()
+            item["ref"] = store.xpath('.//*[@data-label="Suc"]/text()').get()
+            yield item
