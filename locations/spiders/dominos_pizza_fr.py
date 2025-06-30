bb1b6d2b315a8eb9e2b2ec6761a2cc4dd9a3709b
Cj Malone <CjMalone@mail.com>
2025-06-30 17:02:50 +0100

    Clean name

--- locations/spiders/dominos_pizza_fr.py
+++ locations/spiders/dominos_pizza_fr.py
@@ -1,5 +1,9 @@
+from typing import Any
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.pipelines.address_clean_up import clean_address
 from locations.user_agents import BROWSER_DEFAULT
@@ -11,21 +15,19 @@ class DominosPizzaFRSpider(SitemapSpider):
     allowed_domains = ["dominos.fr"]
     user_agent = BROWSER_DEFAULT
     sitemap_urls = ["https://www.dominos.fr/sitemap.aspx"]
-    sitemap_rules = [
-        (
-            r"https:\/\/www\.dominos\.fr\/magasin\/\/?([-\w.]+)_(unknown|[\d]+)$",
-            "parse_store",
-        )
-    ]
+    sitemap_rules = [(r"https:\/\/www\.dominos\.fr\/magasin\/\/?([-\w.]+)_(unknown|[\d]+)$", "parse")]
 
-    def parse_store(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         properties = {
             "ref": response.url,
-            "name": response.xpath('//h2[@class="storetitle"]/text()').extract_first(),
-            "addr_full": clean_address(response.xpath('//a[@id="open-map-address"]/text()').extract()),
+            "branch": response.xpath('//h1[@class="storetitle"]/text()').get().removeprefix("Domino's Pizza "),
+            "addr_full": clean_address(response.xpath('//a[@id="open-map-address"]/text()').get()),
             "lat": response.xpath('//input[@id="store-lat"]/@value').get().replace(",", "."),
             "lon": response.xpath('//input[@id="store-lon"]/@value').get().replace(",", "."),
             "phone": response.xpath('//div[@class="store-phone"]/a/text()').get(),
             "website": response.url,
         }
+
+        apply_category(Categories.FAST_FOOD, properties)
+
         yield Feature(**properties)
