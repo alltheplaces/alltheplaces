fc177197f94dd5ae6ec5b9f7df9dbb214e0935b2
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-07-01 05:49:31 -0400

    [Dominos Pizza NL] Fix spider (#13604)
    Co-authored-by: Ehsan Firdaus <ehsan.firdaus@gmail.com>

--- locations/spiders/dominos_pizza_nl.py
+++ locations/spiders/dominos_pizza_nl.py
@@ -1,5 +1,7 @@
 import re
+from typing import Any, Iterable
 
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.items import Feature
@@ -11,25 +13,25 @@ class DominosPizzaNLSpider(SitemapSpider):
     item_attributes = {"brand": "Domino's", "brand_wikidata": "Q839466"}
     allowed_domains = ["dominos.nl"]
     sitemap_urls = ["https://www.dominos.nl/sitemap.aspx"]
-    url_regex = r"https:\/\/www\.dominos\.nl\/winkel\/([\w]+)-([\w]+)-([\d]+)$"
+    url_regex = r"https://www\.dominos\.nl/+winkel/+[\w]+-[\w]+-([\d]+)$"
     sitemap_rules = [(url_regex, "parse_store")]
     user_agent = BROWSER_DEFAULT
 
-    start_urls = ("https://www.dominos.nl/winkels",)
+    def sitemap_filter(self, entries: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]]:
+        for entry in entries:
+            entry["loc"] = re.sub(r"(\w)/+", r"\1/", entry["loc"])
+            yield entry
 
-    def parse_store(self, response):
-        match = re.match(self.url_regex, response.url)
-        ref = match.group(3)
-        country = match.group(1)
-        address_data = response.xpath('//a[@id="open-map-address"]/text()').extract()
+    def parse_store(self, response: Response) -> Any:
+        address_data = response.xpath('//a[@id="open-map-address"]/text()').getall()
         locality_data = re.search(r"(.*) ([A-Z]{2}) (.*)", address_data[1].strip())
         properties = {
-            "ref": ref.strip("/"),
-            "name": response.xpath('//h1[@class="storetitle"]/text()').extract_first(),
+            "ref": re.match(self.url_regex, response.url).group(1),
+            "branch": response.xpath('//*[@class="storetitle"]/text()').get("").removeprefix("Domino's Pizza "),
             "street_address": address_data[0].strip().strip(","),
-            "country": country,
             "lat": response.xpath('//input[@id="store-lat"]/@value').get().replace(",", "."),
             "lon": response.xpath('//input[@id="store-lon"]/@value').get().replace(",", "."),
+            "phone": response.xpath('//a[contains(@href, "tel:")]/@href').get(),
             "website": response.url,
         }
         if locality_data:
