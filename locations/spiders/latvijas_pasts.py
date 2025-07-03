7d306cc50c2465d3a2188bf94539c3b41aaf767e
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-07-03 14:50:24 +0530

    [Latvijas Pasts LV] Fix Spider (#13627)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

---
+++ locations/spiders/latvijas_pasts.py
@@ -0,0 +1,52 @@
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+from locations.spiders.central_england_cooperative import set_operator
+
+
+class LatvijasPastsSpider(Spider):
+    name = "latvijas_pasts"
+    LATVIJAS_PASTS = {"brand": "Latvijas Pasts", "brand_wikidata": "Q1807088"}
+
+    def start_requests(self):
+        yield JsonRequest(
+            url="https://mans.pasts.lv/api/public/addresses/service_location?type[]=7&type[]=6&type[]=1&type[]=2&search=&itemsPerPage=1500&page=1"
+        )
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["name"] = location["label"]
+            item["addr_full"] = location["readableAddress"]
+            item["extras"]["aa"] = str(location["type"])
+            if location["type"] in [1, 2]:
+                set_operator(self.LATVIJAS_PASTS, item)
+                apply_category(Categories.POST_OFFICE, item)
+            elif location["type"] == 6:
+                item["branch"] = item.pop("name")
+                item.update(self.LATVIJAS_PASTS)
+                apply_category(Categories.PARCEL_LOCKER, item)
+            elif location["type"] == 7:
+                apply_category(Categories.PARCEL_LOCKER, item)
+
+            try:
+                item["opening_hours"] = self.parse_opening_hours(location["workingHours"])
+            except:
+                self.logger.error("Error parsing opening hours")
+
+            yield item
+
+    def parse_opening_hours(self, work_hours: dict) -> OpeningHours:
+        oh = OpeningHours()
+        for day, value in work_hours.items():
+            if value == "-":
+                continue
+            for time in value.split(";"):
+                open_time, close_time = time.replace(".", ":").split("-")
+                oh.add_range(day=day, open_time=open_time.strip(), close_time=close_time.strip())
+        return oh
