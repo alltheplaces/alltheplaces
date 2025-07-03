bfbc6fea8081d078228129f466a14527b009bec1
Cj Malone <CjMalone@mail.com>
2025-07-03 12:59:18 +0100

    Fix website

--- locations/spiders/pizzeria_105_pl.py
+++ locations/spiders/pizzeria_105_pl.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -10,3 +12,7 @@ class Pizzeria105PLSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://105.pl/sitemap.xml"]
     sitemap_rules = [(r"https://105.pl/pizzeria-(.*)/", "parse_sd")]
     wanted_types = ["Place"]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["website"] = item["ref"] = response.url
+        yield item
