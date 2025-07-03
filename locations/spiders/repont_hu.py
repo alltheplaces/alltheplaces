e3afe1a864d5d1d8b0dab87036a4d8b0ae4f2817
Cj Malone <CjMalone@mail.com>
2025-07-03 13:32:33 +0100

    Resore hardcoded category

--- locations/spiders/repont_hu.py
+++ locations/spiders/repont_hu.py
@@ -1,8 +1,9 @@
-from typing import Iterable
+from typing import Any, Iterable
 
 import scrapy
-from scrapy.http import JsonRequest, Request
+from scrapy.http import JsonRequest, Request, Response
 
+from locations.categories import Categories, Vending, add_vending, apply_category
 from locations.dict_parser import DictParser
 
 
@@ -16,13 +17,15 @@ class RepontHUSpider(scrapy.Spider):
     }
     start_urls = ["https://map.mohu.hu/api/Map/GetAllPois"]
 
-    def parse(self, response) -> Iterable[Request]:
+    def parse(self, response: Response, **kwargs: Any) -> Iterable[Request]:
         for poi in response.json():
             yield JsonRequest(
                 f"https://map.mohu.hu/api/Map/GetWastePointDetails?id={poi['id']}", callback=self.parse_details
             )
 
-    def parse_details(self, response):
+    def parse_details(self, response: Response) -> Any:
         item = DictParser.parse(response.json())
         item["street_address"] = item.pop("addr_full")
+        apply_category(Categories.VENDING_MACHINE, item)
+        add_vending(Vending.BOTTLE_RETURN, item)
         yield item
