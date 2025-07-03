7de3cad3f08ccebd3ec4e05bca7e130bd015d0f7
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-07-03 14:56:51 +0530

    Add fuels and services

--- locations/spiders/avia_fr.py
+++ locations/spiders/avia_fr.py
@@ -1,10 +1,21 @@
+from enum import Enum
 from typing import Any
 
 from scrapy import Spider
 from scrapy.http import Response
 
-from locations.categories import Categories, apply_category
+from locations.categories import (
+    Access,
+    Categories,
+    Extras,
+    Fuel,
+    FuelCards,
+    PaymentMethods,
+    apply_category,
+    apply_yes_no,
+)
 from locations.dict_parser import DictParser
+from locations.items import Feature
 from locations.spiders.avia_de import AVIA_SHARED_ATTRIBUTES
 from locations.user_agents import BROWSER_DEFAULT
 
@@ -18,7 +29,7 @@ class AviaFRSpider(Spider):
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json()["data"]:
-            if any(store.get(key, "").upper() == "TRUE" for key in ["Station temporarily closed", "Fuel agency"]):
+            if any(store.get(key, "") == "TRUE" for key in ["Station temporarily closed", "Fuel agency"]):
                 continue
             store["lat"], store["lon"] = [store[key].replace(",", ".") for key in ["geo lat", "geo long"]]
             store["postcode"] = store.pop("ZIP Code")
@@ -35,7 +46,48 @@ class AviaFRSpider(Spider):
                     ],
                 )
             )
-            if store.get("XPRESS", "").upper() == "TRUE":
+            if store.get("XPRESS", "") == "TRUE":
                 item.update(self.AVIA_XPRESS)
             apply_category(Categories.FUEL_STATION, item)
+
+            self.apply_attribute(Fuel.ADBLUE, item, [store.get("AdBlue pump"), store.get("AdBlue can")])
+            self.apply_attribute(Fuel.E10, item, store.get("SP95/10"))
+            self.apply_attribute(Fuel.E5, item, [store.get("SP95/E5"), store.get("SP98/E5")])
+            self.apply_attribute(Fuel.DIESEL, item, [store.get("Diesel B7"), store.get("Diesel B10")])
+            self.apply_attribute(Fuel.E85, item, store.get("E85"))
+            self.apply_attribute(Fuel.BIODIESEL, item, store.get("HVO"))
+            self.apply_attribute(Fuel.CNG, item, store.get("CNG"))
+            self.apply_attribute(Fuel.LNG, item, store.get("LNG"))
+            self.apply_attribute(Fuel.LPG, item, store.get("LPG"))
+            self.apply_attribute(Fuel.LH2, item, store.get("H2"))
+            self.apply_attribute(Fuel.ENGINE_OIL, item, store.get("Lubricant"))
+            self.apply_attribute(Fuel.HEATING_OIL, item, store.get("Heating oil"))
+
+            self.apply_attribute(FuelCards.AVIA, item, store.get("AVIA Carte"))
+            self.apply_attribute(FuelCards.DKV, item, store.get("DKV"))
+            self.apply_attribute(FuelCards.UTA, item, store.get("UTA"))
+            self.apply_attribute(FuelCards.EUROWAG, item, store.get("EUROWAG"))
+            self.apply_attribute(FuelCards.ROUTEX, item, store.get("ROUTEX"))
+            self.apply_attribute(FuelCards.LOGPAY, item, store.get("LOGPAY"))
+            self.apply_attribute(FuelCards.MORGAN_FUELS, item, store.get("MORGAN FUELS"))
+            self.apply_attribute(FuelCards.E100, item, store.get("E100"))
+            self.apply_attribute(FuelCards.ESSO_NATIONAL, item, store.get("ESSO"))
+            self.apply_attribute(FuelCards.SHELL, item, [store.get("SHELL M"), store.get("SHELL S")])
+
+            self.apply_attribute(PaymentMethods.AMERICAN_EXPRESS, item, store.get("AMEX"))
+            self.apply_attribute(PaymentMethods.MASTER_CARD, item, store.get("MASTERCARD"))
+            self.apply_attribute(PaymentMethods.VISA, item, store.get("VISA"))
+
+            self.apply_attribute(Access.HGV, item, store.get("Truck station"))
+            self.apply_attribute(
+                Extras.CAR_WASH, item, [store.get("Gantry   wash"), store.get("Jetwash"), store.get("Roller washing")]
+            )
+            self.apply_attribute(Extras.SHOWERS, item, store.get("Shower"))
+            self.apply_attribute(Extras.TOILETS, item, store.get("Toilettes"))
+
             yield item
+
+    def apply_attribute(self, attribute: str | Enum, item: Feature, value: str | list[str]) -> None:
+        if not isinstance(value, list):
+            value = [value]
+        apply_yes_no(attribute, item, any(v == "TRUE" for v in value))
