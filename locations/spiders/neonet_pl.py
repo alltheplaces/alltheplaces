749f4ed481c075f77193d19106a7e37784933a67
Filip Czaplicki <github@starsep.com>
2025-07-03 14:15:56 +0200

    [Neonet PL] Fix spider (#13629)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/neonet_pl.py
+++ locations/spiders/neonet_pl.py
@@ -1,9 +1,10 @@
 import html
 import json
+from typing import Any
 
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
-from locations.dict_parser import DictParser
 from locations.linked_data_parser import LinkedDataParser
 
 
@@ -13,11 +14,11 @@ class NeonetPLSpider(SitemapSpider):
     sitemap_urls = ["https://sklepy.neonet.pl/sitemap.xml"]
     sitemap_rules = [(r"/polska/.*[0-9]$", "parse")]
 
-    def parse(self, response):
-        data = json.loads(
-            html.unescape(response.xpath('//*[@type = "application/ld+json"]').xpath("normalize-space()").get())
-        )[1]
-        item = DictParser.parse(data)
-        item["ref"] = data.get("@id")
-        item["opening_hours"] = LinkedDataParser.parse_opening_hours(data)
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        data = json.loads(html.unescape(response.xpath('//*[@type="application/ld+json"]/text()').get()))
+        for entry in data.get("@graph") or []:
+            if entry.get("@type") == "LocalBusiness":
+                item = LinkedDataParser.parse_ld(entry)
+                item["name"] = None
+                yield item
+                return
