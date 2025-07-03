a9ca1c5b800e1cad6714c4da5dc34dc88c47253f
Cj Malone <CjMalone@mail.com>
2025-07-03 13:06:19 +0100

    Switch to LD parser

--- locations/spiders/neonet_pl.py
+++ locations/spiders/neonet_pl.py
@@ -1,9 +1,10 @@
 import html
 import json
+from typing import Any
 
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
-from locations.dict_parser import DictParser
 from locations.linked_data_parser import LinkedDataParser
 
 
@@ -13,11 +14,9 @@ class NeonetPLSpider(SitemapSpider):
     sitemap_urls = ["https://sklepy.neonet.pl/sitemap.xml"]
     sitemap_rules = [(r"/polska/.*[0-9]$", "parse")]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         data = json.loads(
             html.unescape(response.xpath('//*[@type = "application/ld+json"]').xpath("normalize-space()").get())
-        )["@graph"][1]
-        item = DictParser.parse(data)
-        item["ref"] = data.get("@id")
-        item["opening_hours"] = LinkedDataParser.parse_opening_hours(data)
+        )
+        item = LinkedDataParser.parse_ld(data["@graph"][1])
         yield item
