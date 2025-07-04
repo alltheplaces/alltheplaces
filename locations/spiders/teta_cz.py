af85fb072e7805116668bf499ed443543aea0c86
Cj Malone <CjMalone@mail.com>
2025-07-04 11:58:25 +0100

    Collect contact details

--- locations/spiders/teta_cz.py
+++ locations/spiders/teta_cz.py
@@ -1,9 +1,13 @@
+from typing import Any
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.google_url import extract_google_position
 from locations.hours import DAYS_CZ, OpeningHours, sanitise_day
 from locations.items import Feature
+from locations.structured_data_spider import extract_email, extract_phone
 
 
 class TetaCZSpider(SitemapSpider):
@@ -12,12 +16,15 @@ class TetaCZSpider(SitemapSpider):
     sitemap_urls = ["https://www.tetadrogerie.cz/sitemap_index.xml"]
     sitemap_rules = [(r"/prodejny/\d+$", "parse")]
 
-    def parse(self, response, **kwargs):
-        result = response
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
-        item["addr_full"] = result.xpath("//h1/text()").get().strip()
+        item["addr_full"] = response.xpath("//h1/text()").get().strip()
         item["website"] = item["ref"] = response.url
+
         extract_google_position(item, response)
+        extract_email(item, response)
+        extract_phone(item, response.xpath('//div[@class="c-store-detail"]'))
+
         oh = OpeningHours()
         for rule in response.xpath('//*[@class="c-hours c-store-detail__main-opening-times"]//li'):
             day = sanitise_day(rule.xpath(".//text()").get(), DAYS_CZ)
