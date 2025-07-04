22dd4ae320d455fe37df946b9e0a8ac2be2fd080
Cj Malone <CjMalone@mail.com>
2025-07-04 07:54:56 +0100

    Collect coords

--- locations/spiders/super_star_car_wash_us.py
+++ locations/spiders/super_star_car_wash_us.py
@@ -1,20 +1,47 @@
+from typing import Any
+
+import chompjs
 from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+from scrapy.spiders import Spider
 
 from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class SuperStarCarWashUSSpider(SitemapSpider, StructuredDataSpider):
+class SuperStarCarWashUSSpider(Spider):
     name = "super_star_car_wash_us"
     item_attributes = {"brand": "Super Star Car Wash", "brand_wikidata": "Q132156104"}
-    sitemap_urls = ["https://www.superstarcarwashaz.com/robots.txt"]
-    sitemap_rules = [("/location/", "parse")]
-    wanted_types = ["AutoWash"]
-    time_format = "%I:%M %p"
+    start_urls = ["https://www.superstarcarwashaz.com/locations/"]
 
-    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
-        item["branch"] = item.pop("name")
-        apply_category(Categories.CAR_WASH, item)
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in chompjs.parse_js_object(
+            response.xpath('//script[contains(text(), "locationList")]/text()').get()
+        ).values():
+            if location["types"] == "Coming Soon":
+                continue
+
+            item = Feature()
+            item["ref"] = location["guid"]
+            item["lat"] = location["lat"]
+            item["lon"] = location["lon"]
+            item["branch"] = location["post_title"]
+            item["website"] = response.urljoin(location["post_name"])
+            item["addr_full"] = location["addr"]
+            item["phone"] = location["phone"]
+
+            try:
+                if hours := location.get("hours"):
+                    item["opening_hours"] = self.parse_opening_hours(hours)
+            except:
+                self.logger.error("Error parsing opening hours")
+
+            apply_category(Categories.CAR_WASH, item)
+
+            yield item
+
+    def parse_opening_hours(self, rules: list) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in rules:
+            oh.add_range(rule["day"], rule["opening_time"], rule["closing_time"], time_format="%I:%M %p")
+        return oh
