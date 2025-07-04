bff50abf926f1bac0ba960b9af15fa4e95de54a0
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-07-04 10:28:20 +0530

    code updated to use sitemap to fix spider

--- locations/spiders/save_a_lot_us.py
+++ locations/spiders/save_a_lot_us.py
@@ -1,47 +1,40 @@
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from typing import Any
+
+import chompjs
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories
 from locations.dict_parser import DictParser
-from locations.geo import country_iseadgg_centroids
 from locations.hours import OpeningHours
 
 
-class SaveALotUSSpider(Spider):
+class SaveALotUSSpider(SitemapSpider):
     name = "save_a_lot_us"
     item_attributes = {"brand": "Save-A-Lot", "brand_wikidata": "Q7427972", "extras": Categories.SHOP_SUPERMARKET.value}
-    allowed_domains = ["savealot.com"]
-    download_delay = 0.2
+    sitemap_urls = ["https://savealot.com/sitemap.xml"]
+    sitemap_rules = [(r"/stores/\d+$", "parse")]
 
-    def start_requests(self):
-        # API returns stores within 25 miles of a provided WGS84 coordinate.
-        for lat, lon in country_iseadgg_centroids(["US"], 24):
-            yield JsonRequest(
-                url=f"https://savealot.com/coupons/*?showStoreLocator=true&lat={lat}&lng={lon}&_data=root"
-            )
-
-    def parse(self, response):
-        if stores := response.json()["storeList"]:
-            if locations := stores.get("stores"):
-                for location in locations:
-                    location.update(location.pop("location"))
-                    item = DictParser.parse(location)
-                    item["branch"] = item.pop("name", None)
-                    item["website"] = "https://savealot.com/stores/" + location["storeId"]
-
-                    for phone_number in location["phoneNumbers"]:
-                        if phone_number["description"] == "Main":
-                            item["phone"] = phone_number["value"]
-                            break
-
-                    item["opening_hours"] = OpeningHours()
-                    for day_hours in location["hours"]["weekly"]:
-                        hours = day_hours["daily"]
-                        if "24_HOURS" in hours["type"]:
-                            item["opening_hours"].add_range(day_hours["day"], "00:00", "24:00", "%H:%M")
-                        else:
-                            item["opening_hours"].add_range(
-                                day_hours["day"], hours["open"]["open"], hours["open"]["close"], "%H:%M:%S"
-                            )
-
-                    yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        raw_data = chompjs.parse_js_object(
+            response.xpath('//*[contains(text(),"window.__remixContext")]/text()').get()
+        )["state"]["loaderData"]["routes/stores.$storeId._index"]["storeDetailsV2"]
+        raw_data.update(raw_data.pop("location"))
+        print(raw_data)
+        item = DictParser.parse(raw_data)
+        item["branch"] = item.pop("name")
+        item["website"] = response.url
+        item["opening_hours"] = OpeningHours()
+        for day_time in raw_data["hours"]["weekly"]:
+            day = day_time["day"]
+            if day_time["daily"]["type"] == "CLOSED":
+                continue
+            if day_time["daily"]["type"] == "OPEN_24_HOURS":
+                item["opening_hours"] = "24/7"
+            else:
+                open_time = day_time["daily"]["open"]["open"]
+                close_time = day_time["daily"]["open"]["close"]
+                item["opening_hours"].add_range(
+                    day=day, open_time=open_time, close_time=close_time, time_format="%H:%M:%S"
+                )
+        yield item
