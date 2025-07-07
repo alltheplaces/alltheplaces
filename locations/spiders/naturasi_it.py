d022b44c66e14d5355bccf080b4e2acb93e30fd9
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-07-07 17:05:56 +0530

    code refactored to fix spider

--- locations/spiders/naturasi_it.py
+++ locations/spiders/naturasi_it.py
@@ -1,16 +1,33 @@
-from scrapy.spiders import SitemapSpider
+from typing import Any
+from urllib.parse import urljoin
 
-from locations.structured_data_spider import StructuredDataSpider
+import scrapy
+from scrapy.http import Response
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_FULL, OpeningHours
 
 
-class NaturasiITSpider(SitemapSpider, StructuredDataSpider):
+class NaturasiITSpider(scrapy.Spider):
     name = "naturasi_it"
     item_attributes = {"brand": "NaturaSì", "brand_wikidata": "Q60840755"}
-    sitemap_urls = ["https://negozi.naturasi.it/sitemap/sitemap_1.xml"]
-    sitemap_rules = [(r"https://negozi.naturasi.it/.*", "parse_sd")]
+    start_urls = ["https://www.naturasi.it/ebsn/api/warehouse-locator/search"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if "city" in item:
-            item["city"] = item["city"].split("(")[0]
-
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["data"]["warehouses"]:
+            item = DictParser.parse(location)
+            item["ref"] = location["warehouseId"]
+            item["branch"] = item.pop("name").removeprefix("NaturaSì ")
+            item["lat"] = location["address"].get("latitude")
+            item["lon"] = location["address"].get("longitude")
+            item["website"] = urljoin("https://www.naturasi.it/punti-vendita/", location["slug"])
+            if location.get("metaData"):
+                item["phone"] = location.get("metaData").get("warehouse_locator").get("PHONE")
+            item["opening_hours"] = OpeningHours()
+            for day_time in location["serviceHours"]["default"]:
+                day = DAYS_FULL[day_time["beginWeekDay"] - 2]
+                open_time = day_time["beginHour"]
+                close_time = day_time["endHour"]
+                item["opening_hours"].add_range(day=day, open_time=open_time, close_time=close_time)
+            yield item
