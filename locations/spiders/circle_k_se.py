ceb225326bdba8f9d03e059aa12b9a9a20aee1ff
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-07-08 16:09:08 +0530

    Switch to CrawlSpider to fix Circle K SE Spider

--- locations/spiders/circle_k_se.py
+++ locations/spiders/circle_k_se.py
@@ -1,20 +1,16 @@
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
 from locations.google_url import extract_google_position
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class CircleKSESpider(SitemapSpider, StructuredDataSpider):
+class CircleKSESpider(CrawlSpider, StructuredDataSpider):
     name = "circle_k_se"
     item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010"}
-    sitemap_urls = ["https://www.circlek.se/stations/sitemap.xml"]
-    sitemap_rules = [("/station/circle-k-", "parse")]
-
-    def sitemap_filter(self, entries):
-        for entry in entries:
-            entry["loc"] = entry["loc"].replace("172.16.34.158", "www.circlek.se")
-            yield entry
+    start_urls = ["https://www.circlek.se/stations"]
+    rules = [Rule(LinkExtractor(allow=r"/station/circle-k-[-\w]+/?$"), callback="parse")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         extract_google_position(item, response)
