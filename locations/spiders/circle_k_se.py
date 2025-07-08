e87c3650982611afa60b485ec8dbd9bf143c2ebb
Cj Malone <CjMalone@mail.com>
2025-07-08 15:30:04 +0100

    Collect branch

--- locations/spiders/circle_k_se.py
+++ locations/spiders/circle_k_se.py
@@ -1,48 +1,31 @@
 from typing import Iterable
 
 from scrapy.http import Response
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, Fuel, apply_category, apply_yes_no
 from locations.google_url import extract_google_position
-from locations.hours import sanitise_day
 from locations.items import Feature
-from locations.linked_data_parser import LinkedDataParser
-from locations.structured_data_spider import StructuredDataSpider
+from locations.spiders.circle_k_dk import CircleKDKSpider
 
 
-class CircleKSESpider(CrawlSpider, StructuredDataSpider):
+class CircleKSESpider(CircleKDKSpider):
     name = "circle_k_se"
-    item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010"}
     start_urls = ["https://www.circlek.se/stations"]
-    rules = [Rule(LinkExtractor(allow=r"/station/circle-k-[-\w]+/?$"), callback="parse")]
-
-    def pre_process_data(self, ld_data: dict, **kwargs):
-        if any(sanitise_day(rule) for rule in ld_data.get("openingHours", [])):  # day without hours
-            ld_data.pop("openingHours")
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        item["name"] = self.item_attributes["brand"]
-        if not ld_data.get("openingHours"):
-            ld_data["openingHours"] = []
-            for rule in response.xpath('//*[@itemprop="openingHours"]'):
-                day = rule.xpath("./@content").get()
-                hours = rule.xpath("./text()").get("").replace("Open 24h", "00:00-23:59")
-                ld_data["openingHours"].append(f"{day} {hours}")
-        try:
-            item["opening_hours"] = LinkedDataParser.parse_opening_hours(ld_data)
-        except:
-            self.logger.error(f'Failed to parse opening hours: {ld_data.get("openingHours")}')
-            item["opening_hours"] = None
+        if item["name"].startswith("CIRCLE K TRUCK "):
+            item["branch"] = item.pop("name").removeprefix("CIRCLE K TRUCK ")
+            item["name"] = "Circle K Truck"
+        elif item["name"].startswith("CIRCLE K "):
+            item["branch"] = item.pop("name").removeprefix("CIRCLE K ")
 
         extract_google_position(item, response)
 
         fuels = [
             fuel.split("FeatureFuel")[-1]
-            for fuel in response.xpath('//img[contains(@src,"FeatureFuel")]/@src').getall()
+            for fuel in response.xpath('//img[contains(@src, "FeatureFuel")]/@src').getall()
         ]
-        charging_station = response.xpath('//img[contains(@src,"FeatureEVCharger")]/@src').get()
+        charging_station = response.xpath('//img[contains(@src, "FeatureEVCharger")]/@src').get()
         if not fuels and charging_station:
             apply_category(Categories.CHARGING_STATION, item)
         else:
@@ -60,4 +43,5 @@ class CircleKSESpider(CrawlSpider, StructuredDataSpider):
         apply_yes_no(Fuel.CNG, item, "CNG" in fuels)
         apply_yes_no(Fuel.BIODIESEL, item, "B100" in fuels)
         apply_yes_no(Fuel.BIODIESEL, item, "HVO100" in fuels)
+
         yield item
