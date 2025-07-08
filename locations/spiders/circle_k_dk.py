46c239c2988eeb8bec947ca886d0d7e126ea5f45
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-07-08 11:17:45 +0530

    code updated to fix spider

--- locations/spiders/circle_k_dk.py
+++ locations/spiders/circle_k_dk.py
@@ -1,17 +1,29 @@
-from scrapy.spiders import SitemapSpider
+import json
+from typing import Any
+
+import scrapy
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.google_url import extract_google_position
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
 
 
-class CircleKDKSpider(SitemapSpider, StructuredDataSpider):
+class CircleKDKSpider(scrapy.Spider):
     name = "circle_k_dk"
     item_attributes = {"brand": "Circle K", "brand_wikidata": "Q3268010"}
-    sitemap_urls = ["https://www.circlek.dk/stations/sitemap.xml"]
-    sitemap_rules = [("/station/circle-k-", "parse")]
+    start_urls = ["https://www.circlek.dk/station-search"]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        extract_google_position(item, response)
-        apply_category(Categories.FUEL_STATION, item)
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for station in json.loads(response.xpath('//*[@type="application/json"]/text()').get())["ck_sim_search"][
+            "station_results"
+        ].values():
+            if station["/sites/{siteId}"]["brand"] == "CIRCLEK":
+                station.update(station["/sites/{siteId}/addresses"].pop("PHYSICAL"))
+                station.update(station.pop("/sites/{siteId}/location"))
+                station.update(station.pop("/sites/{siteId}"))
+                item = DictParser.parse(station)
+                item["branch"] = item.pop("name").removeprefix("CIRCLE K ")
+                item["email"] = station["/sites/{siteId}/contact-details"]["emails"]["DN"][0]
+                item["phone"] = station["/sites/{siteId}/contact-details"]["phones"]["WOR"]
+                apply_category(Categories.FUEL_STATION, item)
+                yield item
