41673dd308229732610c63cca8b68b29e1d33d4b
panasonic-mafia <kolosov.yv@gmail.com>
2025-07-11 18:59:56 +0200

    chore: switch to newer endpoint

--- locations/spiders/mercedes_benz_group.py
+++ locations/spiders/mercedes_benz_group.py
@@ -1,11 +1,13 @@
-import scrapy
+from typing import Iterable
 
-from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
-from locations.hours import OpeningHours, sanitise_day
+from scrapy import Request
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class MercedesBenzGroupSpider(scrapy.Spider):
+class MercedesBenzGroupSpider(JSONBlobSpider):
     name = "mercedes_benz_group"
     available_countries = [
         "VN",
@@ -81,60 +83,35 @@ class MercedesBenzGroupSpider(scrapy.Spider):
         "ES",
         "ZA",
         "KR",
+        "SA",
     ]
 
-    brand_mapping = {
+    BRAND_MAPPING = {
         "Mercedes-Benz": {"brand": "Mercedes-Benz", "brand_wikidata": "Q36008"},
         "Maybach": {"brand": "Maybach", "brand_wikidata": "Q35989"},
         "Smart": {"brand": "Smart", "brand_wikidata": "Q156490"},
         "Fuso": {"brand": "Fuso", "brand_wikidata": "Q36033"},
         "Setra": {"brand": "Setra", "brand_wikidata": "Q938615"},
     }
-    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"x-apikey": "45ab9277-3014-4c9e-b059-6c0542ad9484"}}
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"x-apikey": "ce7d9916-6a3d-407a-b086-fea4cbae05f6"}}
+    locations_key = "dealers"
 
-    def start_requests(self):
+    def make_request(self, country: str, page: int = 1) -> Request:
+        url = "https://api.oneweb.mercedes-benz.com/dms-plus/v3/api/dealers/market?marketCode={}&page={}&size=25&includeFields=*&localeLanguage=true".format(
+            country, page
+        )
+        return Request(url, meta={"country": country})
+
+    def start_requests(self) -> Iterable[Request]:
         for country in self.available_countries:
-            url = f"https://api.oneweb.mercedes-benz.com/dlc-dms/v2/dealers/search?configurationExternalId=OneDLC_Dlp&country={country}&strictGeo=true&expand=false&localeLanguage=false&distance=2500km&includeApplicants=true&spellCheck=false&size=-1&searchProfileName=DLp_{country.lower()}"
-            yield scrapy.Request(url=url, callback=self.get_store_info)
+            yield self.make_request(country)
 
-    def get_store_info(self, response):
-        for store in response.json().get("results", []):
-            url = f"https://api.oneweb.mercedes-benz.com/dlc-dms/v2/dealers/search/byId?localeLanguage=false&id={store.get('baseInfo').get('externalId')}&fields=*"
-            yield scrapy.Request(url=url, callback=self.parse)
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = feature["outletId"]
+        item["state"] = feature.get("address", {}).get("region", {}).get("state")
+        yield item
 
-    def parse(self, response):
-        for data in response.json().get("results", []):
-            oh = OpeningHours()
-            for function in data.get("functions"):
-                if function.get("activityCode") != "SALES":
-                    continue
-                for day, hours in function.get("openingHours").items():
-                    day = sanitise_day(day)
-                    if not hours.get("open"):
-                        continue
-                    for periods in hours.get("timePeriods"):
-                        oh.add_range(
-                            day=day, open_time=periods.get("from"), close_time=periods.get("to"), time_format="%H:%M"
-                        )
-
-            item = DictParser.parse(data)
-            address_details = data.get("address")
-            base_info = data.get("baseInfo")
-            item["ref"] = base_info.get("externalId")
-            item["name"] = base_info.get("name1") or data.get("baseInfo").get("name2")
-            item["lat"] = address_details.get("latitude")
-            item["lon"] = address_details.get("longitude")
-            item["opening_hours"] = oh
-            if len(data.get("brands")) > 1:
-                item["brand"] = self.brand_mapping.get("Mercedes-Benz").get("brand")
-                item["brand_wikidata"] = self.brand_mapping.get("Mercedes-Benz").get("brand_wikidata")
-            else:
-                item["brand"] = data.get("brands")[0].get("brand").get("name")
-                item["brand_wikidata"] = self.brand_mapping.get(item["brand"]).get("brand_wikidata")
-
-            if isinstance(item["state"], dict):
-                item["state"] = item["state"].get("region")
-
-            apply_category(Categories.SHOP_CAR, item)
-
-            yield item
+        data = response.json()
+        current_page = data["page"]["number"]
+        if current_page < data["page"]["totalPages"]:
+            yield self.make_request(response.meta["country"], current_page + 1)
