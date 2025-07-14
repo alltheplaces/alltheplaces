bebf100786945a425488bf821e48e029090f16b4
panasonic-mafia <35237910+panasonic-mafia@users.noreply.github.com>
2025-07-14 11:59:44 +0200

    [Best Western] Sync brands with NSI (#13709)

--- locations/spiders/best_western.py
+++ locations/spiders/best_western.py
@@ -10,15 +10,36 @@ from locations.user_agents import BROWSER_DEFAULT
 
 class BestWesternSpider(scrapy.spiders.SitemapSpider):
     name = "best_western"
-    brands = [
-        ("Best Western Premier", "Q830334"),
-        ("Best Western Plus", "Q830334"),
-        ("Aiden by Best Western", "Q830334"),
-        ("Sure Hotel", "Q830334"),
-        ("Surestay Plus", "Q830334"),
-        ("Surestay", "Q830334"),
-        ("Best Western", "Q830334"),
-    ]
+
+    # Brand mapping is found in HTML of
+    # https://www.bestwestern.com/en_US/book/hotels-in-del-mar/best-western-premier-hotel-del-mar/propertyCode.05731.html
+    # under `data-brand-list`
+    BRANDS_MAPPING = {
+        "ADEN": ("Aiden", "Q135247220"),
+        "BEST": ("Best Western", "Q830334"),
+        "BWSC": ("BW Signature Collection", "Q135249021"),
+        "EXRE": ("Executive Residency", "Q135249087"),
+        "GLO": ("Glo", "Q135249046"),
+        "PLUS": ("Best Western Plus", "Q38623383"),
+        "PRMR": ("Best Western Premier", "Q135248460"),
+        "PMCL": ("BW Premier Collection", "Q135248830"),
+        "SSH": ("SureStay", "Q135246628"),
+        "SSSC": ("SureStay Collection", "Q135246644"),
+        "SSPL": ("SureStay Plus", "Q135246640"),
+        "SSES": ("SureStay Studio", "Q135246687"),
+        "SUH": ("Sure Hotel", "Q135246620"),
+        "SUPL": ("Sure Hotel", "Q135246628"),  # Sure Hotel Plus
+        "SUSC": ("Sure Hotel Collection", "Q135246644"),
+        "SUES": ("Sure Hotel Studio", "Q135246687"),
+        "SADI": ("Sadie", None),
+        "VIB": ("Vib", "Q135249054"),
+        "WHDI": ("WorldHotels", "Q135246666"),  # WorldHotels Distinctive
+        "WHEL": ("WorldHotels", "Q135246666"),  # WorldHotels Elite
+        "WHLX": ("WorldHotels", "Q135246666"),  # WorldHotels Luxury
+        "WHCC": ("WorldHotels", "Q135246666"),  # WorldHotels Crafted
+        "HMBW": ("@HOME", "Q135249100"),
+    }
+
     sitemap_urls = ["https://www.bestwestern.com/etc/seo/bestwestern/hotels-details.xml"]
     sitemap_rules = [(r"/en_US/book/[-\w]+/[-\w]+/propertyCode\.\d+\.html$", "parse_hotel")]
     custom_settings = {"USER_AGENT": BROWSER_DEFAULT, "ROBOTSTXT_OBEY": False}
@@ -33,24 +54,25 @@ class BestWesternSpider(scrapy.spiders.SitemapSpider):
 
         hotel = json.loads(html.unescape(hotel_details))
         summary = hotel["summary"]
-        for brand in self.brands:
-            if summary["name"].lower().startswith(brand[0].lower()):
-                item = DictParser.parse(summary)
-                item["brand"], item["brand_wikidata"] = brand
-                item["street_address"] = summary["address1"]
-                item["website"] = response.url
-                item["ref"] = summary["resort"]
-                item["extras"]["fax"] = summary["faxNumber"]
-                try:
-                    # It's a big hotel chain, worth a bit of work to get the imagery.
-                    image_path = hotel["imageCatalog"]["Media"][0]["ImagePath"]
-                    item["image"] = "https://images.bestwestern.com/bwi/brochures/{}/photos/1024/{}".format(
-                        summary["resort"], image_path
-                    )
-                except IndexError:
-                    pass
+        brand = self.BRANDS_MAPPING.get(summary["resortCategory"])
+        if not brand:
+            self.crawler.stats.inc_value(f"{self.name}/unmapped_brand/{summary['resortCategory']}")
+            brand = (None, None)
+        item = DictParser.parse(summary)
+        item["brand"], item["brand_wikidata"] = brand
+        item["street_address"] = summary["address1"]
+        item["website"] = response.url
+        item["ref"] = summary["resort"]
+        item["extras"]["fax"] = summary["faxNumber"]
+        try:
+            # It's a big hotel chain, worth a bit of work to get the imagery.
+            image_path = hotel["imageCatalog"]["Media"][0]["ImagePath"]
+            item["image"] = "https://images.bestwestern.com/bwi/brochures/{}/photos/1024/{}".format(
+                summary["resort"], image_path
+            )
+        except IndexError:
+            pass
 
-                apply_category(Categories.HOTEL, item)
+        apply_category(Categories.HOTEL, item)
 
-                yield item
-                return
+        yield item
