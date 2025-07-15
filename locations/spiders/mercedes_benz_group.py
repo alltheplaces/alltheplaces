6430c5c1a91dfd6111a024bc439b4f0ddbef140d
panasonic-mafia <kolosov.yv@gmail.com>
2025-07-15 14:26:34 +0200

    feat: add more departments and parse more attributes

--- locations/spiders/mercedes_benz_group.py
+++ locations/spiders/mercedes_benz_group.py
@@ -14,32 +14,34 @@ from locations.json_blob_spider import JSONBlobSpider
 class MercedesBenzGroupSpider(JSONBlobSpider):
     name = "mercedes_benz_group"
 
-    BRAND_MAPPING = {
-        "Mercedes-Benz": {"brand": "Mercedes-Benz", "brand_wikidata": "Q36008"},
-        "Mercedes-Benz Vans": {"brand": "Mercedes-Benz Vans", "brand_wikidata": "Q55383469"},
-        "Mercedes-Benz Trucks": {"brand": "Mercedes-Benz Trucks", "brand_wikidata": "Q36008"},
-        "Maybach": {"brand": "Maybach", "brand_wikidata": "Q35989"},
-        "Smart": {"brand": "Smart", "brand_wikidata": "Q156490"},
-        "Fuso": {"brand": "Fuso", "brand_wikidata": "Q36033"},
-        "Setra": {"brand": "Setra", "brand_wikidata": "Q938615"},
-    }
-
     MERCEDES_BENZ = {"brand": "Mercedes-Benz", "brand_wikidata": "Q36008"}
     MERCEDES_BENZ_VANS = {"brand": "Mercedes-Benz Vans", "brand_wikidata": "Q55383469"}
     MERCEDES_BENZ_TRUCKS = {"brand": "Mercedes-Benz Trucks", "brand_wikidata": "Q36008"}
+    MERCEDES_BENZ_BUS = {"brand": "Mercedes-Benz Buses", "brand_wikidata": "Q1427345"}
     SETRA = {"brand": "Setra", "brand_wikidata": "Q938615"}
+    FUSO = {"brand": "Fuso", "brand_wikidata": "Q1190247"}
+    SMART = {"brand": "Smart", "brand_wikidata": "Q156490"}
+    # OMNI_PLUS = {"brand": "OMNIplus", "brand_wikidata": "Q122922653"}
 
     DEPARTMENT_MAPPING = {
         ("Mercedes-Benz", "New Vehicle Sales", "Passenger Car"): (MERCEDES_BENZ, Categories.SHOP_CAR),
         ("Mercedes-Benz", "New Vehicle Sales", "Vans"): (MERCEDES_BENZ_VANS, Categories.SHOP_CAR),
         ("Mercedes-Benz", "New Vehicle Sales", "Trucks"): (MERCEDES_BENZ_TRUCKS, Categories.SHOP_TRUCK),
-        ("Mercedes-Benz", "New Vehicle Sales", "Busses"): (MERCEDES_BENZ, Categories.SHOP_BUS),
+        ("Mercedes-Benz", "New Vehicle Sales", "Busses"): (MERCEDES_BENZ_BUS, Categories.SHOP_BUS),
         ("Mercedes-Benz", "Repair & Maintenance", "Passenger Car"): (MERCEDES_BENZ, Categories.SHOP_CAR_REPAIR),
         ("Mercedes-Benz", "Repair & Maintenance", "Vans"): (MERCEDES_BENZ_VANS, Categories.SHOP_CAR_REPAIR),
         ("Mercedes-Benz", "Repair & Maintenance", "Trucks"): (MERCEDES_BENZ_TRUCKS, Categories.SHOP_TRUCK_REPAIR),
-        ("Mercedes-Benz", "Repair & Maintenance", "Busses"): (MERCEDES_BENZ, Categories.SHOP_BUS_REPAIR),
+        ("Mercedes-Benz", "Repair & Maintenance", "Busses"): (MERCEDES_BENZ_BUS, Categories.SHOP_BUS_REPAIR),
+        ("Mercedes-Benz", "Mercedes-Benz Rent", "Passenger Car"): (MERCEDES_BENZ, Categories.CAR_RENTAL),
+        ("Mercedes-Benz", "Mercedes-Benz Rent", "Vans"): (MERCEDES_BENZ_VANS, Categories.CAR_RENTAL),
         ("Setra", "New Vehicle Sales", "Busses"): (SETRA, Categories.SHOP_BUS),
         ("Setra", "Repair & Maintenance", "Busses"): (SETRA, Categories.SHOP_BUS_REPAIR),
+        ("FUSO", "New Vehicle Sales", "Trucks"): (FUSO, Categories.SHOP_TRUCK),
+        ("FUSO", "Repair & Maintenance", "Trucks"): (FUSO, Categories.SHOP_TRUCK_REPAIR),
+        ("smart", "New Vehicle Sales", "Passenger Car"): (SMART, Categories.SHOP_CAR),
+        ("smart", "Repair & Maintenance", "Passenger Car"): (SMART, Categories.SHOP_CAR_REPAIR),
+        ("smart", "Mercedes-Benz Rent", "Passenger Car"): (SMART, Categories.CAR_RENTAL),
+        # ("Mercedes-Benz", r"OMNIplus .*", "Busses"): (OMNI_PLUS, Categories.SHOP_BUS_REPAIR)
     }
     custom_settings = {"DEFAULT_REQUEST_HEADERS": {"x-apikey": "ce7d9916-6a3d-407a-b086-fea4cbae05f6"}}
     locations_key = "dealers"
@@ -57,19 +59,28 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["ref"] = feature["outletId"]
+        item["name"] = feature.get("legalName")
+        item["extras"]["alt_name"] = feature.get("nameAddition")
         item["state"] = feature.get("address", {}).get("region", {}).get("state")
+        item["lat"] = feature.get("address", {}).get("coordinates", {}).get("latitude")
+        item["lon"] = feature.get("address", {}).get("coordinates", {}).get("longitude")
         country = response.meta["country"]
         for service in feature.get("offeredServices", []):
+            if service.get("validity", {}).get("validity") == False:
+                continue
             key = (
                 service.get("brand", {}).get("name"),
                 service.get("service", {}).get("name"),
                 service.get("productGroup", {}).get("name"),
             )
-            self.crawler.stats.inc_value(f"{self.name}/services/{country}/{key[0]}/{key[1]}/{key[2]}")
+            self.crawler.stats.inc_value(f"{self.name}/services/{key[0]}/{key[1]}/{key[2]}")
             if key in self.DEPARTMENT_MAPPING:
                 branded_item = deepcopy(item)
+                branded_item["ref"] = "-".join([branded_item["ref"], country] + list(key)).replace(" ", "-")
                 brand, category = self.DEPARTMENT_MAPPING[key]
                 branded_item.update(brand)
+                self.parse_contacts(branded_item, service)
+                self.parse_hours(branded_item, service)
                 apply_category(category, branded_item)
                 yield branded_item
 
@@ -80,9 +91,11 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
 
     def parse_contacts(self, item: Feature, service: dict) -> None:
         communication = service.get("communication", {})
-        item["website"] = communication.get("INTERNET")
+        item["website"] = communication.get("INTERNET", "").strip().lower()
         item["email"] = communication.get("EMAIL")
         item["phone"] = communication.get("PHONE")
+        if item.get("website") and not item.get("website", "").startswith("http"):
+            item["website"] = "https://" + item["website"]
 
     def parse_hours(self, item: Feature, service: dict) -> None:
         if hours := service.get("openingHours"):
