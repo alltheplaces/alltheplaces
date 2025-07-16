c60e91f472e8d1772cb383850f570b3193949db7
panasonic-mafia <kolosov.yv@gmail.com>
2025-07-16 19:02:29 +0200

    feat: add OMNIplus

--- locations/spiders/mercedes_benz_group.py
+++ locations/spiders/mercedes_benz_group.py
@@ -14,6 +14,9 @@ from locations.json_blob_spider import JSONBlobSpider
 class MercedesBenzGroupSpider(JSONBlobSpider):
     name = "mercedes_benz_group"
 
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"x-apikey": "ce7d9916-6a3d-407a-b086-fea4cbae05f6"}}
+    locations_key = "dealers"
+
     MERCEDES_BENZ = {"brand": "Mercedes-Benz", "brand_wikidata": "Q36008"}
     MERCEDES_BENZ_VANS = {"brand": "Mercedes-Benz Vans", "brand_wikidata": "Q55383469"}
     MERCEDES_BENZ_TRUCKS = {"brand": "Mercedes-Benz Trucks", "brand_wikidata": "Q36008"}
@@ -21,9 +24,13 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
     SETRA = {"brand": "Setra", "brand_wikidata": "Q938615"}
     FUSO = {"brand": "Fuso", "brand_wikidata": "Q1190247"}
     SMART = {"brand": "Smart", "brand_wikidata": "Q156490"}
-    # OMNI_PLUS = {"brand": "OMNIplus", "brand_wikidata": "Q122922653"}
+    OMNI_PLUS = {"brand": "OMNIplus", "brand_wikidata": "Q122922653"}
 
-    DEPARTMENT_MAPPING = {
+    # Each location can have multiple services, each with a brand and product group.
+    # Easiest way to work with this data downstream is to yield a POI for each brand/category combination.
+    # This mapping is a way to manage this.
+    DEPARTMENT_MAPPING: dict[tuple[str, str, str], tuple[dict, Categories | dict]] = {
+        # key = (brand, service, productGroup):
         ("Mercedes-Benz", "New Vehicle Sales", "Passenger Car"): (MERCEDES_BENZ, Categories.SHOP_CAR),
         ("Mercedes-Benz", "New Vehicle Sales", "Vans"): (MERCEDES_BENZ_VANS, Categories.SHOP_CAR),
         ("Mercedes-Benz", "New Vehicle Sales", "Trucks"): (MERCEDES_BENZ_TRUCKS, Categories.SHOP_TRUCK),
@@ -41,10 +48,11 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
         ("smart", "New Vehicle Sales", "Passenger Car"): (SMART, Categories.SHOP_CAR),
         ("smart", "Repair & Maintenance", "Passenger Car"): (SMART, Categories.SHOP_CAR_REPAIR),
         ("smart", "Mercedes-Benz Rent", "Passenger Car"): (SMART, Categories.CAR_RENTAL),
-        # ("Mercedes-Benz", r"OMNIplus .*", "Busses"): (OMNI_PLUS, Categories.SHOP_BUS_REPAIR)
+        ("Mercedes-Benz", "OMNIplus BusPort", "Busses"): (OMNI_PLUS, Categories.SHOP_BUS_REPAIR),
+        ("Mercedes-Benz", "OMNIplus BusWorld", "Busses"): (OMNI_PLUS, Categories.SHOP_BUS_REPAIR),
+        ("Setra", "OMNIplus BusPort", "Busses"): (OMNI_PLUS, Categories.SHOP_BUS_REPAIR),
+        ("Setra", "OMNIplus BusWorld", "Busses"): (OMNI_PLUS, Categories.SHOP_BUS_REPAIR),
     }
-    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"x-apikey": "ce7d9916-6a3d-407a-b086-fea4cbae05f6"}}
-    locations_key = "dealers"
 
     def make_request(self, country: str, page: int = 1) -> Request:
         url = "https://api.oneweb.mercedes-benz.com/dms-plus/v3/api/dealers/market?marketCode={}&page={}&size=25&includeFields=*&localeLanguage=true".format(
@@ -59,8 +67,11 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["ref"] = feature["outletId"]
+
+        # Similar to how it's shown on the website
         item["name"] = feature.get("legalName")
         item["extras"]["alt_name"] = feature.get("nameAddition")
+
         item["state"] = feature.get("address", {}).get("region", {}).get("state")
         item["lat"] = feature.get("address", {}).get("coordinates", {}).get("latitude")
         item["lon"] = feature.get("address", {}).get("coordinates", {}).get("longitude")
@@ -73,10 +84,11 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
                 service.get("service", {}).get("name"),
                 service.get("productGroup", {}).get("name"),
             )
-            self.crawler.stats.inc_value(f"{self.name}/services/{key[0]}/{key[1]}/{key[2]}")
+            # Useful in development:
+            # self.crawler.stats.inc_value(f"{self.name}/services/{key[0]}/{key[1]}/{key[2]}")
             if key in self.DEPARTMENT_MAPPING:
                 branded_item = deepcopy(item)
-                branded_item["ref"] = "-".join([branded_item["ref"], country] + list(key)).replace(" ", "-")
+                branded_item["ref"] = self.make_ref(branded_item, key)
                 brand, category = self.DEPARTMENT_MAPPING[key]
                 branded_item.update(brand)
                 self.parse_contacts(branded_item, service)
@@ -107,3 +119,15 @@ class MercedesBenzGroupSpider(JSONBlobSpider):
                     for period in day.get("periods", []):
                         oh.add_range(day["day"], period["from"], period["until"])
             item["opening_hours"] = oh
+
+    def make_ref(self, item: Feature, key: tuple[str, str, str]) -> str:
+        country = item["country"]
+        brand, service, product_group = key
+
+        if service.startswith("OMNIplus"):
+            # Handle presence of multiple different OMNIplus services at the same location
+            # assuming deduplication pipeline will remove POIs with the same ref
+            service = "OMNIplus"
+            brand = "OMNIplus"
+
+        return "-".join([item["ref"], country, brand, service, product_group]).replace(" ", "-")
