d89fade5128012e73f7e69896fccd94da36c26e6
Karen Kerr <karenkerrgithub@gmail.com>
2025-07-18 11:38:14 +0100

    [Zeb] Add spider

---
+++ locations/spiders/zeb.py
@@ -0,0 +1,21 @@
+from locations.categories import apply_category, Categories
+from locations.json_blob_spider import JSONBlobSpider
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+
+
+class ZebSpider(JSONBlobSpider):
+    name = "zeb"
+    item_attributes = {"brand": "Zeb", "brand_wikidata": "Q121879599"}
+    start_urls = ["https://www.zeb.be/apps/fashion-society/locations"]
+    locations_key = "locations"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["phone"] = feature["address"]["phone"]
+        item["lat"] = feature["address"]["latitude"]
+        item["lon"] = feature["address"]["longitude"]
+        apply_category(Categories.SHOP_CLOTHES, item)
+        yield item
