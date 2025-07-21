dc502cc428aa4e304fcccfa86f72d29e9f2f67c7
Cj Malone <CjMalone@mail.com>
2025-07-21 08:45:36 +0100

    Inside the loop

--- locations/spiders/westside_in.py
+++ locations/spiders/westside_in.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 import scrapy
 from scrapy import Selector
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
@@ -11,13 +14,15 @@ class WestsideSpider(scrapy.Spider):
     no_refs = True
 
     def start_requests(self):
-        url = "https://customapp.trent-tata.com/api/custom/getstore-all?type=Westside&page=1"
-        yield scrapy.Request(url=url, headers={"Referer": "https://www.westside.com/"})
+        yield scrapy.Request(
+            url="https://customapp.trent-tata.com/api/custom/getstore-all?type=Westside&page=1",
+            headers={"Referer": "https://www.westside.com/"},
+        )
 
-    def parse(self, response, **kwargs):
-        item = Feature()
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         data = Selector(text=response.json().get("data"))
         for store in data.xpath('//*[@class = "storedetails"]'):
+            item = Feature()
             item["name"] = store.xpath("./h3/text()").get()
             item["addr_full"] = store.xpath(".//p/text()").get()
             item["phone"] = store.xpath(".//p[2]/text()").get()
