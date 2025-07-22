20af523a9dfc22445660d50fedc598403c69f5d6
Karen Kerr <karenkerrgithub@gmail.com>
2025-07-22 10:46:39 +0100

    [HotelBB] Add spider

---
+++ locations/spiders/hotel_bb.py
@@ -0,0 +1,36 @@
+import re
+
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
+
+
+class HotelBbSpider(SitemapSpider, StructuredDataSpider):
+    name = "hotel_bb"
+    BB_HOTELS = {"brand": "B&B Hotels", "brand_wikidata": "Q794939"}
+    sitemap_urls = ["https://www.hotel-bb.com/sitemap.xml"]
+    sitemap_rules = [("/en/hotel/", "parse_sd")]
+    user_agent = BROWSER_DEFAULT
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if item["name"].upper().startswith("B&B HOTEL"):
+            item["branch"] = re.sub("B&B HOTELS?", "", item.pop("name"), re.IGNORECASE).strip()
+            item.update(self.BB_HOTELS)
+
+        item["lat"], item["lon"] = response.xpath('//*[@class="location"]/@value').get().split(",")
+        item["phone"] = response.xpath('//div[@class="phone-contact external-phone"]/text()').get()
+        item["email"] = response.xpath('//div[contains(@class, "field--name-field-email")]/text()').get()
+
+        for link in response.xpath(
+            '//div[@class="combo-selectbox__item language"]//a[contains(@class, "language-link")]'
+        ):
+            item["extras"]["website:{}".format(link.xpath("@hreflang").get())] = response.urljoin(
+                link.xpath("@href").get()
+            )
+
+        apply_category(Categories.HOTEL, item)
+
+        yield item
