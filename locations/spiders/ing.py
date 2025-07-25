73f26c909e56f67fd6830002abdb2fb1c631542e
Karen Kerr <karenkerrgithub@gmail.com>
2025-07-25 15:26:17 +0100

    [ING] Fix opening times and categories

--- locations/spiders/ing.py
+++ locations/spiders/ing.py
@@ -37,12 +37,6 @@ class IngSpider(scrapy.Spider):
 
     def parse_store(self, response):
         store = response.json()
-        oh = OpeningHours()
-        for hours in store.get("timetable").get("office"):
-            for day_hours in hours.get("hours", []):
-                if day := sanitise_day(hours["day"]):
-                    oh.add_range(day, day_hours["open"], day_hours["closed"])
-
         item = Feature(
             {
                 "ref": store.get("id"),
@@ -55,18 +49,18 @@ class IngSpider(scrapy.Spider):
                 "phone": store.get("phone"),
                 "lat": response.meta.get("lat"),
                 "lon": response.meta.get("lon"),
-                "opening_hours": oh,
+                "branch": response.meta["name"].removeprefix("ING "),
             }
         )
-
-        if code := store.get("code"):
-            if code == "001":
-                item["branch"] = response.meta["name"].removeprefix("ING ")
-                apply_category(Categories.BANK, item)
-            else:
-                apply_category(Categories.ATM, item)
-        elif store["type"] == "ING-kantoor":
+        item["opening_hours"] = OpeningHours()
+        if timetable := store["timetable"].get("office"):
             apply_category(Categories.BANK, item)
-            item["branch"] = response.meta["name"]
+        elif timetable := store["timetable"].get("cashpoint"):
+            apply_category(Categories.ATM, item)
+
+        for hours in timetable:
+            for day_hours in hours.get("hours", []):
+                if day := sanitise_day(hours["day"]):
+                    item["opening_hours"].add_range(day, day_hours["open"], day_hours["closed"])
 
         yield item
