356e7fc4cd05f8e80e38dd5ce2bde31e4080a845
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-07-28 14:32:28 +0530

    code refactored to fix spider

--- locations/spiders/dominos_pizza_nl.py
+++ locations/spiders/dominos_pizza_nl.py
@@ -1,40 +1,51 @@
-import re
-from typing import Any, Iterable
+from urllib.parse import urljoin
 
-from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+import scrapy
+from scrapy.http import JsonRequest
 
-from locations.items import Feature
+from locations.dict_parser import DictParser
+from locations.geo import city_locations
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class DominosPizzaNLSpider(SitemapSpider):
+class DominosPizzaNLSpider(scrapy.Spider):
     name = "dominos_pizza_nl"
     item_attributes = {"brand": "Domino's", "brand_wikidata": "Q839466"}
-    allowed_domains = ["dominos.nl"]
-    sitemap_urls = ["https://www.dominos.nl/sitemap.aspx"]
-    url_regex = r"https://www\.dominos\.nl/+winkel/+[\w]+-[\w]+-([\d]+)$"
-    sitemap_rules = [(url_regex, "parse_store")]
-    user_agent = BROWSER_DEFAULT
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
 
-    def sitemap_filter(self, entries: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]]:
-        for entry in entries:
-            entry["loc"] = re.sub(r"(\w)/+", r"\1/", entry["loc"])
-            yield entry
+    def start_requests(self):
+        for city in city_locations("NL", 18000):
+            yield JsonRequest(
+                url=f"https://www.dominos.nl/dynamicstoresearchapi/getstoresfromquery?lon={city['longitude']}&lat={city['latitude']}",
+            )
 
-    def parse_store(self, response: Response) -> Any:
-        address_data = response.xpath('//a[@id="open-map-address"]/text()').getall()
-        locality_data = re.search(r"(.*) ([A-Z]{2}) (.*)", address_data[1].strip())
-        properties = {
-            "ref": re.match(self.url_regex, response.url).group(1),
-            "branch": response.xpath('//*[@class="storetitle"]/text()').get("").removeprefix("Domino's Pizza "),
-            "street_address": address_data[0].strip().strip(","),
-            "lat": response.xpath('//input[@id="store-lat"]/@value').get().replace(",", "."),
-            "lon": response.xpath('//input[@id="store-lon"]/@value').get().replace(",", "."),
-            "phone": response.xpath('//a[contains(@href, "tel:")]/@href').get(),
-            "website": response.url,
-        }
-        if locality_data:
-            properties["city"] = locality_data.group(1)
-            properties["postcode"] = locality_data.group(3)
-        yield Feature(**properties)
+    def parse(self, response, **kwargs):
+        for store in response.json()["PickupSearchStore"]:
+            store.update(store["locations"].pop(0))
+            store.update(store.pop("address"))
+            item = DictParser.parse(store)
+            item["branch"] = item.pop("name")
+            item["website"] = urljoin("https://www.dominos.nl", store["properties"]["storeUrl"])
+            for details in store["attributes"]:
+                if details["key"] == "streetName":
+                    item["street_address"] = details["value"]
+                elif details["key"] == "postCode":
+                    item["postcode"] = details["value"]
+            yield item
+
+    # def parse_store(self, response: Response) -> Any:
+    #     address_data = response.xpath('//a[@id="open-map-address"]/text()').getall()
+    #     locality_data = re.search(r"(.*) ([A-Z]{2}) (.*)", address_data[1].strip())
+    #     properties = {
+    #         "ref": re.match(self.url_regex, response.url).group(1),
+    #         "branch": response.xpath('//*[@class="storetitle"]/text()').get("").removeprefix("Domino's Pizza "),
+    #         "street_address": address_data[0].strip().strip(","),
+    #         "lat": response.xpath('//input[@id="store-lat"]/@value').get().replace(",", "."),
+    #         "lon": response.xpath('//input[@id="store-lon"]/@value').get().replace(",", "."),
+    #         "phone": response.xpath('//a[contains(@href, "tel:")]/@href').get(),
+    #         "website": response.url,
+    #     }
+    #     if locality_data:
+    #         properties["city"] = locality_data.group(1)
+    #         properties["postcode"] = locality_data.group(3)
+    #     yield Feature(**properties)
