alltheplaces/alltheplaces

commit aed3b661c3f97000133f35e9f921c51690e32235

[CoinFlip] Add spider for cryptocurrency ATMs (many countries) (#13696)
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

locations/storefinders/yext_answers.py
@@ -112,7 +112,15 @@ class YextAnswersSpider(Spider):
         if len(response.json()["response"]["results"]) == self.page_limit:
             yield self.make_request(response.meta["offset"] + self.page_limit)
 
-    def _parse_socials(self, item: Feature, location: dict):
+    @staticmethod
+    def parse_socials(item: Feature, location: dict) -> None:
+        """
+        Parse contact and social media fields of information from the
+        supplied dictionary and add to the supplied item (Feature).
+        :param item: item to add contact and social media information to.
+        :param location: dictionary to parse information from.
+        :returns: None
+        """
         phones = []
         for phone_type in ["localPhone", "mainPhone", "mobilePhone"]:
             if phone := location.get(phone_type):
@@ -151,7 +159,15 @@ class YextAnswersSpider(Spider):
             else:
                 item["extras"]["website:menu"] = menu_url_dict.get("url")
 
-    def parse_opening_hours(self, hours: dict, **kwargs: Any) -> str | None:
+    @staticmethod
+    def parse_opening_hours(hours: dict) -> OpeningHours | None:
+        """
+        Parse opening hours from the supplied dictionary and return an
+        OpeningHours object.
+        :param hours: dictionary provided by a Yext API.
+        :returns: OpeningHours object if hours could be parsed, or None if the
+                  supplied hours dictionary cnanot be parsed.
+        """
         if not hours:
             return None
         oh = OpeningHours()
@@ -165,8 +181,7 @@ class YextAnswersSpider(Spider):
                 continue
             for time in rule["openIntervals"]:
                 oh.add_range(day, time["start"], time["end"])
-
-        return oh.as_opening_hours()
+        return oh
 
     def parse_payment_methods(self, location: dict, item: Feature) -> None:
         if payment_methods := location.get("paymentOptions"):

locations/spiders/coinflip.py
@@ -1,0 +1,50 @@
+from typing import Iterable
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.yext_locations import YextLocationsSpider
+
+
+class CoinflipSpider(YextLocationsSpider):
+    name = "coinflip"
+    item_attributes = {"brand": "CoinFlip", "brand_wikidata": "Q109850256"}
+    api_key = "7ef064ccd105050c8cab5880d6bfefa7"
+    api_version = "20231114"
+
+    def parse_item(self, location: dict, item: Feature) -> Iterable[Feature]:
+        item.pop("name", None)
+        item.pop("email", None)
+
+        apply_category(Categories.ATM, item)
+        item["extras"]["currency:XBT"] = "yes"
+        match item.get("country"):
+            case "AU":
+                item["extras"]["currency:AUD"] = "yes"
+            case "BR":
+                item["extras"]["currency:BRL"] = "yes"
+            case "CA":
+                item["extras"]["currency:CAD"] = "yes"
+            case "ES" | "IT":
+                item["extras"]["currency:EUR"] = "yes"
+            case "MX":
+                item["extras"]["currency:MXN"] = "yes"
+            case "NZ":
+                item["extras"]["currency:NZD"] = "yes"
+            case "PA" | "PR" | "US":
+                item["extras"]["currency:USD"] = "yes"
+            case "ZA":
+                item["extras"]["currency:ZAR"] = "yes"
+            case None:
+                pass
+            case _:
+                self.logger.warning(
+                    "ATM is located in country '{}' for which the local currency is undefined by this spider. The spider should be updated to map a currency for this country.".format(
+                        item.get("country")
+                    )
+                )
+        item["extras"]["cash_in"] = "yes"
+        if location.get("c_oneWayTwoWay") == "ONE-WAY_ATM":
+            item["extras"]["cash_out"] = "no"
+        else:
+            item["extras"]["cash_out"] = "yes"
+        yield item

locations/storefinders/yext_locations.py
@@ -1,0 +1,50 @@
+from typing import Iterable
+from urllib.parse import urlencode
+
+from scrapy.http import JsonRequest, Response
+from scrapy.spiders import Spider
+
+from locations.dict_parser import DictParser
+from locations.items import Feature
+from locations.storefinders.yext_answers import YextAnswersSpider
+
+
+class YextLocationsSpider(Spider):
+    dataset_attributes = {"source": "api", "api": "yext"}
+
+    endpoint: str = "https://cdn.yextapis.com/v2/accounts/me/content/locations"
+    api_key: str = ""
+    api_version: str = "20231114"
+    page_limit: int = 50
+
+    def make_request(self, page_token: str) -> JsonRequest:
+        return JsonRequest(
+            url="{}?{}".format(
+                self.endpoint,
+                urlencode(
+                    {
+                        "api_key": self.api_key,
+                        "v": self.api_version,
+                        "limit": str(self.page_limit),
+                        "pageToken": page_token,
+                    }
+                ),
+            )
+        )
+
+    def start_requests(self) -> Iterable[JsonRequest]:
+        yield self.make_request("")
+
+    def parse(self, response: Response) -> Iterable[Feature | JsonRequest]:
+        for location in response.json()["response"]["docs"]:
+            item = DictParser.parse(location)
+            item["branch"] = location.get("geomodifier")
+            YextAnswersSpider.parse_socials(item, location)
+            item["opening_hours"] = YextAnswersSpider.parse_opening_hours(location.get("hours"))
+            yield from self.parse_item(location, item) or []
+
+        if next_page_token := response.json()["response"].get("nextPageToken"):
+            yield self.make_request(next_page_token)
+
+    def parse_item(self, location: dict, item: Feature) -> Iterable[Feature]:
+        yield item

commit 59a35338242be474a9e26934b93918ef63bf5e79

[YextAnswers] Fix opening_hours parsing
Was broken with commit aed3b661c3f97000133f35e9f921c51690e32235.

locations/storefinders/yext_answers.py
@@ -98,11 +98,13 @@ class YextAnswersSpider(Spider):
 
             item["opening_hours"] = self.parse_opening_hours(location.get("hours"))
             if delivery_hours := location.get("deliveryHours"):
-                item["extras"]["opening_hours:delivery"] = self.parse_opening_hours(delivery_hours)
+                item["extras"]["opening_hours:delivery"] = self.parse_opening_hours(delivery_hours).as_opening_hours()
             if happy_hours := location.get("happyHours"):
-                item["extras"]["happy_hours"] = self.parse_opening_hours(happy_hours)
+                item["extras"]["happy_hours"] = self.parse_opening_hours(happy_hours).as_opening_hours()
             if drive_through_hours := location.get("driveThroughHours"):
-                item["extras"]["opening_hours:drive_through"] = self.parse_opening_hours(drive_through_hours)
+                item["extras"]["opening_hours:drive_through"] = self.parse_opening_hours(
+                    drive_through_hours
+                ).as_opening_hours()
 
             self.parse_payment_methods(location, item)
             self.parse_google_attributes(location, item)