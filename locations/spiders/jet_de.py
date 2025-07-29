e926418d14cbdce7ba32eabbc7b5efa13b9d7a33
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-07-29 14:33:50 +0530

    code updated to fix spider

--- locations/spiders/jet_de.py
+++ locations/spiders/jet_de.py
@@ -1,31 +1,20 @@
-import json
-import re
-
 import scrapy
-from scrapy import Selector
 
 from locations.categories import Categories, apply_category
-from locations.items import Feature
+from locations.dict_parser import DictParser
 
 
 class JetDESpider(scrapy.Spider):
     name = "jet_de"
     item_attributes = {"brand": "JET", "brand_wikidata": "Q568940"}
-    start_urls = ["https://www.jet.de/tankstellen-suche"]
+    start_urls = [
+        "https://www.jet.de/api/v1/stations_within_bounds?n=85.0511287798066&e=180&s=-67.90083807146054&w=-180&withCardHtml=1"
+    ]
 
     def parse(self, response, **kwargs):
-        for location in json.loads(re.search(r"stationsData: (\[.+\])\n}", response.text).group(1)):
-            if "fuel" not in location["features"]:
-                continue
-            item = Feature()
-            item["lat"] = location["lat"]
-            item["lon"] = location["lng"]
-            popup = Selector(text=location["cardHtml"])
-            item["ref"] = item["website"] = response.urljoin(popup.xpath('//a[@class="link-ts-list"]/@href').get())
-            item["addr_full"] = (
-                popup.xpath('normalize-space(//div[contains(@class, "search-result-header")]/h2/text())')
-                .get()
-                .removeprefix("JET ")
-            )
+        for station in response.json()["stations"]:
+            item = DictParser.parse(station)
+            item["ref"] = station["publicId"]
+            item["website"] = station["webUrl"]
             apply_category(Categories.FUEL_STATION, item)
             yield item
