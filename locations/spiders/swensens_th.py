2a9f852bad14a852006f29f1355b3dbc529cc8f1
Karen Kerr <karenkerrgithub@gmail.com>
2025-07-30 13:00:24 +0100

    [Swensens TH] Add spider

---
+++ locations/spiders/swensens_th.py
@@ -0,0 +1,31 @@
+import json
+import re
+
+
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+
+
+class SwensensTHSpider(JSONBlobSpider):
+    name = "swensens_th"
+    item_attributes = {"brand": "Swensen's", "brand_wikidata": "Q6640658"}
+    start_urls = ["https://brandsite.swensens1112.com/js/575.72824bfd.js"]
+    no_refs = True
+
+    def extract_json(self, response: Response) -> dict | list[dict]:
+        data = re.search(r"const va=JSON\.parse\('(.+?)'\)", response.text.replace("\\", "")).group(1)
+        locations = json.loads(data)
+        return locations
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["lat"] = feature["Lattitude"]
+        item["lon"] = feature["Longtitude"]
+        item["branch"] = feature["StoreNameEN"]
+        item["extras"]["branch:th"] = feature["StoreNameTH"]
+        apply_category(Categories.ICE_CREAM, item)
+        yield item
