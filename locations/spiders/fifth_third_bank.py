2b7591a88424a80082532beec01672a46ddaeaa6
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-07-30 12:53:39 +0530

    added atm info

--- locations/spiders/fifth_third_bank.py
+++ locations/spiders/fifth_third_bank.py
@@ -1,13 +1,16 @@
 import scrapy
 
-from locations.categories import Categories
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.linked_data_parser import LinkedDataParser
 from locations.microdata_parser import MicrodataParser
 
 
 class FifthThirdBankSpider(scrapy.spiders.SitemapSpider):
     name = "fifth_third_bank"
-    item_attributes = {"brand": "Fifth Third Bank", "brand_wikidata": "Q1411810", "extras": Categories.BANK.value}
+    item_attributes = {
+        "brand": "Fifth Third Bank",
+        "brand_wikidata": "Q1411810",
+    }
     allowed_domains = [
         "53.com",
     ]
@@ -28,4 +31,7 @@ class FifthThirdBankSpider(scrapy.spiders.SitemapSpider):
             return
         if obj:
             item = LinkedDataParser.parse_ld(obj)
+            apply_category(Categories.BANK, item)
+            if atm_info := response.xpath('//*[@class="CoreHero-atmNumber"]//text()').get():
+                apply_yes_no(Extras.ATM, item, True if "ATM" in atm_info else False)
             yield item
