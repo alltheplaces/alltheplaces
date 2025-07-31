4ce5de643c7feeb45d4e750a5671f014713075c9
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-07-31 14:47:38 +0530

    Fix Aldi Sud CN Spider

--- locations/spiders/aldi_sud_cn.py
+++ locations/spiders/aldi_sud_cn.py
@@ -1,4 +1,9 @@
+import json
+import re
+from typing import Any
+
 from scrapy import Spider
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.hours import DAYS, OpeningHours
@@ -8,31 +13,22 @@ from locations.items import Feature
 class AldiSudCNSpider(Spider):
     name = "aldi_sud_cn"
     item_attributes = {"brand_wikidata": "Q41171672", "country": "CN"}
-    start_urls = ["https://aldi.cn/assets/json/stores.json"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    start_urls = ["https://aldi.cn/ourshops/physicalstore/"]
+    custom_settings = {"ROBOTSTXT_OBEY": False, "DOWNLOAD_TIMEOUT": 180}
 
-    def parse(self, response, **kwargs):
-        districts = response.json().values()
-        for district in districts:
-            for store_data in district["district-data"].values():
-                for ref, store in store_data["stores"].items():
-                    item = Feature()
-                    item["ref"] = ref
-                    item["branch"] = store["title-en"].removeprefix("ALDI ").removesuffix(" Store")
-                    item["addr_full"] = store["address-en"]
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store_data in re.findall(r"data_json:\s*\'(\{.+?})\'\s*,", response.text):
+            store = json.loads(store_data)["fields"]
+            item = Feature()
+            item["ref"] = store["mapLink"]
+            item["branch"] = store["storesName"].removesuffix("店").strip()
+            item["addr_full"] = store["storesAddress"]
 
-                    try:
-                        oh = OpeningHours()
-                        start_time, end_time = store["hours"].split("-")
-                        oh.add_days_range(DAYS, start_time, end_time)
-                        item["opening_hours"] = oh
-                    except:
-                        self.logger.error("Error parsing opening hours: {}".format(store["hours"]))
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_days_range(DAYS, store["startTime"], store["endTime"], "%H:%M:%S")
 
-                    # TODO: Transform store["locationX"] store["locationX"]
+            item["extras"] = {"map": store["mapLink"]}
 
-                    item["extras"] = {"map": store["mapLink"]}
+            apply_category(Categories.SHOP_SUPERMARKET, item)
 
-                    apply_category(Categories.SHOP_SUPERMARKET, item)
-
-                    yield item
+            yield item
