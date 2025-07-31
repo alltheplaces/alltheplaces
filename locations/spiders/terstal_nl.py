89d5472a5dbda8c0480f1015cbec0b1f6813d14e
Cj Malone <CjMalone@mail.com>
2025-07-31 12:26:17 +0100

    response.json

--- locations/spiders/terstal_nl.py
+++ locations/spiders/terstal_nl.py
@@ -1,21 +1,22 @@
-import json
+from typing import Any
+from urllib.parse import urljoin
 
-import scrapy
-from scrapy.http import JsonRequest
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
-class TerstalNLSpider(scrapy.Spider):
+class TerstalNLSpider(Spider):
     name = "terstal_nl"
     item_attributes = {"brand": "terStal", "brand_wikidata": "Q114905394"}
 
     def start_requests(self):
         yield JsonRequest(url="https://www.terstal.nl/rest/V1/stores/store_id/1")
 
-    def parse(self, response, **kwargs):
-        for store in json.loads(response.text):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store in response.json():
             item = DictParser.parse(store)
             item["branch"] = item.pop("name")
             apply_category(Categories.SHOP_CLOTHES, item)
