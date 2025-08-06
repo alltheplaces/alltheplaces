4eafa993b90c139db8184ebc584b6a55666c4e2a
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-08-06 09:28:00 +0100

    Clean name

--- locations/spiders/network_tr.py
+++ locations/spiders/network_tr.py
@@ -10,11 +10,12 @@ from locations.json_blob_spider import JSONBlobSpider
 class NetworkTRSpider(JSONBlobSpider):
     name = "network_tr"
     item_attributes = {"brand": "Network", "brand_wikidata": "Q28736409"}
-    start_urls = ["https://www.network.com.tr/Lookup/OfflineStores/?_=1658980444275"]
+    start_urls = ["https://www.network.com.tr/Lookup/OfflineStores"]
     locations_key = "Result"
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["ref"] = item["name"] = feature["MAGAZATITLE"]
+        item["ref"] = feature["MAGAZATITLE"]
+        item["branch"] = feature["MAGAZATITLE"].replace(" NetWork", "")
         item["addr_full"] = feature["ADRES"]
         item["city"] = feature["IL"]
         item["phone"] = feature["MAGAZATEL"]
