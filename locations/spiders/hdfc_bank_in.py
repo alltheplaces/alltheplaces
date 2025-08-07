b63a349624702e351a7b197504939353607d7435
Karen Kerr <karenkerrgithub@gmail.com>
2025-08-07 10:06:23 +0100

    [HDFCBank IN] Remove generic phone number and fix class name

--- locations/spiders/hdfc_bank_in.py
+++ locations/spiders/hdfc_bank_in.py
@@ -4,7 +4,7 @@ from locations.categories import Categories, apply_category
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class HDFCBankINSpider(SitemapSpider, StructuredDataSpider):
+class HdfcBankINSpider(SitemapSpider, StructuredDataSpider):
     name = "hdfc_bank_in"
     item_attributes = {"brand": "HDFC Bank", "brand_wikidata": "Q631047"}
     sitemap_urls = [
@@ -17,6 +17,7 @@ class HDFCBankINSpider(SitemapSpider, StructuredDataSpider):
     requires_proxy = "IN"
 
     def post_process_item(self, item, response, ld_data, **kwargs):
+        item.pop("phone")
         if "ATM" in item["name"]:
             apply_category(Categories.ATM, item)
         else:
