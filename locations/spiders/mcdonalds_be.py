891fc85ad6b8f0253f071af99c5893e98b799eeb
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-08-11 14:12:17 +0530

    [McDonald's BE] Fix Spider (#13979)

--- locations/spiders/mcdonalds_be.py
+++ locations/spiders/mcdonalds_be.py
@@ -17,6 +17,7 @@ class McdonaldsBESpider(Spider):
     allowed_domains = ["www.mcdonalds.be"]
     start_urls = ["https://www.mcdonalds.be/en/restaurants/api/restaurants"]
     user_agent = FIREFOX_LATEST
+    requires_proxy = True
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json():
