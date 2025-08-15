8acfa46a8c7f957b2c65f481bdbd699e7ae17db0
Cj Malone <CjMalone@mail.com>
2025-08-15 10:15:04 +0100

    Clean name

--- locations/spiders/cisalfa_sport_it.py
+++ locations/spiders/cisalfa_sport_it.py
@@ -17,12 +17,16 @@ class CisalfaSportITSpider(JSONBlobSpider):
         yield JsonRequest(
             url="https://api.retailtune.com/storelocator/it/?rt_api_key=LgLwP3qejs1nUXFMt4rNSyZO5Iif0pXVw9wUmlcaWRe40uYfXqekTj7SytclsWxuHwBhHiHn7bwxttY9hMdFbv9uNu0jK44bJ1Bs",
             headers={
-                "Accept": "application/json",
                 "Origin": "https://www.cisalfasport.it",
                 "Referer": "https://www.cisalfasport.it/",
             },
         )
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if item["name"].startswith("Cisalfa Outlet "):
+            item["branch"] = item.pop("name").removeprefix("Cisalfa Outlet ")
+            item["name"] = "Cisalfa Outlet"
+        else:
+            item["branch"] = item.pop("name").removeprefix("Cisalfa Sport ")
         item["state"] = item["state"]["name"]
         yield item
