fee7625676d264ec499753e252a533b69f8653d5
j9d3it <26203575+j9d3it@users.noreply.github.com>
2025-08-16 18:14:51 +0900

    [Fuji Service luggage lockers JP] add spider

---
+++ locations/spiders/fujiservice_jp.py
@@ -0,0 +1,22 @@
+from typing import Any
+from scrapy import Spider
+from scrapy.http import Response
+from locations.dict_parser import DictParser
+
+class FujiserviceJPSpider(Spider):
+    name = "fujiservice_jp"
+    
+    start_urls = ["https://fuji-service-cl.co.jp/wp-json/wpgmza/v1/features/"]
+    allowed_domains = ["fuji-service-cl.co.jp"]
+    country_code = "JP"
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store in response.json()["markers"]:
+
+            item = DictParser.parse(store)
+            item["ref"] = store["id"]
+            item["extras"]["amenity"] = "luggage_locker"
+            item["operator"] = "フジサービス"
+            item["extras"]["operator:en"] = "Fuji Service"
+            item["image"] = store["pic"]
+            yield item
