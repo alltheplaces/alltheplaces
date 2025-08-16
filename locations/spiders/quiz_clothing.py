97b5f25120efcddc1896eb02a843e409c995c048
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-08-16 17:02:55 +0100

    [Quiz Clothing] Add spider

---
+++ locations/spiders/quiz_clothing.py
@@ -0,0 +1,11 @@
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class QuizClothingSpider(JSONBlobSpider):
+    name = "quiz_clothing"
+    item_attributes = {
+        "brand_wikidata": "Q29995941",
+        "brand": "Quiz",
+    }
+    start_urls = ['https://www.quizclothing.co.uk/api/inventory/inventoryGetStoreFilter?countryId=0&cityId=0&cityId=0']
+    locations_key = "data"
