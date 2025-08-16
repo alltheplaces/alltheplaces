732209f4258d788058a26dc1d9ed076b4bf5598b
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-08-16 17:16:07 +0100

    Add coords (latitude spelled incorrectly) and branch

--- locations/spiders/quiz_clothing.py
+++ locations/spiders/quiz_clothing.py
@@ -1,4 +1,7 @@
+from typing import Iterable
 from locations.json_blob_spider import JSONBlobSpider
+from locations.items import Feature
+from scrapy.http import Request, Response
 
 
 class QuizClothingSpider(JSONBlobSpider):
@@ -7,5 +10,11 @@ class QuizClothingSpider(JSONBlobSpider):
         "brand_wikidata": "Q29995941",
         "brand": "Quiz",
     }
-    start_urls = ["https://www.quizclothing.co.uk/api/inventory/inventoryGetStoreFilter?countryId=0&cityId=0&cityId=0"]
+    start_urls = ['https://www.quizclothing.co.uk/api/inventory/inventoryGetStoreFilter?countryId=0&cityId=0&cityId=0']
     locations_key = "data"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        item["lat"] = feature["latitude"]
+        item["lon"] = feature["longtitude"]
+        yield item
