c059a6cde58b7ed74d48102da3058ed517839043
Cj Malone <CjMalone@mail.com>
2025-08-18 09:58:01 +0100

    [TEST-FIX] Sync brands with NSI

--- locations/spiders/eighty_five_degrees_c_au.py
+++ locations/spiders/eighty_five_degrees_c_au.py
@@ -1,15 +1,19 @@
-from scrapy import Spider
+from typing import Any
 
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
 from locations.google_url import extract_google_position
 from locations.items import Feature
 
 
 class EightyFiveDegreesCAUSpider(Spider):
     name = "eighty_five_degrees_c_au"
-    item_attributes = {"brand": "85°C", "brand_wikidata": "Q4644852"}
+    item_attributes = {"brand": "85°C Bakery Cafe", "brand_wikidata": "Q4644852"}
     start_urls = ["https://www.85cafe.com.au/store-finder/"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.xpath("//div[@class='card mb-2']"):
             item = Feature()
             item["website"] = store.xpath(".//h2/a/@href").get()
@@ -17,5 +21,8 @@ class EightyFiveDegreesCAUSpider(Spider):
             item["branch"] = store.xpath(".//h2/a/text()").get().removeprefix("85C Daily Cafe ")
             item["addr_full"] = store.xpath("string(.//address/p[1])").get()
             item["phone"] = store.xpath("string(.//address/p[2])").get()
+
             extract_google_position(item, store)
+            apply_category(Categories.CAFE, item)
+
             yield item
