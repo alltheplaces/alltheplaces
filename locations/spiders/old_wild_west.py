2dbd30ac5b0b9358fbc361c4ad1b61b5b995fa33
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-08-18 18:58:25 +0530

    Update old_wild_west.py

--- locations/spiders/old_wild_west.py
+++ locations/spiders/old_wild_west.py
@@ -8,3 +8,4 @@ class OldWildWestSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Old Wild West", "brand_wikidata": "Q25402475"}
     sitemap_urls = ["https://www.oldwildwest.it/sitemap-index.xml"]
     sitemap_rules = [("/it/ristoranti/", "parse_sd")]
+    skip_auto_cc_domain = True
