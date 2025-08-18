6d6ebdcbdb1b18a588a4e3d9621c81c080416e35
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-08-18 20:19:58 +0530

    Update user agent and add headers to fix Costco CA Spider

--- locations/spiders/costco_ca.py
+++ locations/spiders/costco_ca.py
@@ -1,13 +1,13 @@
-from typing import Any
+from typing import Any, Iterable
 
 import chompjs
-from scrapy import Spider
+from scrapy import Request, Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
-from locations.user_agents import BROWSER_DEFAULT
+from locations.user_agents import CHROME_LATEST
 
 
 class CostcoCASpider(Spider):
@@ -15,7 +15,18 @@ class CostcoCASpider(Spider):
     item_attributes = {"brand": "Costco", "brand_wikidata": "Q715583"}
     start_urls = ["https://www.costco.ca/AjaxWarehouseBrowseLookupView?countryCode=CA"]
     is_playwright_spider = True
-    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": CHROME_LATEST}
+
+    def start_requests(self) -> Iterable[Request]:
+        yield Request(
+            url="https://www.costco.ca/AjaxWarehouseBrowseLookupView?countryCode=CA",
+            headers={
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "sec-ch-ua": '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"Windows"',
+            },
+        )
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in chompjs.parse_js_object(response.text):  # JSON is embedded within HTML
