6c8a56fe13f82e97983067bed9350dddfeb7cb5b
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-08-18 20:33:37 +0530

    Try with hardcoded Windows User Agent

--- locations/spiders/costco_ca.py
+++ locations/spiders/costco_ca.py
@@ -7,7 +7,6 @@ from scrapy.http import Response
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
-from locations.user_agents import CHROME_LATEST
 
 
 class CostcoCASpider(Spider):
@@ -15,7 +14,10 @@ class CostcoCASpider(Spider):
     item_attributes = {"brand": "Costco", "brand_wikidata": "Q715583"}
     start_urls = ["https://www.costco.ca/AjaxWarehouseBrowseLookupView?countryCode=CA"]
     is_playwright_spider = True
-    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": CHROME_LATEST}
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {
+        "ROBOTSTXT_OBEY": False,
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+    }
 
     def start_requests(self) -> Iterable[Request]:
         yield Request(
@@ -24,7 +26,7 @@ class CostcoCASpider(Spider):
                 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                 "sec-ch-ua": '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
                 "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Linux"',
+                "sec-ch-ua-platform": '"Windows"',
             },
         )
 
