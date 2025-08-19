093dea8ee00b9e27846ed6bc9543e15ded2155f0
Karen Kerr <karenkerrgithub@gmail.com>
2025-08-19 14:12:20 +0100

    [Hagebau AT DE] Add spider

---
+++ locations/spiders/hagebau_at_de.py
@@ -0,0 +1,22 @@
+import re
+
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class HagebauATDESpider(SitemapSpider, StructuredDataSpider):
+    name = "hagebau_at_de"
+    item_attributes = {"brand": "Hagebaumarkt", "brand_wikidata": "Q1568279"}
+    sitemap_urls = [
+        "https://www.hagebau.at/sitemap.xml",
+        "https://www.hagebau.de/sitemap.xml",
+    ]
+    sitemap_follow = ["/store/"]
+    sitemap_rules = [("", "parse_sd")]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["lat"], item["lon"] = re.search(r"lat.*:(\d+.\d+).*lon.*:(\d+.\d+)", response.text).groups()
+        apply_category(Categories.SHOP_DOITYOURSELF, item)
+        yield item
