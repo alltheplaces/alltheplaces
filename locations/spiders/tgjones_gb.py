baaccba5583a01f947f9313272a328effe0b38aa
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-08-21 15:36:46 +0100

    Update tgjones_gb.py

--- locations/spiders/tgjones_gb.py
+++ locations/spiders/tgjones_gb.py
@@ -32,7 +32,7 @@ class TgjonesGBSpider(SitemapSpider):
         item["ref"] = item["website"] = response.url
         item["name"] = response.xpath("//h1/text()").get(default="").split(",")[0]
         if not item["name"]:
-            continue
+            return
         item["addr_full"] = clean_address(response.xpath('//*[@class="shop-styling__address"]/p/text()').getall())
         if coordinates := re.search(self.coordinates_pattern, response.text):
             item["lat"], item["lon"] = coordinates.groups()
