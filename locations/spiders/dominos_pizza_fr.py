5001f05156a30d72b972869d20e724be54aeb4d3
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-08-25 04:34:59 -0400

    [Dominos Pizza FR] Fix spider (#14096)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/dominos_pizza_fr.py
+++ locations/spiders/dominos_pizza_fr.py
@@ -1,33 +1,20 @@
-from typing import Any
+from typing import Iterable
 
-from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+from scrapy.http import JsonRequest
 
-from locations.categories import Categories, apply_category
-from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address
+from locations.geo import city_locations
+from locations.spiders.dominos_pizza_au import DominosPizzaAUSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class DominosPizzaFRSpider(SitemapSpider):
+class DominosPizzaFRSpider(DominosPizzaAUSpider):
     name = "dominos_pizza_fr"
     item_attributes = {"brand": "Domino's", "brand_wikidata": "Q839466"}
     allowed_domains = ["dominos.fr"]
     user_agent = BROWSER_DEFAULT
-    sitemap_urls = ["https://www.dominos.fr/sitemap.aspx"]
-    sitemap_rules = [(r"https:\/\/www\.dominos\.fr\/magasin\/\/?([-\w.]+)_(unknown|[\d]+)$", "parse")]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        properties = {
-            "ref": response.url,
-            "branch": response.xpath('//h1[@class="storetitle"]/text()').get().removeprefix("Domino's Pizza "),
-            "addr_full": clean_address(response.xpath('//a[@id="open-map-address"]/text()').get()),
-            "lat": response.xpath('//input[@id="store-lat"]/@value').get().replace(",", "."),
-            "lon": response.xpath('//input[@id="store-lon"]/@value').get().replace(",", "."),
-            "phone": response.xpath('//div[@class="store-phone"]/a/text()').get(),
-            "website": response.url,
-        }
-
-        apply_category(Categories.FAST_FOOD, properties)
-
-        yield Feature(**properties)
+    def start_requests(self) -> Iterable[JsonRequest]:
+        for city in city_locations("FR", 15000):
+            yield JsonRequest(
+                url=f"https://www.dominos.fr/dynamicstoresearchapi/getstoresfromquery?lon={city['longitude']}&lat={city['latitude']}",
+            )
--- locations/spiders/dominos_pizza_nl.py
+++ locations/spiders/dominos_pizza_nl.py
@@ -1,34 +1,17 @@
-from urllib.parse import urljoin
+from typing import Iterable
 
-import scrapy
 from scrapy.http import JsonRequest
 
-from locations.dict_parser import DictParser
 from locations.geo import city_locations
-from locations.user_agents import BROWSER_DEFAULT
+from locations.spiders.dominos_pizza_au import DominosPizzaAUSpider
 
 
-class DominosPizzaNLSpider(scrapy.Spider):
+class DominosPizzaNLSpider(DominosPizzaAUSpider):
     name = "dominos_pizza_nl"
     item_attributes = {"brand": "Domino's", "brand_wikidata": "Q839466"}
-    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
 
-    def start_requests(self):
-        for city in city_locations("NL", 18000):
+    def start_requests(self) -> Iterable[JsonRequest]:
+        for city in city_locations("NL", 15000):
             yield JsonRequest(
                 url=f"https://www.dominos.nl/dynamicstoresearchapi/getstoresfromquery?lon={city['longitude']}&lat={city['latitude']}",
             )
-
-    def parse(self, response, **kwargs):
-        for store in response.json()["PickupSearchStore"]:
-            store.update(store["locations"].pop(0))
-            store.update(store.pop("address"))
-            item = DictParser.parse(store)
-            item["branch"] = item.pop("name")
-            item["website"] = urljoin("https://www.dominos.nl", store["properties"]["storeUrl"])
-            for details in store["attributes"]:
-                if details["key"] == "streetName":
-                    item["street_address"] = details["value"]
-                elif details["key"] == "postCode":
-                    item["postcode"] = details["value"]
-            yield item
