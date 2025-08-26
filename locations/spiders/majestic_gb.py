45fa16a92797acc2127119cddc0f631cdbafd1d0
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-08-26 12:49:59 +0100

    Add details from details pages

--- locations/spiders/majestic_gb.py
+++ locations/spiders/majestic_gb.py
@@ -1,8 +1,12 @@
+from typing import Iterable
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Response, Request
 
-from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.categories import Categories, apply_category
 from locations.user_agents import BROWSER_DEFAULT
 
 
@@ -17,11 +21,7 @@ class MajesticGBSpider(CrawlSpider):
         "ROBOTSTXT_OBEY": False,
         "DEFAULT_REQUEST_HEADERS": {
             "Host": "www.majestic.co.uk",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-GB,en-US;q=0.7,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
             "DNT": "1",
-            "Sec-GPC": "1",
         },
     }
     requires_proxy = True
@@ -30,6 +30,7 @@ class MajesticGBSpider(CrawlSpider):
         for location in response.xpath('//li[contains(@class, "store-item")]'):
             item = Feature()
             item["ref"] = item["website"] = response.urljoin(location.xpath(".//@data-id").get())
+            item["addr_full"] = response.xpath('//p[@class="store__address"]/text()').get()
             item["lat"] = location.xpath(".//@data-lat").get()
             item["lon"] = location.xpath(".//@data-long").get()
             item["name"] = location.xpath(".//@data-name").get()
@@ -38,4 +39,14 @@ class MajesticGBSpider(CrawlSpider):
             item["image"] = location.xpath('./span[@class="store-list-image"]/img/@src').get()
             apply_category(Categories.SHOP_WINE, item)
 
-            yield item
+            yield Request(url=item["website"], meta={"item": item}, callback=self.parse_details)
+
+    def parse_details(self, response: Response) -> Iterable[Feature]:
+        item = response.meta["item"]
+        item["addr_full"] = response.xpath('//p[@class="store__address"]/text()').get()
+        item["facebook"] = response.xpath('//a[contains(@href, "facebook.com/pages/")]/@href').get()
+        item["email"] = response.xpath('//*[@id = "clickEmail"]/text()').get()
+        hours_string = " ".join(response.xpath('//div[contains(@class, "store-time-line")]//text()').getall())
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours_string)
+        yield item
