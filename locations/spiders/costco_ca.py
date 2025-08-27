ef74edb91de522b81d97c58a2531ac9b41c82249
Cj Malone <CjMalone@mail.com>
2025-08-27 11:12:07 +0100

    Revert bispoke useragent

--- locations/spiders/costco_ca.py
+++ locations/spiders/costco_ca.py
@@ -7,7 +7,6 @@ from scrapy.http import Response
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
-from locations.user_agents import FIREFOX_LATEST
 
 
 class CostcoCASpider(Spider):
@@ -15,7 +14,10 @@ class CostcoCASpider(Spider):
     item_attributes = {"brand": "Costco", "brand_wikidata": "Q715583"}
     start_urls = ["https://www.costco.ca/AjaxWarehouseBrowseLookupView?countryCode=CA"]
     is_playwright_spider = True
-    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": FIREFOX_LATEST}
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {
+        "ROBOTSTXT_OBEY": False,
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+    }
 
     def start_requests(self) -> Iterable[Request]:
         yield Request(
