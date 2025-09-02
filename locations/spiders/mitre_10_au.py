8f39f773d5868760c4758ffef62e0f1dc35b1bac
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-09-02 17:19:02 +0530

    code updated to fix spider

--- locations/spiders/mitre_10_au.py
+++ locations/spiders/mitre_10_au.py
@@ -18,32 +18,5 @@ class Mitre10AUSpider(StructuredDataSpider):
         data_clean = " ".join(data_raw.splitlines()).replace("\\/", "/")
         data_json = json.loads(data_clean)
         stores = data_json["*"]["Magento_Ui/js/core/app"]["components"]["store-locator-search"]["markers"]
-        # Note that the stores data obtained at this point is missing
-        # opening hours which is only available on separate request
-        # to the mitre10.com.au store page for each store.
         for store in stores:
             yield scrapy.Request(store["url"], self.parse_sd)
-
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        # Note that the mitre10.com.au store page for each store has
-        # linked data that has already been parsed by this point.
-        # However, the linked data is missing some fields of
-        # information that is found in a separate JavaScript blob on
-        # the same page. The following code seeks to complete the
-        # missing data.
-        data_raw = response.xpath(
-            '//div[contains(@class, "store-locator-content")]/following::script[@type="text/x-magento-init"]/text()'
-        ).extract_first()
-        data_clean = " ".join(data_raw.splitlines()).replace("\\/", "/")
-        data_json = json.loads(data_clean)
-        stores = data_json["*"]["Magento_Ui/js/core/app"]["components"]["preferred-store"]["children"][
-            "retailer-switcher"
-        ]["storeOffers"]
-        for store in stores:
-            if store["address_data"]["latitude"] == item["lat"] and store["address_data"]["longitude"] == item["lon"]:
-                item["ref"] = store["sellerId"]
-                item["postcode"] = store["address_data"]["postcode"]
-                break
-        if "mitre10official" in item["facebook"]:
-            item.pop("facebook")
-        yield item
