540ea39cbdba0f09604ca223215cbf1d7315ae56
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-09-03 10:55:09 +0100

    Get coords

--- locations/spiders/fullers_gb.py
+++ locations/spiders/fullers_gb.py
@@ -1,16 +1,18 @@
-from typing import Iterable
+import json
+import re
 
-from scrapy.http import FormRequest, Response
+from typing import Iterable, Any
+
+from scrapy.http import FormRequest, Response, JsonRequest
 from scrapy.spiders import Request
 
-from locations.dict_parser import DictParser
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
 from locations.user_agents import BROWSER_DEFAULT
+from locations.structured_data_spider import StructuredDataSpider
 
-
-class FullersGBSpider(JSONBlobSpider, StructuredDataSpider):
+class FullersGBSpider(JSONBlobSpider,StructuredDataSpider):
     name = "fullers_gb"
     item_attributes = {
         "brand": "Fuller's",
@@ -21,21 +23,21 @@ class FullersGBSpider(JSONBlobSpider, StructuredDataSpider):
         "COOKIES_ENABLED": True,
         "USER_AGENT": BROWSER_DEFAULT,
     }
-    locations_key = ["items"]
-    wanted_types = ["restaurant"]
+    locations_key = ['items']
+    wanted_types = ['restaurant']
 
     def make_request(self, page: int) -> FormRequest:
         return FormRequest(
-            url="https://www.fullers.co.uk/api/main/pubs/feed",
-            formdata={
-                "pageNumber": str(page),
-                "latitude": "0",
-                "longitude": "0",
-                "categories": [],
-                "area": "",  # D61B5F3C29994C99A3C93FA4144315A9"
+            url = "https://www.fullers.co.uk/api/main/pubs/feed",
+            formdata = {
+                "pageNumber":str(page),
+                "latitude":"0",
+                "longitude":"0",
+                "categories":[],
+                "area":""#D61B5F3C29994C99A3C93FA4144315A9"
             },
             method="POST",
-            headers={
+            headers = {
                 "Host": "www.fullers.co.uk",
                 "Accept": "application/json",
             },
@@ -54,14 +56,21 @@ class FullersGBSpider(JSONBlobSpider, StructuredDataSpider):
                     continue
                 feature["id"] = feature["pubId"]
                 item = DictParser.parse(feature)
-                yield from self.post_process_item_json(item, response, feature) or []
+                if feature["subTitle"]:
+                    item["addr_full"] = feature["subTitle"]
+                if feature["link"]:
+                    item["website"] = feature["link"]
+                yield JsonRequest(url="https://www.fullers.co.uk/api/main/pubs/information?pubId="+feature["pubId"], cb_kwargs={"item":item}, headers={"Host": "www.fullers.co.uk","Accept": "application/json","Content-Type": "application/json", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0"}, callback=self.post_process_item_json)
 
         if response.json()["totalPages"] > response.json()["currentPage"]:
             yield self.make_request(int(response.json()["currentPage"]) + 1)
 
-    def post_process_item_json(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        if feature["subTitle"]:
-            item["addr_full"] = feature["subTitle"]
-        if feature["link"]:
-            item["website"] = feature["link"]
+    def post_process_item_json(self, response, item):
+        item["geometry"] = response.json()["googleMaps"]["coords"]
+        yield item
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if "data-marker-lat" in response.text:
+            item["lat"] = re.search(r'data-marker-lat="(-?\d+\.\d+)"', response.text).group(1)
+            item["lon"] = re.search(r'data-marker-lng="(-?\d+\.\d+)"', response.text).group(1)
         yield item
