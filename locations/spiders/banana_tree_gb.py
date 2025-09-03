160df81d0c9afe7eec5637ef781984740c40ea84
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-09-03 13:37:57 +0530

    updated code to use sitemap to fix spider

--- locations/spiders/banana_tree_gb.py
+++ locations/spiders/banana_tree_gb.py
@@ -1,15 +1,30 @@
-from typing import Iterable
+from typing import Any
 
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.spiders.frankie_and_bennys_gb import FrankieAndBennysGBSpider
 
 
-class BananaTreeGBSpider(FrankieAndBennysGBSpider):
+class BananaTreeGBSpider(SitemapSpider):
     name = "banana_tree_gb"
     item_attributes = {"brand": "Banana Tree", "brand_wikidata": "Q123013837"}
-    brand_key = "banana"
+    sitemap_urls = ["https://bananatree.co.uk/sitemap.xml"]
+    sitemap_rules = [("/restaurants/", "parse")]
 
-    def parse_item(self, item: Feature, location: dict, **kwargs) -> Iterable[Feature]:
-        item["website"] = "https://bananatree.co.uk/restaurants/{}/".format(location["slug"])
-
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath('//*[@class="about-contact-info"]/h1/text()').get()
+        item["addr_full"] = response.xpath('//*[@class="address"]/text()').get()
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
+        oh = OpeningHours()
+        for day_time in response.xpath('//*[@class="opening-hours-day"]'):
+            day = day_time.xpath(".//span").xpath("normalize-space()").get()
+            open_time, close_time = day_time.xpath(".//span[2]").xpath("normalize-space()").get().split(" - ")
+            oh.add_range(day=day, open_time=open_time, close_time=close_time)
+        item["opening_hours"] = oh
         yield item
