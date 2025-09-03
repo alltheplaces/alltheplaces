b10fe1b0695ab743bb00ed45d8e020519c30cb66
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-09-03 15:32:22 +0530

    updated code to use sitemap to fix spider

--- locations/spiders/bella_italia_gb.py
+++ locations/spiders/bella_italia_gb.py
@@ -1,18 +1,30 @@
-from typing import Iterable
+from typing import Any
 
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.spiders.frankie_and_bennys_gb import FrankieAndBennysGBSpider
 
 
-class BellaItaliaGBSpider(FrankieAndBennysGBSpider):
+class BellaItaliaGBSpider(SitemapSpider):
     name = "bella_italia_gb"
     item_attributes = {"brand": "Bella Italia", "brand_wikidata": "Q4883362"}
-    brand_key = "bella"
+    sitemap_urls = ["https://www.bellaitalia.co.uk/sitemap.xml"]
+    sitemap_rules = [("/restaurants/", "parse")]
 
-    def parse_item(self, item: Feature, location: dict, **kwargs) -> Iterable[Feature]:
-        item["website"] = "https://www.bellaitalia.co.uk/restaurants/{}/{}/".format(location["city"], location["slug"])
-
-        if "phone" in item and item["phone"] is not None and item["phone"].replace(" ", "").startswith("+443"):
-            item.pop("phone", None)
-
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath("//h1/span/text()").get()
+        item["addr_full"] = response.xpath('//*[@class="contact-info"]//p').xpath("normalize-space()").get()
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
+        oh = OpeningHours()
+        for day_time in response.xpath('//*[@class="opening-hours-day"]'):
+            day = day_time.xpath(".//span").xpath("normalize-space()").get()
+            open_time, close_time = day_time.xpath(".//span[2]").xpath("normalize-space()").get().split(" - ")
+            oh.add_range(day=day, open_time=open_time, close_time=close_time)
+        item["opening_hours"] = oh
         yield item
