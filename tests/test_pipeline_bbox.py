8c442b944dff0365d5881b00602450713476f5ab
Cj Malone <CjMalone@mail.com>
2025-09-04 14:07:48 +0100

    Add test

---
+++ tests/test_pipeline_bbox.py
@@ -0,0 +1,17 @@
+from scrapy.utils.test import get_crawler
+
+from locations.items import Feature
+from locations.pipelines.bbox_calculator import BboxCalculator
+from locations.spiders.greggs_gb import GreggsGBSpider
+
+
+def test_bbox():
+    pipeline = BboxCalculator()
+    spider = GreggsGBSpider()
+    spider.crawler = get_crawler()
+    pipeline.process_item(Feature(lat=50.7490509, lon=-1.5698471), spider)
+    pipeline.process_item(Feature(lat=50.5817046, lon=-1.0837289), spider)
+    assert spider.crawler.stats.get_value("atp/bbox/lat_min") == 50.5817046
+    assert spider.crawler.stats.get_value("atp/bbox/lat_max") == 50.7490509
+    assert spider.crawler.stats.get_value("atp/bbox/lon_min") == -1.5698471
+    assert spider.crawler.stats.get_value("atp/bbox/lon_max") == -1.0837289
