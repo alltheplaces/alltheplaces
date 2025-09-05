4b0904b360d5f91d2b1f6f6e770534e5f5a394cf
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-09-05 16:29:54 +0100

    Another typo!

--- locations/spiders/whistles_gb.py
+++ locations/spiders/whistles_gb.py
@@ -38,7 +38,7 @@ class WhistlesGBSpider(JSONBlobSpider):
         for day in location["workTimes"]:
             if "closed" in day["value"].lower():
                 continue
-            start, end = day["value"].replace(" ", "").resplace("-:", "-").replace(".", ":").split("-")
+            start, end = day["value"].replace(" ", "").replace("-:", "-").replace(".", ":").split("-")
             oh.add_range(day["weekDay"], start, end)
         item["opening_hours"] = oh
         yield item
