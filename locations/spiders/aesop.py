770ee308ddbbc36fe3b48aa04229f6449537a75f
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-09-10 21:45:34 +0100

    clean url

--- locations/spiders/aesop.py
+++ locations/spiders/aesop.py
@@ -9,11 +9,10 @@ class AesopSpider(CrawlSpider, StructuredDataSpider):
     name = "aesop"
     item_attributes = {"brand": "Aesop", "brand_wikidata": "Q4688560"}
     start_urls = ["https://www.aesop.com/stores/all"]
-
     rules = [Rule(LinkExtractor(allow=r"/stores/"), callback="parse_sd", follow=True)]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["branch"] = item.pop("name").replace("Aesop ", "")
-        item["website"] = item["website"].replace(",", "").replace(" ", "")
+        item["website"] = response.url
         apply_category(Categories.SHOP_COSMETICS, item)
         yield item
