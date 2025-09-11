c8d3b858602edb3f3ce0e89d9cadb558ea0ed00e
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-09-11 14:31:42 +0530

    [Rotten Robbie US] Fix Spider (#14237)

--- locations/spiders/rotten_robbie_us.py
+++ locations/spiders/rotten_robbie_us.py
@@ -1,8 +1,19 @@
+import chompjs
+import scrapy
+from scrapy.http import Response
+
 from locations.categories import Categories
-from locations.storefinders.wp_go_maps import WpGoMapsSpider
+from locations.dict_parser import DictParser
 
 
-class RottenRobbieUSSpider(WpGoMapsSpider):
+class RottenRobbieUSSpider(scrapy.Spider):
     name = "rotten_robbie_us"
     item_attributes = {"brand": "Rotten Robbie", "brand_wikidata": "Q87378070", "extras": Categories.FUEL_STATION.value}
-    allowed_domains = ["www.rottenrobbie.com"]
+    start_urls = ["https://rottenrobbie.com/locations"]
+
+    def parse(self, response: Response, **kwargs):
+        for store in chompjs.parse_js_object(response.xpath('//*[@type="application/ld+json"]/text()').get())["@graph"]:
+            item = DictParser.parse(store)
+            item["ref"] = store["@id"]
+            item["branch"] = item.pop("name")
+            yield item
