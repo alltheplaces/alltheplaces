b868cd4e0f706d84698fbd4612ee29cd6c9ed2ea
Cj Malone <CjMalone@mail.com>
2025-09-11 10:00:04 +0100

    apply_category

--- locations/spiders/rotten_robbie_us.py
+++ locations/spiders/rotten_robbie_us.py
@@ -2,13 +2,13 @@ import chompjs
 import scrapy
 from scrapy.http import Response
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
 class RottenRobbieUSSpider(scrapy.Spider):
     name = "rotten_robbie_us"
-    item_attributes = {"brand": "Rotten Robbie", "brand_wikidata": "Q87378070", "extras": Categories.FUEL_STATION.value}
+    item_attributes = {"brand": "Rotten Robbie", "brand_wikidata": "Q87378070"}
     start_urls = ["https://rottenrobbie.com/locations"]
 
     def parse(self, response: Response, **kwargs):
@@ -16,4 +16,5 @@ class RottenRobbieUSSpider(scrapy.Spider):
             item = DictParser.parse(store)
             item["ref"] = store["@id"]
             item["branch"] = item.pop("name")
+            apply_category(Categories.FUEL_STATION, item)
             yield item
