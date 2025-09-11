638941a7d251298d717326ab2601a7a731b9683a
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-09-11 10:09:15 +0100

    [Huws Gray GB] Fix coord extraction (#14230)

--- locations/spiders/huws_gray_gb.py
+++ locations/spiders/huws_gray_gb.py
@@ -1,7 +1,9 @@
 import re
 
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -11,10 +13,11 @@ class HuwsGrayGBSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://www.huwsgray.co.uk/robots.txt"]
     sitemap_rules = [(r"/storefinder/store/.+-(\d+)$", "parse")]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name")
         item["city"] = None
 
-        if m := re.search(r"storeLocator.initializeSimple\(\d+, (-?\d+\.\d+), (-?\d+\.\d+)\);", response.text):
+        if m := re.search(r"storeFinder24\.InitMap\(\d+, (-?\d+\.\d+), (-?\d+\.\d+)\);", response.text):
             item["lat"], item["lon"] = m.groups()
 
         yield item
