0d3ad804027330a4fed2e8f40cbded3400eb26c0
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-09-11 12:09:25 +0530

    rename the spider as it covers PL and France along with IT

--- locations/spiders/od_store.py
+++ locations/spiders/od_store.py
@@ -2,8 +2,8 @@ from locations.categories import Categories, apply_category
 from locations.json_blob_spider import JSONBlobSpider
 
 
-class OdStoreITSpider(JSONBlobSpider):
-    name = "od_store_it"
+class OdStoreSpider(JSONBlobSpider):
+    name = "od_store"
     item_attributes = {"brand": "ODStore", "brand_wikidata": "Q130492509"}
     start_urls = ["https://odstore.it/wp-admin/admin-ajax.php?action=asl_load_stores&load_all=1&layout=1"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
