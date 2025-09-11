d13d40d13b840265bf42a9e0b6f5d3fc1f8045bd
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-09-11 20:19:05 +0100

    [TK Maxx GB] Add spider

---
+++ locations/spiders/tk_maxx_gb.py
@@ -0,0 +1,10 @@
+
+from locations.storefinders.yext_answers import YextAnswersSpider
+
+
+class TkMaxxGBSpider(YextAnswersSpider):
+    name = "tk_maxx_gb"
+    item_attributes = {"brand": "TK Maxx", "brand_wikidata": "Q23823668"}
+    api_key = "ce8e33e14f7f6706a1a86e05e440d1a0"
+    experience_key = "tk-maxx-search-experience"
+    feature_type = "locations"
