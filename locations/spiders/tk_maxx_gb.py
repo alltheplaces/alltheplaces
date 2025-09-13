82c0cff50914495589c084c328eae23b77463e48
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-09-13 18:12:04 +0100

    Change to YextSearchSpider

--- locations/spiders/tk_maxx_gb.py
+++ locations/spiders/tk_maxx_gb.py
@@ -1,7 +1,7 @@
-from locations.storefinders.yext_answers import YextAnswersSpider
+from locations.storefinders.yext_search import YextSearchSpider
 
 
-class TkMaxxGBSpider(YextAnswersSpider):
+class TkMaxxGBSpider(YextSearchSpider):
     name = "tk_maxx_gb"
     item_attributes = {"brand": "TK Maxx", "brand_wikidata": "Q23823668"}
     api_key = "ce8e33e14f7f6706a1a86e05e440d1a0"
