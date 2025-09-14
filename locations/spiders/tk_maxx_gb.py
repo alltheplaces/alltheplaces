862bd11c9c85e1e56c323206fdd95d434367cbfd
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-09-14 12:43:23 +0100

    Add Homesense

--- locations/spiders/tk_maxx_gb.py
+++ locations/spiders/tk_maxx_gb.py
@@ -7,27 +7,23 @@ from locations.storefinders.yext_answers import YextAnswersSpider
 
 class TkMaxxGBSpider(YextAnswersSpider):
     name = "tk_maxx_gb"
-    item_attributes = {"brand": "TK Maxx", "brand_wikidata": "Q23823668"}
+    STORE_TYPES = {
+        "TKMAXX": {"brand": "TK Maxx", "brand_wikidata": "Q23823668"},
+        "HOMESENSE": {"brand": "Homesense", "brand_wikidata": "Q16844433"},
+    }
     api_key = "ce8e33e14f7f6706a1a86e05e440d1a0"
     experience_key = "tk-maxx-search-experience"
     endpoint = "https://prod-cdn.us.yextapis.com/v2/accounts/me/search/vertical/query"
-    locale = "en-GB"
+    locale="en-GB"
     drop_attributes = {"contact:instagram"}
 
     def parse_item(self, location: dict, item: Feature) -> Iterable[Feature]:
-        item["website"] = (
-            item["website"]
-            + "/uk/en/store/"
-            + item["city"].lower().replace(" ", "-")
-            + "/"
-            + item["street_address"].lower().replace(" ", "-")
-            + "/"
-            + item["ref"]
-        )
+        item["website"] = item["website"]+"/uk/en/store/"+item["city"].lower().replace(" ","-")+"/"+item["street_address"].lower().replace(" ","-")+"/"+item["ref"]
         if "homesense" in item["name"].lower():
-            apply_category(Categories.SHOP_HOME, item)
+            item.update(self.STORE_TYPES.get("HOMESENSE"))
+            apply_category(Categories.SHOP_INTERIOR_DECORATION, item)
+            item["branch"]=item.pop("name").replace("Homesense ","")
         else:
-            apply_category(Categories.SHOP_CLOTHES, item)
-        yield item
-
+            item.update(self.STORE_TYPES.get("TKMAXX"))
+            apply_category(Categories.SHOP_DEPARTMENT_STORE, item)
         yield item
