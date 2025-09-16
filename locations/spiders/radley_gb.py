fe0377bbfcb2a564ad55cd4e60254e3cd7e10ed3
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-09-16 12:06:17 +0100

    Clean name

--- locations/spiders/radley_gb.py
+++ locations/spiders/radley_gb.py
@@ -12,19 +12,18 @@ class RadleyGBSpider(CrawlSpider, StructuredDataSpider):
     name = "radley_gb"
     item_attributes = {
         "brand": "Radley London",
-        "brand_wikidata": "Q7281436",
-        "country": "GB",
+        "brand_wikidata": "Q7281436"
     }
     start_urls = ["https://www.radley.co.uk/pages/stores"]
     rules = [
         Rule(
             LinkExtractor(allow=r"https?://www\.radley\.co\.uk/pages/radley-[a-zA-Z0-9-]+-store"),
-            callback="parse_sd",
-            follow=False,
+            callback="parse_sd"
         ),
     ]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Radley ")
         extract_google_position(item, response)
         apply_category(Categories.SHOP_FASHION_ACCESSORIES, item)
         yield item
