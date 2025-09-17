07f946af173ff753918773ec4376af42d6eee2d7
Spencer Alves <impiaaa@gmail.com>
2025-09-17 01:52:28 -0700

    Fix WpGoMapsSpider on Scrapy 2.13 (#14266)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/bauspezi_de.py
+++ locations/spiders/bauspezi_de.py
@@ -3,8 +3,5 @@ from locations.storefinders.wp_go_maps import WpGoMapsSpider
 
 class BauspeziDESpider(WpGoMapsSpider):
     name = "bauspezi_de"
-    item_attributes = {
-        "brand_wikidata": "Q85324366",
-        "brand": "BauSpezi",
-    }
+    item_attributes = {"brand": "BauSpezi", "brand_wikidata": "Q85324366"}
     allowed_domains = ["bauspezi.de"]
--- locations/spiders/menchies.py
+++ locations/spiders/menchies.py
@@ -4,7 +4,4 @@ from locations.storefinders.wp_go_maps import WpGoMapsSpider
 class MenchiesSpider(WpGoMapsSpider):
     name = "menchies"
     allowed_domains = ["www.menchies.com"]
-    item_attributes = {
-        "brand": "Menchie's",
-        "brand_wikidata": "Q6816528",
-    }
+    item_attributes = {"brand": "Menchie's", "brand_wikidata": "Q6816528"}
