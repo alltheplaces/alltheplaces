74780f08f5e9158cdde63d941f5d72a316442cae
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-09-19 17:31:12 +0100

    [Robert Dyas GB] Fix spider

--- locations/spiders/robert_dyas_gb.py
+++ locations/spiders/robert_dyas_gb.py
@@ -12,6 +12,15 @@ class RobertDyasGBSpider(Spider):
     name = "robert_dyas_gb"
     item_attributes = {"brand": "Robert Dyas", "brand_wikidata": "Q7343720"}
     start_urls = ["https://www.robertdyas.co.uk/storefinder"]
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,  
+        "DEFAULT_REQUEST_HEADERS": {
+          "Host": "www.robertdyas.co.uk",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0",
+          "DNT": "1",
+        },
+    }
+
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         scripttext = response.xpath('//script[contains(text(), "Astound_StoreLocator")]').get()
