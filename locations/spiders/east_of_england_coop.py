c44b923bd06ec1b05dd6348dc87e7a33c8d0a1f3
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-09-24 15:57:57 +0530

    code refactored to fix spider

--- locations/spiders/east_of_england_coop.py
+++ locations/spiders/east_of_england_coop.py
@@ -1,9 +1,12 @@
+from typing import Any
+
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
-from locations.linked_data_parser import LinkedDataParser
-from locations.microdata_parser import MicrodataParser
-from locations.pipelines.address_clean_up import clean_address
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
+from locations.items import Feature
 
 
 class EastOfEnglandCoopSpider(SitemapSpider):
@@ -13,36 +16,28 @@ class EastOfEnglandCoopSpider(SitemapSpider):
         "brand_wikidata": "Q5329759",
         "country": "GB",
     }
-    sitemap_urls = ["https://www.eastofengland.coop/googlesitemap.xml"]
-    sitemap_rules = [
-        (
-            r"https:\/\/www\.eastofengland\.coop\/(supermarket|foodstores|funerals)\/",
-            "parse_item",
-        )
-    ]
+    sitemap_urls = ["https://www.eastofengland.coop/sitemap.xml"]
+    sitemap_rules = [("/store/", "parse")]
 
-    def sitemap_filter(self, entries):
-        for entry in entries:
-            entry["loc"] = entry["loc"].replace("food/stores", "foodstores")
-            yield entry
-
-    def parse_item(self, response):
-        MicrodataParser.convert_to_json_ld(response)
-        item = LinkedDataParser.parse(response, "Store")
-
-        if item is None:
-            return
-
-        if isinstance(item.get("street_address"), list):
-            item["street_address"].reverse()
-            item["street_address"] = clean_address(item["street_address"])
-
-        item["ref"] = item["website"]
-
-        if item.get("lat") is None:
-            item["lat"] = response.xpath('//div[@class="store-map"]/@data-lat').get()
-        if item.get("lon") is None:
-            item["lon"] = response.xpath('//div[@class="store-map"]/@data-long').get()
-
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath("//h2/text()").get()
+        item["street_address"] = response.xpath('//*[@class="flex flex-col gap-1"]/span/text()').get()
+        item["city"] = response.xpath('//*[@class="flex flex-col gap-1"]/span[2]/text()').get()
+        item["postcode"] = response.xpath('//*[@class="flex flex-col gap-1"]/span[3]/text()').get()
+        item["addr_full"] = ",".join([item["street_address"], item["city"], item["postcode"]])
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
         apply_category(Categories.SHOP_SUPERMARKET, item)
-        return item
+        oh = OpeningHours()
+        for day_time in response.xpath('//*[@class="flex flex-col items-center gap-1.5"]//li'):
+            day = day_time.xpath(".//span[1]/text()").get()
+            time = day_time.xpath(".//span[2]").xpath("normalize-space()").get()
+            if time == "Closed":
+                oh.set_closed(day)
+            else:
+                open_time, close_time = time.split(" - ")
+                oh.add_range(day=day, open_time=open_time, close_time=close_time)
+        item["opening_hours"] = oh
+        yield item
