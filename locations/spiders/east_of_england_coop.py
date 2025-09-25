fa9b9130320214bf138aef6d679de4cdace612ef
Cj Malone <CjMalone@mail.com>
2025-09-25 11:06:19 +0100

    Improve branding

--- locations/spiders/east_of_england_coop.py
+++ locations/spiders/east_of_england_coop.py
@@ -11,17 +11,13 @@ from locations.items import Feature
 
 class EastOfEnglandCoopSpider(SitemapSpider):
     name = "east_of_england_coop"
-    item_attributes = {
-        "brand": "East of England CO-OP",
-        "brand_wikidata": "Q5329759",
-        "country": "GB",
-    }
+    EAST_OF_ENGLAND_COOP = {"brand": "East of England CO-OP", "brand_wikidata": "Q5329759"}
+    COOP_DAILY = {"brand": "Co-op Daily", "brand_wikidata": "Q107589681"}
     sitemap_urls = ["https://www.eastofengland.coop/sitemap.xml"]
     sitemap_rules = [("/store/", "parse")]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
-        item["name"] = self.item_attributes["brand"]
         item["branch"] = response.xpath("//h2/text()").get()
         item["street_address"] = response.xpath('//*[@class="flex flex-col gap-1"]/span/text()').get()
         item["city"] = response.xpath('//*[@class="flex flex-col gap-1"]/span[2]/text()').get()
@@ -29,14 +25,25 @@ class EastOfEnglandCoopSpider(SitemapSpider):
         item["addr_full"] = ",".join([item["street_address"], item["city"], item["postcode"]])
         item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
         item["ref"] = item["website"] = response.url
+
         extract_google_position(item, response)
+
         if "post-office" in response.url:
             apply_category(Categories.POST_OFFICE, item)
         elif "food" in response.url:
-            apply_category(Categories.SHOP_SUPERMARKET, item)
+            if item["branch"].endswith(" Supermarket"):
+                item.update(self.EAST_OF_ENGLAND_COOP)
+                apply_category(Categories.SHOP_SUPERMARKET, item)
+            else:
+                item.update(self.COOP_DAILY)
+                apply_category(Categories.SHOP_CONVENIENCE, item)
         elif "travel" in response.url:
+            item["name"] = "CO-OP Travel"
+            item.update(self.EAST_OF_ENGLAND_COOP)
             apply_category(Categories.SHOP_TRAVEL_AGENCY, item)
         elif "funerals" in response.url:
+            item["name"] = "East of England CO-OP Funeral Services"
+            item.update(self.EAST_OF_ENGLAND_COOP)
             apply_category(Categories.SHOP_FUNERAL_DIRECTORS, item)
 
         oh = OpeningHours()
@@ -49,4 +56,5 @@ class EastOfEnglandCoopSpider(SitemapSpider):
                 open_time, close_time = time.split(" - ")
                 oh.add_range(day=day, open_time=open_time, close_time=close_time)
         item["opening_hours"] = oh
+
         yield item
