235c7fac6bb991768069c0baed487a75f4dad17d
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-10-03 17:35:22 +0530

    code refactored to fix spider

--- locations/spiders/mugg_and_bean.py
+++ locations/spiders/mugg_and_bean.py
@@ -1,59 +1,56 @@
-from typing import Iterable
+from typing import Any
 
-from scrapy.http import Request, Response
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories, Extras, PaymentMethods, apply_category, apply_yes_no
+from locations.categories import Extras, PaymentMethods, apply_yes_no
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.linked_data_parser import LinkedDataParser
-from locations.storefinders.go_review_api import GoReviewApiSpider
 
 
-class MuggAndBeanSpider(GoReviewApiSpider):
+class MuggAndBeanSpider(SitemapSpider):
     name = "mugg_and_bean"
     item_attributes = {"brand": "Mugg & Bean", "brand_wikidata": "Q6932113"}
-
-    domain_list = [
-        "locations.muggandbean.co.za",
-        # "kenyalocations.muggandbean.africa", # website exists but has no stores listed as of 2025-08-31
-        "malawilocations.muggandbean.africa",
-        "mauritiuslocations.muggandbean.africa",
-        # Namibia and Zambia also have locations but websites just have a basic list
+    sitemap_urls = [
+        "https://locations.muggandbean.co.za/site-map.xml",
+        "https://malawilocations.muggandbean.africa/site-map.xml",
+        "https://mauritiuslocations.muggandbean.africa/site-map.xml",
     ]
+    sitemap_rules = [("/restaurants-", "parse")]
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Request]:
-        yield Request(
-            url=item["website"],
-            meta={"item": item, "attributes": feature.get("attributes")},
-            callback=self.parse_location_ld,
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = (
+            response.xpath('//*[@id="banner"]//p/text()')
+            .get()
+            .removeprefix("Mugg & Bean On The Move ")
+            .removeprefix("Mugg & Bean ")
         )
-
-    def parse_location_ld(self, response) -> Iterable[Feature]:
-        ld_item = LinkedDataParser.parse(response, "Restaurant")
-
-        ld_item["ref"] = response.meta["item"]["ref"]
-        ld_item["branch"] = ld_item.pop("name").removeprefix("Mugg & Bean ")
-
-        if ld_item.get("postcode") is not None and int(ld_item.get("postcode")) == 0:
-            ld_item.pop("postcode")
-
-        if "image" in ld_item:
-            ld_item.pop("image")
-
-        if response.meta["attributes"] is not None:
-            attributes = [attribute["value"] for attribute in response.meta["attributes"]]
-
-            apply_yes_no(Extras.DELIVERY, ld_item, "Delivery" in attributes)
-            apply_yes_no(PaymentMethods.CARDS, ld_item, "Card" in attributes)
-            apply_yes_no(PaymentMethods.DEBIT_CARDS, ld_item, "Debit cards" in attributes)
-            apply_yes_no(PaymentMethods.CREDIT_CARDS, ld_item, "Credit cards" in attributes)
-            apply_yes_no(Extras.DRIVE_THROUGH, ld_item, "Drive-through" in attributes)
-            apply_yes_no(Extras.DRIVE_THROUGH, ld_item, "Drive Thru" in attributes)
-            apply_yes_no(Extras.BREAKFAST, ld_item, "Breakfast" in attributes)
-            apply_yes_no(Extras.BRUNCH, ld_item, "Brunch" in attributes)
-
-            for attribute in attributes:
-                self.crawler.stats.inc_value(f"atp/{self.name}/attribute/{attribute}")
-
-        apply_category(Categories.FAST_FOOD, ld_item)
-
-        yield ld_item
+        item["addr_full"] = response.xpath('//*[@id="location"]//p/text()').get()
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]//text()').get()
+        item["email"] = response.xpath('//*[contains(@href,"mailto:")]//text()').get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
+        oh = OpeningHours()
+        for day_time in response.xpath('//*[@class="operating-hours flex py-3"]//*[@class="p-3"]'):
+            day = day_time.xpath(".//h3/text()").get().strip()
+            time = day_time.xpath(".//span/text()").get()
+            if time == "Open 24 hours":
+                item["opening_hours"] = "24/7"
+            elif time == "Closed":
+                oh.set_closed(day)
+            else:
+                open_time, close_time = day_time.xpath(".//span/text()").get().split(" | ")
+                oh.add_range(day=day, open_time=open_time, close_time=close_time, time_format="%I:%M %p")
+        item["opening_hours"] = oh
+        attributes = response.xpath('//*[@class = "p-2"]//text()').getall()
+        apply_yes_no(Extras.DELIVERY, item, "Delivery" in attributes)
+        apply_yes_no(PaymentMethods.CARDS, item, "Card" in attributes)
+        apply_yes_no(PaymentMethods.DEBIT_CARDS, item, "Debit cards" in attributes)
+        apply_yes_no(PaymentMethods.CREDIT_CARDS, item, "Credit cards" in attributes)
+        apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive-through" in attributes)
+        apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive Thru" in attributes)
+        apply_yes_no(Extras.BREAKFAST, item, "Breakfast" in attributes)
+        apply_yes_no(Extras.BRUNCH, item, "Brunch" in attributes)
+        yield item
