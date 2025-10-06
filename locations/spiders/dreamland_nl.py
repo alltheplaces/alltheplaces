f19b897d0862f8fb94909ae6674cdb83370a8cce
Cj Malone <CjMalone@mail.com>
2025-10-06 18:35:37 +0100

    [DreamLand NL] Add spider

---
+++ locations/spiders/dreamland_nl.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
+
+
+class DreamlandNLSpider(Spider):
+    name = "dreamland_nl"
+    item_attributes = {"brand_wikidata": "Q13574205"}
+    start_urls = ["https://www.dreamland.nl/vestigingen"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.xpath('//*[@class="overview-store"]'):
+            item = Feature()
+            item["ref"] = location.xpath("@data-store-id").get()
+            item["lat"] = location.xpath("@data-latitude").get()
+            item["lon"] = location.xpath("@data-longitude").get()
+            item["branch"] = (
+                location.xpath('./h2[@class="overview-store__name"]/a/text()').get().removeprefix("DreamLand ")
+            )
+            item["website"] = response.urljoin(location.xpath('./h2[@class="overview-store__name"]/a/@href').get())
+            item["street_address"] = location.xpath("./address/text()").get()
+            item["addr_full"] = merge_address_lines(location.xpath("./address/text()").getall())
+
+            apply_category(Categories.SHOP_TOYS, item)
+
+            yield item
