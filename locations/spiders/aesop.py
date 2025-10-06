97035ae804640cde7142caa9dc2f6a8de14fae52
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-10-06 17:28:14 +0530

    Fix Asop Spider

--- locations/spiders/aesop.py
+++ locations/spiders/aesop.py
@@ -2,7 +2,9 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class AesopSpider(CrawlSpider, StructuredDataSpider):
@@ -10,6 +12,8 @@ class AesopSpider(CrawlSpider, StructuredDataSpider):
     item_attributes = {"brand": "Aesop", "brand_wikidata": "Q4688560"}
     start_urls = ["https://www.aesop.com/stores/all"]
     rules = [Rule(LinkExtractor(allow=r"/stores/"), callback="parse_sd", follow=True)]
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["branch"] = item.pop("name").replace("Aesop ", "")
