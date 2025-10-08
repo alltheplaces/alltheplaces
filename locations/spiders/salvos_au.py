668cf97f645dfc4c72551d5e6c70c0d0432b66f3
Ian Dees <ian.dees@gmail.com>
2025-10-08 08:17:34 -0500

    Cleaning up data parsing

--- locations/spiders/salvos_au.py
+++ locations/spiders/salvos_au.py
@@ -13,21 +13,26 @@ class SalvosAUSpider(SitemapSpider):
     sitemap_rules = [("/stores/", "parse")]
 
     def parse(self, response):
-        store_data = json.loads(response.xpath('//*[@id="__NEXT_DATA__"]/text()').get())["props"]["pageProps"].get(
-            "store"
-        )
-        if store_data.get("StoreID"):
-            item = DictParser.parse(store_data)
-            item["website"] = response.url
-            item["branch"] = item.pop("name")
-            oh = OpeningHours()
-            for day, time in store_data["OpeningHours"].items():
-                day = day
-                if time == "Close":
-                    oh.set_closed(day)
-                else:
-                    open_time = time["Opening"]
-                    close_time = time["Closing"]
-                    oh.add_range(day=day, open_time=open_time, close_time=close_time, time_format="%H:%M:%S")
-            item["opening_hours"] = oh
-            yield item
+        next_data = response.xpath('//*[@id="__NEXT_DATA__"]/text()').get() or ""
+        json_data = json.loads(next_data)
+        store_data = json_data.get("props", {}).get("pageProps", {}).get("store", {})
+
+        if not store_data.get("StoreID"):
+            return
+
+        item = DictParser.parse(store_data)
+        item["website"] = response.url
+        item["branch"] = item.pop("name")
+
+        oh = OpeningHours()
+        for day, time in store_data["OpeningHours"].items():
+            day = day
+            if time == "Close":
+                oh.set_closed(day)
+            else:
+                open_time = time["Opening"]
+                close_time = time["Closing"]
+                oh.add_range(day=day, open_time=open_time, close_time=close_time, time_format="%H:%M:%S")
+        item["opening_hours"] = oh
+
+        yield item
