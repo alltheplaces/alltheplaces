c45739b6797ab101bf42e5e2a8d3c25f0bdc6102
Karen Kerr <karenkerrgithub@gmail.com>
2025-10-08 15:54:03 +0100

    [Motrio] Add spider

---
+++ locations/spiders/motrio.py
@@ -0,0 +1,46 @@
+import scrapy
+from scrapy.http import JsonRequest
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+
+
+class MotrioSpider(scrapy.Spider):
+    name = "motrio"
+    item_attributes = {"brand": "Motrio", "brand_wikidata": "Q6918585"}
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def make_request(self, page: int, page_size: int = 100) -> JsonRequest:
+        return JsonRequest(
+            "https://www.motrio.fr/api/establishments?domain=motrio&size={}&page={}".format(page_size, page)
+        )
+
+    def start_requests(self):
+        yield self.make_request(0)
+
+    def parse(self, response):
+        data = response.json()
+        for store in data.get("content"):
+            store.update(store.pop("billingAddress"))
+            item = DictParser.parse(store)
+            item["branch"] = item.pop("name")
+            item["street_address"] = item.pop("street")
+            item["image"] = store.get("bannerPictureUrl")
+            item["facebook"] = store.get("facebookUrl")
+            if lat_lon := store.get("location"):
+                item["lon"], item["lat"] = lat_lon["coordinates"]
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
+            item["opening_hours"] = OpeningHours()
+            for day, time in store.get("timeTable").items():
+                if time in [" ", None, []]:
+                    continue
+                for open_close_time in time:
+                    open_time = open_close_time.get("startAt")
+                    close_time = open_close_time.get("endAt")
+                    item["opening_hours"].add_range(
+                        day=day, open_time=open_time, close_time=close_time, time_format="%H:%M:%S"
+                    )
+            yield item
+        if not data["last"]:
+            yield self.make_request(data["number"] + 1, data["size"])
