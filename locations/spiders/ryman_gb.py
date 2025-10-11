f40220810c5558bca20c94a5d212521ab260e949
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-10-11 11:57:33 +0100

    Remove unnecessary proxy calls

--- locations/spiders/ryman_gb.py
+++ locations/spiders/ryman_gb.py
@@ -2,19 +2,20 @@ import json
 from typing import Any
 
 import scrapy
-from scrapy import Spider
 from scrapy.http import Response
 
+from locations.json_blob_spider import JSONBlobSpider
 from locations.dict_parser import DictParser
 from locations.linked_data_parser import LinkedDataParser
 
 
-class RymanGBSpider(Spider):
+class RymanGBSpider(JSONBlobSpider):
     name = "ryman_gb"
     item_attributes = {"brand": "Ryman", "brand_wikidata": "Q7385188"}
     start_urls = ["https://www.ryman.co.uk/storefinder/"]
     custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Host": "www.ryman.co.uk"}}
     requires_proxy = True
+    drop_attributes = {"facebook", "twitter"}
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         scripts = response.xpath('//script[@type="text/x-magento-init"]//text()').getall()
@@ -22,15 +23,9 @@ class RymanGBSpider(Spider):
             if "locations" in script:
                 result = json.loads(script)
                 for store in DictParser.get_nested_key(result, "locations"):
-                    yield scrapy.Request(
-                        store["store_url"],
-                        callback=self.parse_store_page,
-                        cb_kwargs=dict(store=store),
-                    )
-
-    def parse_store_page(self, response, store):
-        if item := LinkedDataParser.parse(response, "Store"):
-            item["lat"] = store["latitude"]
-            item["lon"] = store["longitude"]
-            item["branch"] = item.pop("name")
-            yield item
+                    item = DictParser.parse(store)
+                    item["lat"] = store["latitude"]
+                    item["lon"] = store["longitude"]
+                    item["branch"] = item.pop("name")
+                    item["street_address"] = item.pop("addr_full")
+                    yield item
