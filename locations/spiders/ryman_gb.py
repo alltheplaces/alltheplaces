2891d766a3f54598c33a3044ef8cc8832a5025a3
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-10-11 12:44:32 +0100

    Add opening hours

--- locations/spiders/ryman_gb.py
+++ locations/spiders/ryman_gb.py
@@ -1,17 +1,21 @@
+
 import json
 from typing import Any
 
+import scrapy
 from scrapy.http import Response
 
-from locations.dict_parser import DictParser
+from locations.hours import OpeningHours, DAYS_FULL
 from locations.json_blob_spider import JSONBlobSpider
+from locations.dict_parser import DictParser
+from locations.linked_data_parser import LinkedDataParser
 
 
 class RymanGBSpider(JSONBlobSpider):
     name = "ryman_gb"
     item_attributes = {"brand": "Ryman", "brand_wikidata": "Q7385188"}
     start_urls = ["https://www.ryman.co.uk/storefinder/"]
-    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Host": "www.ryman.co.uk"}}
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"Host": "www.ryman.co.uk"}, "ROBOTSTXT_OBEY": False} 
     requires_proxy = True
     drop_attributes = {"facebook", "twitter"}
 
@@ -25,5 +29,22 @@ class RymanGBSpider(JSONBlobSpider):
                     item["lat"] = store["latitude"]
                     item["lon"] = store["longitude"]
                     item["branch"] = item.pop("name")
+                    item["email"] = store["cs_email"]
                     item["street_address"] = item.pop("addr_full")
+                    opening_hours = OpeningHours()
+                    times=json.loads(store["schedule_data"])
+                    for day in DAYS_FULL:
+                        if "lunch_break_ends" in times[day.lower()] and times[day.lower()]["lunch_break_starts"] is not None:
+                            opening_hours.add_range(
+                                day=day, open_time=times[day.lower()]["open_time"], close_time=times[day.lower()]["lunch_break_starts"], time_format="%H:%M"
+                            )
+                            opening_hours.add_range(
+                                day=day, open_time=times[day.lower()]["lunch_break_ends"], close_time=times[day.lower()]["closing_time"], time_format="%H:%M"
+                            )
+                        else:
+                            opening_hours.add_range(
+                                day=day, open_time=times[day.lower()]["open_time"], close_time=times[day.lower()]["closing_time"], time_format="%H:%M"
+                            )
+                    item["opening_hours"] = opening_hours
                     yield item
+
