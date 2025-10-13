d7aca6f703c8017381a28123f6f8fc0e02416e49
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2025-10-13 19:08:28 +0100

    [Zara] Add Spider

---
+++ locations/spiders/zara.py
@@ -0,0 +1,44 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.hours import DAYS, OpeningHours
+from locations.user_agents import BROWSER_DEFAULT
+from locations.categories import Categories, apply_category, Clothes, apply_clothes
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class ZaraSpider(JSONBlobSpider):
+    name = "zara"
+    item_attributes = {"brand": "Zara", "brand_wikidata": "Q147662"}
+    start_urls = [
+        "https://www.zara.com/uk/en/stores-locator/extended/search?lat=51.5072178&lng=-0.1275862&isDonationOnly=false&showOnlyPickup=false&showStoresCapacity=false&radius=3000&ajax=true"
+    ]
+    custom_settings = {
+       "ROBOTSTXT_OBEY": False,
+       "DEFAULT_REQUEST_HEADERS": {
+            "Host": "www.zara.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Connection": "keep-alive",
+        }
+    }
+#    requires_proxy = True
+    drop_attributes = {"facebook", "twitter"}
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        apply_category(Categories.SHOP_CLOTHES, item)
+        if "Woman" in feature.get("sections", []):
+            apply_clothes([Clothes.WOMEN], item)
+        if "Man" in feature.get("sections", []):
+            apply_clothes([Clothes.MEN], item)
+        if "Kids" in feature.get("sections", []):
+            apply_clothes([Clothes.CHILDREN], item)
+        item["street_address"] = " ".join(feature["addressLines"])
+        item["opening_hours"] = OpeningHours()
+        if "openTime" in feature:
+            for i in range(7):
+                item["opening_hours"].add_range(DAYS[i],feature["openingHours"][i]["openingHoursInterval"][0]["openTime"],feature["openingHours"][i]["openingHoursInterval"][0]["closeTime"])
+        yield item
