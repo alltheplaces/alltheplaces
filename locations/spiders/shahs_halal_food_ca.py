a75ed038c63fd335d6513f5957705ff17d711bba
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-10-15 19:59:56 +0530

    Add opening hours

--- locations/spiders/shahs_halal_food_ca.py
+++ locations/spiders/shahs_halal_food_ca.py
@@ -1,7 +1,9 @@
+import json
 from typing import Iterable
 
 from scrapy.http import Response
 
+from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 from locations.spiders.shahs_halal_food_us import ShahsHalalFoodUSSpider
@@ -15,4 +17,13 @@ class ShahsHalalFoodCASpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["street_address"] = item.pop("street")
+        item["opening_hours"] = self.parse_opening_hours(json.loads(feature["open_hours"]))
         yield item
+
+    def parse_opening_hours(self, opening_hours: dict) -> OpeningHours:
+        oh = OpeningHours()
+        for day, hours in opening_hours.items():
+            for shift in hours:
+                open_time, close_time = shift.split("-")
+                oh.add_range(day, open_time.strip(), close_time.strip(), "%I:%M %p")
+        return oh
