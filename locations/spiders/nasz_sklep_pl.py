aba746e71747def14770c86e2742aa7f81859012
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2025-10-30 13:21:20 +0000

    code updated

--- locations/spiders/nasz_sklep_pl.py
+++ locations/spiders/nasz_sklep_pl.py
@@ -10,13 +10,23 @@ class NaszSklepPLSpider(Spider):
     name = "nasz_sklep_pl"
     item_attributes = {"brand": "Nasz Sklep", "brand_wikidata": "Q62070369"}
     start_urls = ["https://nasz-sklep.pl/sklepy-wlasne/"]
+    brands = {
+        "DS": {"brand": "Delikatesy Sezam", "brand_wikidata": "Q120173828"},
+        "Li": {"brand": "Livio", "brand_wikidata": "Q108599511"},
+        "NS": {"brand": "Nasz Sklep", "brand_wikidata": "Q62070369"},
+    }
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.xpath("//table//tr[position()>1]"):
             item = Feature()
             item["ref"] = location.xpath("td[1]/text()").get()
-            item["branch"] = location.xpath("td[2]/text()").get()
+            item["city"] = location.xpath("td[2]/text()").get()
             item["street_address"] = location.xpath("td[3]/text()").get()
-            item["email"] = location.xpath("td[4]/text()").get()
-            item["phone"] = location.xpath("td[5]/text()").get()
+            item["email"] = location.xpath("td[4]//text()").get()
+            item["phone"] = location.xpath("td[5]//text()").get()
+            brand = item["ref"][0:2]
+            if b := self.brands.get(brand):
+                item.update(b)
+            else:
+                self.crawler.stats.inc_value("{}/unmapped_brand/{}".format(self.name, brand))
             yield item
