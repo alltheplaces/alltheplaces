2c71e6983bfcaec7cfad6830f5384a8d45f878db
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-11-05 17:51:43 +0530

    Get rid of hardcoded API query params

--- locations/spiders/metro_cash_and_carry.py
+++ locations/spiders/metro_cash_and_carry.py
@@ -2,7 +2,7 @@ import re
 from typing import Any
 from urllib.parse import urljoin
 
-from scrapy import Selector
+from scrapy import Request, Selector
 from scrapy.http import JsonRequest, Response
 from scrapy.spiders import Spider
 
@@ -16,21 +16,30 @@ class MetroCashAndCarrySpider(Spider):
     name = "metro_cash_and_carry"
     METRO = ("Metro", "Q13610282")
     MAKRO = ("Makro", "Q704606")
-    start_urls = ["https://www.metroag.de/en/about-us/brands"]
+    start_urls = ["https://cdn.metro-online.com/bundles/js-store-locator.js"]
     custom_settings = {
         "ROBOTSTXT_OBEY": False,
         "USER_AGENT": BROWSER_DEFAULT,
         "CONCURRENT_REQUESTS": 1,
         "DOWNLOAD_TIMEOUT": 120,
     }
+    # API query params default values
+    s = "0F3B38A3-7330-4544-B95B-81FC80A6BB6F"
+    v = "D5BC0757-6F6D-4BDB-BFBD-065D34D0B4A3"
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
+        # Check for updated values
+        if match := re.search(r"s={([A-Fa-f0-9-]+)}&v={([A-Fa-f0-9-]+)}", response.text):
+            self.s, self.v = match.groups()
+        yield Request(url="https://www.metroag.de/en/about-us/brands", callback=self.parse_country)
+
+    def parse_country(self, response: Response, **kwargs: Any) -> Any:
         for url in response.xpath('//a[contains(@title, "MAKRO") or contains(@title, "METRO")]/@href').getall():
             if re.search(r"www\.(metro|makro)[-.]", url):
                 yield JsonRequest(
                     urljoin(
                         url,
-                        "/sxa/search/results?s={0F3B38A3-7330-4544-B95B-81FC80A6BB6F}&v={D5BC0757-6F6D-4BDB-BFBD-065D34D0B4A3}&p=1000&o=StoreName,Ascending&g=",
+                        f"/sxa/search/results?s={self.s}&v={self.v}&p=1000&o=StoreName,Ascending&g=",
                     ),
                     callback=self.parse_locations,
                 )
