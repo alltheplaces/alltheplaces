33efefa7e5a09ded6f773c7a02fbafc47a9a6ccc
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-11-07 17:32:13 +0530

    updated sitemap rules to fix spider

--- locations/spiders/casper_us.py
+++ locations/spiders/casper_us.py
@@ -2,12 +2,13 @@ from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class CasperUSSpider(SitemapSpider, StructuredDataSpider):
     name = "casper_us"
     item_attributes = {"brand": "Casper", "brand_wikidata": "Q20539294", "extras": Categories.SHOP_BED.value}
     sitemap_urls = ["https://stores.casper.com/sitemap.xml"]
-    sitemap_rules = [(r"stores\.casper\.com\/casper\-.*$", "parse_sd")]
-    wanted_types = ["HomeGoodsStore"]
+    sitemap_rules = [(r"stores\.casper\.com\/[^/]+/[^/]+/.*html$", "parse_sd")]
     drop_attributes = {"image"}
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
