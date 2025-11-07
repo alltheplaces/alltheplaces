44911a3d81ba70d7326b82b03421851fb134dfd7
Ian Dees <ian.dees@gmail.com>
2025-11-07 08:27:32 -0600

    Cleaning up if statements

--- locations/spiders/halfords_gb.py
+++ locations/spiders/halfords_gb.py
@@ -20,33 +20,42 @@ class HalfordsGBSpider(scrapy.Spider):
         ]["queries"]
         for location in raw_data:
             data = location.get("state").get("data")
-            if isinstance(data, dict) and data.get("stores"):
-                for data in data.get("stores"):
-                    if data.get("coordinates").get("latitude") == 0:
-                        continue
-                    item = DictParser.parse(data)
-                    item["street_address"] = merge_address_lines([item.pop("street_address"), data.get("address2")])
-                    item["website"] = "https://www.halfords.com/locations/" + data["storeDetailsLink"]
-                    oh = OpeningHours()
-                    for day_time in data["storeHours"]["workingHours"]:
-                        for day, time in day_time.items():
-                            for open_close_time in time:
-                                if open_close_time in [{}, {"isToday": True}]:
-                                    continue
-                                open_time = open_close_time.get("Start").strip()
-                                close_time = open_close_time.get("Finish").strip()
-                                oh.add_range(day=day, open_time=open_time, close_time=close_time)
-                    item["opening_hours"] = oh
 
-                    if data["storePrefix"] == "Halfords Autocentre":
-                        item["brand_wikidata"] = "Q5641894"
-                    elif data["storePrefix"] == "Halfords Garage Services":
-                        item["brand"] = "Halfords"
-                        apply_category(Categories.SHOP_CAR_REPAIR, item)
-                    elif data["storePrefix"] == "National Tyres and Autocare":
-                        item["brand_wikidata"] = "Q6979055"
-                    elif data["storePrefix"] == "Halfords Store":
-                        item["brand_wikidata"] = "Q3398786"
-                    else:
-                        item["brand_wikidata"] = "Q5641894"
-                    yield item
+            if not isinstance(data, dict):
+                continue
+
+            store_data = data.get("stores")
+            if not store_data:
+                continue
+
+            for data in store_data:
+                if data.get("coordinates").get("latitude") == 0:
+                    continue
+
+                item = DictParser.parse(data)
+                item["street_address"] = merge_address_lines([item.pop("street_address"), data.get("address2")])
+                item["website"] = "https://www.halfords.com/locations/" + data["storeDetailsLink"]
+
+                oh = OpeningHours()
+                for day_time in data["storeHours"]["workingHours"]:
+                    for day, time in day_time.items():
+                        for open_close_time in time:
+                            if open_close_time in [{}, {"isToday": True}]:
+                                continue
+                            open_time = open_close_time.get("Start").strip()
+                            close_time = open_close_time.get("Finish").strip()
+                            oh.add_range(day=day, open_time=open_time, close_time=close_time)
+                item["opening_hours"] = oh
+
+                if data["storePrefix"] == "Halfords Autocentre":
+                    item["brand_wikidata"] = "Q5641894"
+                elif data["storePrefix"] == "Halfords Garage Services":
+                    item["brand"] = "Halfords"
+                    apply_category(Categories.SHOP_CAR_REPAIR, item)
+                elif data["storePrefix"] == "National Tyres and Autocare":
+                    item["brand_wikidata"] = "Q6979055"
+                elif data["storePrefix"] == "Halfords Store":
+                    item["brand_wikidata"] = "Q3398786"
+                else:
+                    item["brand_wikidata"] = "Q5641894"
+                yield item
