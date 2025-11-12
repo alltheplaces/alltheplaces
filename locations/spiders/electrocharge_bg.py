ce6443563c6ee889a8e9c7c77e7e7b2fb1e60c76
Cj Malone <CjMalone@mail.com>
2025-11-12 10:28:12 +0000

    Add category

--- locations/spiders/electrocharge_bg.py
+++ locations/spiders/electrocharge_bg.py
@@ -1,20 +1,26 @@
-import scrapy
+from typing import Any
 
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 
 
-class ElectrochargeBGSpider(scrapy.Spider):
+class ElectrochargeBGSpider(Spider):
     name = "electrocharge_bg"
-    item_attributes = {"brand": "Electrocharge", "brand_wikidata": "Q133307258", "country": "BG"}
+    item_attributes = {"brand": "Electrocharge", "brand_wikidata": "Q133307258"}
     allowed_domains = ["electrocharge.bg"]
     start_urls = ["https://electrocharge.bg/bg/electrocharge/fetch_charging_stations/"]
     no_refs = True
 
-    def parse(self, response):
-        data = response.json()
-        for station in data["stations"]:
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for station in response.json()["stations"]:
             item = Feature()
             item["addr_full"] = station["location"]
             item["lat"] = station["coordinates"]["lat"]
             item["lon"] = station["coordinates"]["lng"]
+
+            apply_category(Categories.CHARGING_STATION, item)
+
             yield item
