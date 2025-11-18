e209e574f40d44103b3b5b3838beaa2f20152ca7
Cj Malone <CjMalone@mail.com>
2025-11-18 09:58:11 +0000

    Clean name

--- locations/spiders/basic_fit_fr.py
+++ locations/spiders/basic_fit_fr.py
@@ -1,35 +1,27 @@
+from scrapy.http import Response
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
 from locations.hours import DAYS_FR, OpeningHours
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class BasicFitFRSpider(CrawlSpider, StructuredDataSpider):
     name = "basic_fit_fr"
-    item_attributes = {
-        "brand": "Basic-Fit",
-        "brand_wikidata": "Q40165577",
-    }
+    item_attributes = {"brand": "Basic-Fit", "brand_wikidata": "Q40165577"}
     allowed_domains = ["www.basic-fit.com"]
     start_urls = ["https://www.basic-fit.com/fr-fr/salles-de-sport"]
     rules = [
-        Rule(
-            LinkExtractor(allow=r"https://www.basic-fit.com/fr-fr/salles-de-sport"),
-            follow=True,
-        ),
-        Rule(
-            LinkExtractor(allow=r"https://www.basic-fit.com/fr-fr/clubs/.*.html"),
-            callback="parse_sd",
-        ),
+        Rule(LinkExtractor(allow=r"https://www.basic-fit.com/fr-fr/salles-de-sport")),
+        Rule(LinkExtractor(allow=r"https://www.basic-fit.com/fr-fr/clubs/.*.html"), callback="parse_sd"),
     ]
 
-    def post_process_item(self, item, response, ld_data):
-        apply_category(Categories.GYM, item)
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Salle de sport ")
 
-        hours_elements_text = response.css(".club-opening-hours").css("::text").getall()
-        if hours_elements_text:
+        if hours_elements_text := response.css(".club-opening-hours").css("::text").getall():
             hours_text = " ".join([text.strip() for text in hours_elements_text if text.strip()])
             if hours_text:
                 if "24H/24" in hours_text or "24h/24" in hours_text:
@@ -47,5 +39,7 @@ class BasicFitFRSpider(CrawlSpider, StructuredDataSpider):
                     )
                     item["opening_hours"] = OpeningHours()
                     item["opening_hours"].add_ranges_from_string(hours_text, days=DAYS_FR)
+
+        apply_category(Categories.GYM, item)
 
         yield item
