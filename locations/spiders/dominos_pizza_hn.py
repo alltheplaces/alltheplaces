9362e227077eb044f4759afd3bb99b0c03bb3f4e
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-11-18 14:52:37 +0530

    updated domains to fix dominos_pizza spiders

--- locations/spiders/dominos_pizza_hn.py
+++ locations/spiders/dominos_pizza_hn.py
@@ -1,11 +1,12 @@
 from locations.hours import DAYS_ES
-from locations.storefinders.dominos_pizza_international import DominosPizzaInternationalSpider
+from locations.storefinders.dominos_pizza_international import \
+    DominosPizzaInternationalSpider
 
 
 class DominosPizzaHNSpider(DominosPizzaInternationalSpider):
     name = "dominos_pizza_hn"
     region_code = "HN"
     dpz_market = "HONDURAS"
-    domain = "order.golo01.dominos.com"
+    domain = "order.golo04.dominos.com"
     dpz_language = "es"
     days = DAYS_ES
