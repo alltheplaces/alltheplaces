905562a9226731fbe88dafd67aa3bcd5cda4812e
David Hicks <david@hicks.id.au>
2025-11-20 02:18:55 +1100

    Replace deprecated start_requests function in 'i' spiders (#14563)

--- locations/spiders/inpost_gb.py
+++ locations/spiders/inpost_gb.py
@@ -1,6 +1,6 @@
-from typing import Any, Iterable
+from typing import Any, AsyncIterator
 
-from scrapy import Request, Spider
+from scrapy import Spider
 from scrapy.http import JsonRequest, Response
 
 from locations.categories import apply_yes_no
@@ -21,7 +21,7 @@ class InpostGBSpider(Spider):
             meta={"page": page},
         )
 
-    def start_requests(self) -> Iterable[Request]:
+    async def start(self) -> AsyncIterator[JsonRequest]:
         yield self.make_request(1)
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
