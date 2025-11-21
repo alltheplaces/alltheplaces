96bf7c5383a1b6b885c4bbd70b6fe0e014a5fd36
Cj Malone <CjMalone@mail.com>
2025-11-21 12:06:21 +0000

    Clean name

--- locations/spiders/the_good_guys_au.py
+++ locations/spiders/the_good_guys_au.py
@@ -1,5 +1,7 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -8,3 +10,7 @@ class TheGoodGuysAUSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "The Good Guys", "brand_wikidata": "Q7737217"}
     sitemap_urls = ["https://www.thegoodguys.com.au/sitemap.xml"]
     sitemap_rules = [("/stores/", "parse_sd")]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("The Good Guys ")
+        yield item
