5ddf729a6bc0cea0b0fa5d96b0ad21220314e343
Karen Kerr <karenkerrgithub@gmail.com>
2025-11-25 10:58:31 +0000

    [WiseUp BR] Add spider

---
+++ locations/spiders/wise_up_br.py
@@ -0,0 +1,30 @@
+import scrapy
+from scrapy.http import JsonRequest
+
+from locations.categories import apply_category
+from locations.dict_parser import DictParser
+
+
+class WiseupBRSpider(scrapy.Spider):
+    name = "wise_up_br"
+    item_attributes = {"brand": "Wise Up", "brand_wikidata": "Q123166153"}
+
+    def start_requests(self):
+        yield JsonRequest(url="https://api.wiseup.com/v1/franchises/regions?brand=Wise%20Up&country=BR")
+
+    def parse(self, response, **kwargs):
+        for region in response.json():
+            code = region.get("region")
+            yield JsonRequest(
+                url=f"https://api.wiseup.com/v1/franchises/units?country=BR&brand=Wise%20Up&region={code}",
+                callback=self.parse_details,
+            )
+
+    def parse_details(self, response, **kwargs):
+        for school in response.json():
+            item = DictParser.parse(school)
+            item["branch"] = item.pop("name")
+            item["street_address"] = school.get("addressLine")
+            item["housenumber"] = school.get("addressNumber")
+            apply_category({"amenity": "language_school"}, item)
+            yield item
