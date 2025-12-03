56102ef85e2b0d0911363bca4b294502a42dbde0
Cj Malone <Cj-Malone@users.noreply.github.com>
2025-12-03 10:24:55 +0000

    [Castorama FR] Clean up spider (#14641)

--- locations/spiders/castorama_fr.py
+++ locations/spiders/castorama_fr.py
@@ -1,26 +1,27 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS_EN, OpeningHours
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class CastoramaFRSpider(SitemapSpider, StructuredDataSpider):
     name = "castorama_fr"
     item_attributes = {"brand": "Castorama", "brand_wikidata": "Q966971"}
-    sitemap_urls = ["https://www.castorama.fr/static/sitemap.xml"]
+    sitemap_urls = ["https://www.castorama.fr/robots.txt"]
+    sitemap_follow = ["sitemap-magasins.xml"]
     sitemap_rules = [(r"/store/\d+", "parse_sd")]
-    drop_attributes = {"image"}
+    time_format = "%H:%M:%S Europe/Paris"
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        oh = OpeningHours()
-        for opening_hour in ld_data["openingHoursSpecification"]:
-            if "opens" in opening_hour:
-                oh.add_range(
-                    DAYS_EN[opening_hour["dayOfWeek"]],
-                    opening_hour["opens"][0:5],
-                    opening_hour["closes"][0:5],
-                )
-                item["opening_hours"] = oh
-        apply_category(Categories.SHOP_HARDWARE, item)
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["image"] = None
+        if item["name"].startswith("Castorama "):
+            item["branch"] = item.pop("name").removeprefix("Castorama ")
+            item["name"] = "Castorama"
+        elif item["name"].startswith("Casto "):
+            item["branch"] = item.pop("name").removeprefix("Casto ")
+            item["name"] = "Casto"
+
+        apply_category(Categories.SHOP_DOITYOURSELF, item)
         yield item
