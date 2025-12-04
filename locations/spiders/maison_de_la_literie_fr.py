e70531c89d3af50844c42c4ee84ff0882da02015
Cj Malone <CjMalone@mail.com>
2025-12-04 10:08:59 +0000

    Clean name

--- locations/spiders/maison_de_la_literie_fr.py
+++ locations/spiders/maison_de_la_literie_fr.py
@@ -1,5 +1,8 @@
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -8,3 +11,10 @@ class MaisonDeLaLiterieFRSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Maison de la Literie", "brand_wikidata": "Q80955776"}
     sitemap_urls = ["https://magasins.maisondelaliterie.fr/sitemap_pois.xml"]
     sitemap_rules = [(r".+/details$", "parse_sd")]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("Maison de la Literie - ")
+
+        apply_category(Categories.SHOP_BED, item)
+
+        yield item
