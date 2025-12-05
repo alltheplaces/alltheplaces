6f827f6316ba866ff0079bd04fa9f43b4126b74c
jhutton1 <jaimehutton01@gmail.com>
2025-12-05 12:19:03 -0500

    add clai_it

---
+++ locations/spiders/crai_it.py
@@ -0,0 +1,38 @@
+import json
+import re
+from scrapy.spiders import SitemapSpider
+
+from requests import Response
+import scrapy
+from scrapy.http.request import Request
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_DE, DAYS_HR, DAYS_HU, DAYS_IT, DAYS_SI, OpeningHours, sanitise_day
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+
+class CraiITSpider(SitemapSpider,StructuredDataSpider):
+    name = "crai_it"
+    item_attributes = {"brand": "Crai", "brand_wikidata": "Q3696429"}
+    sitemap_urls = ["https://crai.it/negozi-e-volantini/sitemap.xml"]
+
+
+    def pre_process_data(self, ld_data, **kwargs):
+        opening_hours = ld_data.get("openingHoursSpecification")
+
+        if opening_hours:
+            if all(all(key in rule for key in ["dayOfWeek", "opens", "closes"]) for rule in opening_hours):
+                for rule in opening_hours:
+                    rule["dayOfWeek"] = sanitise_day(rule["dayOfWeek"], DAYS_IT)
+                    if '/' in rule["closes"]:
+                        rule["closes"] = rule["closes"].split('/')[1]
+            else:
+                ld_data["openingHoursSpecification"] = None
+
+
+            
+
+   
