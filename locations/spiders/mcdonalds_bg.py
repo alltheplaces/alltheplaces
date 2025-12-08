f59d0a7511a229458fceb85a74b1ed9cfae6b448
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-12-08 17:47:48 +0530

    Fix opening hours

--- locations/spiders/mcdonalds_bg.py
+++ locations/spiders/mcdonalds_bg.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any
 
 import chompjs
@@ -6,7 +7,7 @@ from scrapy.http import Response
 
 from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
-from locations.hours import DAYS, OpeningHours
+from locations.hours import DAYS, DAYS_BG, DAYS_EN, OpeningHours
 from locations.spiders.mcdonalds import McdonaldsSpider
 
 
@@ -35,18 +36,32 @@ class McdonaldsBGSpider(Spider):
             for benefit in location.get("benefits", []):
                 apply_yes_no(Extras.WIFI, item, benefit.get("name") == "WiFi")
                 apply_yes_no(Extras.DRIVE_THROUGH, item, benefit.get("name") == "McDrive™")
-                if benefit.get("name") == "24/7":
-                    item["opening_hours"] = "24/7"
 
             apply_yes_no(Extras.DELIVERY, item, location.get("is_delivery_available"))
 
+            item["opening_hours"] = self.parse_opening_hours(location.get("business_hours", []))
+
             if work_hours := location.get("work_hours", []):
                 for work_hour in work_hours:
-                    oh = OpeningHours()
-                    oh.add_days_range(DAYS, work_hour.get("opens_at"), work_hour.get("closes_at"))
-                    if work_hour.get("label") == "Ресторант":
-                        item["opening_hours"] = oh.as_opening_hours()
-                    elif work_hour.get("label") == "McDrive™":
+                    if work_hour.get("label") == "McDrive™":
+                        oh = OpeningHours()
+                        oh.add_days_range(DAYS, work_hour.get("opens_at"), work_hour.get("closes_at"))
                         item["extras"]["opening_hours:drive_through"] = oh.as_opening_hours()
+                        break
 
             yield item
+
+    def parse_opening_hours(self, opening_hours: list[dict]) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in opening_hours:
+            if not rule.get("label") or "Ресторант" in rule.get("label"):
+                days_format = DAYS_EN
+                days = "Mo-Su"
+            elif re.search(r"[a-zA-Z]+", rule.get("label")):
+                days_format = DAYS_EN
+                days = rule.get("label")
+            else:
+                days_format = DAYS_BG
+                days = rule.get("label")
+            oh.add_ranges_from_string(f'{days}: {rule.get("opens_at")} to {rule.get("closes_at")}', days=days_format)
+        return oh
