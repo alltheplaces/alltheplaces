0d4e64a273856c69619f0d23da132f8073badbc6
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2025-12-08 15:38:53 +0530

    Fix Mcdonalds BG Spider

--- locations/spiders/mcdonalds_bg.py
+++ locations/spiders/mcdonalds_bg.py
@@ -1,6 +1,8 @@
-import json
+from typing import Any
 
+import chompjs
 from scrapy import FormRequest, Spider
+from scrapy.http import Response
 
 from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
@@ -14,39 +16,37 @@ class McdonaldsBGSpider(Spider):
     allowed_domains = ["mcdonalds.bg"]
     start_urls = ["https://mcdonalds.bg/restaurants/"]
 
-    def parse(self, response):
-        name_ids = response.xpath("//strong[@class='restaurant-item__title']/@data-restaurant-id").extract()
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        ajax_endpoints = chompjs.parse_js_object(response.xpath('//script[@id="wgs-endpoints-js-extra"]/text()').get())
+        ajax_action = "get_filtered_posts"
+        yield FormRequest(
+            url="https://mcdonalds.bg/wp-admin/admin-ajax.php",
+            formdata={"action": ajax_action, "ajax-nonce": ajax_endpoints["nonce"][ajax_action]},
+            callback=self.parse_locations,
+        )
 
-        for id in name_ids:
-            yield FormRequest(
-                url="https://mcdonalds.bg/wp-admin/admin-ajax.php",
-                formdata={"action": "get_restaurant", "restaurant_id": id},
-                callback=self.parse_restaurant,
-                method="POST",
-            )
+    def parse_locations(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["data"]:
+            item = DictParser.parse(location)
+            item["city"] = location.get("city", {}).get("city_name")
+            if phone_numbers := location.get("phone_numbers", []):
+                item["phone"] = phone_numbers[0]
 
-    def parse_restaurant(self, response):
-        location = json.loads(response.text)["data"]["data"]
-        item = DictParser.parse(location)
-        item["city"] = location.get("city", {}).get("city_name")
-        if phone_numbers := location.get("phone_numbers", []):
-            item["phone"] = phone_numbers[0]
+            for benefit in location.get("benefits", []):
+                apply_yes_no(Extras.WIFI, item, benefit.get("name") == "WiFi")
+                apply_yes_no(Extras.DRIVE_THROUGH, item, benefit.get("name") == "McDrive™")
+                if benefit.get("name") == "24/7":
+                    item["opening_hours"] = "24/7"
 
-        for benefit in location.get("benefits", []):
-            apply_yes_no(Extras.WIFI, item, benefit.get("name") == "WiFi")
-            apply_yes_no(Extras.DRIVE_THROUGH, item, benefit.get("name") == "McDrive™")
-            if benefit.get("name") == "24/7":
-                item["opening_hours"] = "24/7"
+            apply_yes_no(Extras.DELIVERY, item, location.get("is_delivery_available"))
 
-        apply_yes_no(Extras.DELIVERY, item, location.get("is_delivery_available"))
+            if work_hours := location.get("work_hours", []):
+                for work_hour in work_hours:
+                    oh = OpeningHours()
+                    oh.add_days_range(DAYS, work_hour.get("opens_at"), work_hour.get("closes_at"))
+                    if work_hour.get("label") == "Ресторант":
+                        item["opening_hours"] = oh.as_opening_hours()
+                    elif work_hour.get("label") == "McDrive™":
+                        item["extras"]["opening_hours:drive_through"] = oh.as_opening_hours()
 
-        if work_hours := location.get("work_hours", []):
-            for work_hour in work_hours:
-                oh = OpeningHours()
-                oh.add_days_range(DAYS, work_hour.get("opens_at"), work_hour.get("closes_at"))
-                if work_hour.get("label") == "Ресторант":
-                    item["opening_hours"] = oh.as_opening_hours()
-                elif work_hour.get("label") == "McDrive™":
-                    item["extras"]["opening_hours:drive_through"] = oh.as_opening_hours()
-
-        yield item
+            yield item
