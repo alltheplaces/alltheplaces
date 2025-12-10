8aefb95d5a799ba0d6085f59a99d3d683452669b
Cj Malone <CjMalone@mail.com>
2025-12-10 15:16:34 +0000

    Clean name

--- locations/spiders/pro_hockey_life_ca.py
+++ locations/spiders/pro_hockey_life_ca.py
@@ -1,3 +1,9 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
@@ -6,3 +12,10 @@ class ProHockeyLifeCASpider(JSONBlobSpider):
     item_attributes = {"brand": "Pro Hockey Life", "brand_wikidata": "Q123466336"}
     start_urls = ["https://www.prohockeylife.com/cdn/shop/t/37/assets/stores.json"]
     locations_key = "data"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+
+        apply_category(Categories.SHOP_SPORTS, item)
+
+        yield item
