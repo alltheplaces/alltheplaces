500b052479fbbc19f98da7f46a66d57b061d1ae3
mohit-sharma2194 <sharmaji2194@gmail.com>
2025-12-11 15:47:00 +0530

    updated code to fix spider

--- locations/spiders/thiele_dk.py
+++ locations/spiders/thiele_dk.py
@@ -1,20 +1,28 @@
+import json
+
 import scrapy
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from locations.hours import DAYS_DK, OpeningHours
 
 
 class ThieleDKSpider(scrapy.Spider):
     name = "thiele_dk"
     item_attributes = {"brand": "Thiele", "brand_wikidata": "Q12339176"}
     start_urls = [
-        "https://www.thiele.dk/index.php/tools/search?lat=56.26392&lng=9.501785&radius=10000&units=kilometers",
+        "https://www.thiele.dk/find-butik/",
     ]
 
     def parse(self, response, **kwargs):
-        for store in response.json():
+        for store in json.loads(response.xpath("//@data-butiks").get()):
             item = DictParser.parse(store)
             item["email"] = None
             apply_category(Categories.SHOP_OPTICIAN, item)
             item["street_address"] = item.pop("addr_full", None)
+            item["website"] = item["ref"] = "https://www.thiele.dk/butikker/" + item["name"].lower().replace(" ", "-")
+            oh = OpeningHours()
+            for day_time in store["schedule"]:
+                oh.add_ranges_from_string(day_time["time"], DAYS_DK)
+            item["opening_hours"] = oh
             yield item
