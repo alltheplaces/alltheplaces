e6b149c10c04bf3e374803f987e618b2ca1af595
David Hicks <david@hicks.id.au>
2025-12-16 18:43:06 +1100

    smoothie_king_ky_us_tt: use w2gi domain for lookups

--- locations/spiders/smoothie_king_ky_us_tt.py
+++ locations/spiders/smoothie_king_ky_us_tt.py
@@ -5,9 +5,12 @@ from locations.storefinders.where2getit import Where2GetItSpider
 class SmoothieKingKYUSTTSpider(Where2GetItSpider):
     name = "smoothie_king_ky_us_tt"
     item_attributes = {"brand": "Smoothie King", "brand_wikidata": "Q5491421"}
-    api_endpoint = "https://locations.smoothieking.com/rest/getlist"
-    api_key = "95B912AA-DEE4-11ED-883E-CDDDD7DDC1D0"
-    custom_settings = {"DOWNLOAD_TIMEOUT": 180}
+    api_brand_name = "smoothiekingsites"
+    api_key = "AA777E40-E5F4-11ED-B583-3193A96E38C4"
+    custom_settings = {
+        "DOWNLOAD_TIMEOUT": 30,
+        "DOWNLOAD_WARNSIZE": 268435456, # 256 MiB needed as results are >150 MiB
+    }
 
     def parse_item(self, item: Feature, location: dict, **kwargs):
         item["name"] = None
