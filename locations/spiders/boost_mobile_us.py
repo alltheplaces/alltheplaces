9efd94d898761cfce528997ee03956de669c24be
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2025-12-26 08:54:25 -0500

    [Boost Mobile US] Fix spider (#14817)
    Co-authored-by: Ehsan Firdaus <ehsan.firdaus@gmail.com>

--- locations/spiders/boost_mobile_us.py
+++ locations/spiders/boost_mobile_us.py
@@ -12,7 +12,7 @@ class BoostMobileUSSpider(SitemapSpider, StructuredDataSpider):
         "brand": "Boost Mobile",
         "brand_wikidata": "Q4943790",
     }
-    sitemap_urls = ["https://www.boostmobile.com/locations/sitemap-business-main-pages.xml"]
+    sitemap_urls = ["https://www.boostmobile.com/locations/sitemap.xml"]
     sitemap_rules = [(r"/locations/bd/boost-mobile-[a-z]{2}-[-\w]", "parse_sd")]
     custom_settings = {
         "USER_AGENT": BROWSER_DEFAULT,
