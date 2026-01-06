1f762e8e62d4d4265ba3995a79c8a461538b8fa6
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-01-06 18:50:26 +0530

    Add opening hours for Target US Spider

--- locations/spiders/target_us.py
+++ locations/spiders/target_us.py
@@ -6,6 +6,7 @@ from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
 
 
 class TargetUSSpider(SitemapSpider):
@@ -40,8 +41,22 @@ class TargetUSSpider(SitemapSpider):
                 elif contact_info.get("telephone_type") == "FAX":
                     item["extras"]["fax"] = contact_info.get("telephone_number")
 
+            opening_hours_info = store.get("rolling_operating_hours", {}).get("main_hours", {}).get("days") or []
+            item["opening_hours"] = self.parse_opening_hours(opening_hours_info)
+
             if store.get("physical_specifications", {}).get("format") == "SuperTarget":
                 apply_category(Categories.SHOP_SUPERMARKET, item)
             else:
                 apply_category(Categories.SHOP_DEPARTMENT_STORE, item)
             yield item
+
+    def parse_opening_hours(self, rules: list) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in rules[:7]:
+            day = rule.get("day_name")
+            if rule.get("is_open") is False:
+                oh.set_closed(day)
+                continue
+            for shift in rule.get("hours", []):
+                oh.add_range(day, shift["begin_time"], shift["end_time"], "%H:%M:%S")
+        return oh
