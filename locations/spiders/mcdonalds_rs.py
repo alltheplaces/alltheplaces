685bcc5c3524540e3bcfb0117fca40c0f84cdb58
jhutton1 <jaimehutton01@gmail.com>
2026-01-06 15:23:30 -0500

    split mccafe pois

--- locations/spiders/mcdonalds_rs.py
+++ locations/spiders/mcdonalds_rs.py
@@ -6,6 +6,8 @@ import scrapy
 from locations.categories import Extras, apply_yes_no
 from locations.dict_parser import DictParser
 from locations.spiders.mcdonalds import McdonaldsSpider
+from locations.spiders.mcdonalds_cz import Categories, apply_category
+from locations.hours import OpeningHours, DAYS_RS
 
 SERVICES_MAPPING = {
     "drive": Extras.DRIVE_THROUGH,
@@ -25,11 +27,26 @@ class McdonaldsRSSpider(scrapy.Spider):
         data = response.xpath('//script[contains(., "restaurantMarkers")]/text()').get()
         match = re.search(r"var restaurantMarkers = (\[.*?\]);", data, re.DOTALL)
         pois = json.loads(match.group(1))
+        
         for poi in pois:
             poi["street-address"] = poi.pop("address")
             item = DictParser.parse(poi)
-            # TODO: parse hours
+
             for category in poi["categories"]:
                 if match := SERVICES_MAPPING.get(category):
                     apply_yes_no(match, item, True)
+
+            opening_hours = (re.sub(r'h<br />|H<br />|h|H', '', poi["restaurant_worktime"]))
+            oh = OpeningHours()
+            oh.add_ranges_from_string(opening_hours, DAYS_RS)
+            item["opening_hours"] = oh
+
+            if "mccafe" in poi["categories"]:
+                mccafe = item.deepcopy()
+                mccafe["ref"] = "{}-mccafe".format(item["ref"])
+                mccafe["brand"] = "McCafé"
+                mccafe["brand_wikidata"] = "Q3114287"
+                apply_category(Categories.CAFE, mccafe)
+                yield mccafe
+
             yield item
