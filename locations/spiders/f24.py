1ad4aba21c6e5acbbca2cd00c26a6bc09cde51a0
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-01-09 14:37:15 +0530

    Fix F24 Spider

--- locations/spiders/f24.py
+++ locations/spiders/f24.py
@@ -1,45 +1,50 @@
-from scrapy import Spider
+from typing import Iterable
+
+import chompjs
+from scrapy.http import TextResponse
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 from locations.spiders.q8_italia import Q8ItaliaSpider
 
 
-class F24Spider(Spider):
+class F24Spider(JSONBlobSpider):
     name = "f24"
-    start_urls = [
-        "https://www.f24.dk/-/Station/GetGlobalMapStations?appDataSource=6d56d51e-5ab9-4caf-a18f-20c07ec0fba5"
-    ]
-
+    start_urls = ["https://www.f24.dk/find-station/"]
     BRANDS = {
         "F24": {"brand": "F24", "brand_wikidata": "Q12310853"},
         "Q8": Q8ItaliaSpider.item_attributes,
     }
 
-    def parse(self, response, **kwargs):
-        for location in response.json()["stations"]:
-            location["ref"] = str(location["stationNumber"])
-            location["geo"] = location.pop("position")
-            location["street_address"] = location.pop("address")
-            item = DictParser.parse(location)
+    def extract_json(self, response: TextResponse) -> list[dict]:
+        return DictParser.get_nested_key(
+            chompjs.parse_js_object(
+                response.xpath('//script[contains(text(), "window.__APP_INIT_DATA__")]/text()').get()
+            ),
+            "stations",
+        )
 
-            item["website"] = f'https://www.f24.dk{location["url"]}'
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = item.pop("street")
+        item["website"] = feature["stationPageUrl"]
 
-            if location.get("openingHours"):
-                if location["openingHours"]["AlwaysOpen"]:
-                    item["opening_hours"] = "24/7"
-                else:
-                    item["opening_hours"] = OpeningHours()
-                    for rule in ["WeekDays", "Saturday", "Sunday"]:
-                        self.add_rule(item["opening_hours"], rule, location["openingHours"].get(rule))
+        if feature.get("openingHours"):
+            if feature["openingHours"]["AlwaysOpen"]:
+                item["opening_hours"] = "24/7"
+            else:
+                item["opening_hours"] = OpeningHours()
+                for rule in ["WeekDays", "Saturday", "Sunday"]:
+                    self.add_rule(item["opening_hours"], rule, feature["openingHours"].get(rule))
 
-            if brand := self.BRANDS.get(location["net"]):
-                item.update(brand)
+        if brand := self.BRANDS.get(feature["network"]):
+            item.update(brand)
 
-            apply_category(Categories.FUEL_STATION, item)
+        apply_category(Categories.FUEL_STATION, item)
 
-            yield item
+        yield item
 
     @staticmethod
     def add_rule(oh: OpeningHours, day: str, rule: dict):
