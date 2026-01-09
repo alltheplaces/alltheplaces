524145704729793d7bb6861501b150798a3e8de0
Spencer Alves <impiaaa@gmail.com>
2026-01-09 09:22:19 -0800

    [Napa] Rewrite to use SitemapSpider+StructuredDataSpider
    Also rename to NapaAutoParts since they also have car repair shops that need a separate spider

---
+++ locations/spiders/napa_auto_parts.py
@@ -0,0 +1,23 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class NapaAutoPartsSpider(SitemapSpider, StructuredDataSpider):
+    name = "napa_auto_parts"
+    download_delay = 5
+    item_attributes = {"brand": "NAPA Auto Parts", "brand_wikidata": "Q6970842"}
+    sitemap_urls = ["https://www.napaonline.com/nol_store_sitemap_https.xml"]
+    sitemap_rules = [
+        (r"^https://www.napaonline.com/en/\w+/[^/]+/store/(\d+)$", "parse"),
+    ]
+    search_for_facebook = False
+    search_for_twitter = False
+    drop_attributes = ["image"]
+    time_format = "%H:%M:%S"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["branch"] = item.pop("name").removeprefix("NAPA Auto Parts - ")
+        apply_category(Categories.SHOP_CAR_PARTS, item)
+        yield item
