1fa1e5c432bc660306ff2d6f623a098ec3a16d28
panasonic-mafia <kolosov.yv@gmail.com>
2026-01-15 12:40:51 +0100

    type hints

--- locations/pipelines/extract_gb_postcode.py
+++ locations/pipelines/extract_gb_postcode.py
@@ -1,12 +1,16 @@
 import re
 
+from scrapy import Spider
+
+from locations.items import Feature
+
 GB_POSTCODE_PATTERN = re.compile(r"(\w{1,2}\d{1,2}\w? \d\w{2})")
 GB_POSTCODE_0_PATTERN = re.compile(r"(\w{1,2}\d{1,2}\w?) O(\w{2})")
 IE_POSTCODE_PATTERN = re.compile(r"([AC-FHKNPRTV-Y][0-9]{2}|D6W)[ -]?([0-9AC-FHKNPRTV-Y]{4})")
 
 
 class ExtractGBPostcodePipeline:
-    def process_item(self, item, spider):
+    def process_item(self, item: Feature, spider: Spider) -> Feature:
         if item.get("country") == "GB":
             if item.get("addr_full") and not item.get("postcode"):
                 item["postcode"] = extract_gb_postcode(item["addr_full"])
@@ -17,7 +21,7 @@ class ExtractGBPostcodePipeline:
         return item
 
 
-def extract_gb_postcode(s: str):
+def extract_gb_postcode(s: str) -> str | None:
     """
     Look for first occurrence, if any, of a GB format postcode in a string.
     :param s: the string to search for a GB postcode
