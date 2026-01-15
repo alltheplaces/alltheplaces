7f68fb3a730f39974072effe5416cdb47a9975a5
panasonic-mafia <kolosov.yv@gmail.com>
2026-01-15 12:34:53 +0100

    [ExtractGBPostcodePipeline] compile regexes only once

--- locations/pipelines/extract_gb_postcode.py
+++ locations/pipelines/extract_gb_postcode.py
@@ -1,5 +1,9 @@
 import re
 
+GB_POSTCODE_PATTERN = re.compile(r"(\w{1,2}\d{1,2}\w? \d\w{2})")
+GB_POSTCODE_0_PATTERN = re.compile(r"(\w{1,2}\d{1,2}\w?) O(\w{2})")
+IE_POSTCODE_PATTERN = re.compile(r"([AC-FHKNPRTV-Y][0-9]{2}|D6W)[ -]?([0-9AC-FHKNPRTV-Y]{4})")
+
 
 class ExtractGBPostcodePipeline:
     def process_item(self, item, spider):
@@ -8,9 +12,7 @@ class ExtractGBPostcodePipeline:
                 item["postcode"] = extract_gb_postcode(item["addr_full"])
         elif item.get("country") == "IE":
             if item.get("addr_full") and not item.get("postcode"):
-                if postcode := re.search(
-                    r"([AC-FHKNPRTV-Y][0-9]{2}|D6W)[ -]?([0-9AC-FHKNPRTV-Y]{4})", item["addr_full"].upper()
-                ):
+                if postcode := re.search(IE_POSTCODE_PATTERN, item["addr_full"].upper()):
                     item["postcode"] = "{} {}".format(postcode.group(1), postcode.group(2))
         return item
 
@@ -22,8 +24,8 @@ def extract_gb_postcode(s: str):
     :return: the first candidate postcode instance, None if not present
     """
     s = s.upper()
-    if postcode := re.search(r"(\w{1,2}\d{1,2}\w? \d\w{2})", s):
+    if postcode := re.search(GB_POSTCODE_PATTERN, s):
         return postcode.group(1)
-    if postcode := re.search(r"(\w{1,2}\d{1,2}\w?) O(\w{2})", s):
+    if postcode := re.search(GB_POSTCODE_0_PATTERN, s):
         return postcode.group(1) + " 0" + postcode.group(2)
     return None
