a35d6e73652fb2898d96ef1e7748175f9e7745c3
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-01-16 17:41:04 +0530

    code updated

--- locations/spiders/european_wax_center_us.py
+++ locations/spiders/european_wax_center_us.py
@@ -1,13 +1,19 @@
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class EuropeanWaxCenterUSSpider(SitemapSpider, StructuredDataSpider):
+class EuropeanWaxCenterUSSpider(CrawlSpider, StructuredDataSpider):
     name = "european_wax_center_us"
     item_attributes = {
         "brand_wikidata": "Q5413426",
         "brand": "European Wax Center",
     }
-    sitemap_urls = ["https://locations.waxcenter.com/sitemap/sitemap_index.xml"]
+    start_urls = ["https://locations.waxcenter.com/"]
     sitemap_rules = [(r"https://locations.waxcenter.com/[^/]+/[^/]+/[^/]+\.html$", "parse_sd")]
+    rules = [
+        Rule(LinkExtractor(allow=r"https://locations.waxcenter.com/\w+/$")),
+        Rule(LinkExtractor(allow=r"https://locations.waxcenter.com/\w+/[a-z-]+/$")),
+        Rule(LinkExtractor(allow=r"https://locations.waxcenter.com/\w+/[a-z-]+/[a-z-0-9\.]+$"), callback="parse_sd"),
+    ]
