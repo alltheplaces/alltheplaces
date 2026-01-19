5b95faac80de6aa0a9817cdca2b8a6dade2d013d
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-01-19 18:09:18 +0000

    Upgrade to Scrapy 2.14 (#14897)
    Co-authored-by: Ian Dees <ian.dees@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- locations/pipelines/closed.py
+++ locations/pipelines/closed.py
@@ -1,24 +1,35 @@
-from scrapy import Spider
+from scrapy.crawler import Crawler
 
 from locations.items import Feature
 
 
 class ClosePipeline:
+    crawler: Crawler
+
     closed_labels = ["closed", "coming soon"]
 
-    def process_item(self, item: Feature, spider: Spider) -> Feature:
+    def __init__(self, crawler: Crawler):
+        self.crawler = crawler
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler):
+        return cls(crawler)
+
+    def process_item(self, item: Feature) -> Feature:
         # Skip when we know a feature is closed
         if item["extras"].get("end_date"):
-            if spider.crawler.stats:
-                spider.crawler.stats.inc_value("atp/closed_poi")
+            if self.crawler.stats:
+                self.crawler.stats.inc_value("atp/closed_poi")
             return item
 
         if name := item.get("name"):
             for label in self.closed_labels:
                 if label in str(name).lower():
-                    spider.logger.warning(f'Found {label} in {name} ({item.get("ref")})')
-                    if spider.crawler.stats:
-                        spider.crawler.stats.inc_value("atp/closed_check")
+                    self.crawler.spider.logger.warning(  # ty: ignore[possibly-missing-attribute]
+                        f'Found {label} in {name} ({item.get("ref")})'
+                    )
+                    if self.crawler.stats:
+                        self.crawler.stats.inc_value("atp/closed_check")
                     break
 
         return item
