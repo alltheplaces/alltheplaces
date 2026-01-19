37aa2e1d7d70518a1fdf24df7d1c0d0cf2ba5cef
Cj Malone <CjMalone@mail.com>
2026-01-19 15:57:09 +0000

    Collect coords

--- locations/spiders/grand_optical.py
+++ locations/spiders/grand_optical.py
@@ -1,8 +1,10 @@
+import json
 from typing import Iterable
 
 from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -15,4 +17,12 @@ class GrandOpticalSpider(SitemapSpider, StructuredDataSpider):
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         item["website"] = response.url
+
+        data = json.loads(response.xpath('//script[@id="__NEXT_DATA__"]/text()').get())
+        store = data["props"]["initialProps"]["pageProps"]["storeData"]
+        item["lat"] = store["lat"]
+        item["lon"] = store["lon"]
+
+        apply_category(Categories.SHOP_OPTICIAN, item)
+
         yield item
