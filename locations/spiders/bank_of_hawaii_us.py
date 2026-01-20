878f8094c01f6170eeef1da25892507e214a8c54
Jan Callewaert <jan.callewaert@tomtom.com>
2026-01-20 11:56:01 +0100

    [Bank of Hawaii US]: fix spider

--- locations/spiders/bank_of_hawaii_us.py
+++ locations/spiders/bank_of_hawaii_us.py
@@ -17,21 +17,30 @@ class BankOfHawaiiUSSpider(scrapy.Spider):
             item["lat"] = location["geocode"]["latitude"]
             item["lon"] = location["geocode"]["longitude"]
             oh = OpeningHours()
-            for day_name, day in location["operationalHours"]["hours"].items():
+            for day_name, day in location["operationalHours"].items():
                 if day["isClosed"]:
+                    oh.set_closed(day_name.title())
                     continue
-                for hours_range in day["hours"]:
-                    oh.add_range(day_name.title(), hours_range["openTime"], hours_range["closeTime"], "%H:%M:%S")
+                for hours_range in day.get("openIntervals", []):
+                    # Times are in format "6 a.m.", "11:30 p.m.", etc. - normalize to include minutes
+                    start_time = hours_range["start"].replace(".", "").strip()
+                    end_time = hours_range["end"].replace(".", "").strip()
+                    # Add ":00" if no minutes specified
+                    if ":" not in start_time:
+                        start_time = start_time.replace(" ", ":00 ")
+                    if ":" not in end_time:
+                        end_time = end_time.replace(" ", ":00 ")
+                    oh.add_range(day_name.title(), start_time, end_time, time_format="%I:%M %p")
             # Branch without an ATM
-            if "Branch" in location["type"] and "ATM" not in location["type"]:
+            if "Branch" in location["type"] and "Atm" not in location["type"]:
                 apply_category(Categories.BANK, item)
                 item["opening_hours"] = oh.as_opening_hours()
             # Branch with an ATM
-            elif "Branch" in location["type"] and "ATM" in location["type"]:
+            elif "Branch" in location["type"] and "Atm" in location["type"]:
                 apply_category(Categories.BANK, item)
                 apply_yes_no(Extras.ATM, item, True)
                 item["opening_hours"] = oh.as_opening_hours()
             # ATM only
-            elif "Branch" not in location["type"] and "ATM" in location["type"]:
+            elif "Branch" not in location["type"] and "Atm" in location["type"]:
                 apply_category(Categories.ATM, item)
             yield item
