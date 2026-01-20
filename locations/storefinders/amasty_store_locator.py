alltheplaces/alltheplaces

commit fa8552d8b102a85482a6b60e062d070f0d05fda4

Type hint fixups particularly for storefinders (#14845)
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

locations/storefinders/amasty_store_locator.py
@@ -16,7 +16,7 @@ class AmastyStoreLocatorSpider(Spider):
       v1: https://amasty.com/docs/doku.php?id=magento_1:store_locator
       v2: https://amasty.com/docs/doku.php?id=magento_2:store_locator
 
-    To use this spider, specify one or more `allowed_domain`, and the default
+    To use this spider, specify `allowed_domains`, and the default
     path `/amlocator/index/ajax/` will be requested. If for some reason the
     path is not default, it can be overridden by supplying the full path in
     `start_urls`. Some implementations of Amasty Store Finder limit the number
@@ -34,6 +34,8 @@ class AmastyStoreLocatorSpider(Spider):
     """
 
     # Public attributes. You may set these attributes in a spider.
+    allowed_domains: list[str] = []
+    start_urls: list[str] = []
     pagination_mode: bool = False
 
     # Private attributes. Do not set these attributes in a spider.
@@ -45,14 +47,18 @@ class AmastyStoreLocatorSpider(Spider):
         pagination_attribute = ""
         if self.pagination_mode:
             pagination_attribute = "?p=1"
-        if len(self.start_urls) == 0:
-            for domain in self.allowed_domains:
-                yield Request(
-                    url=f"https://{domain}/amlocator/index/ajax/{pagination_attribute}", headers=headers, method="POST"
-                )
+        if len(self.start_urls) == 0 and len(self.allowed_domains) == 1:
+            yield Request(
+                url=f"https://{self.allowed_domains[0]}/amlocator/index/ajax/{pagination_attribute}",
+                headers=headers,
+                method="POST",
+            )
+        elif len(self.start_urls) == 1:
+            yield Request(url=f"https://{self.start_urls[0]}{pagination_attribute}", headers=headers, method="POST")
         else:
-            for url in self.start_urls:
-                yield Request(url=f"{url}{pagination_attribute}", headers=headers, method="POST")
+            raise ValueError(
+                "Specify one domain name in the allowed_domains list attribute or one URL in the start_urls list attribute."
+            )
 
     def parse(self, response: TextResponse) -> Iterable[Feature | Request]:
         json_blob = re.search(r"items\":(\[.*\]),\"", response.text)
@@ -107,6 +113,8 @@ class AmastyStoreLocatorSpider(Spider):
     def pre_process_data(self, feature: dict) -> None:
         """Override with any pre-processing on the item."""
 
-    def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Feature | Request]:
+    def post_process_item(
+        self, item: Feature, feature: dict, popup_html: Selector | None = None
+    ) -> Iterable[Feature | Request]:
         """Override with any post-processing on the item."""
         yield item

commit 0f5c38f872b6ead3d8dc5c618d8c0975f889b326

Amasty Store Locator: fix use of start_urls
fa8552d8b1 introduced an error for spiders using Amasty Store Locator
with the start_urls attribute specified instead of allowed_domains.

This fixes spiders:
 - beacon_lighting_au
 - eldi_be
 - mephisto
 - paper_source_us
 - tile_africa_na_nz

locations/storefinders/amasty_store_locator.py
@@ -1,6 +1,7 @@
 import re
 from json import loads
 from typing import AsyncIterator, Iterable
+from urllib.parse import urljoin
 
 from scrapy import Request, Selector, Spider
 from scrapy.http import TextResponse
@@ -54,7 +55,7 @@ class AmastyStoreLocatorSpider(Spider):
                 method="POST",
             )
         elif len(self.start_urls) == 1:
-            yield Request(url=f"https://{self.start_urls[0]}{pagination_attribute}", headers=headers, method="POST")
+            yield Request(url=urljoin(self.start_urls[0], pagination_attribute), headers=headers, method="POST")
         else:
             raise ValueError(
                 "Specify one domain name in the allowed_domains list attribute or one URL in the start_urls list attribute."