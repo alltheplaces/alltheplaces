5b8daa0ce3e99fd6f9e630d915745b92f734f48a
David Hicks <david@hicks.id.au>
2026-01-21 00:21:02 +1100

    [Forever Balboa Park US] Add spider for park trees
    Also add support for filtering trees by an organisation ID for
    TreePlotter instances such as this one where tree data is merged
    together across multiple organisations into a single TreePlotter
    instance.

---
+++ locations/spiders/infrastructure/forever_balboa_park_trees_us.py
@@ -0,0 +1,14 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.items import Feature
+from locations.storefinders.treeplotter import TreePlotterSpider
+
+
+class ForeverBalboaParkTreesUSSpider(TreePlotterSpider):
+    name = "forever_balboa_park_trees_us"
+    item_attributes = {"operator": "Forever Balboa Park", "operator_wikidata": "Q137826817", "state": "CA"}
+    folder = "SDTT"
+    layer_name = "trees"
+    organisation_id = 2
