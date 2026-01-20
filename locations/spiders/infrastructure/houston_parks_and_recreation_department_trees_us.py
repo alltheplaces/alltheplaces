7e8738f2a209b637248d77eb045f92423be1d47b
David Hicks <david@hicks.id.au>
2026-01-20 23:28:13 +1100

    [Houston Parks & Rec US] Add spider for street/park trees

---
+++ locations/spiders/infrastructure/houston_parks_and_recreation_department_trees_us.py
@@ -0,0 +1,8 @@
+from locations.storefinders.treeplotter import TreePlotterSpider
+
+
+class HoustonParksAndRecreationDepartmentTreesUSSpider(TreePlotterSpider):
+    name = "houston_parks_and_recreation_department_trees_us"
+    item_attributes = {"operator": "Houston Parks and Recreation Department", "operator_wikidata": "Q115018877", "state": "TX"}
+    folder = "HoustonTX"
+    layer_name = "trees"
