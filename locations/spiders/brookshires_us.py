adc6b0bfc1bca16088097e695e3f2053b146901c
Mateusz Konieczny <matkoniecz@gmail.com>
2026-01-21 10:59:49 +0100

    fix wikidata

--- locations/spiders/brookshires_us.py
+++ locations/spiders/brookshires_us.py
@@ -3,6 +3,6 @@ from locations.storefinders.storefrontgateway import StorefrontgatewaySpider
 
 class BrookshiresUSSpider(StorefrontgatewaySpider):
     name = "brookshires_us"
-    item_attributes = {"brand": "Brookshire's", "brand_wikidata": "Q7427974"}
+    item_attributes = {"brand": "Brookshire's", "brand_wikidata": "Q4975085"}
     start_urls = ["https://storefrontgateway.brookshires.com/api/stores"]
     requires_proxy = "US"  # Cloudflare geoblocking
