95deb0fd6f4287fb30de8f38e13f63801559f557
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-01-22 11:43:15 +0000

    [Vintage Inns GB] Fix spider
    There are a lot of pubs missing in the woosmap feed.

--- locations/spiders/vintage_inns_gb.py
+++ locations/spiders/vintage_inns_gb.py
@@ -1,26 +1,11 @@
-from typing import Iterable
-
 from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.storefinders.woosmap import WoosmapSpider
+from locations.json_blob_spider import JSONBlobSpider
+from locations.user_agents import BROWSER_DEFAULT
 
-
-class VintageInnsGBSpider(WoosmapSpider):
+class VintageInnsGBSpider(JSONBlobSpider):
     name = "vintage_inns_gb"
     item_attributes = {"brand": "Vintage Inns", "brand_wikidata": "Q87067899"}
-    key = "woos-f6518149-ce6e-365c-b912-12ae5d6c8b2f"
-    origin = "https://www.vintageinn.co.uk"
-
-    def parse_item(self, item: Feature, feature: dict) -> Iterable[Feature]:
-        item["website"] = feature.get("properties").get("user_properties").get("primaryWebsiteUrl")
-        oh = OpeningHours()
-        try:
-            for day_time in feature["properties"]["user_properties"].get("tradingHours"):
-                day = day_time["day"]
-                open_time = day_time["openTime"]
-                close_time = day_time["closeTime"]
-                oh.add_range(day=day, open_time=open_time, close_time=close_time)
-        except:
-            pass
-        item["opening_hours"] = oh
-        yield item
+    start_urls = ["https://www.vintageinn.co.uk/cborms/pub/brands/8/outlets/"]
+    custom_settings = {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
+    requires_proxy = True
