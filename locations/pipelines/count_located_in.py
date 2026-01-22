df6b5ac2bb7a0a1be2d72fe8091d30dd4213b850
Jan Callewaert <jan.callewaert@tomtom.com>
2026-01-22 13:33:48 +0100

    migrate to scrapy 2.14

--- locations/pipelines/count_located_in.py
+++ locations/pipelines/count_located_in.py
@@ -1,7 +1,22 @@
+from scrapy.crawler import Crawler
+
+from locations.items import Feature
+
+
 class CountLocatedInPipeline:
-    def process_item(self, item, spider):
+    crawler: Crawler
+
+    def __init__(self, crawler: Crawler):
+        self.crawler = crawler
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler):
+        return cls(crawler)
+
+    def process_item(self, item: Feature):
         if located_in := item.get("located_in"):
-            spider.crawler.stats.inc_value(f"atp/located_in/{located_in}")
+            self.crawler.stats.inc_value(f"atp/located_in/{located_in}")  # ty: ignore[possibly-missing-attribute]
         if wikidata := item.get("located_in_wikidata"):
-            spider.crawler.stats.inc_value(f"atp/located_in_wikidata/{wikidata}")
+            self.crawler.stats.inc_value(
+                f"atp/located_in_wikidata/{wikidata}")  # ty: ignore[possibly-missing-attribute]
         return item
