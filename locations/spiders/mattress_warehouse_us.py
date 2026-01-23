3b7097f268d86bbdb3d72bd5e77f0ba6179d858d
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-01-23 13:54:26 +0530

    updated code to fix spider

--- locations/spiders/mattress_warehouse_us.py
+++ locations/spiders/mattress_warehouse_us.py
@@ -1,30 +1,53 @@
-from locations.storefinders.yext import YextSpider
+import re
+from typing import Any
+
+from scrapy import Request
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.items import Feature
 
 
-class MattressWarehouseUSSpider(YextSpider):
+class MattressWarehouseUSSpider(CrawlSpider):
     name = "mattress_warehouse_us"
-    item_attributes = {"brand": "Mattress Warehouse", "brand_wikidata": "Q61995079"}
-    api_key = "0277c3ad01c944bfc148a777fa36764d"
+    BRANDS = {
+        "mattress_warehouse": {"brand": "Mattress Warehouse", "brand_wikidata": "Q61995079"},
+        "sleep_outfitters": {"brand": "Sleep Outfitters", "brand_wikidata": "Q120509459"},
+    }
+    start_urls = ["https://mattresswarehouse.com/store-locator"]
 
-    def parse_item(self, item, location, **kwargs):
-        if item["ref"] in [
-            "0000",  # "in regards to your delivery"
-            "59",  # HQ
-            "6664888033510164995",  # "Test"
-        ]:
-            return
+    rules = [
+        Rule(LinkExtractor(allow=r"/store-locator/[a-z]+$")),
+        Rule(
+            LinkExtractor(allow=r"/store-locator/mattress-stores-in"),
+            callback="parse",
+            follow=True,
+        ),
+    ]
 
-        if "COMING SOON" in item["name"]:
-            return
+    store_link_extractor = LinkExtractor(allow=r"/store-locator/(sleep-outfitters-of-|mattress-warehouse-of-)")
 
-        item["branch"] = item.pop("name").removeprefix("Mattress Warehouse ").removeprefix("of ").removeprefix("- ")
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        lat_lon_data = re.findall(
+            r"(\d+\.\d+),.*?(-?\d+\.\d+)", response.xpath('//*[contains(text(),"loaderData")]/text()').get(default="")
+        )
+        links = self.store_link_extractor.extract_links(response)
+        for link in links:
+            for lat_lon in lat_lon_data:
+                yield Request(url=link.url, callback=self.parse_details, cb_kwargs={"lat_lon": lat_lon})
 
-        # Remove brand data from POI
-        if item.get("email") == "info@mattresswarehouse.com":
-            del item["email"]
-        if item.get("twitter") == "MWarehouseLLC":
-            del item["twitter"]
-        if item["extras"]["contact:instagram"] == "mattresswarehousellc":
-            del item["extras"]["contact:instagram"]
+    def parse_details(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath("//h1/div[2]/text()").get()
+        item["addr_full"] = response.xpath('//*[@class="flex items-center gap-3"]//text()').get()
+        item["email"] = response.xpath('//*[contains(@href,"mailto:")]/text()').get()
+        item["phone"] = response.xpath('//*[contains(@href,"tel:")]/text()').get()
+        item["ref"] = item["website"] = response.url
+        item["lat"], item["lon"] = kwargs.get("lat_lon", (None, None))
+        if "sleep-outfitters" in response.url:
+            item.update(self.BRANDS["sleep_outfitters"])
 
+        else:
+            item.update(self.BRANDS["mattress_warehouse"])
         yield item
