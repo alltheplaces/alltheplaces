198469e2a67febb9e24904550feb8683d721b9bc
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-01-25 13:49:36 +0000

    Update mamas_and_papas_gb.py

--- locations/spiders/mamas_and_papas_gb.py
+++ locations/spiders/mamas_and_papas_gb.py
@@ -20,8 +20,7 @@ class MamasAndPapasGBSpider(Spider):
             item = DictParser.parse(store)
 
             item["website"] = store.get("web")
-            item["branch"] = item["name"].replace(r"^[^,]+,", "")
-            item["name"] = item["name"].replace(r",.*$", "")
+            item["name"],item["branch"] = item["name"].split(",")
             if "at Next" in item["name"]:
                 item["located_in"] = "Next"
                 item["located_in_wikidata"] = "Q246655"
