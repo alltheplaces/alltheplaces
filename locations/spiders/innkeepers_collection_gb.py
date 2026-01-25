7f29cbd2fb2041dbe69cefe8c7468ebbadbc6ee4
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-01-25 21:44:43 +0000

    Update innkeepers_collection_gb.py

--- locations/spiders/innkeepers_collection_gb.py
+++ locations/spiders/innkeepers_collection_gb.py
@@ -7,21 +7,11 @@ from locations.storefinders.woosmap import WoosmapSpider
 
 class InnkeepersCollectionGBSpider(WoosmapSpider):
     name = "innkeepers_collection_gb"
-    item_attributes = {"brand": "Innkeepers Collection", "brand_wikidata": "Q6035891"}
+    item_attributes = {"brand": "Innkeeper's Collection", "brand_wikidata": "Q6035891"}
     key = "woos-e20ab4a6-1a2d-33cb-b128-3ee59d15c383"
     origin = "https://www.innkeeperscollection.co.uk"
 
     def parse_item(self, item: Feature, feature: dict) -> Iterable[Feature]:
         item["website"] = feature.get("properties").get("user_properties").get("primaryWebsiteUrl")
         item["branch"] = item.pop("name").replace("Innkeeper's Collection ", "")
-        oh = OpeningHours()
-        try:
-            for day_time in feature["properties"]["user_properties"].get("tradingHours"):
-                day = day_time["day"]
-                open_time = day_time["openTime"]
-                close_time = day_time["closeTime"]
-                oh.add_range(day=day, open_time=open_time, close_time=close_time)
-        except:
-            pass
-        item["opening_hours"] = oh
         yield item
