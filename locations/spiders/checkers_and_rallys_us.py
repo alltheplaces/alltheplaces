8e401b6e1dd57bfd6a2785e82df7ee2439ca07bf
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-01-30 15:48:21 +0530

    code updated to fix spider

--- locations/spiders/checkers_and_rallys_us.py
+++ locations/spiders/checkers_and_rallys_us.py
@@ -1,30 +1,34 @@
+from typing import Any
+
+import scrapy
+from scrapy.http import Response
+
 from locations.categories import Categories, apply_category
-from locations.storefinders.yext_search import YextSearchSpider
+from locations.dict_parser import DictParser
 
 
-class CheckersAndRallysUSSpider(YextSearchSpider):
+class CheckersAndRallysUSSpider(scrapy.Spider):
     name = "checkers_and_rallys_us"
-    host = "https://locations.checkersandrallys.com"
     brands = {
         "Checkers": {"brand": "Checkers", "brand_wikidata": "Q63919315"},
         "Rally's": {"brand": "Rally's", "brand_wikidata": "Q63919323"},
     }
+    start_urls = ["https://checkersandrallys.com/unified-gateway/online-ordering/v1/all-stores/8159"]
 
-    def parse_item(self, location, item):
-        if "CLOSED" in item.get("name", "").upper():
-            return
-
-        if location["profile"].get("c_alphaNumericStoreID"):
-            item["ref"] = location["profile"]["c_alphaNumericStoreID"]
-
-        if "Checkers" in location["profile"].get("brands", []):
-            item["brand"] = self.brands["Checkers"]["brand"]
-            item["brand_wikidata"] = self.brands["Checkers"]["brand_wikidata"]
-        elif "Rally's" in location["profile"].get("brands", []):
-            item["brand"] = self.brands["Rally's"]["brand"]
-            item["brand_wikidata"] = self.brands["Rally's"]["brand_wikidata"]
-        elif len(location["profile"].get("brands", [])) >= 1:
-            raise ValueError("Unknown brand detected: {}".format(", ".join(location["profile"]["brands"])))
-
-        apply_category(Categories.FAST_FOOD, item)
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json()["stores_v1"]:
+            location["address"].pop("id")
+            location["address"].pop("name")
+            location.update(location.pop("address"))
+            item = DictParser.parse(location)
+            item["branch"] = item.pop("name").replace("Checkers ", "").replace("Rally's ", "")
+            item["street_address"] = item.pop("street")
+            item["addr_full"] = location["formatted_address"]
+            if "Checkers" in location["name"]:
+                item["brand"] = self.brands["Checkers"]["brand"]
+                item["brand_wikidata"] = self.brands["Checkers"]["brand_wikidata"]
+            elif "Rally's" in location["name"]:
+                item["brand"] = self.brands["Rally's"]["brand"]
+                item["brand_wikidata"] = self.brands["Rally's"]["brand_wikidata"]
+            apply_category(Categories.FAST_FOOD, item)
+            yield item
