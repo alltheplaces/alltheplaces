31da8685448f33f9fea244ef01ba1572d82c2f9f
Cj Malone <CjMalone@mail.com>
2026-01-30 15:54:07 +0000

    Amend opening hours

--- locations/spiders/delko_fr.py
+++ locations/spiders/delko_fr.py
@@ -5,6 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours, sanitise_day, DAYS_FR
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -14,14 +15,27 @@ class DelkoFRSpider(CrawlSpider, StructuredDataSpider):
     contacts = ["team.marketing@delko.com"]
     item_attributes = {"brand": "Delko", "brand_wikidata": "Q132021672"}
     start_urls = ["https://delko.fr/garage/"]
-    rules = [Rule(LinkExtractor("r/garage/delko/[^/]+/(\d+)$"), "parse")]
+    rules = [Rule(LinkExtractor(r"/garage/delko/[^/]+/(\d+)$"), "parse")]
     wanted_types = ["AutoRepair"]
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         item["branch"] = item.pop("name").removeprefix("Delko ")
 
         item["email"] = response.xpath('//b[contains(text(), "@delko.fr")]/text()').get()
+        item["opening_hours"] = self.parse_opening_hours(response)
 
         apply_category(Categories.SHOP_CAR_REPAIR, item)
 
         yield item
+
+    def parse_opening_hours(self, response: TextResponse) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in response.xpath('//div[@id="garage-horaires"]/div/div'):
+            day = sanitise_day(rule.xpath("./p/b/text()").get(), DAYS_FR)
+            times = rule.xpath("./p/text()").getall()
+            for time in times:
+                if time == "fermé":
+                    oh.set_closed(day)
+                else:
+                    oh.add_range(day, *time.split(" - "), time_format="%Hh%M")
+        return oh
