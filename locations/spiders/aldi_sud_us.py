28fcca216d29d4aecb404139e29aaf81f0d857ef
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-01-30 17:01:23 +0530

    code updated to use uberall spider

--- locations/spiders/aldi_sud_us.py
+++ locations/spiders/aldi_sud_us.py
@@ -1,28 +1,12 @@
-from scrapy.spiders import SitemapSpider
-
 from locations.categories import Categories, apply_category
-from locations.structured_data_spider import StructuredDataSpider
+from locations.storefinders.uberall import UberallSpider
 
 
-class AldiSudUSSpider(SitemapSpider, StructuredDataSpider):
+class AldiSudUSSpider(UberallSpider):
     name = "aldi_sud_us"
     item_attributes = {"brand_wikidata": "Q41171672", "country": "US"}
-    drop_attributes = {"image"}
-    allowed_domains = ["stores.aldi.us"]
-    sitemap_urls = ["https://stores.aldi.us/robots.txt"]
-    sitemap_rules = [
-        (r"^https://stores\.aldi\.us/.*/.*/.*$", "parse"),
-    ]
-    wanted_types = ["GroceryStore"]
-
-    def parse(self, response):
-        for city in response.css('[itemprop="address"] .Address-city'):
-            city.root.set("itemprop", "addressLocality")
-        yield from self.parse_sd(response)
+    key = "LETA2YVm6txbe0b9lS297XdxDX4qVQ"
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["name"] = None  # address
-
         apply_category(Categories.SHOP_SUPERMARKET, item)
-
         yield item
