4e943fdccae391f9ee5b0b95a2d3b6f61749e5ae
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-01-31 20:24:13 +0000

    [Village Hotel GB] Add spider

---
+++ locations/spiders/village_hotels_gb.py
@@ -0,0 +1,19 @@
+import html
+
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class VillageHotelsGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "village_hotels_gb"
+    item_attributes = {"brand": "Village Hotels", "brand_wikidata": "Q16963550"}
+    sitemap_urls = ["https://www.village-hotels.co.uk/sitemap.xml"]
+    sitemap_rules = [(r"https://www.village-hotels.co.uk/([^/]+)$", "parse")]
+    wanted_types = ["Hotel"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["branch"] = html.unescape(item.pop("name")).replace("Village Hotel - ")
+        apply_category(Categories.HOTEL, item)
+        yield item
