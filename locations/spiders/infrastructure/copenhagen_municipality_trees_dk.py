cf6f51b469d5ea2d43361820edd474872f9fc8dc
David Hicks <david@hicks.id.au>
2026-02-01 18:23:58 +1100

    Add Copenhagen Municipality trees (DK) (67k trees)

---
+++ locations/spiders/infrastructure/copenhagen_municipality_trees_dk.py
@@ -0,0 +1,27 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class CopenhagenMunicipalityTreesDKSpider(JSONBlobSpider):
+    name = "copenhagen_municipality_trees_dk"
+    item_attributes = {"operator": "Copenhagen Municipality", "operator_wikidata": "Q504125"}
+    allowed_domains = ["wfs-kbhkort.kk.dk"]
+    start_urls = ["https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:trae_basis&outputFormat=application%2Fjson&SRSNAME=EPSG:4326"]
+    locations_key = "features"
+    custom_settings = {"DOWNLOAD_TIMEOUT": 120}
+
+    def pre_process_data(self, feature: dict) -> None:
+        feature.update(feature.pop("properties"))
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature["id"])
+        apply_category(Categories.NATURAL_TREE, item)
+        item["extras"]["species"] = feature["traeart"]
+        item["extras"]["genus"] = feature["slaegt"]
+        item["extras"]["protected"] = "yes"
+        yield item
