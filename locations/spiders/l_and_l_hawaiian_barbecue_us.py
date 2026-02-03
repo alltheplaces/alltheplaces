e3752f8598ea3157e875320ff113cc544ac04ab7
Spencer Alves <impiaaa@gmail.com>
2026-02-03 14:02:20 -0800

    [L&L Hawaiian Barbecue] Update endpoint to fix spider

--- locations/spiders/l_and_l_hawaiian_barbecue_us.py
+++ locations/spiders/l_and_l_hawaiian_barbecue_us.py
@@ -1,9 +1,8 @@
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy import Request, Spider
 
-from locations.categories import Categories, Extras, apply_yes_no
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_FULL, OpeningHours
+from locations.react_server_components import parse_rsc
 
 
 class LAndLHawaiianBarbecueUSSpider(Spider):
@@ -11,38 +10,20 @@ class LAndLHawaiianBarbecueUSSpider(Spider):
     item_attributes = {
         "brand": "L&L Hawaiian Barbecue",
         "brand_wikidata": "Q6455441",
-        "extras": Categories.FAST_FOOD.value | {"cuisine": "hawaiian", Extras.TAKEAWAY.value: "yes"},
     }
-    start_urls = ["https://www.hawaiianbarbecue.com/page-data/locations/page-data.json"]
+
+    async def start(self):
+        yield Request("https://www.hawaiianbarbecue.com/locations", headers={"rsc": "1"})
 
     def parse(self, response):
-        for q in response.json()["staticQueryHashes"]:
-            yield JsonRequest(
-                f"https://www.hawaiianbarbecue.com/page-data/sq/d/{q}.json", callback=self.parse_locations
-            )
-
-    def parse_locations(self, response):
-        for location in response.json().get("data", {}).get("allPrismicLocation", {}).get("nodes", []):
-            if "uid" not in location:
-                continue
-
-            item = DictParser.parse(
-                {
-                    k: list(v.values())[0] if isinstance(v, dict) and len(v) == 1 else v
-                    for k, v in location["data"].items()
-                }
-            )
+        for location in DictParser.get_nested_key(dict(parse_rsc(response.body)), "locations"):
+            item = DictParser.parse(location)
             item["ref"] = location["uid"]
-            item["website"] = response.urljoin(location["url"])
+            item["website"] = response.urljoin(location["href"])
             item["branch"] = item.pop("name")
-            item["name"] = item["brand"] = location["data"]["type"]
-            item["extras"]["website:menu"] = (location["data"]["menu"] or {}).get("url")
-            apply_yes_no(Extras.DELIVERY, item, location["data"]["provides_delivery"])
 
-            oh = OpeningHours()
-            for day in DAYS_FULL:
-                hours = location["data"][f"{day.lower()}_hours"]["text"]
-                oh.add_ranges_from_string(f"{day} {hours}")
-            item["opening_hours"] = oh
+            apply_category(Categories.FAST_FOOD, item)
+            apply_yes_no(Extras.TAKEAWAY, item, True)
+            item["extras"]["cuisine"] = "hawaiian"
 
             yield item
