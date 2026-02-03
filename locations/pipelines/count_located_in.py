8dff040e7109d93be386ea1c430a2b30a729a99c
David Hicks <david@hicks.id.au>
2026-02-03 11:19:52 +1100

    Fix type hints for ty 0.0.14

--- locations/pipelines/count_located_in.py
+++ locations/pipelines/count_located_in.py
@@ -14,10 +14,11 @@ class CountLocatedInPipeline:
         return cls(crawler)
 
     def process_item(self, item: Feature):
-        if located_in := item.get("located_in"):
-            self.crawler.stats.inc_value(f"atp/located_in/{located_in}")  # ty: ignore[possibly-missing-attribute]
-        if wikidata := item.get("located_in_wikidata"):
-            self.crawler.stats.inc_value(
-                f"atp/located_in_wikidata/{wikidata}"
-            )  # ty: ignore[possibly-missing-attribute]
+        if self.crawler.stats:
+            if located_in := item.get("located_in"):
+                self.crawler.stats.inc_value(f"atp/located_in/{located_in}")
+            if wikidata := item.get("located_in_wikidata"):
+                self.crawler.stats.inc_value(
+                    f"atp/located_in_wikidata/{wikidata}"
+                )
         return item
