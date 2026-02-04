44645026d4e74485660224e907c8e3586b3977ed
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-02-04 11:04:56 +0530

    updated mcdonalds_nz and mcdonalds_eg

--- locations/spiders/mcdonalds_eg.py
+++ locations/spiders/mcdonalds_eg.py
@@ -1,7 +1,66 @@
-from locations.spiders.mcdonalds_au import McdonaldsAUSpider
+import re
+
+import scrapy
+
+from locations.categories import apply_yes_no
+from locations.hours import OpeningHours, sanitise_day
+from locations.items import Feature
+from locations.spiders.mcdonalds import McdonaldsSpider
+from locations.spiders.mcdonalds_au import FACILITIES_MAPPING
 
 
-class McdonaldsEGSpider(McdonaldsAUSpider):
+class McdonaldsEGSpider(scrapy.Spider):
     name = "mcdonalds_eg"
+    item_attributes = McdonaldsSpider.item_attributes
     allowed_domains = ["mcdonalds.eg"]
     start_urls = ["https://mcdonalds.eg/Stotes"]
+
+    @staticmethod
+    def parse_hours(rules: list) -> OpeningHours:
+        oh = OpeningHours()
+        for day, start_time, end_time in rules:
+            day = sanitise_day(day)
+            if not day:
+                continue
+            if start_time == "9999" or end_time == "9999":
+                continue
+            if end_time == "2400":
+                end_time = "2359"
+            if start_time and end_time:
+                try:
+                    oh.add_range(day, start_time, end_time, time_format="%H%M")
+                except:
+                    pass
+        return oh
+
+    def parse(self, response):
+        for data in response.json():
+            item = Feature(
+                {
+                    "city": data["store_suburb"],
+                    "ref": data["store_code"],
+                    "street_address": data["store_address"],
+                    "phone": data.get("store_phone"),
+                    "state": data["store_state"],
+                    "postcode": data.get("store_postcode"),
+                }
+            )
+
+            if coordinates := data.get("lat_long"):
+                item["lat"] = coordinates.get("lat")
+                item["lon"] = coordinates.get("lon")
+            if (not item.get("lat") or not item.get("lon")) and data.get("store_geocode"):
+                if m := re.search(r"(?P<lon>-?\d+(?:\.\d+))\s*,\s*(?P<lat>-?\d+(?:\.\d+))", data["store_geocode"]):
+                    item["lat"] = m.group("lat")
+                    item["lon"] = m.group("lon")
+
+            item["opening_hours"] = self.parse_hours(data.get("store_trading_hour", []))
+
+            facilities = [f.get("name") for f in data.get("store_filter", []) if f]
+            for facility in facilities:
+                if match := FACILITIES_MAPPING.get(facility):
+                    apply_yes_no(match, item, True)
+                else:
+                    self.crawler.stats.inc_value(f"atp/{self.name}/facilities/fail/{facility}")
+
+            yield item
