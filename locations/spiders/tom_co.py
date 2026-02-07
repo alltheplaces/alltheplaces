f180e87c315be2eef45f8a67385e51f99e955685
Jan Callewaert <72855158+JanCallewaert-TomTom@users.noreply.github.com>
2026-02-07 03:49:53 +0100

    [Tom & Co BE]: add spider (199 locations) (#14983)

---
+++ locations/spiders/tom_co.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.uberall import UberallSpider
+
+
+class TomCOSpider(UberallSpider):
+    name = "tom_co"
+    item_attributes = {"brand": "Tom & Co", "brand_wikidata": "Q98380763"}
+    key = "Aq5TVOtajZTxBQhZuFv34qsoWaXGKN"
+
+    def post_process_item(self, item: Feature, response: Response, location: dict) -> Iterable[Feature]:
+        item["branch"] = item["name"].replace("Tom&Co ", "").replace("Tom & Co ", "")
+        apply_category(Categories.SHOP_PET, item)
+        yield item
