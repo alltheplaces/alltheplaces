327539618e534022e39c8c6b739a1d724cecf7d1
David Hicks <david@hicks.id.au>
2026-02-07 13:20:43 +1100

    LocallySpider: refactor including ASICS and Crocs brands (#15166)

--- locations/spiders/asics_us.py
+++ locations/spiders/asics_us.py
@@ -7,18 +7,15 @@ from locations.items import Feature
 from locations.storefinders.locally import LocallySpider
 
 
-class CrocsEUSpider(LocallySpider):
-    name = "crocs_eu"
-    item_attributes = {"brand": "Crocs", "brand_wikidata": "Q926699"}
-    company_id = 1762
-    categories = ["Store", "Outlet"]
-    skip_auto_cc_spider_name = True
+class AsicsUSSpider(LocallySpider):
+    name = "asics_us"
+    item_attributes = {"brand": "ASICS", "brand_wikidata": "Q327247"}
+    company_id = 1682
+    categories = ["outlet"]
 
     def post_process_item(self, item: Feature, response: TextResponse, location: dict) -> Iterable[Feature]:
         if store_name := item.pop("name", None):
-            item["branch"] = store_name.removeprefix("Crocs at ").removeprefix("Crocs ")
+            item["branch"] = store_name.removeprefix("ASICS Outlet ")
         item["street_address"] = item.pop("addr_full")
-        if slug := location.get("slug"):
-            item["website"] = f"https://locations.crocs.com/shop/{slug}"
         apply_category(Categories.SHOP_SHOES, item)
         yield item
