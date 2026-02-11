70f724edb6994ddee2d9894b7e1984fc83fe363e
Jan Callewaert <jan.callewaert@tomtom.com>
2026-02-11 17:10:16 +0100

    [Cinnaholic US] fix spider

--- locations/spiders/cinnaholic_us.py
+++ locations/spiders/cinnaholic_us.py
@@ -1,8 +1,21 @@
+from collections.abc import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
 from locations.categories import Categories
-from locations.storefinders.where2getit import Where2GetItSpider
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class CinnaholicUSSpider(Where2GetItSpider):
+class CinnaholicUSSpider(SitemapSpider, StructuredDataSpider):
     name = "cinnaholic_us"
     item_attributes = {"brand": "Cinnaholic", "brand_wikidata": "Q48965480", "extras": Categories.SHOP_BAKERY.value}
-    api_key = "E54F429C-E8DC-11ED-A099-3DDBD7DDC1D0"
+    sitemap_urls = ["https://locations.cinnaholic.com/sitemap_index.xml"]
+    sitemap_rules = [(r"/ll/us/[a-z]{2}/[^/]+/\d+/$", "parse_sd")]
+    wanted_types = ["Bakery"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        yield item
