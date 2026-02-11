b6670736f54442404c7db40ef1246370eca2de1b
Cj Malone <CjMalone@mail.com>
2026-02-11 17:03:01 +0000

    apply_category

--- locations/spiders/cinnaholic_us.py
+++ locations/spiders/cinnaholic_us.py
@@ -3,14 +3,14 @@ from collections.abc import Iterable
 from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class CinnaholicUSSpider(SitemapSpider, StructuredDataSpider):
     name = "cinnaholic_us"
-    item_attributes = {"brand": "Cinnaholic", "brand_wikidata": "Q48965480", "extras": Categories.SHOP_BAKERY.value}
+    item_attributes = {"brand": "Cinnaholic", "brand_wikidata": "Q48965480"}
     sitemap_urls = ["https://locations.cinnaholic.com/sitemap_index.xml"]
     sitemap_rules = [(r"/ll/us/[a-z]{2}/[^/]+/\d+/$", "parse_sd")]
     wanted_types = ["Bakery"]
@@ -18,4 +18,7 @@ class CinnaholicUSSpider(SitemapSpider, StructuredDataSpider):
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
+
+        apply_category(Categories.SHOP_BAKERY, item)
+
         yield item
