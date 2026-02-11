d15557fe121561dea9974f6965cc4e6914b8c6df
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-02-11 15:20:55 +0000

    [New Look GB] Fix spider

--- locations/spiders/new_look_gb.py
+++ locations/spiders/new_look_gb.py
@@ -7,7 +7,7 @@ class NewLookGBSpider(SitemapSpider, StructuredDataSpider):
     name = "new_look_gb"
     item_attributes = {"brand": "New Look", "brand_wikidata": "Q12063852"}
     sitemap_urls = ["https://www.newlook.com/uk/sitemap/maps/sitemap_uk_pos_en_1.xml"]
-    sitemap_rules = [(r"https:\/\/www\.newlook\.com\/uk\/store\/[-\w]+-(\d+)$", "parse_sd")]
+    sitemap_rules = [(r"https:\/\/www\.newlook\.com\/uk\/store\/[\w-]+(\d+)$", "parse_sd")]
     wanted_types = ["Store"]
 
     def sitemap_filter(self, entries):
