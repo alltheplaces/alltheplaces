4c5b77fe40a8e16859284ff2719f458b9f396770
Cj Malone <CjMalone@mail.com>
2026-02-12 08:55:02 +0000

    Clean address

--- locations/spiders/costa_coffee_cz.py
+++ locations/spiders/costa_coffee_cz.py
@@ -1,7 +1,11 @@
+from typing import Any
+
 import scrapy
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class CostaCoffeeCZSpider(scrapy.Spider):
@@ -9,7 +13,7 @@ class CostaCoffeeCZSpider(scrapy.Spider):
     item_attributes = {"brand": "Costa", "brand_wikidata": "Q608845"}
     start_urls = ["https://www.costa-coffee.cz/kavarny/"]
 
-    def parse(self, response, **kwargs):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for shop in response.xpath("//*[@data-latitude]"):
             item = Feature()
             item["ref"] = shop.xpath("./@id").get()
@@ -17,8 +21,11 @@ class CostaCoffeeCZSpider(scrapy.Spider):
             item["name"] = self.item_attributes["brand"]
             item["lat"] = shop.xpath("./@data-latitude").get()
             item["lon"] = shop.xpath("./@data-longitude").get()
-            item["postcode"] = shop.xpath(".//*[@class = 'address px-6']//span[2]/text()").get()
-            item["street_address"] = shop.xpath(".//*[@class = 'address px-6']//text()").get()
-            item["addr_full"] = ",".join(shop.xpath(".//*[@class = 'address px-6']//text()").getall())
+            item["street_address"] = shop.xpath(".//*[@class='address px-6']//text()").get()
+            item["postcode"] = shop.xpath(".//*[@class='address px-6']//span[2]/text()").get()
+            item["city"] = shop.xpath(".//*[@class='address px-6']//span[3]/text()").get()
+            item["addr_full"] = merge_address_lines(shop.xpath(".//*[@class='address px-6']//text()").getall())
+
             apply_category(Categories.COFFEE_SHOP, item)
+
             yield item
