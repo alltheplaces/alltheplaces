3a544e9a779e117fc354c91af4e5c174b6147e1c
applecuckoo <nufjoysb@duck.com>
2026-02-13 01:51:00 +1300

    [The Warehouse NZ] Add spider (#15219)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

---
+++ locations/spiders/the_warehouse_nz.py
@@ -0,0 +1,65 @@
+import json
+from typing import AsyncIterator, Iterable
+
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
+
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class TheWarehouseNZSpider(JSONBlobSpider):
+    name = "the_warehouse_nz"
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+    item_attributes = {"brand": "The Warehouse", "brand_wikidata": "Q110205200"}
+    locations_key = ["stores", "stores"]
+
+    async def start(self) -> AsyncIterator[JsonRequest | Request]:
+        for region in [
+            "NZ-NTL",
+            "NZ-AUK",
+            "NZ-WKO",
+            "NZ-BOP",
+            "NZ-GIS",
+            "NZ-TKI",
+            "NZ-MWT",
+            "NZ-HKB",
+            "NZ-WGN",
+            "NZ-TAS",
+            "NZ-MBH",
+            "NZ-WTC",
+            "NZ-CAN",
+            "NZ-OTA",
+            "NZ-STL",
+        ]:
+            yield Request(
+                "https://www.thewarehouse.co.nz/on/demandware.store/Sites-twl-Site/default/Stores-FindStores?region={}".format(
+                    region
+                )
+            )
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name", None)
+        item["addr_full"] = item["addr_full"].removeprefix("The Warehouse")
+        item["street_address"] = feature["address1"]
+
+        try:
+            item["opening_hours"] = self.parse_opening_hours(json.loads(feature["storeHoursJson"])["openingHours"])
+        except:
+            self.logger.error("Error paring opening hours: {}".format(feature["storeHoursJson"]))
+
+        apply_category(Categories.SHOP_DEPARTMENT_STORE, item)
+
+        yield item
+
+    def parse_opening_hours(self, rules: list[str]) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in rules:
+            day, times = rule.split(" ")
+            if times == "CLOSED":
+                oh.set_closed(day)
+            else:
+                oh.add_range(day, *times.split("-"))
+        return oh
