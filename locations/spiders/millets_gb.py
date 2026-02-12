2b67431d275c785858d9f639a7d61041e47a5ee5
Cj Malone <CjMalone@mail.com>
2026-02-12 11:32:44 +0000

    Collect coords

--- locations/spiders/millets_gb.py
+++ locations/spiders/millets_gb.py
@@ -11,7 +11,7 @@ class MilletsGBSpider(JSONBlobSpider):
     name = "millets_gb"
     item_attributes = {"brand": "Millets", "brand_wikidata": "Q64822903"}
     start_urls = [
-        "https://integrations-c3f9339ff060a907cbd8.o2.myshopify.dev/api/stores?fascia=Millets&limit=48&radius=100&fields=address%2Cname%2CmainPhone%2Cservices%2Chours%2Cc_fullName%2Cc_storeType%2CphotoGallery"
+        "https://integrations-c3f9339ff060a907cbd8.o2.myshopify.dev/api/stores?fascia=Millets&limit=48&radius=100"
     ]
     locations_key = "stores"
     custom_settings = {"ROBOTSTXT_OBEY": False}
