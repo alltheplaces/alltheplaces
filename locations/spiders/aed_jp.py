5fac398352861d19812bb01c2b6507e9ba56c6ff
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-02-13 19:33:55 +0900

    fix syntax error

--- locations/spiders/aed_jp.py
+++ locations/spiders/aed_jp.py
@@ -3,7 +3,7 @@ from typing import AsyncIterator
 from scrapy import Spider
 from scrapy.http import Request
 
-from locations.categories import Categories, apply_category
+from locations.categories import Categories, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
 
 
@@ -27,18 +27,16 @@ class AedJPSpider(Spider):
         for aed in aeds:
             item = DictParser.parse(aed)
             item["ref"] = aed["id"]
-            item["website"] = (
-                f"https://www.qqzaidanmap.jp/map/my_map?latitude={aed["location"]["latitude"]}&longitude={aed["location"]["longitude"]}&zoom=16&id={aed["id"]}"
-            )
             item["lat"] = aed["location"]["latitude"]
             item["lon"] = aed["location"]["longitude"]
+            item["website"] = f"https://www.qqzaidanmap.jp/map/my_map?latitude={item["lat"]}&longitude={item["lon"]}&zoom=16&id={aed["id"]}"
             apply_category(Categories.DEFIBRILLATOR, item)
             item["extras"]["defibrillator:location"] = aed["install_address_detail"]
             item["street_address"] = aed["install_address"]
             item["extras"]["addr:province"] = aed["install_prefecture_name"]
             item["name"] = aed["install_location_name"]
-
+            
             yield item
 
-        if data["pages"]["current_page"] < 2:  # data["pages"]["current_page"] < data["pages"]["total_pages"]:
+        if data["pages"]["current_page"] < 2: # data["pages"]["current_page"] < data["pages"]["total_pages"]:
             yield self.get_page(1 + response.meta["page"])
