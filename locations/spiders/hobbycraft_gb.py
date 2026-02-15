3c12209508d6a3452220e6c051a84daafd0cc6aa
Nat Zimmermann <ntzm@users.noreply.github.com>
2026-02-15 15:36:16 +0000

    [Hobbycraft GB] Add spider

---
+++ locations/spiders/hobbycraft_gb.py
@@ -0,0 +1,13 @@
+import scrapy
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class HobbycraftGBSpider(StructuredDataSpider):
+    name = "hobbycraft_gb"
+    item_attributes = {"brand": "Hobbycraft", "brand_wikidata": "Q16984508"}
+    start_urls = ["https://www.hobbycraft.co.uk/storelist/"]
+
+    def parse(self, response):
+        for link in response.xpath('//a[contains(@href, "/stores/")]/@href').getall():
+            yield scrapy.Request(url=response.urljoin(link), callback=self.parse_sd)
