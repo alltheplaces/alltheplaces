79e49df1b1aa0b8bd81458c9153c690688a22d6d
Nat Zimmermann <ntzm@users.noreply.github.com>
2026-02-15 15:48:39 +0000

    ignore generic fields

--- locations/spiders/hobbycraft_gb.py
+++ locations/spiders/hobbycraft_gb.py
@@ -8,6 +8,12 @@ class HobbycraftGBSpider(StructuredDataSpider):
     item_attributes = {"brand": "Hobbycraft", "brand_wikidata": "Q16984508"}
     start_urls = ["https://www.hobbycraft.co.uk/storelist/"]
 
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item.pop("facebook", None)
+        item.pop("image", None)
+        item.pop("twitter", None)
+        yield item
+
     def parse(self, response):
         for link in response.xpath('//a[contains(@href, "/stores/")]/@href').getall():
             yield scrapy.Request(url=response.urljoin(link), callback=self.parse_sd)
