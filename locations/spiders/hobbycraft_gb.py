61cb6cff8d8d0b23a9ee4261da75eb3c0207caf1
Cj Malone <CjMalone@mail.com>
2026-02-16 09:48:58 +0000

    Use CrawlSpider

--- locations/spiders/hobbycraft_gb.py
+++ locations/spiders/hobbycraft_gb.py
@@ -1,19 +1,17 @@
-import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class HobbycraftGBSpider(StructuredDataSpider):
+class HobbycraftGBSpider(CrawlSpider, StructuredDataSpider):
     name = "hobbycraft_gb"
     item_attributes = {"brand": "Hobbycraft", "brand_wikidata": "Q16984508"}
     start_urls = ["https://www.hobbycraft.co.uk/storelist/"]
+    rules = [Rule(LinkExtractor(allow="/stores/"), callback="parse_sd")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item.pop("facebook", None)
         item.pop("image", None)
         item.pop("twitter", None)
         yield item
-
-    def parse(self, response):
-        for link in response.xpath('//a[contains(@href, "/stores/")]/@href').getall():
-            yield scrapy.Request(url=response.urljoin(link), callback=self.parse_sd)
