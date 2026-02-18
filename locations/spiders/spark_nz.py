4c5419764cb863b619e7b9a3b6053c6e9f950ed2
applecuckoo <nufjoysb@duck.com>
2026-02-18 16:40:17 +1300

    [Spark NZ] add spider

---
+++ locations/spiders/spark_nz.py
@@ -0,0 +1,84 @@
+from typing import Iterable
+
+from scrapy.http import JsonRequest, Response
+
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class SparkNZSpider(JSONBlobSpider):
+    name = "spark_nz"
+    item_attributes = {"brand": "Spark New Zealand", "brand_wikidata": "Q1549526"}
+    locations_key = ["data", "location", "pointsOfInterest"]
+    no_refs = True
+
+    async def start(self):
+        yield JsonRequest(
+            url="https://api.spark.co.nz/graphql",
+            data={
+                "id": "PointsOfInterestQuery",
+                "query": """
+                query PointsOfInterestQuery(
+                  $serviceType: PointOfInterestServiceType!
+                  $location: String
+                ) {
+                  location {
+                    pointsOfInterest(serviceType: $serviceType, location: $location) {
+                      featured
+                      displayName
+                      isStore
+                      isWifiAvailable
+                      isRecyclingOffered
+                      addressLine1
+                      addressLine2
+                      emailAddress
+                      phoneNumber
+                      suburb
+                      city
+                      directions
+                      latitude
+                      longitude
+                      image
+                      distanceFromLocation
+                      contacts {
+                        role
+                        name
+                      }
+                      operatingHours {
+                        day
+                        open
+                        close
+                      }
+                    }
+                  }
+                }""",
+                "variables": {"location": None, "serviceType": "STORE"},
+            },
+        )
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("Spark ")
+        try:
+            item["opening_hours"] = self.parse_opening_hours(feature["operatingHours"])
+        except ValueError:
+            self.log("Error parsing opening hours: {}".format(feature["operatingHours"]))
+
+        apply_category(Categories.SHOP_MOBILE_PHONE, item)
+        yield item
+
+    def parse_opening_hours(self, business_hours: list) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in business_hours:
+            if rule["open"] is None:
+                oh.set_closed(rule["day"])
+            elif "|" in rule["open"]:
+                rule_per_day_count = len(rule["open"].split("|"))
+                open_rules = rule["open"].split("|")
+                closed_rules = rule["close"].split("|")
+                for rule_number in range(0, rule_per_day_count):
+                    oh.add_range(rule["day"], open_rules[rule_number], closed_rules[rule_number], "%H:%M:%S")
+            else:
+                oh.add_range(rule["day"], rule["open"], rule["close"], "%H:%M:%S")
+        return oh
