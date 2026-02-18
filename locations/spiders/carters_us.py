5e1ffc4c115a31cc7930934154a870bb7e0fe6d2
Cj Malone <CjMalone@mail.com>
2026-02-18 17:33:51 +0000

    Clean name

--- locations/spiders/carters_us.py
+++ locations/spiders/carters_us.py
@@ -3,6 +3,7 @@ from typing import Iterable
 from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
@@ -13,14 +14,22 @@ class CartersUSSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "Carter's", "brand_wikidata": "Q5047083"}
     allowed_domains = ["www.carters.com"]
     sitemap_urls = ["https://www.carters.com/sitemap_index.xml"]
-    sitemap_rules = [
-        (r"https://www.carters.com/l/.*/.*/.*", "parse_sd"),
-    ]
-    time_format = "%I:%M%p"
+    sitemap_follow = ["store-page"]
+    sitemap_rules = [(r"^https://www.carters.com/l/\w\w/[^/]+/[^/]+$", "parse_sd")]
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        if item["name"].startswith("Oshkosh "):
+            item["branch"] = item.pop("name").removeprefix("Oshkosh ").removesuffix(" OshKosh")
+            item["name"] = "Carter's OshKosh"
+        elif item["name"].startswith("Carter’s "):
+            item["branch"] = item.pop("name").removeprefix("Carter’s ").removesuffix(" Carters")
+            item["name"] = "Carter's"
+
         oh = OpeningHours()
         for day_time in ld_data["openingHours"]:
             oh.add_ranges_from_string(day_time)
         item["opening_hours"] = oh
+
+        apply_category(Categories.SHOP_CLOTHES, item)
+
         yield item
