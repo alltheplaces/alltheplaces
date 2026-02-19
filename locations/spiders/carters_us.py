b81418b24c04587fbddfb0ff792837a5ddab50fd
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-02-19 08:52:39 +0000

    Restore Carter's US

--- locations/spiders/carters_us.py
+++ locations/spiders/carters_us.py
@@ -11,29 +11,25 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class CartersUSSpider(SitemapSpider, StructuredDataSpider):
     name = "carters_us"
-    BRANDS = {
-        "carter": {"brand": "Carter's", "brand_wikidata": "Q5047083"},
-        "oshkosh": {"brand": "OshKosh B'gosh", "brand_wikidata": "Q1417347"},
-    }
+    item_attributes = {"brand": "Carter's", "brand_wikidata": "Q5047083"}
     allowed_domains = ["www.carters.com"]
     sitemap_urls = ["https://www.carters.com/sitemap_index.xml"]
     sitemap_follow = ["store-page"]
     sitemap_rules = [(r"^https://www.carters.com/l/\w\w/[^/]+/[^/]+$", "parse_sd")]
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        if "oshkosh" in response.url:
+        if item["name"].startswith("Oshkosh "):
             item["branch"] = item.pop("name").removeprefix("Oshkosh ").removesuffix(" OshKosh")
             item["name"] = "Carter's OshKosh"
-            item.update(self.BRANDS["oshkosh"])
-        else:
+        elif item["name"].startswith("Carter’s "):
             item["branch"] = item.pop("name").removeprefix("Carter’s ").removesuffix(" Carters")
             item["name"] = "Carter's"
-            item.update(self.BRANDS["carter"])
 
         oh = OpeningHours()
         for day_time in ld_data["openingHours"]:
             oh.add_ranges_from_string(day_time)
         item["opening_hours"] = oh
+
         apply_category(Categories.SHOP_CLOTHES, item)
 
         yield item
