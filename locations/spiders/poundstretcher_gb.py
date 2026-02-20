de4734277bb928b903f536f3de6b445d36ab8424
Nat Zimmermann <ntzm@users.noreply.github.com>
2026-02-20 11:36:20 +0000

    [Poundstretcher GB] Fix spider

--- locations/spiders/poundstretcher_gb.py
+++ locations/spiders/poundstretcher_gb.py
@@ -1,41 +1,30 @@
-from typing import Any
-from urllib.parse import urljoin
+from typing import Iterable
 
-from scrapy import Selector, Spider
-from scrapy.http import Response
-
-from locations.dict_parser import DictParser
+from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.storefinders.shopapps import ShopAppsSpider
 
 BARGAIN_BUYS = {"brand": "Bargain Buys", "brand_wikidata": "Q19870995"}
 POUNDSTRETCHER = {"brand": "Poundstretcher", "brand_wikidata": "Q7235675"}
 
 
-class PoundstretcherGBSpider(Spider):
+class PoundstretcherGBSpider(ShopAppsSpider):
     name = "poundstretcher_gb"
     item_attributes = POUNDSTRETCHER
-    start_urls = ["https://www.poundstretcher.co.uk/ustorelocator/location/searchJson/"]
+    key = "pound-stretcher-store.myshopify.com"
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json()["markers"]:
-            item = DictParser.parse(location)
-            item["branch"] = item.pop("name")
-            item["addr_full"] = location["address_display"]
-            item["website"] = urljoin("https://www.poundstretcher.co.uk/", location["website_url"])
+    def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
+        item.pop("email")  # Emails always the same
+        item["branch"] = item.pop("name")
+        item["website"] = location["website"].replace("pound-stretcher-store.myshopify.com", "poundstretcher.co.uk")
 
-            if item["branch"].endswith(" Bargain Buys"):
-                item["branch"] = item["branch"].removesuffix(" Bargain Buys")
-                item.update(BARGAIN_BUYS)
+        if item["branch"].endswith(" Bargain Buys"):
+            item["branch"] = item["branch"].removesuffix(" Bargain Buys")
+            item.update(BARGAIN_BUYS)
 
-            try:
-                sel = Selector(text=location["notes"])
-                oh = OpeningHours()
-                for rule in sel.xpath("//tr"):
-                    times = rule.xpath("./td/text()").get()
-                    start_time, end_time = times.split("-")
-                    oh.add_range(rule.xpath("./th/text()").get(), start_time.strip(), end_time.strip())
-                item["opening_hours"] = oh
-            except:
-                pass
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(location.get("hours", ""))
 
-            yield item
+        apply_category(Categories.SHOP_VARIETY_STORE, item)
+        yield item
