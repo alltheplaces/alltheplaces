270af8416f9487d1357ee0ed3da96a48bda73987
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-02-21 21:15:28 +0900

    [Kuroneko Yamato JP] add spider
    Not getting anything in dense cities, need to make another bunch of centroids for that. 24 km is not small enough.
    
    {'atp/brand/NewDays': 22,
     'atp/brand/セブン-イレブン': 10262,
     'atp/brand/デイリーヤマザキ': 534,
     'atp/brand/ファミリーマート': 7072,
     'atp/brand/ポプラ': 63,
     'atp/brand/ヤマト運輸': 2462,
     'atp/brand_wikidata/Q11234763': 22,
     'atp/brand_wikidata/Q11247682': 7072,
     'atp/brand_wikidata/Q259340': 10262,
     'atp/brand_wikidata/Q5209392': 534,
     'atp/brand_wikidata/Q6584353': 2462,
     'atp/brand_wikidata/Q7229380': 63,
     'atp/category/amenity/parcel_locker': 1925,
     'atp/category/amenity/post_office': 2462,
     'atp/category/missing': 1675,
     'atp/category/shop/convenience': 17953,
     'atp/country/JP': 24015,
     'atp/duplicate_count': 9360,
     'atp/field/branch/missing': 24015,
     'atp/field/brand/missing': 3600,
     'atp/field/brand_wikidata/missing': 3600,
     'atp/field/city/missing': 24015,
     'atp/field/country/from_spider_name': 24015,
     'atp/field/email/missing': 24015,
     'atp/field/image/missing': 24015,
     'atp/field/opening_hours/missing': 3714,
     'atp/field/operator/missing': 21553,
     'atp/field/operator_wikidata/missing': 21553,
     'atp/field/phone/invalid': 4,
     'atp/field/phone/missing': 2,
     'atp/field/postcode/missing': 24015,
     'atp/field/state/missing': 24015,
     'atp/field/street_address/missing': 24015,
     'atp/field/twitter/missing': 24015,
     'atp/item_scraped_host_count/www.e-map.ne.jp': 33375,
     'atp/lineage': 'S_?',
     'atp/nsi/cc_match': 17334,
     'atp/nsi/perfect_match': 3081,
     'atp/operator/ヤマト運輸': 2462,
     'atp/operator_wikidata/Q6584353': 2462,
     'downloader/request_bytes': 171525,
     'downloader/request_count': 273,
     'downloader/request_method_count/GET': 273,
     'downloader/response_bytes': 13514643,
     'downloader/response_count': 273,
     'downloader/response_status_count/200': 273,
     'dupefilter/filtered': 26,
     'elapsed_time_seconds': 332.96574,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 2, 21, 11, 42, 27, 875209, tzinfo=datetime.timezone.utc),
     'item_dropped_count': 9360,
     'item_dropped_reasons_count/DropItem': 9360,
     'item_scraped_count': 24015,
     'items_per_minute': 4340.060240963856,
     'log_count/DEBUG': 33649,
     'log_count/ERROR': 10,
     'log_count/INFO': 8,
     'request_depth_max': 1,
     'response_received_count': 273,
     'responses_per_minute': 49.337349397590366,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/200': 1,
     'scheduler/dequeued': 272,
     'scheduler/dequeued/memory': 272,
     'scheduler/enqueued': 272,
     'scheduler/enqueued/memory': 272,
     'spider_exceptions/ValueError': 10,
     'spider_exceptions/count': 10,
     'start_time': datetime.datetime(2026, 2, 21, 11, 36, 54, 909469, tzinfo=datetime.timezone.utc)}

---
+++ locations/spiders/kuroneko_jp.py
@@ -0,0 +1,81 @@
+import csv
+from io import StringIO
+
+from chompjs import parse_js_object
+from scrapy import Request, Spider
+
+from locations.categories import Categories, Extras, Fuel, apply_category, apply_yes_no
+from locations.geo import country_iseadgg_centroids
+from locations.hours import DAYS, OpeningHours
+from locations.items import Feature
+
+BRANDS = {
+    "001": ("ファミリーマート", "Q11247682"),
+    "002": ("セブン-イレブン", "Q259340"),
+    "101": ("ポプラ", "Q7229380"),
+    "171": ("ポプラ", "Q7229380"),
+    "418": ("NewDays", "Q11234763"),
+    "436": ("デイリーヤマザキ", "Q5209392"),
+    "YTC": ("ヤマト運輸", "Q6584353"),
+}
+MAX_ITEMS = 1640  # determined experimentally
+RADIUS_KM = 24
+MAP_ID = "yamato01"  # for storefinder
+
+
+class KuronekoJPSpider(Spider):
+    name = "kuroneko_jp"
+    
+    def make_request(self, lat, lon, offset=1, count=900):
+        radius_m = RADIUS_KM * 1000
+        return Request(
+            f"https://www.e-map.ne.jp/p/{MAP_ID}/zdcemaphttp.cgi?target=http%3A%2F%2F127.0.0.1%2Fcgi%2Fnkyoten.cgi%3F%26cid%3D{MAP_ID}%26pos%3D{offset}%26lat%3D{lat}%26lon%3D{lon}%26knsu%3D{MAX_ITEMS}%26cnt%3D{count}%26hour%3D1%26rad%3D{radius_m}&zdccnt=1",
+            cb_kwargs={"lat": lat, "lon": lon, "offset": offset},
+        )
+
+    async def start(self):
+        for lat, lon in country_iseadgg_centroids("JP", RADIUS_KM):
+            yield self.make_request(lat, lon)
+
+    def parse(self, response, lat, lon, offset):
+        # response is an EUC-encoded JS file that looks like
+        #   ZdcEmapHttpResult[1] = '...';
+        # where the string body is a TSV
+        js_body = response.body.decode("euc-jp")
+        # chompjs sees the array index as an array itself, so get just the string itself:
+        js_str = js_body[js_body.find("'") : js_body.rfind("'") + 1]
+        # For some reason, neither Python json nor chompjs like just the string on its own, so wrap it in an array
+        js_ls = f"[{js_str}]"
+        (tsv_str,) = parse_js_object(js_ls)
+        reader = csv.reader(StringIO(tsv_str), delimiter="\t")
+        ret_code, rec_count, hit_count = map(int, next(reader))
+        assert rec_count <= hit_count, (rec_count, hit_count)
+        if hit_count >= MAX_ITEMS:
+            self.logger.warning("Maximum number of items returned in one query, consider lowering the radius")
+        if rec_count >= hit_count:
+            yield self.make_request(lat, lon, offset + rec_count)
+        for row in reader:
+            item = Feature()
+            item["ref"] = row[0]
+            item["website"] = f"https://www.e-map.ne.jp/p/{MAP_ID}/dtl/{row[0]}/"
+            item["lat"] = row[1]
+            item["lon"] = row[2]
+            if row[3] == "YTC":
+                apply_category(Categories.POST_OFFICE, item)
+            elif row[3] == "563":
+                apply_category(Categories.PARCEL_LOCKER, item)
+            else:
+                apply_category(Categories.POST_PARTNER, item)
+                item["extras"]["post_office:service_provider"] = "ヤマト運輸"          
+            
+            item["brand"], item["brand_wikidata"] = BRANDS.get(row[3], (None, None))
+            item["name"] = row[6]
+            item["addr_full"] = row[7]
+            item["phone"] = row[16]
+            
+            # row 35-42 =1 is days the place is closed. 35-41 is Mon-Sun, 42 is holidays. row 43 =1 means open every day.
+            
+            if row[44] == "1":
+                item["opening_hours"] = "24/7"
+            
+            yield item
