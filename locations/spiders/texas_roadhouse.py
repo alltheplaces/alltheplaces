1f5258f09265b2258dde1efae7ac70e98bb5fbd5
applecuckoo <nufjoysb@duck.com>
2026-02-21 20:54:40 +1300

    [Texas Roadhouse] rebase on NomNomSpider

--- locations/spiders/texas_roadhouse.py
+++ locations/spiders/texas_roadhouse.py
@@ -1,42 +1,10 @@
-from scrapy.spiders import SitemapSpider
-
-from locations.hours import OpeningHours
-from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address
+from locations.storefinders.nomnom import NomNomSpider
 
 
-class TexasRoadhouseSpider(SitemapSpider):
+class TexasRoadhouseSpider(NomNomSpider):
     name = "texas_roadhouse"
     item_attributes = {
         "brand": "Texas Roadhouse",
         "brand_wikidata": "Q7707945",
     }
-
-    sitemap_urls = ["https://www.texasroadhouse.com/sitemap.xml"]
-    sitemap_rules = [("/locations/.*", "parse_store")]
-
-    def parse_hours(self, store_hours):
-        opening_hours = OpeningHours()
-        for store_times in store_hours:
-            if ":" in store_times:
-                day, times = store_times.strip().split(" : ")
-                if "Closed" in times:
-                    opening_hours.set_closed(day)
-                else:
-                    open_time, close_time = times.split(" - ")
-                    opening_hours.add_range(
-                        day=day,
-                        open_time=open_time.replace(" ", ""),
-                        close_time=close_time.replace(" ", ""),
-                        time_format="%I:%M%p",
-                    )
-        return opening_hours.as_opening_hours()
-
-    def parse_store(self, response):
-        item = Feature()
-        item["branch"] = response.xpath('//*[@class="vendor-block-content-top"]/h2/text()').get().strip()
-        item["ref"] = item["branch"]
-        item["addr_full"] = clean_address(response.xpath('//*[@class="store-address"]/text()').getall())
-        item["phone"] = response.xpath('.//*[contains(@href, "tel")]/@href').get()
-        item["opening_hours"] = self.parse_hours(response.xpath('//*[@class="open-dine-hours"]/ul/li/text()').getall())
-        yield item
+    start_urls = ["https://www.texasroadhouse.com/api/stores"]
