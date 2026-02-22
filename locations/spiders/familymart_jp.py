b63b8e16e0d23c1813cc14d377c42e3e255dca8b
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-02-22 23:09:54 +0900

    [FamilyMart JP] add spider
    {'atp/brand/ファミマ!!': 45,
     'atp/brand/ファミリーマート': 15130,
     'atp/brand_wikidata/Q11247682': 15130,
     'atp/brand_wikidata/Q115868189': 45,
     'atp/category/shop/convenience': 15175,
     'atp/country/JP': 15175,
     'atp/field/city/missing': 15175,
     'atp/field/country/from_spider_name': 15175,
     'atp/field/email/missing': 15175,
     'atp/field/image/missing': 15175,
     'atp/field/opening_hours/invalid': 1359,
     'atp/field/operator/missing': 15175,
     'atp/field/operator_wikidata/missing': 15175,
     'atp/field/phone/invalid': 3,
     'atp/field/state/missing': 15175,
     'atp/field/street_address/missing': 15175,
     'atp/field/twitter/missing': 15175,
     'atp/item_scraped_host_count/store.family.co.jp': 15175,
     'atp/lineage': 'S_?',
     'atp/nsi/cc_match': 15130,
     'atp/nsi/perfect_match': 45,
     'downloader/request_bytes': 17703,
     'downloader/request_count': 52,
     'downloader/request_method_count/GET': 52,
     'downloader/response_bytes': 1524370,
     'downloader/response_count': 52,
     'downloader/response_status_count/200': 52,
     'elapsed_time_seconds': 68.586948,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 2, 22, 14, 1, 46, 547816, tzinfo=datetime.timezone.utc),
     'httpcompression/response_bytes': 17282527,
     'httpcompression/response_count': 49,
     'item_scraped_count': 15175,
     'items_per_minute': 13389.705882352942,
     'log_count/DEBUG': 15227,
     'log_count/ERROR': 2,
     'log_count/INFO': 4,
     'response_received_count': 52,
     'responses_per_minute': 45.88235294117647,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/200': 1,
     'scheduler/dequeued': 51,
     'scheduler/dequeued/memory': 51,
     'scheduler/enqueued': 51,
     'scheduler/enqueued/memory': 51,
     'spider_exceptions/KeyError': 2,
     'spider_exceptions/count': 2,
     'start_time': datetime.datetime(2026, 2, 22, 14, 0, 37, 960868, tzinfo=datetime.timezone.utc)}

---
+++ locations/spiders/familymart_jp.py
@@ -0,0 +1,104 @@
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category, apply_yes_no
+from locations.dict_parser import DictParser
+
+
+class FamilymartJPSpider(Spider):
+    name = "familymart_jp"
+    item_attributes = {"brand": "ファミリーマート", "brand_wikidata": "Q11247682", "extras": Categories.SHOP_CONVENIENCE.value}
+
+    start_urls = [
+        "https://store.family.co.jp/api/points/w",
+        "https://store.family.co.jp/api/points/xjb",
+        "https://store.family.co.jp/api/points/xn0",
+        "https://store.family.co.jp/api/points/xn1",
+        "https://store.family.co.jp/api/points/xn2",
+        "https://store.family.co.jp/api/points/xn3",
+        "https://store.family.co.jp/api/points/xn4",
+        "https://store.family.co.jp/api/points/xn5",
+        "https://store.family.co.jp/api/points/xn6",
+        "https://store.family.co.jp/api/points/xn70",
+        "https://store.family.co.jp/api/points/xn71",
+        "https://store.family.co.jp/api/points/xn72",
+        "https://store.family.co.jp/api/points/xn73",
+        "https://store.family.co.jp/api/points/xn74",
+        "https://store.family.co.jp/api/points/xn75",
+        "https://store.family.co.jp/api/points/xn76",
+        "https://store.family.co.jp/api/points/xn77",
+        "https://store.family.co.jp/api/points/xn78",
+        "https://store.family.co.jp/api/points/xn79",
+        "https://store.family.co.jp/api/points/xn7b",
+        "https://store.family.co.jp/api/points/xn7c",
+        "https://store.family.co.jp/api/points/xn7d",
+        "https://store.family.co.jp/api/points/xn7e",
+        "https://store.family.co.jp/api/points/xn7f",
+        "https://store.family.co.jp/api/points/xn7g",
+        "https://store.family.co.jp/api/points/xn7h",
+        "https://store.family.co.jp/api/points/xn7k",
+        "https://store.family.co.jp/api/points/xn7j",
+        "https://store.family.co.jp/api/points/xn7m",
+        "https://store.family.co.jp/api/points/xn7n",
+        "https://store.family.co.jp/api/points/xn7p",
+        "https://store.family.co.jp/api/points/xn7q",
+        "https://store.family.co.jp/api/points/xn7r",
+        "https://store.family.co.jp/api/points/xn7s",
+        "https://store.family.co.jp/api/points/xn7t",
+        "https://store.family.co.jp/api/points/xn7u",
+        "https://store.family.co.jp/api/points/xn7v",
+        "https://store.family.co.jp/api/points/xn7w",
+        "https://store.family.co.jp/api/points/xn7x",
+        "https://store.family.co.jp/api/points/xn7y",
+        "https://store.family.co.jp/api/points/xn7z",
+        "https://store.family.co.jp/api/points/xnk",
+        "https://store.family.co.jp/api/points/xn9",
+        "https://store.family.co.jp/api/points/xnd",
+        "https://store.family.co.jp/api/points/xne",
+        "https://store.family.co.jp/api/points/xns",
+        "https://store.family.co.jp/api/points/xnf",
+        "https://store.family.co.jp/api/points/xng",
+        "https://store.family.co.jp/api/points/xnu",
+        "https://store.family.co.jp/api/points/xp",
+        "https://store.family.co.jp/api/points/z",        
+    ]
+    
+    allowed_domains = ["store.family.co.jp"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store in response.json()["items"]:
+
+            item = DictParser.parse(store)
+            
+            match store["extra_fields"]["C1"]:
+                case "1":
+                    item.update({"brand_wikidata": "Q11247682"})
+                    apply_category(Categories.SHOP_CONVENIENCE, item)
+                case "2":
+                    item.update({"brand_wikidata": "Q115868189"})
+                    item.update({"brand": "ファミマ!!"})
+                    apply_category(Categories.SHOP_CONVENIENCE, item)
+                case "3":
+                    item.update({"brand_wikidata": "Q11247682"}) # using familymart until tomony is accepted.
+                    # item.update({"brand_wikidata": "Q11249798"}) # no NSI for this one yet, pull request submitted 2026-Feb-22
+                    # item.update({"brand": "TOMONY"})
+                    # item["extras"]["brand:en"] = "TOMONY"
+                    apply_category(Categories.SHOP_CONVENIENCE, item)
+                case _:
+                    item.update({"brand_wikidata": "Q11247682"})
+                    apply_category(Categories.SHOP_CONVENIENCE, item)
+
+            if store["extra_fields"]["I1"] == "24時間":
+                item["opening_hours"] = "24/7"
+            else:
+                item["opening_hours"] = store["extra_fields"]["I1"]
+            
+            item["ref"] = store["key"]
+            item["website"] = f"https://store.family.co.jp/points/{store['key']}"
+            item["phone"] = f"+81 {store['extra_fields']['Tel']}"
+            item["postcode"] = store["extra_fields"]["ZipCode"]
+            item["branch"] = store["name"]
+            
+            yield item
