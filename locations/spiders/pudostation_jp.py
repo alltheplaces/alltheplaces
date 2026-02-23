e6d109ddb57daf6288db915f2c29417a6b548b71
Cj Malone <CjMalone@mail.com>
2026-02-23 13:54:34 +0000

    Clean name

--- locations/spiders/pudostation_jp.py
+++ locations/spiders/pudostation_jp.py
@@ -1,26 +1,24 @@
-from typing import AsyncIterator
+from typing import Any, AsyncIterator
 
 from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy.http import JsonRequest, Response
 
 from locations.dict_parser import DictParser
 
 
 class PudostationJPSpider(Spider):
     name = "pudostation_jp"
+    item_attributes = {"brand_wikidata": "Q86738066"}
 
     async def start(self) -> AsyncIterator[JsonRequest]:
         for points in ["w", "x", "z"]:
             yield JsonRequest(url=f"https://map.pudo.jp/api/points/{points}")
 
-    item_attributes = {
-        "brand_wikidata": "Q86738066",
-    }
-
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json()["items"]:
 
             item = DictParser.parse(store)
+            item["branch"] = item.pop("name")
             item["ref"] = store["key"]
             item["website"] = f"https://map.pudo.jp/points/{store['key']}"
             item["postcode"] = store["extra_fields"]["Zip code"]
