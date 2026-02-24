a74a7a44c202b1584c434363cc01fbf135c96a1e
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-02-24 15:13:35 +0100

    Update miniso_gb.py

--- locations/spiders/miniso_gb.py
+++ locations/spiders/miniso_gb.py
@@ -1,14 +1,21 @@
-
 import chompjs
+import json
+from typing import Iterable
+
 from scrapy.http import Response
 
 from locations.json_blob_spider import JSONBlobSpider
+from locations.items import Feature
+from locations.hours import DAYS_FULL, OpeningHours
 
 
 class MinisoGBSpider(JSONBlobSpider):
     name = "miniso_gb"
     item_attributes = {"brand": "Miniso", "brand_wikidata": "Q20732498"}
     start_urls = ["https://minisoshop.co.uk/store-locator"]
+    drop_attributes = {"facebook", "phone", "email", "twitter"}
+    TIME_FORMAT = "%I:%M%p"
+    requires_proxy = "GB"
 
     def extract_json(self, response: Response) -> dict | list[dict]:
         data_raw = response.xpath("//script[contains(text(), 'jsonLocations:')]/text()").get()
@@ -16,3 +23,17 @@ class MinisoGBSpider(JSONBlobSpider):
         data_raw = data_raw.split('jsonLocations: {"items":', 1)[1]
         features_dict = chompjs.parse_js_object(data_raw)
         return features_dict
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").replace("MINISO ","").split(", ")[0]
+        item["street_address"] = item.pop("addr_full")
+        item["website"] = ""
+        oh = OpeningHours()
+        for day in DAYS_FULL:
+            time = json.loads(feature["time_store"])[f"{day.lower()}"]
+            if json.loads(feature["time_store"])[f"{day.lower()}_time"] == "1":
+                open_time = time["from"]["hours"] + ":" + f'{time["to"]["minutes"]:02}' + "AM"
+                close_time = time["to"]["hours"] + ":" + f'{time["to"]["minutes"]:02}' + "PM"
+                oh.add_range(day, open_time, close_time)
+        item["opening_hours"] = oh
+        yield item
