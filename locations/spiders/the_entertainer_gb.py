517914d8fb115af52732063beae138b11867322f
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-02-25 22:06:39 +0100

    Update the_entertainer_gb.py

--- locations/spiders/the_entertainer_gb.py
+++ locations/spiders/the_entertainer_gb.py
@@ -31,7 +31,7 @@ class TheEntertainerGBSpider(JSONBlobSpider):
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         item["ref"] = feature["address"]["id"]
-        item["website"] = "https://www.thetoyshop.com" + feature["name"].lower().replace(" ","-")
+        item["website"] = "https://www.thetoyshop.com/" + feature["name"].lower().replace(" ","-")
         item["street_address"] = merge_address_lines([feature["address"].get("line1"), feature["address"].get("line2")])
         item["branch"] = item.pop("name")
         if "tesco" in item["website"]:
