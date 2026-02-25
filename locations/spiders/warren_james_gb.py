47601960e82cf4f4011cafbde216b9bdb9ab37d4
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-02-25 16:50:15 +0100

    Add branch

--- locations/spiders/warren_james_gb.py
+++ locations/spiders/warren_james_gb.py
@@ -1,7 +1,11 @@
+from typing import Iterable
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Response, TextResponse
 
 from locations.structured_data_spider import StructuredDataSpider
+from locations.items import Feature
 
 
 class WarrenJamesGBSpider(CrawlSpider, StructuredDataSpider):
@@ -16,3 +20,7 @@ class WarrenJamesGBSpider(CrawlSpider, StructuredDataSpider):
     ]
     wanted_types = ["localBusiness"]
     drop_attributes = {"facebook"}
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["name"], item["branch"] = item["name"].split(" - ")
+        yield item
