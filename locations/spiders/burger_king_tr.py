855be9d6957f4cc3ec65aac0c319fd875441a86f
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2026-02-25 05:36:14 -0500

    [Burger King TR] Fix spider (#15383)
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>

--- locations/spiders/burger_king_tr.py
+++ locations/spiders/burger_king_tr.py
@@ -1,20 +1,23 @@
-from scrapy import Spider
+from typing import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
+from locations.items import Feature
 from locations.spiders.burger_king import BURGER_KING_SHARED_ATTRIBUTES
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class BurgerKingTRSpider(Spider):
+class BurgerKingTRSpider(SitemapSpider, StructuredDataSpider):
     name = "burger_king_tr"
     item_attributes = BURGER_KING_SHARED_ATTRIBUTES
-    start_urls = ["https://www.burgerking.com.tr/Restaurants/GetRestaurants/"]
+    sitemap_urls = ["https://www.burgerking.com.tr/restaurants_tr.xml"]
+    sitemap_rules = [(r"https://www.burgerking.com.tr/restoranlar-subeler/[^/]+/[^/]+/[^/]+/?$", "parse_sd")]
 
-    def parse(self, response):
-        for poi in response.json():
-            poi.update(poi.pop("data"))
-            item = DictParser.parse(poi)
-            item["ref"] = poi.get("title")
-            item["street_address"] = item.pop("addr_full", None)
-            apply_category(Categories.FAST_FOOD, item)
-            yield item
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["ref"] = item["website"] = response.url
+        item["name"] = item["phone"] = None
+        item["branch"] = response.xpath('//*[@class="current-page-name"]/text()').get("").strip()
+        apply_category(Categories.FAST_FOOD, item)
+        yield item
