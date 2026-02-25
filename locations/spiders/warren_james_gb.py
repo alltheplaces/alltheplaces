d05c57095c5ab353ff3ab06f27a321a708996256
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-02-25 16:27:16 +0100

    [Warren James GB] Add spider

---
+++ locations/spiders/warren_james_gb.py
@@ -0,0 +1,17 @@
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from locations.structured_data_spider import StructuredDataSpider
+
+class WarrenJamesGBSpider(CrawlSpider, StructuredDataSpider):
+    name = "warren_james_gb"
+    item_attributes = {"brand": "Warren James", "brand_wikidata": "Q19604616"}
+    start_urls = ["https://www.warrenjames.co.uk/shop-locator/"]
+    rules = [
+        Rule(
+            LinkExtractor(allow=r"/shop-locator/[^/]+$"),
+            callback="parse_sd",
+        )]
+
+    wanted_types = ["localBusiness"]
