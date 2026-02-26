fae2170eaf577b84d912a75170f07252cf61ad7f
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2026-02-26 09:42:39 -0500

    [Hornbach] Fix spider and add more countries (#15391)
    Co-authored-by: Ehsan Firdaus <ehsan.firdaus@gmail.com>

--- locations/spiders/hornbach.py
+++ locations/spiders/hornbach.py
@@ -1,52 +1,68 @@
-from typing import Any
+from typing import Any, Iterable
 
-import scrapy
-from scrapy.http import Response
+import chompjs
+from scrapy.http import JsonRequest, Response, TextResponse
 
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
-from locations.hours import DAYS_CH, OpeningHours, day_range
+from locations.hours import OpeningHours, day_range
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class HornbachSpider(scrapy.Spider):
+class HornbachSpider(JSONBlobSpider):
     name = "hornbach"
-    BODENHAUS = {"name": "Bodenhaus", "brand": "Bodenhaus"}
-    HORNBACH = {"brand": "HORNBACH", "brand_wikidata": "Q685926"}
-    start_urls = [
-        "https://www.hornbach.de/hornbach/cms/de/de/technik/alle-maerkte/alle-maerkte.json",
-        "https://www.hornbach.at/hornbach/cms/at/de/technik/alle-maerkte/alle-maerkte.json",
-        "https://www.hornbach.ch/hornbach/cms/ch/de/technik/alle-maerkte/alle-maerkte.json",
-        "https://www.hornbach.lu/hornbach/cms/lu/de/technik/alle-maerkte/alle-maerkte.json",
-    ]
-    custom_settings = {
-        "ROBOTSTXT_OBEY": False,
-        "DOWNLOAD_HANDLERS": {"https": "scrapy.core.downloader.handlers.http2.H2DownloadHandler"},
+    BRANDS = {
+        "bodenhaus": {"name": "Bodenhaus", "brand": "Bodenhaus"},
+        "hornbach": {"brand": "HORNBACH", "brand_wikidata": "Q685926"},
     }
+    start_urls = [
+        "https://www.bodenhaus.de",
+        "https://www.hornbach.de",
+        "https://www.hornbach.at",
+        "https://www.hornbach.ch",
+        "https://www.hornbach.lu",
+        "https://www.hornbach.cz",
+        "https://www.hornbach.nl",
+        "https://www.hornbach.ro",
+        "https://www.hornbach.se",
+        "https://www.hornbach.sk",
+    ]
+    custom_settings = {"DOWNLOAD_TIMEOUT": 180}
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for store in response.json()["stores"]:
-            item = DictParser.parse(store)
+        client_config = chompjs.parse_js_object(
+            response.xpath('//script[contains(text(), "OIDC_CLIENT_CONFIG")]/text()').get()
+        )
+        locale = client_config.get("ui_locales")
+        company_code = client_config.get("companyCode")
+        if locale and company_code:
+            yield JsonRequest(
+                url=f"https://svc.hornbach.de/cmscontent-service/stores?language={locale.replace('-', '_')}&companyCode={company_code}",
+                callback=self.parse_locations,
+            )
 
-            if item["name"].startswith("BODENHAUS "):
-                item["branch"] = item.pop("name").removeprefix("BODENHAUS ")
-                item.update(self.BODENHAUS)
-                apply_category(Categories.SHOP_FLOORING, item)
-            elif item["name"].startswith("HORNBACH "):
-                item["branch"] = item.pop("name").removeprefix("HORNBACH ")
-                item.update(self.HORNBACH)
+    def parse_locations(self, response: TextResponse) -> Any:
+        yield from super().parse(response)
 
-            oh = OpeningHours()
-            for time in store["regularTimes"]:
-                if "-" in time["day"]:
-                    start, end = time["day"].replace(".", "").replace(" ", "").split("-", maxsplit=1)
-                    if "hornbach.ch" in store["url"]:
-                        start = DAYS_CH.get(start)
-                        end = DAYS_CH.get(end)
-                    oh.add_days_range(day_range(start, end), time["from"], time["until"])
-                else:
-                    day = time["day"].replace(".", "").replace(" ", "")
-                    if "hornbach.ch" in store["url"]:
-                        day = DAYS_CH.get(day)
-                    oh.add_range(day, time["from"], time["until"], time_format="%H:%M")
-            item["opening_hours"] = oh
-            yield item
+    def pre_process_data(self, feature: dict) -> None:
+        feature.update(feature.pop("data"))
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = item.pop("street")
+        item["branch"] = item.pop("name").removeprefix("BODENHAUS ").removeprefix("HORNBACH ")
+        if brand_info := self.BRANDS.get(feature["client"]):
+            item.update(brand_info)
+        if feature["client"] == "bodenhaus":
+            apply_category(Categories.SHOP_FLOORING, item)
+
+        item["opening_hours"] = self.parse_opening_hours(feature.get("simplifiedOpeningHours", []))
+        yield item
+
+    def parse_opening_hours(self, rules: list[dict]) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in rules:
+            start_day = rule.get("weekdayFrom")
+            end_day = rule.get("weekdayTo") or start_day
+            if start_day and end_day:
+                oh.add_days_range(day_range(start_day, end_day), rule["timeFrom"], rule["timeTo"], "%H:%M:%S")
+        return oh
