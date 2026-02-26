08821524ef29bc043db0919caa2a2bfe2f436a05
Cj Malone <CjMalone@mail.com>
2026-02-26 11:00:33 +0000

    Simplfy categories

--- locations/spiders/dsk_bank_bg.py
+++ locations/spiders/dsk_bank_bg.py
@@ -1,26 +1,31 @@
-import scrapy
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
 
 
-class DskBankBGSpider(scrapy.Spider):
+class DskBankBGSpider(Spider):
     name = "dsk_bank_bg"
     item_attributes = {"brand": "Банка ДСК", "brand_wikidata": "Q5206146"}
-    allowed_domains = ["https://www.dskbank.bg/", "https://dskbank.bg/"]
+    allowed_domains = ["https://dskbank.bg/"]
     start_urls = ["https://dskbank.bg/GetOfficesData?cultureName=bg"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for data in response.json():
             item = DictParser.parse(data)
+            item["phone"] = None
             item["ref"] = data["office_id"]
-            item["name"] = data["office_name"]
             item["street_address"] = data["office_address"]
             item["lat"] = data["office_latitude"]
             item["lon"] = data["office_longitude"]
-            if (data["office_type"] == ["Branch", "Atm"]) or (data["office_type"] == ["Branch"]):
+
+            if "Branch" in data["office_type"]:
                 apply_category(Categories.BANK, item)
-                apply_yes_no(Extras.ATM, item, True if "Atm" in data["office_type"] else False)
+                apply_yes_no(Extras.ATM, item, "Atm" in data["office_type"])
             elif data["office_type"] == ["Atm"]:
                 apply_category(Categories.ATM, item)
+
             yield item
