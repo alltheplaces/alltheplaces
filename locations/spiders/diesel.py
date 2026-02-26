2cadfd536fe74c9ea74b1fc488c87a11e0e8eb75
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-02-26 14:29:06 +0530

    code updated to use sitemap to fix spider

--- locations/spiders/diesel.py
+++ locations/spiders/diesel.py
@@ -1,25 +1,20 @@
 from typing import Iterable
 
 from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories, apply_category
 from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class DieselSpider(JSONBlobSpider):
+class DieselSpider(SitemapSpider, StructuredDataSpider):
     name = "diesel"
     item_attributes = {"brand": "Diesel", "brand_wikidata": "Q158285"}
-    start_urls = [
-        "https://uk.diesel.com/on/demandware.store/Sites-DieselCA-Site/en_CA/StoreFinder-SearchByBoundaries?latmin=-90&latmax=90&lngmin=-180&lngmax=180"
-    ]
-    locations_key = ["stores", "stores"]
+    sitemap_urls = ["https://uk.diesel.com/en/sitemap-stores-en-gb.xml"]
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
         if email := item.get("email"):
             item["email"] = email.replace(" ", "")
-
-        item["website"] = item["ref"] = "https://uk.diesel.com/en/store-detail?sid={}".format(feature["ID"])
 
         if item["name"].startswith("DIESEL ACCESSORIES STORE "):
             item["branch"] = item.pop("name").removeprefix("DIESEL ACCESSORIES STORE ")
@@ -44,7 +39,4 @@ class DieselSpider(JSONBlobSpider):
             item["name"] = "Diesel Womenswear"
         else:
             item["branch"] = item.pop("name").removeprefix("DIESEL ")
-
-        apply_category(Categories.SHOP_CLOTHES, item)
-
         yield item
