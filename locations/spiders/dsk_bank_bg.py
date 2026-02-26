a318ff1feb85aaf911d947514c35adf3cc696326
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-02-26 15:02:17 +0530

    updated start_urls and parse method to fix spider

--- locations/spiders/dsk_bank_bg.py
+++ locations/spiders/dsk_bank_bg.py
@@ -2,31 +2,25 @@ import scrapy
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_BG, OpeningHours
 
 
 class DskBankBGSpider(scrapy.Spider):
     name = "dsk_bank_bg"
     item_attributes = {"brand": "Банка ДСК", "brand_wikidata": "Q5206146"}
-    allowed_domains = ["https://www.dskbank.bg/"]
-    start_urls = ["https://dskbank.bg/контакти/клонова-мрежа/GetOffices/"]
+    allowed_domains = ["https://www.dskbank.bg/", "https://dskbank.bg/"]
+    start_urls = ["https://dskbank.bg/GetOfficesData?cultureName=bg"]
 
     def parse(self, response):
         for data in response.json():
             item = DictParser.parse(data)
-
-            if "Atm" in data["BranchType"]:
-                apply_category(Categories.ATM, item)
-                apply_yes_no(Extras.CASH_IN, item, "Внасяне на пари" in data["Services"])
-            else:
+            item["ref"] = data["office_id"]
+            item["name"] = data["office_name"]
+            item["street_address"] = data["office_address"]
+            item["lat"] = data["office_latitude"]
+            item["lon"] = data["office_longitude"]
+            if (data["office_type"] == ["Branch", "Atm"]) or (data["office_type"] == ["Branch"]):
                 apply_category(Categories.BANK, item)
-
-            if data["Phone"]:
-                item["phone"] = data.get("Phone").split(";", 1)[0]
-
-            item["opening_hours"] = OpeningHours()
-
-            if data["OpenHours"]:
-                item["opening_hours"].add_ranges_from_string(data["OpenHours"], days=DAYS_BG)
-            item["street_address"] = item.pop("addr_full", None)
+                apply_yes_no(Extras.ATM, item, True if "Atm" in data["office_type"] else False)
+            elif data["office_type"] == ["Atm"]:
+                apply_category(Categories.ATM, item)
             yield item
