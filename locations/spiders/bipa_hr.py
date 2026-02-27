56ef704049dfec7b65841c652974e59b141c4246
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-02-27 15:39:02 +0530

    code updated to fix spider

--- locations/spiders/bipa_hr.py
+++ locations/spiders/bipa_hr.py
@@ -1,10 +1,9 @@
-from typing import AsyncIterator
-
 from scrapy import Spider
-from scrapy.http import JsonRequest, Response
+from scrapy.http import Response
 
 from locations.hours import DAYS_SR, OpeningHours
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class BipaHRSpider(Spider):
@@ -13,29 +12,23 @@ class BipaHRSpider(Spider):
         "brand": "Bipa",
         "brand_wikidata": "Q864933",
     }
-
-    async def start(self) -> AsyncIterator[JsonRequest]:
-        yield JsonRequest(
-            "https://www.bipa.hr/api/2sxc/app/auto/query/SvePoslovnice/Poslovnice",
-            headers={
-                "ModuleId": "841",
-                "TabId": "195",
-            },
-        )
+    start_urls = ["https://bipa.hr/poslovnice/"]
 
     def parse(self, response: Response):
-        for store in response.json()["Poslovnice"]:
-            opening_hours = OpeningHours()
-            opening_hours.add_ranges_from_string(store["RadnoVrijeme"], days=DAYS_SR)
-
-            yield Feature(
-                ref=store["Id"],
-                city=store["Grad"],
-                street_address=store["Adresa"],
-                postcode=store["PPbroj"],
-                phone=store["Telefon"],
-                state=store["Regija"],
-                lat=store["Longitude"],
-                lon=store["Latitude"],
-                opening_hours=opening_hours,
+        for store in response.xpath('//*[@class="entry-list__entries-col col-12 col-md-4 marker"]'):
+            item = Feature()
+            item["name"] = store.xpath(".//@data-name").get()
+            item["lat"] = store.xpath(".//@data-lat").get()
+            item["lon"] = store.xpath(".//@data-lng").get()
+            item["street_address"] = store.xpath(".//address//text()[1]").get()
+            item["addr_full"] = merge_address_lines(
+                [item["street_address"], store.xpath(".//address//text()[1]").get()]
             )
+            item["ref"] = store.xpath(".//article/@class").get()
+            oh = OpeningHours()
+
+            for day_time in store.xpath(".//ul//li"):
+                day_time_string = day_time.xpath("normalize-space()").get()
+                oh.add_ranges_from_string(day_time_string.strip(), days=DAYS_SR)
+            item["opening_hours"] = oh
+            yield item
