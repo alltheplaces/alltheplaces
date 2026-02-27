56150a08fd1c2095cf25207665625646908f91de
Cj Malone <CjMalone@mail.com>
2026-02-27 09:06:19 +0000

    apply_category

--- locations/spiders/cooplands_gb.py
+++ locations/spiders/cooplands_gb.py
@@ -2,7 +2,7 @@ from typing import Iterable
 
 from scrapy.http import TextResponse
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.hours import DAYS_EN
 from locations.items import Feature
 from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
@@ -10,7 +10,7 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 class CooplandsGBSpider(WPStoreLocatorSpider):
     name = "cooplands_gb"
-    item_attributes = {"brand": "Cooplands", "brand_wikidata": "Q5167971", "extras": Categories.SHOP_BAKERY.value}
+    item_attributes = {"brand": "Cooplands", "brand_wikidata": "Q5167971"}
     allowed_domains = ["cooplands-bakery.co.uk"]
     iseadgg_countries_list = ["GB"]
     search_radius = 24
@@ -19,4 +19,5 @@ class CooplandsGBSpider(WPStoreLocatorSpider):
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
+        apply_category(Categories.SHOP_BAKERY, item)
         yield item
