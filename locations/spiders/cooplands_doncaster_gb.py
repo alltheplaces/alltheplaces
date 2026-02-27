6b86d7a6b10d4057be7ee8cbf68129e2030ab771
Cj Malone <CjMalone@mail.com>
2026-02-27 08:59:22 +0000

    Remove static opening hours

--- locations/spiders/cooplands_doncaster_gb.py
+++ locations/spiders/cooplands_doncaster_gb.py
@@ -1,7 +1,9 @@
-import scrapy
+from typing import Any
 
-from locations.categories import Categories
-from locations.hours import DAYS, OpeningHours
+import scrapy
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
 
@@ -9,22 +11,17 @@ from locations.pipelines.address_clean_up import merge_address_lines
 class CooplandsDoncasterGBSpider(scrapy.Spider):
     name = "cooplands_doncaster_gb"
     allowed_domains = ["cooplands.co.uk"]
-    item_attributes = {"brand": "Cooplands", "brand_wikidata": "Q96622197", "extras": Categories.SHOP_BAKERY.value}
+    item_attributes = {"brand": "Cooplands", "brand_wikidata": "Q96622197"}
     start_urls = ["https://cooplands.co.uk/shop-locations"]
 
-    def __init__(self):
-        self.item_attributes["opening_hours"] = OpeningHours()
-        for day in DAYS[0:6]:
-            self.item_attributes["opening_hours"].add_range(day=day, open_time="08:00", close_time="17:00")
-
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         stores = response.xpath("//div[@class='box box-store']")
 
         for index, store in enumerate(stores):
             data = store.xpath("ul/li/text()").extract()
             addr_full = merge_address_lines(data[:-1])
 
-            yield Feature(
+            item = Feature(
                 ref=index,
                 name="Cooplands",
                 branch=store.xpath("h4/text()").extract_first(),
@@ -32,3 +29,5 @@ class CooplandsDoncasterGBSpider(scrapy.Spider):
                 postcode=data[-2].strip(),
                 phone=data[-1].strip(),
             )
+            apply_category(Categories.SHOP_BAKERY, item)
+            yield item
