8a85d845d705f2fe22b849097e4b6ffc9722f296
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-02 14:15:27 +0530

    code updated to fix spider

--- locations/spiders/mitsubishi_gb.py
+++ locations/spiders/mitsubishi_gb.py
@@ -1,18 +1,28 @@
-from typing import Iterable
+from typing import Any
 
+import chompjs
+import scrapy
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.items import Feature
+from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
 from locations.spiders.mitsubishi import MitsubishiSpider
-from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 
-class MitsubishiGBSpider(WPStoreLocatorSpider):
+class MitsubishiGBSpider(scrapy.Spider):
     name = "mitsubishi_gb"
     item_attributes = MitsubishiSpider.item_attributes
     allowed_domains = ["mitsubishi-motors.co.uk"]
+    start_urls = ["https://mitsubishi-motors.co.uk/find-a-dealer/"]
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        apply_category(Categories.SHOP_CAR_REPAIR, item)
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for dealer in chompjs.parse_js_object(response.xpath('//*[contains(text(),"postalcode")]/text()').get()):
+            dealer.update(dealer.pop("basic"))
+            dealer.update(dealer["addresses"].pop("visiting_address"))
+            item = DictParser.parse(dealer)
+            item.pop("email")
+            item["street_address"] = merge_address_lines([dealer["street_1"], dealer["street_2"], dealer["street_3"]])
+            item["name"] = dealer["marketingname"]
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
+            yield item
