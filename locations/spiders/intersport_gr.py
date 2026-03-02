1f574e540976e7c948515a13a7ecb7b94d30a070
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-03-02 12:49:18 +0530

    Use Playwright to fix Intersport GR Spider

--- locations/spiders/intersport_gr.py
+++ locations/spiders/intersport_gr.py
@@ -1,22 +1,26 @@
 import json
 from typing import Any
 
-import scrapy
 from scrapy.http import Response
 
 from locations.linked_data_parser import LinkedDataParser
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class IntersportGRSpider(scrapy.Spider):
+class IntersportGRSpider(PlaywrightSpider):
     name = "intersport_gr"
     item_attributes = {"brand": "Intersport", "brand_wikidata": "Q666888"}
     start_urls = ["https://www.intersport.gr/el/etairia/katastimata/"]
     requires_proxy = True
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT} | DEFAULT_PLAYWRIGHT_SETTINGS
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.xpath('//*[@data-control="box"]'):
-            ld_item = json.loads(store.xpath('.//script[@type="application/ld+json"]/text()').get())
-            item = LinkedDataParser.parse_ld(ld_item)
+            item = LinkedDataParser.parse_ld(
+                json.loads(store.xpath('.//script[@type="application/ld+json"]/text()').get())
+            )
             item["lat"] = store.xpath(".//@data-latitude").get()
             item["lon"] = store.xpath(".//@data-longitude").get()
             item["ref"] = item["website"] = response.urljoin(
