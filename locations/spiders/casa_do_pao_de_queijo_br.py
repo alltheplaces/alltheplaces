33a368ac9e02930a9c5580b2fc4b78c6012df8a5
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-02 10:42:59 +0530

    code updated to fix spider

--- locations/spiders/casa_do_pao_de_queijo_br.py
+++ locations/spiders/casa_do_pao_de_queijo_br.py
@@ -1,22 +1,32 @@
-from typing import Iterable
+import re
+from typing import Any
 
+import chompjs
+import scrapy
 from scrapy.http import Response
 
-from locations.categories import Categories, apply_category
+from locations.hours import DAYS_BR, OpeningHours
 from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
 
 
-class CasaDoPaoDeQueijoBRSpider(JSONBlobSpider):
+class CasaDoPaoDeQueijoBRSpider(scrapy.Spider):
     name = "casa_do_pao_de_queijo_br"
     item_attributes = {"brand": "Casa do Pão de Queijo", "brand_wikidata": "Q9698946"}
-    start_urls = ["http://www.casadopaodequeijo.com.br/service/lojascpq"]
-    locations_key = "res"
+    start_urls = ["https://www.casadopaodequeijo.com.br/"]
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["branch"] = item.pop("name")
-        item["street_address"] = feature["endereco"]
-        item["state"] = feature["uf"]
-        item["postcode"] = feature["cep"]
-        apply_category(Categories.COFFEE_SHOP, item)
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        js_path = response.xpath('//*[@type = "module"]//@src').get()
+        yield scrapy.Request(url="https://www.casadopaodequeijo.com.br" + js_path, callback=self.parse_location)
+
+    def parse_location(self, response):
+        raw_data = chompjs.parse_js_object(re.search(r"Hb=(\[.+\]),Gb=\(\)", response.text).group(1))
+        for location in raw_data:
+            item = Feature()
+            item["name"] = location["nome"]
+            item["addr_full"] = item["ref"] = location["endereco"]
+            item["city"] = location["cidade"]
+            item["state"] = location["estado"]
+            oh = OpeningHours()
+            oh.add_ranges_from_string(location["horario_funcionamento"], DAYS_BR)
+            item["opening_hours"] = oh
+            yield item
