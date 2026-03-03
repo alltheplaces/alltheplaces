418540a44c62c263567ff389abcf5ffda8f052af
jhutton1 <jaimehutton01@gmail.com>
2026-03-03 10:43:56 -0500

    filter out bad locations

--- locations/spiders/ben_and_jerrys.py
+++ locations/spiders/ben_and_jerrys.py
@@ -15,25 +15,28 @@ class BenAndJerrysSpider(Where2GetItSpider):
     custom_settings = {"DOWNLOAD_TIMEOUT": 60}
 
     def parse_item(self, item: Feature, location: dict, **kwargs) -> Iterable[Feature]:
-        item["lat"] = location["latitude"]
-        item["lon"] = location["longitude"]
-        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
-        if website := item.get("website"):
-            if "facebook" in website:
-                item.pop("website")
-            elif "kojote" in website:
-                item["website"] = "https://www." + item["website"]
-        apply_category(Categories.ICE_CREAM, item)
+        if location.get("jsonshopinfo"):
+            item["lat"] = location["latitude"]
+            item["lon"] = location["longitude"]
+            item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
+            if website := item.get("website"):
+                if "facebook" in website:  
+                    item.pop("website") 
+                elif "kojote" in website:
+                    item["website"] = "https://www." + item["website"]
+            apply_category(Categories.ICE_CREAM, item)
 
-        apply_yes_no(Extras.DELIVERY, item, location["offersdelivery"] == "1")
+            apply_yes_no(Extras.DELIVERY, item, location["offersdelivery"] == "1")
 
-        item["opening_hours"] = OpeningHours()
-        for day in DAYS_FULL:
-            if location.get(day.lower()) in [None, ""]:
-                continue
-            if "closed" in location.get(day.lower()):
-                item["opening_hours"].set_closed(day)
-            else:
-                item["opening_hours"].add_ranges_from_string(day + " " + location.get(day.lower()))
+            item["opening_hours"] = OpeningHours()
+            for day in DAYS_FULL:
+                if location.get(day.lower()) in [None, ""]:
+                    continue
+                if "closed" in location.get(day.lower()):
+                    item["opening_hours"].set_closed(day)
+                else:
+                    item["opening_hours"].add_ranges_from_string(day + " " + location.get(day.lower()))
 
-        yield item
+            yield item
+
+
