041c11d7369ea44bd1233b54d6009d743b2c3693
Cj Malone <CjMalone@mail.com>
2026-03-03 13:18:44 +0000

    [Hunkemöller] Clean up parsing

--- locations/spiders/hunkemoller.py
+++ locations/spiders/hunkemoller.py
@@ -1,40 +1,35 @@
 import json
-import re
+from typing import Any
 
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_NL, OpeningHours, sanitise_day
+from locations.hours import DAYS, OpeningHours
 
 
 class HunkemollerSpider(SitemapSpider):
     name = "hunkemoller"
     item_attributes = {"brand": "Hunkemöller", "brand_wikidata": "Q2604175"}
     sitemap_urls = ["https://www.hunkemoller.be/nl/sitemap-stores.xml"]
-    sitemap_rules = [
-        (
-            r"https://www.hunkemoller.be/nl/winkel/.+/.+$",
-            "parse",
-        ),
-    ]
+    sitemap_rules = [(r"https://www.hunkemoller.be/nl/winkel/[^/]+/[^/]+\-(\d+)$", "parse")]
 
-    def parse(self, response, **kwargs):
-        script_text = json.loads(
-            re.search(
-                r"data\"s*:\s*\[({\"address1\".*})\],\s*\"total",
-                response.xpath('//*[contains(text(), "latitude")]/text()').get(),
-            ).group(1)
-        )
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        data = json.loads(response.xpath('//script[@id="mobify-data"]/text()').get())
+        for q in data["__PRELOADED_STATE__"]["__reactQuery"]["queries"]:
+            if "\u002fstores" in q["queryKey"]:
+                poi = q["state"]["data"]["data"][0]
+                break
 
-        item = DictParser.parse(script_text)
+        item = DictParser.parse(poi)
+        item["branch"] = item.pop("name")
         item["website"] = response.url
 
-        oh = OpeningHours()
-        for day_time in script_text.get("c_openingHours") or []:
-            day = sanitise_day(day_time["dayOfWeek"], DAYS_NL)
-            open_time = day_time["open"]
-            close_time = day_time["close"]
-            oh.add_range(day=day, open_time=open_time, close_time=close_time)
-        item["opening_hours"] = oh
+        item["opening_hours"] = OpeningHours()
+        for day, rule in (poi.get("c_hoursObj") or {}).items():
+            item["opening_hours"].add_range(DAYS[int(day) - 2], rule["open"], rule["close"])
+
+        apply_category(Categories.SHOP_CLOTHES, item)
 
         yield item
