44db7d26012da63145a7035b4c8533d74c000361
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-03-04 20:24:42 +0530

    Add opening hours for Gohealth Urgent Care US Spider

--- locations/spiders/gohealth_urgent_care_us.py
+++ locations/spiders/gohealth_urgent_care_us.py
@@ -3,6 +3,7 @@ from typing import Iterable
 
 from scrapy.http import TextResponse
 
+from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -21,4 +22,19 @@ class GohealthUrgentCareUSSpider(JSONBlobSpider):
         item["image"] = feature["image"]["url"].split("?")[0]
         item["website"] = f'https://www.gohealthuc.com/{feature["jv"]}/locations/{feature["uid"]}'
         item["extras"]["ref:google:place_id"] = feature.get("google_place_id")
+        item["opening_hours"] = self.parse_opening_hours(feature.get("hoursArr", []))
         yield item
+
+    def parse_opening_hours(self, rules: list[dict]) -> OpeningHours:
+        opening_hours = OpeningHours()
+        for rule in rules:
+            if "Closed" in rule["open_time"].title():
+                opening_hours.set_closed(rule["day_of_week"])
+                continue
+            opening_hours.add_range(
+                rule["day_of_week"],
+                rule["open_time"],
+                rule["close_time"].replace("Midnight", "12:00 am"),
+                time_format="%I:%M %p",
+            )
+        return opening_hours
