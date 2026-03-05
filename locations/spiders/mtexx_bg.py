7dab8f64f61c23ab2789d9bfebbe560004faa9ee
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-05 14:07:34 +0530

    code updated to fix spider

--- locations/spiders/mtexx_bg.py
+++ locations/spiders/mtexx_bg.py
@@ -1,29 +1,26 @@
-import re
+from typing import Any
 
 import chompjs
-from scrapy import Request, Spider
+import scrapy
+from scrapy.http import Response
 
-from locations.items import Feature
+from locations.dict_parser import DictParser
+from locations.react_server_components import parse_rsc
 
 
-class MtexxBGSpider(Spider):
+class MtexxBGSpider(scrapy.Spider):
     name = "mtexx_bg"
     item_attributes = {"operator": "M-Texx", "operator_wikidata": "Q122947768"}
-    allowed_domains = ["m-texx.com"]
     start_urls = ["https://m-texx.com/locations"]
-    no_refs = True
 
-    def parse(self, response, **kwargs):
-        url = re.search(r"(static/chunks/app/\(website\)/locations/page-\w+\.js)", response.text)
-        yield Request("https://m-texx.com/_next/" + url.group(1), callback=self.parse_locations)
-
-    def parse_locations(self, response, **kwargs):
-        locations = chompjs.parse_js_objects(re.search(r"exports=\[({city:.+}])}", response.text).group(1))
-        for location in locations:
-            properties = {
-                "city": location["city"],
-                "addr_full": location["popUp"],
-                "lat": location["geocode"][0],
-                "lon": location["geocode"][1],
-            }
-            yield Feature(**properties)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        scripts = response.xpath('//*[contains(text(),"coordinates")]/text()').getall()
+        objs = [chompjs.parse_js_object(s) for s in scripts]
+        rsc = "".join([s for n, s in objs]).encode()
+        raw_data = DictParser.get_nested_key(dict(parse_rsc(rsc)), "initialCities")
+        for location in raw_data:
+            for local in location["locations"]:
+                item = DictParser.parse(local)
+                item["name"] = item["name"].split(" - ")[0]
+                item["lat"], item["lon"] = local["coordinates"]
+                yield item
