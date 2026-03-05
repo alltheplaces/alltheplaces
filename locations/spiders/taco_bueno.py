94b0a82a217a08758d60e0e20a645863d4aa7c64
Cj Malone <CjMalone@mail.com>
2026-03-05 14:45:01 +0000

    Collect more attributes

--- locations/spiders/taco_bueno.py
+++ locations/spiders/taco_bueno.py
@@ -1,11 +1,15 @@
 import re
+from datetime import datetime
 from typing import Any
 
 from scrapy import Spider
 from scrapy.http import JsonRequest, Response
 
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
-from locations.hours import OpeningHours
+from locations.hours import DAYS, OpeningHours
+from locations.items import SocialMedia, set_closed, set_social_media
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class TacoBuenoSpider(Spider):
@@ -14,18 +18,39 @@ class TacoBuenoSpider(Spider):
     start_urls = ["https://locations.tacobueno.com/"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        build_id = re.search(r"buildId\"\s*:\s*\"(.+)\",", response.text).group(1)
         yield JsonRequest(
-            url=f"https://locations.tacobueno.com/_next/data/{build_id}/index.json", callback=self.parse_locations
+            url="https://locations.tacobueno.com/_next/data/{}/index.json".format(
+                re.search(r"buildId\"\s*:\s*\"(.+)\",", response.text).group(1)
+            ),
+            callback=self.parse_locations,
         )
 
-    def parse_locations(self, response, **kwargs):
+    def parse_locations(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["pageProps"]["locations"]:
             item = DictParser.parse(location)
             item["ref"] = location["storeCode"]
-            item["street_address"] = ", ".join(location["addressLines"])
+            item["street_address"] = merge_address_lines(location["addressLines"])
             item["website"] = item["website"].replace("\t", "")
-            oh = OpeningHours()
-            oh.add_ranges_from_string(",".join(location["formattedBusinessHours"]))
-            item["opening_hours"] = oh
+            item["phone"] = "; ".join(location["phoneNumbers"])
+
+            item["opening_hours"] = self.parse_opening_hours(location["businessHours"])
+            apply_category(Categories.FAST_FOOD, item)
+
+            set_social_media(item, SocialMedia.FACEBOOK, location["attributes"].get("url_facebook"))
+            item["extras"]["website:menu"] = location["attributes"]["url_order_ahead"]
+            apply_yes_no(Extras.INDOOR_SEATING, item, location["attributes"]["serves_dine_in"] is True)
+            apply_yes_no(Extras.TAKEAWAY, item, location["attributes"]["has_takeout"] is True)
+            apply_yes_no(Extras.DRIVE_THROUGH, item, location["attributes"].get("has_drive_through") is True)
+            if hours := (location.get("otherHours") or {}).get("DRIVE_THROUGH"):
+                item["extras"]["opening_hours:drive_through"] = self.parse_opening_hours(hours).as_opening_hours()
+            item["extras"]["start_date"] = location["openingDate"]
+            if location["closingDate"]:
+                set_closed(item, datetime.fromisoformat(location["closingDate"]))
+
             yield item
+
+    def parse_opening_hours(self, hours: list) -> OpeningHours:
+        oh = OpeningHours()
+        for day, hours in zip(DAYS, hours):
+            oh.add_range(day, hours[0], hours[1])
+        return oh
