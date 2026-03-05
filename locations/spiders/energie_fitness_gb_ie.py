5780a2d3b33535277c0b757aef31a089d42fea2e
Cj Malone <CjMalone@mail.com>
2026-03-05 16:19:01 +0000

    Collect street address

--- locations/spiders/energie_fitness_gb_ie.py
+++ locations/spiders/energie_fitness_gb_ie.py
@@ -4,6 +4,7 @@ import scrapy
 from scrapy.http import Response
 
 from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class EnergieFitnessGBIESpider(scrapy.Spider):
@@ -15,6 +16,13 @@ class EnergieFitnessGBIESpider(scrapy.Spider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["items"]:
             item = DictParser.parse(location["customProperties"]["locationDetails"]["contactDetails"])
-            item["branch"] = item.pop("name").replace("Energie Fitness ", "")
+            item["ref"] = location["id"]
+            item["branch"] = item.pop("name").removeprefix("Energie Fitness ")
+            item["street_address"] = merge_address_lines(
+                [
+                    location["customProperties"]["locationDetails"]["contactDetails"]["streetAddressLine1"],
+                    location["customProperties"]["locationDetails"]["contactDetails"]["streetAddressLine2"],
+                ]
+            )
             item["website"] = response.urljoin(location["urlPath"])
             yield item
