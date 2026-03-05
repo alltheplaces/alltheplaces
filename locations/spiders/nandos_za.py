c389958e665775a3a7ad773c719940306a03e717
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-03-05 14:01:13 +0530

    Sanitise day for Location Bank Spider and convert Nandos QA & ZA Spiders to Location Bank Spider

--- locations/spiders/nandos_za.py
+++ locations/spiders/nandos_za.py
@@ -1,36 +1,19 @@
 from typing import Iterable
-from urllib.parse import urljoin
 
 from scrapy.http import TextResponse
 
 from locations.categories import Extras, apply_yes_no
-from locations.hours import OpeningHours, sanitise_day
 from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
-from locations.pipelines.address_clean_up import merge_address_lines
 from locations.spiders.nandos import NANDOS_SHARED_ATTRIBUTES
+from locations.storefinders.location_bank import LocationBankSpider
 
 
-class NandosZASpider(JSONBlobSpider):
+class NandosZASpider(LocationBankSpider):
     name = "nandos_za"
     item_attributes = NANDOS_SHARED_ATTRIBUTES
-    allowed_domains = ["api.locationbank.net"]
-    start_urls = [
-        "https://api.locationbank.net/storelocator/StoreLocatorAPI?clientId=67b9c5e4-6ddf-4856-b3c0-cf27cfe53255"
-    ]
-    web_root = "https://store.nandos.co.za/details/"
-    locations_key = "locations"
+    client_id = "67b9c5e4-6ddf-4856-b3c0-cf27cfe53255"
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
-        item["phone"] = item["phone"] = "; ".join(
-            filter(None, [feature.get("primaryPhone"), feature.get("additionalPhone1")])
-        )
-        item["state"] = feature.get("administrativeArea")
-        item["website"] = urljoin(self.web_root, feature.get("storeLocatorDetailsShortURL"))
-        item["street_address"] = merge_address_lines([feature.get("addressLine1"), feature.get("addressLine2")])
-        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip()
-        item["opening_hours"] = self.parse_opening_hours(feature.get("regularHours", []))
-
         apply_yes_no(Extras.BACKUP_GENERATOR, item, "Generator" in feature["slAttributes"])
         apply_yes_no(Extras.DELIVERY, item, "Delivery" in feature["slAttributes"])
         apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive Thru" in feature["slAttributes"])
@@ -41,10 +24,3 @@ class NandosZASpider(JSONBlobSpider):
         # Unhandled: "Alcohol License", "Dine In"
 
         yield item
-
-    def parse_opening_hours(self, rules: list[dict]) -> OpeningHours:
-        opening_hours = OpeningHours()
-        for rule in rules:
-            if day := sanitise_day(rule["openDay"]):
-                opening_hours.add_range(day, rule["openTime"], rule["closeTime"])
-        return opening_hours
