28c719be8f35fb592ea741bdaa544e88ccf4e214
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-06 16:40:17 +0530

    code updated to fix spider

--- locations/spiders/allbirds.py
+++ locations/spiders/allbirds.py
@@ -1,11 +1,10 @@
+import json
 from typing import Any
 
-import chompjs
 from scrapy.http import Response
 from scrapy.spiders import Spider
 
-from locations.categories import Categories, apply_category
-from locations.items import Feature
+from locations.dict_parser import DictParser
 
 
 class AllbirdsSpider(Spider):
@@ -14,23 +13,12 @@ class AllbirdsSpider(Spider):
     start_urls = ["https://www.allbirds.com/pages/stores"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in chompjs.parse_js_object(
-            response.xpath('//script[contains(text(), "storeLocations")]/text()').re_first(r"storeLocations = (\[.+\])")
-        ):
-            item = Feature()
-            item["branch"] = location["storeName"]
+        for location in json.loads(
+            response.xpath("//*[@id='store-locator-data-template--16112999661648__store_locator_pdFFMK']/text()").get()
+        )["stores"]:
+            item = DictParser.parse(location)
             item["ref"] = location["handle"]
-            item["website"] = response.urljoin(location["handle"])
-            item["phone"] = location.get("phoneNumber")
-            item["addr_full"] = location.get("storeAddress")
-            item["street_address"] = location["streetAddress"]
-            item["city"] = location["city"]
-            item["state"] = location.get("stateProvince")
             item["postcode"] = location["zipPostalCode"]
-            item["country"] = location["countryRegion"]
-            item["lat"] = location["geoLocation"]["lat"]
-            item["lon"] = location["geoLocation"]["lon"]
-
-            apply_category(Categories.SHOP_SHOES, item)
-
+            item["state"] = location["stateProvince"]
+            item["website"] = "https://www.allbirds.com/pages/stores/" + item["ref"]
             yield item
