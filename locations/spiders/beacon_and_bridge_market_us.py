934485174b111c9a59c8205f9f37d055dc32cd57
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-03-09 19:26:44 +0530

    Add opening hours for Beacon And Bridge Market US Spider

--- locations/spiders/beacon_and_bridge_market_us.py
+++ locations/spiders/beacon_and_bridge_market_us.py
@@ -1,9 +1,10 @@
 from typing import Any
 
-from scrapy import Spider
+from scrapy import Selector, Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
+from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
 from locations.pipelines.address_clean_up import clean_address
 
@@ -27,5 +28,16 @@ class BeaconAndBridgeMarketUSSpider(Spider):
                 item["phone"] = (
                     location_info.xpath('.//span[contains(text(), "PHONE:")]/text()').get("").replace("PHONE:", "")
                 )
+                item["opening_hours"] = self.parse_opening_hours(location_info)
                 apply_category(Categories.FUEL_STATION, item)
                 yield item
+
+    def parse_opening_hours(self, location_info: Selector) -> OpeningHours:
+        opening_hours = OpeningHours()
+        hours_text = location_info.xpath('.//span/text()[contains(.,"HOURS:")]').get("").replace("HOURS:", "")
+        if "24/7" in hours_text:
+            opening_hours = "24/7"
+        else:
+            for day in DAYS:
+                opening_hours.add_ranges_from_string(f"{day} {hours_text}")
+        return opening_hours
