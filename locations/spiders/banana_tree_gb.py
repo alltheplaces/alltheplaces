13a5b21c396fbd73177d381f6f38c0dd0a65e3cf
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-11 12:53:54 +0100

    Update banana_tree_gb.py
    Bananatree seems to have correct urls in the sitemap - so using Chiquito spider for this one too.

--- locations/spiders/banana_tree_gb.py
+++ locations/spiders/banana_tree_gb.py
@@ -3,13 +3,13 @@ from typing import Iterable
 from scrapy.http import Response
 
 from locations.items import Feature
-from locations.spiders.frankie_and_bennys_gb import FrankieAndBennysGBSpider
+from locations.spiders.chiquito_gb import ChiquitoGBSpider
 
 
-class BananaTreeGBSpider(FrankieAndBennysGBSpider):
+class BananaTreeGBSpider(ChiquitoGBSpider):
     name = "banana_tree_gb"
     item_attributes = {"brand": "Banana Tree", "brand_wikidata": "Q123013837"}
-    start_urls = ["https://bananatree.co.uk/locations"]
+    sitemap_urls = ["https://bananatree.co.uk/sitemap.xml"]
 
     def parse_item(self, item: Feature, response: Response, **kwargs) -> Iterable[Feature]:
         item["branch"] = response.xpath("//title/text()").get("").split("|")[0].strip()
