d2c60b535c356e5e5bb04dc8dc22087061a0703c
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-11 15:20:42 +0530

    code updated to use sitemap to fix spider

--- locations/spiders/mercato_it.py
+++ locations/spiders/mercato_it.py
@@ -1,75 +1,32 @@
-from typing import Any, AsyncIterator
+from typing import Iterable
 
-from scrapy import Spider
-from scrapy.http import JsonRequest, Response
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class MercatoITSpider(Spider):
+class MercatoITSpider(SitemapSpider, StructuredDataSpider):
     name = "mercato_it"
     item_attributes = {"brand": "Mercatò", "brand_wikidata": "Q127389715"}
+    sitemap_urls = ["https://www.mymercato.it/sitemap.xml"]
+    sitemap_rules = [(r"https://www.mymercato.it/punti-vendita/[^/]+/[^/]+$", "parse_sd")]
+    wanted_types = [["Store", "GroceryStore"]]
 
-    items = None
-
-    async def start(self) -> AsyncIterator[Any]:
-        yield JsonRequest(
-            "https://www.mymercato.it/action/getStoresList",
-            data={
-                "openToday": False,
-                "servicesFilter": [],
-                "brandsFilter": ["MERCATOBIG", "MERCATOLOCAL", "MERCATO", "MERCATOEXTRA"],
-                "typesFilter": [],
-                "partnersFilter": [],
-                "registration": False,
-            },
-        )
-
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        self.items = {}
-        for location in response.xpath("//div[@data-store-code]"):
-            item = Feature()
-            item["ref"] = location.xpath("@data-store-code").get()
-            item["postcode"] = location.xpath("@data-cap").get()
-            item["website"] = response.urljoin(location.xpath(".//a/@href").get())
-            self.items[item["ref"]] = item
-
-        yield JsonRequest(
-            "https://www.mymercato.it/action/getStoresMapData",
-            data={
-                "openToday": False,
-                "servicesFilter": [],
-                "brandsFilter": ["MERCATOBIG", "MERCATOLOCAL", "MERCATO", "MERCATOEXTRA"],
-                "typesFilter": [],
-                "partnersFilter": [],
-                "registration": False,
-            },
-            callback=self.parse_coords,
-        )
-
-    def parse_coords(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json()["features"]:
-            item = self.items[location["properties"]["storeCode"]]
-            item["geometry"] = location["geometry"]
-
-            if location["properties"]["storeType"] == "mercato":
-                item["branch"] = location["properties"]["name"].removeprefix("Mercatò").strip(" -")
-                item["name"] = "Mercatò"
-                apply_category(Categories.SHOP_SUPERMARKET, item)
-            elif location["properties"]["storeType"] == "mercato-big":
-                item["branch"] = location["properties"]["name"].removeprefix("Mercatò Big").strip(" -")
-                item["name"] = "Mercatò Big"
-                apply_category(Categories.SHOP_SUPERMARKET, item)
-            elif location["properties"]["storeType"] == "mercato-local":
-                item["branch"] = location["properties"]["name"].removeprefix("Mercatò Local").strip(" -")
-                item["name"] = "Mercatò Local"
-                apply_category(Categories.SHOP_SUPERMARKET, item)
-            elif location["properties"]["storeType"] == "mercatoextra":
-                item["branch"] = location["properties"]["name"].removeprefix("Mercatò Extra").strip(" -")
-                item["name"] = "Mercatò Extra"
-                apply_category(Categories.SHOP_SUPERMARKET, item)
-            else:
-                self.logger.error("Unexpected type: {}".format(location["properties"]["storeType"]))
-
-            yield item
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        if "MERCATO' BIG" in item["name"]:
+            item["branch"] = item["name"].removeprefix("MERCATO' BIG ")
+            item["name"] = "MERCATO' BIG"
+        elif "MERCATO' LOCAL" in item["name"]:
+            item["branch"] = item["name"].removeprefix("MERCATO' LOCAL")
+            item["name"] = "MERCATO' LOCAL"
+        elif "MERCATO' EXTRA" in item["name"]:
+            item["branch"] = item["name"].removeprefix("MERCATO' EXTRA")
+            item["name"] = "MERCATO' EXTRA"
+        else:
+            item["branch"] = item["name"].removeprefix("MERCATO'")
+            item["name"] = "MERCATO'"
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+        yield item
