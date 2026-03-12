eae1d4016df21c058cc1004beedf3e21a29b6059
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-12 21:34:49 +0100

    Update cardzone_gb.py

--- locations/spiders/cardzone_gb.py
+++ locations/spiders/cardzone_gb.py
@@ -9,6 +9,7 @@ from locations.pipelines.address_clean_up import clean_address
 
 class CardzoneGBSpider(JSONBlobSpider):
     name = "cardzone_gb"
+    item_attributes = {"brand": "Cardzone", "brand_wikidata": "Q123019897"}
     start_urls = [
         "https://cardzoneltd.com/wp-admin/admin-ajax.php?action=store_search&lat=52.95402&lng=-1.15499&max_results=300&search_radius=605&autoload=1"
     ]
@@ -26,6 +27,8 @@ class CardzoneGBSpider(JSONBlobSpider):
         for brand_key in self.brands.keys():
             if item["name"].startswith(brand_key):
                 item.update(self.brands[brand_key])
+        ifnot brand:
+        
         item.pop("addr_full", None)
         item["street_address"] = clean_address([feature["address"], feature["address2"]])
         yield item
