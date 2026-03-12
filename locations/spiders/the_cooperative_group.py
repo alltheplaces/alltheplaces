9e3bb6fb99826be5de8bb9d4f098e5e7094d1cd6
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-12 19:03:39 +0100

    Names are either Office or Offspring

--- locations/spiders/the_cooperative_group.py
+++ locations/spiders/the_cooperative_group.py
@@ -1,35 +1,26 @@
-from scrapy.spiders import SitemapSpider
+from typing import Iterable
+
+from scrapy.http import TextResponse
 
 from locations.categories import Categories, apply_category
 from locations.spiders.central_england_cooperative import COOP_FUNERALCARE, set_operator
-from locations.structured_data_spider import StructuredDataSpider
+from locations.json_blob_spider import JSONBlobSpider
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
-THE_COOPERATIVE_GROUP = {"brand": "The Co-operative Group", "brand_wikidata": "Q117202"}
-THE_COOPERATIVE_GROUP_FOOD = {"brand": "Co-op Food", "brand_wikidata": "Q3277439"}
 
-
-class TheCooperativeGroupSpider(SitemapSpider, StructuredDataSpider):
+class TheCooperativeGroupSpider(JSONBlobSpider):
     name = "the_cooperative_group"
-    sitemap_urls = [
-        "https://www.coop.co.uk/store-finder/sitemap.xml",
-        "https://www.coop.co.uk/funeralcare/funeral-directors/sitemap.xml",
-    ]
-    sitemap_rules = [
-        ("/store-finder/", "parse_sd"),
-        ("/funeralcare/funeral-directors/", "parse_sd"),
-    ]
-    wanted_types = ["ConvenienceStore", "LocalBusiness"]
-    drop_attributes = {"image"}
-    custom_settings = {"DOWNLOAD_TIMEOUT": 500}
+    item_attributes = {"brand": "The Co-operative Group", "brand_wikidata": "Q117202"}
+    start_urls = ["https://www.coop.co.uk/store-finder/api/locations/food/?&distance=3000&min_distance=0&min_results=1&format=json&page_size=2500"]
+    locations_key = "results"
+    drop_attributes = {"facebook", "twitter"}
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if ld_data["@type"] == "ConvenienceStore":
-            item.update(THE_COOPERATIVE_GROUP_FOOD)
-            apply_category(Categories.SHOP_CONVENIENCE, item)
-        else:
-            item.update(COOP_FUNERALCARE)
-            apply_category(Categories.SHOP_FUNERAL_DIRECTORS, item)
 
-        set_operator(THE_COOPERATIVE_GROUP, item)
-
+   def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["lat"],item["lon"] = feature["position"]["y"],feature["position"]["x"]
+        item["website"] = "https://www.coop.co.uk" + item["website"]
+        item["street_address"] = merge_address_lines([feature["street_address"],feature["street_address2"],feature["street_address3"]])
+        item["branch"] = item.pop("name")
+        item["name"] = feature["society"]
         yield item
