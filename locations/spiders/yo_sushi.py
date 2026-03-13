16f2bde4c938d2e5c766b7eaac45bde67e1c94f4
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-13 16:44:47 +0100

    Update yo_sushi.py

--- locations/spiders/yo_sushi.py
+++ locations/spiders/yo_sushi.py
@@ -19,7 +19,7 @@ class YoSushiSpider(SitemapSpider, StructuredDataSpider):
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["lat"], item["lon"] = ld_data["latitude"], ld_data["longitude"]
         item["branch"] = item.pop("name")
-        if ld_data["description"] and "kiosk" in ld_data["description"].lower():
+        if "kiosk" in ld_data.get("description").lower():
             apply_category(Categories.SHOP_KIOSK, item)
             apply_category(Categories.FAST_FOOD, item)
         else:
