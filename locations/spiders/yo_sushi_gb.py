2d8137a2686b6c799955fe11bfdbbfaecd8f347f
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-13 15:19:02 +0100

    [Yo Sushi GB] Add restaurants
    There are also kiosks but I can't see how to get them.

---
+++ locations/spiders/yo_sushi_gb.py
@@ -0,0 +1,20 @@
+from typing import Any, Iterable
+
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+class YoSushiGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "yo_sushi_gb"
+    item_attributes = {"brand": "Yo! Sushi", "brand_wikidata": "Q3105441"}
+    sitemap_urls = ["https://yosushi.com/sitemap.xml"]
+    sitemap_rules = [(r"https://yosushi.com/restaurants/[-\w]+", "parse")]
+    wanted_types = ["Restaurant"]
+    drop_attributes = {"facbook","twitter","email"}
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
+        item["website"] = response.url
+        item["branch"] = item.pop("name")
+        yield item
