4c6c26f43f70753a24c9439db572fd1012366c0b
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-03-14 07:30:55 +0530

    [Mattress Warehouse US] Fix Spider (#15060)

--- locations/spiders/mattress_warehouse_us.py
+++ locations/spiders/mattress_warehouse_us.py
@@ -1,30 +1,55 @@
-from locations.storefinders.yext import YextSpider
+import json
+import re
+from typing import Any
+
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
 
 
-class MattressWarehouseUSSpider(YextSpider):
+class MattressWarehouseUSSpider(CrawlSpider):
     name = "mattress_warehouse_us"
-    item_attributes = {"brand": "Mattress Warehouse", "brand_wikidata": "Q61995079"}
-    api_key = "0277c3ad01c944bfc148a777fa36764d"
+    BRANDS = {
+        "mattress_warehouse": {"brand": "Mattress Warehouse", "brand_wikidata": "Q61995079"},
+        "sleep_outfitters": {"brand": "Sleep Outfitters", "brand_wikidata": "Q120509459"},
+    }
+    start_urls = ["https://mattresswarehouse.com/store-locator"]
 
-    def parse_item(self, item, location, **kwargs):
-        if item["ref"] in [
-            "0000",  # "in regards to your delivery"
-            "59",  # HQ
-            "6664888033510164995",  # "Test"
-        ]:
-            return
+    rules = [
+        Rule(
+            LinkExtractor(allow=r"/store-locator/mattress-stores-in"),
+            callback="parse",
+            follow=True,
+        ),
+    ]
 
-        if "COMING SOON" in item["name"]:
-            return
-
-        item["branch"] = item.pop("name").removeprefix("Mattress Warehouse ").removeprefix("of ").removeprefix("- ")
-
-        # Remove brand data from POI
-        if item.get("email") == "info@mattresswarehouse.com":
-            del item["email"]
-        if item.get("twitter") == "MWarehouseLLC":
-            del item["twitter"]
-        if item["extras"]["contact:instagram"] == "mattresswarehousellc":
-            del item["extras"]["contact:instagram"]
-
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        if raw_data := response.xpath('//*[contains(text(),"postalCode")]/text()').get():
+            for location in json.loads(
+                re.search(r"locationDetails\"\s*:\s*(\[.*\]),\"cityStateSlug", raw_data).group(1)
+            ):
+                item = DictParser.parse(location)
+                item["addr_full"] = location["addressMetafield"]
+                item["branch"] = (
+                    item.pop("name").replace("Mattress Warehouse of ", "").replace("Sleep Outfitters of ", "")
+                )
+                brand_slug = re.search(r"(.+\s*of)\s*", location["name"]).group(1).lower().replace(" ", "-")
+                branch_slug = location["nameMetafield"].replace(" ", "_").replace(",", "").lower()
+                item["website"] = response.url.replace(
+                    "mattress-stores-in", "-".join([brand_slug, branch_slug, item["ref"]])
+                )
+                if "Sleep Outfitters" in location["name"]:
+                    item.update(self.BRANDS["sleep_outfitters"])
+                else:
+                    item.update(self.BRANDS["mattress_warehouse"])
+                oh = OpeningHours()
+                for day, time in location["hours"].items():
+                    day = day
+                    open_time = time["start"]
+                    close_time = time["end"]
+                    oh.add_range(day, open_time, close_time)
+                item["opening_hours"] = oh
+                yield item
