5f69eeebb6452e50139ecc454fd58dc048e07d8f
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-03-15 20:35:45 +0900

    [Aeon Bank JP] create spider (8,665 locations)
    {'atp/brand/みずほ銀行': 1696,
     'atp/brand/イオン銀行': 6969,
     'atp/brand_wikidata/Q11286327': 6969,
     'atp/brand_wikidata/Q2882956': 1696,
     'atp/category/amenity/atm': 8665,
     'atp/country/JP': 8665,
     'atp/field/city/missing': 8665,
     'atp/field/country/from_spider_name': 8665,
     'atp/field/email/missing': 8665,
     'atp/field/image/missing': 8665,
     'atp/field/opening_hours/missing': 8665,
     'atp/field/phone/missing': 8638,
     'atp/field/state/missing': 8665,
     'atp/field/street_address/missing': 8665,
     'atp/field/twitter/missing': 8665,
     'atp/item_scraped_host_count/map.aeonbank.co.jp': 8665,
     'atp/lineage': 'S_?',
     'atp/nsi/cc_match': 8665,
     'atp/operator/みずほ銀行': 1696,
     'atp/operator/イオン銀行': 6969,
     'atp/operator_wikidata/Q11286327': 6969,
     'atp/operator_wikidata/Q2882956': 1696,
     'downloader/request_bytes': 10475,
     'downloader/request_count': 19,
     'downloader/request_method_count/GET': 19,
     'downloader/response_bytes': 636216,
     'downloader/response_count': 19,
     'downloader/response_status_count/200': 19,
     'elapsed_time_seconds': 29.884913,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 3, 15, 11, 31, 17, 387460, tzinfo=datetime.timezone.utc),
     'httpcompression/response_bytes': 5408127,
     'httpcompression/response_count': 19,
     'item_scraped_count': 8665,
     'items_per_minute': 17927.58620689655,
     'log_count/DEBUG': 8684,
     'log_count/INFO': 3,
     'request_depth_max': 17,
     'response_received_count': 19,
     'responses_per_minute': 39.310344827586206,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/200': 1,
     'scheduler/dequeued': 18,
     'scheduler/dequeued/memory': 18,
     'scheduler/enqueued': 18,
     'scheduler/enqueued/memory': 18,
     'start_time': datetime.datetime(2026, 3, 15, 11, 30, 47, 502547, tzinfo=datetime.timezone.utc)}

---
+++ locations/spiders/aeon_bank_jp.py
@@ -0,0 +1,56 @@
+from typing import AsyncIterator
+
+from scrapy import Spider
+from scrapy.http import Request
+
+from locations.dict_parser import DictParser
+
+
+class AeonBankJPSpider(Spider):
+    name = "aeon_bank_jp"
+
+    item_attributes = {
+        "extras": {
+            "amenity": "atm",
+        },
+    }
+
+    async def start(self) -> AsyncIterator[Request]:
+        yield self.get_page(0)
+
+    def get_page(self, n):
+        return Request(
+            f"https://map.aeonbank.co.jp/aeonbank/api/proxy2/shop/list?datum=wgs84&limit=500&offset={n}",
+            meta={"offset": n},
+        )
+
+    def parse(self, response):
+        data = response.json()
+        stores = data["items"]
+
+        for store in stores:
+            item = DictParser.parse(store)
+            try:
+                match store["categories"][0]["code"]:
+                    case "10":
+                        item["name"] = "みずほ銀行"
+                        item.update({"brand_wikidata": "Q2882956"})
+                    case _:
+                        item["name"] = "イオン銀行"
+                        item.update({"brand_wikidata": "Q11286327"})
+            except:
+                item["name"] = "イオン銀行"
+                item.update({"brand_wikidata": "Q11286327"})
+            
+            item["branch"] = store["name"].removesuffix("出張所")
+            item["ref"] = store["code"]
+            item["lat"] = store["coord"]["lat"]
+            item["lon"] = store["coord"]["lon"]
+            item["extras"]["branch:ja-Hira"] = store.get("ruby")
+            item["addr_full"] = store["address_name"]
+            item["postcode"] = store.get("postal_code")
+            item["website"] = f"https://map.aeonbank.co.jp/aeonbank/spot/detail?code={store['code']}"
+            yield item
+
+        if data["count"]["limit"] == len(data["items"]):
+            yield self.get_page(data["count"]["limit"] + response.meta["offset"])
