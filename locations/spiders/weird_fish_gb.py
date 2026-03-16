7a0eff0f2e3df4cb8ce6e37b68320fb15967a753
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-16 19:33:45 +0100

    [Weird Fish] Add Spider

---
+++ locations/spiders/weird_fish_gb.py
@@ -0,0 +1,35 @@
+from typing import Iterable
+
+from scrapy.http import Response
+from locations.hours import OpeningHours, DAYS_FULL
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+class WeirdFishGBSpider(JSONBlobSpider):
+    name = "weird_fish_gb"
+    item_attributes = {"brand": "Weird Fish", "brand_wikidata": "Q19903788"}
+    start_urls = ["https://www.weirdfish.co.uk/template/GetAllStores"]
+    locations_key = "stores"
+
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if feature.get("link"):
+            if "http" in feature.get("link"):
+                item["website"] = feature["link"]
+            else:
+                item["website"] = "https://www.weirdfish.co.uk" + feature["link"]
+
+        item["branch"] = item.pop("name").replace("Weird Fish","").replace("Store","")
+        item["opening_hours"] = OpeningHours()
+
+        for day in DAYS_FULL:
+            try:
+                if feature.get("openingTimes")[day.lower()]:
+                    open,close=feature["openingTimes"][day.lower()].replace(" ","").split("-")
+                    if "am" in open:
+                        item["opening_hours"].add_range(day, open, close, "%I:%M%p")
+                    else:
+                        item["opening_hours"].add_range(day, open, close, "%H:%M")
+            except:
+                continue
+        yield item
