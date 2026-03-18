889a1da3d36d091702c13e40a6c5bdb8393c1f31
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-18 10:51:37 +0530

    run black and updating website

--- locations/spiders/dunelm_gb.py
+++ locations/spiders/dunelm_gb.py
@@ -21,6 +21,6 @@ class DunelmGBSpider(scrapy.Spider):
             item = DictParser.parse(location)
             item["ref"] = location["sapSiteId"]
             item["street_address"] = merge_address_lines([location["localArea"], item["street_address"]])
-            item["website"] = "https://www.dunelm.com/stores/" + item["city"].lower()
+            item["website"] = "https://www.dunelm.com/stores/" + location["uri"]
             apply_category(Categories.SHOP_INTERIOR_DECORATION, item)
             yield item
