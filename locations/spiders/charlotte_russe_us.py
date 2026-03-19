de374e0e85dea1b338f8d2d9e44d3b72fc3d0a58
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-03-19 19:50:17 +0530

    [Charlotte Russe US] Fix Spider (#15607)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/charlotte_russe_us.py
+++ locations/spiders/charlotte_russe_us.py
@@ -12,7 +12,9 @@ class CharlotteRusseUSSpider(JSONBlobSpider):
     name = "charlotte_russe_us"
     item_attributes = {"brand": "Charlotte Russe", "brand_wikidata": "Q5086126"}
     allowed_domains = ["charlotterusse.com"]
-    start_urls = ["https://charlotterusse.com/apps/api/v1/stores"]
+    start_urls = [
+        "https://charlotterusse.com/apps/api/v1/stores?location%5Blatitude%5D=40.6970193&location%5Blongitude%5D=-74.3093248&location%5Bradius%5D=2240"
+    ]
     locations_key = "stores"
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
