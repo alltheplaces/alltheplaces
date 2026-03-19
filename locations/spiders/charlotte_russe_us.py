4945e401d52269818db221ca6d20d368d80f4866
Cj Malone <CjMalone@mail.com>
2026-03-19 14:05:54 +0000

    Restore coords

--- locations/spiders/charlotte_russe_us.py
+++ locations/spiders/charlotte_russe_us.py
@@ -1,4 +1,11 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
+from locations.spiders.urban_planet_ca_pr_us import UrbanPlanetCAPRUSSpider
 
 
 class CharlotteRusseUSSpider(JSONBlobSpider):
@@ -9,3 +16,8 @@ class CharlotteRusseUSSpider(JSONBlobSpider):
         "https://charlotterusse.com/apps/api/v1/stores?location%5Blatitude%5D=40.6970193&location%5Blongitude%5D=-74.3093248&location%5Bradius%5D=2240"
     ]
     locations_key = "stores"
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        UrbanPlanetCAPRUSSpider.parse_additional_fields(item, feature)
+        apply_category(Categories.SHOP_CLOTHES, item)
+        yield item
