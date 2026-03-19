39043c6a1ecc5f5a8f90a1881dfe0615bf269835
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-19 19:07:34 +0530

    code updated to fix spider

--- locations/spiders/charlotte_russe_us.py
+++ locations/spiders/charlotte_russe_us.py
@@ -1,21 +1,11 @@
-from typing import Iterable
-
-from scrapy.http import Response
-
-from locations.categories import Categories, apply_category
-from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
-from locations.spiders.urban_planet_ca_pr_us import UrbanPlanetCAPRUSSpider
 
 
 class CharlotteRusseUSSpider(JSONBlobSpider):
     name = "charlotte_russe_us"
     item_attributes = {"brand": "Charlotte Russe", "brand_wikidata": "Q5086126"}
     allowed_domains = ["charlotterusse.com"]
-    start_urls = ["https://charlotterusse.com/apps/api/v1/stores"]
+    start_urls = [
+        "https://charlotterusse.com/apps/api/v1/stores?location%5Blatitude%5D=40.6970193&location%5Blongitude%5D=-74.3093248&location%5Bradius%5D=2240"
+    ]
     locations_key = "stores"
-
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        UrbanPlanetCAPRUSSpider.parse_additional_fields(item, feature)
-        apply_category(Categories.SHOP_CLOTHES, item)
-        yield item
