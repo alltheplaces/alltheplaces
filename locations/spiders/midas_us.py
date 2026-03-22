98ac50217a078e59c77de5b44b9d6292bc5547f3
JoshM2 <joshuamarriottcubing@gmail.com>
2026-03-22 13:24:31 -0400

    added midas spider

---
+++ locations/spiders/midas_us.py
@@ -0,0 +1,25 @@
+import scrapy
+
+from locations.dict_parser import DictParser
+
+
+class MidasUSSpider(scrapy.Spider):
+    name = "midas_us"
+    item_attributes = {"brand": "Midas", "brand_wikidata": "Q3312613"}
+    start_urls = ["https://www.midas.com/store"]
+
+    def parse(self, response):
+        for state_url in response.css("a.locations-columns__link::attr(href)").getall():
+            state_abbreviation = state_url.split("/")[-1]
+            yield scrapy.Request(
+                "https://www.midas.com/partialglobalsearch/getstatestorelist?state=" + state_abbreviation,
+                callback=self.parse_state,
+            )
+
+    def parse_state(self, response):
+        for store in response.json():
+            store["street_address"] = store.pop("Address")
+            item = DictParser.parse(store)
+            item["website"] = store["ShopDetailsLink"]
+            item["name"] = "Midas"
+            yield item
