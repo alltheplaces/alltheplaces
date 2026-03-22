b4f2cb64d23031d7a65025bf4fce247e917a65e7
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-03-22 11:23:24 +0900

    add if statements to remove errors
    {'atp/category/tourism/artwork': 567,
     'atp/clean_strings/name': 1,
     'atp/country/BE': 1,
     'atp/country/JP': 496,
     'atp/duplicate_count': 3,
     'atp/field/branch/missing': 567,
     'atp/field/brand/missing': 567,
     'atp/field/brand_wikidata/missing': 567,
     'atp/field/city/missing': 567,
     'atp/field/country/from_reverse_geocoding': 497,
     'atp/field/country/missing': 70,
     'atp/field/email/missing': 567,
     'atp/field/geometry/missing': 70,
     'atp/field/image/missing': 3,
     'atp/field/name/missing': 70,
     'atp/field/opening_hours/missing': 567,
     'atp/field/phone/missing': 567,
     'atp/field/postcode/missing': 567,
     'atp/field/state/missing': 70,
     'atp/field/street_address/missing': 567,
     'atp/field/twitter/missing': 567,
     'atp/item_scraped_host_count/jptca.org': 570,
     'atp/lineage': 'S_?',
     'atp/nsi/operator_missing': 567,
     'atp/operator/日本交通文化協会': 567,
     'atp/operator_wikidata/Q41698337': 567,
     'downloader/request_bytes': 214426,
     'downloader/request_count': 572,
     'downloader/request_method_count/GET': 572,
     'downloader/response_bytes': 13503676,
     'downloader/response_count': 572,
     'downloader/response_status_count/200': 572,
     'elapsed_time_seconds': 704.326509,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 3, 22, 2, 20, 59, 648612, tzinfo=datetime.timezone.utc),
     'httpcompression/response_bytes': 84817969,
     'httpcompression/response_count': 572,
     'item_dropped_count': 3,
     'item_dropped_reasons_count/DropItem': 3,
     'item_scraped_count': 567,
     'items_per_minute': 48.32386363636364,
     'log_count/DEBUG': 1142,
     'log_count/INFO': 51,
     'request_depth_max': 1,
     'response_received_count': 572,
     'responses_per_minute': 48.75,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/200': 1,
     'scheduler/dequeued': 571,
     'scheduler/dequeued/memory': 571,
     'scheduler/enqueued': 571,
     'scheduler/enqueued/memory': 571,
     'start_time': datetime.datetime(2026, 3, 22, 2, 9, 15, 322103, tzinfo=datetime.timezone.utc)}

--- locations/spiders/jptca.py
+++ locations/spiders/jptca.py
@@ -13,6 +13,10 @@ class JptcaSpider(SitemapSpider):
 
     def parse(self, response, **kwargs):
         item = Feature()
+        if latlon := re.search(r"LatLng\(\s*(\d+\.\d+),(\d+\.\d+)\);", response.text): #only get items with location
+            item["lat"], item["lon"] = latlon.groups()
+        else:
+            yield None
         item["ref"] = "".join(filter(str.isdigit, response.url))
         item["website"] = response.url
         item["extras"]["website:en"] = f"https://jptca.org/en/publicart{item['ref']}/"
@@ -22,10 +26,9 @@ class JptcaSpider(SitemapSpider):
 
         if hira := re.search(r'kana">(.+)<\/', response.text):
             item["extras"]["name:ja-Hira"] = hira.group(1)
-
-        item["lat"], item["lon"] = re.search(r"LatLng\(\s*(\d+\.\d+),(\d+\.\d+)\);", response.text).groups()
-        item["image"] = "https://jptca.org" + re.search(
-            r"background-image:url\('(.+sakuhin.jpg)'", response.text
-        ).group(1)
+        
+        if img := re.search(r"background-image:url\('(.+sakuhin.jpg)'", response.text):
+            item["image"] = "https://jptca.org" + img.group(1)
+        
         item["extras"]["tourism"] = "artwork"
         yield item
