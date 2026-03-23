ab87c37710fb1700e2538a1d57de84fbab6fc933
Cj Malone <CjMalone@mail.com>
2026-03-23 09:12:00 +0000

    Add category

--- locations/spiders/title_boxing_club_us.py
+++ locations/spiders/title_boxing_club_us.py
@@ -1,5 +1,9 @@
-import scrapy
+from typing import Any
 
+import scrapy
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
@@ -9,10 +13,11 @@ class TitleBoxingClubUSSpider(scrapy.Spider):
     start_urls = ["https://api.hubapi.com/cms/v3/hubdb/tables/121868642/rows?portalId=48754936"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for poi in response.json()["results"]:
             poi["values"]["street_address"] = poi["values"].pop("address")
             item = DictParser.parse(poi["values"])
             item["website"] = "https://www.titleboxingclub.com/location/" + poi["values"]["site_slug"]
             item["name"] = "Title Boxing Club"
+            apply_category(Categories.GYM, item)
             yield item
