9ec09941e5b8565a3cf3dfa4d5013c65bdfae820
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-03-23 21:04:52 +0900

    [Seijo Ishii JP] create spider (231 locations)
    {'atp/brand/成城石井': 231,
     'atp/brand_wikidata/Q11495410': 231,
     'atp/category/shop/supermarket': 231,
     'atp/country/JP': 231,
     'atp/field/city/missing': 231,
     'atp/field/country/from_spider_name': 231,
     'atp/field/email/missing': 231,
     'atp/field/image/missing': 231,
     'atp/field/opening_hours/missing': 231,
     'atp/field/operator/missing': 231,
     'atp/field/operator_wikidata/missing': 231,
     'atp/field/state/missing': 231,
     'atp/field/street_address/missing': 231,
     'atp/field/twitter/missing': 231,
     'atp/item_scraped_host_count/shop.seijoishii.com': 231,
     'atp/lineage': 'S_?',
     'atp/nsi/perfect_match': 231,
     'downloader/request_bytes': 726,
     'downloader/request_count': 2,
     'downloader/request_method_count/GET': 2,
     'downloader/response_bytes': 23445,
     'downloader/response_count': 2,
     'downloader/response_status_count/200': 2,
     'elapsed_time_seconds': 2.968109,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 3, 23, 12, 0, 31, 30484, tzinfo=datetime.timezone.utc),
     'httpcompression/response_bytes': 154691,
     'httpcompression/response_count': 2,
     'item_scraped_count': 231,
     'items_per_minute': 6930.0,
     'log_count/DEBUG': 233,
     'log_count/INFO': 3,
     'response_received_count': 2,
     'responses_per_minute': 60.0,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/200': 1,
     'scheduler/dequeued': 1,
     'scheduler/dequeued/memory': 1,
     'scheduler/enqueued': 1,
     'scheduler/enqueued/memory': 1,
     'start_time': datetime.datetime(2026, 3, 23, 12, 0, 28, 62375, tzinfo=datetime.timezone.utc)}

---
+++ locations/spiders/seijoishii_jp.py
@@ -0,0 +1,48 @@
+from typing import Any, AsyncIterator
+
+from scrapy import Spider
+from scrapy.http import Request, Response
+
+from locations.items import Feature
+from locations.categories import Categories, apply_category
+
+
+class SeijoishiiJPSpider(Spider):
+    name = "seijoishii_jp"
+    item_attributes = {"brand": "成城石井", "brand_wikidata": "Q11495410"}
+
+    async def start(self) -> AsyncIterator[Request]:
+        yield self.get_page(0)
+
+    def get_page(self, n):
+        return Request(
+            f"https://shop.seijoishii.com/seijoishii/api/proxy2/shop/list?datum=wgs84&limit=500&offset={n}",
+            meta={"offset": n},
+        )
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        data = response.json()
+        stores = data["items"]
+
+        for store in stores:
+            item = Feature()
+            match store["categories"][0]["code"]:
+                case "03":
+                    pass
+                case _:
+                    continue # skip other types
+
+            item["branch"] = store["name"]
+            item["extras"]["branch:ja-Hira"] = store.get("ruby")
+            item["website"] = f"https://shop.seijoishii.com/seijoishii/spot/detail?code={store['code']}"
+            item["ref"] = store["code"]
+            item["phone"] = f"+81 {store['phone']}"
+            item["lat"] = store["coord"]["lat"]
+            item["lon"] = store["coord"]["lon"]
+            item["addr_full"] = store["address_name"]
+            item["postcode"] = store["postal_code"]
+
+            yield item
+
+        if data["count"]["offset"] + data["count"]["limit"] < data["count"]["total"]:
+            yield self.get_page(data["count"]["limit"] + response.meta["offset"])
