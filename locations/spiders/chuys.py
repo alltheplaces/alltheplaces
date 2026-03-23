d229cbd3c5a421363537328a877fed93d0f8e6a7
Cj Malone <CjMalone@mail.com>
2026-03-23 10:47:47 +0000

    Use better websites

--- locations/spiders/chuys.py
+++ locations/spiders/chuys.py
@@ -1,21 +1,29 @@
-from typing import Any, AsyncIterator
+from typing import Any
 
-import scrapy
+from scrapy import Spider
 from scrapy.http import JsonRequest, Response
+from scrapy.spiders.sitemap import iterloc
+from scrapy.utils.sitemap import Sitemap
 
 from locations.dict_parser import DictParser
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class ChuysSpider(scrapy.Spider):
+class ChuysSpider(Spider):
     name = "chuys"
     item_attributes = {"brand": "Chuy's", "brand_wikidata": "Q5118415"}
-    requires_proxy = True
-
-    async def start(self) -> AsyncIterator[Any]:
-        yield JsonRequest(url="https://www.chuys.com/api/restaurants", headers={"x-source-channel": "WEB"})
+    start_urls = ["https://www.chuys.com/locations-sitemap.xml"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
+        self.urls = {}
+        for loc in iterloc(Sitemap(response.body)):
+            self.urls[loc.rsplit("/", 1)[1]] = loc
+
+        yield JsonRequest(
+            url="https://www.chuys.com/api/restaurants", headers={"x-source-channel": "WEB"}, callback=self.parse_api
+        )
+
+    def parse_api(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json()["restaurants"]:
             location.update(location["contactDetail"].pop("address"))
             location.update(location["contactDetail"]["phoneDetail"].pop(0))
@@ -24,13 +32,5 @@ class ChuysSpider(scrapy.Spider):
             item["name"] = location["restaurantName"]
             item["ref"] = location["restaurantNumber"]
             item["street_address"] = merge_address_lines([location.get("street1"), location.get("street2")])
-            item["website"] = "/".join(
-                [
-                    "https://www.chuys.com/locations",
-                    location["stateCode"].lower(),
-                    location["city"].lower(),
-                    location["restaurantName"].lower().replace(" ", "-"),
-                    str(location["restaurantNumber"]),
-                ]
-            )
+            item["website"] = self.urls.get(str(location["restaurantNumber"]))
             yield item
