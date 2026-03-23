9d49126bf2f4116cfcb104c339a2c8c4533aefa5
Cj Malone <CjMalone@mail.com>
2026-03-23 11:57:47 +0000

    apply category

--- locations/spiders/midas_us.py
+++ locations/spiders/midas_us.py
@@ -1,22 +1,26 @@
-import scrapy
+from typing import Any
 
+from scrapy.http import JsonRequest, Response
+from scrapy.spiders import CrawlSpider
+
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
-class MidasUSSpider(scrapy.Spider):
+class MidasUSSpider(CrawlSpider):
     name = "midas_us"
     item_attributes = {"brand": "Midas", "brand_wikidata": "Q3312613"}
     start_urls = ["https://www.midas.com/store"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for state_url in response.css("a.locations-columns__link::attr(href)").getall():
             state_abbreviation = state_url.split("/")[-1]
-            yield scrapy.Request(
+            yield JsonRequest(
                 "https://www.midas.com/partialglobalsearch/getstatestorelist?state=" + state_abbreviation,
                 callback=self.parse_state,
             )
 
-    def parse_state(self, response):
+    def parse_state(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json():
             store["street_address"] = store.pop("Address")
             item = DictParser.parse(store)
@@ -24,4 +28,7 @@ class MidasUSSpider(scrapy.Spider):
             item["name"] = "Midas"
             # Emails are not unique to each store and some are personal email addresses.
             item["email"] = None
+
+            apply_category(Categories.SHOP_CAR_PARTS, item)
+
             yield item
