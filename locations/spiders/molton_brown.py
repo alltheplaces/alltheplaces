c4dbcfdfbee7045979949c2e42386c21cef843a7
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-24 20:39:38 +0100

    Add pages

--- locations/spiders/molton_brown.py
+++ locations/spiders/molton_brown.py
@@ -1,7 +1,9 @@
-from typing import Iterable
+from typing import Iterable, AsyncIterator, Any
 
 from scrapy.http import TextResponse
 
+from scrapy import Request
+from scrapy.http import JsonRequest, Response
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -10,9 +12,10 @@ class MoltonBrownSpider(JSONBlobSpider):
     name = "molton_brown"
     item_attributes = {"brand": "Molton Brown", "brand_wikidata": "Q17100584"}
     #    start_urls = ["https://api.moltonbrown.com/kaowebservices/v2/moltonbrown-gb/kao/stores"]
-    start_urls = [
-        "https://api.cxur-kaocorpor1-p3-public.model-t.cc.commerce.ondemand.com/kaowebservices/v2/moltonbrown-gb/stores/?currentPage=1"
-    ]
+    #page = 0
+    #start_urls = [
+    #    f"https://api.cxur-kaocorpor1-p3-public.model-t.cc.commerce.ondemand.com/kaowebservices/v2/moltonbrown-gb/stores/?currentPage={page}"
+    #]
     locations_key = ["stores"]
 
     custom_settings = {
@@ -28,13 +31,34 @@ class MoltonBrownSpider(JSONBlobSpider):
         "DOWNLOAD_DELAY": 10,
     }
 
+    def make_request(self, page: int) -> JsonRequest:
+        return JsonRequest(
+            url=f"https://api.cxur-kaocorpor1-p3-public.model-t.cc.commerce.ondemand.com/kaowebservices/v2/moltonbrown-gb/stores/?currentPage={page}",
+            meta={"page": page},
+        )
+
+    async def start(self) -> AsyncIterator[Request]:
+        yield self.make_request(0)
+
+
+    def parse(self, response: TextResponse) -> Iterable[Feature]:
+        features = self.extract_json(response)
+        if isinstance(features, dict):
+            yield from self.parse_feature_dict(response, features) or []
+        else:
+            yield from self.parse_feature_array(response, features) or []
+        if response.json()["pagination"]["totalPages"] > response.meta["page"]:
+            yield self.make_request(response.meta["page"] + 1)
+
+
     def pre_process_data(self, feature: dict) -> None:
         feature["id"] = feature["name"]
         if feature.get("displayName"):
             feature["name"] = feature["displayName"]
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
-        item["website"] = "https://www.moltonbrown.co.uk/store/store-finder/" + item["website"].replace(" ", "-")
-        # the link on the Molton Brown site includes a space
-        if feature["storeType"] != "STOCKISTS":
+        if item.get("website"):
+            item["website"] = "https://www.moltonbrown.co.uk/store/store-finder/" + item["website"].replace(" ", "-")
+            # the link on the Molton Brown site includes a space
+        if feature["storeType"] != "STOCKISTS" and feature["storeType"] != "ORIBESTORES":
             yield item
