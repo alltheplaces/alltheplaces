ef4e2cc4d3676c52900154ba28402d0928eae744
JoshM2 <joshuamarriottcubing@gmail.com>
2026-03-24 15:54:06 -0400

    added h mart spider

---
+++ locations/spiders/h_mart_us.py
@@ -0,0 +1,53 @@
+import json
+
+import scrapy
+
+from locations.dict_parser import DictParser
+
+
+class HmartStoresSpider(scrapy.Spider):
+    name = "h_mart_us"
+    item_attributes = {"brand": "H Mart", "brand_wikidata": "Q5636306"}
+
+    def start_requests(self):
+        url = "https://www.hmart.com/_v/private/graphql/v1?workspace=master&maxAge=long&appsEtag=remove&domain=store&locale=en-US&__bindingId=0b48cd1a-951b-4fa9-8472-2b68ae904016"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Origin": "https://www.hmart.com",
+            "Referer": "https://www.hmart.com/storelocator",
+            "User-Agent": "Mozilla/5.0",
+            "x-vtex-use-https": "true",  # keep this, VTEX sometimes needs it
+        }
+
+        payload = {
+            "operationName": "getStores",
+            "variables": {},
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "d126e9e1e4aeaf939fd9cb010881f0a4f78a2394008821968eb8422f78238668",
+                    "sender": "hmartus.store-locator@1.x",
+                    "provider": "hmartus.store-locator@1.x",
+                }
+            },
+        }
+
+        yield scrapy.Request(url=url, method="POST", headers=headers, body=json.dumps(payload), callback=self.parse)
+
+    def parse(self, response):
+        stores = response.json()["data"]["getStores"]["items"]
+        for store in stores:
+            if store["isActive"] != True:
+                continue
+
+            store["address"]["street_address"] = store["address"].pop("street")
+            item = DictParser.parse(store)
+            item.update(DictParser.parse(store["address"]))
+            item["housenumber"] = store["address"]["number"]
+            item["branch"] = store["name"]
+            item["phone"] = store["instructions"]
+            item["ref"] = store["id"]
+
+            yield item
