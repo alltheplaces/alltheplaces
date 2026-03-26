900c6d2ffb3cc9866d446d8c8f941ae042f20ccd
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-03-26 15:00:35 +0530

    code refactored to fetch location id dynamically

--- locations/spiders/millets_gb.py
+++ locations/spiders/millets_gb.py
@@ -1,30 +1,43 @@
-from typing import Iterable
+import json
+from typing import Any
 
-from scrapy.http import TextResponse
+import scrapy
+from scrapy.http import JsonRequest, Response
 
+from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
-from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
 
 
-class MilletsGBSpider(JSONBlobSpider):
+class MilletsGBSpider(scrapy.Spider):
     name = "millets_gb"
     item_attributes = {"brand": "Millets", "brand_wikidata": "Q64822903"}
-    start_urls = [
-        "https://integrations-c3f9339ff060a907cbd8.o2.myshopify.dev/api/stores?fid=ML00361,ML00758,ML01603,ML00339,ML00185,ML00478,ML00024,ML00249,ML00051,ML00023,ML00114,ML00372,ML00629,ML00889,ML00276,ML00375,ML00041,ML00175,ML00217,ML00995,ML00042,ML00327,ML00635,ML00286,ML00477,ML00675,ML00317,ML00396,ML01362,ML00379,ML00099,ML00125,ML00856,ML01945,ML00058,ML00180,ML00401,ML00318,ML00469,ML00718,ML00020"
-    ]
-    locations_key = "stores"
+    start_urls = ["https://www.millets.co.uk/pages/our-stores"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
-        item["street_address"] = feature["address_1"]
-        item["phone"] = feature.get("local_phone")
-        oh = OpeningHours()
-        for day, time in feature["hours_sets"]["primary"]["days"].items():
-            for open_close_time in time:
-                open_time = open_close_time["open"]
-                close_time = open_close_time["close"]
-                oh.add_range(day, open_time, close_time)
-        item["opening_hours"] = oh
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store_id in json.loads(
+            response.xpath('//*[@id="app-store-locator"]//*[@type="application/json"]//text()').get()
+        )["stores"]:
+            yield JsonRequest(
+                url=f"https://integrations-c3f9339ff060a907cbd8.o2.myshopify.dev/api/stores?fid={store_id['id']}",
+                callback=self.parse_location,
+            )
 
-        yield item
+    def parse_location(self, response):
+        if data := response.json()["stores"]:
+            raw_data = data[0]
+            item = DictParser.parse(raw_data)
+            item["street_address"] = raw_data["address_1"]
+            item["phone"] = raw_data.get("local_phone")
+            item["website"] = "https://www.millets.co.uk/pages/stores/" + item["name"].lower().replace(
+                " ", "-"
+            ).replace("\xa0", "-")
+            oh = OpeningHours()
+            for day, time in raw_data["hours_sets"]["primary"]["days"].items():
+                for open_close_time in time:
+                    open_time = open_close_time["open"]
+                    close_time = open_close_time["close"]
+                    oh.add_range(day, open_time, close_time)
+            item["opening_hours"] = oh
+
+            yield item
