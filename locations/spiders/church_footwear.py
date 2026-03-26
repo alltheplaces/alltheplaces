f4aa6d94c523615456ddb90fba9f38b465026d98
Cj Malone <CjMalone@mail.com>
2026-03-26 14:09:11 +0000

    Trim headers

--- locations/spiders/church_footwear.py
+++ locations/spiders/church_footwear.py
@@ -5,26 +5,13 @@ from scrapy.http import TextResponse
 
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
-from locations.user_agents import BROWSER_DEFAULT
 
 
 class ChurchFootwearSpider(JSONBlobSpider):
     name = "church_footwear"
     item_attributes = {"brand": "Church's", "brand_wikidata": "Q2967663"}
     start_urls = ["https://api.church-footwear.com/anon/mwstorestore/store/v2?langId=en_GB"]
-    custom_settings = {
-        "ROBOTSTXT_OBEY": False,
-        "USER_AGENT": BROWSER_DEFAULT,
-        "DEFAULT_REQUEST_HEADERS": {
-            "Host": "api.church-footwear.com",
-            "Accept": "application/json, text/plain, */*",
-            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Referer": "https://www.church-footwear.com/",
-            "storeId": "churchsStore-GB",
-            "Origin": "https://www.church-footwear.com",
-        },
-    }
+    custom_settings = {"DEFAULT_REQUEST_HEADERS": {"storeId": "churchsStore-GB"}}
     locations_key = "allStores"
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
