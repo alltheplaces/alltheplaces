72c5edf2401c224542994c6fe236862e2b765afc
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-26 20:22:08 +0100

    [Auntie Anne's GB] Add spider

---
+++ locations/spiders/auntie_annes_gb.py
@@ -0,0 +1,34 @@
+import re
+import chompjs
+
+from typing import Iterable
+
+from scrapy.spiders import SitemapSpider
+from scrapy.http import JsonRequest, Request, TextResponse, Response
+
+from locations.hours import OpeningHours
+from locations.json_blob_spider import JSONBlobSpider
+from locations.items import Feature
+
+
+class AuntieAnnesGBSpider(JSONBlobSpider):
+    name = "auntie_annes_gb"
+    item_attributes = {"brand": "Auntie Anne's", "brand_wikidata": "Q4822010"}
+    start_urls = ["https://www.auntieannes.co.uk/locations/"]
+    drop_attributes = {"facebook","twitter"}
+
+    def extract_json(self, response: Response) -> dict | list[dict]:
+        search = 'var mapsvg_options = '
+        script = response.xpath(f"//script[contains(text(), {search!r})]/text()").get().split('"data_objects":')[1].split(',"schema":')[0] + "}"
+        return chompjs.parse_js_object(script)["objects"]
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["addr_full"] = feature["location"]["address"]["formatted"]
+        item["city"] = feature["location"]["address"]["postal_town"]
+        item["lat"], item["lon"] = feature["location"]["geoPoint"]["lat"], feature["location"]["geoPoint"]["lng"]
+        item["branch"] = item.pop("name")
+        item["opening_hours"] = OpeningHours()
+        time = str(feature["opening_hours"])
+        time = re.sub('<[^<]+?>', '', time)
+        item["opening_hours"].add_ranges_from_string(time)
+        yield item
