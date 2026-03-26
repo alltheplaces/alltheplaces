1fc0f542e9d1ced9cf88ea82c66a079ba8d71dcc
Cj Malone <CjMalone@mail.com>
2026-03-26 13:59:23 +0000

    Safer opening hours

--- locations/spiders/hmv_gb.py
+++ locations/spiders/hmv_gb.py
@@ -22,17 +22,23 @@ class HmvGBSpider(JSONBlobSpider):
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["street_address"] = merge_address_lines([feature["addressOne"], feature["addressTwo"]])
         #        item["instagram"] = feature["Instagram"]
-        item["opening_hours"] = OpeningHours()
-        for day, time in re.findall(
-            r"(?:<br>|<strong>)(.+?):(?:|</strong>)?<span>(.+?)</span>", feature["openingTimes"]
-        ):
-            if time == "Closed":
-                item["opening_hours"].set_closed(day)
-            else:
-                item["opening_hours"].add_range(day, *time.split(" - "))
         item["branch"] = item.pop("name").replace("hmv ", "")
         item["twitter"] = feature["twitter"]
+
+        try:
+            item["opening_hours"] = self.parse_opening_hours(feature["openingTimes"])
+        except:
+            pass
 
         apply_category(Categories.SHOP_MUSIC, item)
 
         yield item
+
+    def parse_opening_hours(self, opening_times: str) -> OpeningHours:
+        oh = OpeningHours()
+        for day, time in re.findall(r"(?:<br>|<strong>)(.+?):(?:|</strong>)?<span>(.+?)</span>", opening_times):
+            if time == "Closed":
+                oh.set_closed(day)
+            else:
+                oh.add_range(day, *time.split(" - "))
+        return oh
