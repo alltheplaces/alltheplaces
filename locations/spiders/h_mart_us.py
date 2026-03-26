91c428d6a08c6775661d8f06d759e37d6f2d0ec8
Cj Malone <CjMalone@mail.com>
2026-03-26 14:32:20 +0000

    Trim start request

--- locations/spiders/h_mart_us.py
+++ locations/spiders/h_mart_us.py
@@ -1,43 +1,34 @@
-import json
+from typing import Any
 
-import scrapy
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
-class HMartUSSpider(scrapy.Spider):
+class HMartUSSpider(Spider):
     name = "h_mart_us"
     item_attributes = {"brand": "H Mart", "brand_wikidata": "Q5636306"}
 
     async def start(self):
-        url = "https://www.hmart.com/_v/private/graphql/v1?workspace=master&maxAge=long&appsEtag=remove&domain=store&locale=en-US&__bindingId=0b48cd1a-951b-4fa9-8472-2b68ae904016"
-
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Origin": "https://www.hmart.com",
-            "Referer": "https://www.hmart.com/storelocator",
-            "User-Agent": "Mozilla/5.0",
-            "x-vtex-use-https": "true",
-        }
-
-        payload = {
-            "operationName": "getStores",
-            "variables": {},
-            "extensions": {
-                "persistedQuery": {
-                    "version": 1,
-                    "sha256Hash": "d126e9e1e4aeaf939fd9cb010881f0a4f78a2394008821968eb8422f78238668",
-                    "sender": "hmartus.store-locator@1.x",
-                    "provider": "hmartus.store-locator@1.x",
-                }
+        yield JsonRequest(
+            "https://www.hmart.com/_v/private/graphql/v1?workspace=master&maxAge=long&appsEtag=remove&domain=store&locale=en-US&__bindingId=0b48cd1a-951b-4fa9-8472-2b68ae904016",
+            data={
+                "operationName": "getStores",
+                "variables": {},
+                "extensions": {
+                    "persistedQuery": {
+                        "version": 1,
+                        "sha256Hash": "d126e9e1e4aeaf939fd9cb010881f0a4f78a2394008821968eb8422f78238668",
+                        "sender": "hmartus.store-locator@1.x",
+                        "provider": "hmartus.store-locator@1.x",
+                    }
+                },
             },
-        }
+        )
 
-        yield scrapy.Request(url=url, method="POST", headers=headers, body=json.dumps(payload), callback=self.parse)
-
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         stores = response.json()["data"]["getStores"]["items"]
         for store in stores:
             if store["isActive"] is not True:
