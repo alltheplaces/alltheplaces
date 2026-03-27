ba2e63498337b219180b1aa8cd2cf89e61b55dcf
Cj Malone <CjMalone@mail.com>
2026-03-27 13:21:55 +0000

    Drop image

--- locations/spiders/shoneys.py
+++ locations/spiders/shoneys.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 from scrapy.http import TextResponse
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -11,8 +12,11 @@ class ShoneysSpider(StructuredDataSpider):
     item_attributes = {"brand": "Shoney's", "brand_wikidata": "Q7500392"}
     allowed_domains = ["shoneys.com"]
     start_urls = ["https://www.shoneys.com/locations"]
+    wanted_types = ["Restaurant"]
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         item["ref"] = item["lat"] = ld_data["latitude"]
         item["lon"] = ld_data["longitude"]
+        item["image"] = None
+        apply_category(Categories.RESTAURANT, item)
         yield item
