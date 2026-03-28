c9685339099e3a427b6b2c5234e163e21ebc0eda
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-03-28 22:31:08 +0100

    Try with a download delay

--- locations/spiders/new_look_gb.py
+++ locations/spiders/new_look_gb.py
@@ -9,11 +9,14 @@ class NewLookGBSpider(SitemapSpider, StructuredDataSpider):
     sitemap_urls = ["https://www.newlook.com/uk/sitemap/maps/sitemap_uk_pos_en_1.xml"]
     sitemap_rules = [(r"https:\/\/www\.newlook\.com\/uk\/store\/[-\w]+-(\d+)$", "parse_sd")]
     wanted_types = ["Store"]
+    drop_attributes = {"facebook", "twitter", "image"}
+    custom_settings = {"DOWNLOAD_DELAY": 5}
+    
 
-    # def sitemap_filter(self, entries):
-    #    for entry in entries:
-    #        if "closed" not in entry["loc"].lower():
-    #            yield entry
+    def sitemap_filter(self, entries):
+       for entry in entries:
+           if "closed" not in entry["loc"].lower():
+               yield entry
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["website"] = response.urljoin(item["website"])
