33640ea922ad1fe8e57e2179c8fd2dfcb65433b0
Cj Malone <CjMalone@mail.com>
2026-04-01 08:37:20 +0100

    Honour robots.txt

--- locations/spiders/tchibo.py
+++ locations/spiders/tchibo.py
@@ -1,8 +1,8 @@
 import re
 from typing import Any, AsyncIterator
 
-import scrapy
-from scrapy.http import JsonRequest
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
 
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours, sanitise_day
@@ -12,17 +12,16 @@ Covers Following Countries : AT,DE,CZ,SK,HU,PL,CH,TR
 """
 
 
-class TchiboSpider(scrapy.Spider):
+class TchiboSpider(Spider):
     name = "tchibo"
     item_attributes = {"brand": "Tchibo", "brand_wikidata": "Q564213"}
-    custom_settings = {"ROBOTSTXT_OBEY": False}
 
     async def start(self) -> AsyncIterator[Any]:
         yield JsonRequest(
-            url="https://www.tchibo.de/service/storefinder/api/plugins/storefinder/storefinder/api/stores?page=0&size=100&viewLat=48.5335627&viewLng=12.948368&precision=100000&storeTypeFilters=Filiale"
+            url="https://www.tchibo.de/service/storefinder/api/plugins/storefinder/storefinder/api/stores?viewLat=0&viewLng=0&precision=1000000&size=100&page=0&storeTypeFilters=Filiale"
         )
 
-    def parse(self, response, **kwargs):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json().get("content"):
             [store.update(store.pop(key)) for key in ["locationGeographicDto", "addressDto"]]
             item = DictParser.parse(store)
