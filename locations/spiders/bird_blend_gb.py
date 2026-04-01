0c5714ad03435096a1b662b0555311c209be061e
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-04-01 14:25:57 +0530

    category added

--- locations/spiders/bird_blend_gb.py
+++ locations/spiders/bird_blend_gb.py
@@ -1,3 +1,7 @@
+from typing import Iterable
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.storefinders.stockist import StockistSpider
 
 
@@ -5,3 +9,7 @@ class BirdBlendGBSpider(StockistSpider):
     name = "bird_blend_gb"
     item_attributes = {"brand": "Bird & Blend Tea Co", "brand_wikidata": "Q116985265"}
     key = "map_9q94xv43"
+
+    def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
+        apply_category(Categories.SHOP_TEA, item)
+        yield item
