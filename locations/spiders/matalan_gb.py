d5b7f5d968c583809beb9f027c8eaffb904c65f8
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-04-01 18:47:47 +0530

    code updated to fix spider

--- locations/spiders/matalan_gb.py
+++ locations/spiders/matalan_gb.py
@@ -1,30 +1,53 @@
 import json
+import re
+from typing import Any
 
+from requests import Response
 from scrapy.spiders import SitemapSpider
 
-from locations.items import set_closed
-from locations.structured_data_spider import StructuredDataSpider
+from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class MatalanGBSpider(SitemapSpider, StructuredDataSpider):
+class MatalanGBSpider(SitemapSpider):
     name = "matalan_gb"
     item_attributes = {"brand": "Matalan", "brand_wikidata": "Q12061509"}
     allowed_domains = ["www.matalan.co.uk"]
     sitemap_urls = ["https://www.matalan.co.uk/robots.txt"]
-    sitemap_rules = [(r"/stores/uk/[^/]+/[^/]+/[^/]+$", "parse_sd")]
-    time_format = "%H:%M:%S"
+    sitemap_rules = [(r"/stores/uk/[^/]+/[^/]+/[^/]+$", "parse")]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["phone"] = None
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        raw_data = json.loads(
+            re.search(
+                r"storeData\s*=\s*({.*});\s*const\s*nearbyStores",
+                response.xpath('//*[@data-track="storeVisit"]//text()').get(),
+            ).group(1)
+        )
+        raw_data.update(raw_data.pop("address"))
+        item = DictParser.parse(raw_data)
+        item["branch"] = item.pop("name").replace("Matalan - ", "")
         item["website"] = response.url
-
-        if item["name"].startswith("CLOSED "):
-            set_closed(item)
-
-        item["branch"] = item.pop("name").split(" - ", 1)[1]
-
-        storedata = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
-        store = json.loads(storedata)["props"]["pageProps"]["store"]
-        item["lat"] = store["latitude"]
-        item["lon"] = store["longitude"]
+        item["addr_full"] = merge_address_lines(
+            [
+                raw_data["addressLine1"],
+                raw_data["addressLine2"],
+                raw_data["addressLine3"],
+                raw_data["addressLine4"],
+                raw_data["addressLine5"],
+            ]
+        )
+        oh = OpeningHours()
+        try:
+            for day_time in raw_data["openingTimes"]:
+                day = day_time["day"]
+                open_time = day_time["openingTime"][0:5]
+                close_time = day_time["closingTime"][0:5]
+                if open_time == "00:00" and close_time == "00:00":
+                    oh.set_closed(day)
+                else:
+                    oh.add_range(day=day, open_time=open_time, close_time=close_time, time_format="%H:%M")
+        except:
+            pass
+        item["opening_hours"] = oh
         yield item
