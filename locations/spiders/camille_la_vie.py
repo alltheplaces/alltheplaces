cf75470c602970af970964e0dea9e46b2ab53f81
IceBurger <sharma.anurag1995@gmail.com>
2026-04-02 10:20:57 +0530

    fixed camille_la_vie spider

--- locations/spiders/camille_la_vie.py
+++ locations/spiders/camille_la_vie.py
@@ -14,16 +14,23 @@ class CamilleLaVieSpider(CrawlSpider):
     name = "camille_la_vie"
     item_attributes = {"brand": "Camille La Vie"}
     start_urls = ("https://camillelavie.com/pages/store-locations",)
-    rules = [Rule(LinkExtractor(allow="/blogs/stores/"), callback="parse")]
+    rules = [Rule(LinkExtractor(allow="/pages/", restrict_xpaths='//*[@class="store_location"]'), callback="parse")]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         item = Feature()
         item["name"] = self.item_attributes["brand"]
-        item["branch"] = response.xpath("//h1//text()").get().removeprefix("Camille La Vie Dresses at ")
-        item["street_address"] = response.xpath('//*[@class="arcticle_description"]//p/span[2]/text()').get()
-        item["addr_full"] = merge_address_lines(
-            [item["street_address"], response.xpath('//*[@class="arcticle_description"]//p/span[3]/text()').get()]
+
+        base = response.xpath('//*[@class="arcticle_description"]')
+        item["branch"] = base.xpath(".//p/span/text()").get().strip() or base.xpath(".//div/text()").get()
+        item["addr_full"] = (
+            merge_address_lines(base.xpath(".//p/span/text()")[1:3].getall())
+            or merge_address_lines(base.xpath(".//div/text()")[1:3].getall()).split(" Phone:")[0]
         )
+        item["phone"] = (
+            base.xpath('.//a[contains(@href, "tel:")]/@href').get()
+            or base.xpath('.//*[contains(text(), "Phone:")]/text()').get()
+        )
+
         item["website"] = item["ref"] = response.url
         item["country"] = "US"
         extract_google_position(item, response)
