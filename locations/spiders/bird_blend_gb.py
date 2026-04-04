f7af317d385f277e55f3eb8cd5dd7a9b79e02c28
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-04-04 19:46:34 +0530

    [Bird Blend GB] Fix Spider (#15712)

--- locations/spiders/bird_blend_gb.py
+++ locations/spiders/bird_blend_gb.py
@@ -1,25 +1,15 @@
 from typing import Iterable
 
-from scrapy.http import Response
-
 from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.storefinders.stockist import StockistSpider
 
 
-class BirdBlendGBSpider(JSONBlobSpider):
+class BirdBlendGBSpider(StockistSpider):
     name = "bird_blend_gb"
     item_attributes = {"brand": "Bird & Blend Tea Co", "brand_wikidata": "Q116985265"}
-    start_urls = ["https://storemapper-herokuapp-com.global.ssl.fastly.net/api/users/8827/stores.js"]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    locations_key = "stores"
+    key = "map_9q94xv43"
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        if "Bird & Blend" in item["name"]:
-            item["branch"] = item["name"].replace("Bird & Blend Tea Co. - ", "")
+    def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         apply_category(Categories.SHOP_TEA, item)
-        item["opening_hours"] = OpeningHours()
-        item["opening_hours"].add_ranges_from_string(feature["custom_field_1"])
-        item.pop("email", None)
         yield item
