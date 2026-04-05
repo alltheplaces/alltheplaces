8c2312a03f9e4f0070852a42e95b910f1329d4f9
IceBurger <sharma.anurag1995@gmail.com>
2026-04-05 14:42:54 +0530

    rename the spider

--- locations/spiders/camille_la_vie_us.py
+++ locations/spiders/camille_la_vie_us.py
@@ -10,8 +10,8 @@ from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class CamilleLaVieSpider(CrawlSpider):
-    name = "camille_la_vie"
+class CamilleLaVieUSSpider(CrawlSpider):
+    name = "camille_la_vie_us"
     item_attributes = {"brand": "Camille La Vie"}
     start_urls = ("https://camillelavie.com/pages/store-locations",)
     rules = [Rule(LinkExtractor(allow="/pages/", restrict_xpaths='//*[@class="store_location"]'), callback="parse")]
@@ -21,7 +21,7 @@ class CamilleLaVieSpider(CrawlSpider):
         item["name"] = self.item_attributes["brand"]
 
         base = response.xpath('//*[@class="arcticle_description"]')
-        item["branch"] = base.xpath(".//p/span/text()").get().strip() or base.xpath(".//div/text()").get()
+        item["branch"] = base.xpath(".//p/span/text()").get("").strip() or base.xpath(".//div/text()").get()
         item["addr_full"] = (
             merge_address_lines(base.xpath(".//p/span/text()")[1:3].getall())
             or merge_address_lines(base.xpath(".//div/text()")[1:3].getall()).split(" Phone:")[0]
@@ -32,7 +32,6 @@ class CamilleLaVieSpider(CrawlSpider):
         )
 
         item["website"] = item["ref"] = response.url
-        item["country"] = "US"
         extract_google_position(item, response)
         apply_category(Categories.SHOP_CLOTHES, item)
         yield item
