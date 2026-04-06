3a0ed060dcf82215cf60fbcfbef031f6fed61ada
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-04-06 15:17:10 +0900

    Update cosmo_jp.py

--- locations/spiders/cosmo_jp.py
+++ locations/spiders/cosmo_jp.py
@@ -3,6 +3,7 @@ from typing import Any
 from scrapy import Spider
 from scrapy.http import Response
 
+from locations.categories import Categories, Fuel, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
 
 
@@ -10,7 +11,6 @@ class CosmoJPSpider(Spider):
     name = "cosmo_jp"
 
     start_urls = ["https://map.cosmo-energy.co.jp/api/points"]
-    allowed_domains = ["shop.big-echo.jp"]
     item_attributes = {
         "brand_wikidata": "Q2498318",
     }
@@ -20,8 +20,15 @@ class CosmoJPSpider(Spider):
             store.update(store.pop("extra_fields"))
             item = DictParser.parse(store)
 
-            item["branch"] = store.get("name")
-            item["website"] = f"https://map.cosmo-energy.co.jp/points/{store.get('key')}"
+            item["name"] = None
+            item["branch"] = store.get("name").removesuffix("ＳＳ")
+            item["extras"]["branch:ja-Hira"] = store.get("店名カナ")
+            item["operator"] = store.get("運営者名")
+            item["website"] = store.get("確認用URL")
             item["ref"] = store.get("key")
+            apply_yes_no("full_service", item, store.get("フル") == "1")
+            apply_yes_no("self_service", item, store.get("セルフ") == "1")
+
+            apply_category(Categories.FUEL_STATION, item)
 
             yield item
