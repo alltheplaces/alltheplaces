6a3e7798c5b2152ebd28b67a0bafbbeab27f0d2e
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-04-07 14:05:54 +0200

    Try to fix categories

--- locations/spiders/central_england_cooperative.py
+++ locations/spiders/central_england_cooperative.py
@@ -12,7 +12,6 @@ def set_operator(brand: dict, item: Feature):
 
 COOP_FOOD = {"brand": "The Co-operative Food", "brand_wikidata": "Q107617274"}
 COOP_FUNERALCARE = {"brand": "The Co-operative Funeralcare", "brand_wikidata": "Q7726521"}
-
 CENTRAL_COOP = {"brand": "Central England Co-operative", "brand_wikidata": "Q16986583"}
 
 
@@ -22,19 +21,28 @@ class CentralEnglandCooperativeSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [(r"https:\/\/stores\.centralengland\.coop\/[-\w]+\/.*$", "parse")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["image"] = None
         set_operator(CENTRAL_COOP, item)
         name = item["name"]
         item.pop("twitter", None)
+        item.pop("image", None)
         item["website"] = response.url
-        if "FOOD" in name.upper():
-            apply_category(Categories.SHOP_CONVENIENCE, item)
-            item.update(COOP_FOOD)
-            item["branch"] = item.pop("name").split(" - ", 1)[1].strip()
-            item["name"] = "Central Coop Food"
-        else:
+        if "FUNERAL" in name.upper() or "CREMATORIUM" in name.upper() or "COFFINS" in name.upper():
             apply_category(Categories.SHOP_FUNERAL_DIRECTORS, item)
             if "Central Co-op Funeral" in item["name"]:
                 item.update(COOP_FUNERALCARE)
                 item["branch"] = item.pop("name").replace("Central Co-op Funeral", "").strip(" -")
+        else if "FLORIST" in name.upper():
+            apply_category(Categories.SHOP_FLORIST, item)
+            if "Central Co-op Florist" in item["name"]:
+                item.update(CENTRAL_COOP)
+                item["name"],item["branch"] = item["name"].split(" - ")
+        else:
+            apply_category(Categories.SHOP_CONVENIENCE, item)
+            if item["name"].startswith("The Co-operative"):
+                item.update(COOP_FOOD)
+                item["branch"] = item["name"].replace("The Co-operative ","")
+            else:
+                item.update(CENTRAL_COOP)
+                item["name"],item["branch"] = item["name"].split(" - ")
+
         yield item
