4d68afdf3b22426b5b2c1d2046fd5fd91bd39e36
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-04-07 12:09:22 +0530

    Try using playwright to fix Northern Railway GB Spider

--- locations/spiders/northern_railway_gb.py
+++ locations/spiders/northern_railway_gb.py
@@ -1,25 +1,29 @@
-from scrapy import Spider
+import json
+
 from scrapy.http import JsonRequest
 
 from locations.categories import Categories, apply_category
 from locations.google_url import url_to_coords
 from locations.items import Feature
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
-class NorthernRailwayGBSpider(Spider):
+class NorthernRailwayGBSpider(PlaywrightSpider):
     name = "northern_railway_gb"
     item_attributes = {"operator": "Northern", "operator_wikidata": "Q85789775"}
     start_urls = ["https://www.northernrailway.co.uk/api/northern_station_list_auto_complete"]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
     def parse(self, response, **kwargs):
-        for location in response.json()["results"]:
+        for location in json.loads(response.xpath("//pre/text()").get())["results"]:
             yield JsonRequest(
                 "https://www.northernrailway.co.uk/api/stations/data/{}".format(location["crs_code"]),
                 callback=self.parse_station,
             )
 
     def parse_station(self, response, **kwargs):
-        location = response.json()
+        location = json.loads(response.xpath("//pre/text()").get())
         item = Feature()
         item["ref"] = item["extras"]["ref:crs"] = location["crs"]
         item["name"] = location["name"]
