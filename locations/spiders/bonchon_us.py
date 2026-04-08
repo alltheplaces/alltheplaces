74639172e91772c7efcaf15c569d061bab893e0b
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-04-08 11:45:13 +0100

    Sync brand with NSI

--- locations/spiders/bonchon_us.py
+++ locations/spiders/bonchon_us.py
@@ -5,17 +5,13 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class BonchonUSSpider(SitemapSpider, StructuredDataSpider):
     name = "bonchon_us"
-    item_attributes = {"brand": "Bonchon", "brand_wikidata": "Q4941248"}
+    item_attributes = {"brand": "Bonchon Chicken", "brand_wikidata": "Q4941248"}
     drop_attributes = {"facebook"}
 
     sitemap_urls = ["https://restaurants.bonchon.com/sitemap.xml"]
-
-    sitemap_rules = [
-        (r"/locations/[^/]+/[^/]+/[^/]+$", "parse_sd"),
-    ]
+    sitemap_rules = [(r"/locations/[^/]+/[^/]+/[^/]+$", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["branch"] = item["name"].split("Bonchon")[1].strip(" -")
-        item["name"] = "Bonchon"
+        item["branch"] = item.pop("name").split("Bonchon")[1].strip(" -")
 
         yield item
