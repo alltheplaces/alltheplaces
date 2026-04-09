a5b3ecac539baff9c00070d31bcb2eaea06772d8
Cj Malone <CjMalone@mail.com>
2026-04-09 15:51:33 +0100

    Restore imports

--- locations/spiders/credit_agricole.py
+++ locations/spiders/credit_agricole.py
@@ -2,13 +2,13 @@ import json
 
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class CreditAgricoleSpider(SitemapSpider, StructuredDataSpider):
     name = "credit_agricole"
-    item_attributes = {"brand": "Crédit Agricole", "brand_wikidata": "Q590952", "extras": Categories.BANK.value}
+    item_attributes = {"brand": "Crédit Agricole", "brand_wikidata": "Q590952"}
     allowed_domains = ["credit-agricole.fr"]
     sitemap_urls = ["https://www.credit-agricole.fr/robots.txt"]
     sitemap_rules = [(r"/particulier/agence/[-\w]+/([-\w]+)\.html$", "parse_sd")]
@@ -16,8 +16,7 @@ class CreditAgricoleSpider(SitemapSpider, StructuredDataSpider):
     time_format = "%H:%M:%S"
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        if name := ld_data.get("Name"):
-            ld_data["name"] = name
+        item["branch"] = item.pop("name").removeprefix("Agence Crédit Agricole ")
 
         if geodata := response.xpath('//div[@class="npc-sl-strct-map Card js-CardAgencyMap"]/@data-value').get():
             coords = json.loads(geodata)
@@ -25,7 +24,7 @@ class CreditAgricoleSpider(SitemapSpider, StructuredDataSpider):
             item["lon"] = coords["longitude"]
 
         services = response.xpath('//span[@class="npc-sl-strct-srv-card--text "]/text()').getall()
-        has_atm = any("distributeur" in s.lower() and "billets" in s.lower() for s in services)
-        apply_yes_no(Extras.ATM, item, has_atm)
+        apply_yes_no(Extras.ATM, item, any("distributeur" in s.lower() and "billets" in s.lower() for s in services))
+        apply_category(Categories.BANK, item)
 
         yield item
