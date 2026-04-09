2763313bb2c61760f30e159b9878b19734ea359e
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-04-09 10:39:44 +0300

    [lidl_bg] Add website for each location

--- locations/spiders/lidl_bg.py
+++ locations/spiders/lidl_bg.py
@@ -4,6 +4,45 @@ from locations.categories import Categories, apply_category
 from locations.hours import DAYS_BG, OpeningHours, sanitise_day
 from locations.storefinders.virtualearth import VirtualEarthSpider
 
+def transliterate(text):
+    cyrillic_to_latin = {
+        "а": "a",
+        "б": "b",
+        "в": "v",
+        "г": "g",
+        "д": "d",
+        "е": "e",
+        "ж": "zh",
+        "з": "z",
+        "и": "i",
+        "й": "y",
+        "к": "k",
+        "л": "l",
+        "м": "m",
+        "н": "n",
+        "о": "o",
+        "п": "p",
+        "р": "r",
+        "с": "s",
+        "т": "t",
+        "у": "u",
+        "ф": "f",
+        "х": "kh",
+        "ц": "ts",
+        "ч": "ch",
+        "ш": "sh",
+        "щ": "shch",
+        "'": "'",
+        "ъ": "y",
+        "ь": "'",
+        "э": "e",
+        "ю": "yu",
+        "я": "ya",
+        ".": "",
+        " ": "-",
+    }
+    return text.translate(str.maketrans(cyrillic_to_latin))
+
 
 class LidlBGSpider(VirtualEarthSpider):
     name = "lidl_bg"
@@ -21,6 +60,11 @@ class LidlBGSpider(VirtualEarthSpider):
         ):
             if day := sanitise_day(day, DAYS_BG):
                 item["opening_hours"].add_range(day, start_time, end_time)
+
+        if item["city"] and item["street_address"]:
+            city = transliterate(item["city"].lower())
+            street = transliterate(item["street_address"].lower())
+            item["website"] = f"https://www.lidl.bg/s/bg-BG/magazini/{city}/{street}/"
 
         apply_category(Categories.SHOP_SUPERMARKET, item)
         yield item
