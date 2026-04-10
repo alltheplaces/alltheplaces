eaea9533cf9425c7c096815bdaf6f3bb623c3cbc
Cj Malone <CjMalone@mail.com>
2026-04-10 10:23:15 +0100

    Remove unrequired drop_attributes

--- locations/spiders/qd_stores_gb.py
+++ locations/spiders/qd_stores_gb.py
@@ -11,19 +11,18 @@ from locations.pipelines.address_clean_up import merge_address_lines
 
 class QdStoresGBSpider(JSONBlobSpider):
     name = "qd_stores_gb"
-    item_attributes = {"brand": "QD Stores", "brand_wikidata": "Q22101973"}
+    item_attributes = {"name": "QD Stores", "brand": "QD Stores", "brand_wikidata": "Q22101973"}
     start_urls = [
         "https://storelocator.metizapps.com/v2/api/front/store-locator/?shop=bde5e8-2.myshopify.com&_t=1775759065863"
     ]
     locations_key = "stores"
-    drop_attributes = {"email", "twitter", "facebook"}
+    drop_attributes = {"email"}
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["street_address"] = merge_address_lines([item.pop("addr_full"), feature["address2"]])
         item["branch"] = item.pop("name").removeprefix("QD ")
-        item["name"] = "QD Stores"
-        name = item["branch"].replace(" ", "-").lower()
-        item["website"] = "https://www.qdstores.co.uk/pages/" + name + "-qd"
+        slug = item["branch"].replace(" ", "-").lower()
+        item["website"] = "https://www.qdstores.co.uk/pages/" + slug + "-qd"
         apply_category(Categories.SHOP_VARIETY_STORE, item)
         oh = OpeningHours()
         oh.add_ranges_from_string(feature["hour_of_operation"])
