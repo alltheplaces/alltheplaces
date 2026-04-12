b7d33be3b939111f4d9ee3a75ee7e5842393d457
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-04-12 14:30:29 +0900

    Update hakuyosha_jp.py

--- locations/spiders/hakuyosha_jp.py
+++ locations/spiders/hakuyosha_jp.py
@@ -12,13 +12,25 @@ class HakuyoshaJPSpider(CanlySpider):
     brand_key = "380"
 
     def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        skip = ['株式会社', '営業所']
+        skip = ["株式会社", "営業所"]
         if any(x in feature.get("nameKanji") for x in skip):
             item = None
             return  # skip headquarters, sales locations
 
-        item["branch"] = feature.get("nameKanji").removeprefix("白洋舍スマートプラス").removeprefix("白洋舍 スマートプラス").removeprefix("白洋舍 ").removeprefix("白洋舍").removesuffix("店")
-        item["extras"]["branch:ja-Hira"] = feature.get("nameKana").removeprefix("ハクヨウシャスマートプラス").removeprefix("ハクヨウシャ").removesuffix("テン")
+        item["branch"] = (
+            feature.get("nameKanji")
+            .removeprefix("白洋舍スマートプラス")
+            .removeprefix("白洋舍 スマートプラス")
+            .removeprefix("白洋舍 ")
+            .removeprefix("白洋舍")
+            .removesuffix("店")
+        )
+        item["extras"]["branch:ja-Hira"] = (
+            feature.get("nameKana")
+            .removeprefix("ハクヨウシャスマートプラス")
+            .removeprefix("ハクヨウシャ")
+            .removesuffix("テン")
+        )
         item["website"] = f"https://map.hakuyosha.co.jp/detail/{feature.get('storeCode')}/"
         item["extras"]["start_date"] = feature.get("openingDate")
 
