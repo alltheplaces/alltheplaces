6d21eac072651987ebadb772859019b5da168b5f
Cj Malone <CjMalone@mail.com>
2026-04-13 12:11:27 +0100

    Collect fuels

--- locations/spiders/nouria_us.py
+++ locations/spiders/nouria_us.py
@@ -1,7 +1,11 @@
-from scrapy import Spider
+from typing import Any
 
-from locations.categories import Categories, apply_category, apply_yes_no
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category, apply_yes_no, Fuel, Extras
 from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class NouriaUSSpider(Spider):
@@ -9,20 +13,27 @@ class NouriaUSSpider(Spider):
     start_urls = ["https://nouria.com/wp-json/nouria/v1/locations"]
     item_attributes = {"brand": "Nouria", "brand_wikidata": "Q120637476"}
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.json():
             location = location | location["address"]
             item = DictParser.parse(location)
             item["branch"] = item.pop("name")
             item["website"] = location["link"]
-            item["street_address"] = location["address"]["address_line_1"]
-            if location["address"]["address_line_2"]:
-                item["street_address"] += " " + location["address"]["address_line_2"]
+            item["street_address"] = merge_address_lines(
+                [location["address"]["address_line_1"], location["address"]["address_line_2"]]
+            )
 
             # every location is a convenience store but not every location has a Nouria branded gas station
-            apply_category(Categories.SHOP_CONVENIENCE, item)
             if "nouria-fuel" in location["filters"]:
-                apply_category(Categories.FUEL_STATION, item)
+                fuel = item.deepcopy()
+                fuel["ref"] = "{}_fuel".format(fuel["ref"])
+                apply_category(Categories.FUEL_STATION, fuel)
+                apply_yes_no(Fuel.DIESEL, fuel, "diesel" in location["filters"])
+                apply_yes_no(Fuel.ELECTRIC, fuel, "ev-charging" in location["filters"])
+                apply_yes_no(Extras.CAR_WASH, fuel, "golden-nozzle-car-wash" in location["filters"])
+                yield fuel
 
+            apply_category(Categories.SHOP_CONVENIENCE, item)
             apply_yes_no("sells:alcohol", item, "beer-wine" in location["filters"])
+
             yield item
