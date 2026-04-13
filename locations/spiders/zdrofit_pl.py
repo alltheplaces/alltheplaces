100765a2a3c548cbd79553d839aa55e7853228d9
Cj Malone <CjMalone@mail.com>
2026-04-13 12:32:51 +0100

    Try with proxy

--- locations/spiders/zdrofit_pl.py
+++ locations/spiders/zdrofit_pl.py
@@ -1,27 +1,24 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories, apply_category
+from locations.categories import Categories, apply_category, apply_yes_no, Extras
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class ZdrofitPLSpider(SitemapSpider, StructuredDataSpider):
     name = "zdrofit_pl"
-    item_attributes = {
-        "brand": "Zdrofit",
-        "brand_wikidata": "Q113457353",
-    }
+    item_attributes = {"brand": "Zdrofit", "brand_wikidata": "Q113457353"}
     time_format = "%H:%M:%S"
     sitemap_urls = ["https://zdrofit.pl/sitemaps/clubs-1.xml"]
-    sitemap_rules = [
-        (r"https://zdrofit.pl/kluby-fitness/[a-z0-9-]+/$", "parse_sd"),
-    ]
+    sitemap_rules = [(r"https://zdrofit.pl/kluby-fitness/[a-z0-9-]+/$", "parse_sd")]
+    proxy_required = True
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        apply_yes_no("sauna", item, response.xpath('//*[contains(text(), "Sauna")]'))
+        apply_yes_no(Extras.WIFI, item, response.xpath('//*[contains(text(), "WiFi")]'))
         apply_category(Categories.GYM, item)
-        item["branch"] = item["name"]
-        item["name"] = "Zdrofit"
-        if response.xpath('//*[contains(text(), "Sauna")]'):
-            item["extras"]["sauna"] = "yes"
-        if response.xpath('//*[contains(text(), "WiFi")]'):
-            item["extras"]["internet_access"] = "wlan"
         yield item
