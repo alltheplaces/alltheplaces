0955cd7a8a7445de6383148b0fb06c76d4ed23f2
Cj Malone <CjMalone@mail.com>
2026-04-13 09:15:15 +0100

    Snip

--- locations/spiders/kona_grill_us.py
+++ locations/spiders/kona_grill_us.py
@@ -1,4 +1,3 @@
-
 from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
@@ -6,19 +5,9 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class KonaGrillUSSpider(SitemapSpider, StructuredDataSpider):
     name = "kona_grill_us"
-    item_attributes = {
-        "brand": "Kona Grill",
-        "brand_wikidata": "Q6428706",
-    }
-
+    item_attributes = {"brand": "Kona Grill", "brand_wikidata": "Q6428706"}
+    sitemap_urls = ["https://konagrill.com/locations-sitemap.xml"]
+    sitemap_rules = [(r"/location/([^/]+)/$", "parse_sd")]
+    wanted_types = [["Restaurant"]]
     drop_attributes = {"name", "image"}
     search_for_facebook = False
-
-    sitemap_urls = ["https://konagrill.com/locations-sitemap.xml"]
-    sitemap_rules = [(r"/location/", "parse_sd")]
-
-    # parse sd returns two locations for every restaurant url, one of which is an office location (the same one for every restaurant)
-    # excluding items that link to the kona grill home page excludes this office from the results.
-    def post_process_item(self, item, response, ld_item):
-        if item["website"] != "https://konagrill.com/":
-            yield item
