36b7ec007e359b902379ca11a5b8b7cbf4e80205
Cj Malone <CjMalone@mail.com>
2026-04-15 10:47:57 +0100

    Sync brands with NSI

--- locations/spiders/doro_it.py
+++ locations/spiders/doro_it.py
@@ -1,4 +1,4 @@
-import re
+from typing import Iterable
 
 from scrapy.http import TextResponse
 
@@ -9,28 +9,21 @@ from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
 
 class DoroITSpider(WPStoreLocatorSpider):
     name = "doro_it"
-    item_attributes = {"brand": "Doro", "brand_wikidata": "Q136464598"}
+    item_attributes = {"brand": "Doro Supermercati", "brand_wikidata": "Q136464598"}
     allowed_domains = ["www.dorosupermercati.it"]
     iseadgg_countries_list = ["IT"]
     search_radius = 100
     max_results = 100
 
-    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> None:
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         if "Daily" in item["name"]:
-            item["brand"] = "Doro Daily"
-            item["brand_wikidata"] = "Q137801479"
+            item["name"] = "Doro Daily"
+            apply_category(Categories.SHOP_CONVENIENCE, item)
         else:
-            item["brand"] = "Doro"
-            item["brand_wikidata"] = "Q136464598"
-
-        item["branch"] = re.sub(r"^Doro(?:\s+Daily)?\s+", "", item["name"])
+            item["name"] = "Doro Supermercati"
+            apply_category(Categories.SHOP_SUPERMARKET, item)
 
         if state := item.get("state"):
             item["state"] = state.strip("()")
-
-        if "Daily" in item["name"]:
-            apply_category(Categories.SHOP_CONVENIENCE, item)
-        else:
-            apply_category(Categories.SHOP_SUPERMARKET, item)
 
         yield item
