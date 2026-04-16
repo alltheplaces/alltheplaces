2129127fd123663fbc2a27d4e427c3b7e3327b44
Cj Malone <CjMalone@mail.com>
2026-04-16 12:38:20 +0100

    Parse weird json

--- locations/spiders/lunch_garden_be.py
+++ locations/spiders/lunch_garden_be.py
@@ -1,12 +1,12 @@
-import re
 from typing import Any
 from urllib.parse import urljoin
 
-import scrapy
 from scrapy import Spider
-from scrapy.http import Response
+from scrapy.http import JsonRequest, Response
 
-from locations.items import Feature
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.dict_parser import DictParser
+from locations.spiders.dino_pl import populate
 
 
 class LunchGardenBESpider(Spider):
@@ -15,18 +15,22 @@ class LunchGardenBESpider(Spider):
     start_urls = ["https://www.lunchgarden.be/nl/restaurants"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        url_path = response.xpath('//*[@type="application/json"]//@data-src').get()
-        yield scrapy.Request(url=urljoin(response.url, url_path), callback=self.parse_location)
+        yield JsonRequest(
+            url=urljoin(response.url, response.xpath('//*[@type="application/json"]/@data-src').get()),
+            callback=self.parse_location,
+        )
 
-    def parse_location(self, response, **kwargs):
-        pattern = r"\"([^\"]+,\s*\d{4}\s*[^\"]+)\".*?\{\s*\"lat\"\s*:\s*\d+,\s*\"lng\"\s*:\s*\d+\s*\}\s*,\s*([\d.]+)\s*,\s*([\d.]+).*?\"(Lunch Garden[^\"]+)\".*?\"([a-z0-9-]+)\""
-        matches = re.findall(pattern, response.text, re.DOTALL)
-        if matches:
-            for location in matches:
-                item = Feature()
-                item["branch"] = location[3].removeprefix("Lunch Garden ")
-                item["addr_full"] = location[0]
-                item["lat"] = location[1]
-                item["lon"] = location[2]
-                item["website"] = item["ref"] = "https://www.lunchgarden.be/nl/restaurants/" + location[-1]
-                yield item
+    def parse_location(self, response: Response, **kwargs: Any) -> Any:
+        for location in populate(0, response.json())["data"][1]["restaurants-nl"]:
+            item = DictParser.parse(location)
+            item["ref"] = location["_id"]
+            item["branch"] = item.pop("name").removeprefix("Lunch Garden ")
+            item["website"] = urljoin("https://www.lunchgarden.be/nl/restaurants/", location["slug"])
+
+            apply_yes_no(Extras.WHEELCHAIR, item, "accessible" in location["facilities"])
+            apply_yes_no(Extras.TAKEAWAY, item, "takeaway" in location["facilities"])
+            apply_yes_no(Extras.WIFI, item, "wifi" in location["facilities"])
+
+            apply_category(Categories.RESTAURANT, item)
+
+            yield item
