6a99518a81ab4d00e078342b760d9a269ee55530
IceBurger <sharma.anurag1995@gmail.com>
2026-04-17 11:09:35 +0530

    fix spider

--- locations/spiders/rei_us.py
+++ locations/spiders/rei_us.py
@@ -1,27 +1,56 @@
-from scrapy.spiders import SitemapSpider
+import json
+from typing import Iterable
 
-from locations.hours import OpeningHours, day_range
-from locations.structured_data_spider import StructuredDataSpider
+from scrapy.http import Response, TextResponse
+
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class ReiUSSpider(SitemapSpider, StructuredDataSpider):
+class ReiUSSpider(JSONBlobSpider, PlaywrightSpider):
     name = "rei_us"
-    item_attributes = {"brand": "REI", "brand_wikidata": "Q3414933", "country": "US"}
-    sitemap_urls = ["https://www.rei.com/sitemap-stores.xml"]
-    sitemap_rules = [(r"^https://www.rei.com/stores/([^/]+)$", "parse_sd")]
-    wanted_types = ["Store"]
+    item_attributes = {
+        "brand": "REI",
+        "brand_wikidata": "Q3414933",
+        "country": "US",
+    }
+
+    start_urls = ["https://www.rei.com/stores/map"]
     requires_proxy = True
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        item["website"] = response.url
-        item["branch"] = item.pop("name")
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {
+        "USER_AGENT": BROWSER_DEFAULT,
+    }
 
-        item["opening_hours"] = OpeningHours()
-        for rule in ld_data.get("openingHours", []):
-            days, times = rule.rsplit(" ", 1)
-            if "-" in days:
-                start_day, end_day = days.split(" - ")
-            else:
-                start_day = end_day = days
-            item["opening_hours"].add_days_range(day_range(start_day, end_day), *times.split("-"))
+    def extract_json(self, response: TextResponse) -> list[dict]:
+        raw_data = response.xpath('//script[@id="modelData"]/text()').get()
+        if not raw_data:
+            return []
+        data = json.loads(raw_data)
+        raw_stores = data.get("pageData", {}).get("allStores", {}).get("storeLocatorDataQueryModelList", [])
+        return [s.get("storeDataModel") for s in raw_stores if s.get("storeDataModel")]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["ref"] = str(feature.get("storeNumber"))
+
+        if url_path := feature.get("storePageUrl"):
+            item["website"] = f"https://www.rei.com/{url_path.lstrip('/')}"
+
+        if hours_list := feature.get("storeHours"):
+            item["opening_hours"] = self.parse_opening_hours(hours_list)
+
         yield item
+
+    def parse_opening_hours(self, hours_list: list) -> str:
+        oh = OpeningHours()
+        for h in hours_list:
+            day_part = h.get("days")
+            open_t = h.get("openAt")
+            close_t = h.get("closeAt")
+            if day_part and open_t and close_t:
+                oh.add_ranges_from_string(f"{day_part} {open_t} - {close_t}")
+        return oh
