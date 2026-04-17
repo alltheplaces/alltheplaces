3a947db831aa52c250d17943e32be975eb5228b1
Cj Malone <CjMalone@mail.com>
2026-04-17 13:35:18 +0100

    Collect branch

--- locations/spiders/cue_au.py
+++ locations/spiders/cue_au.py
@@ -1,7 +1,9 @@
 import json
 import re
+from typing import Any
 
 from scrapy import Spider
+from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
@@ -13,7 +15,7 @@ class CueAUSpider(Spider):
     item_attributes = {"brand": "Cue", "brand_wikidata": "Q5192554"}
     start_urls = ["https://www.cue.com/stores"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         raw_data = json.loads(
             re.search(
                 r"locations\":(\[.*\]),\"coords", response.xpath('//*[contains(text(),"address2")]/text()').get()
@@ -22,6 +24,7 @@ class CueAUSpider(Spider):
         for location in raw_data:
             location.update(location.pop("address"))
             item = DictParser.parse(location)
+            item["branch"] = item.pop("name").removesuffix(" Cue")
             item["street_address"] = merge_address_lines([location["address1"], location["address2"]])
             item["addr_full"] = merge_address_lines(location["formatted"])
             apply_category(Categories.SHOP_CLOTHES, item)
