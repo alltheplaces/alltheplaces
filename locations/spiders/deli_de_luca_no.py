defd5ee497344435a5722e29f448562ae64168c4
Cj Malone <CjMalone@mail.com>
2026-04-20 08:41:25 +0100

    Drop image

--- locations/spiders/deli_de_luca_no.py
+++ locations/spiders/deli_de_luca_no.py
@@ -9,7 +9,6 @@ class DeliDeLucaNOSpider(SylinderSpider):
     name = "deli_de_luca_no"
     item_attributes = {"name": "Deli de Luca", "brand": "Deli de Luca", "brand_wikidata": "Q11965047"}
     app_keys = ["1800"]
-    base_url = "https://delideluca.no/finn-oss/#"  # Kind of a hack. The website doesn't have the storefinder in place.
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name").removeprefix("Deli de Luca ")
