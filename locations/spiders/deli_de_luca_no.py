fe4b9aeec0f85bd074e4a7081aee0e1115cf9352
Cj Malone <CjMalone@mail.com>
2026-04-20 08:43:47 +0100

    apply category

--- locations/spiders/deli_de_luca_no.py
+++ locations/spiders/deli_de_luca_no.py
@@ -9,6 +9,7 @@ class DeliDeLucaNOSpider(SylinderSpider):
     name = "deli_de_luca_no"
     item_attributes = {"name": "Deli de Luca", "brand": "Deli de Luca", "brand_wikidata": "Q11965047"}
     app_keys = ["1800"]
+    warn_if_no_base_url = False
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name").removeprefix("Deli de Luca ")
--- locations/spiders/foodora_market_no.py
+++ locations/spiders/foodora_market_no.py
@@ -1,21 +1,19 @@
 from typing import Iterable
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.sylinder import SylinderSpider
 
 
 class FoodoraMarketNOSpider(SylinderSpider):
     name = "foodora_market_no"
-    item_attributes = {
-        "brand": "Foodora Market",
-        "brand_wikidata": "Q24068412",
-        "extras": Categories.DARK_STORE_GROCERY.value,
-    }
+    item_attributes = {"name": "Foodora Market", "brand": "Foodora Market", "brand_wikidata": "Q24068412"}
     app_keys = ["4140"]
     warn_if_no_base_url = False
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name").removeprefix("Foodora Market ").removesuffix(" DV")
-        item["name"] = "Foodora Market"
+
+        apply_category(Categories.DARK_STORE_GROCERY, item)
+
         yield item
