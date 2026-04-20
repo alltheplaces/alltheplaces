d896601ca268c9e45dafa628f69027355f9efdf0
Cj Malone <CjMalone@mail.com>
2026-04-20 09:00:45 +0100

    apply category

--- locations/spiders/snarkjop_no.py
+++ locations/spiders/snarkjop_no.py
@@ -1,19 +1,17 @@
 from typing import Iterable
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.sylinder import SylinderSpider
 
 
 class SnarkjopNOSpider(SylinderSpider):
     name = "snarkjop_no"
-    item_attributes = {"brand": "Snarkjøp", "extras": Categories.SHOP_CONVENIENCE.value}
+    item_attributes = {"name": "Snarkjøp", "brand": "Snarkjøp"}
     app_keys = ["4150"]
-    base_url = (
-        "https://www.snarkjop.no/butikkene/#"  # Kind of a hack. The website doesn't have the storefinder in place.
-    )
+    warn_if_no_base_url = False
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name").removeprefix("Snarkjøp ").removesuffix(" DV")
-        item["name"] = "Snarkjøp"
+        apply_category(Categories.SHOP_CONVENIENCE, item)
         yield item
