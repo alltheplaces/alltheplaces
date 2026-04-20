de3a70a6021a8d9a39fedb81f393d279d117e071
Johannes Andersen <johannes.andersen@telenor.no>
2026-04-20 10:23:04 +0200

    feat: add and extend SylinderSpiders (#15973)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/mix_no.py
+++ locations/spiders/mix_no.py
@@ -5,15 +5,13 @@ from locations.items import Feature
 from locations.storefinders.sylinder import SylinderSpider
 
 
-class DeliDeLucaNOSpider(SylinderSpider):
-    name = "deli_de_luca_no"
-    item_attributes = {"name": "Deli de Luca", "brand": "Deli de Luca", "brand_wikidata": "Q11965047"}
-    app_keys = ["1800"]
+class MixNOSpider(SylinderSpider):
+    name = "mix_no"
+    item_attributes = {"name": "Mix", "brand": "Mix", "brand_wikidata": "Q56404240"}
+    app_keys = ["1410"]
     warn_if_no_base_url = False
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
-        item["branch"] = item.pop("name").removeprefix("Deli de Luca ")
-
+        item["branch"] = item.pop("name").removeprefix("MIX ")
         apply_category(Categories.SHOP_CONVENIENCE, item)
-
         yield item
--- locations/spiders/snarkjop_no.py
+++ locations/spiders/snarkjop_no.py
@@ -5,15 +5,13 @@ from locations.items import Feature
 from locations.storefinders.sylinder import SylinderSpider
 
 
-class DeliDeLucaNOSpider(SylinderSpider):
-    name = "deli_de_luca_no"
-    item_attributes = {"name": "Deli de Luca", "brand": "Deli de Luca", "brand_wikidata": "Q11965047"}
-    app_keys = ["1800"]
+class SnarkjopNOSpider(SylinderSpider):
+    name = "snarkjop_no"
+    item_attributes = {"name": "Snarkjøp", "brand": "Snarkjøp"}
+    app_keys = ["4150"]
     warn_if_no_base_url = False
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
-        item["branch"] = item.pop("name").removeprefix("Deli de Luca ")
-
+        item["branch"] = item.pop("name").removeprefix("Snarkjøp ").removesuffix(" DV")
         apply_category(Categories.SHOP_CONVENIENCE, item)
-
         yield item
