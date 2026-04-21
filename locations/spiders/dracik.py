5a8ba206ad4ca5597265f937eafc5ebe30156c96
IceBurger <sharma.anurag1995@gmail.com>
2026-04-21 12:31:09 +0530

    fix spider

--- locations/spiders/dracik.py
+++ locations/spiders/dracik.py
@@ -1,97 +1,89 @@
 import re
-from urllib.parse import parse_qs, urlsplit
+from urllib.parse import urljoin
 
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Request, Spider
 
 from locations.categories import Categories, Extras, PaymentMethods, apply_category, apply_yes_no
-from locations.google_url import extract_google_position
 from locations.hours import DAYS_CZ, DAYS_SK, OpeningHours
 from locations.items import Feature
 from locations.structured_data_spider import extract_phone
 
 
-class DracikSpider(CrawlSpider):
+class DracikSpider(Spider):
     name = "dracik"
+    item_attributes = {"brand": "Dráčik", "brand_wikidata": "Q57653669"}
     allowed_domains = ["www.dracik.cz", "www.dracik.sk"]
     start_urls = [
         "https://www.dracik.cz/mapa-prodejen/",
         "https://www.dracik.sk/mapa-predajni/",
     ]
-    rules = [
-        Rule(
-            LinkExtractor(tags=("button",), attrs=("data-modal-href",), allow=r"/branches/branchDetailPopup"),
-            callback="parse",
-        )
-    ]
-    item_attributes = {"brand": "Dráčik", "brand_wikidata": "Q57653669"}
 
     def parse(self, response):
-        def get_query_param(link, query_param):
-            parsed_link = urlsplit(link)
-            queries = parse_qs(parsed_link.query)
-            return queries.get(query_param, [])
+        country = "CZ" if "dracik.cz" in response.url else "SK"
 
-        id = get_query_param(response.url, "branchId")[0]
-        country = response.url.split("/")[2].split(".")[-1].upper()
-        detail = response.xpath("//*[@id='storeModal']")
+        for branch in response.xpath("//article[@class='BranchCard']"):
+            item = Feature()
+            item["lat"] = branch.xpath("@data-map-lat").get()
+            item["lon"] = branch.xpath("@data-map-lng").get()
 
-        item = Feature()
-        item["ref"] = id
-        item["website"] = response.url
-        item["name"] = detail.xpath("normalize-space(//h3/text())").get()
-        extract_phone(item, detail)
-        extract_google_position(item, detail)
+            full_name = branch.xpath("normalize-space(.//h3/text())").get()
+            item["name"] = full_name
+            item["branch"] = full_name.split(" - ", 1)[1] if " - " in full_name else full_name
+            item["website"] = response.url
+            item["country"] = country
 
-        # Example:
-        #   City Arena
-        #   Kollárová 20
-        #   917 01 Trnava
-        #   City Arena
-        addr_lines = detail.xpath(
-            "//div[@class='StoreModal-address']/div[@class='StoreModal-text']/p/text()[normalize-space()]"
-        ).getall()
-        item["street_address"] = addr_lines[1].strip()
-        item["postcode"] = re.findall(r"[0-9]{3}\s?[0-9]{2}", addr_lines[2].strip())[0]
-        item["city"] = addr_lines[2].replace(item["postcode"], "").strip()
-        item["country"] = country
+            addr_parts = [
+                p.strip() for p in branch.xpath(".//div[@class='BranchCard-text']//p/text()").getall() if p.strip()
+            ]
+            if addr_parts:
+                item["street_address"] = addr_parts[-2] if len(addr_parts) > 1 else addr_parts[0]
 
-        # Platba kartou: áno
-        card_accepted = (
-            detail.xpath(
-                "//li[span[contains(text(), 'kartou')]]/span[@class='StoreModalDetails-value']/text()[normalize-space()]"
+                if pc_match := re.search(r"(\d{3}\s?\d{2})", addr_parts[-1]):
+                    item["postcode"] = pc_match.group(1)
+                    item["city"] = addr_parts[-1].replace(pc_match.group(1), "").strip(", ")
+                else:
+                    item["city"] = addr_parts[-1]
+
+            if modal_path := branch.xpath(".//button/@data-modal-href").get():
+                item["ref"] = re.search(r"branchId=(\d+)", modal_path).group(1)
+                yield Request(
+                    url=urljoin(response.url, modal_path),
+                    callback=self.parse_modal,
+                    meta={"item": item, "country": country},
+                )
+
+    def parse_modal(self, response):
+        item = response.meta["item"]
+        extract_phone(item, response)
+
+        oh = OpeningHours()
+        for row in response.xpath(".//div[contains(@class, 'time')]//tr | .//table//tr"):
+            if (day := row.xpath("normalize-space(.//th/text())").get()) and (
+                times := row.xpath("normalize-space(.//td/text())").get()
+            ):
+                oh.add_ranges_from_string(f"{day} {times}", DAYS_CZ if response.meta["country"] == "CZ" else DAYS_SK)
+        item["opening_hours"] = oh
+
+        card_val = (
+            response.xpath(
+                "normalize-space(.//li[span[contains(text(), 'kartou')]]/span[@class='StoreModalDetails-value']/text())"
             )
-            .get()
-            .strip()
+            .get("")
+            .lower()
         )
-        if card_accepted == "ano" or card_accepted == "áno" or card_accepted == "ne":
-            apply_yes_no(PaymentMethods.DEBIT_CARDS, item, card_accepted == "ano" or card_accepted == "áno", False)
-            apply_yes_no(PaymentMethods.CREDIT_CARDS, item, card_accepted == "ano" or card_accepted == "áno", False)
+        if card_val in ["ano", "áno", "ne"]:
+            apply_yes_no(PaymentMethods.DEBIT_CARDS, item, card_val in ["ano", "áno"])
+            apply_yes_no(PaymentMethods.CREDIT_CARDS, item, card_val in ["ano", "áno"])
 
-        # Bezbariérový prístup: áno
-        wheelchair = (
-            detail.xpath(
-                "//li[span[contains(text(),'Bezbariérový')]]/span[@class='StoreModalDetails-value']/text()[normalize-space()]"
+        wheel_val = (
+            response.xpath(
+                "normalize-space(.//li[span[contains(text(), 'Bezbariérový')]]/span[@class='StoreModalDetails-value']/text())"
             )
-            .get()
-            .strip()
+            .get("")
+            .lower()
         )
-        if wheelchair == "ano" or wheelchair == "áno" or wheelchair == "ne":
-            apply_yes_no(Extras.WHEELCHAIR, item, wheelchair == "ano" or wheelchair == "áno", False)
-
-        if "CZ" == country:
-            days = DAYS_CZ
-        elif "SK" == country:
-            days = DAYS_SK
-
-        if days is not None:
-            oh = OpeningHours()
-            for row in response.xpath("//*[@class='StoreModal-time']//tr"):
-                day = row.xpath("normalize-space(th/text())").get()
-                hrs = row.xpath("normalize-space(td/text())").get()
-                oh.add_ranges_from_string(day + " " + hrs, days)
-            item["opening_hours"] = oh
+        if wheel_val in ["ano", "áno", "ne"]:
+            apply_yes_no(Extras.WHEELCHAIR, item, wheel_val in ["ano", "áno"])
 
         apply_category(Categories.SHOP_TOYS, item)
-
         yield item
