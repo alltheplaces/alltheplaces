c0358c8678e82d775df16c0a142e1a86322bfb83
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-04-23 13:50:16 +0530

    code updated to fix spider

--- locations/spiders/bos_coffee_ph.py
+++ locations/spiders/bos_coffee_ph.py
@@ -1,15 +1,30 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.storefinders.metizsoft import MetizsoftSpider
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class BosCoffeePHSpider(MetizsoftSpider):
+class BosCoffeePHSpider(JSONBlobSpider):
     name = "bos_coffee_ph"
     item_attributes = {
         "brand_wikidata": "Q30591352",
         "brand": "Bo's Coffee",
     }
-    shopify_url = "bos-coffee.myshopify.com"
+    start_urls = ["https://storelocator.metizapps.com/v2/api/front/store-locator/?shop=bos-coffee.myshopify.com"]
+    locations_key = "stores"
 
-    def parse_item(self, item: Feature, location: dict):
-        item.pop("website")
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        if website := item.get("website"):
+            if "https" not in website:
+                item["website"] = f"https://{website}" if "http" not in website else website
+        else:
+            item["website"] = "https://www.boscoffee.com/"
+        oh = OpeningHours()
+        oh.add_ranges_from_string(feature.get("hour_of_operation"))
+        item["opening_hours"] = oh
+
         yield item
