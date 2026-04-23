0573634da44f3186ee386c4507496b918661f8ec
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-04-23 11:09:47 +0530

    Fixes attribute error

--- locations/spiders/suzuki_fr.py
+++ locations/spiders/suzuki_fr.py
@@ -14,16 +14,19 @@ class SuzukiFRSpider(SitemapSpider):
     sitemap_rules = [("/show/", "parse")]
 
     def parse(self, response, **kwargs):
-        item = Feature()
-        item["lat"] = response.xpath(r'//div[@id = "store-locator-map"]/@data-store-lat').get()
-        item["lon"] = response.xpath(r'//div[@id = "store-locator-map"]/@data-store-lng').get()
-        item["name"] = response.xpath(r"//title/text()").get().replace("|", "-")
-        item["street_address"] = clean_address(response.xpath(r'//*[@class = "uppercase"]/text()').get().strip())
-        item["addr_full"] = (
-            item["street_address"] + ", " + clean_address(response.xpath(r'//*[@class = "uppercase"]/text()[2]').get())
-        )
-        if phone := response.xpath(r'//*[contains(@href,"tel:")]/text()').get():
-            item["phone"] = phone.replace(".", "")
-        item["ref"] = item["website"] = response.url
-        apply_category(Categories.SHOP_CAR, item)
-        yield item
+        if data := response.xpath(r'//div[@id = "store-locator-map"]'):
+            item = Feature()
+            item["lat"] = data.xpath(".//@data-store-lat").get()
+            item["lon"] = data.xpath(".//@data-store-lng").get()
+            item["name"] = response.xpath("//title/text()").get().replace("|", "-")
+            item["street_address"] = clean_address(response.xpath(r'//*[@class = "uppercase"]/text()').get().strip())
+            item["addr_full"] = (
+                item["street_address"]
+                + ", "
+                + clean_address(response.xpath(r'//*[@class = "uppercase"]/text()[2]').get())
+            )
+            if phone := response.xpath(r'//*[contains(@href,"tel:")]/text()').get():
+                item["phone"] = phone.replace(".", "")
+            item["ref"] = item["website"] = response.url
+            apply_category(Categories.SHOP_CAR, item)
+            yield item
