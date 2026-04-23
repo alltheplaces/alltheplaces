d128d2117d1bbdc66a1aecdcf87284a1f96c4781
Cj Malone <CjMalone@mail.com>
2026-04-23 08:07:57 +0100

    Shrink diff

--- locations/spiders/suzuki_fr.py
+++ locations/spiders/suzuki_fr.py
@@ -8,25 +8,22 @@ from locations.pipelines.address_clean_up import clean_address
 class SuzukiFRSpider(SitemapSpider):
     name = "suzuki_fr"
     item_attributes = {"brand": "Suzuki", "brand_wikidata": "Q181642"}
-    sitemap_urls = [
-        "https://www.suzuki.fr/sitemap.xml",
-    ]
+    sitemap_urls = ["https://www.suzuki.fr/sitemap.xml"]
     sitemap_rules = [("/show/", "parse")]
 
     def parse(self, response, **kwargs):
-        if data := response.xpath(r'//div[@id = "store-locator-map"]'):
-            item = Feature()
-            item["lat"] = data.xpath(".//@data-store-lat").get()
-            item["lon"] = data.xpath(".//@data-store-lng").get()
-            item["name"] = response.xpath("//title/text()").get().replace("|", "-")
-            item["street_address"] = clean_address(response.xpath(r'//*[@class = "uppercase"]/text()').get().strip())
-            item["addr_full"] = (
-                item["street_address"]
-                + ", "
-                + clean_address(response.xpath(r'//*[@class = "uppercase"]/text()[2]').get())
-            )
-            if phone := response.xpath(r'//*[contains(@href,"tel:")]/text()').get():
-                item["phone"] = phone.replace(".", "")
-            item["ref"] = item["website"] = response.url
-            apply_category(Categories.SHOP_CAR, item)
-            yield item
+        if not response.xpath('//div[@id = "store-locator-map"]'):
+            return
+        item = Feature()
+        item["lat"] = response.xpath(r'//div[@id = "store-locator-map"]/@data-store-lat').get()
+        item["lon"] = response.xpath(r'//div[@id = "store-locator-map"]/@data-store-lng').get()
+        item["name"] = response.xpath(r"//title/text()").get().replace("|", "-")
+        item["street_address"] = clean_address(response.xpath(r'//*[@class = "uppercase"]/text()').get().strip())
+        item["addr_full"] = (
+            item["street_address"] + ", " + clean_address(response.xpath(r'//*[@class = "uppercase"]/text()[2]').get())
+        )
+        if phone := response.xpath(r'//*[contains(@href,"tel:")]/text()').get():
+            item["phone"] = phone.replace(".", "")
+        item["ref"] = item["website"] = response.url
+        apply_category(Categories.SHOP_CAR, item)
+        yield item
