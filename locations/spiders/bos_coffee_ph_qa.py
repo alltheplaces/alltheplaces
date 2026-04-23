be025bafd95165e7ccca6a6f0761b63627a11d51
Cj Malone <CjMalone@mail.com>
2026-04-23 13:10:04 +0100

    Clean up website

--- locations/spiders/bos_coffee_ph_qa.py
+++ locations/spiders/bos_coffee_ph_qa.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 from scrapy.http import TextResponse
 
+from locations.categories import apply_category, Categories
 from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
@@ -9,20 +10,18 @@ from locations.json_blob_spider import JSONBlobSpider
 
 class BosCoffeePHQASpider(JSONBlobSpider):
     name = "bos_coffee_ph_qa"
-    item_attributes = {
-        "brand_wikidata": "Q30591352",
-        "brand": "Bo's Coffee",
-    }
+    item_attributes = {"brand": "Bo's Coffee", "brand_wikidata": "Q30591352"}
     start_urls = ["https://storelocator.metizapps.com/v2/api/front/store-locator/?shop=bos-coffee.myshopify.com"]
     locations_key = "stores"
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
         if website := item.get("website"):
-            if "https" not in website:
-                item["website"] = f"https://{website}" if "http" not in website else website
-        else:
-            item["website"] = "https://www.boscoffee.com/"
+            if not website.startswith("http"):
+                item["website"] = f"https://{website}"
+
+        apply_category(Categories.CAFE, item)
+
         oh = OpeningHours()
         oh.add_ranges_from_string(feature.get("hour_of_operation"))
         item["opening_hours"] = oh
