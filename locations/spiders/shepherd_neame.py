28246f8289a9eff8adeae6ecbc12af58b61b6e41
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-04-24 14:17:05 +0200

    [Shepherd Neame] Add spider

---
+++ locations/spiders/shepherd_neame.py
@@ -0,0 +1,16 @@
+from typing import Iterable
+
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.items import Feature
+from locations.storefinders.yext_answers import YextAnswersSpider
+
+
+class ShepherdNeameSpider(YextAnswersSpider):
+    name = "shepherd_neame"
+    item_attributes = {"brand": "Shepherd Neame", "brand_wikidata": "Q12002324"}
+    endpoint = "https://prod-cdn.eu.yextapis.com/v2/accounts/me/search/vertical/query"
+    experience_key = "search-copy"
+    api_key = "b81096dc4941844b0b98616f9a715358"
+    locale = "en-GB"
+    v = "20220511"
+    feature_type = "restaurants"
