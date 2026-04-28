c88a29c2ae2a42e576f338c6779e1481b2a4c658
IceBurger <79398013+sharmaanurag95@users.noreply.github.com>
2026-04-28 15:10:52 +0530

    [Burger King UY] Fix spider (#16077)
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>

--- locations/spiders/burger_king_uy.py
+++ locations/spiders/burger_king_uy.py
@@ -1,8 +1,28 @@
-from locations.spiders.burger_king_pr import BurgerKingPRSpider
+from scrapy import Spider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 
 
-class BurgerKingUYSpider(BurgerKingPRSpider):
+class BurgerKingUYSpider(Spider):
     name = "burger_king_uy"
-    start_urls = ["https://api-lac.menu.app/api/directory/search"]
-    request_headers = {"application": "1da186d3677c7de1eed5906ac4fc2b25"}
-    website_root = "https://www.burgerking.com.uy/restaurantes/"
+    item_attributes = {"brand": "Burger King", "brand_wikidata": "Q177054"}
+    start_urls = ["https://www.burgerking.com.uy/restaurantes/"]
+
+    def parse(self, response):
+        for location in response.xpath("//div[@data-lat]"):
+            raw_name = location.xpath(".//h5/text()").get("").strip()
+
+            item = {
+                # ref: name used because site lacks IDs and contains overlapping lat/lon pairs
+                "ref": f"{raw_name}",
+                "branch": raw_name.replace("SHOPP.", "").strip(),
+                "lat": location.attrib.get("data-lat"),
+                "lon": location.attrib.get("data-lon"),
+                "addr_full": location.xpath('.//div[@class="title-rest"]/p/text()').get().strip(),
+                "phone": location.xpath('.//div[contains(@class, "phone-rest")]//text()').getall()[1].strip(),
+            }
+
+            apply_category(Categories.FAST_FOOD, item)
+
+            yield Feature(**item)
