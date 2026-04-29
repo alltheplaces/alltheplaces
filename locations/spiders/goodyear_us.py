ce0081fecaa6b38c24b988c9d09a3a13d8266b8c
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-04-29 14:38:22 +0100

    regex string

--- locations/spiders/goodyear_us.py
+++ locations/spiders/goodyear_us.py
@@ -7,7 +7,7 @@ class GoodyearUSSpider(SitemapSpider, StructuredDataSpider):
     name = "goodyear_us"
     item_attributes = {"brand": "Goodyear", "brand_wikidata": "Q620875"}
     sitemap_urls = ["https://www.goodyear.com/en-us/retail-locations-sitemap.xml"]
-    sitemap_rules = [("https://www.goodyear.com/en-us/shops/[^/]+/[^/]+/Goodyear-.+$", "parse_sd")]
+    sitemap_rules = [(r"^https://www.goodyear.com/en-us/shops/[^/]+/[^/]+/Goodyear-.+$", "parse_sd")]
 
     def post_process_item(self, item, response, ld_data):
         item["branch"] = item.pop("name").replace("Goodyear Auto Service - ", "")
