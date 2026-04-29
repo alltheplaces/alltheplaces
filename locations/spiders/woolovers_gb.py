c16e4079ba07d5b85a9356f091d2654bb15b9d77
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-04-29 16:09:59 +0200

    [Woolovers GB] Add spider
    Not sure whether you want this as there are only 9 stores?

---
+++ locations/spiders/woolovers_gb.py
@@ -0,0 +1,29 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.hours import OpeningHours, DAYS_FULL
+
+
+class WooloversGBSpider(JSONBlobSpider):
+    name = "woolovers_gb"
+    item_attributes = {"name": "WoolOvers", "brand": "WoolOvers","brand_wikidata": "Q139586777"}
+    start_urls = ["https://api.storepoint.co/v1/1678f814fa50d1/locations?rq"]
+    locations_key = ["results","locations"]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["addr_full"] = item.pop("street_address")
+        item["website"] = "https://" + item["website"]
+        item["lat"],item["lon"] = feature["loc_lat"],feature["loc_long"]
+        item["branch"] = item.pop("name")
+        oh = OpeningHours()
+        for day in DAYS_FULL:
+            if "-" in feature[day.lower()]:
+                open,closed = feature[day.lower()].replace(".",":").split(" - ")
+                oh.add_range(day, open, closed)
+        item["opening_hours"] = oh
+        yield item
