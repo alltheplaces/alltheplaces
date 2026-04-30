c8dd391ce3a0e2fb55967777322e8d5a72211f57
Cj Malone <CjMalone@mail.com>
2026-04-30 09:34:26 +0100

    Clean name

--- locations/spiders/coach_us.py
+++ locations/spiders/coach_us.py
@@ -1,5 +1,10 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import apply_category, Categories
+from locations.items import Feature
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
 from locations.user_agents import BROWSER_DEFAULT
@@ -12,3 +17,8 @@ class CoachUSSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [(r"https://www.coach.com/stores/[^/]+/[^/]+/[^/]+$", "parse_sd")]
     is_playwright_spider = True
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("About ")
+        apply_category(Categories.SHOP_BAG, item)
+        yield item
