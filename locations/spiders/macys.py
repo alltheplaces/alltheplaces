e9dc94fe9c8061d7b079df2772180c036d8faf29
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-04-30 10:21:06 +0530

    code refactored to fix spider

--- locations/spiders/macys.py
+++ locations/spiders/macys.py
@@ -2,13 +2,12 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.structured_data_spider import StructuredDataSpider
-from locations.user_agents import BROWSER_DEFAULT
 
 
 class MacysSpider(CrawlSpider, StructuredDataSpider):
     name = "macys"
     item_attributes = {"brand": "Macy's", "brand_wikidata": "Q629269"}
-    allowed_domains = ["macys.com"]
+    allowed_domains = ["www.macys.com"]
     start_urls = ["https://www.macys.com/stores/browse/"]
     rules = [
         Rule(LinkExtractor(r"/stores/\w\w/$")),
@@ -16,9 +15,33 @@ class MacysSpider(CrawlSpider, StructuredDataSpider):
         Rule(LinkExtractor(r"/stores/\w\w/[^/]+/[^/]+\_(\d+).html$"), "parse"),
     ]
     wanted_types = ["Store"]
-    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
-    requires_proxy = True
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+        "DEFAULT_REQUEST_HEADERS": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "referer": "https://www.macys.com/stores/ca/",
+            "sec-ch-ua": '"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+        },
+    }
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["name"] = None
         yield item
+
+    # start_urls = ["https://www.macys.com/stores/browse/"]
+    # rules = [
+    #     Rule(LinkExtractor(r"/stores/\w\w/$")),
+    #     Rule(LinkExtractor(r"/stores/\w\w/[^/]+/$")),
+    #     Rule(LinkExtractor(r"/stores/\w\w/[^/]+/[^/]+\_(\d+).html$"), "parse"),
+    # ]
+    # wanted_types = ["Store"]
+    # custom_settings = {"USER_AGENT": BROWSER_DEFAULT,"ROBOTSTXT_OBEY":False}
+    # # requires_proxy = True
+    #
+    # def post_process_item(self, item, response, ld_data, **kwargs):
+    #     item["name"] = None
+    #     yield item
