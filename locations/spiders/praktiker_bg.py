8ed6b34e4aa4dcb768a3d56f997fe2018f41ba68
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-05-03 13:49:13 +0300

    fix praktiker_bg spider

--- locations/spiders/praktiker_bg.py
+++ locations/spiders/praktiker_bg.py
@@ -1,8 +1,17 @@
-from locations.spiders.technopolis_bg import TechnopolisBGSpider
+from typing import Any
+
+from scrapy.http import JsonRequest
+
+from locations.spiders.technopolis_bg import AsyncIterator, TechnopolisBGSpider
 
 
 class PraktikerBGSpider(TechnopolisBGSpider):
     name = "praktiker_bg"
     item_attributes = {"brand": "Praktiker", "brand_wikidata": "Q110399491"}
     allowed_domains = ["praktiker.bg"]
-    start_urls = ["https://api.praktiker.bg/videoluxcommercewebservices/v2/praktiker/mapbox/customerpreferedstore"]
+
+    async def start(self) -> AsyncIterator[Any]:
+        yield JsonRequest(
+            "https://api.praktiker.bg/videoluxcommercewebservices/v2/praktiker/mapbox/customerpreferedstore",
+            headers={"Origin": "https://www.technopolis.bg"},
+        )
