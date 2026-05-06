75318fa69d0dac3884555e57e326c2a4e1ae1df2
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2026-05-06 12:54:15 +0000

    Fix Banco Del Bienestar MX Spider

--- locations/spiders/banco_del_bienestar_mx.py
+++ locations/spiders/banco_del_bienestar_mx.py
@@ -1,14 +1,12 @@
 import json
 from typing import Any
 
-import scrapy
 from scrapy import FormRequest, Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.google_url import url_to_coords
 from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address, merge_address_lines
 
 
 class BancoDelBienestarMXSpider(Spider):
@@ -28,20 +26,24 @@ class BancoDelBienestarMXSpider(Spider):
                 url="https://directoriodesucursales.bancodelbienestar.gob.mx/Home/ListarMunalc",
                 formdata={"idEnt": str(state["id_entidad"])},
                 callback=self.parse_state,
-                cb_kwargs={"state":  state["descripcion"]},
+                cb_kwargs={"state": state["descripcion"]},
             )
 
     def parse_state(self, response: Response, state: str) -> Any:
         for area in response.json()["response"]:
             yield FormRequest(
                 url="https://directoriodesucursales.bancodelbienestar.gob.mx/Busqueda",
-                formdata={"entidad": str(area["idEnt"]), "munalc": area["munalc"], "__RequestVerificationToken": self.token},
+                formdata={
+                    "entidad": str(area["idEnt"]),
+                    "munalc": area["munalc"],
+                    "__RequestVerificationToken": self.token,
+                },
                 callback=self.parse_locations,
                 cb_kwargs={"state": state},
             )
 
     def parse_locations(self, response: Response, state: str) -> Any:
-        if locations_data:= response.xpath('//@data-sucursales').get():
+        if locations_data := response.xpath("//@data-sucursales").get():
             for location in json.loads(locations_data).get("response", []):
                 item = Feature()
                 item["state"] = state
