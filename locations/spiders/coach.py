6e9946855bc3b8fff75daeb396976531cdccc750
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-05-06 15:46:07 +0530

    fix spider

--- locations/spiders/coach.py
+++ locations/spiders/coach.py
@@ -1,34 +1,39 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
 
-from locations.items import SocialMedia, set_social_media
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class CoachSpider(CrawlSpider, StructuredDataSpider):
+class CoachSpider(SitemapSpider, StructuredDataSpider, PlaywrightSpider):
     name = "coach"
     item_attributes = {"brand": "Coach", "brand_wikidata": "Q727697"}
-    start_urls = [
-        "https://de.coach.com/stores/index.html",
-        "https://es.coach.com/stores/index.html",
-        "https://fr.coach.com/stores/index.html",
-        "https://it.coach.com/stores/index.html",
-        "https://uk.coach.com/stores/index.html",
+
+    sitemap_urls = [
+        "https://uk.coach.com/stores/sitemap.xml",
+        "https://de.coach.com/stores/sitemap.xml",
+        "https://fr.coach.com/stores/sitemap.xml",
+        "https://it.coach.com/stores/sitemap.xml",
+        "https://es.coach.com/stores/sitemap.xml",
     ]
-    rules = [
-        Rule(LinkExtractor(allow=r"/stores/[-\w]+\.html$")),
-        Rule(LinkExtractor(allow=r"/stores/[-\w]+/[-\w]+\.html$")),
-        Rule(LinkExtractor(allow=r"/stores/[-\w]+/[-\w]+/[-\w]+\.html$"), callback="parse_sd"),
-    ]
-    wanted_types = ["Organization"]
 
-    def post_process_item(self, item, response, ld_data, **kwargs):
-        if not ld_data.get("address", {}).get("streetAddress"):
-            return  # skip bad duplicate linked data
+    sitemap_rules = [(r"/stores/[^/]+/[^/]+/[^/]+(?:\.html)?/?$", "parse_sd")]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
 
-        if wa := response.xpath('//a[contains(@href, "https://wa.me/")]/@href').get():
-            set_social_media(item, SocialMedia.WHATSAPP, wa)
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs):
+        raw_name = item.pop("name", "")
+        prefixes_to_remove = ["About ", "Ã€ propos ", "À propos ", "Acerca de ", "Di ", "Um "]
 
-        item["website"] = response.url
+        for prefix in prefixes_to_remove:
+            if raw_name.startswith(prefix):
+                raw_name = raw_name.removeprefix(prefix).strip()
+                break
+
+        item["branch"] = raw_name
+        apply_category(Categories.SHOP_BAG, item)
 
         yield item
