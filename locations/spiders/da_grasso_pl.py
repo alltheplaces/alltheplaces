28339bdeab287b740d964104a749d1acaee22e26
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-05-07 21:03:39 +0530

    fix spider

--- locations/spiders/da_grasso_pl.py
+++ locations/spiders/da_grasso_pl.py
@@ -1,31 +1,24 @@
-from scrapy import Spider
-
-from locations.dict_parser import DictParser
-from locations.hours import OpeningHours, day_range
+from locations.hours import DAYS, OpeningHours
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class DaGrassoPLSpider(Spider):
+class DaGrassoPLSpider(JSONBlobSpider):
     name = "da_grasso_pl"
     item_attributes = {"brand": "Da Grasso", "brand_wikidata": "Q11692586"}
-    start_urls = ["https://www.dagrasso.pl/api/v1/restaurant_list"]
+    start_urls = ["https://api.dagrasso.pl/localization-api/api/v2/stores/pickup"]
 
-    def parse(self, response, **kwargs):
-        for feature in response.json():
-            if feature["isMenu"]:
-                continue
-            feature["street_address"] = feature["street"]
-            del feature["street"]
-            item = DictParser.parse(feature)
-            item["phone"] = ";".join(
-                [feature[key] for key in ["phone", "phone2", "phone3", "mobilePhone"] if feature[key]]
-            )
-            opening_hours = OpeningHours()
-            for hours in feature["workingHours"]:
-                opening_hours.add_days_range(
-                    days=day_range(hours["from"], hours["to"] or hours["from"]),
-                    open_time=hours["open"],
-                    close_time=hours["close"],
-                )
-            item["opening_hours"] = opening_hours
-            item.pop("name", None)
-            yield item
+    def post_process_item(self, item, response, feature):
+        if raw_name := item.pop("name", ""):
+            item["branch"] = raw_name.replace("aaa.", "").strip()
+
+        self.parse_opening_hours(item, feature)
+        yield item
+
+    def parse_opening_hours(self, item, feature):
+        opening_times = feature.get("openingTimes", [])
+        closing_times = feature.get("closingTimes", [])
+
+        if opening_times and closing_times:
+            oh = OpeningHours()
+            oh.add_days_range(DAYS, opening_times[0].split("T")[1][:5], closing_times[0].split("T")[1][:5])
+            item["opening_hours"] = oh
