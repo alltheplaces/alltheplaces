fe8750e0c89887cbe3a22e8af6ac7d584b8ede0c
Cj Malone <CjMalone@mail.com>
2026-05-12 10:00:06 +0100

    Collect name

--- locations/spiders/mitsubishi_uy.py
+++ locations/spiders/mitsubishi_uy.py
@@ -16,14 +16,10 @@ class MitsubishiUYSpider(Spider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for shop_node in response.css("a.link-ubicacion"):
             item = Feature()
-            name = shop_node.xpath('.//*[@fs-list-field="nombre"]/text()').get("").strip()
+            item["ref"] = item["name"] = shop_node.xpath('.//*[@fs-list-field="nombre"]/text()').get("").strip()
             item["street_address"] = shop_node.xpath('.//*[@fs-list-field="direccion"]/text()').get()
             item["phone"] = shop_node.xpath('.//*[@fs-list-field="telefono"]/text()').get()
             item["email"] = shop_node.xpath('.//*[@fs-list-field="mail"]/text()').get()
-
-            dept = shop_node.xpath('.//*[@fs-list-field="departamento"]/text()').get(default="").strip()
-            # Combine name and department for a unique ref and descriptive branch
-            item["ref"] = item["branch"] = f"{name} - {dept}" if dept else name
 
             if map_url := shop_node.xpath("./@data-map").get():
                 item["lat"], item["lon"] = url_to_coords(map_url)
