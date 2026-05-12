a4d25f065fd334db34786f5ac5a96c92d2935f86
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-05-12 12:59:50 +0530

    fix spider

--- locations/spiders/mitsubishi_uy.py
+++ locations/spiders/mitsubishi_uy.py
@@ -1,29 +1,33 @@
-import json
-import re
-from typing import Any
-
-import scrapy
-from scrapy.http import Response
+from scrapy import Spider
+from scrapy.selector import Selector
 
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
+from locations.google_url import extract_google_position
+from locations.items import Feature
 
 
-class MitsubishiUYSpider(scrapy.Spider):
+class MitsubishiUYSpider(Spider):
     name = "mitsubishi_uy"
     item_attributes = {
         "brand": "Mitsubishi",
         "brand_wikidata": "Q36033",
     }
-    start_urls = ["https://www.mitsubishi-motors.com.uy/Ventas/Concesionarios"]
+    start_urls = ["https://www.mitsubishi-motors.com.uy/concesionarios"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in json.loads(
-            re.search(
-                r"\((\[.*\])\);", response.xpath('//*[@id="form1"]//*[contains(text(),"InitMap")]/text()').get()
-            ).group(1)
-        ):
-            item = DictParser.parse(location)
-            item["branch"] = item.pop("name")
+    def parse(self, response):
+        for shop_node in response.css("a.link-ubicacion"):
+            item = Feature()
+            name = shop_node.xpath('.//*[@fs-list-field="nombre"]/text()').get("").strip()
+            item["street_address"] = shop_node.xpath('.//*[@fs-list-field="direccion"]/text()').get()
+            item["phone"] = shop_node.xpath('.//*[@fs-list-field="telefono"]/text()').get()
+            item["email"] = shop_node.xpath('.//*[@fs-list-field="mail"]/text()').get()
+
+            dept = shop_node.xpath('.//*[@fs-list-field="departamento"]/text()').get(default="").strip()
+            # Combine name and department for a unique ref and descriptive branch
+            item["ref"] = item["branch"] = f"{name} - {dept}" if dept else name
+
+            if map_url := shop_node.xpath("./@data-map").get():
+                extract_google_position(item, Selector(text=f'<a href="{map_url}"></a>'))
+
             apply_category(Categories.SHOP_CAR, item)
             yield item
