55ac09318e43d4504766e6f8d1870832f21a4f18
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-05-13 17:09:55 +0530

    added user agent to fix spider

--- locations/spiders/greene_king_pubs_gb.py
+++ locations/spiders/greene_king_pubs_gb.py
@@ -1,7 +1,12 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class GreeneKingPubsGBSpider(SitemapSpider, StructuredDataSpider):
@@ -9,8 +14,11 @@ class GreeneKingPubsGBSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {
         "brand": "Greene King",
         "brand_wikidata": "Q5564162",
-        "extras": Categories.PUB.value,
     }
     sitemap_urls = ["https://www.greeneking.co.uk/sitemap.xml"]
     sitemap_rules = [(r"\/pubs\/([-\w]+)\/([-\w]+)\/?$", "parse_sd")]
-    custom_settings = {"REDIRECT_ENABLED": False}
+    custom_settings = {"REDIRECT_ENABLED": False, "USER_AGENT": BROWSER_DEFAULT}
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        apply_category(Categories.PUB, item)
+        yield item
