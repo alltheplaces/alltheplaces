96fe9b0e908cdc3e74b33f415d1e60241a8a7406
Cj Malone <CjMalone@mail.com>
2026-05-13 10:57:30 +0100

    Restore ref

--- locations/spiders/consum_es.py
+++ locations/spiders/consum_es.py
@@ -12,7 +12,7 @@ class ConsumESSpider(SitemapSpider, StructuredDataSpider):
     name = "consum_es"
     item_attributes = {"brand": "Consum", "brand_wikidata": "Q8350308"}
     sitemap_urls = ["https://www.consum.es/robots.txt"]
-    sitemap_rules = [(r"/supermercados/", "parse")]
+    sitemap_rules = [(r"/supermercados/([^/]+)/$", "parse")]
     wanted_types = ["Store"]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
