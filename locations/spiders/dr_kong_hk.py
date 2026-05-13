f4f35840518c1a841de5abf33b0cc8e76bb76adb
Cj Malone <CjMalone@mail.com>
2026-05-13 11:05:23 +0100

    apply_category

--- locations/spiders/dr_kong_hk.py
+++ locations/spiders/dr_kong_hk.py
@@ -2,13 +2,14 @@ from typing import Iterable
 
 from scrapy.http import TextResponse
 
+from locations.categories import apply_category, Categories
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
 class DrKongHKSpider(JSONBlobSpider):
     name = "dr_kong_hk"
-    item_attributes = {"brand_wikidata": "Q116547631", "brand": "Dr. Kong"}
+    item_attributes = {"brand": "Dr. Kong", "brand_wikidata": "Q116547631"}
     start_urls = ["https://webapi.91app.hk/webapi/LocationV2/GetLocationList?shopId=101&lang=en-HK"]
     locations_key = ["Data", "List"]
     custom_settings = {"ROBOTSTXT_OBEY": False}
@@ -16,4 +17,7 @@ class DrKongHKSpider(JSONBlobSpider):
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
         item["website"] = f"https://www.dr-kong.com.hk/Shop/StoreDetail/101/{item['ref']}"
+
+        apply_category(Categories.SHOP_SHOES, item)
+
         yield item
