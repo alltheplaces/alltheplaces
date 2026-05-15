56348347d36fc6175a110cede6e753bf6eaa3554
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-05-15 22:51:46 +0200

    [Boots] Try different spider

---
+++ locations/spiders/boots.py
@@ -0,0 +1,60 @@
+import scrapy
+from typing import AsyncIterator, Iterable
+import chompjs
+import json
+
+from scrapy.http import JsonRequest, Request, TextResponse
+from locations.pipelines.address_clean_up import merge_address_lines
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours, sanitise_day
+from locations.items import Feature
+from locations.geo import city_locations
+from locations.json_blob_spider import JSONBlobSpider
+from locations.dict_parser import DictParser
+
+class BootsSpider(JSONBlobSpider):
+    name = "boots"
+    item_attributes = {"brand": "Boots", "brand_wikidata": "Q6123139"}
+    locations_key = "searchResults"
+
+    async def start(self) -> AsyncIterator[JsonRequest]:
+        for city in city_locations("GB", 15000):
+            yield JsonRequest(
+                url=f'https://www.boots.com/AjaxStoreLocatorSearch?storeId=11352&storeAddressSearch_city={city["name"]}&requesttype=ajax',
+                method="GET",
+            )
+        for city in city_locations("IE", 15000):
+            yield JsonRequest(
+                url=f'https://www.boots.ie/AjaxStoreLocatorSearch?storeId=11352&storeAddressSearch_city={city["name"]}&requesttype=ajax',
+                method="GET",
+            )
+
+
+    def extract_json(self, response: TextResponse) -> dict | list[dict]:
+        data = response.text.replace("/*","").replace("*/","")
+        json_data = json.loads(data)
+        if self.locations_key:
+            if isinstance(self.locations_key, str):
+                json_data = json_data[self.locations_key]
+            elif isinstance(self.locations_key, list):
+                for key in self.locations_key:
+                    json_data = json_data[key]
+        return json.loads(json_data)
+
+    def pre_process_data(self, feature: dict) -> None:
+        feature["ref"] = feature["storeId"]
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        if item["name"].startswith("Opticians"):
+            item["brand"] = "Boots Opticians"
+            item["brand_wikidata"] = "Q4944037"
+            item["name"] = item["name"].replace("Opticians", "").strip("- ")
+            apply_category(Categories.SHOP_OPTICIAN, item)
+        else:
+            apply_category(Categories.PHARMACY, item)
+        item["branch"] = item.pop("name")
+        item["postcode"] = feature["storeAddPostcode"]
+        item["city"] = feature["storeAddCity"]
+        item["state"] = feature["storeAddCounty"]
+        item["street_address"] = merge_address_lines([feature["storeAddL1"], feature["storeAddL2"], feature["storeAddL3"]])
+        yield item
