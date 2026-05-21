01a85c31326a0b8a8fdc2520f8d641acc0d66bdc
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-05-21 13:59:34 +0530

    Add opening hours for Bags To Go AU Spider

--- locations/spiders/bags_to_go_au.py
+++ locations/spiders/bags_to_go_au.py
@@ -1,11 +1,12 @@
 from typing import Any
 from urllib.parse import urljoin
 
-from scrapy.http import Response
+from scrapy.http import Response, TextResponse
 from scrapy.spiders import Spider
 
 from locations.categories import Categories, apply_category
 from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
 
 
@@ -22,6 +23,15 @@ class BagsToGoAUSpider(Spider):
             item["branch"] = item["ref"].title().replace("-", " ")
             item["addr_full"] = response.xpath(f'//*[@data-title="{item["branch"]}"]/@data-address').get()
             item["website"] = urljoin("https://bagstogo.com.au/pages/", item["ref"])
+            item["opening_hours"] = self.parse_opening_hours(location)
             extract_google_position(item, location)
             apply_category(Categories.SHOP_BAG, item)
             yield item
+
+    def parse_opening_hours(self, location: TextResponse) -> OpeningHours:
+        opening_hours = OpeningHours()
+        for rule in location.xpath('.//*[contains(text(),"Opening hours")]/following-sibling::table/tr'):
+            opening_hours.add_ranges_from_string(
+                f"{rule.xpath('./td[1]/text()').get()}: {rule.xpath('./td[2]/text()').get()}"
+            )
+        return opening_hours
