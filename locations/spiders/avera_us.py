3e26baffcc815ed0b40c2f8326156d5ffe4b7ab0
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-05-21 15:20:57 +0530

    Use default API URL of YextAnswersSpider

--- locations/spiders/avera_us.py
+++ locations/spiders/avera_us.py
@@ -4,6 +4,5 @@ from locations.storefinders.yext_answers import YextAnswersSpider
 class AveraUSSpider(YextAnswersSpider):
     name = "avera_us"
     item_attributes = {"operator": "Avera Health", "operator_wikidata": "Q4828238"}
-    endpoint = "https://prod-cdn.us.yextapis.com/v2/accounts/me/search/vertical/query"
     experience_key = "locator"
     api_key = "413a34db5016c35c9f4a790833a03dd3"
