7927c7e3c4a796606dad1aa6cc8a3c9923498fae
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-05-22 14:05:39 +0530

    Try using proxy for Essex US Spider

--- locations/spiders/essex_us.py
+++ locations/spiders/essex_us.py
@@ -18,6 +18,7 @@ class EssexUSSpider(JSONBlobSpider, PlaywrightSpider):
         "operator_wikidata": "Q134703255",
     }
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
+    requires_proxy = True
 
     def extract_json(self, response: TextResponse) -> dict | list[dict]:
         return json.loads(response.xpath("//pre/text()").get())["communities"]
