b79c52937c636075c7876790518424038832b895
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-05-22 12:27:11 +0530

    Fix Essex US Spider

--- locations/spiders/essex_us.py
+++ locations/spiders/essex_us.py
@@ -1,26 +1,33 @@
-import re
+import json
+from typing import Iterable
+
+from scrapy.http import TextResponse
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
-class EssexUSSpider(JSONBlobSpider):
+class EssexUSSpider(JSONBlobSpider, PlaywrightSpider):
     name = "essex_us"
-    start_urls = ["https://www.essexapartmenthomes.com/EPT_Feature/Search/GetSearchResults"]
+    start_urls = ["https://www.essexapartmenthomes.com/api/properties/search?query="]
     item_attributes = {
         "operator": "Essex",
         "operator_wikidata": "Q134703255",
     }
-    locations_key = "communities"
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
-    def post_process_item(self, item, response, feature):
+    def extract_json(self, response: TextResponse) -> dict | list[dict]:
+        return json.loads(response.xpath("//pre/text()").get())["communities"]
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["image"] = feature["imagelist"].split("|")[0]
         item["street_address"] = item.pop("addr_full")
         item["lat"], item["lon"] = feature["coordinate"].split(",")
-        city_slug = re.sub(r"\W+", "-", feature["city"]).lower()
-        item["website"] = f"https://www.essexapartmenthomes.com/apartments/{city_slug}/{feature['itemname']}"
-
-        amenities = feature["communityamenities"].split("|")
+        item["website"] = response.urljoin(feature["communityurl"])
+        amenities = feature["communityamenities"]
         apply_yes_no(Extras.PETS_ALLOWED, item, "Pet friendly" in amenities)
         apply_yes_no(Extras.SWIMMING_POOL, item, "Swimming pool" in amenities)
 
