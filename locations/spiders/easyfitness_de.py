51452c9ad02359eea4da7828d1963f1d5bcae918
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-05-22 08:58:53 +0100

    Clean name

--- locations/spiders/easyfitness_de.py
+++ locations/spiders/easyfitness_de.py
@@ -12,20 +12,18 @@ class EasyfitnessDESpider(SitemapSpider, StructuredDataSpider):
     name = "easyfitness_de"
     item_attributes = {"brand": "EasyFitness", "brand_wikidata": "Q106166703"}
     sitemap_urls = ["https://easyfitness.club/robots.txt"]
-    sitemap_rules = [
-        (r"/studio/([^/]+)/?$", "parse_sd"),
-    ]
+    sitemap_rules = [(r"/studio/([^/]+)/?$", "parse_sd")]
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         # To prevent one location from AE
         if item.get("country") == "AE":
             return
 
+        item["image"] = None
+        item["branch"] = item.pop("name").removeprefix("EASYFITNESS ")
         item["ref"] = response.url.rstrip("/").split("/")[-1]
 
         if email_href := response.xpath('//a[contains(@href, "mailto:")]/@href').get():
             item["email"] = re.search(r"mailto:\s*([\w\.-]+@[\w\.-]+\.\w+)", email_href).group(1).strip()
-
-        item["image"] = None
 
         yield item
