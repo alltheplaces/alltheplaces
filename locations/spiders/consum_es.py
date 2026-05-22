6d36e23a1074177358a570e90ad605f2c2b86421
Cj Malone <CjMalone@mail.com>
2026-05-22 12:46:45 +0100

    Collect opening hours

--- locations/spiders/consum_es.py
+++ locations/spiders/consum_es.py
@@ -4,6 +4,7 @@ from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours, sanitise_day, DAYS_ES
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -36,6 +37,23 @@ class ConsumESSpider(SitemapSpider, StructuredDataSpider):
         item["extras"]["website:ca"] = response.xpath('//link[@rel="alternate"][@hreflang="ca"]/@href').get()
         item["extras"]["website:en"] = response.xpath('//link[@rel="alternate"][@hreflang="en"]/@href').get()
 
+        try:
+            item["opening_hours"] = self.parse_hours(response)
+        except Exception:
+            pass
+
         apply_category(Categories.SHOP_SUPERMARKET, item)
 
         yield item
+
+    def parse_hours(self, response: Response) -> OpeningHours:
+        oh = OpeningHours()
+        for rule in response.xpath('//p[@class="day"]/text()').getall():
+            day, times = rule.split(": ", 1)
+            day = sanitise_day(day, DAYS_ES)
+            if times == "Cerrado":
+                oh.set_closed(day)
+            else:
+                for t in times.split(" y "):
+                    oh.add_range(day, *t.strip().split("-"))
+        return oh
