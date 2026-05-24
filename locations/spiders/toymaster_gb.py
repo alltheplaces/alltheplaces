be462f13832760ecef8a1b42890ca40416c650a0
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-05-24 16:48:55 +0200

    Create toymaster_gb.py

---
+++ locations/spiders/toymaster_gb.py
@@ -0,0 +1,23 @@
+from typing import Iterable
+from scrapy.http import FormRequest, Request, Response
+
+from locations.pipelines.address_clean_up import merge_address_lines
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+class ToymasterGBSpider(JSONBlobSpider):
+    name = "toymaster_gb"
+    item_attributes = {"brand": "Toymaster", "brand_wikidata": "Q7830615"}
+    start_urls = ["https://api.toymaster.co.uk/stores?status=Active&storefinder=true&locale=en-GB&api_key=137601c9-ef9c-41e1-8f07-a4cde23f8c7c"]
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Request]:
+        item["street_address"] = merge_address_lines([feature.get("Address1"), feature.get("Address2"),feature.get("Address3"),feature.get("Address4")])
+        item["website"] = feature["Website"]
+        item["facebook"] = feature["SocialFacebook"]
+        item["twitter"] = feature["SocialTwitter"]
+        #item["instagram"] = feature["SocialInstagram"]
+        item["email"] = feature["StorefinderEmail"]
+        item["phone"] = feature["StorefinderPhone"]
+        item["ref"] = feature["MemberAccountNumber"]
+        #Could add opening hours
+        yield item
