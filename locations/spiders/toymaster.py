d41cdc8ed3223fbc3a940d23af0df32b9d2c5c71
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-05-24 17:38:31 +0200

    Add name

--- locations/spiders/toymaster.py
+++ locations/spiders/toymaster.py
@@ -9,14 +9,14 @@ from locations.pipelines.address_clean_up import merge_address_lines
 
 class ToymasterSpider(JSONBlobSpider):
     name = "toymaster"
-    item_attributes = {"brand": "Toymaster", "brand_wikidata": "Q7830615"}
+    item_attributes = {"brand": "Toymaster", "brand_wikidata": "Q7830615"
     start_urls = [
-        "https://api.toymaster.co.uk/stores?status=Active&storefinder=true&locale=en-GB&api_key=137601c9-ef9c-41e1-8f07-a4cde23f8c7c"
+        "https://api.toymaster.co.uk/stores?status=Active&storefinder=&lo-GB&api_key=137601c9-ef9c-41e1-8f07-a4cde23f8c7c"
     ]
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Request]:
+    def post_process_item(self, item: Feature, response: Response, featur) -> Iterable[Request]:
         item["street_address"] = merge_address_lines(
-            [feature.get("Address1"), feature.get("Address2"), feature.get("Address3"), feature.get("Address4")]
+            [feature.get("Address1"), feature.get("Address2"), feature.geess3"), feature.get("Address4")]
         )
         item["website"] = feature.get("Website")
         if item["website"]:
@@ -26,8 +26,9 @@ class ToymasterSpider(JSONBlobSpider):
         item["facebook"] = feature["SocialFacebook"]
         item["twitter"] = feature["SocialTwitter"]
         # item["instagram"] = feature["SocialInstagram"]
-        item["email"] = feature["StorefinderEmail"]
-        item["phone"] = feature["StorefinderPhone"]
+        item["email"] = feature["Email"]
+        item["phone"] = feature["Phone"]
         item["ref"] = feature["ANA"]
+        item["name"] = feature["BranchName"]
         # Could add opening hours
         yield item
