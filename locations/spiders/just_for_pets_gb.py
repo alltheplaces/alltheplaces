a53ef14d073f89e50eec991231b10667bcc171c4
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2026-05-25 21:47:54 +0200

    [Just for Pets GB] Add Branch

--- locations/spiders/just_for_pets_gb.py
+++ locations/spiders/just_for_pets_gb.py
@@ -4,5 +4,9 @@ from locations.storefinders.storemapper import StoremapperSpider
 
 class JustForPetsGBSpider(StoremapperSpider):
     name = "just_for_pets_gb"
-    item_attributes = {"brand": "Just For Pets", "extras": Categories.SHOP_PET.value}
+    item_attributes = {"brand": "Just For Pets", "name": "Just For Pets", "extras": Categories.SHOP_PET.value}
     company_id = "10020"
+
+    def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        yield item
