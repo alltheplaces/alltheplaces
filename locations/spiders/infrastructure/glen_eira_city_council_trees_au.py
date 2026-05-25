75e16e106fa72a6d0816271421a144c299754ecd
David Hicks <david@hicks.id.au>
2026-05-25 11:50:41 +1000

    [Glen Eira City Council AU] Replace trees spider (58k trees)
    Pozi GIS system appears more likely to be recent/updated than
    many-year-old data from data.gov.au. Use Pozi GIS system data instead.

--- locations/spiders/infrastructure/glen_eira_city_council_trees_au.py
+++ locations/spiders/infrastructure/glen_eira_city_council_trees_au.py
@@ -1,6 +1,7 @@
+import re
 from typing import Iterable
 
-from scrapy.http import Response
+from scrapy.http import TextResponse
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
@@ -10,19 +11,29 @@ from locations.json_blob_spider import JSONBlobSpider
 class GlenEiraCityCouncilTreesAUSpider(JSONBlobSpider):
     name = "glen_eira_city_council_trees_au"
     item_attributes = {"operator": "Glen Eira City Council", "operator_wikidata": "Q56477767", "state": "VIC"}
-    allowed_domains = ["data.gov.au"]
-    start_urls = [
-        "https://data.gov.au/data/dataset/ed15e3ea-48dc-47d2-afa6-518e6f5276e1/resource/85c2d44c-8ccf-4a32-9881-872f1a511ef7/download/streetandparktrees.json"
-    ]
+    allowed_domains = ["connect.pozi.com"]
+    start_urls = ["https://connect.pozi.com/userdata/gleneira-publisher/Council-Facilities-and-Services/Park_and_Street_Trees.json"]
     locations_key = "features"
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,  # Avoid HTTP 403 error
+        "DOWNLOAD_WARNSIZE": 67108864  # Data is >32MiB
+    }
 
     def pre_process_data(self, feature: dict) -> None:
         feature.update(feature.pop("properties"))
 
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        if "Alive" not in feature["Tree_Status"]:
+            return
         item["ref"] = feature["feature_id"]
+        if addr := feature.get("feature_location"):
+            if located_in := re.match(r"^([^\(]+)\(([^\)]+)\)$", addr):
+                item["addr_full"] = located_in.group(1)
+                item["located_in"] = located_in.group(2)
+            else:
+                item["addr_full"] = addr
         apply_category(Categories.NATURAL_TREE, item)
+        item["extras"]["alt_ref"] = feature["central_asset_id"]
         if species := feature.get("Botanical"):
             item["extras"]["species"] = species
         if common_name := feature.get("Common_Name"):
