8932a2c8026d1552741f77b50a3315b0dae0eb0b
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-05-25 06:01:22 +0530

    [Bottlemart AU] Fix Spider (#16172)

--- locations/spiders/bottlemart_au.py
+++ locations/spiders/bottlemart_au.py
@@ -1,20 +1,55 @@
+import json
+
+import scrapy
 from scrapy import Spider
+from scrapy.http import JsonRequest
 
 from locations.dict_parser import DictParser
+from locations.geo import city_locations
+from locations.hours import OpeningHours
+from locations.pipelines.address_clean_up import merge_address_lines
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
 class BottlemartAUSpider(Spider):
     name = "bottlemart_au"
     item_attributes = {"brand": "Bottlemart", "brand_wikidata": "Q102863175"}
-    start_urls = [
-        "https://app.ehoundplatform.com/api/1.4/proximity_search?output=json&lat=-31.95224&lon=115.8614&count=10000&api_key=1qt7ts2bsw8a73v"
-    ]
+    is_playwright_spider = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
+
+    async def start(self):
+        for city in city_locations("GB", 30000):
+            yield scrapy.Request(
+                url=f"https://bottlemart.com.au/api/geolocation/suburbs/search/{city['name']}",
+                headers={
+                    "tenant": "BM",
+                },
+            )
 
     def parse(self, response, **kwargs):
-        for location in response.json()["record_set"]:
-            item = DictParser.parse(location)
-            item["ref"] = location["loc_id"]
-            item["name"] = location["details"]["location_name"]
-            item["phone"] = location["details"]["phone"]
+        if response.xpath("//pre/text()").get():
+            for city in json.loads(response.xpath("//pre/text()").get()):
+                yield JsonRequest(
+                    url=f"https://bottlemart.com.au/api/members/search/{city['locality']}/{city['postal_code']}/{city['lat']}/{city['lon']}?maxDistance=200",
+                    headers={
+                        "tenant": "BM",
+                    },
+                    callback=self.parse_details,
+                )
 
+    def parse_details(self, response):
+        for location in json.loads(response.xpath("//pre/text()").get()):
+            item = DictParser.parse(location)
+            item["ref"] = location["store_number"]
+            item["state"] = location["address"].get("administrative_area")
+            item["street_address"] = merge_address_lines(location["address"].get("lines"))
+            try:
+                oh = OpeningHours()
+                for day, time in location["regular_hours"].items():
+                    open_time = time.get("from")
+                    close_time = time.get("until")
+                    oh.add_range(day=day, open_time=open_time, close_time=close_time, time_format="%I:%M %p")
+                item["opening_hours"] = oh
+            except:
+                pass
             yield item
