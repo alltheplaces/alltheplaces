e7aae00ce1bc0014acf68d654828d57a56db8d70
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-05-25 11:25:05 +0530

    fix spider

--- locations/spiders/hy_vee_us.py
+++ locations/spiders/hy_vee_us.py
@@ -1,51 +1,42 @@
 import re
+from typing import Any
 
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
 from locations.items import Feature
 
 
-class HyVeeUSSpider(CrawlSpider):
+class HyVeeUSSpider(SitemapSpider):
     name = "hy_vee_us"
-    item_attributes = {"brand": "Hy-Vee", "brand_wikidata": "Q1639719", "country": "US"}
+    item_attributes = {"brand": "Hy-Vee", "brand_wikidata": "Q1639719"}
     allowed_domains = ["www.hy-vee.com"]
-    start_urls = ["https://www.hy-vee.com/aisles-online/stores"]
-    rules = [Rule(LinkExtractor(allow=r"^https:\/\/www\.hy-vee\.com\/stores\/detail\.aspx\?sc=\d+$"), callback="parse")]
-    # Some location pages were observed to redirect back to the location list page.
+    sitemap_urls = ["https://www.hy-vee.com/sitemap-locations.xml"]
+    sitemap_rules = [(r"/stores/detail\.aspx\?s(c)?=\d+$", "parse")]
     custom_settings = {"REDIRECT_ENABLED": False}
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         properties = {
             "ref": response.url.split("=", 1)[1],
             "name": response.xpath('//div[@id="page_content"]/h1/text()').get().strip(),
             "addr_full": re.sub(
-                r"\s+", " ", " ".join(response.xpath('//div[@id="page_content"]/div[2]/div[1]/text()').getall())
+                r"\s+",
+                " ",
+                " ".join(response.xpath('//div[contains(strong/text(), "Address")]/text()[not(parent::a)]').getall()),
             ).strip(),
-            "phone": response.xpath('//div[@id="page_content"]/div[2]/div[2]/a[contains(@href, "tel:")]/@href')
-            .get()
-            .replace("tel:", ""),
+            "phone": response.xpath('//a[contains(@href, "tel:")]/@href').get("").replace("tel:", "").strip(),
             "website": response.url,
         }
 
-        image_path = response.xpath('//img[@class="page_banner"]/@src').get()
-        if image_path:
-            if "https://" in image_path:
-                properties["image"] = image_path
-            else:
-                properties["image"] = "https://www.hy-vee.com" + image_path
+        if image_path := response.xpath('//img[@class="page_banner"]/@src').get():
+            properties["image"] = image_path if "https://" in image_path else "https://www.hy-vee.com" + image_path
 
-        hours_string = (
-            re.sub(r"\s+", " ", " ".join(response.xpath('//div[@id="page_content"]/p[2]/text()').getall()))
-            .upper()
-            .replace("OPEN DAILY,", "Mon-Sun:")
-            .replace("A.M.", "AM")
-            .replace("P.M.", "PM")
-        )
-        properties["opening_hours"] = OpeningHours()
-        properties["opening_hours"].add_ranges_from_string(hours_string)
+        if hours_raw := response.xpath('//div[@id="page_content"]/p[last()]/text()').get():
+            oh = OpeningHours()
+            oh.add_ranges_from_string(hours_raw.upper().replace("A.M.", "AM").replace("P.M.", "PM"))
+            properties["opening_hours"] = oh
 
         apply_category(Categories.SHOP_SUPERMARKET, properties)
 
