9e21852bf0d17b1fa6009b378623ae0f70866af3
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-05-27 00:49:05 +0530

    add GB locations in existing spider

--- locations/spiders/tanning_shop_ie_gb.py
+++ locations/spiders/tanning_shop_ie_gb.py
@@ -8,15 +8,22 @@ from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class TanningShopIESpider(SitemapSpider, StructuredDataSpider):
-    name = "tanning_shop_ie"
+class TanningShopIEGBSpider(SitemapSpider, StructuredDataSpider):
+    name = "tanning_shop_ie_gb"
     item_attributes = {"brand_wikidata": "Q123101132", "brand": "Tanning Shop"}
-    sitemap_urls = ["https://thetanningshop.ie/locations-sitemap.xml"]
+    sitemap_urls = [
+        "https://thetanningshop.ie/locations-sitemap.xml",
+        "https://thetanningshop.co.uk/location-sitemap.xml",
+    ]
     sitemap_rules = [(r"/location/[^/]+/$", "parse_sd")]
     search_for_facebook = False
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        item["branch"] = item.pop("name").removeprefix("The Tanning Shop ")
+        # To filter out the generic UK HQ block so we don't yield duplicates.
+        if item.get("name") == "The Tanning Shop UK":
+            return
+
+        item["branch"] = item.pop("name").removeprefix("The Tanning Shop – ").removeprefix("The Tanning Shop ")
         apply_category(Categories.SHOP_BEAUTY, item)
 
         yield item
