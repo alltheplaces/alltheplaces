2dbff15dff9a7b8376ff5ef68ef790caebcaa07c
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-05-26 16:49:03 +0530

    Fix Total Tools AU Spider

--- locations/spiders/total_tools_au.py
+++ locations/spiders/total_tools_au.py
@@ -1,27 +1,35 @@
+import json
+from typing import Any, Iterable
+
 import scrapy
+from scrapy.http import Response
 
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_EN, OpeningHours
+from locations.items import Feature
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.spiders.outdoor_supply_hardware_us import decode_email
 
 
-class TotalToolsAUSpider(scrapy.Spider):
+class TotalToolsAUSpider(PlaywrightSpider):
     name = "total_tools_au"
     item_attributes = {"brand": "Total Tools", "brand_wikidata": "Q116779059"}
     allowed_domains = ["www.totaltools.com.au"]
     start_urls = ["https://www.totaltools.com.au/totaltools_storelocator/index/loadStoreRewrite"]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
-    def parse(self, response):
-        for store in response.json()["storesjson"]:
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store in json.loads(response.xpath("//pre/text()").get())["storesjson"]:
             item = DictParser.parse(store)
             item["ref"] = store["storelocator_id"]
             item["street_address"] = item.pop("addr_full")
             item["website"] = "https://www.totaltools.com.au/" + store["rewrite_request_path"]
             yield scrapy.Request(url=item["website"], callback=self.parse_store_page, meta={"item": item})
 
-    def parse_store_page(self, response):
+    def parse_store_page(self, response: Response) -> Iterable[Feature]:
         item = response.meta["item"]
-        email_raw = response.xpath('//a[contains(@class, "group-info") and contains(@href, "mailto:")]/@href').get()
-        item["email"] = email_raw.split(":", 1)[1]
+        item["email"] = decode_email(response.xpath("//@data-cfemail").get(""))
         hours_raw = (
             " ".join(response.xpath('//div[@id="open_hour"]/div/table/tbody/tr/td/text()').getall())
             .replace("-", " ")
