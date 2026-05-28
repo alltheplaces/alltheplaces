6c25a74019994bd9bac78c1c79f7f1debc3a32c1
Cj Malone <CjMalone@mail.com>
2026-05-28 16:39:51 +0100

    Share request creation

--- locations/spiders/indigo.py
+++ locations/spiders/indigo.py
@@ -1,8 +1,8 @@
-from typing import AsyncIterator, Any
+from typing import Any, AsyncIterator
 from urllib.parse import urlencode
 
 from scrapy import Spider
-from scrapy.http import Request, Response
+from scrapy.http import JsonRequest, Request, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
@@ -11,14 +11,14 @@ from locations.dict_parser import DictParser
 class IndigoSpider(Spider):
     name = "indigo"
     item_attributes = {"operator": "Indigo", "operator_wikidata": "Q3559970"}
-    url = "https://salesforce.parkindigo.com/locations?"
+
+    def _make_request(self, page: int) -> JsonRequest:
+        return JsonRequest(
+            "https://salesforce.parkindigo.com/locations?" + urlencode({"location.language": "en", "page": page})
+        )
 
     async def start(self) -> AsyncIterator[Request]:
-        params = {
-            "location.language": "en",
-            "page": 0,
-        }
-        yield Request(self.url + urlencode(params))
+        yield self._make_request(0)
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         data = response.json()
@@ -35,8 +35,4 @@ class IndigoSpider(Spider):
             yield item
 
         if not data.get("last"):
-            params = {
-                "location.language": "en",
-                "page": int(data.get("number")) + 1,
-            }
-            yield Request(self.url + urlencode(params), callback=self.parse)
+            yield self._make_request(data["number"] + 1)
