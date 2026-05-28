76a5791cd464bb651ba885671ccca83797c3a66a
Cj Malone <CjMalone@mail.com>
2026-05-28 08:49:17 +0100

    [Tekkie Town] Fix spider

--- locations/spiders/tekkie_town_na_za.py
+++ locations/spiders/tekkie_town_na_za.py
@@ -1,12 +1,33 @@
-from locations.storefinders.amai_promap import AmaiPromapSpider
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import apply_category, Categories
+from locations.dict_parser import DictParser
+from locations.hours import DAYS_FULL, OpeningHours
 
 
-class TekkieTownNAZASpider(AmaiPromapSpider):
+class TekkieTownNAZASpider(Spider):
     name = "tekkie_town_na_za"
-    start_urls = ["https://tekkietown.co.za/pages/store-locator"]
     item_attributes = {"brand": "Tekkie Town", "brand_wikidata": "Q116620895"}
+    start_urls = ["https://store-locator-shopify-app-d9f525452f2f.herokuapp.com/api/stores?brand=Tekkie%20Town"]
 
-    def post_process_item(self, item, response, feature):
-        item["branch"] = item.pop("name").replace(self.item_attributes["brand"], "").strip().replace("  ", " ")
-        item.pop("email")
-        yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["branch"] = item.pop("name").removeprefix("Tekkie Town").strip()
+            item["ref"] = location["_id"]
+
+            item["opening_hours"] = self.parse_opening_hours(location)
+            apply_category(Categories.SHOP_SHOES, item)
+
+            yield item
+
+    def parse_opening_hours(self, location: dict) -> OpeningHours:
+        oh = OpeningHours()
+        for day in map(str.lower, DAYS_FULL):
+            rule = location["operating_hours"][day]
+            oh.add_range(day, rule["open"], rule["close"], "%H:%M:%S")
+
+        return oh
