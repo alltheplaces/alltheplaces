b7f09acc7a2281066eaa0994ae948daca74b3bb4
Cj Malone <CjMalone@mail.com>
2026-05-29 17:31:48 +0100

    [American Eagle Outfitters] Improve branding

--- locations/spiders/american_eagle_outfitters.py
+++ locations/spiders/american_eagle_outfitters.py
@@ -7,17 +7,24 @@ from locations.storefinders.yext import YextSpider
 
 class AmericanEagleOutfittersSpider(YextSpider):
     name = "american_eagle_outfitters"
-    item_attributes = {"brand": "American Eagle Outfitters", "brand_wikidata": "Q2842931"}
+    AMERICAN_EAGLE = {"brand": "American Eagle Outfitters", "brand_wikidata": "Q2842931"}
     AERIE = {"brand": "Aerie", "brand_wikidata": "Q25351619"}
-    OFFLINE = {"brand": "OFFLINE By Aerie", "brand_wikidata": ""}
     api_key = "90d8e45747b6a14476d8f1a327f692fe"
     drop_attributes = {"image", "twitter"}
 
     def parse_item(self, item: Feature, location: dict, **kwargs):
         item["website"] = urljoin("https://storelocations.ae.com/", location.get("slug", ""))
-        if item["name"].title().startswith("Aerie"):
+        if "OFFLINE" in item["name"]:
+            item["name"] = "OFFLINE By Aerie"
             item.update(self.AERIE)
-        elif item["name"].title().startswith("Offline"):
-            item.update(self.OFFLINE)
-            apply_category(Categories.SHOP_CLOTHES, item)
+        elif item["name"].startswith("Aerie"):
+            item.update(self.AERIE)
+        elif item["name"].startswith("Todd Snyder"):
+            item["brand"] = "Todd Snyder"
+        elif "American Eagle" in item["name"]:
+            item["name"] = None
+            item.update(self.AMERICAN_EAGLE)
+        else:  # Unsubscribed + AEO Distribution Center + Test stores
+            return
+        apply_category(Categories.SHOP_CLOTHES, item)
         yield item
