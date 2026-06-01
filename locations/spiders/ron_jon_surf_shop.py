5f570a97dca2bcbdc2f2727edb1a92c7d48659e6
Cj Malone <CjMalone@mail.com>
2026-06-01 14:28:58 +0100

    clean name

--- locations/spiders/ron_jon_surf_shop.py
+++ locations/spiders/ron_jon_surf_shop.py
@@ -1,5 +1,6 @@
 import json
 
+from locations.categories import apply_category, Categories
 from locations.dict_parser import DictParser
 from locations.playwright_spider import PlaywrightSpider
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
@@ -8,13 +9,7 @@ from locations.user_agents import BROWSER_DEFAULT
 
 class RonJonSurfShopSpider(PlaywrightSpider):
     name = "ron_jon_surf_shop"
-    item_attributes = {
-        "brand": "Ron Jon Surf Shop",
-        "brand_wikidata": "Q7363993",
-        "extras": {
-            "shop": "surf",
-        },
-    }
+    item_attributes = {"name": "Ron Jon Surf Shop", "brand": "Ron Jon Surf Shop", "brand_wikidata": "Q7363993"}
     allowed_domains = ["www.ronjonsurfshop.com"]
     start_urls = ["https://www.ronjonsurfshop.com/ajax/store-locator?searchText=&pagesize=15&pageNumber=1"]
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
@@ -22,7 +17,9 @@ class RonJonSurfShopSpider(PlaywrightSpider):
     def parse(self, response):
         for data in json.loads(response.xpath("//pre/text()").get())["results"]:
             item = DictParser.parse(data)
-
+            item["branch"] = item.pop("name")
             item["website"] = response.urljoin(data["url"])
+
+            apply_category({"shop": "surf"}, item)
 
             yield item
