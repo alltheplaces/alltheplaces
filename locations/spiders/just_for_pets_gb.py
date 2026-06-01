67ff845cde22e999ebce9b08a29f09f5cc78208e
Cj Malone <CjMalone@mail.com>
2026-06-01 09:53:38 +0100

    apply_category

--- locations/spiders/just_for_pets_gb.py
+++ locations/spiders/just_for_pets_gb.py
@@ -1,15 +1,18 @@
 from typing import Iterable
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.storemapper import StoremapperSpider
 
 
 class JustForPetsGBSpider(StoremapperSpider):
     name = "just_for_pets_gb"
-    item_attributes = {"brand": "Just For Pets", "name": "Just For Pets", "extras": Categories.SHOP_PET.value}
+    item_attributes = {"name": "Just For Pets", "brand": "Just For Pets"}
     company_id = "10020"
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
+
+        apply_category(Categories.SHOP_PET, item)
+
         yield item
