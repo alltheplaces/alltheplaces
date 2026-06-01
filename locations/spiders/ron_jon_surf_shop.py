5a7ca85e60d669250e6b517f649fee42da1f6985
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-06-01 18:29:49 +0530

    tying with playwright

--- locations/spiders/ron_jon_surf_shop.py
+++ locations/spiders/ron_jon_surf_shop.py
@@ -1,10 +1,12 @@
-from scrapy import Spider
+import json
 
 from locations.dict_parser import DictParser
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class RonJonSurfShopSpider(Spider):
+class RonJonSurfShopSpider(PlaywrightSpider):
     name = "ron_jon_surf_shop"
     item_attributes = {
         "brand": "Ron Jon Surf Shop",
@@ -15,10 +17,10 @@ class RonJonSurfShopSpider(Spider):
     }
     allowed_domains = ["www.ronjonsurfshop.com"]
     start_urls = ["https://www.ronjonsurfshop.com/ajax/store-locator?searchText=&pagesize=15&pageNumber=1"]
-    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
 
     def parse(self, response):
-        for data in response.json()["results"]:
+        for data in json.loads(response.xpath("//pre/text()").get())["results"]:
             item = DictParser.parse(data)
 
             item["website"] = response.urljoin(data["url"])
