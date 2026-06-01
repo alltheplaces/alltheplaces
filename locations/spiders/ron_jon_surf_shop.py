c1f5042420535308e2f269669d7105b034a9f147
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-06-01 18:11:11 +0530

    added user agent to fix spider

--- locations/spiders/ron_jon_surf_shop.py
+++ locations/spiders/ron_jon_surf_shop.py
@@ -1,6 +1,7 @@
 from scrapy import Spider
 
 from locations.dict_parser import DictParser
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class RonJonSurfShopSpider(Spider):
@@ -14,6 +15,7 @@ class RonJonSurfShopSpider(Spider):
     }
     allowed_domains = ["www.ronjonsurfshop.com"]
     start_urls = ["https://www.ronjonsurfshop.com/ajax/store-locator?searchText=&pagesize=15&pageNumber=1"]
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
 
     def parse(self, response):
         for data in response.json()["results"]:
