4daf6754f10b8d0389ece1e1f918a28c44882d85
Cj Malone <CjMalone@mail.com>
2026-06-01 10:36:10 +0100

    Improve name/brand

--- locations/spiders/pepe_jeans.py
+++ locations/spiders/pepe_jeans.py
@@ -7,16 +7,35 @@ from locations.google_url import extract_google_position
 from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
 
+PEPE_JEANS = {"brand": "Pepe Jeans", "brand_wikidata": "Q426992"}
+
 
 class PepeJeansSpider(Spider):
     name = "pepe_jeans"
-    item_attributes = {"brand": "Pepe Jeans", "brand_wikidata": "Q426992"}
     start_urls = ["https://www.pepejeans.com/intl/en/page/store-locator.html"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for location in response.xpath('//*[@class="store-card"]'):
             item = Feature()
-            item["branch"] = location.xpath(".//h2/text()").get().removeprefix("Pepe Jeans ")
+            if name := location.xpath(".//h2/text()").get():
+                if name.startswith("AWWG Outlet "):
+                    item["name"] = "AWWG Outlet"
+                    item["branch"] = name.removeprefix("AWWG Outlet ")
+                elif name.startswith("Pepe Jeans Outlet Factory "):
+                    item["name"] = "Pepe Jeans Outlet Factory"
+                    item["branch"] = name or name.removeprefix("Pepe Jeans Outlet Factory ")
+                    item.update(PEPE_JEANS)
+                elif name.startswith("Pepe Jeans Outlet "):
+                    item["name"] = "Pepe Jeans Outlet"
+                    item["branch"] = name or name.removeprefix("Pepe Jeans Outlet ")
+                    item.update(PEPE_JEANS)
+                elif name.startswith("Pepe Jeans "):
+                    item["name"] = "Pepe Jeans"
+                    item["branch"] = name or name.removeprefix("Pepe Jeans ")
+                    item.update(PEPE_JEANS)
+                else:
+                    item["name"] = name
+
             item["street_address"] = location.xpath(".//p/text()").get()
             item["addr_full"] = item["ref"] = merge_address_lines(
                 [item["street_address"], location.xpath(".//p[2]/text()").get()]
