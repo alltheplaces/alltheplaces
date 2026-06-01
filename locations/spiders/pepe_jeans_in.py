5b0aaf9d5840137c4f648347bed7126d709038e1
Cj Malone <CjMalone@mail.com>
2026-06-01 10:41:59 +0100

    apply_category

--- locations/spiders/pepe_jeans_in.py
+++ locations/spiders/pepe_jeans_in.py
@@ -3,6 +3,7 @@ from typing import Any, AsyncIterator
 from scrapy import Spider
 from scrapy.http import JsonRequest, Response
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.geo import city_locations
 from locations.pipelines.address_clean_up import merge_address_lines
@@ -25,4 +26,7 @@ class PepeJeansINSpider(Spider):
                 item = DictParser.parse(location)
                 item["branch"] = item.pop("name").replace("Pepe Jeans - ", "")
                 item["addr_full"] = merge_address_lines([item.pop("street_address"), location["address2"]])
+
+                apply_category(Categories.SHOP_CLOTHES, item)
+
                 yield item
