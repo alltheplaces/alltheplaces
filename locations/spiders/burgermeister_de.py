7603227f92abdfc5c4a864ce8fd07a825d540e99
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-06-01 13:17:47 +0530

    fix spider

--- locations/spiders/burgermeister_de.py
+++ locations/spiders/burgermeister_de.py
@@ -1,18 +1,24 @@
-from locations.storefinders.wp_go_maps import WpGoMapsSpider
-from locations.user_agents import BROWSER_DEFAULT
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class BurgermeisterDESpider(WpGoMapsSpider):
+class BurgermeisterDESpider(JSONBlobSpider):
     name = "burgermeister_de"
     item_attributes = {"brand": "Burgermeister", "brand_wikidata": "Q116382535"}
     start_urls = [
-        "https://burgermeister.com/api-json.php?filter=%7B%22map_id%22%3A%226%22%2C%22mashupIDs%22%3A%5B%5D%2C%22customFields%22%3A%5B%5D%7D&route=%2Ffeatures%2F&action=wpgmza_rest_api_request"
+        "https://burgermeister.com/wp-admin/admin-ajax.php?filter=%7B%22map_id%22%3A%226%22%2C%22mashupIDs%22%3A%5B%5D%2C%22customFields%22%3A%5B%5D%7D&route=%2Ffeatures%2F&action=wpgmza_rest_api_request"
     ]
-    allowed_domains = ["burgermeister.com"]
-    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
+    locations_key = "markers"
 
-    def post_process_item(self, item, location):
-        item["image"] = location.get("pic")
-        item["branch"] = item.pop("name")
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if "COMING SOON" in feature.get("description", ""):
+            return
+
+        item["branch"] = item.pop("name", "").split(" | ")[-1].strip()
+        item["image"] = feature.get("pic")
 
         yield item
