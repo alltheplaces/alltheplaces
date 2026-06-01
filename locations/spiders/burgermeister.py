6ca83d598e1ba9667c92fabfd76b01d39bdb1305
Cj Malone <CjMalone@mail.com>
2026-06-01 10:03:22 +0100

    Rename spider

--- locations/spiders/burgermeister.py
+++ locations/spiders/burgermeister.py
@@ -7,8 +7,8 @@ from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
-class BurgermeisterDESpider(JSONBlobSpider):
-    name = "burgermeister_de"
+class BurgermeisterSpider(JSONBlobSpider):
+    name = "burgermeister"
     item_attributes = {"brand": "Burgermeister", "brand_wikidata": "Q116382535"}
     start_urls = [
         "https://burgermeister.com/wp-admin/admin-ajax.php?filter=%7B%22map_id%22%3A%226%22%2C%22mashupIDs%22%3A%5B%5D%2C%22customFields%22%3A%5B%5D%7D&route=%2Ffeatures%2F&action=wpgmza_rest_api_request"
