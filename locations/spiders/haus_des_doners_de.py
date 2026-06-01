dabd6a6d2d1e6107d4472f8745de25bf27d4bc1a
Cj Malone <CjMalone@mail.com>
2026-06-01 13:04:35 +0100

    Use locations_key

--- locations/spiders/haus_des_doners_de.py
+++ locations/spiders/haus_des_doners_de.py
@@ -10,9 +10,7 @@ class HausDesDonersDESpider(JSONBlobSpider):
     name = "haus_des_doners_de"
     start_urls = ["https://api.atlist.com/v1/map/41de535d-e707-46fa-8440-87c6fe111fb8/markers"]
     item_attributes = {"brand": "Haus des Döners", "brand_wikidata": "Q133461235"}
-
-    def extract_json(self, response: TextResponse) -> dict:
-        return super().extract_json(response)["markers"]
+    locations_key = "markers"
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
