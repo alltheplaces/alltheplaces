822c44ab8e132887f608e45b9d9273768d335204
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-06-01 15:23:44 +0530

    updated to JsonRequest

--- locations/spiders/caseys_general_store.py
+++ locations/spiders/caseys_general_store.py
@@ -1,9 +1,7 @@
-import json
 from typing import Any, AsyncIterator
 
-import scrapy
 from scrapy import Spider
-from scrapy.http import TextResponse
+from scrapy.http import JsonRequest, TextResponse
 
 from locations.dict_parser import DictParser
 from locations.geo import city_locations
@@ -16,22 +14,82 @@ class CaseysGeneralStoreSpider(Spider):
     requires_proxy = True
 
     async def start(self) -> AsyncIterator[Any]:
+        url = "https://www.caseys.com/api/graphql"
+
         for city in city_locations("US", 10000):
             payload = {
                 "operationName": "GetStoresByFilters",
                 "variables": {
                     "input": {"latitude": f"{city['latitude']}", "longitude": f"{city['longitude']}"},
                     "occasionType": "CARRYOUT",
+                    "carryoutType": None,
                     "withTimeSlots": True,
                 },
                 "extensions": {"clientLibrary": {"name": "@apollo/client", "version": "4.1.9"}},
-                "query": "query GetStoresByFilters($input: StoresByCoordinateInput!, $occasionType: OccasionType!, $carryoutType: CarryoutType, $withTimeSlots: Boolean!) {\n  storesByCoordinate(input: $input) {\n    formattedDistance\n    store {\n      storeNumber\n      name\n      shortName\n      displayName\n      brand\n      brandLogo {\n        url\n        altText\n        __typename\n      }\n      address {\n        id\n        line1\n        city\n        stateAbbreviation\n        postalCode\n        phoneNumber\n        __typename\n      }\n      geoPoint {\n        latitude\n        longitude\n        __typename\n      }\n      amenities {\n        key\n        value\n        isEnabled\n        __typename\n      }\n      amenityGroups {\n        code\n        displayName\n        displayOrder\n        amenities {\n          displayName\n          code\n          displayOrder\n          value {\n            ... on BooleanAmenityValue {\n              value\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      storeOpenHours\n      carryoutHours\n      curbsideHours\n      carryoutWaitTime\n      deliveryWaitTime\n      deliveryHours\n      carwashHours\n      locationUrl\n      hours {\n        type\n        hours {\n          dayOfWeek\n          blocks {\n            openDateTime\n            closeDateTime\n            closeTime {\n              hour\n              minute\n              __typename\n            }\n            openTime {\n              minute\n              hour\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      timeZoneId\n      availableTimeSlots(occasionType: $occasionType, carryoutType: $carryoutType) @include(if: $withTimeSlots) {\n        displayText\n        option\n        enabled\n        timeSlots {\n          displayText\n          value\n          __typename\n        }\n        __typename\n      }\n      onlineOrdering\n      onlineOrderingTemporarilyUnavailable\n      deliveryDisruption\n      __typename\n    }\n    __typename\n  }\n}",
+                "query": """query GetStoresByFilters($input: StoresByCoordinateInput!, $occasionType: OccasionType!, $carryoutType: CarryoutType, $withTimeSlots: Boolean!) {
+              storesByCoordinate(input: $input) {
+                formattedDistance
+                store {
+                  storeNumber
+                  name
+                  shortName
+                  displayName
+                  brand
+                  address {
+                    id
+                    line1
+                    city
+                    stateAbbreviation
+                    postalCode
+                    phoneNumber
+                  }
+                  geoPoint {
+                    latitude
+                    longitude
+                  }
+                  amenities {
+                    key
+                    value
+                    isEnabled
+                  }
+                  storeOpenHours
+                  carryoutHours
+                  curbsideHours
+                  deliveryHours
+                  carwashHours
+                  locationUrl
+                  hours {
+                    type
+                    hours {
+                      dayOfWeek
+                      blocks {
+                        openDateTime
+                        closeDateTime
+                      }
+                    }
+                  }
+                  timeZoneId
+                  availableTimeSlots(occasionType: $occasionType, carryoutType: $carryoutType) @include(if: $withTimeSlots) {
+                    displayText
+                    option
+                    enabled
+                    timeSlots {
+                      displayText
+                      value
+                    }
+                  }
+                  onlineOrdering
+                  onlineOrderingTemporarilyUnavailable
+                  deliveryDisruption
+                }
+              }
+            }""",
             }
 
-            yield scrapy.Request(
-                url="https://www.caseys.com/api/graphql",
+            yield JsonRequest(
+                url=url,
                 method="POST",
-                body=json.dumps(payload),
+                data=payload,
                 callback=self.parse,
             )
 
