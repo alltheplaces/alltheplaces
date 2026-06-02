181548a87cd2a5b0cf065bfc681ce8f0cfa41575
Cj Malone <CjMalone@mail.com>
2026-06-02 09:06:11 +0100

    apply_category

--- locations/spiders/great_wolf_resorts_us.py
+++ locations/spiders/great_wolf_resorts_us.py
@@ -3,16 +3,13 @@ from typing import Any
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import apply_category
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class GreatWolfResortsUSSpider(SitemapSpider, StructuredDataSpider):
     name = "great_wolf_resorts_us"
-    item_attributes = {
-        "brand": "Great Wolf Resorts",
-        "brand_wikidata": "Q5600260",
-        "extras": {"leisure": "water_park"},
-    }
+    item_attributes = {"name": "Great Wolf Lodge", "brand": "Great Wolf Lodge", "brand_wikidata": "Q5600260"}
     allowed_domains = ["www.greatwolf.com"]
     sitemap_urls = ["https://www.greatwolf.com/php-root.sitemap.xml"]
     sitemap_rules = [(r"/customer-service$", "parse_sd")]
@@ -22,4 +19,6 @@ class GreatWolfResortsUSSpider(SitemapSpider, StructuredDataSpider):
         item.pop("facebook")
         item.pop("twitter")
         item["branch"] = item.pop("name").removeprefix("Great Wolf Lodge ")
+
+        apply_category({"leisure": "water_park"}, item)
         yield item
