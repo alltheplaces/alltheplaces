2e1e47260801dc97d1810767e2e91283623916b8
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-03 19:19:30 +0530

    [BPS PL] Fix Spider (#16547)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/bps_pl.py
+++ locations/spiders/bps_pl.py
@@ -1,37 +1,41 @@
 import json
 from typing import Any
 
-import scrapy
+import json5
+from scrapy import Spider
 from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
-class BpsPLSpider(scrapy.Spider):
+class BpsPLSpider(Spider):
     name = "bps_pl"
     item_attributes = {"brand": "Bank Polskiej Spółdzielczości", "brand_wikidata": "Q9165001"}
-    start_urls = ["https://www.bankbps.pl/znajdz-placowke"]
+    start_urls = ["https://mojbank.pl/znajdz-placowke"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         token_data = response.xpath('//*[@type="application/json"]/text()').get()
         token = json.loads(token_data).get("csrf.token")
-        yield JsonRequest(
-            url=f"https://www.bankbps.pl/znajdz-placowke?view=maps&format=json&task=getpoints&menuitem=145&{token}=1",
-            callback=self.parse_details,
-        )
+        for location_type, value in [("znajdz-placowke", "145"), ("bankomaty", "210")]:
+            yield JsonRequest(
+                url=f"https://mojbank.pl/{location_type}?view=maps&amp;format=json&amp;task=getpoints&menuitem={value}&{token}=1",
+                callback=self.parse_details,
+            )
 
     def parse_details(self, response: Response, **kwargs: Any) -> Any:
-        data = response.json().get("data", {})
-        for location_type, category in [("department", Categories.BANK), ("atm", Categories.ATM)]:
-            for location in data.get(location_type, []):
-                if location_type == "atm" and not location["name"].startswith("Bank Spółdzielczy"):
-                    continue
-                yield self.parse_location(location, category)
+        data = json5.loads(response.text).get("data", {})
 
-    def parse_location(self, location: dict, category: Categories) -> dict:
-        item = DictParser.parse(location)
-        item["branch"] = item.pop("name", None)
-        item["name"] = self.item_attributes["brand"]
-        apply_category(category, item)
-        return item
+        # Process branches
+        for location in data.get("department", []):
+            if location.get("bank") == "BPS":
+                item = DictParser.parse(location)
+                apply_category(Categories.BANK, item)
+                yield item
+
+        # Process ATMs
+        for location in data.get("atm", []):
+            if location.get("name", "").startswith("Bank Spółdzielczy"):
+                item = DictParser.parse(location)
+                apply_category(Categories.ATM, item)
+                yield item
