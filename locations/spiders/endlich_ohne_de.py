3d586da7df2f2342668d3035a412eb0c65d875d6
Cj Malone <CjMalone@mail.com>
2026-06-03 13:32:02 +0100

    Collect coords

--- locations/spiders/endlich_ohne_de.py
+++ locations/spiders/endlich_ohne_de.py
@@ -1,5 +1,7 @@
-from typing import Iterable
+import json
+from typing import Iterable, AsyncIterator, Any
 
+from scrapy import Request
 from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
@@ -10,15 +12,27 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class EndlichOhneDESpider(SitemapSpider, StructuredDataSpider):
     name = "endlich_ohne_de"
-    item_attributes = {"brand_wikidata": "Q119982663", "brand": "Endlich Ohne"}
-    sitemap_urls = ["https://www.endlich-ohne.de/standorte-sitemap.xml"]
+    item_attributes = {"brand": "Endlich Ohne", "brand_wikidata": "Q119982663"}
     sitemap_rules = [("/standorte/filiale-", "parse_sd")]
     search_for_facebook = False
+
+    async def start(self) -> AsyncIterator[Any]:
+        yield Request("https://www.endlich-ohne.de/standorte/", callback=self.parse_coords)
+
+    def parse_coords(self, response: TextResponse, **kwargs):
+        self.coords = {}
+        for loc in json.loads(response.xpath("//@data-markers").get()):
+            self.coords[str(loc["id"])] = (loc["latLang"]["lat"], loc["latLang"]["lng"])
+        yield Request("https://www.endlich-ohne.de/standorte-sitemap.xml", self._parse_sitemap)
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         # Filter out non-location entities
         if not item.get("street_address"):
             return
+
+        if link := response.xpath('//link[@rel="shortlink"]/@href').get():
+            ref = link.rsplit("=", 1)[1]
+            item["lat"], item["lon"] = self.coords.get(ref, (None, None))
 
         if "PLZ" in item.get("postcode"):
             item["postcode"] = None
