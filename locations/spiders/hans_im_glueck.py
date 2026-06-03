2fa47896a7d5912edb670ed2a6ed09aa92d7e485
hfs <538109+hfs@users.noreply.github.com>
2026-06-03 11:54:51 +0200

    [Hans im Glück DE] Add spider (58 locations)

---
+++ locations/spiders/hans_im_glueck.py
@@ -0,0 +1,42 @@
+from typing import Iterable
+
+import chompjs
+from scrapy.http import Response
+
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+
+
+class HansImGlueckSpider(JSONBlobSpider):
+    name = "hans_im_glueck"
+    start_urls = ["https://hansimglueck-burgergrill.de/typo3conf/ext/hig_site/Yext/locations.js"]
+    item_attributes = {"brand": "Hans im Glück", "brand_wikidata": "Q22569868"}
+
+    def extract_json(self, response: Response) -> list[dict]:
+        return chompjs.parse_js_object(response.text)
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        if item.get("name"):
+            if item["name"].startswith("HANS IM GLÜCK - "):
+                item["branch"] = item["name"].removeprefix("HANS IM GLÜCK - ").title()
+        else:
+            item["branch"] = feature["c_website_hig_name"]
+        item["name"] = "Hans im Glück"
+        item["ref"] = feature["meta"]["id"]
+        if "googlePlaceId" in feature:
+            item["extras"]["ref:google"] = feature["googlePlaceId"]
+        item["website"] = feature["c_baseURL"]
+        if "emails" in feature:
+            item["email"] = feature["emails"][0]
+        if "c_website_standortfotos" in feature:
+            item["image"] = response.urljoin(feature["c_website_standortfotos"][0]["url"])
+
+        if "c_websiteÖffnungszeiten" in feature:
+            oh = OpeningHours()
+            for day, intervals in feature["c_websiteÖffnungszeiten"].items():
+                for interval in intervals.get("openIntervals", []):
+                    oh.add_range(day, interval["start"], interval["end"])
+            item["opening_hours"] = oh
+
+        yield item
