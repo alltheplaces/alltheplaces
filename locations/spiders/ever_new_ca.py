7ac368b7681b5b2b482f5aede970737aa3ca3f0e
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-06-03 16:52:21 +0530

    code updated for forever_new_au_nz and ever_new_ca to fix spiders

--- locations/spiders/ever_new_ca.py
+++ locations/spiders/ever_new_ca.py
@@ -1,10 +1,16 @@
+from locations.playwright_spider import PlaywrightSpider
 from locations.spiders.forever_new_au_nz import FOREVER_NEW_SHARED_ATTRIBUTES, ForeverNewAUNZSpider
 
 
-class EverNewCASpider(ForeverNewAUNZSpider):
+class EverNewCASpider(ForeverNewAUNZSpider, PlaywrightSpider):
     name = "ever_new_ca"
     item_attributes = FOREVER_NEW_SHARED_ATTRIBUTES
     item_attributes["brand"] = "Ever New"
     start_urls = [
-        "https://www.evernew.ca/locator/index/search/?longitude=0&latitude=0&radius=100000&type=all",
+        "https://www.evernew.ca/locator/index/search/?address=vancouver&components[country]=CA&radius=10000000&type=all",
     ]
+    # custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
+    #
+    # def extract_json(self, response: TextResponse):
+    #     data = json.loads(response.xpath("//pre/text()").get())["results"]["results"]
+    #     return data
--- locations/spiders/forever_new_au_nz.py
+++ locations/spiders/forever_new_au_nz.py
@@ -1,27 +1,35 @@
 import json
 
+from scrapy.http import TextResponse
+
 from locations.hours import OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.user_agents import BROWSER_DEFAULT
 
 FOREVER_NEW_SHARED_ATTRIBUTES = {"brand": "Forever New", "brand_wikidata": "Q119221929"}
 
 
-class ForeverNewAUNZSpider(JSONBlobSpider):
+class ForeverNewAUNZSpider(JSONBlobSpider, PlaywrightSpider):
     name = "forever_new_au_nz"
     item_attributes = FOREVER_NEW_SHARED_ATTRIBUTES
     start_urls = [
-        "https://www.forevernew.com.au/locator/index/search/?longitude=0&latitude=0&radius=100000&type=all",
-        "https://www.forevernew.co.nz/locator/index/search/?longitude=0&latitude=0&radius=100000&type=all",
+        "https://www.forevernew.com.au/locator/index/search/?address=sydney&components[country]=AU&radius=1000000000&type=all",
+        "https://www.forevernew.co.nz/locator/index/search/?address=wellington&components[country]=NZ&radius=1000000000&type=all",
     ]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    locations_key = ["results", "results"]
-    needs_json_request = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
+
+    def extract_json(self, response: TextResponse):
+        data = json.loads(response.xpath("//pre/text()").get())["results"]["results"]
+        return data
 
     def post_process_item(self, item, response, location):
         # Ignore locations which are embedded within a Myer department store.
         if location.get("name") is not None and " MYER " in location["name"].upper():
             return
         hours = json.loads(location["trading_hours"])
+        item["branch"] = item.pop("name").replace("Forever New - ", "")
         item["opening_hours"] = OpeningHours()
         item["opening_hours"].add_ranges_from_string(str(hours))
         yield item
