25236bc87946ced589d2b4a29ad02126aefdd05f
Cj Malone <CjMalone@mail.com>
2026-06-04 11:29:11 +0100

    Trim

--- locations/spiders/lafarmacia_it.py
+++ locations/spiders/lafarmacia_it.py
@@ -15,21 +15,13 @@ class LafarmaciaITSpider(AlgoliaSpider):
     app_id = "WKANYZFMAK"
     api_key = "8b957ae6bfa82c42e761eb8dea2767bd"
     index_name = "prod_LAF_STORES"
-    dataset_attributes = AlgoliaSpider.dataset_attributes | {"website": "https://www.lafarmacia.it/trova-farmacia"}
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
-        item["ref"] = feature["storeCode"]
-        item["name"] = feature["storeName"]
-        item["street_address"] = feature["address"]
-        item["city"] = feature["city"]
-        item["postcode"] = feature["zipCode"]
-        item["state"] = feature.get("region", "")
-        item["phone"] = feature.get("phoneNumber")
-        item["email"] = feature.get("email")
-        item["website"] = f"https://www.lafarmacia.it/farmacia/{feature['slug']}"
+        item["street_address"] = item.pop("addr_full", None)
         item["lat"] = feature["_geoloc"]["lat"]
         item["lon"] = feature["_geoloc"]["lng"]
-        item.pop("addr_full", None)
+        item["ref"] = feature["storeCode"]
+        item["website"] = f"https://www.lafarmacia.it/farmacia/{feature['slug']}"
 
         if whatsapp := feature.get("whatsapp", "").strip():
             set_social_media(item, SocialMedia.WHATSAPP, "+39" + whatsapp)
@@ -37,10 +29,9 @@ class LafarmaciaITSpider(AlgoliaSpider):
         oh = OpeningHours()
         for entry in feature.get("timetable", []):
             for h in entry.get("hours", []):
-                oh.add_range(entry["weekday"].title(), h["openingTime"], h["closingTime"])
+                oh.add_range(entry["weekday"], h["openingTime"], h["closingTime"])
         item["opening_hours"] = oh
 
         apply_category(Categories.PHARMACY, item)
-        item["extras"]["dispensing"] = "yes"
 
         yield item
