e908ddba81674c9ae7e93fa11f85b19ac6fbee50
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-06-04 11:16:21 +0530

    code updated to fix spider

--- locations/spiders/freshstop_za.py
+++ locations/spiders/freshstop_za.py
@@ -1,41 +1,30 @@
 import re
-from typing import Any
+from typing import AsyncIterator, Iterable
 
-import chompjs
+from scrapy import Request
+from scrapy.http import JsonRequest, TextResponse
 
-from locations.categories import Categories, Extras, apply_category, apply_yes_no
-from locations.items import SocialMedia, set_social_media
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
 class FreshstopZASpider(JSONBlobSpider):
     name = "freshstop_za"
     item_attributes = {"brand": "FreshStop", "brand_wikidata": "Q116620734"}
-    start_urls = ["https://freshstop.co.za/store-locator/"]
+    locations_key = "data"
 
-    def extract_json(self, response):
-        return chompjs.parse_js_object(
-            re.search(r"bhStoreLocatorLocations = (\[.+?\]);", response.text, re.DOTALL).group(1)
+    async def start(self) -> AsyncIterator[JsonRequest | Request]:
+
+        yield JsonRequest(
+            url="https://freshstop.co.za/wp-admin/admin-ajax.php",
+            method="POST",
+            headers={"content-type": "application/x-www-form-urlencoded"},
+            body="action=list_stores&lat=28.6327&lng=77.2198",
+            callback=self.parse,
         )
 
-    def pre_process_data(self, location) -> Any:
-        location["street_address"] = location.pop("address")
-        location["postcode"] = location.pop("postal")
-
-    def post_process_item(self, item, response, location):
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
-        item["facebook"] = location.get("store_contact_info_facebook")
-        if location.get("store_contact_info_whatsapp") is not None:
-            set_social_media(
-                item, SocialMedia.WHATSAPP, "+27 " + location.get("store_contact_info_whatsapp").lstrip("0")
-            )
-        apply_yes_no(
-            Extras.WIFI, item, "yes" in location["amenities_wifi"] if location.get("amenities_wifi") else False
-        )
-        apply_yes_no(
-            Extras.TOILETS,
-            item,
-            "yes" in location["amenities_bathroom"] if location.get("amenities_bathroom") else False,
-        )
-        apply_category(Categories.SHOP_CONVENIENCE, item)
+        item["website"] = feature["store_permalink"]
+        item["lat"], item["lon"] = re.search(r".*=(-?\d+\.\d+),(-?\d+\.\d+)", feature["maps_url"]).groups()
         yield item
