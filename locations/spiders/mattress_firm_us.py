cbad6a08caa0b232bb3dbbfa5483f7644b227ff1
Cj Malone <CjMalone@mail.com>
2026-06-05 15:53:02 +0100

    Clean brand

--- locations/spiders/mattress_firm_us.py
+++ locations/spiders/mattress_firm_us.py
@@ -7,25 +7,21 @@ from locations.storefinders.rio_seo import RioSeoSpider
 
 class MattressFirmUSSpider(RioSeoSpider):
     name = "mattress_firm_us"
+    item_attributes = {"brand": "Mattress Firm", "brand_wikidata": "Q6791878"}
     end_point = "https://maps.stores.mattressfirm.com.prod.rioseo.com"
     custom_settings = {"DOWNLOAD_TIMEOUT": 60}
 
     def post_process_feature(self, feature: Feature, location: dict) -> Iterable[Feature]:
-        # Maintain original brand separation
-        if "Clearance" in location.get("location_name", ""):
-            feature["brand"] = "Mattress Firm Clearance"
-            feature["brand_wikidata"] = "Q6791878"
+        if feature["name"].startswith("Mattress Firm Outlet - "):
+            feature["branch"] = feature.pop("name").removeprefix("Mattress Firm Outlet - ")
+            feature["name"] = "Mattress Firm Outlet"
+        elif feature["name"].startswith("Mattress Firm SuperCenter - "):
+            feature["branch"] = feature.pop("name").removeprefix("Mattress Firm SuperCenter - ")
+            feature["name"] = "Mattress Firm SuperCenter"
         else:
-            feature["brand"] = "Mattress Firm"
-            feature["brand_wikidata"] = "Q6791878"
+            feature["branch"] = feature.pop("name").removeprefix("Mattress Firm ")
+            feature["name"] = "Mattress Firm"
 
-        feature["branch"] = (
-            feature.pop("name", "")
-            .removeprefix("Mattress Firm ")
-            .replace("Mesa Clearance Center at ", "")
-            .replace("Outlet", "")
-            .strip(" -")
-        )
         apply_category(Categories.SHOP_BED, feature)
 
         yield feature
