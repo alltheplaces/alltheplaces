695f12d92bc0c04221dc8a132e7a580d08841077
Cj Malone <CjMalone@mail.com>
2026-06-05 11:52:44 +0100

    Trim double parse

--- locations/spiders/thai_express_ca_us.py
+++ locations/spiders/thai_express_ca_us.py
@@ -1,35 +1,32 @@
 import json
 import re
-from typing import Any
+from typing import Any, Iterable
 
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.linked_data_parser import LinkedDataParser
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class ThaiExpressCAUSSpider(SitemapSpider, StructuredDataSpider):
     name = "thai_express_ca_us"
-    item_attributes = {
-        "brand_wikidata": "Q7711610",
-        "brand": "Thaï Express",
-    }
+    item_attributes = {"brand": "Thaï Express", "brand_wikidata": "Q7711610"}
     sitemap_urls = ["https://locations.thaiexpress.ca/sitemap.xml"]
     sitemap_rules = [(r"https://locations\.thaiexpress\.ca/(?:qc|on|ab|bc|ns|mb|sk|nb|nl|pe)/[^/]+/[^/]+$", "parse_sd")]
+
+    def iter_linked_data(self, response: Response) -> Iterable[dict]:
+        for ld_obj in LinkedDataParser.iter_linked_data(response):
+            if ld_obj.get("credentialSubject"):
+                yield ld_obj["credentialSubject"]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs: Any) -> Any:
         branch = re.sub(r"^Tha[iï] Express( Restaurant)?\s*", "", item.pop("name", "") or "").strip()
         item["branch"] = branch or None
         item["image"] = None
 
-        for ld in response.xpath('//script[@type="application/ld+json"]/text()').getall():
-            if '"GeoCoordinates"' not in ld:
-                continue
-            if geo := json.loads(ld).get("credentialSubject", {}).get("geo"):
-                item["lat"], item["lon"] = geo.get("latitude"), geo.get("longitude")
-                break
-
         apply_category(Categories.FAST_FOOD, item)
+
         yield item
