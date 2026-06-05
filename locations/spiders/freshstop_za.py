163ddde5a62360e50f0f2c99ac37f23c75246add
Cj Malone <CjMalone@mail.com>
2026-06-05 11:37:42 +0100

    apply_category

--- locations/spiders/freshstop_za.py
+++ locations/spiders/freshstop_za.py
@@ -1,9 +1,10 @@
 import re
 from typing import Iterable
 
-import scrapy
 from scrapy.http import TextResponse
+from scrapy.http.request.form import FormRequest
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
@@ -14,19 +15,20 @@ class FreshstopZASpider(JSONBlobSpider):
     locations_key = "data"
 
     async def start(self):
-        yield scrapy.FormRequest(
+        yield FormRequest(
             url="https://freshstop.co.za/wp-admin/admin-ajax.php",
-            method="POST",
             formdata={
                 "action": "list_stores",
                 "lat": "28.6327",
                 "lng": "77.2198",
             },
-            callback=self.parse,
         )
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
         item["website"] = feature["store_permalink"]
         item["lat"], item["lon"] = re.search(r".*=(-?\d+\.\d+),(-?\d+\.\d+)", feature["maps_url"]).groups()
+
+        apply_category(Categories.SHOP_CONVENIENCE, item)
+
         yield item
