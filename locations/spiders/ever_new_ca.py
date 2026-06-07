7735b70874d6bbbd4248190598d4a7bce850c2b7
Ian Dees <ian.dees@gmail.com>
2026-06-07 18:47:18 -0500

    Fix mutable shared dict: use dict spread instead of mutating FOREVER_NEW_SHARED_ATTRIBUTES

--- locations/spiders/ever_new_ca.py
+++ locations/spiders/ever_new_ca.py
@@ -4,8 +4,7 @@ from locations.spiders.forever_new_au_nz import FOREVER_NEW_SHARED_ATTRIBUTES, F
 
 class EverNewCASpider(ForeverNewAUNZSpider, PlaywrightSpider):
     name = "ever_new_ca"
-    item_attributes = FOREVER_NEW_SHARED_ATTRIBUTES
-    item_attributes["brand"] = "Ever New"
+    item_attributes = {**FOREVER_NEW_SHARED_ATTRIBUTES, "brand": "Ever New"}
     start_urls = [
         "https://www.evernew.ca/locator/index/search/?address=vancouver&components[country]=CA&radius=10000000&type=all",
     ]
