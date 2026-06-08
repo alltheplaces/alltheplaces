b4bc871462f6000bd62550d6e419aed7b2664dd2
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-06-08 19:17:27 +0530

    Fix Invitro RU Spider

--- locations/spiders/invitro_ru.py
+++ locations/spiders/invitro_ru.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
@@ -8,11 +12,16 @@ class InvitroRUSpider(SitemapSpider, StructuredDataSpider):
     name = "invitro_ru"
     item_attributes = {"brand": "Инвитро", "brand_wikidata": "Q4200546"}
     sitemap_urls = ["https://www.invitro.ru/sitemap/offices.xml"]
-    sitemap_rules = [(r"/offices/.*/clinic.php\?ID=.*", "parse_sd")]
+    sitemap_rules = [(r"/offices/[-\w]+", "parse")]
+    link_extractor = LinkExtractor(allow=r"/offices/[-\w]+/clinic.php\?ID=\d+$")
     wanted_types = ["MedicalBusiness"]
     json_parser = "chompjs"
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    custom_settings = {"ROBOTSTXT_OBEY": False, "DOWNLOAD_TIMEOUT": 300, "RETRY_TIMES": 5}
     requires_proxy = True
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for link in self.link_extractor.extract_links(response):
+            yield response.follow(url=link.url, callback=self.parse_sd)
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["name"] = None
