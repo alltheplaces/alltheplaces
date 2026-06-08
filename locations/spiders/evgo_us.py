2f6f0ba27d6fe04a92ddbea3ca84689a806ff503
Kevin Ratzel <scuff-unclip-jam@duck.com>
2026-06-08 09:49:50 -0400

    Refactor EvgoUSSpider by removing Camoufox integration

--- locations/spiders/evgo_us.py
+++ locations/spiders/evgo_us.py
@@ -1,43 +1,26 @@
-from typing import Any, AsyncIterator, Iterable, Iterator
+from typing import Any, Iterator
 
-from scrapy.http import Request, Response
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
-from scrapy_camoufox.page import PageMethod
 
-from locations.camoufox_spider import CamoufoxSpider
 from locations.categories import Categories, apply_category
 from locations.google_url import extract_google_position
 from locations.items import Feature
-from locations.settings import DEFAULT_CAMOUFOX_SETTINGS
 
 
-class EvgoUSSpider(SitemapSpider, CamoufoxSpider):
+class EvgoUSSpider(SitemapSpider):
     name = "evgo_us"
     item_attributes = {"brand": "EVgo", "brand_wikidata": "Q61803820"}
     sitemap_urls = ["https://evgo.com/find-a-charger/sites-sitemap.xml"]
     sitemap_rules = [(r"/find-a-charger/[^/]+/[^/]+/[^/]+-\d+/?$", "parse")]
     custom_settings = {
-        **DEFAULT_CAMOUFOX_SETTINGS,
-        "CAMOUFOX_ABORT_REQUEST": lambda request: request.resource_type not in ("document", "script", "xhr", "fetch"),
+        "DOWNLOAD_HANDLERS": {
+            "http": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+            "https": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+        },
+        "ZYTE_API_TRANSPARENT_MODE": True,
+        "ROBOTSTXT_OBEY": False,
     }
-
-    async def start(self) -> AsyncIterator[Any]:
-        yield Request(
-            "https://evgo.com/find-a-charger",
-            callback=self._after_warmup,
-            meta={
-                "camoufox_page_methods": [
-                    PageMethod("wait_for_load_state", "networkidle"),
-                ],
-                "handle_httpstatus_all": True,
-                "dont_retry": True,
-            },
-            dont_filter=True,
-        )
-
-    def _after_warmup(self, response: Response, **kwargs: Any) -> Iterable[Request]:
-        for url in self.sitemap_urls:
-            yield Request(url, callback=self._parse_sitemap, dont_filter=True)
 
     def parse(self, response: Response, **kwargs: Any) -> Iterator[Feature]:
         item = Feature()
@@ -48,8 +31,12 @@ class EvgoUSSpider(SitemapSpider, CamoufoxSpider):
         state = response.xpath("//ol/li[2]//a/text()").get()
         item["state"] = state.upper() if state else None
         title = response.xpath("//title/text()").get() or ""
-        item["addr_full"] = title.split(" | ", 1)[0].removeprefix("EVgo EV Charging Station in ") or None
-        capacity = response.xpath('//div[contains(@title, " stalls at this location")]/@title').get()
+        item["addr_full"] = (
+            title.split(" | ", 1)[0].removeprefix("EVgo EV Charging Station in ") or None
+        )
+        capacity = response.xpath(
+            '//div[contains(@title, " stalls at this location")]/@title'
+        ).get()
         if capacity:
             item["extras"]["capacity"] = capacity.removesuffix(" stalls at this location")
 
