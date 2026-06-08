6dea1b382fb00ee100773e052d46535ff41547eb
Kevin Ratzel <scuff-unclip-jam@duck.com>
2026-06-08 09:23:01 -0400

    Refactor EVgo spider to improve request handling

--- locations/spiders/evgo_us.py
+++ locations/spiders/evgo_us.py
@@ -1,7 +1,8 @@
-from typing import Any, Iterator
+from typing import Any, Iterable, Iterator
 
-from scrapy.http import Response
+from scrapy.http import Request, Response
 from scrapy.spiders import SitemapSpider
+from scrapy_camoufox.page import PageMethod
 
 from locations.camoufox_spider import CamoufoxSpider
 from locations.categories import Categories, apply_category
@@ -17,8 +18,25 @@ class EvgoUSSpider(SitemapSpider, CamoufoxSpider):
     sitemap_rules = [(r"/find-a-charger/[^/]+/[^/]+/[^/]+-\d+/?$", "parse")]
     custom_settings = {
         **DEFAULT_CAMOUFOX_SETTINGS,
-        "CAMOUFOX_ABORT_REQUEST": lambda request: request.resource_type not in ("document", "script", "xhr", "fetch"),
+        "CAMOUFOX_ABORT_REQUEST": lambda request: request.resource_type
+        not in ("document", "script", "xhr", "fetch"),
     }
+
+    def start_requests(self) -> Iterable[Request]:
+        yield Request(
+            "https://evgo.com/find-a-charger",
+            callback=self._after_warmup,
+            meta={
+                "camoufox_page_methods": [
+                    PageMethod("wait_for_load_state", "networkidle"),
+                ],
+            },
+            dont_filter=True,
+        )
+
+    def _after_warmup(self, response: Response, **kwargs: Any) -> Iterable[Request]:
+        for url in self.sitemap_urls:
+            yield Request(url, callback=self._parse_sitemap, dont_filter=True)
 
     def parse(self, response: Response, **kwargs: Any) -> Iterator[Feature]:
         item = Feature()
@@ -29,8 +47,12 @@ class EvgoUSSpider(SitemapSpider, CamoufoxSpider):
         state = response.xpath("//ol/li[2]//a/text()").get()
         item["state"] = state.upper() if state else None
         title = response.xpath("//title/text()").get() or ""
-        item["addr_full"] = title.split(" | ", 1)[0].removeprefix("EVgo EV Charging Station in ") or None
-        capacity = response.xpath('//div[contains(@title, " stalls at this location")]/@title').get()
+        item["addr_full"] = (
+            title.split(" | ", 1)[0].removeprefix("EVgo EV Charging Station in ") or None
+        )
+        capacity = response.xpath(
+            '//div[contains(@title, " stalls at this location")]/@title'
+        ).get()
         if capacity:
             item["extras"]["capacity"] = capacity.removesuffix(" stalls at this location")
 
