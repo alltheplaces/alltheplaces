02fadde8969d4051ff4814dd5a09ae9880c6fbfd
mohit-sharma2194 <sharmaji2194@gmail.com>
2026-06-08 14:28:49 +0530

    code updatd to fix spider

--- locations/spiders/great_wolf_resorts_us.py
+++ locations/spiders/great_wolf_resorts_us.py
@@ -4,21 +4,35 @@ from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import apply_category
-from locations.structured_data_spider import StructuredDataSpider
+from locations.google_url import extract_google_position
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class GreatWolfResortsUSSpider(SitemapSpider, StructuredDataSpider):
+class GreatWolfResortsUSSpider(SitemapSpider):
     name = "great_wolf_resorts_us"
     item_attributes = {"name": "Great Wolf Lodge", "brand": "Great Wolf Lodge", "brand_wikidata": "Q5600260"}
     allowed_domains = ["www.greatwolf.com"]
     sitemap_urls = ["https://www.greatwolf.com/php-root.sitemap.xml"]
-    sitemap_rules = [(r"/customer-service$", "parse_sd")]
+    sitemap_rules = [(r"/customer-service$", "parse")]
     custom_settings = {"ROBOTSTXT_OBEY": False}
 
-    def post_process_item(self, item: Any, response: Response, ld_data: dict, **kwargs: Any) -> Any:
-        item.pop("facebook")
-        item.pop("twitter")
-        item["branch"] = item.pop("name").removeprefix("Great Wolf Lodge ")
-
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath('//*[@property="og:site_name"]/@content').get()
+        item["street_address"] = response.xpath('//*[@class="cmp-text__content"]//p/text()').get()
+        item["addr_full"] = merge_address_lines(
+            [item["street_address"], response.xpath('//*[@class="cmp-text__content"]//p//text()').get()]
+        )
+        item["website"] = item["ref"] = response.url
+        extract_google_position(item, response)
         apply_category({"leisure": "water_park"}, item)
         yield item
+
+    # def post_process_item(self, item: Any, response: Response, ld_data: dict, **kwargs: Any) -> Any:
+    #     item.pop("facebook")
+    #     item.pop("twitter")
+    #     item["branch"] = item.pop("name").removeprefix("Great Wolf Lodge ")
+    #
+    #     apply_category({"leisure": "water_park"}, item)
+    #     yield item
