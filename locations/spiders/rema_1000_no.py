a55d9ec39e0694d0fc63d91052cb456fb4100490
Max Brosnahan <maximilianbrosnahan@gmail.com>
2026-06-10 20:38:43 +0200

    Add website URL to rema stores

--- locations/spiders/rema_1000_no.py
+++ locations/spiders/rema_1000_no.py
@@ -18,10 +18,42 @@ class Rema1000NOSpider(Spider):
         for url in self.start_urls:
             yield JsonRequest(url)
 
+    def build_store_website(self, location: dict, counties: dict) -> str | None:
+        if not location.get("slug") or not location.get("countyName") or not location.get("municipalityName"):
+            return None
+
+        county_slug = None
+        city_slug = None
+
+        for county in counties.values():
+            if county.get("name") != location["countyName"]:
+                continue
+
+            county_slug = county.get("slug")
+
+            for city in county.get("cities", {}).values():
+                if city.get("name") == location["municipalityName"]:
+                    city_slug = city.get("slug")
+                    break
+
+            break
+
+        if not county_slug or not city_slug:
+            return None
+
+        return "https://www.rema.no/butikker/{}/{}/{}".format(
+            county_slug,
+            city_slug,
+            location["slug"],
+        )
+
     def parse(self, response):
-        for location in response.json()["stores"]:
+        response_data = response.json()
+
+        for location in response_data["stores"]:
             item = DictParser.parse(location)
             item.pop("website", None)
+            item["website"] = self.build_store_website(location, response_data.get("counties", {}))
             item["street_address"] = location.get("visitAddress")
             item["city"] = location.get("visitPlaceName")
             item["postcode"] = location.get("visitPostCode")
--- tests/test_rema_1000_no_spider.py
+++ tests/test_rema_1000_no_spider.py
@@ -30,3 +30,43 @@ def test_parse_opening_hours_marks_closed_days():
     items = list(spider.parse(FakeResponse()))
 
     assert items[0]["opening_hours"].as_opening_hours() == "Mo 07:00-23:00; Su closed"
+
+
+def test_parse_builds_store_website():
+    spider = Rema1000NOSpider()
+
+    class WebsiteResponse:
+        def json(self):
+            return {
+                "stores": [
+                    {
+                        "id": 1493941,
+                        "name": "REMA 1000 SURNADAL",
+                        "slug": "rema-1000-surnadal",
+                        "countyName": "Møre og Romsdal",
+                        "municipalityName": "Surnadal",
+                        "visitAddress": "Some Street 1",
+                        "visitPostCode": "4817",
+                        "visitPlaceName": "SURNADAL",
+                        "latitude": "58.461",
+                        "longitude": "8.766",
+                        "openingHours": {},
+                    }
+                ],
+                "counties": {
+                    "more-og-romsdal": {
+                        "name": "Møre og Romsdal",
+                        "slug": "more-og-romsdal",
+                        "cities": {
+                            "surnadal": {
+                                "name": "Surnadal",
+                                "slug": "surnadal",
+                            }
+                        },
+                    }
+                },
+            }
+
+    items = list(spider.parse(WebsiteResponse()))
+
+    assert items[0]["website"] == "https://www.rema.no/butikker/more-og-romsdal/surnadal/rema-1000-surnadal"
