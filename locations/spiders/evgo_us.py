37bbc445b486a6eb30ff9a28d0416c94b1a67946
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-06-10 15:49:00 +0530

    fix spider

--- locations/spiders/evgo_us.py
+++ locations/spiders/evgo_us.py
@@ -1,6 +1,6 @@
-from typing import Any, Iterable, Iterator
+from typing import Any, Iterator
 
-from scrapy.http import Request, Response
+from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
@@ -8,33 +8,32 @@ from locations.google_url import extract_google_position
 from locations.items import Feature
 from locations.playwright_spider import PlaywrightSpider
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class EvgoUSSpider(SitemapSpider, PlaywrightSpider):
     name = "evgo_us"
     item_attributes = {"brand": "EVgo", "brand_wikidata": "Q61803820"}
     sitemap_urls = ["https://evgo.com/find-a-charger/sites-sitemap.xml"]
-    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
-
-    def parse_sitemap(self, response: Response) -> Iterable[Request]:
-        for request in super()._parse_sitemap(response):
-            request.meta["playwright"] = True
-            request.meta["playwright_include_page"] = True
-            yield request
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT} | DEFAULT_PLAYWRIGHT_SETTINGS
 
     def parse(self, response: Response, **kwargs: Any) -> Iterator[Feature]:
+        # Skip the root overview page if it comes through the sitemap
+        if response.url.strip("/").endswith("find-a-charger"):
+            return
+
         item = Feature()
         item["website"] = response.url
         item["ref"] = response.url.rsplit("-", 1)[1].strip("/")
         item["branch"] = response.xpath("//h1/text()").get()
         item["street_address"] = response.xpath("//ol/li[last()]//span/text()").get()
-        item["state"] = response.xpath("//ol/li[2]//a/text()").get().upper()
+        item["state"] = response.xpath("//ol/li[2]//a/text()").get("").upper()
         item["addr_full"] = (
             response.xpath("//title/text()").get().split(" | ", 1)[0].removeprefix("EVgo EV Charging Station in ")
         )
         item["extras"]["capacity"] = (
             response.xpath('//div[contains(@title, " stalls at this location")]/@title')
-            .get()
+            .get("")
             .removesuffix(" stalls at this location")
         )
 
