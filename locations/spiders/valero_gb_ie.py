4a5a4995fc0738a4fe83b0e76b534f737eef4925
Cj Malone <CjMalone@mail.com>
2026-06-10 15:35:34 +0100

    Trim

--- locations/spiders/valero_gb_ie.py
+++ locations/spiders/valero_gb_ie.py
@@ -3,8 +3,6 @@ from locations.spiders.valero import ValeroSpider
 
 class ValeroGBIESpider(ValeroSpider):
     name = "valero_gb_ie"
-    item_attributes = {"brand": "Valero", "brand_wikidata": "Q1283291"}
-
     usa_bbox = [-10, 49, 2, 61]
     xstep = 20
     ystep = 20
