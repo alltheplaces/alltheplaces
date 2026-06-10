934a01c20f5be34816015c38480d0a167e767a6d
Cj Malone <CjMalone@mail.com>
2026-06-10 14:44:09 +0100

    Count logos

--- locations/spiders/valero_gb_ie.py
+++ locations/spiders/valero_gb_ie.py
@@ -4,7 +4,6 @@ from locations.spiders.valero import ValeroSpider
 class ValeroGBIESpider(ValeroSpider):
     name = "valero_gb_ie"
     item_attributes = {"brand": "Valero", "brand_wikidata": "Q1283291"}
-    allowed_domains = ["valero.com"]
 
     usa_bbox = [-10, 49, 2, 61]
     xstep = 20
