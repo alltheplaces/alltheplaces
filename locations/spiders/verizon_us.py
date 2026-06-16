8be12f9d7a221e133ec504f5b07e42bb3646ef1d
Ehsan Firdaus <ehsanfirdaus@gmail.com>
2026-06-16 20:12:54 +0530

    [Verizon US] Fix spider to cover all consumer stores (#16682)
    Co-authored-by: Ehsan Firdaus <ehsan.firdaus@gmail.com>
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/verizon_us.py
+++ locations/spiders/verizon_us.py
@@ -1,47 +1,79 @@
-import json
+from typing import AsyncIterator, Iterable
 
-from scrapy.http import TextResponse
-from scrapy.spiders import SitemapSpider
+from scrapy.http import JsonRequest, TextResponse
 
 from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
+from locations.geo import country_iseadgg_centroids
+from locations.hours import DAYS_3_LETTERS, OpeningHours
 from locations.items import Feature
-from locations.pipelines.address_clean_up import merge_address_lines
-from locations.structured_data_spider import StructuredDataSpider
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class VerizonUSSpider(SitemapSpider, StructuredDataSpider):
+class VerizonUSSpider(JSONBlobSpider):
     name = "verizon_us"
     item_attributes = {"brand": "Verizon", "brand_wikidata": "Q919641"}
-    sitemap_urls = ["https://www.verizon.com/business/locations/sitemap.xml"]
-    sitemap_rules = [(r"https://www.verizon.com/business/locations/[^/]+/[^/]+/[^/]+/$", "parse")]
+    operators = {
+        "ABC Phones of North Carolina, Inc.": ("Victra", "Q118402656"),
+        "BeMobile, Inc": ("BeMobile", None),
+        "Cellular Sales Management Group LLC via its affili": ("Cellular Sales", "Q5058345"),
+        "Credico (USA) LLC": ("Credico", None),
+        "Cydcor LLC": ("Cydcor", None),
+        "Gee Papa Enterprises, Inc.": ("Team Wireless", None),
+        "LCM Enterprises Inc.": ("Smartmart", None),
+        "Mobile Generation, LLC": ("Mobile Generation", None),
+        "R Wireless Inc": ("R Wireless", None),
+        "Russell Cellular, Inc.": ("Russell Cellular", "Q125523800"),
+        "The Cellular Connection, LLC": ("The Cellular Connection", "Q121336519"),
+        "UNITED TELECOM USA, INC. DBA YOUR WIRELESS": ("Your Wireless", None),
+        "Verizon Wireless": ("Verizon", "Q919641"),
+        "Wireless Plus, Inc.": ("Wireless Plus", None),
+        "Wireless Zone, LLC": ("Wireless Zone", "Q122517436"),
+    }
+    locations_key = ["body", "data", "stores"]
 
-    def parse(self, response: TextResponse, **kwargs):
-        if street_address := response.xpath('//*[@class="vbg-address-text"]/strong/text()').get():
-            item = Feature()
-            item["branch"] = (
-                response.xpath("//title//text()")
-                .get()
-                .removeprefix("Verizon Business Services - ")
-                .removesuffix("| Verizon")
+    async def start(self) -> AsyncIterator[JsonRequest]:
+        for lat, lon in country_iseadgg_centroids("US", 458):
+            yield JsonRequest(
+                url="https://www.verizon.com/digital/nsa/nos/gw/retail/searchresultsdata",
+                data={
+                    "latitude": lat,
+                    "longitude": lon,
+                    "range": 10000,
+                    "noOfStores": 2500,
+                    "excludeIndirect": False,
+                    "retrieveBy": "GEO",
+                },
             )
-            item["street_address"] = street_address
-            item["addr_full"] = merge_address_lines(
-                response.xpath('//*[@class="vbg-address-text"]//text()').getall()
-            ).replace("Address, ", "")
-            item["lat"] = response.xpath("//@data-lat").get()
-            item["lon"] = response.xpath("//@data-lng").get()
-            item["ref"] = item["website"] = response.url
-            apply_category(Categories.SHOP_MOBILE_PHONE, item)
-            day_time_data = response.xpath("//@data-hours-json").get()
-            oh = OpeningHours()
-            if data := json.loads(day_time_data):
-                for day, time in data.items():
-                    day = day
-                    if time in ["Closed", "Closed Closed"]:
-                        oh.set_closed(day)
-                    else:
-                        open_time, close_time = time.split("-")
-                        oh.add_range(day, open_time.strip(), close_time.strip(), "%I:%M %p")
-            item["opening_hours"] = oh
-            yield item
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["ref"] = feature["storeNumber"]
+        item["branch"] = item.pop("name", None)
+        if website := item.get("website"):
+            item["website"] = (
+                f'https://www.verizon.com/nextgendigital/nos/storelocator/detail/{website.removeprefix("/stores/")}'
+            )
+
+        if business_name := feature.get("businessName"):
+            if operator_info := self.operators.get(business_name):
+                item["operator"], item["operator_wikidata"] = operator_info
+                if item["operator"] in ["Victra", "The Cellular Connection"]:  # Covered by Victra & Tcc US Spiders
+                    return
+            else:
+                self.logger.warning(f"Unknown operator for business name: {business_name}")
+
+        try:
+            item["opening_hours"] = self.parse_opening_hours(feature)
+        except Exception as e:
+            self.logger.error(f"Error parsing opening hours: {e}")
+        apply_category(Categories.SHOP_MOBILE_PHONE, item)
+        yield item
+
+    def parse_opening_hours(self, feature: dict) -> OpeningHours:
+        opening_hours = OpeningHours()
+        for day in DAYS_3_LETTERS:
+            if hours := feature.get(f"hours{day}"):
+                if "Closed" in hours:
+                    opening_hours.set_closed(day)
+                    continue
+                opening_hours.add_range(day, *hours.strip().replace("M ", "M-").split("-"), "%I:%M %p")
+        return opening_hours
