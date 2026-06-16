3251e2284f546ab328eddf709e82d4985dc3a4c1
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-06-16 17:15:51 +0530

    Add opening hours for Colonial Parking US Spider

--- locations/spiders/colonial_parking_us.py
+++ locations/spiders/colonial_parking_us.py
@@ -5,6 +5,7 @@ from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
 from locations.items import Feature
 
 
@@ -28,4 +29,15 @@ class ColonialParkingUSSpider(Spider):
     def parse_location_details(self, response: Response, item: Feature) -> Iterable[Feature]:
         item["addr_full"] = response.xpath('//*[@class="adresse"]/span/text()').get()
         item["phone"] = response.xpath('//*[contains(text(), "Phone")]/following-sibling::span/text()').get()
+        item["opening_hours"] = self.parse_opening_hours(response)
         yield item
+
+    def parse_opening_hours(self, response: Response) -> OpeningHours:
+        opening_hours = OpeningHours()
+        for rule in (
+            response.xpath('//*[contains(@class, "openhours")]//p[@class="intend"]')
+            .xpath("normalize-space(.)")
+            .getall()
+        ):
+            opening_hours.add_ranges_from_string(rule.replace("from 12:00 AM to 12:00 AM", "from 12:00 AM to 11:59 PM"))
+        return opening_hours
