4c42cda73ac63bf9ebca81f547438d6835e4b3ad
vinzdb <vinzdb@gmail.com>
2026-06-17 11:57:21 +0200

    Add OTP banka HR opening hours

--- locations/spiders/otp_banka_hr.py
+++ locations/spiders/otp_banka_hr.py
@@ -5,6 +5,7 @@ from scrapy import Spider
 from scrapy.http import Request, Response
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.hours import DAYS_HR, OpeningHours
 from locations.items import Feature
 
 
@@ -27,6 +28,7 @@ class OtpBankaHRSpider(Spider):
             item = self.parse_location(row, response)
 
             if location_type == "branch":
+                self.parse_hours(item, row)
                 apply_yes_no(
                     Extras.WHEELCHAIR,
                     item,
@@ -53,6 +55,51 @@ class OtpBankaHRSpider(Spider):
                 continue
 
             yield item
+
+    def parse_hours(self, item: Feature, row: Any) -> None:
+        rules = row.xpath(
+            './/*[contains(@class, "views-field-field-radno-vrijeme")]'
+            '//*[contains(concat(" ", normalize-space(@class), " "), " oh-display ")]'
+        )
+        if not rules:
+            return
+
+        days_hr = DAYS_HR | {
+            "Pon": "Mo",
+            "Uto": "Tu",
+            "Sri": "We",
+            "\u010cet": "Th",
+            "Pet": "Fr",
+            "Sub": "Sa",
+            "Ned": "Su",
+        }
+        oh = OpeningHours()
+        try:
+            for rule in rules:
+                day = days_hr[
+                    rule.xpath(
+                        'normalize-space(.//*[contains(concat(" ", normalize-space(@class), " "), " oh-display-label ")])'
+                    )
+                    .get("")
+                    .rstrip(":")
+                    .strip()
+                ]
+                hours = (
+                    rule.xpath(
+                        'normalize-space(.//*[contains(concat(" ", normalize-space(@class), " "), " oh-display-times ")])'
+                    )
+                    .get("")
+                    .strip()
+                )
+                if hours == "Zatvoreno":
+                    oh.set_closed(day)
+                elif "-" in hours:
+                    oh.add_range(day, *hours.split("-", 1))
+                else:
+                    raise ValueError("Unexpected opening hours value: {}".format(hours))
+            item["opening_hours"] = oh
+        except Exception:
+            self.logger.warning("Failed to parse hours for {}".format(item.get("ref")))
 
     @staticmethod
     def parse_location(row: Any, response: Response) -> Feature:
