e4cfa61986d4b18e325a84068c347f58c0c368e7
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-17 16:20:37 +0530

    code refactored to fix spider

--- locations/spiders/go_outdoors_gb.py
+++ locations/spiders/go_outdoors_gb.py
@@ -1,51 +1,41 @@
 import json
-from typing import Any, AsyncIterator
-from urllib.parse import urljoin
+from typing import Any
 
-from scrapy.http import FormRequest, Response
+from scrapy.http import JsonRequest, Response
 from scrapy.spiders import Spider
 
 from locations.dict_parser import DictParser
-from locations.hours import DAYS, OpeningHours
+from locations.hours import OpeningHours
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class GoOutdoorsGBSpider(Spider):
     name = "go_outdoors_gb"
     item_attributes = {"brand": "Go Outdoors", "brand_wikidata": "Q75293941"}
-
-    async def start(self) -> AsyncIterator[FormRequest]:
-        yield FormRequest(
-            url="https://www.gooutdoors.co.uk/google/store-locator",
-            formdata={"ac_store_limit": "300"},
-        )
-
-    def find_between(self, text, first, last):
-        start = text.index(first) + len(first)
-        end = text.index(last, start)
-        return text[start:end]
+    start_urls = ["https://www.gooutdoors.co.uk/pages/stores"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        data = self.find_between(response.text, ",stores = ", ",smarty_postcode =")
-        json_data = json.loads(data)
-        for store in json_data:
-            item = DictParser.parse(store)
-            item["addr_full"].replace("<br />", ",")
-            item["website"] = urljoin("https://www.gooutdoors.co.uk/stores/", store["filename"])
+        for location in json.loads(
+            response.xpath('//*[@id="app-store-locator"]//script[@type="application/json"]/text()').get()
+        )["stores"]:
+            yield JsonRequest(
+                url=f"https://extensions.gooutdoors.co.uk/api/stores/{location['id']}", callback=self.parse_details
+            )
 
-            item["opening_hours"] = OpeningHours()
-            for rule in store["opening_times_data"].split(","):
-                day, ohour, omin, chour, cmin = rule.split(":")
-                day = int(day)
-                open = ohour + ":" + omin
-                close = chour + ":" + cmin
-                DAYS_FROM_SUNDAY = DAYS[-1:] + DAYS[:-1]
-                item["opening_hours"].add_range(DAYS_FROM_SUNDAY[day], open, close)
-
-            if item["name"].startswith("GO Express "):
-                item["branch"] = item.pop("name").removeprefix("GO Express ")
-                item["name"] = "Go Outdoors Express"
-            else:
-                item["branch"] = item.pop("name").removeprefix("GO Outdoors ")
-                item["name"] = "Go Outdoors"
+    def parse_details(self, response: Response):
+        data = response.json().get("store")
+        if data:
+            item = DictParser.parse(data)
+            item["branch"] = item.pop("name").replace("GO Outdoors ", "")
+            item["state"] = data["big_region"]
+            item["phone"] = data["local_phone"]
+            item["street_address"] = merge_address_lines([data["address_2"], data["address_1"]])
+            oh = OpeningHours()
+            for day, time in data.get("hours_sets", "").get("primary", "").get("days").items():
+                for open_close_time in time:
+                    open_time = open_close_time["open"]
+                    close_time = open_close_time["close"]
+                    oh.add_range(day=day, open_time=open_time, close_time=close_time)
+            item["opening_hours"] = oh
 
             yield item
