f5c9d46c0e6eca7acf8094741f5668ce1feba568
IceBurger <79398013+sharmaanurag95@users.noreply.github.com>
2026-06-18 08:52:05 +0530

    [RidleysFamilyMarkets] Fix spider (#16882)

--- locations/spiders/ridleys_family_markets.py
+++ locations/spiders/ridleys_family_markets.py
@@ -1,33 +1,31 @@
-import scrapy
+from typing import AsyncIterator, Iterable
 
-from locations.categories import Categories
+from scrapy import Request
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class RidleysFamilyMarketsSpider(scrapy.Spider):
+class RidleysFamilyMarketsSpider(JSONBlobSpider):
     name = "ridleys_family_markets"
-    item_attributes = {
-        "brand": "Ridley's Family Markets",
-        "brand_wikidata": "Q7332999",
-        "extras": Categories.SHOP_SUPERMARKET.value,
-    }
-    start_urls = ["https://shopridleys.com/_ajax_map.php"]
+    item_attributes = {"brand": "Ridley's Family Markets", "brand_wikidata": "Q7332999"}
 
-    def parse(self, response):
-        results = response.json()
-        for data in results:
-            props = {
-                "ref": data.get("storeNumber"),
-                "street_address": data.get("streetAddress"),
-                "name": data.get("storeName"),
-                "city": data.get("city"),
-                "postcode": data.get("zip"),
-                "state": data.get("state"),
-                "website": data.get("rio_url"),
-                "phone": data.get("phone"),
-                "lat": data.get("lat"),
-                "lon": data.get("lon"),
-                "country": "US",
-            }
+    async def start(self) -> AsyncIterator[Request]:
+        yield Request(
+            url="https://shopridleys.com/wp-admin/admin-ajax.php",
+            body="action=imm_sections_get_stores_ajax&nonce=43299e3cf7&lat=41.3080357&lng=-105.5557724&city=&count=100&all_store=yes",
+            method="POST",
+        )
 
-            yield Feature(**props)
+    def extract_json(self, response: Response) -> list[dict]:
+        return response.json().get("data", {}).get("data", {}).get("message", [])
+
+    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
+        item["country"] = "US"
+        item["street_address"] = item.pop("addr_full")
+        item["branch"] = feature.get("DisplayName")
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+
+        yield item
