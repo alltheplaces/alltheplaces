7f8ec5aaf9ebb0c9de6535dd9e4a45b0e3d4ce04
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-18 10:46:05 +0530

    code refactored to fix spider

--- locations/spiders/great_american_cookies_us.py
+++ locations/spiders/great_american_cookies_us.py
@@ -1,13 +1,37 @@
-from locations.storefinders.wp_store_locator import WPStoreLocatorSpider
+from typing import Any
+
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
+
+from locations.dict_parser import DictParser
 
 
-class GreatAmericanCookiesUSSpider(WPStoreLocatorSpider):
+class GreatAmericanCookiesUSSpider(Spider):
     name = "great_american_cookies_us"
     item_attributes = {
         "brand_wikidata": "Q5598629",
         "brand": "Great American Cookies",
     }
-    allowed_domains = [
-        "www.greatamericancookies.com",
-    ]
-    time_format = "%I:%M %p"
+
+    async def start(self):
+        yield JsonRequest(
+            url="https://ordernew-admin.greatamericancookies.com/api/v1/restaurants/", headers={"projectid": "1"}
+        )
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location_id in response.json():
+            yield JsonRequest(
+                url=f"https://ordernew-admin.greatamericancookies.com/api/v1/restaurants/{location_id['id']}",
+                headers={"projectid": "1"},
+                callback=self.parse_details,
+            )
+
+    def parse_details(self, response: Response):
+        data = response.json()
+        if data:
+            data.update(data.pop("address"))
+            item = DictParser.parse(data)
+            item["branch"] = item.pop("name")
+            item["street_address"] = item.pop("street")
+            item["website"] = "https://ordernew.greatamericancookies.com/gac/" + data["location_slug"]
+            yield item
