4e9f2c7c92c6da4fab9d3ddf1f91f89528e93a7f
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-19 13:23:14 +0530

    code updated to fix spider

--- locations/spiders/brandywine_living.py
+++ locations/spiders/brandywine_living.py
@@ -16,14 +16,16 @@ class BrandywineLivingSpider(scrapy.Spider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         raw_data = re.search(
             r"locations\":(\[.*\])},\"locationData",
-            response.xpath('//*[contains(text(),"addresscity")]/text()').get().replace("\\", ""),
+            response.xpath('//*[contains(text(),"postal_code")]/text()').get().replace("\\", ""),
         ).group(1)
         for location in json.loads(raw_data):
+            location.update(location.pop("address"))
             item = DictParser.parse(location)
+            item["street_address"] = location["address_1"]
+            item["website"] = "https://www.brandycare.com/community/" + item["name"].lower().replace(
+                "community - ", ""
+            ).replace(" ", "-")
             item["ref"] = location["communityid"]
-            item["lat"] = location["addresslatitude"]
-            item["lon"] = location["addresslongitude"]
-
             apply_category(Categories.NURSING_HOME, item)
 
             yield item
