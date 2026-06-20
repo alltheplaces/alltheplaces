078aa43bdf336be5d61c5c949ad1c9ce54d4043c
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-06-20 15:32:39 +0900

    add branch name

--- locations/spiders/arcland.py
+++ locations/spiders/arcland.py
@@ -18,12 +18,18 @@ class ArclandSpider(LocationCloudSpider):
         match source_feature["categories"][0]["name"]:
             case "かつや":
                 item["brand_wikidata"] = "Q2855257"
+                item["branch"] = source_feature.get("name").removeprefix("かつや ")
+                item["extras"]["branch:ja-Hira"] = source_feature.get("ruby").removeprefix("カツヤ ")
                 apply_category(Categories.FAST_FOOD, item)
             case "からやま":
                 item["brand_wikidata"] = "Q96145071"
+                item["branch"] = source_feature.get("name").removeprefix("からやま ")
+                item["extras"]["branch:ja-Hira"] = source_feature.get("ruby").removeprefix("カラヤマ ")
                 apply_category(Categories.RESTAURANT, item)
             case _:
                 item["brand"] = source_feature["categories"][0]["name"]
+                item["branch"] = source_feature.get("name")
+                item["extras"]["branch:ja-Hira"] = source_feature.get("ruby")
                 apply_category(Categories.RESTAURANT, item)
 
         yield item
