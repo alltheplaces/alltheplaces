6b211ca84747885afd60bb930eda7be7ac3e68dd
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-22 00:22:09 +0530

    [Brandywine Living] Fix Spider (#16916)
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

--- locations/spiders/brandywine_living_us.py
+++ locations/spiders/brandywine_living_us.py
@@ -1,6 +1,7 @@
 import json
 import re
 from typing import Any
+from urllib.parse import quote
 
 import scrapy
 from scrapy.http import Response
@@ -9,21 +10,22 @@ from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 
 
-class BrandywineLivingSpider(scrapy.Spider):
-    name = "brandywine_living"
+class BrandywineLivingUSSpider(scrapy.Spider):
+    name = "brandywine_living_us"
     start_urls = ["https://www.brandycare.com/company/locations/"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         raw_data = re.search(
             r"locations\":(\[.*\])},\"locationData",
-            response.xpath('//*[contains(text(),"addresscity")]/text()').get().replace("\\", ""),
+            response.xpath('//*[contains(text(),"postal_code")]/text()').get().replace("\\", ""),
         ).group(1)
         for location in json.loads(raw_data):
+            location.update(location.pop("address"))
             item = DictParser.parse(location)
+            item["street_address"] = location["address_1"]
+            slug = item["name"].lower().replace("community - ", "").replace(" ", "-")
+            item["website"] = "https://www.brandycare.com/community/" + quote(slug, safe="-")
             item["ref"] = location["communityid"]
-            item["lat"] = location["addresslatitude"]
-            item["lon"] = location["addresslongitude"]
-
             apply_category(Categories.NURSING_HOME, item)
 
             yield item
