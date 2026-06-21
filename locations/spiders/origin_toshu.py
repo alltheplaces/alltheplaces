0ec731a76008678e5dd451f22cfd8163a2957f43
Ian Dees <ian.dees@gmail.com>
2026-06-21 13:36:34 -0500

    Guard against None name and ruby in origin_toshu spider

--- locations/spiders/origin_toshu.py
+++ locations/spiders/origin_toshu.py
@@ -11,35 +11,37 @@ class OriginToshuSpider(LocationCloudSpider):
     website_formatter = "https://shop.toshu.co.jp/toshu/spot/detail?code={}"
 
     def post_process_feature(self, item: Feature, source_feature: dict, **kwargs) -> Iterable[Feature]:
+        name = source_feature.get("name") or ""
         match source_feature["categories"][0]["name"]:
             case "キッチンオリジン":
                 item["brand_wikidata"] = "Q92658990"
-                item["branch"] = source_feature.get("name").removeprefix("キッチンオリジン ")
+                item["branch"] = name.removeprefix("キッチンオリジン ") or None
                 if ruby := source_feature.get("ruby"):
                     item["extras"]["branch:ja-Hira"] = ruby.removeprefix("きっちんおりじん ")
                 apply_category(Categories.FAST_FOOD, item)
             case "れんげ食堂Toshu":
                 item["brand_wikidata"] = "Q92662226"
-                item["branch"] = source_feature.get("name").removeprefix("れんげ食堂Toshu ")
+                item["branch"] = name.removeprefix("れんげ食堂Toshu ") or None
                 if ruby := source_feature.get("ruby"):
                     item["extras"]["branch:ja-Hira"] = ruby.removeprefix("れんげしょくどうとうしゅう ")
                 apply_category(Categories.RESTAURANT, item)
             case "オリジンデリカ":
                 item["brand"] = "オリジンデリカ"
-                item["branch"] = source_feature.get("name").removeprefix("オリジンデリカ ")
+                item["branch"] = name.removeprefix("オリジンデリカ ") or None
                 if ruby := source_feature.get("ruby"):
                     item["extras"]["branch:ja-Hira"] = ruby.removeprefix("おりじんでりか ")
                 apply_category(Categories.FAST_FOOD, item)
             case "オリジン弁当":
                 item["brand_wikidata"] = "Q11292632"
-                item["branch"] = source_feature.get("name").removeprefix("オリジン弁当 ")
+                item["branch"] = name.removeprefix("オリジン弁当 ") or None
                 if ruby := source_feature.get("ruby"):
                     item["extras"]["branch:ja-Hira"] = ruby.removeprefix("おりじんべんとう ")
                 apply_category(Categories.FAST_FOOD, item)
             case _:
                 item["brand"] = source_feature["categories"][0]["name"]
-                item["branch"] = source_feature.get("name")
-                item["extras"]["branch:ja-Hira"] = source_feature.get("ruby")
+                item["branch"] = name or None
+                if ruby := source_feature.get("ruby"):
+                    item["extras"]["branch:ja-Hira"] = ruby
                 apply_category(Categories.RESTAURANT, item)
 
         yield item
