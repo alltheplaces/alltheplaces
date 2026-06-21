7545eeadf4f6b1614e5a110e69891fdf7d91207c
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-06-22 04:12:39 +0900

    Arcland Service Holdings (JP) create spider (#16949)
    Co-authored-by: Ian Dees <ian.dees@gmail.com>

---
+++ locations/spiders/arcland.py
@@ -0,0 +1,40 @@
+from typing import Iterable
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.location_cloud import LocationCloudSpider
+
+
+class ArclandSpider(LocationCloudSpider):
+    name = "arcland"
+    skip_auto_cc_domain = True
+    api_endpoint = "https://shop.arclandservice.co.jp/ae-shop/api/proxy2/shop/list"
+    website_formatter = "https://shop.arclandservice.co.jp/ae-shop/spot/detail?code={}"
+
+    def post_process_feature(self, item: Feature, source_feature: dict, **kwargs) -> Iterable[Feature]:
+        if "os" in source_feature["code"]:
+            return  # skip places with fake locations
+
+        match source_feature["categories"][0]["name"]:
+            case "かつや":
+                item["brand_wikidata"] = "Q2855257"
+                if name := source_feature.get("name"):
+                    item["branch"] = name.removeprefix("かつや ")
+                if ruby := source_feature.get("ruby"):
+                    item["extras"]["branch:ja-Hira"] = ruby.removeprefix("カツヤ ")
+                apply_category(Categories.FAST_FOOD, item)
+            case "からやま":
+                item["brand_wikidata"] = "Q96145071"
+                if name := source_feature.get("name"):
+                    item["branch"] = name.removeprefix("からやま ")
+                if ruby := source_feature.get("ruby"):
+                    item["extras"]["branch:ja-Hira"] = ruby.removeprefix("カラヤマ ")
+                apply_category(Categories.RESTAURANT, item)
+            case _:
+                item["brand"] = source_feature["categories"][0]["name"]
+                item["branch"] = source_feature.get("name")
+                if ruby := source_feature.get("ruby"):
+                    item["extras"]["branch:ja-Hira"] = ruby
+                apply_category(Categories.RESTAURANT, item)
+
+        yield item
