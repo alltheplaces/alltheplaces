a8bda9ed5d76503a0f210687fa3676a6526326ae
IceBurger <79398013+sharmaanurag95@users.noreply.github.com>
2026-06-21 08:12:40 +0530

    [KeyFood US] Fix spider (#16903)

--- locations/spiders/key_food_us.py
+++ locations/spiders/key_food_us.py
@@ -1,69 +1,83 @@
-from typing import AsyncIterator
+from typing import Any, AsyncIterator
 
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy import Request, Spider
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS, OpeningHours
-from locations.pipelines.address_clean_up import clean_address
+from locations.geo import bbox_contains, country_iseadgg_centroids
+from locations.hours import OpeningHours
 
 
 class KeyFoodUSSpider(Spider):
     name = "key_food_us"
-    allowed_domains = ["keyfoodstores.keyfood.com"]
-    start_urls = [
-        "https://keyfoodstores.keyfood.com/store/keyFood/en/store-locator?q=90210&page=0&radius=100000&all=true"
-    ]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    allowed_domains = ["www.keyfood.com"]
+
     brands = {
-        "https://ernestklein.keyfood.com/store": {"brand": "Ernest Klein"},
-        "https://fooddynasty.keyfood.com/store": {"brand": "Food Dynasty"},
-        "https://foodemporium.keyfood.com/store": {"brand": "The Food Emporium"},
-        "https://fooduniverse.keyfood.com/store": {"brand": "Food Universe"},
-        "https://galafoods.keyfood.com/store": {"brand": "Gala Foods"},
-        "https://galafresh.keyfood.com/store": {"brand": "GalaFresh Farms"},
-        "https://halseytradersmarket.keyfood.com/store": {"brand": "Halsey Traders Market"},
-        "https://keyfoodmarketplace.keyfood.com/store": {"brand": "Key Food Marketplace"},
-        "https://keyfoodstores.keyfood.com/store": {"brand": "Key Food", "brand_wikidata": "Q6398037"},
-        "https://marketplace.keyfood.com/store": {"brand": "Marketplace"},
-        "https://superfresh.keyfood.com/store": {"brand": "SuperFresh"},
-        "https://tropicalsupermarket.keyfood.com/store": {"brand": "Tropical Supermarket"},
-        "https://urbanmarketplace.keyfood.com/store": {"brand": "Key Food Urban Marketplace"},
-        "https://www.keyfood.com/store": {"brand": "Key Food", "brand_wikidata": "Q6398037"},
+        "Ernest Klein": {"brand": "Ernest Klein"},
+        "Food Dynasty": {"brand": "Food Dynasty"},
+        "Food Emporium": {"brand": "The Food Emporium"},
+        "Food Universe": {"brand": "Food Universe"},
+        "Gala Foods": {"brand": "Gala Foods"},
+        "Gala Fresh": {"brand": "GalaFresh Farms"},
+        "GalaFresh": {"brand": "GalaFresh Farms"},
+        "Halsey Traders": {"brand": "Halsey Traders Market"},
+        "Key Food Marketplace": {"brand": "Key Food Marketplace"},
+        "Urban Market": {"brand": "Key Food Urban Marketplace"},
+        "Tropical Supermarket": {"brand": "Tropical Supermarket"},
+        "SuperFresh": {"brand": "SuperFresh"},
+        "Key Food": {"brand": "Key Food", "brand_wikidata": "Q6398037"},
     }
 
-    async def start(self) -> AsyncIterator[JsonRequest]:
-        for url in self.start_urls:
-            yield JsonRequest(url=url, meta={"page": 0})
+    async def start(self) -> AsyncIterator[Request]:
+        northeast_bbox = (-81.0, 38.0, -69.0, 45.0)  # Covers PA, NJ, NY, CT, MA, RI
+        florida_bbox = (-88.0, 24.0, -79.0, 31.0)  # Covers Florida
 
-    def parse(self, response):
-        for location in response.json()["data"]:
-            if location.get("comingSoon") != "false":
-                continue
+        for lat, lon in country_iseadgg_centroids(["US"], 48):
+            point = (lon, lat)
+            # Only yield a request if the centroid falls inside our target regions
+            if bbox_contains(northeast_bbox, point) or bbox_contains(florida_bbox, point):
+                url = f"https://www.keyfood.com/?lat={lat}&lng={lon}&miles=30&_data=root"
+                yield JsonRequest(url=url)
 
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for store in response.json().get("storeList", {}).get("stores", []):
+            location = store.get("location", {})
             item = DictParser.parse(location)
-            item["ref"] = location["name"]
-            item["name"] = location["displayName"]
-            item["street_address"] = clean_address([location["line1"], location["line2"]])
-            item["website"] = location["siteUrl"] + location["url"].split("?", 1)[0]
+            item["ref"] = store.get("storeId")
+            item["name"] = store.get("displayName").removesuffix(item.get("street_address")).strip()
 
-            if brand := self.brands.get(location["siteUrl"]):
-                item.update(brand)
+            if phones := store.get("phoneNumbers", []):
+                item["phone"] = phones[0].get("value")
+
+            found_match = False
+            for b_key, b_data in self.brands.items():
+                if b_key.lower() in item["name"].lower():
+                    item.update(b_data)
+                    found_match = True
+                    break
+
+            if not found_match:
+                item.update(self.brands["Key Food"])
 
             apply_category(Categories.SHOP_SUPERMARKET, item)
 
             item["opening_hours"] = OpeningHours()
-            if location.get("open24Hours"):
-                item["opening_hours"].add_days_range(DAYS, "00:00", "23:59")
-            else:
-                for day_name, day_hours in location["openings"].items():
-                    item["opening_hours"].add_range(day_name, *day_hours.split(" - ", 1), "%I:%M %p")
+            weekly_hours = store.get("hours", {}).get("weekly", [])
+            for day_hours in weekly_hours:
+                day_name = day_hours.get("day")
+                if not day_name:
+                    continue
+
+                daily = day_hours.get("daily", {})
+                h_type = daily.get("type")
+
+                if h_type == "OPEN_24_HOURS":
+                    item["opening_hours"].add_range(day_name.title(), "00:00", "23:59")
+                elif h_type == "OPEN":
+                    open_time = daily.get("open", {}).get("open")
+                    close_time = daily.get("open", {}).get("close")
+                    if open_time and close_time:
+                        item["opening_hours"].add_range(day_name.title(), open_time[:5], close_time[:5])
 
             yield item
-
-        next_page = response.meta["page"] + 1
-        if next_page * 250 < response.json()["total"]:
-            yield JsonRequest(
-                url=self.start_urls[0].replace("&page=0", "&page=" + str(next_page)), meta={"page": next_page}
-            )
