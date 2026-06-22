394b4f916e9ed39d020e5ca04d3b1ea5b79eedca
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-06-22 23:30:41 +0900

    [Ecbo Cloak] add more details from API, use city locations
    To get more locations this uses city locations rather than iseadgg centroids. It works better for dense cities like Tokyo. Also, I renamed the spider so it accurately displays the country name.

--- locations/spiders/ecbo_cloak.py
+++ locations/spiders/ecbo_cloak.py
@@ -3,23 +3,28 @@ from typing import AsyncIterator, Iterable
 from scrapy import Spider
 from scrapy.http import JsonRequest, Response
 
-from locations.geo import country_iseadgg_centroids
-from locations.hours import DAYS, OpeningHours
+from locations.categories import Extras, apply_yes_no
+from locations.geo import city_locations
 from locations.items import Feature
 
 
-class EcboCloakJPSpider(Spider):
-    name = "ecbo_cloak_jp"
+class EcboCloakSpider(Spider):
+    name = "ecbo_cloak"
     item_attributes = {"brand": "ecbo cloak"}
+    skip_auto_cc_domain = True
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._seen: set[str] = set()
 
     async def start(self) -> AsyncIterator[JsonRequest]:
-        for lat, lon in country_iseadgg_centroids(["JP", "TW"], 24):
+        for city in city_locations("JP", 10000):
             yield JsonRequest(
-                url=f"https://search.ecbo.io/api/v1/search?latitude={lat}&longitude={lon}",
+                url=f"https://search.ecbo.io/api/v1/search?latitude={city['latitude']}&longitude={city['longitude']}",
+            )
+        for city in city_locations("TW", 10000):
+            yield JsonRequest(
+                url=f"https://search.ecbo.io/api/v1/search?latitude={city['latitude']}&longitude={city['longitude']}",
             )
 
     def parse(self, response: Response, **kwargs) -> Iterable[Feature]:
@@ -32,17 +37,24 @@ class EcboCloakJPSpider(Spider):
             loc = location.get("location", {})
             item = Feature()
             item["ref"] = ref
-            item["name"] = location.get("en_name") or location.get("name")
+            item["name"] = location.get("name") or location.get("en_name")
+            item["extras"]["name:en"] = location.get("en_name")
             item["lat"] = loc.get("latitude")
             item["lon"] = loc.get("longitude")
             item["website"] = f"https://cloak.ecbo.io/ja/space/{ref}"
+            item["extras"]["website:en"] = f"https://cloak.ecbo.io/en/space/{ref}"
+            item["extras"]["website:ja"] = f"https://cloak.ecbo.io/ja/space/{ref}"
+            item["extras"]["website:zh-TW"] = f"https://cloak.ecbo.io/zh-TW/space/{ref}"
+            item["image"] = location.get("main_image_url")
 
             basic = location.get("basic_info", {})
 
-            oh = OpeningHours()
             if basic.get("available_24_hour"):
-                oh.add_days_range(DAYS, "00:00", "23:59")
-            item["opening_hours"] = oh
+                item["opening_hours"] = "24/7"
+            apply_yes_no("elevator", item, basic.get("has_elevator"))
+            apply_yes_no("socket:nema_1_15", item, basic.get("has_charge"))
+            apply_yes_no("language:en", item, basic.get("available_in_english"))
+            apply_yes_no(Extras.WIFI, item, basic.get("has_wifi"))
 
             location_type = location.get("type")
             if location_type == "space":
