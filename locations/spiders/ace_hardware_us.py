22c5067d1bb67e3418a8294e5862d9f466ccc14f
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-23 12:15:44 +0530

    trying with playwright

--- locations/spiders/ace_hardware_us.py
+++ locations/spiders/ace_hardware_us.py
@@ -1,9 +1,11 @@
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.storefinders.kibo import KiboSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class AceHardwareUSSpider(KiboSpider):
+class AceHardwareUSSpider(KiboSpider, PlaywrightSpider):
     name = "ace_hardware_us"
     item_attributes = {"brand": "Ace Hardware", "brand_wikidata": "Q4672981"}
     start_urls = ["https://www.acehardware.com/api/commerce/storefront/locationUsageTypes/SP/locations"]
-    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
