0b0853168e0187383624cfb09214d78d86ad8eb3
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-06-23 20:47:09 +0900

    skip warehouses, add phone

--- locations/spiders/nitori_jp.py
+++ locations/spiders/nitori_jp.py
@@ -14,7 +14,11 @@ class NitoriJPSpider(LocationCloudSpider):
     website_formatter = "https://shop.nitori-net.jp/nitori/spot/detail?code={}"
 
     def post_process_feature(self, item: Feature, source_feature: dict, **kwargs) -> Iterable[Feature]:
+        if source_feature["categories"][0]["code"] == "03":
+            return  # skip logistics centers
+        
         item["branch"] = source_feature.get("name", "")
         item["extras"]["branch:ja-Hira"] = source_feature.get("ruby", "")
+        item["phone"] = f"+81 {source_feature.get('phone', '')}"
 
         yield item
