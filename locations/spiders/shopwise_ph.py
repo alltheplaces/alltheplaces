0add826196823e2e01b1547a980e9b445a50a5af
Cj Malone <CjMalone@mail.com>
2026-06-23 16:41:34 +0100

    [Shopwise PH] Only calculate regions once

--- locations/spiders/shopwise_ph.py
+++ locations/spiders/shopwise_ph.py
@@ -1,38 +1,38 @@
 import re
-from typing import AsyncIterator
+from typing import Any
 
 from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy.http import Response
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 
 
 class ShopwisePHSpider(Spider):
     name = "shopwise_ph"
-    item_attributes = {"brand": "Shopwise", "brand_wikidata": "Q118674375", "extras": Categories.SHOP_SUPERMARKET.value}
+    item_attributes = {"brand": "Shopwise", "brand_wikidata": "Q118674375"}
     allowed_domains = ["api.shopwise.com.ph"]
     start_urls = ["https://api.shopwise.com.ph/api/web/our-stores?site_id=2"]
 
-    async def start(self) -> AsyncIterator[JsonRequest]:
-        for url in self.start_urls:
-            yield JsonRequest(url=url)
-
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        ph_regions = {
+            region["id"]: re.sub(r"^Region [\w\-]+ \(([^\)]+)\)$", r"\1", region["name"]).title()
+            for region in response.json()["perRegions"]
+        }
         for location in response.json()["branches"]:
             if not location.get("enabled"):
                 continue
             item = DictParser.parse(location)
+            item["branch"] = item.pop("name")
             item["phone"] = re.sub(
                 r"<[^>]+>", "", location["phone"].split(" loc", 1)[0].split(";", 1)[0].split("/", 1)[0]
             )
-            ph_regions = {
-                region["id"]: re.sub(r"^Region [\w\-]+ \(([^\)]+)\)$", r"\1", region["name"]).title()
-                for region in response.json()["perRegions"]
-            }
             item["state"] = ph_regions[location["region_id"]]
             hours_string = "Mo-Su: " + re.sub(r"<[^>]+>", "", location.get("store_hours", ""))
             item["opening_hours"] = OpeningHours()
             item["opening_hours"].add_ranges_from_string(hours_string)
+
+            apply_category(Categories.SHOP_SUPERMARKET, item)
+
             yield item
