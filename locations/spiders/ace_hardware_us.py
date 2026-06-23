e31aec4e83ed532917bed8d49f4a4b740c47dbdc
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-23 12:08:39 +0530

    rename spider

--- locations/spiders/ace_hardware_us.py
+++ locations/spiders/ace_hardware_us.py
@@ -2,8 +2,8 @@ from locations.storefinders.kibo import KiboSpider
 from locations.user_agents import BROWSER_DEFAULT
 
 
-class AceHardwareSpider(KiboSpider):
-    name = "ace_hardware"
+class AceHardwareUSSpider(KiboSpider):
+    name = "ace_hardware_us"
     item_attributes = {"brand": "Ace Hardware", "brand_wikidata": "Q4672981"}
     start_urls = ["https://www.acehardware.com/api/commerce/storefront/locationUsageTypes/SP/locations"]
     custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
