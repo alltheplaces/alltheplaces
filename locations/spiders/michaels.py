42454b0a9d38ba133bb818f8cddcc29197c88c2f
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-25 10:51:22 +0530

    remove redundant lines of code

--- locations/spiders/michaels.py
+++ locations/spiders/michaels.py
@@ -38,31 +38,3 @@ class MichaelsSpider(SitemapSpider):
         item["opening_hours"] = oh
 
         yield item
-
-    # api_brand_name = "michaelsca"
-    # api_key = "288FB6BC-71EE-11E1-AA2E-C7DA57283E85"
-    #
-    # def parse_item(self, item, location):
-    #     item["state"] = location["state"]
-    #     if item["country"] == "US":
-    #         item["website"] = (
-    #             "https://locations.michaels.com/"
-    #             + item["state"].lower()
-    #             + "/"
-    #             + item["city"].lower().replace(" ", "-")
-    #             + "/"
-    #             + item["ref"]
-    #         )
-    #     item["opening_hours"] = OpeningHours()
-    #     if location.get("mon_sat") and location.get("mon_sat").upper() != "CLOSED":
-    #         item["opening_hours"].add_days_range(
-    #             ["Mo", "Tu", "We", "Th", "Fr", "Sa"],
-    #             location.get("mon_sat").split(" - ", 1)[0],
-    #             location.get("mon_sat").split(" - ", 1)[1],
-    #             "%I:%M%p",
-    #         )
-    #     if location.get("sun") and location.get("sun").upper() != "CLOSED":
-    #         item["opening_hours"].add_range(
-    #             "Su", location.get("sun").split(" - ", 1)[0], location.get("sun").split(" - ", 1)[1], "%I:%M%p"
-    #         )
-    #     yield item
