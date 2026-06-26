3a0bc752ddaa688963eae6ef6272acf4f62d95df
Cj Malone <CjMalone@mail.com>
2026-06-26 08:39:08 +0100

    Clean name

--- locations/spiders/jd_sports_pt.py
+++ locations/spiders/jd_sports_pt.py
@@ -1,6 +1,11 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -9,3 +14,8 @@ class JdSportsPTSpider(CrawlSpider, StructuredDataSpider):
     item_attributes = {"brand": "JD Sports", "brand_wikidata": "Q6108019"}
     start_urls = ["https://www.jdsports.pt/store-locator/all-stores/"]
     rules = [Rule(LinkExtractor(allow=r"store-locator/[^/]+/\d+/$"), callback="parse_sd")]
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+        apply_category(Categories.SHOP_CLOTHES, item)
+        yield item
