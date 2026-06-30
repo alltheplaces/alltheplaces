ff1c1fb79d7ca99ced25d880b5704420ee759c9d
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-06-30 15:17:56 +0530

    [Caseys General Store] Fix Spider (#17131)
    Co-authored-by: Cj Malone <Cj-Malone@users.noreply.github.com>
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/caseys_general_store_us.py
+++ locations/spiders/caseys_general_store_us.py
@@ -1,16 +1,21 @@
+import json
 from typing import Any, AsyncIterator
 
-from scrapy import Spider
 from scrapy.http import JsonRequest, TextResponse
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.geo import city_locations
 from locations.hours import OpeningHours
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 
 
-class CaseysGeneralStoreSpider(Spider):
-    name = "caseys_general_store"
+class CaseysGeneralStoreUSSpider(PlaywrightSpider):
+    name = "caseys_general_store_us"
     item_attributes = {"brand": "Casey's General Store", "brand_wikidata": "Q2940968"}
+    requires_proxy = True
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
 
     async def start(self) -> AsyncIterator[Any]:
         url = "https://www.caseys.com/api/graphql"
@@ -93,7 +98,7 @@ class CaseysGeneralStoreSpider(Spider):
             )
 
     def parse(self, response: TextResponse, **kwargs):
-        for location in response.json()["data"]["storesByCoordinate"]:
+        for location in json.loads(response.xpath("//pre//text()").get())["data"]["storesByCoordinate"]:
             location.update(location.pop("store"))
             item = DictParser.parse(location)
             item.pop("name")
@@ -103,4 +108,5 @@ class CaseysGeneralStoreSpider(Spider):
             for day_time in location["storeOpenHours"]:
                 oh.add_ranges_from_string(day_time)
             item["opening_hours"] = oh
+            apply_category(Categories.SHOP_CONVENIENCE, item)
             yield item
