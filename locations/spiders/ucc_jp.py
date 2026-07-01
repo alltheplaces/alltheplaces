1cef79cb4b5be7399a0a655f79872ce7110e4721
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-07-01 10:23:10 +0900

    [UCC JP] remove duplicate, fix branding
    removes duplicate spider, limits Ueshima Coffee to only those with that brand. The other brands under the UCC umbrella will no longer appear as Ueshima Coffee locations, but as their own brands.

--- locations/spiders/ucc_jp.py
+++ locations/spiders/ucc_jp.py
@@ -7,15 +7,23 @@ from locations.storefinders.location_cloud import LocationCloudSpider
 
 class UccJPSpider(LocationCloudSpider):
     name = "ucc_jp"
-    item_attributes = {"brand": "上島珈琲店", "brand_wikidata": "Q96152143"}
     api_endpoint = "https://shop.ufs.co.jp/ufs/api/proxy2/shop/list"
     website_formatter = "https://shop.ufs.co.jp/ufs/spot/detail?code={}"
 
     def post_process_feature(self, item: Feature, source_feature: dict, **kwargs) -> Iterable[Feature]:
 
-        item["branch"] = source_feature["name"].removeprefix("上島珈琲店　")
-        if ruby := source_feature.get("ruby"):
-            item["extras"]["branch:ja-Hira"] = ruby.removeprefix("UESHIMA COFFEE ")
+        match source_feature["categories"][0]["code"]:
+            case "01":
+                item["brand"] = "上島珈琲店"
+                item["brand_wikidata"] = "Q96152143"
+                item["branch"] = source_feature["name"].removeprefix("上島珈琲店　")
+                if ruby := source_feature.get("ruby"):
+                    item["extras"]["branch:ja-Hira"] = ruby.removeprefix("UESHIMA COFFEE ")
+            case _:
+                item["brand"] = source_feature["categories"][0]["name"]
+                item["branch"] = source_feature.get("name")
+                if ruby := source_feature.get("ruby"):
+                    item["extras"]["branch:ja-Hira"] = ruby
 
         apply_category(Categories.COFFEE_SHOP, item)
 
