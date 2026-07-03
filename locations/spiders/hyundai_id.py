1bc37228e616b2417fb4a1783fe237af1451566a
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-03 13:45:04 +0530

    added parse method to fix spider

--- locations/spiders/hyundai_id.py
+++ locations/spiders/hyundai_id.py
@@ -1,11 +1,14 @@
-from typing import AsyncIterator
+from copy import deepcopy
+from typing import Any, AsyncIterator
 
-from scrapy.http import FormRequest
+from scrapy import Spider
+from scrapy.http import FormRequest, Response
 
-from locations.spiders.hyundai_th import HyundaiTHSpider
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 
 
-class HyundaiIDSpider(HyundaiTHSpider):
+class HyundaiIDSpider(Spider):
     name = "hyundai_id"
     allowed_domains = ["www.hyundai.com"]
     start_urls = ["https://www.hyundai.com/wsvc/template_en/spa/common/dealer/list.html"]
@@ -33,3 +36,29 @@ class HyundaiIDSpider(HyundaiTHSpider):
             "search_dealer_type": "D",
         }
         yield FormRequest(url=self.start_urls[0], method="POST", formdata=formdata, headers=headers)
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for data in response.json():
+            item = Feature()
+            item["ref"] = data.get("dealer_seq")
+            item["branch"] = data.get("dealer_name").removeprefix("Hyundai ")
+            item["lat"] = data.get("latitude")
+            item["lon"] = data.get("longtitude")
+            item["addr_full"] = data.get("dealer_address")
+            item["postcode"] = data.get("dealer_post_code").removesuffix(".0")
+            item["phone"] = data.get("dealer_phone1", "").removeprefix("Tel ")
+            services = data.get("dealer_service_nm", "")
+            has_showroom = "Sales" in services
+            has_service = "Service and Spare Part" in services
+
+            if has_showroom:
+                sales_item = deepcopy(item)
+                sales_item["ref"] = f"{item['ref']}-sales"
+                apply_category(Categories.SHOP_CAR, sales_item)
+                yield sales_item
+
+            if has_service:
+                service_item = deepcopy(item)
+                service_item["ref"] = f"{item['ref']}-service"
+                apply_category(Categories.SHOP_CAR_REPAIR, service_item)
+                yield service_item
