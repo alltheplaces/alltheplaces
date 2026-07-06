5f07197061d5b726cd24b86433a0d0beb722ecc2
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-06 13:25:53 +0530

    code refactored to use uberall spider

--- locations/spiders/aldi_sud_hu.py
+++ locations/spiders/aldi_sud_hu.py
@@ -1,35 +1,17 @@
-from scrapy import Spider
+from typing import Iterable
+
+from scrapy.http import TextResponse
 
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
-from locations.hours import DAYS_HU, OpeningHours, sanitise_day
+from locations.items import Feature
+from locations.storefinders.uberall import UberallSpider
 
 
-class AldiSudHUSpider(Spider):
+class AldiSudHUSpider(UberallSpider):
     name = "aldi_sud_hu"
     item_attributes = {"brand_wikidata": "Q41171672"}
-    start_urls = [
-        "https://www.aldi.hu/hu/hu/.get-stores-in-radius.json?latitude=47.162494&longitude=19.503304&radius=2500000"
-    ]
+    key = "rDCKKjdtbi2w0Qx3Cq1axERNtccFqZ"
 
-    def parse(self, response):
-        for store in response.json()["stores"]:
-            item = DictParser.parse(store)
-            if store["storeType"] == "N":
-                continue
-            if item["country"] != "HU":
-                continue
-            item["name"] = None  # ref
-
-            item["website"] = store.get("url")
-
-            oh = OpeningHours()
-            for rule in store["openUntilSorted"]["openingHours"]:
-                day = sanitise_day(rule["day"], DAYS_HU)
-                if not rule.get("closed", False):
-                    oh.add_range(day, rule["openFormatted"], rule["closeFormatted"])
-            item["opening_hours"] = oh
-
-            apply_category(Categories.SHOP_SUPERMARKET, item)
-
-            yield item
+    def post_process_item(self, item: Feature, response: TextResponse, location: dict) -> Iterable[Feature]:
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+        yield item
