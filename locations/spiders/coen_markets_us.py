1cf0488ca3a587445a1a46c1b94bbb4901f97106
Cj Malone <CjMalone@mail.com>
2026-07-07 13:06:13 +0100

    apply_category

--- locations/spiders/coen_markets_us.py
+++ locations/spiders/coen_markets_us.py
@@ -1,6 +1,11 @@
-from scrapy import Spider
+from typing import Any
 
+from scrapy import Spider
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class CoenMarketsUSSpider(Spider):
@@ -9,17 +14,17 @@ class CoenMarketsUSSpider(Spider):
     allowed_domains = ["www.coenmarkets.com"]
     start_urls = ["https://www.coenmarkets.com/locations/?location_city=10000&zip_radius=10000"]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for marker in response.xpath(
-            '//*[@class = "location-archive-map"]//*[contains(@class, "acf-map-list-item acf-map-list-item")]'
+            '//*[@class="location-archive-map"]//*[contains(@class, "acf-map-list-item acf-map-list-item")]'
         ):
             item = Feature()
-            item["name"] = marker.xpath(".//h5/text()").get()
-            item["lat"] = marker.xpath('.//*[@class = "location-lat"]/text()').get()
-            item["lon"] = marker.xpath('.//*[@class = "location-long"]/text()').get()
+            item["lat"] = marker.xpath('.//*[@class="location-lat"]/text()').get()
+            item["lon"] = marker.xpath('.//*[@class="location-long"]/text()').get()
             item["ref"] = marker.xpath(".//@data-id").get()
             item["street_address"] = marker.xpath('.//p[contains(@class, "location-address")]/text()').get()
-            item["addr_full"] = (
-                marker.xpath('.//p[contains(@class, "location-address")]').xpath("normalize-space()").get()
+            item["addr_full"] = merge_address_lines(
+                marker.xpath('.//p[contains(@class, "location-address")]/text()').getall()
             )
+            apply_category(Categories.SHOP_CONVENIENCE, item)
             yield item
