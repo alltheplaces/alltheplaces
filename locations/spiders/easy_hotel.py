02917ca46f972d6903381976327ce5da28473977
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-07 16:25:24 +0530

    code updated and rename spider

---
+++ locations/spiders/easy_hotel.py
@@ -0,0 +1,24 @@
+from typing import Iterable
+
+import scrapy
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class EasyHotelSpider(SitemapSpider, StructuredDataSpider):
+    name = "easy_hotel"
+    item_attributes = {"brand": "easyHotel", "brand_wikidata": "Q17011598"}
+    sitemap_urls = ["https://www.easyhotel.com/sitemap.xml"]
+    sitemap_rules = [(r"https:\/\/www\.easyhotel\.com\/hotels\/[^/]+/[^/]+$", "parse")]
+    wanted_types = ["Hotel"]
+
+    def parse(self, response: TextResponse, **kwargs):
+        for link in response.xpath('//*[@data-test-id="hotel-card"]/@href').getall():
+            yield scrapy.Request(url=response.urljoin(link), callback=self.parse_sd)
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["addr_full"] = response.xpath('//*[@data-test-id="location__address"]/p/text()').get()
+        yield item
