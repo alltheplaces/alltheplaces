526ff171f4b22f95be5e94a19324340b5f0ae106
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-08 13:38:58 +0530

    code refactored to fix spider

--- locations/spiders/f_hinds_gb.py
+++ locations/spiders/f_hinds_gb.py
@@ -1,26 +1,44 @@
-from typing import Iterable
+from typing import Any
 from urllib.parse import urljoin
 
 import scrapy
-from scrapy.http import TextResponse
+from scrapy import Spider
+from scrapy.http import Response
 
+from locations.google_url import extract_google_position
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class FHindsGBSpider(StructuredDataSpider):
+class FHindsGBSpider(Spider):
     name = "f_hinds_gb"
     item_attributes = {"brand": "F.Hinds", "brand_wikidata": "Q5423915"}
     start_urls = [
         "https://www.fhinds.co.uk/store-locator?view_all=true",
     ]
 
-    def parse(self, response):
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         locations = response.xpath('//*[@class="store-title"]//@href').getall()
         for location in locations:
             url = urljoin("https://www.fhinds.co.uk", location)
-            yield scrapy.Request(url=url, callback=self.parse_sd)
+            yield scrapy.Request(url=url, callback=self.parse_location)
 
-    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        item["branch"] = item.pop("name").replace("F.Hinds the Jewellers, ", "")
+    def parse_location(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath("//h1/text()").get().replace("F.Hinds the Jewellers, ", "")
+        item["street_address"] = response.xpath('//*[@class="container store-details-template"]//p/text()').get()
+        item["city"] = response.xpath('//*[@class="container store-details-template"]//p/text()[2]').get()
+        item["state"] = response.xpath('//*[@class="container store-details-template"]//p/text()[3]').get()
+        item["postcode"] = response.xpath('//*[@class="container store-details-template"]//p/text()[5]').get()
+        item["ref"] = item["website"] = response.url
+        extract_google_position(item, response)
+        oh = OpeningHours()
+        item["opening_hours"] = oh
+        hours_text = " ".join(
+            response.xpath(
+                "//h3[normalize-space()='Opening Hours']/following-sibling::p[1]//text()[normalize-space()]"
+            ).getall()
+        )
+        oh.add_ranges_from_string(hours_text)
+        item["opening_hours"] = oh
         yield item
