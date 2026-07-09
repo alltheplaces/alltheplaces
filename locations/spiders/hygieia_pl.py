325e15dfb6262bc2f37e6ec00555dc08e8adcb9f
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-09 15:42:38 +0530

    code updated to fix spider

--- locations/spiders/hygieia_pl.py
+++ locations/spiders/hygieia_pl.py
@@ -4,7 +4,6 @@ from scrapy import Spider
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS_PL, OpeningHours
 from locations.items import Feature
 
 
@@ -12,20 +11,17 @@ class HygieiaPLSpider(Spider):
     name = "hygieia_pl"
     item_attributes = {"brand": "Hygieia", "brand_wikidata": "Q122906354"}
     allowed_domains = ["www.hygieia.pl"]
-    start_urls = ["https://www.hygieia.pl/apteki.html"]
+    start_urls = ["https://www.hygieia.pl/apteki/"]
+    no_refs = True
 
     def parse(self, response: Response) -> Iterable[Feature]:
-        for store in response.xpath('//div[contains(@class, "media-body")]'):
-            properties = {
-                "ref": store.xpath('.//a[contains(@class, "link")]/@href').get().removeprefix("https://goo.gl/maps/"),
-                "street_address": store.xpath("./h3/text()").get(),
-                "city": store.xpath("./p[1]/text()").get(),
-                "phone": store.xpath('.//i[contains(@class, "fa-phone")]/parent::p/text()').get(),
-                "email": store.xpath('.//i[contains(@class, "fa-envelope")]/parent::p/text()').get(),
-                "opening_hours": OpeningHours(),
-            }
-            hours_string = " ".join(store.xpath('.//i[contains(@class, "fa-clock")]/parent::p//text()').getall())
-            properties["opening_hours"].add_ranges_from_string(hours_string, days=DAYS_PL)
-            apply_category(Categories.PHARMACY, properties)
-            properties["extras"]["ref:google:place_id"] = properties["ref"]
-            yield Feature(**properties)
+        for store in response.xpath('//*[contains(@class," mfn-module-wrapper mfn-wrapper-for-wraps")]'):
+            if title := store.xpath(".//h4/text()").get():
+                item = Feature()
+                item["name"] = self.item_attributes["brand"]
+                item["branch"] = title
+                item["street_address"] = store.xpath(".//p/text()").get()
+                item["phone"] = store.xpath('.//*[contains(@href,"tel:")]/@href').get().replace("tel:", "")
+                item["email"] = store.xpath('.//*[contains(@href,"mailto:")]/@href').get().replace("mailto:", "")
+                apply_category(Categories.PHARMACY, item)
+                yield item
