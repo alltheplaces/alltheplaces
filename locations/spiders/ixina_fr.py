b9524dfea56e90d405fe153b374cffe62c9919f2
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-10 11:39:59 +0530

    updated urls to fix spider

--- locations/spiders/ixina_fr.py
+++ locations/spiders/ixina_fr.py
@@ -1,12 +1,20 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class IxinaFRSpider(SitemapSpider, StructuredDataSpider):
     name = "ixina_fr"
     item_attributes = {"brand": "Ixina", "brand_wikidata": "Q3156424"}
-    sitemap_urls = ["https://magasins.ixina.fr/sitemap.xml"]
+    sitemap_urls = ["https://www.ixina.fr/pages/sitemap.xml"]
     sitemap_rules = [
-        (r"https://magasins.ixina.fr/fr/magasins/[\w-]+/[\w-]+/[\w-]+", "parse"),
+        (r"/trouvez-votre-magasin/magasin-de.*", "parse_sd"),
     ]
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").replace("Magasin ixina ", "")
+        yield item
