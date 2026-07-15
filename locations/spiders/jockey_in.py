2265048c1667e6889964f4123d424c665447163d
Cj Malone <CjMalone@mail.com>
2026-07-15 10:35:55 +0100

    [Jockey] Add wikidata

--- locations/spiders/jockey_in.py
+++ locations/spiders/jockey_in.py
@@ -10,13 +10,14 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class JockeyINSpider(SitemapSpider, StructuredDataSpider):
     name = "jockey_in"
-    item_attributes = {"name": "Jockey", "brand": "Jockey"}
+    item_attributes = {"brand": "Jockey", "brand_wikidata": "Q534235"}
     sitemap_urls = ["https://stores.jockey.in/sitemaps/v1/google/index-10001.xml"]
     sitemap_rules = [(r"jockey-exclusive-store-[\w\-/]+/home", "parse_sd")]
     time_format = "%I:%M %p"
+    wanted_types = ["ClothingStore"]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs: Any) -> Iterable[Feature]:
-        item["name"] = None
+        item["name"] = item["name"].split("|", 1)[1].strip()
         if "stores.jockey.in" in item["facebook"] or "com/jockeyindia" in item["facebook"]:
             item["facebook"] = None
         apply_category(Categories.SHOP_CLOTHES, item)
