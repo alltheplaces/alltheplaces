eb1b4e4ec27a19e16e1cbd4ace3d9516b35d2956
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-16 15:03:37 +0530

    code updated to use crawl spider to fix spider

--- locations/spiders/beobank_be.py
+++ locations/spiders/beobank_be.py
@@ -1,34 +1,46 @@
 from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class BeobankBESpider(SitemapSpider, StructuredDataSpider):
+class BeobankBESpider(CrawlSpider, StructuredDataSpider):
     name = "beobank_be"
     item_attributes = {"brand": "Beobank", "brand_wikidata": "Q14911971"}
-    allowed_domains = ["www.beobank.be"]
-    sitemap_urls = ["https://www.beobank.be/fr/sitemap.xml"]
-    sitemap_rules = [
-        (r"/nl/kantoor/vlaams-gewest/.+/\d+\.html$", "parse_sd"),
-        (r"/fr/agence/region-wallonne/.+/\d+\.html$", "parse_sd"),
-        (r"/fr/agence/region-de-bruxelles-capitale/.+/\d+\.html$", "parse_sd"),
+    # allowed_domains = ["www.beobank.be"]
+    start_urls = ["https://www.beobank.be/fr/agences/regions.html"]
+    rules = [
+        Rule(LinkExtractor(restrict_xpaths='//*[@class="ei_richlinkmenuitem"]')),
+        Rule(
+            LinkExtractor(
+                allow=r"https://www.beobank.be/fr/agences/ResultatsRechercheGeographique.aspx?inseeCode=\d+&postalCode=\d+"
+            )
+        ),
+        Rule(
+            LinkExtractor(allow=r"https://www.beobank.be/fr/agence/region-[^/]+/[^/]+/\d+\.html$"), callback="parse_sd"
+        ),
     ]
+    custom_settings = {"ROBOTSTXT_OBEY": False}
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
-        item["branch"] = (
-            item.pop("name").removeprefix("BEOBANK AG ").removeprefix("BEOBANK BR ").removeprefix("BEOBANK PRO CENTER ")
-        )
-        item["ref"] = response.url.split("/")[-1].replace(".html", "")
-        item["country"] = "BE"
+        if ld_data.get("geo"):
+            item["branch"] = (
+                item.pop("name")
+                .removeprefix("BEOBANK AG ")
+                .removeprefix("BEOBANK BR ")
+                .removeprefix("BEOBANK PRO CENTER ")
+            )
+            item["ref"] = response.url
+            item["country"] = "BE"
 
-        services_text = " ".join(response.xpath("//table[@role='presentation']//p/text()").getall())
+            services_text = " ".join(response.xpath("//table[@role='presentation']//p/text()").getall())
 
-        apply_yes_no(Extras.ATM, item, "opname" in services_text.lower() or "retrait" in services_text.lower())
-        apply_yes_no(Extras.CASH_IN, item, "storting" in services_text.lower() or "dépôt" in services_text.lower())
-        apply_yes_no("parking", item, "parking" in services_text.lower())
+            apply_yes_no(Extras.ATM, item, "opname" in services_text.lower() or "retrait" in services_text.lower())
+            apply_yes_no(Extras.CASH_IN, item, "storting" in services_text.lower() or "dépôt" in services_text.lower())
+            apply_yes_no("parking", item, "parking" in services_text.lower())
 
-        apply_category(Categories.BANK, item)
-        yield item
+            apply_category(Categories.BANK, item)
+            yield item
