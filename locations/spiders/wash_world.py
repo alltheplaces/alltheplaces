aa04a4e3acb164df3d070a6e3fc54aa040f3180b
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-16 12:00:49 +0530

    code rafctored to fix spider and extended the coverage for SE,NO,DE

---
+++ locations/spiders/wash_world.py
@@ -0,0 +1,31 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class WashWorldSpider(SitemapSpider, StructuredDataSpider):
+    name = "wash_world"
+    item_attributes = {"brand": "Wash World", "brand_wikidata": "Q130249954"}
+    sitemap_urls = [
+        "https://washworld.dk/pages-sitemap.xml",
+        "https://washworld.se/pages-sitemap.xml",
+        "https://washworld.de/pages-sitemap.xml",
+        "https://washworld.no/pages-sitemap.xml",
+    ]
+    sitemap_rules = [
+        ("/find-wash-world-vaskehal/", "parse_sd"),
+        ("/hitta-biltvatt/", "parse_sd"),
+        ("/waschanlagen/", "parse_sd"),
+        ("/finn-vaskehall/", "parse_sd"),
+    ]
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").replace("Wash World ", "")
+        item["addr_full"] = item.pop("street_address")
+        apply_category(Categories.CAR_WASH, item)
+        yield item
