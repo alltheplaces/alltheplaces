dbc60748b239b4e69b00fe988c3f27662a805316
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-07-21 13:51:27 +0000

    [La Place] Add spider (#17428)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ locations/spiders/la_place.py
@@ -0,0 +1,23 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class LaPlaceSpider(SitemapSpider, StructuredDataSpider):
+    name = "la_place"
+    item_attributes = {"brand": "La Place", "brand_wikidata": "Q2041183"}
+    sitemap_urls = ["https://www.laplace.com/sitemap.xml"]
+    sitemap_rules = [("/locaties/la-place-", "parse")]
+    search_for_facebook = False
+    search_for_twitter = False
+    wanted_types = ["Restaurant"]
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("La Place ")
+        apply_category(Categories.RESTAURANT, item)
+        yield item
