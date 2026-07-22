5a7377330696abe415071f49d529639f126fd7d0
Cj Malone <CjMalone@mail.com>
2026-07-22 11:38:30 +0100

    simplfy

--- locations/spiders/douglas_pl.py
+++ locations/spiders/douglas_pl.py
@@ -5,15 +5,16 @@ from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_PL, OpeningHours
+from locations.hours import DAYS, OpeningHours
 from locations.playwright_spider import PlaywrightSpider
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.spiders.douglas_de import DOUGLAS_SHARED_ATTRIBUTES
 from locations.user_agents import BROWSER_DEFAULT
 
 
 class DouglasPLSpider(PlaywrightSpider):
     name = "douglas_pl"
-    item_attributes = {"brand": "Douglas", "brand_wikidata": "Q2052213"}
+    item_attributes = DOUGLAS_SHARED_ATTRIBUTES
     allowed_domains = ["www.douglas.pl"]
     start_urls = ["https://www.douglas.pl/api/v2/stores?accuracy=0&currentPage=0&fields=FULL&pageSize=10000&sort=asc"]
     custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
@@ -21,15 +22,17 @@ class DouglasPLSpider(PlaywrightSpider):
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for data in json.loads(response.xpath("//pre//text()").get()).get("stores"):
             data.update(data.pop("address"))
-            data["id"] = data.pop("name")
             item = DictParser.parse(data)
-            item["branch"] = item.pop("name")
-            item["name"] = self.item_attributes["brand"]
-            item["website"] = "https://{}{}".format(self.allowed_domains[0], data.pop("url"))
+            item["phone"] = None
+            item["ref"] = item.pop("name")
+            item["branch"] = data["displayName"]
+            item["website"] = response.urljoin(data.get("url"))
+
             apply_category(Categories.SHOP_PERFUMERY, item)
+
             item["opening_hours"] = OpeningHours()
             for day_hours in data["openingHours"]["weekDayOpeningList"]:
-                day_abbrev = DAYS_PL[day_hours["weekDayFull"].title()]
+                day_abbrev = DAYS[day_hours["dayOfWeek"] - 1]
                 if day_hours["closed"]:
                     item["opening_hours"].set_closed(day_abbrev)
                 else:
