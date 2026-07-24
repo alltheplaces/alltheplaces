6151d19dcb548dc8f4d778af2f53a452643cb643
Cj Malone <CjMalone@mail.com>
2026-07-24 14:29:53 +0100

    [Auntie Anne's] Collect more attributes

--- locations/spiders/auntie_annes.py
+++ locations/spiders/auntie_annes.py
@@ -1,5 +1,8 @@
+from scrapy import Selector
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, Extras, apply_category, apply_yes_no
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -11,6 +14,26 @@ class AuntieAnnesSpider(SitemapSpider, StructuredDataSpider):
     sitemap_rules = [(r"https://locations\.auntieannes\.com/\w\w/[-.\w]+/[-–\w]+$", "parse_sd")]
     drop_attributes = {"image"}
 
+    def extract_amenity_features(self, item: Feature | dict, selector: Selector, ld_item: dict) -> None:
+        if features := ld_item.get("amenityFeature"):
+            if isinstance(features, str):
+                features = [features]
+            apply_yes_no(Extras.DELIVERY, item, "Delivery" in features)
+            apply_yes_no(Extras.DRIVE_THROUGH, item, "Drive-Thru" in features)
+
     def post_process_item(self, item, response, ld_data, **kwargs):
-        item["name"] = response.xpath('//meta[@property="og:title"]/@content').get().split(" | ")[0]
+        item["name"] = None
+        item["branch"] = (
+            response.xpath('//meta[@property="og:title"]/@content')
+            .get()
+            .split(" | ")[0]
+            .replace("Auntie Anne's", "")
+            .strip(" /")
+            .replace(
+                "Dallas Ft. Worth Int'lDallas Ft. Worth Int'l Airport (TA;G39) Airport",
+                "Dallas Ft. Worth Int'l Airport (TA;G39)",
+            )
+            .replace(";", ":")
+        )
+        apply_category(Categories.FAST_FOOD, item)
         yield item
