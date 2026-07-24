559e213d67930a38945b6a2ae950f2cdee009dee
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-24 12:18:31 +0000

    rename spider

---
+++ locations/spiders/village_inn_us.py
@@ -0,0 +1,23 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.hours import OpeningHours
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class VillageInnUSSpider(CrawlSpider, StructuredDataSpider):
+    name = "village_inn_us"
+    item_attributes = {"brand": "Village Inn", "brand_wikidata": "Q7930659"}
+    # allowed_domains = ["www.villageinn.com"]
+    start_urls = ("https://www.villageinn.com/Locations",)
+    rules = [
+        Rule(LinkExtractor(restrict_xpaths='//*[@id="state-list"]')),
+        Rule(LinkExtractor(restrict_xpaths='//*[@id="storeListContainer"]'), callback="parse_sd"),
+    ]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        item["branch"] = item.pop("name").replace("Village Inn - ", "")
+        oh = OpeningHours()
+        oh.add_ranges_from_string(ld_data.get("openingHours"))
+        item["opening_hours"] = oh
+        yield item
