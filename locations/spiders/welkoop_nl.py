8fdcbd7d1a9da1819dd1f32da70aceecedcaeae5
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-24 06:38:20 +0000

    code refactored to fix spider

--- locations/spiders/welkoop_nl.py
+++ locations/spiders/welkoop_nl.py
@@ -1,31 +1,40 @@
-import re
+import json
 
-from scrapy.spiders import SitemapSpider
+import scrapy
+from requests import Response
 
-from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
 from locations.hours import DAYS_NL, OpeningHours, sanitise_day
-from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class WelkoopNLSpider(SitemapSpider):
+class WelkoopNLSpider(scrapy.Spider):
     name = "welkoop_nl"
     item_attributes = {"brand": "Welkoop", "brand_wikidata": "Q72799253"}
-    sitemap_urls = ["https://www.welkoop.nl/sitemap/sitemap_dealers1.xml"]
+    start_urls = ["https://www.welkoop.nl/winkels"]
 
-    def parse(self, response, **kwargs):
-        item = Feature()
-        item["branch"] = response.xpath("//h2/text()").get().replace("Welkoop ", "")
-        item["lat"] = re.search(r"DealerDetail\.lat.*?(-?\d+\.\d+);", response.text).group(1)
-        item["lon"] = re.search(r"DealerDetail\.lng.*?(-?\d+\.\d+);", response.text).group(1)
-        item["addr_full"] = re.search(r"DealerDetail\.destination.*\'(.*)\';", response.text).group(1)
-        item["ref"] = item["website"] = response.url
-        item["opening_hours"] = OpeningHours()
-        day_time = re.findall(
-            r"([a-zA-Z]+)(\d+:\d+)\s*tot\s*(\d+:\d+)",
-            response.xpath('//*[@id="datawrapper"]//*[@class="columns six"][1]').xpath("normalize-space()").get(),
-        )
-        for time_details in day_time:
-            day, open_time, close_time = time_details
-            item["opening_hours"].add_range(day=sanitise_day(day, DAYS_NL), open_time=open_time, close_time=close_time)
-        apply_category(Categories.SHOP_GARDEN_CENTRE, item)
-        yield item
+    def parse(self, response: Response, **kwargs):
+        data = json.loads(response.xpath('//*[@type="application/json"]/text()').get())["__PRELOADED_STATE__"][
+            "__reactQuery"
+        ]["queries"]
+        for location_data in data:
+            if data := location_data.get("state").get("data").get("data"):
+                for location in data:
+                    item = DictParser.parse(location)
+                    item["street_address"] = merge_address_lines([location.get("address2"), location.get("address1")])
+                    item["branch"] = item.pop("name").replace("Welkoop ", "")
+                    oh = OpeningHours()
+                    for hours_data in json.loads(location.get("store_hours")):
+                        day = sanitise_day(list(hours_data.get("day").values())[0], DAYS_NL)
+                        open_close_time_data = hours_data.get("hours")
+                        if open_close_time_data == "Gesloten":
+                            oh.set_closed(day)
+                        else:
+                            for open_close_time in open_close_time_data:
+                                print(open_close_time)
+                                open_time = open_close_time.get("open")
+                                close_time = open_close_time.get("close")
+                                oh.add_range(day=day, open_time=open_time, close_time=close_time)
+                    item["opening_hours"] = oh
+
+                    yield item
