8bca2d192f0d7b3377729fd4822cc625adf91613
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-28 08:26:36 +0000

    added playwright to fix spider

--- locations/spiders/karwei_nl.py
+++ locations/spiders/karwei_nl.py
@@ -1,10 +1,13 @@
 from scrapy.spiders import SitemapSpider
 
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class KarweiNLSpider(SitemapSpider, StructuredDataSpider):
+class KarweiNLSpider(SitemapSpider, StructuredDataSpider, PlaywrightSpider):
     name = "karwei_nl"
     item_attributes = {"brand": "Karwei", "brand_wikidata": "Q2097480"}
     sitemap_urls = ["https://sitemap.karwei.nl/stores.xml"]
     wanted_types = ["HardwareStore"]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
