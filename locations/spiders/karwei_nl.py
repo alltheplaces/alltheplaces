0c2d8b2ffc488646c1aa2fe566a6e25a0e9e9abd
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-28 14:40:43 +0530

    trying with proxy

--- locations/spiders/karwei_nl.py
+++ locations/spiders/karwei_nl.py
@@ -3,7 +3,6 @@ from scrapy.spiders import SitemapSpider
 from locations.playwright_spider import PlaywrightSpider
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
-from locations.user_agents import BROWSER_DEFAULT
 
 
 class KarweiNLSpider(SitemapSpider, StructuredDataSpider, PlaywrightSpider):
@@ -11,4 +10,5 @@ class KarweiNLSpider(SitemapSpider, StructuredDataSpider, PlaywrightSpider):
     item_attributes = {"brand": "Karwei", "brand_wikidata": "Q2097480"}
     sitemap_urls = ["https://sitemap.karwei.nl/stores.xml"]
     wanted_types = ["HardwareStore"]
-    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT, "ROBOTSTXT_OBEY": False}
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS 
+    requires_proxy = True
