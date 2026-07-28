9786ea9c4e21e665b9d7b2aff5f8686e8e85e2ee
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-07-28 14:30:31 +0530

    Update karwei_nl.py

--- locations/spiders/karwei_nl.py
+++ locations/spiders/karwei_nl.py
@@ -3,11 +3,11 @@ from scrapy.spiders import SitemapSpider
 from locations.playwright_spider import PlaywrightSpider
 from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
-
+from locations.user_agents import BROWSER_DEFAULT
 
 class KarweiNLSpider(SitemapSpider, StructuredDataSpider, PlaywrightSpider):
     name = "karwei_nl"
     item_attributes = {"brand": "Karwei", "brand_wikidata": "Q2097480"}
     sitemap_urls = ["https://sitemap.karwei.nl/stores.xml"]
     wanted_types = ["HardwareStore"]
-    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT, "ROBOTSTXT_OBEY": False}
