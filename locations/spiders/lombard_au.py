602b10ad007be8081e379f66687b8bd99e12f43e
Ian Dees <ian.dees@gmail.com>
2026-07-27 21:02:29 -0500

    Fix Cloudflare 403 and unguarded .get() calls

--- locations/spiders/lombard_au.py
+++ locations/spiders/lombard_au.py
@@ -8,6 +8,7 @@ from locations.google_url import url_to_coords
 from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class LombardAUSpider(Spider):
@@ -16,38 +17,42 @@ class LombardAUSpider(Spider):
     allowed_domains = ["www.lombard.com.au"]
     start_urls = ["https://www.lombard.com.au/site/pages/party-shop-finder"]
     no_refs = True
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
 
     def parse(self, response: Response) -> Iterable[Feature]:
         properties = {}
         for store in response.xpath("//table/tbody/tr"):
             if store.xpath('./td[@colspan="6"]'):
-                lat, lon = url_to_coords(
-                    store.xpath('.//iframe[contains(@src, "https://www.google.com/maps/embed")]/@src').get()
-                )
-                if lat and lon and "branch" in properties.keys():
-                    properties["lat"] = lat
-                    properties["lon"] = lon
-                    apply_category(Categories.SHOP_PARTY, properties)
-                    yield Feature(**properties)
+                if map_url := store.xpath('.//iframe[contains(@src, "https://www.google.com/maps/embed")]/@src').get():
+                    lat, lon = url_to_coords(map_url)
+                    if lat and lon and "branch" in properties.keys():
+                        properties["lat"] = lat
+                        properties["lon"] = lon
+                        apply_category(Categories.SHOP_PARTY, properties)
+                        yield Feature(**properties)
             else:
-                properties = {
-                    "branch": store.xpath("./td[1]//text()").get().strip(),
-                    "addr_full": merge_address_lines(
-                        store.xpath('./td[contains(@class, "party-shop-finder-address")]//text()').getall()
-                    ),
-                    "opening_hours": OpeningHours(),
-                    "phone": store.xpath('./td/a[contains(@href, "tel:")]/@href').get().removeprefix("tel:"),
-                }
-                if "Events By Lombard" in properties["branch"]:
-                    properties = {}
-                    continue
-                hours_string = " ".join(
-                    filter(
-                        None,
-                        map(
-                            str.strip,
-                            store.xpath('./td[contains(@class, "party-shop-finder-trading-hours")]//text()').getall(),
+                if branch := store.xpath("./td[1]//text()").get():
+                    branch = branch.strip()
+                    phone = store.xpath('./td/a[contains(@href, "tel:")]/@href').get()
+                    phone = phone.removeprefix("tel:") if phone else None
+                    properties = {
+                        "branch": branch,
+                        "addr_full": merge_address_lines(
+                            store.xpath('./td[contains(@class, "party-shop-finder-address")]//text()').getall()
                         ),
+                        "opening_hours": OpeningHours(),
+                        "phone": phone,
+                    }
+                    if "Events By Lombard" in properties["branch"]:
+                        properties = {}
+                        continue
+                    hours_string = " ".join(
+                        filter(
+                            None,
+                            map(
+                                str.strip,
+                                store.xpath('./td[contains(@class, "party-shop-finder-trading-hours")]//text()').getall(),
+                            ),
+                        )
                     )
-                )
-                properties["opening_hours"].add_ranges_from_string(hours_string)
+                    properties["opening_hours"].add_ranges_from_string(hours_string)
