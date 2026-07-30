11328615376161069a8597a25391ab53bf014d9d
Cj Malone <CjMalone@mail.com>
2026-07-30 14:14:33 +0100

    [Classic Collision US] Add spider

---
+++ locations/spiders/classic_collision_us.py
@@ -0,0 +1,21 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import apply_category, Categories
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class ClassicCollisionUSSpider(SitemapSpider, StructuredDataSpider):
+    name = "classic_collision_us"
+    item_attributes = {"brand": "Classic Collision", "brand_wikidata": "Q122760989"}
+    sitemap_urls = ["https://classiccollision.com/wpsl_stores-sitemap.xml"]
+    wanted_types = ["AutoRepair"]
+    search_for_twitter = False
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = response.xpath('//span[@class="locnamecol"]/text()').get()
+        apply_category(Categories.SHOP_CAR_REPAIR, item)
+        yield item
