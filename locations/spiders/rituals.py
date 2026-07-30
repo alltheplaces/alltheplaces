d443c85356d7c5fc5c993e80126bec103af62934
Cj Malone <CjMalone@mail.com>
2026-07-30 09:44:35 +0100

    apply_category

--- locations/spiders/rituals.py
+++ locations/spiders/rituals.py
@@ -3,17 +3,14 @@ import re
 from scrapy import Spider
 from scrapy.http import JsonRequest
 
+from locations.categories import apply_category, Categories
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
 
 
 class RitualsSpider(Spider):
     name = "rituals"
-
-    item_attributes = {
-        "brand": "Rituals",
-        "brand_wikidata": "Q62874140",
-    }
+    item_attributes = {"brand": "Rituals", "brand_wikidata": "Q62874140"}
 
     start_urls = ["https://www.rituals.com/en-nl/stores"]
 
@@ -36,28 +33,22 @@ class RitualsSpider(Spider):
     def parse_locations(self, response):
         json_data = response.json()
 
-        for location in json_data.get(
-            "data",
-        ):
+        for location in json_data.get("data"):
             item = DictParser.parse(location)
+            item["branch"] = item.pop("name")
             try:
                 oh = OpeningHours()
-                for day_time in location.get("openingHours"):
-                    day = day_time.get("name")
-                    open_time = day_time.get("openingTime")
-                    close_time = day_time.get("closingTime")
+                for rule in location.get("openingHours"):
+                    open_time = rule.get("openingTime")
+                    close_time = rule.get("closingTime")
                     if open_time is None and close_time is None:
-                        oh.set_closed(day)
+                        oh.set_closed(rule.get("name"))
                     else:
-                        oh.add_range(
-                            day=day,
-                            open_time=open_time,
-                            close_time=close_time.replace("59.9999999", "59"),
-                            time_format="%H:%M:%S",
-                        )
+                        oh.add_range(rule.get("name"), open_time, close_time.replace("59.9999999", "59"), "%H:%M:%S")
                 item["opening_hours"] = oh
             except:
                 pass
+            apply_category(Categories.SHOP_COSMETICS, item)
             yield item
 
         next_offset = json_data.get("meta").get("pagination").get("nextOffset")
