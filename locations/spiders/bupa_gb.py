097b697d5f9b91aea58a2c39b3042edb1b791327
Ehsan Firdaus <ehsan.firdaus@gmail.com>
2026-07-30 13:40:22 +0530

    [Bupa GB] Use CrawlSpider with playwright to fix spider

--- locations/spiders/bupa_gb.py
+++ locations/spiders/bupa_gb.py
@@ -1,19 +1,29 @@
 from scrapy.http import Response
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature, set_closed
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 BUPA = {"brand": "Bupa", "brand_wikidata": "Q931628"}
 
 
-class BupaGBSpider(SitemapSpider, StructuredDataSpider):
+class BupaGBSpider(CrawlSpider, StructuredDataSpider, PlaywrightSpider):
     name = "bupa_gb"
-    sitemap_urls = ["https://www.bupa.co.uk/robots.txt"]
-    sitemap_rules = [(r"/practices/([-\w]+)$", "parse_sd")]
+    start_urls = ["https://www.bupa.co.uk/dental/dental-care/practices"]
+    rules = [
+        Rule(LinkExtractor(r"/practices/([-\w]+)$"), "parse_sd"),
+        Rule(
+            LinkExtractor(r"/browse-by-region/[-\w]+$"),
+        ),
+    ]
     time_format = "%I:%M %p"
     requires_proxy = True
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT} | DEFAULT_PLAYWRIGHT_SETTINGS
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         if "Total Dental Care" in item["name"]:
