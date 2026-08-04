ceeffb9bbaebdef0acb5fa72f8314c6be2009f55
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-04 16:53:39 +0530

    [Banque Populaire FR] Fix Spider (#17615)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

--- locations/spiders/banque_populaire_fr.py
+++ locations/spiders/banque_populaire_fr.py
@@ -7,14 +7,19 @@ from locations.structured_data_spider import StructuredDataSpider
 class BanquePopulaireFRSpider(SitemapSpider, StructuredDataSpider):
     name = "banque_populaire_fr"
     item_attributes = {"brand": "Banque Populaire", "brand_wikidata": "Q846647"}
-    sitemap_urls = ["https://agences.banquepopulaire.fr/banque-assurance/sitemap-bp.xml"]
-    sitemap_rules = [("-id", "parse_sd")]
+    sitemap_urls = ["https://agences.banquepopulaire.fr/sitemap_pois.xml"]
+    sitemap_rules = [(r"https://agences.banquepopulaire.fr/fr/agence/[^/]+$", "parse_sd")]
     drop_attributes = {"image"}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["branch"] = item.pop("name")
-        item["website"] = response.url  # Bad url in linked data+
+        item["website"] = response.url
 
         apply_category(Categories.BANK, item)
-        apply_yes_no(Extras.ATM, item, bool(response.xpath('//*[contains(@class, "em-details__services-dab")]')))
+        apply_yes_no(
+            Extras.ATM,
+            item,
+            "Distributeur automatique de billets"
+            in response.xpath('//*[contains(@data-testid,"service-card")]//text()').getall(),
+        )
         yield item
