b319e7b6679946836a0d3a9533df5d90ce0653e5
Cj Malone <CjMalone@mail.com>
2026-08-04 12:13:20 +0100

    Honor robots.txt

--- locations/spiders/pague_menos_br.py
+++ locations/spiders/pague_menos_br.py
@@ -1,31 +1,28 @@
 from typing import Any, AsyncIterator, Iterable
 
 from scrapy import Request, Spider
-from scrapy.http import Response
+from scrapy.http import JsonRequest, Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.geo import country_iseadgg_centroids
-from locations.hours import OpeningHours
+from locations.hours import DAYS, OpeningHours
 from locations.items import Feature
 
 
 class PagueMenosBRSpider(Spider):
     name = "pague_menos_br"
     item_attributes = {"brand": "Pague Menos", "brand_wikidata": "Q7124466"}
-    custom_settings = {"ROBOTSTXT_OBEY": False}
-    days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
 
-    def request(self, latitude: float, longitude: float, page: int) -> Request:
-        return Request(
-            url=f"https://www.paguemenos.com.br/api/checkout/pub/pickup-points"
-            f"?geoCoordinates={longitude};{latitude}&pageSize=100&page={page}",
+    def make_request(self, latitude: float, longitude: float, page: int) -> JsonRequest:
+        return JsonRequest(
+            url=f"https://www.paguemenos.com.br/api/checkout/pub/pickup-points?geoCoordinates={longitude};{latitude}&pageSize=100&page={page}",
             meta={"latitude": latitude, "longitude": longitude, "page": page},
         )
 
     async def start(self) -> AsyncIterator[Request]:
         for latitude, longitude in country_iseadgg_centroids("BR", 48):
-            yield self.request(latitude, longitude, 1)
+            yield self.make_request(latitude, longitude, 1)
 
     def parse(self, response: Response, **kwargs: Any) -> Iterable[Request | Feature]:
         data = response.json()
@@ -40,10 +37,10 @@ class PagueMenosBRSpider(Spider):
             item["opening_hours"] = OpeningHours()
             for rule in location.get("businessHours") or []:
                 item["opening_hours"].add_range(
-                    self.days[rule["DayOfWeek"]], rule["OpeningTime"][:5], rule["ClosingTime"][:5]
+                    DAYS[rule["DayOfWeek"] - 1], rule["OpeningTime"], rule["ClosingTime"], time_format="%H:%M:%S"
                 )
             apply_category(Categories.PHARMACY, item)
             yield item
 
         if response.meta["page"] < data["paging"]["pages"]:
-            yield self.request(response.meta["latitude"], response.meta["longitude"], response.meta["page"] + 1)
+            yield self.make_request(response.meta["latitude"], response.meta["longitude"], response.meta["page"] + 1)
