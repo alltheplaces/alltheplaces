8941e349d41962c788705b1f3509460649a1bbde
Cj Malone <CjMalone@mail.com>
2026-08-05 12:24:57 +0100

    apply_category

--- locations/spiders/starbucks_mena.py
+++ locations/spiders/starbucks_mena.py
@@ -1,6 +1,7 @@
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
@@ -29,4 +30,5 @@ class StarbucksMenaSpider(SitemapSpider, StructuredDataSpider):
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["image"] = None
         item["branch"] = item.pop("name").removeprefix("Starbucks ")
+        apply_category(Categories.CAFE, item)
         yield item
