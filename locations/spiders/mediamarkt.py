e54cf71cde5375be6366fc9c027bd019de09ee99
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-05 08:28:57 +0000

    code refactored to fix spider

--- locations/spiders/mediamarkt.py
+++ locations/spiders/mediamarkt.py
@@ -1,8 +1,8 @@
-import json
 import re
 
+from scrapy.spiders import SitemapSpider
+
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
 from locations.structured_data_spider import StructuredDataSpider
 
 MEDIAMARKT = {"brand": "MediaMarkt", "brand_wikidata": "Q2381223"}
@@ -10,7 +10,7 @@ MEDIAWORLD = {"brand": "MediaWorld", "brand_wikidata": "Q125054068"}
 SATURN = {"brand": "Saturn", "brand_wikidata": "Q2543504"}
 
 
-class MediamarktSpider(StructuredDataSpider):
+class MediamarktSpider(SitemapSpider, StructuredDataSpider):
     name = "mediamarkt"
     requires_proxy = True
     allowed_domains = [
@@ -25,18 +25,20 @@ class MediamarktSpider(StructuredDataSpider):
         "mediamarkt.pl",
         "www.saturn.de",
     ]
-    start_urls = [
-        "https://www.mediamarkt.at/de/store/store-finder",
-        "https://www.mediamarkt.be/fr/store/store-finder",
-        "https://www.mediamarkt.ch/de/store/store-finder",
-        "https://www.mediamarkt.de/de/store/store-finder",
-        "https://www.mediamarkt.es/es/store/store-finder",
-        "https://www.mediamarkt.hu/hu/store/store-finder",
-        "https://www.mediamarkt.nl/nl/store/store-finder",
-        "https://www.mediaworld.it/it/store/store-finder",
-        "https://mediamarkt.pl/pl/store/store-finder",
-        "https://www.saturn.de/de/store/store-finder",
+    sitemap_urls = [
+        "https://www.mediamarkt.at/sitemaps/sitemap-marketpages.xml",
+        "https://www.mediamarkt.be/sitemaps/fr/sitemap-marketpages.xml",
+        "https://www.mediamarkt.ch/sitemaps/fr/sitemap-marketpages.xml",
+        "https://www.mediamarkt.de/sitemaps/sitemap-marketpages.xml",
+        "https://www.mediamarkt.es/sitemaps/sitemap-marketpages.xml",
+        "https://www.mediamarkt.hu/sitemaps/sitemap-marketpages.xml",
+        "https://www.mediamarkt.nl/sitemaps/sitemap-marketpages.xml",
+        "https://www.mediaworld.it/sitemaps/sitemap-marketpages.xml",
+        "https://www.saturn.de/sitemaps/sitemap-marketpages.xml",
+        "https://mediamarkt.pl/sitemaps/sitemap-marketpages.xml",
     ]
+    sitemap_rules = [("/store/", "parse_sd")]
+
     brands = {
         "www.mediamarkt.at": MEDIAMARKT,
         "www.mediamarkt.be": MEDIAMARKT,
@@ -50,24 +52,12 @@ class MediamarktSpider(StructuredDataSpider):
         "www.saturn.de": SATURN,
     }
 
-    def parse(self, response, **kwargs):
-        base_path = response.url.rsplit("/", 1)[0]
-
-        script = response.xpath('//script[contains(text(), "storeName")]/text()').get()
-
-        if json_match := re.search(r'JSON\.parse\("(.+?)"\)', script):
-            data = json.loads(json.loads(f'"{json_match.group(1)}"'))
-
-            for stores in DictParser.iter_matching_keys(data, "regionStores"):
-                for store in stores:
-                    if (uid := store.get("uid")) and uid != "store-finder":
-                        yield response.follow(f"{base_path}/{uid}", callback=self.parse_sd)
-
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["ref"] = response.url.split("/")[-1]
         host = response.url.split("/")[2]
         item.update(self.brands[host])
-        item["branch"] = item.get("name", "").removeprefix(f"{item['brand']} ")
+        item["branch"] = item.pop("name", "").replace(f"{item['brand']} ", "")
+        item["name"] = item["brand"]
         if phone := item.get("phone"):
             item["phone"] = re.sub(r"[^0-9+]", "", phone)
         apply_category(Categories.SHOP_ELECTRONICS, item)
