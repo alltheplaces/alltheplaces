436efce802dab186aa94e52e4a007542139dfd99
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-05 15:24:51 +0530

    trying without proxy

--- locations/spiders/mediamarkt.py
+++ locations/spiders/mediamarkt.py
@@ -12,7 +12,6 @@ SATURN = {"brand": "Saturn", "brand_wikidata": "Q2543504"}
 
 class MediamarktSpider(SitemapSpider, StructuredDataSpider):
     name = "mediamarkt"
-    requires_proxy = True
     allowed_domains = [
         "www.mediamarkt.at",
         "www.mediamarkt.be",
