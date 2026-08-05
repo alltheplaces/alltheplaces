d23ac2c07e069f30f752e7662dadee7e7ff03330
Cj Malone <CjMalone@mail.com>
2026-08-05 12:23:49 +0100

    Drop double ld parse

--- locations/spiders/starbucks_mena.py
+++ locations/spiders/starbucks_mena.py
@@ -1,5 +1,3 @@
-import json
-
 from scrapy.http import Response
 from scrapy.spiders import SitemapSpider
 
@@ -12,13 +10,13 @@ class StarbucksMenaSpider(SitemapSpider, StructuredDataSpider):
     item_attributes = {"brand": "ستاربكس", "brand_wikidata": "Q37158"}
     sitemap_urls = [
         "https://locations.starbucks.ae/robots.txt",
-        "https://locations.starbucks.qa/sitemap.xml",
-        "https://locations.starbucks.sa/sitemap.xml",
-        "https://locations.starbucks.eg/sitemap.xml",
-        "https://locations.starbucks.com.bh/sitemap.xml",
-        "https://locations.starbucks.com.kw/sitemap.xml",
-        "https://locations.starbucks.com.lb/sitemap.xml",
-        "https://locations.starbucks.com.om/sitemap.xml",
+        "https://locations.starbucks.qa/robots.txt",
+        "https://locations.starbucks.sa/robots.txt",
+        "https://locations.starbucks.eg/robots.txt",
+        "https://locations.starbucks.com.bh/robots.txt",
+        "https://locations.starbucks.com.kw/robots.txt",
+        "https://locations.starbucks.com.lb/robots.txt",
+        "https://locations.starbucks.com.om/robots.txt",
     ]
     sitemap_rules = [
         (r"https://locations.starbucks.com.\w+/[a-z-]+/[a-z-]+$", "parse_sd"),
@@ -26,15 +24,9 @@ class StarbucksMenaSpider(SitemapSpider, StructuredDataSpider):
         (r"https://locations.starbucks.\w+/fr/[a-z-]+/[a-z-]+$", "parse_sd"),
         (r"https://locations.starbucks.\w+/ar/[a-z-]+/[a-z-]+$", "parse_sd"),
     ]
+    wanted_types = ["Restaurant"]
 
     def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs):
         item["image"] = None
         item["branch"] = item.pop("name").removeprefix("Starbucks ")
-        ld_data = (
-            json.loads(response.xpath('//*[@type="application/ld+json"][2]//text()').get())
-            .get("credentialSubject")
-            .get("geo")
-        )
-        item["lat"] = ld_data.get("latitude")
-        item["lon"] = ld_data.get("longitude")
         yield item
