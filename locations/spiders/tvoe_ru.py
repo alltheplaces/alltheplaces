81ea8f05c944da9aae1952eb20aac505c152e0c7
IceBurger <79398013+sharmaanurag95@users.noreply.github.com>
2026-08-05 08:21:17 +0530

    [Tvoe RU] Fix spider (#17646)

--- locations/spiders/tvoe_ru.py
+++ locations/spiders/tvoe_ru.py
@@ -1,44 +1,29 @@
-from urllib.parse import urljoin
+import json
+from typing import Any
 
-import scrapy
+from scrapy.http import Response
+from scrapy.spiders import Spider
 
 from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
 from locations.hours import DAYS, OpeningHours
-from locations.items import Feature
 
 
-class TvoeRUSpider(scrapy.Spider):
+class TvoeRUSpider(Spider):
     name = "tvoe_ru"
+    item_attributes = {"brand": "ТВОЕ", "brand_wikidata": "Q110034939"}
     start_urls = ["https://tvoe.ru/contacts/"]
-    item_attributes = {
-        "brand": "ТВОЕ",
-        "brand_wikidata": "Q110034939",
-    }
 
-    def parse(self, response):
-        pois = response.xpath('//div[@class="contacts-stores shops-list1"]/div[@class="item bordered box-shadow wti"]')
-        for poi in pois:
-            item = Feature()
-            slug = poi.xpath('.//div[contains(@class, "title")]/a/@href').get()
-            item["ref"] = item["website"] = urljoin(response.url, slug)
-            item["phone"] = poi.xpath('.//div[contains(@class,"phone")]/a/text()').get()
-            item["email"] = poi.xpath('.//div[contains(@class,"email")]/a/text()').get()
-            coords = poi.xpath(".//span/@data-coordinates").get(default="").split(",")
-            if len(coords) == 2:
-                item["lat"] = coords[0]
-                item["lon"] = coords[1]
-            item["addr_full"] = poi.xpath('.//div[contains(@class, "title")]/a/text()').get(default="").strip()
-            self.parse_hours(item, poi)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for shop in json.loads(response.xpath('//component[@is="shops"]').attrib.get(":shops")):
+            item = DictParser.parse(shop)
+            item["branch"] = item.pop("name")
+            item["street_address"] = item.pop("addr_full")
+            oh = OpeningHours()
+            open_time, close_time = shop.get("workhours").replace(" ", "").split("-")
+            oh.add_days_range(DAYS, open_time, close_time)
+            item["opening_hours"] = oh
+
             apply_category(Categories.SHOP_CLOTHES, item)
-            yield item
 
-    def parse_hours(self, item, poi):
-        if schedule := poi.xpath('.//div[@class="schedule"]/span/text()').get():
-            try:
-                oh = OpeningHours()
-                open, close = schedule.replace(" ", "").split("-")
-                oh.add_days_range(DAYS, open, close)
-                item["opening_hours"] = oh
-            except Exception as e:
-                self.crawler.stats.inc_value(f"atp/{self.name}/hours/fail")
-                self.logger.warning(f"Failed to parse hours: {schedule}, {e}")
+            yield item
