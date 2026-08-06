ee9500fe1d70c22ed7f4e49dc5a4790ea770a38e
IceBurger <sharma.anurag1995@gmail.com>
2026-08-06 15:58:11 +0530

    fix spider

--- locations/spiders/mcdonalds_cy.py
+++ locations/spiders/mcdonalds_cy.py
@@ -1,26 +1,31 @@
 import re
+from typing import Any
 
 import chompjs
-import scrapy
+from scrapy.http import Response
 
 from locations.categories import Categories, Extras, apply_category, apply_yes_no
 from locations.dict_parser import DictParser
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
 from locations.spiders.mcdonalds import McdonaldsSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class McdonaldsCYSpider(scrapy.Spider):
+class McdonaldsCYSpider(PlaywrightSpider):
     name = "mcdonalds_cy"
     item_attributes = McdonaldsSpider.item_attributes
     start_urls = ["https://www.mcdonalds.com.cy/locate"]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
 
-    def parse(self, response):
-        raw_data = response.xpath(
-            '//script[contains(text(), "function distance(lat1, lng1, lat2, lng2)")]/text()'
-        ).get()
-        data = re.findall(r"var McDonald_s_\w+\s*=\s*(.*)", raw_data)
-        for i in data:
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for i in re.findall(
+            r"var McDonald_s_\w+\s*=\s*(.*)",
+            response.xpath('//script[contains(text(), "function distance(lat1, lng1, lat2, lng2)")]/text()').get(),
+        ):
             location = chompjs.parse_js_object(i)
             item = DictParser.parse(location)
+            item["branch"] = item.pop("name", "").removeprefix("McDonald's ")
             item["ref"] = location["key"]
 
             services = location["services_listed"]
