27c15274f6fd523c6e4d79a0a02de8e4d48003be
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-06 05:54:41 +0000

    code refactored to fix spider

--- locations/spiders/virgin_active_it.py
+++ locations/spiders/virgin_active_it.py
@@ -1,32 +1,25 @@
-from html import unescape
+import re
 
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-from locations.hours import DAYS_IT, OpeningHours
+from locations.items import Feature
 from locations.spiders.virgin_active_bw_na_za import VIRGIN_ACTIVE_SHARED_ATTRIBUTES
-from locations.structured_data_spider import StructuredDataSpider
 
 
-class VirginActiveITSpider(CrawlSpider, StructuredDataSpider):
+class VirginActiveITSpider(CrawlSpider):
     name = "virgin_active_it"
     item_attributes = VIRGIN_ACTIVE_SHARED_ATTRIBUTES
-    allowed_domains = ["www.virginactive.it"]
     start_urls = ["https://www.virginactive.it/club"]
     rules = [
-        Rule(LinkExtractor(allow=r"/club/[-\w]+/[-\w]+$"), follow=False, callback="parse"),
+        Rule(LinkExtractor(allow=r"/club/[-\w]+/[-\w]+$"), callback="parse"),
     ]
-    time_format = "%H.%M"
-    search_for_facebook = False
-    search_for_twitter = False
 
-    def post_process_item(self, item, response, ld_data):
-        item["branch"] = item.pop("name")
-        item["image"] = item["image"].split("?")[0]
-        item["opening_hours"] = OpeningHours()
-        for spec in ld_data.get("openingHoursSpecification"):
-            for day in spec["dayOfWeek"]:
-                item["opening_hours"].add_ranges_from_string(
-                    f"{unescape(day)} {spec['opens']}-{spec['closes']}", DAYS_IT
-                )
+    def parse(self, response, **kwargs):
+        item = Feature()
+        item["branch"] = response.xpath("//h1/text()").get()
+        item["addr_full"] = response.xpath('//*[@class="clubAddress d-none d-sm-block"]/text()').get()
+        item["lat"] = re.search(r"latitude\":\s*(\d+,\d+)", response.text).group(1).replace(",", ".")
+        item["lon"] = re.search(r"longitude\":\s*(\d+,\d+)", response.text).group(1).replace(",", ".")
+        item["website"] = item["ref"] = response.url
         yield item
