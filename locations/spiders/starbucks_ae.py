9d446ae3160c69efbe0ef7c2d70117725205e479
Cj Malone <CjMalone@mail.com>
2026-08-07 10:19:59 +0100

    Restore AE

---
+++ locations/spiders/starbucks_ae.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class StarbucksAESpider(SitemapSpider, StructuredDataSpider):
+    name = "starbucks_ae"
+    item_attributes = {"brand": "ستاربكس", "brand_wikidata": "Q37158"}
+    sitemap_urls = ["https://locations.starbucks.ae/robots.txt"]
+    sitemap_rules = [(r"^https://locations\.starbucks\.ae/(?!ar/)[^/]+/[^/]+$", "parse")]
+    wanted_types = ["Restaurant"]
+
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs: Any) -> Any:
+        item["branch"] = response.xpath("//h1/text()").get("").removeprefix("Starbucks ")
+        item["website"] = response.url
+        apply_category(Categories.CAFE, item)
+        yield item
