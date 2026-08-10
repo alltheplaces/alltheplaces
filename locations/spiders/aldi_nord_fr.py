7b84bd0a93d606ace6e4e6b5ee15030266864395
Cj Malone <CjMalone@mail.com>
2026-08-10 14:37:45 +0100

    Move to UberallSpider

--- locations/spiders/aldi_nord_fr.py
+++ locations/spiders/aldi_nord_fr.py
@@ -1,5 +1,4 @@
 from locations.categories import Categories, apply_category
-from locations.hours import DAYS
 from locations.storefinders.uberall import UberallSpider
 
 
@@ -9,13 +8,8 @@ class AldiNordFRSpider(UberallSpider):
     key = "ALDINORDFR_Mmljd17th8w26DMwOy4pScWk4lCvj5"
 
     def post_process_item(self, item, response, location):
-        item["branch"] = item.pop("name").removeprefix("ALDI").strip() or None
+        item["branch"] = item.pop("name").removeprefix("ALDI").strip()
         item["phone"] = None  # national customer service number, identical for every store
-
-        # The Uberall storefinder skips days flagged as closed, losing the explicit closure.
-        for rule in location["openingHours"]:
-            if rule.get("closed"):
-                item["opening_hours"].set_closed(DAYS[rule["dayOfWeek"] - 1])
 
         apply_category(Categories.SHOP_SUPERMARKET, item)
 
