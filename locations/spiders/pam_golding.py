09b824b8e947663f52525c0e3ad712473cb8146b
Cj Malone <CjMalone@mail.com>
2026-08-11 14:52:15 +0100

    Trim

--- locations/spiders/pam_golding.py
+++ locations/spiders/pam_golding.py
@@ -11,23 +11,16 @@ from locations.json_blob_spider import JSONBlobSpider
 
 class PamGoldingSpider(JSONBlobSpider):
     name = "pam_golding"
-    item_attributes = {
-        "brand": "Pam Golding Properties",
-        "brand_wikidata": "Q65051429",
-    }
+    item_attributes = {"brand": "Pam Golding Properties", "brand_wikidata": "Q65051429"}
     skip_auto_cc_domain = True
-    country_utils = CountryUtils()
 
     async def start(self) -> Any:
         yield JsonRequest("https://webapi.pamgolding.co.za/api/agentsoffices/search-offices", data={})
 
     def extract_json(self, response: Response) -> list[dict]:
-        offices = [
+        return [
             office for country in response.json() for section in country["sections"] for office in section["offices"]
         ]
-        self.unique_phones = {k for k, v in Counter(o.get("number") for o in offices).items() if v == 1}
-        self.unique_emails = {k for k, v in Counter(o.get("email") for o in offices).items() if v == 1}
-        return offices
 
     def post_process_item(self, item: Feature, response: Response, location: dict, **kwargs: Any) -> Iterable[Feature]:
         item["ref"] = str(location["id"])
@@ -37,8 +30,8 @@ class PamGoldingSpider(JSONBlobSpider):
         item["lat"] = location["geoPoint"]["lat"]
         item["lon"] = location["geoPoint"]["lon"]
         item["state"] = location["location"]["provinceName"]
-        item["country"] = self.country_utils.to_iso_alpha2_country_code(location["location"]["countryName"])
-        item["phone"] = location["number"] if location.get("number") in self.unique_phones else None
-        item["email"] = location["email"] if location.get("email") in self.unique_emails else None
+        item["country"] = location["location"]["countryName"]
+        item["phone"] = location["number"]
+        item["email"] = location["email"]
         apply_category(Categories.OFFICE_ESTATE_AGENT, item)
         yield item
