d994af1ab7d4c322780cfad01e5ed8a58474fea6
Cj Malone <CjMalone@mail.com>
2026-08-13 16:46:13 +0100

    Drop network requests

--- locations/spiders/pinch_a_penny_us.py
+++ locations/spiders/pinch_a_penny_us.py
@@ -3,7 +3,8 @@ from typing import Any
 from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
-from locations.items import Feature
+from locations.dict_parser import DictParser
+from locations.pipelines.address_clean_up import merge_address_lines
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -11,16 +12,11 @@ class PinchAPennyUSSpider(StructuredDataSpider):
     name = "pinch_a_penny_us"
     item_attributes = {"brand": "Pinch A Penny", "brand_wikidata": "Q121436109"}
     start_urls = ["https://pinchapenny.com/api/store/search?lat=28.5&lng=-81.4"]
-    wanted_types = ["LocalBusiness"]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store in response.json()["data"]:
-            yield response.follow(store["url"], callback=self.parse_sd)
-
-    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs: Any) -> Any:
-        item["ref"] = ld_data.get("branchCode")
-        item["branch"] = item.pop("name")
-        item["lat"] = response.xpath("//@data-lat").get()
-        item["lon"] = response.xpath("//@data-lng").get()
-        apply_category(Categories.SHOP_SWIMMING_POOL, item)
-        yield item
+            item = DictParser.parse(store)
+            item["name"] = None
+            item["street_address"] = merge_address_lines([item.pop("addr_full"), store["address_extended"]])
+            apply_category(Categories.SHOP_SWIMMING_POOL, item)
+            yield item
