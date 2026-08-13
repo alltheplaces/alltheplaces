f49ac0c486e3ad30f99c5aff9920923fc9b83507
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-13 11:08:16 +0000

    code refactored to fix spider

--- locations/spiders/dehner_at_de.py
+++ locations/spiders/dehner_at_de.py
@@ -1,26 +1,19 @@
 from typing import Iterable
 
-from scrapy.http import Response
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.json_blob_spider import JSONBlobSpider
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class DehnerATDESpider(JSONBlobSpider):
+class DehnerATDESpider(SitemapSpider, StructuredDataSpider):
     name = "dehner_at_de"
     item_attributes = {"brand": "Dehner", "brand_wikidata": "Q1183029"}
-    start_urls = ["https://www.dehner.at/merchant-list/", "https://www.dehner.de/merchant-list/"]
-    locations_key = "data"
+    sitemap_urls = ["https://www.dehner.at/robots.txt", "https://www.dehner.de/robots.txt"]
+    sitemap_rules = [("/stores/", "parse_sd")]
+    wanted_types = ["GardenStore"]
 
-    def pre_process_data(self, feature: dict) -> None:
-        feature["ref"] = feature.pop("id_merchant")
-        feature["address"]["housenumber"] = feature["address"].pop("number")
-
-    def post_process_item(self, item: Feature, response: Response, feature: dict) -> Iterable[Feature]:
-        item["website"] = response.urljoin(item["website"])
-        oh = OpeningHours()
-        for day_line in feature["opening_hours"]:
-            oh.add_range(day_line["weekDayKey"], day_line["timeFrom"], day_line["timeTo"], time_format="%H:%M:%S.%f")
-        item["opening_hours"] = oh
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").replace("- Dehner GC", "")
         yield item
