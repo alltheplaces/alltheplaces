47c1e1178fe128969a83e7f9c0ec83efdd3d61a4
Cj Malone <CjMalone@mail.com>
2026-08-14 16:38:53 +0100

    Improve category

--- locations/spiders/deutsche_post_de.py
+++ locations/spiders/deutsche_post_de.py
@@ -9,11 +9,11 @@ from locations.geo import point_locations
 from locations.hours import DAYS, OpeningHours
 from locations.user_agents import BROWSER_DEFAULT
 
+DEUTSCHE_POST = {"brand": "Deutsche Post", "brand_wikidata": "Q157645"}
 
 class DeutschePostDESpider(Spider):
     name = "deutsche_post_de"
     allowed_domains = ["www.deutschepost.de"]
-    item_attributes = {"brand": "Deutsche Post", "brand_wikidata": "Q157645"}
     custom_settings = {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
 
     cats = {
@@ -25,7 +25,6 @@ class DeutschePostDESpider(Spider):
         "STATEMENT_PRINTER": None,
         "POSTBANK_FINANCE_CENTER": None,
         "RETAIL_OUTLET": Categories.POST_OFFICE,
-        "PAKETSHOP": None,
         "SELLING_POINT": None,
         "BULK_ACCEPTANCE_OFFICE": None,
         "BUSINESS_MAIL_ACCEPTANCE_POINT": None,
@@ -64,7 +63,11 @@ class DeutschePostDESpider(Spider):
             item["opening_hours"] = self.parse_hours(location["pfTimeinfos"])
 
             if cat := self.cats.get(location["locationType"]):
+                item.update(DEUTSCHE_POST)
                 apply_category(cat, item)
+            elif location["locationType"] == "PAKETSHOP":
+                item["extras"]["post_office"] = "post_partner"
+                apply_category(Categories.GENERIC_POI, item)
             else:
                 item["extras"]["type"] = location["locationType"]
                 self.crawler.stats.inc_value(f'atp/deutsche_post_de/unmapped_category/{location["locationType"]}')
