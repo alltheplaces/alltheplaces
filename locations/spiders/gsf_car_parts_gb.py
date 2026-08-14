bbfe17b7adee5a987dfe78c96ab2e62c297428bb
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-14 07:45:43 +0000

    code updated to fix spider

--- locations/spiders/gsf_car_parts_gb.py
+++ locations/spiders/gsf_car_parts_gb.py
@@ -2,20 +2,21 @@ import re
 from typing import Iterable
 
 from scrapy.http import TextResponse
-from scrapy.spiders import SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
-from locations.hours import OpeningHours
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class GsfCarPartsGBSpider(SitemapSpider, StructuredDataSpider):
+class GsfCarPartsGBSpider(CrawlSpider, StructuredDataSpider):
     name = "gsf_car_parts_gb"
     item_attributes = {"brand": "GSF Car Parts", "brand_wikidata": "Q80963064"}
-    sitemap_urls = ["https://www.gsfcarparts.com/robots.txt"]
-    sitemap_follow = ["branch-sitemap"]
-    sitemap_rules = [(r"/branches/([-\w]+)$", "parse")]
+    start_urls = ["https://www.gsfcarparts.com/branches"]
+    rules = [Rule(LinkExtractor(allow=r"/branches/[^/]+$"), callback="parse_sd")]
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT, "DOWNLOAD_DELAY": 2}
     wanted_types = ["Organization"]
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
@@ -23,26 +24,11 @@ class GsfCarPartsGBSpider(SitemapSpider, StructuredDataSpider):
         item["city"] = item.pop("state")
         item["facebook"] = None
 
-        item["opening_hours"] = self.parse_opening_hours(response)
-
-        if m := re.search(r"\\\"center\\\":\{\\\"lat\\\":(-?\d+\.\d+),\\\"lng\\\":(-?\d+\.\d+)}", response.text):
+        if m := re.search(
+            r"\\\"longitude\\\":\\\"(-?\d+\.\d+)\\\",\\\"latitude\\\":\\\"(-?\d+\.\d+)\\\"", response.text
+        ):
             item["lat"], item["lon"] = m.groups()
 
         apply_category(Categories.SHOP_CAR_PARTS, item)
 
         yield item
-
-    def parse_opening_hours(self, response: TextResponse) -> OpeningHours:
-        oh = OpeningHours()
-        for rule in response.xpath('//div[@class="store-timings"]/ul/li'):
-            day = rule.xpath('./span[@class="day"]/text()').get()
-            if rule.xpath('./span[contains(@class, "timings")][contains(text(), "Closed")]'):
-                oh.set_closed(day)
-            else:
-                oh.add_range(
-                    day,
-                    rule.xpath('./div[contains(@class, "timings")]/span[1]/text()').get(),
-                    rule.xpath('./div[contains(@class, "timings")]/span[2]/text()').get(),
-                    time_format="%I:%M%p",
-                )
-        return oh
