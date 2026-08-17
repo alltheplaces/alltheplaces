f55fabcc47817efdd00ce0efcc8e64194e9e53e8
Cj Malone <CjMalone@mail.com>
2026-08-17 11:05:46 +0100

    [Roche Bobois] Add spider

---
+++ locations/spiders/roche_bobois.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+from typing import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import apply_category, Categories
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class RocheBoboisSpider(SitemapSpider, StructuredDataSpider):
+    name = "roche_bobois"
+    item_attributes = {"brand": "Roche Bobois", "brand_wikidata": "Q3437504"}
+    sitemap_urls = ["https://www.roche-bobois.com/en-GB/sitemap_index.xml"]
+    sitemap_rules = [(r"com/en-GB/showrooms/[^/]+/\w(\d+)\.html$", "parse")]
+    wanted_types = ["FurnitureStore"]
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        item["branch"] = item.pop("name")
+
+        try:
+            oh = OpeningHours()
+            for rule in ld_data["openingHours"]:
+                day, times = rule.split(" ", 1)
+                if times == "Closed":
+                    oh.set_closed(day)
+                else:
+                    for time in times.split(" / "):
+                        oh.add_range(day, *time.split(" - "), time_format="%I:%M %p")
+            item["opening_hours"] = oh
+        except Exception:
+            pass
+
+        item["website"] = item["extras"]["website:en"] = response.xpath(
+            '//link[@rel="alternate"][@hreflang="en"]/@href'
+        ).get()
+        item["extras"]["website:fr"] = response.xpath('//link[@rel="alternate"][@hreflang="fr-fr"]/@href').get()
+
+        apply_category(Categories.SHOP_FURNITURE, item)
+        yield item
