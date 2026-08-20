9b003e559522e2430aac7a999d9a0e3cefc7957d
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-20 13:14:44 +0530

    rename_spider

--- locations/spiders/netto_fr.py
+++ locations/spiders/netto_fr.py
@@ -1,7 +1,7 @@
 from locations.storefinders.uberall import UberallSpider
 
 
-class NettoLesMousquetairesSpider(UberallSpider):
-    name = "netto_les_mousquetaires"
+class NettoFRSpider(UberallSpider):
+    name = "netto_fr"
     item_attributes = {"brand": "Netto", "brand_wikidata": "Q2720988"}
     key = "OWc3zgl9ql555j9wvYm0ecrD94vaeK"
