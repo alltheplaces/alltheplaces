76b6ce512ee39e56b78e75d4055cf82fdfe2aa71
MeGilles <35843078+MeGilles@users.noreply.github.com>
2026-08-20 13:43:22 +0200

    [Citya] Add spider (291 locations)

---
+++ locations/spiders/citya.py
@@ -0,0 +1,28 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class CityaSpider(SitemapSpider, StructuredDataSpider):
+    name = "citya"
+    item_attributes = {
+        "brand": "Citya",
+        "brand_wikidata": "Q2974597",
+    }
+    sitemap_urls = ["https://www.citya.com/sitemap.agences.xml"]
+    sitemap_rules = [(r"/agences-immobilieres/.*/[0-9]*$", "parse_sd")]
+
+
+
+    drop_attributes = {"image", "twitter", "facebook"}
+    # wanted_types = ["GroceryStore"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+
+        if item["email"] in ["qualite@citya.com", "rgpd@citya.com"]:
+            item.pop("email", None)
+        
+        apply_category(Categories.OFFICE_ESTATE_AGENT, item)
+
+        yield item
