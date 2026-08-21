84019f9fbdce15bc66afa8848993fcde9c72d7c0
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-21 14:24:26 +0200

    normalizes missing values on opening_hours

--- locations/spiders/apm_monaco.py
+++ locations/spiders/apm_monaco.py
@@ -13,14 +13,26 @@ class ApmMonacoSpider(JSONBlobSpider):
     }
     start_urls = ["https://storemapper-herokuapp-com.global.ssl.fastly.net/api/users/12892/stores.js"]
     locations_key = "stores"
-    custom_settings = {"ROBOTSTXT_OBEY": False,}
-
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+    }
+    
     def post_process_item(self, item, response, location):
         apply_category(Categories.SHOP_JEWELRY, item)
         item["name"] = "APM Monaco"
 
-        oh = OpeningHours()
-        oh.add_ranges_from_string(location["custom_field_1"]+" "+location["custom_field_2"]+" "+location["custom_field_3"])
-        item["opening_hours"] = oh.as_opening_hours()
+
+        fields = [
+            location.get("custom_field_1") or "",
+            location.get("custom_field_2") or "",
+            location.get("custom_field_3") or "",
+        ]
+
+        if any(fields):
+            oh = OpeningHours()
+            oh.add_ranges_from_string(" ".join(fields))
+            item["opening_hours"] = oh
+        else:
+            item["opening_hours"] = None
 
         yield item
