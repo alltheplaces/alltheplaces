33701bc01c79319593be2e773975151aad7e5ef4
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-21 18:19:06 +0200

    fixes: permanetly closed shops, using         item["opening_hours"] = OpeningHours(), using the main page url instead of the _next/data url

--- locations/spiders/dreams_donuts.py
+++ locations/spiders/dreams_donuts.py
@@ -1,7 +1,8 @@
 from locations.categories import Categories, apply_category
 from locations.hours import OpeningHours
 from locations.json_blob_spider import JSONBlobSpider
-
+from locations.items import set_closed
+import json
 
 class DreamsDonutsSpider(JSONBlobSpider):
     name = "dreams_donuts"
@@ -9,13 +10,26 @@ class DreamsDonutsSpider(JSONBlobSpider):
         "brand": "Dreams Donuts",
         "brand_wikidata": "Q141142873",
     }
-    start_urls = ["https://boutiques.dreamsdonuts.com/_next/data/0fZaY95cqqjf19_M8VcLX/index.json"]
-    locations_key = ["pageProps", "data", "stores"]
+
+    start_urls = ["https://boutiques.dreamsdonuts.com/"]
+
+    def extract_json(self, response):
+        data = response.css("script#__NEXT_DATA__::text").get()
+
+        if not data:
+            raise ValueError("__NEXT_DATA__ not found")
+
+        data = json.loads(data)
+        return data["props"]["pageProps"]["data"]["stores"]
 
     def post_process_item(self, item, response, location):
-        item["name"] = "Dreams Donuts"
+        item["branch"] = item.pop("name", "").removeprefix("Dreams Donuts ")
 
-        oh = OpeningHours()
+        if location.get("businessStatus") == "CLOSED_PERMANENTLY":
+            set_closed(item)
+
+
+        item["opening_hours"] = OpeningHours()
 
         if location.get("openingHours") is not None:
             for day in location["openingHours"]:
@@ -30,7 +44,7 @@ class DreamsDonutsSpider(JSONBlobSpider):
                     openMinutes = day.get("openTime").get("minutes") or "00"
                     closeMinutes = day.get("closeTime").get("minutes") or "00"
 
-                    oh.add_ranges_from_string(
+                    item["opening_hours"].add_ranges_from_string(
                         day.get("openDay")
                         + " "
                         + str(day.get("openTime").get("hours"))
@@ -42,6 +56,5 @@ class DreamsDonutsSpider(JSONBlobSpider):
                         + str(closeMinutes)
                     )
 
-        item["opening_hours"] = oh
         apply_category(Categories.SHOP_PASTRY, item)
         yield item
