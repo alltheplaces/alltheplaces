42d0767e654b08755e4b307b37330d03b08f623d
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-21 14:31:23 +0530

    code updated to fix spider

--- locations/spiders/nick_scali_furniture.py
+++ locations/spiders/nick_scali_furniture.py
@@ -1,42 +1,32 @@
-from typing import AsyncIterator, Iterable
+import json
+import re
+from typing import Iterable
 
-from scrapy import Selector
-from scrapy.http import Request, Response
+from scrapy import Spider
+from scrapy.http import Request, TextResponse
 
-from locations.hours import OpeningHours
+from locations.dict_parser import DictParser
 from locations.items import Feature
-from locations.storefinders.amasty_store_locator import AmastyStoreLocatorSpider
 
 
-class NickScaliFurnitureSpider(AmastyStoreLocatorSpider):
+class NickScaliFurnitureSpider(Spider):
     name = "nick_scali_furniture"
     item_attributes = {"brand": "Nick Scali", "brand_wikidata": "Q17053453"}
     allowed_domains = ["www.nickscali.com.au", "www.nickscali.co.nz"]
+    start_urls = ["https://www.nickscali.com.au/showrooms", "https://www.nickscali.co.nz/showrooms"]
 
-    async def start(self) -> AsyncIterator[Request]:
-        for domain in self.allowed_domains:
-            headers = {
-                "X-Requested-With": "XMLHttpRequest",
-                "Origin": f"https://{domain}",
-            }
-            yield Request(url=f"https://{domain}/amlocator/index/ajax/", method="POST", headers=headers)
-
-    def post_process_item(self, item: Feature, feature: dict, popup_html: Selector) -> Iterable[Request]:
-        item["addr_full"] = " ".join(
-            (" ".join(popup_html.xpath('//div[contains(@class, "amlocator-info-popup")]/text()').getall())).split()
+    def parse(self, response: TextResponse) -> Iterable[Feature | Request]:
+        locations = json.loads(
+            re.search(
+                r"showrooms\":(\[.*\]),\"allShowroomPath",
+                response.xpath('//*[contains(text(),"coordinate")]/text()').get().replace("\\", ""),
+            ).group(1)
         )
-        yield Request(url=item["website"], meta={"item": item}, callback=self.parse_opening_hours)
-
-    def parse_opening_hours(self, response: Response) -> Iterable[Feature]:
-        item = response.meta["item"]
-        item["opening_hours"] = OpeningHours()
-        hours_raw = (
-            " ".join(response.xpath('//div[contains(@class, "amlocator-schedule-table")]/div/span/text()').getall())
-            .replace(" - ", " ")
-            .replace(" am", "AM")
-            .replace(" pm", "PM")
-        ).split()
-        hours_raw = [hours_raw[n : n + 3] for n in range(0, len(hours_raw), 3)]
-        for day in hours_raw:
-            item["opening_hours"].add_range(day[0], day[1], day[2], "%I:%M%p")
-        yield item
+        for store in locations:
+            store.update(store.pop("fields"))
+            store.update(store.pop("coordinate"))
+            item = DictParser.parse(store)
+            item["branch"] = item.pop("name")
+            item["name"] = self.item_attributes["brand"]
+            item["website"] = response.urljoin(store.get("redirectUrl"))
+            yield item
