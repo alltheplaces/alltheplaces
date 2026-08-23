96bee6827a479b48a784c26a40f5ffdf69e3b6c5
Ian Dees <ian.dees@gmail.com>
2026-08-23 10:54:07 -0500

    add Chausséa spider

---
+++ locations/spiders/chaussea_fr.py
@@ -0,0 +1,19 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.structured_data_spider import StructuredDataSpider
+from locations.user_agents import BROWSER_DEFAULT
+
+
+class ChausseaFRSpider(SitemapSpider, StructuredDataSpider):
+    name = "chaussea_fr"
+    item_attributes = {"brand": "Chausséa", "brand_wikidata": "Q62082044"}
+    sitemap_urls = ["https://www.chaussea.com/modules/chssitemap/sitemaps/fr-fr-magasins.xml"]
+    sitemap_rules = [(r"/fr/magasin/[^/]+$", "parse_sd")]
+    wanted_types = ["Store"]
+    custom_settings = {"ROBOTSTXT_OBEY": False, "DOWNLOAD_DELAY": 0.5, "USER_AGENT": BROWSER_DEFAULT}
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.SHOP_SHOES, item)
+
+        yield item
