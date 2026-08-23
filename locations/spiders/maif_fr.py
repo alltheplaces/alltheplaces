9718ddd4d104dffdc6210f8bac438c1e3c6296ba
Ian Dees <ian.dees@gmail.com>
2026-08-23 11:28:52 -0500

    Match NSI brand string for Maif

--- locations/spiders/maif_fr.py
+++ locations/spiders/maif_fr.py
@@ -5,6 +5,6 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class MaifFRSpider(SitemapSpider, StructuredDataSpider):
     name = "maif_fr"
-    item_attributes = {"brand": "MAIF", "brand_wikidata": "Q3331029"}
+    item_attributes = {"brand": "Maif", "brand_wikidata": "Q3331029"}
     sitemap_urls = ["https://agence.maif.fr/sitemap_pois.xml"]
     sitemap_rules = [(r"/details$", "parse_sd")]
