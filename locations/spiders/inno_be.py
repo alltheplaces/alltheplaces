19fc60e1fe48b901c144cf727222b671a418bb7f
Keerthan <poojarykeerthan163@gmail.com>
2026-08-23 20:00:39 +0530

    [Inno BE] Add proxy requirement and browser user agent (#17892)

--- locations/spiders/inno_be.py
+++ locations/spiders/inno_be.py
@@ -5,8 +5,10 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
+from locations.google_url import extract_google_position
 from locations.hours import DAYS_NL, OpeningHours
 from locations.items import Feature
+from locations.user_agents import BROWSER_DEFAULT
 
 
 class InnoBESpider(CrawlSpider):
@@ -14,6 +16,8 @@ class InnoBESpider(CrawlSpider):
     item_attributes = {"brand": "Inno", "brand_wikidata": "Q300632"}
     allowed_domains = ["www.inno.be"]
     start_urls = ["https://www.inno.be/nl/stores"]
+    custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
+    requires_proxy = "BE"
     rules = [Rule(LinkExtractor(r"^https:\/\/www\.inno\.be\/nl\/stores\/detail\?id=[\w\-]+$"), "parse")]
 
     def parse(self, response: Response) -> Iterable[Feature]:
@@ -32,4 +36,9 @@ class InnoBESpider(CrawlSpider):
         apply_category(Categories.SHOP_DEPARTMENT_STORE, properties)
         hours_text = " ".join(response.xpath('//div[contains(@class, "js-store-hours")]//text()').getall())
         properties["opening_hours"].add_ranges_from_string(hours_text, days=DAYS_NL)
+        extract_google_position(properties, response)
+        if not properties.get("lat"):
+            if lat := response.xpath("//@data-lat|//@data-latitude").get():
+                properties["lat"] = float(lat)
+                properties["lon"] = float(response.xpath("//@data-lng|//@data-longitude").get())
         yield Feature(**properties)
