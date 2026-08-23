adbd864c49bbac651d657f53c0eb8caa89cd314b
Ian Dees <ian.dees@gmail.com>
2026-08-23 11:06:29 -0500

    Match NSI brand string and use default download delay

--- locations/spiders/chaussea_fr.py
+++ locations/spiders/chaussea_fr.py
@@ -7,11 +7,11 @@ from locations.user_agents import BROWSER_DEFAULT
 
 class ChausseaFRSpider(SitemapSpider, StructuredDataSpider):
     name = "chaussea_fr"
-    item_attributes = {"brand": "Chausséa", "brand_wikidata": "Q62082044"}
+    item_attributes = {"brand": "Chaussea", "brand_wikidata": "Q62082044"}
     sitemap_urls = ["https://www.chaussea.com/modules/chssitemap/sitemaps/fr-fr-magasins.xml"]
     sitemap_rules = [(r"/fr/magasin/[^/]+$", "parse_sd")]
     wanted_types = ["Store"]
-    custom_settings = {"ROBOTSTXT_OBEY": False, "DOWNLOAD_DELAY": 0.5, "USER_AGENT": BROWSER_DEFAULT}
+    custom_settings = {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         apply_category(Categories.SHOP_SHOES, item)
