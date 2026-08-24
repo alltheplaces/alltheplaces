db3ecade178352c411c91eadc01ad0678e533ea4
Cj Malone <CjMalone@mail.com>
2026-08-24 11:12:58 +0100

    lint

--- locations/spiders/orpi.py
+++ locations/spiders/orpi.py
@@ -7,23 +7,16 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class OrpiSpider(SitemapSpider, StructuredDataSpider):
     name = "orpi"
-    item_attributes = {
-        "brand": "Orpi",
-        "brand_wikidata": "Q3356080",
-    }
+    item_attributes = {"brand": "Orpi", "brand_wikidata": "Q3356080"}
     sitemap_urls = ["https://www.orpi.com/sitemap-agences.xml"]
-    sitemap_rules = [
-        (r"/", "parse_sd"),
-    ]
     wanted_types = ["RealEstateAgent"]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        apply_category(Categories.OFFICE_ESTATE_AGENT, item)
+        item["branch"] = item.pop("name", "")
 
         url = response.xpath("//a[contains(@href, 'google.com/maps')]/@href").get()
         if url is not None:
             item["lat"], item["lon"] = url_to_coords(url)
 
-        item["branch"] = item.pop("name", "")
-
+        apply_category(Categories.OFFICE_ESTATE_AGENT, item)
         yield item
