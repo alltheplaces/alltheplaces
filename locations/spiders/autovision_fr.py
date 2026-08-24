cbaaf81377dc35012683ea07eda0d3d0f9f68c6c
Cj Malone <CjMalone@mail.com>
2026-08-24 11:56:24 +0100

    lint

--- locations/spiders/autovision_fr.py
+++ locations/spiders/autovision_fr.py
@@ -6,19 +6,13 @@ from locations.structured_data_spider import StructuredDataSpider, clean_faceboo
 
 class AutovisionFRSpider(SitemapSpider, StructuredDataSpider):
     name = "autovision_fr"
-    item_attributes = {
-        "brand": "Autovision",
-        "brand_wikidata": "Q64224842",
-    }
+    item_attributes = {"brand": "Autovision", "brand_wikidata": "Q64224842"}
     sitemap_urls = ["https://www.autovision.fr/centres-sitemap.xml"]
-    sitemap_rules = [(r"/", "parse_sd")]
     wanted_types = ["AutomotiveBusiness"]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        apply_category(Categories.VEHICLE_INSPECTION, item)
-        item["branch"] = item.pop("name", "")
-
         if clean_facebook(item.get("facebook")) == "https://www.facebook.com/AutovisionOfficiel/":
             item["facebook"] = None
 
+        apply_category(Categories.VEHICLE_INSPECTION, item)
         yield item
