adfde3d38341b2cdcc71b3a5bc03ebfbcbff3122
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-24 20:16:17 +0200

    [adopt] Add Spider (639 locations)

---
+++ locations/spiders/adopt.py
@@ -0,0 +1,27 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+
+class AdoptSpider(SitemapSpider, StructuredDataSpider):
+    name = "adopt"
+    item_attributes = {
+        "brand": "Adopt'",
+        "brand_wikidata": "Q104649177",
+    }
+    sitemap_urls = ["https://www.adopt.com/en/sitemap/sitemap.xml"]
+    sitemap_rules = [
+        (r"/store-locator/", "parse_sd"),
+    ]
+    wanted_types = ["LocalBusiness"]
+    drop_attributes = ["facebook"]
+    time_format = "%I:%M %p"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.SHOP_PERFUMERY, item)
+
+        item["branch"] = item.pop("name", "").removeprefix("Adopt Parfums ")
+
+        yield item
