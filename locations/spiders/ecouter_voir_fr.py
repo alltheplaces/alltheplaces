6d05c380d8c0ef90fc1414839b338da6dd3422c0
Cj Malone <CjMalone@mail.com>
2026-08-24 12:38:07 +0100

    Rename spider

--- locations/spiders/ecouter_voir_fr.py
+++ locations/spiders/ecouter_voir_fr.py
@@ -4,12 +4,9 @@ from locations.categories import Categories, apply_category
 from locations.structured_data_spider import StructuredDataSpider, clean_facebook
 
 
-class EcouterVoirFrSpider(SitemapSpider, StructuredDataSpider):
+class EcouterVoirFRSpider(SitemapSpider, StructuredDataSpider):
     name = "ecouter_voir_fr"
-    item_attributes = {
-        "brand": "Écouter Voir",
-        "brand_wikidata": "Q18414551",
-    }
+    item_attributes = {"brand": "Écouter Voir", "brand_wikidata": "Q18414551"}
     sitemap_urls = ["https://magasins.ecoutervoir.fr/sitemap.xml"]
     sitemap_rules = [(r"[0-9]+$", "parse_sd")]
     drop_attributes = ["image"]
