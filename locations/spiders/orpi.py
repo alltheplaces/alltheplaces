e1a79780cde8b533cc3081d1d2cd29952668f484
Cj Malone <CjMalone@mail.com>
2026-08-24 11:14:57 +0100

    extract_google_position

--- locations/spiders/orpi.py
+++ locations/spiders/orpi.py
@@ -1,7 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
-from locations.google_url import url_to_coords
+from locations.google_url import extract_google_position
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -14,9 +14,7 @@ class OrpiSpider(SitemapSpider, StructuredDataSpider):
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["branch"] = item.pop("name", "")
 
-        url = response.xpath("//a[contains(@href, 'google.com/maps')]/@href").get()
-        if url is not None:
-            item["lat"], item["lon"] = url_to_coords(url)
+        extract_google_position(item, response)
 
         apply_category(Categories.OFFICE_ESTATE_AGENT, item)
         yield item
