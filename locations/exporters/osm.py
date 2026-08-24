5d0ee4b2ea90243902e354187ac344906b9a82b7
Cj Malone <CjMalone@mail.com>
2026-08-24 17:30:07 +0100

    Use new func

--- locations/exporters/osm.py
+++ locations/exporters/osm.py
@@ -48,7 +48,7 @@ class OSMExporter(XmlItemExporter):
         self.xg.startElement(self.item_element, AttributesImpl(attrs))
         self.next_id -= 1
         self._beautify_newline()
-        for name, value in self._get_serialized_fields(item, default_value=""):
+        for name, value in self.get_serialized_fields(item, default_value=""):
             self._export_xml_field(name, value, depth=2)
         self._beautify_indent(depth=1)
         self.xg.endElement(self.item_element)
