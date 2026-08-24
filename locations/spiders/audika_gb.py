6e11a785cd229c9a32e70e389f048cbe32d55260
rjw62 <rjw62@cantab.net>
2026-08-24 19:04:22 +0100

    Add new spider for Audika UK

---
+++ locations/spiders/audika_gb.py
@@ -0,0 +1,45 @@
+import json
+from typing import Any
+
+import re
+
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.dict_parser import DictParser
+from locations.hours import DAYS, OpeningHours
+
+from locations.categories import Categories, apply_category
+#from locations.items import Feature
+
+
+class AudikaSpider(CrawlSpider):
+    name = "audika_gb"
+    item_attributes = {"brand": "Audika", "brand_wikidata": "Q2870745"}
+    start_urls = [
+        "https://www.audika.co.uk/hearing-aids-centre/all-clinics"
+    ]
+    rules = [Rule(LinkExtractor(allow=r"/hearing-aids-centre/[a-z-]+/audika-"), callback="parse")]
+    #rules = [Rule(LinkExtractor(allow=r"/hearing-aids-centre/aberdeenshire/audika-"), callback="parse")]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        raw_data = json.loads(response.xpath('//*[@type="application/ld+json"][@class="clinic-schema"]/text()').get())
+        item = DictParser.parse(raw_data)
+        item["ref"] = item["website"] = response.url
+        pagetitle = response.xpath("//title/text()").get()
+        pagetitle = pagetitle.removeprefix('Audika ').removeprefix('clinics in ')
+        pagetitle = re.sub(r' ?\|.*$','',pagetitle)
+        item["branch"] = pagetitle
+        item["name"] = 'Audika'
+        centerid = re.search(r'([0-9]+)$',response.xpath("//a[starts-with(@href,'/book-appointment/online-booking?centerid=')]/@href").get())
+        if centerid:
+            item["extras"]["center_id"] = centerid.group(1)
+        item["addr_full"] = item.pop("street_address")
+        if item.get("email") and 'info@audika.co.uk' in item["email"]:
+            del item["email"]
+        item["opening_hours"] = OpeningHours()
+        for str in raw_data.get("openingHours"):
+            item["opening_hours"].add_ranges_from_string(str)
+        apply_category(Categories.SHOP_HEARING_AIDS, item)
+        yield item
