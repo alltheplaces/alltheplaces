61216e9b16c3dc77b4b713d01fde3173c55cff40
ehsan-firdaus <ehsanfirdaus@gmail.com>
2026-08-25 16:34:58 +0530

    [Adidas] Fix spider

--- locations/spiders/adidas.py
+++ locations/spiders/adidas.py
@@ -1,35 +1,58 @@
-import json
+from typing import Any
 
-from scrapy import Spider
-from scrapy.http import Response
+from scrapy.http import JsonRequest, Response
 
-from locations.categories import apply_yes_no
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from locations.hours import OpeningHours
+from locations.items import set_closed
+from locations.pipelines.address_clean_up import merge_address_lines
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class AdidasSpider(Spider):
+class AdidasSpider(PlaywrightSpider):
     name = "adidas"
     item_attributes = {"brand": "Adidas", "brand_wikidata": "Q3895"}
-    start_urls = [
-        "https://placesws.adidas-group.com/API/search?brand=adidas&geoengine=google&method=get&category=store&format=json&storetype=ownretail&latlng=0%2C0"
-    ]
-    custom_settings = {"ROBOTSTXT_OBEY": False}
+    start_urls = ["https://www.adidas.com/gw/locations/stores/directory"]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"ROBOTSTXT_OBEY": False, "USER_AGENT": BROWSER_DEFAULT}
 
-    def parse(self, response: Response, **kwargs):
-        data = json.loads(response.body.replace(b"\n", b"\\n").replace(b"\r", b"\\r").replace(b"\\2", b"\\\\2"))
-        for shop in data["wsResponse"]["result"]:
-            item = DictParser.parse(shop)
-            item["lat"] = shop["latitude_google"]
-            item["lon"] = shop["longitude_google"]
-            item["street_address"] = shop["street1"]
-            item["email"] = shop.get("email1")
-            slug = (
-                "-".join([shop["city"], shop["street1"], shop["name"]])
-                .lower()
-                .replace(" ", "-")
-                .replace(",", "")
-                .replace("/", "")
-            )
-            item["website"] = f"https://www.adidas.co.uk/storefront/{item['ref']}-{slug}"
-            apply_yes_no("factory_outlet", item, shop.get("factory_outlet") == "1")
-            yield item
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for countries in DictParser.iter_matching_keys(response.json(), "countries"):
+            for country in countries:
+                yield JsonRequest(
+                    url=f'https://www.adidas.com/gw/locations/stores/directory/{country["country_code"]}',
+                    callback=self.parse_stores,
+                    cb_kwargs={"country": country["country_code"]},
+                )
+
+    def parse_stores(self, response: Response, country: str) -> Any:
+        for stores in DictParser.iter_matching_keys(response.json(), "stores"):
+            for store in stores:
+                store.update(store.pop("location", {}))
+                item = DictParser.parse(store)
+                item["street_address"] = merge_address_lines(
+                    [store["address"].get("address_line1"), store["address"].get("address_line2")]
+                )
+                item["country"] = country
+
+                if opening_hours := store.get("opening_hours"):
+                    item["opening_hours"] = self.parse_opening_hours(opening_hours)
+
+                apply_category(Categories.SHOP_SPORTS, item)
+
+                if store["status"] == "CLOSED":
+                    set_closed(item)
+
+                yield item
+
+    def parse_opening_hours(self, rules: list) -> OpeningHours:
+        opening_hours = OpeningHours()
+        for rule in rules:
+            day = rule["day"]
+            if rule.get("status") == "CLOSED":
+                opening_hours.set_closed(day)
+                continue
+            opening_hours.add_range(day, rule["hours"]["from"], rule["hours"]["to"])
+        return opening_hours
