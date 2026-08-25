cc280cb34af2f5376cfa8198639aa1321da690ce
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-25 21:05:26 +0200

    add wikidata

--- locations/spiders/gold_union_fr.py
+++ locations/spiders/gold_union_fr.py
@@ -8,7 +8,7 @@ from locations.categories import Categories, apply_category
 
 class GoldUnionFRSpider(StockistSpider):
     name = "gold_union_fr"
-    item_attributes = {"brand": "Gold Union", "brand_wikidata": ""}
+    item_attributes = {"brand": "Gold Union", "brand_wikidata": "Q131622916"}
     key = "u20465"
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
