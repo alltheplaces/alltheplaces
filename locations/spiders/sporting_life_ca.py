7e9c55925497c6fddb5b5e2c7b8eee7ccc6e9eba
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-08-25 11:01:13 +0530

    fix spider

--- locations/spiders/sporting_life_ca.py
+++ locations/spiders/sporting_life_ca.py
@@ -1,12 +1,21 @@
-from locations.storefinders.sweetiq import SweetIQSpider
+from typing import Any, Iterable
+
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature, set_closed
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class SportingLifeCASpider(SweetIQSpider):
+class SportingLifeCASpider(SitemapSpider, StructuredDataSpider):
     name = "sporting_life_ca"
     item_attributes = {"brand": "Sporting Life", "brand_wikidata": "Q7579583"}
-    start_urls = ["https://stores.sportinglife.ca/"]
+    sitemap_urls = ["https://locations.sportinglife.ca/sitemap.xml"]
+    sitemap_rules = [(r"https://locations\.sportinglife\.ca/[-\w]+$", "parse")]
 
-    def parse_item(self, item, location):
-        item.pop("email")
-        item.pop("website")
+    def post_process_item(self, item: Feature, response: Response, ld_data: dict, **kwargs: Any) -> Iterable[Feature]:
+        if "closed" in item["name"].lower():
+            set_closed(item)
+        apply_category(Categories.SHOP_SPORTS, item)
         yield item
