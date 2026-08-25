bbb2c5dfc7ffa58af837a1a53781742cd883ac6f
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-25 19:58:54 +0200

    [repetto] Add Spider (106 locations)

---
+++ locations/spiders/repetto.py
@@ -0,0 +1,16 @@
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+
+class RepettoSpider(JSONBlobSpider):
+    name = "repetto"
+    item_attributes = {
+        "brand": "Repetto",
+        "brand_wikidata": "Q3427237",
+    }
+    start_urls = ["https://xnkosumdt5.execute-api.eu-west-3.amazonaws.com/prod/getStoreLocators"]
+
+    def post_process_item(self, item, response, location):
+        item["ref"] = item.pop("name","")
+        apply_category(Categories.SHOP_CLOTHES, item)
+
+        yield item
