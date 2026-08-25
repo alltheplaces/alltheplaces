3f86cab1090b9305ff9b9d9aad5cce19e8dfa3ce
Cj Malone <CjMalone@mail.com>
2026-08-25 16:28:32 +0100

    Stricter sitemap_rules

--- locations/spiders/noz_fr.py
+++ locations/spiders/noz_fr.py
@@ -6,19 +6,14 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class NozFRSpider(SitemapSpider, StructuredDataSpider):
     name = "noz_fr"
-    item_attributes = {
-        "brand": "NOZ",
-        "brand_wikidata": "Q3345688",
-    }
+    item_attributes = {"brand": "NOZ", "brand_wikidata": "Q3345688"}
     sitemap_urls = ["https://www.noz.fr/map-location-sitemap.xml"]
-    sitemap_rules = [
-        (r"", "parse_sd"),
-    ]
+    sitemap_rules = [("/noz-", "parse_sd")]
     wanted_types = ["LocalBusiness"]
     drop_attributes = ["facebook"]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        apply_category(Categories.SHOP_VARIETY_STORE, item)
         item["branch"] = item.pop("name", "").removeprefix("NOZ ")
+        apply_category(Categories.SHOP_VARIETY_STORE, item)
 
         yield item
