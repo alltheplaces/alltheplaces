3d0e8a53dffc1cf951a293db0ec8afe9b494f62c
Cj Malone <CjMalone@mail.com>
2026-08-25 10:21:41 +0100

    Switch to StructuredDataSpider

--- locations/spiders/audika_gb.py
+++ locations/spiders/audika_gb.py
@@ -1,31 +1,28 @@
-import json
 import re
-from typing import Any
+from typing import Iterable
 
-from scrapy.http import Response
+from scrapy.http import TextResponse
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
-from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class AudikaGBSpider(CrawlSpider):
+class AudikaGBSpider(CrawlSpider, StructuredDataSpider):
     name = "audika_gb"
     item_attributes = {"brand": "Audika", "brand_wikidata": "Q2870745"}
     start_urls = ["https://www.audika.co.uk/hearing-aids-centre/all-clinics"]
     rules = [Rule(LinkExtractor(allow=r"/hearing-aids-centre/[a-z-]+/audika-"), callback="parse")]
+    wanted_types = ["LocalBusiness"]
+    search_for_facebook = False
+    search_for_email = False
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        raw_data = json.loads(response.xpath('//*[@type="application/ld+json"][@class="clinic-schema"]/text()').get())
-        item = DictParser.parse(raw_data)
+    def post_process_item(self, item: Feature, response: TextResponse, raw_data: dict, **kwargs) -> Iterable[Feature]:
         item["ref"] = item["website"] = response.url
-        pagetitle = response.xpath("//title/text()").get()
-        pagetitle = pagetitle.removeprefix("Audika ").removeprefix("clinics in ")
-        pagetitle = re.sub(r" ?\|.*$", "", pagetitle)
-        item["branch"] = pagetitle
-        item["name"] = "Audika"
+        item["branch"] = item.pop("name").removeprefix("Audika ")
         centerid = re.search(
             r"([0-9]+)$",
             response.xpath("//a[starts-with(@href,'/book-appointment/online-booking?centerid=')]/@href").get(),
@@ -33,8 +30,6 @@ class AudikaGBSpider(CrawlSpider):
         if centerid:
             item["extras"]["center_id"] = centerid.group(1)
         item["addr_full"] = item.pop("street_address")
-        if item.get("email") and "info@audika.co.uk" in item["email"]:
-            del item["email"]
         item["opening_hours"] = OpeningHours()
         for str in raw_data.get("openingHours"):
             item["opening_hours"].add_ranges_from_string(str)
