8b22479610dcf7c0e7424b855e1bbf9ff86891d3
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-25 20:58:15 +0200

    fix class name and apply category

--- locations/spiders/gold_union_fr.py
+++ locations/spiders/gold_union_fr.py
@@ -2,13 +2,16 @@ from typing import Iterable
 
 from locations.items import Feature
 from locations.storefinders.stockist import StockistSpider
+from locations.categories import Categories, apply_category
 
 
-class GoldUnionFR(StockistSpider):
+
+class GoldUnionFRSpider(StockistSpider):
     name = "gold_union_fr"
     item_attributes = {"brand": "Gold Union", "brand_wikidata": ""}
     key = "u20465"
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
+        apply_category(Categories.SHOP_GOLD_BUYER, item)
         yield item
