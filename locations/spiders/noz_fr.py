450761bf5a1f9ed8d4b93352e3d047af9eb766e2
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-25 13:26:43 +0200

    [noz_fr] Add Spider (369 locations)

---
+++ locations/spiders/noz_fr.py
@@ -0,0 +1,24 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class NozFRSpider(SitemapSpider, StructuredDataSpider):
+    name = "noz_fr"
+    item_attributes = {
+        "brand": "NOZ",
+        "brand_wikidata": "Q3345688",
+    }
+    sitemap_urls = ["https://www.noz.fr/map-location-sitemap.xml"]
+    sitemap_rules = [
+        (r"", "parse_sd"),
+    ]
+    wanted_types = ["LocalBusiness"]
+    drop_attributes = ["facebook"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.SHOP_VARIETY_STORE, item)
+        item["branch"] = item.pop("name", "").removeprefix("NOZ ")
+
+        yield item
