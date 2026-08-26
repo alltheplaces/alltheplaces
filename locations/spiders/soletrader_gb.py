d803e5ae5597ce04aa0d0269393c4ac3dc4267e4
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-08-26 11:00:09 +0530

    fix spider

--- locations/spiders/soletrader_gb.py
+++ locations/spiders/soletrader_gb.py
@@ -1,44 +1,46 @@
-import json
-import re
 from typing import Any
 
+import chompjs
 from scrapy.http import Response
-from scrapy.spiders import Spider
 
-from locations.categories import Categories
+from locations.camoufox_spider import CamoufoxSpider
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import DAYS_EN, DAYS_FULL, OpeningHours
-from locations.items import Feature
+from locations.react_server_components import parse_rsc
+from locations.settings import DEFAULT_CAMOUFOX_SETTINGS
 
 
-class SoletraderGBSpider(Spider):
+class SoletraderGBSpider(CamoufoxSpider):
     name = "soletrader_gb"
-    item_attributes = {"brand": "Soletrader", "brand_wikidata": "Q25101942", "extras": Categories.SHOP_SHOES.value}
+    item_attributes = {"brand": "Soletrader", "brand_wikidata": "Q25101942"}
     start_urls = ["https://www.soletrader.co.uk/store-locator"]
+    custom_settings = DEFAULT_CAMOUFOX_SETTINGS
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
-        data = response.xpath('//script[contains(text(),"postalCode")]/text()').get()
-        data = re.sub(r'\\"', '"', data)
-        data = re.sub(r'^.*locations":', "", data)
-        data = re.sub(r'\}\]\\n"\]\)$', "", data)
-        for location in json.loads(data):
-            item = Feature()
+        scripts = response.xpath("//script[starts-with(text(), 'self.__next_f.push')]/text()").getall()
+        payload = "".join(s for _, s in map(chompjs.parse_js_object, scripts) if isinstance(s, str))
+
+        for location in DictParser.get_nested_key(dict(parse_rsc(payload.encode())), "locations"):
+            if not location["code"].isdigit():
+                continue  # Fulfilment regions, not shops.
+
             address = location["address"]
             item = DictParser.parse(address)
+            item["ref"] = location["code"]
+            item["branch"] = (
+                location["label"].removeprefix("SOLETRADER OUTLET ").removeprefix("SOLETRADER ").removeprefix("SOLE ")
+            )
             item["street_address"] = address["address2"]
-            item["name"] = address["address1"]
-            item["ref"] = location["entityId"]
 
-            oh = OpeningHours()
+            item["opening_hours"] = OpeningHours()
             for day in DAYS_FULL:
-                open_time = location["operatingHours"][day.lower()]["opening"]
-                close_time = location["operatingHours"][day.lower()]["closing"]
-                oh.add_range(
-                    day=DAYS_EN[day],
-                    open_time=open_time,
-                    close_time=close_time,
-                )
+                hours = location["operatingHours"][day.lower()]
+                if not hours["open"]:
+                    item["opening_hours"].set_closed(DAYS_EN[day])
+                    continue
+                item["opening_hours"].add_range(DAYS_EN[day], hours["opening"], hours["closing"])
 
-            item["opening_hours"] = oh
+            apply_category(Categories.SHOP_SHOES, item)
 
             yield item
