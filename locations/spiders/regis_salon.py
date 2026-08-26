58716920b587d64469403fc967c40a4ef2245d61
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-26 17:38:22 +0530

    code refactored to fix spider

--- locations/spiders/regis_salon.py
+++ locations/spiders/regis_salon.py
@@ -1,40 +1,35 @@
-import json
-import re
+from typing import Any
 
-import scrapy
+from scrapy.http import Response
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories
+from locations.hours import OpeningHours
 from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class RegisSalonSpider(scrapy.Spider):
+class RegisSalonSpider(SitemapSpider):
     name = "regis_salon"
     item_attributes = {"brand": "Regis", "brand_wikidata": "Q7309325", "extras": Categories.SHOP_HAIRDRESSER.value}
-    allowed_domains = ["www.regissalons.com"]
-    start_urls = ["https://www.regissalons.com/salon-locator.html"]
+    sitemap_urls = ["https://www.signaturestyle.com/sitemap.xml"]
+    sitemap_rules = [(r"https://www.signaturestyle.com/locations/[^/]+/[^/]+/[^/]+", "parse")]
 
-    def parse(self, response):
-        for href in response.xpath('//@href[contains(., "/locations/")]').extract():
-            yield scrapy.Request(response.urljoin(href), callback=self.parse_store)
-
-    def parse_store(self, response):
-        script = response.xpath('//script[contains(text(), "wpslMap_0")]/text()').get()
-        json_txt = re.search("wpslMap_0 = (.*);$", script, re.M)[1]
-        data = json.loads(json_txt)["locations"][0]
-        # decode entities
-        name = scrapy.Selector(text=data["store"]).xpath("//text()").get()
-        phone = response.xpath('//@href[contains(., "tel:")]').extract_first().replace("tel:", "")
-        properties = {
-            "lat": data["lat"],
-            "lon": data["lng"],
-            "ref": data["id"],
-            "name": name,
-            "website": response.url,
-            "street_address": data["address2"],
-            "city": data["city"],
-            "state": data["state"],
-            "postcode": data["zip"],
-            "country": data["country"],
-            "phone": phone,
-        }
-        yield Feature(**properties)
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        item = Feature()
+        item["branch"] = response.xpath('//*[@itemprop="headline"]/@content').get().split("-", 1)[1]
+        item["street_address"] = response.xpath('//*[@class="sqs-html-content"]/p//text()').get()
+        item["addr_full"] = merge_address_lines(
+            [item["street_address"], response.xpath('//*[@class="sqs-html-content"]/p/span[2]/text()').get()]
+        )
+        item["lat"] = response.xpath('//*[@property="og:latitude"]/@content').get()
+        item["lon"] = response.xpath('//*[@property="og:longitude"]/@content').get()
+        item["phone"] = response.xpath('//*[@class="sqs-html-content"]/p[2]//text()').get()
+        item["website"] = item["ref"] = response.url
+        oh = OpeningHours()
+        for time_text in response.xpath(
+                '//*[@class="col sqs-col-5 span-5"]//*[@class="sqs-html-content"]//p//text()'
+        ).getall():
+            oh.add_ranges_from_string(time_text)
+        item["opening_hours"] = oh
+        yield item
