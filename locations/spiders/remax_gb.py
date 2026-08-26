4aa9b7f53e10aa9346259b7f2fd5f5e0f19841e6
mohit-sharma2194 <51069345+mohit-sharma2194@users.noreply.github.com>
2026-08-26 17:45:38 +0530

    code updated to fix spider

--- locations/spiders/remax_gb.py
+++ locations/spiders/remax_gb.py
@@ -1,9 +1,10 @@
-from typing import AsyncIterator
+from typing import Any
 
 from scrapy import Spider
-from scrapy.http import Request
+from scrapy.http import Response
 
-from locations.dict_parser import DictParser
+from locations.items import Feature
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
 class RemaxGBSpider(Spider):
@@ -12,27 +13,15 @@ class RemaxGBSpider(Spider):
         "brand": "RE/MAX",
         "brand_wikidata": "Q965845",
     }
-    allowed_domains = ["remax.uk"]
+    start_urls = ["https://remax.co.uk/re-max-offices/"]
 
-    async def start(self) -> AsyncIterator[Request]:
-        template = "https://remax.uk/api/locations?name={}"
-        for i in range(97, 123):
-            yield Request(url=template.format(chr(i)), callback=self.parse_place_id)
-
-    def parse_place_id(self, response):
-        template = "https://remax.uk/api/locationsbyplaceid?placeid={}"
-        for id in response.json().get("predictions"):
-            yield Request(url=template.format(id.get("place_id")), callback=self.parse)
-
-    def parse(self, response):
-        for data in response.json().get("branches"):
-            item = DictParser.parse(data)
-            item["ref"] = data.get("branch_id")
-            item["name"] = data.get("branch_name")
-            item["street_address"] = data.get("branch_address1")
-            item["city"] = data.get("branch_town")
-            item["postcode"] = data.get("branch_postcode")
-            item["phone"] = data.get("branch_phone")
-            item["email"] = data.get("branch_email")
-
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        for location in response.xpath('//*[@id="resultscontainer"]//*[@class="pf-itemcontainer"]'):
+            item = Feature()
+            item["name"] = location.xpath('.//*[@class="officename"]/text()').get()
+            item["street_address"] = location.xpath('.//*[@class="profileaddrdetails"]/text()').get()
+            item["addr_full"] = merge_address_lines(
+                location.xpath('.//*[@class="profileaddrdetails"]//text()').getall()
+            )
+            item["website"] = item["ref"] = response.urljoin(location.xpath(".//a/@href").get())
             yield item
