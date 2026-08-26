e1b005520a1077e87429f67dd1c6c1ab82e3c566
Cj Malone <CjMalone@mail.com>
2026-08-26 09:31:03 +0100

    All or nothing opening hours

--- locations/spiders/darjeeling_fr.py
+++ locations/spiders/darjeeling_fr.py
@@ -1,39 +1,39 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
 from locations.categories import Categories, Clothes, apply_category, apply_clothes
 from locations.hours import DAYS_FULL, OpeningHours
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
 class DarjeelingFRSpider(JSONBlobSpider):
     name = "darjeeling_fr"
-    item_attributes = {
-        "brand": "Darjeeling",
-        "brand_wikidata": "Q3016203",
-    }
+    item_attributes = {"brand": "Darjeeling", "brand_wikidata": "Q3016203"}
     start_urls = ["https://cdn.new.darjeeling.fr/yext/stores.json"]
     locations_key = "stores"
 
-    def post_process_item(self, item, response, location):
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         apply_category(Categories.SHOP_CLOTHES, item)
         apply_clothes(Clothes.UNDERWEAR, item)
 
         name = item.pop("name", "") or ""
         item["branch"] = name.removeprefix("Darjeeling").removeprefix(" ")
-        item["opening_hours"] = self.parse_hours(location.get("hours"))
+        try:
+            if hours := feature.get("hours"):
+                item["opening_hours"] = self.parse_hours(hours)
+        except Exception:
+            pass
         yield item
 
     @staticmethod
     def parse_hours(hours: dict) -> OpeningHours:
-        if hours is None:
-            return
-
         oh = OpeningHours()
-        try:
-            for day in map(str.lower, DAYS_FULL):
-                if hours[day].get("isClosed") is True:
-                    oh.set_closed(day)
-                else:
-                    for time in hours[day]["openIntervals"]:
-                        oh.add_range(day, time["start"], time["end"])
-        except KeyError:
-            pass
+        for day in map(str.lower, DAYS_FULL):
+            if hours[day].get("isClosed") is True:
+                oh.set_closed(day)
+            else:
+                for time in hours[day]["openIntervals"]:
+                    oh.add_range(day, time["start"], time["end"])
         return oh
