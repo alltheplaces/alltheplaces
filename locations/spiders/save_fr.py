592ae2ad9d0fdb5b6e08b4d278b5dba4b98007a3
Cj Malone <CjMalone@mail.com>
2026-08-28 12:40:05 +0100

    Exclude Point Service Mobiles

--- locations/spiders/save_fr.py
+++ locations/spiders/save_fr.py
@@ -12,7 +12,7 @@ class SaveFRSpider(SitemapSpider, StructuredDataSpider):
     name = "save_fr"
     item_attributes = {"brand": "Save", "brand_wikidata": "Q121289450"}
     sitemap_urls = ["https://magasin.save.co/sitemap.xml"]
-    sitemap_rules = [(r"https://magasin\.save\.co/.+\d+/?$", "parse_sd")]
+    sitemap_rules = [(r"https://magasin\.save\.co/.+/save\-[^/]+(\d+)$", "parse_sd")]
     wanted_types = ["MobilePhoneStore"]
     drop_attributes = {"image"}
 
