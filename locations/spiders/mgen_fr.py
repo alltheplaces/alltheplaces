ea732f69b185c9abece3c26b5f2439bd20a339d6
Cj Malone <CjMalone@mail.com>
2026-08-28 10:01:15 +0100

    apply_category

--- locations/spiders/mgen_fr.py
+++ locations/spiders/mgen_fr.py
@@ -1,17 +1,21 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
-from locations.categories import Categories
+from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class MGENFRSpider(SitemapSpider, StructuredDataSpider):
     name = "mgen_fr"
-    item_attributes = {
-        "brand": "MGEN",
-        "brand_wikidata": "Q3331039",
-        "extras": Categories.OFFICE_INSURANCE.value,
-    }
+    item_attributes = {"brand": "MGEN", "brand_wikidata": "Q3331039"}
     sitemap_urls = ["https://proximite.mgen.fr/locationsitemap1.xml"]
     sitemap_rules = [(r"https://proximite\.mgen\.fr/.+", "parse_sd")]
     wanted_types = ["LocalBusiness"]
     drop_attributes = {"image"}
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        apply_category(Categories.OFFICE_INSURANCE, item)
+        yield item
