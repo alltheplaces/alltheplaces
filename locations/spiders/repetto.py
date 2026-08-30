a31c7155a5c7e9f867e44bb3c7afe0271ff4dbf2
Ian Dees <ian.dees@gmail.com>
2026-08-29 19:57:52 -0500

    Normalize French country names to ISO codes in Repetto spider

--- locations/spiders/repetto.py
+++ locations/spiders/repetto.py
@@ -10,8 +10,36 @@ class RepettoSpider(JSONBlobSpider):
     }
     start_urls = ["https://xnkosumdt5.execute-api.eu-west-3.amazonaws.com/prod/getStoreLocators"]
 
+    # Map French country names to ISO codes
+    COUNTRY_MAPPING = {
+        "France": "FR",
+        "Belgique": "BE",
+        "Italie": "IT",
+        "Canada": "CA",
+        "Émirats arabes unis": "AE",
+        "Danemark": "DK",
+        "Japon": "JP",
+        "Corée du Sud": "KR",
+        "États-Unis": "US",
+        "Suisse": "CH",
+        "Allemagne": "DE",
+        "Espagne": "ES",
+        "Australie": "AU",
+        "Turquie": "TR",
+        "Irlande": "IE",
+        "Norvège": "NO",
+        "Autriche": "AT",
+        "Chypre": "CY",
+        "Colombie": "CO",
+    }
+
     def post_process_item(self, item, response, location):
         item["ref"] = item.get("name")
         item["branch"] = item.pop("name", "")
+
+        # Map French country names to ISO codes
+        if "country" in item:
+            item["country"] = self.COUNTRY_MAPPING.get(item["country"], item["country"])
+
         apply_category(Categories.SHOP_CLOTHES, item)
         yield item
