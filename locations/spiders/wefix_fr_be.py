584171195ff5511162edc03065171ac63c38edfb
Rémi Simonnin <81098692+remi-sim@users.noreply.github.com>
2026-08-31 01:10:55 +0200

    [WeFix] Add spider (128 locations)

---
+++ locations/spiders/wefix_fr_be.py
@@ -0,0 +1,24 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class WeFixFRBESpider(SitemapSpider, StructuredDataSpider):
+    name = "wefix_fr_be"
+    item_attributes = {"brand": "WeFix", "brand_wikidata": "Q121543865"}
+    sitemap_urls = ["https://boutique.wefix.net/locationsitemap1.xml"]
+    sitemap_rules = [(r"https\:\/\/boutique\.wefix\.net\/.+$", "parse_sd")]
+    wanted_types = ["LocalBusiness"]
+    drop_attributes = {"image", "twitter", "facebook"}
+
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        apply_category(Categories.SHOP_MOBILE_PHONE, item)
+        if item.get("phone") == "+33 1 76 50 76 50":
+        	item.pop("phone", None)
+            
+        yield item
