6e3d276a1d3a30ef85aaff9a4c72fde258d88c55
Cj Malone <Cj-Malone@users.noreply.github.com>
2026-08-30 01:56:20 +0100

    [BigMat] Collect FR locations (#18134)

--- locations/spiders/bigmat.py
+++ locations/spiders/bigmat.py
@@ -3,16 +3,20 @@ from typing import Iterable
 from scrapy.http import TextResponse
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class BigmatBESpider(SitemapSpider, StructuredDataSpider):
-    name = "bigmat_be"
+class BigmatSpider(SitemapSpider, StructuredDataSpider):
+    name = "bigmat"
     item_attributes = {"brand": "BigMat", "brand_wikidata": "Q101851862"}
-    sitemap_urls = ["https://magasins.bigmat.be/sitemap.xml"]
+    sitemap_urls = ["https://magasins.bigmat.be/sitemap.xml", "https://magasins.bigmat.fr/sitemap.xml"]
     sitemap_rules = [("/bigmat-", "parse_sd")]
+    wanted_types = ["LocalBusiness"]
+    search_for_twitter = False
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         item["branch"] = item.pop("name").replace("BigMat ", "")
+        apply_category(Categories.SHOP_TRADE, item)
         yield item
