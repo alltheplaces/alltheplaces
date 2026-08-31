cdb595c4b66289b153c4151c8d42bfcc433f054d
TAKAHASHI Shuuji <id@shuuji3.xyz>
2026-09-01 01:21:43 +0900

    refactor: clean up comments

--- locations/spiders/welcia_jp.py
+++ locations/spiders/welcia_jp.py
@@ -199,7 +199,7 @@ PAYMENT_METHODS = {
     "00255": "payment:pitapa",
 }
 
-# service-flag / detail-field code -> meaning (ref. welcia_detail_page.md)
+# service-flag / detail-field code
 FLAG_CLOSED = "00147"  # 閉店 -> remove from dataset when true
 FLAG_FAX = "00040"  # Fax番号
 FLAG_TOILETS_OSTOMY = "00056"  # オストメイトトイレ
@@ -220,44 +220,7 @@ class WelciaJPSpider(LocationCloudSpider):
     api_endpoint = "https://store.welcia.co.jp/welcia/api/proxy2/shop/list"
     website_formatter = "https://store.welcia.co.jp/welcia/spot/detail?code={}"
 
-    """
-    example:
-    {
-        "code": "1001D",
-        "name": "ウエルシア春日部一ノ割店",
-        "ruby": "ウエルシアカスカベイチノワリテン", // 822 stores have non-empty ruby / 2144 has no 'ruby' key
-        "phone": "048-735-4739",
-        "address_name": "埼玉県春日部市一ノ割1-11-20",
-        "address_code": "11214", // municipality code / 11214 === 埼玉県春日部市
-                                 // ref. 市区町村コードから探す(市区町村) | 政府統計の総合窓口 - https://www.e-stat.go.jp/municipalities/cities/areacodesearch?date_year=2026&date_month=8&date_day=29&ht=11214&op=search&keyword_kd=code&item%5B%5D=htCode&item%5B%5D=todoNm&item%5B%5D=parentCityNm&item%5B%5D=parentCityKana&item%5B%5D=selfCityNm&item%5B%5D=selfCityKana&item%5B%5D=htCodeSDate&item%5B%5D=jiyuId&sort%5B%5D=htCode-asc&choices_to_show%5B%5D=cityType&choices_to_show%5B%5D=kasoFlg&choices_to_show%5B%5D=htCodeKokujiDate&choices_to_show%5B%5D=htCodeKokujiNo&choices_to_show%5B%5D=htCodeEDate&choices_to_sort%5B%5D=kasoFlg&choices_to_sort%5B%5D=htCodeSDate&choices_to_sort%5B%5D=htCodeEDate&choices_to_sort%5B%5D=htCodeKokujiDate&choices_to_sort%5B%5D=htCodeKokujiNo&choices_to_sort_value%5B%5D=htCode-desc&choices_to_sort_value%5B%5D=kasoFlg-asc&choices_to_sort_value%5B%5D=kasoFlg-desc&choices_to_sort_value%5B%5D=htCodeSDate-asc&choices_to_sort_value%5B%5D=htCodeSDate-desc&choices_to_sort_value%5B%5D=htCodeEDate-asc&choices_to_sort_value%5B%5D=htCodeEDate-desc&choices_to_sort_value%5B%5D=htCodeKokujiDate-asc&choices_to_sort_value%5B%5D=htCodeKokujiDate-desc&choices_to_sort_value%5B%5D=htCodeKokujiNo-asc&choices_to_sort_value%5B%5D=htCodeKokujiNo-desc&form_id=city_areacode_search_form&source=setup&page=
-        "postal_code": "3440031",
-        "coord": {
-            "lon": 139.768752,
-            "lat": 35.960414
-        },
-        "status": "normal", // always "normal". purpose is unknown
-        "list_no": 0, // always 0. purpose is unknown
-        "external_code": "1001D", // external_code === code for all first 500 shops
-        "categories": [ // always array.length === 1
-            {
-                "code": "01", // STRING! 21 brand codes, corresponding to next 'name'
-                "name": "ウエルシア", // alias for code
-                "level": "large", // always 'large'
-                "last_update": "2023-10-12T20:22:57+09:00"
-            }
-        ],
-        "last_update": "2026-08-21T16:34:05+09:00"
-    }
-    """
-
     def post_process_feature(self, item: Feature, source_feature: dict, **kwargs) -> Iterable[Feature]:
-        """
-        NARCIS = 9 stores, luxury  cosmetic business, transferred to MASAYA.
-        ref. NARCIS事業承継完了およびウェブサイト統合に関するお知らせ | COLOR STUDIO・MASAYA | カラースタジオ・マサヤ - https://colorstudio.co.jp/news/
-        Zoomore is a pet shop business brand by Welcia.
-        ハックドラッグ (HAC drug) was a brand by CFS corporation, which was merged by Welcia, but those 12 store keep the old brand name.
-        ref. ウエルシア薬局 - Wikipedia - https://ja.wikipedia.org/wiki/%E3%82%A6%E3%82%A8%E3%83%AB%E3%82%B7%E3%82%A2%E8%96%AC%E5%B1%80#%E6%8C%81%E6%A0%AA%E4%BC%9A%E7%A4%BE%E3%81%AE%E8%A8%AD%E7%AB%8B%E5%BE%8C
-        """
         if phone := source_feature.get("phone"):
             item["phone"] = f"+81 {phone}"
 
