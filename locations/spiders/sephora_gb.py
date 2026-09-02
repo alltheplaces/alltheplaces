22616bf4e72bc6fd2ad7be1dc687fe43871b2b45
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-09-02 15:58:47 +0530

    try with proxy

--- locations/spiders/sephora_gb.py
+++ locations/spiders/sephora_gb.py
@@ -6,7 +6,7 @@ from scrapy.http import Response
 
 from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
-from locations.hours import DAYS_FROM_SUNDAY, OpeningHours
+from locations.hours import DAYS, OpeningHours
 from locations.user_agents import BROWSER_DEFAULT
 
 
@@ -15,6 +15,7 @@ class SephoraGBSpider(Spider):
     item_attributes = {"brand": "Sephora", "brand_wikidata": "Q2408041"}
     start_urls = ["https://www.sephora.co.uk/stores/"]
     custom_settings = {"USER_AGENT": BROWSER_DEFAULT}
+    requires_proxy = True
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
         for store_json in response.xpath("//@data-feelunique-store-info").getall():
@@ -25,17 +26,8 @@ class SephoraGBSpider(Spider):
             item["street_address"] = item.pop("street")
 
             item["opening_hours"] = OpeningHours()
-            for entry in store.get("openHours", []):
-                day_index = entry.get("dow")
-                if day_index is None:
-                    continue
-                day_index -= 1
-                if 0 <= day_index < 7:
-                    day = DAYS_FROM_SUNDAY[day_index]
-                    try:
-                        item["opening_hours"].add_range(day, entry["start"][:5], entry["end"][:5])
-                    except (KeyError, ValueError):
-                        continue
+            for entry in store["openHours"]:
+                item["opening_hours"].add_range(DAYS[entry["dow"] - 1], entry["start"][:5], entry["end"][:5])
 
             apply_category(Categories.SHOP_COSMETICS, item)
             yield item
