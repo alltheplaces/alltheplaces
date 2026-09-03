04df53e610fb681985455d8fee7f27822248386c
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-09-03 17:16:37 +0530

    fix spider

--- locations/spiders/shoneys.py
+++ locations/spiders/shoneys.py
@@ -1,22 +1,65 @@
-from typing import Iterable
+from typing import AsyncIterator, Iterable
 
-from scrapy.http import TextResponse
+from scrapy.http import JsonRequest, TextResponse
 
 from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.structured_data_spider import StructuredDataSpider
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class ShoneysSpider(StructuredDataSpider):
+class ShoneysSpider(JSONBlobSpider):
     name = "shoneys"
     item_attributes = {"brand": "Shoney's", "brand_wikidata": "Q7500392"}
     allowed_domains = ["shoneys.com"]
-    start_urls = ["https://www.shoneys.com/locations"]
-    wanted_types = ["Restaurant"]
+    locations_key = ["data", "restaurant", "locations"]
 
-    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        item["ref"] = item["lat"] = ld_data["latitude"]
-        item["lon"] = ld_data["longitude"]
-        item["image"] = None
+    async def start(self) -> AsyncIterator[JsonRequest]:
+        yield JsonRequest(
+            url="https://www.shoneys.com/graphql",
+            data={
+                "operationName": "restaurantWithLocations",
+                "variables": {"restaurantId": 101178},
+                "query": """query restaurantWithLocations($restaurantId: Int!) {
+                    restaurant(id: $restaurantId) {
+                        locations {
+                            id
+                            streetAddress
+                            city
+                            state
+                            postalCode
+                            country
+                            lat
+                            lng
+                            displayPhone
+                            isLocationClosed
+                            openingRanges {
+                                days
+                                openTime
+                                closeTime
+                            }
+                        }
+                    }
+                }""",
+            },
+            headers={"Origin": "https://www.shoneys.com"},
+        )
+
+    def pre_process_data(self, feature: dict) -> None:
+        feature["phone"] = feature.pop("displayPhone")
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        if feature["isLocationClosed"]:
+            return
+
+        item.pop("name")
+        item["opening_hours"] = OpeningHours()
+        for opening_range in feature["openingRanges"]:
+            # openTime and closeTime are seconds since midnight.
+            open_time = f"{opening_range['openTime'] // 3600:02}:{opening_range['openTime'] % 3600 // 60:02}"
+            close_time = f"{opening_range['closeTime'] // 3600:02}:{opening_range['closeTime'] % 3600 // 60:02}"
+            for day in opening_range["days"]:
+                item["opening_hours"].add_range(day, open_time, close_time)
+
         apply_category(Categories.RESTAURANT, item)
         yield item
