dc8b48a0a2db584064059e6a81984a4b2c061051
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-03 12:12:24 +0200

    [mma_fr] Add Spider (1605 locations)

---
+++ locations/spiders/mma_fr.py
@@ -0,0 +1,23 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class MmaFRSpider(SitemapSpider, StructuredDataSpider):
+    name = "mma_fr"
+    item_attributes = {
+        "brand": "MMA",
+        "brand_wikidata": "Q3331046",
+    }
+    sitemap_urls = ["https://agence.mma.fr/home.sitemap.xml"]
+    sitemap_rules = [
+        (r"", "parse_sd"),
+    ]
+    wanted_types = ["InsuranceAgency"]
+    drop_attributes = ["facebook", "image"]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.OFFICE_INSURANCE, item)
+        item["branch"] = (item.pop("name","") or '').removeprefix("AGENCE D'ASSURANCE MMA ")
+        yield item
