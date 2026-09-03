3796cb26818fcd673421f9da5e173947f3163641
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-09-03 15:27:35 +0530

    code updated to fix spider

--- locations/spiders/northside_hospital_us.py
+++ locations/spiders/northside_hospital_us.py
@@ -1,19 +1,18 @@
+from scrapy import Spider
 from scrapy.http import JsonRequest
 
-from locations.camoufox_spider import CamoufoxSpider
 from locations.categories import Categories, HealthcareSpecialities, apply_category, apply_healthcare_specialities
 from locations.dict_parser import DictParser
 from locations.pipelines.address_clean_up import merge_address_lines
-from locations.settings import DEFAULT_CAMOUFOX_SETTINGS_FOR_CLOUDFLARE_TURNSTILE
 
 
-class NorthsideHospitalUSSpider(CamoufoxSpider):
+class NorthsideHospitalUSSpider(Spider):
     name = "northside_hospital_us"
     item_attributes = {"brand": "Northside Hospital", "brand_wikidata": "Q7059745"}
     start_urls = ["https://locations-api-prod.northside.com/api/LocationsSearch?&Page=1&PageSize=10"]
-    captcha_type = "cloudflare_turnstile"
-    captcha_selector_indicating_success = '//link[@href="resource://content-accessible/plaintext.css"]'
-    custom_settings = DEFAULT_CAMOUFOX_SETTINGS_FOR_CLOUDFLARE_TURNSTILE
+    # captcha_type = "cloudflare_turnstile"
+    # captcha_selector_indicating_success = '//link[@href="resource://content-accessible/plaintext.css"]'
+    # custom_settings = DEFAULT_CAMOUFOX_SETTINGS_FOR_CLOUDFLARE_TURNSTILE
     handle_httpstatus_list = [403]
 
     _category_map = {
