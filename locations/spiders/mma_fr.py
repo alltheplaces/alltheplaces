39dd87e93ef526daec6512557c6801140827d916
Cj Malone <CjMalone@mail.com>
2026-09-03 17:24:28 +0100

    Lint

--- locations/spiders/mma_fr.py
+++ locations/spiders/mma_fr.py
@@ -6,18 +6,12 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class MmaFRSpider(SitemapSpider, StructuredDataSpider):
     name = "mma_fr"
-    item_attributes = {
-        "brand": "MMA",
-        "brand_wikidata": "Q3331046",
-    }
+    item_attributes = {"brand": "MMA", "brand_wikidata": "Q3331046"}
     sitemap_urls = ["https://agence.mma.fr/home.sitemap.xml"]
-    sitemap_rules = [
-        (r"", "parse_sd"),
-    ]
     wanted_types = ["InsuranceAgency"]
     drop_attributes = ["facebook", "image"]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        apply_category(Categories.OFFICE_INSURANCE, item)
         item["branch"] = (item.pop("name", "") or "").removeprefix("AGENCE D'ASSURANCE MMA ")
+        apply_category(Categories.OFFICE_INSURANCE, item)
         yield item
