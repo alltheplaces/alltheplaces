c2087e2cadea98574ea88fea0460de1a0c7564fe
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-03 14:33:25 +0200

    [wash_n_dry_fr] Add Spider (499 locations)

---
+++ locations/spiders/wash_n_dry_fr.py
@@ -0,0 +1,39 @@
+import chompjs
+
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+from locations.hours import DAYS_FR, DELIMITERS_FR, OpeningHours
+
+
+class WashNDryFRSpider(JSONBlobSpider):
+    name = "wash_n_dry_fr"
+    item_attributes = {
+        "brand": "Wash’N Dry",
+        "brand_wikidata": "Q127277495",
+    }
+    start_urls = ["https://www.washndry-concept.com/nos-laveries/"]
+    locations_key = "laveries"
+    drop_attributes = ["state"]
+
+    def extract_json(self, response):
+        data = chompjs.parse_js_object(
+            response.xpath('//script[contains(text(), "var Theme = ")]/text()').get()
+        )
+        return chompjs.parse_js_object(data["laveries"])
+
+    def post_process_item(self, item, response, location):
+        item["ref"] = location["url"]
+        item["branch"] = (item.pop("name","") or "").removeprefix("LAVERIE ").removeprefix("WASH’N DRY ").removeprefix("– ")
+
+        apply_category(Categories.SHOP_LAUNDRY, item)
+
+        hours = location["openings"].lower().replace("7j/7","Lu-Di").replace("7/7","Lu-Di").replace("h00",":00").replace("h30",":00").replace("h45",":45")
+
+        item["opening_hours"] = OpeningHours()
+        item["opening_hours"].add_ranges_from_string(hours, DAYS_FR, delimiters=DELIMITERS_FR)
+        
+        print("===")
+        print(hours)
+        print(item["opening_hours"].as_opening_hours())
+
+        yield item
