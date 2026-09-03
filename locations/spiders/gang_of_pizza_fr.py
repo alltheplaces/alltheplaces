7672dfa2654264c8804c3008013ac24e15736f1f
Cj Malone <CjMalone@mail.com>
2026-09-03 11:45:13 +0100

    Lint

--- locations/spiders/gang_of_pizza_fr.py
+++ locations/spiders/gang_of_pizza_fr.py
@@ -6,21 +6,13 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class GangOfPizzaFRSpider(SitemapSpider, StructuredDataSpider):
     name = "gang_of_pizza_fr"
-    item_attributes = {
-        "brand": "Gang of Pizza",
-        "brand_wikidata": "Q89123626",
-    }
+    item_attributes = {"brand": "Gang of Pizza", "brand_wikidata": "Q89123626"}
     sitemap_urls = ["https://distributeurs.gangofpizza.com/sitemap.xml"]
-    sitemap_rules = [
-        (r"/gang-of-pizza-", "parse_sd"),
-    ]
+    sitemap_rules = [("/gang-of-pizza-", "parse_sd")]
     wanted_types = ["FoodEstablishment"]
-    drop_attributes = [
-        "facebook",
-        "image",
-    ]
+    drop_attributes = {"facebook", "image"}
 
     def post_process_item(self, item, response, ld_data, **kwargs):
-        apply_category(Categories.VENDING_MACHINE, item)
         item["branch"] = (item.pop("name", "") or "").removeprefix("Gang Of Pizza ")
+        apply_category(Categories.VENDING_MACHINE, item)
         yield item
