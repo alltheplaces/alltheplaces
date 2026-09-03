ece55a1ef0c1b201ad5c3262d879d073e7ad9978
Cj Malone <CjMalone@mail.com>
2026-09-03 17:10:25 +0100

    Support locationsitemap2

--- locations/spiders/elsie_sante_fr.py
+++ locations/spiders/elsie_sante_fr.py
@@ -11,8 +11,9 @@ from locations.structured_data_spider import StructuredDataSpider
 class ElsieSanteFRSpider(SitemapSpider, StructuredDataSpider):
     name = "elsie_sante_fr"
     item_attributes = {"brand": "Elsie Santé", "brand_wikidata": "Q127603214"}
-    sitemap_urls = ["https://pharmacie.elsie-sante.fr/locationsitemap1.xml"]
-    sitemap_rules = [(r"https://pharmacie.elsie-sante.fr/+", "parse_sd")]
+    sitemap_urls = ["https://pharmacie.elsie-sante.fr/robots.txt"]
+    sitemap_follow = ["locationsitemap"]
+    sitemap_rules = [(r"^https://pharmacie.elsie-sante.fr/(\d+)-.+$", "parse_sd")]
     wanted_types = ["LocalBusiness"]
     drop_attributes = {"image", "facebook"}
 
