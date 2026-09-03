0ddd648970183d5cce8c58686bb8dd7cadc9b793
Cj Malone <CjMalone@mail.com>
2026-09-03 17:25:13 +0100

    Parse opening hours

--- locations/spiders/mma_fr.py
+++ locations/spiders/mma_fr.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -13,5 +14,17 @@ class MmaFRSpider(SitemapSpider, StructuredDataSpider):
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         item["branch"] = (item.pop("name", "") or "").removeprefix("AGENCE D'ASSURANCE MMA ")
+        item["opening_hours"] = self.parse_opening_hours(ld_data)
         apply_category(Categories.OFFICE_INSURANCE, item)
         yield item
+
+    def parse_opening_hours(self, ld_data: dict) -> OpeningHours:
+        oh = OpeningHours()
+        for l in ld_data["openingHours"]:
+            day, times = l.split(" ", 1)
+            if times == "Fermée":
+                oh.set_closed(day)
+            else:
+                for t in times.strip().split(" "):
+                    oh.add_range(day, *t.split("-"))
+        return oh
