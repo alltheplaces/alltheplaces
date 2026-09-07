6d9ff8cabcf472db64dce7e20da338de32a4a75f
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-07 15:05:10 +0200

    [brioche_doree_fr] Add Spider (231 locations)

---
+++ locations/spiders/brioche_doree_fr.py
@@ -0,0 +1,38 @@
+import re
+
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+from locations.hours import OpeningHours, DAYS
+
+
+
+class BriocheDoreeFrSpider(JSONBlobSpider):
+    name = "brioche_doree_fr"
+    item_attributes = {
+        "brand": "Brioche Dorée",
+        "brand_wikidata": "Q2925606",
+    }
+    start_urls = ["https://www.briochedoree.fr/api/stores"]
+
+    locations_key = "stores"
+
+    def post_process_item(self, item, response, location):
+        # apply_category(Categories.SHOP_XYZ, item)
+        print(location)
+        item["branch"] = item.pop("name", "")
+
+        match = re.search(r"\b\d{4,5}\b", location.get("address"))
+        item["postcode"] = match.group().zfill(5) if match else None        
+
+
+        item["opening_hours"] = OpeningHours()
+        for day in location.get("opening_hours"):
+            day_of_week = DAYS[day.get("day")-1]
+            if day["is_closed"]:
+                item["opening_hours"].set_closed(day_of_week)
+            elif day.get("open") == day.get("close"):
+                item["opening_hours"].add_range(day_of_week, day.get("open"), "24:00")
+            else : 
+                item["opening_hours"].add_range(day_of_week, day.get("open"), day.get("close"))
+
+        yield item
