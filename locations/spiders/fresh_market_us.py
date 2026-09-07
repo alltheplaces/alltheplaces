6c07ab4984011ff4e0c28e18fae60956aba971e0
ehsan-firdaus <ehsanfirdaus@gmail.com>
2026-09-07 19:13:53 +0530

    [Fresh Market US] Increase page size to prevent record truncation

--- locations/spiders/fresh_market_us.py
+++ locations/spiders/fresh_market_us.py
@@ -1,16 +1,22 @@
-import scrapy
+from typing import Iterable
 
-from locations.dict_parser import DictParser
+from scrapy.http import TextResponse
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
 
 
-class FreshMarketUSSpider(scrapy.Spider):
+class FreshMarketUSSpider(JSONBlobSpider):
     name = "fresh_market_us"
     item_attributes = {"brand": "The Fresh Market", "brand_wikidata": "Q7735265"}
-    start_urls = ["https://api.thefreshmarket.com/v1/stores?page[number]=1&page[size]=200&Filter[radius]=100000"]
+    start_urls = ["https://api.thefreshmarket.com/v1/stores?page[number]=1&page[size]=1000&Filter[radius]=100000"]
+    locations_key = "data"
 
-    def parse(self, response):
-        for store in response.json()["data"]:
-            store.update(store.pop("attributes"))
-            item = DictParser.parse(store)
-            item["street_address"] = item.pop("addr_full")
-            yield item
+    def pre_process_data(self, feature: dict) -> None:
+        feature.update(feature.pop("attributes", {}))
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["street_address"] = item.pop("addr_full", None)
+        apply_category(Categories.SHOP_SUPERMARKET, item)
+        yield item
