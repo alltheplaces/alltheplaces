5ae3b1a6ad061d3b501c728daa63c0c50c6935f7
Ayush Singh <singhayush1803@gmail.com>
2026-09-07 23:58:14 +0530

    Harden Buffalo Grill spider

--- locations/spiders/buffalo_grill_fr.py
+++ locations/spiders/buffalo_grill_fr.py
@@ -1,5 +1,5 @@
 import re
-from typing import Iterable
+from typing import ClassVar, Iterable
 
 from scrapy import FormRequest
 from scrapy.http import TextResponse
@@ -13,31 +13,41 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class BuffaloGrillFRSpider(SitemapSpider, StructuredDataSpider):
     name = "buffalo_grill_fr"
-    item_attributes = {"brand": "Buffalo Grill", "brand_wikidata": "Q944655"}
-    sitemap_urls = ["https://www.buffalo-grill.fr/sitemap.xml"]
-    sitemap_rules = [(r"/nos-restaurants/([^/]+)/?$", "parse")]
-    wanted_types = ["Restaurant"]
+    item_attributes: ClassVar[dict[str, str]] = {"brand": "Buffalo Grill", "brand_wikidata": "Q944655"}
+    sitemap_urls: ClassVar[list[str]] = ["https://www.buffalo-grill.fr/sitemap.xml"]
+    sitemap_rules: ClassVar[list[tuple[str, str]]] = [(r"/nos-restaurants/([^/]+)/?$", "parse")]
+    wanted_types: ClassVar[list[str]] = ["Restaurant"]
 
     def parse(self, response: TextResponse, **kwargs):
-        if restaurant_id := response.css(".restaurant_full::attr(data-restaurant)").get():
-            yield FormRequest(
-                "https://www.buffalo-grill.fr/ajax/restaurant/schedules",
-                formdata={"restaurantId": restaurant_id, "type": "schedules_full"},
-                callback=self.parse_schedules,
-                cb_kwargs={"location_response": response},
-            )
+        if not (restaurant_id := response.css(".restaurant_full::attr(data-restaurant)").get()):
+            yield from self.parse_sd(response)
+            return
+
+        yield FormRequest(
+            "https://www.buffalo-grill.fr/ajax/restaurant/schedules",
+            formdata={"restaurantId": restaurant_id, "type": "schedules_full"},
+            callback=self.parse_schedules,
+            errback=self.parse_schedules_error,
+            cb_kwargs={"location_response": response},
+        )
 
     def parse_schedules(self, response: TextResponse, location_response: TextResponse):
         body = location_response.body.replace(b"</body>", response.body + b"</body>")
         yield from self.parse_sd(location_response.replace(body=body))
 
+    def parse_schedules_error(self, failure):
+        yield from self.parse_sd(failure.request.cb_kwargs["location_response"])
+
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        if item["country"] not in {"FR", "FRA"}:
+        if item["country"] == "FRA":
+            item["country"] = "FR"
+        elif item["country"] != "FR":
             return
 
         item["branch"] = item.pop("name")
         item["opening_hours"] = self.parse_opening_hours(response)
-        item["extras"]["@source_uri"] = item["website"]
+        item["website"] = response.url
+        item["extras"]["@source_uri"] = response.url
 
         apply_category(Categories.RESTAURANT, item)
         yield item
