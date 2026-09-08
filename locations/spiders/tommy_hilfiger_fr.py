1707845d17859737c9476556be6e40c9a2d3e9f4
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-08 15:38:07 +0200

    bugfix

--- locations/spiders/tommy_hilfiger_fr.py
+++ locations/spiders/tommy_hilfiger_fr.py
@@ -3,14 +3,16 @@ from typing import Iterable
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.yext_answers import YextAnswersSpider
+from locations.user_agents import FIREFOX_LATEST
 
 
 class TommyHilfigerFRSpider(YextAnswersSpider):
     name = "tommy_hilfiger_fr"
     item_attributes = {"brand": "Tommy Hilfiger", "brand_wikidata": "Q634881"}
-    api_key = "22e922a5467c105dbc672e892b1c6a6564"
-    experience_key = "th-locator-fr"
+    api_key = "e922a5467c105dbc672e892b1c6a6564"
+    experience_key = "tommy-hilfiger-locator-fr"
     feature_type = "fr-locations"
+    locale = "fr-FR"
 
     def parse_item(self, location: dict, item: Feature) -> Iterable[Feature]:
         item["website"] = "https://fr.tommy.com/store/" + location["slug"]
