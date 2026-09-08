fdd5fe12985310cab2a030872442db5278c283eb
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-08 18:23:50 +0200

    [camara_fr] Add Spider (76 locations)

---
+++ locations/spiders/camara_fr.py
@@ -0,0 +1,40 @@
+import chompjs
+
+from locations.categories import Categories, apply_category
+from locations.json_blob_spider import JSONBlobSpider
+from scrapy.http import FormRequest
+from locations.hours import DAYS_FR, OpeningHours, CLOSED_FR
+
+
+class CamaraFrSpider(JSONBlobSpider):
+    name = "camara_fr"
+    item_attributes = {
+        "brand": "Camara",
+        "brand_wikidata": "Q2930917",
+    }
+
+    locations_key = ['data','stores']
+
+    async def start(self):
+        yield FormRequest(
+            url="https://www.camara.net/module/antstore/storeLocator",
+            method="POST",
+            formdata={
+                "action": "getStores",
+                "id_lang": "1",
+                "modal": "0",
+                "full": "1",
+            },
+            callback=self.parse,
+        )
+
+
+    def post_process_item(self, item, response, location):
+        apply_category(Categories.SHOP_PHOTO, item)
+        item["branch"] = item.pop("name", "").removeprefix("CAMARA ").lower()
+
+        item["opening_hours"] = OpeningHours()
+        for d in location.get("business_hours",[]):
+            item["opening_hours"].add_ranges_from_string(d.get("day","") + " " + " ".join(d.get("hours", "")).replace("h",":"), DAYS_FR, closed=CLOSED_FR)
+        
+        yield item
