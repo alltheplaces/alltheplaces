afd48efd85c5f8c7c15a6930cde551fed4e2571d
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-08 13:09:42 +0200

    [quick_be_lu] Convert Spider to JSONBlobSpider

--- locations/spiders/quick_be_lu.py
+++ locations/spiders/quick_be_lu.py
@@ -1,50 +1,40 @@
-from typing import AsyncIterator
+import chompjs
 
-from scrapy import Spider
-from scrapy.http import JsonRequest, Request
-
-from locations.dict_parser import DictParser
+from locations.json_blob_spider import JSONBlobSpider
+from locations.categories import Categories, apply_category
 from locations.hours import DAYS, OpeningHours
 
 
-class QuickBELUSpider(Spider):
+class QuickBELUSpider(JSONBlobSpider):
     name = "quick_be_lu"
     item_attributes = {"brand": "Quick", "brand_wikidata": "Q286494"}
     allowed_domains = ["www.quick.be"]
     start_urls = ["https://www.quick.be/fr/restaurants"]
 
-    async def start(self) -> AsyncIterator[Request]:
-        for url in self.start_urls:
-            yield Request(url=url, callback=self.find_json_file)
+    def extract_json(self, response):
+        return chompjs.parse_js_object(
+            response.xpath('//script[@id="__NEXT_DATA__"]//text()').get()
+        )["props"]["pageProps"]["restaurants"]
 
-    def find_json_file(self, response):
-        build_id = (
-            response.xpath('//script[contains(@src, "/_buildManifest.js")]/@src')
-            .get()
-            .replace("/_next/static/", "")
-            .replace("/_buildManifest.js", "")
-        )
-        yield JsonRequest(f"https://www.quick.be/_next/data/{build_id}/fr/restaurants.json")
+    def post_process_item(self, item, response, location):
+        apply_category(Categories.FAST_FOOD, item)
+        item["branch"] = item.pop("name","")
+        item["lat"] = location["latlng"]["lat"]
+        item["lon"] = location["latlng"]["lng"]
+        item["street_address"] = item.pop("addr_full")
+        item["country"] = "BE"
+        if location["country_luxembourg"]:
+            item["country"] = "LU"
+        item["website"] = "https://www.quick.be/fr/restaurant/" + location["slug"]
+        item["opening_hours"] = OpeningHours()
+        for day in location["opening_hours"]:
+            if day["opening_type"] != 1:
+                continue
+            item["opening_hours"].add_range(
+                DAYS[day["weekday_from"] - 1], day["from_hour"], day["to_hour"], "%H:%M:%S"
+            )
 
-    def parse(self, response):
-        for location in response.json()["pageProps"]["restaurants"]:
-            item = DictParser.parse(location)
-            item["lat"] = location["latlng"]["lat"]
-            item["lon"] = location["latlng"]["lng"]
-            item["street_address"] = item.pop("addr_full")
-            item["country"] = "BE"
-            if location["country_luxembourg"]:
-                item["country"] = "LU"
-            item["website"] = "https://www.quick.be/fr/restaurant/" + location["slug"]
-            item["opening_hours"] = OpeningHours()
-            for day in location["opening_hours"]:
-                if day["opening_type"] != 1:
-                    continue
-                item["opening_hours"].add_range(
-                    DAYS[day["weekday_from"] - 1], day["from_hour"], day["to_hour"], "%H:%M:%S"
-                )
+        if postcode := item.get("postcode"):
+            item["postcode"] = str(postcode)
 
-            if postcode := item.get("postcode"):
-                item["postcode"] = str(postcode)
-
-            yield item
+        yield item
