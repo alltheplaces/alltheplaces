4249b5707e13e9109ead94d842512b1542a79079
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-09 17:41:50 +0200

    fix geometry

--- locations/spiders/autodistribution_fr.py
+++ locations/spiders/autodistribution_fr.py
@@ -40,4 +40,10 @@ class AutodistributionFRSpider(SitemapSpider):
             data["schedule"].replace("et", " ").split("Atelier")[0], DAYS_FR, delimiters=DELIMITERS_FR, closed=CLOSED_FR
         )
 
+        item["lat"] = item.pop("lat","").replace(",",".")
+        lon = item.pop("lon","").replace(",",".")
+        if lon.startswith("."):
+            lon = "0" + lon
+        item["lon"] = lon
+
         yield item
