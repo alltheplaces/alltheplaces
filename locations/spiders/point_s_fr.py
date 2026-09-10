5b45fc6393f6f43a5733f175c79b1704184fec8e
Cj Malone <CjMalone@mail.com>
2026-09-10 12:03:23 +0100

    lint

--- locations/spiders/point_s_fr.py
+++ locations/spiders/point_s_fr.py
@@ -1,37 +1,30 @@
 import json
+from typing import Iterable
 
-from scrapy.http import FormRequest
+from scrapy.http import FormRequest, TextResponse
 
 from locations.categories import Categories, apply_category
 from locations.hours import CLOSED_FR, DAYS_FR, DELIMITERS_FR, OpeningHours
+from locations.items import Feature
 from locations.json_blob_spider import JSONBlobSpider
 
 
 class PointSFRSpider(JSONBlobSpider):
     name = "point_s_fr"
-    item_attributes = {
-        "brand": "Point S",
-        "brand_wikidata": "Q3393358",
-    }
+    item_attributes = {"brand": "Point S", "brand_wikidata": "Q3393358"}
     locations_key = ["data", "centers"]
 
     async def start(self):
         yield FormRequest(
-            url="https://www.points.fr/wp-admin/admin-ajax.php",
-            method="POST",
-            formdata={
-                "action": "center_list",
-                "filters": "",
-            },
-            callback=self.parse,
+            url="https://www.points.fr/wp-admin/admin-ajax.php", formdata={"action": "center_list", "filters": ""}
         )
 
-    def post_process_item(self, item, response, location):
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
         apply_category(Categories.SHOP_CAR_REPAIR, item)
         item["country"] = "FR"
         item["branch"] = item.pop("name", "")
 
-        schedule = location.pop("schedules")
+        schedule = feature.pop("schedules")
         if schedule:
             try:
                 item["opening_hours"] = OpeningHours()
