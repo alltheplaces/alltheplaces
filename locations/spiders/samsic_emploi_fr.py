4247f4420a0b74247a4e620487a31c1396a32662
Cj Malone <CjMalone@mail.com>
2026-09-10 11:25:30 +0100

    lint

--- locations/spiders/samsic_emploi_fr.py
+++ locations/spiders/samsic_emploi_fr.py
@@ -1,9 +1,12 @@
 import json
+from typing import Iterable
 
+from scrapy.http import Response, TextResponse
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from locations.categories import Categories, apply_category
+from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
@@ -12,18 +15,17 @@ class SamsicEmploiFRSpider(CrawlSpider, StructuredDataSpider):
     item_attributes = {"brand": "Samsic Emploi", "brand_wikidata": "Q136431282"}
     allowed_domains = ["www.samsic-emploi.fr"]
     start_urls = ["https://www.samsic-emploi.fr/nos-agences/tous-nos-etablissements"]
-    rules = [
-        Rule(LinkExtractor(allow=r"samsic-emploi.fr/nos-agences/[^/]+/[^/]+/[^/]+$"), follow=True, callback="parse_sd")
-    ]
+    rules = [Rule(LinkExtractor(allow=r"samsic-emploi.fr/nos-agences/[^/]+/[^/]+/[^/]+$"), callback="parse_sd")]
+    wanted_types = ["EmploymentAgency"]
 
-    def iter_linked_data(self, response):
+    def iter_linked_data(self, response: Response) -> Iterable[dict]:
         raw = response.xpath(
             '//script[@type="application/ld+json" and contains(text(), "EmploymentAgency")]/text()'
         ).get()
         if raw:
             yield from json.loads(raw)["@graph"][0]["itemListElement"]
 
-    def post_process_item(self, item, response, ld_data):
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
         apply_category(Categories.OFFICE_EMPLOYMENT_AGENCY, item)
         item.pop("facebook", "")
         item.pop("image", "")
