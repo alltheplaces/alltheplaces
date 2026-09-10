7df1c8e799cd1ed54d1e1a43b0c7a1dafa566625
Cj Malone <CjMalone@mail.com>
2026-09-10 17:05:50 +0100

    Flip ifs

--- locations/spiders/carglass_fr.py
+++ locations/spiders/carglass_fr.py
@@ -6,22 +6,19 @@ from locations.structured_data_spider import StructuredDataSpider
 
 class CarglassFRSpider(SitemapSpider, StructuredDataSpider):
     name = "carglass_fr"
-    item_attributes = {
-        "brand": "Carglass",
-        "brand_wikidata": "Q1035997",
-    }
+    item_attributes = {"brand": "Carglass", "brand_wikidata": "Q1035997"}
     sitemap_urls = ["https://www.carglass.fr/centre/sitemap.xml"]
-    sitemap_rules = [(r"", "parse_sd")]
     wanted_types = ["AutoRepair"]
     drop_attributes = ["facebook", "phone", "twitter"]
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         # drop duplicate item containing no information
-        if ld_data.get("geo") is not None:
-            street_address = item.get("street_address")
+        if not ld_data.get("geo"):
+            return
+        # drop locations that inside norauto car repair shops, the brand of these shops is norauto and already collected by norauto_fr spider
+        if item.get("street_address") and item["street_address"].lower() == "norauto":
+            return
 
-            # drop locations that inside norauto car repair shops, the brand of these shops is norauto and already collected by norauto_fr spider
-            if street_address and street_address.lower() != "norauto":
-                apply_category(Categories.SHOP_CAR_REPAIR, item)
-                item["branch"] = (item.pop("name", "") or "").removeprefix("Carglass® ")
-                yield item
+        apply_category(Categories.SHOP_CAR_REPAIR, item)
+        item["branch"] = (item.pop("name", "") or "").removeprefix("Carglass® ")
+        yield item
