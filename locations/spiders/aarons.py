d5be60c9cc4efe9ed5269667a9126f418fca997d
IceBurger <79398013+sharmaanurag95@users.noreply.github.com>
2026-09-11 16:49:39 +0530

    [Aarons] Fix spider (#18644)

--- locations/spiders/aarons.py
+++ locations/spiders/aarons.py
@@ -1,10 +1,12 @@
-from typing import Any, AsyncIterator
+from typing import Any, AsyncIterator, Iterable
 
 from scrapy.http import JsonRequest, Response
 from scrapy.spiders import Spider
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
 from locations.hours import OpeningHours
+from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
 
 
@@ -12,20 +14,20 @@ class AaronsSpider(Spider):
     name = "aarons"
     item_attributes = {"brand": "Aaron's", "brand_wikidata": "Q10397787"}
 
-    def make_request(self, offset: int, limit: int = 10) -> JsonRequest:
+    def make_request(self, offset: int, limit: int = 50) -> JsonRequest:
         return JsonRequest(
-            url=f"https://www.aarons.com/locations/search?limit={limit}&offset={offset}",
+            url=f"https://www.aarons.com/locations/search-api?r=250000&per={limit}&offset={offset}",
             cb_kwargs=dict(offset=offset, limit=limit),
         )
 
     async def start(self) -> AsyncIterator[JsonRequest]:
         yield self.make_request(0)
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
+    def parse(self, response: Response, **kwargs: Any) -> Iterable[Feature | JsonRequest]:
         for location in response.json()["response"]["entities"]:
             location.update(location.pop("profile"))
             item = DictParser.parse(location)
-            item["ref"] = location.get("c_storeCode")
+            item["ref"] = location.get("c_storeCode") or location["meta"]["id"]
             address = location["address"]
             item["street_address"] = merge_address_lines(
                 [address.get("line1"), address.get("line2"), address.get("line3")]
@@ -46,6 +48,7 @@ class AaronsSpider(Spider):
                         item["opening_hours"].add_range(
                             rule["day"], str(shift["start"]).zfill(4), str(shift["end"]).zfill(4), "%H%M"
                         )
+            apply_category(Categories.SHOP_FURNITURE, item)
             yield item
 
         new_offset = kwargs["offset"] + kwargs["limit"]
