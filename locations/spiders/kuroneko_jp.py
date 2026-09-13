5ec4f1b8d3cfaafa151087be622fdd7c1feba820
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-09-13 22:44:55 +0900

    update, only request the ones we want. next step is try increasing radius
    {'atp/brand/ヤマト運輸': 4477,
     'atp/brand_wikidata/Q6584353': 4477,
     'atp/category/amenity/post_office': 4477,
     'atp/category/amenity/yes': 701,
     'atp/country/JP': 5178,
     'atp/duplicate_count': 66913,
     'atp/field/branch/missing': 701,
     'atp/field/brand/missing': 701,
     'atp/field/brand_wikidata/missing': 701,
     'atp/field/city/missing': 5178,
     'atp/field/country/from_spider_name': 5178,
     'atp/field/email/missing': 5178,
     'atp/field/housenumber/missing': 5178,
     'atp/field/opening_hours/missing': 4885,
     'atp/field/operator/missing': 5178,
     'atp/field/operator_wikidata/missing': 5178,
     'atp/field/phone/invalid': 2,
     'atp/field/postcode/missing': 5178,
     'atp/field/state/missing': 5178,
     'atp/field/street/missing': 5178,
     'atp/field/street_address/missing': 5178,
     'atp/field/twitter/missing': 5178,
     'atp/field/unit/missing': 5178,
     'atp/item_scraped_host_count/www.e-map.ne.jp': 72091,
     'atp/lineage': 'S_?',
     'atp/nsi/brand_or_operator_missing': 701,
     'atp/nsi/category_unknown': 4477,
     'atp/nsi/match_failed': 5178,
     'downloader/request_bytes': 10183684,
     'downloader/request_count': 6379,
     'downloader/request_method_count/GET': 6379,
     'downloader/response_bytes': 28091925,
     'downloader/response_count': 6379,
     'downloader/response_status_count/200': 6379,
     'dupefilter/filtered': 2345,
     'elapsed_time_seconds': 7820.343999999999,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 9, 13, 13, 1, 13, 938072, tzinfo=datetime.timezone.utc),
     'item_dropped_count': 66913,
     'item_dropped_reasons_count/DropItem': 66913,
     'item_scraped_count': 5178,
     'items_per_minute': 39.728900255754475,
     'log_count/DEBUG': 78471,
     'log_count/INFO': 133,
     'request_depth_max': 4,
     'response_received_count': 6379,
     'responses_per_minute': 48.943734015345264,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/200': 1,
     'scheduler/dequeued': 6378,
     'scheduler/dequeued/memory': 6378,
     'scheduler/enqueued': 6378,
     'scheduler/enqueued/memory': 6378,
     'start_time': datetime.datetime(2026, 9, 13, 10, 50, 53, 604456, tzinfo=datetime.timezone.utc)}

--- locations/spiders/kuroneko_jp.py
+++ locations/spiders/kuroneko_jp.py
@@ -10,15 +10,6 @@ from locations.categories import Categories, apply_category
 from locations.geo import city_locations, country_iseadgg_centroids
 from locations.items import Feature
 
-BRANDS = {
-    "001": ("ファミリーマート", "Q11247682"),
-    "002": ("セブン-イレブン", "Q259340"),
-    "101": ("ポプラ", "Q7229380"),
-    "171": ("ポプラ", "Q7229380"),
-    "418": ("NewDays", "Q11234763"),
-    "436": ("デイリーヤマザキ", "Q5209392"),
-    "YTC": ("ヤマト運輸", "Q6584353"),
-}
 MAX_ITEMS = 1640  # determined experimentally
 TOKYO_TO_WGS84 = Transformer.from_pipeline("EPSG:15484")
 RADIUS_KM = 24
@@ -30,7 +21,7 @@ class KuronekoJPSpider(Spider):
 
     def make_request(self, lat, lon, distance, offset=1, count=900):
         return Request(
-            f"https://www.e-map.ne.jp/p/{MAP_ID}/zdcemaphttp.cgi?target=http%3A%2F%2F127.0.0.1%2Fcgi%2Fnkyoten.cgi%3F%26cid%3D{MAP_ID}%26pos%3D{offset}%26lat%3D{lat}%26lon%3D{lon}%26knsu%3D{MAX_ITEMS}%26cnt%3D{count}%26hour%3D1%26rad%3D{distance}&zdccnt=1",
+            f"https://www.e-map.ne.jp/p/{MAP_ID}/zdcemaphttp.cgi?target=http%3A%2F%2F127.0.0.1%2Fcgi%2Fnkyoten.cgi%3F%26cid%3D{MAP_ID}%26pos%3D{offset}%26lat%3D{lat}%26lon%3D{lon}%26knsu%3D{MAX_ITEMS}%26cnt%3D{count}%26hour%3D1%26rad%3D{distance}%26jkn%3D(COL_01%3A1%20OR%20COL_01%3A2%20AND%20((COL_39!%3A002%20AND%20COL_39!%3A001%20AND%20COL_39!%3A003%20AND%20COL_39!%3A418%20AND%20COL_39!%3A436%20AND%20COL_39!%3A101%20AND%20COL_39!%3A171%20AND%20COL_39!%3A207%20AND%20COL_39!%3AZ98%20AND%20COL_39!%3AZ99%20AND%20COL_39!%3A563)%20OR%20(COL_39%20IS%20NULL)))%20AND%20(((COL_01%3A1%20AND%20(COL_10%3AA%20OR%20COL_10%3AB))%20OR%20COL_01%3A2)%20AND%20((COL_39!%3AZ96%20AND%20COL_39!%3AZ97%20AND%20COL_39!%3A003%20AND%20COL_39!%3A207%20AND%20COL_39!%3AZ98%20AND%20COL_39!%3AZ99)%20OR%20COL_39%3A%40%40NULL%40%40))&zdccnt=1",
             cb_kwargs={"lat": lat, "lon": lon, "offset": offset},
         )
 
@@ -61,7 +52,7 @@ class KuronekoJPSpider(Spider):
         if rec_count >= hit_count:
             yield self.make_request(lat, lon, offset + rec_count)
         for row in reader:
-            if row[3] in ("001", "002"):  # skip FamilyMart, 7-11
+            if any(i in row[6] for i in ["ツルハ", "福太郎", "イレブン", "ウォンツ", "B＆D", "Ｂ＆Ｄ"]):  # skip Tsuruha Drug locations
                 continue
             item = Feature()
             item["ref"] = row[0]
@@ -74,24 +65,13 @@ class KuronekoJPSpider(Spider):
             if row[3] == "YTC":
                 apply_category(Categories.POST_OFFICE, item)
                 item["branch"] = row[6]
-                item["name"] = "ヤマト運輸"
-            elif row[3] == "563":
-                apply_category(Categories.PARCEL_LOCKER, item)
-                item["name"] = row[6].replace("宅配便ロッカー", "").replace("宅配便ロッカー　", "")
+                item["name"] = item["brand"] = "ヤマト運輸"
+                item["brand_wikidata"] = "Q6584353"
             else:
-                if row[3] != "SHP":
-                    try:
-                        item["branch"] = re.search(r"(?:　+)\S+$", str(row[6])).group().replace("　", "")
-                        item["name"] = None
-                    except:
-                        item["branch"] = row[6]
-                else:
-                    item["name"] = row[6]
+                item["name"] = row[6]
                 apply_category(Categories.GENERIC_POI, item)
                 item.set_tag("post_office", "post_partner")
-                item["extras"]["post_office:service_provider"] = "ヤマト運輸"
-
-            item["brand"], item["brand_wikidata"] = BRANDS.get(row[3], (None, None))
+                item.set_tag("post_office:service_provider", "ヤマト運輸")
 
             item["addr_full"] = row[7]
             item["phone"] = row[16]
