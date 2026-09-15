7a4d554b4a8aa100f5120f95f070e41508ea53a8
Cj Malone <CjMalone@mail.com>
2026-09-15 15:14:55 +0100

    [Delko FR] Update spider for new website

--- locations/spiders/delko_fr.py
+++ locations/spiders/delko_fr.py
@@ -1,26 +1,28 @@
 from typing import Iterable
 
 from scrapy.http import TextResponse
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import SitemapSpider
 
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.structured_data_spider import StructuredDataSpider
 
 
-class DelkoFRSpider(CrawlSpider, StructuredDataSpider):
+class DelkoFRSpider(SitemapSpider, StructuredDataSpider):
     name = "delko_fr"
     contacts = ["team.marketing@delko.com"]
-    item_attributes = {"brand": "Delko", "brand_wikidata": "Q132021672"}
-    start_urls = ["https://delko.fr/garage/"]
-    rules = [Rule(LinkExtractor(r"/garage/delko/[^/]+/(\d+)$"), "parse")]
+    item_attributes = {"brand": "Delko", "brand_wikidata": "Q24934757"}
+    sitemap_urls = ["https://www.delko.com/robots.txt"]
+    sitemap_rules = [(r"com/garages/[^/]+$", "parse")]
     wanted_types = ["AutoRepair"]
+    search_for_facebook = False
 
     def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
-        item["branch"] = item.pop("name").removeprefix("Delko ")
+        item["branch"] = item.pop("name").removeprefix("DELKO ")
 
-        item["email"] = response.xpath('//b[contains(text(), "@delko.fr")]/text()').get()
+        item["lat"] = ld_data["latitude"]
+        item["lon"] = ld_data["longitude"]
+        item["extras"]["start_date"] = ld_data["foundingDate"]
 
         apply_category(Categories.SHOP_CAR_REPAIR, item)
 
