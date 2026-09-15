30fc127c1d3d38cd8d651be5e9c1ac11235a2cca
Valeri Barbakadze <v.barbakadze86@gmail.com>
2026-09-15 18:41:40 +0400

    Add just_salad_us spider (129 locations)

---
+++ locations/spiders/just_salad_us.py
@@ -0,0 +1,58 @@
+from typing import Any, Iterable
+
+from scrapy import Spider
+from scrapy.http import JsonRequest, Response
+
+from locations.categories import Categories, apply_category
+from locations.dict_parser import DictParser
+from locations.hours import DAYS, OpeningHours
+from locations.items import Feature
+
+# Just Salad publishes every location in a single JSON file on its CDN. The
+# feed carries a states lookup table which the address blocks reference by
+# numeric id, and rows for stores that have not opened yet, flagged by
+# "coming_soon".
+
+
+class JustSaladUSSpider(Spider):
+    name = "just_salad_us"
+    item_attributes = {"brand": "Just Salad", "brand_wikidata": "Q23091823"}
+    allowed_domains = ["cdn1.justsalad.com"]
+    start_urls = ["https://cdn1.justsalad.com/public/store_locations.json"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Iterable[Feature]:
+        data = response.json()
+        states = {state["id"]: state["abbreviation"] for state in data["states"]}
+
+        for location in data["storelocations"]:
+            if location.get("coming_soon") or location.get("business_status") != "OPERATIONAL":
+                continue
+
+            item = DictParser.parse(location["address"])
+            item["ref"] = location["store_num"]
+            item["branch"] = location["name"]
+            item["phone"] = location.get("phone")
+            item["state"] = states.get(location["address"].get("state"))
+            item["street_address"] = " ".join(
+                filter(None, [location["address"].get("address_1"), location["address"].get("address_2")])
+            )
+
+            item["opening_hours"] = OpeningHours()
+            for period in location.get("opening_hours", {}).get("periods", []):
+                item["opening_hours"].add_range(
+                    DAYS[period["open"]["day"]],
+                    period["open"]["time"],
+                    period["close"]["time"],
+                    time_format="%H%M",
+                )
+
+            amenities = location.get("amenities") or {}
+            if amenities.get("drive_thru"):
+                item["extras"]["drive_through"] = "yes"
+            if amenities.get("outdoor_seat"):
+                item["extras"]["outdoor_seating"] = "yes"
+
+            apply_category(Categories.FAST_FOOD, item)
+            item["extras"]["cuisine"] = "salad"
+
+            yield item
