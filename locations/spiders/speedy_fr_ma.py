de8517f3f024eee6d18d7707c590a6e75d7b2e5a
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-16 17:28:37 +0200

    [speedy_fr_ma] Add Spider (485 locations)

---
+++ locations/spiders/speedy_fr_ma.py
@@ -0,0 +1,29 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class SpeedyFRMASpider(SitemapSpider, StructuredDataSpider):
+    name = "speedy_fr_ma"
+    item_attributes = {
+        "brand": "Speedy",
+        "brand_wikidata": "Q3492969",
+    }
+    sitemap_urls = ["https://centres-auto.speedy.fr/sitemap/18/sitemap-standard-0.xml"]
+    sitemap_rules = [
+        (r"\d+/\d+$", "parse"),
+    ]
+    custom_settings = {"DOWNLOAD_DELAY": 4}
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if(item.get("lat") is not None):
+            if "+212" in (item.get("phone") or "") or (item.get("city") or "") in ["El Jadida", "Mohammedia"]:
+                item["country"] = "MA"
+
+            apply_category(Categories.SHOP_CAR_REPAIR, item)
+            item["branch"] = item.pop("name", "").removeprefix("Centre Auto SPEEDY ")
+            for i in ["facebook","image","twitter"]:
+                item.pop(i,"")
+            
+            yield item
