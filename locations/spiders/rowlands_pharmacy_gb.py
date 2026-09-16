41d449227aa056a029d1e18dadf3a140b9bbbe1c
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-09-16 11:20:26 +0530

    fix spider

--- locations/spiders/rowlands_pharmacy_gb.py
+++ locations/spiders/rowlands_pharmacy_gb.py
@@ -1,17 +1,32 @@
-from locations.storefinders.uberall import UberallSpider
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.categories import Categories, apply_category
+from locations.hours import OpeningHours
+from locations.items import Feature
+from locations.json_blob_spider import JSONBlobSpider
+from locations.pipelines.address_clean_up import merge_address_lines
 
 
-class RowlandsPharmacyGBSpider(UberallSpider):
+class RowlandsPharmacyGBSpider(JSONBlobSpider):
     name = "rowlands_pharmacy_gb"
     item_attributes = {"brand": "Rowlands Pharmacy", "brand_wikidata": "Q62663235"}
-    key = "bwzEvjRHRFzJjkeRduLJgHcDYens3x"
+    start_urls = ["https://shop-services.rowlandspharmacy.co.uk/pharmacy-tools/api/locations"]
+    locations_key = "locations"
 
-    def post_process_item(self, item, response, location):
-        if not location["ref"]:
-            return None
+    def pre_process_data(self, feature: dict) -> None:
+        address = feature["address"]
+        address["street_address"] = merge_address_lines([address.pop("addressLine1"), address.pop("addressLine2")])
 
-        item["website"] = "https://rowlandspharmacy.co.uk/find-local-pharmacy#!/l/{}".format(
-            "/".join([item["city"], item["street_address"], location["ref"]]).lower().replace(" ", "-")
-        )
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["opening_hours"] = OpeningHours()
+        for day, intervals in feature["openIntervals"].items():
+            if not intervals["openIntervals"]:
+                item["opening_hours"].set_closed(day)
+            for interval in intervals["openIntervals"]:
+                item["opening_hours"].add_range(day, interval["start"], interval["end"])
+
+        apply_category(Categories.PHARMACY, item)
 
         yield item
