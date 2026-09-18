1a6aea47d7ccf10eb81f20b23fc419e83e15cb8d
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-18 09:52:05 +0200

    [square_habitat_fr] Add Spider (614 locations)

---
+++ locations/spiders/square_habitat_fr.py
@@ -0,0 +1,24 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class SquareHabitatFrSpider(SitemapSpider, StructuredDataSpider):
+    name = "square_habitat_fr"
+    item_attributes = {
+        "brand": "Square Habitat",
+        "brand_wikidata": "Q64027038",
+    }
+    sitemap_urls = ["https://www.squarehabitat.fr/sitemap-agences-immo.xml"]
+    sitemap_rules = [
+        (r"/agence/[^/]+", "parse"),
+    ]
+    # wanted_types = ["GroceryStore"]
+    time_format="%H:%M:%S"
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        apply_category(Categories.OFFICE_ESTATE_AGENT, item)
+        item["branch"] = item.pop("name", "").removesuffix(" Square Habitat")
+
+        yield item
