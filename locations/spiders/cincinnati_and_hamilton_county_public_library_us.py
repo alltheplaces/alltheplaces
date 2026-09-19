9343d057b708214b063f42b9388aec89b78ae5fa
Matt Whilden <matthew.whilden+osm@gmail.com>
2026-09-19 13:30:10 -0700

    Add cincinnati_and_hamilton_county_public_library_us spider (40 locations)

---
+++ locations/spiders/cincinnati_and_hamilton_county_public_library_us.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.biblio_commons import BiblioCommonsSpider
+
+
+class CincinnatiAndHamiltonCountyPublicLibraryUSSpider(BiblioCommonsSpider):
+    name = "cincinnati_and_hamilton_county_public_library_us"
+    item_attributes = {
+        "operator": "Cincinnati & Hamilton County Public Library",
+        "operator_wikidata": "Q5492644",
+    }
+    library_id = "cincinnatilibrary"
+
+    def post_process_item(self, item: Feature, response: TextResponse, location: dict, **kwargs) -> Iterable[Feature]:
+        if not location.get("hours") and "temporary" in (location.get("hoursNote") or ""):
+            # e.g. Avondale, whose listed address is a temporary site that
+            # has stopped service while the branch building is renovated.
+            return
+
+        if item["name"].endswith(" Branch Library"):
+            item["branch"] = item["name"].removesuffix(" Branch Library")
+        apply_category(Categories.LIBRARY, item)
+
+        yield item
