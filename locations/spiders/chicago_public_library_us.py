7c950eb0356d98a0cc49b05bd303033878dfa98d
Matt Whilden <118567155+watmildon@users.noreply.github.com>
2026-09-21 04:19:08 -0700

    [Chicago Public Library US] Add spider (82 locations) (#18951)
    Co-authored-by: Cj Malone <CjMalone@mail.com>

---
+++ locations/spiders/chicago_public_library_us.py
@@ -0,0 +1,34 @@
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.biblio_commons import BiblioCommonsSpider
+
+# The API lists these two branches surname first with the neighbourhood
+# appended; their own branch pages call them "Richard J. Daley Branch" and
+# "Richard M. Daley Branch".
+INVERTED_BRANCH_NAMES = {
+    "Daley, Richard J.-Bridgeport": "Richard J. Daley",
+    "Daley, Richard M.-W Humboldt": "Richard M. Daley",
+}
+
+
+class ChicagoPublicLibraryUSSpider(BiblioCommonsSpider):
+    name = "chicago_public_library_us"
+    item_attributes = {"operator": "Chicago Public Library", "operator_wikidata": "Q1060421"}
+    library_id = "chipublib"
+
+    def post_process_item(self, item: Feature, response: TextResponse, location: dict, **kwargs) -> Iterable[Feature]:
+        branch = item.pop("name")
+        if branch == "Harold Washington Library Center":
+            item["name"] = branch
+        elif branch.endswith(" Regional"):
+            item["name"] = "{} Library".format(branch)
+        else:
+            item["name"] = "{} Branch Library".format(INVERTED_BRANCH_NAMES.get(branch, branch))
+
+        apply_category(Categories.LIBRARY, item)
+
+        yield item
