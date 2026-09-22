1abeab8aa46f991c3df527d81b80e530f62b49d5
IceBurger <79398013+sharmaanurag95@users.noreply.github.com>
2026-09-22 19:43:46 +0530

    [Brow Art 23 US] Fix spider (#19012)

--- locations/spiders/brow_art_23_us.py
+++ locations/spiders/brow_art_23_us.py
@@ -1,8 +1,23 @@
-from locations.storefinders.yith import YithSpider
+from typing import Iterable
+
+from scrapy.http import TextResponse
+
+from locations.categories import Categories, apply_category
+from locations.items import Feature
+from locations.storefinders.agile_store_locator import AgileStoreLocatorSpider
 
 
-class BrowArt23USSpider(YithSpider):
+class BrowArt23USSpider(AgileStoreLocatorSpider):
     name = "brow_art_23_us"
     item_attributes = {"brand": "Brow Art 23", "brand_wikidata": "Q115675881"}
-    allowed_domains = ["browart23.com"]
-    requires_proxy = True
+    allowed_domains = ["www.browart23.com"]
+
+    def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removesuffix(" (Franchise)")
+        item["addr_full"] = item.pop("street_address")
+        # Every store has placeholder "0" hours for all days, not real closures
+        item.pop("opening_hours")
+        item.pop("website")
+        item.pop("email")
+        apply_category(Categories.SHOP_BEAUTY, item)
+        yield item
