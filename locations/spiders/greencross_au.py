000168fe42afb5f024353e7f3d0941f9b15ff67b
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-09-23 09:21:52 +0530

    code updated to fix spider

--- locations/spiders/greencross_au.py
+++ locations/spiders/greencross_au.py
@@ -1,52 +1,26 @@
-from typing import Any
+from typing import Iterable
 
-from scrapy import Selector, Spider
-from scrapy.http import Response
+from scrapy.http import TextResponse
+from scrapy.spiders import SitemapSpider
 
-from locations.hours import OpeningHours
 from locations.items import Feature
-from locations.pipelines.address_clean_up import clean_address
+from locations.structured_data_spider import StructuredDataSpider
 
 
-class GreencrossAUSpider(Spider):
+class GreencrossAUSpider(SitemapSpider, StructuredDataSpider):
     name = "greencross_au"
     allowed_domains = ["www.petbarn.com.au"]
-    start_urls = ["https://www.petbarn.com.au/store-finder/index/dataAjax/?types=pb%2Cgx%2Ccf"]
+    sitemap_urls = ["https://www.petbarn.com.au/media/sitemap/au/sitemap.xml"]
+    sitemap_rules = [("/stores/", "parse_sd")]
+    wanted_types = ["PetStore"]
 
-    def parse(self, response: Response, **kwargs: Any) -> Any:
-        for location in response.json():
-            properties = {
-                "ref": location["i"],
-                "name": location["n"],
-                "lat": location["l"],
-                "lon": location["g"],
-                "street_address": clean_address(location["a"][0 : len(location["a"]) - 3]),
-                "city": location["a"][-3],
-                "state": location["a"][-2],
-                "postcode": location["a"][-1],
-                "email": location["e"],
-                "phone": location["p"],
-                "website": response.urljoin(location["u"]),
-            }
-            if "Greencross Vets" in properties["name"]:
-                properties["brand"] = "Greencross Vets"
-                properties["brand_wikidata"] = "Q41179992"
-                properties["branch"] = properties.pop("name").removeprefix("Greencross Vets ")
-            elif "City Farmers" in properties["name"]:
-                properties["brand"] = "City Farmers"
-                properties["brand_wikidata"] = "Q117357785"
-                properties["branch"] = properties.pop("name").removeprefix("City Farmers ")
-            else:
-                properties["brand"] = "Petbarn"
-                properties["brand_wikidata"] = "Q104746468"
-                properties["branch"] = properties.pop("name")
-
-            properties["opening_hours"] = OpeningHours()
-            hours_raw = [s for s in Selector(text=location["oh"]).xpath("//text()").getall() if s.strip()]
-            day_names = hours_raw[: len(hours_raw) // 2]
-            day_times = hours_raw[len(hours_raw) // 2 :]
-            hours_string = ""
-            for index, day_name in enumerate(day_names):
-                hours_string = f"{hours_string} {day_name}: {day_times[index]}"
-            properties["opening_hours"].add_ranges_from_string(hours_string)
-            yield Feature(**properties)
+    def post_process_item(self, item: Feature, response: TextResponse, ld_data: dict, **kwargs) -> Iterable[Feature]:
+        if "Greencross Vets" in item["name"]:
+            item["brand"] = "Greencross Vets"
+            item["brand_wikidata"] = "Q41179992"
+            item["branch"] = item.pop("name").removeprefix("Greencross Vets ")
+        else:
+            item["brand"] = "Petbarn"
+            item["brand_wikidata"] = "Q104746468"
+            item["branch"] = item.pop("name").removeprefix("Petbarn ").removeprefix("Animal Emergency Centre ")
+        yield item
