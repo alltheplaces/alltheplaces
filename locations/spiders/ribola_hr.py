da9736be255aea9c249f84955ad031783d65caaf
Mohit Sharma <51069345+mohit-sharma2194@users.noreply.github.com>
2026-09-23 15:05:20 +0530

    revoke proxy changes and updated url

--- locations/spiders/ribola_hr.py
+++ locations/spiders/ribola_hr.py
@@ -14,11 +14,11 @@ class RibolaHRSpider(JSONBlobSpider):
         "brand": "Ribola",
         "brand_wikidata": "Q65124070",
     }
-    start_urls = [
-        "https://ribola.hr/wp-admin/admin-ajax.php?action=asl_load_stores&nonce=50703d0778&load_all=1&layout=1"
-    ]
+    start_urls = ["https://ribola.hr/wp-admin/admin-ajax.php?action=asl_load_stores"]
+    requires_proxy = "HR"
 
     def post_process_item(self, item: Feature, response: TextResponse, feature: dict) -> Iterable[Feature]:
+        item["branch"] = item.pop("name").removeprefix("Ribola ")
         item["opening_hours"] = OpeningHours()
         item["opening_hours"].add_ranges_from_string(feature["open_hours"])
         apply_category(Categories.SHOP_SUPERMARKET, item)
