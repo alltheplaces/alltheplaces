2eccffcfb85495cc78e9b2e3f342cb6943de57a4
sharmaanurag95 <sharma.anurag1995@gmail.com>
2026-09-24 14:55:10 +0530

    fix spider

--- locations/spiders/landmark_cinemas_ca.py
+++ locations/spiders/landmark_cinemas_ca.py
@@ -1,18 +1,24 @@
 import json
+from typing import Any, Iterable
 
-from scrapy import Spider
-from scrapy.http import JsonRequest
+from scrapy.http import JsonRequest, Response
 
+from locations.categories import Categories, apply_category
 from locations.dict_parser import DictParser
+from locations.items import Feature
 from locations.pipelines.address_clean_up import merge_address_lines
+from locations.playwright_spider import PlaywrightSpider
+from locations.settings import DEFAULT_PLAYWRIGHT_SETTINGS
+from locations.user_agents import BROWSER_DEFAULT
 
 
-class LandmarkCinemasCASpider(Spider):
+class LandmarkCinemasCASpider(PlaywrightSpider):
     name = "landmark_cinemas_ca"
     item_attributes = {"brand": "Landmark Cinemas", "brand_wikidata": "Q6484762"}
     start_urls = ["https://www.landmarkcinemas.com/"]
+    custom_settings = DEFAULT_PLAYWRIGHT_SETTINGS | {"USER_AGENT": BROWSER_DEFAULT}
 
-    def parse(self, response, **kwargs):
+    def parse(self, response: Response, **kwargs: Any) -> Iterable[JsonRequest]:
         for ref in response.xpath('//select[@id="preflocationHomepage"]/option/@value').getall():
             if ref != "-1":
                 yield JsonRequest(
@@ -20,12 +26,13 @@ class LandmarkCinemasCASpider(Spider):
                     callback=self.parse_location,
                 )
 
-    def parse_location(self, response, **kwargs):
+    def parse_location(self, response: Response, **kwargs: Any) -> Iterable[Feature]:
         location = json.loads(response.json())
         item = DictParser.parse(location)
-        item["name"] = location["CinemaName"]
+        item["branch"] = location["CinemaName"]
         item["website"] = item["ref"] = f'https://www.landmarkcinemas.com{location["CinemaInfoUrl"]}'
         item["image"] = f'https://www.landmarkcinemas.com{location["Image"]}'
         item["street_address"] = merge_address_lines([location["Address1"], location["Address2"]])
+        apply_category(Categories.CINEMA, item)
 
         yield item
