c8af23328ad717fbf811a61dd504e85874f35908
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-25 16:42:46 +0200

    [Body Minute FR] Add spider (363 locations)

---
+++ locations/spiders/body_minute_fr.py
@@ -0,0 +1,23 @@
+from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+from locations.categories import Categories, apply_category
+
+
+class BodyMinuteFRSpider(SitemapSpider, StructuredDataSpider):
+    name = "body_minute_fr"
+    item_attributes = {
+        "brand": "Body Minute",
+        "brand_wikidata": "Q104972220",
+    }
+    sitemap_urls = ["https://bodyminute.com/robots.txt"]
+    sitemap_rules = [
+        (r"/instituts/[^/]+/", "parse"),
+    ]
+
+    def post_process_item(self, item, response, ld_data, **kwargs):
+        if item["name"].startswith("Institut beauté et épilation sans RDV à"):
+            apply_category(Categories.SHOP_BEAUTY, item)
+            item["branch"] = (item.pop("name","") or "").removeprefix("Institut beauté et épilation sans RDV à ")
+            item["image"] = None
+            yield item
