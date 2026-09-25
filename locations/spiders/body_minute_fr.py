0df9c67325d0fcec63fe8362f801edee6eaf8ed9
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-09-25 17:03:50 +0200

    fixes

--- locations/spiders/body_minute_fr.py
+++ locations/spiders/body_minute_fr.py
@@ -11,13 +11,15 @@ class BodyMinuteFRSpider(SitemapSpider, StructuredDataSpider):
         "brand_wikidata": "Q104972220",
     }
     sitemap_urls = ["https://bodyminute.com/robots.txt"]
+    allowed_domains = ["bodyminute.com"]
     sitemap_rules = [
         (r"/instituts/[^/]+/$", "parse"),
     ]
+
+    search_for_image = False
 
     def post_process_item(self, item, response, ld_data, **kwargs):
         if item["name"].startswith("Institut beauté et épilation sans RDV à"):
             apply_category(Categories.SHOP_BEAUTY, item)
             item["branch"] = (item.pop("name","") or "").removeprefix("Institut beauté et épilation sans RDV à ")
-            item["image"] = None
             yield item
