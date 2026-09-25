b43cb74b86253b1aecb2b207eac44b2dfebddf9b
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-09-25 22:41:42 +0900

    fix removeprefix

--- locations/spiders/hotto_motto_jp.py
+++ locations/spiders/hotto_motto_jp.py
@@ -15,7 +15,7 @@ class HottoMottoJPSpider(MapionSpider):
 
     def post_process_item(self, item: Feature, data: dict, response: Response) -> Iterable[Feature]:
         item["name"] = None
-        item["branch"] = data.get("name").removeprefix("ほっともっと ")
+        item["branch"] = data.get("name").removeprefix("ほっともっと ").removeprefix("ほっともっと")
         apply_category(Categories.FAST_FOOD, item)
 
         yield item
