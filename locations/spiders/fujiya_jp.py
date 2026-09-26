29abde520f845e4f42ddfd2bd13e1ecca9e68960
j9d3it <26203575+j9d3it@users.noreply.github.com>
2026-09-26 10:32:12 +0900

    [fujiya_jp] add spider (733 locations)
    {'atp/brand/不二家': 733,
     'atp/brand_wikidata/Q858452': 733,
     'atp/category/amenity/restaurant': 37,
     'atp/category/multiple': 37,
     'atp/category/shop/confectionery': 696,
     'atp/country/JP': 733,
     'atp/field/country/from_spider_name': 733,
     'atp/field/email/missing': 733,
     'atp/field/housenumber/missing': 733,
     'atp/field/name/missing': 37,
     'atp/field/operator/missing': 733,
     'atp/field/operator_wikidata/missing': 733,
     'atp/field/state/missing': 733,
     'atp/field/street/missing': 733,
     'atp/field/twitter/missing': 733,
     'atp/field/unit/missing': 733,
     'atp/item_scraped_host_count/shop.fujiya-peko.co.jp': 733,
     'atp/lineage': 'S_?',
     'atp/nsi/category_unknown': 37,
     'atp/nsi/match_failed': 37,
     'atp/nsi/match_perfect': 696,
     'downloader/request_bytes': 334108,
     'downloader/request_count': 773,
     'downloader/request_method_count/GET': 773,
     'downloader/response_bytes': 47570101,
     'downloader/response_count': 773,
     'downloader/response_status_count/200': 771,
     'downloader/response_status_count/403': 1,
     'downloader/response_status_count/404': 1,
     'elapsed_time_seconds': 939.8280000000004,
     'feedexport/success_count/FileFeedStorage': 1,
     'finish_reason': 'finished',
     'finish_time': datetime.datetime(2026, 9, 26, 1, 19, 53, 824029, tzinfo=datetime.timezone.utc),
     'httperror/response_ignored_count': 1,
     'httperror/response_ignored_status_count/404': 1,
     'item_scraped_count': 733,
     'items_per_minute': 46.8370607028754,
     'log_count/DEBUG': 1506,
     'log_count/ERROR': 1,
     'log_count/INFO': 20,
     'request_depth_max': 37,
     'response_received_count': 773,
     'responses_per_minute': 49.39297124600639,
     'robotstxt/request_count': 1,
     'robotstxt/response_count': 1,
     'robotstxt/response_status_count/403': 1,
     'scheduler/dequeued': 772,
     'scheduler/dequeued/memory': 772,
     'scheduler/enqueued': 772,
     'scheduler/enqueued/memory': 772,
     'spider_exceptions/ValueError': 1,
     'spider_exceptions/count': 1,
     'start_time': datetime.datetime(2026, 9, 26, 1, 4, 13, 989478, tzinfo=datetime.timezone.utc)}

---
+++ locations/spiders/fujiya_jp.py
@@ -0,0 +1,29 @@
+from typing import Iterable
+
+from scrapy.http import Response
+
+from locations.categories import Categories, apply_category
+from locations.hours import DAYS, OpeningHours
+from locations.items import Feature
+from locations.storefinders.mapion import MapionSpider
+
+
+class FujiyaJPSpider(MapionSpider):
+    name = "fujiya_jp"
+    item_attributes = {"brand": "不二家", "brand_wikidata": "Q858452"}
+    allowed_domains = ["shop.fujiya-peko.co.jp"]
+    feature_url_template = "https://shop.fujiya-peko.co.jp/b/fujiya/attr/?t=attr_con&start={}"
+
+    def post_process_item(self, item: Feature, data: dict, response: Response) -> Iterable[Feature]:
+        item["name"] = None
+        item["branch"] = data.get("name").removeprefix("ペコちゃんmilkyドーナツ").removeprefix("milky70 since1951").removeprefix("ペコちゃんmilkyタイム").lstrip()
+        item["phone"] = f"+81 {data.get('tel1')}-{data.get('tel2')}-{data.get('tel3')}"
+        if (open_time := f"{data.get('openhour1')}:{data.get('openmin1')}") and (close_time := f"{data.get('closehour1')}:{data.get('closemin1')}"):
+            oh = OpeningHours()
+            oh.add_days_range(DAYS, open_time, close_time)
+            item["opening_hours"] = oh
+        if data.get("flag4"): # flag4 is for sit-down restaurants. they sell confectionery as well
+            apply_category(Categories.RESTAURANT, item)
+        apply_category(Categories.SHOP_CONFECTIONERY, item)
+
+        yield item
